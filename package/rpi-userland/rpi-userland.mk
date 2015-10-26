41ed413b9629d78cbbc7f41fccb2562aa8f16ed0
albertd <albert@dahan.nl>
2015-10-26 20:52:52 +0100

    [rpi-userland] bump version

--- package/rpi-userland/rpi-userland.mk
+++ package/rpi-userland/rpi-userland.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_USERLAND_VERSION = b864a841e5a459a66a890c22b3a34127cd226238
+RPI_USERLAND_VERSION = 40e377862410371a9962db79b81fd4f0f266430a
 RPI_USERLAND_SITE = $(call github,raspberrypi,userland,$(RPI_USERLAND_VERSION))
 RPI_USERLAND_LICENSE = BSD-3c
 RPI_USERLAND_LICENSE_FILES = LICENCE
