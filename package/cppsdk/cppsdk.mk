2a700cd8818a0405910216f63cfa1239858572cc
albertd <albert@dahan.nl>
2015-10-28 12:36:51 +0100

    [metrological] adding proprietary packages

---
+++ package/cppsdk/cppsdk.mk
@@ -0,0 +1,38 @@
+################################################################################
+#
+# cppsdk
+#
+################################################################################
+CPPSDK_VERSION = 01bb1175b7cba3363b1dd9ddde20e29e62f49b0d
+CPPSDK_SITE_METHOD = git
+CPPSDK_SITE = git@github.com:Metrological/cppsdk.git
+CPPSDK_INSTALL_STAGING = YES
+
+ifeq ($(BR2_ENABLE_DEBUG),y)
+CPPSDK_CONF_OPTS += -DCPPSDK_DEBUG=ON
+else ifeq ($(BR2_PACKAGE_CPPSDK_DEBUG),y)
+CPPSDK_CONF_OPTS += -DCPPSDK_DEBUG=ON
+endif
+
+ifeq ($(BR2_PACKAGE_CPPSDK_GENERICS),y)
+CPPSDK_CONF_OPTS += -DCPPSDK_GENERICS=ON
+endif
+
+ifeq ($(BR2_PACKAGE_CPPSDK_CRYPTALGO),y)
+CPPSDK_CONF_OPTS += -DCPPSDK_CRYPTALGO=ON
+endif
+
+ifeq ($(BR2_PACKAGE_CPPSDK_WEBSOCKET),y)
+CPPSDK_CONF_OPTS += -DCPPSDK_WEBSOCKET=ON
+CPPSDK_DEPENDENCIES += zlib
+endif
+
+ifeq ($(BR2_PACKAGE_CPPSDK_TRACING),y)
+CPPSDK_CONF_OPTS += -DCPPSDK_TRACING=ON
+endif
+
+ifeq ($(BR2_PACKAGE_CPPSDK_DEVICES),y)
+CPPSDK_CONF_OPTS += -DCPPSDK_DEVICES=ON
+endif
+
+$(eval $(cmake-package))
---
+++ package/dxdrm/dxdrm.mk
@@ -0,0 +1,57 @@
+################################################################################
+#
+# dxdrm
+#
+################################################################################
+
+DXDRM_VERSION = 0a0e23113a5517b84e3e4c81f99f29a04ab99440
+DXDRM_SITE_METHOD = git
+DXDRM_SITE = git@github.com:Metrological/dxdrm.git
+DXDRM_LICENSE = PROPRIETARY
+DXDRM_REDISTRIBUTE = NO
+
+DXDRM_INSTALL_STAGING = YES
+
+DXDRM_DEPENDENCIES += libcurl
+
+ifeq ($(BR2_PACKAGE_DXDRM_INTERNAL), y)
+DXDRM_LOCATOR = internal
+else
+DXDRM_LOCATOR = external
+DXDRM_DEPENDENCIES += openssl
+DXDRM_DEPENDENCIES += cppsdk
+endif
+
+define DXDRM_INSTALL_STAGING_CMDS
+	$(INSTALL) -m 755 $(@D)/$(DXDRM_LOCATOR)/$(call qstrip,$(BR2_ARCH))/release/libDxDrm.so $(STAGING_DIR)/usr/lib/libDxDrm.so
+	$(INSTALL) -m 755 $(@D)/$(DXDRM_LOCATOR)/$(call qstrip,$(BR2_ARCH))/release/libTrusted.so $(STAGING_DIR)/usr/lib/libTrusted.so
+	$(INSTALL) -d -m 755 $(STAGING_DIR)/usr/include/dxdrm
+	$(INSTALL) -m 644 $(@D)/$(DXDRM_LOCATOR)/include/*.h $(STAGING_DIR)/usr/include/dxdrm
+	$(INSTALL) -m 644 $(@D)/external/dxdrm.pc $(STAGING_DIR)/usr/lib/pkgconfig
+	
+	if [ "x$(BR2_PACKAGE_DXDRM_EXTERNAL)" = "xy" ] ; then \
+		$(INSTALL) -m 755 $(@D)/$(DXDRM_LOCATOR)/$(call qstrip,$(BR2_ARCH))/release/libprovision.so $(STAGING_DIR)/usr/lib/libprovision.so; \
+		$(INSTALL) -m 755 $(@D)/$(DXDRM_LOCATOR)/$(call qstrip,$(BR2_ARCH))/release/libprovisionproxy.so $(STAGING_DIR)/usr/lib/libprovisionproxy.so; \
+		$(INSTALL) -d -m 755 $(STAGING_DIR)/usr/include/rpc; \
+		$(INSTALL) -m 644 $(@D)/$(DXDRM_LOCATOR)/include/rpc/*.h $(STAGING_DIR)/usr/include/rpc; \
+		$(INSTALL) -d -m 755 $(STAGING_DIR)/usr/include/provision; \
+		$(INSTALL) -m 644 $(@D)/$(DXDRM_LOCATOR)/include/provision/*.h $(STAGING_DIR)/usr/include/provision; \
+		$(INSTALL) -m 644 $(@D)/external/provision.pc $(STAGING_DIR)/usr/lib/pkgconfig; \
+	fi
+endef
+
+define DXDRM_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 755 $(@D)/$(DXDRM_LOCATOR)/$(call qstrip,$(BR2_ARCH))/release/libDxDrm.so $(TARGET_DIR)/usr/lib/libDxDrm.so
+	$(INSTALL) -m 755 $(@D)/$(DXDRM_LOCATOR)/$(call qstrip,$(BR2_ARCH))/release/libTrusted.so $(TARGET_DIR)/usr/lib/libTrusted.so
+	$(INSTALL) -d -m 755 $(TARGET_DIR)/etc/dxdrm
+	$(INSTALL) -m 644 $(@D)/$(DXDRM_LOCATOR)/dxdrm.config $(TARGET_DIR)/etc/dxdrm
+	if [ "x$(BR2_PACKAGE_DXDRM_INTERNAL)" = "xy" ]; then \
+		$(INSTALL) -m 644 $(@D)/$(DXDRM_LOCATOR)/credentials/* $(TARGET_DIR)/etc/dxdrm; \
+	fi
+	if [ "x$(BR2_PACKAGE_DXDRM_EXTERNAL)" = "xy" ]; then \
+		$(INSTALL) -m 755 $(@D)/$(DXDRM_LOCATOR)/$(call qstrip,$(BR2_ARCH))/release/libprovision.so $(TARGET_DIR)/usr/lib/libprovision.so; \
+		$(INSTALL) -m 755 $(@D)/$(DXDRM_LOCATOR)/$(call qstrip,$(BR2_ARCH))/release/libprovisionproxy.so $(TARGET_DIR)/usr/lib/libprovisionproxy.so; \
+	fi
+endef
+
+$(eval $(generic-package))
