4ec04a1e728d1ba22a336db2aa68edce38c71eeb
albertd <albert@dahan.nl>
2015-11-03 01:35:28 +0100

    [wpe-launcher] add missing config files

--- package/wpe-launcher/wpe-launcher.mk
+++ package/wpe-launcher/wpe-launcher.mk
@@ -10,6 +10,7 @@ WPE_LAUNCHER_SITE = $(call github,Metrological,wpe-launcher,$(WPE_LAUNCHER_VERSI
 WPE_LAUNCHER_DEPENDENCIES = wpe
 
 define WPE_LAUNCHER_AUTOSTART
+	$(INSTALL) -D -m 0644 package/wpe-launcher/wpe.{txt,conf} $(BINARIES_DIR)/
 	$(INSTALL) -D -m 0755 package/wpe-launcher/wpe $(TARGET_DIR)/usr/bin
 	$(INSTALL) -D -m 0755 package/wpe-launcher/S90wpe $(TARGET_DIR)/etc/init.d
 	if [ -f package/wpe-launcher/wpe-update ]; then \
