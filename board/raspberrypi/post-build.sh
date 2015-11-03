9832dca19afec4a446539d570869cd71ee78abee
albertd <albert@dahan.nl>
2015-11-03 01:36:05 +0100

    [rpi] custom config and cmdline, update defconfigs

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+BOARD_DIR="$(dirname $0)"
+
 echo "Updating the zImage to load the device tree"
 ${HOST_DIR}/usr/bin/mkknlimg ${BINARIES_DIR}/zImage ${BINARIES_DIR}/zImage-dt
+
+echo "Updating the config and cmdline files"
+cp -af ${BOARD_DIR}/config.txt ${BINARIES_DIR}/rpi-firmware/
+cp -af ${BOARD_DIR}/cmdline.txt ${BINARIES_DIR}/rpi-firmware/
