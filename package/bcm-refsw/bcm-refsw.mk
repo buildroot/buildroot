1d05f2c4f8979edb28250b4fb2ed20b4f148d213
albertd <albert@dahan.nl>
2015-11-09 23:28:12 +0100

    [bcm-refsw] correctly build vc5 and v3d options

--- package/bcm-refsw/bcm-refsw.mk
+++ package/bcm-refsw/bcm-refsw.mk
@@ -22,7 +22,7 @@ BCM_REFSW_PLATFORM_VC = vc5
 else ifeq ($(BR2_mipsel),y)
 BCM_REFSW_PLATFORM = 97429
 BCM_REFSW_PLATFORM_REV = B0
-BCM_REFSW_PLATFORM_VC = vc4
+BCM_REFSW_PLATFORM_VC = v3d
 endif
 
 BCM_REFSW_CONF_OPTS += \
@@ -78,6 +78,13 @@ define BCM_REFSW_INSTALL_LIBS
 	cd $1/usr/lib && ln -sf libv3ddriver.so libEGL.so && ln -sf libv3ddriver.so libGLESv2.so
 endef
 
+ifeq ($(BCM_REFSW_PLATFORM_VC),vc5)
+define BCM_REFSW_INSTALL_EXTRA
+	$(INSTALL) -D -m 755 package/bcm-refsw/S11wakeup $1/etc/init.d/S11wakeup
+	$(INSTALL) -m 644 -D $(BCM_REWSW_BIN)/wakeup_drv.ko $1/lib/modules/wakeup_drv.ko
+endef
+endif
+
 define BCM_REFSW_BUILD_CMDS
 	$(BCM_REFSW_BUILD_NEXUS)
 	$(BCM_REFSW_BUILD_VCX)
@@ -104,9 +111,8 @@ endef
 define BCM_REFSW_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 750 -D $(BCM_REWSW_BIN)/nexus $(TARGET_DIR)/sbin/nexus
 	$(INSTALL) -m 644 -D $(BCM_REWSW_BIN)/nexus.ko $(TARGET_DIR)/lib/modules/nexus.ko
-    $(INSTALL) -m 644 -D $(BCM_REWSW_BIN)/wakeup_drv.ko $(TARGET_DIR)/lib/modules/wakeup_drv.ko
 	$(INSTALL) -D -m 755 package/bcm-refsw/S11nexus $(TARGET_DIR)/etc/init.d/S11nexus
-	$(INSTALL) -D -m 755 package/bcm-refsw/S11wakeup $(TARGET_DIR)/etc/init.d/S11wakeup
+	$(call BCM_REFSW_INSTALL_EXTRA,$(TARGET_DIR))
 	$(call BCM_REFSW_INSTALL_LIBS,$(TARGET_DIR))
 endef
 
