4feeefe6fc20c8efc3398e58c04e8bc189b57ec2
Abi Hafshin Alfarouq <abi@hafs.in>
2015-11-22 15:28:04 +0700

    fix libfprint site

--- package/libfprint/libfprint.mk
+++ package/libfprint/libfprint.mk
@@ -6,7 +6,7 @@
 
 LIBFPRINT_VERSION = 0.5.1
 LIBFPRINT_SOURCE = libfprint-$(LIBFPRINT_VERSION).tar.xz
-LIBFPRINT_SITE = http://people.freedesktop.org/~hadess/
+LIBFPRINT_SITE = http://people.freedesktop.org/~hadess
 LIBFPRINT_LICENSE = LGPLv2.1+
 LIBFPRINT_LICENSE_FILES = COPYING
 LIBFPRINT_INSTALL_STAGING = YES
