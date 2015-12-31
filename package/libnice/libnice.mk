08146628ff05ec7b93c2c02574371a53cd19fcfd
albertd <albert@dahan.nl>
2015-12-31 10:25:49 +0100

    [libnice] support gstreamer plugins

--- package/libnice/libnice.mk
+++ package/libnice/libnice.mk
@@ -11,8 +11,17 @@ LIBNICE_LICENSE_FILES = COPYING COPYING.MPL COPYING.LGPL
 LIBNICE_DEPENDENCIES = libglib2 host-pkgconf
 LIBNICE_INSTALL_STAGING = YES
 LIBNICE_AUTORECONF = YES
-LIBNICE_CONF_OPTS = \
-	--without-gstreamer \
-	--without-gstreamer-0.10 \
+
+ifeq ($(BR2_PACKAGE_GSTREAMER1),)
+LIBNICE_CONF_OPTS += --without-gstreamer
+else
+LIBNICE_DEPENDENCIES += gstreamer1 gst1-plugins-base
+endif
+
+ifeq ($(BR2_PACKAGE_GSTREAMER),)
+LIBNICE_CONF_OPTS += --without-gstreamer-0.10
+else
+LIBNICE_DEPENDENCIES += gstreamer gst-plugins-base
+endif
 
 $(eval $(autotools-package))
