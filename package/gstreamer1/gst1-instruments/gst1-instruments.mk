779f830290cf407e6102ce2db7dfb85e5c57f9d6
Philippe Normand <philn@igalia.com>
2016-01-05 10:23:11 +0100

    [gst1-instruments] bump

--- package/gstreamer1/gst1-instruments/gst1-instruments.mk
+++ package/gstreamer1/gst1-instruments/gst1-instruments.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GST1_INSTRUMENTS_VERSION = 624244f76d1496ab1e442546e6c3997bf094608d
+GST1_INSTRUMENTS_VERSION = 2c69d0f685b2276493d0bb05ca83bf03a18233dd
 GST1_INSTRUMENTS_SITE = $(call github,kirushyk,gst-instruments,$(GST1_INSTRUMENTS_VERSION))
 GST1_INSTRUMENTS_LICENSE = LGPLv2.1+
 GST1_INSTRUMENTS_LICENSE_FILES = COPYING
