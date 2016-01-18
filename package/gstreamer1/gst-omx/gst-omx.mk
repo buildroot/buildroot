b915b55120fdb994fd42b218a9fd32f3ee5dc667
albertd <albert@dahan.nl>
2016-01-18 16:52:01 +0100

    [gst-omx] fix order of dependencies

--- package/gstreamer1/gst-omx/gst-omx.mk
+++ package/gstreamer1/gst-omx/gst-omx.mk
@@ -23,6 +23,8 @@ endif
 GST_OMX_LICENSE = LGPLv2.1
 GST_OMX_LICENSE_FILES = COPYING
 
+GST_OMX_DEPENDENCIES = gstreamer1 gst1-plugins-base libopenmax
+
 ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
 GST_OMX_CONF_OPTS = \
 	--with-omx-target=rpi
@@ -47,8 +49,6 @@ endif
 
 GST_OMX_CONF_OPTS += \
 	--disable-examples
-
-GST_OMX_DEPENDENCIES += gstreamer1 gst1-plugins-base libopenmax
 
 # adjust library paths to where buildroot installs them
 define GST_OMX_FIXUP_CONFIG_PATHS
