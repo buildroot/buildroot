600bf78e0b7308dea186f507a8325b0ab6381663
albertd <albert@dahan.nl>
2016-01-21 20:05:42 +0100

    [wpe] bump to latest version

--- package/wpe/wpe.mk
+++ package/wpe/wpe.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPE_VERSION = d23cb18a7acb1a2f053834e315de0ebce928cab5
+WPE_VERSION = ba91fe8ef5819bd2616efe01904b2201c6e4f478
 WPE_SITE = $(call github,Metrological,WebKitForWayland,$(WPE_VERSION))
 
 WPE_INSTALL_STAGING = YES
