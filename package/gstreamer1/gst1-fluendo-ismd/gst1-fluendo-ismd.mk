d68f4028644271c5299a4a5bf094af964f01bfde
bramoosterhuis <bram.oosterhuis.nl@gmail.com>
2016-02-05 20:42:34 +0100

    adding intelce gstreamer support

---
+++ package/gstreamer1/gst1-fluendo-ismd/gst1-fluendo-ismd.mk
@@ -0,0 +1,46 @@
+################################################################################
+#
+# gst1-fluendo-ismd
+#
+################################################################################
+
+GST1_FLUENDO_ISMD_VERSION = 3517ede33a9418608686ec2d2c7da4703f077156
+GST1_FLUENDO_ISMD_SITE_METHOD = git
+GST1_FLUENDO_ISMD_SITE = git@github.com:Metrological/gst-fluendo-ismd.git
+
+GST1_FLUENDO_ISMD_LICENSE = LGPLv2.1
+GST1_FLUENDO_ISMD_LICENSE_FILES = COPYING
+
+GST1_FLUENDO_ISMD_POST_INSTALL_TARGET_HOOKS += GSTREAMER1_REMOVE_LA_FILES
+
+GST1_FLUENDO_ISMD_DEPENDENCIES = \
+    gstreamer1 \
+    gst1-plugins-base \
+    intelce-audio \
+    intelce-bufmon \
+    intelce-clock \
+    intelce-clock_recovery \
+    intelce-core \
+    intelce-demux \
+    intelce-display \
+    intelce-mux \
+    intelce-system_utils \
+    intelce-viddec \
+    intelce-videnc \
+    intelce-vidpproc \
+    intelce-vidrend \
+    intelce-vidsink \
+    zlib \
+
+GST1_FLUENDO_ISMD_AUTORECONF = YES
+
+GST1_FLUENDO_ISMD_MAKE_OPTS += -I${STAGING_DIR}/usr/include/intelce
+
+GST1_FLUENDO_ISMD_CONF_ENV = \
+	CFLAGS="$(TARGET_CFLAGS) \
+		-I$(STAGING_DIR)/usr/include/intelce \
+		-I$(STAGING_DIR)/usr/include/intelce/linux_user"
+
+GST1_FLUENDO_ISMD_CONF_OPTS = --disable-valgrind --disable-transcode --disable-examples
+
+$(eval $(autotools-package))
