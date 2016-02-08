8c6b453ed70d1d5a7f69bb183bdee422533d010a
albertd <albert@dahan.nl>
2016-02-08 11:18:01 +0100

    [wpe-launcher] bump to latest version

--- package/wpe-launcher/wpe-launcher.mk
+++ package/wpe-launcher/wpe-launcher.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPE_LAUNCHER_VERSION = 7e218aacc728e574e4d8f6df1c4b354d7d812451
+WPE_LAUNCHER_VERSION = 8f6d5b57cc1a36ef7a01db4f3a395b6e082203f4
 WPE_LAUNCHER_SITE = $(call github,Metrological,wpe-launcher,$(WPE_LAUNCHER_VERSION))
 
 WPE_LAUNCHER_DEPENDENCIES = wpe
