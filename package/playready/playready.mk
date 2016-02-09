2a675f9fbd626d147663a9d113ffb09da642bf39
albertd <albert@dahan.nl>
2016-02-09 18:29:18 +0100

    [playready] add storage path symlink

--- package/playready/playready.mk
+++ package/playready/playready.mk
@@ -22,20 +22,17 @@ define PLAYREADY_INSTALL_STAGING_PC
 		$(STAGING_DIR)/usr/lib/pkgconfig/playready.pc
 endef
 
-define PLAYREADY_INSTALL_TARGET_BGROUPCERT
+define PLAYREADY_INSTALL_TARGET_ETC_PLAYREADY
 	if [ -f package/playready/bgroupcert.dat ]; then \
 		$(INSTALL) -D -m 0644 package/playready/bgroupcert.dat $(TARGET_DIR)/etc/playready/; \
 	fi
-endef
-
-define PLAYREADY_INSTALL_TARGET_ZGPRIV
 	if [ -f package/playready/zgpriv.dat ]; then \
 		$(INSTALL) -D -m 0644 package/playready/zgpriv.dat $(TARGET_DIR)/etc/playready/; \
 	fi
+	ln -sf /tmp $(TARGET_DIR)/etc/playready/storage
 endef
 
 PLAYREADY_POST_INSTALL_STAGING_HOOKS += PLAYREADY_INSTALL_STAGING_PC
-PLAYREADY_POST_INSTALL_TARGET_HOOKS += PLAYREADY_INSTALL_TARGET_BGROUPCERT
-PLAYREADY_POST_INSTALL_TARGET_HOOKS += PLAYREADY_INSTALL_TARGET_ZGPRIV
+PLAYREADY_POST_INSTALL_TARGET_HOOKS += PLAYREADY_INSTALL_TARGET_ETC_PLAYREADY
 
 $(eval $(cmake-package))
