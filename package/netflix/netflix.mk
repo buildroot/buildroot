19db6e80ce492899ba3df4b705143614234cea0b
albertd <albert@dahan.nl>
2016-02-18 18:29:55 +0100

    [netflix] support for shared library

--- package/netflix/netflix.mk
+++ package/netflix/netflix.mk
@@ -16,15 +16,21 @@ NETFLIX_RESOURCE_LOC = $(call qstrip,${BR2_PACKAGE_NETFLIX_RESOURCE_LOCATION})
 NETFLIX_CONF_OPTS = \
 	-DBUILD_DPI_DIRECTORY=$(@D)/partner/dpi \
 	-DCMAKE_BUILD_TYPE=Release \
-	-DCMAKE_INSTALL_PREFIX=/netflix \
+	-DCMAKE_INSTALL_PREFIX=$(@D)/release \
 	-DBUILD_COMPILE_RESOURCES=1 \
 	-DNRDP_HAS_IPV6=0 \
 	-DBUILD_SHARED_LIBS=0 \
-	-DGIBBON_MODE=executable \
 	-DGIBBON_SCRIPT_JSC_DYNAMIC=1 \
 	-DGIBBON_SCRIPT_JSC_DEBUG=0 \
 	-DGIBBON_INPUT=devinput \
-	-DNRDP_TOOLS="all"
+	-DNRDP_TOOLS="manufSSgenerator"
+
+ifeq ($(BR2_PACKAGE_NETFLIX_LIB), y)
+NETFLIX_INSTALL_STAGING = YES
+NETFLIX_CONF_OPTS += -DGIBBON_MODE=shared
+else
+NETFLIX_CONF_OPTS += -DGIBBON_MODE=executable
+endif
 
 NETFLIX_CONF_ENV += \
 	TARGET_CROSS="$(GNU_TARGET_NAME)-"
@@ -94,6 +100,35 @@ endef
 
 NETFLIX_POST_EXTRACT_HOOKS += NETFLIX_FIX_CONFIG_XMLS
 
+ifeq ($(BR2_PACKAGE_NETFLIX_LIB),y)
+
+define NETFLIX_INSTALL_STAGING_CMDS
+	make -C $(@D)/netflix install
+	$(INSTALL) -m 755 $(@D)/netflix/src/platform/gibbon/libJavaScriptCore.so $(STAGING_DIR)/usr/lib
+	$(INSTALL) -m 755 $(@D)/netflix/src/platform/gibbon/libnetflix.so $(STAGING_DIR)/usr/lib
+	$(INSTALL) -D package/netflix/netflix.pc $(STAGING_DIR)/usr/lib/pkgconfig/netflix.pc
+	mkdir -p $(STAGING_DIR)/usr/include/netflix
+	cp -Rpf $(@D)/release/include/* $(STAGING_DIR)/usr/include/netflix/
+	mkdir -p $(STAGING_DIR)/usr/include/netflix
+	cp -Rpf $(@D)/netflix/src/platform/gibbon/*.h $(STAGING_DIR)/usr/include/netflix
+	cp -Rpf $(@D)/netflix/src/platform/gibbon/bridge/*.h $(STAGING_DIR)/usr/include/netflix
+	mkdir -p $(STAGING_DIR)/usr/include/netflix/gibbon
+	cp -Rpf $(@D)/netflix/src/platform/gibbon/include/gibbon/*.h $(STAGING_DIR)/usr/include/netflix/gibbon
+	$(SED) 's:^using std\:\:isnan;:\/\/using std\:\:isnan;:' \
+		-e 's:^using std\:\:isinf:\/\/using std\:\:isinf:' \
+		$(STAGING_DIR)/usr/include/netflix/nrdbase/tr1.h
+endef
+
+define NETFLIX_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 755 $(@D)/netflix/src/platform/gibbon/libJavaScriptCore.so $(TARGET_DIR)/usr/lib
+	$(INSTALL) -m 755 $(@D)/netflix/src/platform/gibbon/libnetflix.so $(TARGET_DIR)/usr/lib
+	mkdir -p $(TARGET_DIR)/usr/share/fonts/netflix
+	$(INSTALL) -m 644 $(@D)/netflix/src/platform/gibbon/data/fonts/* $(TARGET_DIR)/usr/share/fonts/netflix/
+	$(INSTALL) -m 644 $(@D)/netflix/src/platform/gibbon/resources/gibbon/fonts/LastResort.ttf $(TARGET_DIR)/usr/share/fonts/netflix/
+endef
+
+else
+
 define NETFLIX_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 $(@D)/netflix/src/platform/gibbon/libJavaScriptCore.so $(TARGET_DIR)/usr/lib
 	$(INSTALL) -m 755 $(@D)/netflix/src/platform/gibbon/netflix $(TARGET_DIR)/usr/bin
@@ -102,5 +137,7 @@ define NETFLIX_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 644 $(@D)/netflix/src/platform/gibbon/data/fonts/* $(TARGET_DIR)/usr/share/fonts/netflix/
 	$(INSTALL) -m 644 $(@D)/netflix/src/platform/gibbon/resources/gibbon/fonts/LastResort.ttf $(TARGET_DIR)/usr/share/fonts/netflix/
 endef
+
+endif
 
 $(eval $(cmake-package))
