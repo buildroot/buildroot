04226b5bd6a068349d7af21b7ea7afe26186d14b
albertd <albert@dahan.nl>
2016-03-08 22:23:10 +0100

    [libprovision] bump to latest version

--- package/libprovision/libprovision.mk
+++ package/libprovision/libprovision.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBPROVISION_VERSION = 8ac49d2b9e661422ff61a75cf330f8ada1107a35
+LIBPROVISION_VERSION = 32aa512e38a574ec69e06d862bc8049b3d719388
 LIBPROVISION_SITE_METHOD = git
 LIBPROVISION_SITE = git@github.com:Metrological/libprovision.git
 LIBPROVISION_LICENSE = PROPRIETARY
