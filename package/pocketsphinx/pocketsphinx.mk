496edb345ecf906468978358881fa1ee91b3598e
haseenamol@tataelxsi.co.in <ubuntu@ip-172-31-60-75.ec2.internal>
2016-04-06 12:24:41 +0000

    sysroot install command added for Sphinx Packages

--- package/pocketsphinx/pocketsphinx.mk
+++ package/pocketsphinx/pocketsphinx.mk
@@ -3,13 +3,14 @@
 # pocketsphinx
 #
 ################################################################################
-
 POCKETSPHINX_VERSION = 5prealpha
 POCKETSPHINX_SOURCE = pocketsphinx-$(POCKETSPHINX_VERSION).tar.gz
 POCKETSPHINX_SITE = https://sourceforge.net/projects/cmusphinx/files/pocketsphinx/5prealpha
 POCKETSPHINX_LICENSE = BSD
 POCKETSPHINX_LICENSE_FILES = License.txt
 POCKETSPHINX_DEPENDENCIES = host-pkgconf
+POCKETSPHINX_INSTALL_STAGING = YES
+POCKETSPHINX_DEPENDENCIES += sphinxbase
 
 
 define POCKETSPHINX_CONFIGURE_CMDS
@@ -24,6 +25,12 @@ define POCKETSPHINX_CONFIGURE_CMDS
         --prefix=/usr  --without-python
 endef
 
-POCKETSPHINX_DEPENDENCIES += sphinxbase
+define POCKETSPHINX_INSTALL_STAGING_CMDS
+        $(MAKE1) -C $(@D) DESTDIR=$(STAGING_DIR) install
+endef
+
+define POCKETSPHINX_INSTALL_TARGET_CMDS
+        $(MAKE1) -C $(@D) DESTDIR=$(TARGET_DIR) install
+endef
 
 $(eval $(autotools-package))
