b26e3a6e9d62c395dfb5eac9f63c8e7f6a9fe7f0
albertd <albert@dahan.nl>
2016-04-11 19:39:47 +0200

    [gst1-plugins-dorne] bump to latest version

--- package/gstreamer1/gst1-plugins-dorne/gst1-plugins-dorne.mk
+++ package/gstreamer1/gst1-plugins-dorne/gst1-plugins-dorne.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GST1_PLUGINS_DORNE_VERSION = b28c59868f3091b4e6e99b3fe2c2357643b77d77
+GST1_PLUGINS_DORNE_VERSION = 1e38503a5e3502263ba571abc7214bcfe57547da
 GST1_PLUGINS_DORNE_SITE_METHOD = git
 GST1_PLUGINS_DORNE_SITE = git@github.com:Metrological/gst-plugins-dorne.git
 
@@ -21,6 +21,6 @@ GST1_PLUGINS_DORNE_POST_EXTRACT_HOOKS += GSTREAMER1_COMMON_EXTRACT
 GST1_PLUGINS_DORNE_PRE_CONFIGURE_HOOKS += GSTREAMER1_FIX_AUTOPOINT
 GST1_PLUGINS_DORNE_POST_INSTALL_TARGET_HOOKS += GSTREAMER1_REMOVE_LA_FILES
 
-GST1_PLUGINS_DORNE_DEPENDENCIES = gst1-common gstreamer1 gst1-plugins-base
+GST1_PLUGINS_DORNE_DEPENDENCIES = gluelogic gst1-common gstreamer1 gst1-plugins-base
 
 $(eval $(autotools-package))
