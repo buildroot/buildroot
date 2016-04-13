e8e1287461564b917faf142bd1d9c4dd55e91f1f
bramoosterhuis <bram.oosterhuis.nl@gmail.com>
2016-04-13 15:26:33 +0200

    [horizon] Add mime and lib/gio symlinks

--- board/intel/intelce/horizon/usb_script.sh
+++ board/intel/intelce/horizon/usb_script.sh
@@ -7,10 +7,12 @@ ln -sfn $SOURCE/NDS/webbridge-stub /NDS/bin/webbridge
 ln -sfn $SOURCE/etc/playready /etc/playready
 ln -sfn $SOURCE/etc/ssl /etc/ssl
 ln -sfn $SOURCE/etc/fonts /etc/fonts
+ln -sfn $SOURCE/usr/lib/gio /usr/lib/gio
 ln -sfn $SOURCE/usr/lib/libgenerics.so.1.0 /usr/lib/libgenerics.so.1.0
 ln -sfn $SOURCE/usr/lib/libmqc.so.1.0 /usr/lib/libmqc.so.1.0
 ln -sfn $SOURCE/usr/lib/libtracing.so.1.0 /usr/lib/libtracing.so.1.0
 mkdir -p /usr/share/fonts
+ln -sfn $SOURCE/usr/share/mime /usr/share/mime
 ln -sfn $SOURCE/usr/share/X11 /usr/share/X11
 ln -sfn $SOURCE/usr/share/fonts/ttf-bitstream-vera /usr/share/fonts/ttf-bitstream-vera
 ln -sfn $SOURCE/usr/share/fonts/netflix /usr/share/fonts/netflix
