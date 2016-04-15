0d0a1c8ce029781c81317b275baa36a213c49824
albertd <albert@dahan.nl>
2016-04-15 12:32:45 +0200

    [gst1-plugins-dorne] bump to latest version

--- package/gstreamer1/gst1-plugins-dorne/gst1-plugins-dorne.mk
+++ package/gstreamer1/gst1-plugins-dorne/gst1-plugins-dorne.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GST1_PLUGINS_DORNE_VERSION = 0391c93a8945478d765070f0955807c366ae3fb2
+GST1_PLUGINS_DORNE_VERSION = c3ab2d323c662b3fdf2cd413283a21b297e84db0
 GST1_PLUGINS_DORNE_SITE_METHOD = git
 GST1_PLUGINS_DORNE_SITE = git@github.com:Metrological/gst-plugins-dorne.git
 
