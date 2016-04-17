9f03a512d3378d4cb8bd6e83aa510018863d7053
albertd <albert@dahan.nl>
2016-04-17 23:31:54 +0200

    [webbridge] bump to latest version

--- package/webbridge/webbridge.mk
+++ package/webbridge/webbridge.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WEBBRIDGE_VERSION = 3c235a8c7efd921d85a0453917060ced88a5f751
+WEBBRIDGE_VERSION = 229afb2a1706cde55c9e039a66c9c5d9cb4f8721
 WEBBRIDGE_SITE_METHOD = git
 WEBBRIDGE_SITE = git@github.com:Metrological/webbridge.git
 WEBBRIDGE_INSTALL_STAGING = YES
