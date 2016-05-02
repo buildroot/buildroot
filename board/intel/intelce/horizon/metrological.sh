d2828264ea0dbd296c4145a1d0e7a0bfb11f82b6
Wouter Meek <woutermeek@users.noreply.github.com>
2016-05-02 17:08:29 +0200

    [horizon] rdate script added max tries

--- board/intel/intelce/horizon/metrological.sh
+++ board/intel/intelce/horizon/metrological.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 COUNTER=0
+TRIES=0
+MAXTRIES=20
 SLEEP=1
 SERVERS=("time-a.nist.gov" "time-b.nist.gov" "time-c.nist.gov" "utcnist.colorado.edu" "utcnist2.colorado.edu")
 
@@ -12,12 +14,17 @@ function updateTime {
 		echo "retry to set time in $SLEEP seconds"
 		sleep $SLEEP
 		((COUNTER+=1))
-
+		((TRIES+=1))
 		if [ $COUNTER -eq ${#SERVERS[@]} ]; then
 			COUNTER=0
 		fi
 
-		updateTime
+		if [ $TRIES -ge $MAXTRIES ]; then
+			echo "set time failed after $TRIES times"
+			exit
+		else	
+			updateTime
+		fi
 	fi	
 }	
 
