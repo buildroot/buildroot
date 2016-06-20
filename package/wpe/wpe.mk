8d92311b6c851f3f3d015b4ee0e72a00621c085b
albertd <albert@dahan.nl>
2016-06-20 15:18:17 +0200

    [wpe] revert wpe bump

--- package/wpe/wpe.mk
+++ package/wpe/wpe.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPE_VERSION = 3f4ede2c38a6aa34170fcb6cb583a2f04d5c0eba
+WPE_VERSION = 858b51ed35487a4d524c48fa27e7e89ed1d6adf9
 WPE_SITE = $(call github,Metrological,WebKitForWayland,$(WPE_VERSION))
 
 WPE_INSTALL_STAGING = YES
@@ -224,7 +224,7 @@ WPE_BUILD_TARGETS += jsc
 endif
 ifeq ($(WPE_BUILD_WEBKIT),y)
 WPE_BUILD_TARGETS += libWPEWebKit.so libWPEWebInspectorResources.so \
-	WPE{Database,Network,Web}Process libWPE.so libWPE-metrological.so
+	WPE{Database,Network,Web}Process
 
 endif
 
