d4eeb1a56abf2a39392627cef8a396982542c6e0
albertd <albert@dahan.nl>
2016-07-20 16:52:56 +0200

    [cppsdk] bump to latest version

--- package/cppsdk/cppsdk.mk
+++ package/cppsdk/cppsdk.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CPPSDK_VERSION = 8d67371b74765372a649391464fd2a6fab2a9586
+CPPSDK_VERSION = 370c1ef3626aabb7d887c9a26ca969e0137a7a46
 CPPSDK_SITE_METHOD = git
 CPPSDK_SITE = git@github.com:Metrological/cppsdk.git
 CPPSDK_INSTALL_STAGING = YES
