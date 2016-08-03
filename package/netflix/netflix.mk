e36d3576bf15dc099d5cc97a484dd6fdc1b0cff5
albertd <albert@dahan.nl>
2016-08-03 20:27:18 +0200

    [netflix] bump to latest version

--- package/netflix/netflix.mk
+++ package/netflix/netflix.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NETFLIX_VERSION = 9818335c3689a96edf4e29da6b004a6d0e7edbd2
+NETFLIX_VERSION = 5a004ca1ac97b31d05db9f8fe6da4133a176e3ea
 NETFLIX_SITE = git@github.com:Metrological/netflix.git
 NETFLIX_SITE_METHOD = git
 NETFLIX_LICENSE = PROPRIETARY
