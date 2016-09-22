ff7e278af78a10572e565d5dbd22769f2cc08ba6
DoniaAbraham <doniaabraham@tataelxsi.co.in>
2016-09-22 11:28:21 +0000

    opencdmi package integrated to buildroot

---
+++ package/opencdmi/opencdmi.mk
@@ -0,0 +1,38 @@
+################################################################################
+#
+# open cdmi
+#
+################################################################################
+
+OPENCDMI_VERSION = a0940e12e8cbc2a0954ab6d9957c29e158439bd7
+OPENCDMI_SITE_METHOD = git
+OPENCDMI_SITE = https://github.com/fraunhoferfokus/open-content-decryption-module-cdmi.git
+
+OPENCDMI_INSTALL_STAGING = YES
+OPENCDMI_DEPENDENCIES = rpcbind
+OPENCDMI_LICENSE = Apache-2.0
+OPENCDMI_LICENSE_FILES = LICENSE 
+
+define OPENCDMI_BUILD_CMDS
+        export OPENCDMI_TARGET_DIR="$(TARGET_DIR)";\
+        export OPENCDMI_STAGING_DIR="$(STAGING_DIR)";\
+        $(MAKE) CROSS_COMPILE="$(TARGET_CROSS)" \
+        CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" AR="$(TARGET_AR)" \
+        CXXFLAGS="$(TARGET_CXXFLAGS)" \
+        LDFLAGS="$(TARGET_LDFLAGS)  -L$(STAGING_DIR)/usr/lib  -pthread" -C $(@D)
+
+endef
+define OPENCDMI_INSTALL_TARGET_CMDS
+        cp $(@D)/cdmiservice $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(generic-package))
+$(eval $(host-generic-package))
+
+    
+
+                                         
+                                                                                                        
+                                                                                                        
+                                
+                                  
