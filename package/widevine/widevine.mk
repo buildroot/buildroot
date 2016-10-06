15c593fb576521c564c1452d62e741ea60867f9f
haseenamol <haseenamol@tataelxsi.co.in>
2016-10-06 07:17:31 +0000

    widevine changes added

--- package/widevine/widevine.mk
+++ package/widevine/widevine.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WIDEVINE_VERSION = 8d115c5c82464ba2c9ea0bb58e88f7f2db19a8b6
+WIDEVINE_VERSION = 5a2581a1464c610a69351f765d6b1de29d64903d
 WIDEVINE_SITE_METHOD = git
 WIDEVINE_SITE = https://github.com/Metrological/widevine.git
 
@@ -13,13 +13,23 @@ WIDEVINE_DEPENDENCIES =
 WIDEVINE_LICENSE = BSD
 WIDEVINE_LICENSE_FILES = LICENSE
 
+ifeq ($(BR2_PACKAGE_WIDEVINE_SOC_RPI), y)
+export TARGET_PLATFORM="rpi"; 
+endif #BR2_PACKAGE_WIDEVINE_SOC_RPI
+
 define WIDEVINE_CONFIGURE_CMDS
-      (cd $(@D);rm -rf out; rm -rf Makefile;./build.py arm)
+      (cd $(@D);rm -rf out; rm -rf Makefile;\
+       find . -name \*.mk -delete;\
+       find . -name \*.pyc -delete;\
+       ./build.py arm)
 endef
 
 define WIDEVINE_BUILD_CMDS
         export WIDEVINE_TARGET_DIR="$(TARGET_DIR)";\
         export WIDEVINE_STAGING_DIR="$(STAGING_DIR)";\
+        export TARGET_CC="$(TARGET_CC)"; \
+        export TARGET_CXX="$(TARGET_CXX)"; \
+        export TARGET_AR="$(TARGET_AR)"; \
         make CROSS_COMPILE="$(TARGET_CROSS)" \
         CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" AR="$(TARGET_AR)" \
         CXXFLAGS="$(TARGET_CXXFLAGS)" \
@@ -32,7 +42,8 @@ define WIDEVINE_INSTALL_TARGET_CMDS
 endef
 
 define WIDEVINE_INSTALL_STAGING_CMDS
-        cp $(@D)/out/arm/Debug/lib.target/lib*.so $(TARGET_DIR)/usr/lib/
+        cp $(@D)/out/arm/Debug/widevine_ce_cdm_unittest $(STAGING_DIR)/usr/bin
+        cp $(@D)/out/arm/Debug/lib.target/lib*.so $(STAGING_DIR)/usr/lib/
 endef
 
 $(eval $(generic-package))
