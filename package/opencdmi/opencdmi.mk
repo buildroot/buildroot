2a37aaa1abf1d1d59f220c4149dd9272b97a227b
haseenamol <haseenamol@tataelxsi.co.in>
2016-10-20 14:02:06 +0000

    Init script added for OpenCDMi + Seperate rule added for service and lib

--- package/opencdmi/opencdmi.mk
+++ package/opencdmi/opencdmi.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENCDMI_VERSION = a5748b2869b7af3b6c147e1465fce117bed0656f
+OPENCDMI_VERSION = 47ef38ec04caeb82f0102f4ec65a65b0b46025a3
 OPENCDMI_SITE_METHOD = git
 OPENCDMI_SITE = git@github.com:Metrological/open-content-decryption-module-cdmi.git
 
@@ -18,6 +18,13 @@ export CDMI_WV=1;
 OPENCDMI_DEPENDENCIES += widevine
 endif #BR2_PACKAGE_WIDEVINE
 
+ifeq ($(BR2_PACKAGE_OPENCDMI_SERVICE), y)
+OPENCDMI_TARGET=all
+OPENCDMI_SERVICE=$(@D)/cdmiservice
+else
+OPENCDMI_TARGET=ocdmilib
+endif
+
 define OPENCDMI_BUILD_CMDS
         export OPENCDMI_TARGET_DIR="$(TARGET_DIR)";\
         export OPENCDMI_STAGING_DIR="$(STAGING_DIR)";\
@@ -25,13 +32,22 @@ define OPENCDMI_BUILD_CMDS
         make CROSS_COMPILE="$(TARGET_CROSS)" \
         CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" AR="$(TARGET_AR)" \
         CXXFLAGS="$(TARGET_CXXFLAGS) -fPIC" \
-        LDFLAGS="$(TARGET_LDFLAGS)  -L$(STAGING_DIR)/usr/lib  -pthread" -C $(@D)
-
+        LDFLAGS="$(TARGET_LDFLAGS)  -L$(STAGING_DIR)/usr/lib  -pthread" -C $(@D) $(OPENCDMI_TARGET)
 endef
+
+ifeq ($(BR2_PACKAGE_OPENCDMI_SERVICE), y)
 define OPENCDMI_INSTALL_TARGET_CMDS
+        cp $(@D)/libocdmi.so $(TARGET_DIR)/usr/lib
         cp $(@D)/cdmiservice $(TARGET_DIR)/usr/bin
+endef
+define OPENCDMI_INSTALL_INIT_SYSV
+        $(INSTALL) -D -m 0755 package/opencdmi/S85opencdmi $(TARGET_DIR)/etc/init.d/S85opencdmi
+endef
+else
+define OPENCDMI_INSTALL_TARGET_CMDS
         cp $(@D)/libocdmi.so $(TARGET_DIR)/usr/lib
 endef
+endif
 
 $(eval $(generic-package))
 $(eval $(host-generic-package))
