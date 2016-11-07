cb508a682564c7e7d98d696922d84af1f2663af4
SKumarMetro <s.kumar@metrological.com>
2016-11-07 11:54:40 -0800

    [wpe] vc5/GL files are in different location in RefSw 16.2

--- package/bcm-refsw/bcm-refsw.mk
+++ package/bcm-refsw/bcm-refsw.mk
@@ -131,7 +131,7 @@ define BCM_REFSW_BUILD_EGLCUBE
 	$(TARGET_MAKE_ENV) \
 	$(BCM_REFSW_CONF_OPTS) \
 	$(BCM_REFSW_MAKE_ENV) \
-		$(MAKE) -C $(@D)/rockford/applications/khronos/v3d/nexus/cube all \
+		$(MAKE) -C $(@D)/rockford/applications/khronos/v3d/nexus/cube \
 			LIBDIR=${BCM_REFSW_BIN}
 endef
 endif
@@ -180,22 +180,24 @@ define BCM_REFSW_BUILD_CMDS
 	$(BCM_REFSW_BUILD_EGLCUBE)
 endef
 
+ifeq ($(BCM_REFSW_PLATFORM_VC),vc5)
+BCM_REFSW_VCX_KHRNPLAT = $(BCM_REFSW_VCX)/driver/libs/khrn/include
 ifeq ($(BR2_PACKAGE_BCM_REFSW_16_2),y) 
-define BCM_REFSW_INSTALL_KHRONOS
-	$(INSTALL) -m 644 ${BCM_REFSW_VCX}/driver/libs/khrn/include/GLES/*.h $(STAGING_DIR)/usr/include/GLES/
-	$(INSTALL) -m 644 ${BCM_REFSW_VCX}/driver/libs/khrn/include/GLES2/*.h $(STAGING_DIR)/usr/include/GLES2/
-	$(INSTALL) -m 644 ${BCM_REFSW_VCX}/driver/libs/khrn/include/EGL/*.h $(STAGING_DIR)/usr/include/EGL/
-	$(INSTALL) -m 644 -D ${BCM_REFSW_VCX}/driver/libs/khrn/include/KHR/khrplatform.h $(STAGING_DIR)/usr/include/KHR/khrplatform.h;
-endef
+	BCM_REFSW_VCX_KHRN = $(BCM_REFSW_VCX)/driver/libs/khrn/include
 else
-define BCM_REFSW_INSTALL_KHRONOS
-	$(INSTALL) -m 644 ${BCM_REFSW_VCX}/driver/interface/khronos/include/GLES/*.h $(STAGING_DIR)/usr/include/GLES/
-	$(INSTALL) -m 644 ${BCM_REFSW_VCX}/driver/interface/khronos/include/GLES2/*.h $(STAGING_DIR)/usr/include/GLES2/
-	$(INSTALL) -m 644 ${BCM_REFSW_VCX}/driver/interface/khronos/include/EGL/*.h $(STAGING_DIR)/usr/include/EGL/
-	$(INSTALL) -m 644 -D ${BCM_REFSW_VCX}/driver/interface/khronos/include/KHR/khrplatform.h $(STAGING_DIR)/usr/include/KHR/khrplatform.h;
-endef
+	BCM_REFSW_VCX_KHRN = $(BCM_REFSW_VCX)/driver/interface/khronos/include
+endif
+else
+	BCM_REFSW_VCX_KHRN = $(BCM_REFSW_VCX)/driver/interface/khronos/include
+	BCM_REFSW_VCX_KHRNPLAT = $(BCM_REFSW_VCX_KHRN)
 endif
 
+define BCM_REFSW_INSTALL_KHRONOS
+	$(INSTALL) -m 644 ${BCM_REFSW_VCX_KHRN}/GLES/*.h $(STAGING_DIR)/usr/include/GLES/
+	$(INSTALL) -m 644 ${BCM_REFSW_VCX_KHRN}/GLES2/*.h $(STAGING_DIR)/usr/include/GLES2/
+	$(INSTALL) -m 644 ${BCM_REFSW_VCX_KHRN}/EGL/*.h $(STAGING_DIR)/usr/include/EGL/
+	$(INSTALL) -m 644 -D ${BCM_REFSW_VCX_KHRNPLAT}/KHR/khrplatform.h $(STAGING_DIR)/usr/include/KHR/khrplatform.h;
+endef
 
 ifeq ($(BR2_PACKAGE_BCM_REFSW_16_2)$(BR2_PACKAGE_BCM_REFSW_SAGE),yy) 
 define BCM_REFSW_INSTALL_SAGE_BIN
