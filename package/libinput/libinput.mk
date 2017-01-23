01c806ff8c1a92d36e9c85b4a92cdbe386519251
albertd <albert@dahan.nl>
2017-01-23 11:45:13 +0100

    [libinput] use python3 for build

--- package/libinput/libinput.mk
+++ package/libinput/libinput.mk
@@ -7,12 +7,13 @@
 LIBINPUT_VERSION = 1.5.4
 LIBINPUT_SOURCE = libinput-$(LIBINPUT_VERSION).tar.xz
 LIBINPUT_SITE = http://www.freedesktop.org/software/libinput
-LIBINPUT_DEPENDENCIES = host-pkgconf libevdev mtdev udev
+LIBINPUT_DEPENDENCIES = host-pkgconf host-python3 libevdev mtdev udev
 LIBINPUT_INSTALL_STAGING = YES
 LIBINPUT_LICENSE = MIT
 LIBINPUT_LICENSE_FILES = COPYING
 # Tests need fork, so just disable them everywhere.
 LIBINPUT_CONF_OPTS = --disable-tests --disable-libwacom
+LIBINPUT_CONF_ENV = PYTHON=python3
 
 ifeq ($(BR2_PACKAGE_LIBGTK3),y)
 LIBINPUT_CONF_OPTS += --enable-event-gui
