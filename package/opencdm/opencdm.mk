daeffae33aa8403ba06ae3b925473c34d3edd049
wouterlucas <wouter@wouterlucas.com>
2017-02-01 14:39:44 -0800

    [opencdm] Switch to Metro repo and cleanup

--- package/opencdm/opencdm.mk
+++ package/opencdm/opencdm.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-OPENCDM_VERSION = 1e64a23ded5e64df0ae62f52c4114f2ed54b1f04
+OPENCDM_VERSION = 04669dd00977e23c137d5bb9f84c9c5e1af52165
 OPENCDM_SITE_METHOD = git
-OPENCDM_SITE = https://github.com/fraunhoferfokus/open-content-decryption-module.git
+OPENCDM_SITE = https://github.com/Metrological/open-content-decryption-module.git
 
 OPENCDM_INSTALL_STAGING = YES
 OPENCDMI_LICENSE = Apache-2.0
@@ -16,23 +16,23 @@ OPENCDMI_LICENSE_FILES = LICENSE
 
 #Build
 define OPENCDM_BUILD_CMDS
-        export OPENCDM_TARGET_DIR="$(TARGET_DIR)";\
-        export OPENCDM_STAGING_DIR="$(STAGING_DIR)";\
-        make CROSS_COMPILE="$(TARGET_CROSS)" \
-        CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" AR="$(TARGET_AR)" \
-        CXXFLAGS="$(TARGET_CXXFLAGS)" \
-        LDFLAGS="$(TARGET_LDFLAGS)  -L$(STAGING_DIR)/usr/lib  -pthread" -C $(@D)
+    export OPENCDM_TARGET_DIR="$(TARGET_DIR)";\
+    export OPENCDM_STAGING_DIR="$(STAGING_DIR)";\
+    make CROSS_COMPILE="$(TARGET_CROSS)" \
+    CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" AR="$(TARGET_AR)" \
+    CXXFLAGS="$(TARGET_CXXFLAGS)" \
+    LDFLAGS="$(TARGET_LDFLAGS)  -L$(STAGING_DIR)/usr/lib  -pthread" -C $(@D)
 endef
 
 define OPENCDM_INSTALL_TARGET_CMDS
-        cp $(@D)/src/browser/wpe/bin/ocdm_client $(TARGET_DIR)/usr/bin
-        cp $(@D)/src/browser/wpe/bin/ocdm_decryptor $(TARGET_DIR)/usr/bin
-        cp $(@D)/src/browser/wpe/lib/libocdm.so $(TARGET_DIR)/usr/lib
+    cp $(@D)/src/browser/wpe/bin/ocdm_client $(TARGET_DIR)/usr/bin
+    cp $(@D)/src/browser/wpe/bin/ocdm_decryptor $(TARGET_DIR)/usr/bin
+    cp $(@D)/src/browser/wpe/lib/libocdm.so $(TARGET_DIR)/usr/lib
 endef
 define OPENCDM_INSTALL_STAGING_CMDS
-	cp $(@D)/src/browser/wpe/lib/libocdm.so $(STAGING_DIR)/usr/lib
-        mkdir -p $(STAGING_DIR)/usr/include/opencdm
-	cp -r $(@D)/src/browser/wpe/include/* $(STAGING_DIR)/usr/include/opencdm
+    cp $(@D)/src/browser/wpe/lib/libocdm.so $(STAGING_DIR)/usr/lib
+    mkdir -p $(STAGING_DIR)/usr/include/opencdm
+    cp -r $(@D)/src/browser/wpe/include/* $(STAGING_DIR)/usr/include/opencdm
 endef
 
 $(eval $(generic-package))
