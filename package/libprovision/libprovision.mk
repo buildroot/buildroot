726f4e84c8ed3c6f189cd4335313bae025a91b9d
Pierre Wielders <pierre@wielders.net>
2017-02-06 10:32:03 +0100

    [PROVISIONING] In case we build the libprovisionproxy, no need to copy it.

--- package/libprovision/libprovision.mk
+++ package/libprovision/libprovision.mk
@@ -13,21 +13,14 @@ LIBPROVISION_INSTALL_STAGING = YES
 
 LIBPROVISION_DEPENDENCIES = openssl cppsdk
 
-ifeq ($(BR2_PACKAGE_DXDRM),y)
-define LIBPROVISIONPROXY_INSTALL_STAGING
-	$(INSTALL) -m 755 $(@D)/$(call qstrip,$(BR2_ARCH))/libprovisionproxy.so $(STAGING_DIR)/usr/lib/libprovisionproxy.so
-endef
-define LIBPROVISIONPROXY_INSTALL_TARGET
-	$(INSTALL) -m 755 $(@D)/$(call qstrip,$(BR2_ARCH))/libprovisionproxy.so $(TARGET_DIR)/usr/lib/libprovisionproxy.so
-endef
-else
+ifneq ($(BR2_PACKAGE_GLUELOGIC_PROVISIONING),y)
 define LIBPROVISIONPROXY_INSTALL_STAGING
 	$(INSTALL) -m 644 $(@D)/$(call qstrip,$(BR2_ARCH))/libprovisionproxy.a $(STAGING_DIR)/usr/lib/libprovisionproxy.a
 endef
 endif
 
 define LIBPROVISION_INSTALL_STAGING_CMDS
-	$(LIBPROVISIONPROXY_INSTALL_STAGING)
+        $(LIBPROVISIONPROXY_INSTALL_STAGING)
 	$(INSTALL) -m 755 $(@D)/$(call qstrip,$(BR2_ARCH))/libprovision.so $(STAGING_DIR)/usr/lib/libprovision.so
 	$(INSTALL) -d -m 755 $(STAGING_DIR)/usr/include/provision
 	$(INSTALL) -m 644 $(@D)/include/provision/*.h $(STAGING_DIR)/usr/include/provision
