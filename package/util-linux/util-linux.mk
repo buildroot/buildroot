624594df6c6e63d8c0b7ff9814884e62bcf73d87
albertd <albert@dahan.nl>
2017-03-17 10:07:55 +0100

    Revert "[util-linux] fix configure after adding patch"
    This reverts commit 6e15dc674f05e3b370e919944120133c7a042e2a.

--- package/util-linux/util-linux.mk
+++ package/util-linux/util-linux.mk
@@ -16,8 +16,6 @@ UTIL_LINUX_AUTORECONF = YES
 # Some files are GPLv3+ but only in tests.
 UTIL_LINUX_LICENSE = GPLv2+, BSD-4c, LGPLv2.1+ (libblkid, libfdisk, libmount), BSD-3c (libuuid)
 UTIL_LINUX_LICENSE_FILES = README.licensing Documentation/licenses/COPYING.GPLv2 Documentation/licenses/COPYING.UCB Documentation/licenses/COPYING.LGPLv2.1 Documentation/licenses/COPYING.BSD-3
-# For 0001-uclibc-scanf-inplementation.patch
-UTIL_LINUX_AUTORECONF = YES
 UTIL_LINUX_INSTALL_STAGING = YES
 UTIL_LINUX_DEPENDENCIES = host-pkgconf
 # uClibc needs NTP_LEGACY for sys/timex.h -> ntp_gettime() support
