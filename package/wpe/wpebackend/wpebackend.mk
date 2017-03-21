4b6e394b1719b9bb9cf97590f03049d41a0eb2de
albertd <albert@dahan.nl>
2017-03-21 18:20:35 +0100

    [wpe-backend] bump to latest version

--- package/wpe/wpebackend/wpebackend.mk
+++ package/wpe/wpebackend/wpebackend.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEBACKEND_VERSION = 4aff0ead8b0d12483800e39856662cd955b2ece5
+WPEBACKEND_VERSION = baa960b781a88addf778650e1226563ef12f8170
 WPEBACKEND_SITE = $(call github,WebPlatformForEmbedded,WPEBackend,$(WPEBACKEND_VERSION))
 WPEBACKEND_INSTALL_STAGING = YES
 WPEBACKEND_DEPENDENCIES += libegl
