19bb60af540405b3c88c3fdfa8b7e05ec109532c
albertd <albert@dahan.nl>
2017-04-07 19:53:18 +0200

    [wpebackend-rdk] bump to latest version

--- package/wpe/wpebackend-rdk/wpebackend-rdk.mk
+++ package/wpe/wpebackend-rdk/wpebackend-rdk.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEBACKEND_RDK_VERSION = 330e02a09d144c12c973291ca2cd7d52554df7a3
+WPEBACKEND_RDK_VERSION = 7d262041c75cdfcf6b2a3de5343ba0dedce1901b
 WPEBACKEND_RDK_SITE = $(call github,WebPlatformForEmbedded,WPEBackend-rdk,$(WPEBACKEND_RDK_VERSION))
 WPEBACKEND_RDK_INSTALL_STAGING = YES
 WPEBACKEND_RDK_DEPENDENCIES = wpebackend libglib2
