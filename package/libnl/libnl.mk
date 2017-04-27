e63c2c35665e17e9b63e53bc6620a20d6e748571
Baruch Siach <baruch@tkos.co.il>
2017-04-27 13:50:58 +0300

    libnl: add upstream security fix
    CVE-2017-0553: An elevation of privilege vulnerability in libnl could enable a
    local malicious application to execute arbitrary code within the context of
    the Wi-Fi service
    
    https://www.mail-archive.com/debian-bugs-dist@lists.debian.org/msg1511855.html
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 5efbd573c0a4df751e038a927c09af5aac1a233e)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libnl/libnl.mk
+++ package/libnl/libnl.mk
@@ -11,6 +11,8 @@ LIBNL_LICENSE_FILES = COPYING
 LIBNL_INSTALL_STAGING = YES
 LIBNL_DEPENDENCIES = host-bison host-flex
 
+LIBNL_PATCH = https://github.com/thom311/libnl/commit/3e18948f17148e6a3c4255bdeaaf01ef6081ceeb.patch
+
 ifeq ($(BR2_PACKAGE_LIBNL_TOOLS),y)
 LIBNL_CONF_OPTS += --enable-cli
 else
