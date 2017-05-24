8bbfda3742beeb33867d617a3f71949b4a1677e2
wouterlucas <wouter@wouterlucas.com>
2017-05-23 20:55:57 -0700

    [netflix] Bump to latest

--- package/netflix/netflix.mk
+++ package/netflix/netflix.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NETFLIX_VERSION = 79f893f0708ae79a175fa33e619e1c69eb505d89
+NETFLIX_VERSION = c33c275a9569981655095e8a2f9475d97cc762c6
 NETFLIX_SITE = git@github.com:Metrological/netflix.git
 NETFLIX_SITE_METHOD = git
 NETFLIX_LICENSE = PROPRIETARY
