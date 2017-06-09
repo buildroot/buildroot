afed8e60fee4041ac38518ba0e66e7585a52ba7a
Pierre Wielders <pierre@wielders.net>
2017-06-09 09:04:34 +0200

    [WPEFRamework] Restructured the menu configs.

--- package/libprovision/libprovision.mk
+++ package/libprovision/libprovision.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-LIBPROVISION_VERSION = 47ecb7154c43215a4f4e8096da9e182b3e884532
+LIBPROVISION_VERSION = b76f6b0c7c83a444c1a1d3f2625ba36723a87c8a
 LIBPROVISION_SITE_METHOD = git
 LIBPROVISION_SITE = git@github.com:Metrological/libprovision.git
 LIBPROVISION_LICENSE = PROPRIETARY
 LIBPROVISION_REDISTRIBUTE = NO
 LIBPROVISION_INSTALL_STAGING = YES
 
-LIBPROVISION_DEPENDENCIES = openssl cppsdk
+LIBPROVISION_DEPENDENCIES = openssl WPEFramework
 
 $(eval $(cmake-package))
