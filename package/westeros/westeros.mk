41d921b4750b613a56e719205c97c8cef0223d80
Pierre Wielders <pierre@wielders.net>
2017-06-15 10:07:13 +0200

    [WESTEROS] Bump Westeros/WPEFramework

--- package/westeros/westeros.mk
+++ package/westeros/westeros.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WESTEROS_VERSION = 9345a77d2e547c0ea6e625a65d9de76c03948322
+WESTEROS_VERSION = 0b853a6ae0d84cedbe141189d658fae70f8474cc
 WESTEROS_SITE_METHOD = git
 WESTEROS_SITE = git://github.com/rdkcmf/westeros
 WESTEROS_INSTALL_STAGING = YES
