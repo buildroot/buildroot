f2871c3beae5ec111b4a3429c00af4c91c1567c2
Dmitry Belenko <dmitryb@allenai.org>
2017-07-27 16:21:55 -0700

    Re-applying gstreamermm patch #1

---
+++ package/gstreamermm/gstreamermm.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# gstreamermm
+#
+################################################################################
+
+GSTREAMERMM_VERSION_MAJOR = 1.4
+GSTREAMERMM_VERSION = $(GSTREAMERMM_VERSION_MAJOR).3
+GSTREAMERMM_SOURCE = gstreamermm-$(GSTREAMERMM_VERSION).tar.xz
+GSTREAMERMM_SITE = https://download.gnome.org/sources/gstreamermm/$(GSTREAMERMM_VERSION_MAJOR)
+GSTREAMERMM_LICENSE = LGPLv2.1+ (library), GPLv2+ (tools, examples)
+GSTREAMERMM_LICENSE_FILES = COPYING COPYING.tools COPYING.examples
+GSTREAMERMM_INSTALL_STAGING = YES
+GSTREAMERMM_DEPENDENCIES = glibmm libsigc host-pkgconf
+
+$(eval $(autotools-package))
