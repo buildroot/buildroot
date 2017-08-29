af76cb192ae57b1f2e1a01e365a849d271780730
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-08-29 21:10:00 +0200

    package/dialog: fix upstream source URL
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    (cherry picked from commit 5fdebd3b8aad0407c0eeaeef5998562d17c5e42a)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/dialog/dialog.mk
+++ package/dialog/dialog.mk
@@ -6,7 +6,7 @@
 
 DIALOG_VERSION = 1.2-20150125
 DIALOG_SOURCE = dialog-$(DIALOG_VERSION).tgz
-DIALOG_SITE = ftp://invisible-island.net/dialog
+DIALOG_SITE = ftp://ftp.invisible-island.net/dialog
 DIALOG_CONF_OPTS = --with-ncurses --with-curses-dir=$(STAGING_DIR)/usr \
 	--disable-rpath-hack
 DIALOG_DEPENDENCIES = host-pkgconf ncurses
