78a3051a412f78c853a519683ab3edf31ae83fcc
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2017-09-06 11:37:23 +0200

    [westeros-soc] Adding VC5 support

--- package/westeros-soc/westeros-soc.mk
+++ package/westeros-soc/westeros-soc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WESTEROS_SOC_VERSION = 703ad8b0e081242a2a9a659b5fc8756f206d6fc2
+WESTEROS_SOC_VERSION = 578e580f1e140dee13d226aeb731fc64eea24441
 WESTEROS_SOC_SITE_METHOD = git
 WESTEROS_SOC_SITE = git://github.com/rdkcmf/westeros
 WESTEROS_SOC_INSTALL_STAGING = YES
@@ -13,18 +13,25 @@ WESTEROS_SOC_AUTORECONF_OPTS = "-Icfg"
 
 WESTEROS_SOC_DEPENDENCIES = host-pkgconf host-autoconf wayland libegl
 
+WESTEROS_SOC_CONF_OPTS += \
+	--prefix=/usr/ \
+    --disable-silent-rules \
+    --disable-dependency-tracking \
+
 ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
 	WESTEROS_SOC_CONF_OPTS += CFLAGS="$(TARGET_CFLAGS) -I ${STAGING_DIR}/usr/include/interface/vmcs_host/linux/"
 	WESTEROS_SOC_SUBDIR = rpi
 else ifeq ($(BR2_PACKAGE_HAS_NEXUS),y)
 	WESTEROS_SOC_MAKE_ENV += \
 		$(BCM_REFSW_MAKE_ENV) \
+        REFSW_VERSION="$(STAGING_DIR)/usr/share/wayland-egl" \
 		PKG_CONFIG_SYSROOT_DIR=$(STAGING_DIR)
 	WESTEROS_SOC_CONF_OPTS += \
+        --enable-vc5 \
 		CFLAGS="$(TARGET_CFLAGS) -I ${STAGING_DIR}/usr/include/refsw/" \
 		CXXFLAGS="$(TARGET_CXXFLAGS) -I ${STAGING_DIR}/usr/include/refsw/"
 	WESTEROS_SOC_SUBDIR = brcm
-	WESTEROS_SOC_DEPENDENCIES += bcm-refsw
+    WESTEROS_SOC_DEPENDENCIES += wayland-egl-bnxs bcm-refsw
 else ifeq ($(BR2_PACKAGE_LIBDRM),y)
 	WESTEROS_SOC_CONF_OPTS += CFLAGS="$(TARGET_CFLAGS) -I $(STAGING_DIR)/usr/include/libdrm"
 	WESTEROS_SOC_SUBDIR = drm
@@ -36,12 +43,9 @@ define WESTEROS_SOC_RUN_AUTOCONF
 endef
 WESTEROS_SOC_PRE_CONFIGURE_HOOKS += WESTEROS_SOC_RUN_AUTOCONF
 
-define WESTEROS_SOC_INSTALL_STAGING_CMDS
-	$(MAKE1) -C $(@D)/$(WESTEROS_SOC_SUBDIR) PKG_CONFIG_SYSROOT_DIR=$(STAGING_DIR) DESTDIR=$(STAGING_DIR) install
+define WESTEROS_SOC_ENTER_BUILD_DIR
+	cd $(@D)/$(WESTEROS_SOC_SUBDIR)
 endef
-
-define WESTEROS_SOC_INSTALL_TARGET_CMDS
-	$(MAKE1) -C $(@D)/$(WESTEROS_SOC_SUBDIR) PKG_CONFIG_SYSROOT_DIR=$(STAGING_DIR) DESTDIR=$(TARGET_DIR) install
-endef
+WESTEROS_SOC_PRE_BUILD_HOOKS += WESTEROS_SOC_ENTER_BUILD_DIR
 
 $(eval $(autotools-package))
