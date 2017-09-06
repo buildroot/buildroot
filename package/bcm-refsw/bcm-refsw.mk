eb19f1df9085ba6ddf45a3e4dc1738fb99463df0
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2017-09-06 10:52:36 +0200

    [bcm-refsw] aligned installed headers wih RKD and added cflags to the pkgconfig

--- package/bcm-refsw/bcm-refsw.mk
+++ package/bcm-refsw/bcm-refsw.mk
@@ -38,26 +38,31 @@ ifeq ($(BR2_PACKAGE_BCM_REFSW_PLATFORM_7437),y)
 BCM_REFSW_PLATFORM = 974371
 BCM_REFSW_PLATFORM_VC = v3d
 BCM_REFSW_PLATFORM_REV = A0
+BCM_REFSW_BCHP_CHIP = 7437
+BCM_REFSW_BCHP_VER_LOWER = a0
 BCM_REFSW_MAKE_ENV += \
 	NEXUS_USE_74371_XID="y"
 else ifeq ($(BR2_PACKAGE_BCM_REFSW_PLATFORM_7271),y)
 BCM_REFSW_PLATFORM = 97271
-BCM_REFSW_PLATFORM_REV = A0
+BCM_REFSW_PLATFORM_REV = B0
 BCM_REFSW_PLATFORM_VC = vc5
-else ifeq ($(BR2_PACKAGE_BCM_REFSW_PLATFORM_72604),y)
-BCM_REFSW_PLATFORM = 97260
-BCM_REFSW_PLATFORM_REV = A0
-BCM_REFSW_PLATFORM_VC = vc5
+BCM_REFSW_BCHP_CHIP = 7271
+BCM_REFSW_BCHP_VER_LOWER = b0
+
 else ifeq ($(BR2_PACKAGE_BCM_REFSW_PLATFORM_7250),y)
 BCM_REFSW_PLATFORM = 97250
 BCM_REFSW_PLATFORM_REV = B0
 BCM_REFSW_PLATFORM_VC = vc5
+BCM_REFSW_BCHP_CHIP = 7250
+BCM_REFSW_BCHP_VER_LOWER = b0
 BCM_REFSW_MAKE_ENV += NEXUS_USE_7250_SV=y
 else
 BCM_REFSW_PLATFORM = 97439
 BCM_REFSW_PLATFORM_REV = B0
 BCM_REFSW_MAKE_ENV += NEXUS_USE_7439_SFF=y
 BCM_REFSW_PLATFORM_VC = vc5
+BCM_REFSW_BCHP_CHIP = 7439
+BCM_REFSW_BCHP_VER_LOWER = b0
 endif
 BCM_REFSW_MAKE_ENV += \
 	NEXUS_ENDIAN=BSTD_ENDIAN_LITTLE
@@ -93,7 +98,7 @@ BCM_REFSW_MAKE_ENV += \
 	NEXUS_PLATFORM=$(BCM_REFSW_PLATFORM) \
 	NEXUS_MODE=proxy \
 	NEXUS_HEADERS=y \
-	NEXUS_EXTRA_CFLAGS="$(TARGET_CFLAGS)" \
+	NEXUS_EXTRA_CFLAGS="$(TARGET_CFLAGS) -Wno-error=undef" \
 	NEXUS_EXTRA_LDFLAGS="$(TARGET_LDFLAGS)" \
 	VCX=$(BCM_REFSW_PLATFORM_VC) \
 	V3D_EXTRA_CFLAGS="$(TARGET_CFLAGS)" \
@@ -228,11 +233,102 @@ define BCM_REFSW_BUILD_WAYLAND_EGL
 			APPLIBS_TARGET_INC_DIR=${BCM_REFSW_BIN}/include
 endef
 
+ifeq ($(BCM_REFSW_PLATFORM_VC),vc5)
+define BCM_REFSW_INSTALL_STAGING_WAYLAND_EGL_GPU
+		    $(INSTALL) -d -m 0755 $(STAGING_DIR)/usr/include/interface/khronos/include/
+		    $(INSTALL) -d -m 0755 $(STAGING_DIR)/usr/include/interface/khronos/include/bcg_abstract/
+		    
+			ln -sf ../../../EGL $(STAGING_DIR)/usr/include/interface/khronos/include/
+			ln -sf ../../../GLES $(STAGING_DIR)/usr/include/interface/khronos/include/
+			ln -sf ../../../GLES2 $(STAGING_DIR)/usr/include/interface/khronos/include/
+			ln -sf ../helpers $(STAGING_DIR)/usr/include/interface/khronos/include/
+			
+		    $(INSTALL) -m 0644 $(@D)/BSEAV/lib/gpu/vc5/platform/nexus/*.h $(STAGING_DIR)/usr/include/interface/khronos/include
+			$(INSTALL) -m 0644 $(@D)/BSEAV/lib/gpu/vc5/driver/libs/khrn/egl/platform/bcg_abstract/*.h $(STAGING_DIR)/usr/include/interface/khronos/include/bcg_abstract
+			$(INSTALL) -m 0644 $(@D)/BSEAV/lib/gpu/vc5/driver/libs/platform/bcg_abstract/*.h $(STAGING_DIR)/usr/include/interface/khronos/include/bcg_abstract
+			$(INSTALL) -m 0644 $(@D)/BSEAV/lib/gpu/vc5/driver/libs/platform/*.h $(STAGING_DIR)/usr/include/interface/khronos/include
+			
+			$(INSTALL) -d -m 0755 $(STAGING_DIR)/usr/include/vcos
+		    $(INSTALL) -d -m 0755 $(STAGING_DIR)/usr/include/vcos/include
+		    $(INSTALL) -d -m 0755 $(STAGING_DIR)/usr/include/vcos/posix
+	    	$(INSTALL) -d -m 0755 $(STAGING_DIR)/usr/include/vcos/gcc
+		
+			$(INSTALL) -m 0755 $(@D)/BSEAV/lib/gpu/vc5/driver/libs/core/vcos/include/*.h $(STAGING_DIR)/usr/include/vcos/include
+			$(INSTALL) -m 0755 $(@D)/BSEAV/lib/gpu/vc5/driver/libs/core/vcos/posix/*.h $(STAGING_DIR)/usr/include/vcos/posix
+			$(INSTALL) -m 0755 $(@D)/BSEAV/lib/gpu/vc5/driver/libs/core/vcos/gcc/*.h $(STAGING_DIR)/usr/include/vcos/gcc
+			
+			$(INSTALL) -d -m 0755 $(STAGING_DIR)/usr/include/libs
+			$(INSTALL) -d -m 0755 $(STAGING_DIR)/usr/include/libs/util
+			$(INSTALL) -d -m 0755 $(STAGING_DIR)/usr/include/libs/util/log
+			$(INSTALL) -d -m 0755 $(STAGING_DIR)/usr/include/libs/core
+			$(INSTALL) -d -m 0755 $(STAGING_DIR)/usr/include/libs/core/v3d
+			
+			$(INSTALL) -m 0644 $(@D)/BSEAV/lib/gpu/vc5/driver/libs/util/*.h $(STAGING_DIR)/usr/include/libs/util
+			$(INSTALL) -m 0644 $(@D)/BSEAV/lib/gpu/vc5/driver/libs/util/log/*.h $(STAGING_DIR)/usr/include/libs/util/log
+			$(INSTALL) -m 0644 $(@D)/BSEAV/lib/gpu/vc5/driver/libs/core/v3d/*.h $(STAGING_DIR)/usr/include/libs/core/v3d
+			
+			$(INSTALL) -m 0644 $(@D)/BSEAV/lib/gpu/vc5/driver/libs/platform/gmem.inl $(STAGING_DIR)/usr/include/interface/khronos/include
+			$(INSTALL) -m 0644 $(@D)/BSEAV/lib/gpu/vc5/driver/libs/platform/bcg_abstract/gmem_plat.inl $(STAGING_DIR)/usr/include/interface/khronos/include
+			
+			#$(INSTALL) the c file for wayland-egl building usage.
+			$(INSTALL) -m 0644 $(@D)/BSEAV/lib/gpu/vc5/platform/nexus/display_nexus.c $(STAGING_DIR)/usr/share/wayland-egl
+			$(INSTALL) -m 0644 $(@D)/BSEAV/lib/gpu/vc5/platform/nexus/*.c             $(STAGING_DIR)/usr/share/wayland-egl/nexus
+			$(INSTALL) -m 0644 $(@D)/BSEAV/lib/gpu/vc5/platform/nexus/*.h             $(STAGING_DIR)/usr/share/wayland-egl/nexus
+			$(INSTALL) -m 0644 $(@D)/BSEAV/lib/gpu/vc5/platform/common/*.c            $(STAGING_DIR)/usr/share/wayland-egl/common
+			$(INSTALL) -m 0644 $(@D)/BSEAV/lib/gpu/vc5/platform/common/*.h            $(STAGING_DIR)/usr/share/wayland-egl/common
+
+			if [ -e $(@D)/trellis/display/weston/include/EGL/eglext.h ]; then \
+				$(INSTALL) -m 0755 $(@D)/trellis/display/weston/include/EGL/eglext.h        $(STAGING_DIR)/usr/include/EGL; \
+				$(INSTALL) -m 0755 $(@D)/trellis/display/weston/include/EGL/eglext_brcm.h   $(STAGING_DIR)/usr/include/EGL; \
+			fi
+			if [ -e $(@D)/magnum/basemodules/chp/include/${BCM_REFSW_BCHP_CHIP}/rdb/${BCM_REFSW_BCHP_VER_LOWER}/bchp_common.h ]; then \
+				$(INSTALL) -m 0755 $(@D)/magnum/basemodules/chp/include/${BCM_REFSW_BCHP_CHIP}/rdb/${BCM_REFSW_BCHP_VER_LOWER}/bchp_common.h        $(STAGING_DIR)/usr/include; \
+			fi
+			if [ -e $(@D)/magnum/basemodules/chp/include/${BCM_REFSW_BCHP_CHIP}/rdb/${BCM_REFSW_BCHP_VER_LOWER}/bchp_v3d_hub_ctl.h ]; then \
+				$(INSTALL) -m 0755 $(@D)/magnum/basemodules/chp/include/${BCM_REFSW_BCHP_CHIP}/rdb/${BCM_REFSW_BCHP_VER_LOWER}/bchp_v3d_hub_ctl.h        $(STAGING_DIR)/usr/include; \
+			fi
+endef
+else
+define BCM_REFSW_INSTALL_STAGING_WAYLAND_EGL_GPU
+     #$(INSTALL) the c file for wayland-egl building usage.
+    $(INSTALL) -m 0644 $(@D)/BSEAV/lib/gpu/v3d/platform/nexus/*.c             $(STAGING_DIR)/usr/share/wayland-egl/nexus
+    $(INSTALL) -m 0644 $(@D)/BSEAV/lib/gpu/v3d/platform/common/*.c            $(STAGING_DIR)/usr/share/wayland-egl/common
+    $(INSTALL) -m 0644 $(@D)/BSEAV/lib/gpu/v3d/platform/nexus/*.h             $(STAGING_DIR)/usr/share/wayland-egl
+    $(INSTALL) -m 0644 $(@D)/BSEAV/lib/gpu/v3d/platform/common/*.h            $(STAGING_DIR)/usr/share/wayland-egl
+endef
+endif
+
 define BCM_REFSW_INSTALL_STAGING_WAYLAND_EGL
 	$(INSTALL) -m 644 -D $(WAYLAND_EGL_DIR)/lib_${BCM_REFSW_PLATFORM}_release/*.so $(STAGING_DIR)/usr/lib
 	$(INSTALL) -m 644 -D $(WAYLAND_EGL_DIR)/lib_${BCM_REFSW_PLATFORM}_release/libbcm_wayland_egl.so $(STAGING_DIR)/usr/lib/libwayland-egl.so
-	$(INSTALL) -m 644 package/bcm-refsw/wayland-egl.pc $(STAGING_DIR)/usr/lib/pkgconfig/
+	$(INSTALL) -m 644 package/bcm-refsw/${BCM_REFSW_PLATFORM_VC}/wayland-egl.pc $(STAGING_DIR)/usr/lib/pkgconfig/
 	$(INSTALL) -m 644 $(WAYLAND_EGL_DIR)/autogen/*.h $(STAGING_DIR)/usr/include/refsw/
+		
+	$(INSTALL) -d -m 0755 $(STAGING_DIR)/usr/share
+	$(INSTALL) -d -m 0755 $(STAGING_DIR)/usr/share/wayland-egl
+	$(INSTALL) -d -m 0755 $(STAGING_DIR)/usr/share/wayland-egl/common
+	$(INSTALL) -d -m 0755 $(STAGING_DIR)/usr/share/wayland-egl/nexus
+		
+	$(call BCM_REFSW_INSTALL_STAGING_WAYLAND_EGL_GPU,$(STAGING_DIR))
+			
+    #$(INSTALL) the c file for wayland-egl, cube/earth_es2 building usage.
+    $(INSTALL) -d -m 0755 $(STAGING_DIR)/usr/include/nxclient
+    $(INSTALL) -d -m 0755 $(STAGING_DIR)/usr/share/wayland-egl/common
+    $(INSTALL) -d -m 0755 $(STAGING_DIR)/usr/share/wayland-egl/cube
+    $(INSTALL) -d -m 0755 $(STAGING_DIR)/usr/share/wayland-egl/earth_es2
+    $(INSTALL) -d -m 0755 $(STAGING_DIR)/usr/bin/textures
+    $(INSTALL) -m 0644 $(@D)/nexus/nxclient/server/*.h                                    $(STAGING_DIR)/usr/include/nxclient
+    $(INSTALL) -m 0644 $(@D)/BSEAV/lib/gpu/applications/nexus/common/*             $(STAGING_DIR)/usr/share/wayland-egl/common
+    $(INSTALL) -m 0644 $(@D)/BSEAV/lib/gpu/applications/nexus/cube/cube.c          $(STAGING_DIR)/usr/share/wayland-egl/cube
+    $(INSTALL) -m 0644 $(@D)/BSEAV/lib/gpu/applications/nexus/earth_es2/*.c         $(STAGING_DIR)/usr/share/wayland-egl/earth_es2
+    $(INSTALL) -m 0644 $(@D)/BSEAV/lib/gpu/applications/nexus/earth_es2/*.h         $(STAGING_DIR)/usr/share/wayland-egl/earth_es2
+    $(INSTALL) -m 0755 $(@D)/BSEAV/lib/gpu/applications/nexus/earth_es2/textures/* $(STAGING_DIR)/usr/bin/textures
+
+    $(INSTALL) -m 0755 $(@D)/BSEAV/lib/utils/*.h $(STAGING_DIR)/usr/include
+		
+    $(INSTALL) -m 0644 $(@D)/rockford/lib/psip/include/*.h $(STAGING_DIR)/usr/include
+    $(INSTALL) -m 0644 $(@D)/nexus/lib/playback_ip/include/*.h $(STAGING_DIR)/usr/include
+    $(INSTALL) -m 0644 $(@D)/BSEAV/lib/tshdrbuilder/*.h $(STAGING_DIR)/usr/include
 endef
 
 define BCM_REFSW_INSTALL_TARGET_WAYLAND_EGL
@@ -300,7 +396,7 @@ define BCM_REFSW_INSTALL_KHRONOS
 	$(INSTALL) -m 644 ${BCM_REFSW_VCX_KHRN}/GLES/*.h $(STAGING_DIR)/usr/include/GLES/
 	$(INSTALL) -m 644 ${BCM_REFSW_VCX_KHRN}/GLES2/*.h $(STAGING_DIR)/usr/include/GLES2/
 	$(INSTALL) -m 644 ${BCM_REFSW_VCX_KHRN}/EGL/*.h $(STAGING_DIR)/usr/include/EGL/
-	$(INSTALL) -m 644 -D ${BCM_REFSW_VCX_KHRN}/KHR/khrplatform.h $(STAGING_DIR)/usr/include/KHR/khrplatform.h;
+	$(INSTALL) -m 644 ${BCM_REFSW_VCX_KHRN}/KHR/*.h $(STAGING_DIR)/usr/include/KHR/
 endef
 
 
@@ -322,9 +418,10 @@ define BCM_REFSW_INSTALL_STAGING_CMDS
 	$(INSTALL) -m 755 -d $(STAGING_DIR)/usr/include/GLES
 	$(INSTALL) -m 755 -d $(STAGING_DIR)/usr/include/GLES2
 	$(INSTALL) -m 755 -d $(STAGING_DIR)/usr/include/EGL
+	$(INSTALL) -m 755 -d $(STAGING_DIR)/usr/include/KHR
 	$(INSTALL) -m 755 -d $(STAGING_DIR)/usr/include/refsw
-	$(INSTALL) -m 644 package/bcm-refsw/egl.pc $(STAGING_DIR)/usr/lib/pkgconfig/egl.pc
-	$(INSTALL) -m 644 package/bcm-refsw/glesv2.pc $(STAGING_DIR)/usr/lib/pkgconfig/
+	$(INSTALL) -m 644 package/bcm-refsw/${BCM_REFSW_PLATFORM_VC}/egl.pc $(STAGING_DIR)/usr/lib/pkgconfig/egl.pc
+	$(INSTALL) -m 644 package/bcm-refsw/${BCM_REFSW_PLATFORM_VC}/glesv2.pc $(STAGING_DIR)/usr/lib/pkgconfig/
 	$(INSTALL) -m 644 $(BCM_REFSW_BIN)/include/*.h $(STAGING_DIR)/usr/include/refsw/
 	$(INSTALL) -m 644 $(BCM_REFSW_OUTPUT)/nexus/bin/include/*.h $(STAGING_DIR)/usr/include/refsw/
 	$(INSTALL) -m 644 $(BCM_REFSW_DIR)/nexus/nxclient/server/*.h $(STAGING_DIR)/usr/include/refsw/
