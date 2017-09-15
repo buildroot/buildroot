46b07c8a87f354527d4173d873b6674d99e4fbab
Baruch Siach <baruch@tkos.co.il>
2017-09-15 07:40:20 +0300

    libidn: add fix for CVE-2017-14062
    Add upstream patch fixing CVE-2017-14062:
    
    Integer overflow in the decode_digit function in puny_decode.c in
    Libidn2 before 2.0.4 allows remote attackers to cause a denial of
    service or possibly have unspecified other impact.
    
    This issue also affects libidn.
    
    Unfortunately, the patch also triggers reconf of the documentation
    subdirectory, since lib/punycode.c is listed in GDOC_SRC that is defined
    in doc/Makefile.am. Add autoreconf to handle that.
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 49cb795f7965328ce7a57cbc3736b0fc03919fe7)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libidn/libidn.mk
+++ package/libidn/libidn.mk
@@ -12,6 +12,8 @@ LIBIDN_CONF_OPTS = --disable-java --enable-csharp=no
 LIBIDN_DEPENDENCIES = host-pkgconf $(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext) $(if $(BR2_PACKAGE_LIBICONV),libiconv)
 LIBIDN_LICENSE = GPLv2+, GPLv3+, LGPLv3+
 LIBIDN_LICENSE_FILES = COPYINGv2 COPYINGv3 COPYING.LESSERv3
+# lib/punycode.c patch triggers reconf in doc/
+LIBIDN_AUTORECONF = YES
 
 define LIBIDN_REMOVE_BINARY
 	rm -f $(TARGET_DIR)/usr/bin/idn
