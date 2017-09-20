3d2195bf20513e250993503e48c9842d2879d2f2
Pierre Wielders <pierre@wielders.net>
2017-09-20 14:56:26 +0200

    [VIP] Link the settings in the buidroot config to the actual generated config for WPEFramework.

--- package/vip-sdk/vip-sdk.mk
+++ package/vip-sdk/vip-sdk.mk
@@ -4,4 +4,8 @@
 #
 ################################################################################
 
+define VIP_SDK_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 644 -D $(STAGING_DIR)/usr/lib/libnxclient.so $(TARGET_DIR)/usr/lib
+endef
+	
 $(eval $(virtual-package))
