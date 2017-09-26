c3a6d51c3148b804692fab0a5f22c7c2240f6dd3
albertd <albert@dahan.nl>
2017-09-26 16:37:23 +0200

    [wpeframework] move spotify plugin

--- package/wpe/wpeframework-spotify/wpeframework-spotify.mk
+++ package/wpe/wpeframework-spotify/wpeframework-spotify.mk
@@ -5,7 +5,7 @@
 ################################################################################
 WPEFRAMEWORK_SPOTIFY_VERSION = 5616ca7a7001aa51ccc7de63ec877e756d9e3120
 WPEFRAMEWORK_SPOTIFY_SITE_METHOD = git
-WPEFRAMEWORK_SPOTIFY_SITE = git@github.com:Metrological/WPEPluginsPOC.git
+WPEFRAMEWORK_SPOTIFY_SITE = git@github.com:WebPlatformForEmbedded/WPEPluginSpotify.git
 WPEFRAMEWORK_SPOTIFY_INSTALL_STAGING = YES
 WPEFRAMEWORK_SPOTIFY_DEPENDENCIES = wpeframework
 
