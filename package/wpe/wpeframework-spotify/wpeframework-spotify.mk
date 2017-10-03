c93727a870a700c920cfbb5558f03bf57797253c
wouterlucas <wouter@wouterlucas.com>
2017-10-03 12:24:11 -0700

    [wpeframework-spotify] bump to latest

--- package/wpe/wpeframework-spotify/wpeframework-spotify.mk
+++ package/wpe/wpeframework-spotify/wpeframework-spotify.mk
@@ -3,7 +3,7 @@
 # wpeframework-spotify
 #
 ################################################################################
-WPEFRAMEWORK_SPOTIFY_VERSION = 5616ca7a7001aa51ccc7de63ec877e756d9e3120
+WPEFRAMEWORK_SPOTIFY_VERSION = dc690340ce90d51db5ce78bfbed7a53444f4495e
 WPEFRAMEWORK_SPOTIFY_SITE_METHOD = git
 WPEFRAMEWORK_SPOTIFY_SITE = git@github.com:WebPlatformForEmbedded/WPEPluginSpotify.git
 WPEFRAMEWORK_SPOTIFY_INSTALL_STAGING = YES
