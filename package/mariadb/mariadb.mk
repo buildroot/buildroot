20595a8050e0a6e58438e9d564fcab70a7ab2b5a
Ryan Coe <bluemrp9@gmail.com>
2017-11-03 06:30:02 -0700

    mariadb: bump version to 10.1.28
    Release notes: https://mariadb.com/kb/en/mariadb-10128-release-notes/
    Changelog: https://mariadb.com/kb/en/mariadb-10128-changelog/
    
    Signed-off-by: Ryan Coe <bluemrp9@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    (cherry picked from commit ff614db18e0b0f34a1ed14ef6bee4bae522039f4)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/mariadb/mariadb.mk
+++ package/mariadb/mariadb.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MARIADB_VERSION = 10.1.26
+MARIADB_VERSION = 10.1.28
 MARIADB_SITE = https://downloads.mariadb.org/interstitial/mariadb-$(MARIADB_VERSION)/source
 MARIADB_LICENSE = GPLv2 (server), GPLv2 with FLOSS exception (GPL client library), LGPLv2 (LGPL client library)
 # Tarball no longer contains LGPL license text
