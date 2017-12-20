495d42cdcd1c49ac6e737942f5bb288a5bfce428
Haseenamol <haseenamol@tataelxsi.co.in>
2017-12-20 12:21:13 +0000

    host tool chain env added for ninja

--- package/widevine/widevine.mk
+++ package/widevine/widevine.mk
@@ -3,7 +3,7 @@
 # widevine
 #
 ################################################################################
-WIDEVINE_VERSION = a4998dc60e358c436ef56577f94ac2ae793f09ce
+WIDEVINE_VERSION = 315655c6ebf58a696b6dde78b14db7aad527e32f
 WIDEVINE_SITE = git@github.com:Metrological/widevine.git
 WIDEVINE_SITE_METHOD = git
 
@@ -22,6 +22,8 @@ export WV_CC=$(TARGET_CC)
 export WV_CXX=$(TARGET_CXX)
 export WV_AR=$(TARGET_AR)
 export WV_HOST_CXX_FLAGS = $(HOST_CXXFLAGS)
+export WV_HOST_CC = $(HOSTCC)
+export WV_HOST_CXX = $(HOSTCXX)
 
 define WIDEVINE_CONFIGURE_CMDS
       (cd $(@D);rm -rf out; rm -rf Makefile;\
@@ -30,22 +32,14 @@ define WIDEVINE_CONFIGURE_CMDS
        ./build.py arm)
 endef
 
-define WIDEVINE_BUILD_CMDS
-        export WIDEVINE_TARGET_DIR="$(TARGET_DIR)";\
-        export WIDEVINE_STAGING_DIR="$(STAGING_DIR)";\
-        make CROSS_COMPILE="$(TARGET_CROSS)" \
-        CXXFLAGS="$(TARGET_CXXFLAGS)" \
-        LDFLAGS="$(TARGET_LDFLAGS)  -L$(STAGING_DIR)/usr/lib" -C $(@D) -f Makefile
-endef
-
 define WIDEVINE_INSTALL_TARGET_CMDS
         cp $(@D)/out/arm/Debug/widevine_ce_cdm_unittest $(TARGET_DIR)/usr/bin
-        cp $(@D)/out/arm/Debug/lib.target/lib*.so $(TARGET_DIR)/usr/lib/
+        cp $(@D)/out/arm/Debug/lib*/lib*.so $(TARGET_DIR)/usr/lib/
 endef
 
 define WIDEVINE_INSTALL_STAGING_CMDS
         cp $(@D)/out/arm/Debug/widevine_ce_cdm_unittest $(STAGING_DIR)/usr/bin
-        cp $(@D)/out/arm/Debug/lib.target/lib*.so $(STAGING_DIR)/usr/lib/
+        cp $(@D)/out/arm/Debug/lib*/lib*.so $(STAGING_DIR)/usr/lib/
         cp $(@D)/cdm/include/*.h $(STAGING_DIR)/usr/include
         cp $(@D)/core/include/*.h $(STAGING_DIR)/usr/include
         mkdir -p $(STAGING_DIR)/usr/include/host
