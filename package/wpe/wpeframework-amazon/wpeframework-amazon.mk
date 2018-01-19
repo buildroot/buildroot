0237abbb8e162eb5d26465f6e7707faca98fd6aa
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2018-01-19 19:46:14 +0100

    [wpeframework-amazon] Bump version

--- package/wpe/wpeframework-amazon/wpeframework-amazon.mk
+++ package/wpe/wpeframework-amazon/wpeframework-amazon.mk
@@ -3,7 +3,7 @@
 # wpeframework-amazon
 #
 ################################################################################
-WPEFRAMEWORK_AMAZON_VERSION = ea46ea101f436a9e5c3f533732a8f9328b6ae388
+WPEFRAMEWORK_AMAZON_VERSION = 401cdaaaed43f3b56fb7965778bb0d8cfb48b973
 WPEFRAMEWORK_AMAZON_SITE_METHOD = git
 WPEFRAMEWORK_AMAZON_SITE = git@github.com:Metrological/WPEPluginAmazon.git
 WPEFRAMEWORK_AMAZON_INSTALL_STAGING = YES
