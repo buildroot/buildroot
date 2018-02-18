28041d13ef31001580dfd65d3ad43bf599034e21
albertd <albert@dahan.nl>
2018-02-18 08:40:01 +0100

    [wpeframework-ui] bump to latest version

--- package/wpe/wpeframework-ui/wpeframework-ui.mk
+++ package/wpe/wpeframework-ui/wpeframework-ui.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEFRAMEWORK_UI_VERSION = 572cb8bda9b19b180d65d24382f65deef39ee61d
+WPEFRAMEWORK_UI_VERSION = f1c68b25893b6e237c527097e1cbff28a4c5597b
 WPEFRAMEWORK_UI_SITE_METHOD = git
 WPEFRAMEWORK_UI_SITE = git@github.com:WebPlatformForEmbedded/WPEFrameworkUI.git
 WPEFRAMEWORK_UI_DEPENDENCIES = wpeframework wpeframework-plugins
