4bab3ecd6f6125fd1ed8eade451bb65c5c23ec3b
albertd <albert@dahan.nl>
2018-02-18 18:09:39 +0100

    [wpewebkit] bump to latest version

--- package/wpe/wpewebkit/wpewebkit.mk
+++ package/wpe/wpewebkit/wpewebkit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEWEBKIT_VERSION = 14ccfaad9461286eddcbff255744d4dbc547bc7b
+WPEWEBKIT_VERSION = e1472addc9b6100fea2eff9815638241c1cdccc2
 WPEWEBKIT_SITE = $(call github,WebPlatformForEmbedded,WPEWebKit,$(WPEWEBKIT_VERSION))
 
 WPEWEBKIT_INSTALL_STAGING = YES
