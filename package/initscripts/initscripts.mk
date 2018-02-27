e77cc2122484b73f77648dd017124fa73680e329
albertd <albert@dahan.nl>
2018-02-27 20:34:34 +0100

    [initscripts] support for non network handling

--- package/initscripts/initscripts.mk
+++ package/initscripts/initscripts.mk
@@ -4,9 +4,16 @@
 #
 ################################################################################
 
+ifeq ($(BR2_TARGET_GENERIC_NETWORK),y)
 define INITSCRIPTS_INSTALL_TARGET_CMDS
 	mkdir -p  $(TARGET_DIR)/etc/init.d
 	$(INSTALL) -D -m 0755 package/initscripts/init.d/* $(TARGET_DIR)/etc/init.d/
 endef
+else
+define INITSCRIPTS_INSTALL_TARGET_CMDS
+	mkdir -p  $(TARGET_DIR)/etc/init.d
+	$(INSTALL) -D -m 0755 package/initscripts/init.d/{rc?,S20urandom} $(TARGET_DIR)/etc/init.d/
+endef
+endif
 
 $(eval $(generic-package))
