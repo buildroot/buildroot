da478ebe700839881d8eae3af4c0243c525c2f18
albertd <albert@dahan.nl>
2018-03-15 11:10:35 +0100

    [wpeframework-plugins] bump to latest version

--- package/wpe/wpeframework-plugins/wpeframework-plugins.mk
+++ package/wpe/wpeframework-plugins/wpeframework-plugins.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEFRAMEWORK_PLUGINS_VERSION = 2d0c03f55d188a8d78cce2f7d03f7beb944f7527
+WPEFRAMEWORK_PLUGINS_VERSION = d20fd8aead1a73092535c078e85479f9961e2448
 WPEFRAMEWORK_PLUGINS_SITE_METHOD = git
 WPEFRAMEWORK_PLUGINS_SITE = git@github.com:WebPlatformForEmbedded/WPEFrameworkPlugins.git
 WPEFRAMEWORK_PLUGINS_INSTALL_STAGING = YES
