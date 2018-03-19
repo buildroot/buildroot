f0c4ad8de7851475ff2947a66add4a83d24bd95a
Pierre Wielders <pierre@wielders.net>
2018-03-19 10:20:40 +0100

    [PACKAGER] Add new plugin Packager.

---
+++ package/wpe/wpeframework-packager/wpeframework-packager.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# WPEFramework Packager 
+#
+################################################################################
+
+WPEFRAMEWORK_PACKAGER_VERSION = 59640a18b9fe97b192a63e09ea671788afb21679
+WPEFRAMEWORK_PACKAGER_SITE_METHOD = git
+WPEFRAMEWORK_PACKAGER_SITE = git@github.com:WebPlatformForEmbedded/WPEPluginPackager.git
+WPEFRAMEWORK_PACKAGER_INSTALL_STAGING = YES
+WPEFRAMEWORK_PACKAGER_DEPENDENCIES = wpeframework
+
+WPEFRAMEWORK_PACKAGER_CONF_OPTS += -DBUILD_REFERENCE=${WPEFRAMEWORK_PACKAGER_VERSION}
+
+ifeq ($(BR2_PACKAGE_WPEFRAMEWORK_DEBUG),y)
+# TODO: This recently stopped working, for now pass debug flags explicitly.
+#WPEFRAMEWORK_PACKAGER_CONF_OPTS += -DCMAKE_BUILD_TYPE=Debug
+WPEFRAMEWORK_PACKAGER_CONF_OPTS += -DCMAKE_CXX_FLAGS='-g -Og'
+endif
+
+$(eval $(cmake-package))
