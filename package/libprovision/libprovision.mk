6c57892dbc8e96e928c5b9a9b7f53a7cc95313fe
albertd <albert@dahan.nl>
2018-03-20 10:19:42 +0100

    [libprovision] bump to latest version

--- package/libprovision/libprovision.mk
+++ package/libprovision/libprovision.mk
@@ -4,22 +4,13 @@
 #
 ################################################################################
 
-ifeq ($(BR2_PACKAGE_CPPSDK),y)
-LIBPROVISION_VERSION = ce8eac774f653828d8fa817888ee2a69bc40ff44
-else
-LIBPROVISION_VERSION = ce8eac774f653828d8fa817888ee2a69bc40ff44
-endif
-
+LIBPROVISION_VERSION = f221973555f4ef1fc72c890751cbaa06bf806882
 LIBPROVISION_SITE_METHOD = git
 LIBPROVISION_SITE = git@github.com:Metrological/libprovision.git
 LIBPROVISION_LICENSE = PROPRIETARY
 LIBPROVISION_REDISTRIBUTE = NO
 LIBPROVISION_INSTALL_STAGING = YES
 
-ifeq ($(BR2_PACKAGE_CPPSDK),y)
-LIBPROVISION_DEPENDENCIES = openssl cppsdk
-else
 LIBPROVISION_DEPENDENCIES = openssl
-endif
 
 $(eval $(cmake-package))
