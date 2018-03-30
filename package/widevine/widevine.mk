f649b4f90ab4d200d9dc98c2f8ef52d0885dfaa6
Pierre Wielders <pierre@wielders.net>
2018-03-30 19:27:27 +0200

    [WIDEVINE] Bump WideVine CDM to 3.5/WPE platform.

--- package/widevine/widevine.mk
+++ package/widevine/widevine.mk
@@ -3,7 +3,7 @@
 # widevine
 #
 ################################################################################
-WIDEVINE_VERSION = 066c91ad1b7ac69315a0455e93cc242ad83a061c
+WIDEVINE_VERSION = 13e071face6c9292d223e3408a099153cee14f4b
 WIDEVINE_SITE = git@github.com:Metrological/widevine.git
 WIDEVINE_SITE_METHOD = git
 
