28feb86a08c1619e48c0ef0d30026b3597bf37b6
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2018-05-04 16:09:56 +0200

    Fix Broadcom wayland-egl build

--- package/wayland-egl-bnxs/wayland-egl-bnxs.mk
+++ package/wayland-egl-bnxs/wayland-egl-bnxs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WAYLAND_EGL_BNXS_VERSION = 34d42332cb4b43bfa656060e1a4314c856ff768a
+WAYLAND_EGL_BNXS_VERSION = b7c878b8d88abc324b6303ac61da44f702533909
 WAYLAND_EGL_BNXS_SITE_METHOD = git
 WAYLAND_EGL_BNXS_SITE = git@github.com:Metrological/wayland-egl-bnxs.git
 WAYLAND_EGL_BNXS_INSTALL_STAGING = YES
@@ -12,21 +12,41 @@ WAYLAND_EGL_BNXS_AUTORECONF = YES
 WAYLAND_EGL_BNXS_AUTORECONF_OPTS = "-Icfg"
 WAYLAND_EGL_BNXS_DEPENDENCIES = host-pkgconf host-autoconf wayland bcm-refsw
 
-WAYLAND_EGL_BNXS_CFLAGS = \
-	-I$(STAGING_DIR)/usr/include/interface/khronos/include/bcg_abstract/ \
-	-I$(STAGING_DIR)/usr/include/interface/khronos/include/ \
-	-I${STAGING_DIR}/usr/include/refsw/
-	
 WAYLAND_EGL_BNXS_CONF_OPTS = \
-	--prefix=/usr/ \
+    --prefix=/usr/ \
     --disable-silent-rules \
     --disable-dependency-tracking \
-    --enable-refsw_latest \
-    --enable-vc5	
+    --enable-refsw_latest
+
+WAYLAND_EGL_BNXS_INCLUDES += \
+	-I$(STAGING_DIR)/usr/include/interface/khronos/include/bcg_abstract/ \
+	-I$(STAGING_DIR)/usr/include/interface/khronos/include/ \
+	-I${STAGING_DIR}/usr/include/refsw/ \
+	-I$(STAGING_DIR)/usr/include/EGL/ \
+	-I$(STAGING_DIR)/usr/include/
+
+WAYLAND_EGL_BNXS_CFLAGS += \
+	${WAYLAND_EGL_BNXS_INCLUDES} \
+	-std=gnu99
+
+WAYLAND_EGL_BNXS_CXXFLAGS += \
+	${WAYLAND_EGL_BNXS_INCLUDES} \
+	-std=c++11
+
+ifeq ($(BR2_mipsel),y)
+	WAYLAND_EGL_BNXS_CONF_OPTS += --enable-xg1v3_v3d
+endif
+
+ifeq ($(BR2_arm),y)
+	WAYLAND_EGL_BNXS_CONF_OPTS += --enable-vc5
+	WAYLAND_EGL_BNXS_CFLAGS += -DVCX=5
+	WAYLAND_EGL_BNXS_CXXFLAGS += -DVCX=5
+
+endif
 
 WAYLAND_EGL_BNXS_MAKE_ENV = \
     REFSW_VERSION="$(STAGING_DIR)/usr/share/wayland-egl" \
-    PKG_CONFIG_SYSROOT_DIR="$(STAGING_DIR)" 
+    PKG_CONFIG_SYSROOT_DIR="$(STAGING_DIR)"
 
 WAYLAND_EGL_BNXS_MAKE_OPTS = \
 	CC="$(TARGET_CC)" \
@@ -35,8 +55,7 @@ WAYLAND_EGL_BNXS_MAKE_OPTS = \
 	CROSS_COMPILE="$(TARGET_CROSS)" \
 	CONFIG_PREFIX="$(TARGET_DIR)" \
     CFLAGS="$(TARGET_CFLAGS) $(WAYLAND_EGL_BNXS_CFLAGS)" \
-	CXXFLAGS="$(TARGET_CXXFLAGS) $(WAYLAND_EGL_BNXS_CFLAGS)" 
-     
+	CXXFLAGS="$(TARGET_CXXFLAGS) $(WAYLAND_EGL_BNXS_CXXFLAGS)"
 
 define WAYLAND_EGL_BNXS_RUN_AUTOCONF
 	mkdir -p $(@D)/cfg
@@ -46,6 +65,7 @@ WAYLAND_EGL_BNXS_PRE_CONFIGURE_HOOKS += WAYLAND_EGL_BNXS_RUN_AUTOCONF
 define WAYLAND_EGL_BNXS_GENERATE_PROTOCOL
 	SCANNER_TOOL=${HOST_DIR}/usr/bin/wayland-scanner $(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/protocol
 endef
+
 WAYLAND_EGL_BNXS_PRE_BUILD_HOOKS += WAYLAND_EGL_BNXS_GENERATE_PROTOCOL
 
 $(eval $(autotools-package))
--- package/westeros-sink/westeros-sink.mk
+++ package/westeros-sink/westeros-sink.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WESTEROS_SINK_VERSION = 9633867603dcfb2a5c71af71e7e1abd12f0c2ca3
+WESTEROS_SINK_VERSION = 1edcfb04d139f14a12231cf97d1c37338e97f515
 WESTEROS_SINK_SITE_METHOD = git
 WESTEROS_SINK_SITE = git://github.com/Metrological/westeros
 WESTEROS_SINK_INSTALL_STAGING = YES
@@ -33,7 +33,10 @@ else ifeq ($(BR2_PACKAGE_MARVELL_AMPSDK),y)
 else ifeq ($(BR2_PACKAGE_HAS_NEXUS),y)
 	WESTEROS_SINK_SUBDIR = brcm/westeros-sink
 	WESTEROS_SINK_DEPENDENCIES += gstreamer1
-	WESTEROS_SINK_CONF_OPTS += --enable-gstreamer1=yes
+	WESTEROS_SINK_CONF_OPTS += --enable-gstreamer1=yes \
+			CFLAGS="$(TARGET_CFLAGS) -I${STAGING_DIR}/usr/include/refsw -I${STAGING_DIR}/usr/include/refsw/bseav" \
+			CXXFLAGS="$(TARGET_CXXFLAGS) -I${STAGING_DIR}/usr/include/refsw -I${STAGING_DIR}/usr/include/refsw/bseav"
+	WESTEROS_SINK_MAKE_ENV += PKG_CONFIG_SYSROOT_DIR=${STAGING_DIR}
 endif
 
 define WESTEROS_SINK_RUN_AUTOCONF
