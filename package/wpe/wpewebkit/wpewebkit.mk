ec57a8ef1cbb16f7584b4871472dba18bc3d444f
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2018-06-13 22:17:02 +0200

    [wpewebkit] Add conditional patches for vss platforms

--- package/wpe/wpewebkit/wpewebkit.mk
+++ package/wpe/wpewebkit/wpewebkit.mk
@@ -292,4 +292,18 @@ endif
 
 RSYNC_VCS_EXCLUSIONS += --exclude LayoutTests --exclude WebKitBuild
 
+# Temporary fix for vss platforms
+ifeq ($(BR2_PACKAGE_VSS_SDK),y)
+WPEWEBKIT_PKGDIR = "$(TOP_DIR)/package/wpe/wpewebkit"
+
+define WPEWEBKIT_APPLY_LOCAL_PATCHES
+ $(APPLY_PATCHES) $(@D) $(WPEWEBKIT_PKGDIR) 0003-MSE2018_MSE_Conformance_Test_55_DelayedAACAudio.patch.conditional
+ $(APPLY_PATCHES) $(@D) $(WPEWEBKIT_PKGDIR) 0005-YT_Seek_To_Buffered_Position_Fix_For_BRCM.patch.conditional
+ $(APPLY_PATCHES) $(@D) $(WPEWEBKIT_PKGDIR) 0006-brcm-force-sink-av-factories.patch.conditional
+ $(APPLY_PATCHES) $(@D) $(WPEWEBKIT_PKGDIR) 0011-change-position-query-frequency-10ms.patch.conditional
+endef
+  
+WPEWEBKIT_POST_PATCH_HOOKS += WPEWEBKIT_APPLY_LOCAL_PATCHES 
+endif
+
 $(eval $(cmake-package))
