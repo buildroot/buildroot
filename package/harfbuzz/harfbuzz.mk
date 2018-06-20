153c74a380d48290050029ebbbac3dea045a2b27
modeveci <o.deveci@metrological.com>
2018-06-20 13:58:09 +0200

    [icu][harfbuzz]: remove comment-hashes in front of if conditions

--- package/harfbuzz/harfbuzz.mk
+++ package/harfbuzz/harfbuzz.mk
@@ -8,9 +8,9 @@ HARFBUZZ_VERSION = 1.4.4
 ifeq ($(BR2_PACKAGE_NETFLIX),y)
 HARFBUZZ_VERSION = 1.0.1
 endif
-#ifeq ($(BR2_PACKAGE_NETFLIX5),y)
+ifeq ($(BR2_PACKAGE_NETFLIX5),y)
 HARFBUZZ_VERSION = 1.4.2
-#endif
+endif
 HARFBUZZ_SITE = https://www.freedesktop.org/software/harfbuzz/release
 HARFBUZZ_SOURCE = harfbuzz-$(HARFBUZZ_VERSION).tar.bz2
 HARFBUZZ_LICENSE = MIT, ISC (ucdn library)
