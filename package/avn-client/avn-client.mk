489a9eb36bb5a74a2c94766e0e4a3dc77e82f028
HaseenaSainul <h.sainul@metrological.com>
2018-07-14 17:20:59 +0530

    AVN Client app with so creation support added

--- package/avn-client/avn-client.mk
+++ package/avn-client/avn-client.mk
@@ -15,9 +15,20 @@ export SYSROOTPATH=$(STAGING_DIR)
 
 AVN_CLIENT_MAKE_OPTS = ARCH=$(BR2_ARCH) CC="$(TARGET_CC)" CXX="$(TARGET_CXX)"
 
+ifeq ($(BR2_PACKAGE_AVN_CLIENT_APP),y)
+export AVNCLIENT_APP=true
 define AVN_CLIENT_INSTALL_IMAGE
 	cp -a $(@D)/output/linux/release/bin/mclient $(TARGET_DIR)/usr/bin
 endef
+else
+export AVNCLIENT_APP=false
+define AVN_CLIENT_INSTALL_IMAGE
+	cp -a $(@D)/output/linux/release/lib/libcloudtv_mclient.so $(TARGET_DIR)/usr/lib
+endef
+define AVN_CLIENT_INSTALL_STAGING_IMAGE
+	cp -a $(@D)/output/linux/release/lib/libcloudtv_mclient.so $(STAGING_DIR)/usr/lib
+endef
+endif
 
 define AVN_CLIENT_BUILD_CMDS
     	$(MAKE) -C $(@D) clean
@@ -28,4 +39,7 @@ define AVN_CLIENT_INSTALL_TARGET_CMDS
     	$(call AVN_CLIENT_INSTALL_IMAGE)
 endef
 
+define AVN_CLIENT_INSTALL_STAGING_CMDS
+        $(call AVN_CLIENT_INSTALL_STAGING_IMAGE)
+endef
 $(eval $(generic-package))
