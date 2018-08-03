20ff81e62ff2b474d52f39fda9e081ec3ae462e3
wouterlucas <wouter@wouterlucas.com>
2018-08-02 22:08:43 -0700

    [wpeframework-plugins] bump to latest

--- package/wpe/wpeframework-plugins/wpeframework-plugins.mk
+++ package/wpe/wpeframework-plugins/wpeframework-plugins.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEFRAMEWORK_PLUGINS_VERSION = 87bc5a391384a6a2ab60f2163ec7d7bc3dc75949
+WPEFRAMEWORK_PLUGINS_VERSION = c64c925f19b3283cfb72ecef7b23710462d9dc03
 WPEFRAMEWORK_PLUGINS_SITE = $(call github,WebPlatformForEmbedded,WPEFrameworkPlugins,$(WPEFRAMEWORK_PLUGINS_VERSION))
 WPEFRAMEWORK_PLUGINS_INSTALL_STAGING = YES
 WPEFRAMEWORK_PLUGINS_DEPENDENCIES = wpeframework libpng
