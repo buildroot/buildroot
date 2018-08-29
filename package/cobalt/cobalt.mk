69124138863c3db69c3b05d5e7c09ab06cf19212
Arunakiran Chennubhatla <arunakiran.chennubhatla@valuelabs.com>
2018-08-29 11:23:56 +0530

    Updated Cobalt's buildroot config to Cobalt 19

--- package/cobalt/cobalt.mk
+++ package/cobalt/cobalt.mk
@@ -4,17 +4,17 @@
 #
 ################################################################################
 
-COBALT_VERSION = 9eb2e829be81aab86f2e6b57eb035b6f10911301
+COBALT_VERSION = 8c7a8bd5b66160eb32bcc59d535665ae02961159
 COBALT_SITE_METHOD = git
 COBALT_SITE = git@github.com:Metrological/cobalt
 COBALT_INSTALL_STAGING = YES
 COBALT_DEPENDENCIES = alsa-lib gstreamer1 gst1-plugins-base gst1-plugins-good gst1-plugins-bad host-bison host-ninja
 
-export BUILDROOT_HOME=$(HOST_DIR)/usr
+export RASPI_HOME=$(HOST_DIR)/usr
 export PATH := $(HOST_DIR)/bin:$(HOST_DIR)/usr/bin:$(HOST_DIR)/usr/sbin:$(PATH)
 
-PLATFORM_DIR = wpe-rpi
-PLATFORM_QA_DIR = wpe-rpi_qa
+PLATFORMDIR = raspi-2
+PLATFORM_QA_DIR = raspi-2_qa
 
 ifeq ($(BR2_PACKAGE_COBALT_IMAGE_AS_LIB), y)
 export COBALT_EXECUTABLE_TYPE = shared_library
@@ -27,12 +27,12 @@ define COBALT_INSTALL_IMAGE
     cp -a $(@D)/src/out/$(PLATFORM_QA_DIR)/lib/libcobalt.so  $(TARGET_DIR)/usr/lib
 endef
 define COBALT_INSTALL_STAGING_IMAGE
-    mkdir -p $(STAGING_DIR)/usr/include/starboard/wpe/shared
-    mkdir -p $(STAGING_DIR)/usr/include/starboard/wpe/rpi
+    mkdir -p $(STAGING_DIR)/usr/include/starboard/raspi/shared
+    mkdir -p $(STAGING_DIR)/usr/include/starboard/raspi/2
 
     cp $(@D)/src/starboard/*.h $(STAGING_DIR)/usr/include/starboard/
-    cp $(@D)/src/starboard/wpe/rpi/*.h  $(STAGING_DIR)/usr/include/starboard/wpe/rpi
-    cp $(@D)/src/starboard/wpe/shared/*.h  $(STAGING_DIR)/usr/include/starboard/wpe/shared
+    cp $(@D)/src/starboard/raspi/2/*.h  $(STAGING_DIR)/usr/include/starboard/raspi/2
+    cp $(@D)/src/starboard/raspi/shared/*.h  $(STAGING_DIR)/usr/include/starboard/raspi/shared
 
     cp -a $(@D)/src/out/$(PLATFORM_QA_DIR)/lib/libcobalt.so  $(STAGING_DIR)/usr/lib
 endef
@@ -43,8 +43,8 @@ endef
 endif
 
 define COBALT_BUILD_CMDS
-    $(@D)/src/cobalt/build/gyp_cobalt -C qa $(PLATFORM_DIR)
-    $(BUILDROOT_HOME)/bin/ninja -C $(@D)/src/out/$(PLATFORM_QA_DIR) cobalt
+    $(@D)/src/cobalt/build/gyp_cobalt -C qa $(PLATFORMDIR)
+    $(RASPI_HOME)/bin/ninja -C $(@D)/src/out/$(PLATFORM_QA_DIR) cobalt
 endef
 
 define COBALT_INSTALL_TARGET_CMDS
