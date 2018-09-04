e39e52ac658a8e213732f0f06211bc7a7118ecb3
albertd <albert@dahan.nl>
2018-09-04 18:13:43 +0200

    [bcm] use default /www directory for webserver

--- board/bcm/post-build.sh
+++ board/bcm/post-build.sh
@@ -10,6 +10,6 @@ BOARD_DIR="$(dirname $0)"
 
 # Copy index.html page for WPE Framework
 if [ -f "${BOARD_DIR}/index.html" ]; then
-	mkdir -p "${TARGET_DIR}/var/www/"
-	cp -pf "${BOARD_DIR}/index.html" "${TARGET_DIR}/var/www/"
+	mkdir -p "${TARGET_DIR}/www/"
+	cp -pf "${BOARD_DIR}/index.html" "${TARGET_DIR}/www/"
 fi
