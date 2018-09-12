f0b50cbd1cb07ec7075535eee821b5d1c01c7f6e
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2018-09-12 20:13:46 +0200

    [board] update vss scripts

--- board/bcm/vss.sh
+++ board/bcm/vss.sh
@@ -22,19 +22,9 @@ do
 done < "${BOARD_DIR}/vss.txt"
 
 # Append missing folders
-echo "usr/lib/gstreamer-1.0" >> "${ROOTFS_FILES}"
 echo "usr/lib/gio" >> "${ROOTFS_FILES}"
 
 rsync -ar --files-from="${ROOTFS_FILES}" "${TARGET_DIR}" "${ROOTFS_INSTALL_DIR}"
-
-# select general libs that need priority over default version on the box
-mkdir -p "${ROOTFS_INSTALL_DIR}/usr/ml_libs/gstreamer-1.0"
-find "${ROOTFS_INSTALL_DIR}/usr/lib/" -maxdepth 1 -name "libgst*" -exec mv {} "${ROOTFS_INSTALL_DIR}/usr/ml_libs/" \;
-find "${ROOTFS_INSTALL_DIR}/usr/lib/" -maxdepth 1 -name "libbrcm*" -exec mv {} "${ROOTFS_INSTALL_DIR}/usr/ml_libs/" \;
-mv -f "${ROOTFS_INSTALL_DIR}/usr/lib/gstreamer-1.0" "${ROOTFS_INSTALL_DIR}/usr/ml_libs/"
-# workaround to prevent mixing libs and slow HTTPS
-find "${TARGET_DIR}/usr/lib/" -maxdepth 1 -name "libsoup*" -exec cp -pf {} "${ROOTFS_INSTALL_DIR}/usr/ml_libs/" \;
-
 
 # WPEFramework launcher
 cp -apf "${BOARD_DIR}/start.vss.sh" "${ROOTFS_DIR}/usr/bin/wpe"
