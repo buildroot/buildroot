805882979acb850c8c471dad0b55ff51bf2aefc6
Pierre Wielders <pierre@wielders.net>
2018-09-20 19:48:28 +0200

    [UMA] Copy artifacts needed for building full UMA image.

--- package/uma-sdk/uma-sdk.mk
+++ package/uma-sdk/uma-sdk.mk
@@ -3,5 +3,19 @@
 # uma-sdk
 #
 ################################################################################
+UMA_SDK_VERSION = master
+UMA_SDK_SITE = git@github.com:Metrological/OperatorDeliveries.git
+UMA_SDK_SITE_METHOD = git
+UMA_SDK_INSTALL_STAGING = YES
+UMA_SDK_INSTALL_TARGET = YES
+
+define UMA_SDK_INSTALL_STAGING_CMDS
+        cp -Rf $(@D)/nos/player/* $(STAGING_DIR)/
+endef
+
+define UMA_SDK_INSTALL_TARGET_CMDS
+        cp -Rf $(@D)/nos/player/* $(TARGET_DIR)/
+endef
+
 
 $(eval $(virtual-package))
