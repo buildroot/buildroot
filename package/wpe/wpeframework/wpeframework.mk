91529a5beda0bcaaaba9560ecc6196e8cbc4970d
albertd <albert@dahan.nl>
2018-10-03 09:29:20 +0200

    [wpeframework] bump to latest version

--- package/wpe/wpeframework/wpeframework.mk
+++ package/wpe/wpeframework/wpeframework.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEFRAMEWORK_VERSION = 92e60855ddb00bd5a82bc4fdbde2c6f77b20a698
+WPEFRAMEWORK_VERSION = d0a89526683fcec5c83614eb52c084c505f0d638
 WPEFRAMEWORK_SITE = $(call github,WebPlatformForEmbedded,WPEFramework,$(WPEFRAMEWORK_VERSION))
 WPEFRAMEWORK_INSTALL_STAGING = YES
 WPEFRAMEWORK_DEPENDENCIES = zlib
