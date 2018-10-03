99c2da26d74ec4d61ca9617c5528a1157caff382
albertd <albert@dahan.nl>
2018-10-03 09:30:07 +0200

    [wpeframework-cdmi] bump all modules to latest version

--- package/wpe/wpeframework-cdmi/wpeframework-cdmi-clearkey/wpeframework-cdmi-clearkey.mk
+++ package/wpe/wpeframework-cdmi/wpeframework-cdmi-clearkey/wpeframework-cdmi-clearkey.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEFRAMEWORK_CDMI_CLEARKEY_VERSION = e931c530883a46fb7e8f33b4ff7e841ab1762fb7
+WPEFRAMEWORK_CDMI_CLEARKEY_VERSION = 3fb37d8ee90a8af941a6ebeff6309f89ead60fd4
 WPEFRAMEWORK_CDMI_CLEARKEY_SITE_METHOD = git
 WPEFRAMEWORK_CDMI_CLEARKEY_SITE = git@github.com:WebPlatformForEmbedded/OCDM-Clearkey.git
 WPEFRAMEWORK_CDMI_CLEARKEY_INSTALL_STAGING = YES
