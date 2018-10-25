d46c2c6e637a35a7127565ae3ab6f3dfc870ad04
Pierre Wielders <pierre@wielders.net>
2018-10-25 17:16:22 +0200

    [ALIGN] Allign the latest SDK with the UMA build.

--- package/uma-sdk/uma-sdk.mk
+++ package/uma-sdk/uma-sdk.mk
@@ -3,19 +3,21 @@
 # uma-sdk
 #
 ################################################################################
-UMA_SDK_VERSION = master
-UMA_SDK_SITE = git@github.com:Metrological/OperatorDeliveries.git
+UMA_SDK_VERSION = 787f93d172ae7d01dbc5201b1014c20b0133b036
+UMA_SDK_SITE = git@github.com:Metrological/SDK_UMA.git
 UMA_SDK_SITE_METHOD = git
 UMA_SDK_INSTALL_STAGING = YES
 UMA_SDK_INSTALL_TARGET = YES
 
 define UMA_SDK_INSTALL_STAGING_CMDS
-	cp -Rpf $(@D)/nos/player/usr/include/*.h $(STAGING_DIR)/usr/include/
-	$(INSTALL) -m 0755 -D $(@D)/nos/player/usr/lib/libplayer.so $(STAGING_DIR)/usr/lib/
+	cp -Rpf $(@D)/usr/include/* $(STAGING_DIR)/usr/include/
+	ln -sf $(STAGING_DIR)/usr/include/NOSPlayer/Player.h $(STAGING_DIR)/usr/include/Player.h
+	cp -Rpf $(@D)/usr/lib/Player/* $(STAGING_DIR)/usr/lib/
+	cp -Rpf $(@D)/usr/lib/Nagra/* $(STAGING_DIR)/usr/lib/
 endef
 
 define UMA_SDK_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 -D $(@D)/nos/player/usr/lib/libplayer.so $(TARGET_DIR)/usr/lib/
+	$(INSTALL) -m 0755 -D $(@D)/usr/lib/Player/* $(TARGET_DIR)/usr/lib/
 endef
 
 $(eval $(generic-package))
--- package/wpe/wpeframework-cdmi/wpeframework-cdmi-nagra/wpeframework-cdmi-nagra.mk
+++ package/wpe/wpeframework-cdmi/wpeframework-cdmi-nagra/wpeframework-cdmi-nagra.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEFRAMEWORK_CDMI_NAGRA_VERSION = 615f202efe4f2de942190c806ee4ccf6dd46784a
+WPEFRAMEWORK_CDMI_NAGRA_VERSION = a1e7bde9d36757cc756ecab58e90d27a80b3da9a
 WPEFRAMEWORK_CDMI_NAGRA_SITE_METHOD = git
 WPEFRAMEWORK_CDMI_NAGRA_SITE = git@github.com:WebPlatformForEmbedded/OCDM-Nagra.git
 WPEFRAMEWORK_CDMI_NAGRA_INSTALL_STAGING = NO
