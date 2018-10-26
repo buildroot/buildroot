09b45f7549427dc29fc35519b3f4ce2b42c4cc5d
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2018-10-26 15:19:04 +0200

    [vgdrm] Switch to vss-sdk and bump verion

--- package/wpe/wpeframework-cdmi/wpeframework-cdmi-playready-vgdrm/wpeframework-cdmi-playready-vgrdm.mk
+++ package/wpe/wpeframework-cdmi/wpeframework-cdmi-playready-vgdrm/wpeframework-cdmi-playready-vgrdm.mk
@@ -1,13 +1,13 @@
 ################################################################################
 #
-# wpeframework-cdmi-playready-vgdrm
+# wpeframework-cdmi-playready-vgdrm 
 #
 ################################################################################
 
-WPEFRAMEWORK_CDMI_PLAYREADY_VGDRM_VERSION = 8d5467184f544c3b7f6a6226df58e96fb4be06bd
+WPEFRAMEWORK_CDMI_PLAYREADY_VGDRM_VERSION = 9d8ecdc48a35774f97d778411e8c0441003846c5
 WPEFRAMEWORK_CDMI_PLAYREADY_VGDRM_SITE_METHOD = git
 WPEFRAMEWORK_CDMI_PLAYREADY_VGDRM_SITE = git@github.com:WebPlatformForEmbedded/OCDM-Playready-VGDRM.git
 WPEFRAMEWORK_CDMI_PLAYREADY_VGDRM_INSTALL_STAGING = YES
-WPEFRAMEWORK_CDMI_PLAYREADY_VGDRM_DEPENDENCIES = wpeframework vgdrm
+WPEFRAMEWORK_CDMI_PLAYREADY_VGDRM_DEPENDENCIES = wpeframework vss-sdk
 
 $(eval $(cmake-package))
