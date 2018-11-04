a54a0b24edb4423a2303b2acbb4fdfb182f1ad7d
Charles Eidsness <charles@ccxtechnologies.com>
2018-11-04 07:35:53 -0500

    uprev mongodb to 3.4

--- package/mongodb/mongodb.mk
+++ package/mongodb/mongodb.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MONGODB_VERSION_BASE = 3.3.4
+MONGODB_VERSION_BASE = 3.4.17
 MONGODB_VERSION = r$(MONGODB_VERSION_BASE)
 MONGODB_SITE = $(call github,mongodb,mongo,$(MONGODB_VERSION))
 
