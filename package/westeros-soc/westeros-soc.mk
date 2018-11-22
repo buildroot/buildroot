532dc6a275b27dc92cf5eb1d85341ba81bafc013
HaseenaSainul <h.sainul@metrological.com>
2018-11-22 21:39:32 +0530

    [Westeros] repo of westeros changed to rdkcmf/westeros

--- package/westeros-soc/westeros-soc.mk
+++ package/westeros-soc/westeros-soc.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-WESTEROS_SOC_VERSION = 8ec4db488273a81720934bebe0ab27ab511cc249
+WESTEROS_SOC_VERSION = e7225b5f6066b65b6e50c5d2515b6ae3f7d7a801
 WESTEROS_SOC_SITE_METHOD = git
-WESTEROS_SOC_SITE = git://github.com/Metrological/westeros
+WESTEROS_SOC_SITE = git://github.com/rdkcmf/westeros
 WESTEROS_SOC_INSTALL_STAGING = YES
 WESTEROS_SOC_AUTORECONF = YES
 WESTEROS_SOC_AUTORECONF_OPTS = "-Icfg"
