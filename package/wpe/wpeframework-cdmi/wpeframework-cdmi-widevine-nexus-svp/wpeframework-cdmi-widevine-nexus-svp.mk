d9167e8a3f072a210d17c0e4c3e34911648bc0d9
modeveci <o.deveci@metrological.com>
2018-11-22 01:49:19 +0100

    [Widevine-Nexus]: Introducing Widevine broadcom SVP/Sage

---
+++ package/wpe/wpeframework-cdmi/wpeframework-cdmi-widevine-nexus-svp/wpeframework-cdmi-widevine-nexus-svp.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# wpeframework-cdmi-widevine-nexus-svp
+#
+################################################################################
+
+WPEFRAMEWORK_CDMI_WIDEVINE_NEXUS_SVP_VERSION = 02e67eec87fa7d2e8db6e5b331fd5687de2ca32a
+WPEFRAMEWORK_CDMI_WIDEVINE_NEXUS_SVP_SITE_METHOD = git
+WPEFRAMEWORK_CDMI_WIDEVINE_NEXUS_SVP_SITE = git@github.com:WebPlatformForEmbedded/OCDM-Widevine-Nexus-SVP.git
+WPEFRAMEWORK_CDMI_WIDEVINE_NEXUS_SVP_INSTALL_STAGING = NO
+WPEFRAMEWORK_CDMI_WIDEVINE_NEXUS_SVP_DEPENDENCIES = wpeframework
+
+ifeq ($(BR2_PACKAGE_BCM_REFSW),y)
+WPEFRAMEWORK_CDMI_WIDEVINE_NEXUS_SVP_DEPENDENCIES += bcm-refsw
+endif
+
+$(eval $(cmake-package))
