Hancock33/batocera.piboy

package/x11r7/xlib_libfontenc: add missing dependency on host-pkgconf
The xlib_libfontenc configure.ac uses PKG_CHECK_MODULES(), but the
Buildroot package does not have a dependency on host-pkgconf. This
causes a build failure with per-package host/target folders, or if one
builds just with "make xlib_libfontenc", which is why it was never
detected by the autobuilders.

Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

package/x11r7/xlib_libfontenc/xlib_libfontenc.mk
@@ -10,8 +10,8 @@ XLIB_LIBFONTENC_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBFONTENC_LICENSE = MIT
 XLIB_LIBFONTENC_LICENSE_FILES = COPYING
 XLIB_LIBFONTENC_INSTALL_STAGING = YES
-XLIB_LIBFONTENC_DEPENDENCIES = zlib xorgproto
-HOST_XLIB_LIBFONTENC_DEPENDENCIES = host-zlib host-xorgproto
+XLIB_LIBFONTENC_DEPENDENCIES = zlib xorgproto host-pkgconf
+HOST_XLIB_LIBFONTENC_DEPENDENCIES = host-zlib host-xorgproto host-pkgconf
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))

buildroot/buildroot

(cherry picked from commit 7b1238055c2d369df335711c04cf8007c5b7edba)

package/x11r7/xlib_libfontenc/xlib_libfontenc.mk
@@ -10,8 +10,8 @@ XLIB_LIBFONTENC_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBFONTENC_LICENSE = MIT
 XLIB_LIBFONTENC_LICENSE_FILES = COPYING
 XLIB_LIBFONTENC_INSTALL_STAGING = YES
-XLIB_LIBFONTENC_DEPENDENCIES = zlib xproto_xproto
-HOST_XLIB_LIBFONTENC_DEPENDENCIES = host-zlib host-xproto_xproto
+XLIB_LIBFONTENC_DEPENDENCIES = zlib xproto_xproto host-pkgconf
+HOST_XLIB_LIBFONTENC_DEPENDENCIES = host-zlib host-xproto_xproto host-pkgconf
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))