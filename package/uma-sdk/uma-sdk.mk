a7d987f7e32d53689e4dab831cba09dc52994f18
Marcel Fransen <m.fransen@metrological.com>
2018-11-27 10:52:59 +0100

    [NAGRA] Bump UMA

--- package/uma-sdk/uma-sdk.mk
+++ package/uma-sdk/uma-sdk.mk
@@ -3,7 +3,7 @@
 # uma-sdk
 #
 ################################################################################
-UMA_SDK_VERSION = 5e70f06a5b627deab51ca24580802dbe8e215f25
+UMA_SDK_VERSION = 48f894f03ef963bc427d560a4625818dc67ac99e
 UMA_SDK_SITE = git@github.com:Metrological/SDK_UMA.git
 UMA_SDK_SITE_METHOD = git
 UMA_SDK_INSTALL_STAGING = YES
