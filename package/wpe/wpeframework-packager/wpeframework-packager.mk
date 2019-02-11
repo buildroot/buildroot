7cbf817d7f7b710fe7bb79190073a56e450ee6de
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2019-02-11 15:20:37 +0100

    [wpeframework-packager] version bump + cleanup

--- package/wpe/wpeframework-packager/wpeframework-packager.mk
+++ package/wpe/wpeframework-packager/wpeframework-packager.mk
@@ -1,21 +1,15 @@
 ################################################################################
 #
-# WPEFramework Packager 
+# wpeframework-packager 
 #
 ################################################################################
 
-WPEFRAMEWORK_PACKAGER_VERSION = 59640a18b9fe97b192a63e09ea671788afb21679
+WPEFRAMEWORK_PACKAGER_VERSION = 8401322e9fa285373fef5e546fd4db00bed87cf4
 WPEFRAMEWORK_PACKAGER_SITE_METHOD = git
 WPEFRAMEWORK_PACKAGER_SITE = git@github.com:WebPlatformForEmbedded/WPEPluginPackager.git
 WPEFRAMEWORK_PACKAGER_INSTALL_STAGING = YES
 WPEFRAMEWORK_PACKAGER_DEPENDENCIES = wpeframework opkg
 
 WPEFRAMEWORK_PACKAGER_CONF_OPTS += -DBUILD_REFERENCE=${WPEFRAMEWORK_PACKAGER_VERSION}
-
-ifeq ($(BR2_PACKAGE_WPEFRAMEWORK_DEBUG),y)
-# TODO: This recently stopped working, for now pass debug flags explicitly.
-#WPEFRAMEWORK_PACKAGER_CONF_OPTS += -DCMAKE_BUILD_TYPE=Debug
-WPEFRAMEWORK_PACKAGER_CONF_OPTS += -DCMAKE_CXX_FLAGS='-g -Og'
-endif
 
 $(eval $(cmake-package))
