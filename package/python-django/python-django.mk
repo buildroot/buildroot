2c22ecefa676640ff4b0ec20d6df320a800d2fa6
Peter Korsgaard <peter@korsgaard.com>
2019-02-21 17:11:10 +0100

    package/python-django: security bump to version 1.11.20
    Fixes the following security issues:
    
    CVE-2019-6975: Memory exhaustion in django.utils.numberformat.format()
    
    If django.utils.numberformat.format() – used by contrib.admin as well as the
    the floatformat, filesizeformat, and intcomma templates filters – received a
    Decimal with a large number of digits or a large exponent, it could lead to
    significant memory usage due to a call to '{:f}'.format().
    
    To avoid this, decimals with more than 200 digits are now formatted using
    scientific notation.
    
    https://docs.djangoproject.com/en/2.1/releases/1.11.19/
    
    1.11.19 contained a packaging error, fixed by 1.11.20:
    
    https://docs.djangoproject.com/en/2.1/releases/1.11.20/
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 1.11.18
+PYTHON_DJANGO_VERSION = 1.11.20
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/90/84/7981bdfcfa80fe81df5325899f9fc1cbebce1fbe4fac092a32dca00d0ab2
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/99/2a/6cb6fdae67a101e19cd02b1af75131eee51b8dcd0cc22c9cfdd2270b5715
+
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
 PYTHON_DJANGO_SETUP_TYPE = setuptools
