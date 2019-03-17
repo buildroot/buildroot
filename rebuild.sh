5ecece14e377a5363e749ea75cb3476b58b2073b
Paul Cercueil <paul@crapouillou.net>
2019-03-17 23:00:58 +0100

    Add rebuild.sh script
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

---
+++ rebuild.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -e
+
+if [ -z "$CONFIG" ] ; then
+	echo "\$CONFIG not set. Please set it to the variant to build."
+	echo "Valid values are: gcw0, rs90"
+	exit 1
+fi
+
+# Use the default config.
+make od_${CONFIG}_defconfig BR2_EXTERNAL=board/opendingux
+
+# Clear the install location.
+echo "Clearing install location..."
+mkdir -p /opt/${CONFIG}-toolchain
+rm -rf /opt/${CONFIG}-toolchain/*
+
+# Clear the build location.
+echo "Clearing build location..."
+rm -rf output/
+
+# Perform the build.
+echo "Starting build..."
+nice make BR2_EXTERNAL=board/opendingux
+
+# Create packages.
+echo "Creating packages..."
+tar -C/opt --exclude='.fakeroot.*' -jcf opendingux-${CONFIG}-toolchain.`date +'%Y-%m-%d'`.tar.bz2 ${CONFIG}-toolchain
