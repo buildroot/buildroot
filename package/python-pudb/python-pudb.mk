4c3d00fefc1ce5734527453390b4f7461ce1d47f
charles <charles@ccxtechnologies.com>
2019-04-01 09:30:39 -0400

    uprev pudb

--- package/python-pudb/python-pudb.mk
+++ package/python-pudb/python-pudb.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PUDB_VERSION = 2017.1.2
+PYTHON_PUDB_VERSION = 2018.1
 PYTHON_PUDB_SOURCE = pudb-$(PYTHON_PUDB_VERSION).tar.gz
-PYTHON_PUDB_SITE = https://pypi.python.org/packages/d3/8a/e0fa18cf6f939a63364117546c9bd933add800642004ad40fd5f5bdba0c6
+PYTHON_PUDB_SITE = https://files.pythonhosted.org/packages/d1/e9/d27fb1a5f00c722247b8003c9a81240e49c01dfa6f7fb510d19355a48679
 PYTHON_PUDB_SETUP_TYPE = setuptools
 PYTHON_PUDB_LICENSE = MIT
 PYTHON_PUDB_LICENSE_FILES = LICENSE
