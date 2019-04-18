446005a1fe866a745f55e33cd2c2c239bceeb33a
Pawel Stanek <p.stanek@metrological.com>
2019-04-18 13:11:04 +0200

    [wpeframework-plugins] Point at 2.22 friendy branch.

--- package/wpe/wpeframework-plugins/wpeframework-plugins.mk
+++ package/wpe/wpeframework-plugins/wpeframework-plugins.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEFRAMEWORK_PLUGINS_VERSION = 9532e34c1ef2912b9ed6c7d7f0b75eb6f8b0b205 
+WPEFRAMEWORK_PLUGINS_VERSION = ccf4ad6a0b6e1696263a89029bc01688b6490ac0
 WPEFRAMEWORK_PLUGINS_SITE = $(call github,WebPlatformForEmbedded,WPEFrameworkPlugins,$(WPEFRAMEWORK_PLUGINS_VERSION))
 WPEFRAMEWORK_PLUGINS_INSTALL_STAGING = YES
 WPEFRAMEWORK_PLUGINS_DEPENDENCIES = wpeframework libpng
