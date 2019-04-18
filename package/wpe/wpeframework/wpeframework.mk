db911c69ee50b68a4dd7888df7e5e92bbe5705fa
Pawel Stanek <p.stanek@metrological.com>
2019-04-18 13:10:08 +0200

    [wpeframework] Bump version

--- package/wpe/wpeframework/wpeframework.mk
+++ package/wpe/wpeframework/wpeframework.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEFRAMEWORK_VERSION = 693528fbed314271350752e3fa115eb1d3679fe8 
+WPEFRAMEWORK_VERSION = 8ebe09fd2c5ce3921bd1c860652d9e6b2f615d1f
 WPEFRAMEWORK_SITE = $(call github,WebPlatformForEmbedded,WPEFramework,$(WPEFRAMEWORK_VERSION))
 WPEFRAMEWORK_INSTALL_STAGING = YES
 WPEFRAMEWORK_DEPENDENCIES = zlib $(call qstrip,$(BR2_PACKAGE_SDK_INSTALL))
