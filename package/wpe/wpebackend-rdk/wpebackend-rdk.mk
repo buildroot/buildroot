9781454325c14f48fb09bc85386bad5424d8de96
Pierre Wielders <pierre@wielders.net>
2019-05-28 11:13:24 +0200

    [WPEBACKEND-RDK] Limit the maximum FPS to a configured value (default 60FPS)

--- package/wpe/wpebackend-rdk/wpebackend-rdk.mk
+++ package/wpe/wpebackend-rdk/wpebackend-rdk.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEBACKEND_RDK_VERSION = 09e4643651cc8855a57c620d869ceea7e014b67a
+WPEBACKEND_RDK_VERSION = 1f433a07b784ee85481f52b3b5be1cc62670e7e9
 WPEBACKEND_RDK_SITE = $(call github,WebPlatformForEmbedded,WPEBackend-rdk,$(WPEBACKEND_RDK_VERSION))
 WPEBACKEND_RDK_INSTALL_STAGING = YES
 WPEBACKEND_RDK_DEPENDENCIES = wpebackend libglib2
