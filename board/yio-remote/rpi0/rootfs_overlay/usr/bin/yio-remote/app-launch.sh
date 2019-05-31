744e3416348cac8057837e04c4a08823dde89315
Marton Borzak <hello@martonborzak.com>
2019-05-31 18:02:38 +0200

    Update

--- board/yio-remote/rpi0/rootfs_overlay/usr/bin/yio-remote/app-launch.sh
+++ board/yio-remote/rpi0/rootfs_overlay/usr/bin/yio-remote/app-launch.sh
@@ -2,7 +2,3 @@
 mount /dev/mmcblk0p1 /mnt/boot
 export QT_QPA_EVDEV_TOUCHSCREEN_PARAMETERS=/dev/input/event0:rotate=90:invertx
 /usr/bin/yio-remote/remote &
-#sleep 5
-#/usr/bin/standby_control.py &
-#sleep 4
-#/usr/bin/battery.py -l &
