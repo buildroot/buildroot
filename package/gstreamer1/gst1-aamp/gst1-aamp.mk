d2878ac7f15a9a6603082961f4414c5c8e6ecb07
HaseenaSainul <h.sainul@metrological.com>
2019-06-04 22:41:33 +0530

    [Aamp] package selection changes

--- package/gstreamer1/gst1-aamp/gst1-aamp.mk
+++ package/gstreamer1/gst1-aamp/gst1-aamp.mk
@@ -13,7 +13,7 @@ GST1_AAMP_DEPENDENCIES = gstreamer1 gst1-plugins-base aamp
 ifeq ($(BR2_PACKAGE_WPEFRAMEWORK_CDMI), y)
 GST1_AAMP_CONF_OPTS += \
     -DCMAKE_DASH_DRM=ON \
-    -DCMAKE_USE_OPENCDM=ON
+    -DCMAKE_USE_OPENCDM_ADAPTER=ON
 endif
 
 $(eval $(cmake-package))
