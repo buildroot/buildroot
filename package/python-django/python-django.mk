da01bac9cccb9786eec4ae67a8f4cbd783835329
Pierre-Jean Texier <pjtexier@koncepto.io>
2019-07-08 22:42:21 +0200

    package/python-django: bump to version 2.2.3
    See https://docs.djangoproject.com/en/2.2/releases/2.2.3/
    
    Also, 2.2.x is the new LTS series.
    
    Signed-off-by: Pierre-Jean Texier <pjtexier@koncepto.io>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 0ef236c72606d3dcdb0018740c365589e64461bc)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 2.1.15
+PYTHON_DJANGO_VERSION = 2.2.3
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/a5/ea/a3424e68851acb44a1f8f823dc32ee3eb10b7fda474b03d527f7e666b443
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/d3/20/b447eb3d820e0d05fe83cbfb016842bd8da35f4b0f83498dca43d02aebc3
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
 PYTHON_DJANGO_SETUP_TYPE = setuptools
