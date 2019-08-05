f85a5ae0cd01af3cd15270bb79882d1b324d3cc4
HaseenaSainul <h.sainul@metrological.com>
2019-08-05 20:13:54 +0530

    Spark: added slite3 also in the build dependency

--- package/spark/spark.mk
+++ package/spark/spark.mk
@@ -8,7 +8,7 @@ SPARK_SITE_METHOD = git
 SPARK_SITE = git://github.com/pxscene/pxCore
 SPARK_INSTALL_STAGING = YES
 
-SPARK_DEPENDENCIES = openssl freetype util-linux libpng libcurl pxcore-libnode giflib ca-certificates
+SPARK_DEPENDENCIES = openssl freetype util-linux libpng libcurl pxcore-libnode giflib ca-certificates sqlite
 
 SPARK_CONF_OPTS += \
     -DBUILD_SHARED_LIBS=OFF \
