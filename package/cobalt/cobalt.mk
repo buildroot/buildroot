aa47949a8ea8b5e865ecb43e962ad8c736b02a47
Neeraj Deshpande <n.deshpande@metrological.com>
2019-09-30 14:17:21 +0530

    Modified cobalt package for cobalt 20 repository.

--- package/cobalt/cobalt.mk
+++ package/cobalt/cobalt.mk
@@ -4,17 +4,17 @@
 #
 ################################################################################
 
-COBALT_VERSION = 8c7a8bd5b66160eb32bcc59d535665ae02961159
+COBALT_VERSION = 6791bf3a0168b54a8c3a5abde830b0690ce801fc
 COBALT_SITE_METHOD = git
 COBALT_SITE = git@github.com:Metrological/cobalt
 COBALT_INSTALL_STAGING = YES
 COBALT_DEPENDENCIES = alsa-lib gstreamer1 gst1-plugins-base gst1-plugins-good gst1-plugins-bad host-bison host-ninja
 
-export RASPI_HOME=$(HOST_DIR)/usr
+export BUILDROOT_HOME=$(HOST_DIR)/usr
 export PATH := $(HOST_DIR)/bin:$(HOST_DIR)/usr/bin:$(HOST_DIR)/usr/sbin:$(PATH)
 
-PLATFORMDIR = raspi-2
-PLATFORM_QA_DIR = raspi-2_qa
+PLATFORMDIR = wpe-rpi
+PLATFORM_QA_DIR = wpe-rpi_qa
 
 ifeq ($(BR2_PACKAGE_COBALT_IMAGE_AS_LIB), y)
 export COBALT_EXECUTABLE_TYPE = shared_library
@@ -22,17 +22,23 @@ else
 export COBALT_EXECUTABLE_TYPE = executable
 endif
 
+ifeq ($(BR2_PACKAGE_WPEFRAMEWORK_COMPOSITORCLIENT),y)
+export COBALT_USE_COMPOSITOR=y	
+else
+export COBALT_USE_COMPOSITOR=n	
+endif
+
 ifeq ($(BR2_PACKAGE_COBALT_IMAGE_AS_LIB), y)
 define COBALT_INSTALL_IMAGE
     cp -a $(@D)/src/out/$(PLATFORM_QA_DIR)/lib/libcobalt.so  $(TARGET_DIR)/usr/lib
 endef
 define COBALT_INSTALL_STAGING_IMAGE
-    mkdir -p $(STAGING_DIR)/usr/include/starboard/raspi/shared
-    mkdir -p $(STAGING_DIR)/usr/include/starboard/raspi/2
+    mkdir -p $(STAGING_DIR)/usr/include/starboard/wpe/shared
+    mkdir -p $(STAGING_DIR)/usr/include/starboard/wpe/rpi
 
     cp $(@D)/src/starboard/*.h $(STAGING_DIR)/usr/include/starboard/
-    cp $(@D)/src/starboard/raspi/2/*.h  $(STAGING_DIR)/usr/include/starboard/raspi/2
-    cp $(@D)/src/starboard/raspi/shared/*.h  $(STAGING_DIR)/usr/include/starboard/raspi/shared
+    cp $(@D)/src/starboard/wpe/rpi/*.h  $(STAGING_DIR)/usr/include/starboard/wpe/rpi
+    cp $(@D)/src/starboard/wpe/shared/*.h  $(STAGING_DIR)/usr/include/starboard/wpe/shared
 
     cp -a $(@D)/src/out/$(PLATFORM_QA_DIR)/lib/libcobalt.so  $(STAGING_DIR)/usr/lib
 endef
@@ -44,7 +50,7 @@ endif
 
 define COBALT_BUILD_CMDS
     $(@D)/src/cobalt/build/gyp_cobalt -C qa $(PLATFORMDIR)
-    $(RASPI_HOME)/bin/ninja -C $(@D)/src/out/$(PLATFORM_QA_DIR) cobalt
+    $(BUILDROOT_HOME)/bin/ninja -C $(@D)/src/out/$(PLATFORM_QA_DIR) cobalt
 endef
 
 define COBALT_INSTALL_TARGET_CMDS
