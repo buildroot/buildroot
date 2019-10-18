2cceaec12c29a1f2adc94d82f989c9251ba8d4cc
¨Divya_563¨ <divdus@gmail.com>
2019-10-18 15:44:55 +0200

    Switching to Unified GST version

--- package/gstreamer1/gst1-bcm/gst1-bcm.mk
+++ package/gstreamer1/gst1-bcm/gst1-bcm.mk
@@ -6,7 +6,7 @@
 
 ifneq ($(filter y,$(BR2_PACKAGE_GST1_BCM_UNIFIED_VERSION)),)
 GST1_BCM_SITE = git@github.com:Metrological/bcm-gstreamer.git
-GST1_BCM_VERSION = 4bd348f3a01fbba830e80ae3b30ac4eccdef1da2
+GST1_BCM_VERSION = 17cff8a7ab92dbbac5c30415be52f705929b7953
 else
 GST1_BCM_SITE = git@github.com:Metrological/gstreamer-plugins-soc.git
 
