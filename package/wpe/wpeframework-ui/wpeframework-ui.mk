34c1828d1e74d35fdba3132d28157caead6b3442
wouterlucas <wouter@wouterlucas.com>
2019-10-23 13:19:36 -0700

    [wpeframework-ui] bump to latest

--- package/wpe/wpeframework-ui/wpeframework-ui.mk
+++ package/wpe/wpeframework-ui/wpeframework-ui.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEFRAMEWORK_UI_VERSION = ef53dcfa8d9dd4bf0894caf1367ab6fe2c2a48ee 
+WPEFRAMEWORK_UI_VERSION = eba82e1336cc669161ae5145ce8d05ba856ab592
 WPEFRAMEWORK_UI_SITE = $(call github,WebPlatformForEmbedded,WPEFrameworkUI,$(WPEFRAMEWORK_UI_VERSION))
 WPEFRAMEWORK_UI_DEPENDENCIES = wpeframework wpeframework-plugins
 
@@ -17,7 +17,7 @@ WPEFRAMEWORK_UI_BUILD_CMDS = true
 define WPEFRAMEWORK_UI_INSTALL_TARGET_CMDS
 	rm -rf $(TARGET_DIR)/usr/share/WPEFramework/Controller/UI
 	mkdir -p $(TARGET_DIR)/usr/share/WPEFramework/Controller/UI
-	cp -r $(@D)/build/* $(TARGET_DIR)/usr/share/WPEFramework/Controller/UI
+	cp -r $(@D)/dist/* $(TARGET_DIR)/usr/share/WPEFramework/Controller/UI
 endef
 
 $(eval $(generic-package))
