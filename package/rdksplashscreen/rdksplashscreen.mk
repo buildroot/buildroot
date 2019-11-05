06b76ce1a554e6fcacb69ec2a0b9c554f6beb558
wouterlucas <wouter@wouterlucas.com>
2019-11-04 23:51:50 -0800

    [rdksplashscreen] bump and enable set operator/url options

--- package/rdksplashscreen/rdksplashscreen.mk
+++ package/rdksplashscreen/rdksplashscreen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RDKSPLASHSCREEN_VERSION = 720f13e9ff59d8470bb869d927c83e199768c36d
+RDKSPLASHSCREEN_VERSION = 47a633ede5cea5f6f4efd3370640de9a68542fb2
 RDKSPLASHSCREEN_SITE = $(call github,WebPlatformForEmbedded,RDKSplashScreen,$(RDKSPLASHSCREEN_VERSION))
 RDKSPLASHSCREEN_DEPENDENCIES = wpeframework wpeframework-plugins
 
@@ -18,5 +18,23 @@ define RDKSPLASHSCREEN_INSTALL_TARGET_CMDS
 	rm -rf $(TARGET_DIR)/www/*
 	cp -r $(@D)/* $(TARGET_DIR)/www/
 endef
+
+define RDKSPLASHSCREEN_WRITE_URL
+        cp -r $(@D)/static/config/config.url.in $(TARGET_DIR)/www/config.json
+        sed -i -e s,%url%,$(BR2_PACKAGE_RDKSPLASHSCREEN_URL),g $(TARGET_DIR)/www/config.json
+endef
+
+define RDKSPLASHSCREEN_WRITE_OPERATOR
+	cp -r $(@D)/static/config/config.operator.in $(TARGET_DIR)/www/config.json
+	sed -i -e s,%operator%,$(BR2_PACKAGE_RDKSPLASHSCREEN_OPERATOR),g $(TARGET_DIR)/www/config.json
+endef
+
+ifneq ($(BR2_PACKAGE_RDKSPLASHSCREEN_URL),"")
+RDKSPLASHSCREEN_POST_INSTALL_TARGET_HOOKS += RDKSPLASHSCREEN_WRITE_URL
+endif
+
+ifneq ($(BR2_PACKAGE_RDKSPLASHSCREEN_OPERATOR),"")
+RDKSPLASHSCREEN_POST_INSTALL_TARGET_HOOKS += RDKSPLASHSCREEN_WRITE_OPERATOR
+endif
 
 $(eval $(generic-package))
