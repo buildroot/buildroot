ff6d6dd10561b148d52242bb9b1458c8ae3ac814
Artur Gebicz <a.gebicz@metrological.com>
2020-02-28 12:34:37 +0100

    [explora-sdk] bump to include widevine libs and headers

--- package/explora-sdk/explora-sdk.mk
+++ package/explora-sdk/explora-sdk.mk
@@ -3,13 +3,17 @@
 # explora-sdk
 #
 ################################################################################
-EXPLORA_SDK_VERSION = 7aaab9f07fd8b0fc4e1a3a078e7d02983a8912fe
+EXPLORA_SDK_VERSION = 11f96e3cccb4aaca90feb1bfcb67210ede723a14
 EXPLORA_SDK_SITE = git@github.com:Metrological/SDK_Explora.git
 EXPLORA_SDK_SITE_METHOD = git
 EXPLORA_SDK_INSTALL_STAGING = YES
 
 define EXPLORA_SDK_INSTALL_STAGING_CMDS
+	$(INSTALL) -d $(STAGING_DIR)/usr/lib
 	$(INSTALL) -D -m 0644 $(@D)/libs/widevine/* $(STAGING_DIR)/usr/lib/
+
+	$(INSTALL) -d $(STAGING_DIR)/usr/include/widevine
+	$(INSTALL) -D -m 0644 $(@D)/includes/widevine/* $(STAGING_DIR)/usr/include/widevine
 endef
 
 $(eval $(generic-package))
