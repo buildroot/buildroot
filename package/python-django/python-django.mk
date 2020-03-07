9f0455c91137e75d9eedb93e0dcef887f817300b
Peter Korsgaard <peter@korsgaard.com>
2020-03-07 21:48:49 +0100

    package/python-django: security bump to version 2.2.11
    Fixes the following security issues (2.2.10):
    
    - CVE-2020-7471: Potential SQL injection via StringAgg(delimiter)
      django.contrib.postgres.aggregates.StringAgg aggregation function was
      subject to SQL injection, using a suitably crafted delimiter.
    
    For more details, see the advisory:
    https://www.djangoproject.com/weblog/2020/feb/03/security-releases/
    
    Fixes the following security issues (2.2.11):
    
    - CVE-2020-9402: Potential SQL injection via tolerance parameter in GIS
      functions and aggregates on Oracle.
      GIS functions and aggregates on Oracle were subject to SQL injection,
      using a suitably crafted tolerance.
    
    For more details, see the advisory:
    https://www.djangoproject.com/weblog/2020/mar/04/security-releases/
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 2.2.9
+PYTHON_DJANGO_VERSION = 2.2.11
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/2c/0d/2aa8e58c791d2aa65658fa26f2b035a9da13a6a34d1b2d991912c8a33729
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/f0/5b/428db83c37c2cf69e077ed327ada3511837371356204befc654b5b4bd444
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
 PYTHON_DJANGO_SETUP_TYPE = setuptools
