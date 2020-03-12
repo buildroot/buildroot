58fedbbf1a120e41af76b51b29d01b0705739896
Wouter Meek <woutermeek@users.noreply.github.com>
2020-03-12 23:33:18 +0100

    [wpeframework-tools] remove space

--- package/wpe/wpeframework-tools/wpeframework-tools.mk
+++ package/wpe/wpeframework-tools/wpeframework-tools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEFRAMEWORK_TOOLS_VERSION = b0a602de12a8002d16958ed7a472588aaab27043 
+WPEFRAMEWORK_TOOLS_VERSION = b0a602de12a8002d16958ed7a472588aaab27043
 
 HOST_WPEFRAMEWORK_TOOLS_SITE = $(call github,rdkcentral,Thunder,$(WPEFRAMEWORK_TOOLS_VERSION))
 HOST_WPEFRAMEWORK_TOOLS_INSTALL_STAGING = YES
