f18237ff7c195d70f44e54b2b26803d0bfb74133
Pierre Wielders <pierre@wielders.net>
2020-03-17 11:39:29 +0100

    [BLEUTOOTH] Fix the setup in the Bluetooth firmware installation.

--- package/rpi-bt-firmware/rpi-bt-firmware.mk
+++ package/rpi-bt-firmware/rpi-bt-firmware.mk
@@ -35,14 +35,14 @@ define RPI_BT_FIRMWARE_INSTALL_TARGET_CMDS
 endef
 
 else ifeq ($(RPI_BT_CHIP),"BCM4345C0")
-RPI3PLUS_BT_FIRMWARE_VERSION = fff76cb15527c435ce99a9787848eacd6288282c
-RPI3PLUS_BT_FIRMWARE_SITE = https://github.com/RPi-Distro/bluez-firmware.git
-RPI3PLUS_BT_FIRMWARE_SITE_METHOD = git
-RPI3PLUS_BT_FIRMWARE_LICENSE = PROPRIETARY
+RPI_BT_FIRMWARE_VERSION = fff76cb15527c435ce99a9787848eacd6288282c
+RPI_BT_FIRMWARE_SITE = https://github.com/RPi-Distro/bluez-firmware.git
+RPI_BT_FIRMWARE_SITE_METHOD = git
+RPI_BT_FIRMWARE_LICENSE = PROPRIETARY
 
 # The BlueZ hciattach utility looks for firmware in /etc/firmware. Add a
 # compatibility symlink.
-define RPI3PLUS_BT_FIRMWARE_INSTALL_TARGET_CMDS
+define RPI_BT_FIRMWARE_INSTALL_TARGET_CMDS
 	ln -sf ../lib/firmware $(TARGET_DIR)/etc/firmware
 	$(INSTALL) -D -m 0644 $(@D)/broadcom/BCM4345C0.hcd \
 		$(TARGET_DIR)/lib/firmware/BCM4345C0.hcd
