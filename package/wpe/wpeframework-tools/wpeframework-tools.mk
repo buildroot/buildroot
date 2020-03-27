4968b4a6a6ab53615b92a73bd820af03e2127a26
Pierre Wielders <pierre@wielders.net>
2020-03-27 15:30:09 +0100

    [BUMP] Bump to the potentially first release to be done on the RDKCentral.

--- package/wpe/wpeframework-tools/wpeframework-tools.mk
+++ package/wpe/wpeframework-tools/wpeframework-tools.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-WPEFRAMEWORK_TOOLS_VERSION = b0a602de12a8002d16958ed7a472588aaab27043
+WPEFRAMEWORK_TOOLS_VERSION = 4594f75b7aa7c86b89f4a488b8472d6f9a41d4ea
 
 HOST_WPEFRAMEWORK_TOOLS_SITE = $(call github,rdkcentral,Thunder,$(WPEFRAMEWORK_TOOLS_VERSION))
 HOST_WPEFRAMEWORK_TOOLS_INSTALL_STAGING = YES
 HOST_WPEFRAMEWORK_TOOLS_INSTALL_TARGET = NO
-HOST_WPEFRAMEWORK_TOOLS_DEPENDENCIES = host-cmake host-python3-jsonref
+HOST_WPEFRAMEWORK_TOOLS_DEPENDENCIES = host-cmake host-python3 host-python3-jsonref
 HOST_WPEFRAMEWORK_TOOLS_SUBDIR = Tools
 
 $(eval $(host-cmake-package))
