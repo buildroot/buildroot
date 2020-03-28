ff435b52f294e0033088e00c599eab3d0123adfe
Gleb Mazovetskiy <glex.spb@gmail.com>
2020-03-28 23:30:34 +0000

    Build a relocatable SDK instead of hardcoding path
    This SDK can be extracted and used from anywhere.
    Simply run ./relocate-sdk.sh after extracting.
    
    Signed-off-by: Gleb Mazovetskiy <glex.spb@gmail.com>

--- rebuild.sh
+++ rebuild.sh
@@ -10,19 +10,18 @@ fi
 # Use the default config.
 make od_${CONFIG}_defconfig BR2_EXTERNAL=board/opendingux
 
-# Clear the install location.
-echo "Clearing install location..."
-mkdir -p /opt/${CONFIG}-toolchain
-rm -rf /opt/${CONFIG}-toolchain/*
-
 # Clear the build location.
 echo "Clearing build location..."
-rm -rf output/
+rm -rf output/host
 
 # Perform the build.
 echo "Starting build..."
-nice make BR2_EXTERNAL=board/opendingux
+nice make sdk BR2_SDK_PREFIX=${CONFIG}-toolchain
 
-# Create packages.
-echo "Creating packages..."
-nice tar -C$(dirname $(realpath /opt/${CONFIG}-toolchain)) --exclude='.fakeroot.*' -jcf opendingux-${CONFIG}-toolchain.`date +'%Y-%m-%d'`.tar.bz2 ${CONFIG}-toolchain
+ARCHIVE_NAME=opendingux-${CONFIG}-toolchain.`date +'%Y-%m-%d'`
+mv output/images/${CONFIG}-toolchain.tar.gz "output/images/$ARCHIVE_NAME.tar.gz"
+
+echo "The SDK has been built at:"
+echo "output/images/$ARCHIVE_NAME.tar.gz"
+echo
+echo "Remember to run ./relocate-sdk.sh after extracting it to your desired location"
