905b73754ba076bbed6e3242c3f527ac26dee51a
woutermeek <woutermeek@gmail.com>
2020-04-14 02:33:05 -0700

    [wpeframework-tools] bump version

--- package/wpe/wpeframework-tools/wpeframework-tools.mk
+++ package/wpe/wpeframework-tools/wpeframework-tools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEFRAMEWORK_TOOLS_VERSION = R1
+WPEFRAMEWORK_TOOLS_VERSION = bc116648ebacfb600ea9e69d0b5a775426d0d85c
 
 HOST_WPEFRAMEWORK_TOOLS_SITE = $(call github,rdkcentral,Thunder,$(WPEFRAMEWORK_TOOLS_VERSION))
 HOST_WPEFRAMEWORK_TOOLS_INSTALL_STAGING = YES
