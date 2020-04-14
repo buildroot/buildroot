e63a6883184d0869ce5bda3e49a2191d0437e1ec
Neeraj Deshpande <n.deshpande@metrological.com>
2020-04-14 21:43:48 +0530

    Modified to add Cobalt package.

---
+++ package/cobalt/cobalt.mk
@@ -0,0 +1,51 @@
+################################################################################
+#
+# COBALT
+#
+################################################################################
+
+COBALT_VERSION = 1aa6857406258643982489c775f282ab83137748
+COBALT_SITE_METHOD = git
+COBALT_SITE = git@github.com:Metrological/cobalt
+COBALT_INSTALL_STAGING = YES
+COBALT_DEPENDENCIES = gstreamer1 gst1-plugins-base gst1-plugins-good gst1-plugins-bad host-bison host-ninja wpeframework
+
+export COBALT_STAGING_DIR=$(STAGING_DIR)
+export COBALT_TOOLCHAIN_PREFIX=$(TARGET_CROSS)
+export COBALT_INSTALL_DIR=$(TARGET_DIR)
+
+export PATH := $(HOST_DIR)/bin:$(HOST_DIR)/usr/bin:$(HOST_DIR)/usr/sbin:$(PATH)
+
+ifeq ($(BR2_PACKAGE_HAS_NEXUS),y)
+# TODO: we might also have mips here at some point.
+COBALT_PLATFORM = wpe-brcm-arm
+COBALT_DEPENDENCIES += gst1-bcm
+else
+COBALT_PLATFORM = wpe-rpi
+endif
+
+ifeq ($(BR2_PACKAGE_WPEFRAMEWORK_CDM),y)
+export COBALT_HAS_OCDM=1
+else
+export COBALT_HAS_OCDM=0
+endif
+
+COBALT_BUILD_TYPE = qa
+
+ifeq ($(BR2_PACKAGE_COBALT_IMAGE_AS_LIB), y)
+export COBALT_EXECUTABLE_TYPE = shared_library
+else
+export COBALT_EXECUTABLE_TYPE = executable
+endif
+
+define COBALT_BUILD_CMDS
+    $(@D)/src/cobalt/build/gyp_cobalt -C $(COBALT_BUILD_TYPE) $(COBALT_PLATFORM)
+    $(HOST_DIR)/usr/bin/ninja -C $(@D)/src/out/$(COBALT_PLATFORM)_$(COBALT_BUILD_TYPE) cobalt_deploy
+endef
+
+define COBALT_INSTALL_TARGET_CMDS
+    cp -a $(@D)/src/out/$(COBALT_PLATFORM)_$(COBALT_BUILD_TYPE)/content $(TARGET_DIR)/usr/share
+endef
+
+
+$(eval $(generic-package))
