17b6d81442e7ca155ef9b11dc68c32ca7948a5bf
charles <charles@ccxtechnologies.com>
2020-05-09 06:52:19 -0400

    uprev cmake again, pre 3.10 won't work because of issues with other packages

--- package/cmake/cmake.mk
+++ package/cmake/cmake.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CMAKE_VERSION_MAJOR = 3.8
+CMAKE_VERSION_MAJOR = 3.17
 CMAKE_VERSION = $(CMAKE_VERSION_MAJOR).2
 CMAKE_SITE = https://cmake.org/files/v$(CMAKE_VERSION_MAJOR)
 CMAKE_LICENSE = BSD-3-Clause
