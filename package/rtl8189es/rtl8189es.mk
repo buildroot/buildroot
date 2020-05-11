65da7755d9266f245b711a19dd97499594415bde
Louis Aussedat <aussedat.louis@gmail.com>
2020-05-11 20:10:51 +0200

    rtl8189es: new package
    This package adds the rtl8189es WiFi driver.
       repo: https://github.com/jwrdegoede/rtl8189ES_linux.git
       branch: master
    
    Driver is known to support Realtek RTL8189ES-VB-CG chip.
    
    Signed-off-by: Louis Aussedat <aussedat.louis@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

---
+++ package/rtl8189es/rtl8189es.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# rtl8189es
+#
+################################################################################
+
+RTL8189ES_VERSION = 2c8d44ae26485052f39d933a3a132b3ff395803a
+RTL8189ES_SITE = $(call github,jwrdegoede,rtl8189ES_linux,$(RTL8189ES_VERSION))
+RTL8189ES_LICENSE = GPL-2.0
+
+RTL8189ES_MODULE_MAKE_OPTS = \
+	CONFIG_RTL8189ES=m \
+	KVER=$(LINUX_VERSION_PROBED) \
+	KSRC=$(LINUX_DIR)
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
