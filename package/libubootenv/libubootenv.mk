7a3be87d55735798407aa92d66c2ea302c1ee028
Pierre-Jean Texier <pjtexier@koncepto.io>
2020-05-25 18:22:38 +0200

    package/libubootenv: bump to version c91dcca
    c91dcca ubi, write: fix invalid envsize ptr to UBI_IOCVOLUP
    
    Signed-off-by: Pierre-Jean Texier <pjtexier@koncepto.io>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libubootenv/libubootenv.mk
+++ package/libubootenv/libubootenv.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBUBOOTENV_VERSION = f4b9cde3815abe84a98079cedd515283ea08c16b
+LIBUBOOTENV_VERSION = c91dccab34294b5ea9c6502d01bfdaa49639fd90
 LIBUBOOTENV_SITE = $(call github,sbabic,libubootenv,$(LIBUBOOTENV_VERSION))
 LIBUBOOTENV_LICENSE = LGPL-2.1
 LIBUBOOTENV_LICENSE_FILES = Licenses/lgpl-2.1.txt
