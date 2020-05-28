32f5ef9a7eb33ef57f759387835e6df0b73f39be
krp97 <k.plata576@gmail.com>
2020-05-28 13:42:28 +0200

    [WideVine] Improvements for WideVine recipe
    Until now, the widevine libraries have been built in debug mode
    regardless of any options. This change brings in the possibility
    to specify a build type. Additionally, the unit test executable
    has been split into seperate option.

--- package/widevine/widevine.mk
+++ package/widevine/widevine.mk
@@ -23,6 +23,8 @@ else
 export WV_BOARD=dummy
 endif #BR2_PACKAGE_WIDEVINE_SOC_RPI
 
+
+
 export WV_CC=$(TARGET_CC)
 export WV_CXX=$(TARGET_CXX)
 export WV_AR=$(TARGET_AR)
@@ -33,24 +35,40 @@ export WV_STAGING = $(STAGING_DIR)
 export WV_STAGING_NATIVE = $(STAGING_DIR)
 export WV_PROTOBUF_CONFIG = source
 
+
+ifeq ($(BR2_ENABLE_DEBUG),y)
+        WIDEVINE_BUILD_DIR=Debug
+else ifeq ($($(BR2_PACKAGE_WIDEVINE_BUILD_TYPE_DEBUG)),y)
+        WIDEVINE_BUILD_DIR=Debug
+else
+        WIDEVINE_BUILD_TYPE_OPTION=-r
+        WIDEVINE_BUILD_DIR=Release
+endif
+
 define WIDEVINE_CONFIGURE_CMDS
       (cd $(@D);rm -rf out; rm -rf Makefile;\
        find . -name \*.mk -delete;\
        find . -name \*.pyc -delete;\
-       ./build.py $(WIDEVINE_ARCHITECTURE) )
+       ./build.py $(WIDEVINE_ARCHITECTURE) $(WIDEVINE_BUILD_TYPE_OPTION))
 endef
 
+ifeq ($(BR2_PACKAGE_WIDEVINE_INSTALL_UT),y)
+define WIDEVINE_UNIT_TEST_INSTALL
+        $(INSTALL) -D $(@D)/out/$(WIDEVINE_ARCHITECTURE)/$(WIDEVINE_BUILD_DIR)/widevine_ce_cdm_unittest $(TARGET_DIR)/usr/bin
+endef
+endif
+
 define WIDEVINE_INSTALL_TARGET_CMDS
-        cp $(@D)/out/$(WIDEVINE_ARCHITECTURE)/Debug/widevine_ce_cdm_unittest $(TARGET_DIR)/usr/bin
-        cp $(@D)/out/$(WIDEVINE_ARCHITECTURE)/Debug/lib*/lib*.so $(TARGET_DIR)/usr/lib/
+        $(call WIDEVINE_UNIT_TEST_INSTALL)
+        $(INSTALL) -Ds --strip-program=$(TARGET_STRIP) \
+                $(@D)/out/$(WIDEVINE_ARCHITECTURE)/$(WIDEVINE_BUILD_DIR)/lib*/lib*.so \
+                $(TARGET_DIR)/usr/lib/
 endef
 
 define WIDEVINE_INSTALL_STAGING_CMDS
-        cp $(@D)/out/$(WIDEVINE_ARCHITECTURE)/Debug/lib*/lib*.so $(STAGING_DIR)/usr/lib/
-        cp $(@D)/cdm/include/*.h $(STAGING_DIR)/usr/include
-        cp $(@D)/core/include/*.h $(STAGING_DIR)/usr/include
-        # mkdir -p $(STAGING_DIR)/usr/include/host
-        # cp $(@D)/cdm/src/host/$(WIDEVINE_ARCHITECTURE)/*.h $(STAGING_DIR)/usr/include/host
+        $(INSTALL) -D $(@D)/out/$(WIDEVINE_ARCHITECTURE)/$(WIDEVINE_BUILD_DIR)/lib*/lib*.so $(STAGING_DIR)/usr/lib/
+        $(INSTALL) -D $(@D)/cdm/include/*.h $(STAGING_DIR)/usr/include
+        $(INSTALL) -D $(@D)/core/include/*.h $(STAGING_DIR)/usr/include
 endef
 
 $(eval $(generic-package))
