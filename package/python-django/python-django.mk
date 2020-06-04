36d78abceb79ad8b8ea73054cd2e6fcfcc835c4f
Peter Korsgaard <peter@korsgaard.com>
2020-06-04 14:39:26 +0200

    package/python-django: security bump to version 3.0.7
    Fixes the following security issues:
    
    - CVE-2020-13254: Potential data leakage via malformed memcached keys
    
      In cases where a memcached backend does not perform key validation,
      passing malformed cache keys could result in a key collision, and
      potential data leakage.  In order to avoid this vulnerability, key
      validation is added to the memcached cache backends.
    
    - CVE-2020-13596: Possible XSS via admin ForeignKeyRawIdWidget
    
      Query parameters for the admin ForeignKeyRawIdWidget were not properly URL
      encoded, posing an XSS attack vector.  ForeignKeyRawIdWidget now ensures
      query parameters are correctly URL encoded.
    
    For details, see the announcement:
    https://docs.djangoproject.com/en/dev/releases/3.0.7/
    
    Additionally, 3.0.5..3.0.7 contains a number of non-security related
    bugfixes.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 3.0.4
+PYTHON_DJANGO_VERSION = 3.0.7
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/1d/38/89ea18b5aeb9b56fff7430388946e8e9dfd7a451f3e6ddb8a9b637f442c1
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/74/ad/8a1bc5e0f8b740792c99c7bef5ecc043018e2b605a2fe1e2513fde586b72
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
 PYTHON_DJANGO_SETUP_TYPE = setuptools
