9b2ca79a57a71b1d29e77609ac120087a8cb62f8
Pierre Wielders <pierre@wielders.net>
2020-06-05 12:32:21 +0200

    [NETFLIX52] Install the Metering certificate as well..

--- package/netflix52/netflix52.mk
+++ package/netflix52/netflix52.mk
@@ -174,9 +174,12 @@ define NETFLIX52_INSTALL_TO_TARGET
 	cp -r $(@D)/netflix/src/platform/gibbon/resources $(1)/root/Netflix
 	cp -r $(@D)/netflix/resources/configuration/* $(1)/root/Netflix/etc/conf
 
-        $(NETFLIX52_INSTALL_WPEFRAMEWORK_XML)
+	$(NETFLIX52_INSTALL_WPEFRAMEWORK_XML)
 	cp $(@D)/netflix/src/platform/gibbon/resources/js/*.js $(1)/root/Netflix/resources/js
 	cp $(@D)/netflix/src/platform/gibbon/resources/default/PartnerBridge.js $(1)/root/Netflix/resources/js
+
+	mkdir -p $(1)/root/Netflix/artifacts
+	cp -r $(@D)/artifacts/MeteringCertificate.bin $(1)/root/Netflix/artifacts
 endef
 
 else
