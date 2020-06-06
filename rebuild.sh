e856084211efc8d9600f42a4bd9101c97bea76ed
Paul Cercueil <paul@crapouillou.net>
2020-06-06 13:39:11 +0200

    rebuild.sh: Build into config-specific output folder
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- rebuild.sh
+++ rebuild.sh
@@ -9,19 +9,19 @@ fi
 
 # Clear the build location.
 echo "Clearing build location..."
-rm -rf output/
+rm -rf output/${CONFIG}
 
 # Use the default config.
-make od_${CONFIG}_defconfig BR2_EXTERNAL=board/opendingux
+make od_${CONFIG}_defconfig BR2_EXTERNAL=board/opendingux O=output/${CONFIG}
 
 # Perform the build.
 echo "Starting build..."
-nice make sdk BR2_SDK_PREFIX=${CONFIG}-toolchain
+nice make sdk BR2_SDK_PREFIX=${CONFIG}-toolchain O=output/${CONFIG}
 
 ARCHIVE_NAME=opendingux-${CONFIG}-toolchain.`date +'%Y-%m-%d'`
-mv output/images/${CONFIG}-toolchain.tar.gz "output/images/$ARCHIVE_NAME.tar.gz"
+mv output/${CONFIG}/images/${CONFIG}-toolchain.tar.gz "output/${CONFIG}/images/$ARCHIVE_NAME.tar.gz"
 
 echo "The SDK has been built at:"
-echo "output/images/$ARCHIVE_NAME.tar.gz"
+echo "output/${CONFIG}/images/$ARCHIVE_NAME.tar.gz"
 echo
 echo "Remember to run ./relocate-sdk.sh after extracting it to your desired location"
