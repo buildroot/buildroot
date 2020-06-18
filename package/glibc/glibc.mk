d58fbd0e13e8f7f70103002cef137db6863ff94e
Romain Naour <romain.naour@gmail.com>
2020-06-18 18:17:34 +0200

    package/glibc: remove outdated cvs configure option
    --without-cvs has been removed from glibc tree since glibc 2.15 [1]
    
    [1] https://sourceware.org/git/?p=glibc.git;a=commitdiff;h=92963737c4376bcfd65235d5c325fa7f48302f89
    
    Signed-off-by: Romain Naour <romain.naour@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -130,7 +130,6 @@ define GLIBC_CONFIGURE_CMDS
 		--enable-shared \
 		$(if $(BR2_x86_64),--enable-lock-elision) \
 		--with-pkgversion="Buildroot" \
-		--without-cvs \
 		--disable-profile \
 		--without-gd \
 		--enable-obsolete-rpc \
