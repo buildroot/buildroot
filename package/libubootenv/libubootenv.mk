70cf2ae06a76958757d68ebbf38ac05953b2a8a7
Pierre-Jean Texier <pjtexier@koncepto.io>
2020-06-24 13:05:59 +0200

    package/libubootenv: bump to version 86bd30a
    This includes the following changes:
    
    86bd30a Restore ability to feed script file via stdin, using `-s -`.
    
    Signed-off-by: Pierre-Jean Texier <pjtexier@koncepto.io>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libubootenv/libubootenv.mk
+++ package/libubootenv/libubootenv.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBUBOOTENV_VERSION = c91dccab34294b5ea9c6502d01bfdaa49639fd90
+LIBUBOOTENV_VERSION = 86bd30a14e153a18f670b25708795253d8736f0f
 LIBUBOOTENV_SITE = $(call github,sbabic,libubootenv,$(LIBUBOOTENV_VERSION))
 LIBUBOOTENV_LICENSE = LGPL-2.1
 LIBUBOOTENV_LICENSE_FILES = Licenses/lgpl-2.1.txt
