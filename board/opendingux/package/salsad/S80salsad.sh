48938b3f3b4f8ed8d19fa06971aaad8c79c3f891
Paul Cercueil <paul@crapouillou.net>
2020-06-25 00:20:29 +0200

    opendingux: Add salsad package
    salsad is a small daemon which will toggle between speakers and
    headphones when headphones are plugged or unplugged.
    
    https://github.com/JohnnyonFlame/salsad
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

---
+++ board/opendingux/package/salsad/S80salsad.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+case "$1" in
+
+        start)
+                echo "Starting salsad..."
+				/usr/sbin/start-stop-daemon -S -p /run/salsad.pid -b -c od:users -m -x /usr/bin/salsad -- default
+                ;;
+
+        stop)
+                echo "Stopping salsad..."
+				/usr/sbin/start-stop-daemon -K -p /run/salsad.pid
+                ;;
+
+        *)
+                echo "Usage: $0 {start|stop}"
+                exit 1
+esac
---
+++ board/opendingux/package/salsad/salsad.mk
@@ -0,0 +1,18 @@
+#############################################################
+#
+# salsad
+#
+#############################################################
+SALSAD_VERSION = 151eb07
+SALSAD_SITE = $(call github,JohnnyonFlame,salsad,$(SALSAD_VERSION))
+SALSAD_DEPENDENCIES = alsa-lib
+SALSAD_LICENSE = GPLv3
+SALSAD_LICENSE_FILES = LICENSE.md
+
+define SALSAD_INSTALL_INIT_FILE
+	$(INSTALL) -D -m 0755 board/opendingux/package/salsad/S80salsad.sh \
+		$(TARGET_DIR)/etc/init.d/S80salsad.sh
+endef
+SALSAD_POST_INSTALL_TARGET_HOOKS += SALSAD_INSTALL_INIT_FILE
+
+$(eval $(cmake-package))
