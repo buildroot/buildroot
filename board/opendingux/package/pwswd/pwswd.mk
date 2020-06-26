9c4f2814f21517a381939f1ad2d823d4ec4acf7d
Paul Cercueil <paul@crapouillou.net>
2020-06-26 17:04:46 +0200

    opendingux: pwswd: Update to latest version
    The latest version uses CMake instead of a Makefile, and only needs a
    semicolon-separated list of backends to enable.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- board/opendingux/package/pwswd/pwswd.mk
+++ board/opendingux/package/pwswd/pwswd.mk
@@ -4,22 +4,15 @@
 #
 ################################################################################
 
-PWSWD_VERSION = 9a82cbf
+PWSWD_VERSION = 6656a6d
 PWSWD_SITE = $(call github,pcercuei,pwswd,$(PWSWD_VERSION))
 PWSWD_DEPENDENCIES = alsa-lib libpng libini
 
-PWSWD_MAKE_ENV = CFLAGS="$(TARGET_CFLAGS)" LDFLAGS="$(TARGET_LDFLAGS)" \
-				 CROSS_COMPILE="$(TARGET_CROSS)" PREFIX=/usr \
-				 CONFIG=$(BR2_PACKAGE_PWSWD_PLATFORM)
+PWSWD_CONF_OPTS += -DBACKENDS=$(BR2_PACKAGE_PWSWD_BACKENDS)
 
-define PWSWD_BUILD_CMDS
-	$(PWSWD_MAKE_ENV) $(MAKE) -C $(@D)
-endef
-
-define PWSWD_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/pwswd $(TARGET_DIR)/usr/sbin/pwswd
-	$(INSTALL) -D -m 0644 $(@D)/pwswd-$(BR2_PACKAGE_PWSWD_PLATFORM).conf $(TARGET_DIR)/etc/pwswd.conf
+define PWSWD_INSTALL_INIT_FILE
 	$(INSTALL) -D -m 0755 board/opendingux/package/pwswd/S91pwswd.sh $(TARGET_DIR)/etc/init.d/S91pwswd.sh
 endef
+PWSWD_POST_INSTALL_TARGET_HOOKS += PWSWD_INSTALL_INIT_FILE
 
-$(eval $(generic-package))
+$(eval $(cmake-package))
