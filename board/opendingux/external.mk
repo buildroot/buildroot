6c6217b571127d770cda7067bbe4e6683f6ca88b
Paul Cercueil <paul@crapouillou.net>
2020-07-05 17:47:56 +0200

    opendingux: ubiboot: update package
    - Move ubiboot package to board/opendingux/package. It does not need its
      own folder.
    - Install only .bin files in the images directory and not in a
      sub-directory.
    - Install images in mode 0644 instead of 0755.
    - Rename BR2_TARGET_UBIBOOT to BR2_PACKAGE_UBIBOOT
    - Remove useless check for board name. If the board name is incorrect
      the package won't build anyway.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- board/opendingux/external.mk
+++ board/opendingux/external.mk
@@ -1,2 +1,1 @@
-include $(sort $(wildcard $(BR2_EXTERNAL_OPENDINGUX_PATH)/boot/*/*.mk))
 include $(sort $(wildcard $(BR2_EXTERNAL_OPENDINGUX_PATH)/package/*/*.mk))
--- board/opendingux/package/ubiboot/ubiboot.mk
+++ board/opendingux/package/ubiboot/ubiboot.mk
@@ -6,7 +6,7 @@
 
 UBIBOOT_VERSION = 121d5ae7af
 UBIBOOT_SITE = $(call github,pcercuei,UBIBoot,$(UBIBOOT_VERSION))
-UBIBOOT_BOARD_NAME = $(call qstrip,$(BR2_TARGET_UBIBOOT_BOARDNAME))
+UBIBOOT_BOARD_NAME = $(call qstrip,$(BR2_PACKAGE_UBIBOOT_BOARDNAME))
 
 UBIBOOT_LICENSE = GPLv2+
 UBIBOOT_LICENSE_FILES = README
@@ -18,18 +18,7 @@ define UBIBOOT_BUILD_CMDS
 endef
 
 define UBIBOOT_INSTALL_IMAGES_CMDS
-	mkdir -p $(BINARIES_DIR)/ubiboot
-	$(INSTALL) -D -m 0755 $(@D)/output/$(UBIBOOT_BOARD_NAME)/* $(BINARIES_DIR)/ubiboot
+	$(INSTALL) -D -m 0644 $(@D)/output/$(UBIBOOT_BOARD_NAME)/*.bin $(BINARIES_DIR)
 endef
 
 $(eval $(generic-package))
-
-ifeq ($(BR2_TARGET_UBIBOOT),y)
-# we NEED a board name unless we're at make source
-ifeq ($(filter source,$(MAKECMDGOALS)),)
-ifeq ($(UBIBOOT_BOARD_NAME),)
-$(error NO UBIBoot board name set. Check your BR2_BOOT_UBIBOOT_BOARDNAME setting)
-endif
-endif
-
-endif
