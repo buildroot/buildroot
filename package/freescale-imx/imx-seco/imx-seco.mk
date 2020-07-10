34c6de8f96a599f4a5038cbc744d5061625140c3
Maeva Manuel <maeva.manuel@oss.nxp.com>
2020-07-10 10:16:55 +0200

    package/freescale-imx/imx-seco: bump version to 3.6.3
    This version is aligned with 5.4.24_2.1.0 NXP Linux BSP.
    Firmware file names now include the SoC revision.
    
    In order not to break the compatibility with the imx-seco 2.3.1
    package, it remains B0 support for i.MX8QXP MEK. C0 support should
    introduce a Kconfig option and this will be done in a future patch.
    
    License was updated from:
    LA_OPT_NXP_Software_License v10 December 2019
    to:
    LA_OPT_NXP_Software_License v11 February 2020
    which explains the change of EULA/COPYING license files.
    
    Tested-by: Maeva Manuel <maeva.manuel@oss.nxp.com>
    [Maeva: tested on i.MX8QXP MEK (Board rev D1, SoC rev B0)
    and on i.MX8QM MEK (SoC rev B0)]
    Signed-off-by: Maeva Manuel <maeva.manuel@oss.nxp.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/freescale-imx/imx-seco/imx-seco.mk
+++ package/freescale-imx/imx-seco/imx-seco.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-IMX_SECO_VERSION = 2.3.1
+IMX_SECO_VERSION = 3.6.3
 IMX_SECO_SITE = $(FREESCALE_IMX_SITE)
 IMX_SECO_SOURCE = imx-seco-$(IMX_SECO_VERSION).bin
 
@@ -22,12 +22,12 @@ IMX_SECO_INSTALL_IMAGES = YES
 # is done in post-image script.
 ifeq ($(BR2_PACKAGE_FREESCALE_IMX_PLATFORM_IMX8X),y)
 define IMX_SECO_INSTALL_IMAGES_CMDS
-	cp $(@D)/firmware/seco/mx8qx-ahab-container.img \
+	cp $(@D)/firmware/seco/mx8qxb0-ahab-container.img \
 		$(BINARIES_DIR)/ahab-container.img
 endef
 else ifeq ($(BR2_PACKAGE_FREESCALE_IMX_PLATFORM_IMX8),y)
 define IMX_SECO_INSTALL_IMAGES_CMDS
-	cp $(@D)/firmware/seco/mx8qm-ahab-container.img \
+	cp $(@D)/firmware/seco/mx8qmb0-ahab-container.img \
 		$(BINARIES_DIR)/ahab-container.img
 endef
 endif
