f32cf8e7527990b603d8c05b35be93b55d7a9f53
Robert Hancock <hancock@sedsystems.ca>
2020-07-13 21:38:48 -0600

    package/gpsd: bump version to 3.20
    Removes BR2_PACKAGE_GPSD_PPS config option, since PPS functionality is
    no longer optional and always enabled in gpsd's SCons configuration.
    
    Removed passing ntpshm=y to SCons since that feature is also no longer
    optional.
    
    Added a patch adapted from changes merged upstream post-3.20 to fix a
    build failure during cross-compilation when checking sizeof(time_t)
    and where shared libraries were being linked with ld rather than g++.
    
    Signed-off-by: Robert Hancock <hancock@sedsystems.ca>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/gpsd/gpsd.mk
+++ package/gpsd/gpsd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GPSD_VERSION = 3.19
+GPSD_VERSION = 3.20
 GPSD_SITE = http://download-mirror.savannah.gnu.org/releases/gpsd
 GPSD_LICENSE = BSD-2-Clause
 GPSD_LICENSE_FILES = COPYING
@@ -25,7 +25,6 @@ GPSD_SCONS_OPTS = \
 	strip=no \
 	python=no \
 	qt=no \
-	ntpshm=yes \
 	systemd=$(if $(BR2_INIT_SYSTEMD),yes,no)
 
 ifeq ($(BR2_PACKAGE_NCURSES),y)
@@ -163,9 +162,6 @@ GPSD_SCONS_OPTS += ublox=no
 endif
 
 # Features
-ifneq ($(BR2_PACKAGE_GPSD_PPS),y)
-GPSD_SCONS_OPTS += pps=no
-endif
 ifeq ($(BR2_PACKAGE_GPSD_SQUELCH),y)
 GPSD_SCONS_OPTS += squelch=yes
 endif
