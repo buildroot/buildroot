d535581c00163f99f8fa323981cc00fad5ee0334
Adam Duskett <Aduskett@gmail.com>
2020-07-15 10:37:05 -0700

    package/mender: bump version to 2.3.0
    Update the following license file hashes due to copyright year changes:
      - vendor/github.com/mendersoftware/mendertesting/LICENSE
      - vendor/github.com/mendersoftware/mender-artifact/LICENSE
    
    Remove hashes for the the following removed files:
      - vendor/github.com/mendersoftware/log/LICENSE
      - vendor/github.com/mendersoftware/scopestack/LICENSE
    
    Signed-off-by: Adam Duskett <Aduskett@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/mender/mender.mk
+++ package/mender/mender.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MENDER_VERSION = 2.2.0
+MENDER_VERSION = 2.3.0
 MENDER_SITE = https://github.com/mendersoftware/mender/archive
 MENDER_SOURCE = $(MENDER_VERSION).tar.gz
 MENDER_LICENSE = Apache-2.0, BSD-2-Clause, BSD-3-Clause, ISC, MIT, OLDAP-2.8
@@ -15,8 +15,6 @@ MENDER_LICENSE_FILES = \
 	LICENSE \
 	LIC_FILES_CHKSUM.sha256 \
 	vendor/github.com/mendersoftware/mendertesting/LICENSE \
-	vendor/github.com/mendersoftware/log/LICENSE \
-	vendor/github.com/mendersoftware/scopestack/LICENSE \
 	vendor/github.com/mendersoftware/mender-artifact/LICENSE \
 	vendor/github.com/pkg/errors/LICENSE \
 	vendor/github.com/pmezard/go-difflib/LICENSE \
