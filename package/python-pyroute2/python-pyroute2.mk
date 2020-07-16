2371675ad10f2031356a41412f566234b1ea31cd
charles <charles@ccxtechnologies.com>
2020-07-16 07:15:22 -0400

    uprev pyroute2

--- package/python-pyroute2/python-pyroute2.mk
+++ package/python-pyroute2/python-pyroute2.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYROUTE2_VERSION = 0.5.7
+PYTHON_PYROUTE2_VERSION = 0.5.12
 PYTHON_PYROUTE2_SOURCE = pyroute2-$(PYTHON_PYROUTE2_VERSION).tar.gz
-PYTHON_PYROUTE2_SITE = https://files.pythonhosted.org/packages/6b/94/2a5f6c11369a21efd27a0b4cb92adf5012da7acdbce7e78055710986ca23
+PYTHON_PYROUTE2_SITE = https://files.pythonhosted.org/packages/6d/59/27f6ba1a87bbc5abcd860513edc0f72f4dddd3d5417171af4187f1d8ae6e
 PYTHON_PYROUTE2_LICENSE = Apache-2.0 or GPL-2.0+
 PYTHON_PYROUTE2_LICENSE_FILES = LICENSE.Apache.v2 LICENSE.GPL.v2 README.license.md
 PYTHON_PYROUTE2_SETUP_TYPE = distutils
