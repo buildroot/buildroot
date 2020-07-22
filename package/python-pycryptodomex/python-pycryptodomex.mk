bd78244174d42e7970b28044326c3f438da841cb
Dick Olsson <hi@senzilla.io>
2020-07-22 07:11:26 +0000

    package/python3-pycryptodomex: new package
    This package will ensure that pycryptodomex is built for Python 3.
    Comments in both python-pycryptodome and python3-pycryptodomex are
    added to ensure they stay in sync.
    
    Signed-off-by: Dick Olsson <hi@senzilla.io>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-pycryptodomex/python-pycryptodomex.mk
+++ package/python-pycryptodomex/python-pycryptodomex.mk
@@ -4,6 +4,7 @@
 #
 ################################################################################
 
+# Please keep in sync with package/python3-pycryptodomex/python3-pycryptodomex.mk
 PYTHON_PYCRYPTODOMEX_VERSION = 3.9.8
 PYTHON_PYCRYPTODOMEX_SOURCE = pycryptodomex-$(PYTHON_PYCRYPTODOMEX_VERSION).tar.gz
 PYTHON_PYCRYPTODOMEX_SITE = https://files.pythonhosted.org/packages/f5/79/9d9206688385d1e7a5ff925e7aab1d685636256e34a409037aa7adbbe611
