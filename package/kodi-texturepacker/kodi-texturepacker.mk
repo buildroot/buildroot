247664b6b2b21cebf88ad7dc6193e116bff9addf
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-07-28 22:40:50 +0200

    package/kodi: bump version to 18.8-Leia
    Removed patch which was applied upstream:
    https://github.com/xbmc/xbmc/commit/920cc74da069216d19f724ecf5246cf997861bc6
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/kodi-texturepacker/kodi-texturepacker.mk
+++ package/kodi-texturepacker/kodi-texturepacker.mk
@@ -6,7 +6,7 @@
 
 # Not possible to directly refer to kodi variables, because of
 # first/second expansion trickery...
-KODI_TEXTUREPACKER_VERSION = 18.7.1-Leia
+KODI_TEXTUREPACKER_VERSION = 18.8-Leia
 KODI_TEXTUREPACKER_SITE = $(call github,xbmc,xbmc,$(KODI_TEXTUREPACKER_VERSION))
 KODI_TEXTUREPACKER_SOURCE = kodi-$(KODI_TEXTUREPACKER_VERSION).tar.gz
 KODI_TEXTUREPACKER_DL_SUBDIR = kodi
