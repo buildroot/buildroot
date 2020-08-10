67b409778d69e9160fafaf6e824e3eaa6e38f115
charles <charles@ccxtechnologies.com>
2020-08-10 07:15:46 -0400

    uprev uvloop to fix the crash issue

--- package/python-uvloop/python-uvloop.mk
+++ package/python-uvloop/python-uvloop.mk
@@ -4,9 +4,8 @@
 #
 ################################################################################
 
-PYTHON_UVLOOP_VERSION = 0.14.0
-PYTHON_UVLOOP_SOURCE = uvloop-$(PYTHON_UVLOOP_VERSION).tar.gz
-PYTHON_UVLOOP_SITE = https://files.pythonhosted.org/packages/84/2e/462e7a25b787d2b40cf6c9864a9e702f358349fc9cfb77e83c38acb73048
+PYTHON_UVLOOP_VERSION = 6ef69a7
+PYTHON_UVLOOP_SITE = $(call github,MagicStack,uvloop,$(PYTHON_UVLOOP_VERSION))
 PYTHON_UVLOOP_SETUP_TYPE = setuptools
 PYTHON_UVLOOP_LICENSE = Apache-2.0, MIT
 PYTHON_UVLOOP_LICENSE_FILES = LICENSE-APACHE LICENSE-MIT
