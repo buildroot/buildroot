3846f2ea4e09b146c3cc00b66e6e62caa38a4603
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-08-27 19:26:44 +0200

    package/json-c: security bump to version 0.15
    Fix CVE-2020-12762: json-c through 0.14 has an integer overflow and
    out-of-bounds write via a large JSON file, as demonstrated by
    printbuf_memappend.
    
    Also update indentation in hash file (two spaces)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 071e719d586ed2a551011ea3bcc378ba66f37c47)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/json-c/json-c.mk
+++ package/json-c/json-c.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-JSON_C_VERSION = 0.14
+JSON_C_VERSION = 0.15
 JSON_C_SITE = https://s3.amazonaws.com/json-c_releases/releases
 JSON_C_INSTALL_STAGING = YES
 JSON_C_LICENSE = MIT
