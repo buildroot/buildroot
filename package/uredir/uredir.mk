37ce30631d50075bedb11bd9d7047617c9200549
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2020-08-30 17:17:53 +0200

    uredir: remove default value of _SOURCE variable
    As reported by check-package.
    
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/uredir/uredir.mk
+++ package/uredir/uredir.mk
@@ -5,7 +5,6 @@
 ################################################################################
 
 UREDIR_VERSION = 3.3
-UREDIR_SOURCE = uredir-$(UREDIR_VERSION).tar.gz
 UREDIR_SITE = https://github.com/troglobit/uredir/releases/download/v$(UREDIR_VERSION)
 UREDIR_LICENSE = ISC
 UREDIR_LICENSE_FILES = LICENSE
