a85c7e0b38760d9daf98a2fad298a44e2a986869
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-09-07 20:44:59 +0200

    package/wayland-utils: needs wayland-protocols
    wayland-utils needs wayland-protocols:
    
    Run-time dependency wayland-protocols found: NO (tried pkgconfig)
    
    ../output-1/build/wayland-utils-1.0.0/wayland-info/meson.build:4:0: ERROR: Dependency "wayland-protocols" not found, tried pkgconfig
    
    Fixes:
     - http://autobuild.buildroot.org/results/ea4daeb94c25232e3b4a34c1da72bf9bbd5f3cce
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/wayland-utils/wayland-utils.mk
+++ package/wayland-utils/wayland-utils.mk
@@ -9,6 +9,6 @@ WAYLAND_UTILS_SITE = http://wayland.freedesktop.org/releases
 WAYLAND_UTILS_SOURCE = wayland-utils-$(WAYLAND_UTILS_VERSION).tar.xz
 WAYLAND_UTILS_LICENSE = MIT
 WAYLAND_UTILS_LICENSE_FILES = COPYING
-WAYLAND_UTILS_DEPENDENCIES = host-pkgconf wayland
+WAYLAND_UTILS_DEPENDENCIES = host-pkgconf wayland wayland-protocols
 
 $(eval $(meson-package))
