fc27e7e1fc386662e338430b10d8c4f991c4965e
Joseph Kogut <joseph.kogut@gmail.com>
2020-09-08 12:23:34 -0700

    package/python-sentry-sdk: bump version to 0.17.3
    Signed-off-by: Joseph Kogut <joseph.kogut@gmail.com>
    Reviewed-by: Titouan Christophe <titouan.christophe@railnova.eu>
    Tested-by: Titouan Christophe <titouan.christophe@railnova.eu>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-sentry-sdk/python-sentry-sdk.mk
+++ package/python-sentry-sdk/python-sentry-sdk.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SENTRY_SDK_VERSION = 0.14.4
+PYTHON_SENTRY_SDK_VERSION = 0.17.3
 PYTHON_SENTRY_SDK_SOURCE = sentry-sdk-$(PYTHON_SENTRY_SDK_VERSION).tar.gz
-PYTHON_SENTRY_SDK_SITE = https://files.pythonhosted.org/packages/ab/67/b653a44ce124a2b700a7fc349ca129282c5fec4fdc9197f558178a9e32ae
+PYTHON_SENTRY_SDK_SITE = https://files.pythonhosted.org/packages/64/7e/f1725d8649ef8f7d58cbec582157c454884238e59fef00b1707d555c7bea
 PYTHON_SENTRY_SDK_SETUP_TYPE = setuptools
 PYTHON_SENTRY_SDK_LICENSE = BSD-2-Clause
 PYTHON_SENTRY_SDK_LICENSE_FILES = LICENSE
