a29fd86cf4a09450a6809f43d2326f7b73406842
Francois Perrad <fperrad@gmail.com>
2020-09-10 15:26:13 +0200

    package/harfbuzz: bump to version 2.6.8
    - update home page URL (old one is redirected to github)
    - change download to new location (github)
    - update license hash because of copyright year update:
    
    diff COPING:
    -Copyright © 2010,2011,2012,2013,2014,2015,2016,2017,2018,2019  Google, Inc.
    -Copyright © 2019  Facebook, Inc.
    +Copyright © 2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020  Google, Inc.
    +Copyright © 2018,2019,2020  Ebrahim Byagowi
    +Copyright © 2019,2020  Facebook, Inc.
    
    note: newer versions (2.7.x) require a migration to meson
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/harfbuzz/harfbuzz.mk
+++ package/harfbuzz/harfbuzz.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-HARFBUZZ_VERSION = 2.6.4
-HARFBUZZ_SITE = https://www.freedesktop.org/software/harfbuzz/release
+HARFBUZZ_VERSION = 2.6.8
+HARFBUZZ_SITE = https://github.com/harfbuzz/harfbuzz/releases/download/$(HARFBUZZ_VERSION)
 HARFBUZZ_SOURCE = harfbuzz-$(HARFBUZZ_VERSION).tar.xz
 HARFBUZZ_LICENSE = MIT, ISC (ucdn library)
 HARFBUZZ_LICENSE_FILES = COPYING
