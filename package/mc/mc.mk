5e0a64e58dc3b98e8855173ee9aa379c71cf8ae1
Francois Perrad <fperrad@gmail.com>
2020-09-14 21:46:59 +0200

    package/mc: bump to version 4.8.25
    The patch was merged upstream, so we drop it, and consequently drop
    AUTORECONF = YES.
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/mc/mc.mk
+++ package/mc/mc.mk
@@ -4,15 +4,13 @@
 #
 ################################################################################
 
-MC_VERSION = 4.8.24
+MC_VERSION = 4.8.25
 MC_SOURCE = mc-$(MC_VERSION).tar.xz
 MC_SITE = http://ftp.midnight-commander.org
 MC_LICENSE = GPL-3.0+
 MC_LICENSE_FILES = COPYING
 MC_DEPENDENCIES = libglib2 host-pkgconf $(TARGET_NLS_DEPENDENCIES)
 MC_CONF_ENV = LIBS=$(TARGET_NLS_LIBS)
-# We're patching misc/Makefile.am
-MC_AUTORECONF = YES
 
 ifeq ($(BR2_PACKAGE_GPM),y)
 MC_CONF_OPTS += --with-gpm-mouse
