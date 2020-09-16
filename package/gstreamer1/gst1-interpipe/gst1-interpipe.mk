37c03dc3696eafdef41fe71eb9068e260baf8949
Peter Seiderer <ps.report@gmx.net>
2020-09-16 21:49:21 +0200

    package/gst1-interpipe: bump version to 94dea52 (1.1.2)
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/gstreamer1/gst1-interpipe/gst1-interpipe.mk
+++ package/gstreamer1/gst1-interpipe/gst1-interpipe.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GST1_INTERPIPE_VERSION = 9af5b40d106f35ce75f8baa5efc8c59fc5f7eda1
+GST1_INTERPIPE_VERSION = 94dea528dc9e2d0df02fdf2114deecfef6a23740
 GST1_INTERPIPE_SITE = https://github.com/RidgeRun/gst-interpipe
 GST1_INTERPIPE_SITE_METHOD = git
 # fetch gst-interpipe/common sub module
