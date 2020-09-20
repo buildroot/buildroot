ec9f2ad7522e71bde8072bc66ffd7c958f2d52e7
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-09-20 08:45:01 +0200

    package/kodi-visualisation-starburst: bump version to 2.0.3-Leia
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-visualisation-starburst/kodi-visualisation-starburst.mk
+++ package/kodi-visualisation-starburst/kodi-visualisation-starburst.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_VISUALISATION_STARBURST_VERSION = 2.0.2-Leia
+KODI_VISUALISATION_STARBURST_VERSION = 2.0.3-Leia
 KODI_VISUALISATION_STARBURST_SITE = $(call github,xbmc,visualization.starburst,$(KODI_VISUALISATION_STARBURST_VERSION))
 KODI_VISUALISATION_STARBURST_LICENSE = GPL-2.0+
 KODI_VISUALISATION_STARBURST_LICENSE_FILES = debian/copyright
