c211570904249855d0a8668d8b5a8dfa965466d4
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-09-20 08:45:00 +0200

    package/kodi-visualisation-shadertoy: bump version to 1.2.4-Leia
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-visualisation-shadertoy/kodi-visualisation-shadertoy.mk
+++ package/kodi-visualisation-shadertoy/kodi-visualisation-shadertoy.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_VISUALISATION_SHADERTOY_VERSION = 1.2.3-Leia
+KODI_VISUALISATION_SHADERTOY_VERSION = 1.2.4-Leia
 KODI_VISUALISATION_SHADERTOY_SITE = $(call github,xbmc,visualization.shadertoy,$(KODI_VISUALISATION_SHADERTOY_VERSION))
 KODI_VISUALISATION_SHADERTOY_LICENSE = GPL-2.0+
 KODI_VISUALISATION_SHADERTOY_LICENSE_FILES = debian/copyright
