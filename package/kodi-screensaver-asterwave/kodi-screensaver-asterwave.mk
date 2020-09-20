b989a19e27770e7b6c0acca696ae84caa598fadf
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-09-20 08:44:53 +0200

    package/kodi-screensaver-asterwave: bump version to 3.0.5-Leia
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-screensaver-asterwave/kodi-screensaver-asterwave.mk
+++ package/kodi-screensaver-asterwave/kodi-screensaver-asterwave.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_SCREENSAVER_ASTERWAVE_VERSION = 3.0.4-Leia
+KODI_SCREENSAVER_ASTERWAVE_VERSION = 3.0.5-Leia
 KODI_SCREENSAVER_ASTERWAVE_SITE = $(call github,xbmc,screensaver.asterwave,$(KODI_SCREENSAVER_ASTERWAVE_VERSION))
 KODI_SCREENSAVER_ASTERWAVE_LICENSE = GPL-2.0+
 KODI_SCREENSAVER_ASTERWAVE_LICENSE_FILES = debian/copyright
