2be774303dc14be8f80eeac3f9a0a9cb10fb51e8
Peter Korsgaard <peter@korsgaard.com>
2020-09-25 12:03:40 +0200

    package/python-semver: bump version to 2.10.2
    Bugfix release, fixing an issue with version fields containing '0'.  For
    details, see the changelog:
    
    https://python-semver.readthedocs.io/en/2.10.2/changelog.html#version-2-10-2
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-semver/python-semver.mk
+++ package/python-semver/python-semver.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SEMVER_VERSION = 2.10.1
+PYTHON_SEMVER_VERSION = 2.10.2
 PYTHON_SEMVER_SOURCE = semver-$(PYTHON_SEMVER_VERSION).tar.gz
-PYTHON_SEMVER_SITE = https://files.pythonhosted.org/packages/bc/82/5a9ee107810deb6cb142ea3b07aba619d6b6c0bd74c9e276b2b48b62696e
+PYTHON_SEMVER_SITE = https://files.pythonhosted.org/packages/aa/e8/cb894f70a52887f001aff5f264f68272c21fa58268495aca17df396c161f
 PYTHON_SEMVER_SETUP_TYPE = setuptools
 PYTHON_SEMVER_LICENSE = BSD-3-Clause
 PYTHON_SEMVER_LICENSE_FILES = LICENSE.txt
