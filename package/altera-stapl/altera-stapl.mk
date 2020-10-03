09340dd5bc12edf2fa50048ea54539d820e0fce7
Heiko Thiery <heiko.thiery@gmail.com>
2020-10-03 14:26:36 +0200

    package/altera-stapl: change to tagged version 0.3.3
    Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/altera-stapl/altera-stapl.mk
+++ package/altera-stapl/altera-stapl.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ALTERA_STAPL_VERSION = 7044a63b7d1a3fc6840cd3130ec984454f1559c0
+ALTERA_STAPL_VERSION = 0.3.3
 ALTERA_STAPL_SITE = $(call github,kontron,altera-stapl,$(ALTERA_STAPL_VERSION))
 ALTERA_STAPL_LICENSE = GPLv2+
 ALTERA_STAPL_LICENSE_FILES = COPYING
