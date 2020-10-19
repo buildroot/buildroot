f96dc1e64acc2635c01d60c57af9c8cee5a1586f
Peter Seiderer <ps.report@gmx.net>
2020-10-19 21:27:57 +0200

    package/gst1-devtools: add optional gst1-rtsp-server dependency
    Add optional gst1-rtsp-server dependency as checked for in
    gst1-devtools-1.18.0/validate/tools/meson.build.
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/gstreamer1/gst1-devtools/gst1-devtools.mk
+++ package/gstreamer1/gst1-devtools/gst1-devtools.mk
@@ -23,6 +23,10 @@ else
 GST1_DEVTOOLS_DEPENDENCIES += host-python python
 endif
 
+ifeq ($(BR2_PACKAGE_GST1_RTSP_SERVER),y)
+GST1_DEVTOOLS_DEPENDENCIES += gst1-rtsp-server
+endif
+
 GST1_DEVTOOLS_CONF_OPTS = \
 	-Dvalidate=enabled \
 	-Ddebug_viewer=disabled \
