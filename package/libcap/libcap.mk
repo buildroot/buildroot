fffe1e45cd31de4d697db395ec84b3885d2384fb
Heiko Thiery <heiko.thiery@gmail.com>
2020-10-25 10:34:07 +0100

    package/libcap: bump to version 2.44
    - remove patches that are already upstrem
    - modified and renumbered existing patch
    - add CROSS_COMPILE make arg
    - update hashfile for new version
    
    Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libcap/libcap.mk
+++ package/libcap/libcap.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBCAP_VERSION = 2.43
+LIBCAP_VERSION = 2.44
 LIBCAP_SITE = https://www.kernel.org/pub/linux/libs/security/linux-privs/libcap2
 LIBCAP_SOURCE = libcap-$(LIBCAP_VERSION).tar.xz
 LIBCAP_LICENSE = GPL-2.0 or BSD-3-Clause
@@ -16,6 +16,7 @@ LIBCAP_INSTALL_STAGING = YES
 HOST_LIBCAP_DEPENDENCIES = host-gperf
 
 LIBCAP_MAKE_FLAGS = \
+	CROSS_COMPILE="$(TARGET_CROSS)" \
 	BUILD_CC="$(HOSTCC)" \
 	BUILD_CFLAGS="$(HOST_CFLAGS)" \
 	DYNAMIC=$(if $(BR2_STATIC_LIBS),,yes)
