772eea51525116b647a4c386d1a0b7d3cbe61ac8
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-10-25 23:39:57 +0100

    package/python-intelhex: bump to version 2.3.0
    Fix Python 3.9 compatibility issue with 'array' module
    
    https://github.com/python-intelhex/intelhex/releases/tag/2.3.0
    
    Fixes:
     - http://autobuild.buildroot.org/results/1c217f0c67c63ee43494ef84af832df8e856e2df
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Reviewed-by: Marcin Niestroj <m.niestroj@grinn-global.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-intelhex/python-intelhex.mk
+++ package/python-intelhex/python-intelhex.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_INTELHEX_VERSION = 2.2.1
+PYTHON_INTELHEX_VERSION = 2.3.0
 PYTHON_INTELHEX_SOURCE = intelhex-$(PYTHON_INTELHEX_VERSION).tar.gz
-PYTHON_INTELHEX_SITE = https://files.pythonhosted.org/packages/01/66/8fab869edcc0eaf8fc030472ff379b8eeee2ef3b42f8aec6bd84e9f735e3
-PYTHON_INTELHEX_SETUP_TYPE = distutils
+PYTHON_INTELHEX_SITE = https://files.pythonhosted.org/packages/66/37/1e7522494557d342a24cb236e2aec5d078fac8ed03ad4b61372586406b01
+PYTHON_INTELHEX_SETUP_TYPE = setuptools
 PYTHON_INTELHEX_LICENSE = BSD-3-Clause
 PYTHON_INTELHEX_LICENSE_FILES = LICENSE.txt
 
