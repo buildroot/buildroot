999aa33826901d619214ed2c32ea8552eef5e53c
Yann E. MORIN <yann.morin.1998@free.fr>
2020-10-27 16:12:29 +0100

    package/busybox: update licensing info
    Busybox is mainly licensed under the GPL-2.0, but the bzip2 part is a
    modified copy of the bzip2/libbzip2 project, which comes with its own
    license.
    
    Update the licensing information accordingly.
    
    Add the hash for the new license file, and fixup indentation (2 spaces).
    
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit ca76d0336d89b7380aadb23aad2d32d240f4ec1c)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/busybox/busybox.mk
+++ package/busybox/busybox.mk
@@ -7,8 +7,8 @@
 BUSYBOX_VERSION = 1.31.1
 BUSYBOX_SITE = http://www.busybox.net/downloads
 BUSYBOX_SOURCE = busybox-$(BUSYBOX_VERSION).tar.bz2
-BUSYBOX_LICENSE = GPL-2.0
-BUSYBOX_LICENSE_FILES = LICENSE
+BUSYBOX_LICENSE = GPL-2.0, bzip2-1.0.4
+BUSYBOX_LICENSE_FILES = LICENSE archival/libarchive/bz/LICENSE
 
 define BUSYBOX_HELP_CMDS
 	@echo '  busybox-menuconfig     - Run BusyBox menuconfig'
