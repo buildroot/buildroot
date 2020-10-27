6c954361270fd22d07c470791cf76046d0b23015
bram oosterhuis <mail@bybram.com>
2020-10-27 18:50:55 +0000

    amazon-backend: Bump version

--- package/amazon-backend/amazon-backend.mk
+++ package/amazon-backend/amazon-backend.mk
@@ -4,14 +4,12 @@
 #
 ################################################################################
 
-AMAZON_BACKEND_VERSION = 6916e98947a38c1800f9906e6d07e1429cd621b1
+AMAZON_BACKEND_VERSION = 4babcc88d96b10f6944b3f36f9417b342b25a788
 AMAZON_BACKEND_SITE = git@github.com:Metrological/amazon-backend.git
 AMAZON_BACKEND_SITE_METHOD = git
-AMAZON_BACKEND_DEPENDENCIES =
+AMAZON_BACKEND_DEPENDENCIES = wpeframework wpeframework-clientlibraries
 AMAZON_BACKEND_LICENSE = PROPRIETARY
 AMAZON_BACKEND_INSTALL_STAGING = YES
-
-AMAZON_BACKEND_DEPENDENCIES += wpeframework
 
 ifeq ($(BR2_PACKAGE_GSTREAMER1),y)
 AMAZON_BACKEND_DEPENDENCIES += gstreamer1 gst1-plugins-base gst1-plugins-good gst1-plugins-bad
