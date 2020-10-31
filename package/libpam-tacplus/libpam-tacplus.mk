064251e292f1299d278a84746d2fea64600fc1c3
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-10-31 17:20:10 +0100

    package/libpam-tacplus: bump to version 1.6.1
    - Drop all patches (already in version)
    - Update indentation in hash file (two spaces)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libpam-tacplus/libpam-tacplus.mk
+++ package/libpam-tacplus/libpam-tacplus.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBPAM_TACPLUS_VERSION = 1.5.1
+LIBPAM_TACPLUS_VERSION = 1.6.1
 LIBPAM_TACPLUS_SITE = $(call github,jeroennijhof,pam_tacplus,v$(LIBPAM_TACPLUS_VERSION))
 LIBPAM_TACPLUS_LICENSE = GPL-2.0+
 LIBPAM_TACPLUS_LICENSE_FILES = COPYING
@@ -16,8 +16,5 @@ LIBPAM_TACPLUS_AUTORECONF = YES
 LIBPAM_TACPLUS_INSTALL_STAGING = YES
 LIBPAM_TACPLUS_CONF_ENV = \
 	ax_cv_check_cflags___fstack_protector_all=$(if $(BR2_TOOLCHAIN_HAS_SSP),yes,no)
-
-# 0005-Check-for-failure-of-OpenSSL-RAND_pseudo_bytes.patch
-LIBPAM_TACPLUS_IGNORE_CVES += CVE-2020-27743
 
 $(eval $(autotools-package))
