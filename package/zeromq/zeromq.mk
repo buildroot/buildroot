f744fc3a015e3990ac4b2fb3a2ad221609453c06
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-11-02 22:53:00 +0100

    package/zeromq: add libbsd optional dependency
    libbsd is an optional dependency which is enabled by default since
    version 4.3.3 and
    https://github.com/zeromq/libzmq/commit/068385c951c0608edec6264d55ba9c4c923acccc
    
    Fixes:
     - http://autobuild.buildroot.org/results/51220b1b82774e8f6f6ed8593c58d2e3c31a1531
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 25b5dc747a1bf4e1a18def00387246808c3c0a4e)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/zeromq/zeromq.mk
+++ package/zeromq/zeromq.mk
@@ -11,6 +11,8 @@ ZEROMQ_DEPENDENCIES = util-linux
 ZEROMQ_CONF_OPTS = --without-documentation
 ZEROMQ_LICENSE = LGPL-3.0+ with exceptions
 ZEROMQ_LICENSE_FILES = COPYING COPYING.LESSER
+# We're patching configure.ac
+ZEROMQ_AUTORECONF = YES
 
 # Assume these flags are always available. It is true, at least for
 # SOCK_CLOEXEC, since linux v2.6.27.
@@ -51,6 +53,13 @@ ifeq ($(BR2_PACKAGE_ZEROMQ_DRAFTS),y)
 ZEROMQ_CONF_OPTS += --enable-drafts
 else
 ZEROMQ_CONF_OPTS += --disable-drafts
+endif
+
+ifeq ($(BR2_PACKAGE_LIBBSD),y)
+ZEROMQ_DEPENDENCIES += host-pkgconf libbsd
+ZEROMQ_CONF_OPTS += --enable-libbsd
+else
+ZEROMQ_CONF_OPTS += --disable-libbsd
 endif
 
 # ZeroMQ uses libsodium if it's available.
