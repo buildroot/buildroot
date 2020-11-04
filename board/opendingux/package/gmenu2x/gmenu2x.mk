2bad773a5d61fb2d4ffc4b5e9123e6028df8f86e
Paul Cercueil <paul@crapouillou.net>
2020-11-04 07:49:26 +0000

    opendingux: gmenu2x: Bump version
    Bump GMenu2X to its latest master.
    
    Latest GMenu2X introduced support for CJK fallback fonts.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- board/opendingux/package/gmenu2x/gmenu2x.mk
+++ board/opendingux/package/gmenu2x/gmenu2x.mk
@@ -3,7 +3,7 @@
 # gmenu2x
 #
 #############################################################
-GMENU2X_VERSION = 2b7774f
+GMENU2X_VERSION = 9403b30
 GMENU2X_SITE = $(call github,opendingux,gmenu2x,$(GMENU2X_VERSION))
 GMENU2X_DEPENDENCIES = sdl sdl_ttf sdl_gfx dejavu libpng fonts-droid
 GMENU2X_CONF_OPTS = -DBIND_CONSOLE=ON
