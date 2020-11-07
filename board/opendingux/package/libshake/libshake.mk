6f913fe4c39909afacbcb150bdb46da3fa4e2b18
Gleb Mazovetskiy <glex.spb@gmail.com>
2020-11-07 14:53:55 +0000

    opendingux: Update libshake and add backwards compat symlink
    * Updates libshake to v0.3.2.
    * Symlinks libshake.so.2 to libshake.so.1 for backwards compatibility.
    
    Signed-off-by: Gleb Mazovetskiy <glex.spb@gmail.com>

--- board/opendingux/package/libshake/libshake.mk
+++ board/opendingux/package/libshake/libshake.mk
@@ -3,7 +3,7 @@
 # libshake
 #
 #############################################################
-LIBSHAKE_VERSION = 8ca1aaf
+LIBSHAKE_VERSION = v0.3.2
 LIBSHAKE_SITE = $(call github,zear,libShake,$(LIBSHAKE_VERSION))
 LIBSHAKE_LICENSE = MIT
 LIBSHAKE_LICENSE_FILES = LICENSE.txt
@@ -21,6 +21,12 @@ endef
 
 define LIBSHAKE_INSTALL_TARGET_CMDS
 	$(LIBSHAKE_MAKE_ENV) DESTDIR="$(TARGET_DIR)" $(MAKE) -C $(@D) BACKEND=LINUX install-lib
+
+	# libshake.so.1 symlink for backwards compatibility with existing OPKs.
+	# The API changes are [minimal][1] and do not seem to affect any existing OPKs.
+	#
+	# [1]: https://github.com/zear/libShake/commit/fb9b9e2c0ab391949317778e710aa8c235684783
+	ln -sf libshake.so.2 "$(TARGET_DIR)/usr/lib/libshake.so.1"
 endef
 
 $(eval $(generic-package))
