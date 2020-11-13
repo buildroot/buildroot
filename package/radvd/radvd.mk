6e85ab44493624748398ffb2c6bf4bda409f2de7
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-11-13 23:07:38 +0100

    package/radvd: bump to version 2.19
    Drop patch (already in version) and so autoreconf
    
    http://www.litech.org/radvd/CHANGES.txt:w
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/radvd/radvd.mk
+++ package/radvd/radvd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RADVD_VERSION = 2.18
+RADVD_VERSION = 2.19
 RADVD_SITE = http://www.litech.org/radvd/dist
 RADVD_DEPENDENCIES = host-bison host-flex host-pkgconf
 # We need to ignore <linux/if_arp.h>, because radvd already includes
@@ -12,8 +12,6 @@ RADVD_DEPENDENCIES = host-bison host-flex host-pkgconf
 RADVD_CONF_ENV = \
 	ac_cv_prog_cc_c99='-std=gnu99' \
 	ac_cv_header_linux_if_arp_h=no
-# For 0002-Don-t-force-fstack-protector-the-toolchain-might-lac.patch
-RADVD_AUTORECONF = YES
 RADVD_LICENSE = BSD-4-Clause-like
 RADVD_LICENSE_FILES = COPYRIGHT
 
