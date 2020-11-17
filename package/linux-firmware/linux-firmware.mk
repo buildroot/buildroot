b2a0b879d59d93c1c10bffed2c924e4833e102d4
Julien Olivain <ju.o@free.fr>
2020-11-17 22:21:44 +0100

    package/linux-firmware: bump version to 20201022
    This update is motivated by the inclusion SDIO firmware for QCA9377 WiFi
    cards in this new version. See [1].
    
    The license file "WHENCE" content/checksum has changed, since it's an
    index of firmware provenance and their licenses, and many new firmware
    files were added.
    
    For the full linux-firmware change log, see tag 20201022 log [2].
    
    [1] https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/commit/?id=d7904d5b07a9e2c4cdd9f8b2c5a5faa9c6e665cf
    [2] https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/log/?h=20201022
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/linux-firmware/linux-firmware.mk
+++ package/linux-firmware/linux-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LINUX_FIRMWARE_VERSION = 20200122
+LINUX_FIRMWARE_VERSION = 20201022
 LINUX_FIRMWARE_SITE = http://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git
 LINUX_FIRMWARE_SITE_METHOD = git
 
