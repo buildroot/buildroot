d399646aed9a335ce44b17d84daebe6b3c1eb4db
Paul Cercueil <paul@crapouillou.net>
2020-11-22 13:09:35 +0000

    opendingux/gcw0: Add Bluetooth support
    Add support for running bluez5 as the Bluetooth stack.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

---
+++ board/opendingux/gcw0/overlay/etc/init.d/S50bluetooth.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# Start bluetooth daemon
+#
+
+case "$1" in
+	start)
+		if [ -x /usr/libexec/bluetooth/bluetoothd ] ; then
+			echo "Starting Bluetooth... "
+			/usr/sbin/start-stop-daemon -S -b -m -p /var/run/bluetoothd.pid -x /usr/libexec/bluetooth/bluetoothd
+		fi
+		;;
+	stop)
+		if [ -x /usr/libexec/bluetooth/bluetoothd ] ; then
+			echo "Stopping Bluetooth..."
+			/usr/sbin/start-stop-daemon -K -p /var/run/bluetoothd.pid
+		fi
+		;;
+	*)
+		echo "Usage: $0 {start|stop}"
+		exit 1
+esac
+
+exit $?
