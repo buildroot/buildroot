262a7f6d2f406ed21b2379bb3c4a6c8f6fa0edd7
Angelo Compagnucci <angelo@amarulasolutions.com>
2020-11-24 07:59:17 +0100

    package/qt5/qt5knx: new package
    The Qt KNX module implements the client side of a connection between a
    client and a KNXnet/IP server.
    
    Signed-off-by: Angelo Compagnucci <angelo@amarulasolutions.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/qt5/qt5knx/qt5knx.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# qt5knx
+#
+################################################################################
+
+QT5KNX_VERSION = $(QT5_VERSION)
+QT5KNX_SITE = $(call github,qt,qtknx,v$(QT5_VERSION))
+QT5KNX_INSTALL_STAGING = YES
+QT5KNX_LICENSE = GPL-3.0 with exception
+QT5KNX_LICENSE_FILES = LICENSE.GPL3 LICENSE.GPL3-EXCEPT
+QT5KNX_SYNC_QT_HEADERS = YES
+
+$(eval $(qmake-package))
