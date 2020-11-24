16430fd8c607eec3e4447d67317e4af858ec4217
Romain Naour <romain.naour@gmail.com>
2020-11-24 21:30:22 +0100

    configs/qemu_arm_vexpress_tz_defconfig: build start-qemu.sh
    When tags was added by commit 011206b2bffd509b29365bc1eaa761414ee13726
    to detect the qemu command line, the qemu_arm_vexpress_tz_defconfig
    was ignored due to a build issue.
    
    This build issue has been fixed by previous patches, so we can
    enable the runtime testing by adding the tag in the readme.txt
    and the post-image script in the defconfig.
    
    Since Qemu from HOST_DIR is now executed directly from BINARIES_DIR,
    we can remove all the string before "qemu-system-*".
    
    Signed-off-by: Romain Naour <romain.naour@gmail.com>
    Cc: Yann E. MORIN <yann.morin.1998@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- board/qemu/post-image.sh
+++ board/qemu/post-image.sh
@@ -27,6 +27,9 @@ QEMU_CMD_LINE="${QEMU_CMD_LINE//output\/images\//}"
 DEFAULT_ARGS="$(sed -r -e '/-serial stdio/!d; s/.*(-serial stdio).*/\1/' <<<"${QEMU_CMD_LINE}")"
 QEMU_CMD_LINE="${QEMU_CMD_LINE//-serial stdio/}"
 
+# Remove any string before qemu-system-*
+QEMU_CMD_LINE="$(sed -r -e 's/^.*(qemu-system-)/\1/' <<<"${QEMU_CMD_LINE}")"
+
 # Disable graphical output and redirect serial I/Os to console
 case ${DEFCONFIG_NAME} in
   (qemu_sh4eb_r2d_defconfig|qemu_sh4_r2d_defconfig)
