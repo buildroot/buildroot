8526bebc88f031bdbe1e0fb07a851a8ae7d11246
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-12-03 20:01:33 +0100

    package/{mesa3d, mesa3d-headers}: bump version to 20.3.0
    Rebased patch 0002 & 0003.
    Removed patch 0005 which was applied upstream.
    
    Only Intel and Broadcom drivers depend on expat since upstream commit:
    https://cgit.freedesktop.org/mesa/mesa/commit/?id=81a0f1eca24b8605b9f2506122ff90db7129c39e
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/mesa3d/mesa3d.mk
+++ package/mesa3d/mesa3d.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 # When updating the version, please also update mesa3d-headers
-MESA3D_VERSION = 20.2.3
+MESA3D_VERSION = 20.3.0
 MESA3D_SOURCE = mesa-$(MESA3D_VERSION).tar.xz
 MESA3D_SITE = https://mesa.freedesktop.org/archive
 MESA3D_LICENSE = MIT, SGI, Khronos
@@ -19,9 +19,12 @@ MESA3D_DEPENDENCIES = \
 	host-bison \
 	host-flex \
 	host-python3-mako \
-	expat \
 	libdrm \
 	zlib
+
+ifeq ($(BR2_PACKAGE_EXPAT),y)
+MESA3D_DEPENDENCIES += expat
+endif
 
 MESA3D_CONF_OPTS = \
 	-Dgallium-omx=disabled \
