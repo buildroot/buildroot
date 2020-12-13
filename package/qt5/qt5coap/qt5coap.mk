6cfbd51d98d1b5fc7326ba0b0cc376f2f7c5ce59
Peter Seiderer <ps.report@gmx.net>
2020-12-13 12:19:46 +0100

    package/qt5/qt5coap: bump version to 5.15.2 (and fix download)
    - bump version to 5.15.2
    - change download url to original site
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/qt5/qt5coap/qt5coap.mk
+++ package/qt5/qt5coap/qt5coap.mk
@@ -5,7 +5,8 @@
 ################################################################################
 
 QT5COAP_VERSION = $(QT5_VERSION)
-QT5COAP_SITE = $(call github,qt,qtcoap,v$(QT5_VERSION))
+QT5COAP_SITE = https://code.qt.io/cgit/qt/qtcoap.git
+QT5COAP_SITE_METHOD = git
 QT5COAP_INSTALL_STAGING = YES
 QT5COAP_LICENSE = GPL-3.0, GFDL-1.3
 QT5COAP_LICENSE_FILES = LICENSE.GPL3 LICENSE.FDL
