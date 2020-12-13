d838a416c4ed447cec425ec479235d650b0b8b20
Peter Seiderer <ps.report@gmx.net>
2020-12-13 12:19:45 +0100

    package/qt5/qt5knx: bump version to 5.15.2 (and fix download)
    - bump version to 5.15.2
    - change download url to original site
    
    Fixes:
    
      - http://autobuild.buildroot.net/results/a043bb991a827880d6c743e63f070fc6a3b72a2f
    
      >>> qt5knx 5.15.2 Downloading[27m
      --2020-12-12 13:42:47--  https://github.com/qt/qtknx/archive/v5.15.2/qt5knx-5.15.2.tar.gz
      Resolving github.com (github.com)... 140.82.114.3
      Connecting to github.com (github.com)|140.82.114.3|:443... connected.
      HTTP request sent, awaiting response... 302 Found
      Location: https://codeload.github.com/qt/qtknx/tar.gz/v5.15.2/qt5knx-5.15.2 [following]
      --2020-12-12 13:42:47--  https://codeload.github.com/qt/qtknx/tar.gz/v5.15.2/qt5knx-5.15.2
      Resolving codeload.github.com (codeload.github.com)... 140.82.114.10
      Connecting to codeload.github.com (codeload.github.com)|140.82.114.10|:443... connected.
      HTTP request sent, awaiting response... 404 Not Found
      2020-12-12 13:42:47 ERROR 404: Not Found.
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/qt5/qt5knx/qt5knx.mk
+++ package/qt5/qt5knx/qt5knx.mk
@@ -5,7 +5,8 @@
 ################################################################################
 
 QT5KNX_VERSION = $(QT5_VERSION)
-QT5KNX_SITE = $(call github,qt,qtknx,v$(QT5_VERSION))
+QT5KNX_SITE = https://code.qt.io/cgit/qt/qtknx.git
+QT5KNX_SITE_METHOD = git
 QT5KNX_INSTALL_STAGING = YES
 QT5KNX_LICENSE = GPL-3.0 with exception
 QT5KNX_LICENSE_FILES = LICENSE.GPL3 LICENSE.GPL3-EXCEPT
