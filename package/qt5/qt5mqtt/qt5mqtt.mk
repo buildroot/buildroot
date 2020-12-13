4e8b5f9bee9cc127c07316ab043040adeada91a8
Peter Seiderer <ps.report@gmx.net>
2020-12-13 12:19:47 +0100

    package/qt5/qt5mqtt: bump version to 5.15.2 (and fix download)
    - bump version to 5.15.2
    - change download url to original site
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/qt5/qt5mqtt/qt5mqtt.mk
+++ package/qt5/qt5mqtt/qt5mqtt.mk
@@ -5,7 +5,8 @@
 ################################################################################
 
 QT5MQTT_VERSION = $(QT5_VERSION)
-QT5MQTT_SITE = $(call github,qt,qtmqtt,v$(QT5_VERSION))
+QT5MQTT_SITE = https://code.qt.io/cgit/qt/qtmqtt.git
+QT5MQTT_SITE_METHOD = git
 QT5MQTT_INSTALL_STAGING = YES
 QT5MQTT_LICENSE = GPL-3.0 with exception
 QT5MQTT_LICENSE_FILES = LICENSE.GPL3 LICENSE.GPL3-EXCEPT
