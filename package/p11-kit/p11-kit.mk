8a04076ec04fa4251e0739ac4eaaaefe1a5f85c5
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-12-21 13:36:17 +0100

    package/p11-kit: libffi is optional, not mandatory
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 10a2fcc81eb6fedd83849b5ece7c6f7b220f465f)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/p11-kit/p11-kit.mk
+++ package/p11-kit/p11-kit.mk
@@ -6,7 +6,7 @@
 
 P11_KIT_VERSION = 0.23.16.1
 P11_KIT_SITE = https://github.com/p11-glue/p11-kit/releases/download/$(P11_KIT_VERSION)
-P11_KIT_DEPENDENCIES = host-pkgconf libffi libtasn1
+P11_KIT_DEPENDENCIES = host-pkgconf libtasn1
 P11_KIT_INSTALL_STAGING = YES
 P11_KIT_CONF_OPTS = --disable-static
 P11_KIT_CONF_ENV = ac_cv_have_decl_program_invocation_short_name=yes \
@@ -18,6 +18,13 @@ ifeq ($(BR2_PACKAGE_CA_CERTIFICATES),y)
 P11_KIT_CONF_OPTS += --with-trust-paths=/etc/ssl/certs/ca-certificates.crt
 else
 P11_KIT_CONF_OPTS += --without-trust-paths
+endif
+
+ifeq ($(BR2_PACKAGE_LIBFFI),y)
+P11_KIT_DEPENDENCIES += host-pkgconf libffi
+P11_KIT_CONF_OPTS += --with-libffi
+else
+P11_KIT_CONF_OPTS += --without-libffi
 endif
 
 $(eval $(autotools-package))
