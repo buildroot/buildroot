36c428aaa10a034b994e6297828c33e7abca5ceb
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-12-23 12:22:59 +0100

    package/sysstat: enable lm-sensors support
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/sysstat/sysstat.mk
+++ package/sysstat/sysstat.mk
@@ -7,9 +7,16 @@
 SYSSTAT_VERSION = 12.4.2
 SYSSTAT_SOURCE = sysstat-$(SYSSTAT_VERSION).tar.xz
 SYSSTAT_SITE = http://pagesperso-orange.fr/sebastien.godard
-SYSSTAT_CONF_OPTS = --disable-file-attr --disable-sensors
+SYSSTAT_CONF_OPTS = --disable-file-attr
 SYSSTAT_DEPENDENCIES = host-gettext $(TARGET_NLS_DEPENDENCIES)
 SYSSTAT_LICENSE = GPL-2.0+
 SYSSTAT_LICENSE_FILES = COPYING
+
+ifeq ($(BR2_PACKAGE_LM_SENSORS),y)
+SYSSTAT_DEPENDENCIES += lm-sensors
+SYSSTAT_CONF_OPTS += --enable-sensors
+else
+SYSSTAT_CONF_OPTS += --disable-sensors
+endif
 
 $(eval $(autotools-package))
