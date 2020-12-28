924c1f93d4fe41d7ac332032b67b2d9d5d44e533
Pascal Vizeli <pvizeli@syshack.ch>
2020-12-28 17:05:32 +0100

    package/docker-containerd: add AppArmor support
    Signed-off-by: Pascal Vizeli <pvizeli@syshack.ch>
    Signed-off-by: Stefan Agner <stefan@agner.ch>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/docker-containerd/docker-containerd.mk
+++ package/docker-containerd/docker-containerd.mk
@@ -18,6 +18,11 @@ DOCKER_CONTAINERD_BUILD_TARGETS = cmd/ctr cmd/containerd cmd/containerd-shim
 
 DOCKER_CONTAINERD_INSTALL_BINS = containerd containerd-shim
 
+ifeq ($(BR2_PACKAGE_LIBAPPARMOR),y)
+DOCKER_CONTAINERD_DEPENDENCIES += libapparmor
+DOCKER_CONTAINERD_TAGS += apparmor
+endif
+
 ifeq ($(BR2_PACKAGE_LIBSECCOMP),y)
 DOCKER_CONTAINERD_DEPENDENCIES += libseccomp host-pkgconf
 DOCKER_CONTAINERD_TAGS += seccomp
