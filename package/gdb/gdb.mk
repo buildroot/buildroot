39267b5b1e19971b0e0e3bc06820a53b31c5eadf
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2020-12-28 21:10:22 +0100

    package/gdb: add support for gdb 10.x
    All patches from gdb 9.2 are kept, except
    0006-sim-ppc-Fix-linker-error-with-fno-common.patch which is
    upstream. The other patches are rebased to accommodate some code
    re-organization in gdb 10.x, especially the move of the gdbserver code
    from gdb/gdbserver/ to gdbserver/.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/gdb/gdb.mk
+++ package/gdb/gdb.mk
@@ -8,6 +8,12 @@ GDB_VERSION = $(call qstrip,$(BR2_GDB_VERSION))
 GDB_SITE = $(BR2_GNU_MIRROR)/gdb
 GDB_SOURCE = gdb-$(GDB_VERSION).tar.xz
 
+# recent gdb versions (>= 10) have gdbserver moved at the top-level,
+# which requires a different build logic.
+ifeq ($(BR2_GDB_VERSION_10),y)
+GDB_GDBSERVER_TOPLEVEL = y
+endif
+
 ifeq ($(BR2_arc),y)
 GDB_SITE = $(call github,foss-for-synopsys-dwc-arc-processors,binutils-gdb,$(GDB_VERSION))
 GDB_SOURCE = gdb-$(GDB_VERSION).tar.gz
