e935601d70a1dd118fc130e1af4280c467ecea70
Maxime Chevallier <maxime.chevallier@bootlin.com>
2020-12-28 17:11:25 +0100

    package/ipset: add SELinux module
    Support for ipset is added by the system/iptables module in the SELinux
    refpolicy.
    
    Signed-off-by: Maxime Chevallier <maxime.chevallier@bootlin.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/ipset/ipset.mk
+++ package/ipset/ipset.mk
@@ -12,5 +12,6 @@ IPSET_CONF_OPTS = --with-kmod=no
 IPSET_LICENSE = GPL-2.0
 IPSET_LICENSE_FILES = COPYING
 IPSET_INSTALL_STAGING = YES
+IPSET_SELINUX_MODULES = iptables
 
 $(eval $(autotools-package))
