58e319927ffeb1b68d67ce8c1a5d84bebc5c4157
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-01-15 08:52:49 +0100

    package/lcms2: add CPE variables
    cpe:2.3:a:littlecms:little_cms is a valid CPE identifier for this
    package:
    
      https://nvd.nist.gov/products/cpe/search/results?namingFormat=2.3&keyword=cpe%3A2.3%3Aa%3Alittlecms%3Alittle_cms
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/lcms2/lcms2.mk
+++ package/lcms2/lcms2.mk
@@ -8,6 +8,8 @@ LCMS2_VERSION = 2.11
 LCMS2_SITE = http://downloads.sourceforge.net/project/lcms/lcms/$(LCMS2_VERSION)
 LCMS2_LICENSE = MIT
 LCMS2_LICENSE_FILES = COPYING
+LCMS2_CPE_ID_VENDOR = littlecms
+LCMS2_CPE_ID_NAME = little_cms
 LCMS2_INSTALL_STAGING = YES
 
 ifeq ($(BR2_PACKAGE_JPEG),y)
