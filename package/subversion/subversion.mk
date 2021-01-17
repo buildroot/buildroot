1cd5d83ed91f526dba3bd264ba290486b1e36fbd
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-01-17 15:03:54 +0100

    package/subversion: add SUBVERSION_CPE_ID_VENDOR
    cpe:2.3:a:apache:subversion is a valid CPE identifier for this package:
    
      https://nvd.nist.gov/products/cpe/search/results?namingFormat=2.3&keyword=cpe%3A2.3%3Aa%3Aapache%3Asubversion
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/subversion/subversion.mk
+++ package/subversion/subversion.mk
@@ -9,6 +9,7 @@ SUBVERSION_SOURCE = subversion-$(SUBVERSION_VERSION).tar.bz2
 SUBVERSION_SITE = https://downloads.apache.org/subversion
 SUBVERSION_LICENSE = Apache-2.0
 SUBVERSION_LICENSE_FILES = LICENSE
+SUBVERSION_CPE_ID_VENDOR = apache
 SUBVERSION_DEPENDENCIES = \
 	host-pkgconf \
 	apr \
