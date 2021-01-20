d25818dfcc1f23b46089dfbc9e40d451d7613833
Petr Vorel <petr.vorel@gmail.com>
2021-01-20 21:22:10 +0100

    package/kmod: bump version to 28
    Signed-off-by: Petr Vorel <petr.vorel@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kmod/kmod.mk
+++ package/kmod/kmod.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KMOD_VERSION = 27
+KMOD_VERSION = 28
 KMOD_SOURCE = kmod-$(KMOD_VERSION).tar.xz
 KMOD_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/kernel/kmod
 KMOD_INSTALL_STAGING = YES
