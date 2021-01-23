fca4eb784207ce1c698fd6d73a0bd6728d7e29d1
Paul Cercueil <paul@crapouillou.net>
2021-01-23 21:55:52 +0000

    opendingux: libopk: Support building as host package
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- board/opendingux/package/libopk/libopk.mk
+++ board/opendingux/package/libopk/libopk.mk
@@ -8,4 +8,7 @@ LIBOPK_SITE = $(call github,pcercuei,libopk,$(LIBOPK_VERSION))
 LIBOPK_DEPENDENCIES = libini zlib
 LIBOPK_INSTALL_STAGING = YES
 
+HOST_LIBOPK_DEPENDENCIES = host-libini host-zlib
+
 $(eval $(cmake-package))
+$(eval $(host-cmake-package))
