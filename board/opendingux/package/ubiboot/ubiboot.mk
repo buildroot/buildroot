cac311ca9c88cb5ce2bc3c937c21f4a8bd90f321
Paul Cercueil <paul@crapouillou.net>
2021-01-23 16:34:26 +0000

    opendingux: ubiboot: Bump version & compile STAGE1
    Bump the version to the latest master, which adds support for lepus
    boards.
    
    Additionally, we now also build and install the STAGE1 version of
    ubiboot.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- board/opendingux/package/ubiboot/ubiboot.mk
+++ board/opendingux/package/ubiboot/ubiboot.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UBIBOOT_VERSION = f5a317e
+UBIBOOT_VERSION = 09dc1a5
 UBIBOOT_SITE = $(call github,pcercuei,UBIBoot,$(UBIBOOT_VERSION))
 UBIBOOT_BOARD_NAME = $(call qstrip,$(BR2_PACKAGE_UBIBOOT_BOARDNAME))
 
@@ -14,11 +14,20 @@ UBIBOOT_LICENSE_FILES = README
 UBIBOOT_INSTALL_IMAGES = YES
 
 define UBIBOOT_BUILD_CMDS
+	mkdir $(@D)/compiled
+
+	# Compile regular build
 	$(MAKE) CROSS_COMPILE="$(TARGET_CROSS)" -C $(@D) CONFIG=$(UBIBOOT_BOARD_NAME)
+	mv $(@D)/output/$(UBIBOOT_BOARD_NAME)/*.bin $(@D)/compiled
+
+	# Compile STAGE1 build
+	$(MAKE) STAGE1_ONLY=y CROSS_COMPILE="$(TARGET_CROSS)" -C $(@D) CONFIG=$(UBIBOOT_BOARD_NAME) clean
+	$(MAKE) STAGE1_ONLY=y CROSS_COMPILE="$(TARGET_CROSS)" -C $(@D) CONFIG=$(UBIBOOT_BOARD_NAME)
+	mv $(@D)/output/$(UBIBOOT_BOARD_NAME)/*.bin $(@D)/compiled
 endef
 
 define UBIBOOT_INSTALL_IMAGES_CMDS
-	$(INSTALL) -D -m 0644 $(@D)/output/$(UBIBOOT_BOARD_NAME)/*.bin $(BINARIES_DIR)
+	$(INSTALL) -D -m 0644 $(@D)/compiled/*.bin $(BINARIES_DIR)
 endef
 
 $(eval $(generic-package))
