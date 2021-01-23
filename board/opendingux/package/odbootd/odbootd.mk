9ff1a0e517feeeabf257ff629f625521ccf88e6e
Paul Cercueil <paul@crapouillou.net>
2021-01-23 21:56:27 +0000

    opendingux: odbootd: bump package
    Bump package to the latest version, which received a great overhaul.
    
    The odboot-client now performs the full boot operation: it will upload
    the stage1 bootloader over USB, the installer, and device tree. Instead
    of reading filenames from its command line, it now opens the
    rootfs/kernel/etc. files from within an update OPK.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- board/opendingux/package/odbootd/odbootd.mk
+++ board/opendingux/package/odbootd/odbootd.mk
@@ -4,13 +4,18 @@
 #
 ################################################################################
 
-ODBOOTD_VERSION = fc4f329
+ODBOOTD_VERSION = 89100ed
 ODBOOTD_SITE = $(call github,opendingux,odbootd,$(ODBOOTD_VERSION))
 
-HOST_ODBOOTD_DEPENDENCIES = host-libusb
+HOST_ODBOOTD_DEPENDENCIES = host-libusb host-libopk
 
 ODBOOTD_CONF_OPTS = -DWITH_ODBOOT_CLIENT:BOOL=OFF
 HOST_ODBOOTD_CONF_OPTS = -DWITH_ODBOOTD:BOOL=OFF
+
+ifeq ($(HOST_ODBOOTD_EMBEDDED_KERNEL),y)
+	HOST_ODBOOTD_DEPENDENCIES += linux
+	HOST_ODBOOTD_CONF_OPTS += -DEMBEDDED_INSTALLER=$(BINARIES_DIR)/$(LINUX_IMAGE_NAME)
+endif
 
 $(eval $(cmake-package))
 $(eval $(host-cmake-package))
