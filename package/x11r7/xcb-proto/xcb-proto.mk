6b66b64e49c53a4ff02a075c00ece654305b25ec
Titouan Christophe <titouanchristophe@gmail.com>
2021-01-24 11:57:35 +0100

    package/x11r7/xcb-proto: use host-python3 instead of host-python(2)
    Signed-off-by: Titouan Christophe <titouanchristophe@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/x11r7/xcb-proto/xcb-proto.mk
+++ package/x11r7/xcb-proto/xcb-proto.mk
@@ -12,8 +12,12 @@ XCB_PROTO_LICENSE_FILES = COPYING
 
 XCB_PROTO_INSTALL_STAGING = YES
 
-XCB_PROTO_DEPENDENCIES = host-python
-HOST_XCB_PROTO_DEPENDENCIES = host-python
+XCB_PROTO_DEPENDENCIES = host-python3
+HOST_XCB_PROTO_DEPENDENCIES = host-python3
+
+# Force detection of Buildroot host-python3 over system python
+XCB_PROTO_CONF_OPTS += ac_cv_path_PYTHON=$(HOST_DIR)/bin/python3
+HOST_XCB_PROTO_CONF_OPTS += ac_cv_path_PYTHON=$(HOST_DIR)/bin/python3
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
