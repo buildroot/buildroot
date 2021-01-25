38d04e6b13410f79ab18044737bdb078c637136a
Peter Korsgaard <peter@korsgaard.com>
2021-01-25 09:11:30 +0100

    configs/orangepi_r1_defconfig: change to GPT partitions for root=PARTLABEL support
    GPT provides partition labels, which can be used to tell the kernel to find
    the rootfs based on it (root=PARTLABEL=rootfs) as a nicer/more extensible
    solution than the MBR disk signature / PARTUUID.
    
    When using GPT, the name of the partition in genimage.cfg is used as the
    label for that partition.
    
    The default GPT partition table location unfortunately conflicts with the
    SPL location, so move the 16KB GPT table after it.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    [yann.morin.1998@free.fr: add the comment about 'rootfs']
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- board/orangepi/orangepi-r1/boot.cmd
+++ board/orangepi/orangepi-r1/boot.cmd
@@ -1,6 +1,6 @@
 setenv fdt_high ffffffff
 
-setenv bootargs console=ttyS0,115200 earlyprintk root=PARTUUID=feedc0de-02 rootwait
+setenv bootargs console=ttyS0,115200 earlyprintk root=PARTLABEL=rootfs rootwait
 
 fatload mmc 0 $kernel_addr_r zImage
 fatload mmc 0 $fdt_addr_r sun8i-h2-plus-orangepi-r1.dtb
