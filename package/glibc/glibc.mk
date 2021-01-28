3d7f510cc1b0c5fb21cdad0d9395005d9a74b5b3
Peter Korsgaard <peter@korsgaard.com>
2021-01-28 23:52:54 +0100

    package/glibc: security bump for additional post-2.30.x fixes
    Fixes the following security issue:
    
    - CVE-2021-3326: Assertion failure in ISO-2022-JP-3 gconv module related to
      combining characters
    
    For details, see https://sourceware.org/bugzilla/show_bug.cgi?id=27256 and
    https://www.openwall.com/lists/oss-security/2021/01/27/3
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -17,7 +17,7 @@ else
 # Generate version string using:
 #   git describe --match 'glibc-*' --abbrev=40 origin/release/MAJOR.MINOR/master | cut -d '-' -f 2-
 # When updating the version, please also update localedef
-GLIBC_VERSION = 2.30-73-gd59630f9959b0bb8991964758ab854ff4378b20d
+GLIBC_VERSION = 2.30-85-g79bf3c9774b0c2aa865b2ca397554ff425225d3c
 # Upstream doesn't officially provide an https download link.
 # There is one (https://sourceware.org/git/glibc.git) but it's not reliable,
 # sometimes the connection times out. So use an unofficial github mirror.
