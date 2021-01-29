57e8afed2dc829c728c67bb3b8bf416c606bc0c8
Paul Cercueil <paul@crapouillou.net>
2021-01-29 19:56:17 +0000

    opendingux: libini: Build host lib statically
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- board/opendingux/package/libini/libini.mk
+++ board/opendingux/package/libini/libini.mk
@@ -3,9 +3,11 @@
 # libini
 #
 #############################################################
-LIBINI_VERSION = c3413da
+LIBINI_VERSION = add8d48
 LIBINI_SITE = $(call github,pcercuei,libini,$(LIBINI_VERSION))
 LIBINI_INSTALL_STAGING = YES
+
+HOST_LIBINI_CONF_OPTS = -DBUILD_SHARED_LIBS=OFF
 
 $(eval $(cmake-package))
 $(eval $(host-cmake-package))
