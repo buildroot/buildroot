31a8fc9230aca531c0cfec4b5f2b816dc343c0f6
Paul Cercueil <paul@crapouillou.net>
2021-01-29 20:00:57 +0000

    opendingux: odbood: Bump and build host binary statically
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- board/opendingux/package/odbootd/odbootd.mk
+++ board/opendingux/package/odbootd/odbootd.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-ODBOOTD_VERSION = 89100ed
+ODBOOTD_VERSION = fe14438
 ODBOOTD_SITE = $(call github,opendingux,odbootd,$(ODBOOTD_VERSION))
 
 HOST_ODBOOTD_DEPENDENCIES = host-libusb host-libopk
 
 ODBOOTD_CONF_OPTS = -DWITH_ODBOOT_CLIENT:BOOL=OFF
-HOST_ODBOOTD_CONF_OPTS = -DWITH_ODBOOTD:BOOL=OFF
+HOST_ODBOOTD_CONF_OPTS = -DWITH_ODBOOTD:BOOL=OFF -DSTATIC_EXE=ON
 
 ifeq ($(HOST_ODBOOTD_EMBEDDED_KERNEL),y)
 	HOST_ODBOOTD_DEPENDENCIES += linux-rebuild-with-initramfs
