d142af595cc810149f746211413ac12c9967a228
Paul Cercueil <paul@crapouillou.net>
2021-01-29 19:56:43 +0000

    opendingux: libopk: Build host lib statically
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- board/opendingux/package/libopk/libopk.mk
+++ board/opendingux/package/libopk/libopk.mk
@@ -3,12 +3,13 @@
 # libopk
 #
 #############################################################
-LIBOPK_VERSION = 3c918c8
+LIBOPK_VERSION = 0b40691
 LIBOPK_SITE = $(call github,pcercuei,libopk,$(LIBOPK_VERSION))
 LIBOPK_DEPENDENCIES = libini zlib
 LIBOPK_INSTALL_STAGING = YES
 
 HOST_LIBOPK_DEPENDENCIES = host-libini host-zlib
+HOST_LIBOPK_CONF_OPTS = -DBUILD_SHARED_LIBS=OFF
 
 $(eval $(cmake-package))
 $(eval $(host-cmake-package))
