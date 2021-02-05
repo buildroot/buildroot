2ae81002d9165c66a9a385b23ed2c95879662aaa
Neeraj Deshpande <n.deshpande@metrological.com>
2021-02-05 20:11:03 +0530

    libprovision : version bump.

--- package/libprovision/libprovision.mk
+++ package/libprovision/libprovision.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-LIBPROVISION_VERSION = fed35396061478a72d770554d64ae3856e9846a2
+LIBPROVISION_VERSION = 10322dfe52ca4225289825dfdb4f78ae01345ed6
 LIBPROVISION_SITE_METHOD = git
 LIBPROVISION_SITE = git@github.com:Metrological/libprovision.git
 LIBPROVISION_LICENSE = PROPRIETARY
 LIBPROVISION_REDISTRIBUTE = NO
 LIBPROVISION_INSTALL_STAGING = YES
 
-LIBPROVISION_DEPENDENCIES = openssl
+LIBPROVISION_DEPENDENCIES = libopenssl
 
 $(eval $(cmake-package))
