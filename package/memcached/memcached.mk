5276b2f924ceda7a3b69fa565286af243810eacf
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-02-07 13:31:54 +0100

    package/memcached: add MEMCACHED_CPE_ID_VENDOR
    cpe:2.3:a:memcached:memcached is a valid CPE identifier for this
    package:
    
      https://nvd.nist.gov/products/cpe/search/results?namingFormat=2.3&keyword=cpe%3A2.3%3Aa%3Amemcached%3Amemcached
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/memcached/memcached.mk
+++ package/memcached/memcached.mk
@@ -11,6 +11,7 @@ MEMCACHED_CONF_ENV = ac_cv_prog_cc_c99='-std=gnu99'
 MEMCACHED_CONF_OPTS = --disable-coverage
 MEMCACHED_LICENSE = BSD-3-Clause
 MEMCACHED_LICENSE_FILES = COPYING
+MEMCACHED_CPE_ID_VENDOR = memcached
 
 ifeq ($(BR2_ENDIAN),"BIG")
 MEMCACHED_CONF_ENV += ac_cv_c_endian=big
