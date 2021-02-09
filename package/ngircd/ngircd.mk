a7bd76b6f5224ad78d344b6f8f0e065720c9ddb7
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-02-09 21:39:12 +0100

    package/ngircd: add NGIRCD_CPE_ID_VENDOR
    cpe:2.3:a:barton:ngircd is a valid CPE identifier for this package:
    
      https://nvd.nist.gov/products/cpe/search/results?namingFormat=2.3&keyword=cpe%3A2.3%3Aa%3Abarton%3Angircd
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ngircd/ngircd.mk
+++ package/ngircd/ngircd.mk
@@ -9,6 +9,7 @@ NGIRCD_SOURCE = ngircd-$(NGIRCD_VERSION).tar.xz
 NGIRCD_SITE = https://arthur.barton.de/pub/ngircd
 NGIRCD_LICENSE = GPL-2.0+
 NGIRCD_LICENSE_FILES = COPYING
+NGIRCD_CPE_ID_VENDOR = barton
 
 ifeq ($(BR2_PACKAGE_LINUX_PAM),y)
 NGIRCD_CONF_OPTS += --with-pam=$(STAGING_DIR)/usr
