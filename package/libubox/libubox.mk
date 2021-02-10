7851f5a1279e918e38ee531b8e77b21aef0848c1
Peter Seiderer <ps.report@gmx.net>
2021-02-11 00:16:34 +0100

    package/libubox: bump version to 5bc0146a
    - change download url to https
    
    - update homepage url (the old one returns 404 - No projects found)
    
    - add hash file
    
    - delete 0001-blobmsg-fix-array-out-of-bounds-GCC-10-warning.patch
      (from upstream [1])
    
    [1] https://git.openwrt.org/?p=project/libubox.git;a=commit;h=eb7eb6393d47a918c420f5b287946dbd6c0d5f57
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libubox/libubox.mk
+++ package/libubox/libubox.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-LIBUBOX_VERSION = 723730214732862de7e892bae2f7a7889e66bf1b
-LIBUBOX_SITE = git://git.openwrt.org/project/libubox.git
+LIBUBOX_VERSION = 5bc0146a1d6474d339c4ade9a227f32cc27a8b93
+LIBUBOX_SITE = https://git.openwrt.org/project/libubox.git
+LIBUBOX_SITE_METHOD = git
 LIBUBOX_LICENSE = ISC, BSD-3-Clause
 LIBUBOX_INSTALL_STAGING = YES
 LIBUBOX_DEPENDENCIES = $(if $(BR2_PACKAGE_JSON_C),json-c)
