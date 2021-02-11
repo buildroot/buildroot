464bbe26ff5fb9e5bfe26a26ea65c700b90598f5
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2021-02-11 22:37:29 +0100

    package/libxcrypt: new package
    libxcrypt is a modern library for one-way hashing of passwords.  It
    supports a wide variety of both modern and historical hashing methods:
    yescrypt, gost-yescrypt, scrypt, bcrypt, sha512crypt, sha256crypt,
    md5crypt, SunMD5, sha1crypt, NT.
    
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/libxcrypt/libxcrypt.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# libxcrypt
+#
+################################################################################
+
+LIBXCRYPT_VERSION = 4.4.17
+LIBXCRYPT_SITE = $(call github,besser82,libxcrypt,v$(LIBXCRYPT_VERSION))
+LIBXCRYPT_LICENSE = LGPL-2.1+
+LIBXCRYPT_LICENSE_FILES = LICENSING COPYING.LIB
+LIBXCRYPT_AUTORECONF = YES
+
+$(eval $(autotools-package))
