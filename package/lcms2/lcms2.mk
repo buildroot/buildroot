d4da2e2083a2d56bc7d7d41a4c8baf2b2fb8fd26
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-02-15 18:28:48 +0100

    package/lcms2: disable tiff
    tiff is only used by tificc sample and upstream rejected the patch to
    fix the static build failure because "adding pkg-config dependency for a
    sample is an overkill": https://github.com/mm2/Little-CMS/pull/244
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/lcms2/lcms2.mk
+++ package/lcms2/lcms2.mk
@@ -11,19 +11,14 @@ LCMS2_LICENSE_FILES = COPYING
 LCMS2_CPE_ID_VENDOR = littlecms
 LCMS2_CPE_ID_PRODUCT = little_cms
 LCMS2_INSTALL_STAGING = YES
+# tiff is only used by tificc sample
+LCMS2_CONF_OPTS = --without-tiff
 
 ifeq ($(BR2_PACKAGE_JPEG),y)
 LCMS2_CONF_OPTS += --with-jpeg
 LCMS2_DEPENDENCIES += jpeg
 else
 LCMS2_CONF_OPTS += --without-jpeg
-endif
-
-ifeq ($(BR2_PACKAGE_TIFF),y)
-LCMS2_CONF_OPTS += --with-tiff
-LCMS2_DEPENDENCIES += tiff
-else
-LCMS2_CONF_OPTS += --without-tiff
 endif
 
 ifeq ($(BR2_PACKAGE_ZLIB),y)
