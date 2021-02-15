af49734349ca06f88ed4c17e456fc385ebee2012
Peter Korsgaard <peter@korsgaard.com>
2021-02-15 17:05:56 +0100

    package/linux-firmware: also install into images for early loading support
    Some drivers request their firmware very early when built into the kernel,
    even before the initramfs is mounted - So the only way to provide firmware
    for those drivers is to include them directly in the kernel with the
    CONFIG_EXTRA_FIRMWARE option.
    
    An example of this is the uC firmware for modern Intel GPUs.
    
    Conceptually you can point CONFIG_EXTRA_FIRMWARE to
    ${TARGET_DIR}/lib/firmware, but then you cannot remove the firmware from the
    initramfs and pay the size cost twice (inside the kernel + in initramfs), so
    instead also install linux-firmware to the images dir, similar to how we do
    it for intel-microcode.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/linux-firmware/linux-firmware.mk
+++ package/linux-firmware/linux-firmware.mk
@@ -7,6 +7,7 @@
 LINUX_FIRMWARE_VERSION = 20201022
 LINUX_FIRMWARE_SITE = http://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git
 LINUX_FIRMWARE_SITE_METHOD = git
+LINUX_FIRMWARE_INSTALL_IMAGES = YES
 
 LINUX_FIRMWARE_CPE_ID_VENDOR = kernel
 
@@ -689,6 +690,10 @@ endef
 
 define LINUX_FIRMWARE_INSTALL_TARGET_CMDS
 	$(call LINUX_FIRMWARE_INSTALL_FW, $(TARGET_DIR)/lib/firmware)
+endef
+
+define LINUX_FIRMWARE_INSTALL_IMAGES_CMDS
+	$(call LINUX_FIRMWARE_INSTALL_FW, $(BINARIES_DIR))
 endef
 
 $(eval $(generic-package))
