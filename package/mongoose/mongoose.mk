baef15dffa7aa9516491e1d24f7e073726745077
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-02-16 09:07:55 +0100

    package/mongoose: security bump to version 7.1
    - Fix CVE-2021-26528: The mg_http_serve_file function in Cesanta
      Mongoose HTTP server 7.0 is vulnerable to remote OOB write attack via
      connection request after exhausting memory pool.
    - Fix CVE-2021-26529: The mg_tls_init function in Cesanta Mongoose HTTPS
      server 7.0 and 6.7-6.18 (compiled with mbedTLS support) is vulnerable
      to remote OOB write attack via connection request after exhausting
      memory pool.
    - Fix CVE-2021-26530: The mg_tls_init function in Cesanta Mongoose HTTPS
      server 7.0 (compiled with OpenSSL support) is vulnerable to remote OOB
      write attack via connection request after exhausting memory pool.
    
    https://github.com/cesanta/mongoose/releases/tag/7.1
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/mongoose/mongoose.mk
+++ package/mongoose/mongoose.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MONGOOSE_VERSION = 7.0
+MONGOOSE_VERSION = 7.1
 MONGOOSE_SITE = $(call github,cesanta,mongoose,$(MONGOOSE_VERSION))
 MONGOOSE_LICENSE = GPL-2.0
 MONGOOSE_LICENSE_FILES = LICENSE
