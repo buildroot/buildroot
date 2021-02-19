82abd78a01fc832f758fc2b2d7326879500fb786
Peter Korsgaard <peter@korsgaard.com>
2021-02-19 10:59:41 +0100

    package/python-django: security bump to version 3.0.13
    Fixes the following security issue:
    
    - CVE-2021-23336: Web cache poisoning via django.utils.http.limited_parse_qsl()
    
      Django contains a copy of urllib.parse.parse_qsl() which was added to
      backport some security fixes.  A further security fix has been issued
      recently such that parse_qsl() no longer allows using ; as a query
      parameter separator by default.  Django now includes this fix.  See
      bpo-42967 for further details.
    
    For more details, see the advisory:
    https://www.djangoproject.com/weblog/2021/feb/19/security-releases/
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 3.0.12
+PYTHON_DJANGO_VERSION = 3.0.13
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/32/e3/e7e9a9378321fdfc3eb55de151911dce968fa245d1f16d8c480c63ea4ed1
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/3b/fe/11ec9b4cbae447e7b90d551be035d55c1293973592b491540334452f1f1f
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
 PYTHON_DJANGO_CPE_ID_VENDOR = djangoproject
