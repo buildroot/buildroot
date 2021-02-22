d1d5c1c9141ab4e510938715d1a397a817f078e9
Antoine Tenart <atenart@kernel.org>
2021-02-22 13:47:58 +0100

    package/refpolicy: bump to 2.20210203
    Changelog:
    https://github.com/SELinuxProject/refpolicy/releases/tag/RELEASE_2_20210203
    
    Signed-off-by: Antoine Tenart <atenart@kernel.org>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/refpolicy/refpolicy.mk
+++ package/refpolicy/refpolicy.mk
@@ -22,9 +22,9 @@ REFPOLICY_SITE = $(call qstrip,$(BR2_PACKAGE_REFPOLICY_CUSTOM_REPO_URL))
 REFPOLICY_SITE_METHOD = git
 BR_NO_CHECK_HASH_FOR += $(REFPOLICY_SOURCE)
 else
-REFPOLICY_VERSION = 2.20200818
+REFPOLICY_VERSION = 2.20210203
 REFPOLICY_SOURCE = refpolicy-$(REFPOLICY_VERSION).tar.bz2
-REFPOLICY_SITE = https://github.com/SELinuxProject/refpolicy/releases/download/RELEASE_2_20200818
+REFPOLICY_SITE = https://github.com/SELinuxProject/refpolicy/releases/download/RELEASE_2_20210203
 endif
 
 # Cannot use multiple threads to build the reference policy
