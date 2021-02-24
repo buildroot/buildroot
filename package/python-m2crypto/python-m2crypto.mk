8c017f37773079d83acf147e0517af538bd3514b
Gustavo Heinz <gustavo@gsthnz.com>
2021-02-24 20:28:50 -0300

    package/python-m2crypto: bump to version 0.37.1
    Also, removed 0001 patch since it's already merged upstream.
    
    https://gitlab.com/m2crypto/m2crypto/-/merge_requests/248
    
    Signed-off-by: Gustavo Heinz <gustavo@gsthnz.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-m2crypto/python-m2crypto.mk
+++ package/python-m2crypto/python-m2crypto.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_M2CRYPTO_VERSION = 0.36.0
+PYTHON_M2CRYPTO_VERSION = 0.37.1
 PYTHON_M2CRYPTO_SOURCE = M2Crypto-$(PYTHON_M2CRYPTO_VERSION).tar.gz
-PYTHON_M2CRYPTO_SITE = https://files.pythonhosted.org/packages/ff/df/84609ed874b5e6fcd3061a517bf4b6e4d0301f553baf9fa37bef2b509797
+PYTHON_M2CRYPTO_SITE = https://files.pythonhosted.org/packages/aa/36/9fef97358e378c1d3bd567c4e8f8ca0428a8d7e869852cef445ee6da91fd
 PYTHON_M2CRYPTO_SETUP_TYPE = setuptools
 PYTHON_M2CRYPTO_LICENSE = MIT
 PYTHON_M2CRYPTO_LICENSE_FILES = LICENCE
