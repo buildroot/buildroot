0e60a9aa835a2141d4f8e382dc736862a29f6e7f
Peter Korsgaard <peter@korsgaard.com>
2021-02-27 13:38:44 +0100

    package/python-aiohttp: security bump to version 3.7.4
    Fixes the following security issue:
    
    CVE-2021-21330: Open redirect vulnerability in aiohttp
    (normalize_path_middleware middleware)
    
    Beast Glatisant and Jelmer Vernooij reported that python-aiohttp, a async
    HTTP client/server framework, is prone to an open redirect vulnerability.  A
    maliciously crafted link to an aiohttp-based web-server could redirect the
    browser to a different website.
    
    For more details, see the advisory:
    https://github.com/aio-libs/aiohttp/security/advisories/GHSA-v6wp-4m6f-gcjg
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-aiohttp/python-aiohttp.mk
+++ package/python-aiohttp/python-aiohttp.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_AIOHTTP_VERSION = 3.7.3
+PYTHON_AIOHTTP_VERSION = 3.7.4
 PYTHON_AIOHTTP_SOURCE = aiohttp-$(PYTHON_AIOHTTP_VERSION).tar.gz
-PYTHON_AIOHTTP_SITE = https://files.pythonhosted.org/packages/68/96/40a765d7d68028c5a6d169b2747ea3f4828ec91a358a63818d468380521c
+PYTHON_AIOHTTP_SITE = https://files.pythonhosted.org/packages/7a/95/eb60aaad7943e18c9d091de93c9b0b5ed40aa67c7d5e3c5ee9b36f100a38
 PYTHON_AIOHTTP_SETUP_TYPE = setuptools
 PYTHON_AIOHTTP_LICENSE = Apache-2.0
 PYTHON_AIOHTTP_LICENSE_FILES = LICENSE.txt
