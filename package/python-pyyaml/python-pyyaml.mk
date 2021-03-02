31052d0dc1e2f12da0279e44433e96e4ddb8f20c
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-03-02 18:25:23 +0100

    package/python-pyyaml: security bump to version 5.4.1
    Fix CVE-2020-14343: A vulnerability was discovered in the PyYAML library
    in versions before 5.4, where it is susceptible to arbitrary code
    execution when it processes untrusted YAML files through the full_load
    method or with the FullLoader loader. Applications that use the library
    to process untrusted input may be vulnerable to this flaw. This flaw
    allows an attacker to execute arbitrary code on the system by abusing
    the python/object/new constructor. This flaw is due to an incomplete fix
    for CVE-2020-1747.
    
    Update hash of LICENSE file (update in year:
    https://github.com/yaml/pyyaml/commit/58d0cb7ee09954c67fabfbd714c5673b03e7a9e1)
    
    https://github.com/yaml/pyyaml/blob/5.4.1/CHANGES
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit de43a9775d4646035b18eb5737e5fa4cd2eeedea)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-pyyaml/python-pyyaml.mk
+++ package/python-pyyaml/python-pyyaml.mk
@@ -5,9 +5,9 @@
 ################################################################################
 
 # Please keep in sync package/python3-pyyaml/python3-pyyaml.mk
-PYTHON_PYYAML_VERSION = 5.3.1
+PYTHON_PYYAML_VERSION = 5.4.1
 PYTHON_PYYAML_SOURCE = PyYAML-$(PYTHON_PYYAML_VERSION).tar.gz
-PYTHON_PYYAML_SITE = https://files.pythonhosted.org/packages/64/c2/b80047c7ac2478f9501676c988a5411ed5572f35d1beff9cae07d321512c
+PYTHON_PYYAML_SITE = https://files.pythonhosted.org/packages/a0/a4/d63f2d7597e1a4b55aa3b4d6c5b029991d3b824b5bd331af8d4ab1ed687d
 PYTHON_PYYAML_SETUP_TYPE = distutils
 PYTHON_PYYAML_LICENSE = MIT
 PYTHON_PYYAML_LICENSE_FILES = LICENSE
