831a1bd2999ccda2e9e05b7c6ad9e7f3070ecd60
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-03-03 15:43:44 +0100

    package/sdl2_image: add SDL2_IMAGE_CPE_ID_VENDOR
    cpe:2.3:a:libsdl:sdl2_image is a valid CPE identifier for this package:
    
      https://nvd.nist.gov/products/cpe/search/results?namingFormat=2.3&keyword=cpe%3A2.3%3Aa%3Alibsdl%3Asdl2_image
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/sdl2_image/sdl2_image.mk
+++ package/sdl2_image/sdl2_image.mk
@@ -10,6 +10,7 @@ SDL2_IMAGE_SITE = http://www.libsdl.org/projects/SDL_image/release
 SDL2_IMAGE_INSTALL_STAGING = YES
 SDL2_IMAGE_LICENSE = Zlib
 SDL2_IMAGE_LICENSE_FILES = COPYING.txt
+SDL2_IMAGE_CPE_ID_VENDOR = libsdl
 
 # Unconditionally enable support for image formats that don't require
 # any dependency.
