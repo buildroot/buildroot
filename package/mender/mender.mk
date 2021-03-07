b5056518cd870f6245360312e86ab0a38f6c0061
Adam Duskett <aduskett@gmail.com>
2021-03-07 14:25:34 -0800

    package/mender: bump version to 2.4.2
    Other changes:
      - Openssl is now a dependency.
      - Set new license hashes due to new dependencies.
      - Set new license file hash due to a year change.
    
    Signed-off-by: Adam Duskett <aduskett@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/mender/mender.mk
+++ package/mender/mender.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MENDER_VERSION = 2.3.0
+MENDER_VERSION = 2.4.2
 MENDER_SITE = https://github.com/mendersoftware/mender/archive
 MENDER_SOURCE = $(MENDER_VERSION).tar.gz
 MENDER_LICENSE = Apache-2.0, BSD-2-Clause, BSD-3-Clause, ISC, MIT, OLDAP-2.8
@@ -16,6 +16,7 @@ MENDER_LICENSE_FILES = \
 	LIC_FILES_CHKSUM.sha256 \
 	vendor/github.com/mendersoftware/mendertesting/LICENSE \
 	vendor/github.com/mendersoftware/mender-artifact/LICENSE \
+	vendor/github.com/mendersoftware/openssl/LICENSE \
 	vendor/github.com/pkg/errors/LICENSE \
 	vendor/github.com/pmezard/go-difflib/LICENSE \
 	vendor/golang.org/x/crypto/LICENSE \
@@ -27,13 +28,14 @@ MENDER_LICENSE_FILES = \
 	vendor/github.com/davecgh/go-spew/LICENSE \
 	vendor/github.com/sirupsen/logrus/LICENSE \
 	vendor/github.com/stretchr/testify/LICENSE \
-	vendor/github.com/stretchr/testify/LICENCE.txt \
-	vendor/github.com/stretchr/objx/LICENSE.md \
 	vendor/github.com/ungerik/go-sysfs/LICENSE \
-	vendor/github.com/urfave/cli/LICENSE \
+	vendor/github.com/urfave/cli/v2/LICENSE \
+	vendor/github.com/stretchr/objx/LICENSE \
+	vendor/github.com/konsorten/go-windows-terminal-sequences/LICENSE \
+	vendor/gopkg.in/yaml.v3/LICENSE \
 	vendor/github.com/bmatsuo/lmdb-go/LICENSE.mdb.md
 
-MENDER_DEPENDENCIES = xz
+MENDER_DEPENDENCIES = openssl xz
 
 MENDER_LDFLAGS = -X main.Version=$(MENDER_VERSION)
 
