7e958a24e233ffabffbd2edf601ebf516f2a35c5
Grzegorz Blach <grzegorz@blach.pl>
2021-03-08 18:30:00 +0100

    package/python-bluezero: bump to version 0.6.0
    Signed-off-by: Grzegorz Blach <grzegorz@blach.pl>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-bluezero/python-bluezero.mk
+++ package/python-bluezero/python-bluezero.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_BLUEZERO_VERSION = 0.5.0
+PYTHON_BLUEZERO_VERSION = 0.6.0
 PYTHON_BLUEZERO_SOURCE = bluezero-$(PYTHON_BLUEZERO_VERSION).tar.gz
-PYTHON_BLUEZERO_SITE = https://files.pythonhosted.org/packages/90/63/67c56cebe6f741b190f047531810911cfb528e9c0ec6db8d4c911808ba32
+PYTHON_BLUEZERO_SITE = https://files.pythonhosted.org/packages/be/15/4a806580ffd359a03184776f37cf201298918f302b414b8a3e594d1be65c
 PYTHON_BLUEZERO_SETUP_TYPE = setuptools
 PYTHON_BLUEZERO_LICENSE = MIT
 PYTHON_BLUEZERO_LICENSE_FILES = LICENSE
