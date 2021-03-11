9e778b044c949d165207150cc9e79fdcfbafcb9d
Peter Seiderer <ps.report@gmx.net>
2021-03-11 21:44:48 +0100

    package/bash: bump version to 5.1
    - removed 0001-bash50-001.patch to 0018-bash50-018.patch
    
    - added 0001-bash51-001.patch, 0002-bash51-002.patch, 0003-bash51-003.patch,
      0004-bash51-004.patch
    
    - moved 0019-input.h-add-missing-include-on-stdio.h.patch to
      0005-input.h-add-missing-include-on-stdio.h.patch
    
    - removed 0020-locale.c-fix-build-without-wchar.patch
      (superseded by upstream commit)
    
    - added 0006-locale-fix-typo-local_shiftstates-vs.-locale_shiftst.patch
      to fix typo from upstream commit (see previous patch)
    
    - added 0007-glob-fix-dequote_pathname-vs.-udequote_pathname.patch
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/bash/bash.mk
+++ package/bash/bash.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BASH_VERSION = 5.0
+BASH_VERSION = 5.1
 BASH_SITE = $(BR2_GNU_MIRROR)/bash
 BASH_DEPENDENCIES = ncurses readline host-bison
 BASH_CONF_OPTS = --with-installed-readline --without-bash-malloc
