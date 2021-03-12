f14662bad7e3d7fb8430bdd7a7e88b3eed4e232c
Christian Stewart <christian@paral.in>
2021-03-12 12:56:59 -0800

    package/go: security bump to 1.16.2
    go1.16.1 (released 2021/03/10) includes security fixes to the archive/zip and
    encoding/xml packages.
    
    go1.16.2 (released 2021/03/11) includes fixes to cgo, the compiler, linker, the
    go command, and the syscall and time packages.
    
    https://golang.org/doc/devel/release.html#go1.16
    
    Signed-off-by: Christian Stewart <christian@paral.in>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 9b36b4aa284466e7294ef8d3ef741b674a96a993)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/go/go.mk
+++ package/go/go.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GO_VERSION = 1.16
+GO_VERSION = 1.16.2
 GO_SITE = https://storage.googleapis.com/golang
 GO_SOURCE = go$(GO_VERSION).src.tar.gz
 
