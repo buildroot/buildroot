46e54492ba2b9e9e8aa9cb8c362666e2fcca66f9
Peter Seiderer <ps.report@gmx.net>
2021-03-13 23:42:11 +0100

    package/rsync: bump version to 3.2.3
    - disable simd, openssl, xxhash, zstd, lz4, asm options
    
    - update COPYING hash (add openssl and xxhash license
      enhancement):
    
      In addition, as a special exception, the copyright holders give
      permission to dynamically link rsync with the OpenSSL and xxhash
      libraries when those libraries are being distributed in compliance
      with their license terms, and to distribute a dynamically linked
      combination of rsync and these libraries.  This is also considered
      to be covered under the GPL's System Libraries exception.
    
    For details see [1].
    
    [1] https://download.samba.org/pub/rsync/NEWS#3.2.3
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Cc: Baruch Siach <baruch@tkos.co.il>
    [yann.morin.1998@free.fr: add 'with exception' to _LICENSE (Baruch)]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/rsync/rsync.mk
+++ package/rsync/rsync.mk
@@ -4,15 +4,21 @@
 #
 ################################################################################
 
-RSYNC_VERSION = 3.1.3
+RSYNC_VERSION = 3.2.3
 RSYNC_SITE = http://rsync.samba.org/ftp/rsync/src
-RSYNC_LICENSE = GPL-3.0+
+RSYNC_LICENSE = GPL-3.0+ with exceptions
 RSYNC_LICENSE_FILES = COPYING
 RSYNC_CPE_ID_VENDOR = samba
 RSYNC_DEPENDENCIES = zlib popt
 RSYNC_CONF_OPTS = \
 	--with-included-zlib=no \
-	--with-included-popt=no
+	--with-included-popt=no \
+	--disable-simd \
+	--disable-openssl \
+	--disable-xxhash \
+	--disable-zstd \
+	--disable-lz4 \
+	--disable-asm
 
 ifeq ($(BR2_PACKAGE_ACL),y)
 RSYNC_DEPENDENCIES += acl
