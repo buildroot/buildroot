1a4738fc2a7895e2ecbb8064ae3fcf17ed1405ce
Pierre Wielders <pierre@wielders.net>
2021-03-17 10:07:12 +0100

    [REVERT] BUMP of RPI-Userland and RPI-Firmware

--- package/rpi-userland/rpi-userland.mk
+++ package/rpi-userland/rpi-userland.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_USERLAND_VERSION = 4a0a19b88b43e48c6b51b526b9378289fb712a4c
+RPI_USERLAND_VERSION = 06bc6daa02137ca72b7a2104afad81e82a44de17
 RPI_USERLAND_SITE = $(call github,raspberrypi,userland,$(RPI_USERLAND_VERSION))
 RPI_USERLAND_LICENSE = BSD-3-Clause
 RPI_USERLAND_LICENSE_FILES = LICENCE
