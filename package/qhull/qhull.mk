c9012ac8e60ce6580d2ce9821237375ca790d511
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-03-23 20:18:26 +0100

    package/qhull: bump to version 8.0.2
    - Static libs are supported since version 8.0.2 and
      https://github.com/qhull/qhull/commit/613debeaea72ee66626dace9ba1a2eff11b5d37d
    - Update hash of COPYING, update year and authors with
      https://github.com/qhull/qhull/commit/4733a95be0298919a646a1d6044c1bee7a552880
    - Update indentation in hash file (two spaces)
    
    https://github.com/qhull/qhull/releases/tag/v8.0.2
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/qhull/qhull.mk
+++ package/qhull/qhull.mk
@@ -4,11 +4,22 @@
 #
 ################################################################################
 
-QHULL_VERSION = 7.3.2
+QHULL_VERSION = 8.0.2
 QHULL_SITE = http://www.qhull.org/download
-QHULL_SOURCE = qhull-2019-src-$(QHULL_VERSION).tgz
+QHULL_SOURCE = qhull-2020-src-$(QHULL_VERSION).tgz
 QHULL_INSTALL_STAGING = YES
 QHULL_LICENSE = BSD-Style
 QHULL_LICENSE_FILES = COPYING.txt
+
+# BUILD_SHARED_LIBS is handled in pkg-cmake.mk as it is a generic cmake variable
+# although BUILD_STATIC_LIBS=ON is default, make it explicit,
+# cmake and static/shared libs is confusing enough already.
+ifeq ($(BR2_STATIC_LIBS),y)
+QHULL_CONF_OPTS += -DBUILD_STATIC_LIBS=ON
+else ifeq ($(BR2_SHARED_STATIC_LIBS),y)
+QHULL_CONF_OPTS += -DBUILD_STATIC_LIBS=ON
+else ifeq ($(BR2_SHARED_LIBS),y)
+QHULL_CONF_OPTS += -DBUILD_STATIC_LIBS=OFF
+endif
 
 $(eval $(cmake-package))
