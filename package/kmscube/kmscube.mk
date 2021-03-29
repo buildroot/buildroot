d48ad723dda57020110c2cf8d91864bbc41cc45a
msieben <github@desiderium.eu>
2021-03-29 12:55:49 +0200

    [kmscube]: Change download URL for Pi4 used version.

--- package/kmscube/kmscube.mk
+++ package/kmscube/kmscube.mk
@@ -16,7 +16,7 @@ $(eval $(meson-package))
 else
 
 KMSCUBE_VERSION = 76bb57d539cb43d267e561024c34e031bf351e04
-KMSCUBE_SITE = https://cgit.freedesktop.org/mesa/kmscube/snapshot
+KMSCUBE_SITE = https://gitlab.freedesktop.org/mesa/kmscube/-/archive/$(KMSCUBE_VERSION)
 KMSCUBE_LICENSE = MIT
 KMSCUBE_DEPENDENCIES = host-pkgconf mesa3d libdrm
 KMSCUBE_AUTORECONF = YES
