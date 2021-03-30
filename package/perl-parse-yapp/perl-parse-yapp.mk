3fe60eda6a22bd5508aa9e64a373609164e7a1c2
Francois Perrad <fperrad@gmail.com>
2021-03-30 11:00:26 +0200

    package/perl-parse-yapp: remove useless dependencies
    Parse-Yapp comes with a Makefile.PL,
    so it is built with the perl core module ExtUtils-MakeMaker
    
    regenerated with `utils/scancpan -force -host Parse-Yapp`
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 6bcc220ccd5d2bdedeea81518e8cedf49b5af007)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/perl-parse-yapp/perl-parse-yapp.mk
+++ package/perl-parse-yapp/perl-parse-yapp.mk
@@ -7,11 +7,9 @@
 PERL_PARSE_YAPP_VERSION = 1.21
 PERL_PARSE_YAPP_SOURCE = Parse-Yapp-$(PERL_PARSE_YAPP_VERSION).tar.gz
 PERL_PARSE_YAPP_SITE = $(BR2_CPAN_MIRROR)/authors/id/W/WB/WBRASWELL
-PERL_PARSE_YAPP_DEPENDENCIES = host-perl-module-build
 PERL_PARSE_YAPP_LICENSE = Artistic or GPL-1.0+
 PERL_PARSE_YAPP_LICENSE_FILES = lib/Parse/Yapp.pm
 PERL_PARSE_YAPP_DISTNAME = Parse-Yapp
-HOST_PERL_PARSE_YAPP_DEPENDENCIES = host-perl-module-build
 
 $(eval $(perl-package))
 $(eval $(host-perl-package))
