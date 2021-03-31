69405d89596988b5b7d25886b7f9c07efad70741
Grzegorz Blach <grzegorz@blach.pl>
2021-03-31 14:26:24 +0200

    package/python-hiredis: bump to version 2.0.0
    Depends on BR2_PACKAGE_PYTHON3
    
    Signed-off-by: Grzegorz Blach <grzegorz@blach.pl>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-hiredis/python-hiredis.mk
+++ package/python-hiredis/python-hiredis.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_HIREDIS_VERSION = 1.1.0
+PYTHON_HIREDIS_VERSION = 2.0.0
 PYTHON_HIREDIS_SOURCE = hiredis-$(PYTHON_HIREDIS_VERSION).tar.gz
-PYTHON_HIREDIS_SITE = https://files.pythonhosted.org/packages/3d/9f/abc69e73055f73d42ddf9c46b3e01a08b9e74b579b8fc413cbd31112a749
+PYTHON_HIREDIS_SITE = https://files.pythonhosted.org/packages/0c/39/eae11344d69ba435ec13d6bcc1a9eea3d2278324506fcd0e52d1ed8958c8
 PYTHON_HIREDIS_SETUP_TYPE = setuptools
 PYTHON_HIREDIS_LICENSE = BSD-3-Clause
 PYTHON_HIREDIS_LICENSE_FILES = COPYING vendor/hiredis/COPYING
