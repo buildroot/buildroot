50dfbe22a92036ae0c5642880603cd894954cc15
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-04-02 18:33:41 +0200

    package/qpid-proton: fix build without threads
    Build of qpid-proton is broken since bump to version 0.33.0 in commit
    d4c0fde91da0d79204a21ed8de1bd410efa1c4d6 because epoll proactor
    unconditonally uses pthread
    
    Fixes:
     - http://autobuild.buildroot.org/results/ec34da16a11f0600ecfbbbc4039e8210aea0498c
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    [yann.morin.1998@free.fr: C++ precision in comment]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/qpid-proton/qpid-proton.mk
+++ package/qpid-proton/qpid-proton.mk
@@ -31,6 +31,14 @@ QPID_PROTON_CONF_OPTS = \
 	-DENABLE_WARNING_ERROR=OFF \
 	-DPYTHON_EXECUTABLE=$(HOST_DIR)/bin/python2
 
+# epoll proactor unconditionally uses pthread and cpp (C++) bindings
+# unconditionally use proactor
+ifeq ($(BR2_TOOLCHAIN_HAS_THREADS),)
+QPID_PROTON_CONF_OPTS += \
+	-DBUILD_CPP=OFF \
+	-DPROACTOR=none
+endif
+
 ifeq ($(BR2_PACKAGE_JSONCPP),y)
 QPID_PROTON_DEPENDENCIES += jsoncpp
 QPID_PROTON_CONF_OPTS += -DENABLE_JSONCPP=ON
