cac23e01afc7241d4e2f90285256722b451d4391
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-04-02 21:54:07 +0200

    package/lldpd: add LLDPD_CPE_ID_VENDOR
    cpe:2.3:a:lldpd_project:lldpd is a valid CPE identifier for this
    package:
    
      https://nvd.nist.gov/products/cpe/search/results?namingFormat=2.3&keyword=cpe%3A2.3%3Aa%3Alldpd_project%3Alldpd
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit dc66d2d2a8648821754190d2bb35c3f23af2ac5b)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/lldpd/lldpd.mk
+++ package/lldpd/lldpd.mk
@@ -14,6 +14,7 @@ LLDPD_DEPENDENCIES = \
 	$(if $(BR2_PACKAGE_VALGRIND),valgrind)
 LLDPD_LICENSE = ISC
 LLDPD_LICENSE_FILES = LICENSE
+LLDPD_CPE_ID_VENDOR = lldpd_project
 
 # Detection of c99 support in configure fails without WCHAR. To enable
 # automatic detection of c99 support by configure, we need to enable
