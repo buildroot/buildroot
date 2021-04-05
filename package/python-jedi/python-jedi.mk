7f268154a00666685a1d91273100bddc640e4a6f
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-04-05 15:36:12 +0200

    package/python-jedi: bump to version 0.18.0
    python 2 support has been dropped since version 0.18.0 and
    https://github.com/davidhalter/jedi/commit/d67dfba7f5a65d5ee064d37e1fb894a25b39bdab
    
    Add django-stubs license file (MIT)
    
    https://github.com/davidhalter/jedi/blob/v0.18.0/CHANGELOG.rst
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-jedi/python-jedi.mk
+++ package/python-jedi/python-jedi.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_JEDI_VERSION = 0.17.0
+PYTHON_JEDI_VERSION = 0.18.0
 PYTHON_JEDI_SOURCE = jedi-$(PYTHON_JEDI_VERSION).tar.gz
-PYTHON_JEDI_SITE = https://files.pythonhosted.org/packages/e3/5b/65ff9c102d92bf719dfaeff57bc8074d68f26ea480005704a956da995799
+PYTHON_JEDI_SITE = https://files.pythonhosted.org/packages/ac/11/5c542bf206efbae974294a61febc61e09d74cb5d90d8488793909db92537
 PYTHON_JEDI_SETUP_TYPE = setuptools
 PYTHON_JEDI_LICENSE = MIT, Apache-2.0 (typeshed)
-PYTHON_JEDI_LICENSE_FILES = LICENSE.txt jedi/third_party/typeshed/LICENSE
+PYTHON_JEDI_LICENSE_FILES = LICENSE.txt jedi/third_party/django-stubs/LICENSE.txt jedi/third_party/typeshed/LICENSE
 
 $(eval $(python-package))
