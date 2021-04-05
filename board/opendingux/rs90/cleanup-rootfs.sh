46b1f9b154eed2c7e90488e4821996ad2d063d31
Paul Cercueil <paul@crapouillou.net>
2021-04-05 19:01:04 +0100

    od: Fix cmake/ccmake scripts
    echo '\n' does NOT print a new line, echo -e '\n' does.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- board/opendingux/rs90/cleanup-rootfs.sh
+++ board/opendingux/rs90/cleanup-rootfs.sh
@@ -20,11 +20,11 @@ if [ ! -h $1/usr/share/fonts/truetype ] ; then
 fi
 
 # Create cmake helpers
-echo '#!/bin/sh\n\nexec cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_TOOLCHAIN_FILE=`dirname $0`/../share/buildroot/toolchainfile.cmake $*' > ${HOST_DIR}/usr/bin/mipsel-rs90-linux-musl-cmake
+echo -e '#!/bin/sh\n\nexec cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_TOOLCHAIN_FILE=`dirname $0`/../share/buildroot/toolchainfile.cmake $*' > ${HOST_DIR}/usr/bin/mipsel-rs90-linux-musl-cmake
 chmod +x ${HOST_DIR}/usr/bin/mipsel-rs90-linux-musl-cmake
 ln -sf mipsel-rs90-linux-musl-cmake ${HOST_DIR}/usr/bin/mipsel-linux-cmake
 
 # Create ccmake helpers
-echo '#!/bin/sh\n\nexec ccmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_TOOLCHAIN_FILE=`dirname $0`/../share/buildroot/toolchainfile.cmake $*' > ${HOST_DIR}/usr/bin/mipsel-rs90-linux-musl-ccmake
+echo -e '#!/bin/sh\n\nexec ccmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_TOOLCHAIN_FILE=`dirname $0`/../share/buildroot/toolchainfile.cmake $*' > ${HOST_DIR}/usr/bin/mipsel-rs90-linux-musl-ccmake
 chmod +x ${HOST_DIR}/usr/bin/mipsel-rs90-linux-musl-ccmake
 ln -sf mipsel-rs90-linux-musl-ccmake ${HOST_DIR}/usr/bin/mipsel-linux-ccmake
