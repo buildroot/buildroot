fcba0aec7b840c6e6e68ba8b5b04c3b093d0f372
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-04-05 16:47:08 +0200

    package/python-paramiko: bump to version 2.7.2
    Update indentation in hash file (two spaces)
    
    https://github.com/paramiko/paramiko/blob/2.7.2/sites/www/changelog.rst
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    [Peter: fix LICENSE hash]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-paramiko/python-paramiko.mk
+++ package/python-paramiko/python-paramiko.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PARAMIKO_VERSION = 2.7.1
+PYTHON_PARAMIKO_VERSION = 2.7.2
 PYTHON_PARAMIKO_SOURCE = paramiko-$(PYTHON_PARAMIKO_VERSION).tar.gz
-PYTHON_PARAMIKO_SITE = https://files.pythonhosted.org/packages/ac/15/4351003352e11300b9f44a13576bff52dcdc6e4a911129c07447bda0a358
+PYTHON_PARAMIKO_SITE = https://files.pythonhosted.org/packages/cf/a1/20d00ce559a692911f11cadb7f94737aca3ede1c51de16e002c7d3a888e0
 PYTHON_PARAMIKO_SETUP_TYPE = setuptools
 PYTHON_PARAMIKO_LICENSE = LGPL-2.1+
 PYTHON_PARAMIKO_LICENSE_FILES = LICENSE
