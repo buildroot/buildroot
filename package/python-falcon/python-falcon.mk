85d912cb9d87e410c007519f2991ee887bab28b6
Grzegorz Blach <grzegorz@blach.pl>
2021-04-06 18:06:04 +0200

    package/python-falcon: bump to version 3.0.0
    Depends on BR2_PACKAGE_PYTHON3
    
    The hash of the license file has changed because a short license file
    was replaced by the complete text of the Apache 2.0 license. The
    license itself remains the same.
    
    Signed-off-by: Grzegorz Blach <grzegorz@blach.pl>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-falcon/python-falcon.mk
+++ package/python-falcon/python-falcon.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_FALCON_VERSION = 2.0.0
+PYTHON_FALCON_VERSION = 3.0.0
 PYTHON_FALCON_SOURCE = falcon-$(PYTHON_FALCON_VERSION).tar.gz
-PYTHON_FALCON_SITE = https://files.pythonhosted.org/packages/19/30/edff5a1fea7a8e9876c8391e170263e1bb207875b6a65cd619818487b27b
+PYTHON_FALCON_SITE = https://files.pythonhosted.org/packages/f1/59/2748d63922a46e040be06cd175a6242a33ade7839aac37f441544bb6d651
 PYTHON_FALCON_SETUP_TYPE = setuptools
 PYTHON_FALCON_LICENSE = Apache-2.0
 PYTHON_FALCON_LICENSE_FILES = LICENSE
