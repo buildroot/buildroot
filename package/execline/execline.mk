bf66772c9b089dfcd67e1abbe04d7db52562310a
Dick Olsson <hi@senzilla.io>
2021-04-06 17:57:20 +0000

    package/execline: bump to version 2.8.0.0
    http://skarnet.org/cgi-bin/archive.cgi?1:mss:1535:lpehbljhhcpaopbnkkbf
    
    Signed-off-by: Dick Olsson <hi@senzilla.io>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/execline/execline.mk
+++ package/execline/execline.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-EXECLINE_VERSION = 2.6.1.0
+EXECLINE_VERSION = 2.8.0.0
 EXECLINE_SITE = http://skarnet.org/software/execline
 EXECLINE_LICENSE = ISC
 EXECLINE_LICENSE_FILES = COPYING
