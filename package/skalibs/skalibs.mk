4d5587cb56224b2b28f53b0202fb14b2ab32d5fb
Dick Olsson <hi@senzilla.io>
2021-04-06 17:57:13 +0000

    package/skalibs: bump to version 2.10.0.2
    - Drop patch that has been included upstream
    
    http://skarnet.org/cgi-bin/archive.cgi?1:mss:1535:lpehbljhhcpaopbnkkbf
    
    Signed-off-by: Dick Olsson <hi@senzilla.io>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/skalibs/skalibs.mk
+++ package/skalibs/skalibs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SKALIBS_VERSION = 2.9.2.1
+SKALIBS_VERSION = 2.10.0.2
 SKALIBS_SITE = http://skarnet.org/software/skalibs
 SKALIBS_LICENSE = ISC
 SKALIBS_LICENSE_FILES = COPYING
