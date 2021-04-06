1e44b1b838cbd9b99ceec48fc80640372046ebb5
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-04-06 22:04:31 +0200

    package/python-websocket-client: bump to version 0.58
    - Update hash of LICENSE (license switched back to LGPL-2.1+:
      https://github.com/websocket-client/websocket-client/commit/6eaed48d49ea6a1a792b152a477bf9026f0c29b4)
    - Update indentation in hash file (two spaces)
    
    https://github.com/websocket-client/websocket-client/blob/v0.58.0/ChangeLog
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/python-websocket-client/python-websocket-client.mk
+++ package/python-websocket-client/python-websocket-client.mk
@@ -4,14 +4,11 @@
 #
 ################################################################################
 
-PYTHON_WEBSOCKET_CLIENT_VERSION = 0.57.0
+PYTHON_WEBSOCKET_CLIENT_VERSION = 0.58.0
 PYTHON_WEBSOCKET_CLIENT_SOURCE = websocket_client-$(PYTHON_WEBSOCKET_CLIENT_VERSION).tar.gz
-PYTHON_WEBSOCKET_CLIENT_SITE = https://files.pythonhosted.org/packages/8b/0f/52de51b9b450ed52694208ab952d5af6ebbcbce7f166a48784095d930d8c
+PYTHON_WEBSOCKET_CLIENT_SITE = https://files.pythonhosted.org/packages/4a/df/112c278ba1ead96786d24d973429ce1e1a2c86b9843183d9f8ef8c6330d7
 PYTHON_WEBSOCKET_CLIENT_SETUP_TYPE = setuptools
-# Project was under LGPL-2.1+, but was relicensed under
-# BSD-3-Clause. The LICENSE file contains the BSD-3-Clause text, but
-# the source files still contain a LGPL-2.1+ header.
-PYTHON_WEBSOCKET_CLIENT_LICENSE = LGPL-2.1+, BSD-3-Clause
+PYTHON_WEBSOCKET_CLIENT_LICENSE = LGPL-2.1+
 PYTHON_WEBSOCKET_CLIENT_LICENSE_FILES = LICENSE
 
 $(eval $(python-package))
