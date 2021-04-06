18462cf00b46e439be424417ed8f1612802988b5
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-04-06 20:39:27 +0200

    package/libupnp: security bump to version 1.14.5
    Non-recursive version of ixmlNode_free() avoids stack overflow
    attack. Fixes CVE-2021-28302.
    
    Also a number of other bugfixes:
    https://github.com/pupnp/pupnp/blob/release-1.14.5/ChangeLog
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    (cherry picked from commit c9d339c3d1da30c4bff5fb1577e094e02838ffc1)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libupnp/libupnp.mk
+++ package/libupnp/libupnp.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-LIBUPNP_VERSION = 1.14.0
+LIBUPNP_VERSION = 1.14.5
 LIBUPNP_SOURCE = libupnp-$(LIBUPNP_VERSION).tar.bz2
 LIBUPNP_SITE = \
-	http://downloads.sourceforge.net/project/pupnp/pupnp/libupnp-$(LIBUPNP_VERSION)
+	http://downloads.sourceforge.net/project/pupnp/release-$(LIBUPNP_VERSION)
 LIBUPNP_CONF_ENV = ac_cv_lib_compat_ftime=no
 LIBUPNP_INSTALL_STAGING = YES
 LIBUPNP_LICENSE = BSD-3-Clause
