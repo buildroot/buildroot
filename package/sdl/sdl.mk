377d0d1006c8f157d74d4e670489bcffbf7d544a
Paul Cercueil <paul@crapouillou.net>
2021-04-08 17:54:06 +0100

    sdl: bump package version
    Bump to the latest OpenDingux version, which re-introduces support for
    8bpp emulated with YUV surfaces.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- package/sdl/sdl.mk
+++ package/sdl/sdl.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SDL_VERSION = fe16d0552
+SDL_VERSION = 5fb18f6c4
 SDL_SITE = $(call github,OpenDingux,SDL,$(SDL_VERSION))
 SDL_LICENSE = LGPL-2.1+
 SDL_LICENSE_FILES = COPYING
