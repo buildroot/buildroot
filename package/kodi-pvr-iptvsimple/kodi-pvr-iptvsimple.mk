25135631d22e5c581984a0006847df5a5ea790bd
Bernd Kuhls <bernd.kuhls@t-online.de>
2021-04-09 20:21:47 +0200

    package/kodi-pvr-iptvsimple: bump version to 7.6.1-Matrix
    Changelog:
    https://github.com/kodi-pvr/pvr.iptvsimple/blob/Matrix/pvr.iptvsimple/changelog.txt
    
    Upstream added a dependency to xz:
    https://github.com/kodi-pvr/pvr.iptvsimple/commit/8f19dac9a5f394d44a16fcfa4235ea8c11e9cc96
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/kodi-pvr-iptvsimple/kodi-pvr-iptvsimple.mk
+++ package/kodi-pvr-iptvsimple/kodi-pvr-iptvsimple.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-KODI_PVR_IPTVSIMPLE_VERSION = 7.6.0-Matrix
+KODI_PVR_IPTVSIMPLE_VERSION = 7.6.1-Matrix
 KODI_PVR_IPTVSIMPLE_SITE = $(call github,kodi-pvr,pvr.iptvsimple,$(KODI_PVR_IPTVSIMPLE_VERSION))
 KODI_PVR_IPTVSIMPLE_LICENSE = GPL-2.0+
 KODI_PVR_IPTVSIMPLE_LICENSE_FILES = LICENSE.md
-KODI_PVR_IPTVSIMPLE_DEPENDENCIES = kodi pugixml zlib
+KODI_PVR_IPTVSIMPLE_DEPENDENCIES = kodi pugixml xz zlib
 
 $(eval $(cmake-package))
