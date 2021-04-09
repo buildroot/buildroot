73bf3292e16b9419c5c88d10e9755d7208ca3623
Peter Seiderer <ps.report@gmx.net>
2021-04-09 23:12:57 +0200

    package/python-colorzero: bump version to 2.0
    - change to python3 only
    
    - update license file hash:
      @@ -1,4 +1,4 @@
      -Copyright 2016-2018 Dave Jones <dave@waveform.org.uk>
      +SPDX-License-Identifier: BSD-3-Clause
    
    Changelog ([1]):
    
      - Dropped Python 2.x support. Current Python support level is 3.5 and above.
    
      - Added html and css format specifications to the :class:`Color` class'
        string-formatting capabilities.
    
    [1] https://github.com/waveform80/colorzero/blob/master/docs/changelog.rst
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-colorzero/python-colorzero.mk
+++ package/python-colorzero/python-colorzero.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_COLORZERO_VERSION = 1.1
+PYTHON_COLORZERO_VERSION = 2.0
 PYTHON_COLORZERO_SITE = $(call github,waveform80,colorzero,release-$(PYTHON_COLORZERO_VERSION))
 PYTHON_COLORZERO_LICENSE = BSD-3-Clause
 PYTHON_COLORZERO_LICENSE_FILES = LICENSE.txt
