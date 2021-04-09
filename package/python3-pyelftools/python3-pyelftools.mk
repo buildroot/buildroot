f90056070b9643d5e9a1626ed3592748a2c6a1f7
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-04-10 00:36:40 +0200

    package/python-pyelftools: bump to version 0.27
    Update indentation in hash file (two spaces)
    
    https://github.com/eliben/pyelftools/blob/v0.27/CHANGES
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python3-pyelftools/python3-pyelftools.mk
+++ package/python3-pyelftools/python3-pyelftools.mk
@@ -5,9 +5,9 @@
 ################################################################################
 
 # Please keep in sync with package/python-pyelftools/python-pyelftools.mk
-PYTHON3_PYELFTOOLS_VERSION = 0.25
+PYTHON3_PYELFTOOLS_VERSION = 0.27
 PYTHON3_PYELFTOOLS_SOURCE = pyelftools-$(PYTHON3_PYELFTOOLS_VERSION).tar.gz
-PYTHON3_PYELFTOOLS_SITE = https://files.pythonhosted.org/packages/fa/9a/0674cb1725196568bdbca98304f2efb17368b57af1a4bb3fc772c026f474
+PYTHON3_PYELFTOOLS_SITE = https://files.pythonhosted.org/packages/6b/b5/f7022f2d950327ba970ec85fb8f85c79244031092c129b6f34ab17514ae0
 PYTHON3_PYELFTOOLS_LICENSE = Public domain
 PYTHON3_PYELFTOOLS_LICENSE_FILES = LICENSE
 PYTHON3_PYELFTOOLS_SETUP_TYPE = setuptools
