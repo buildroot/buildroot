108c8d4e15f13888596a6b8b615f721a9b5280e6
charles <charles@ccxtechnologies.com>
2021-04-09 12:44:11 -0400

    uprev asterisk

--- package/asterisk/asterisk.mk
+++ package/asterisk/asterisk.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ASTERISK_VERSION = 86afe10
+ASTERISK_VERSION = 1933216
 # Use the github mirror: it's an official mirror maintained by Digium, and
 # provides tarballs, which the main Asterisk git tree (behind Gerrit) does not.
 ASTERISK_SITE = $(call github,ccxtechnologies,asterisk,$(ASTERISK_VERSION))
