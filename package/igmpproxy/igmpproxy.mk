4cd04b401c9ecad6f0db5515fe7d2881b1fadf1a
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-04-10 14:14:51 +0200

    package/igmpproxy: bump to version 0.3
    - Update indention in hash file (two spaces)
    - Use official tarball
    
    https://github.com/pali/igmpproxy/releases/tag/0.3
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/igmpproxy/igmpproxy.mk
+++ package/igmpproxy/igmpproxy.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-IGMPPROXY_VERSION = 0.2.1
-IGMPPROXY_SITE = $(call github,pali,igmpproxy,$(IGMPPROXY_VERSION))
+IGMPPROXY_VERSION = 0.3
+IGMPPROXY_SITE = \
+	https://github.com/pali/igmpproxy/releases/download/$(IGMPPROXY_VERSION)
 IGMPPROXY_AUTORECONF = YES
 IGMPPROXY_LICENSE = GPL-2.0+, BSD-3-Clause (mrouted)
 IGMPPROXY_LICENSE_FILES = COPYING GPL.txt Stanford.txt
