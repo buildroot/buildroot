bbda38afbbfa79ff436e1a187bb267b8d9558036
Chris Simons <chris.simons@rockwellcollins.com>
2021-04-12 11:23:47 -0500

    package/xml-security-c: new package
    Adds support for the Apache Santuario XML Security for C++ that
    implementations the XML Digital Signature and Encryption
    specifications.
    
    Signed-off-by: Chris Simons <chris.simons@rockwellcollins.com>
    Signed-off-by: Matt Weber <matthew.weber@rockwellcollins.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/xml-security-c/xml-security-c.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# xml-security-c
+#
+################################################################################
+
+XML_SECURITY_C_VERSION = 2.0.2
+XML_SECURITY_C_SITE = http://archive.apache.org/dist/santuario/c-library
+XML_SECURITY_C_LICENSE = Apache-2.0
+XML_SECURITY_C_LICENSE_FILES = LICENSE.txt
+XML_SECURITY_C_DEPENDENCIES = openssl xerces
+XML_SECURITY_C_INSTALL_STAGING = YES
+
+XML_SECURITY_C_CONF_ENV = \
+	xml_cv_func_getcwd_null=yes \
+	CXXFLAGS="$(TARGET_CXXFLAGS) -std=c++11"
+
+# Patched configure.ac
+XML_SECURITY_C_AUTORECONF = YES
+
+$(eval $(autotools-package))
