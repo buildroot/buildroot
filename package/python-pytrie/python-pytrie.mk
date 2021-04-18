5c81a1a833b43afefef7d8172a59cd0d67f81bc8
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-04-18 22:38:58 +0200

    package/python-pytrie: bump to version 0.4.0
    - python 2 support has been dropped since
      https://github.com/gsakkis/pytrie/commit/a60a601d85a660976f4aaab4cb0efcb15b91263b
    - Use LICENSE instead of PKG-INFO which is available in the official
      tarball since
      https://github.com/gsakkis/pytrie/commit/1ba5d547dfbf0d384dd16cf78a2cce91ef69fce8
    - Update indentation in hash file (two spaces)
    
    https://github.com/gsakkis/pytrie/blob/0.4.0/README.md
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-pytrie/python-pytrie.mk
+++ package/python-pytrie/python-pytrie.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_PYTRIE_VERSION = 0.3.1
+PYTHON_PYTRIE_VERSION = 0.4.0
 PYTHON_PYTRIE_SOURCE = PyTrie-$(PYTHON_PYTRIE_VERSION).tar.gz
 PYTHON_PYTRIE_LICENSE = BSD-3-Clause
-PYTHON_PYTRIE_LICENSE_FILES = PKG-INFO
-PYTHON_PYTRIE_SITE = https://files.pythonhosted.org/packages/e1/eb/ae1f098969c9e9b81e821fb8e916cbf720b900ec1c0f3359e47a427395ec
+PYTHON_PYTRIE_LICENSE_FILES = LICENSE
+PYTHON_PYTRIE_SITE = https://files.pythonhosted.org/packages/d3/19/15ec77ab9c85f7c36eb590d6ab7dd529f8c8516c0e2219f1a77a99d7ee77
 PYTHON_PYTRIE_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
