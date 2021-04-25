6e75a990ae35580800f24f3ddc4573c6bc38fa05
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-04-25 19:22:22 +0200

    package/daq3: new package
    Add daq3 package to be able to add the snort3 package.
    
    daq3 is not compatatible with snort as daq_load_modules has been removed
    
    daq3 package has been created from the daq package, here is a summary of
    the changes:
    - Drop patch (not needed anymore)
    - Enable parallel build as there is no more tokdefs.h
    - Drop libdnet dependency
    - Drop host-bison and host-flex dependencies
    - Drop libpcap workarounds
    - ipq module dropped since version 3.0.0-alpha1 and
      https://github.com/snort3/libdaq/commit/80c62799a936118c80894bf91f396c8aa8ba45a6
    - Add LICENSE file (same content than COPYING)
    - Update hash of COPYING (project name and GPLv2 text updated with
      https://github.com/snort3/libdaq/commit/46e8722da230082a5728175270ea3c04432d1af0)
    - Add a dependency on gcc 4.9 for {t,u}h_{d,s}port that are protected on
      glibc by _FAVOR_BSD (and so !defined _GNU_SOURCE) until version 2.19:
      https://sourceware.org/git/?p=glibc.git;a=commit;h=7011c2622fe3e10a29dbe74f06aaebd07710127d
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

---
+++ package/daq3/daq3.mk
@@ -0,0 +1,30 @@
+################################################################################
+#
+# daq3
+#
+################################################################################
+
+DAQ3_VERSION = 3.0.3
+DAQ3_SITE = $(call github,snort3,libdaq,v$(DAQ3_VERSION))
+DAQ3_LICENSE = GPL-2.0
+DAQ3_LICENSE_FILES = COPYING LICENSE
+DAQ3_INSTALL_STAGING = YES
+DAQ3_DEPENDENCIES = host-pkgconf
+# From git
+DAQ3_AUTORECONF = YES
+
+ifeq ($(BR2_PACKAGE_LIBNETFILTER_QUEUE),y)
+DAQ3_DEPENDENCIES += libnetfilter_queue
+DAQ3_CONF_OPTS += --enable-nfq-module
+else
+DAQ3_CONF_OPTS += --disable-nfq-module
+endif
+
+ifeq ($(BR2_PACKAGE_LIBPCAP),y)
+DAQ3_DEPENDENCIES += libpcap
+DAQ3_CONF_OPTS += --enable-pcap-module
+else
+DAQ3_CONF_OPTS += --disable-pcap-module
+endif
+
+$(eval $(autotools-package))
