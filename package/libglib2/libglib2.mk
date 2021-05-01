c72524fb1b1c6b7dd1b1f511eea0fdf5a39dd3a0
Adam Duskett <aduskett@gmail.com>
2021-05-01 12:56:18 -0700

    package/libglib2: bump to version 2.68.1
    Other changes:
      - Drop 0002-allow-explicit-disabling-of-tests.patch as this patch has been
        upstreamed and the option is now simply "tests."
      - Add -Dglib_debug=disabled and -Dlibelf=disabled as defaults to both
        HOST_LIBGLIB2_CONF_OPTS and LIBGLIB2_CONF_OPTS
      - Refactor existing patches to apply to 2.68.1
    
    Tested with test-pkg -p libglib2 -a.
    
    Signed-off-by: Adam Duskett <aduskett@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/libglib2/libglib2.mk
+++ package/libglib2/libglib2.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-LIBGLIB2_VERSION_MAJOR = 2.66
-LIBGLIB2_VERSION = $(LIBGLIB2_VERSION_MAJOR).8
+LIBGLIB2_VERSION_MAJOR = 2.68
+LIBGLIB2_VERSION = $(LIBGLIB2_VERSION_MAJOR).1
 LIBGLIB2_SOURCE = glib-$(LIBGLIB2_VERSION).tar.xz
 LIBGLIB2_SITE = http://ftp.gnome.org/pub/gnome/sources/glib/$(LIBGLIB2_VERSION_MAJOR)
 LIBGLIB2_LICENSE = LGPL-2.1+
@@ -25,11 +25,13 @@ endif
 HOST_LIBGLIB2_CONF_OPTS = \
 	-Ddtrace=false \
 	-Dfam=false \
+	-Dglib_debug=disabled \
+	-Dlibelf=disabled \
 	-Dselinux=disabled \
 	-Dsystemtap=false \
 	-Dxattr=false \
 	-Dinternal_pcre=false \
-	-Dinstalled_tests=false \
+	-Dtests=false \
 	-Doss_fuzz=disabled
 
 LIBGLIB2_DEPENDENCIES = \
@@ -49,9 +51,11 @@ HOST_LIBGLIB2_DEPENDENCIES = \
 # ${libdir} would be prefixed by the sysroot by pkg-config, causing a
 # bogus installation path once combined with $(DESTDIR).
 LIBGLIB2_CONF_OPTS = \
+	-Dglib_debug=disabled \
 	-Dinternal_pcre=false \
+	-Dlibelf=disabled \
 	-Dgio_module_dir=/usr/lib/gio/modules \
-	-Dinstalled_tests=false \
+	-Dtests=false \
 	-Doss_fuzz=disabled
 
 LIBGLIB2_MESON_EXTRA_PROPERTIES = \
