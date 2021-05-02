bef953db89530c81360f890d00cebd42a7db968a
Peter Seiderer <ps.report@gmx.net>
2021-05-02 11:48:29 +0200

    package/rpi-wifi-firmware: bump version to 4c47758
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Tested-by: Marcin Niestroj <m.niestroj@grinn-global.com>
    Reviewed-by: Marcin Niestroj <m.niestroj@grinn-global.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rpi-wifi-firmware/rpi-wifi-firmware.mk
+++ package/rpi-wifi-firmware/rpi-wifi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_WIFI_FIRMWARE_VERSION = 9c0ffe9a7f0753b36ed88f7981905a989d940ea9
+RPI_WIFI_FIRMWARE_VERSION = 4c47758bed7cd547dff3618a1739dcf64b5a52a7
 RPI_WIFI_FIRMWARE_SITE = $(call github,LibreELEC,brcmfmac_sdio-firmware-rpi,$(RPI_WIFI_FIRMWARE_VERSION))
 RPI_WIFI_FIRMWARE_LICENSE = PROPRIETARY
 RPI_WIFI_FIRMWARE_LICENSE_FILES = LICENCE.broadcom_bcm43xx
