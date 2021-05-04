buildroot/buildroot

commit 8283e838f040767b7e232d3f914324915f303e4b

package/freescale-imx/imx-gpu-viv: bump to version 6.4.3.p1.2
- Same version as NXP release 5.10.9_1.0.0
- EULA/COPYING: update to LA_OPT_NXP_Software_License v19

Signed-off-by: Sébastien Szymanski <sebastien.szymanski@armadeus.com>
Reviewed-by: Stephane Viau <stephane.viau@oss.nxp.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/freescale-imx/imx-gpu-viv/imx-gpu-viv.mk
@@ -5,9 +5,9 @@
 ################################################################################
 
 ifeq ($(BR2_aarch64),y)
-IMX_GPU_VIV_VERSION = 6.4.3.p1.0-aarch64
+IMX_GPU_VIV_VERSION = 6.4.3.p1.2-aarch64
 else
-IMX_GPU_VIV_VERSION = 6.4.3.p1.0-aarch32
+IMX_GPU_VIV_VERSION = 6.4.3.p1.2-aarch32
 endif
 IMX_GPU_VIV_SITE = $(FREESCALE_IMX_SITE)
 IMX_GPU_VIV_SOURCE = imx-gpu-viv-$(IMX_GPU_VIV_VERSION).bin

package/freescale-imx/imx-gpu-viv/imx-gpu-viv.hash
@@ -1,5 +1,5 @@
 # Locally calculated
-sha256	9960fadbc1ea919f8b1b5c1e2e38a52b7293b70c906518d7fff2ada4140c59ea  imx-gpu-viv-6.4.3.p1.0-aarch32.bin
-sha256	0505620c3851a980d2e08fee4b4fcd06eff92efac02d1646924bea86e4384ad4  imx-gpu-viv-6.4.3.p1.0-aarch64.bin
-sha256	9c16421e7c702f56756650b8ac954d34556327e598a8666e6e8f4eb3a1aa95f1  COPYING
-sha256	72edc2072c86d93aa1993d15d4d19d96270af3749b0108995ad50c81d1461f52  EULA
+sha256  0f139f5544250a3059c0a0266d44f70e89632694861de3e186272b170b37c464  imx-gpu-viv-6.4.3.p1.2-aarch32.bin
+sha256  3de60afb1f54c0888d9849c5a27f938ba229f9f35675456a173bdf00e3538c5d  imx-gpu-viv-6.4.3.p1.2-aarch64.bin
+sha256  4f3cc2dcbe3b7369bd4a51df749f432b69d8189fc2bde88f9fadbec73c686683  COPYING
+sha256  a07e8df685161553d7e0b78b8b93ebe9086d95bb8635abff0ed3247992181e85  EULA

commit 34b893a6d7abb6381080a66e1aa4cee57cdf0bbc

package/freescale-imx/imx-gpu-viv: select libdrm
Some libraries (libGL.so, vivante_dri.so, libEGL.so, libgbm_viv.so) are
linked against libdrm so select libdrm package.

Fixes: 8283e838f040 ("package/freescale-imx/imx-gpu-viv: bump to version 6.4.3.p1.2")
Signed-off-by: Sébastien Szymanski <sebastien.szymanski@armadeus.com>
Reviewed-by: Gary Bisson <gary.bisson@boundarydevices.com>
Tested-by: Gary Bisson <gary.bisson@boundarydevices.com>
Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

package/freescale-imx/imx-gpu-viv/Config.in
@@ -12,6 +12,9 @@ config BR2_PACKAGE_IMX_GPU_VIV
 	depends on (BR2_arm && BR2_ARM_EABIHF) || BR2_aarch64
 	# Library binaries are linked against libc.so.6
 	depends on BR2_TOOLCHAIN_USES_GLIBC
+	# Library binaries are linked against libdrm.so.2, except framebuffer
+	# output on ARM
+	select BR2_PACKAGE_LIBDRM if !(BR2_arm && BR2_PACKAGE_IMX_GPU_VIV_OUTPUT_FB)
 	select BR2_PACKAGE_HAS_LIBEGL
 	select BR2_PACKAGE_HAS_LIBGLES
 	select BR2_PACKAGE_HAS_LIBOPENCL
@@ -56,8 +59,6 @@ config BR2_PACKAGE_IMX_GPU_VIV_OUTPUT_FB
 
 config BR2_PACKAGE_IMX_GPU_VIV_OUTPUT_WL
 	bool "Wayland"
-	# libdrm needed by gbm_viv.so
-	select BR2_PACKAGE_LIBDRM
 	select BR2_PACKAGE_WAYLAND
 	select BR2_PACKAGE_HAS_LIBEGL_WAYLAND
 

package/freescale-imx/imx-gpu-viv/imx-gpu-viv.mk
@@ -29,8 +29,13 @@ ifeq ($(IMX_GPU_VIV_LIB_TARGET),x11)
 IMX_GPU_VIV_DEPENDENCIES += xlib_libXdamage xlib_libXext xlib_libXfixes
 endif
 
+# Libraries are linked against libdrm, except framebuffer output on ARM
+ifneq ($(IMX_GPU_VIV_LIB_TARGET)$(BR2_arm),fby)
+IMX_GPU_VIV_DEPENDENCIES += libdrm
+endif
+
 ifeq ($(IMX_GPU_VIV_LIB_TARGET),wayland)
-IMX_GPU_VIV_DEPENDENCIES += libdrm wayland
+IMX_GPU_VIV_DEPENDENCIES += wayland
 endif
 
 define IMX_GPU_VIV_EXTRACT_CMDS