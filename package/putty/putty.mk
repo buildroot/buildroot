216902567f4340c3a29d9bfa2d4bbe41aae97a8f
Alexander Dahl <post@lespocky.de>
2021-05-08 21:53:42 +0200

    package/putty: bump to version 0.75
    Upstream does not set -Werror in its build files anymore.  License file
    just changed copyright years and holders.  PGP signatures of source
    tarball and hashes were checked.
    
    Link: https://www.chiark.greenend.org.uk/~sgtatham/putty/releases/0.75.html
    Link: https://www.chiark.greenend.org.uk/~sgtatham/putty/changes.html
    Signed-off-by: Alexander Dahl <post@lespocky.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit d562009f7b9701cb20bc4b1d389d19f9a647cc3b)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/putty/putty.mk
+++ package/putty/putty.mk
@@ -4,13 +4,12 @@
 #
 ################################################################################
 
-PUTTY_VERSION = 0.74
+PUTTY_VERSION = 0.75
 PUTTY_SITE = http://the.earth.li/~sgtatham/putty/$(PUTTY_VERSION)
 PUTTY_LICENSE = MIT
 PUTTY_LICENSE_FILES = LICENCE
 PUTTY_CPE_ID_VENDOR = putty
 PUTTY_CONF_OPTS = --disable-gtktest
-PUTTY_CONF_ENV = CFLAGS="$(TARGET_CFLAGS) -Wno-error"
 
 # Windows only, fixed for Windows with 0.75
 PUTTY_IGNORE_CVES += CVE-2021-33500
