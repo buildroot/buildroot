3b551f68a55d74f4fe7e814ebdf1101453f1255d
Peter Korsgaard <peter@korsgaard.com>
2021-05-10 11:06:57 +0200

    boot/beaglev-ddrlnit: rename to beaglev-ddrinit to match renamed upstream repo
    And adjust DEVELOPERS and beaglev_defconfig to match.
    
    The typo in the repo name has now been fixed:
    
    https://github.com/starfive-tech/beagle_ddrinit/issues/6
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- boot/beaglev-ddrinit/beaglev-ddrinit.mk
+++ boot/beaglev-ddrinit/beaglev-ddrinit.mk
@@ -1,27 +1,27 @@
 ################################################################################
 #
-# beaglev-ddrlnit
+# beaglev-ddrinit
 #
 ################################################################################
 
 # Commit on the 'starfive' branch
-BEAGLEV_DDRLNIT_VERSION = 15b80de81263996affb2a29332aa681925709983
-BEAGLEV_DDRLNIT_SITE = $(call github,starfive-tech,beagle_ddrlnit,$(BEAGLEV_DDRLNIT_VERSION))
-BEAGLEV_DDRLNIT_INSTALL_TARGET = NO
-BEAGLEV_DDRLNIT_INSTALL_IMAGES = YES
-BEAGLEV_DDRLNIT_DEPENDENCIES = host-riscv64-elf-toolchain
+BEAGLEV_DDRINIT_VERSION = 15b80de81263996affb2a29332aa681925709983
+BEAGLEV_DDRINIT_SITE = $(call github,starfive-tech,beagle_ddrinit,$(BEAGLEV_DDRINIT_VERSION))
+BEAGLEV_DDRINIT_INSTALL_TARGET = NO
+BEAGLEV_DDRINIT_INSTALL_IMAGES = YES
+BEAGLEV_DDRINIT_DEPENDENCIES = host-riscv64-elf-toolchain
 # unfortunately, no real license file, but several sources files are
 # under GPL-2.0+, making the whole work GPL-2.0+
-BEAGLEV_DDRLNIT_LICENSE = GPL-2.0+
+BEAGLEV_DDRINIT_LICENSE = GPL-2.0+
 
-define BEAGLEV_DDRLNIT_BUILD_CMDS
+define BEAGLEV_DDRINIT_BUILD_CMDS
 	$(MAKE) -C $(@D)/build \
 		CROSSCOMPILE=$(HOST_DIR)/bin/riscv64-unknown-elf- \
 		SUFFIX=buildroot \
-		GIT_VERSION=$(BEAGLEV_DDRLNIT_VERSION)
+		GIT_VERSION=$(BEAGLEV_DDRINIT_VERSION)
 endef
 
-define BEAGLEV_DDRLNIT_INSTALL_IMAGES_CMDS
+define BEAGLEV_DDRINIT_INSTALL_IMAGES_CMDS
 	$(INSTALL) -D -m 0644 $(@D)/build/ddrinit-2133-buildroot.bin.out \
 		$(BINARIES_DIR)/ddrinit-2133-buildroot.bin.out
 endef
