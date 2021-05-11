aded85d38105d682b0bbf84187b40ee275b7ed30
Jörg Krause <joerg.krause@embedded.rocks>
2021-05-11 09:17:05 +0000

    package/upmpdcli: bump to version 1.5.12
    Signed-off-by: Jörg Krause <joerg.krause@embedded.rocks>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/upmpdcli/upmpdcli.mk
+++ package/upmpdcli/upmpdcli.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UPMPDCLI_VERSION = 1.5.11
+UPMPDCLI_VERSION = 1.5.12
 UPMPDCLI_SITE = http://www.lesbonscomptes.com/upmpdcli/downloads
 UPMPDCLI_LICENSE = LGPL-2.1+
 UPMPDCLI_LICENSE_FILES = COPYING
