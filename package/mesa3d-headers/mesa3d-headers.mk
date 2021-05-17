buildroot/buildroot

commit 7fa481437e71484aa7064398e69aa63cdabb86b2

package/mesa3d{,-headers}: add CPE variables
cpe:2.3:a:mesa3d:mesa is a valid CPE identifier for this package:

  https://nvd.nist.gov/products/cpe/search/results?namingFormat=2.3&keyword=cpe%3A2.3%3Aa%3Amesa3d%3Amesa

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
[yann.morin.1998@free.fr: add to mesa3d-headers too]
Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

package/mesa3d-headers/mesa3d-headers.mk
@@ -18,6 +18,9 @@ MESA3D_HEADERS_SITE = https://mesa.freedesktop.org/archive
 MESA3D_HEADERS_DL_SUBDIR = mesa3d
 MESA3D_HEADERS_LICENSE = MIT, SGI, Khronos
 MESA3D_HEADERS_LICENSE_FILES = docs/license.rst
+MESA3D_HEADERS_CPE_ID_VENDOR = mesa3d
+MESA3D_HEADERS_CPE_ID_PRODUCT = mesa
+
 
 # Only installs header files
 MESA3D_HEADERS_INSTALL_STAGING = YES

package/mesa3d/mesa3d.mk
@@ -10,6 +10,8 @@ MESA3D_SOURCE = mesa-$(MESA3D_VERSION).tar.xz
 MESA3D_SITE = https://mesa.freedesktop.org/archive
 MESA3D_LICENSE = MIT, SGI, Khronos
 MESA3D_LICENSE_FILES = docs/license.rst
+MESA3D_CPE_ID_VENDOR = mesa3d
+MESA3D_CPE_ID_PRODUCT = mesa
 
 MESA3D_INSTALL_STAGING = YES
 

commit dfb045af5b4fc4c55a5674945b90d670c5fee68c

package/mesa3d-headers: remove spurious empty line
Detected by check-package

Fixes: 7fa481437e71484aa7064398e69aa63cdabb86b2

Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

package/mesa3d-headers/mesa3d-headers.mk
@@ -21,7 +21,6 @@ MESA3D_HEADERS_LICENSE_FILES = docs/license.rst
 MESA3D_HEADERS_CPE_ID_VENDOR = mesa3d
 MESA3D_HEADERS_CPE_ID_PRODUCT = mesa
 
-
 # Only installs header files
 MESA3D_HEADERS_INSTALL_STAGING = YES
 MESA3D_HEADERS_INSTALL_TARGET = NO