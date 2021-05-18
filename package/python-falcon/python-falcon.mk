4d172c8c01726c2c5a6d90977ad8fa7e77433022
Grzegorz Blach <grzegorz@blach.pl>
2021-05-18 17:14:10 +0200

    package/python-falcon: Bump to version 3.0.1
    Signed-off-by: Grzegorz Blach <grzegorz@blach.pl>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-falcon/python-falcon.mk
+++ package/python-falcon/python-falcon.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_FALCON_VERSION = 3.0.0
+PYTHON_FALCON_VERSION = 3.0.1
 PYTHON_FALCON_SOURCE = falcon-$(PYTHON_FALCON_VERSION).tar.gz
-PYTHON_FALCON_SITE = https://files.pythonhosted.org/packages/f1/59/2748d63922a46e040be06cd175a6242a33ade7839aac37f441544bb6d651
+PYTHON_FALCON_SITE = https://files.pythonhosted.org/packages/63/22/6a9009c53ad78e65d88a44db8eccc7f39c6f54fc05fb43b1e9cbbc481d06
 PYTHON_FALCON_SETUP_TYPE = setuptools
 PYTHON_FALCON_LICENSE = Apache-2.0
 PYTHON_FALCON_LICENSE_FILES = LICENSE
