4686cba316c4d5730b3d96b0f8795184e4bb0e2e
Pierre Wielders <pierre@wielders.net>
2021-05-19 18:37:00 +0200

    [CDMI-CLEARKEY] Add a module name otherwise compilation will fail with the latest greatest :-)

--- package/wpe/wpeframework-cdmi/wpeframework-cdmi-clearkey/wpeframework-cdmi-clearkey.mk
+++ package/wpe/wpeframework-cdmi/wpeframework-cdmi-clearkey/wpeframework-cdmi-clearkey.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEFRAMEWORK_CDMI_CLEARKEY_VERSION = R1
+WPEFRAMEWORK_CDMI_CLEARKEY_VERSION = 3fac75f6d0cbb8c0d854ef56754e2bc64d11b965
 WPEFRAMEWORK_CDMI_CLEARKEY_SITE_METHOD = git
 WPEFRAMEWORK_CDMI_CLEARKEY_SITE = git@github.com:rdkcentral/OCDM-Clearkey.git
 WPEFRAMEWORK_CDMI_CLEARKEY_INSTALL_STAGING = YES
