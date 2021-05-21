508cdd7b813825668752cc57024ae5f5342393b6
Adrian Perez de Castro <aperez@igalia.com>
2021-05-21 13:37:37 +0300

    package/libsoup: bump to version 2.66.5
    Change libsoup to version 2.66.5, which introduces many fixes. This
    needs adding the libpsl dependency, and switching over to Meson as the
    autotools based build system is no more. The existing patch is not
    needed anymore.
    
    Signed-off-by: Adrian Perez de Castro <aperez@igalia.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/libsoup/libsoup.mk
+++ package/libsoup/libsoup.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-LIBSOUP_VERSION_MAJOR = 2.62
-LIBSOUP_VERSION = $(LIBSOUP_VERSION_MAJOR).3
+LIBSOUP_VERSION_MAJOR = 2.66
+LIBSOUP_VERSION = $(LIBSOUP_VERSION_MAJOR).5
 LIBSOUP_SOURCE = libsoup-$(LIBSOUP_VERSION).tar.xz
 LIBSOUP_SITE = http://ftp.gnome.org/pub/gnome/sources/libsoup/$(LIBSOUP_VERSION_MAJOR)
 LIBSOUP_LICENSE = LGPL-2.0+
@@ -13,27 +13,27 @@ LIBSOUP_LICENSE_FILES = COPYING
 LIBSOUP_CPE_ID_VENDOR = gnome
 LIBSOUP_INSTALL_STAGING = YES
 LIBSOUP_CONF_ENV = ac_cv_path_GLIB_GENMARSHAL=$(LIBGLIB2_HOST_BINARY)
-LIBSOUP_CONF_OPTS = --disable-glibtest --enable-vala=no --with-gssapi=no
+LIBSOUP_CONF_OPTS = -Dtests=false -Dvapi=false -Dgssapi=false
 LIBSOUP_DEPENDENCIES = host-pkgconf host-libglib2 \
-	libglib2 libxml2 sqlite host-intltool
+	libglib2 libpsl libxml2 sqlite host-intltool
 
 ifeq ($(BR2_PACKAGE_GOBJECT_INTROSPECTION),y)
-LIBSOUP_CONF_OPTS += --with-introspection
+LIBSOUP_CONF_OPTS += -Dintrospection=true
 LIBSOUP_DEPENDENCIES += gobject-introspection
 else
-LIBSOUP_CONF_OPTS += --without-introspection
+LIBSOUP_CONF_OPTS += -Dintrospection=false
 endif
 
 ifeq ($(BR2_PACKAGE_LIBSOUP_GNOME),y)
-LIBSOUP_CONF_OPTS += --with-gnome
+LIBSOUP_CONF_OPTS += -Dgnome=true
 else
-LIBSOUP_CONF_OPTS += --without-gnome
+LIBSOUP_CONF_OPTS += -Dgnome=false
 endif
 
 ifeq ($(BR2_PACKAGE_LIBSOUP_SSL),y)
 LIBSOUP_DEPENDENCIES += glib-networking
 else
-LIBSOUP_CONF_OPTS += --disable-tls-check
+LIBSOUP_CONF_OPTS += -Dtls_check=false
 endif
 
-$(eval $(autotools-package))
+$(eval $(meson-package))
