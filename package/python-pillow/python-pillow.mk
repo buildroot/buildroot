e4625ae8d5e23bb66a774d085fe74a0463f835c5
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-05-22 19:41:59 +0200

    package/python-pillow: security bump to version 8.2.0
    - Fix numerous CVEs:
      https://pillow.readthedocs.io/en/stable/releasenotes/8.2.0.html#security
      https://pillow.readthedocs.io/en/stable/releasenotes/8.1.2.html#security
      https://pillow.readthedocs.io/en/stable/releasenotes/8.1.1.html#security
      https://pillow.readthedocs.io/en/stable/releasenotes/8.1.0.html#security
    - Update license to HPND:
      https://github.com/python-pillow/Pillow/commit/81078e8a0d26c9094446a64aadfa8047b8af3484
    
    https://pillow.readthedocs.io/en/stable/releasenotes/index.html
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-pillow/python-pillow.mk
+++ package/python-pillow/python-pillow.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_PILLOW_VERSION = 8.0.1
-PYTHON_PILLOW_SITE = https://files.pythonhosted.org/packages/2b/06/93bf1626ef36815010e971a5ce90f49919d84ab5d2fa310329f843a74bc1
+PYTHON_PILLOW_VERSION = 8.2.0
+PYTHON_PILLOW_SITE = https://files.pythonhosted.org/packages/21/23/af6bac2a601be6670064a817273d4190b79df6f74d8012926a39bc7aa77f
 PYTHON_PILLOW_SOURCE = Pillow-$(PYTHON_PILLOW_VERSION).tar.gz
-PYTHON_PILLOW_LICENSE = PIL Software License
+PYTHON_PILLOW_LICENSE = HPND
 PYTHON_PILLOW_LICENSE_FILES = LICENSE
 PYTHON_PILLOW_CPE_ID_VENDOR = python
 PYTHON_PILLOW_CPE_ID_PRODUCT = pillow
