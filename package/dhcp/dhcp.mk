d15b37d492a7e1a55a0d4c1994daffd78bf975d1
Peter Korsgaard <peter@korsgaard.com>
2021-05-30 10:44:57 +0200

    package/dhcp: security bump to version 4.4.2-P1
    Fixes the following security issue:
    
    - CVE-2021-25217: A buffer overrun in lease file parsing code can be used to
      exploit a common vulnerability shared by dhcpd and dhclient
    
    For details, see the advisory:
    https://kb.isc.org/docs/cve-2021-25217
    
    Update the LICENSE hash for a change of copyright years.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit cebf2b91b3e46efb8ca1fd3f092544fb158909d8)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/dhcp/dhcp.mk
+++ package/dhcp/dhcp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DHCP_VERSION = 4.4.2
+DHCP_VERSION = 4.4.2-P1
 DHCP_SITE = http://ftp.isc.org/isc/dhcp/$(DHCP_VERSION)
 DHCP_INSTALL_STAGING = YES
 DHCP_LICENSE = MPL-2.0
