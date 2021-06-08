22864d570dd18d78a77dedab8fbcc5e0122c0acc
Kamel Bouhara <kamel.bouhara@bootlin.com>
2021-06-08 14:32:09 +0200

    package/libqb: new package
    libqb is a library providing features for client-server architecture,
    such as logging, tracing, inter-process communication (IPC) and polling.
    
    see: https://github.com/ClusterLabs/libqb
    
    Signed-off-by: Kamel Bouhara <kamel.bouhara@bootlin.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

---
+++ package/libqb/libqb.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# libqb
+#
+################################################################################
+
+LIBQB_VERSION = 2.0.2
+LIBQB_SITE = $(call github,ClusterLabs,libqb,v$(LIBQB_VERSION))
+LIBQB_LICENSE = LGPL-2.1+
+LIBQB_LICENSE_FILES = COPYING
+LIBQB_INSTALL_STAGING = YES
+LIBQB_AUTORECONF = YES
+LIBQB_DEPENDENCIES = libxml2
+
+$(eval $(autotools-package))
