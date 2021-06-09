94a0a8d9e3a2bd712d951bc4ba74f3cf24276263
Drew Fustini <drew@beagleboard.org>
2021-06-08 23:38:33 -0700

    beaglev: update to new starfive linux and u-boot
    Update the commit id for u-boot to include fixes from the starfive-tech
    u-boot repository:
    
    494e5ef7b807 jh7100: Enable full 2M L2 cache
    4571f5a4e1e4 vic7100: enable cache ways (L2 cache)
    abb06422a5cd Remove fdt_high and initrd_high for Starfive
    93099a61c7b0 starfive: beaglev: Disable SIFIVE_CLINT and enable RISCV_TIMER
    bfb5abac3d1f Revert "riscv: Enable the SiFive CLINT block driver in S-Mode(VIC7100 ONLY)"
    7b70e1d44ba9 defconfig: enable cache_wayenable for better performance
    7c585978616b configs: earlycon=sbi is deprecated
    
    The commit abb06422a5cd ("Remove fdt_high and initrd_high for Starfive")
    has removed the need for buildroot to patch the fdt_addr_r address.
    
    Update linux from commit in the old 5.10 branch that Fedora image was
    using to head of esmil_starlight branch [1] which is currently based
    on 5.13-rc5 and represents the best kernel for this board [2].
    
    In addition, the updated kernel now has jh7100-beaglev-starlight.dtb so
    buildroot no longer needs to copy the dtb from u-boot.
    
    [1] https://github.com/starfive-tech/linux/tree/esmil_starlight
    [2] https://github.com/starfive-tech/linux/issues/26
    
    Signed-off-by: Drew Fustini <drew@beagleboard.org>

--- board/beaglev/post-build.sh
+++ board/beaglev/post-build.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 BOARD_DIR=$(dirname $0)
 
-# The DTB to use is provided within the U-Boot source tree, so we grab
-# it from there, and install it to TARGET_DIR/boot/.
-eval $(make -C ${CONFIG_DIR} --no-print-directory QUOTED_VARS=YES VARS=UBOOT_DIR printvars)
-install -D -m0644 ${UBOOT_DIR}/arch/riscv/dts/starfive_vic7100_beagle_v.dtb \
-	${TARGET_DIR}/boot/starfive_vic7100_beagle_v.dtb
-
 # Bring the extlinux.conf file in.
 install -D -m 0644 ${BOARD_DIR}/extlinux.conf \
 	${TARGET_DIR}/boot/extlinux/extlinux.conf
