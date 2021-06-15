e85bde9cc672fc228eeb079e23ef302468ae3826
Charles Hardin <ckhardin@gmail.com>
2021-06-14 17:59:23 -0700

    package/alsa-plugins: new package
    During testing of bluez-alsa in particular, there is no compilation
    dependency - but, some configurations will request the load of a
    particular plugin:
    
        ALSA lib dlmisc.c:337:(snd_dlobj_cache_get0) \
          Cannot open shared library libasound_module_rate_samplerate_best.so \
          (/usr/lib/alsa-lib/libasound_module_rate_samplerate_best.so: \
           cannot open shared object file: No such file or directory)
    
    Adding in the alsa-plugins package allows to build build the
    appropriate plugins.
    
    Signed-off-by: Charles Hardin <ckhardin@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/alsa-plugins/alsa-plugins.mk
@@ -0,0 +1,32 @@
+################################################################################
+#
+# alsa-plugins
+#
+################################################################################
+
+ALSA_PLUGINS_VERSION = 1.2.5
+ALSA_PLUGINS_SOURCE = alsa-plugins-$(ALSA_PLUGINS_VERSION).tar.bz2
+ALSA_PLUGINS_SITE = https://www.alsa-project.org/files/pub/plugins
+ALSA_PLUGINS_LICENSE = LGPL-2.1+
+ALSA_PLUGINS_LICENSE_FILES = COPYING
+ALSA_PLUGINS_DEPENDENCIES = host-pkgconf alsa-lib
+
+ALSA_PLUGINS_CONF_OPTS = \
+	--disable-jack \
+	--disable-usbstream \
+	--disable-pulseaudio \
+	--disable-libav \
+	--disable-maemo-plugin \
+	--disable-maemo-resource-manager \
+	--with-speex=no
+
+ifeq ($(BR2_PACKAGE_LIBSAMPLERATE),y)
+ALSA_PLUGINS_CONF_OPTS += --enable-samplerate
+ALSA_PLUGINS_DEPENDENCIES += libsamplerate
+ALSA_PLUGINS_LICENSE += , GPL-2.0+ (samplerate plugin)
+ALSA_PLUGINS_LICENSE_FILES += COPYING.GPL
+else
+ALSA_PLUGINS_CONF_OPTS += --disable-samplerate
+endif
+
+$(eval $(autotools-package))
