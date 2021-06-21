91aa6efa8588bf7617cc4a640eb55052b524ceb7
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-06-21 22:22:18 +0200

    package/tpm2-tools: security bump to version 4.3.2
    - Fix CVE-2021-3565: A flaw was found in tpm2-tools in versions before
      5.1.1 and before 4.3.2. tpm2_import used a fixed AES key for the inner
      wrapper, potentially allowing a MITM attacker to unwrap the inner
      portion and reveal the key being imported. The highest threat from
      this vulnerability is to data confidentiality.
    - LICENSE moved in doc directory since
      https://github.com/tpm2-software/tpm2-tools/commit/23aa5dca660f596b2ad89542d5100bd4ef0c871a
      and hash updated due to the following line added with
      https://github.com/tpm2-software/tpm2-tools/commit/305011b2a7d091740fa01dbfbd27a48a76f670f7
      Copyright 2019      Fraunhofer SIT sponsored by Infineon Technologies AG
    - libuuid and wchar (for mbstate_t) are mandatory since version 4.2 and
      https://github.com/tpm2-software/tpm2-tools/commit/eca77c1419617a8e2d6d8008bac716878b0c27ca
    
    https://github.com/tpm2-software/tpm2-tools/blob/4.3.2/doc/CHANGELOG.md
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/tpm2-tools/tpm2-tools.mk
+++ package/tpm2-tools/tpm2-tools.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-TPM2_TOOLS_VERSION = 4.1.2
+TPM2_TOOLS_VERSION = 4.3.2
 TPM2_TOOLS_SITE = https://github.com/tpm2-software/tpm2-tools/releases/download/$(TPM2_TOOLS_VERSION)
 TPM2_TOOLS_LICENSE = BSD-3-Clause
-TPM2_TOOLS_LICENSE_FILES = LICENSE
-TPM2_TOOLS_DEPENDENCIES = libcurl openssl tpm2-tss host-pkgconf
+TPM2_TOOLS_LICENSE_FILES = doc/LICENSE
+TPM2_TOOLS_DEPENDENCIES = libcurl openssl tpm2-tss host-pkgconf util-linux
 
 # -fstack-protector-all and FORTIFY_SOURCE=2 is used by
 # default. Disable that so the BR2_SSP_* / BR2_FORTIFY_SOURCE_* options
