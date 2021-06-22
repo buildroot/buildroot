2f6cd888f4d0befd179693bb4f6d7f5d50e7864b
Geoff Levand <geoff@infradead.org>
2021-06-22 06:45:26 -0700

    package/flannel: update project URL
    Update flannel project URL.
    
      from: github.com/coreos/flannel
      to:   github.com/flannel-io/flannel
    
    Signed-off-by: Geoff Levand <geoff@infradead.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/flannel/flannel.mk
+++ package/flannel/flannel.mk
@@ -5,13 +5,13 @@
 ################################################################################
 
 FLANNEL_VERSION = 0.12.0
-FLANNEL_SITE = https://github.com/coreos/flannel/archive
+FLANNEL_SITE = https://github.com/flannel-io/flannel/archive
 FLANNEL_SOURCE = v$(FLANNEL_VERSION).tar.gz
 
 FLANNEL_LICENSE = Apache-2.0
 FLANNEL_LICENSE_FILES = LICENSE
 
-FLANNEL_LDFLAGS = -X github.com/coreos/flannel/version.Version=$(FLANNEL_VERSION)
+FLANNEL_LDFLAGS = -X github.com/flannel-io/flannel/version.Version=$(FLANNEL_VERSION)
 
 # Install flannel to its well known location.
 define FLANNEL_INSTALL_TARGET_CMDS
