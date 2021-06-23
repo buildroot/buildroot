20616a1b65aa718a2d18cac560404fa3b3e70fc8
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-06-23 18:59:23 +0200

    package/usbguard: improve option handling
    Don't pass --disable-seccomp and --disable-systemd unconditionally
    While at it, also add a space and a new line when needed, and split
    the initial CONF_OPTS assignment to have one option per line.
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/usbguard/usbguard.mk
+++ package/usbguard/usbguard.mk
@@ -8,9 +8,13 @@ USBGUARD_VERSION = 1.0.0
 USBGUARD_SITE = https://github.com/USBGuard/usbguard/releases/download/usbguard-$(USBGUARD_VERSION)
 USBGUARD_LICENSE = GPL-2.0+
 USBGUARD_LICENSE_FILES = LICENSE
-USBGUARD_CONF_OPTS= --with-bundled-catch --with-bundled-pegtl \
-		    --disable-debug-build --without-dbus --without-polkit \
-		    --disable-seccomp --disable-umockdev --disable-systemd
+USBGUARD_CONF_OPTS = \
+	--with-bundled-catch \
+	--with-bundled-pegtl \
+	--disable-debug-build \
+	--without-dbus \
+	--without-polkit \
+	--disable-umockdev
 
 USBGUARD_DEPENDENCIES += libqb protobuf
 
@@ -18,10 +22,12 @@ ifeq ($(BR2_PACKAGE_LIBOPENSSL),y)
 USBGUARD_CONF_OPTS += --with-crypto-library=openssl
 USBGUARD_DEPENDENCIES += libopenssl
 endif
+
 ifeq ($(BR2_PACKAGE_LIBGCRYPT),y)
 USBGUARD_CONF_OPTS += --with-crypto-library=gcrypt
 USBGUARD_DEPENDENCIES += libgcrypt
 endif
+
 ifeq ($(BR2_PACKAGE_LIBSODIUM),y)
 USBGUARD_CONF_OPTS += --with-crypto-library=sodium
 USBGUARD_DEPENDENCIES += libsodium
@@ -30,11 +36,15 @@ endif
 ifeq ($(BR2_PACKAGE_SYSTEMD),y)
 USBGUARD_CONF_OPTS += --enable-systemd
 USBGUARD_DEPENDENCIES += systemd
+else
+USBGUARD_CONF_OPTS += --disable-systemd
 endif
 
 ifeq ($(BR2_PACKAGE_LIBSECCOMP),y)
 USBGUARD_CONF_OPTS += --enable-seccomp
 USBGUARD_DEPENDENCIES += libseccomp
+else
+USBGUARD_CONF_OPTS += --disable-seccomp
 endif
 
 ifeq ($(BR2_PACKAGE_LIBCAP_NG),y)
