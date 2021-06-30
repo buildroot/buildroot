3335c8d8685119ebb5b2a9a6e575f3a5337c4d90
Adam Duskett <aduskett@gmail.com>
2021-06-30 10:36:58 -0700

    package/nginx-naxsi: bump version to 1.3
    Other changes:
      - Change license to GPL-3.0 and BSD-3-Clause
      - Change license file to LICENSE and naxsi_src/ext/libinjection/COPYING
      - Update license hash's in nginx-naxsi.hash
    
    Signed-off-by: Adam Duskett <aduskett@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/nginx-naxsi/nginx-naxsi.mk
+++ package/nginx-naxsi/nginx-naxsi.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-NGINX_NAXSI_VERSION = 0.56
+NGINX_NAXSI_VERSION = 1.3
 NGINX_NAXSI_SITE = $(call github,nbs-system,naxsi,$(NGINX_NAXSI_VERSION))
-NGINX_NAXSI_LICENSE = GPL-2.0+ with OpenSSL exception
-NGINX_NAXSI_LICENSE_FILES = naxsi_src/naxsi_json.c
+NGINX_NAXSI_LICENSE = GPL-3.0, BSD-3-Clause (libinjection)
+NGINX_NAXSI_LICENSE_FILES = LICENSE naxsi_src/ext/libinjection/COPYING
 
 $(eval $(generic-package))
