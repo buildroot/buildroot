3d86f7f14157895801b7279ac49fa98ad8d12d19
Peter Seiderer <ps.report@gmx.net>
2021-07-03 23:13:13 +0200

    package/rpi-userland: bump version to 97bc818
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/rpi-userland/rpi-userland.mk
+++ package/rpi-userland/rpi-userland.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_USERLAND_VERSION = 45a0022ac64b4d0788def3c5230c972430f6fc23
+RPI_USERLAND_VERSION = 97bc8180ad682b004ea224d1db7b8e108eda4397
 RPI_USERLAND_SITE = $(call github,raspberrypi,userland,$(RPI_USERLAND_VERSION))
 RPI_USERLAND_LICENSE = BSD-3-Clause
 RPI_USERLAND_LICENSE_FILES = LICENCE
