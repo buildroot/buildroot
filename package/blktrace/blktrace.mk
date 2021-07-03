ae3700f5797396157a29e2ce42b27c93df3a4665
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-07-04 00:04:24 +0200

    package/blktrace: bump to version 1.3.0
    - Use tar.bz2 tarball
    - Update indentation in hash file (two spaces)
    - Drop patch (already in version)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/blktrace/blktrace.mk
+++ package/blktrace/blktrace.mk
@@ -4,15 +4,13 @@
 #
 ################################################################################
 
-BLKTRACE_VERSION = 1.2.0
+BLKTRACE_VERSION = 1.3.0
+BLKTRACE_SOURCE = blktrace-$(BLKTRACE_VERSION).tar.bz2
 BLKTRACE_SITE = http://brick.kernel.dk/snaps
 BLKTRACE_DEPENDENCIES = libaio
 BLKTRACE_LICENSE = GPL-2.0+
 BLKTRACE_LICENSE_FILES = COPYING
 BLKTRACE_CPE_ID_VENDOR = blktrace_project
-
-# 0001-btt-make-device-devno-use-PATH_MAX-to-avoid-overflow.patch
-BLKTRACE_IGNORE_CVES += CVE-2018-10689
 
 define BLKTRACE_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE1) -C $(@D) $(TARGET_CONFIGURE_OPTS)
