9273b0df81e09e4ee02488cb65c34fc1e0ede4e9
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-07-04 00:18:44 +0200

    package/findutils: bump to version 4.8.0
    Update indentation in hash file (two spaces)
    
    https://git.savannah.gnu.org/cgit/findutils.git/tree/NEWS?h=v4.8.0
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/findutils/findutils.mk
+++ package/findutils/findutils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FINDUTILS_VERSION = 4.7.0
+FINDUTILS_VERSION = 4.8.0
 FINDUTILS_SOURCE = findutils-$(FINDUTILS_VERSION).tar.xz
 FINDUTILS_SITE = $(BR2_GNU_MIRROR)/findutils
 FINDUTILS_LICENSE = GPL-3.0+
