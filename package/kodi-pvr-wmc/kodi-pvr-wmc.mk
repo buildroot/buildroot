05c66eed36b87c07bdb70063a85397e47109e4a6
Bernd Kuhls <bernd.kuhls@t-online.de>
2021-07-05 22:45:55 +0200

    package/kodi-pvr-wmc: bump version to 6.1.2-Matrix
    Changelog:
    https://github.com/kodi-pvr/pvr.wmc/blob/Matrix/pvr.wmc/changelog.txt
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-pvr-wmc/kodi-pvr-wmc.mk
+++ package/kodi-pvr-wmc/kodi-pvr-wmc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PVR_WMC_VERSION = 6.1.1-Matrix
+KODI_PVR_WMC_VERSION = 6.1.2-Matrix
 KODI_PVR_WMC_SITE = $(call github,kodi-pvr,pvr.wmc,$(KODI_PVR_WMC_VERSION))
 KODI_PVR_WMC_LICENSE = GPL-2.0+
 KODI_PVR_WMC_LICENSE_FILES = LICENSE.md
