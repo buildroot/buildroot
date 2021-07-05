7fbbcd7bcc1e1bf48fb12ed43c6722693d4190c6
Bernd Kuhls <bernd.kuhls@t-online.de>
2021-07-05 22:45:51 +0200

    package/kodi-pvr-stalker: bump version to 7.1.1-Matrix
    Changelog:
    https://github.com/kodi-pvr/pvr.stalker/blob/Matrix/pvr.stalker/changelog.txt
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-pvr-stalker/kodi-pvr-stalker.mk
+++ package/kodi-pvr-stalker/kodi-pvr-stalker.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PVR_STALKER_VERSION = 7.1.0-Matrix
+KODI_PVR_STALKER_VERSION = 7.1.1-Matrix
 KODI_PVR_STALKER_SITE = $(call github,kodi-pvr,pvr.stalker,$(KODI_PVR_STALKER_VERSION))
 KODI_PVR_STALKER_LICENSE = GPL-2.0+
 KODI_PVR_STALKER_LICENSE_FILES = LICENSE.md
