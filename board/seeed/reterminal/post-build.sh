dcd5a6315eeab6ff1663122ecbe30914088bf491
bigbearishappy <953308023@qq.com>
2021-07-07 08:26:42 +0000

    update the post-build.sh for copy file and modify config.txt

--- board/seeed/reterminal/post-build.sh
+++ board/seeed/reterminal/post-build.sh
@@ -10,4 +10,20 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
 
-touch $TARGET_DIR/opt/testfile01.txt
+#copy the dtoverlay file
+cp ${BUILD_DIR}/linux-custom/arch/arm/boot/dts/overlays/reTerminal.dtbo ${BINARIES_DIR}/rpi-firmware/overlays/
+
+#modify the config.txt
+CFG_PATH=${BINARIES_DIR}/rpi-firmware/config.txt
+grep -q "^dtparam=i2c_arm=on$" $CFG_PATH || echo "dtparam=i2c_arm=on" >> $CFG_PATH
+grep -q "^dtoverlay=vc4-fkms-v3d$" $CFG_PATH || echo "dtoverlay=vc4-fkms-v3d" >> $CFG_PATH
+grep -q "^max_framebuffers=2$" $CFG_PATH || echo "max_framebuffers=2" >> $CFG_PATH
+grep -q "^enable_uart=1$" $CFG_PATH || echo "enable_uart=1" >> $CFG_PATH
+grep -q "^dtoverlay=dwc2,dr_mode=host$" $CFG_PATH || echo "dtoverlay=dwc2,dr_mode=host" >> $CFG_PATH
+grep -q "^dtparam=ant2$" $CFG_PATH || echo "dtparam=ant2" >> $CFG_PATH
+grep -q "^disable_splash=1$" $CFG_PATH || echo "disable_splash=1" >> $CFG_PATH
+grep -q "^ignore_lcd=1$" $CFG_PATH || echo "ignore_lcd=1" >> $CFG_PATH
+grep -q "^dtoverlay=vc4-kms-v3d-pi4$" $CFG_PATH || echo "dtoverlay=vc4-kms-v3d-pi4" >> $CFG_PATH
+grep -q "^dtoverlay=i2c3,pins_4_5$" $CFG_PATH || echo "dtoverlay=i2c3,pins_4_5" >> $CFG_PATH
+grep -q "^gpio=13=pu$" $CFG_PATH || echo "gpio=13=pu" >> $CFG_PATH
+grep -q "^dtoverlay=reTerminal$" $CFG_PATH || echo "dtoverlay=reTerminal" >> $CFG_PATH
