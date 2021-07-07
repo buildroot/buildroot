f11128b779a7e285665902d09977f268b1bbee77
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-07-07 22:41:14 +0200

    package/libnice: bump to version 0.1.18
    - Update indentation in hash file (two spaces)
    - Switch to meson-package
    
    https://github.com/libnice/libnice/blob/0.1.18/NEWS
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libnice/libnice.mk
+++ package/libnice/libnice.mk
@@ -4,36 +4,36 @@
 #
 ################################################################################
 
-LIBNICE_VERSION = 0.1.16
+LIBNICE_VERSION = 0.1.18
 LIBNICE_SITE = http://nice.freedesktop.org/releases
 LIBNICE_LICENSE = MPL-1.1 or LGPL-2.1
 LIBNICE_LICENSE_FILES = COPYING COPYING.MPL COPYING.LGPL
 LIBNICE_DEPENDENCIES = libglib2 host-pkgconf
 LIBNICE_INSTALL_STAGING = YES
-LIBNICE_CONF_OPTS = --without-gstreamer-0.10
+LIBNICE_CONF_OPTS = \
+	-Dexamples=disabled \
+	-Dtests=disabled
 
 ifeq ($(BR2_PACKAGE_GNUTLS),y)
-LIBNICE_CONF_OPTS += --with-crypto-library=gnutls
+LIBNICE_CONF_OPTS += -Dcrypto-library=gnutls
 LIBNICE_DEPENDENCIES += gnutls
 else
-LIBNICE_CONF_OPTS += \
-	--with-crypto-library=openssl \
-	--with-openssl=$(STAGING_DIR)/usr
+LIBNICE_CONF_OPTS += -Dcrypto-library=openssl
 LIBNICE_DEPENDENCIES += openssl
 endif
 
 ifeq ($(BR2_PACKAGE_GOBJECT_INTROSPECTION),y)
-LIBNICE_CONF_OPTS += --enable-introspection
+LIBNICE_CONF_OPTS += -Dintrospection=enabled
 LIBNICE_DEPENDENCIES += gobject-introspection
 else
-LIBNICE_CONF_OPTS += --disable-introspection
+LIBNICE_CONF_OPTS += -Dintrospection=disabled
 endif
 
 ifeq ($(BR2_PACKAGE_GST1_PLUGINS_BASE),y)
-LIBNICE_CONF_OPTS += --with-gstreamer
+LIBNICE_CONF_OPTS += -Dgstreamer=enabled
 LIBNICE_DEPENDENCIES += gst1-plugins-base
 else
-LIBNICE_CONF_OPTS += --without-gstreamer
+LIBNICE_CONF_OPTS += -Dgstreamer=disabled
 endif
 
-$(eval $(autotools-package))
+$(eval $(meson-package))
