42ee122862bf226b304340f5b42f4280abfacb7e
Bernd Kuhls <bernd.kuhls@t-online.de>
2021-07-13 22:29:11 +0200

    package/kodi-pvr-hts: bump version to 8.3.3-Matrix
    Changelog:
    https://github.com/kodi-pvr/pvr.hts/blob/Matrix/pvr.hts/changelog.txt
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-pvr-hts/kodi-pvr-hts.mk
+++ package/kodi-pvr-hts/kodi-pvr-hts.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PVR_HTS_VERSION = 8.3.0-Matrix
+KODI_PVR_HTS_VERSION = 8.3.3-Matrix
 KODI_PVR_HTS_SITE = $(call github,kodi-pvr,pvr.hts,$(KODI_PVR_HTS_VERSION))
 KODI_PVR_HTS_LICENSE = GPL-2.0+
 KODI_PVR_HTS_LICENSE_FILES = LICENSE.md
