8141ee44561ed48a04c006795fead330f605fe24
Bernd Kuhls <bernd.kuhls@t-online.de>
2021-07-15 16:58:18 +0200

    package/kodi-pvr-filmon: bump version to 6.1.2-Matrix
    Changelog:
    https://github.com/kodi-pvr/pvr.filmon/blob/Matrix/pvr.filmon/changelog.txt
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-pvr-filmon/kodi-pvr-filmon.mk
+++ package/kodi-pvr-filmon/kodi-pvr-filmon.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PVR_FILMON_VERSION = 6.1.0-Matrix
+KODI_PVR_FILMON_VERSION = 6.1.2-Matrix
 KODI_PVR_FILMON_SITE = $(call github,kodi-pvr,pvr.filmon,$(KODI_PVR_FILMON_VERSION))
 KODI_PVR_FILMON_LICENSE = GPL-2.0+
 KODI_PVR_FILMON_LICENSE_FILES = LICENSE.md
