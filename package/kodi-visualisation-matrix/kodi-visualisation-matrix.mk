893d55199a37331421949f03acf0ac76c9dedf9a
Bernd Kuhls <bernd.kuhls@t-online.de>
2021-07-15 16:58:21 +0200

    package/kodi-visualisation-matrix: bump version to 1.0.1-Matrix
    Changelog:
    https://github.com/xbmc/visualization.matrix/blob/Matrix/visualization.matrix/changelog.txt
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-visualisation-matrix/kodi-visualisation-matrix.mk
+++ package/kodi-visualisation-matrix/kodi-visualisation-matrix.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_VISUALISATION_MATRIX_VERSION = 1.0.0-Matrix
+KODI_VISUALISATION_MATRIX_VERSION = 1.0.1-Matrix
 KODI_VISUALISATION_MATRIX_SITE = $(call github,xbmc,visualization.matrix,$(KODI_VISUALISATION_MATRIX_VERSION))
 KODI_VISUALISATION_MATRIX_LICENSE = GPL-2.0+
 KODI_VISUALISATION_MATRIX_LICENSE_FILES = LICENSE.md
