1176b02a1cf562b5d13fe176b9aed35c6e19868d
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-07-16 23:04:32 +0200

    package/libqb: needs threads and dynamic library
    libqb fails to build without threads or dynamic library since the
    addition of the package in commit
    22864d570dd18d78a77dedab8fbcc5e0122c0acc
    
    Also add ac_cv_prog_cc_c99=-std=gnu99 to fix build failure without wchar
    
    Fixes:
     - http://autobuild.buildroot.org/results/21c1b5d29da4949f4582056ab4e2335ec225d8d6
     - http://autobuild.buildroot.org/results/aa4df6364cdf3c862be13b6b738595f420ec6105
     - http://autobuild.buildroot.org/results/376d316094238ba7097e8bdc51960ba38532683a
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libqb/libqb.mk
+++ package/libqb/libqb.mk
@@ -13,4 +13,8 @@ LIBQB_INSTALL_STAGING = YES
 LIBQB_AUTORECONF = YES
 LIBQB_DEPENDENCIES = libxml2
 
+# ac_cv_prog_cc_c99 is required for BR2_USE_WCHAR=n because the C99 test
+# provided by autoconf relies on wchar_t.
+LIBQB_CONF_ENV = ac_cv_prog_cc_c99=-std=gnu99
+
 $(eval $(autotools-package))
