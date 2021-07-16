4fb4b5e1acce5178b709d1c4d9d569553deb9fc8
Neeraj Deshpande <n.deshpande@metrological.com>
2021-07-16 17:29:10 +0530

    Westeros : version bump.

--- package/westeros-sink/westeros-sink.mk
+++ package/westeros-sink/westeros-sink.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WESTEROS_SINK_VERSION = a13ce42ba4fbbae371411f63bd7dd65dccf15a0c
+WESTEROS_SINK_VERSION = 23a65d1fa48f6d82d51c3cb6cd08bf403f95187d
 WESTEROS_SINK_SITE_METHOD = git
 WESTEROS_SINK_SITE = git://github.com/rdkcmf/westeros
 WESTEROS_SINK_INSTALL_STAGING = YES
--- package/westeros/westeros.mk
+++ package/westeros/westeros.mk
@@ -3,7 +3,7 @@
 # westeros
 #
 ################################################################################
-WESTEROS_VERSION = a13ce42ba4fbbae371411f63bd7dd65dccf15a0c
+WESTEROS_VERSION = 23a65d1fa48f6d82d51c3cb6cd08bf403f95187d
 WESTEROS_SITE_METHOD = git
 WESTEROS_SITE = git://github.com/rdkcmf/westeros
 WESTEROS_INSTALL_STAGING = YES
