20d9f7da4d04117e7f0b98b3382d77b39106afa1
bigbearishappy <953308023@qq.com>
2021-07-17 05:07:36 +0000

    fix the package of qt5 demo

--- package/reterminal_qt5_demo/reterminal_qt5_demo.mk
+++ package/reterminal_qt5_demo/reterminal_qt5_demo.mk
@@ -1,10 +1,11 @@
-RETERMINAL_QT5_DEMO_VERSION = 7986128237c1bdb5d8a77db21ac3c3e4486f2947
+RETERMINAL_QT5_DEMO_VERSION = main
 RETERMINAL_QT5_DEMO_SITE = https://github.com/Seeed-Studio/Seeed_Python_ReTerminalQt5Examples.git
 RETERMINAL_QT5_DEMO_SITE_METHOD = git
 RETERMINAL_QT5_DEMO_INSTALL_STAGING = NO
 RETERMINAL_QT5_DEMO_INSTALL_TARGET = YES
 
 define RETERMINAL_QT5_DEMO_INSTALL_TARGET_CMDS
+	rm -rf $(TARGET_DIR)/root/root/Seeed_Python_ReTerminalQt5Examples/
 	mkdir -p $(TARGET_DIR)/root/Seeed_Python_ReTerminalQt5Examples/
 	cp -rf $(@D)/* $(TARGET_DIR)/root/Seeed_Python_ReTerminalQt5Examples/
 	cp -r $(@D)/fonts $(TARGET_DIR)/usr/lib/
