83e70c289381c2f58361270ed1384463728d3599
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-07-18 21:57:12 +0200

    package/libgudev: bump to version 236
    - Switch to meson-package
    - Update indentation in hash file (two spaces)
    
    https://gitlab.gnome.org/GNOME/libgudev/-/blob/236/NEWS
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Reviewed-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libgudev/libgudev.mk
+++ package/libgudev/libgudev.mk
@@ -4,20 +4,20 @@
 #
 ################################################################################
 
-LIBGUDEV_VERSION = 233
+LIBGUDEV_VERSION = 236
 LIBGUDEV_SOURCE = libgudev-$(LIBGUDEV_VERSION).tar.xz
 LIBGUDEV_SITE = http://ftp.gnome.org/pub/GNOME/sources/libgudev/$(LIBGUDEV_VERSION)
 LIBGUDEV_INSTALL_STAGING = YES
 LIBGUDEV_DEPENDENCIES = host-pkgconf udev libglib2
 LIBGUDEV_LICENSE = LGPL-2.1+
 LIBGUDEV_LICENSE_FILES = COPYING
-LIBGUDEV_CONF_OPTS = --disable-umockdev
+LIBGUDEV_CONF_OPTS = -Dtests=disabled -Dvapi=disabled
 
 ifeq ($(BR2_PACKAGE_GOBJECT_INTROSPECTION),y)
-LIBGUDEV_CONF_OPTS += --enable-introspection
+LIBGUDEV_CONF_OPTS += -Dintrospection=enabled
 LIBGUDEV_DEPENDENCIES += gobject-introspection
 else
-LIBGUDEV_CONF_OPTS += --disable-introspection
+LIBGUDEV_CONF_OPTS += -Dintrospection=disabled
 endif
 
-$(eval $(autotools-package))
+$(eval $(meson-package))
