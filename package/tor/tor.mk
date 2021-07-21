fc6cb3caf8c7db951c9780c1eb9c5559dc415ca9
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-07-21 19:09:52 +0200

    package/tor: add SELinux module
    Support for tor is added by the services/tor module in the SELinux
    refpolicy.
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 1f9090b6b4337a61eba5ebfd1ea57fcd6ff2a8e1)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/tor/tor.mk
+++ package/tor/tor.mk
@@ -9,6 +9,7 @@ TOR_SITE = https://dist.torproject.org
 TOR_LICENSE = BSD-3-Clause
 TOR_LICENSE_FILES = LICENSE
 TOR_CPE_ID_VENDOR = torproject
+TOR_SELINUX_MODULES = tor
 TOR_DEPENDENCIES = libevent openssl zlib
 TOR_AUTORECONF = YES
 
