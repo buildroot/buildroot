9a53e2810378fb255df0b5ec1d632a906b25a97f
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-07-26 10:51:58 +0200

    package/xl2tp: add SELinux module
    Support for xl2tp is added by the services/l2tp module in the SELinux
    refpolicy.
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/xl2tp/xl2tp.mk
+++ package/xl2tp/xl2tp.mk
@@ -9,6 +9,7 @@ XL2TP_SITE = $(call github,xelerance,xl2tpd,v$(XL2TP_VERSION))
 XL2TP_DEPENDENCIES = libpcap
 XL2TP_LICENSE = GPL-2.0
 XL2TP_LICENSE_FILES = LICENSE
+XL2TP_SELINUX_MODULES = l2tp
 
 ifeq ($(BR2_STATIC_LIBS),y)
 XL2TP_LDLIBS = LDLIBS="`$(STAGING_DIR)/usr/bin/pcap-config --static --additional-libs`"
