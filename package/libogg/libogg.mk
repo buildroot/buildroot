c8dd578b135041eed639c06810e67b8b018402d4
Bernd Kuhls <bernd.kuhls@t-online.de>
2021-07-26 08:10:46 +0200

    package/libogg: bump version to 1.3.5
    Changelog:
    https://gitlab.xiph.org/xiph/ogg/-/blob/master/CHANGES
    
    Removed md5 and sha1 hashes, not provided by upstream anymore.
    Reformatted hashes.
    
    Switched _SITE to https.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libogg/libogg.mk
+++ package/libogg/libogg.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-LIBOGG_VERSION = 1.3.4
+LIBOGG_VERSION = 1.3.5
 LIBOGG_SOURCE = libogg-$(LIBOGG_VERSION).tar.xz
-LIBOGG_SITE = http://downloads.xiph.org/releases/ogg
+LIBOGG_SITE = https://downloads.xiph.org/releases/ogg
 LIBOGG_LICENSE = BSD-3-Clause
 LIBOGG_LICENSE_FILES = COPYING
 
