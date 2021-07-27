42b46562a286d0082638f9bac5346c523877b5c2
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-07-27 14:05:35 +0200

    package/gssdp: bump to version 1.3.0
    Update license to LGPL-2.1+:
    https://gitlab.gnome.org/GNOME/gssdp/-/commit/b018ff574f482a178f0c8515faf32962da70396a
    
    https://gitlab.gnome.org/GNOME/gssdp/-/blob/gssdp-1.3.0/NEWS
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/gssdp/gssdp.mk
+++ package/gssdp/gssdp.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-GSSDP_VERSION_MAJOR = 1.2
-GSSDP_VERSION = $(GSSDP_VERSION_MAJOR).3
+GSSDP_VERSION_MAJOR = 1.3
+GSSDP_VERSION = $(GSSDP_VERSION_MAJOR).0
 GSSDP_SOURCE = gssdp-$(GSSDP_VERSION).tar.xz
 GSSDP_SITE = http://ftp.gnome.org/pub/gnome/sources/gssdp/$(GSSDP_VERSION_MAJOR)
-GSSDP_LICENSE = LGPL-2.0+
+GSSDP_LICENSE = LGPL-2.1+
 GSSDP_LICENSE_FILES = COPYING
 GSSDP_INSTALL_STAGING = YES
 GSSDP_DEPENDENCIES = host-pkgconf libglib2 libsoup
