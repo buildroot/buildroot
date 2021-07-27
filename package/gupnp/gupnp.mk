4a1cd7581ae81e6b04b8f6182efa25ecb65e58c9
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-07-27 14:05:36 +0200

    package/gupnp: bump to version 1.3.0
    - Drop patch (already in version)
    - Update license to LGPL-2.1+:
      https://gitlab.gnome.org/GNOME/gupnp/-/commit/37b22d0a050842d91dc64678518c425b1e2a3451
    
    https://gitlab.gnome.org/GNOME/gupnp/-/blob/gupnp-1.3.0/NEWS
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/gupnp/gupnp.mk
+++ package/gupnp/gupnp.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-GUPNP_VERSION_MAJOR = 1.2
-GUPNP_VERSION = $(GUPNP_VERSION_MAJOR).6
+GUPNP_VERSION_MAJOR = 1.3
+GUPNP_VERSION = $(GUPNP_VERSION_MAJOR).0
 GUPNP_SOURCE = gupnp-$(GUPNP_VERSION).tar.xz
 GUPNP_SITE = http://ftp.gnome.org/pub/gnome/sources/gupnp/$(GUPNP_VERSION_MAJOR)
-GUPNP_LICENSE = LGPL-2.0+
+GUPNP_LICENSE = LGPL-2.1+
 GUPNP_LICENSE_FILES = COPYING
 GUPNP_INSTALL_STAGING = YES
 GUPNP_DEPENDENCIES = host-pkgconf libglib2 libxml2 gssdp util-linux
