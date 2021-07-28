79660c173e06b3290cf7b5f3c56da0f1bacecf92
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-07-28 08:06:36 +0200

    package/libogg: switch back to http
    LIBOGG_SITE has been switched to https since commit
    c8dd578b135041eed639c06810e67b8b018402d4 resulting in the following
    download failure:
    
    Connecting to downloads.xiph.org (downloads.xiph.org)|2001:470:eb26:42::1|:443... connected.
    OpenSSL: error:1407742E:SSL routines:SSL23_GET_SERVER_HELLO:tlsv1 alert protocol version
    
    So switch back to http
    
    Fixes:
     - http://autobuild.buildroot.org/results/56225b5daa471668d25cbe3b794e83c75c6724f7
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libogg/libogg.mk
+++ package/libogg/libogg.mk
@@ -6,7 +6,7 @@
 
 LIBOGG_VERSION = 1.3.5
 LIBOGG_SOURCE = libogg-$(LIBOGG_VERSION).tar.xz
-LIBOGG_SITE = https://downloads.xiph.org/releases/ogg
+LIBOGG_SITE = http://downloads.xiph.org/releases/ogg
 LIBOGG_LICENSE = BSD-3-Clause
 LIBOGG_LICENSE_FILES = COPYING
 
