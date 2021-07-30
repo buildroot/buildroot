buildroot/buildroot

commit f043646044261578fd554c1f899b84e798ef9437

package/fetchmail: add SELinux module
Support for fetchmail is added by the services/fetchmail module in the
SELinux refpolicy.

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/fetchmail/fetchmail.mk
@@ -11,6 +11,7 @@ FETCHMAIL_SITE = http://downloads.sourceforge.net/project/fetchmail/branch_$(FET
 FETCHMAIL_LICENSE = GPL-2.0; some exceptions are mentioned in COPYING
 FETCHMAIL_LICENSE_FILES = COPYING
 FETCHMAIL_CPE_ID_VENDOR = fetchmail
+FETCHMAIL_SELINUX_MODULES = fetchmail
 
 FETCHMAIL_CONF_OPTS = \
 	--with-ssl=$(STAGING_DIR)/usr

commit 46fde0f29363edd8233c4b20b97a702838aa541e

package/fetchmail: fix SELinux
Build is broken since commit f043646044261578fd554c1f899b84e798ef9437
because services/mta module is mandatory with fetchmail

Fixes:
 - No autobuilder failures yet

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/fetchmail/fetchmail.mk
@@ -11,7 +11,7 @@ FETCHMAIL_SITE = http://downloads.sourceforge.net/project/fetchmail/branch_$(FET
 FETCHMAIL_LICENSE = GPL-2.0; some exceptions are mentioned in COPYING
 FETCHMAIL_LICENSE_FILES = COPYING
 FETCHMAIL_CPE_ID_VENDOR = fetchmail
-FETCHMAIL_SELINUX_MODULES = fetchmail
+FETCHMAIL_SELINUX_MODULES = fetchmail mta
 
 FETCHMAIL_CONF_OPTS = \
 	--with-ssl=$(STAGING_DIR)/usr