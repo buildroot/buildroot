buildroot/buildroot

commit 6aeee4007322574083533a71c2be9a74e2faed55

package/exim: add SELinux module
Support for exim is added by the services/exim module in the SELinux
refpolicy.

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/exim/exim.mk
@@ -10,6 +10,7 @@ EXIM_SITE = https://ftp.exim.org/pub/exim/exim4
 EXIM_LICENSE = GPL-2.0+
 EXIM_LICENSE_FILES = LICENCE
 EXIM_CPE_ID_VENDOR = exim
+EXIM_SELINUX_MODULES = exim
 EXIM_DEPENDENCIES = host-berkeleydb host-pcre pcre berkeleydb host-pkgconf
 
 # Modify a variable value. It must already exist in the file, either

commit 5428413f77e5a0246319c3fcbd50b757b7ae9135

package/exim: fix SELinux
Build is broken since commit 6aeee4007322574083533a71c2be9a74e2faed55
because services/mta module is mandatory with exim

Fixes:
 - http://autobuild.buildroot.org/results/7032d13c4a1b5979f6737d1482a8452e26c705aa

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/exim/exim.mk
@@ -10,7 +10,7 @@ EXIM_SITE = https://ftp.exim.org/pub/exim/exim4
 EXIM_LICENSE = GPL-2.0+
 EXIM_LICENSE_FILES = LICENCE
 EXIM_CPE_ID_VENDOR = exim
-EXIM_SELINUX_MODULES = exim
+EXIM_SELINUX_MODULES = exim mta
 EXIM_DEPENDENCIES = host-berkeleydb host-pcre pcre berkeleydb host-pkgconf
 
 # Modify a variable value. It must already exist in the file, either