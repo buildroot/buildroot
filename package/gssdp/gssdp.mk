88b07f86befd30734b111e00a1160eab3194bfb5
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-07-30 13:30:47 +0200

    package/gssdp: disable sniffer
    Disable sniffer as it depends on gtk4 since
    https://gitlab.gnome.org/GNOME/gssdp/-/commit/c251ddcd03331ecba8702658be8023fb32743a85
    and so build failures are raised since bump to version 1.3.0 in commit
    42b46562a286d0082638f9bac5346c523877b5c2
    
    Fixes:
     - http://autobuild.buildroot.org/results/3b4f4a2c19696b32c3a70af5792a305cd861328c
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/gssdp/gssdp.mk
+++ package/gssdp/gssdp.mk
@@ -12,20 +12,13 @@ GSSDP_LICENSE = LGPL-2.1+
 GSSDP_LICENSE_FILES = COPYING
 GSSDP_INSTALL_STAGING = YES
 GSSDP_DEPENDENCIES = host-pkgconf libglib2 libsoup
-GSSDP_CONF_OPTS = -Dexamples=false
+GSSDP_CONF_OPTS = -Dexamples=false -Dsniffer=false
 
 ifeq ($(BR2_PACKAGE_GOBJECT_INTROSPECTION),y)
 GSSDP_CONF_OPTS += -Dintrospection=true -Dvapi=true
 GSSDP_DEPENDENCIES += host-vala gobject-introspection
 else
 GSSDP_CONF_OPTS += -Dintrospection=false -Dvapi=false
-endif
-
-ifeq ($(BR2_PACKAGE_LIBGTK3),y)
-GSSDP_DEPENDENCIES += libgtk3
-GSSDP_CONF_OPTS += -Dsniffer=true
-else
-GSSDP_CONF_OPTS += -Dsniffer=false
 endif
 
 $(eval $(meson-package))
