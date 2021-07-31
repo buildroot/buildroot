54054448bb661653f6c8a2291fc3b78471c30917
Bernd Kuhls <bernd.kuhls@t-online.de>
2021-07-31 07:25:25 +0200

    package/kodi-pvr-nextpvr: bump version to 8.2.6-Matrix
    Changelog:
    https://github.com/kodi-pvr/pvr.nextpvr/blob/Matrix/pvr.nextpvr/changelog.txt
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/kodi-pvr-nextpvr/kodi-pvr-nextpvr.mk
+++ package/kodi-pvr-nextpvr/kodi-pvr-nextpvr.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PVR_NEXTPVR_VERSION = 8.2.5-Matrix
+KODI_PVR_NEXTPVR_VERSION = 8.2.6-Matrix
 KODI_PVR_NEXTPVR_SITE = $(call github,kodi-pvr,pvr.nextpvr,$(KODI_PVR_NEXTPVR_VERSION))
 KODI_PVR_NEXTPVR_LICENSE = GPL-2.0+
 KODI_PVR_NEXTPVR_LICENSE_FILES = LICENSE.md
