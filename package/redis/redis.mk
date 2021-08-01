71c83cc21f783d2e14c62da057cb412743722c4b
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-08-01 10:44:33 +0200

    package/redis: add SELinux module
    Support for redis is added by the services/redis module in the SELinux
    refpolicy.
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/redis/redis.mk
+++ package/redis/redis.mk
@@ -9,6 +9,7 @@ REDIS_SITE = http://download.redis.io/releases
 REDIS_LICENSE = BSD-3-Clause (core); MIT and BSD family licenses (Bundled components)
 REDIS_LICENSE_FILES = COPYING
 REDIS_CPE_ID_VENDOR = redislabs
+REDIS_SELINUX_MODULES = redis
 
 define REDIS_USERS
 	redis -1 redis -1 * /var/lib/redis /bin/false - Redis Server
