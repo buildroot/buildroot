ec43be27bb4f0416646dcf81e5ecf7d65c79f945
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-08-01 22:53:49 +0200

    package/squid: add SELinux module
    Support for squid is added by the services/squid module in the SELinux
    refpolicy.
    
    Moreover, add apache module as for now it is unconditionally used in
    squid.fc
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/squid/squid.mk
+++ package/squid/squid.mk
@@ -10,6 +10,7 @@ SQUID_SITE = http://www.squid-cache.org/Versions/v4
 SQUID_LICENSE = GPL-2.0+
 SQUID_LICENSE_FILES = COPYING
 SQUID_CPE_ID_VENDOR = squid-cache
+SQUID_SELINUX_MODULES = apache squid
 SQUID_DEPENDENCIES = libcap host-libcap libtool libxml2 host-pkgconf \
 	$(if $(BR2_PACKAGE_LIBNETFILTER_CONNTRACK),libnetfilter_conntrack)
 SQUID_CONF_ENV = \
