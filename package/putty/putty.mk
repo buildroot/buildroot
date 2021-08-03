75f37e2b646aca8420d2ef35736d6f55f1e5a6ee
Peter Korsgaard <peter@korsgaard.com>
2021-08-03 21:53:07 +0200

    Revert "package/putty: Ignore CVE-2021-33500"
    This reverts commit 01063c4291a7300285ad9ec044b94968dd6c2d09.
    
    With putty now bumped to 0.75, this can be dropped.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/putty/putty.mk
+++ package/putty/putty.mk
@@ -11,9 +11,6 @@ PUTTY_LICENSE_FILES = LICENCE
 PUTTY_CPE_ID_VENDOR = putty
 PUTTY_CONF_OPTS = --disable-gtktest
 
-# Windows only, fixed for Windows with 0.75
-PUTTY_IGNORE_CVES += CVE-2021-33500
-
 ifeq ($(BR2_PACKAGE_LIBGTK2),y)
 PUTTY_CONF_OPTS += --with-gtk=2
 PUTTY_DEPENDENCIES += libgtk2
