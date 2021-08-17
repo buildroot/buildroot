2169cd671d51bb280e6de21024d2b47d020ed27a
Neeraj Deshpande <n.deshpande@metrological.com>
2021-08-17 20:03:31 +0530

    wpeframework-rdkservices : version bump.

--- package/wpe/wpeframework-rdkservices/wpeframework-rdkservices.mk
+++ package/wpe/wpeframework-rdkservices/wpeframework-rdkservices.mk
@@ -3,7 +3,7 @@
 # wpeframework-rdkservices
 #
 ################################################################################
-WPEFRAMEWORK_RDKSERVICES_VERSION = c9b4c1253a379f30c64e5e132fcab6a212a5ab7b
+WPEFRAMEWORK_RDKSERVICES_VERSION = 429dea8124310df9b7ca12436cd8a4e2521c607c
 WPEFRAMEWORK_RDKSERVICES_SITE_METHOD = git
 WPEFRAMEWORK_RDKSERVICES_SITE = git@github.com:WebPlatformForEmbedded/ThunderNanoServicesRDK.git
 WPEFRAMEWORK_RDKSERVICES_INSTALL_STAGING = YES
