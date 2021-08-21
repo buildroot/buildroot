5bb4e281c0c9fdd867eb7471f4e1ae82028d596d
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-08-21 11:39:07 +0200

    package/sdl2: kmsdrm needs GBM
    kmsdrm needs GBM (and so mesa3d) since its addition in version 2.0.6:
    https://github.com/libsdl-org/SDL/commit/56363ebf6124b345e1cfbd14fb6c0e654837910c
    
    If libgbm is not found, kmsdrm will be silently disabled
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/sdl2/sdl2.mk
+++ package/sdl2/sdl2.mk
@@ -152,7 +152,7 @@ SDL2_CONF_OPTS += --disable-alsa
 endif
 
 ifeq ($(BR2_PACKAGE_SDL2_KMSDRM),y)
-SDL2_DEPENDENCIES += libdrm
+SDL2_DEPENDENCIES += libdrm mesa3d
 SDL2_CONF_OPTS += --enable-video-kmsdrm
 else
 SDL2_CONF_OPTS += --disable-video-kmsdrm
