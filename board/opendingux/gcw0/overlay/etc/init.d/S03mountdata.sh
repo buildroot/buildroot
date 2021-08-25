489d3a790cb90ff53c8a3c1c3380eec498f51b38
Paul Cercueil <paul@crapouillou.net>
2021-08-25 13:06:03 +0100

    od: Use a unique S03mountdata.sh init.d script
    Instead of using one script per OpenDingux flavour, use one script that
    will handle all flavours.
    
    This has the benefit that a rs90 build, for instance, can be booted
    from a SD card, and the data partition of the SD card will be used
    properly.
    
    It also supports using filesystems that aren't built-in but exported as
    external modules. This allows the kernel to provide different
    filesystems that can be used for the data partition (e.g. EXT4 or F2FS)
    without clobbering the kernel with drivers that may not be used.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- board/opendingux/gcw0/overlay/etc/init.d/S03mountdata.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-[ -z "$1" ] || [ "x$1" = "xstart" ] || exit 0
-
-mkdir -p /media/data
-mount /dev/mmcblk0p2 -o nosuid,nodev /media/data
---
+++ board/opendingux/target_skeleton/etc/init.d/S03mountdata.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+[ -z "$1" ] || [ "x$1" = "xstart" ] || exit 0
+
+mkdir -p /media/data
+
+BOOT_PARTITION=$(sed -n 's/.*root=\([[:alnum:]:\/]\+\).*/\1/p' /proc/cmdline)
+
+case "$BOOT_PARTITION" in
+	/dev/mmcblk*)
+		# Booting from MMC/SD. The first partition is the system,
+		# the second partition is the data.
+		DATA_PARTITION=${BOOT_PARTITION::-1}$(expr ${BOOT_PARTITION: -1} \+ 1)
+		DATA_FS=$(blkid_fs $DATA_PARTITION)
+
+		# Modprobe the module needed for the filesystem if needed.
+		if [ -z "$(grep "$DATA_FS" /proc/filesystems)" ] ; then
+			modprobe "$DATA_FS"
+		fi
+
+		mount $DATA_PARTITION -t $DATA_FS -o nosuid,nodev /media/data
+		;;
+	"")
+		# Booting without a system partition - do nothing.
+		;;
+	*)
+		# Booting with a merged system / data partition.
+		# Create a read-write mountpoint of the partition to
+		# /media/data.
+		mount --bind /media/data /media/data
+		mount --make-shared /boot
+		mount --make-slave /media/data
+		mount --bind /boot /media/data
+		mount -o remount,rw /media/data
+		;;
+esac
