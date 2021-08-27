bf856adc19e3d139c6f39315a10c11d5e0d508e6
Vinod Sathyaseelan <vinod_sathyaseelan@comcast.com>
2021-08-26 18:17:16 -0700

    Enabled auto-reconfiguration option

--- package/crun/crun.mk
+++ package/crun/crun.mk
@@ -9,6 +9,7 @@ CRUN_SITE = https://github.com/containers/crun/releases/download/$(CRUN_VERSION)
 CRUN_LICENSE = GPLv2+
 CRUN_LICENSE_FILES = COPYING
 CRUN_INSTALL_STAGING = YES
+CRUN_AUTORECONF = YES
 CRUN_DEPENDENCIES = host-python3 yajl libcap libseccomp
 
 CRUN_CONF_OPTS = --disable-systemd --enable-shared
