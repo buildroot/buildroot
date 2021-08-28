bfb764c6dbfa721d3fe35a7bfe951f84b2a904c4
Bernd Kuhls <bernd.kuhls@t-online.de>
2021-08-28 10:40:01 +0200

    package/kodi-pvr-zattoo: bump version to 19.7.10-Matrix
    Release notes:
    https://github.com/rbuehlma/pvr.zattoo/releases/tag/19.7.10-Matrix
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/kodi-pvr-zattoo/kodi-pvr-zattoo.mk
+++ package/kodi-pvr-zattoo/kodi-pvr-zattoo.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PVR_ZATTOO_VERSION = 19.7.9-Matrix
+KODI_PVR_ZATTOO_VERSION = 19.7.10-Matrix
 KODI_PVR_ZATTOO_SITE = $(call github,rbuehlma,pvr.zattoo,$(KODI_PVR_ZATTOO_VERSION))
 KODI_PVR_ZATTOO_LICENSE = GPL-2.0+
 KODI_PVR_ZATTOO_LICENSE_FILES = LICENSE.md
