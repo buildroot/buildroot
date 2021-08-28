98fb8a6786463761521c757ff44a6f7a82856f29
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-08-28 17:06:03 +0200

    package/c-ares: fix build with latest nodejs
    Fix the following build failure with nodejs raised since bump to version
    12.22.5 in commit 7038b029d8c8774eca6d7888d6642d7e84ff5165:
    
    ../src/cares_wrap.cc:42:11: fatal error: ares_nameser.h: No such file or directory
       42 | # include <ares_nameser.h>
          |           ^~~~~~~~~~~~~~~~
    
    Fixes:
     - http://autobuild.buildroot.org/results/a0f867d5e765fc1aa052de5e53ed350b3b20743f
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit a9ca15cf9263becea9fd2ba79ba2a8f9884765ba)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/c-ares/c-ares.mk
+++ package/c-ares/c-ares.mk
@@ -10,6 +10,8 @@ C_ARES_INSTALL_STAGING = YES
 C_ARES_CONF_OPTS = --with-random=/dev/urandom
 C_ARES_LICENSE = MIT
 C_ARES_LICENSE_FILES = LICENSE.md
+# We're patching configure.ac
+C_ARES_AUTORECONF = YES
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
