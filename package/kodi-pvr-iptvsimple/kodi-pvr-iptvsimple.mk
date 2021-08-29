48ed613dab718ef8ab854b4f7439f2dc00630608
Bernd Kuhls <bernd.kuhls@t-online.de>
2021-08-29 13:39:14 +0200

    package/kodi-pvr-iptvsimple: bump version to 7.6.10-Matrix
    Changelog:
    https://github.com/kodi-pvr/pvr.iptvsimple/blob/Matrix/pvr.iptvsimple/changelog.txt
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/kodi-pvr-iptvsimple/kodi-pvr-iptvsimple.mk
+++ package/kodi-pvr-iptvsimple/kodi-pvr-iptvsimple.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PVR_IPTVSIMPLE_VERSION = 7.6.9-Matrix
+KODI_PVR_IPTVSIMPLE_VERSION = 7.6.10-Matrix
 KODI_PVR_IPTVSIMPLE_SITE = $(call github,kodi-pvr,pvr.iptvsimple,$(KODI_PVR_IPTVSIMPLE_VERSION))
 KODI_PVR_IPTVSIMPLE_LICENSE = GPL-2.0+
 KODI_PVR_IPTVSIMPLE_LICENSE_FILES = LICENSE.md
