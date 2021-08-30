3f81aa885de833805999b168465042071ba831ad
Joachim Wiberg <troglobit@gmail.com>
2021-08-30 17:30:54 +0200

    package/smcroute: bump version to v2.5.2
    - Upstream has changed to tar.gz
     - Drop _SOURCE, fixes check-package warning
     - setpgrp not used anymore, no need to override configure cache
     - Makefile no longer uses CC?=, drop comment
     - Enable MRDISC build option, must be activated per phyint in
       smcroute.conf, so safe to enable as a default build option
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/smcroute/smcroute.mk
+++ package/smcroute/smcroute.mk
@@ -4,16 +4,13 @@
 #
 ################################################################################
 
-SMCROUTE_VERSION = 2.4.4
-SMCROUTE_SOURCE = smcroute-$(SMCROUTE_VERSION).tar.xz
+SMCROUTE_VERSION = 2.5.2
 SMCROUTE_SITE = https://github.com/troglobit/smcroute/releases/download/$(SMCROUTE_VERSION)
 SMCROUTE_LICENSE = GPL-2.0+
 SMCROUTE_LICENSE_FILES = COPYING
 SMCROUTE_CPE_ID_VENDOR = troglobit
 
-SMCROUTE_CONF_OPTS = ac_cv_func_setpgrp_void=yes
-#BUG:The package Makefile uses CC?= even though the package is autotools based
-SMCROUTE_MAKE_ENV = $(TARGET_CONFIGURE_OPTS)
+SMCROUTE_CONF_OPTS = --enable-mrdisc
 
 ifeq ($(BR2_PACKAGE_LIBCAP),y)
 SMCROUTE_DEPENDENCIES = libcap
