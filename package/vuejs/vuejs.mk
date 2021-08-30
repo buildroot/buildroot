9955ab9a09f1b7d33f018855eca3935d25784cab
Thomas Claveirole <thomas.claveirole@green-communications.fr>
2021-08-30 13:24:30 +0200

    package/vuejs: bump version to 3.2.6
    Also correct the comment about installing the minified js file, which
    was changed with the update to 3.0.5 in commit 42bf38dca.
    
    Signed-off-by: Thomas Claveirole <thomas.claveirole@green-communications.fr>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/vuejs/vuejs.mk
+++ package/vuejs/vuejs.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-VUEJS_VERSION = 3.1.5
+VUEJS_VERSION = 3.2.6
 VUEJS_SOURCE = vue-$(VUEJS_VERSION).tgz
 VUEJS_SITE = https://registry.npmjs.org/vue/-
 VUEJS_LICENSE = MIT
 VUEJS_LICENSE_FILES = LICENSE
 
-# Install .min.js as .js
+# Install .prod.js as .js
 define VUEJS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 644 -D $(@D)/dist/vue.global.prod.js \
 		$(TARGET_DIR)/var/www/vue.js
