4b924eef6111ebeea52fa68f173138446c4625c7
Paul Cercueil <paul@crapouillou.net>
2021-08-31 18:06:23 +0100

    od: odbootd: bump package
    Bump odbootd to the latest master. It adds support for the
    PocketGo2/PlayGo Rev. 1 & 2 handhelds.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- board/opendingux/package/odbootd/odbootd.mk
+++ board/opendingux/package/odbootd/odbootd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ODBOOTD_VERSION = 2e2234f
+ODBOOTD_VERSION = 49e4f2f
 ODBOOTD_SITE = $(call github,opendingux,odbootd,$(ODBOOTD_VERSION))
 
 ODBOOTD_CONF_OPTS = -DWITH_ODBOOT_CLIENT:BOOL=OFF
