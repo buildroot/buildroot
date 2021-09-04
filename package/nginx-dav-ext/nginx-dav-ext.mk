c4cae0b7c032e792b208360848659662aee4692f
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-09-04 13:36:32 +0200

    package/nginx-dav-ext: needs libxslt
    Build is broken since bump to version 3.0.0 in commit
    45524f10cd84855868c9be3901c2bd6ea1c9f079 because libxslt is needed
    instead of expat since
    https://github.com/arut/nginx-dav-ext-module/commit/37772c545314aa4a250066f4e7cbc8806d406fa8:
    
    adding module in /tmp/instance-0/output-1/build/nginx-dav-ext-3.0.0
     + ngx_http_dav_ext_module was configured
    checking for PCRE library ... found
    checking for PCRE JIT support ... found
    Package libxslt was not found in the pkg-config search path.
    Perhaps you should add the directory containing `libxslt.pc'
    to the PKG_CONFIG_PATH environment variable
    Package 'libxslt', required by 'virtual:world', not found
    Package libxslt was not found in the pkg-config search path.
    Perhaps you should add the directory containing `libxslt.pc'
    to the PKG_CONFIG_PATH environment variable
    Package 'libxslt', required by 'virtual:world', not found
    checking for libxslt ... not found
    checking for libxslt in /usr/local/ ... not found
    checking for libxslt in /usr/pkg/ ... not found
    checking for libxslt in /opt/local/ ... not found
    
    ./configure: error: the HTTP XSLT module requires the libxml2/libxslt
    libraries. You can either do not enable the module or install the libraries.
    
    Fixes:
     - http://autobuild.buildroot.org/results/8735d20ca7ccf4eda2f79f9400bed70474855b40
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/nginx-dav-ext/nginx-dav-ext.mk
+++ package/nginx-dav-ext/nginx-dav-ext.mk
@@ -8,6 +8,6 @@ NGINX_DAV_EXT_VERSION = 3.0.0
 NGINX_DAV_EXT_SITE = $(call github,arut,nginx-dav-ext-module,v$(NGINX_DAV_EXT_VERSION))
 NGINX_DAV_EXT_LICENSE = BSD-2-Clause
 NGINX_DAV_EXT_LICENSE_FILES = LICENSE
-NGINX_DAV_EXT_DEPENDENCIES = expat
+NGINX_DAV_EXT_DEPENDENCIES = libxslt
 
 $(eval $(generic-package))
