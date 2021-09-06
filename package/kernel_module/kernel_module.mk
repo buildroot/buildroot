07bd0799bc08b6963cc5b9bdf34d1283d553c3f6
bigbearishappy <953308023@qq.com>
2021-09-06 02:34:22 +0000

    fix the kernel module compile

--- package/kernel_module/kernel_module.mk
+++ package/kernel_module/kernel_module.mk
@@ -1,4 +1,4 @@
-KERNEL_MODULE_VERSION = master
+KERNEL_MODULE_VERSION = 82fed6ef59ea9a7b64fffd7904eae0c3f052a36d
 KERNEL_MODULE_SITE = https://github.com/Seeed-Studio/seeed-linux-dtoverlays.git
 KERNEL_MODULE_SITE_METHOD = git
 KERNEL_MODULE_INSTALL_STAGING = NO
@@ -13,7 +13,7 @@ define KERNEL_MODULE_BUILD_CMDS
 endef
 
 define KERNEL_MODULE_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0644 $(@D)/overlays/rpi/reTerminal-overlay.dtbo $(BINARIES_DIR)/rpi-firmware/roverlays/eTerminal.dtbo
+	$(INSTALL) -D -m 0644 $(@D)/overlays/rpi/reTerminal-overlay.dtbo $(BINARIES_DIR)/rpi-firmware/roverlays/reTerminal.dtbo
 	mkdir -p $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra/
 	$(MAKE) ARCH="$(KERNEL_ARCH)" CROSS_COMPILE="$(TARGET_CROSS)" KBUILD="$(LINUX_DIR)" KO_DIR="$(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra/" -C $(@D) install_rpi
 	$(HOST_DIR)/sbin/depmod -a -b $(TARGET_DIR) $(LINUX_VERSION_PROBED)
