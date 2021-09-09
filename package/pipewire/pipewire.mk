d52be73cbb90642e2d0724f7555c9fb86c0499f1
James Hilliard <james.hilliard1@gmail.com>
2021-09-09 16:41:21 -0600

    package/pipewire: install systemd services when using systemd
    Also create a pipewire user, which is used for systemd service. There is
    no sysvinit start script at the moment, but if there were, it should
    also take care of changing the uid to pipewire before starting the
    daemon.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/pipewire/pipewire.mk
+++ package/pipewire/pipewire.mk
@@ -49,10 +49,16 @@ PIPEWIRE_CONF_OPTS += -Dgstreamer=disabled
 endif
 
 ifeq ($(BR2_PACKAGE_SYSTEMD),y)
-PIPEWIRE_CONF_OPTS += -Dsystemd=enabled
+PIPEWIRE_CONF_OPTS += \
+	-Dsystemd=enabled \
+	-Dsystemd-system-service=enabled \
+	-Dsystemd-user-service=enabled
 PIPEWIRE_DEPENDENCIES += systemd
 else
-PIPEWIRE_CONF_OPTS += -Dsystemd=disabled
+PIPEWIRE_CONF_OPTS += \
+	-Dsystemd=disabled \
+	-Dsystemd-system-service=disabled \
+	-Dsystemd-user-service=disabled
 endif
 
 ifeq ($(BR2_PACKAGE_ALSA_LIB),y)
@@ -149,5 +155,9 @@ PIPEWIRE_DEPENDENCIES += webrtc-audio-processing
 else
 PIPEWIRE_CONF_OPTS += -Decho-cancel-webrtc=disabled
 endif
+
+define PIPEWIRE_USERS
+	pipewire -1 pipewire -1 * - - - PipeWire System Daemon
+endef
 
 $(eval $(meson-package))
