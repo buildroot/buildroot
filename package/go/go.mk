280719ba7f10a43ee7a59c4ad89746841fb39ca1
Christian Stewart <christian@paral.in>
2021-09-10 02:44:14 -0700

    package/go: security bump to 1.17.1
    The fix for CVE-2021-33196 can be bypassed by crafted inputs. As a result, the
    NewReader and OpenReader functions in archive/zip can still cause a panic or an
    unrecoverable fatal error when reading an archive that claims to contain a large
    number of files, regardless of its actual size.
    
    This is CVE-2021-39293.
    
    https://golang.org/doc/devel/release.html#go1.16.minor
    
    Signed-off-by: Christian Stewart <christian@paral.in>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/go/go.mk
+++ package/go/go.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GO_VERSION = 1.17
+GO_VERSION = 1.17.1
 GO_SITE = https://storage.googleapis.com/golang
 GO_SOURCE = go$(GO_VERSION).src.tar.gz
 
