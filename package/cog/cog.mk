59a19eb4c877067ec0db4eeed091f9bddccdd667
Adrian Perez de Castro <aperez@igalia.com>
2021-09-12 12:28:55 +0300

    package/cog: bump to version 0.10.1
    Update to version 0.10.1, which fixes the build when the DRM platform
    module is enabled, fixes handling of absolute pointer events, and makes
    the headless platform optional.
    
    Although the headless platform is optional, Buildroot keeps it enabled
    unconditionally (as previously) because the wpebackend-fdo dependency
    is always needed anyway because Buildroot does not include any other
    WPE backend.
    
    Release notes:
    
      https://wpewebkit.org/release/cog-0.10.1.html
    
    Signed-off-by: Adrian Perez de Castro <aperez@igalia.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/cog/cog.mk
+++ package/cog/cog.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-COG_VERSION = 0.10.0
+COG_VERSION = 0.10.1
 COG_SITE = https://wpewebkit.org/releases
 COG_SOURCE = cog-$(COG_VERSION).tar.xz
 COG_INSTALL_STAGING = YES
@@ -13,6 +13,7 @@ COG_LICENSE = MIT
 COG_LICENSE_FILES = COPYING
 COG_CONF_OPTS = \
 	-DCOG_BUILD_PROGRAMS=ON \
+	-DCOG_PLATFORM_HEADLESS=ON \
 	-DCOG_WESTON_DIRECT_DISPLAY=OFF \
 	-DINSTALL_MAN_PAGES=OFF \
 	-DCOG_HOME_URI='$(call qstrip,$(BR2_PACKAGE_COG_PROGRAMS_HOME_URI))'
