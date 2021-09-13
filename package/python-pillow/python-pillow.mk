6434e5910f7c83ebca9c15bd577efc688a03cfb1
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-09-13 22:51:31 +0200

    package/python-pillow: security bump to version 8.3.2
    - Fix CVE-2021-23437 Raise ValueError if color specifier is too long
    - Fix 6-byte OOB read in FliDecode
    - Update indentation in hash file (two spaces)
    
    https://github.com/python-pillow/Pillow/releases/tag/8.3.2
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit a7919e68a60c6055bb2a729b5aeb6d47e7bbe924)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-pillow/python-pillow.mk
+++ package/python-pillow/python-pillow.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PYTHON_PILLOW_VERSION = 8.3.1
-PYTHON_PILLOW_SITE = https://files.pythonhosted.org/packages/8f/7d/1e9c2d8989c209edfd10f878da1af956059a1caab498e5bc34fa11b83f71
+PYTHON_PILLOW_VERSION = 8.3.2
+PYTHON_PILLOW_SITE = https://files.pythonhosted.org/packages/90/d4/a7c9b6c5d176654aa3dbccbfd0be4fd3a263355dc24122a5f1937bdc2689
 PYTHON_PILLOW_SOURCE = Pillow-$(PYTHON_PILLOW_VERSION).tar.gz
 PYTHON_PILLOW_LICENSE = HPND
 PYTHON_PILLOW_LICENSE_FILES = LICENSE
