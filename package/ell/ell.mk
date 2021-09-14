5f3fa7db2ada1808f89e855251167e9e9497be09
Peter Seiderer <ps.report@gmx.net>
2021-09-14 23:00:34 +0200

    package/ell: bump version to 0.43
    - Changelog (since 0.41, from [1]):
    
      ver 0.43:
        Add support for DHCP Rapid Commit feature.
        Add support for DHCP authoritative mode feature.
    
      ver 0.42:
        Add support for constant time security functions.
        Add support for manipulating DHCP leases.
    
    [1] https://git.kernel.org/pub/scm/libs/ell/ell.git/tree/ChangeLog
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/ell/ell.mk
+++ package/ell/ell.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ELL_VERSION = 0.41
+ELL_VERSION = 0.43
 ELL_SOURCE = ell-$(ELL_VERSION).tar.xz
 ELL_SITE = $(BR2_KERNEL_MIRROR)/linux/libs/ell
 ELL_LICENSE = LGPL-2.1+
