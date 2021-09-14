a2b0c9b5f98090d85a2cdf0362f2862f0251805d
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-09-14 18:17:30 +0200

    package/x11r7/xapp_xeyes: xrender is optional, not mandatory
    xrender is optional, not mandatory since its addition in version 1.0.99:
    https://gitlab.freedesktop.org/xorg/app/xeyes/-/commit/5e825a140f4022b88dd7a1a20a9a01b653f1a95c
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/x11r7/xapp_xeyes/xapp_xeyes.mk
+++ package/x11r7/xapp_xeyes/xapp_xeyes.mk
@@ -9,6 +9,13 @@ XAPP_XEYES_SOURCE = xeyes-$(XAPP_XEYES_VERSION).tar.bz2
 XAPP_XEYES_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XEYES_LICENSE = MIT
 XAPP_XEYES_LICENSE_FILES = COPYING
-XAPP_XEYES_DEPENDENCIES = xlib_libX11 xlib_libXext xlib_libXmu xlib_libXrender xlib_libXt
+XAPP_XEYES_DEPENDENCIES = xlib_libX11 xlib_libXext xlib_libXmu xlib_libXt
+
+ifeq ($(BR2_PACKAGE_XLIB_LIBXRENDER),y)
+XAPP_XEYES_DEPENDENCIES += xlib_libXrender
+XAPP_XEYES_CONF_OPTS += --with-xrender
+else
+XAPP_XEYES_CONF_OPTS += --without-xrender
+endif
 
 $(eval $(autotools-package))
