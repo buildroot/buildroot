c54f85ca0d2ba7b73c899a27e62c67d3389617b8
Adrian Perez de Castro <aperez@igalia.com>
2021-09-17 21:31:32 +0300

    package/seatd: bump to version 0.6.2
    Update seatd to version 0.6.2, which makes the patches unnecessary (they
    have all been integrated in 0.6.0) and fixes a number of bugs. Some
    Meson build options have been renamed, so the build recipe is updated
    accordingly, too.
    
    Release notes:
    
      https://git.sr.ht/~kennylevinsen/seatd/refs/0.6.0
      https://git.sr.ht/~kennylevinsen/seatd/refs/0.6.1
      https://git.sr.ht/~kennylevinsen/seatd/refs/0.6.2
    
    Signed-off-by: Adrian Perez de Castro <aperez@igalia.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/seatd/seatd.mk
+++ package/seatd/seatd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SEATD_VERSION = 0.5.0
+SEATD_VERSION = 0.6.2
 SEATD_SOURCE = $(SEATD_VERSION).tar.gz
 SEATD_SITE = https://git.sr.ht/~kennylevinsen/seatd/archive
 SEATD_LICENSE = MIT
@@ -17,20 +17,20 @@ SEATD_CONF_OPTS += \
 	-Dwerror=false
 
 ifeq ($(BR2_PACKAGE_SYSTEMD_LOGIND),y)
-SEATD_CONF_OPTS += -Dlogind=enabled
+SEATD_CONF_OPTS += -Dlibseat-logind=enabled
 SEATD_DEPENDENCIES += systemd
 else
-SEATD_CONF_OPTS += -Dlogind=disabled
+SEATD_CONF_OPTS += -Dlibseat-logind=disabled
 endif
 
 ifeq ($(BR2_PACKAGE_SEATD_BUILTIN),y)
-SEATD_CONF_OPTS += -Dbuiltin=enabled
+SEATD_CONF_OPTS += -Dlibseat-builtin=enabled
 else
-SEATD_CONF_OPTS += -Dbuiltin=disabled
+SEATD_CONF_OPTS += -Dlibseat-builtin=disabled
 endif
 
 ifeq ($(BR2_PACKAGE_SEATD_DAEMON),y)
-SEATD_CONF_OPTS += -Dseatd=enabled -Dserver=enabled
+SEATD_CONF_OPTS += -Dlibseat-seatd=enabled -Dserver=enabled
 
 define SEATD_USERS
 	- - video -1 - - - - -
@@ -47,7 +47,7 @@ define SEATD_INSTALL_INIT_SYSTEMD
 endef
 
 else
-SEATD_CONF_OPTS += -Dseatd=disabled -Dserver=disabled
+SEATD_CONF_OPTS += -Dlibseat-seatd=disabled -Dserver=disabled
 endif
 
 $(eval $(meson-package))
