860a518965877980d86cf5f360aa920a94c5bc44
Paul Cercueil <paul@crapouillou.net>
2021-09-17 18:08:07 +0100

    od: Move some overlay init.d files to target_skeleton
    Most of the init.d startup files can be shared across versions of
    OpenDingux.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- board/opendingux/gcw0/overlay/etc/init.d/S01checkroot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ -z "$1" ] || [ "x$1" = "xstart" ]; then
-
-	echo "Checking system partition for errors..."
-	fsck.fat -a /dev/mmcblk0p1
-fi
---
+++ board/opendingux/target_skeleton/etc/init.d/S01checkroot.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ -z "$1" ] || [ "x$1" = "xstart" ]; then
+	BOOT_PARTITION=$(sed -n 's/.*root=\([[:alnum:]:\/]\+\).*/\1/p' /proc/cmdline)
+
+	if [ "$BOOT_PARTITION" = "/dev/mmcblk0p1" ] ; then
+		echo "Checking root FS for errors..."
+		fsck.fat -a /dev/mmcblk0p1
+	fi
+fi
