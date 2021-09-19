86e09b6ed17ad6ea0f3c2dafa8fd4ba112f43347
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-09-19 22:25:23 +0200

    package/libsrtp: bump to version 2.4.1
    - libnss is an optional dependency since version 2.3.0 and
      https://github.com/cisco/libsrtp/commit/a946b3f561757a08b879cb337bfa932086df3081
    - Drop comment about pkg-config as libsrtp uses PKG_CHECK_MODULES to
      find libnss and openssl
    - Update indentation in hash file (two spaces)
    
    https://github.com/cisco/libsrtp/blob/v2.4.1/CHANGES
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/libsrtp/libsrtp.mk
+++ package/libsrtp/libsrtp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBSRTP_VERSION = 2.2.0
+LIBSRTP_VERSION = 2.4.1
 LIBSRTP_SITE = $(call github,cisco,libsrtp,v$(LIBSRTP_VERSION))
 LIBSRTP_INSTALL_STAGING = YES
 LIBSRTP_LICENSE = BSD-3-Clause
@@ -19,18 +19,17 @@ else
 LIBSRTP_MAKE_OPTS = libsrtp2.a shared_library
 endif
 
-# While libsrtp is not using pkg-config itself, it checks if
-# pkg-config is available to determine whether it should install
-# libsrtp.pc. Since installing it seems useful, let's depend on
-# host-pkgconf to make sure pkg-config is installed.
 LIBSRTP_DEPENDENCIES = host-pkgconf
 
 # openssl handling needs libdl support
 ifeq ($(BR2_PACKAGE_OPENSSL)x$(BR2_STATIC_LIBS),yx)
 LIBSRTP_DEPENDENCIES += openssl
-LIBSRTP_CONF_OPTS += --enable-openssl
+LIBSRTP_CONF_OPTS += --disable-nss --enable-openssl
+else ifeq ($(BR2_PACKAGE_LIBNSS),y)
+LIBSRTP_DEPENDENCIES += libnss
+LIBSRTP_CONF_OPTS += --enable-nss --disable-openssl
 else
-LIBSRTP_CONF_OPTS += --disable-openssl
+LIBSRTP_CONF_OPTS += --disable-nss --disable-openssl
 endif
 
 $(eval $(autotools-package))
