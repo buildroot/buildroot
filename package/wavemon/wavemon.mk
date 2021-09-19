5cae1a0d675eca41125b6430157d3d0d78d1220c
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-09-19 16:53:31 +0200

    package/wavemon: bump to version 0.9.4
    Use the new LICENSE file added in version 0.9.3 with
    https://github.com/uoaerg/wavemon/commit/2251870415c5be3850ce59d99d0e352e55a3280a
    as COPYING has been dropped with
    https://github.com/uoaerg/wavemon/commit/bfa12c46c773e5fe49ec4f91fc6260aa4f66e258
    The text has been updated with https URLs.
    
    https://github.com/uoaerg/wavemon/releases/tag/v0.9.2
    https://github.com/uoaerg/wavemon/releases/tag/v0.9.3
    https://github.com/uoaerg/wavemon/releases/tag/v0.9.4
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/wavemon/wavemon.mk
+++ package/wavemon/wavemon.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-WAVEMON_VERSION = 0.9.1
+WAVEMON_VERSION = 0.9.4
 WAVEMON_SITE = $(call github,uoaerg,wavemon,v$(WAVEMON_VERSION))
 WAVEMON_LICENSE = GPL-3.0+
-WAVEMON_LICENSE_FILES = COPYING
+WAVEMON_LICENSE_FILES = LICENSE
 WAVEMON_DEPENDENCIES = host-pkgconf libnl ncurses
 
 # Handwritten Makefile.in, automake isn't used
