22f8b08780d143f0ac02120054cf5a1f000ea149
Giulio Benetti <giulio.benetti@benettiengineering.com>
2021-09-20 21:12:23 +0200

    package/harfbuzz: bump to version 3.0.0
    This major version introduces 16 new APIs[1] and remove 20 of them
    considered unstable[2]. All packages that depends on harfbuzz build
    correctly with this new version because they don't use the "old unstable"
    APIs.
    
    [1]: https://github.com/harfbuzz/harfbuzz/blob/main/NEWS#L11-L27
    [2]: https://github.com/harfbuzz/harfbuzz/blob/main/NEWS#L29-L49
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/harfbuzz/harfbuzz.mk
+++ package/harfbuzz/harfbuzz.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HARFBUZZ_VERSION = 2.9.1
+HARFBUZZ_VERSION = 3.0.0
 HARFBUZZ_SITE = https://github.com/harfbuzz/harfbuzz/releases/download/$(HARFBUZZ_VERSION)
 HARFBUZZ_SOURCE = harfbuzz-$(HARFBUZZ_VERSION).tar.xz
 HARFBUZZ_LICENSE = MIT, ISC (ucdn library)
