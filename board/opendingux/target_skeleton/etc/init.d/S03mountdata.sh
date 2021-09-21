74df3c7e599f186aea09613515a1b4505fb2af46
Paul Cercueil <paul@crapouillou.net>
2021-09-21 18:04:46 +0100

    od: S03mountdata.sh: Enhance detection of the data partition
    Instead of checking for a data partition if the system partition matches
    /dev/mmcblk*, check unconditionally, with the assumption that the data
    partition, if present, will always follow the system partition.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- board/opendingux/target_skeleton/etc/init.d/S03mountdata.sh
+++ board/opendingux/target_skeleton/etc/init.d/S03mountdata.sh
@@ -5,37 +5,33 @@
 mkdir -p /media/data
 
 BOOT_PARTITION=$(sed -n 's/.*root=\([[:alnum:]:\/]\+\).*/\1/p' /proc/cmdline)
+DATA_PARTITION=${BOOT_PARTITION::-1}$(expr ${BOOT_PARTITION: -1} \+ 1)
 
-case "$BOOT_PARTITION" in
-	/dev/mmcblk*)
-		# Booting from MMC/SD. The first partition is the system,
-		# the second partition is the data.
-		DATA_PARTITION=${BOOT_PARTITION::-1}$(expr ${BOOT_PARTITION: -1} \+ 1)
-		DATA_FS=$(blkid_fs $DATA_PARTITION)
+if [ -b "$DATA_PARTITION" ] ; then
+	# If present, use the partition following the system partition
+	# for the user data.
 
-		psplash_write "Mounting $DATA_PARTITION..."
+	psplash_write "Mounting $DATA_PARTITION..."
 
-		# Modprobe the module needed for the filesystem if needed.
-		if [ -z "$(grep "$DATA_FS" /proc/filesystems)" ] ; then
-			modprobe "$DATA_FS"
-		fi
+	DATA_FS=$(blkid_fs $DATA_PARTITION)
 
-		mount $DATA_PARTITION -t $DATA_FS -o nosuid,nodev /media/data
-		;;
-	"")
-		# Booting without a system partition - do nothing.
-		;;
-	*)
-		# Booting with a merged system / data partition.
-		# Create a read-write mountpoint of the partition to
-		# /media/data.
+	# Modprobe the module needed for the filesystem if needed.
+	if [ -z "$(grep "$DATA_FS" /proc/filesystems)" ] ; then
+		modprobe "$DATA_FS"
+	fi
 
-		psplash_write "Mounting /media/data..."
+	mount $DATA_PARTITION -t $DATA_FS -o nosuid,nodev /media/data
 
-		mount --bind /media/data /media/data
-		mount --make-shared /boot
-		mount --make-slave /media/data
-		mount --bind /boot /media/data
-		mount -o remount,rw /media/data
-		;;
-esac
+elif [ "$BOOT_PARTITION" ] ; then
+	# Booting with a merged system / data partition.
+	# Create a read-write mountpoint of the partition to
+	# /media/data.
+
+	psplash_write "Mounting /media/data..."
+
+	mount --bind /media/data /media/data
+	mount --make-shared /boot
+	mount --make-slave /media/data
+	mount --bind /boot /media/data
+	mount -o remount,rw /media/data
+fi
