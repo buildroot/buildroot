d92f86849a75813e88f2a98a5294738d6806be9f
Peter Seiderer <ps.report@gmx.net>
2021-09-21 22:21:37 +0200

    package/rpi-wifi-firmware: provide convenience symlinks
    Provide convenience links for BCM43455/CYW43455 based RPi3A+, RPi3B+
    and RPi4B WIFI firmware configuration files to avoid runtime firmware
    loading warning in the kernel logs, e.g. (RPi3B+):
    
      brcmfmac mmc1:0001:1: Direct firmware load for brcm/brcmfmac43455-sdio.raspberrypi,3-model-b-plus.txt failed with error -2
    
    or (RP4B):
    
      brcmfmac mmc1:0001:1: Direct firmware load for brcm/brcmfmac43455-sdio.raspberrypi,4-model-b.txt failed with error -2
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    [me: slight rewording/rewrapping in commit log]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/rpi-wifi-firmware/rpi-wifi-firmware.mk
+++ package/rpi-wifi-firmware/rpi-wifi-firmware.mk
@@ -12,6 +12,9 @@ RPI_WIFI_FIRMWARE_LICENSE_FILES = LICENCE.broadcom_bcm43xx
 define RPI_WIFI_FIRMWARE_INSTALL_TARGET_CMDS
 	$(INSTALL) -d $(TARGET_DIR)/lib/firmware/brcm
 	$(INSTALL) -m 0644 $(@D)/firmware/brcm/brcmfmac* $(TARGET_DIR)/lib/firmware/brcm
+	ln -sf brcmfmac43455-sdio.txt $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,3-model-a-plus.txt
+	ln -sf brcmfmac43455-sdio.txt $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,3-model-b-plus.txt
+	ln -sf brcmfmac43455-sdio.txt $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,4-model-b.txt
 endef
 
 $(eval $(generic-package))
