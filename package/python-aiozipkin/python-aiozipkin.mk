d1677b8e96337d66f63cb77bcfbc7c5824a67bd6
James Hilliard <james.hilliard1@gmail.com>
2021-09-21 22:40:12 -0600

    package/python-aiozipkin: bump to version 1.1.0
    License hash change due to linebreak removal:
    https://github.com/aio-libs/aiozipkin/commit/5712878b214d87a2c9b5e95bed30d309b5ea099f
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/python-aiozipkin/python-aiozipkin.mk
+++ package/python-aiozipkin/python-aiozipkin.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_AIOZIPKIN_VERSION = 0.7.1
+PYTHON_AIOZIPKIN_VERSION = 1.1.0
 PYTHON_AIOZIPKIN_SOURCE = aiozipkin-$(PYTHON_AIOZIPKIN_VERSION).tar.gz
-PYTHON_AIOZIPKIN_SITE = https://files.pythonhosted.org/packages/9e/33/120925f90470b7f52f46e2b9f71caf2514e389f42b3ed3b62f6389baee95
+PYTHON_AIOZIPKIN_SITE = https://files.pythonhosted.org/packages/0a/46/5f328fb8163163f0d4401a1c07585cdf567e56305cb12ce54c9bb62e3f74
 PYTHON_AIOZIPKIN_SETUP_TYPE = setuptools
 PYTHON_AIOZIPKIN_LICENSE = Apache-2.0
 PYTHON_AIOZIPKIN_LICENSE_FILES = LICENSE
