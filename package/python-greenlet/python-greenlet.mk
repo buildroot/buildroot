47ef8ab6cea619060d07c8618f6777dd23db0728
James Hilliard <james.hilliard1@gmail.com>
2021-09-22 00:37:40 -0600

    package/python-greenlet: bump to version 1.1.1
    License hash change due to path change in:
    https://github.com/python-greenlet/greenlet/commit/8448b094d1b81788ac9cbaf86a184712f1daf5e0
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/python-greenlet/python-greenlet.mk
+++ package/python-greenlet/python-greenlet.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_GREENLET_VERSION = 0.4.16
+PYTHON_GREENLET_VERSION = 1.1.1
 PYTHON_GREENLET_SOURCE = greenlet-$(PYTHON_GREENLET_VERSION).tar.gz
-PYTHON_GREENLET_SITE = https://files.pythonhosted.org/packages/20/5e/b989a19f4597b825f44125345cd8a8574216fae7fafe69e2cb1238ebd18a
-PYTHON_GREENLET_SETUP_TYPE = distutils
+PYTHON_GREENLET_SITE = https://files.pythonhosted.org/packages/72/7e/d8586068d47adba73afc085249712bd266cd7ffbf27d8bc260c33e9d6133
+PYTHON_GREENLET_SETUP_TYPE = setuptools
 PYTHON_GREENLET_LICENSE = MIT, PSF-2.0
 PYTHON_GREENLET_LICENSE_FILES = LICENSE LICENSE.PSF
 
