d13ad855181d573885ea127ef42f0b84e187539d
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-09-25 23:38:03 +0200

    package/mtr: add libcap optional dependency
    libcap is an optional dependency which is enabled by default since
    version 0.88 and
    https://github.com/traviscross/mtr/commit/2ff8de310222d26d10b13a104a1a82f2b86a84c6
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    (cherry picked from commit 3d99699f82eeb4d7c3eec00aba547556969a85ad)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/mtr/mtr.mk
+++ package/mtr/mtr.mk
@@ -8,7 +8,10 @@ MTR_VERSION = 0.93
 MTR_SITE = $(call github,traviscross,mtr,v$(MTR_VERSION))
 MTR_AUTORECONF = YES
 MTR_CONF_OPTS = --without-gtk
-MTR_DEPENDENCIES = host-pkgconf $(if $(BR2_PACKAGE_NCURSES),ncurses)
+MTR_DEPENDENCIES = \
+	host-pkgconf \
+	$(if $(BR2_PACKAGE_LIBCAP),libcap) \
+	$(if $(BR2_PACKAGE_NCURSES),ncurses)
 MTR_LICENSE = GPL-2.0
 MTR_LICENSE_FILES = COPYING
 MTR_SELINUX_MODULES = netutils
