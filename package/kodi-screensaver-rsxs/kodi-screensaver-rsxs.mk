d6a417f1c32684d42a87c48307cacc9f3905f286
Bernd Kuhls <bernd.kuhls@t-online.de>
2021-09-26 13:56:58 +0200

    package/kodi-screensaver-rsxs: bump version to 19.0.0-Matrix
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>

--- package/kodi-screensaver-rsxs/kodi-screensaver-rsxs.mk
+++ package/kodi-screensaver-rsxs/kodi-screensaver-rsxs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_SCREENSAVER_RSXS_VERSION = 21c65de59546bde770225f023747a39b345baffd
+KODI_SCREENSAVER_RSXS_VERSION = 19.0.0-Matrix
 KODI_SCREENSAVER_RSXS_SITE = $(call github,xbmc,screensavers.rsxs,$(KODI_SCREENSAVER_RSXS_VERSION))
 KODI_SCREENSAVER_RSXS_LICENSE = GPL-2.0+
 KODI_SCREENSAVER_RSXS_LICENSE_FILES = LICENSE.md
