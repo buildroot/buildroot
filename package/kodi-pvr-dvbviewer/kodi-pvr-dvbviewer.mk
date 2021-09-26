fb26e14321fd916fa343d76f232f8e3cc10fae94
Bernd Kuhls <bernd.kuhls@t-online.de>
2021-09-26 13:57:02 +0200

    package/kodi-pvr-dvbviewer: bump version to 19.0.1-Matrix
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>

--- package/kodi-pvr-dvbviewer/kodi-pvr-dvbviewer.mk
+++ package/kodi-pvr-dvbviewer/kodi-pvr-dvbviewer.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PVR_DVBVIEWER_VERSION = 7.3.3-Matrix
+KODI_PVR_DVBVIEWER_VERSION = 19.0.1-Matrix
 KODI_PVR_DVBVIEWER_SITE = $(call github,kodi-pvr,pvr.dvbviewer,$(KODI_PVR_DVBVIEWER_VERSION))
 KODI_PVR_DVBVIEWER_LICENSE = GPL-2.0+
 KODI_PVR_DVBVIEWER_LICENSE_FILES = LICENSE.md
