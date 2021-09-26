9d5727bff9ae7af10cd5c4e6b549374dc4f727d8
Bernd Kuhls <bernd.kuhls@t-online.de>
2021-09-26 13:57:11 +0200

    package/kodi-pvr-octonet: bump version to 19.0.0-Matrix
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>

--- package/kodi-pvr-octonet/kodi-pvr-octonet.mk
+++ package/kodi-pvr-octonet/kodi-pvr-octonet.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PVR_OCTONET_VERSION = 4.1.0-2-Matrix
+KODI_PVR_OCTONET_VERSION = 19.0.0-Matrix
 KODI_PVR_OCTONET_SITE = $(call github,DigitalDevices,pvr.octonet,$(KODI_PVR_OCTONET_VERSION))
 KODI_PVR_OCTONET_LICENSE = GPL-2.0+
 KODI_PVR_OCTONET_LICENSE_FILES = LICENSE.md
