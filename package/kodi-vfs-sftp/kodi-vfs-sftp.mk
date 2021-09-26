211683cf01a51dca276e047be3f3e68caa6e5ff4
Bernd Kuhls <bernd.kuhls@t-online.de>
2021-09-26 13:56:45 +0200

    package/kodi-vfs-sftp: bump version to 19.0.0-Matrix
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>

--- package/kodi-vfs-sftp/kodi-vfs-sftp.mk
+++ package/kodi-vfs-sftp/kodi-vfs-sftp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_VFS_SFTP_VERSION = 2.0.0-Matrix
+KODI_VFS_SFTP_VERSION = 19.0.0-Matrix
 KODI_VFS_SFTP_SITE = $(call github,xbmc,vfs.sftp,$(KODI_VFS_SFTP_VERSION))
 KODI_VFS_SFTP_LICENSE = GPL-2.0+
 KODI_VFS_SFTP_LICENSE_FILES = LICENSE.md
