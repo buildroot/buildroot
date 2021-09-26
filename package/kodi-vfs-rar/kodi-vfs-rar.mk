a43a40ea688ca9e45ff21331e1c1aae86393b0ad
Bernd Kuhls <bernd.kuhls@t-online.de>
2021-09-26 13:56:44 +0200

    package/kodi-vfs-rar: bump version to 19.0.0-Matrix
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>

--- package/kodi-vfs-rar/kodi-vfs-rar.mk
+++ package/kodi-vfs-rar/kodi-vfs-rar.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_VFS_RAR_VERSION = 4.0.0-Matrix
+KODI_VFS_RAR_VERSION = 19.0.0-Matrix
 KODI_VFS_RAR_SITE = $(call github,xbmc,vfs.rar,$(KODI_VFS_RAR_VERSION))
 KODI_VFS_RAR_LICENSE = unrar, GPL-2.0+
 KODI_VFS_RAR_LICENSE_FILES = lib/UnrarXLib/license.txt LICENSE.md
