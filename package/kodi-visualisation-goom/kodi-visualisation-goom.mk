cb7a3f2b20a0fcac8e92d2f8bec2263ed6d37075
Bernd Kuhls <bernd.kuhls@t-online.de>
2021-09-26 13:56:47 +0200

    package/kodi-visualisation-goom: bump version to 19.0.0-Matrix
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>

--- package/kodi-visualisation-goom/kodi-visualisation-goom.mk
+++ package/kodi-visualisation-goom/kodi-visualisation-goom.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_VISUALISATION_GOOM_VERSION = 3.4.0-Matrix
+KODI_VISUALISATION_GOOM_VERSION = 19.0.0-Matrix
 KODI_VISUALISATION_GOOM_SITE = $(call github,xbmc,visualization.goom,$(KODI_VISUALISATION_GOOM_VERSION))
 KODI_VISUALISATION_GOOM_LICENSE = GPL-2.0+
 KODI_VISUALISATION_GOOM_LICENSE_FILES = LICENSE.md
