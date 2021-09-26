be55d05d5ed3ff6ce6b03625cdd26aac5acff319
Bernd Kuhls <bernd.kuhls@t-online.de>
2021-09-26 15:21:19 +0200

    package/kodi-pvr-octonet: fix json dependency
    While creating the initial package file a wrong dependency was chosen.
    
    This package really depends on jsoncpp, not json-for-modern-cpp:
    https://github.com/DigitalDevices/pvr.octonet/commit/f724c5934c63c64a0085c3dbe4cd3c3fc58f1318
    
    This bug was found while testing per-package directories.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    (cherry picked from commit 2d8e45289531fda33d1a157dffc60b758d6a8377)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/kodi-pvr-octonet/kodi-pvr-octonet.mk
+++ package/kodi-pvr-octonet/kodi-pvr-octonet.mk
@@ -8,6 +8,6 @@ KODI_PVR_OCTONET_VERSION = 4.1.0-2-Matrix
 KODI_PVR_OCTONET_SITE = $(call github,DigitalDevices,pvr.octonet,$(KODI_PVR_OCTONET_VERSION))
 KODI_PVR_OCTONET_LICENSE = GPL-2.0+
 KODI_PVR_OCTONET_LICENSE_FILES = LICENSE.md
-KODI_PVR_OCTONET_DEPENDENCIES = json-for-modern-cpp kodi
+KODI_PVR_OCTONET_DEPENDENCIES = jsoncpp kodi
 
 $(eval $(cmake-package))
