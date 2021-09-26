5e00787fb9c442c61023edd765e657df85b07793
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-09-26 23:15:03 +0200

    package/mtr: bump to version 0.94
    - Add jansson optional dependency:
      https://github.com/traviscross/mtr/commit/c2739eaf139b96f4b4937aee9aa484f0e6d1df55
    - Add libgtk3 optional dependency:
      https://github.com/traviscross/mtr/commit/5bd8e6c3ba67fdb9445b1ddc3777aca3b0f7053e
    - Update indentation in hash file (two spaces)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>

--- package/mtr/mtr.mk
+++ package/mtr/mtr.mk
@@ -4,16 +4,29 @@
 #
 ################################################################################
 
-MTR_VERSION = 0.93
+MTR_VERSION = 0.94
 MTR_SITE = $(call github,traviscross,mtr,v$(MTR_VERSION))
 MTR_AUTORECONF = YES
-MTR_CONF_OPTS = --without-gtk
 MTR_DEPENDENCIES = \
 	host-pkgconf \
 	$(if $(BR2_PACKAGE_LIBCAP),libcap)
 MTR_LICENSE = GPL-2.0
 MTR_LICENSE_FILES = COPYING
 MTR_SELINUX_MODULES = netutils
+
+ifeq ($(BR2_PACKAGE_JANSSON),y)
+MTR_CONF_OPTS += --with-jansson
+MTR_DEPENDENCIES += jansson
+else
+MTR_CONF_OPTS += --without-jansson
+endif
+
+ifeq ($(BR2_PACKAGE_LIBGTK3),y)
+MTR_CONF_OPTS += --with-gtk
+MTR_DEPENDENCIES += libgtk3
+else
+MTR_CONF_OPTS += --without-gtk
+endif
 
 ifeq ($(BR2_PACKAGE_NCURSES),y)
 MTR_CONF_OPTS += --with-ncurses
