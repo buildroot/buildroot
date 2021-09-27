54790b134283f012d2491ebd7cea21e367aadf8f
Asaf Kahlon <asafka7@gmail.com>
2021-09-27 22:51:35 +0300

    package/python-html5lib: bump to version 1.1
    Plus, indent with two spaces in the hash file.
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/python-html5lib/python-html5lib.mk
+++ package/python-html5lib/python-html5lib.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_HTML5LIB_VERSION = 1.0.1
+PYTHON_HTML5LIB_VERSION = 1.1
 PYTHON_HTML5LIB_SOURCE = html5lib-$(PYTHON_HTML5LIB_VERSION).tar.gz
-PYTHON_HTML5LIB_SITE = https://files.pythonhosted.org/packages/85/3e/cf449cf1b5004e87510b9368e7a5f1acd8831c2d6691edd3c62a0823f98f
+PYTHON_HTML5LIB_SITE = https://files.pythonhosted.org/packages/ac/b6/b55c3f49042f1df3dcd422b7f224f939892ee94f22abcf503a9b7339eaf2
 PYTHON_HTML5LIB_LICENSE = MIT
 PYTHON_HTML5LIB_LICENSE_FILES = LICENSE
 PYTHON_HTML5LIB_SETUP_TYPE = setuptools
