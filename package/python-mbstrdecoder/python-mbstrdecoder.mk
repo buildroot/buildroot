46164280f4271786f7673e0ab72045ffe6da4e53
Asaf Kahlon <asafka7@gmail.com>
2021-09-27 09:38:04 +0300

    package/python-mbstrdecoder: bump to version 1.1.0
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>

--- package/python-mbstrdecoder/python-mbstrdecoder.mk
+++ package/python-mbstrdecoder/python-mbstrdecoder.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_MBSTRDECODER_VERSION = 1.0.1
+PYTHON_MBSTRDECODER_VERSION = 1.1.0
 PYTHON_MBSTRDECODER_SOURCE = mbstrdecoder-$(PYTHON_MBSTRDECODER_VERSION).tar.gz
-PYTHON_MBSTRDECODER_SITE = https://files.pythonhosted.org/packages/b7/3b/2524dd4771dfca572269036fb08356715cb78a1c1c2a4dae3a810a86b777
+PYTHON_MBSTRDECODER_SITE = https://files.pythonhosted.org/packages/6c/10/f82ba5a91489c91bf6adaa0e1aca38a7ab2d1d7d80195a5f6ad8c2ff387a
 PYTHON_MBSTRDECODER_SETUP_TYPE = setuptools
 PYTHON_MBSTRDECODER_LICENSE = MIT
 PYTHON_MBSTRDECODER_LICENSE_FILES = LICENSE
