ace87cd389efcbc7323ca503a3096e9dcdc6e215
Asaf Kahlon <asafka7@gmail.com>
2021-09-27 09:38:08 +0300

    package/python-typepy: bump to version 1.3.0
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>

--- package/python-typepy/python-typepy.mk
+++ package/python-typepy/python-typepy.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_TYPEPY_VERSION = 1.1.5
+PYTHON_TYPEPY_VERSION = 1.3.0
 PYTHON_TYPEPY_SOURCE = typepy-$(PYTHON_TYPEPY_VERSION).tar.gz
-PYTHON_TYPEPY_SITE = https://files.pythonhosted.org/packages/08/c8/ebfa4381563f1946a7cd2b03fab14df641c3a9acf8e6ca5f5787dd1f735a
+PYTHON_TYPEPY_SITE = https://files.pythonhosted.org/packages/07/7b/fb32933f2a17992af75c0f96e5538a25fecebd439a82dcc31926ba55d336
 PYTHON_TYPEPY_SETUP_TYPE = setuptools
 PYTHON_TYPEPY_LICENSE = MIT
 PYTHON_TYPEPY_LICENSE_FILES = LICENSE
