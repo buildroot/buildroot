70440e28c8eefd469f6994c524d062dee168493e
Asaf Kahlon <asafka7@gmail.com>
2021-09-28 14:02:12 +0300

    package/python-pudb: bump to version 2021.1
    * Update dependency list (added python-jedi).
    * The package is python3-only, and not because of pygments, so
      the comment after the "depends on" can be removed.
    * Indent with two spaces in the hash file.
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-pudb/python-pudb.mk
+++ package/python-pudb/python-pudb.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PUDB_VERSION = 2019.1
+PYTHON_PUDB_VERSION = 2021.1
 PYTHON_PUDB_SOURCE = pudb-$(PYTHON_PUDB_VERSION).tar.gz
-PYTHON_PUDB_SITE = https://files.pythonhosted.org/packages/32/2a/96f72649e5dfc90cf69d8590c00884c6897d6cbc54f727fb40f47b4faae3
+PYTHON_PUDB_SITE = https://files.pythonhosted.org/packages/c7/69/813e93519fc28744457ff68fa2eaac3b4ea30dda1e6994e969fe9d3008d9
 PYTHON_PUDB_SETUP_TYPE = setuptools
 PYTHON_PUDB_LICENSE = MIT
 PYTHON_PUDB_LICENSE_FILES = LICENSE
