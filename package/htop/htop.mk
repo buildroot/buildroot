349b0a08f6def2c400055bd53a1ad86c72f59f0f
Francois Perrad <fperrad@gmail.com>
2021-10-03 10:13:24 +0200

    package/htop: bump to version 3.1.0
    Updated COPYING file to remove the PLPA exemption (appendix 2)
    With this change the license is now GPLv2 without any additional wording.
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/htop/htop.mk
+++ package/htop/htop.mk
@@ -4,12 +4,7 @@
 #
 ################################################################################
 
-# This commit hash corresponds to version 3.0.5.
-# htop sources were moved from bintray to github and the sources tar archive
-# was also changed (the build process requires `HTOP_AUTORECONF = YES` now). We
-# use commit hash instead of git tag here to avoid breaking existing source
-# caches
-HTOP_VERSION = ce6d60e7def146c13d0b8bca4642e7401a0a8995
+HTOP_VERSION = 3.1.0
 HTOP_SITE = $(call github,htop-dev,htop,$(HTOP_VERSION))
 HTOP_DEPENDENCIES = ncurses
 HTOP_AUTORECONF = YES
