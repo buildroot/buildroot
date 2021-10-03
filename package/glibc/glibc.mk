ebc6c7e5b4317add90aca500656b51a4aaa30e7f
Romain Naour <romain.naour@gmail.com>
2021-10-03 14:40:25 +0200

    package/glibc: remove glibc csky fork
    Before removing csky gcc fork, remove the glibc fork.
    
    csky support has been merged in glibc 2.29 [1].
    
    [1] https://sourceware.org/git/?p=glibc.git;a=commitdiff;h=5f72b00591ce4d1b4c0418294ffe1623983d5679
    
    Signed-off-by: Romain Naour <romain.naour@gmail.com>
    Cc: Guo Ren <ren_guo@c-sky.com>
    Cc: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Asked-by: Guo Ren <guoren@kernel.org>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -4,10 +4,6 @@
 #
 ################################################################################
 
-ifeq ($(BR2_csky),y)
-GLIBC_VERSION = 7630ed2fa60caea98f500e4a7a51b88f9bf1e176
-GLIBC_SITE = $(call github,c-sky,glibc,$(GLIBC_VERSION))
-else
 # Generate version string using:
 #   git describe --match 'glibc-*' --abbrev=40 origin/release/MAJOR.MINOR/master | cut -d '-' -f 2-
 # When updating the version, please also update localedef
@@ -19,7 +15,6 @@ GLIBC_VERSION = 2.34-9-g9acab0bba6a5a57323b1f94bf95b21618a9e5aa4
 # *NEVER* decide on a version string by looking at the mirror.
 # Then check that the mirror has been synced already (happens once a day.)
 GLIBC_SITE = $(call github,bminor,glibc,$(GLIBC_VERSION))
-endif
 
 GLIBC_LICENSE = GPL-2.0+ (programs), LGPL-2.1+, BSD-3-Clause, MIT (library)
 GLIBC_LICENSE_FILES = COPYING COPYING.LIB LICENSES
