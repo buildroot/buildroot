1cbb4ab2d1379e328b2c42b1995330ce347858a1
Pierre Wielders <pierre@wielders.net>
2021-10-04 11:45:35 +0200

    [BUMP] Bumping packages and changes to the explora config (NXServer external)

--- package/wpe/wpeframework-amazon/wpeframework-amazon.mk
+++ package/wpe/wpeframework-amazon/wpeframework-amazon.mk
@@ -5,7 +5,7 @@
 #
 ################################################################################
 
-WPEFRAMEWORK_AMAZON_VERSION = e3c687f8e6483c11ba450056803891e4e7401b63
+WPEFRAMEWORK_AMAZON_VERSION = baa7c8b11bd757f6b32efff8c7896befc0f5ea5d
 WPEFRAMEWORK_AMAZON_SITE_METHOD = git
 WPEFRAMEWORK_AMAZON_SITE = git@github.com:Metrological/WPEPluginAmazon.git
 WPEFRAMEWORK_AMAZON_INSTALL_STAGING = YES
