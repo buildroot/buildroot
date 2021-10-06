9aaba203689cfe24aa102512d1326a44cc65af6c
Peter Seiderer <ps.report@gmx.net>
2021-10-06 23:37:25 +0200

    package/rpi-firmware: bump version to 2ef3800
    - bump version to 2ef3800 (past [1] which fixes the boot starvation
      introduced by [2] as reported by Vincent Fazio <vfazio@xes-inc.com>
      see [3] and [4])
    
    [1] https://github.com/raspberrypi/firmware/commit/b5257da58c0e652633592c6fce09a7f3b100a2eb
    [2] https://github.com/raspberrypi/firmware/commit/25e2b597ebfb2495eab4816a276758dcc6ea21f1
    [3] http://lists.busybox.net/pipermail/buildroot/2021-September/623659.html
    [4] http://lists.busybox.net/pipermail/buildroot/2021-September/624550.html
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 33e09977ff3b02967d191801e3361224455acb9d
+RPI_FIRMWARE_VERSION = 2ef3800b1b71c0e30c2a61ecad6e007ae96ea342
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
