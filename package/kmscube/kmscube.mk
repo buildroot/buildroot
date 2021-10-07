630530831fbf560d4a856b1f4e45ed57d9d94d67
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2021-10-07 11:51:46 +0200

    package/kmscube: use libgbm virtual package
    kmscube requires a libgbm implementation, which until now, only Mesa3D
    was providing, so kmscube had a direct dependency on Mesa3D.
    
    However, now that we have a proper libgbm virtual package, so this
    commit converts the kmscube package to use the libegl, libgbm and
    libgles virtual packages.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Kamel Bouhara <kamel.bouhara@bootlin.com>
    [ Kamel : add dependency on recent libgbm ]
    [Arnout: fix syntax of Config.in comment; order depends alphabetically]
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/kmscube/kmscube.mk
+++ package/kmscube/kmscube.mk
@@ -8,6 +8,6 @@ KMSCUBE_VERSION = 4660a7dca6512b6e658759d00cff7d4ad2a2059d
 KMSCUBE_SITE = https://gitlab.freedesktop.org/mesa/kmscube/-/archive/$(KMSCUBE_VERSION)
 KMSCUBE_LICENSE = MIT
 KMSCUBE_LICENSE_FILES = COPYING
-KMSCUBE_DEPENDENCIES = host-pkgconf mesa3d libdrm
+KMSCUBE_DEPENDENCIES = host-pkgconf libdrm libegl libgbm libgles
 
 $(eval $(meson-package))
