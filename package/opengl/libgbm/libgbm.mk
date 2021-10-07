4a541ee93242e67f7fab625607f5501c6925d451
Bernd Kuhls <bernd.kuhls@t-online.de>
2021-10-07 11:51:41 +0200

    package/opengl/libgbm: new virtual package
    Since 18.0-Leia, Kodi implements stand-alone gbm support alongside x11 &
    wayland.  To enable building libgbm in mesa3d without x11 & wayland we
    need to create a virtual package for libgbm.
    
    Also other packages besides mesa3d may provide libgbm.so, see
    http://patchwork.ozlabs.org/patch/647235/
    http://patchwork.ozlabs.org/patch/939703/
    
    It turns out that libgbm has seen several additions in its API over
    time, and therefore not all libgbm implementations provide support for
    all features. In order to account for this, this commit adds two hidden
    boolean options that allow libgbm providers to indicate which optional
    features they support:
    BR2_PACKAGE_LIBGBM_HAS_FEATURE_FORMAT_MODIFIER_PLANE_COUNT and
    BR2_PACKAGE_LIBGBM_HAS_FEATURE_DMA_BUF. These booleans must be selected
    by the packages providing libgbm implementations, and depended on by
    packages using libgbm.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Kamel Bouhara <kamel.bouhara@bootlin.com>
    [ Kamel : introduce gbm api features ]
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

---
+++ package/opengl/libgbm/libgbm.mk
@@ -0,0 +1,9 @@
+################################################################################
+#
+# libgbm
+#
+################################################################################
+
+# This package requires to install a gbm.pc which needs
+# to be provided by GBM providers.
+$(eval $(virtual-package))
