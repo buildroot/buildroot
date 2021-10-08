048681814ad6493547851d1ea73a06aecddd80b5
Santhosh Ramani <s.ramani@metrological.com>
2021-10-08 13:04:11 +0530

    Update CDMI hashes to match the new interface definition for pattern encryption

--- package/wpe/wpeframework-cdmi/wpeframework-cdmi-clearkey/wpeframework-cdmi-clearkey.mk
+++ package/wpe/wpeframework-cdmi/wpeframework-cdmi-clearkey/wpeframework-cdmi-clearkey.mk
@@ -3,7 +3,7 @@
 # wpeframework-cdmi-clearkey
 #
 ################################################################################
-WPEFRAMEWORK_CDMI_CLEARKEY_VERSION = 709a3bec04fb722712a3395c9033eb1c9e757e0e
+WPEFRAMEWORK_CDMI_CLEARKEY_VERSION = 216f15eea89adc777ceed2b702eeccb9ee02d1ee
 WPEFRAMEWORK_CDMI_CLEARKEY_SITE_METHOD = git
 WPEFRAMEWORK_CDMI_CLEARKEY_SITE = git@github.com:rdkcentral/OCDM-Clearkey.git
 WPEFRAMEWORK_CDMI_CLEARKEY_INSTALL_STAGING = YES
--- package/wpe/wpeframework-cdmi/wpeframework-cdmi-widevine/wpeframework-cdmi-widevine.mk
+++ package/wpe/wpeframework-cdmi/wpeframework-cdmi-widevine/wpeframework-cdmi-widevine.mk
@@ -3,7 +3,7 @@
 # wpeframework-cdmi-widevine
 #
 ################################################################################
-WPEFRAMEWORK_CDMI_WIDEVINE_VERSION = 7736fc6ecf696089553258db74a8cb17c8970541
+WPEFRAMEWORK_CDMI_WIDEVINE_VERSION = c03425345a2ca92f13d4592b9e6e1e04ef342429
 WPEFRAMEWORK_CDMI_WIDEVINE_SITE_METHOD = git
 WPEFRAMEWORK_CDMI_WIDEVINE_SITE = git@github.com:rdkcentral/OCDM-Widevine.git
 WPEFRAMEWORK_CDMI_WIDEVINE_INSTALL_STAGING = NO
