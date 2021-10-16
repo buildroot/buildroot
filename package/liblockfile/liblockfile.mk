84be5d6cf061cbe260810e096654bb674f93b51f
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-10-16 12:37:11 +0200

    package/liblockfile: bump to version 1.17
    - MMU is mandatory since version 1.11 and
      https://github.com/miquels/liblockfile/commit/09b8935abac7417870d6fbb2db8c4f1c7c35f4b6
    - DESTDIR must be used since version 1.10 and
      https://github.com/miquels/liblockfile/commit/e35f9eabcbba224ecc70b145d5d2a2d81064c195
      so drop custom commands (LIBLOCKFILE_INSTALL_{STAGING,TARGET}_CMDS)
      and replace patch by an upstreamable one
    - Update hash of COPYRIGHT and add licenses/{GPL-2,LGPL-2} to license
      files:
      https://github.com/miquels/liblockfile/commit/40f8d8092bbb88177d810131c1b1f96ad0ec0bf7
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/liblockfile/liblockfile.mk
+++ package/liblockfile/liblockfile.mk
@@ -4,26 +4,17 @@
 #
 ################################################################################
 
-LIBLOCKFILE_VERSION = 1.09
+LIBLOCKFILE_VERSION = 1.17
 LIBLOCKFILE_SOURCE = liblockfile_$(LIBLOCKFILE_VERSION).orig.tar.gz
-LIBLOCKFILE_SITE = http://snapshot.debian.org/archive/debian/20151026T153523Z/pool/main/libl/liblockfile
-LIBLOCKFILE_PATCH = liblockfile_$(LIBLOCKFILE_VERSION)-6.debian.tar.bz2
+LIBLOCKFILE_SITE = http://snapshot.debian.org/archive/debian/20210128T210947Z/pool/main/libl/liblockfile
+LIBLOCKFILE_PATCH = liblockfile_$(LIBLOCKFILE_VERSION)-1.debian.tar.bz2
 
 LIBLOCKFILE_LICENSE = LGPL-2.0+, GPL-2.0+ (dotlockfile)
-LIBLOCKFILE_LICENSE_FILES = COPYRIGHT
+LIBLOCKFILE_LICENSE_FILES = COPYRIGHT licenses/GPL-2 licenses/LGPL-2
 
+# We're patching Makefile.in
+LIBLOCKFILE_AUTORECONF = YES
 LIBLOCKFILE_INSTALL_STAGING = YES
 LIBLOCKFILE_CONF_OPTS = --mandir=/usr/share/man
-
-define LIBLOCKFILE_INSTALL_STAGING_CMDS
-	mkdir -p $(addprefix $(STAGING_DIR)/usr/share/man/man,1 3)
-	rm -f $(STAGING_DIR)/usr/lib/liblockfile.so
-	$(TARGET_MAKE_ENV) $(MAKE) -C $(LIBLOCKFILE_DIR) ROOT=$(STAGING_DIR) install
-	ln -sf liblockfile.so $(STAGING_DIR)/usr/lib/liblockfile.so.1
-endef
-
-define LIBLOCKFILE_INSTALL_TARGET_CMDS
-	cp -a $(STAGING_DIR)/usr/lib/liblockfile.so* $(TARGET_DIR)/usr/lib
-endef
 
 $(eval $(autotools-package))
