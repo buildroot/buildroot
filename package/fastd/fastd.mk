4d203fedfbd4a9763539dd8e93ec198df6f7a132
Alexander Dahl <post@lespocky.de>
2021-10-17 17:25:11 +0200

    package/fastd: bump version to 22
    There's a new feature to offload L2TP to the Linux kernel, which depends
    on libmnl.  A new menu option was introduced for that.
    
    It's possible to link an internal version of libmnl
    statically for smaller binary size on constraint targets, but this was
    not added to the buildroot package for complexity reasons (both build
    and licensing).
    
    The COPYING file gained an additional paragraph for internal libmnl
    which does not apply, because internal libmnl is not used in the
    package.
    
    Link: https://fastd.readthedocs.io/en/latest/releases/v22.html
    Signed-off-by: Alexander Dahl <post@lespocky.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/fastd/fastd.mk
+++ package/fastd/fastd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FASTD_VERSION = 21
+FASTD_VERSION = 22
 FASTD_SITE = https://github.com/NeoRaider/fastd/releases/download/v$(FASTD_VERSION)
 FASTD_SOURCE = fastd-$(FASTD_VERSION).tar.xz
 FASTD_LICENSE = BSD-2-Clause
@@ -31,6 +31,13 @@ FASTD_CONF_OPTS += -Dstatus_socket=enabled
 FASTD_DEPENDENCIES += json-c
 else
 FASTD_CONF_OPTS += -Dstatus_socket=disabled
+endif
+
+ifeq ($(BR2_PACKAGE_FASTD_OFFLOAD_L2TP),y)
+FASTD_CONF_OPTS += -Doffload_l2tp=enabled -Dlibmnl_builtin=false
+FASTD_DEPENDENCIES += libmnl
+else
+FASTD_CONF_OPTS += -Doffload_l2tp=disabled
 endif
 
 ifeq ($(BR2_INIT_SYSTEMD),y)
