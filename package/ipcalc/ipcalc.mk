77f4d2f8efcb6e3d842cd9719311907ecec6147b
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-10-20 18:54:45 +0200

    package/ipcalc: add libmaxminddb support
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/ipcalc/ipcalc.mk
+++ package/ipcalc/ipcalc.mk
@@ -10,8 +10,13 @@ IPCALC_SOURCE = ipcalc-$(IPCALC_VERSION).tar.bz2
 IPCALC_LICENSE = GPL-2.0+
 IPCALC_LICENSE_FILES = COPYING
 
-IPCALC_CONF_OPTS = \
-	-Duse_maxminddb=disabled \
-	-Duse_geoip=disabled
+IPCALC_CONF_OPTS = -Duse_geoip=disabled
+
+ifeq ($(BR2_PACKAGE_LIBMAXMINDDB),y)
+IPCALC_CONF_OPTS += -Duse_maxminddb=enabled
+IPCALC_DEPENDENCIES += host-pkgconf libmaxminddb
+else
+IPCALC_CONF_OPTS += -Duse_maxminddb=disabled
+endif
 
 $(eval $(meson-package))
