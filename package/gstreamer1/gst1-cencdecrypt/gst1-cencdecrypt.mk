6e504b7b4d11c6179c7eaeadab281ade95f40c84
krp97 <k.plata@metrological.com>
2021-10-21 16:54:44 +0200

    Introduce gst1-cencdecrypt package

---
+++ package/gstreamer1/gst1-cencdecrypt/gst1-cencdecrypt.mk
@@ -0,0 +1,7 @@
+GST1_CENCDECRYPT_VERSION = dad6b3e32783f8c127eddf824813d8e9725f579f
+GST1_CENCDECRYPT_SITE = git@github.com:WebPlatformForEmbedded/gstcencdecryptor.git
+GST1_CENCDECRYPT_SITE_METHOD = git
+GST1_CENCDECRYPT_INSTALL_STAGING = YES
+GST1_CENCDECRYPT_DEPENDENCIES = gstreamer1 wpeframework wpeframework-clientlibraries wpeframework-plugins 
+
+$(eval $(cmake-package))
