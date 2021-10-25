bcde80febd2f0455ba5c34f8aa870fc8133749ae
James Hilliard <james.hilliard1@gmail.com>
2021-10-25 02:04:46 -0600

    package/libpsl: remove docs config option
    This option is only available in master and not any release.
    
    Fixes:
    output/build/libpsl-0.21.1/meson.build:1:0: ERROR: Unknown options: "docs"
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Acked-by: Adrian Perez de Castro <aperez@igalia.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/libpsl/libpsl.mk
+++ package/libpsl/libpsl.mk
@@ -10,7 +10,6 @@ LIBPSL_LICENSE = MIT, BSD-3-Clause
 LIBPSL_LICENSE_FILES = COPYING src/LICENSE.chromium
 LIBPSL_DEPENDENCIES = host-pkgconf
 LIBPSL_INSTALL_STAGING = YES
-LIBPSL_CONF_OPTS = -Ddocs=false
 
 # The order of checks is the same as done by libpsl when configured.
 ifeq ($(BR2_PACKAGE_LIBIDN2)$(BR2_PACKAGE_LIBUNISTRING),yy)
