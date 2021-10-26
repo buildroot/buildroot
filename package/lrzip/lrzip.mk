3332c143c0675818beeebf1be6880dc282527519
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-10-26 19:09:29 +0200

    package/lrzip: security bump to version 0.641
    - Fix CVE-2021-27347: Use after free in lzma_decompress_buf function in
      stream.c in Irzip 0.631 allows attackers to cause Denial of Service
      (DoS) via a crafted compressed file.
    - Fix CVE-2021-27345: A null pointer dereference was discovered in
      ucompthread in stream.c in Irzip 0.631 which allows attackers to cause
      a denial of service (DOS) via a crafted compressed file.
    - Fix CVE-2020-25467: A null pointer dereference was discovered
      lzo_decompress_buf in stream.c in Irzip 0.621 which allows an attacker
      to cause a denial of service (DOS) via a crafted compressed file.
    - lz4 is a mandatory dependency since version 0.640 and
      https://github.com/ckolivas/lrzip/commit/3345a239b7f5353a1c1296d6a5d6b90729d4b669
    
    https://github.com/ckolivas/lrzip/compare/7f3bf46203bf45ea115d8bd9f310ea219be88af4...v0.641
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/lrzip/lrzip.mk
+++ package/lrzip/lrzip.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-LRZIP_VERSION = 7f3bf46203bf45ea115d8bd9f310ea219be88af4
-LRZIP_SITE = $(call github,ckolivas,lrzip,$(LRZIP_VERSION))
+LRZIP_VERSION = 0.641
+LRZIP_SITE = $(call github,ckolivas,lrzip,v$(LRZIP_VERSION))
 LRZIP_AUTORECONF = YES
 LRZIP_LICENSE = GPL-2.0+
 LRZIP_LICENSE_FILES = COPYING
-LRZIP_DEPENDENCIES = zlib lzo bzip2
+LRZIP_DEPENDENCIES = zlib lz4 lzo bzip2
 
 ifeq ($(BR2_i386)$(BR2_x86_64),y)
 LRZIP_DEPENDENCIES += host-nasm
