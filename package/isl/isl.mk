c29f8228e8754250a7a5d263d1c173a2227b8861
John Keeping <john@metanate.com>
2021-10-26 18:04:00 +0100

    package/isl: switch to new site
    isl.gforge.inria.fr no longer responds and the main site is now at
    sourceforge.io.  This change corresponds to the upstream commit that
    updates the documentation [1].
    
    [1] https://repo.or.cz/isl.git/commitdiff/002820d725479b456c97ca9b0d59d6b3e18f7ee4
    
    Signed-off-by: John Keeping <john@metanate.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    (cherry picked from commit 748b7878e90ab5813a6f48e156410cf38ef087d9)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/isl/isl.mk
+++ package/isl/isl.mk
@@ -6,7 +6,7 @@
 
 ISL_VERSION = 0.22
 ISL_SOURCE = isl-$(ISL_VERSION).tar.xz
-ISL_SITE = http://isl.gforge.inria.fr
+ISL_SITE = https://libisl.sourceforge.io
 ISL_LICENSE = MIT
 ISL_LICENSE_FILES = LICENSE
 HOST_ISL_DEPENDENCIES = host-gmp
