033ef8497aba5a87e3812ca474af2320fbabe72f
Bernd Kuhls <bernd.kuhls@t-online.de>
2021-10-28 17:31:07 +0200

    package/kodi: use libgbm virtual package
    Kodi needs gbm_bo_get_fd:
    https://github.com/xbmc/xbmc/blob/Matrix/xbmc/utils/GBMBufferObject.cpp#L54
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/kodi/kodi.mk
+++ package/kodi/kodi.mk
@@ -92,7 +92,7 @@ endif
 
 ifeq ($(BR2_PACKAGE_KODI_PLATFORM_SUPPORTS_GBM),y)
 KODI_CORE_PLATFORM_NAME += gbm
-KODI_DEPENDENCIES += libinput libxkbcommon mesa3d
+KODI_DEPENDENCIES += libgbm libinput libxkbcommon
 endif
 
 ifeq ($(BR2_PACKAGE_KODI_PLATFORM_SUPPORTS_WAYLAND),y)
