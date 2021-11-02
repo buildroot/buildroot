661917efd8101a4bc5b7417f8926b48014a0b082
Pierre-Jean Texier <texier.pj2@gmail.com>
2021-11-02 22:28:01 +0100

    package/libubootenv: bump to version 7dbfffa
    This includes the following changes:
    
    7dbfffa Fix parsing multiple .flags variables
    de31f3d Add REUSE badge
    6390c1c Make libubootenv REUSE compliant
    0573071 doxygen: fix libuboot_open/close return value description
    0d12468 Add documentation for the configuration file
    b1178c1 Add configuration for disabling MTD lock
    5dadcb3 main: Remove duplicated code about '=' in variable name.
    e085e37 libubootenv: Forbid to give empty variable name to fw_setenv.
    42f919e main: Check libuboot_set_env() result.
    1bc91d1 CI: add gitlab config
    
    License update details: see commit 6390c1c ("Make libubootenv REUSE
    compliant") in [0]
    
    [0] - https://github.com/sbabic/libubootenv/commit/6390c1c26a816a96c9a72588679df3b9445dc192
    
    Signed-off-by: Pierre-Jean Texier <texier.pj2@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libubootenv/libubootenv.mk
+++ package/libubootenv/libubootenv.mk
@@ -4,10 +4,13 @@
 #
 ################################################################################
 
-LIBUBOOTENV_VERSION = 0.3.2
-LIBUBOOTENV_SITE = $(call github,sbabic,libubootenv,v$(LIBUBOOTENV_VERSION))
-LIBUBOOTENV_LICENSE = LGPL-2.1
-LIBUBOOTENV_LICENSE_FILES = Licenses/lgpl-2.1.txt
+LIBUBOOTENV_VERSION = 7dbfffa4cc0e42ad3febb122a711fe9d1b20e9f7
+LIBUBOOTENV_SITE = $(call github,sbabic,libubootenv,$(LIBUBOOTENV_VERSION))
+LIBUBOOTENV_LICENSE = LGPL-2.1+, MIT, CC0-1.0
+LIBUBOOTENV_LICENSE_FILES = LICENSES/CC0-1.0.txt \
+	LICENSES/LGPL-2.1-or-later.txt \
+	LICENSES/MIT.txt
+
 LIBUBOOTENV_INSTALL_STAGING = YES
 LIBUBOOTENV_DEPENDENCIES = zlib
 
