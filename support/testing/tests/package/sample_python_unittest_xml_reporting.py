9d3428beca1cf5a900c330e2fcbb0e622fd1385a
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2021-11-02 23:21:26 +0100

    package/python-unitest-xml-reporting: new package
    Based on initial work from Nicolas Carrier
    <nicolas.carrier@orolia.com>, with the following additions:
    
    - Updated to a newer version
    - Added proper license file handling
    - Added runtime test case
    - Restricted to Python 3.x
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ support/testing/tests/package/sample_python_unittest_xml_reporting.py
@@ -0,0 +1,9 @@
+import unittest
+import xmlrunner
+
+class Test1(unittest.TestCase):
+    def test_something(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
