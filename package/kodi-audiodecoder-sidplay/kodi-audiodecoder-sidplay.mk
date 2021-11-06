34e12c4c3bfe7b2b23912d99a138fa8e43bc169b
Bernd Kuhls <bernd.kuhls@t-online.de>
2021-11-06 09:43:10 +0100

    package/kodi-audiodecoder-sidplay: bump version to 19.0.1-Matrix
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-audiodecoder-sidplay/kodi-audiodecoder-sidplay.mk
+++ package/kodi-audiodecoder-sidplay/kodi-audiodecoder-sidplay.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_AUDIODECODER_SIDPLAY_VERSION = 19.0.0-Matrix
+KODI_AUDIODECODER_SIDPLAY_VERSION = 19.0.1-Matrix
 KODI_AUDIODECODER_SIDPLAY_SITE = $(call github,xbmc,audiodecoder.sidplay,$(KODI_AUDIODECODER_SIDPLAY_VERSION))
 KODI_AUDIODECODER_SIDPLAY_LICENSE = GPL-2.0+
 KODI_AUDIODECODER_SIDPLAY_LICENSE_FILES = LICENSE.md
