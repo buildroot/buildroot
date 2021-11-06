87b06f3a803a26b4d6822646ef3c2be0210ba163
Peter Korsgaard <peter@korsgaard.com>
2021-11-06 18:29:54 +0100

    boot/sun20i-d1-spl: new package
    U-Boot does not yet have SPL code to initialize the DDR controller on the
    Allwinner D1 - So instead package the sun20i-d1-spl bootloader, which is
    based on boot0 from the Allwinner BSP with some modifications to build it
    separately from the BSP and boot mainline U-Boot as explained on the
    linux-sunxi wiki:
    
    https://linux-sunxi.org/Allwinner_Nezha
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ boot/sun20i-d1-spl/sun20i-d1-spl.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# sun20i-d1-spl
+#
+################################################################################
+
+# Commit on the 'mainline' branch
+SUN20I_D1_SPL_VERSION = 771192d0b3737798d7feca87263c8fa74a449787
+SUN20I_D1_SPL_SITE = $(call github,smaeul,sun20i_d1_spl,$(SUN20I_D1_SPL_VERSION))
+SUN20I_D1_SPL_INSTALL_TARGET = NO
+SUN20I_D1_SPL_INSTALL_IMAGES = YES
+SUN20I_D1_SPL_LICENSE = GPL-2.0+
+
+define SUN20I_D1_SPL_BUILD_CMDS
+	$(MAKE) -C $(@D) CROSS_COMPILE="$(TARGET_CROSS)" p=sun20iw1p1 mmc
+endef
+
+define SUN20I_D1_SPL_INSTALL_IMAGES_CMDS
+	$(INSTALL) -D -m 0644 $(@D)/nboot/boot0_sdcard_sun20iw1p1.bin \
+		$(BINARIES_DIR)/boot0_sdcard_sun20iw1p1.bin
+endef
+
+$(eval $(generic-package))
