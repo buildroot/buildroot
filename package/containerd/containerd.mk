bedcafa633894aee5c665dc7da3f89d27c9b6727
Peter Korsgaard <peter@korsgaard.com>
2021-11-09 11:45:20 +0100

    package/containerd: security bump to version 1.4.11
    Fixes the following security issues:
    
    - CVE-2021-41103: Insufficiently restricted permissions on plugin
      directories
      https://github.com/advisories/GHSA-c2h3-6mxw-7mvq
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/containerd/containerd.mk
+++ package/containerd/containerd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CONTAINERD_VERSION = 1.4.9
+CONTAINERD_VERSION = 1.4.11
 CONTAINERD_SITE = $(call github,containerd,containerd,v$(CONTAINERD_VERSION))
 CONTAINERD_LICENSE = Apache-2.0
 CONTAINERD_LICENSE_FILES = LICENSE
