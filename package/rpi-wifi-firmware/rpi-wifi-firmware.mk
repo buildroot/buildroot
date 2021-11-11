b88c545efd79ea6e287884213ed6cdab1f670e7a
Peter Seiderer <ps.report@gmx.net>
2021-11-11 21:04:37 +0100

    package/rpi-wifi-firmware: bump version to 3888ba2
    Changelog (since 883b726):
    
      - 3e020f7 Add 43436S firmware
                Future Zero 2 W boards will have an alternative version of 43436
                that should have different firmware, named brcmfmac43436s-sdio.
    
      - 3888ba2 Merge pull request #18 from HiassofT/43436S
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/rpi-wifi-firmware/rpi-wifi-firmware.mk
+++ package/rpi-wifi-firmware/rpi-wifi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_WIFI_FIRMWARE_VERSION = 883b72628de1d7efa45b421da0cbf175ac2374f8
+RPI_WIFI_FIRMWARE_VERSION = 3888ba29898bb3f056d5f1eb283cb8de4c533bef
 RPI_WIFI_FIRMWARE_SITE = $(call github,LibreELEC,brcmfmac_sdio-firmware-rpi,$(RPI_WIFI_FIRMWARE_VERSION))
 RPI_WIFI_FIRMWARE_LICENSE = PROPRIETARY
 RPI_WIFI_FIRMWARE_LICENSE_FILES = LICENCE.broadcom_bcm43xx
