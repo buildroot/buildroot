dfc468c4af354dbead36aec0c80b986415ac4be9
Bernd Kuhls <bernd.kuhls@t-online.de>
2021-11-13 13:56:18 +0100

    package/x11r7/xdriver_xf86-input-libinput: bump version to 1.2.0
    Switched _SITE to https.
    
    Release notes:
    https://lists.x.org/archives/xorg-announce/2021-September/003110.html
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/x11r7/xdriver_xf86-input-libinput/xdriver_xf86-input-libinput.mk
+++ package/x11r7/xdriver_xf86-input-libinput/xdriver_xf86-input-libinput.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XDRIVER_XF86_INPUT_LIBINPUT_VERSION = 1.1.0
+XDRIVER_XF86_INPUT_LIBINPUT_VERSION = 1.2.0
 XDRIVER_XF86_INPUT_LIBINPUT_SOURCE = xf86-input-libinput-$(XDRIVER_XF86_INPUT_LIBINPUT_VERSION).tar.bz2
-XDRIVER_XF86_INPUT_LIBINPUT_SITE = http://xorg.freedesktop.org/releases/individual/driver
+XDRIVER_XF86_INPUT_LIBINPUT_SITE = https://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_INPUT_LIBINPUT_LICENSE = MIT
 XDRIVER_XF86_INPUT_LIBINPUT_LICENSE_FILES = COPYING
 XDRIVER_XF86_INPUT_LIBINPUT_DEPENDENCIES = libinput xserver_xorg-server xorgproto
