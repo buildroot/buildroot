e1cc45314969ff78d1c3a453c5510a9fa67148ca
Romain Naour <romain.naour@gmail.com>
2021-11-14 18:39:45 +0100

    support/testing: test_openssh: use Bootlin stable toolchain
    The commit backported by [1] enabled the Bootlin bleeding-edge
    toolchain providing 5.4 kernel headers but the openssh test
    still use a 4.19 prebuilt kernel. Indeed the prebuilt kernel
    has been updated to 5.10 in Buildroot 2021.08 [2].
    
    Fixes:
    https://gitlab.com/kubu93/buildroot/-/jobs/1780867274
    
    [1] 8d4939c9e0342d8b99d863fbdcaf935d42038469
    [2] https://git.buildroot.net/buildroot/commit/?id=3cf2782906d5cc05a3966c25518d2ce01f11afc8
    
    Signed-off-by: Romain Naour <romain.naour@gmail.com>
    Cc: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- support/testing/tests/package/test_openssh.py
+++ support/testing/tests/package/test_openssh.py
@@ -51,6 +51,7 @@ class TestOpenSshGlibc(TestOpensshBase):
         BR2_arm=y
         BR2_TOOLCHAIN_EXTERNAL=y
         BR2_TOOLCHAIN_EXTERNAL_BOOTLIN=y
+        BR2_TOOLCHAIN_EXTERNAL_BOOTLIN_ARMV5_EABI_GLIBC_STABLE=y
         BR2_PACKAGE_RNG_TOOLS=y
         BR2_TARGET_ROOTFS_CPIO=y
         """
