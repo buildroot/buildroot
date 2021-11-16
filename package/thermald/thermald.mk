82760316a1a98461d8c46f6fb5947a5fa8d037b4
Ricardo Martincoski <ricardo.martincoski@gmail.com>
2021-11-15 21:09:57 -0300

    package/thermald: bump to 2.4.6
    Drop patch (already in version).
    
    Add new dependencies:
    - host-autoconf-archive
    - libevdev
    - upower
    - xz (liblzma)
    
    Create a pre-configure hook to avoid this error when reconfiguring:
    automake: error: cannot open < gtk-doc.make: No such file or directory
    Based on the solution used in 178eb1d7ea165d87460224d297ce615bb63090f0.
    During the build gtk-doc is already disabled by pkg-autotools.
    
    Create a post-patch hook to avoid this error when reconfiguring:
    aclocal: error: couldn't open directory 'm4': No such file or directory
    Based on the solution used in 6bf74ce3dbfec8979e379bc1b919f29d09f0d87b.
    
    Signed-off-by: Ricardo Martincoski <ricardo.martincoski@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/thermald/thermald.mk
+++ package/thermald/thermald.mk
@@ -4,16 +4,40 @@
 #
 ################################################################################
 
-THERMALD_VERSION = 1.9.1
+THERMALD_VERSION = 2.4.6
 THERMALD_SITE = $(call github,intel,thermal_daemon,v$(THERMALD_VERSION))
 # fetched from Github, with no configure script
 THERMALD_AUTORECONF = YES
-THERMALD_DEPENDENCIES = dbus dbus-glib libxml2 $(TARGET_NLS_DEPENDENCIES)
+THERMALD_AUTORECONF_OPTS = --include=$(HOST_DIR)/share/autoconf-archive
+THERMALD_DEPENDENCIES = \
+	$(TARGET_NLS_DEPENDENCIES) \
+	host-autoconf-archive \
+	dbus \
+	dbus-glib \
+	libevdev \
+	libxml2 \
+	upower \
+	xz
 # tools are GPL-3.0+ but are not added to the target
 THERMALD_LICENSE = GPL-2.0+
 THERMALD_LICENSE_FILES = COPYING
 THERMALD_CPE_ID_VENDOR = intel
 THERMALD_CONF_ENV = LIBS=$(TARGET_NLS_LIBS)
+
+# avoid this error when reconfiguring:
+# automake: error: cannot open < gtk-doc.make: No such file or directory
+define THERMALD_GTK_DOC_HOOK
+	echo "CLEANFILES=" > $(@D)/gtk-doc.make
+endef
+THERMALD_PRE_CONFIGURE_HOOKS += THERMALD_GTK_DOC_HOOK
+
+# Autoreconf is missing the m4/ directory, which might actually be missing
+# iff it was the first argument, but unfortunately we are overriding the
+# first include directory above. Thus we need that hook here.
+define THERMALD_CREATE_M4
+	mkdir -p $(@D)/m4
+endef
+THERMALD_POST_PATCH_HOOKS += THERMALD_CREATE_M4
 
 ifeq ($(BR2_INIT_SYSTEMD),y)
 THERMALD_DEPENDENCIES += systemd
