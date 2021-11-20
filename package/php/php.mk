b834cfef79c8fc5857f6b89fa24d414821396d90
Bernd Kuhls <bernd.kuhls@t-online.de>
2021-11-20 11:07:45 +0100

    package/php: security bump version to 8.0.13
    Changelog: https://www.php.net/ChangeLog-8.php#8.0.13
    
    Fixes CVE-2021-21707: http://bugs.php.net/79971
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 3c91d5f8dd0751eeb9be35b50b032ab60646cf0c)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/php/php.mk
+++ package/php/php.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PHP_VERSION = 8.0.12
+PHP_VERSION = 8.0.13
 PHP_SITE = https://www.php.net/distributions
 PHP_SOURCE = php-$(PHP_VERSION).tar.xz
 PHP_INSTALL_STAGING = YES
