8d31d09bac527f1a6b20e3b8621eea6a7e28f095
Sébastien Szymanski <sebastien.szymanski@armadeus.com>
2021-11-21 14:05:34 -0300

    package/stress-ng: fix broken URL
    Do the same as commit "12c01079bd package/stress-ng: bump to version
    0.13.05" did for 2022.02 but without the version bump.
    
    The project URL returns 404 error because:
    
    Quoting Coling King:
    "Unfortunately when I left Canonical last week they removed my tarballs"
    
    So use github and update hash.
    
    Signed-off-by: Sébastien Szymanski <sebastien.szymanski@armadeus.com>
    Cc: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    [Ricardo: do not bump the version.
     There will be no version clash at sources.buildroot.net since the file
     format changed]
    Signed-off-by: Ricardo Martincoski <ricardo.martincoski@datacom.com.br>
    Cc: Romain Naour <romain.naour@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/stress-ng/stress-ng.mk
+++ package/stress-ng/stress-ng.mk
@@ -5,8 +5,7 @@
 ################################################################################
 
 STRESS_NG_VERSION = 0.12.02
-STRESS_NG_SOURCE = stress-ng-$(STRESS_NG_VERSION).tar.xz
-STRESS_NG_SITE = https://kernel.ubuntu.com/~cking/tarballs/stress-ng
+STRESS_NG_SITE = $(call github,ColinIanKing,stress-ng,V$(STRESS_NG_VERSION))
 STRESS_NG_LICENSE = GPL-2.0+
 STRESS_NG_LICENSE_FILES = COPYING
 
