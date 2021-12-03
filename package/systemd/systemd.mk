f90d23112d2b9d52d2368a0a3f907ba37e2c60ce
Norbert Lange <nolange79@gmail.com>
2021-12-03 23:31:23 +0100

    package/systemd: set cgroups default-hierarchy to unified
    Use the unified hierarchy, which is systemds default since v243.
    The biggest holdover was docker which now supports the unified
    layout.
    
    Note that systemd will automatically fallback to hybrid if the
    kernel is too old or the unified hierarchy fails for other reasons.
    
    Signed-off-by: Norbert Lange <nolange79@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/systemd/systemd.mk
+++ package/systemd/systemd.mk
@@ -46,7 +46,7 @@ SYSTEMD_SELINUX_MODULES = systemd udev xdg
 SYSTEMD_PROVIDES = udev
 
 SYSTEMD_CONF_OPTS += \
-	-Ddefault-hierarchy=hybrid \
+	-Ddefault-hierarchy=unified \
 	-Didn=true \
 	-Dima=false \
 	-Dkexec-path=/usr/sbin/kexec \
