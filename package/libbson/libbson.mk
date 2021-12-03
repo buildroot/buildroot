1de5bcd1c88acc4a5bd77a7bc9163114129048bf
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-12-04 00:05:55 +0100

    package/libbson: add LIBBSON_CPE_ID_VENDOR
    cpe:2.3:a:mongodb:libbson is a valid CPE identifier for this package:
    
      https://nvd.nist.gov/products/cpe/search/results?namingFormat=2.3&keyword=cpe%3A2.3%3Aa%3Amongodb%3Alibbson
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 8d23efce2858232fe4e9dac417fe0e3835a57e2d)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libbson/libbson.mk
+++ package/libbson/libbson.mk
@@ -8,6 +8,7 @@ LIBBSON_VERSION = 1.9.5
 LIBBSON_SITE = https://github.com/mongodb/libbson/releases/download/$(LIBBSON_VERSION)
 LIBBSON_LICENSE = Apache-2.0, MIT (jsonl), ISC (b64), Zlib (md5)
 LIBBSON_LICENSE_FILES = COPYING THIRD_PARTY_NOTICES
+LIBBSON_CPE_ID_VENDOR = mongodb
 LIBBSON_CONF_OPTS = \
 	--disable-tests \
 	--disable-examples \
