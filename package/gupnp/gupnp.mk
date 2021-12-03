3db9cf6e0e1c7ab75e79116cfb7e4cc9d472b9d0
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-12-03 23:52:08 +0100

    package/gupnp: add GUPNP_CPE_ID_VENDOR
    cpe:2.3:a:gnome:gupnp is a valid CPE identifier for this package:
    
      https://nvd.nist.gov/products/cpe/search/results?namingFormat=2.3&keyword=cpe%3A2.3%3Aa%3Agnome%3Agupnp
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 6c3122b85a3bbdac5fa3c5a0b6f6f4d1fe9af1e0)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gupnp/gupnp.mk
+++ package/gupnp/gupnp.mk
@@ -10,6 +10,7 @@ GUPNP_SOURCE = gupnp-$(GUPNP_VERSION).tar.xz
 GUPNP_SITE = http://ftp.gnome.org/pub/gnome/sources/gupnp/$(GUPNP_VERSION_MAJOR)
 GUPNP_LICENSE = LGPL-2.0+
 GUPNP_LICENSE_FILES = COPYING
+GUPNP_CPE_ID_VENDOR = gnome
 GUPNP_INSTALL_STAGING = YES
 GUPNP_DEPENDENCIES = host-pkgconf libglib2 libxml2 gssdp util-linux
 GUPNP_CONF_OPTS = -Dexamples=false
