2d2888c7c0e5afa30789325700d1c2ab51c5c6ab
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-12-04 10:25:10 +0100

    package/mxml: add CPE variables
    cpe:2.3:a:mini-xml_project:mini-xml is a valid CPE identifier for this
    package:
    
      https://nvd.nist.gov/products/cpe/search/results?namingFormat=2.3&keyword=cpe%3A2.3%3Aa%3Amini-xml_project%3Amini-xml
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/mxml/mxml.mk
+++ package/mxml/mxml.mk
@@ -8,6 +8,8 @@ MXML_VERSION = 3.2
 MXML_SITE = https://github.com/michaelrsweet/mxml/releases/download/v$(MXML_VERSION)
 MXML_LICENSE = Apache-2.0 with exceptions
 MXML_LICENSE_FILES = LICENSE NOTICE
+MXML_CPE_ID_VENDOR = mini-xml_project
+MXML_CPE_ID_PRODUCT = mini-xml
 MXML_INSTALL_STAGING = YES
 
 MXML_INSTALL_STAGING_OPTS = DSTROOT=$(STAGING_DIR) install
