c9f21979eac4e29f9a3ce4b81684df853991f415
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-12-04 22:33:39 +0100

    package/unrar: add UNRAR_CPE_ID_VENDOR
    cpe:2.3:a:rarlab:unrar is a valid CPE identifier for this package:
    
      https://nvd.nist.gov/products/cpe/search/results?namingFormat=2.3&keyword=cpe%3A2.3%3Aa%3Ararlab%3Aunrar
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit ba9ca16b33e2e3a6893a95842711d34cfe47c52b)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/unrar/unrar.mk
+++ package/unrar/unrar.mk
@@ -9,6 +9,7 @@ UNRAR_SOURCE = unrarsrc-$(UNRAR_VERSION).tar.gz
 UNRAR_SITE = https://www.rarlab.com/rar
 UNRAR_LICENSE = unrar
 UNRAR_LICENSE_FILES = license.txt
+UNRAR_CPE_ID_VENDOR = rarlab
 
 define UNRAR_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) CXX="$(TARGET_CXX)" STRIP="/bin/true" \
