7d59055f791ff5f44f534e5f72ab6f2704091493
Joachim Wiberg <troglobit@gmail.com>
2021-12-09 21:14:55 +0100

    package/inadyn: bump to version 2.9.1
    See the ChangeLog, or release notes at GitHub, for full details:
    https://github.com/troglobit/inadyn/releases/tag/v2.9.1
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/inadyn/inadyn.mk
+++ package/inadyn/inadyn.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-INADYN_VERSION = 2.9.0
+INADYN_VERSION = 2.9.1
 INADYN_SITE = https://github.com/troglobit/inadyn/releases/download/v$(INADYN_VERSION)
 INADYN_LICENSE = GPL-2.0+
 INADYN_LICENSE_FILES = COPYING
