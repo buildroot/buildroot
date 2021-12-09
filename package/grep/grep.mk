f1f0b633eb1c4bc006b95daad8f299028e89d19d
Angelo Compagnucci <angelo@amarulasolutions.com>
2021-12-09 23:15:29 +0100

    package/grep: bump to version 3.7
    Signed-off-by: Angelo Compagnucci <angelo@amarulasolutions.com>
    Reviewed-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/grep/grep.mk
+++ package/grep/grep.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GREP_VERSION = 3.6
+GREP_VERSION = 3.7
 GREP_SITE = $(BR2_GNU_MIRROR)/grep
 GREP_SOURCE = grep-$(GREP_VERSION).tar.xz
 GREP_LICENSE = GPL-3.0+
