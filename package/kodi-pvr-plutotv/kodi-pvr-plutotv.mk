6ae728cc0167c31ca3642d50e396b5bd1ce00852
Bernd Kuhls <bernd.kuhls@t-online.de>
2021-12-12 20:17:06 +0100

    package/kodi-pvr-plutotv: bump version to 19.0.1-Matrix
    Changelog:
    https://github.com/kodi-pvr/pvr.plutotv/blob/Matrix/pvr.plutotv/changelog.txt
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/kodi-pvr-plutotv/kodi-pvr-plutotv.mk
+++ package/kodi-pvr-plutotv/kodi-pvr-plutotv.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PVR_PLUTOTV_VERSION = 19.0.0-Matrix
+KODI_PVR_PLUTOTV_VERSION = 19.0.1-Matrix
 KODI_PVR_PLUTOTV_SITE = $(call github,kodi-pvr,pvr.plutotv,$(KODI_PVR_PLUTOTV_VERSION))
 KODI_PVR_PLUTOTV_LICENSE = GPL-2.0+
 KODI_PVR_PLUTOTV_LICENSE_FILES = LICENSE.md
