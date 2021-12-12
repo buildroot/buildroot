73b1f5e43a9da822f82e20e9341a6dd552489f1f
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-12-12 22:05:25 +0100

    package/glog: bump to version 0.5.0
    - Drop patches (already in version)
    - Disable gtest which has been added (and is enabled by default) with
      https://github.com/google/glog/commit/be25d94c21137fa2fdc53f2dc9c61a94bcf72ba4
    - Update indentation in hash file (two spaces)
    
    https://github.com/google/glog/releases/tag/v0.5.0
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/glog/glog.mk
+++ package/glog/glog.mk
@@ -4,12 +4,13 @@
 #
 ################################################################################
 
-GLOG_VERSION = 0.4.0
+GLOG_VERSION = 0.5.0
 GLOG_SITE = $(call github,google,glog,v$(GLOG_VERSION))
 GLOG_INSTALL_STAGING = YES
 GLOG_LICENSE = BSD-3-Clause
 GLOG_LICENSE_FILES = COPYING
 GLOG_CONF_OPTS = \
+	-DWITH_GTEST=OFF \
 	$(if $(BR2_TOOLCHAIN_HAS_THREADS),-DWITH_THREADS=ON, -DWITH_THREADS=OFF)
 
 ifeq ($(BR2_PACKAGE_GFLAGS),y)
