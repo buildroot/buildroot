086d357dfb177ef28d7831716707d2424a2ea4bd
Peter Korsgaard <peter@korsgaard.com>
2021-12-15 18:08:02 +0100

    package/python-django: security bump to version 3.2.10
    Fixes the following security issues:
    
    - CVE-2021-44420: Potential bypass of an upstream access control based on
      URL paths
    
      HTTP requests for URLs with trailing newlines could bypass an upstream
      access control based on URL paths.
    
      This issue has low severity, according to the Django security policy.
    
      https://www.djangoproject.com/weblog/2021/dec/07/security-releases/
    
    In addition, 3.2.8 / 3.2.9 fixes a number of bugs.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 3.2.7
+PYTHON_DJANGO_VERSION = 3.2.10
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/59/45/c6fbb3a206df0b7dc3e6e8fae738e042c63d4ddf828c6e1ba10d7417a1d9
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/a5/8e/c6dfc718d572e4b33b56824b9e71e5ab9be8072e6747fc6184d206c3fdb3
+
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
 PYTHON_DJANGO_CPE_ID_VENDOR = djangoproject
