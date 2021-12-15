9c00e74a36f66e3bcc327fe12cfe607b21b3ab37
charles <charles@ccxtechnologies.com>
2021-12-15 10:25:12 -0500

    transition to local version of latest python-pyroute2 package

--- package/python-pyroute2/python-pyroute2.mk
+++ package/python-pyroute2/python-pyroute2.mk
@@ -4,11 +4,23 @@
 #
 ################################################################################
 
-PYTHON_PYROUTE2_VERSION = 0.5.12
-PYTHON_PYROUTE2_SOURCE = pyroute2-$(PYTHON_PYROUTE2_VERSION).tar.gz
-PYTHON_PYROUTE2_SITE = https://files.pythonhosted.org/packages/6d/59/27f6ba1a87bbc5abcd860513edc0f72f4dddd3d5417171af4187f1d8ae6e
+PYTHON_PYROUTE2_SITE = $(TOPDIR)/../thirdparty/pyroute2
+PYTHON_PYROUTE2_SITE_METHOD = local
+
 PYTHON_PYROUTE2_LICENSE = Apache-2.0 or GPL-2.0+
 PYTHON_PYROUTE2_LICENSE_FILES = LICENSE.Apache.v2 LICENSE.GPL.v2 README.license.md
-PYTHON_PYROUTE2_SETUP_TYPE = distutils
 
-$(eval $(python-package))
+PYTHON_PYROUTE2_DEPENDENCIES = host-python3-pip
+
+define PYTHON_PYROUTE2_BUILD_CMDS
+	$(MAKE) python=$(HOST_DIR)/bin/python3 root=$(TARGET_DIR) -C $(@D) clean
+	$(MAKE) python=$(HOST_DIR)/bin/python3 root=$(TARGET_DIR) -C $(@D) VERSION
+	$(MAKE) python=$(HOST_DIR)/bin/python3 root=$(TARGET_DIR) -C $(@D) setup
+	$(MAKE) python=$(HOST_DIR)/bin/python3 root=$(TARGET_DIR) -C $(@D) dist
+endef
+
+define PYTHON_PYROUTE2_INSTALL_TARGET_CMDS
+	$(MAKE) python=$(HOST_DIR)/bin/python3 prefix=$(TARGET_DIR) -C $(@D) install
+endef
+
+$(eval $(generic-package))
