2b906b975a956bd2b6352b2eec6c3c697317bbbe
Peter Korsgaard <peter@korsgaard.com>
2021-12-15 21:38:20 +0100

    package/libopenssl: bump version to 1.1.1m
    Minor bugfix release:
    
     Changes between 1.1.1l and 1.1.1m [14 Dec 2021]
    
      *) Avoid loading of a dynamic engine twice.
         [Bernd Edlinger]
    
      *) Fixed building on Debian with kfreebsd kernels
         [Mattias Ellert]
    
      *) Prioritise DANE TLSA issuer certs over peer certs
         [Viktor Dukhovni]
    
      *) Fixed random API for MacOS prior to 10.12
         These MacOS versions don't support the CommonCrypto APIs
         [Lenny Primak]
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/libopenssl/libopenssl.mk
+++ package/libopenssl/libopenssl.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBOPENSSL_VERSION = 1.1.1l
+LIBOPENSSL_VERSION = 1.1.1m
 LIBOPENSSL_SITE = https://www.openssl.org/source
 LIBOPENSSL_SOURCE = openssl-$(LIBOPENSSL_VERSION).tar.gz
 LIBOPENSSL_LICENSE = OpenSSL or SSLeay
