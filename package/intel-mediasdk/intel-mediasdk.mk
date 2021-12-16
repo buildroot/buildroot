feeed6f38eb592c8f72e4232c7c4f11e713e1d4b
Bernd Kuhls <bernd.kuhls@t-online.de>
2021-12-16 22:27:22 +0100

    package/intel-mediasdk: switch _SITE to https
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/intel-mediasdk/intel-mediasdk.mk
+++ package/intel-mediasdk/intel-mediasdk.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 INTEL_MEDIASDK_VERSION = 21.4.3
-INTEL_MEDIASDK_SITE = http://github.com/Intel-Media-SDK/MediaSDK/archive
+INTEL_MEDIASDK_SITE = https://github.com/Intel-Media-SDK/MediaSDK/archive
 INTEL_MEDIASDK_LICENSE = MIT
 INTEL_MEDIASDK_LICENSE_FILES = LICENSE
 
