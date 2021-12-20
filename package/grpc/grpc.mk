ffb2b79004837c80467e51a50b2ce7566399da7b
Michael Nosthoff <buildroot@heine.tech>
2021-12-20 09:46:14 +0100

    package/grpc: bump version to 1.43.0
    Signed-off-by: Michael Nosthoff <buildroot@heine.tech>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/grpc/grpc.mk
+++ package/grpc/grpc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GRPC_VERSION = 1.42.0
+GRPC_VERSION = 1.43.0
 GRPC_SITE = $(call github,grpc,grpc,v$(GRPC_VERSION))
 GRPC_LICENSE = Apache-2.0, BSD-3-Clause (third_party code), MPL-2.0 (etc/roots.pem)
 GRPC_LICENSE_FILES = LICENSE
