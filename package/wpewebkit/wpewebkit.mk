b9d7644c53a501a6abf15c5a9c7ff7eedd4f60c7
Adrian Perez de Castro <aperez@igalia.com>
2021-12-23 01:18:25 +0200

    package/wpewebkit: security bump to version 2.34.3
    This is a minor release which provides fixes for CVE-2021-30887
    and CVE-2021-30890. Full release notes can be found at:
    
      https://wpewebkit.org/release/wpewebkit-2.34.3.html
    
    An accompanying security advisory has been published at:
    
      https://wpewebkit.org/security/WSA-2021-0007.html
    
    Signed-off-by: Adrian Perez de Castro <aperez@igalia.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/wpewebkit/wpewebkit.mk
+++ package/wpewebkit/wpewebkit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEWEBKIT_VERSION = 2.34.2
+WPEWEBKIT_VERSION = 2.34.3
 WPEWEBKIT_SITE = http://www.wpewebkit.org/releases
 WPEWEBKIT_SOURCE = wpewebkit-$(WPEWEBKIT_VERSION).tar.xz
 WPEWEBKIT_INSTALL_STAGING = YES
