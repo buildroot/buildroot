733bf73e3ff86dd80e68e22c0ccc2f43e4a3d475
Norbert Lange <nolange79@gmail.com>
2021-12-23 22:30:33 +0100

    package/lttng-libust: bump to version 2.13.1
    Add a patch to disable building tests, building the tests can
    fail unless the package is reconfigured.
    
    Signed-off-by: Norbert Lange <nolange79@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/lttng-libust/lttng-libust.mk
+++ package/lttng-libust/lttng-libust.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LTTNG_LIBUST_SITE = http://lttng.org/files/lttng-ust
-LTTNG_LIBUST_VERSION = 2.12.1
+LTTNG_LIBUST_VERSION = 2.13.1
 LTTNG_LIBUST_SOURCE = lttng-ust-$(LTTNG_LIBUST_VERSION).tar.bz2
 LTTNG_LIBUST_LICENSE = LGPL-2.1, MIT (system headers), GPL-2.0 (liblttng-ust-ctl/ustctl.c used by lttng-sessiond)
 LTTNG_LIBUST_LICENSE_FILES = COPYING
@@ -14,7 +14,9 @@ LTTNG_LIBUST_DEPENDENCIES = liburcu util-linux
 LTTNG_LIBUST_CONF_OPTS = \
 	--disable-man-pages \
 	--disable-examples \
+	--disable-tests \
 	--with-lttng-system-rundir=/run/lttng
+LTTNG_LIBUST_AUTORECONF = YES
 
 ifeq ($(BR2_PACKAGE_PYTHON),y)
 LTTNG_LIBUST_DEPENDENCIES += python
