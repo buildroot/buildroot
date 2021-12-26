24e4c888c3d36e3943206ea55894b84344cc8b2e
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-12-26 21:22:14 +0100

    package/hiawatha: bump to version 11.1
    https://gitlab.com/hsleisink/hiawatha/-/blob/v11.1/ChangeLog
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/hiawatha/hiawatha.mk
+++ package/hiawatha/hiawatha.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HIAWATHA_VERSION = 10.11
+HIAWATHA_VERSION = 11.1
 HIAWATHA_SITE = https://www.hiawatha-webserver.org/files
 HIAWATHA_DEPENDENCIES = zlib
 HIAWATHA_LICENSE = GPL-2.0
