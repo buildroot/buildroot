f54776b6b145a4ccab81ba2717f6540f8e48b17e
Bernd Kuhls <bernd.kuhls@t-online.de>
2021-12-27 18:25:59 +0100

    package/kodi-screensaver-pingpong: bump version to 19.0.1-Matrix
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-screensaver-pingpong/kodi-screensaver-pingpong.mk
+++ package/kodi-screensaver-pingpong/kodi-screensaver-pingpong.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_SCREENSAVER_PINGPONG_VERSION = 19.0.0-Matrix
+KODI_SCREENSAVER_PINGPONG_VERSION = 19.0.1-Matrix
 KODI_SCREENSAVER_PINGPONG_SITE = $(call github,xbmc,screensaver.pingpong,$(KODI_SCREENSAVER_PINGPONG_VERSION))
 KODI_SCREENSAVER_PINGPONG_LICENSE = GPL-2.0+
 KODI_SCREENSAVER_PINGPONG_LICENSE_FILES = LICENSE.md
