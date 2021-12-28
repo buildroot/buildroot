22ad0f2569ff849dd2eefdd6c0412d961c762f95
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-12-28 15:49:35 +0100

    package/libxmlpp: bump to version 5.0.1
    Switch to meson-package as configure is not shipped anymore
    
    This bump will fix the following build failure raised since bump of
    glibmm to version 2.68.2 in commit
    dddb65efbdf2c7952f22a1e51009f17f1676db4a:
    
    Package 'glibmm-2.4', required by 'virtual:world', not found
    
    https://github.com/libxmlplusplus/libxmlplusplus/blob/5.0.1/NEWS
    
    Fixes:
     - http://autobuild.buildroot.org/results/ab3c3ace6d9409a70a4a8c1e27dfa9b915be10fd
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libxmlpp/libxmlpp.mk
+++ package/libxmlpp/libxmlpp.mk
@@ -4,13 +4,17 @@
 #
 ################################################################################
 
-LIBXMLPP_VERSION_MAJOR = 3.2
-LIBXMLPP_VERSION = $(LIBXMLPP_VERSION_MAJOR).0
+LIBXMLPP_VERSION_MAJOR = 5.0
+LIBXMLPP_VERSION = $(LIBXMLPP_VERSION_MAJOR).1
 LIBXMLPP_LICENSE = LGPL-2.1 (library), LGPL-2.0+ (examples)
 LIBXMLPP_LICENSE_FILES = COPYING
 LIBXMLPP_SOURCE = libxml++-$(LIBXMLPP_VERSION).tar.xz
 LIBXMLPP_SITE = http://ftp.gnome.org/pub/GNOME/sources/libxml++/$(LIBXMLPP_VERSION_MAJOR)
 LIBXMLPP_INSTALL_STAGING = YES
 LIBXMLPP_DEPENDENCIES = libxml2 glibmm host-pkgconf
+LIBXMLPP_CONF_OPTS = \
+	-Dbuild-examples=false \
+	-Dbuild-tests=false \
+	-Dvalidation=false
 
-$(eval $(autotools-package))
+$(eval $(meson-package))
