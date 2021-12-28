d676c0e494ff75f00402f8bbd4afa90d27cea89b
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-12-28 11:21:27 +0100

    package/libsigc: bump to version 3.0.7
    Change license to LGPL-3.0+:
    https://github.com/libsigcplusplus/libsigcplusplus/commit/8a231956bc0a8d9c5d0db59a3eaf4311ee36275f
    
    https://github.com/libsigcplusplus/libsigcplusplus/blob/3.0.7/NEWS
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libsigc/libsigc.mk
+++ package/libsigc/libsigc.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-LIBSIGC_VERSION_MAJOR = 2.10
-LIBSIGC_VERSION = $(LIBSIGC_VERSION_MAJOR).6
+LIBSIGC_VERSION_MAJOR = 3.0
+LIBSIGC_VERSION = $(LIBSIGC_VERSION_MAJOR).7
 LIBSIGC_SOURCE = libsigc++-$(LIBSIGC_VERSION).tar.xz
 LIBSIGC_SITE = http://ftp.gnome.org/pub/GNOME/sources/libsigc++/$(LIBSIGC_VERSION_MAJOR)
 LIBSIGC_INSTALL_STAGING = YES
-LIBSIGC_LICENSE = LGPL-2.1+
+LIBSIGC_LICENSE = LGPL-3.0+
 LIBSIGC_LICENSE_FILES = COPYING
 LIBSIGC_CONF_OPTS = \
 	-Dbuild-examples=false \
