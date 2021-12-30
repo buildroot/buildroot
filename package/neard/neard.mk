eae438fccf32722c6d38b859fdd76739b35e2cb2
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-12-30 17:53:51 +0100

    package/neard: fix autoreconf
    Fix the following build failure raised since bump to version 0.18 in
    commit 81d14a86258592b66b3cb170cda554a1eabf2c3f:
    
    aclocal: error: couldn't open directory 'm4': No such file or directory
    
    Fixes:
     - http://autobuild.buildroot.org/results/f8e26416e38c83e27e7945343f497a6c9310bfcf
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/neard/neard.mk
+++ package/neard/neard.mk
@@ -15,6 +15,14 @@ NEARD_AUTORECONF = YES
 NEARD_AUTORECONF_OPTS = --include=$(HOST_DIR)/share/autoconf-archive
 NEARD_CONF_OPTS = --disable-traces
 
+# Autoreconf is missing the m4/ directory, which might actually be missing
+# iff it was the first argument, but unfortunately we are overriding the
+# first include directory above. Thus we need that hook here.
+define NEARD_CREATE_M4
+	mkdir -p $(@D)/m4
+endef
+NEARD_POST_PATCH_HOOKS += NEARD_CREATE_M4
+
 ifeq ($(BR2_PACKAGE_NEARD_TOOLS),y)
 NEARD_CONF_OPTS += --enable-tools
 endif
