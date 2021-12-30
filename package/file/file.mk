d38b72bcd75392a88b5100839ac410859d0a6a79
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2021-12-31 00:18:36 +0100

    package/file: bump to version 5.41
    - Drop patch (already in version) and so autoreconf
    - Update hash of vasprintf.c (changes not related to license:
      https://github.com/file/file/commit/80816651588efb79a5d054e13703bf2a3378d024
      https://github.com/file/file/commit/43f7989076aa3731f3558c6954780bc7b2734b64)
    
    https://github.com/file/file/blob/FILE5_41/ChangeLog
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/file/file.mk
+++ package/file/file.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FILE_VERSION = 5.38
+FILE_VERSION = 5.41
 FILE_SITE = ftp://ftp.astron.com/pub/file
 FILE_DEPENDENCIES = host-file
 HOST_FILE_DEPENDENCIES = host-zlib
@@ -13,8 +13,6 @@ FILE_INSTALL_STAGING = YES
 FILE_LICENSE = BSD-2-Clause, BSD-4-Clause (one file), BSD-3-Clause (one file)
 FILE_LICENSE_FILES = COPYING src/mygetopt.h src/vasprintf.c
 FILE_CPE_ID_VENDOR = file_project
-# We're patching configure.ac
-FILE_AUTORECONF = YES
 HOST_FILE_CONF_OPTS = --disable-libseccomp
 
 ifeq ($(BR2_PACKAGE_BZIP2),y)
