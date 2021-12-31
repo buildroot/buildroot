27b6400b3fe591345080c06029c2af082d7246df
Romain Naour <romain.naour@gmail.com>
2021-12-31 13:47:26 +0100

    package/flare-engine: enable ogg support in SDL2-mixer via Tremor
    Flare games use OGG audio file format througt SDL2-mixer.
    
    Without OGG support, flare-engine trigger a lot of errors in its
    log and fail to start the game.
    
    ERROR: SoundManager: ItemManager: Loading sound /usr/share/flare/mods/fantasycore/soundfx/inventory/inventory_gem.ogg (soundfx/inventory/inventory_gem.ogg) failed: Unrecognized audio format
    
    Signed-off-by: Romain Naour <romain.naour@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 1f66c3557a391a00a7527e2b2e8fddd83f0ba0aa)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/flare-engine/flare-engine.mk
+++ package/flare-engine/flare-engine.mk
@@ -9,7 +9,7 @@ FLARE_ENGINE_SITE =  $(call github,flareteam,flare-engine,v$(FLARE_ENGINE_VERSIO
 FLARE_ENGINE_LICENSE = GPL-3.0+
 FLARE_ENGINE_LICENSE_FILES = COPYING
 
-FLARE_ENGINE_DEPENDENCIES += sdl2 sdl2_image sdl2_mixer sdl2_ttf
+FLARE_ENGINE_DEPENDENCIES += sdl2 sdl2_image sdl2_mixer sdl2_ttf tremor
 
 # Don't use /usr/games and /usr/share/games
 FLARE_ENGINE_CONF_OPTS += -DBINDIR=bin -DDATADIR=share/flare
