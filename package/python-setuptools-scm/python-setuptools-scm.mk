12e3527fd97df9ae1b60b892c61d10ce291b7a00
James Hilliard <james.hilliard1@gmail.com>
2022-01-01 12:30:17 -0700

    package/python-setuptools-scm: bump to version 6.3.2
    Add new host-python-packaging and host-python-tomli dependencies.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-setuptools-scm/python-setuptools-scm.mk
+++ package/python-setuptools-scm/python-setuptools-scm.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_SETUPTOOLS_SCM_VERSION = 5.0.1
+PYTHON_SETUPTOOLS_SCM_VERSION = 6.3.2
 PYTHON_SETUPTOOLS_SCM_SOURCE = setuptools_scm-$(PYTHON_SETUPTOOLS_SCM_VERSION).tar.gz
-PYTHON_SETUPTOOLS_SCM_SITE = https://files.pythonhosted.org/packages/af/df/f8aa8a78d4d29e0cffa4512e9bc223ed02f24893fe1837c6cee2749ebd67
+PYTHON_SETUPTOOLS_SCM_SITE = https://files.pythonhosted.org/packages/4b/0d/ecb9595fae02467edba5023eb8a23c688d2b438a6a8d1a9e2b8649faf23d
 PYTHON_SETUPTOOLS_SCM_LICENSE = MIT
 PYTHON_SETUPTOOLS_SCM_LICENSE_FILES = LICENSE
 PYTHON_SETUPTOOLS_SCM_SETUP_TYPE = setuptools
+HOST_PYTHON_SETUPTOOLS_SCM_DEPENDENCIES = host-python-packaging host-python-tomli
 
 $(eval $(host-python-package))
