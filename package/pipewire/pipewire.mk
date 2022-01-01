f5131f6f28e384eb9dca1b11f571536e38d3ee9d
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-01-01 15:26:16 +0100

    package/pipewire: fix avahi build
    Fix the following build failure raised since commit
    27908c6eab768d0b9fd3444a929b3f1b1510da71:
    
    ../output-1/build/pipewire-0.3.40/meson.build:348:0: ERROR: Dependency "avahi-client" not found, tried pkgconfig
    
    Fixes:
     - http://autobuild.buildroot.org/results/d1707ad82159aafbddb69c503109bf2e830f0f29
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/pipewire/pipewire.mk
+++ package/pipewire/pipewire.mk
@@ -82,7 +82,8 @@ else
 PIPEWIRE_CONF_OPTS += -Dalsa=disabled -Dpipewire-alsa=disabled
 endif
 
-ifeq ($(BR2_PACKAGE_AVAHI),y)
+# avahi support needs avahi-client, which needs avahi-daemon and dbus
+ifeq ($(BR2_PACKAGE_AVAHI)$(BR2_PACKAGE_AVAHI_DAEMON)$(BR2_PACKAGE_DBUS),yyy)
 PIPEWIRE_CONF_OPTS += -Davahi=enabled
 PIPEWIRE_DEPENDENCIES += avahi
 else
