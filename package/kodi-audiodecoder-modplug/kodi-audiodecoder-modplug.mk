deec3a88876cdb1454cb550ea57d4b3692d54cea
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-01-02 20:10:07 +0100

    package/kodi-audiodecoder-modplug: bump version to 19.0.2-Matrix
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/kodi-audiodecoder-modplug/kodi-audiodecoder-modplug.mk
+++ package/kodi-audiodecoder-modplug/kodi-audiodecoder-modplug.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_AUDIODECODER_MODPLUG_VERSION = 19.0.1-Matrix
+KODI_AUDIODECODER_MODPLUG_VERSION = 19.0.2-Matrix
 KODI_AUDIODECODER_MODPLUG_SITE = $(call github,xbmc,audiodecoder.modplug,$(KODI_AUDIODECODER_MODPLUG_VERSION))
 KODI_AUDIODECODER_MODPLUG_LICENSE = GPL-2.0+
 KODI_AUDIODECODER_MODPLUG_LICENSE_FILES = LICENSE.md
