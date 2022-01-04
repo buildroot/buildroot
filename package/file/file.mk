b07842e20d945649dec1815afffba253c7932211
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-01-04 21:54:55 +0100

    package/file: fix build with gcc 4.8
    Fix the following build failure raised since bump to version 5.41 in
    commit d38b72bcd75392a88b5100839ac410859d0a6a79:
    
    readelf.c: In function 'do_auxv_note':
    readelf.c:1046:2: error: 'for' loop initial declarations are only allowed in C99 mode
      for (size_t off = 0; off + elsize <= descsz; off += elsize) {
      ^
    
    Fixes:
     - http://autobuild.buildroot.org/results/31cbc313fceb84c0cbb1969fca5ac44244871dbc
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/file/file.mk
+++ package/file/file.mk
@@ -9,6 +9,7 @@ FILE_SITE = ftp://ftp.astron.com/pub/file
 FILE_DEPENDENCIES = host-file
 HOST_FILE_DEPENDENCIES = host-zlib
 FILE_CONF_ENV = ac_cv_prog_cc_c99='-std=gnu99'
+HOST_FILE_CONF_ENV = ac_cv_prog_cc_c99='-std=gnu99'
 FILE_INSTALL_STAGING = YES
 FILE_LICENSE = BSD-2-Clause, BSD-4-Clause (one file), BSD-3-Clause (one file)
 FILE_LICENSE_FILES = COPYING src/mygetopt.h src/vasprintf.c
