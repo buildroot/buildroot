5fdd3c3e7ce3c0468133e4e644ada78701518d64
charles <charles@ccxtechnologies.com>
2022-01-05 08:48:07 -0500

    update ca-certificates so they include the new LetsEncrypt CA chain, that isn't expired

--- package/python-cryptography/python-cryptography.mk
+++ package/python-cryptography/python-cryptography.mk
@@ -4,7 +4,6 @@
 #
 ################################################################################
 
-# Please keep in sync with package/python3-cryptography/python3-cryptography.mk
 PYTHON_CRYPTOGRAPHY_VERSION = 3.3.2
 PYTHON_CRYPTOGRAPHY_SOURCE = cryptography-$(PYTHON_CRYPTOGRAPHY_VERSION).tar.gz
 PYTHON_CRYPTOGRAPHY_SITE = https://files.pythonhosted.org/packages/d4/85/38715448253404186029c575d559879912eb8a1c5d16ad9f25d35f7c4f4c
@@ -14,5 +13,11 @@ PYTHON_CRYPTOGRAPHY_LICENSE_FILES = LICENSE LICENSE.APACHE LICENSE.BSD
 PYTHON_CRYPTOGRAPHY_CPE_ID_VENDOR = cryptography_project
 PYTHON_CRYPTOGRAPHY_CPE_ID_PRODUCT = cryptography
 PYTHON_CRYPTOGRAPHY_DEPENDENCIES = host-python-cffi openssl
+HOST_PYTHON_CRYPTOGRAPHY_NEEDS_HOST_PYTHON = python3
+HOST_PYTHON_CRYPTOGRAPHY_DEPENDENCIES = \
+	host-python3-cffi \
+	host-python3-six \
+	host-openssl
 
 $(eval $(python-package))
+$(eval $(host-python-package))
