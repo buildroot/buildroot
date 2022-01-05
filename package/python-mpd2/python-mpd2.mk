c9647e423db4abcba315d37e2fe0caae32238ce2
Uladzimir Bely <wiselord1983@gmail.com>
2022-01-06 02:15:43 +0300

    package/python-mpd2: new package
    A client interface for the Music Player Daemon.
    
    [Peter: license is LGPL-3.0+, add DEVELOPERS entry]
    Signed-off-by: Uladzimir Bely <wiselord1983@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/python-mpd2/python-mpd2.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# python-mpd2
+#
+################################################################################
+
+PYTHON_MPD2_VERSION = 3.0.4
+PYTHON_MPD2_SITE = https://files.pythonhosted.org/packages/ad/92/bec26455a3a6d6146bcf2a974f5c3509d451306413b386fc12b037d9e3b0
+PYTHON_MPD2_SETUP_TYPE = setuptools
+PYTHON_MPD2_LICENSE = LGPL-3.0+
+PYTHON_MPD2_LICENSE_FILES = LICENSE.txt
+
+$(eval $(python-package))
