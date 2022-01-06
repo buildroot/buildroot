2da7b07a22fc2c610f670c153e987d35d966022b
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2022-01-06 21:59:50 +0100

    package/pkg-download.mk: add <pkg>_DOWNLOAD_POST_PROCESS variable
    This will allow packages to register that a download post-processing
    is needed. Note that this variable is intentionally not documented: it
    is an internal variable meant to be set by package infrastructures,
    not directly by packages.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/pkg-download.mk
+++ package/pkg-download.mk
@@ -118,6 +118,7 @@ define DOWNLOAD
 		-n '$($(2)_BASENAME_RAW)' \
 		-N '$($(2)_RAWNAME)' \
 		-o '$($(2)_DL_DIR)/$(notdir $(1))' \
+		$(if $($(2)_DOWNLOAD_POST_PROCESS),-p '$($(2)_DOWNLOAD_POST_PROCESS)') \
 		$(if $($(2)_GIT_SUBMODULES),-r) \
 		$(if $($(2)_GIT_LFS),-l) \
 		$(foreach uri,$(call DOWNLOAD_URIS,$(1),$(2)),-u $(uri)) \
