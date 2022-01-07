b7244efa8284627412a0385d9f07c2110717ee2c
Colin Foster <colin.foster@in-advantage.com>
2022-01-07 14:26:44 -0800

    package/mstpd: bump version to 0.1.0
    Shortly after MSTPd was added, version 0.1.0 was released. Update to the
    latest version.
    
    Signed-off-by: Colin Foster <colin.foster@in-advantage.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/mstpd/mstpd.mk
+++ package/mstpd/mstpd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MSTPD_VERSION = 0.0.9
+MSTPD_VERSION = 0.1.0
 MSTPD_SITE = $(call github,mstpd,mstpd,$(MSTPD_VERSION))
 MSTPD_AUTORECONF = YES
 MSTPD_LICENSE = GPL-2.0, RSA Data Security (md5)
