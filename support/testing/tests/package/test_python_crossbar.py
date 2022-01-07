e5ead094e18cd4a5a9a22b69a42e687f43ee8a8b
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2022-01-07 10:18:01 +0100

    support/testing/tests: switch python-cryptography related tests to glibc
    In a follow-up commit, we are about to bump python-cryptography to a
    new version, which has the interesting charateristic of using Rust
    code. This means python-cryptography will now only be available on
    platforms supported by Rust, which for now excludes uclibc-based
    configurations (none of the Rust Tier1/Tier2 platforms use uClibc,
    there is some uClibc support in Tier3 platforms but they have not been
    added to Buildroot for now).
    
    So in preparation for this bump, we switch the few test cases of
    Python packages that directly or indirectly use python-cryptography to
    use a glibc toolchain. Another impacted test case is the
    docker-compose test case, but it already uses a glibc toolchain;
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- support/testing/tests/package/test_python_crossbar.py
+++ support/testing/tests/package/test_python_crossbar.py
@@ -3,10 +3,18 @@ from tests.package.test_python import TestPythonPackageBase
 
 class TestPythonPy3Crossbar(TestPythonPackageBase):
     __test__ = True
-    config = TestPythonPackageBase.config + \
+    # Need to use a different toolchain than the default due to
+    # python-cryptography using Rust (not available with uclibc)
+    config = \
         """
+        BR2_arm=y
+        BR2_TOOLCHAIN_EXTERNAL=y
+        BR2_TOOLCHAIN_EXTERNAL_BOOTLIN=y
+        BR2_TOOLCHAIN_EXTERNAL_BOOTLIN_ARMV5_EABI_GLIBC_STABLE=y
         BR2_PACKAGE_PYTHON3=y
         BR2_PACKAGE_PYTHON_CROSSBAR=y
+        BR2_TARGET_ROOTFS_CPIO=y
+        # BR2_TARGET_ROOTFS_TAR is not set
         """
     sample_scripts = ["tests/package/sample_python_crossbar.py"]
     timeout = 60
