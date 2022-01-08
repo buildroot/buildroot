cc232de771baf51f17f59264246d722ff241e125
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-01-08 23:38:28 +0100

    package/libmd: new package
    This library provides message digest functions found on BSD systems
    either on their libc (NetBSD, OpenBSD) or libmd (FreeBSD, DragonflyBSD,
    macOS, Solaris) libraries and lacking on others like GNU systems.
    
    https://www.hadrons.org/software/libmd/
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

---
+++ package/libmd/libmd.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# libmd
+#
+################################################################################
+
+LIBMD_VERSION = 1.0.4
+LIBMD_SOURCE = libmd-$(LIBMD_VERSION).tar.xz
+LIBMD_SITE = https://archive.hadrons.org/software/libmd
+LIBMD_LICENSE = BSD-2-Clause, BSD-3-Clause, Beerware, ISC, Public Domain
+LIBMD_LICENSE_FILES = COPYING
+LIBMD_INSTALL_STAGING = YES
+
+$(eval $(autotools-package))
