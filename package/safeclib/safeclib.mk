3fac3b2b88574e605d713ac917024ba39d444563
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-01-08 22:07:52 +0100

    package/safeclib: fix build without stack-protector
    Fix the following build failure without stack-protector raised since
    bump to version 02092020 in commit
    ebe808ac4071395b4d1e90d617c33a5e6e514af1 and
    https://github.com/rurban/safeclib/commit/caa4408eb4a5c767f0474258af3b6ccf444e10aa:
    
    /home/buildroot/autobuild/instance-3/output-1/host/opt/ext-toolchain/m68k-buildroot-uclinux-uclibc/bin/ld.real: ../src/.libs/libsafec-3.6.0.a(safe_mem_constraint.o): in function `handle_mem_bos_chk_warn':
    safe_mem_constraint.c:(.text+0x40): undefined reference to `__stack_chk_guard'
    
    If stack-protector is enabled in the Buildroot config, the toolchain
    wrapper will make sure it is used, so there's no need for the safeclib
    configure to handle it.
    
    Fixes:
     - http://autobuild.buildroot.org/results/a481ee2d26a094358b0298617cce691be3077f22
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    [Peter: add comment explaining why this is done]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/safeclib/safeclib.mk
+++ package/safeclib/safeclib.mk
@@ -11,5 +11,10 @@ SAFECLIB_SOURCE = libsafec-$(SAFECLIB_VERSION).tar.xz
 SAFECLIB_LICENSE = MIT
 SAFECLIB_LICENSE_FILES = COPYING
 SAFECLIB_INSTALL_STAGING = YES
+# We're patching configure.ac
+SAFECLIB_AUTORECONF = YES
+# -fstack-protector-strong is used by default. Disable that so the
+# BR2_SSP_* options in the toolchain wrapper are used instead
+SAFECLIB_CONF_OPTS = --disable-hardening
 
 $(eval $(autotools-package))
