860e9b3f553511d772573427ebaa6371e205aa9c
James Hilliard <james.hilliard1@gmail.com>
2022-01-08 20:55:59 -0700

    package/python-pep517: new package
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

---
+++ package/python-pep517/python-pep517.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# python-pep517
+#
+################################################################################
+
+PYTHON_PEP517_VERSION = 0.12.0
+PYTHON_PEP517_SOURCE = pep517-$(PYTHON_PEP517_VERSION).tar.gz
+PYTHON_PEP517_SITE = https://files.pythonhosted.org/packages/0a/65/6e656d49c679136edfba25f25791f45ffe1ea4ae2ec1c59fe9c35e061cd1
+PYTHON_PEP517_LICENSE = MIT
+PYTHON_PEP517_LICENSE_FILES = LICENSE
+PYTHON_PEP517_SETUP_TYPE = distutils
+HOST_PYTHON_PEP517_NEEDS_HOST_PYTHON = python3
+HOST_PYTHON_PEP517_DEPENDENCIES = host-python-tomli
+
+$(eval $(host-python-package))
