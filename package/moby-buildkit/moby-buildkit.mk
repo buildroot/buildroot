4e33104c26139f7a6cfc14d5a1b71ac552630e14
Christian Stewart <christian@paral.in>
2022-01-09 22:06:07 +0100

    package/moby-buildkit: new package
    Signed-off-by: Christian Stewart <christian@paral.in>
    [Romain:
      - remove host-pkgconf dependency
      - Add the license file hash
      - Fix the check-package warning in Config.in]
    Signed-off-by: Romain Naour <romain.naour@smile.fr>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

---
+++ package/moby-buildkit/moby-buildkit.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# moby-buildkit
+#
+################################################################################
+
+MOBY_BUILDKIT_VERSION = 0.8.1
+MOBY_BUILDKIT_SITE = $(call github,moby,buildkit,v$(MOBY_BUILDKIT_VERSION))
+MOBY_BUILDKIT_LICENSE = Apache-2.0
+MOBY_BUILDKIT_LICENSE_FILES = LICENSE
+
+MOBY_BUILDKIT_GOMOD = github.com/moby/buildkit
+
+MOBY_BUILDKIT_TAGS = cgo
+MOBY_BUILDKIT_BUILD_TARGETS = cmd/buildctl cmd/buildkitd
+
+MOBY_BUILDKIT_LDFLAGS = \
+	-X $(MOBY_BUILDKIT_GOMOD)/version.Version="$(MOBY_BUILDKIT_VERSION)"
+
+MOBY_BUILDKIT_INSTALL_BINS = $(notdir $(MOBY_BUILDKIT_BUILD_TARGETS))
+
+$(eval $(golang-package))
