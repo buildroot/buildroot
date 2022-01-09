4043bc0c9c55e1bcac58677c361cd1b8724716b8
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-01-09 23:16:32 +0100

    package/hiawatha: mbedtls is mandatory, not optional
    hiawatha is unconditionally using mbedtls since bump to version 11.1 in
    commit 24e4c888c3d36e3943206ea55894b84344cc8b2e (e.g. wigwam.c includes
    "mbedtls/md5.h")
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/hiawatha/hiawatha.mk
+++ package/hiawatha/hiawatha.mk
@@ -11,7 +11,10 @@ HIAWATHA_LICENSE = GPL-2.0
 HIAWATHA_LICENSE_FILES = LICENSE
 HIAWATHA_CPE_ID_VENDOR = hiawatha-webserver
 
+# Disable system mbedtls as hiawatha needs mbedtls 3.x
 HIAWATHA_CONF_OPTS = \
+	-DINSTALL_MBEDTLS_HEADERS=OFF \
+	-DUSE_SYSTEM_MBEDTLS=OFF \
 	-DENABLE_TOOLKIT=OFF \
 	-DCONFIG_DIR=/etc/hiawatha \
 	-DLOG_DIR=/var/log \
@@ -20,11 +23,7 @@ HIAWATHA_CONF_OPTS = \
 	-DWORK_DIR=/var/lib/hiawatha
 
 ifeq ($(BR2_PACKAGE_HIAWATHA_SSL),y)
-# Disable system mbedtls as hiawatha needs mbedtls 3.x
-HIAWATHA_CONF_OPTS += \
-	-DENABLE_TLS=ON \
-	-DINSTALL_MBEDTLS_HEADERS=OFF \
-	-DUSE_SYSTEM_MBEDTLS=OFF
+HIAWATHA_CONF_OPTS += -DENABLE_TLS=ON
 else
 HIAWATHA_CONF_OPTS += -DENABLE_TLS=OFF
 endif
