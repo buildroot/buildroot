4317b1f9f21eda6d01c10da1ed15a2d87cfc666c
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-01-11 23:41:56 +0100

    package/snappy: bump to version 1.1.9
    - Disable benchmarks through option added by
      https://github.com/google/snappy/commit/4ebd8b2f23dd4ab67ca695256a128cd796d39ce6
    - Update indentation in hash file (two spaces)
    
    https://github.com/google/snappy/releases/tag/1.1.9
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/snappy/snappy.mk
+++ package/snappy/snappy.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-SNAPPY_VERSION = 1.1.8
+SNAPPY_VERSION = 1.1.9
 SNAPPY_SITE = $(call github,google,snappy,$(SNAPPY_VERSION))
 SNAPPY_LICENSE = BSD-3-Clause
 SNAPPY_LICENSE_FILES = COPYING
 SNAPPY_CPE_ID_VENDOR = google
 SNAPPY_INSTALL_STAGING = YES
-SNAPPY_CONF_OPTS = -DSNAPPY_BUILD_TESTS=OFF
+SNAPPY_CONF_OPTS = -DSNAPPY_BUILD_BENCHMARKS=OFF -DSNAPPY_BUILD_TESTS=OFF
 
 $(eval $(cmake-package))
