f8a5f4ecd89a2bd84ab0a7a72f3c367a0980af4f
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-01-12 20:17:08 +0100

    package/kodi-pvr-mythtv: bump version to 19.0.6-Matrix
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/kodi-pvr-mythtv/kodi-pvr-mythtv.mk
+++ package/kodi-pvr-mythtv/kodi-pvr-mythtv.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PVR_MYTHTV_VERSION = 19.0.3-Matrix
+KODI_PVR_MYTHTV_VERSION = 19.0.6-Matrix
 KODI_PVR_MYTHTV_SITE = $(call github,janbar,pvr.mythtv,$(KODI_PVR_MYTHTV_VERSION))
 KODI_PVR_MYTHTV_LICENSE = GPL-2.0+
 KODI_PVR_MYTHTV_LICENSE_FILES = LICENSE.md
