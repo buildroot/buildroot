0d0f84d2004b04bc93d5ceb58c86640f190e5d78
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-01-12 18:21:00 +0100

    package/riscv-isa-sim: needs host-dtc
    Fix the following build failure raised since the addition of the package
    in commit ccfc90e1010e42e6529afae3a5ea8bf7226dabc1:
    
    configure: error: device-tree-compiler not found
    
    Fixes:
     - http://autobuild.buildroot.org/results/d35ff4a0ca0a8463121d717a9727e0d863ca83e8
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/riscv-isa-sim/riscv-isa-sim.mk
+++ package/riscv-isa-sim/riscv-isa-sim.mk
@@ -8,5 +8,6 @@ RISCV_ISA_SIM_VERSION = 1.1.0
 RISCV_ISA_SIM_SITE = $(call github,riscv-software-src,riscv-isa-sim,v$(RISCV_ISA_SIM_VERSION))
 RISCV_ISA_SIM_LICENSE = BSD-3-Clause
 RISCV_ISA_SIM_LICENSE_FILES = LICENSE
+HOST_RISCV_ISA_SIM_DEPENDENCIES = host-dtc
 
 $(eval $(host-autotools-package))
