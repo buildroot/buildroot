dc819438f61c693ffe01cd6827f23372570cb7c6
Julien Olivain <ju.o@free.fr>
2022-01-12 21:26:40 +0100

    board/spike/riscv64/start.sh: add buildroot host dir in PATH
    commit 0d0f84d2004b04bc93d5ceb58c86640f190e5d78
    added the missing host-dtc dependency to riscv-isa-sim.
    
    The spike simulator calls the dtc binary at its startup. The host dtc
    command needs to be in the PATH at that time.
    
    This commit add the buildroot host binary directory into the PATH of the
    start.sh helper script. It make sure spike will use the buildroot dtc
    version. This commit fixes this start.sh script on host not providing
    the dtc command. Since the "spike" binary is now in the PATH, the
    binary name is also simplified.
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- board/spike/riscv64/start.sh
+++ board/spike/riscv64/start.sh
@@ -3,9 +3,13 @@
 SCRIPT_DIR="$(dirname "$0")"
 BR_BASEDIR="$(readlink -e "${SCRIPT_DIR}/../../..")"
 
+# spike uses dtc at runtime startup, so make sure buildroot host
+# directory is in the PATH
+export PATH="${BR_BASEDIR}/output/host/usr/bin:$PATH"
+
 # Use Buildroot host spike by default, but allow the caller to
 # redefine another spike binary
-: "${SPIKE:=${BR_BASEDIR}/output/host/usr/bin/spike}"
+: "${SPIKE:=spike}"
 
 exec "${SPIKE}" \
     --initrd "${BR_BASEDIR}"/output/images/rootfs.cpio \
