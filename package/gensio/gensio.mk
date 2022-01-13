0b420b8e2133bcb06808e9c39c20996f6b33671e
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-01-14 00:29:03 +0100

    package/gensio: add avahi optional dependency
    avahi-client is an optional dependency which is enabled by default since
    version 2.2.0 and
    https://github.com/cminyard/gensio/commit/5ab117c9743fd4719f610cf9f1ba45023cc25d7c
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    (cherry picked from commit f0a1d47f6fe6bda3b18b3fcfca37757b37695dbb)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gensio/gensio.mk
+++ package/gensio/gensio.mk
@@ -9,6 +9,7 @@ GENSIO_SITE = http://downloads.sourceforge.net/project/ser2net/ser2net
 GENSIO_LICENSE = LGPL-2.1+ (library), GPL-2.0+ (tools)
 GENSIO_LICENSE_FILES = COPYING.LIB COPYING
 GENSIO_INSTALL_STAGING = YES
+GENSIO_DEPENDENCIES = $(if $(BR2_PACKAGE_AVAHI_LIBAVAHI_CLIENT),avahi)
 GENSIO_CONF_OPTS = \
 	--without-openipmi \
 	--without-swig \
