a10835fac773e68eef18d48cb89d76f9f553c33a
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-01-13 19:07:38 +0100

    package/riscv-isa-sim: needs host-boost
    Fix the following build failure raised since the addition of the package
    in commit ea033cecf990ed3f86619647523a9ad2ccb09adb:
    
    configure: error: Could not link against boost_system !
    
    Fixes:
     - http://autobuild.buildroot.org/results/d1ec407d3a9106be1271e4897d6f578d501439fe
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/riscv-isa-sim/riscv-isa-sim.mk
+++ package/riscv-isa-sim/riscv-isa-sim.mk
@@ -8,6 +8,6 @@ RISCV_ISA_SIM_VERSION = 1.1.0
 RISCV_ISA_SIM_SITE = $(call github,riscv-software-src,riscv-isa-sim,v$(RISCV_ISA_SIM_VERSION))
 RISCV_ISA_SIM_LICENSE = BSD-3-Clause
 RISCV_ISA_SIM_LICENSE_FILES = LICENSE
-HOST_RISCV_ISA_SIM_DEPENDENCIES = host-dtc
+HOST_RISCV_ISA_SIM_DEPENDENCIES = host-boost host-dtc
 
 $(eval $(host-autotools-package))
