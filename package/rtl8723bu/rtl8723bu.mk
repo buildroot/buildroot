6306bed7f5bffd16ca3627671ae3ddf1d850e3e3
Doug Brown <doug@schmorgal.com>
2022-01-13 21:07:33 -0800

    package/rtl8723bu: bump to version 19b4bdc05483a1e79a4f955a80f8f72cdd5dedc3
    This version fixes a bug that prevents the user from switching from one
    access point to another.
    
    Signed-off-by: Doug Brown <doug@schmorgal.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    (cherry picked from commit 89211450c68df76f3b850c651733c86fb02b40d6)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rtl8723bu/rtl8723bu.mk
+++ package/rtl8723bu/rtl8723bu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTL8723BU_VERSION = 9ce1c38439f4f574bb7adaf33949835c25536a28
+RTL8723BU_VERSION = 19b4bdc05483a1e79a4f955a80f8f72cdd5dedc3
 RTL8723BU_SITE = $(call github,lwfinger,rtl8723bu,$(RTL8723BU_VERSION))
 RTL8723BU_LICENSE = GPL-2.0, proprietary (*.bin firmware blobs)
 
