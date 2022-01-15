8ae840ba193a90f4cb950526646b831294477454
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-01-15 17:04:49 +0100

    package/riscv-isa-sim: set --with-boost
    Set --with-boost to avoid the following build failure when a non working
    boost version is installed on host. This failure is raised since the
    addition of the package in commit
    ea033cecf990ed3f86619647523a9ad2ccb09adb:
    
    configure:5559: error: Could not find a version of the Boost::Asio library!
    
    Fixes:
     - http://autobuild.buildroot.org/results/855b66b86d979cc922b947da932b0e2388481263
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/riscv-isa-sim/riscv-isa-sim.mk
+++ package/riscv-isa-sim/riscv-isa-sim.mk
@@ -9,5 +9,6 @@ RISCV_ISA_SIM_SITE = $(call github,riscv-software-src,riscv-isa-sim,v$(RISCV_ISA
 RISCV_ISA_SIM_LICENSE = BSD-3-Clause
 RISCV_ISA_SIM_LICENSE_FILES = LICENSE
 HOST_RISCV_ISA_SIM_DEPENDENCIES = host-boost host-dtc
+HOST_RISCV_ISA_SIM_CONF_OPTS = --with-boost=$(HOST_DIR)
 
 $(eval $(host-autotools-package))
