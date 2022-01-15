a17d4aba54e62359cba638809fde5752dd40cf59
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-01-15 12:42:44 +0100

    package/python-django: security bump to version 3.2.11
    Fixes:
     - CVE-2021-45115: Denial-of-service possibility in
       UserAttributeSimilarityValidator
     - CVE-2021-45116: Potential information disclosure in dictsort template
       filter
     - CVE-2021-45452: Potential directory-traversal via Storage.save()
    
    https://www.djangoproject.com/weblog/2022/jan/04/security-releases
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 3.2.10
+PYTHON_DJANGO_VERSION = 3.2.11
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/a5/8e/c6dfc718d572e4b33b56824b9e71e5ab9be8072e6747fc6184d206c3fdb3
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/20/86/e4348aac45bc83fc8e9dda2cfd81004b007c65b68c1499a4233acabdaa3b
 
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
