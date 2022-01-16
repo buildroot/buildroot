a800c82206b287e147de5560d146a32f743dd1aa
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-01-16 22:08:16 +0100

    package/expat: security bump to version 2.4.3
    Fix CVE-2021-45960, CVE-2021-46143 and CVE-2022-22822 to CVE-2022-22827
    
    https://blog.hartwork.org/posts/expat-2-4-3-released
    https://github.com/libexpat/libexpat/blob/R_2_4_3/expat/Changes
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 829bf508bba7df9aee3f86bcd70394ee487b19a6)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/expat/expat.mk
+++ package/expat/expat.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-EXPAT_VERSION = 2.4.1
+EXPAT_VERSION = 2.4.3
 EXPAT_SITE = http://downloads.sourceforge.net/project/expat/expat/$(EXPAT_VERSION)
 EXPAT_SOURCE = expat-$(EXPAT_VERSION).tar.xz
 EXPAT_INSTALL_STAGING = YES
