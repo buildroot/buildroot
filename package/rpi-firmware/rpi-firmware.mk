9958744a0cddfd01e8359ab560e04e9a8cdd5578
Peter Seiderer <ps.report@gmx.net>
2022-01-18 22:14:30 +0100

    package/rpi-firmware: bump version to 3f20b83
    Changelog:
    
      095deea759 kernel: overlays: vc4-kms-v3d: Change composite handling
      2e9b609e53c kernel: Bump to 5.10.79
      12bc6e36773 firmware: dtoverlay: Rebase aliases in overlays like labels
      24d966d35bb kernel: Bump to 5.10.81
      ca52e7c1303 kernel: edt-ft5x06 touchscreen fixes
      1a0297bfbf7 kernel: Bump to 5.10.82
      0a1a871d04c kernel: Fix for IN Token of USB Endpoint 1 stopping occasionally on Pi 4
      303d0012cd7 kernel: Bump to 5.10.83
      bd8b6d5dc8e kernel: Bump to 5.10.85
      56f1b8bbc88 kernel: Bump to 5.10.87
      536baf33ae9 kernel: Bump to 5.10.88
      ce71e3e9b34 kernel: Bump to 5.10.89
      19b570d0d6a firmware: arm-loader: Fix kernel8.img selection on 2837 with arm_64bit=1
      fbbde90b709 kernel: drm/vc4: fix non-working audio on RPi3 and no video with DVI
      165bd7bc562 firmware: improve firmware camera detection
      318e3373496 kernel: Bump to 5.10.90
      175984a6dc4 kernel: drm/vc4: Add alpha_blend_mode property to each plane
      20c5829b0af kernel: Bump to 5.10.92
      3f20b832b27 firmware: arm_loader: Only clip min/max to the same value for turbo clocks
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = fa9a00624e7d5d3dcdb297dff132dc32cc2d9a25
+RPI_FIRMWARE_VERSION = 3f20b832b27cd730deb6419b570f31a98167eef6
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
