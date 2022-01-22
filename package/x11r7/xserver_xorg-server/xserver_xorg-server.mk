ff1cd8a27540f677dcf6106f3958701922fdb40e
Peter Korsgaard <peter@korsgaard.com>
2022-01-22 17:50:12 +0100

    package/xserver_xorg-server: security bump to version 1.20.14
    Fixes the following vulnerabilities:
    
    * CVE-2021-4008/ZDI-CAN-14192 SProcRenderCompositeGlyphs out-of-bounds
      access
    
      The handler for the CompositeGlyphs request of the Render extension does
      not properly validate the request length leading to out of bounds memory
      write.
    
    * CVE-2021-4009/ZDI-CAN 14950 SProcXFixesCreatePointerBarrier out-of-bounds
      access
    
      The handler for the CreatePointerBarrier request of the XFixes extension
      does not properly validate the request length leading to out of bounds
      memory write.
    
    * CVE-2021-4010/ZDI-CAN-14951 SProcScreenSaverSuspend out-of-bounds access
    
      The handler for the Suspend request of the Screen Saver extension does not
      properly validate the request length leading to out of bounds memory
      write.
    
    * CVE-2021-4011/ZDI-CAN-14952 SwapCreateRegister out-of-bounds access
    
      The handlers for the RecordCreateContext and RecordRegisterClients
      requests of the Record extension do not properly validate the request
      length leading to out of bounds memory write.
    
    For details, see the advisory:
    https://lists.x.org/archives/xorg-announce/2021-December/003122.html
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xserver_xorg-server/xserver_xorg-server.mk
+++ package/x11r7/xserver_xorg-server/xserver_xorg-server.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XSERVER_XORG_SERVER_VERSION = 1.20.13
+XSERVER_XORG_SERVER_VERSION = 1.20.14
 XSERVER_XORG_SERVER_SOURCE = xorg-server-$(XSERVER_XORG_SERVER_VERSION).tar.xz
 XSERVER_XORG_SERVER_SITE = https://xorg.freedesktop.org/archive/individual/xserver
 XSERVER_XORG_SERVER_LICENSE = MIT
