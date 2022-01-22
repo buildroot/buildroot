a394c17b79fbdd9d7f441e858e280c2cf225bfd0
Sergey Bobrenok <bobrofon@gmail.com>
2022-01-22 15:41:11 +0300

    package/sdbus-cpp: bump to version 1.1.0
    Signed-off-by: Sergey Bobrenok <bobrofon@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/sdbus-cpp/sdbus-cpp.mk
+++ package/sdbus-cpp/sdbus-cpp.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-SDBUS_CPP_VERSION = 0.8.3
+SDBUS_CPP_VERSION = 1.1.0
 SDBUS_CPP_SOURCE = v$(SDBUS_CPP_VERSION).tar.gz
 SDBUS_CPP_SITE = $(call github,Kistler-Group,sdbus-cpp,v$(SDBUS_CPP_VERSION))
 SDBUS_CPP_INSTALL_STAGING = YES
 SDBUS_CPP_DEPENDENCIES = host-pkgconf systemd
-SDBUS_CPP_LICENSE = LGPL-2.1+
-SDBUS_CPP_LICENSE_FILES = COPYING
+SDBUS_CPP_LICENSE = LGPL-2.1+ with exception (headers)
+SDBUS_CPP_LICENSE_FILES = COPYING COPYING-LGPL-Exception
 
 $(eval $(cmake-package))
