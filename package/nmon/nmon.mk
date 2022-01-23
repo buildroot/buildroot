6a42e347222a2d04947d2e6985ea241ce593a0ad
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-01-23 12:11:26 +0100

    package/nmon: bump to version 16n
    Update indentation in hash file (two spaces)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/nmon/nmon.mk
+++ package/nmon/nmon.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NMON_VERSION = 16m
+NMON_VERSION = 16n
 NMON_SITE = https://sourceforge.net/projects/nmon/files
 NMON_SOURCE = lmon$(NMON_VERSION).c
 NMON_LICENSE = GPL-3.0+
