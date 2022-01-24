9c2a2aa1faeff4d3396b7ba402b54e1d874b679f
James Hilliard <james.hilliard1@gmail.com>
2022-01-24 04:00:14 -0700

    package/python{,3}-pycparser: bump to version 2.21
    License hash changed due to year update:
    https://github.com/eliben/pycparser/commit/78beeb7d6f0a760f90a8592d9533042363a82810
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python3-pycparser/python3-pycparser.mk
+++ package/python3-pycparser/python3-pycparser.mk
@@ -5,9 +5,9 @@
 ################################################################################
 
 # Please keep in sync with package/python-pycparser/python-pycparser.mk
-PYTHON3_PYCPARSER_VERSION = 2.20
+PYTHON3_PYCPARSER_VERSION = 2.21
 PYTHON3_PYCPARSER_SOURCE = pycparser-$(PYTHON3_PYCPARSER_VERSION).tar.gz
-PYTHON3_PYCPARSER_SITE = https://files.pythonhosted.org/packages/0f/86/e19659527668d70be91d0369aeaa055b4eb396b0f387a4f92293a20035bd
+PYTHON3_PYCPARSER_SITE = https://files.pythonhosted.org/packages/5e/0b/95d387f5f4433cb0f53ff7ad859bd2c6051051cebbb564f139a999ab46de
 PYTHON3_PYCPARSER_SETUP_TYPE = setuptools
 PYTHON3_PYCPARSER_LICENSE = BSD-3-Clause
 PYTHON3_PYCPARSER_LICENSE_FILES = LICENSE
