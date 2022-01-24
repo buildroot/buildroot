6e7b8f9412ac12225e441acbd5228e97c2e38ca7
Christian Stewart <christian@paral.in>
2022-01-23 23:25:27 -0800

    package/delve: support building as host package
    Users may want to debug Go programs using dlv on the host machine.
    
    Adds package host-delve and BR2_PACKAGE_HOST_DELVE.
    
    Signed-off-by: Christian Stewart <christian@paral.in>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/delve/delve.mk
+++ package/delve/delve.mk
@@ -14,4 +14,9 @@ DELVE_TAGS = cgo
 DELVE_BUILD_TARGETS = cmd/dlv
 DELVE_INSTALL_BINS = $(notdir $(DELVE_BUILD_TARGETS))
 
+HOST_DELVE_TAGS = cgo
+HOST_DELVE_BUILD_TARGETS = cmd/dlv
+HOST_DELVE_INSTALL_BINS = $(notdir $(HOST_DELVE_BUILD_TARGETS))
+
 $(eval $(golang-package))
+$(eval $(host-golang-package))
