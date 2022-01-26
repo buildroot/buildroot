4f9a5659023c66fc166de28fd39a6c97a9490667
Francois Perrad <fperrad@gmail.com>
2022-01-26 15:55:02 +0100

    package/lua: bump to version 5.4.4
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/lua/lua.mk
+++ package/lua/lua.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 ifeq ($(BR2_PACKAGE_LUA_5_4),y)
-LUA_VERSION = 5.4.3
+LUA_VERSION = 5.4.4
 else ifeq ($(BR2_PACKAGE_LUA_5_3),y)
 LUA_VERSION = 5.3.6
 else
