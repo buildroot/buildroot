0d4c8ce8a2f26989e0e2d95b977d60e450c090a9
James Hilliard <james.hilliard1@gmail.com>
2022-01-27 12:23:08 -0700

    package/sord: new package
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Tested-by: Gilles Talis <gilles.talis@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

---
+++ package/sord/sord.mk
@@ -0,0 +1,25 @@
+################################################################################
+#
+# sord
+#
+################################################################################
+
+SORD_VERSION = 0.16.8
+SORD_SITE = https://download.drobilla.net
+SORD_SOURCE = sord-$(SORD_VERSION).tar.bz2
+SORD_LICENSE = ISC
+SORD_LICENSE_FILES = COPYING
+SORD_DEPENDENCIES = host-pkgconf serd
+SORD_INSTALL_STAGING = YES
+
+ifeq ($(BR2_PACKAGE_PCRE),y)
+SORD_DEPENDENCIES += pcre
+endif
+
+SORD_CONF_OPTS += --no-coverage
+
+ifeq ($(BR2_STATIC_LIBS),y)
+SORD_CONF_OPTS += --static --no-shared --static-progs
+endif
+
+$(eval $(waf-package))
