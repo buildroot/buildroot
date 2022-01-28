61797c52f5deef2cb67e60cf075ca243078c8855
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-01-28 23:07:38 +0100

    package/libuhttpd: bump to version 3.14.1
    Drop patch (already in version)
    
    https://github.com/zhaojh329/libuhttpd/releases/tag/v3.14.1
    https://github.com/zhaojh329/libuhttpd/releases/tag/v3.14.0
    https://github.com/zhaojh329/libuhttpd/releases/tag/v3.13.0
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libuhttpd/libuhttpd.mk
+++ package/libuhttpd/libuhttpd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBUHTTPD_VERSION = 3.12.1
+LIBUHTTPD_VERSION = 3.14.1
 LIBUHTTPD_SITE = https://github.com/zhaojh329/libuhttpd/releases/download/v$(LIBUHTTPD_VERSION)
 LIBUHTTPD_LICENSE = MIT
 LIBUHTTPD_LICENSE_FILES = LICENSE
