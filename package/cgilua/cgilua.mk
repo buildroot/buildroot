755116460b8de88cb833ab4bf37649a7fb26728d
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-01-31 23:02:40 +0100

    package/cgilua: add CGILUA_CPE_ID_VENDOR
    cpe:2.3:a:keplerproject:cgilua is a valid CPE identifier for this
    package:
    
      https://nvd.nist.gov/products/cpe/search/results?namingFormat=2.3&keyword=cpe%3A2.3%3Aa%3Akeplerproject%3Acgilua
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/cgilua/cgilua.mk
+++ package/cgilua/cgilua.mk
@@ -8,5 +8,6 @@ CGILUA_VERSION = 6.0.2-0
 CGILUA_SUBDIR = cgilua
 CGILUA_LICENSE = MIT
 CGILUA_LICENSE_FILES = $(CGILUA_SUBDIR)/doc/us/license.html
+CGILUA_CPE_ID_VENDOR = keplerproject
 
 $(eval $(luarocks-package))
