89cf28a8f30cfeb05bfce326019bec8344a7a115
James Hilliard <james.hilliard1@gmail.com>
2022-02-02 13:34:11 -0700

    package/python3-setuptools: bump to version 60.7.0
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-setuptools/python-setuptools.mk
+++ package/python-setuptools/python-setuptools.mk
@@ -9,9 +9,9 @@
 ifeq ($(BR2_PACKAGE_PYTHON3),y)
 # Please keep in sync with
 # package/python3-setuptools/python3-setuptools.mk
-PYTHON_SETUPTOOLS_VERSION = 60.6.0
+PYTHON_SETUPTOOLS_VERSION = 60.7.0
 PYTHON_SETUPTOOLS_SOURCE = setuptools-$(PYTHON3_SETUPTOOLS_VERSION).tar.gz
-PYTHON_SETUPTOOLS_SITE = https://files.pythonhosted.org/packages/8f/89/9fec81ec84737c925a1ee992af2c6c7153aec4051c26afeadd6b822354d2
+PYTHON_SETUPTOOLS_SITE = https://files.pythonhosted.org/packages/29/dd/48d662bb93e5e51e72265ef612f869947d4ae4126328d7156824cd50d440
 else # Python
 PYTHON_SETUPTOOLS_VERSION = 44.0.0
 PYTHON_SETUPTOOLS_SOURCE = setuptools-$(PYTHON_SETUPTOOLS_VERSION).zip
