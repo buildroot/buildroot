02358c962c43b90f272aedd77a43033643056557
James Hilliard <james.hilliard1@gmail.com>
2022-02-01 21:11:15 -0700

    package/python-pyparsing: bump to version 3.0.7
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-pyparsing/python-pyparsing.mk
+++ package/python-pyparsing/python-pyparsing.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYPARSING_VERSION = 3.0.6
+PYTHON_PYPARSING_VERSION = 3.0.7
 PYTHON_PYPARSING_SOURCE = pyparsing-$(PYTHON_PYPARSING_VERSION).tar.gz
-PYTHON_PYPARSING_SITE = https://files.pythonhosted.org/packages/ab/61/1a1613e3dcca483a7aa9d446cb4614e6425eb853b90db131c305bd9674cb
+PYTHON_PYPARSING_SITE = https://files.pythonhosted.org/packages/d6/60/9bed18f43275b34198eb9720d4c1238c68b3755620d20df0afd89424d32b
 PYTHON_PYPARSING_LICENSE = MIT
 PYTHON_PYPARSING_LICENSE_FILES = LICENSE
 PYTHON_PYPARSING_SETUP_TYPE = setuptools
