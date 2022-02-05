9ecf705d8c65f6dfce189bc49e880ff6cd009882
James Hilliard <james.hilliard1@gmail.com>
2022-02-05 13:18:31 -0700

    package/re2: switch to generic-package make build
    The cmake build appears to be missing features, such as pkg-config
    generation support which is needed for some packages to find re2, like
    qt5webengine.
    
    Upstream is not keen on fixing the CMake-based buildsystem [0]
    
    Instead of patching it on our side, and then have to carry the patch
    forever, we switch to the Makefile-based generic buildsystem.
    
    Note that previously some packages like grpc failed when the generic
    Makefile was used, because they would not find the cmake-equivalent
    of pkg-config, but this is now fixed [1].
    
    [0] https://github.com/google/re2/issues/349
    [1] https://github.com/grpc/grpc/commit/45e413d2520795e7281e9a592af81620349bc186
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    [Peter: Reword commit message as suggested by Yann]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/re2/re2.mk
+++ package/re2/re2.mk
@@ -10,8 +10,32 @@ RE2_LICENSE = BSD-3-Clause
 RE2_LICENSE_FILES = LICENSE
 RE2_INSTALL_STAGING = YES
 
-RE2_CONF_OPTS += -DRE2_BUILD_TESTING=OFF
-HOST_RE2_CONF_OPTS += -DRE2_BUILD_TESTING=OFF -DBUILD_SHARED_LIBS=ON
+define RE2_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) \
+		-C $(@D) $(if $(BR2_STATIC_LIBS),static)
+endef
 
-$(eval $(cmake-package))
-$(eval $(host-cmake-package))
+define RE2_INSTALL_STAGING_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) \
+		DESTDIR="$(STAGING_DIR)" prefix=/usr -C $(@D) \
+		$(if $(BR2_STATIC_LIBS),static-install,install)
+endef
+
+define RE2_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) \
+		DESTDIR="$(TARGET_DIR)" prefix=/usr -C $(@D) \
+		$(if $(BR2_STATIC_LIBS),static-install,install)
+endef
+
+define HOST_RE2_BUILD_CMDS
+	$(HOST_MAKE_ENV) $(MAKE) $(HOST_CONFIGURE_OPTS) \
+		-C $(@D)
+endef
+
+define HOST_RE2_INSTALL_CMDS
+	$(HOST_MAKE_ENV) $(MAKE) $(HOST_CONFIGURE_OPTS) \
+		-C $(@D) DESTDIR="$(HOST_DIR)" prefix=/usr install
+endef
+
+$(eval $(generic-package))
+$(eval $(host-generic-package))
