cb2d17921bfb1f34601f1f2fcf27aa89a88a9591
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-02-06 15:41:48 +0100

    package/python-lmdb: bump to version 1.3.0
    Update indentation in hash file (two spaces)
    
    https://github.com/jnwatson/py-lmdb/blob/py-lmdb_1.3.0/ChangeLog
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-lmdb/python-lmdb.mk
+++ package/python-lmdb/python-lmdb.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_LMDB_VERSION = 1.2.1
+PYTHON_LMDB_VERSION = 1.3.0
 PYTHON_LMDB_SOURCE = lmdb-$(PYTHON_LMDB_VERSION).tar.gz
-PYTHON_LMDB_SITE = https://files.pythonhosted.org/packages/2f/df/3aea5279753cb8ab0c96dec43106e24f388d4179d5224f6d3e652016c095
+PYTHON_LMDB_SITE = https://files.pythonhosted.org/packages/ed/61/41f3c7cbd8a67202ef24fad3375ed936093a0547dc645581dd11c09581b7
 PYTHON_LMDB_LICENSE = OLDAP-2.8
 PYTHON_LMDB_LICENSE_FILES = LICENSE
 PYTHON_LMDB_SETUP_TYPE = setuptools
