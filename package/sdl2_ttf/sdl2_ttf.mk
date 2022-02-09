e253e41ce2e50d3af689a40b4dd0f5c615985787
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-02-09 20:13:29 +0100

    package/sdl2_ttf: add harfbuzz optional dependency
    harfbuzz is an optional dependency (which is enabled by default) since
    version 2.0.18 and
    https://github.com/libsdl-org/SDL_ttf/commit/328bbed78db496ff36720e8a809661ffb236e673
    
    If harfbuzz is not disabled and not found, builtin harfbuzz is enabled
    resulting in the following build failure without C++ since commit
    f4da031a77f200515bcc5b2e0fe665a7b4f2a780 and
    https://github.com/libsdl-org/SDL_ttf/commit/9a7ef3fb640a1f086d9b02954c3b4bbfb3578871:
    
    configure: error: *** A compiler with support for C++11 language features is required.
    
    Fixes:
     - http://autobuild.buildroot.org/results/3fecb96a8063b1a28703682e9373714c1c9cfa24
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/sdl2_ttf/sdl2_ttf.mk
+++ package/sdl2_ttf/sdl2_ttf.mk
@@ -13,6 +13,13 @@ SDL2_TTF_INSTALL_STAGING = YES
 SDL2_TTF_DEPENDENCIES = sdl2 freetype host-pkgconf
 SDL2_TTF_CONF_OPTS = --disable-freetype-builtin
 
+ifeq ($(BR2_PACKAGE_HARFBUZZ),y)
+SDL2_TTF_DEPENDENCIES += harfbuzz
+SDL2_TTF_CONF_OPTS += --enable-harfbuzz
+else
+SDL2_TTF_CONF_OPTS += --disable-harfbuzz
+endif
+
 # x-includes and x-libraries must be set for cross-compiling
 # By default x_includes and x_libraries contains unsafe paths.
 # (/usr/include and /usr/lib)
