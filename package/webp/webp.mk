4284eea7c9f66b22a7f87a4edd827692f88afed4
Peter Korsgaard <peter@korsgaard.com>
2022-02-13 12:31:44 +0100

    package/webp: use configure options for jpeg/png/tiff support
    Webp nowadays has explicit configure options for jpeg/png/tiff, so use those
    to explicitly enable/disable support.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/webp/webp.mk
+++ package/webp/webp.mk
@@ -13,12 +13,6 @@ WEBP_CPE_ID_VENDOR = webmproject
 WEBP_CPE_ID_PRODUCT = libwebp
 WEBP_INSTALL_STAGING = YES
 
-WEBP_CONF_OPTS += \
-	--with-jpegincludedir=$(STAGING_DIR)/usr/include \
-	--with-jpeglibdir=$(STAGING_DIR)/usr/lib \
-	--with-tiffincludedir=$(STAGING_DIR)/usr/include \
-	--with-tifflibdir=$(STAGING_DIR)/usr/lib
-
 HOST_WEBP_CONF_OPTS += \
 	--enable-libwebpdemux \
 	--enable-libwebpmux \
@@ -47,6 +41,16 @@ else
 WEBP_CONF_OPTS += --disable-gif
 endif
 
+ifeq ($(BR2_PACKAGE_JPEG),y)
+WEBP_DEPENDENCIES += jpeg
+WEBP_CONF_OPTS += \
+	--enable-jpeg \
+	--with-jpegincludedir=$(STAGING_DIR)/usr/include \
+	--with-jpeglibdir=$(STAGING_DIR)/usr/lib
+else
+WEBP_CONF_OPTS += --disable-jpeg
+endif
+
 ifeq ($(BR2_PACKAGE_LIBFREEGLUT),y)
 WEBP_DEPENDENCIES += libfreeglut
 WEBP_CONF_OPTS += --enable-gl
@@ -56,13 +60,21 @@ endif
 
 ifeq ($(BR2_PACKAGE_LIBPNG),y)
 WEBP_DEPENDENCIES += libpng
+WEBP_CONF_OPTS += --enable-png
 WEBP_CONF_ENV += ac_cv_path_LIBPNG_CONFIG=$(STAGING_DIR)/usr/bin/libpng-config
 else
-WEBP_CONF_ENV += ac_cv_path_LIBPNG_CONFIG=/bin/false
+WEBP_CONF_OPTS += --disable-png
 endif
 
-WEBP_DEPENDENCIES += $(if $(BR2_PACKAGE_JPEG),jpeg)
-WEBP_DEPENDENCIES += $(if $(BR2_PACKAGE_TIFF),tiff)
+ifeq ($(BR2_PACKAGE_TIFF),y)
+WEBP_DEPENDENCIES += tiff
+WEBP_CONF_OPTS += \
+	--enable-tiff \
+	--with-tiffincludedir=$(STAGING_DIR)/usr/include \
+	--with-tifflibdir=$(STAGING_DIR)/usr/lib
+else
+WEBP_CONF_OPTS += --disable-tiff
+endif
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
