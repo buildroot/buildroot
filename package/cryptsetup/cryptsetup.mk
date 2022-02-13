0ce909afa98223bbd6bb1b2c65dec6cc8982ad31
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-02-13 22:29:09 +0100

    package/cryptsetup: openssl is the default crypto backend
    openssl is the default crypto backend since version 2.1.0 and
    https://github.com/mbroz/cryptsetup/commit/bc3d0feb5cd33f9181102b418cc7bf44fe112e82
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Reviewed-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Tested-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/cryptsetup/cryptsetup.mk
+++ package/cryptsetup/cryptsetup.mk
@@ -20,15 +20,15 @@ CRYPTSETUP_INSTALL_STAGING = YES
 CRYPTSETUP_CONF_ENV += LDFLAGS="$(TARGET_LDFLAGS) $(TARGET_NLS_LIBS)"
 CRYPTSETUP_CONF_OPTS += --enable-blkid --enable-libargon2
 
-# cryptsetup uses libgcrypt by default, but can be configured to use OpenSSL
+# cryptsetup uses OpenSSL by default, but can be configured to use libgcrypt
 # or kernel crypto modules instead
-ifeq ($(BR2_PACKAGE_LIBGCRYPT),y)
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+CRYPTSETUP_DEPENDENCIES += openssl
+CRYPTSETUP_CONF_OPTS += --with-crypto_backend=openssl
+else ifeq ($(BR2_PACKAGE_LIBGCRYPT),y)
 CRYPTSETUP_DEPENDENCIES += libgcrypt
 CRYPTSETUP_CONF_ENV += LIBGCRYPT_CONFIG=$(STAGING_DIR)/usr/bin/libgcrypt-config
 CRYPTSETUP_CONF_OPTS += --with-crypto_backend=gcrypt
-else ifeq ($(BR2_PACKAGE_OPENSSL),y)
-CRYPTSETUP_DEPENDENCIES += openssl
-CRYPTSETUP_CONF_OPTS += --with-crypto_backend=openssl
 else
 CRYPTSETUP_CONF_OPTS += --with-crypto_backend=kernel
 endif
