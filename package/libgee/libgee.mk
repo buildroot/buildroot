10780e8d6df83d7a92b6dd3282e91fdc0ba369af
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-02-14 11:29:59 +0100

    package/libgee: bump to version 0.20.5
    Drop patch (already in version) and so autoreconf
    
    https://gitlab.gnome.org/GNOME/libgee/-/blob/0.20.5/NEWS
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/libgee/libgee.mk
+++ package/libgee/libgee.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LIBGEE_VERSION_MAJOR = 0.20
-LIBGEE_VERSION = $(LIBGEE_VERSION_MAJOR).4
+LIBGEE_VERSION = $(LIBGEE_VERSION_MAJOR).5
 LIBGEE_SITE = http://ftp.gnome.org/pub/gnome/sources/libgee/$(LIBGEE_VERSION_MAJOR)
 LIBGEE_SOURCE = libgee-$(LIBGEE_VERSION).tar.xz
 LIBGEE_DEPENDENCIES = host-pkgconf host-vala libglib2
@@ -13,8 +13,6 @@ LIBGEE_INSTALL_STAGING = YES
 LIBGEE_LICENSE = LGPL-2.1+
 LIBGEE_LICENSE_FILES = COPYING
 LIBGEE_CPE_ID_VENDOR = gnome
-# We're patching gee/Makefile.am
-LIBGEE_AUTORECONF = YES
 
 ifeq ($(BR2_PACKAGE_GOBJECT_INTROSPECTION),y)
 LIBGEE_CONF_OPTS += --enable-introspection
