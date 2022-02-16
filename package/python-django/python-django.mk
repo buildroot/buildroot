2fbfb5271e3e7b93fa57ef13de14e54e265e9136
Peter Korsgaard <peter@korsgaard.com>
2022-02-16 21:54:00 +0100

    package/python-django: security bump to version 3.2.12
    Fixes the following security issues:
    
    - CVE-2022-22818: Possible XSS via {% debug %} template tag
    
      The {% debug %} template tag didn't properly encode the current context,
      posing an XSS attack vector.
    
      In order to avoid this vulnerability, {% debug %} no longer outputs
      information when the DEBUG setting is False, and it ensures all context
      variables are correctly escaped when the DEBUG setting is True.
    
    - CVE-2022-23833: Denial-of-service possibility in file uploads
    
      Passing certain inputs to multipart forms could result in an infinite loop
      when parsing files.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 3.2.11
+PYTHON_DJANGO_VERSION = 3.2.12
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/20/86/e4348aac45bc83fc8e9dda2cfd81004b007c65b68c1499a4233acabdaa3b
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/83/d9/7f28811ff78ce1903dc9a32ac439e4e6c98298cd2e99cb01f528e51dd796
 
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
