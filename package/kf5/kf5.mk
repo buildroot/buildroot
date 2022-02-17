84e7062cba31f64fbca98b042d3ac1da1866418e
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-02-17 17:36:55 +0100

    package/kf5: bump to version 5.91
    This bump will fix the following build failure of kf5-kcoreaddons with
    python 3.10 raised since commit
    25b1fc2898d68ddf2674b14c738045875fc5a2dc:
    
    CMake Error at /home/giuliobenetti/autobuild/run/instance-1/output-1/host/riscv32-buildroot-linux-gnu/sysroot/usr/share/ECM/find-modules/FindPythonModuleGeneration.cmake:149 (message):
      The max python version in PythonModuleGeneration must be updated.
    Call Stack (most recent call first):
      src/lib/CMakeLists.txt:208 (find_package)
    
    It will also fix the following build failure of kf5-kcoreaddons raised
    since commit 5770a645a3a49a3f0f02972131a4ff5283b4c11e:
    
    In file included from /home/peko/autobuild/instance-0/output-1/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/qt5/QtCore/qchar.h:43,
                     from /home/peko/autobuild/instance-0/output-1/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/qt5/QtCore/qstring.h:49,
                     from /home/peko/autobuild/instance-0/output-1/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/qt5/QtCore/QString:1,
                     from /home/peko/autobuild/instance-0/output-1/build/kf5-kcoreaddons-5.47.0/src/lib/randomness/krandom.h:27,
                     from /home/peko/autobuild/instance-0/output-1/build/kf5-kcoreaddons-5.47.0/src/lib/randomness/krandom.cpp:22:
    /home/peko/autobuild/instance-0/output-1/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/qt5/QtCore/qglobal.h:1274:80: note: declared here
     1274 | Q_CORE_EXPORT QT_DEPRECATED_VERSION_X_5_15("use QRandomGenerator instead") int qrand();
          |                                                                                ^~~~~
    /home/peko/autobuild/instance-0/output-1/build/kf5-kcoreaddons-5.47.0/src/lib/randomness/krandom.cpp: In function 'QString KRandom::randomString(int)':
    /home/peko/autobuild/instance-0/output-1/build/kf5-kcoreaddons-5.47.0/src/lib/randomness/krandom.cpp:76:27: error: use of deleted function 'QCharRef& QCharRef::operator=(char)'
       76 |         str[i++] =  char(r);
          |                           ^
    
    - Update indentation in hash file (two spaces)
    - Update licenses due to
      https://invent.kde.org/frameworks/kcoreaddons/-/commit/93480398b551aee572f9f1a0ee48ed45a8476580
      https://invent.kde.org/frameworks/modemmanager-qt/-/commit/8ad494d957cab343a60a61a2163f1ca23e9e3511
      https://invent.kde.org/frameworks/networkmanager-qt/-/commit/fa9ef0d84898ad5e1f28857307bd5fcb8eb38e20
    
    Fixes:
     - http://autobuild.buildroot.org/results/0267ff54ee82d853476ff7d6b14edb4aba308aa2
     - http://autobuild.buildroot.org/results/694a2322d9161b55f5db66d1ace57d836360ed84
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/kf5/kf5.mk
+++ package/kf5/kf5.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KF5_VERSION_MAJOR = 5.47
+KF5_VERSION_MAJOR = 5.91
 KF5_VERSION = $(KF5_VERSION_MAJOR).0
 KF5_SITE = https://download.kde.org/stable/frameworks/$(KF5_VERSION_MAJOR)
 
