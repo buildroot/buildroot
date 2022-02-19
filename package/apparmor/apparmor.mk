e0da90512c04f5cfcdcbe1c4a8f32e559828e0c9
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-02-19 15:19:22 +0100

    package/apparmor: drop APPARMOR_FIXUP_APXS
    Drop APPARMOR_FIXUP_APXS which was added by commit
    3c836e54203b16015c9c52cbe007adc92d293a0d but is not needed since the
    addition of PPD_FIXUP_PATHS in commit
    b06294e9897e90862656416e8b67fdace15488d7
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/apparmor/apparmor.mk
+++ package/apparmor/apparmor.mk
@@ -54,16 +54,6 @@ ifeq ($(BR2_PACKAGE_APACHE),y)
 APPARMOR_DEPENDENCIES += apache
 APPARMOR_TOOLS += changehat/mod_apparmor
 APPARMOR_MAKE_OPTS += APXS=$(STAGING_DIR)/usr/bin/apxs
-
-ifeq ($(BR2_PER_PACKAGE_DIRECTORIES),y)
-define APPARMOR_FIXUP_APXS
-	$(SED) "s@$(PER_PACKAGE_DIR)/[^/]\+/@$(PER_PACKAGE_DIR)/apparmor/@g" \
-		$(STAGING_DIR)/usr/bin/apxs \
-		$(STAGING_DIR)/usr/build/config_vars.mk \
-		$(STAGING_DIR)/usr/build-1/libtool
-endef
-APPARMOR_POST_CONFIGURE_HOOKS += APPARMOR_FIXUP_APXS
-endif
 endif
 
 define APPARMOR_BUILD_CMDS
