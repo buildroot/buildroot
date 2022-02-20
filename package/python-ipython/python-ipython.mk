84fd98b48b53065ec30f88ded5ea9a5f8c313591
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-02-20 22:54:09 +0100

    package/python-ipython: security bump to version 7.31.1
    Fix CVE-2022-21699: IPython (Interactive Python) is a command shell for
    interactive computing in multiple programming languages, originally
    developed for the Python programming language. Affected versions are
    subject to an arbitrary code execution vulnerability achieved by not
    properly managing cross user temporary files. This vulnerability allows
    one user to run code as another on the same machine. All users are
    advised to upgrade.
    
    Also update indentation in hash file (two spaces)
    
    https://github.com/ipython/ipython/security/advisories/GHSA-pq7m-3gw7-gq5x
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit aeb138911e773cf4011b81fc4fcec0037f366bb1)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-ipython/python-ipython.mk
+++ package/python-ipython/python-ipython.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_IPYTHON_VERSION = 7.28.0
+PYTHON_IPYTHON_VERSION = 7.31.1
 PYTHON_IPYTHON_SOURCE = ipython-$(PYTHON_IPYTHON_VERSION).tar.gz
-PYTHON_IPYTHON_SITE = https://files.pythonhosted.org/packages/e2/c8/7046d0409a90e31263d5bbaa708347d522ac584a1140c01a951d9deb1792
+PYTHON_IPYTHON_SITE = https://files.pythonhosted.org/packages/fb/39/c1947dc1bb993a35469ca474535cb3214c91f5f9a7752a1fcbd9b6b37dde
 PYTHON_IPYTHON_LICENSE = BSD-3-Clause
 PYTHON_IPYTHON_LICENSE_FILES = COPYING.rst LICENSE
 PYTHON_IPYTHON_CPE_ID_VENDOR = ipython
