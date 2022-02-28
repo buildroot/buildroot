38dbc73d3989c9081f94f1ed1380af3b3be02bf3
Théo Lebrun <theo.lebrun@bootlin.com>
2022-02-28 17:13:45 +0100

    package/wireplumber: bump version to 0.4.8
    Changelog:
    https://gitlab.freedesktop.org/pipewire/wireplumber/-/blob/e14bb72dcc85e2130d0ea96768e5ae3b375a041e/NEWS.rst
    
    Signed-off-by: Théo Lebrun <theo.lebrun@bootlin.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/wireplumber/wireplumber.mk
+++ package/wireplumber/wireplumber.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WIREPLUMBER_VERSION = 0.4.2
+WIREPLUMBER_VERSION = 0.4.8
 WIREPLUMBER_SOURCE = wireplumber-$(WIREPLUMBER_VERSION).tar.bz2
 WIREPLUMBER_SITE = https://gitlab.freedesktop.org/pipewire/wireplumber/-/archive/$(WIREPLUMBER_VERSION)
 WIREPLUMBER_LICENSE = MIT
