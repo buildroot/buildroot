c1f3dacf4568dc1b995ca89ed04b44d2cf2416a4
Giulio Benetti <giulio.benetti@benettiengineering.com>
2022-02-28 13:02:43 +0100

    package/at: bump version to 3.2.5
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/at/at.mk
+++ package/at/at.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-AT_VERSION = 3.2.4
+AT_VERSION = 3.2.5
 AT_SOURCE = at_$(AT_VERSION).orig.tar.gz
 AT_SITE = http://software.calhariz.com/at
 AT_DEPENDENCIES = $(if $(BR2_PACKAGE_FLEX),flex) host-bison host-flex
