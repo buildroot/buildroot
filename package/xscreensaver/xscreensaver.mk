cbcbd66b0f241184288232972d9cd9d03c9f0ce1
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-03-09 22:09:53 +0100

    package/xscreensaver: bump to version 6.03
    xlib_libXft and xlib_libXi are now mandatory
    
    https://www.jwz.org/xscreensaver/changelog.html
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/xscreensaver/xscreensaver.mk
+++ package/xscreensaver/xscreensaver.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XSCREENSAVER_VERSION = 5.45
+XSCREENSAVER_VERSION = 6.03
 XSCREENSAVER_SITE = https://www.jwz.org/xscreensaver
 
 # N.B. GPL-2.0+ code (in the hacks/glx subdirectory) is not currently built.
@@ -20,6 +20,8 @@ XSCREENSAVER_DEPENDENCIES = \
 	libgtk2 \
 	libxml2 \
 	xlib_libX11 \
+	xlib_libXft \
+	xlib_libXi \
 	xlib_libXt \
 	$(TARGET_NLS_DEPENDENCIES) \
 	host-intltool
@@ -49,20 +51,6 @@ XSCREENSAVER_CONF_OPTS += --with-systemd=yes
 XSCREENSAVER_DEPENDENCIES += systemd
 else
 XSCREENSAVER_CONF_OPTS += --with-systemd=no
-endif
-
-ifeq ($(BR2_PACKAGE_XLIB_LIBXFT),y)
-XSCREENSAVER_CONF_OPTS += --with-xft=yes
-XSCREENSAVER_DEPENDENCIES += xlib_libXft
-else
-XSCREENSAVER_CONF_OPTS += --with-xft=no
-endif
-
-ifeq ($(BR2_PACKAGE_XLIB_LIBXI),y)
-XSCREENSAVER_CONF_OPTS += --with-xinput-ext=yes
-XSCREENSAVER_DEPENDENCIES += xlib_libXi
-else
-XSCREENSAVER_CONF_OPTS += --with-xinput-ext=no
 endif
 
 ifeq ($(BR2_PACKAGE_XLIB_LIBXINERAMA),y)
