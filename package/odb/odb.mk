88471fe4fdc3d25d81b5b6898415a0550d06b90b
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2022-03-10 10:56:02 +0100

    package/odb: add dependency on host-libodb
    host-odb installs the ODB compiler, which when executed at runtime,
    needs access to the libodb headers. This is a runtime dependency,
    normally added in Config.in, but for host packages there is no way to
    express a runtime dependency. In order to have them installed, add a
    dependency on host-libodb.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Kamel Bouhara <kamel.bouhara@bootlin.com>
    [Kamel: Add optional host-libodb-boost dependency]
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/odb/odb.mk
+++ package/odb/odb.mk
@@ -10,7 +10,13 @@ ODB_SOURCE = odb-$(ODB_VERSION).tar.bz2
 ODB_SITE = https://www.codesynthesis.com/download/odb/$(ODB_VERSION_MAJOR)
 ODB_LICENSE = GPL-3.0
 ODB_LICENSE_FILES = LICENSE
-HOST_ODB_DEPENDENCIES = host-libcutl
+# host-libodb is not needed to build host-odb, but it is needed to use
+# the ODB compiler, as it install header files that are needed at
+# runtime by the odb compiler.
+HOST_ODB_DEPENDENCIES = host-libcutl host-libodb
+ifeq ($(BR2_PACKAGE_LIBODB_BOOST),y)
+HOST_ODB_DEPENDENCIES += host-libodb-boost
+endif
 HOST_ODB_CONF_ENV = CXXFLAGS="$(HOST_CXXFLAGS) -std=c++11"
 
 # Prevent odb from trying to install the gcc plugin into the hosts
