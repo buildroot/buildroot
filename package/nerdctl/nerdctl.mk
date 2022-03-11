b95b8fb44cc1a0ba18f2575a5da4faa0b6e27a10
Christian Stewart <christian@paral.in>
2022-03-10 20:48:16 -0800

    package/nerdctl: new package
    nerdctl is a CLI for containerd (package docker-containerd) which is
    drop-in compatible with the Docker Daemon CLI.
    
    This allows using the lighter weight containerd daemon directly,
    instead of via the additional docker daemon. It also implements
    rootless mode.
    
    https://github.com/containerd/nerdctl
    
    Signed-off-by: Christian Stewart <christian@paral.in>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/nerdctl/nerdctl.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# nerdctl
+#
+################################################################################
+
+NERDCTL_VERSION = 0.17.1
+NERDCTL_SITE = $(call github,containerd,nerdctl,v$(NERDCTL_VERSION))
+
+NERDCTL_LICENSE = Apache-2.0
+NERDCTL_LICENSE_FILES = LICENSE
+
+NERDCTL_GOMOD = github.com/containerd/nerdctl
+
+NERDCTL_LDFLAGS = \
+	-X $(NERDCTL_GOMOD)/pkg/version.Version=$(NERDCTL_VERSION)
+
+NERDCTL_BUILD_TARGETS = cmd/nerdctl
+
+$(eval $(golang-package))
