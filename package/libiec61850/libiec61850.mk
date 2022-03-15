83a15447c1a45802e1c15fcd0879f6d0f1c3694f
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-03-15 18:34:29 +0100

    package/libiec61850: security bump to version 1.5.1
    - Retrieve official tarball
    - Fix CVE-2021-45769: A NULL pointer dereference in
      AcseConnection_parseMessage at src/mms/iso_acse/acse.c of libiec61850
      v1.5.0 can lead to a segmentation fault or application crash.
    - Fix many other vulnerabilities:
      https://libiec61850.com/new-release-1-5-1-of-libiec61850
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 52c372446f034cf8277951ad083abe5e1c10f52d)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libiec61850/libiec61850.mk
+++ package/libiec61850/libiec61850.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-LIBIEC61850_VERSION = 1.5.0
-LIBIEC61850_SITE = $(call github,mz-automation,libiec61850,v$(LIBIEC61850_VERSION))
+LIBIEC61850_VERSION = 1.5.1
+LIBIEC61850_SITE = https://libiec61850.com/wp-content/uploads/2022/03
 LIBIEC61850_INSTALL_STAGING = YES
 LIBIEC61850_LICENSE = GPL-3.0+
 LIBIEC61850_LICENSE_FILES = COPYING
