2fc2191845799ae2358b752fa29839817d33ad82
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-03-16 22:22:18 +0100

    package/zynaddsubfx: fix alsa-lib handling
    alsa-lib is optional, not mandatory, since the addition of the package
    in commit 18a863063735e2c658fa6947a4e540e9dda6c431
    
    Moreover, alsa support depends on BR2_PACKAGE_ALSA_LIB_SEQ (otherwise,
    it is silently disabled)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/zynaddsubfx/zynaddsubfx.mk
+++ package/zynaddsubfx/zynaddsubfx.mk
@@ -18,10 +18,16 @@ ZYNADDSUBFX_CONF_OPTS += -DOS_LIBRARIES=-latomic
 endif
 
 ZYNADDSUBFX_DEPENDENCIES = \
-	alsa-lib \
 	fftw-single \
 	liblo \
 	mxml \
 	zlib
+
+ifeq ($(BR2_PACKAGE_ALSA_LIB_SEQ),y)
+ZYNADDSUBFX_DEPENDENCIES += alsa-lib
+ZYNADDSUBFX_CONF_OPTS += -DAlsaEnable=ON
+else
+ZYNADDSUBFX_CONF_OPTS += -DAlsaEnable=OFF
+endif
 
 $(eval $(cmake-package))
