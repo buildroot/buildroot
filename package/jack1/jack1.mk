f0bda6dd18e9276cc00813e62c6c53680c318c67
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-03-20 22:18:15 +0100

    package/jack1: libsndfile is optional, not mandatory
    libsndfile is only needed to build jackrec example client
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    (cherry picked from commit 9a78a756134831da20c471dee575f3e8108f62a4)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/jack1/jack1.mk
+++ package/jack1/jack1.mk
@@ -11,10 +11,14 @@ JACK1_LICENSE = GPL-2.0+ (jack server), LGPL-2.1+ (jack library)
 JACK1_LICENSE_FILES = COPYING COPYING.GPL COPYING.LGPL
 JACK1_INSTALL_STAGING = YES
 
-JACK1_DEPENDENCIES = host-pkgconf alsa-lib berkeleydb libsndfile
+JACK1_DEPENDENCIES = host-pkgconf alsa-lib berkeleydb
 
 ifeq ($(BR2_PACKAGE_LIBSAMPLERATE),y)
 JACK1_DEPENDENCIES += libsamplerate
+endif
+
+ifeq ($(BR2_PACKAGE_LIBSNDFILE),y)
+JACK1_DEPENDENCIES += libsndfile
 endif
 
 ifeq ($(BR2_PACKAGE_READLINE),y)
