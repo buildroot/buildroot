05bbdfa382ec8b26d9b2f98b1696cb884ed7c2ca
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-03-20 22:18:14 +0100

    package/jack1: libsamplerate is optional, not mandatory
    libsamplerate is only needed to build the NetJack backend and internal
    client
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/jack1/jack1.mk
+++ package/jack1/jack1.mk
@@ -11,7 +11,11 @@ JACK1_LICENSE = GPL-2.0+ (jack server), LGPL-2.1+ (jack library)
 JACK1_LICENSE_FILES = COPYING COPYING.GPL COPYING.LGPL
 JACK1_INSTALL_STAGING = YES
 
-JACK1_DEPENDENCIES = host-pkgconf alsa-lib berkeleydb libsamplerate libsndfile
+JACK1_DEPENDENCIES = host-pkgconf alsa-lib berkeleydb libsndfile
+
+ifeq ($(BR2_PACKAGE_LIBSAMPLERATE),y)
+JACK1_DEPENDENCIES += libsamplerate
+endif
 
 ifeq ($(BR2_PACKAGE_READLINE),y)
 JACK1_DEPENDENCIES += readline
