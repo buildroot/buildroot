016e21eb77c98863b5bbc8e6b0b7edf44f890d94
James Hilliard <james.hilliard1@gmail.com>
2022-03-22 17:22:17 -0600

    package/spirv-llvm-translator: new host package
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

---
+++ package/spirv-llvm-translator/spirv-llvm-translator.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# spirv-llvm-translator
+#
+################################################################################
+
+SPIRV_LLVM_TRANSLATOR_VERSION = llvm_release_110
+SPIRV_LLVM_TRANSLATOR_SITE = $(call github,KhronosGroup,SPIRV-LLVM-Translator,$(SPIRV_LLVM_TRANSLATOR_VERSION))
+SPIRV_LLVM_TRANSLATOR_LICENSE = NCSA
+SPIRV_LLVM_TRANSLATOR_LICENSE_FILES = LICENSE.TXT
+HOST_SPIRV_LLVM_TRANSLATOR_DEPENDENCIES = host-clang host-llvm
+HOST_SPIRV_LLVM_TRANSLATOR_CONF_OPTS = \
+	-DLLVM_BUILD_TOOLS=ON \
+	-DLLVM_DIR=$(HOST_DIR)/lib/cmake/llvm \
+	-DLLVM_SPIRV_BUILD_EXTERNAL=YES \
+	-DLLVM_SPIRV_INCLUDE_TESTS=OFF
+
+$(eval $(host-cmake-package))
