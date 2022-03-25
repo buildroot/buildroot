da7678357fe37e7a942fb08188caadc71e2d884b
HaseenaSainul <41037131+HaseenaSainul@users.noreply.github.com>
2022-03-25 10:44:53 +0530

    Python3: set BZIP2 on and use DISABLE_BZIP2 to disable bzip explicitly (#424)

--- package/python3/python3.mk
+++ package/python3/python3.mk
@@ -41,10 +41,10 @@ PYTHON3_DEPENDENCIES = host-python3 libffi
 
 HOST_PYTHON3_DEPENDENCIES = host-expat host-zlib host-libffi
 
-ifeq ($(BR2_PACKAGE_HOST_PYTHON3_BZIP2),y)
-HOST_PYTHON3_DEPENDENCIES += host-bzip2
-else
+ifeq ($(BR2_PACKAGE_HOST_DISABLE_PYTHON3_BZIP2),y)
 HOST_PYTHON3_CONF_OPTS += --disable-bzip2
+else
+HOST_PYTHON3_DEPENDENCIES += host-bzip2
 endif
 
 ifeq ($(BR2_PACKAGE_HOST_PYTHON3_SSL),y)
