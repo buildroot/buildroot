8153c3620a436e5eca948aacb6a214424dbf834f
Romain Naour <romain.naour@gmail.com>
2022-03-31 22:17:18 +0200

    package/python-{treq, twisted}: add a comment to use the same version
    As reported by [1], python-twisted and python-treq must use the same
    version because its the same project:
    
    https://github.com/twisted/twisted
    https://github.com/twisted/treq
    
    We have to update python-treq to version 22.2.0 [2] otherwise we have
    a runtime issue:
    
    Traceback (most recent call last):
      File "/root/sample_python_treq.py", line 2, in <module>
        import treq
      File "/usr/lib/python3.10/site-packages/treq/__init__.py", line 5, in <module>
      File "/usr/lib/python3.10/site-packages/treq/api.py", line 5, in <module>
      File "/usr/lib/python3.10/site-packages/treq/client.py", line 11, in <module>
    ImportError: cannot import name '_PY3' from 'twisted.python.compat' (unknown
    location)
    
    The comment should avoid forgeting one of them while upgrading or
    backporting version bump.
    
    [1] http://lists.busybox.net/pipermail/buildroot/2022-February/637505.html
    [2] https://github.com/twisted/treq/releases/tag/release-22.2.0
    [3] https://gitlab.com/buildroot.org/buildroot/-/jobs/2179206653
    
    Signed-off-by: Romain Naour <romain.naour@gmail.com>
    Cc: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 5140d10bf6b9f01e7e1ecb2b357885e03e9d5fb4)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-twisted/python-twisted.mk
+++ package/python-twisted/python-twisted.mk
@@ -4,6 +4,9 @@
 #
 ################################################################################
 
+# When bumping this package, make sure to also verify if the
+# python-treq package still works and to update its hash,
+# as they share the same version/site variables.
 PYTHON_TWISTED_VERSION = 22.2.0
 PYTHON_TWISTED_SOURCE = Twisted-$(PYTHON_TWISTED_VERSION).tar.gz
 PYTHON_TWISTED_SITE = https://files.pythonhosted.org/packages/40/8b/56e8870d412c550b3ff2d6714ee212c7e80a6634f4e720c3a26a983e7b46
