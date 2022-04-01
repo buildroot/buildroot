b49e3da1abf3a6c8affac08089311fc6590f54d0
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-04-01 19:02:55 +0200

    package/udpcast: fix build without pod2man
    udpcast unconditionally build manpages which will raise the following
    build failure without pod2man since at least bump to version 20200328 in
    commit 4fb91d8b9dff314fc53438144f843b3e3ceaacaa:
    
    sh: line 1: pod2man: command not found
    
    To fix this issue, rework how udpcast is built and installed to always
    build and install the sender or the receiver and never build the
    manpages. As a side effect, this will also avoid to install the unneeded
    rateGovernor.h
    
    Fixes:
     - http://autobuild.buildroot.org/results/ce602a09357e950d79794391cd4a852d565914f1
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/udpcast/udpcast.mk
+++ package/udpcast/udpcast.mk
@@ -9,23 +9,15 @@ UDPCAST_SITE = http://www.udpcast.linux.lu/download
 UDPCAST_DEPENDENCIES = host-m4
 UDPCAST_LICENSE = BSD-2-Clause, GPL-2.0+
 UDPCAST_LICENSE_FILES = COPYING
+UDPCAST_TARGETS = \
+	$(if $(BR2_PACKAGE_UDPCAST_RECEIVER),udp-receiver) \
+	$(if $(BR2_PACKAGE_UDPCAST_SENDER),udp-sender)
+UDPCAST_MAKE_OPTS = $(UDPCAST_TARGETS)
 
-define UDPCAST_REMOVE_UDP_SENDER
-	rm -f $(TARGET_DIR)/usr/sbin/udp-sender
-	rm -f $(TARGET_DIR)/usr/sbin/udp-sender.1
+define UDPCAST_INSTALL_TARGET_CMDS
+	$(foreach f,$(UDPCAST_TARGETS),\
+		$(INSTALL) -D -m 755 $(@D)/$(f) $(TARGET_DIR)/usr/sbin/$(f)
+	)
 endef
-
-ifneq ($(BR2_PACKAGE_UDPCAST_SENDER),y)
-UDPCAST_POST_INSTALL_TARGET_HOOKS += UDPCAST_REMOVE_UDP_SENDER
-endif
-
-define UDPCAST_REMOVE_UDP_RECEIVER
-	rm -f $(TARGET_DIR)/usr/sbin/udp-receiver
-	rm -f $(TARGET_DIR)/usr/sbin/udp-receiver.1
-endef
-
-ifneq ($(BR2_PACKAGE_UDPCAST_RECEIVER),y)
-UDPCAST_POST_INSTALL_TARGET_HOOKS += UDPCAST_REMOVE_UDP_RECEIVER
-endif
 
 $(eval $(autotools-package))
