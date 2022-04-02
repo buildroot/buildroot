6a11646968011c7209cfadb17d0714949e174f21
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-04-02 10:27:15 +0200

    package/ipmiutil: bump to version 3.1.8
    Drop IPMIUTIL_TOUCH_CONFIG_H_IN as aclocal.m4 is not newer than
    config.h.in anymore
    
    https://sourceforge.net/p/ipmiutil/news/2021/11/ipmiutil-318-is-released
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/ipmiutil/ipmiutil.mk
+++ package/ipmiutil/ipmiutil.mk
@@ -4,18 +4,12 @@
 #
 ################################################################################
 
-IPMIUTIL_VERSION = 3.1.7
+IPMIUTIL_VERSION = 3.1.8
 IPMIUTIL_SITE = https://sourceforge.net/projects/ipmiutil/files
 IPMIUTIL_LICENSE = BSD-3-Clause
 IPMIUTIL_LICENSE_FILES = COPYING
 
 IPMIUTIL_MAKE = $(MAKE1)
-
-# aclocal.m4 is newer than config.h.in. Touch the latter to avoid autoreconf
-define IPMIUTIL_TOUCH_CONFIG_H_IN
-	touch $(@D)/config.h.in
-endef
-IPMIUTIL_PRE_CONFIGURE_HOOKS += IPMIUTIL_TOUCH_CONFIG_H_IN
 
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
 # tests against distro libcrypto so it might get a false positive when
