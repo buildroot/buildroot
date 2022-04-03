3e36fa4c2200c63d7cae2239b5e9b82f008509b7
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-04-03 20:35:03 +0200

    package/minizip: bump to version 3.0.5
    This bump will fix the following build failure with zlib-ng thanks to
    https://github.com/zlib-ng/minizip-ng/commit/9184ba1216589077803b3e4e85f2b226f861ab2d:
    
    /home/autobuild/autobuild/instance-10/output-1/build/minizip-3.0.4/mz_crypt.c:19:14: fatal error: zlib-ng.h: No such file or directory
       19 | #    include "zlib-ng.h"
          |              ^~~~~~~~~~~
    
    https://github.com/zlib-ng/minizip-ng/releases/tag/3.0.5
    
    Fixes:
     - http://autobuild.buildroot.org/results/530fee780557941356dcd6aabb8cc9eb048574f3
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit b1d80083781f844cc70229c4a592253c9a34cbaa)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/minizip/minizip.mk
+++ package/minizip/minizip.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MINIZIP_VERSION = 3.0.4
+MINIZIP_VERSION = 3.0.5
 MINIZIP_SITE = $(call github,nmoinvaz,minizip,$(MINIZIP_VERSION))
 MINIZIP_DEPENDENCIES = host-pkgconf
 MINIZIP_INSTALL_STAGING = YES
