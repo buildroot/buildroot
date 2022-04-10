9853cb9580caa3b6d9db551b124e38ddf5efbb04
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-04-10 16:34:13 +0200

    package/kompexsqlite: security bump to version 1.12.15
    - Update site to get latest version
    - Switch to generic-package and drop patches as upstream doesn't provide
      autotools support
    - License is MIT since version 1.12.15
    - This bump will fix the following build failure with BR2_OPTIMIZE_FAST:
    
    src/sqlite3.c: In function 'sqlite3IsNaN':
    src/sqlite3.c:21797:3: error: #error SQLite will not work correctly with the -ffast-math option of GCC.
    21797 | # error SQLite will not work correctly with the -ffast-math option of GCC.
          |   ^~~~~
    
    - This bump will also fix security issues by bumping sqlite to 3.36.0
    - Update indentation in hash file (two spaces)
    
    http://sqlitewrapper.kompex-online.com/index.php?content=changelog
    
    Fixes:
     - http://autobuild.buildroot.org/results/63e07345b97faa6d6239933f1790c6f2e02da77f
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    (cherry picked from commit e2df7d28c2e3572e2b61b6823606f84f052fa833)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/kompexsqlite/kompexsqlite.mk
+++ package/kompexsqlite/kompexsqlite.mk
@@ -4,11 +4,44 @@
 #
 ################################################################################
 
-KOMPEXSQLITE_VERSION = 1.10.12-1
-KOMPEXSQLITE_SITE = \
-	$(call github,Aethelflaed,kompex-sqlite-wrapper,v$(KOMPEXSQLITE_VERSION))
+KOMPEXSQLITE_VERSION = 1.12.15
+KOMPEXSQLITE_SOURCE = download.php?dl=KompexSQLiteWrapper-Source_$(KOMPEXSQLITE_VERSION).tar.gz
+KOMPEXSQLITE_SITE = http://sqlitewrapper.kompex-online.com/counter
 KOMPEXSQLITE_INSTALL_STAGING = YES
-KOMPEXSQLITE_LICENSE = LGPL-3.0+ (wrapper), Public Domain (bundled sqlite)
-KOMPEXSQLITE_LICENSE_FILES = LICENSE.txt
+KOMPEXSQLITE_LICENSE = MIT (wrapper), Public Domain (bundled sqlite)
+KOMPEXSQLITE_LICENSE_FILES = inc/KompexSQLiteDatabase.h inc/sqlite3.h
 
-$(eval $(autotools-package))
+ifeq ($(BR2_STATIC_LIBS)$(BR2_SHARED_STATIC_LIBS),y)
+KOMPEXSQLITE_CONFS += ReleaseStaticLib
+define KOMPEXSQLITE_INSTALL_STATIC_LIB
+	$(INSTALL) -D -m 0644 $(@D)/lib/release/KompexSQLiteWrapper_Static.a \
+		$(STAGING_DIR)/usr/lib/libkompex-sqlite-wrapper.a
+endef
+endif
+
+ifeq ($(BR2_SHARED_LIBS)$(BR2_SHARED_STATIC_LIBS),y)
+KOMPEXSQLITE_CONFS += ReleaseDynamicLib
+define KOMPEXSQLITE_INSTALL_SHARED_LIB
+	$(INSTALL) -D -m 0755 $(@D)/lib/release/KompexSQLiteWrapper.so \
+		$(1)/usr/lib/libkompex-sqlite-wrapper.so
+endef
+endif
+
+define KOMPEXSQLITE_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) \
+		ALLCONFS="$(KOMPEXSQLITE_CONFS)" \
+		-C "$(@D)/Kompex SQLite Wrapper" all
+endef
+
+define KOMPEXSQLITE_INSTALL_STAGING_CMDS
+	mkdir -p $(STAGING_DIR)/usr/include/kompex
+	$(INSTALL) -m 644 $(@D)/inc/* $(STAGING_DIR)/usr/include/kompex
+	$(KOMPEXSQLITE_INSTALL_STATIC_LIB)
+	$(call KOMPEXSQLITE_INSTALL_SHARED_LIB,$(STAGING_DIR))
+endef
+
+define KOMPEXSQLITE_INSTALL_TARGET_CMDS
+	$(call KOMPEXSQLITE_INSTALL_SHARED_LIB,$(TARGET_DIR))
+endef
+
+$(eval $(generic-package))
