2759c40df782ca4f936c3dc995a27bf6cb9020b4
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-04-10 15:44:15 +0200

    package/tbb: add CPE variables
    cpe:2.3:a:intel:threading_building_blocks is a valid CPE identifier for
    this package:
    
      https://nvd.nist.gov/products/cpe/search/results?namingFormat=2.3&keyword=cpe%3A2.3%3Aa%3Aintel%3Athreading_building_blocks
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/tbb/tbb.mk
+++ package/tbb/tbb.mk
@@ -9,6 +9,8 @@ TBB_SITE = $(call github,01org,tbb,$(TBB_VERSION))
 TBB_INSTALL_STAGING = YES
 TBB_LICENSE = Apache-2.0
 TBB_LICENSE_FILES = LICENSE
+TBB_CPE_ID_VENDOR = intel
+TBB_CPE_ID_PRODUCT = threading_building_blocks
 
 TBB_SO_VERSION = 2
 TBB_LIBS = libtbb libtbbmalloc libtbbmalloc_proxy
