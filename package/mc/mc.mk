9388b29dd6df5db5085df4fb2a5c5f0eb9b5e26d
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-04-16 17:26:32 +0200

    package/mc: fix build with gcc 4.8
    Fix the following build failure with gcc 4.8 raised since bump to
    version 4.8.26 in commit e648dfa6f7078a6fbbc5feb83292c6a98300fe8f and
    https://github.com/MidnightCommander/mc/commit/8b4386df83ab5a525f0568113fe1e53d362f433e:
    
    tty-ncurses.c: In function 'tty_colorize_area':
    tty-ncurses.c:575:5: error: 'for' loop initial declarations are only allowed in C99 mode
         for (int row = 0; row < rows; row++)
         ^
    
    Fixes:
     - http://autobuild.buildroot.org/results/d9f0a11bde42fe6ae2e9449d3365609d59a6545d
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    (cherry picked from commit c99d2d34298ddfed16581ae3405ed70b52640cb5)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/mc/mc.mk
+++ package/mc/mc.mk
@@ -10,7 +10,9 @@ MC_SITE = http://ftp.midnight-commander.org
 MC_LICENSE = GPL-3.0+
 MC_LICENSE_FILES = COPYING
 MC_DEPENDENCIES = libglib2 host-pkgconf $(TARGET_NLS_DEPENDENCIES)
-MC_CONF_ENV = LIBS=$(TARGET_NLS_LIBS)
+MC_CONF_ENV = \
+	CFLAGS="$(TARGET_CFLAGS) -std=c99" \
+	LIBS=$(TARGET_NLS_LIBS)
 
 ifeq ($(BR2_PACKAGE_GPM),y)
 MC_CONF_OPTS += --with-gpm-mouse
