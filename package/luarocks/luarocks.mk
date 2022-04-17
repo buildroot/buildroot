9cd09d9d3763f977b086f15642471c135a76d717
Francois Perrad <fperrad@gmail.com>
2022-04-17 10:28:00 +0200

    package/luarocks: bump to version 3.9.0
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/luarocks/luarocks.mk
+++ package/luarocks/luarocks.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LUAROCKS_VERSION = 3.8.0
+LUAROCKS_VERSION = 3.9.0
 LUAROCKS_SITE = https://luarocks.org/releases
 LUAROCKS_LICENSE = MIT
 LUAROCKS_LICENSE_FILES = COPYING
