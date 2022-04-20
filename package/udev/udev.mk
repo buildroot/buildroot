5430ae90e5556a577333cccd8bd1998971576d88
TIAN Yuanhao <tianyuanhao3@163.com>
2022-04-20 06:55:49 -0700

    package/udev: add the missing sgx group
    systemd-udevd needs this group:
      /usr/lib/udev/rules.d/50-udev-default.rules:43 Unknown group 'sgx', ignoring
    
    Signed-off-by: TIAN Yuanhao <tianyuanhao3@163.com>
    [yann.morin.1998@free.fr: udev users/groups go to udev virtual package]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 798a4983eae7217281d84b76057fe61401e0f71d)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/udev/udev.mk
+++ package/udev/udev.mk
@@ -9,6 +9,7 @@ define UDEV_USERS
 	- - input -1 * - - - Input device group
 	- - render -1 * - - - DRI rendering nodes
 	- - kvm -1 * - - - kvm nodes
+	- - sgx -1 * - - - SGX device nodes
 endef
 
 $(eval $(virtual-package))
