7663482d22d1fc69ede0617845085420143bbad0
Kris Bahnsen <kris@embeddedTS.com>
2022-04-22 15:36:08 -0700

    package/wilc1000-firmware: bump to 15.6
    Supports both WILC1000 (Wi-Fi only) and WILC3000 (Wi-Fi/BLE) hardware.
    
    To support WILC3000 this commit adds a separate config option,
    the original config option from this package is still valid.
    This will be refactored in a follow-up commit.
    
    There is now a license file.
    
    Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>
    [Arnout: keep license as PROPRIETARY and add license file hash]
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/wilc1000-firmware/wilc1000-firmware.mk
+++ package/wilc1000-firmware/wilc1000-firmware.mk
@@ -4,21 +4,32 @@
 #
 ################################################################################
 
-WILC1000_FIRMWARE_VERSION = 14.1
-WILC1000_FIRMWARE_SITE = https://github.com/linux4sc/wireless-firmware/archive
-WILC1000_FIRMWARE_SOURCE = v$(WILC1000_FIRMWARE_VERSION)_Firmware.zip
+WILC1000_FIRMWARE_VERSION = wilc_linux_15_6
+WILC1000_FIRMWARE_SITE = $(call github,linux4wilc,firmware,$(WILC1000_FIRMWARE_VERSION))
 
 WILC1000_FIRMWARE_LICENSE = PROPRIETARY
+WILC1000_FIRMWARE_LICENSE_FILES = LICENSE.wilc_fw
 
-define WILC1000_FIRMWARE_EXTRACT_CMDS
-	$(UNZIP) -d $(BUILD_DIR) $(WILC1000_FIRMWARE_DL_DIR)/$(WILC1000_FIRMWARE_SOURCE)
-	mv $(BUILD_DIR)/wireless-firmware-$(WILC1000_FIRMWARE_VERSION)_Firmware/* $(@D)
-	rmdir $(BUILD_DIR)/wireless-firmware-$(WILC1000_FIRMWARE_VERSION)_Firmware
+ifeq ($(BR2_PACKAGE_WILC1000_FIRMWARE),y)
+WILC1000_FIRMWARE_FILES += \
+	wilc1000_wifi_firmware.bin
+endif
+
+ifeq ($(BR2_PACKAGE_WILC3000_FIRMWARE),y)
+WILC1000_FIRMWARE_FILES += \
+	wilc3000_ble_firmware.bin \
+	wilc3000_wifi_firmware.bin
+endif
+
+define WILC1000_FIRMWARE_INSTALL_FILES
+	cd $(@D) && \
+		$(TAR) cf install.tar $(sort $(WILC1000_FIRMWARE_FILES)) && \
+		$(TAR) xf install.tar -C $(TARGET_DIR)/lib/firmware/mchp
 endef
 
 define WILC1000_FIRMWARE_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0644 $(@D)/wilc1003_firmware.bin \
-		$(TARGET_DIR)/lib/firmware/atmel/wilc1003_firmware.bin
+	$(INSTALL) -d -m 0755 $(TARGET_DIR)/lib/firmware/mchp/
+	$(WILC1000_FIRMWARE_INSTALL_FILES)
 endef
 
 $(eval $(generic-package))
