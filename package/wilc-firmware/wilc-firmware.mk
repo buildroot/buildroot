244f607f4c4a2e0609d1fb8e8b62c4ae751b8136
Kris Bahnsen <kris@embeddedTS.com>
2022-04-22 15:36:09 -0700

    package/wilc-firmware: rename from wilc1000-firmware
    Separates out WILC1000 and WILC3000 in to individual config options
    since in reality only one or the other set would be needed.
    
    Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/wilc-firmware/wilc-firmware.mk
+++ package/wilc-firmware/wilc-firmware.mk
@@ -1,35 +1,35 @@
 ################################################################################
 #
-# wilc1000-firmware
+# wilc-firmware
 #
 ################################################################################
 
-WILC1000_FIRMWARE_VERSION = wilc_linux_15_6
-WILC1000_FIRMWARE_SITE = $(call github,linux4wilc,firmware,$(WILC1000_FIRMWARE_VERSION))
+WILC_FIRMWARE_VERSION = wilc_linux_15_6
+WILC_FIRMWARE_SITE = $(call github,linux4wilc,firmware,$(WILC_FIRMWARE_VERSION))
 
-WILC1000_FIRMWARE_LICENSE = PROPRIETARY
-WILC1000_FIRMWARE_LICENSE_FILES = LICENSE.wilc_fw
+WILC_FIRMWARE_LICENSE = PROPRIETARY
+WILC_FIRMWARE_LICENSE_FILES = LICENSE.wilc_fw
 
 ifeq ($(BR2_PACKAGE_WILC1000_FIRMWARE),y)
-WILC1000_FIRMWARE_FILES += \
+WILC_FIRMWARE_FILES += \
 	wilc1000_wifi_firmware.bin
 endif
 
 ifeq ($(BR2_PACKAGE_WILC3000_FIRMWARE),y)
-WILC1000_FIRMWARE_FILES += \
+WILC_FIRMWARE_FILES += \
 	wilc3000_ble_firmware.bin \
 	wilc3000_wifi_firmware.bin
 endif
 
-define WILC1000_FIRMWARE_INSTALL_FILES
+define WILC_FIRMWARE_INSTALL_FILES
 	cd $(@D) && \
-		$(TAR) cf install.tar $(sort $(WILC1000_FIRMWARE_FILES)) && \
+		$(TAR) cf install.tar $(sort $(WILC_FIRMWARE_FILES)) && \
 		$(TAR) xf install.tar -C $(TARGET_DIR)/lib/firmware/mchp
 endef
 
-define WILC1000_FIRMWARE_INSTALL_TARGET_CMDS
+define WILC_FIRMWARE_INSTALL_TARGET_CMDS
 	$(INSTALL) -d -m 0755 $(TARGET_DIR)/lib/firmware/mchp/
-	$(WILC1000_FIRMWARE_INSTALL_FILES)
+	$(WILC_FIRMWARE_INSTALL_FILES)
 endef
 
 $(eval $(generic-package))
