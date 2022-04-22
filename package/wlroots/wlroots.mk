b02b8ee3bb99cf453675d216701dec7ae6313e38
Adrian Perez de Castro <aperez@igalia.com>
2022-04-23 00:49:17 +0300

    package/wlroots: bump to version 0.15.1
    Update to version 0.15.1, and adapt to upstream changes:
    
    - Adapted from the old -Dfoo-backend Meson option to the new -Dbackends
      one, which is an array. The set of always enabled backends matches the
      previous selection: drm+libinput always enabled, x11 only if selected.
    - Removed the libpng and ffmpeg dependency, which are no longer needed
      with examples disabled (which is always the case for Buildroot).
    - Changed homepage and download site URLs to the FreeDesktop.org GitLab.
    
    This release changes the API/ABI, and applications which use wlroots
    must be adapted and rebuilt. Currently in Buildroot we have only the
    cage compositor, to be updated in a follow-up patch.
    
    Signed-off-by: Adrian Perez de Castro <aperez@igalia.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/wlroots/wlroots.mk
+++ package/wlroots/wlroots.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-WLROOTS_VERSION = 0.14.1
-WLROOTS_SITE = https://github.com/swaywm/wlroots/releases/download/$(WLROOTS_VERSION)
+WLROOTS_VERSION = 0.15.1
+WLROOTS_SITE = https://gitlab.freedesktop.org/wlroots/wlroots/-/releases/$(WLROOTS_VERSION)/downloads
 WLROOTS_LICENSE = MIT
 WLROOTS_LICENSE_FILES = LICENSE
 WLROOTS_INSTALL_STAGING = YES
@@ -25,19 +25,16 @@ WLROOTS_DEPENDENCIES = \
 
 WLROOTS_CONF_OPTS = -Dexamples=false -Dxcb-errors=disabled -Drenderers=gles2
 
-ifeq ($(BR2_PACKAGE_FFMPEG),y)
-WLROOTS_DEPENDENCIES += ffmpeg
-endif
-
-ifeq ($(BR2_PACKAGE_LIBPNG),y)
-WLROOTS_DEPENDENCIES += libpng
-endif
+WLROOTS_BACKENDS = libinput drm
 
 ifeq ($(BR2_PACKAGE_WLROOTS_X11),y)
-WLROOTS_CONF_OPTS += -Dx11-backend=enabled -Dxwayland=enabled
+WLROOTS_BACKENDS += x11
+WLROOTS_CONF_OPTS += -Dxwayland=enabled
 WLROOTS_DEPENDENCIES += libxcb xcb-util-wm xcb-util-renderutil xlib_libX11
 else
-WLROOTS_CONF_OPTS += -Dx11-backend=disabled -Dxwayland=disabled
+WLROOTS_CONF_OPTS += -Dxwayland=disabled
 endif
+
+WLROOTS_CONF_OPTS += -Dbackends=$(subst $(space),$(comma),$(strip $(WLROOTS_BACKENDS)))
 
 $(eval $(meson-package))
