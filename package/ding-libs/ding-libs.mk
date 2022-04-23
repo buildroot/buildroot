80bd1002787815295118355f742f79930bf2974d
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-04-23 17:49:03 +0200

    package/ding-libs: bump to version 0.6.2
    - Update site to get latest release
    - Update indentation in hash file (two spaces)
    
    https://github.com/SSSD/ding-libs/releases/tag/0.6.2
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/ding-libs/ding-libs.mk
+++ package/ding-libs/ding-libs.mk
@@ -4,10 +4,9 @@
 #
 ################################################################################
 
-DING_LIBS_VERSION = 0.6.1
-DING_LIBS_SOURCE = ding-libs-ding_libs-$(subst .,_,$(DING_LIBS_VERSION)).tar.gz
+DING_LIBS_VERSION = 0.6.2
 DING_LIBS_SITE = \
-	https://pagure.io/SSSD/ding-libs/archive/ding_libs-$(subst .,_,$(DING_LIBS_VERSION))
+	https://github.com/SSSD/ding-libs/releases/download/$(DING_LIBS_VERSION)
 DING_LIBS_DEPENDENCIES = host-pkgconf \
 	$(TARGET_NLS_DEPENDENCIES) \
 	$(if $(BR2_PACKAGE_LIBICONV),libiconv)
