a6e7a8511526687d27d45570171bd9e4dda6bd59
Andreas Ziegler <br015@umbiko.net>
2022-04-24 11:32:08 +0200

    package/libnpupnp: update version to 4.2.2
    Bugfix release for:
    http://autobuild.buildroot.net/results/a87/a8754431aad8808ae2d67e32d4a7973b00eda1c1/
    
    Technical discussion:
    https://github.com/MusicPlayerDaemon/MPD/pull/1513
    
    Signed-off-by: Andreas Ziegler <br015@umbiko.net>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/libnpupnp/libnpupnp.mk
+++ package/libnpupnp/libnpupnp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBNPUPNP_VERSION = 4.2.1
+LIBNPUPNP_VERSION = 4.2.2
 LIBNPUPNP_SITE = http://www.lesbonscomptes.com/upmpdcli/downloads
 LIBNPUPNP_LICENSE = BSD-3-Clause
 LIBNPUPNP_LICENSE_FILES = COPYING
