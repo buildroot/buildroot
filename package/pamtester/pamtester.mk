bc61e52ae5f8f6983c2033156df8bced12a0284a
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-04-24 19:13:55 +0200

    package/pamtester: fix build without C++
    pamtester uses an archaic configure.in with archaic constructs. This
    had generated a configure script that incorrectly tries to look for and
    validate a C++ compiler:
    
        checking for powerpc64le-buildroot-linux-gnu-g++... no
        checking whether we are using the GNU C++ compiler... no
        checking whether no accepts -g... no
        checking how to run the C++ preprocessor... /lib/cpp
        configure: error: C++ preprocessor "/lib/cpp" fails sanity check
    
    Calling autoreconf fixes the issue, as the generated configure no longer
    tries to look for a C++ compiler at all anymore.
    
    Fixes:
     - http://autobuild.buildroot.org/results/c000aff659da024b6eef42bca824bdea7b5541c2
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    [yann.morin.1998@free.fr: expand commit log; add big-fat comment]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 9993a36f5e1296e86f00376f795de485386bd913)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/pamtester/pamtester.mk
+++ package/pamtester/pamtester.mk
@@ -10,4 +10,9 @@ PAMTESTER_DEPENDENCIES = linux-pam
 PAMTESTER_LICENSE = BSD-3-Clause
 PAMTESTER_LICENSE_FILES = LICENSE
 
+# Obsolete constructs in the archaic configure.in generated an outworn
+# configure script that incorrectly searches a C++ compiler. Regenerate
+# the autoconf machinery to avoid failures without a C++ compiler.
+PAMTESTER_AUTORECONF = YES
+
 $(eval $(autotools-package))
