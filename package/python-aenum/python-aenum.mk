71592236f74768720a05e76fccac4a4a286f4693
James Hilliard <james.hilliard1@gmail.com>
2022-04-28 14:09:26 -0600

    package/python-aenum: bump to version 3.1.11
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit b492a2671bea67b9af033df2ae0d3aec0ba5ded1)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-aenum/python-aenum.mk
+++ package/python-aenum/python-aenum.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_AENUM_VERSION = 3.1.0
+PYTHON_AENUM_VERSION = 3.1.11
 PYTHON_AENUM_SOURCE = aenum-$(PYTHON_AENUM_VERSION).tar.gz
-PYTHON_AENUM_SITE = https://files.pythonhosted.org/packages/ae/25/00b2949186e76a4c9732e33221274964fecf5c88178cbe5a14a80cfc04e8
+PYTHON_AENUM_SITE = https://files.pythonhosted.org/packages/63/6c/a71e18de7c651f384b328be6bccadbbd472aca62f547c1a307b9388d03ca
 PYTHON_AENUM_SETUP_TYPE = setuptools
 PYTHON_AENUM_LICENSE = BSD-3-Clause
 PYTHON_AENUM_LICENSE_FILES = aenum/LICENSE
