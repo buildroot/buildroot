a081ec3b1d54057e096683c3294b9dd90799b5d6
TIAN Yuanhao <tianyuanhao3@163.com>
2022-04-28 04:39:29 -0700

    package/udev: move render and sgx to package/systemd
    Moved groups render and sgx from UDEV_USERS to SYSTEMD_USERS as they
    currently only appear in systemd's udev rules.
    
    In systemd, group render was introduced since commit 4e15a73, and group
    sgx was introduced since commit c9c4899.
    
    In eudev, group render was introduced since commit bb070c1, but was
    removed since commit a8ffcd1 [1].
    
    [1]: https://github.com/eudev-project/eudev/issues/160
    
    Signed-off-by: TIAN Yuanhao <tianyuanhao3@163.com>
    Cc: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Cc: Carlos Santos <unixmania@gmail.com>
    Cc: Yann E. MORIN <yann.morin.1998@free.fr>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 6d163e12a47845804f7f979ddc55402e54ae88b3)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/udev/udev.mk
+++ package/udev/udev.mk
@@ -4,12 +4,10 @@
 #
 ################################################################################
 
-# Required by default rules for input devices
+# Required by default rules
 define UDEV_USERS
 	- - input -1 * - - - Input device group
-	- - render -1 * - - - DRI rendering nodes
 	- - kvm -1 * - - - kvm nodes
-	- - sgx -1 * - - - SGX device nodes
 endef
 
 $(eval $(virtual-package))
