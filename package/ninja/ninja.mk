227d7e0cbaaf093f509f5728f06fad5f53caed7b
James Hilliard <james.hilliard1@gmail.com>
2022-05-04 17:27:52 -0600

    package/ninja: add make jobserver support
    Switch to the Kitware(cmake maintainer) fork of ninja with make
    jobserver support.
    
    This fork has tagged releases based on upstream release tags with
    jobserver support added.
    
    It is sad that we need to switch to a fork to get this feature, but
    upstream is fighting it over philosophical issues, even though dozens of
    people request the feature and a PR for it has existed since 6 years
    [1].
    
    [1] https://github.com/ninja-build/ninja/issues/1139
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/ninja/ninja.mk
+++ package/ninja/ninja.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-NINJA_VERSION = 1.10.2
-NINJA_SITE = $(call github,ninja-build,ninja,v$(NINJA_VERSION))
+NINJA_VERSION_MAJOR = 1.10.2
+NINJA_VERSION = $(NINJA_VERSION_MAJOR).g51db2.kitware.jobserver-1
+NINJA_SITE = $(call github,Kitware,ninja,v$(NINJA_VERSION))
 NINJA_LICENSE = Apache-2.0
 NINJA_LICENSE_FILES = COPYING
 
