0eca74ff6eaf2ed2ef8eb63084a179fb15679d26
James Hilliard <james.hilliard1@gmail.com>
2022-05-08 17:23:49 -0600

    package/python-cbor2: bump to version 5.4.3
    Set CBOR2_BUILD_C_EXTENSION=1 to ensure c extensions are always built.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/python-cbor2/python-cbor2.mk
+++ package/python-cbor2/python-cbor2.mk
@@ -4,12 +4,13 @@
 #
 ################################################################################
 
-PYTHON_CBOR2_VERSION = 5.4.2.post1
+PYTHON_CBOR2_VERSION = 5.4.3
 PYTHON_CBOR2_SOURCE = cbor2-$(PYTHON_CBOR2_VERSION).tar.gz
-PYTHON_CBOR2_SITE = https://files.pythonhosted.org/packages/49/74/2b9f9b76e2831f54d70e177a4e602e72cb86441632d1bcc15be1f04a73f6
+PYTHON_CBOR2_SITE = https://files.pythonhosted.org/packages/9d/c9/cfa5c35a62642a19c14bf9a12dfbf0ee134466be1f062df2258a2ec2f2f7
 PYTHON_CBOR2_SETUP_TYPE = setuptools
 PYTHON_CBOR2_LICENSE = MIT
 PYTHON_CBOR2_LICENSE_FILES = LICENSE.txt
 PYTHON_CBOR2_DEPENDENCIES = host-python-setuptools-scm
+PYTHON_CBOR2_ENV = CBOR2_BUILD_C_EXTENSION=1
 
 $(eval $(python-package))
