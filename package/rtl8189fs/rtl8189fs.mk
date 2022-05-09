bf4b7f7455c21b0dc8f76452e257ccbe8f281f72
Sergey Matyukevich <geomatsi@gmail.com>
2022-05-09 09:07:20 +0300

    package/rtl8189fs: bump revision
    Bump package revision to pull various fixes for v5.1 - v5.17 kernels.
    Patch that disables verbose debug is no more compatible with updated
    driver since build flags in Makefile has been changed. Drop it.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    (cherry picked from commit 25956b29e4d63f599b9baf7298f69538ba2f22fa)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rtl8189fs/rtl8189fs.mk
+++ package/rtl8189fs/rtl8189fs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTL8189FS_VERSION = 73f826f0fa74b2fc1ad67eaf92bf9787947ae673
+RTL8189FS_VERSION = 94a61cbf46ce87b7c9b8aa53123aeb142133a3c9
 RTL8189FS_SITE = $(call github,jwrdegoede,rtl8189ES_linux,$(RTL8189FS_VERSION))
 RTL8189FS_LICENSE = GPL-2.0
 
