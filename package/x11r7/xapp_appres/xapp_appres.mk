c800c9a56cbc559b61d1772ee5b7dcf9f1844ee7
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-05-12 20:53:37 +0200

    package/x11r7/xapp_appres: bump version to 1.0.6
    Release notes:
    https://lists.x.org/archives/xorg-announce/2022-April/003141.html
    
    Removed md5 and sha1 hashes, not provided by upstream anymore.
    Reformatted license hash.
    Switched tarball to upstream-provided .xz format.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/x11r7/xapp_appres/xapp_appres.mk
+++ package/x11r7/xapp_appres/xapp_appres.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-XAPP_APPRES_VERSION = 1.0.5
-XAPP_APPRES_SOURCE = appres-$(XAPP_APPRES_VERSION).tar.bz2
+XAPP_APPRES_VERSION = 1.0.6
+XAPP_APPRES_SOURCE = appres-$(XAPP_APPRES_VERSION).tar.xz
 XAPP_APPRES_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_APPRES_LICENSE = MIT
 XAPP_APPRES_LICENSE_FILES = COPYING
