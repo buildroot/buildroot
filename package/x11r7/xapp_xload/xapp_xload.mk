b457585728f598effaaf8607586ffb5ac9c7a368
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-05-12 20:53:45 +0200

    package/x11r7/xapp_xload: bump version to 1.1.4
    Release notes:
    https://lists.x.org/archives/xorg-announce/2022-April/003150.html
    
    Removed md5 and sha1 hashes, not provided by upstream anymore.
    Reformatted license hash.
    Switched tarball to upstream-provided .xz format.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/x11r7/xapp_xload/xapp_xload.mk
+++ package/x11r7/xapp_xload/xapp_xload.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-XAPP_XLOAD_VERSION = 1.1.3
-XAPP_XLOAD_SOURCE = xload-$(XAPP_XLOAD_VERSION).tar.bz2
+XAPP_XLOAD_VERSION = 1.1.4
+XAPP_XLOAD_SOURCE = xload-$(XAPP_XLOAD_VERSION).tar.xz
 XAPP_XLOAD_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XLOAD_LICENSE = MIT
 XAPP_XLOAD_LICENSE_FILES = COPYING
