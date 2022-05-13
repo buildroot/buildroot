ac4186db91410fba01eab2fe6c370b059c3f7e18
Heiko Thiery <heiko.thiery@gmail.com>
2022-05-13 09:10:51 +0200

    package/netopeer2: bump version to 2.1.16
    Add a dependency to sytemd if enabled so cmake can find libsystemd
    and install the systemd service file.
    
    Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/netopeer2/netopeer2.mk
+++ package/netopeer2/netopeer2.mk
@@ -4,12 +4,16 @@
 #
 ################################################################################
 
-NETOPEER2_VERSION = 2.0.35
+NETOPEER2_VERSION = 2.1.16
 NETOPEER2_SITE = $(call github,CESNET,Netopeer2,v$(NETOPEER2_VERSION))
 NETOPEER2_DL_SUBDIR = netopeer2
 NETOPEER2_LICENSE = BSD-3-Clause
 NETOPEER2_LICENSE_FILES = LICENSE
 NETOPEER2_DEPENDENCIES = libnetconf2 libyang sysrepo host-sysrepo
+
+ifeq ($(BR2_INIT_SYSTEMD),y)
+NETOPEER2_DEPENDENCIES += systemd
+endif
 
 NETOPEER2_CONF_OPTS = \
 	-DBUILD_CLI=$(if $(BR2_PACKAGE_NETOPEER2_CLI),ON,OFF) \
