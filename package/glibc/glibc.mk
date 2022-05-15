d11d2d474605a62988fb7e61e59a08bbf72cf800
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-05-15 11:02:28 +0200

    package/glibc: can't be built with Optimize for fast
    Fix the following build failure:
    
    In file included from <command-line>:
    ./../include/libc-symbols.h:82:3: error: #error "glibc must not be compiled with -ffast-math"
       82 | # error "glibc must not be compiled with -ffast-math"
          |   ^~~~~
    
    Fixes:
     - http://autobuild.buildroot.org/results/3d91f3685ae6c3e1f765d419ea2c0da4cb0eb3be
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -118,6 +118,11 @@ ifeq ($(BR2_nios2),y)
 GLIBC_CFLAGS += -O2
 endif
 
+# glibc can't be built with Optimize for fast
+ifeq ($(BR2_OPTIMIZE_FAST),y)
+GLIBC_CFLAGS += -O2
+endif
+
 define GLIBC_CONFIGURE_CMDS
 	mkdir -p $(@D)/build
 	# Do the configuration
