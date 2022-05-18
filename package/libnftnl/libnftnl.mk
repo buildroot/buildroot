f4f8ed6d16307679f8f354f2788915b22d35c7b9
Baruch Siach <baruch@tkos.co.il>
2022-05-18 11:26:00 +0300

    package/libnftnl: remove json support
    JSON support and the jansson dependency have been removed in version
    1.1.2.
    
    Cc: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    (cherry picked from commit b634d504cb5a0b621f115f36c50a712e85fc8027)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libnftnl/libnftnl.mk
+++ package/libnftnl/libnftnl.mk
@@ -12,11 +12,4 @@ LIBNFTNL_LICENSE_FILES = COPYING
 LIBNFTNL_INSTALL_STAGING = YES
 LIBNFTNL_DEPENDENCIES = host-pkgconf libmnl
 
-ifeq ($(BR2_PACKAGE_LIBNFTNL_JSON),y)
-LIBNFTNL_CONF_OPTS += --with-json-parsing
-LIBNFTNL_DEPENDENCIES += jansson
-else
-LIBNFTNL_CONF_OPTS += --without-json-parsing
-endif
-
 $(eval $(autotools-package))
