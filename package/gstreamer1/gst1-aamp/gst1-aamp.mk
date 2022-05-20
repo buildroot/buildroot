b5aef461f92cee70f76f2c073eab55903f659967
HaseenaSainul <41037131+HaseenaSainul@users.noreply.github.com>
2022-05-20 12:01:11 +0530

    Streamer: changes to compile it again :) (#443)
    * Streamer: changes to compile it again :)
    * Aamp: add wpeframework-clientlibraries into the dependency list i n OCDM enabled case

--- package/gstreamer1/gst1-aamp/gst1-aamp.mk
+++ package/gstreamer1/gst1-aamp/gst1-aamp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GST1_AAMP_VERSION = c9d03d4df04e0a09a0b8cd45c122c6add12b0501
+GST1_AAMP_VERSION = 8a0d9ef607ea254aff4b897137fd1f743db74c29
 GST1_AAMP_SITE = http://code.rdkcentral.com/r/rdk/components/generic/gst-plugins-rdk-aamp
 GST1_AAMP_SITE_METHOD = git
 
@@ -12,6 +12,7 @@ GST1_AAMP_DEPENDENCIES = aamp
 
 ifeq ($(BR2_PACKAGE_WPEFRAMEWORK_CDMI), y)
 GST1_AAMP_CONF_OPTS += \
+    -DCMAKE_CDM_DRM=ON \
     -DCMAKE_DASH_DRM=ON \
     -DCMAKE_USE_OPENCDM_ADAPTER=ON
 endif
