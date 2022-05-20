2ed62ba214100ae61da13742ac0dceafd3015812
HaseenaSainul <41037131+HaseenaSainul@users.noreply.github.com>
2022-05-20 12:00:48 +0530

    Spark: changes to compile and run it again :) (#444)

---
+++ package/pxcore-libnode/pxcore-libnode.mk
@@ -0,0 +1,150 @@
+################################################################################
+#
+# pxcore-libnode
+#
+################################################################################
+
+PXCORE_LIBNODE_VERSION = 51333037650ecee44191492b541106efa573cc35
+PXCORE_LIBNODE_SITE_METHOD = git
+PXCORE_LIBNODE_SITE = https://github.com/pxscene/pxCore
+PXCORE_LIBNODE_DEPENDENCIES = host-python openssl host-qemu
+PXCORE_LIBNODE_INSTALL_STAGING = YES
+PXCORE_LIBNODE_LICENSE = MIT (core code); MIT, Apache and BSD family licenses (Bundled components)
+PXCORE_LIBNODE_LICENSE_FILES = LICENSE
+
+PXCORE_LIBNODE_CONF_OPTS = \
+	--shared \
+	--dest-os=linux \
+	--with-snapshot \
+	--shared-openssl \
+	--without-intl \
+	--without-inspector
+
+#TARGET_CPPFLAGS += -Os -Wno-deprecated-declarations -Wno-maybe-uninitialized -Wno-address -fno-delete-null-pointer-checks -DPNG_APNG_SUPPORTED -fno-omit-frame-pointer -fno-optimize-sibling-calls
+#TARGET_CXXFLAGS += -fpermissive -fvisibility-inlines-hidden
+#TARGET_CFLAGS :=$(subst -O2,-Os,$(TARGET_CFLAGS))
+#TARGET_FCFLAGS :=$(subst -O2,-Os,$(TARGET_FCFLAGS))
+
+ifeq ($(BR2_i386),y)
+PXCORE_LIBNODE_CPU = ia32
+else ifeq ($(BR2_x86_64),y)
+PXCORE_LIBNODE_CPU = x64
+else ifeq ($(BR2_mips),y)
+PXCORE_LIBNODE_CPU = mips
+else ifeq ($(BR2_mipsel),y)
+PXCORE_LIBNODE_CPU = mipsel
+else ifeq ($(BR2_arm),y)
+PXCORE_LIBNODE_CPU = arm
+PXCORE_LIBNODE_CONF_OPTS += --with-arm-float-abi=hard
+else ifeq ($(BR2_aarch64),y)
+PXCORE_LIBNODE_CPU = arm64
+# V8 needs to know what floating point ABI the target is using.
+PXCORE_LIBNODE_ARM_FP = $(call qstrip,$(BR2_GCC_TARGET_FLOAT_ABI))
+endif
+
+# MIPS architecture specific options
+ifeq ($(BR2_mips)$(BR2_mipsel),y)
+ifeq ($(BR2_MIPS_CPU_MIPS32R6),y)
+PXCORE_LIBNODE_MIPS_ARCH_VARIANT = r6
+PXCORE_LIBNODE_MIPS_FPU_MODE = fp64
+else ifeq ($(BR2_MIPS_CPU_MIPS32R2),y)
+PXCORE_LIBNODE_MIPS_ARCH_VARIANT = r2
+else ifeq ($(BR2_MIPS_CPU_MIPS32),y)
+PXCORE_LIBNODE_MIPS_ARCH_VARIANT = r1
+endif
+endif
+
+PXCORE_LIBNODE_VER = 10.15.3
+PXCORE_LIBNODE_LIB_VER = 64
+PXCORE_LIBNODE_GYP_PATH = tools/gyp
+PXCORE_LIBNODE_DIRECTORY = libnode-v$(PXCORE_LIBNODE_VER)
+PXCORE_LIBNODE_PATH = examples/pxScene2d/external
+
+define PXCORE_LIBNODE_PATCHING
+    echo "Test" ; \
+    patch -p1 <$(PXCORE_LIBNODE_PATH)/node-v$(PXCORE_LIBNODE_VER)_mods.patch; \
+    patch -p1 <$(PXCORE_LIBNODE_PATH)/node-v$(PXCORE_LIBNODE_VER)_qemu_wrapper.patch; \
+    patch -p1 <$(PXCORE_LIBNODE_PATH)/openssl_1.0.2_compatibility.patch; \
+    sed -i "s:KERNEL_VERSION:$(BR2_TOOLCHAIN_HEADERS_AT_LEAST):g" $(@D)/$(PXCORE_LIBNODE_PATH)/$(PXCORE_LIBNODE_DIRECTORY)/v8-qemu-wrapper.sh; \
+    sed -i "s:STAGING:$(STAGING_DIR):g" $(@D)/$(PXCORE_LIBNODE_PATH)/$(PXCORE_LIBNODE_DIRECTORY)/v8-qemu-wrapper.sh; \
+    sed -i "s:ARCH:$(PXCORE_LIBNODE_CPU):g" $(@D)/$(PXCORE_LIBNODE_PATH)/$(PXCORE_LIBNODE_DIRECTORY)/v8-qemu-wrapper.sh; \
+    chmod +x $(@D)/$(PXCORE_LIBNODE_PATH)/$(PXCORE_LIBNODE_DIRECTORY)/v8-qemu-wrapper.sh;
+endef
+
+define PXCORE_LIBNODE_COPY_PATCH
+    cp package/pxcore-libnode/node-v$(PXCORE_LIBNODE_VER)_mods.patch.file $(@D)/$(PXCORE_LIBNODE_PATH)/node-v$(PXCORE_LIBNODE_VER)_mods.patch; \
+    cp package/pxcore-libnode/node-v$(PXCORE_LIBNODE_VER)_qemu_wrapper.patch.file $(@D)/$(PXCORE_LIBNODE_PATH)/node-v$(PXCORE_LIBNODE_VER)_qemu_wrapper.patch; \
+    cp package/pxcore-libnode/openssl_1.0.2_compatibility.patch.file $(@D)/$(PXCORE_LIBNODE_PATH)/openssl_1.0.2_compatibility.patch \
+    cp package/pxcore-libnode/v8-qemu-wrapper.sh $(@D)/$(PXCORE_LIBNODE_PATH)/$(PXCORE_LIBNODE_DIRECTORY)/;
+endef
+
+define PXCORE_LIBNODE_EXTRACT
+        $(PXCORE_LIBNODE_COPY_PATCH) \
+        cd $(@D)/; \
+        find . -name examples -prune -o -type f -exec rm -rf {} +; \
+        find . -name examples -prune -o -type d -exec rm -rf {} +; \
+        $(PXCORE_LIBNODE_PATCHING) \
+        mv $(PXCORE_LIBNODE_PATH)/$(PXCORE_LIBNODE_DIRECTORY)/* $(@D)/; \
+        rm -rf examples/; \
+        touch $(@D)/.stamp_downloaded \
+        touch $(@D)/.stamp_extracted
+endef
+PXCORE_LIBNODE_POST_EXTRACT_HOOKS += PXCORE_LIBNODE_EXTRACT
+
+define PXCORE_LIBNODE_CONFIGURE_CMDS
+        echo CPPFLAGS
+        echo $(TARGET_CPPFLAGS)
+        echo CFLAGS
+        echo $(CFLAGS)
+	mkdir -p $(@D)/bin
+	ln -sf $(HOST_DIR)/usr/bin/python2 $(@D)/bin/python
+
+	(cd $(@D); \
+		$(TARGET_CONFIGURE_OPTS) \
+		PATH=$(@D)/bin:$(BR_PATH) \
+		LD="$(TARGET_CXX)" \
+		PYTHON=$(HOST_DIR)/usr/bin/python2 \
+		$(HOST_DIR)/usr/bin/python2 ./configure \
+		--prefix=/usr \
+		--dest-cpu=$(PXCORE_LIBNODE_CPU) \
+		$(if $(PXCORE_LIBNODE_ARM_FP),--with-arm-float-abi=$(PXCORE_LIBNODE_ARM_FP)) \
+		$(if $(PXCORE_LIBNODE_MIPS_ARCH_VARIANT),--with-mips-arch-variant=$(PXCORE_LIBNODE_MIPS_ARCH_VARIANT)) \
+		$(if $(PXCORE_LIBNODE_MIPS_FPU_MODE),--with-mips-fpu-mode=$(PXCORE_LIBNODE_MIPS_FPU_MODE)) \
+		$(PXCORE_LIBNODE_CONF_OPTS) \
+	)
+
+	# use host version of mkpeephole
+	sed "s#<(mkpeephole_exec)#$(HOST_DIR)/usr/bin/mkpeephole#g" -i $(@D)/deps/v8/gypfiles/v8.gyp
+
+endef
+
+define PXCORE_LIBNODE_BUILD_CMDS
+	$(TARGET_MAKE_ENV) PYTHON=$(HOST_DIR)/usr/bin/python2 \
+		$(MAKE) -C $(@D) \
+		$(TARGET_CONFIGURE_OPTS) \
+		PATH=$(@D)/bin:$(BR_PATH) \
+		LD="$(TARGET_CXX)"
+endef
+
+define PXCORE_LIBNODE_INSTALL_SHARED
+        $(INSTALL) -m 755 $(@D)/out/Release/lib.target/libnode.so.$(PXCORE_LIBNODE_LIB_VER) $(1)/usr/lib/
+        ln -sf libnode.so.$(PXCORE_LIBNODE_LIB_VER) $(1)/usr/lib/libnode.so
+endef
+
+define PXCORE_LIBNODE_INSTALL_STAGING_CMDS
+        $(TARGET_MAKE_ENV) PYTHON=$(HOST_DIR)/usr/bin/python2 \
+                HEADERS_ONLY=1 \
+                $(MAKE) -C $(@D) install \
+                DESTDIR=$(STAGING_DIR) \
+                PATH=$(@D)/bin:$(BR_PATH) \
+                LD="$(TARGET_CXX)"
+        $(call PXCORE_LIBNODE_INSTALL_SHARED,$(STAGING_DIR))
+        #cp $(@D)/src/node_contextify_mods.h $(STAGING_DIR)/usr/include/
+        $(PXCORE_LIBNODE_INSTALL_MODULES)
+endef
+define PXCORE_LIBNODE_INSTALL_TARGET_CMDS
+        $(call PXCORE_LIBNODE_INSTALL_SHARED,$(TARGET_DIR))
+        $(PXCORE_LIBNODE_INSTALL_MODULES)
+endef
+
+$(eval $(generic-package))
---
+++ package/spark/spark.mk
@@ -0,0 +1,151 @@
+################################################################################
+#
+# spark
+#
+################################################################################
+SPARK_VERSION = 51333037650ecee44191492b541106efa573cc35
+SPARK_SITE_METHOD = git
+SPARK_SITE = https://github.com/pxscene/pxCore
+SPARK_INSTALL_STAGING = YES
+
+SPARK_DEPENDENCIES = openssl freetype util-linux libpng libcurl pxcore-libnode giflib ca-certificates sqlite
+
+SPARK_CONF_OPTS += \
+    -DHIDE_NON_EXTERNAL_SYMBOLS=OFF \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DBUILD_WITH_TEXTURE_USAGE_MONITORING=ON \
+    -DPXCORE_COMPILE_WARNINGS_AS_ERRORS=OFF \
+    -DPXSCENE_COMPILE_WARNINGS_AS_ERRORS=OFF \
+    -DCMAKE_SKIP_RPATH=ON \
+    -DPXCORE_MATRIX_HELPERS=OFF \
+    -DBUILD_PXWAYLAND_SHARED_LIB=OFF \
+    -DBUILD_PXWAYLAND_STATIC_LIB=OFF \
+    -DPREFER_SYSTEM_LIBRARIES=ON \
+    -DDISABLE_TURBO_JPEG=ON \
+    -DDISABLE_DEBUG_MODE=ON \
+    -DSPARK_BACKGROUND_TEXTURE_CREATION=ON \
+    -DSPARK_ENABLE_LRU_TEXTURE_EJECTION=OFF \
+    -DSUPPORT_DUKTAPE=OFF \
+    -DBUILD_DUKTAPE=ON \
+    -DSPARK_ENABLE_OPTIMIZED_UPDATE=ON \
+    -DSPARK_BACKGROUND_TEXTURE_CREATION=OFF
+
+PXSCENE_PLATFORM_DEFINES := " -DPNG_APNG_SUPPORTED "
+
+ifeq ($(BR2_PACKAGE_WPEFRAMEWORK_COMPOSITORCLIENT), y)
+    SPARK_CONF_OPTS += \
+        -DBUILD_WITH_WPEFRAMEWORK=ON \
+        -DPXCORE_WPEFRAMEWORK=ON \
+        -DPXCORE_ESSOS_SETTINGS_SUPPORT=OFF
+
+    COMPOSITOR=wpeframework
+    COMPOSITOR_BIN=wpe
+    SPARK_DEPENDENCIES += wpeframework
+
+else ifeq ($(BR2_PACKAGE_WESTEROS_ESSOS), y)
+    SPARK_DEPENDENCIES += westeros
+
+    SPARK_CONF_OPTS += \
+        -DPXCORE_ESSOS=ON \
+        -DBUILD_PXSCENE_ESSOS=ON
+
+    COMPOSITOR=essos
+    COMPOSITOR_BIN=egl
+
+else
+    SPARK_DEPENDENCIES += westeros
+
+    SPARK_CONF_OPTS += \
+        -DBUILD_WITH_WAYLAND=ON \
+        -DBUILD_WITH_WESTEROS=ON \
+        -DPXCORE_WAYLAND_EGL=ON \
+        -DBUILD_PXSCENE_WAYLAND_EGL=ON
+
+    COMPOSITOR=wayland_egl
+    COMPOSITOR_BIN=egl
+
+endif
+
+ifeq ($(BR2_PACKAGE_SPARK_LIB), y)
+
+SPARK_CONF_OPTS += \
+    -DBUILD_PXSCENE_APP=OFF \
+    -DBUILD_PXSCENE_STATIC_LIB=OFF \
+    -DBUILD_PXSCENE_SHARED_LIB=ON
+else
+
+SPARK_CONF_OPTS += \
+    -DBUILD_PXSCENE_APP_WITH_PXSCENE_LIB=ON
+endif
+
+SPARK_CONF_OPTS += -DCMAKE_CXX_FLAGS="$(TARGET_CXXFLAGS) -fno-delete-null-pointer-checks"
+
+SPARK_CONF_OPTS += \
+    -DBUILD_RTCORE_LIBS=ON \
+    -DBUILD_RTCORE_STATIC_LIB=OFF
+
+define RTCORE_INSTALL_LIBS
+    $(INSTALL) -m 755 $(@D)/build/$(COMPOSITOR_BIN)/librtCore.so $(1)/usr/lib/
+endef
+define RTCORE_INSTALL_INCLUDES
+    mkdir -p $(STAGING_DIR)/usr/include/unix
+    cp -Rpf $(@D)/src/unix/*.h $(STAGING_DIR)/usr/include/unix
+endef
+
+SPARK_INSTALL_PATH = usr/share/WPEFramework/Spark
+define SPARK_INSTALL_DEPS
+    mkdir -p $(TARGET_DIR)/$(SPARK_INSTALL_PATH)
+    cp -ar $(@D)/examples/pxScene2d/src/node_modules $(TARGET_DIR)/$(SPARK_INSTALL_PATH)/
+    $(INSTALL) -m 755 $(@D)/examples/pxScene2d/src/*.js $(TARGET_DIR)/$(SPARK_INSTALL_PATH)/
+    $(INSTALL) -m 755 $(@D)/examples/pxScene2d/src/*.json $(TARGET_DIR)/$(SPARK_INSTALL_PATH)/
+    $(INSTALL) -m 755 $(@D)/examples/pxScene2d/src/*.ttf $(TARGET_DIR)/$(SPARK_INSTALL_PATH)/
+    $(INSTALL) -m 755 $(@D)/examples/pxScene2d/src/sparkpermissions.conf $(TARGET_DIR)/$(SPARK_INSTALL_PATH)/
+    $(SPARK_INSTALL_WAYLAND_CONF)
+    cp -ar $(@D)/examples/pxScene2d/src/rcvrcore $(TARGET_DIR)/$(SPARK_INSTALL_PATH)/
+    cp -ar $(@D)/examples/pxScene2d/src/browser $(TARGET_DIR)/$(SPARK_INSTALL_PATH)/
+endef
+
+ifeq ($(BR2_PACKAGE_WPEFRAMEWORK_COMPOSITORCLIENT), )
+define SPARK_INSTALL_WAYLAND_CONF
+    $(INSTALL) -m 755 $(@D)/examples/pxScene2d/src/waylandregistry.conf $(TARGET_DIR)/$(SPARK_INSTALL_PATH)/
+endef
+endif
+
+ifeq ($(BR2_PACKAGE_SPARK_LIB), y)
+define SPARK_INSTALL_PX_NATIVE_WINDOW
+    mkdir -p $(STAGING_DIR)/usr/include/spark/$(COMPOSITOR)
+    cp -ar $(@D)/src/$(COMPOSITOR)/*.h $(STAGING_DIR)/usr/include/spark/$(COMPOSITOR)
+endef
+
+define SPARK_INSTALL_STAGING_CMDS
+    $(call RTCORE_INSTALL_LIBS, $(STAGING_DIR))
+    cp -ar $(@D)/src/*.h $(STAGING_DIR)/usr/include/
+    $(RTCORE_INSTALL_INCLUDES)
+    mkdir -p $(STAGING_DIR)/usr/include/spark
+    cp -ar $(@D)/examples/pxScene2d/src/*.h $(STAGING_DIR)/usr/include/spark/
+    $(SPARK_INSTALL_PX_NATIVE_WINDOW)
+    $(INSTALL) -D package/spark/Spark.pc $(STAGING_DIR)/usr/lib/pkgconfig/Spark.pc
+    $(INSTALL) -m 755 $(@D)/examples/pxScene2d/src/libSpark.so $(STAGING_DIR)/usr/lib
+endef
+
+define SPARK_INSTALL_TARGET_CMDS
+    $(SPARK_INSTALL_DEPS)
+    $(call RTCORE_INSTALL_LIBS, $(TARGET_DIR))
+    $(call SPARK_INSTALL_LIBS, $(TARGET_DIR))
+    $(INSTALL) -m 755 $(@D)/examples/pxScene2d/src/libSpark.so $(TARGET_DIR)/usr/lib
+endef
+
+else
+
+define SPARK_INSTALL_STAGING_CMDS
+    $(call RTCORE_INSTALL_LIBS, $(TARGET_DIR))
+    $(call SPARK_INSTALL_LIBS, $(STAGING_DIR))
+endef
+
+define SPARK_INSTALL_TARGET_CMDS
+    $(SPARK_INSTALL_DEPS)
+    $(call RTCORE_INSTALL_LIBS, $(TARGET_DIR))
+    $(INSTALL) -m 755 $(@D)/examples/pxScene2d/src/Spark $(TARGET_DIR)/usr/bin
+endef
+endif
+$(eval $(cmake-package))
