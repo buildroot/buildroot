228a92f6bd90552424a3b7cf6b105e0ffdd1af3b
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-05-29 21:42:49 +0200

    package/janet: security bump to version 1.22.0
    Fix CVE-2022-30763: Janet before 1.22.0 mishandles arrays.
    
    https://github.com/janet-lang/janet/releases/tag/v1.22.0
    https://blog.convisoappsec.com/en/bug-hunting-in-the-janet-language-interpreter
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/janet/janet.mk
+++ package/janet/janet.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-JANET_VERSION = 1.21.2
+JANET_VERSION = 1.22.0
 JANET_SITE = $(call github,janet-lang,janet,v$(JANET_VERSION))
 JANET_LICENSE = MIT
 JANET_LICENSE_FILES = LICENSE
