f8a979ff18dea28f0785501d6fc4de8cde2ddd95
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-06-06 14:13:12 +0200

    package/qemu: fix armeb build
    Fix the following armeb build failure raised since at least version
    6.2.0:
    
    ERROR: Unknown target name 'armeb-softmmu'
    
    Fixes:
     - http://autobuild.buildroot.org/results/dbd1417fb638ad41e9f23941141bbeb9c999fb05
     - http://autobuild.buildroot.org/results/a6f97308aa2e634ed3d5b80b33b4663aa16c4693
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    (cherry picked from commit 66496a40564a2ea590081c4461075ea9d256e579)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/qemu/qemu.mk
+++ package/qemu/qemu.mk
@@ -277,6 +277,9 @@ HOST_QEMU_DEPENDENCIES = host-meson host-pkgconf host-zlib host-libglib2 host-pi
 #       xtensa          xtensa
 
 HOST_QEMU_ARCH = $(ARCH)
+ifeq ($(HOST_QEMU_ARCH),armeb)
+HOST_QEMU_SYS_ARCH = arm
+endif
 ifeq ($(HOST_QEMU_ARCH),i486)
 HOST_QEMU_ARCH = i386
 endif
