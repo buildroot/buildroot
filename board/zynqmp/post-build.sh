32fcadba3ed75ad2449693258e66b089af5eaa01
Neal Frager <neal.frager@amd.com>
2022-06-09 08:04:46 -0600

    board/zynqmp/post-build.sh: auto-generate extlinux.conf
    This patch uses the BR2_ROOTFS_POST_SCRIPT_ARGS to auto-generate the
    extlinux.conf file, so developers will only need to modify the
    board_defconfig file to change the console and boot file system locations.
    
    Signed-off-by: Neal Frager <neal.frager@amd.com>
    Reviewed-by: Luca Ceresoli <luca.ceresoli@bootlin.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- board/zynqmp/post-build.sh
+++ board/zynqmp/post-build.sh
@@ -4,5 +4,18 @@
 # in the binaries directory
 
 BOARD_DIR="$(dirname $0)"
+CONSOLE=$2
+ROOT=$3
 
-install -m 0644 -D $BOARD_DIR/extlinux.conf $BINARIES_DIR/extlinux.conf
+FILE=${BOARD_DIR}/extlinux.conf
+if test -f "${FILE}"; then
+  install -m 0644 -D "${FILE}" "${BINARIES_DIR}/extlinux.conf"
+else
+  mkdir -p "${BINARIES_DIR}"
+  cat <<-__HEADER_EOF > "${BINARIES_DIR}/extlinux.conf"
+	label linux
+	  kernel /Image
+	  devicetree /system.dtb
+	  append console=${CONSOLE} root=/dev/${ROOT} rw rootwait
+	__HEADER_EOF
+fi
