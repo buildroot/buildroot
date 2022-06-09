83adec71b9380c91a34df992157b7a25ee46d7ca
Neal Frager <neal.frager@amd.com>
2022-06-09 08:04:49 -0600

    board/zynqmp/post-build.sh: remove unnecessary if
    Now that all of the extlinux.conf files have been removed,
    it is no longer necessary to check if the file exists.
    
    Signed-off-by: Neal Frager <neal.frager@amd.com>
    Reviewed-by: Luca Ceresoli <luca.ceresoli@bootlin.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- board/zynqmp/post-build.sh
+++ board/zynqmp/post-build.sh
@@ -7,15 +7,10 @@ BOARD_DIR="$(dirname $0)"
 CONSOLE=$2
 ROOT=$3
 
-FILE=${BOARD_DIR}/extlinux.conf
-if test -f "${FILE}"; then
-  install -m 0644 -D "${FILE}" "${BINARIES_DIR}/extlinux.conf"
-else
-  mkdir -p "${BINARIES_DIR}"
-  cat <<-__HEADER_EOF > "${BINARIES_DIR}/extlinux.conf"
+mkdir -p "${BINARIES_DIR}"
+cat <<-__HEADER_EOF > "${BINARIES_DIR}/extlinux.conf"
 	label linux
 	  kernel /Image
 	  devicetree /system.dtb
 	  append console=${CONSOLE} root=/dev/${ROOT} rw rootwait
 	__HEADER_EOF
-fi
