9bba9f1062b25f0117b5eefa5d415f75609d251a
Neal Frager <neal.frager@amd.com>
2022-06-09 08:04:48 -0600

    configs/zynqmp_kria_kv260_defconfig: add parameters to generate extlinux.conf
    This patch enables the zynqmp_kria_kv260_defconfig to auto-generate the
    extlinux.conf file.
    
    The board/zynqmp/kria/extlinux.conf and board/zynqmp/kria/post-build.sh have
    been removed as they are no longer necessary.
    
    Signed-off-by: Neal Frager <neal.frager@amd.com>
    Reviewed-by: Luca Ceresoli <luca.ceresoli@bootlin.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- board/zynqmp/kria/post-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# genimage will need to find the extlinux.conf
-# in the binaries directory
-
-BOARD_DIR="$(dirname $0)"
-
-install -m 0644 -D $BOARD_DIR/extlinux.conf $BINARIES_DIR/extlinux.conf
