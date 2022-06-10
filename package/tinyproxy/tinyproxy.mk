97f4b9eefad54ee60cfee2890992504a5e9939f2
Alexander Mukhin <alexander.i.mukhin@gmail.com>
2022-06-10 20:11:13 +0300

    package/tinyproxy: bump to version 1.11.1
    https://github.com/tinyproxy/tinyproxy/releases/tag/1.11.1
    
    Signed-off-by: Alexander Mukhin <alexander.i.mukhin@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    (cherry picked from commit 96de6bfa7098bb0e856cadce151f8c668eff6e4f)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/tinyproxy/tinyproxy.mk
+++ package/tinyproxy/tinyproxy.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TINYPROXY_VERSION = 1.11.0
+TINYPROXY_VERSION = 1.11.1
 TINYPROXY_SITE = https://github.com/tinyproxy/tinyproxy/releases/download/$(TINYPROXY_VERSION)
 TINYPROXY_SOURCE = tinyproxy-$(TINYPROXY_VERSION).tar.xz
 TINYPROXY_LICENSE = GPL-2.0+
