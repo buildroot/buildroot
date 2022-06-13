978800fadbb7b3d1383e7cd7e166b1f0a35eb999
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-06-13 21:53:24 +0200

    package/sofia-sip: fix version
    Fix version to better match what is expected by NVD NIST database,
    release-monitoring.org as well as upstream github
    (https://github.com/freeswitch/sofia-sip/tags)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/sofia-sip/sofia-sip.mk
+++ package/sofia-sip/sofia-sip.mk
@@ -4,7 +4,8 @@
 #
 ################################################################################
 
-SOFIA_SIP_VERSION = 1.13.6-4152840260
+SOFIA_SIP_VERSION = 1.13.6
+SOFIA_SIP_SOURCE = sofia-sip-$(SOFIA_SIP_VERSION)-4152840260.tar.gz
 SOFIA_SIP_SITE = https://files.freeswitch.org/downloads/libs/sofia-sip
 SOFIA_SIP_INSTALL_STAGING = YES
 SOFIA_SIP_DEPENDENCIES = host-pkgconf
