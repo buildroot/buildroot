727889c87975530d084d156689eb6b6ffbe8f4f9
Raphaël Mélotte <raphael.melotte@mind.be>
2022-06-14 21:03:24 +0200

    package/python-s3transfer: bump to version 0.6.0
    While at it, remove the md5 hash as there is already a sha256 hash.
    
    Note that the checksum of the license changed, but the license is
    still the same (the only change in the license file is the removal of
    a blank line at the end of the file).
    
    Changelog: https://github.com/boto/s3transfer/blob/develop/CHANGELOG.rst
    
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-s3transfer/python-s3transfer.mk
+++ package/python-s3transfer/python-s3transfer.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_S3TRANSFER_VERSION = 0.5.0
+PYTHON_S3TRANSFER_VERSION = 0.6.0
 PYTHON_S3TRANSFER_SOURCE = s3transfer-$(PYTHON_S3TRANSFER_VERSION).tar.gz
-PYTHON_S3TRANSFER_SITE = https://files.pythonhosted.org/packages/88/ef/4d1b3f52ae20a7e72151fde5c9f254cd83f8a49047351f34006e517e1655
+PYTHON_S3TRANSFER_SITE = https://files.pythonhosted.org/packages/e1/eb/e57c93d5cd5edf8c1d124c831ef916601540db70acd96fa21fe60cef1365
 PYTHON_S3TRANSFER_SETUP_TYPE = setuptools
 PYTHON_S3TRANSFER_LICENSE = Apache-2.0
 PYTHON_S3TRANSFER_LICENSE_FILES = LICENSE.txt
