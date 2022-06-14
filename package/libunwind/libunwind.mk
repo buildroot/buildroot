67d87bf7d255cd05f84fab3e3625e073b5bfccf8
Frederik Van Bogaert <frederik.vanbogaert@mind.be>
2022-06-14 19:03:34 +0200

    package/libunwind: Upstep to v1.6.2
    This also removes an old patch to fix atomic_ops for ARM pre-ARMv6.
    atomic_ops support has been replaced with C11 atomics, so the patch
    did not apply anymore, and should be unnecessary.
    
    This does mean compilation will fail with systems that don't support
    C11 atomics, i.e. GCC versions older than 4.9
    
    Signed-off-by: Frederik Van Bogaert <frederik.vanbogaert@mind.be>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/libunwind/libunwind.mk
+++ package/libunwind/libunwind.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBUNWIND_VERSION = 1.5.0
+LIBUNWIND_VERSION = 1.6.2
 LIBUNWIND_SITE = http://download.savannah.gnu.org/releases/libunwind
 LIBUNWIND_INSTALL_STAGING = YES
 LIBUNWIND_LICENSE_FILES = COPYING
@@ -15,9 +15,5 @@ LIBUNWIND_AUTORECONF = YES
 LIBUNWIND_CONF_OPTS = \
 	--disable-tests \
 	$(if $(BR2_INSTALL_LIBSTDCPP),--enable-cxx-exceptions,--disable-cxx-exceptions)
-
-ifeq ($(BR2_PACKAGE_LIBATOMIC_OPS),y)
-LIBUNWIND_DEPENDENCIES += libatomic_ops
-endif
 
 $(eval $(autotools-package))
