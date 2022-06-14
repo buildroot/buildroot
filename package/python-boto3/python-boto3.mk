a38da5e3cae6b786bb193d317a0e5e89466d9f6e
Raphaël Mélotte <raphael.melotte@mind.be>
2022-06-14 14:40:18 +0200

    package/python-boto3: bump to version 1.24.8
    Changelog: https://github.com/boto/boto3/blob/master/CHANGELOG.rst
    
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-boto3/python-boto3.mk
+++ package/python-boto3/python-boto3.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_BOTO3_VERSION = 1.19.2
+PYTHON_BOTO3_VERSION = 1.24.8
 PYTHON_BOTO3_SOURCE = boto3-$(PYTHON_BOTO3_VERSION).tar.gz
-PYTHON_BOTO3_SITE = https://files.pythonhosted.org/packages/ec/45/d12f9c09b5c4dad8bcb098dfae5e9e1253ed6408efbbd9a3e60bff55b824
+PYTHON_BOTO3_SITE = https://files.pythonhosted.org/packages/60/3b/6fed306341003fd059f402d22747ba33d07b8ac8bfe53dbedb2c6e42ee3b
 PYTHON_BOTO3_SETUP_TYPE = setuptools
 PYTHON_BOTO3_LICENSE = Apache-2.0
 PYTHON_BOTO3_LICENSE_FILES = LICENSE
