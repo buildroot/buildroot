8d6e07e525617ee0c4ccc4f07345b1b1a70526f7
James Hilliard <james.hilliard1@gmail.com>
2022-06-14 20:23:48 -0600

    package/{glibc, localedef}: bump to version 2.35-134-gb6aade18a7e5719c942aa2da6cf3157aca993fa4
    Contains various fixes some of which may effect pthread fork handling.
    
    Add patch reverting a commit breaking builds.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -7,7 +7,7 @@
 # Generate version string using:
 #   git describe --match 'glibc-*' --abbrev=40 origin/release/MAJOR.MINOR/master | cut -d '-' -f 2-
 # When updating the version, please also update localedef
-GLIBC_VERSION = 2.35-96-g2c4fc8e5ca742c6a3a1933799495bb0b00a807f0
+GLIBC_VERSION = 2.35-134-gb6aade18a7e5719c942aa2da6cf3157aca993fa4
 # Upstream doesn't officially provide an https download link.
 # There is one (https://sourceware.org/git/glibc.git) but it's not reliable,
 # sometimes the connection times out. So use an unofficial github mirror.
