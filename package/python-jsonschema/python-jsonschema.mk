1f6ab13194867272d4ec67d8e847c2a9e60d29f4
James Hilliard <james.hilliard1@gmail.com>
2022-06-15 17:17:30 -0600

    package/python-jsonschema: bump to version 4.5.1
    Add new python-pyrsistent runtime dependency.
    
    Drop unused host-python-vcversioner build dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-jsonschema/python-jsonschema.mk
+++ package/python-jsonschema/python-jsonschema.mk
@@ -4,12 +4,11 @@
 #
 ################################################################################
 
-PYTHON_JSONSCHEMA_VERSION = 2.5.1
+PYTHON_JSONSCHEMA_VERSION = 4.5.1
 PYTHON_JSONSCHEMA_SOURCE = jsonschema-$(PYTHON_JSONSCHEMA_VERSION).tar.gz
-PYTHON_JSONSCHEMA_SITE = https://pypi.python.org/packages/58/0d/c816f5ea5adaf1293a1d81d32e4cdfdaf8496973aa5049786d7fdb14e7e7
+PYTHON_JSONSCHEMA_SITE = https://files.pythonhosted.org/packages/9e/62/93a54db0e44c4de57868a7d638d7a8abce113c8bc43a20b10b1109b2a517
 PYTHON_JSONSCHEMA_SETUP_TYPE = setuptools
 PYTHON_JSONSCHEMA_LICENSE = MIT
 PYTHON_JSONSCHEMA_LICENSE_FILES = COPYING json/LICENSE
-PYTHON_JSONSCHEMA_DEPENDENCIES = host-python-vcversioner
 
 $(eval $(python-package))
