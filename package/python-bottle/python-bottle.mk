c25178ced95cd95cb5d802fef0d8580a6d9fd08e
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-06-15 23:07:24 +0200

    package/python-bottle: security bump to version 0.12.21
    - Fix CVE-2022-31799: Bottle before 0.12.20 mishandles errors during
      early request binding.
    - Update indentation in hash file (two spaces)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 5deea4be64a62e9b5fb4f0085b14d712e3921ac9)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-bottle/python-bottle.mk
+++ package/python-bottle/python-bottle.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_BOTTLE_VERSION = 0.12.19
+PYTHON_BOTTLE_VERSION = 0.12.21
 PYTHON_BOTTLE_SOURCE = bottle-$(PYTHON_BOTTLE_VERSION).tar.gz
-PYTHON_BOTTLE_SITE = https://files.pythonhosted.org/packages/ea/80/3d2dca1562ffa1929017c74635b4cb3645a352588de89e90d0bb53af3317
+PYTHON_BOTTLE_SITE = https://files.pythonhosted.org/packages/95/e3/5749d7657b6fb38d65afb3c0b345514a783de7a9feb4fab594fa0bacc2a0
 PYTHON_BOTTLE_LICENSE = MIT
 PYTHON_BOTTLE_LICENSE_FILES = LICENSE
 PYTHON_BOTTLE_CPE_ID_VENDOR = bottlepy
