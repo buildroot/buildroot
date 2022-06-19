4cdbec6e6ee75c90b4554d9bb0660ce2a4934f2e
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-06-19 18:42:24 +0200

    package/python-pylru: bump to version 1.2.1
    Update indentation in hash file (two spaces)
    
    https://github.com/jlhutch/pylru/compare/v1.2.0...v1.2.1
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-pylru/python-pylru.mk
+++ package/python-pylru/python-pylru.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYLRU_VERSION = 1.2.0
+PYTHON_PYLRU_VERSION = 1.2.1
 PYTHON_PYLRU_SOURCE = pylru-$(PYTHON_PYLRU_VERSION).tar.gz
-PYTHON_PYLRU_SITE = https://files.pythonhosted.org/packages/9c/88/30972cd0518452563221c80bffc2a5832499d736648ef8fe492affae15c5
+PYTHON_PYLRU_SITE = https://files.pythonhosted.org/packages/95/8e/2a0d3426738db0b41d69d36243bdd00420ad231e802d09dad8db02005d13
 PYTHON_PYLRU_SETUP_TYPE = setuptools
 PYTHON_PYLRU_LICENSE = GPL-2.0
 PYTHON_PYLRU_LICENSE_FILES = LICENSE.txt
