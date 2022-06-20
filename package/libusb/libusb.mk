b1fe952835ba0113f0f6f4f54686e29d6e1fc73b
Yegor Yefremov <yegorslists@googlemail.com>
2022-06-20 14:56:36 +0200

    package/libusb: bump to version 1.0.26
    Remove upstreamed patches.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    [Peter: drop _AUTORECONF]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libusb/libusb.mk
+++ package/libusb/libusb.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LIBUSB_VERSION_MAJOR = 1.0
-LIBUSB_VERSION = $(LIBUSB_VERSION_MAJOR).25
+LIBUSB_VERSION = $(LIBUSB_VERSION_MAJOR).26
 LIBUSB_SOURCE = libusb-$(LIBUSB_VERSION).tar.bz2
 LIBUSB_SITE = https://github.com/libusb/libusb/releases/download/v$(LIBUSB_VERSION)
 LIBUSB_LICENSE = LGPL-2.1+
@@ -13,8 +13,6 @@ LIBUSB_LICENSE_FILES = COPYING
 LIBUSB_CPE_ID_VENDOR = libusb
 LIBUSB_DEPENDENCIES = host-pkgconf
 LIBUSB_INSTALL_STAGING = YES
-# We're patching configure.ac
-LIBUSB_AUTORECONF = YES
 
 # Avoid the discovery of udev for the host variant
 HOST_LIBUSB_CONF_OPTS = --disable-udev
