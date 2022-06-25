c099842544248b10f4284a5d47084db8f476fcda
Леонид Юрьев (Leonid Yuriev) <leo@yuriev.ru>
2022-06-25 20:18:29 +0300

    package/libmdbx: bump version to 0.11.8
    This is stable bugfix release of libmdbx.
    The project's website now is on https://libmdbx.dqdkfa.ru/
    
    Release notes for v0.11.8 https://gitflic.ru/project/erthink/libmdbx/release/06268038-39ff-4270-9be8-9f26d5543015
    
    The complete ChangeLog: https://gitflic.ru/project/erthink/libmdbx/blob?file=ChangeLog.md
    
    Signed-off-by: Леонид Юрьев (Leonid Yuriev) <leo@yuriev.ru>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libmdbx/libmdbx.mk
+++ package/libmdbx/libmdbx.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-LIBMDBX_VERSION = 0.11.7
+LIBMDBX_VERSION = 0.11.8
 LIBMDBX_SOURCE = libmdbx-amalgamated-$(LIBMDBX_VERSION).tar.xz
-LIBMDBX_SITE = https://libmdbx.website.yandexcloud.net/release
+LIBMDBX_SITE = https://libmdbx.dqdkfa.ru/release
 LIBMDBX_SUPPORTS_IN_SOURCE_BUILD = NO
 LIBMDBX_LICENSE = OLDAP-2.8
 LIBMDBX_LICENSE_FILES = LICENSE
