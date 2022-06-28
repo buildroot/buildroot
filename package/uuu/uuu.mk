7d58c413940438262a94e8ed53a0784c53f869b2
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-06-28 18:40:38 +0200

    package/uuu: add host-openssl dependency
    Fix the following build failure raised since the addition of the package
    in commit 776385d645b37cfc43479b37e106244aa9ab2e85:
    
    CMake Error: The following variables are used in this project, but they are set to NOTFOUND.
    Please set them or make sure they are set and tested correctly in the CMake files:
    OPENSSL_CRYPTO_LIBRARY (ADVANCED)
        linked by target "uuu" in directory /home/buildroot/autobuild/instance-0/output-1/build/host-uuu-1.4.193/uuu
    OPENSSL_SSL_LIBRARY (ADVANCED)
        linked by target "uuu" in directory /home/buildroot/autobuild/instance-0/output-1/build/host-uuu-1.4.193/uuu
    
    Fixes:
     - http://autobuild.buildroot.org/results/2f05bc67112a59eba8f59ff707e43d76e41dbad1
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Reviewed-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/uuu/uuu.mk
+++ package/uuu/uuu.mk
@@ -9,6 +9,6 @@ UUU_SOURCE = uuu_source-$(UUU_VERSION).tar.gz
 UUU_SITE = https://github.com/NXPmicro/mfgtools/releases/download/uuu_$(UUU_VERSION)
 UUU_LICENSE = BSD 3-Clause "New" or "Revised" License
 UUU_LICENSE_FILES = LICENSE README.md
-HOST_UUU_DEPENDENCIES = host-bzip2 host-zlib host-libusb
+HOST_UUU_DEPENDENCIES = host-bzip2 host-openssl host-zlib host-libusb
 
 $(eval $(host-cmake-package))
