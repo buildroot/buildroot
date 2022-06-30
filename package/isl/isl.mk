d932260f8fa95a9c27fb011943ceee5a29f6ebf6
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-07-01 00:02:48 +0200

    package/isl: bump to version 0.24
    https://repo.or.cz/isl.git/blob/3ea3e8b77475bea49b35b8215d8f66af94121b3c:/ChangeLog
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/isl/isl.mk
+++ package/isl/isl.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ISL_VERSION = 0.23
+ISL_VERSION = 0.24
 ISL_SOURCE = isl-$(ISL_VERSION).tar.xz
 ISL_SITE = https://libisl.sourceforge.io
 ISL_LICENSE = MIT
