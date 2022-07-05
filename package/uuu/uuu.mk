f414afe53d5f050cf42173d7870791013a958919
Dario Binacchi <dario.binacchi@amarulasolutions.com>
2022-07-05 08:16:27 +0200

    package/uuu: bump to version 1.4.224
    It requires the host-zstd package.
    
    The hash of README.md changed due to changes unrelated to the license
    terms:
    
    -- `sudo apt-get install libusb-1.0-0-dev libbz2-dev pkg-config cmake libssl-dev g++`
    +- `sudo apt-get install libusb-1.0-0-dev libbz2-dev libzstd-dev pkg-config cmake libssl-dev g++`
    
    Signed-off-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/uuu/uuu.mk
+++ package/uuu/uuu.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-UUU_VERSION = 1.4.193
+UUU_VERSION = 1.4.224
 UUU_SOURCE = uuu_source-$(UUU_VERSION).tar.gz
 UUU_SITE = https://github.com/NXPmicro/mfgtools/releases/download/uuu_$(UUU_VERSION)
 UUU_LICENSE = BSD 3-Clause "New" or "Revised" License
 UUU_LICENSE_FILES = LICENSE README.md
-HOST_UUU_DEPENDENCIES = host-bzip2 host-openssl host-zlib host-libusb
+HOST_UUU_DEPENDENCIES = host-bzip2 host-openssl host-zlib host-libusb host-zstd
 
 $(eval $(host-cmake-package))
