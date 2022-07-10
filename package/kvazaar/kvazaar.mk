91923baf49f358160fe6e0850bdba3d03329a7f5
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-07-10 23:28:21 +0200

    package/kvazaar: bump to version 2.1.0
    - Use official tarball and so drop autoreconf
    - License is BSD-3-Clause and ISC since
      https://github.com/ultravideo/kvazaar/commit/57883369ca05bee0dbc0b36fb2c78985f906a523
    - Update indentation in hash file (two spaces)
    
    https://github.com/ultravideo/kvazaar/releases/tag/v2.1.0
    https://github.com/ultravideo/kvazaar/releases/tag/v2.0.0
    https://github.com/ultravideo/kvazaar/releases/tag/v1.3.0
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/kvazaar/kvazaar.mk
+++ package/kvazaar/kvazaar.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-KVAZAAR_VERSION = 1.2.0
-KVAZAAR_SITE = $(call github,ultravideo,kvazaar,v$(KVAZAAR_VERSION))
-KVAZAAR_LICENSE = LGPL-2.1+
-KVAZAAR_LICENSE_FILES = COPYING
-KVAZAAR_AUTORECONF = YES
+KVAZAAR_VERSION = 2.1.0
+KVAZAAR_SOURCE = kvazaar-$(KVAZAAR_VERSION).tar.xz
+KVAZAAR_SITE = https://github.com/ultravideo/kvazaar/releases/download/v$(KVAZAAR_VERSION)
+KVAZAAR_LICENSE = BSD-3-Clause, ISC (greatest, x264asm)
+KVAZAAR_LICENSE_FILES = LICENSE LICENSE.greatest LICENSE.x264asm
 KVAZAAR_INSTALL_STAGING = YES
 KVAZAAR_DEPENDENCIES = host-pkgconf
 KVAZAAR_CONF_OPTS = --without-cryptopp
