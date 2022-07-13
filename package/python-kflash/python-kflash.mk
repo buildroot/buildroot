bf38ea9c41a338444f1079da156eec97dc49fbbc
Damien Le Moal <damien.lemoal@opensource.wdc.com>
2022-07-13 17:46:48 +0900

    package/python-kflash: Update to version 1.1.6
    Update the python-kflash package to the new verseion 1.1.6 (from 1.1.5).
    
    Signed-off-by: Damien Le Moal <damien.lemoal@opensource.wdc.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-kflash/python-kflash.mk
+++ package/python-kflash/python-kflash.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_KFLASH_VERSION = 1.1.5
+PYTHON_KFLASH_VERSION = 1.1.6
 PYTHON_KFLASH_SOURCE = kflash-$(PYTHON_KFLASH_VERSION).tar.gz
-PYTHON_KFLASH_SITE = https://files.pythonhosted.org/packages/c4/b3/1c25a92922bfd3dbc05d972352c071167cad035c615f6f068f75c2f0fd01
+PYTHON_KFLASH_SITE = https://files.pythonhosted.org/packages/4f/80/d2eaa114bf4434b18fcf8960a907ffce12c9400b08c05902b36007d545b7
 PYTHON_KFLASH_SETUP_TYPE = setuptools
 PYTHON_KFLASH_LICENSE = MIT
 PYTHON_KFLASH_LICENSE_FILES = LICENSE
