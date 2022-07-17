cb433ecfd066db576c2bc59d2d42b6ea092b7caa
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-07-18 00:22:33 +0200

    package/crucible: fix CRUCIBLE_VERSION
    Make CRUCIBLE_VERSION compatible with https://release-monitoring.org
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/crucible/crucible.mk
+++ package/crucible/crucible.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-CRUCIBLE_VERSION = v2022.05.25
-CRUCIBLE_SITE = $(call github,usbarmory,crucible,$(CRUCIBLE_VERSION))
+CRUCIBLE_VERSION = 2022.05.25
+CRUCIBLE_SITE = $(call github,usbarmory,crucible,v$(CRUCIBLE_VERSION))
 CRUCIBLE_LICENSE = GPL-3.0
 CRUCIBLE_LICENSE_FILES = LICENSE
 CRUCIBLE_GOMOD = ./cmd/crucible
