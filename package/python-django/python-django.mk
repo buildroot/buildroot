3e4f6e1b205a8e3ebb3c82ac704627618a8400a3
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-07-19 00:11:02 +0200

    package/python-django: security bump to version 4.0.6
    Fix CVE-2022-34265: An issue was discovered in Django 3.2 before 3.2.14
    and 4.0 before 4.0.6. The Trunc() and Extract() database functions are
    subject to SQL injection if untrusted data is used as a kind/lookup_name
    value. Applications that constrain the lookup name and kind choice to a
    known safe list are unaffected.
    
    https://www.djangoproject.com/weblog/2022/jul/04/security-releases
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 4.0.4
+PYTHON_DJANGO_VERSION = 4.0.6
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/d2/95/93d1f75da95624bf89e373d079fa72debf77f9b10acc31998cc52a5ff3f9
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/a4/17/b10aa26d7a566a3c19e9d29fac39c8643cbceb6cd7649a378d676839b5db
 
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
