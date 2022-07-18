462f132c7310eebbcbf516846d90738b571259ce
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-07-19 00:22:08 +0200

    package/python-smmap2: bump to version 5.0.0
    Update indentation in hash file (two spaces)
    
    https://github.com/gitpython-developers/smmap/blob/v5.0.0/doc/source/changes.rst
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/python-smmap2/python-smmap2.mk
+++ package/python-smmap2/python-smmap2.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SMMAP2_VERSION = 4.0.0
+PYTHON_SMMAP2_VERSION = 5.0.0
 PYTHON_SMMAP2_SOURCE = smmap-$(PYTHON_SMMAP2_VERSION).tar.gz
-PYTHON_SMMAP2_SITE = https://files.pythonhosted.org/packages/dd/d4/2b4f196171674109f0fbb3951b8beab06cd0453c1b247ec0c4556d06648d
+PYTHON_SMMAP2_SITE = https://files.pythonhosted.org/packages/21/2d/39c6c57032f786f1965022563eec60623bb3e1409ade6ad834ff703724f3
 PYTHON_SMMAP2_SETUP_TYPE = setuptools
 PYTHON_SMMAP2_LICENSE = BSD-3-Clause
 PYTHON_SMMAP2_LICENSE_FILES = LICENSE
