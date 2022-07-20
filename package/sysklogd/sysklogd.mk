b467aa1cd6a5d341dfc2bbdfb75ecf409ec2493d
Joachim Wiberg <troglobit@gmail.com>
2022-07-20 12:13:06 +0200

    package/sysklogd: install to staging, provides syslog() replacement
    Since v2.0 the sysklogd project has provided an interposing library
    libsyslog.{a,so} with a syslog() replacement, including the NetBSD
    syslogp() API for RFC5424 logging.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/sysklogd/sysklogd.mk
+++ package/sysklogd/sysklogd.mk
@@ -8,6 +8,7 @@ SYSKLOGD_VERSION = 2.4.0
 SYSKLOGD_SITE = https://github.com/troglobit/sysklogd/releases/download/v$(SYSKLOGD_VERSION)
 SYSKLOGD_LICENSE = BSD-3-Clause
 SYSKLOGD_LICENSE_FILES = LICENSE
+SYSKLOGD_INSTALL_STAGING = YES
 SYSKLOGD_CPE_ID_VENDOR = sysklogd_project
 
 # Busybox install logger in /usr/bin, and syslogd in /sbin, so install in
