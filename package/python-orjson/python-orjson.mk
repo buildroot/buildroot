4dfa809c2cdcb6994e5462e6e8f3087ae3055861
James Hilliard <james.hilliard1@gmail.com>
2022-07-24 14:10:41 +0200

    package/python-orjson: bump to 3.7.7
    And switch to PyPi to fetch the source code.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-orjson/python-orjson.mk
+++ package/python-orjson/python-orjson.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ORJSON_VERSION = 3.6.8
-PYTHON_ORJSON_SITE = $(call github,ijl,orjson,$(PYTHON_ORJSON_VERSION))
+PYTHON_ORJSON_VERSION = 3.7.7
+PYTHON_ORJSON_SOURCE = orjson-$(PYTHON_ORJSON_VERSION).tar.gz
+PYTHON_ORJSON_SITE = https://files.pythonhosted.org/packages/8a/68/47bc5e624431d77445f06a2d3ba32777fda650f05a2f803ea1dc2b07b65a
 PYTHON_ORJSON_SETUP_TYPE = pep517
 PYTHON_ORJSON_LICENSE = Apache-2.0 or MIT
 PYTHON_ORJSON_LICENSE_FILES = LICENSE-APACHE LICENSE-MIT
