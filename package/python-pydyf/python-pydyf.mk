2ffabc24627c38d1572a0cf702dd6d1e5d47d839
James Hilliard <james.hilliard1@gmail.com>
2022-07-23 21:29:29 -0600

    package/python-pydyf: bump to version 0.2.0
    Migrate from distutils to flit build system.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/python-pydyf/python-pydyf.mk
+++ package/python-pydyf/python-pydyf.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_PYDYF_VERSION = 0.1.2
+PYTHON_PYDYF_VERSION = 0.2.0
 PYTHON_PYDYF_SOURCE = pydyf-$(PYTHON_PYDYF_VERSION).tar.gz
-PYTHON_PYDYF_SITE = https://files.pythonhosted.org/packages/78/ed/2ccc153d50d21a56916fd5c9d367cad798d4ca8a450cef03e7faa3b920c4
-PYTHON_PYDYF_SETUP_TYPE = distutils
+PYTHON_PYDYF_SITE = https://files.pythonhosted.org/packages/3a/5e/4d4f5f77c706b0b871652cb4ccb98a52647ce917168a48e2b8cae742da1e
+PYTHON_PYDYF_SETUP_TYPE = flit
 PYTHON_PYDYF_LICENSE = BSD-3-Clause
 PYTHON_PYDYF_LICENSE_FILES = LICENSE
 
