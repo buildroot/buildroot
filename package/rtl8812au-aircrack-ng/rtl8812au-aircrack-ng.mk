f0e7c687f7fddb47b75740b8278a4ff95aa28336
Christian Stewart <christian@paral.in>
2022-07-25 00:10:58 -0700

    package/rtl8812au-aircrack-ng: bump to latest 4ab079
    Bump to the latest commit hash as of 2022-07-25.
    
    Fixes the build against kernel 5.18.x and GCC 12 and ARM64.
    
    Signed-off-by: Christian Stewart <christian@paral.in>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/rtl8812au-aircrack-ng/rtl8812au-aircrack-ng.mk
+++ package/rtl8812au-aircrack-ng/rtl8812au-aircrack-ng.mk
@@ -4,15 +4,21 @@
 #
 ################################################################################
 
-RTL8812AU_AIRCRACK_NG_VERSION = 37e27f9165300c89607144b646545fac576ec510
+RTL8812AU_AIRCRACK_NG_VERSION = 4ab079f7cb172740c7abc8cbd6e6383bef0f65dc
 RTL8812AU_AIRCRACK_NG_SITE = $(call github,aircrack-ng,rtl8812au,$(RTL8812AU_AIRCRACK_NG_VERSION))
 RTL8812AU_AIRCRACK_NG_LICENSE = GPL-2.0
 RTL8812AU_AIRCRACK_NG_LICENSE_FILES = LICENSE
 
+RTL8812AU_AIRCRACK_NG_EXTRA_CFLAGS = \
+	-DCONFIG_$(call qstrip,$(BR2_ENDIAN))_ENDIAN \
+	-Wno-error=address \
+	-Wno-error=array-bounds \
+	-Wno-error=cast-function-type
+
 RTL8812AU_AIRCRACK_NG_MODULE_MAKE_OPTS = \
 	CONFIG_88XXAU=m \
 	KVER=$(LINUX_VERSION_PROBED) \
-	USER_EXTRA_CFLAGS="-DCONFIG_$(call qstrip,$(BR2_ENDIAN))_ENDIAN"
+	USER_EXTRA_CFLAGS="$(RTL8812AU_AIRCRACK_NG_EXTRA_CFLAGS)"
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
