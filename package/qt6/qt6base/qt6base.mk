9522dde0708fc49c337e94b0653a41a83acb93ac
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2022-07-25 17:25:20 +0200

    package/qt6/qt6base: new package
    This commit proposes a very minimal package for qt6base. It only
    supports building QtCore, so it *really* is minimal. But that's a
    starting point, which we can progressively build on top. It is based
    on initial work from Peter Seiderer.
    
    This minimal QtCore build is however sufficient to build and run
    simple non-graphical Qt applications.
    
    A number of comments:
    
     - Even though there's only qt6base for now, many other qt6 modules
       will come later on, which is why we're using the same structure as
       for qt5, with a subdir for package/qt6/
    
     - Qt6 is mutually exclusive with Qt5. Even though the library names
       on the target and the location of the header files are distinct,
       the host tools (qmake, moc and al.) have the same name, so at least
       for now, we make them mutually exclusive.
    
     - We've chosen to use non-bundled libraries for zlib, bb2,
       double-conversion and pcre2, for both the target and the host
       qt6base packages.
    
     - Contrary to qt5 where the target package was building the host
       tools, now we have a host qt6base package building the host tools,
       and which is needed as a dependency for the target qt6base package.
    
     - qt6base is using CMake. However, it strongly recommends to use
       Ninja as a backend instead of make, a recommendation that we follow
       in this commit. Since we don't have support for Ninja in the
       cmake-package infrastructure (yet), we do this manually in
       qt6base.mk itself, by passing -Gninja to CMake at configure time,
       and then by using cmake --build at build time and cmake --install
       at install time, using explicitly provided build and install
       commands. Hopefully these can go away once we have support for
       Ninja directly in cmake-package.
    
     - We disable a number of features or external libraries using FEATURE
       options. However, because there are over 400 FEATURE options in
       qt6base, we didn't go all the way to explicitly disabling *all* of
       them (which would be needed for both the host and target
       packages). We expect that this list of explicit FEATURE options
       disabling will need to grow based on the feedback of users and
       issues encountered.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

---
+++ package/qt6/qt6base/qt6base.mk
@@ -0,0 +1,79 @@
+################################################################################
+#
+# qt6base
+#
+################################################################################
+
+QT6BASE_VERSION = $(QT6_VERSION)
+QT6BASE_SITE = $(QT6_SITE)
+QT6BASE_SOURCE = qtbase-$(QT6_SOURCE_TARBALL_PREFIX)-$(QT6BASE_VERSION).tar.xz
+QT6BASE_DEPENDENCIES = \
+	host-ninja \
+	host-qt6base \
+	double-conversion \
+	libb2 \
+	pcre2 \
+	zlib
+QT6BASE_INSTALL_STAGING = YES
+
+QT6BASE_CONF_OPTS = \
+	-GNinja \
+	-DQT_HOST_PATH=$(HOST_DIR) \
+	-DFEATURE_gui=OFF \
+	-DFEATURE_concurrent=OFF \
+	-DFEATURE_xml=OFF \
+	-DFEATURE_sql=OFF \
+	-DFEATURE_testlib=OFF \
+	-DFEATURE_network=OFF \
+	-DFEATURE_dbus=OFF \
+	-DFEATURE_icu=OFF \
+	-DFEATURE_glib=OFF \
+	-DFEATURE_system_doubleconversion=ON \
+	-DFEATURE_system_pcre2=ON \
+	-DFEATURE_system_zlib=ON \
+	-DFEATURE_system_libb2=ON
+
+define QT6BASE_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(BR2_CMAKE) --build $(QT6BASE_BUILDDIR)
+endef
+
+define QT6BASE_INSTALL_STAGING_CMDS
+	$(TARGET_MAKE_ENV) $(BR2_CMAKE) --install $(QT6BASE_BUILDDIR) --prefix $(STAGING_DIR)/usr
+endef
+
+define QT6BASE_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(BR2_CMAKE) --install $(QT6BASE_BUILDDIR) --prefix $(TARGET_DIR)/usr
+endef
+
+HOST_QT6BASE_DEPENDENCIES = \
+	host-ninja \
+	host-double-conversion \
+	host-libb2 \
+	host-pcre2 \
+	host-zlib
+HOST_QT6BASE_CONF_OPTS = \
+	-GNinja \
+	-DFEATURE_gui=OFF \
+	-DFEATURE_concurrent=OFF \
+	-DFEATURE_xml=ON \
+	-DFEATURE_sql=OFF \
+	-DFEATURE_testlib=OFF \
+	-DFEATURE_network=OFF \
+	-DFEATURE_dbus=OFF \
+	-DFEATURE_icu=OFF \
+	-DFEATURE_glib=OFF \
+	-DFEATURE_system_doubleconversion=ON \
+	-DFEATURE_system_libb2=ON \
+	-DFEATURE_system_pcre2=ON \
+	-DFEATURE_system_zlib=ON
+
+define HOST_QT6BASE_BUILD_CMDS
+	$(HOST_MAKE_ENV) $(BR2_CMAKE) --build $(HOST_QT6BASE_BUILDDIR)
+endef
+
+define HOST_QT6BASE_INSTALL_CMDS
+	$(HOST_MAKE_ENV) $(BR2_CMAKE) --install $(HOST_QT6BASE_BUILDDIR)
+endef
+
+$(eval $(cmake-package))
+$(eval $(host-cmake-package))
