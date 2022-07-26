a830961a589c793b4350fea5bd17b6210ae7adf2
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2022-07-26 10:14:32 +0200

    package/avocado: fix license
    In commit ef321ff4a90acf2133ee29d5fd2a65fc9906c748 ("package/avocado:
    new package") the license information that was introduced was
    incorrect. The license is GPL-2.0, not Apache-2.0.
    
    According to the LICENSE file:
    
    Most files in Avocado are released under the GPLv2 with a "and any later
    version" clause (GPLv2+), but some files are explicitly bound to the
    version 2 of the license and therefore that's the official license of
    the project itself. The header on each individual file contains
    information about the GPL version that applies to it.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/avocado/avocado.mk
+++ package/avocado/avocado.mk
@@ -7,7 +7,7 @@
 AVOCADO_VERSION = 98.0
 AVOCADO_SITE = $(call github,avocado-framework,avocado,$(AVOCADO_VERSION))
 AVOCADO_SETUP_TYPE = setuptools
-AVOCADO_LICENSE = Apache-2.0
+AVOCADO_LICENSE = GPL-2.0
 AVOCADO_LICENSE_FILES = LICENSE
 
 $(eval $(python-package))
