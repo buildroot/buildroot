b9a38a42ac817b963be2b4b51d218c5eabb1059d
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2022-07-27 15:23:25 +0200

    package/postgis: add optional dependency on gdal
    With the gdal package added, we can add optional support for gdal in
    postgis. This needs a small patch, which is in fact similar to the
    existing patch, just for a different part of the postgis source
    code. This new patch has been submitted upstream.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/postgis/postgis.mk
+++ package/postgis/postgis.mk
@@ -19,14 +19,20 @@ POSTGIS_DEPENDENCIES = postgresql libgeos proj libxml2
 POSTGIS_CONF_OPTS += \
 	--with-pgconfig=$(STAGING_DIR)/usr/bin/pg_config \
 	--with-geosconfig=$(STAGING_DIR)/usr/bin/geos-config \
-	--with-xml2config=$(STAGING_DIR)/usr/bin/xml2-config \
-	--without-raster
+	--with-xml2config=$(STAGING_DIR)/usr/bin/xml2-config
 
 ifeq ($(BR2_PACKAGE_JSON_C),y)
 POSTGIS_DEPENDENCIES += json-c
 POSTGIS_CONF_OPTS += --with-json
 else
 POSTGIS_CONF_OPTS += --without-json
+endif
+
+ifeq ($(BR2_PACKAGE_GDAL),y)
+POSTGIS_DEPENDENCIES += gdal
+POSTGIS_CONF_OPTS += --with-raster --with-gdalconfig=$(STAGING_DIR)/usr/bin/gdal-config
+else
+POSTGIS_CONF_OPTS += --without-raster
 endif
 
 ifeq ($(BR2_PACKAGE_PCRE),y)
