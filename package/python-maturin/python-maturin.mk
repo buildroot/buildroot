6cc84f8c20861f86164239baa727d8d8f37b8b8e
James Hilliard <james.hilliard1@gmail.com>
2022-08-06 18:38:29 -0600

    package/python-maturin: migrate to setuptools-rust infrastructure
    The python-maturin build backend while itself is a pep517 backend
    uses setuptools-rust for bootstrapping purposes.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/python-maturin/python-maturin.mk
+++ package/python-maturin/python-maturin.mk
@@ -7,18 +7,9 @@
 PYTHON_MATURIN_VERSION = 1.1.0
 PYTHON_MATURIN_SOURCE = maturin-$(PYTHON_MATURIN_VERSION).tar.gz
 PYTHON_MATURIN_SITE = https://files.pythonhosted.org/packages/7b/78/2814bc0e46a96861aab337fd0404e7937acdb4ce187da239224ab4560d35
-PYTHON_MATURIN_SETUP_TYPE = setuptools
+PYTHON_MATURIN_SETUP_TYPE = setuptools-rust
 PYTHON_MATURIN_LICENSE = Apache-2.0 or MIT
 PYTHON_MATURIN_LICENSE_FILES = license-apache license-mit
-HOST_PYTHON_MATURIN_DEPENDENCIES = \
-	host-python-setuptools-rust \
-	host-rustc
-HOST_PYTHON_MATURIN_ENV = \
-	$(HOST_PKG_CARGO_ENV) \
-	PYO3_CROSS_LIB_DIR="$(HOST_DIR)/lib/python$(PYTHON3_VERSION_MAJOR)"
-# We need to vendor the Cargo crates at download time
-PYTHON_MATURIN_DOWNLOAD_POST_PROCESS = cargo
-PYTHON_MATURIN_DOWNLOAD_DEPENDENCIES = host-rustc
-HOST_PYTHON_MATURIN_DL_ENV = $(HOST_PKG_CARGO_ENV)
+HOST_PYTHON_MATURIN_DEPENDENCIES = host-python-tomli
 
 $(eval $(host-python-package))
