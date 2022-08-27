0a9ca5150f3aed597f1a4aa01f4c3b3a83daed3d
Paul Cercueil <paul@crapouillou.net>
2022-08-27 10:20:55 +0100

    od: rs90: Only start USB at init on RS90
    On the RS90 the USB needs to be started at bootup, because it lacks the
    USB cable detection facility. On the RG99 USB daemons can be started
    automatically by the udev rule.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- board/opendingux/rs90/overlay/etc/init.d/S98usb.sh
+++ board/opendingux/rs90/overlay/etc/init.d/S98usb.sh
@@ -2,6 +2,19 @@
 
 USB_MODE=mtp
 
+MODEL=`sed -n 's/\(.*\)ingenic.*/\1/p' /sys/firmware/devicetree/base/compatible`
+
+case "$MODEL" in
+	# Only the RS90 needs to start USB at bootup, because it does not have
+	# USB cable detection.
+	ylm,rs90)
+		break
+		;;
+	*)
+		exit 0
+		;;
+esac
+
 [ -r /usr/local/etc/usb.conf ] && . /usr/local/etc/usb.conf
 
 case "$1" in
@@ -19,6 +32,6 @@ case "$1" in
 		/usr/sbin/usb $1 $USB_MODE
 		;;
 	*)
-		echo "Usage: S15configfs.sh [start|stop|status]"
+		echo "Usage: S98usb.sh [start|stop|status]"
 		;;
 esac
