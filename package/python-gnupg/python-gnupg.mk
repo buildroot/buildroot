0418810e7eb23875ccd2c9ba1bbc32e0ba90d45c
Julien Olivain <ju.o@free.fr>
2022-08-29 21:58:31 +0200

    package/python-gnupg: bump to version 0.5.0
    For change log, see:
    https://github.com/vsajip/python-gnupg/releases/tag/0.5.0
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-gnupg/python-gnupg.mk
+++ package/python-gnupg/python-gnupg.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PYTHON_GNUPG_VERSION = 0.4.9
-PYTHON_GNUPG_SITE = https://files.pythonhosted.org/packages/c8/cb/46fb80639cf0dd4251aeb075a1a5e2ebbb8c9656f28ddfe9d8c99b68b6da
+PYTHON_GNUPG_VERSION = 0.5.0
+PYTHON_GNUPG_SITE = https://files.pythonhosted.org/packages/e3/5c/5ff9877001616912a74f4377cd5f80925b31a678087800beae5b28bdb80e
 PYTHON_GNUPG_LICENSE = BSD-3-Clause
 PYTHON_GNUPG_LICENSE_FILES = LICENSE.txt
 PYTHON_GNUPG_CPE_ID_VENDOR = python
