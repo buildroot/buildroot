68c32ce33806c5e9ffc89851e28e6a817d7e9706
Petr Vorel <petr.vorel@gmail.com>
2022-09-03 00:19:36 +0200

    package/ltp-testsuite: build getcontext01.c on musl
    It was fixed for musl during conversion to the new API in 0f519d0da
    (released in 20220527).
    
    Signed-off-by: Petr Vorel <petr.vorel@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/ltp-testsuite/ltp-testsuite.mk
+++ package/ltp-testsuite/ltp-testsuite.mk
@@ -81,7 +81,6 @@ LTP_TESTSUITE_UNSUPPORTED_TEST_CASES_$(BR2_TOOLCHAIN_USES_UCLIBC) += \
 
 LTP_TESTSUITE_UNSUPPORTED_TEST_CASES_$(BR2_TOOLCHAIN_USES_MUSL) += \
 	testcases/kernel/syscalls/fmtmsg/fmtmsg01.c \
-	testcases/kernel/syscalls/getcontext/getcontext01.c \
 	testcases/kernel/syscalls/rt_tgsigqueueinfo/rt_tgsigqueueinfo01.c \
 	testcases/kernel/syscalls/timer_create/timer_create01.c \
 	testcases/kernel/syscalls/timer_create/timer_create03.c
