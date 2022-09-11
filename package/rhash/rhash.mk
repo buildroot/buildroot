95c2493a8e52c32ce97f0a1348bfc71dcb2b28b9
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-09-11 14:55:33 +0200

    package/rhash: bump to version 1.4.3
    https://github.com/rhash/RHash/releases/tag/v1.4.3
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/rhash/rhash.mk
+++ package/rhash/rhash.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RHASH_VERSION = 1.4.2
+RHASH_VERSION = 1.4.3
 RHASH_SOURCE = rhash-$(RHASH_VERSION)-src.tar.gz
 RHASH_SITE = https://sourceforge.net/projects/rhash/files/rhash/$(RHASH_VERSION)
 RHASH_LICENSE = 0BSD
