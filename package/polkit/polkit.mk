c2f642256395afdd51946722bd0c37c380a3d9ad
Lang Daniel <d.lang@abatec.at>
2022-09-12 06:11:00 +0000

    package/polkit: improve sysv script
    As specified in the manual (18.5 The SNNfoo start script),
    the name of the script should reflect the name of the daemon,
    which in this case in polkitd.
    
    Also remove the redundant/broken reload case.
    
    Signed-off-by: Daniel Lang <d.lang@abatec.at>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/polkit/polkit.mk
+++ package/polkit/polkit.mk
@@ -68,8 +68,8 @@ define POLKIT_INSTALL_INIT_SYSTEMD
 endef
 
 define POLKIT_INSTALL_INIT_SYSV
-	$(INSTALL) -D -m 0755 package/polkit/S50polkit \
-		$(TARGET_DIR)/etc/init.d/S50polkit
+	$(INSTALL) -D -m 0755 package/polkit/S50polkitd \
+		$(TARGET_DIR)/etc/init.d/S50polkitd
 endef
 
 $(eval $(meson-package))
