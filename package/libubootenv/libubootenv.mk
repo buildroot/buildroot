44767bb7dda4f45ee0c00c3223ed0ddf2ebb09ad
Lang Daniel <d.lang@abatec.at>
2022-09-20 07:52:29 +0000

    package/libubootenv: bump to version 0.3.3
    Signed-off-by: Daniel Lang <d.lang@abatec.at>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libubootenv/libubootenv.mk
+++ package/libubootenv/libubootenv.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-LIBUBOOTENV_VERSION = 7dbfffa4cc0e42ad3febb122a711fe9d1b20e9f7
-LIBUBOOTENV_SITE = $(call github,sbabic,libubootenv,$(LIBUBOOTENV_VERSION))
+LIBUBOOTENV_VERSION = 0.3.3
+LIBUBOOTENV_SITE = $(call github,sbabic,libubootenv,v$(LIBUBOOTENV_VERSION))
 LIBUBOOTENV_LICENSE = LGPL-2.1+, MIT, CC0-1.0
 LIBUBOOTENV_LICENSE_FILES = LICENSES/CC0-1.0.txt \
 	LICENSES/LGPL-2.1-or-later.txt \
