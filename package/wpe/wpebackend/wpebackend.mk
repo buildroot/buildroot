31ae301bd7cd8d9068607d76dc5f169b08328019
Adrian Perez de Castro <aperez@igalia.com>
2022-09-22 13:54:30 +0300

    package/wpe/wpebackend: bring closer to upstream
    Add missing dependencies in the Config fragment, add licensing terms
    and conditionals from the upstream libwpe recipe. Having build recipes
    closer to the upstream ones eases maintenance going forward.
    
    Signed-off-by: Adrian Perez de Castro <aperez@igalia.com>

--- package/wpe/wpebackend/wpebackend.mk
+++ package/wpe/wpebackend/wpebackend.mk
@@ -15,9 +15,15 @@ WPEBACKEND_VERSION = 4be4c7df5734d125148367a90da477c8d40d9eaf
 WPEBACKEND_SITE = $(call github,WebPlatformForEmbedded,WPEBackend,$(WPEBACKEND_VERSION))
 endif
 WPEBACKEND_INSTALL_STAGING = YES
-WPEBACKEND_DEPENDENCIES += libegl libxkbcommon
+WPEBACKEND_LICENSE = BSD-2-Clause
+WPEBACKEND_LICENSE_FILES = COPYING
+WPEBACKEND_DEPENDENCIES = libegl libxkbcommon
+
+# Workaround for https://github.com/raspberrypi/userland/issues/316
+ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
 WPEBACKEND_CONF_OPTS += \
 	-DCMAKE_C_FLAGS="$(TARGET_CFLAGS) -D_GNU_SOURCE" \
 	-DCMAKE_CXX_FLAGS="$(TARGET_CXXFLAGS) -D_GNU_SOURCE"
+endif
 
 $(eval $(cmake-package))
