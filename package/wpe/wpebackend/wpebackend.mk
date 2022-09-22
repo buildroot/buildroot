20b41c3bad28dd37cdbf20f4ddd69af240987346
Adrian Perez de Castro <aperez@igalia.com>
2022-09-22 14:15:52 +0300

    package/wpe/wpebackend: add wpewebkit 2.38 variant
    Add a BR2_PACKAGE_WPEBACKEND_LIBWPE_VERSION symbol to the Config
    fragment which contains a string value with the libwpe to be used
    with the chosen wpewebkit version. This contains the "1.14.0" value
    when building wpewebkit 2.38.x, and "1.6.0" (the existing version)
    for 2.28.x. Using a Config symbol avoids piling up conditionals in
    the Make fragment, which is easier to maintain.
    
    Signed-off-by: Adrian Perez de Castro <aperez@igalia.com>

--- package/wpe/wpebackend/wpebackend.mk
+++ package/wpe/wpebackend/wpebackend.mk
@@ -4,15 +4,13 @@
 #
 ################################################################################
 
-ifeq ($(BR2_PACKAGE_WPEWEBKIT2_28),y)
-WPEBACKEND_VERSION = 1.6.0
-WPEBACKEND_SITE = https://wpewebkit.org/releases
-WPEBACKEND_SOURCE = libwpe-$(WPEBACKEND_VERSION).tar.xz
-WPEBACKEND_LICENSE = BSD-2-Clause
-WPEBACKEND_LICENSE_FILES = COPYING
-else
+ifeq ($(BR2_PACKAGE_WPEWEBKIT2_22),y)
 WPEBACKEND_VERSION = 4be4c7df5734d125148367a90da477c8d40d9eaf
 WPEBACKEND_SITE = $(call github,WebPlatformForEmbedded,WPEBackend,$(WPEBACKEND_VERSION))
+else
+WPEBACKEND_VERSION = $(call qstrip,$(BR2_PACKAGE_WPEBACKEND_LIBWPE_VERSION))
+WPEBACKEND_SITE = https://wpewebkit.org/releases
+WPEBACKEND_SOURCE = libwpe-$(WPEBACKEND_VERSION).tar.xz
 endif
 WPEBACKEND_INSTALL_STAGING = YES
 WPEBACKEND_LICENSE = BSD-2-Clause
