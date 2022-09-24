7677520a0d52e98fc2097df1c6f2c4aa46fce757
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-09-24 22:51:48 +0200

    package/forge: add FORGE_CPE_ID_VENDOR
    cpe:2.3:a:digitalbazaar:forge is a valid CPE identifier for this package:
    
      https://nvd.nist.gov/products/cpe/search/results?namingFormat=2.3&keyword=cpe%3A2.3%3Aa%3Adigitalbazaar%3Aforge
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/forge/forge.mk
+++ package/forge/forge.mk
@@ -9,6 +9,7 @@ FORGE_SITE = https://registry.npmjs.org/node-forge/-
 FORGE_SOURCE = node-forge-$(FORGE_VERSION).tgz
 FORGE_LICENSE = BSD-3-Clause, GPL-2.0, vendored dependencies licenses probably not listed
 FORGE_LICENSE_FILES = LICENSE
+FORGE_CPE_ID_VENDOR = digitalbazaar
 
 # Install .min.js as .js
 define FORGE_INSTALL_TARGET_CMDS
