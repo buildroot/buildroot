406a12176353e8e9950cdb6ef98626e7f55bf1ba
Adrian Perez de Castro <aperez@igalia.com>
2022-09-27 10:23:01 +0300

    package/wpebackend-fdo: add wpewebkit 2.38 variant
    Build wpebackend-fdo 1.14.x when targeting WebKit 2.38, and fix the
    name of the dependency: the package for libwpe is called wpebackend
    in this fork.
    
    Signed-off-by: Adrian Perez de Castro <aperez@igalia.com>

--- package/wpebackend-fdo/wpebackend-fdo.mk
+++ package/wpebackend-fdo/wpebackend-fdo.mk
@@ -4,12 +4,17 @@
 #
 ################################################################################
 
+ifeq ($(BR2_PACKAGE_WPEWEBKIT2_38),y)
+WPEBACKEND_FDO_VERSION = 1.14.0
+else
 WPEBACKEND_FDO_VERSION = 1.4.1
+endif
+
 WPEBACKEND_FDO_SITE = https://wpewebkit.org/releases
 WPEBACKEND_FDO_SOURCE = wpebackend-fdo-$(WPEBACKEND_FDO_VERSION).tar.xz
 WPEBACKEND_FDO_INSTALL_STAGING = YES
 WPEBACKEND_FDO_LICENSE = BSD-2-Clause
 WPEBACKEND_FDO_LICENSE_FILES = COPYING
-WPEBACKEND_FDO_DEPENDENCIES = libglib2 libwpe wayland
+WPEBACKEND_FDO_DEPENDENCIES = libglib2 wpebackend wayland
 
 $(eval $(cmake-package))
