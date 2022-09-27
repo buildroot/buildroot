270601acb91eb37ab369b698ce5b7fb5633f7e21
Adrian Perez de Castro <aperez@igalia.com>
2022-09-27 11:12:16 +0300

    package/cog: add wpewebkit 2.38 variant
    Build Cog 0.14.x when targeting WebKit 2.38, importing some changes
    from the upstream build recipe.
    
    The new headless backend is selectable only when building 0.14.x,
    and the logic to pick dependencies is somewhat improved by adding
    config comments to know why platform backends may not be available
    (typically wpebackend-fdo not being enabled), and adding the
    wpebackend-fdo dependency in a single place in the Make fragment.
    
    Build option COG_PLATFORM_FDO has been renamed to COG_PLATFORM_WL,
    which is taken into account by adding one more level of indirection
    (David Wheeler would approve.)
    
    Lastly, only apply the custom commands to install the D-Bus policy
    under /usr/share if the feature has been enabled, and improve the
    command list to remove the copy installed by the CMake build system
    under /etc.
    
    Signed-off-by: Adrian Perez de Castro <aperez@igalia.com>

--- package/cog/cog.mk
+++ package/cog/cog.mk
@@ -4,7 +4,12 @@
 #
 ################################################################################
 
+ifeq ($(BR2_PACKAGE_WPEWEBKIT2_38),y)
+COG_VERSION = 0.14.1
+else
 COG_VERSION = 0.6.0
+endif
+
 COG_SITE = https://wpewebkit.org/releases
 COG_SOURCE = cog-$(COG_VERSION).tar.xz
 COG_INSTALL_STAGING = YES
@@ -13,34 +18,68 @@ COG_LICENSE = MIT
 COG_LICENSE_FILES = COPYING
 COG_CONF_OPTS = \
 	-DCOG_BUILD_PROGRAMS=ON \
-	-DCOG_WESTON_DIRECT_DISPLAY=OFF \
 	-DINSTALL_MAN_PAGES=OFF \
-	-DCOG_DBUS_SYSTEM_BUS=ON \
 	-DCOG_HOME_URI='$(call qstrip,$(BR2_PACKAGE_COG_PROGRAMS_HOME_URI))'
 
-ifeq ($(BR2_PACKAGE_COG_PLATFORM_FDO),y)
-COG_CONF_OPTS += -DCOG_PLATFORM_FDO=ON
-COG_DEPENDENCIES += libxkbcommon wpebackend-fdo wayland wayland-protocols
+# Add the wpebackend-fdo dependency if any of the backends which
+# need it have been selected (i.e. the expansion is non-empty.)
+ifneq ($(BR2_PACKAGE_COG_PLATFORM_HEADLESS)$(BR2_PACKAGE_COG_PLATFORM_FDO)$(BR2_PACKAGE_COG_PLATFORM_DRM),)
+COG_DEPENDENCIES += wpebackend-fdo
+endif
+
+ifeq ($(BR2_PACKAGE_WPEWEBKIT2_38),y)
+COG_FDO_PLATFORM_CMAKE_OPTION = COG_PLATFORM_WL
+COG_CONF_OPTS += \
+	-DCOG_PLATFORM_GTK4=OFF \
+	-DCOG_PLATFORM_X11=OFF \
+	-DUSE_SOUP2=OFF
 else
-COG_CONF_OPTS += -DCOG_PLATFORM_FDO=OFF
+COG_FDO_PLATFORM_CMAKE_OPTION = COG_PLATFORM_FDO
+COG_CONF_OPTS += \
+	-DUSE_SOUP2=ON
+endif # BR2_PACKAGE_WPEWEBKIT2_38
+
+ifeq ($(BR2_PACKAGE_WESTON),y)
+COG_CONF_OPTS += -DCOG_WESTON_DIRECT_DISPLAY=ON
+COG_DEPENDENCIES += weston
+else
+COG_CONF_OPTS += -DCOG_WESTON_DIRECT_DISPLAY=OFF
+endif
+
+ifeq ($(BR2_PACKAGE_COG_PLATFORM_HEADLESS),y)
+COG_CONF_OPTS += -DCOG_PLATFORM_HEADLESS=ON
+else
+COG_CONF_OPTS += -DCOG_PLATFORM_HEADLESS=OFF
+endif
+
+ifeq ($(BR2_PACKAGE_COG_PLATFORM_FDO),y)
+COG_CONF_OPTS += -D$(COG_FDO_PLATFORM_CMAKE_OPTION)=ON
+COG_DEPENDENCIES += libxkbcommon wayland-protocols wayland
+else
+COG_CONF_OPTS += -D$(COG_FDO_PLATFORM_CMAKE_OPTION)=OFF
 endif
 
 ifeq ($(BR2_PACKAGE_COG_PLATFORM_DRM),y)
 COG_CONF_OPTS += -DCOG_PLATFORM_DRM=ON
-COG_DEPENDENCIES += libdrm libinput wpebackend-fdo wayland
+COG_DEPENDENCIES += libdrm libinput libgbm libegl udev
 else
 COG_CONF_OPTS += -DCOG_PLATFORM_DRM=OFF
 endif
 
+ifeq ($(BR2_PACKAGE_COG_USE_SYSTEM_DBUS),y)
+define COG_INSTALL_DBUS_POLICY
+	$(RM) $(TARGET_DIR)/etc/dbus-1/systemd.d/com.igalia.Cog.conf
+	$(INSTALL) -D -m 0644 $(@D)/com.igalia.Cog.conf $(TARGET_DIR)/usr/share/dbus-1/system.d/
+endef
+COG_POST_INSTALL_TARGET_HOOKS += COG_INSTALL_DBUS_POLICY
+COG_CONF_OPTS += -DCOG_DBUS_SYSTEM_BUS=ON
+else
+COG_CONF_OPTS += -DCOG_DBUS_SYSTEM_BUS=OFF
+endif # BR2_PACKAGE_COG_USE_SYSTEM_DBUS
+
 define COG_INSTALL_SETTINGS
 	$(INSTALL) -D -m 0644 package/cog/websettings.txt $(TARGET_DIR)/root
 endef
-
-define COG_INSTALL_DBUS_POLICY
-	$(INSTALL) -D -m 0644 $(@D)/com.igalia.Cog.conf $(TARGET_DIR)/usr/share/dbus-1/system.d/
-endef
-
 COG_POST_INSTALL_TARGET_HOOKS += COG_INSTALL_SETTINGS
-COG_POST_INSTALL_TARGET_HOOKS += COG_INSTALL_DBUS_POLICY
 
 $(eval $(cmake-package))
