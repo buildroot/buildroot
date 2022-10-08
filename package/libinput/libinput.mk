15547176843494c60abb0cbb60000cda3f52ba77
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-10-08 19:36:19 +0200

    package/libinput: add LIBINPUT_CPE_ID_VENDOR
    cpe:2.3:a:freedesktop:libinput is a valid CPE identifier for this package:
    
      https://nvd.nist.gov/products/cpe/search/results?namingFormat=2.3&keyword=cpe%3A2.3%3Aa%3Afreedesktop%3Alibinput
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit db5c4a06a6da5e64268667c7c07aa4154fec6919)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libinput/libinput.mk
+++ package/libinput/libinput.mk
@@ -11,6 +11,7 @@ LIBINPUT_DEPENDENCIES = host-pkgconf libevdev mtdev udev
 LIBINPUT_INSTALL_STAGING = YES
 LIBINPUT_LICENSE = MIT
 LIBINPUT_LICENSE_FILES = COPYING
+LIBINPUT_CPE_ID_VENDOR = freedesktop
 # Tests need fork, so just disable them everywhere.
 LIBINPUT_CONF_OPTS = -Dtests=false -Dlibwacom=false -Ddocumentation=false
 
