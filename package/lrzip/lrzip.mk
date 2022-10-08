e468e372f997d1ba3320e7f00a95199c466123f3
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-10-09 00:19:08 +0200

    package/lrzip: add CPE variables
    cpe:2.3:a:long_range_zip_project:long_range_zip is a valid CPE
    identifier for this package:
    
      https://nvd.nist.gov/products/cpe/search/results?namingFormat=2.3&keyword=cpe%3A2.3%3Aa%3Along_range_zip_project%3Along_range_zip
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit d09ff6fd1f16e017c0ff55e4fe76d5b0b00ba400)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/lrzip/lrzip.mk
+++ package/lrzip/lrzip.mk
@@ -9,6 +9,8 @@ LRZIP_SITE = $(call github,ckolivas,lrzip,v$(LRZIP_VERSION))
 LRZIP_AUTORECONF = YES
 LRZIP_LICENSE = GPL-2.0+
 LRZIP_LICENSE_FILES = COPYING
+LRZIP_CPE_ID_VENDOR = long_range_zip_project
+LRZIP_CPE_ID_PRODUCT = long_range_zip
 LRZIP_DEPENDENCIES = zlib lz4 lzo bzip2
 
 ifeq ($(BR2_i386)$(BR2_x86_64),y)
