fed7c4406aa07f1fb99bad5d5669f8562eb6e6b9
Dario Binacchi <dario.binacchi@amarulasolutions.com>
2022-10-15 17:07:02 +0200

    package/uuu: bump to version 1.5.4
    - Fix memory leak cause by getaddrinfo
    - Fix crash when last block actual size < m_output_size
    - Add -dm to disable small memory for debug purpose
    - Fix a dead lock when reset stream
    - Stop http download when error happen
    - Add error check for all request_data
    - Fix linux build warning
    - Add gzip stream support
    - Rework buffer management to support small memory version
    - Fix support of empty environment variables in Windows OS
    - Fix crash when environment variable is last word in script
    - Fix update libusb path
    - Update libusb submodule
    - Drop vs2017 support.
    - Use $(DefaultPlatformToolset)
    - Try remove PlatformToolset and set WindowsTargetPlatformVersion to 10.0
    
    Signed-off-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/uuu/uuu.mk
+++ package/uuu/uuu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UUU_VERSION = 1.4.243
+UUU_VERSION = 1.5.4
 UUU_SOURCE = uuu_source-$(UUU_VERSION).tar.gz
 UUU_SITE = https://github.com/NXPmicro/mfgtools/releases/download/uuu_$(UUU_VERSION)
 UUU_LICENSE = BSD 3-Clause "New" or "Revised" License
