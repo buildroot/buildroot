df77618b12bfd442e5efd39abd013c3ba88481b8
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-10-16 00:34:39 +0200

    package/libxml2: security bump to version 2.10.3
    - [CVE-2022-40304] Fix dict corruption caused by entity reference cycles
    - [CVE-2022-40303] Fix integer overflows with XML_PARSE_HUGE
    - Fix overflow check in SAX2.c
    
    https://gitlab.gnome.org/GNOME/libxml2/-/tags/v2.10.3
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libxml2/libxml2.mk
+++ package/libxml2/libxml2.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LIBXML2_VERSION_MAJOR = 2.10
-LIBXML2_VERSION = $(LIBXML2_VERSION_MAJOR).2
+LIBXML2_VERSION = $(LIBXML2_VERSION_MAJOR).3
 LIBXML2_SOURCE = libxml2-$(LIBXML2_VERSION).tar.xz
 LIBXML2_SITE = \
 	http://ftp.gnome.org/pub/gnome/sources/libxml2/$(LIBXML2_VERSION_MAJOR)
