998bf9206461855fc13e7a338abffdad2a1d0610
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-10-16 17:41:47 +0200

    package/libva: bump version to 2.16.0
    Release notes: https://github.com/intel/libva/blob/v2.16-branch/NEWS
    
    Removed ac_cv_path_WAYLAND_SCANNER hack due to upstream commit
    https://github.com/intel/libva/commit/a60756887e59bef0c7e8b7b20814c64666be497d
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libva/libva.mk
+++ package/libva/libva.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBVA_VERSION = 2.15.0
+LIBVA_VERSION = 2.16.0
 LIBVA_SOURCE = libva-$(LIBVA_VERSION).tar.bz2
 LIBVA_SITE = https://github.com/intel/libva/releases/download/$(LIBVA_VERSION)
 LIBVA_LICENSE = MIT
@@ -30,7 +30,6 @@ endif
 
 ifeq ($(BR2_PACKAGE_WAYLAND),y)
 LIBVA_DEPENDENCIES += wayland
-LIBVA_CONF_ENV += ac_cv_path_WAYLAND_SCANNER=$(HOST_DIR)/bin/wayland-scanner
 LIBVA_CONF_OPTS += --enable-wayland
 else
 LIBVA_CONF_OPTS += --disable-wayland
