5f37aeaa7b1149c3fde66cebc488812ace1c7ef5
Peter Korsgaard <peter@korsgaard.com>
2022-10-17 19:29:17 +0200

    package/python-django: security bump to version 4.0.8
    Fixes the following security issues:
    
    - CVE-2022-36359: Potential reflected file download vulnerability in
      FileResponse (4.0.7)
      https://www.djangoproject.com/weblog/2022/aug/03/security-releases/
    
    - CVE-2022-41323: Potential denial-of-service vulnerability in
      internationalized URLs (4.0.8)
      https://www.djangoproject.com/weblog/2022/oct/04/security-releases/
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 5b5d3befef9c92a7485c1c68989d95749882ee2a)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 4.0.6
+PYTHON_DJANGO_VERSION = 4.0.8
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/a4/17/b10aa26d7a566a3c19e9d29fac39c8643cbceb6cd7649a378d676839b5db
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/1a/de/08d8a349ed0e3e1999eb86ae0347cc9eaf634cd65f1eb80b9387ac1dbe3c
 
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
