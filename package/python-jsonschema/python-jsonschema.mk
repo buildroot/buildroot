146e5a1b218b154f844058e24ccc94a3e341c34e
James Hilliard <james.hilliard1@gmail.com>
2022-10-19 15:53:48 -0600

    package/python-jsonschema: bump to version 4.16.0
    Migrate to new pep517 based hatchling build system.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-jsonschema/python-jsonschema.mk
+++ package/python-jsonschema/python-jsonschema.mk
@@ -4,11 +4,15 @@
 #
 ################################################################################
 
-PYTHON_JSONSCHEMA_VERSION = 4.5.1
+PYTHON_JSONSCHEMA_VERSION = 4.16.0
 PYTHON_JSONSCHEMA_SOURCE = jsonschema-$(PYTHON_JSONSCHEMA_VERSION).tar.gz
-PYTHON_JSONSCHEMA_SITE = https://files.pythonhosted.org/packages/9e/62/93a54db0e44c4de57868a7d638d7a8abce113c8bc43a20b10b1109b2a517
-PYTHON_JSONSCHEMA_SETUP_TYPE = setuptools
+PYTHON_JSONSCHEMA_SITE = https://files.pythonhosted.org/packages/cf/54/8923ba38b5145f2359d57e5516715392491d674c83f446cd4cd133eeb4d6
+PYTHON_JSONSCHEMA_SETUP_TYPE = pep517
 PYTHON_JSONSCHEMA_LICENSE = MIT
 PYTHON_JSONSCHEMA_LICENSE_FILES = COPYING json/LICENSE
+PYTHON_JSONSCHEMA_DEPENDENCIES = \
+	host-python-hatchling \
+	host-python-hatch-fancy-pypi-readme \
+	host-python-hatch-vcs
 
 $(eval $(python-package))
