9454960cd11ca728ab148551064b32f45aa54d9f
Bartosz Bilas <b.bilas@grinn-global.com>
2022-10-20 23:13:12 +0200

    package/python-esptool: bump version to 4.3
    Signed-off-by: Bartosz Bilas <b.bilas@grinn-global.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-esptool/python-esptool.mk
+++ package/python-esptool/python-esptool.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ESPTOOL_VERSION = 4.1
+PYTHON_ESPTOOL_VERSION = 4.3
 PYTHON_ESPTOOL_SOURCE = esptool-$(PYTHON_ESPTOOL_VERSION).tar.gz
-PYTHON_ESPTOOL_SITE = https://files.pythonhosted.org/packages/bb/d5/9d000803520b68fd9f6447cd7c15352a79984196f57f0519839b0f80fb8f
+PYTHON_ESPTOOL_SITE = https://files.pythonhosted.org/packages/5b/d7/0dae311a94a490d7b7af2f4fab079b34f6244c6129017997bc994f7b360b
 PYTHON_ESPTOOL_SETUP_TYPE = setuptools
 PYTHON_ESPTOOL_LICENSE = GPL-2.0+
 PYTHON_ESPTOOL_LICENSE_FILES = LICENSE
