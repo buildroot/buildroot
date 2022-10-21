014ec19dfe9361ea2b7bc557b73f18719d76dc17
James Hilliard <james.hilliard1@gmail.com>
2022-10-21 13:01:09 -0600

    package/python-aiojobs: bump to version 1.1.0
    Add new python-async-timeout runtime dependency.
    
    License hash changed due to removal of full license text:
    https://github.com/aio-libs/aiojobs/commit/db3e047550d9fb102742d02f8b7bba63ee3ca040
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-aiojobs/python-aiojobs.mk
+++ package/python-aiojobs/python-aiojobs.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_AIOJOBS_VERSION = 0.3.0
+PYTHON_AIOJOBS_VERSION = 1.1.0
 PYTHON_AIOJOBS_SOURCE = aiojobs-$(PYTHON_AIOJOBS_VERSION).tar.gz
-PYTHON_AIOJOBS_SITE = https://files.pythonhosted.org/packages/da/09/76d9c0d66d2fd1333835f110cd85f7456de7cf3f60425e3b283fba562731
-PYTHON_AIOJOBS_SETUP_TYPE = distutils
+PYTHON_AIOJOBS_SITE = https://files.pythonhosted.org/packages/99/75/b6d3678d804fffda81ebbac6214c35a4417d5ddbd70ffe7958ad951e64d2
+PYTHON_AIOJOBS_SETUP_TYPE = setuptools
 PYTHON_AIOJOBS_LICENSE = Apache-2.0
 PYTHON_AIOJOBS_LICENSE_FILES = LICENSE
 
