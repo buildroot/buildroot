a30e31a05e21461e73175c814ba858edb23a363e
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-10-23 00:08:08 +0200

    package/sratom: bump to version 0.6.14
    - Update hash of COPYING (standard ISC licence used since
      https://github.com/lv2/sratom/commit/145ed2fe9756f707d9fd5f03ada5327d04e1b972)
    - meson must be used since
      https://github.com/lv2/sratom/commit/75e647def0f67647ec7ba1405e26d809a87c62fc
    
    https://github.com/lv2/sratom/blob/v0.6.14/NEWS
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/sratom/sratom.mk
+++ package/sratom/sratom.mk
@@ -4,18 +4,14 @@
 #
 ################################################################################
 
-SRATOM_VERSION = 0.6.8
+SRATOM_VERSION = 0.6.14
 SRATOM_SITE = https://download.drobilla.net
-SRATOM_SOURCE = sratom-$(SRATOM_VERSION).tar.bz2
+SRATOM_SOURCE = sratom-$(SRATOM_VERSION).tar.xz
 SRATOM_LICENSE = ISC
 SRATOM_LICENSE_FILES = COPYING
 SRATOM_DEPENDENCIES = host-pkgconf lv2 serd sord
 SRATOM_INSTALL_STAGING = YES
 
-SRATOM_CONF_OPTS += --no-coverage
+SRATOM_CONF_OPTS += -Ddocs=disabled -Dtests=disabled
 
-ifeq ($(BR2_STATIC_LIBS),y)
-SRATOM_CONF_OPTS += --static --no-shared
-endif
-
-$(eval $(waf-package))
+$(eval $(meson-package))
