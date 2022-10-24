325a354234594fdcf557c6fa3db75976acfde4bd
Julien Olivain <ju.o@free.fr>
2022-10-24 21:54:36 +0200

    package/python-distro: bump to version 1.8.0
    Also adds md5 file hash published on:
    https://pypi.org/pypi/distro/json
    
    For change log since 1.7.0, see:
    https://github.com/python-distro/distro/releases/tag/v1.8.0
    
    Most notable change is the upstream support for Buildroot
    (included in python module docs and tests).
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-distro/python-distro.mk
+++ package/python-distro/python-distro.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PYTHON_DISTRO_VERSION = 1.7.0
-PYTHON_DISTRO_SITE = https://files.pythonhosted.org/packages/b5/7e/ddfbd640ac9a82e60718558a3de7d5988a7d4648385cf00318f60a8b073a
+PYTHON_DISTRO_VERSION = 1.8.0
+PYTHON_DISTRO_SITE = https://files.pythonhosted.org/packages/4b/89/eaa3a3587ebf8bed93e45aa79be8c2af77d50790d15b53f6dfc85b57f398
 PYTHON_DISTRO_SOURCE = distro-$(PYTHON_DISTRO_VERSION).tar.gz
 PYTHON_DISTRO_LICENSE = Apache-2.0
 PYTHON_DISTRO_LICENSE_FILES = LICENSE
