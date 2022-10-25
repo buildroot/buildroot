73890799ee14cba1ae650f19ec7a6e4fd2dd4086
Sébastien Szymanski <sebastien.szymanski@armadeus.com>
2022-10-25 09:16:34 +0200

    package/python-flask-jsonrpc: bump to version 2.2.2
    Drop patch 0001 as it is not needed anymore:
    https://github.com/cenobites/flask-jsonrpc/commit/2426b7f6fabeef9eb3a8dc6570e2fce33a30f98e
    
    Signed-off-by: Sébastien Szymanski <sebastien.szymanski@armadeus.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-flask-jsonrpc/python-flask-jsonrpc.mk
+++ package/python-flask-jsonrpc/python-flask-jsonrpc.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_FLASK_JSONRPC_VERSION = 2.2.1
+PYTHON_FLASK_JSONRPC_VERSION = 2.2.2
 PYTHON_FLASK_JSONRPC_SOURCE = Flask-JSONRPC-$(PYTHON_FLASK_JSONRPC_VERSION).tar.gz
-PYTHON_FLASK_JSONRPC_SITE = https://files.pythonhosted.org/packages/ca/2a/3f2bd79840d9643fc909cf27f730b2c5006351746ba09c4dab1b898ddee9
+PYTHON_FLASK_JSONRPC_SITE = https://files.pythonhosted.org/packages/a5/7b/8cbd53084a1efb58a1105905a38b0cbefcd3ae13ef2c90c07eedf0fdb6dc
 PYTHON_FLASK_JSONRPC_LICENSE = BSD-3-Clause
 PYTHON_FLASK_JSONRPC_LICENSE_FILES = LICENSE COPYING
 PYTHON_FLASK_JSONRPC_SETUP_TYPE = setuptools
