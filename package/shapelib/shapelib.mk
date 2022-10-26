810c0eecf1fb81de43c67d602290e911d6a3a486
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-10-26 23:03:34 +0200

    package/shapelib: fix CVE-2022-0699
    A double-free condition exists in contrib/shpsort.c of shapelib 1.5.0
    and older releases. This issue may allow an attacker to cause a denial
    of service or have other unspecified impact via control over malloc.
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/shapelib/shapelib.mk
+++ package/shapelib/shapelib.mk
@@ -11,4 +11,7 @@ SHAPELIB_LICENSE_FILES = web/license.html COPYING
 SHAPELIB_CPE_ID_VENDOR = osgeo
 SHAPELIB_INSTALL_STAGING = YES
 
+# 0001-Remove-double-free-in-contrib-shpsrt.patch
+SHAPELIB_IGNORE_CVES += CVE-2022-0699
+
 $(eval $(autotools-package))
