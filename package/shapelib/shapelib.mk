db93b802bcaa1ce1349129b177687d40735866a7
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-10-26 23:03:33 +0200

    package/shapelib: add SHAPELIB_CPE_ID_VENDOR
    cpe:2.3:a:osgeo:shapelib is a valid CPE identifier for this package:
    
      https://nvd.nist.gov/products/cpe/search/results?namingFormat=2.3&keyword=cpe%3A2.3%3Aa%3Aosgeo%3Ashapelib
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 1545a88f9d5a694eed1a01dc0220ec1dabf5cabe)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/shapelib/shapelib.mk
+++ package/shapelib/shapelib.mk
@@ -8,6 +8,7 @@ SHAPELIB_VERSION = 1.5.0
 SHAPELIB_SITE = http://download.osgeo.org/shapelib
 SHAPELIB_LICENSE = MIT or LGPL-2.0
 SHAPELIB_LICENSE_FILES = web/license.html COPYING
+SHAPELIB_CPE_ID_VENDOR = osgeo
 SHAPELIB_INSTALL_STAGING = YES
 
 $(eval $(autotools-package))
