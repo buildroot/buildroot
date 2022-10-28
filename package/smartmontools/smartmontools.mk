e60fb6d252d5ece448a70957ab45389b151f7f79
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-10-28 23:53:25 +0200

    package/smartmontools: bump to version 7.3
    https://github.com/smartmontools/smartmontools/releases/tag/RELEASE_7_3
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/smartmontools/smartmontools.mk
+++ package/smartmontools/smartmontools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SMARTMONTOOLS_VERSION = 7.2
+SMARTMONTOOLS_VERSION = 7.3
 SMARTMONTOOLS_SITE = http://downloads.sourceforge.net/project/smartmontools/smartmontools/$(SMARTMONTOOLS_VERSION)
 SMARTMONTOOLS_LICENSE = GPL-2.0+
 SMARTMONTOOLS_LICENSE_FILES = COPYING
