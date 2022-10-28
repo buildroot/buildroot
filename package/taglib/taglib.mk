5ef22cb6d10958a9867fc372725826819a8824ef
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-10-29 00:02:23 +0200

    package/taglib: bump to version 1.13
    - Drop patch (already in version)
    - Use WITH_ZLIB which is available since
      https://github.com/taglib/taglib/commit/1644c0dd87f40a2492bde4011fe72257fc72ee0e
    
    https://github.com/taglib/taglib/releases/tag/v1.13
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/taglib/taglib.mk
+++ package/taglib/taglib.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TAGLIB_VERSION = 1.12
+TAGLIB_VERSION = 1.13
 TAGLIB_SITE = http://taglib.github.io/releases
 TAGLIB_INSTALL_STAGING = YES
 TAGLIB_LICENSE = LGPL-2.1 or MPL-1.1
@@ -13,6 +13,9 @@ TAGLIB_CPE_ID_VENDOR = taglib
 
 ifeq ($(BR2_PACKAGE_ZLIB),y)
 TAGLIB_DEPENDENCIES += zlib
+TAGLIB_CONF_OPTS += -DWITH_ZLIB=ON
+else
+TAGLIB_CONF_OPTS += -DWITH_ZLIB=OFF
 endif
 
 define TAGLIB_REMOVE_DEVFILE
