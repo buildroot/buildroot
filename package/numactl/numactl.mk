e9bc980d93a189a16e7320bd254b906c87ca9f87
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-10-29 10:44:55 +0200

    package/numactl: bump to version 2.0.16
    - Drop patches (already in version)
    - Use official tarball and so drop autoreconf
    
    https://github.com/numactl/numactl/releases/tag/v2.0.15
    https://github.com/numactl/numactl/releases/tag/v2.0.16
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/numactl/numactl.mk
+++ package/numactl/numactl.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-NUMACTL_VERSION = 2.0.14
-NUMACTL_SITE = $(call github,numactl,numactl,v$(NUMACTL_VERSION))
+NUMACTL_VERSION = 2.0.16
+NUMACTL_SITE = \
+	https://github.com/numactl/numactl/releases/download/v$(NUMACTL_VERSION)
 NUMACTL_LICENSE = LGPL-2.1 (libnuma), GPL-2.0 (programs)
 NUMACTL_LICENSE_FILES = LICENSE.GPL2 LICENSE.LGPL2.1
 NUMACTL_INSTALL_STAGING = YES
-NUMACTL_AUTORECONF = YES
 NUMACTL_CONF_ENV = CFLAGS="$(TARGET_CFLAGS) -fPIC"
 
 $(eval $(autotools-package))
