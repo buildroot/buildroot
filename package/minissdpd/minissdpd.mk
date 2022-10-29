3be3113b570715121d5b8f9ea689cf42ed512d66
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-10-29 21:51:08 +0200

    package/minissdpd: bump to version 1.6.0
    Update hash of LICENSE file (license refined for github:
    https://github.com/miniupnp/miniupnp/commit/af812c877532f2432b0566633e1e539797ef9c13)
    
    http://miniupnp.free.fr/files/changelog.php?file=minissdpd-1.6.0.tar.gz
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/minissdpd/minissdpd.mk
+++ package/minissdpd/minissdpd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MINISSDPD_VERSION = 1.5.20211105
+MINISSDPD_VERSION = 1.6.0
 MINISSDPD_SITE = http://miniupnp.free.fr/files
 MINISSDPD_LICENSE = BSD-3-Clause
 MINISSDPD_LICENSE_FILES = LICENSE
