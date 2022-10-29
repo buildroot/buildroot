a855e21bcbcabfc6fec0c1c34bc3346180031a34
James Hilliard <james.hilliard1@gmail.com>
2022-10-28 19:00:36 -0600

    package/python-ujson: bump to version 5.5.0
    Switch to now supported system double-conversion library.
    
    Disable built in strip so that stripping is handled by buildroot.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-ujson/python-ujson.mk
+++ package/python-ujson/python-ujson.mk
@@ -4,12 +4,16 @@
 #
 ################################################################################
 
-PYTHON_UJSON_VERSION = 5.2.0
+PYTHON_UJSON_VERSION = 5.5.0
 PYTHON_UJSON_SOURCE = ujson-$(PYTHON_UJSON_VERSION).tar.gz
-PYTHON_UJSON_SITE = https://files.pythonhosted.org/packages/e4/fc/2dee0e78162aa1ad03dadde9a9b5c281d6f8bb0eed6810a270486d8fc041
+PYTHON_UJSON_SITE = https://files.pythonhosted.org/packages/6e/4a/03ddad85a10dd52e209993a14afa0cb0dc5c348e4647329f1c53856ad9e6
 PYTHON_UJSON_SETUP_TYPE = setuptools
 PYTHON_UJSON_LICENSE = BSD-3-Clause
 PYTHON_UJSON_LICENSE_FILES = LICENSE.txt
-PYTHON_UJSON_DEPENDENCIES = host-python-setuptools-scm
+PYTHON_UJSON_DEPENDENCIES = host-python-setuptools-scm double-conversion
+PYTHON_UJSON_ENV = \
+	UJSON_BUILD_DC_INCLUDES="$(STAGING_DIR)/usr/include/double-conversion" \
+	UJSON_BUILD_DC_LIBS="-ldouble-conversion" \
+	UJSON_BUILD_NO_STRIP=1
 
 $(eval $(python-package))
