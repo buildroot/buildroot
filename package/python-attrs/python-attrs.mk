de4a180b80905ee74a064d0fbc74d8de4fea7464
James Hilliard <james.hilliard1@gmail.com>
2022-10-28 19:20:20 -0600

    package/python-attrs: bump to version 22.1.0
    License hash changed due to ownership clarification:
    https://github.com/python-attrs/attrs/commit/899497f165b3c8291b66c9c3a01b64881b69cf39
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-attrs/python-attrs.mk
+++ package/python-attrs/python-attrs.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ATTRS_VERSION = 21.4.0
+PYTHON_ATTRS_VERSION = 22.1.0
 PYTHON_ATTRS_SOURCE = attrs-$(PYTHON_ATTRS_VERSION).tar.gz
-PYTHON_ATTRS_SITE = https://files.pythonhosted.org/packages/d7/77/ebb15fc26d0f815839ecd897b919ed6d85c050feeb83e100e020df9153d2
+PYTHON_ATTRS_SITE = https://files.pythonhosted.org/packages/1a/cb/c4ffeb41e7137b23755a45e1bfec9cbb76ecf51874c6f1d113984ecaa32c
 PYTHON_ATTRS_SETUP_TYPE = setuptools
 PYTHON_ATTRS_LICENSE = MIT
 PYTHON_ATTRS_LICENSE_FILES = LICENSE
