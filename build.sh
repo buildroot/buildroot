397c3d36868a58551133b13e46dfd889f8af38fb
Gleb Mazovetskiy <glex.spb@gmail.com>
2022-10-29 13:22:14 +0100

    opendingux: (re)build.sh improvements
    1. Add a `build.sh` script that does not nuke the build directory by
       default.
    2. `unset` some environment variables known to cause issues with
       buildroot.
    
    Signed-off-by: Gleb Mazovetskiy <glex.spb@gmail.com>

---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+set -e
+
+if [ -z "$CONFIG" ] ; then
+	echo "\$CONFIG not set. Please set it to the variant to build."
+	echo "Valid values are: gcw0, rs90, lepus, installer"
+	exit 1
+fi
+
+# Clear the build location.
+if [ "$1" = clean ]; then
+  echo "Clearing build location..."
+  rm -rf output/${CONFIG}
+fi
+
+# Unset client variables that cause issues with buildroot.
+unset PERL_MM_OPT CMAKE_GENERATOR CMAKE_GENERATOR_PLATFORM CMAKE_GENERATOR_TOOLSET CMAKE_GENERATOR_INSTANCE
+
+# Use the default config.
+make od_${CONFIG}_defconfig BR2_EXTERNAL=board/opendingux O=output/${CONFIG}
+
+echo "Copy .config to target"
+mkdir output/${CONFIG}/host && cp output/${CONFIG}/.config output/${CONFIG}/host/
+
+if [ "${CONFIG}" = "installer" ] ; then
+	TARGET="all host-odbootd"
+else
+	TARGET=sdk
+fi
+
+# Perform the build.
+echo "Starting build..."
+nice make ${TARGET} BR2_SDK_PREFIX=${CONFIG}-toolchain O=output/${CONFIG}
+
+if [ "${TARGET}" = sdk ] ; then
+	echo "Recompressing SDK to XZ..."
+	ARCHIVE_NAME=opendingux-${CONFIG}-toolchain.`date +'%Y-%m-%d'`
+	gzip -d -c output/${CONFIG}/images/${CONFIG}-toolchain.tar.gz | xz -T0 -9 > output/${CONFIG}/images/$ARCHIVE_NAME.tar.xz
+	rm output/${CONFIG}/images/${CONFIG}-toolchain.tar.gz
+
+	echo "The SDK has been built at:"
+	echo "output/${CONFIG}/images/$ARCHIVE_NAME.tar.xz"
+	echo
+	echo "Remember to run ./relocate-sdk.sh after extracting it to your desired location"
+fi
