c0a13b667b8587816734c432c2e691d4c3ed9763
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-10-30 10:27:49 +0100

    package/jack2: bump to version 1.9.21
    https://github.com/jackaudio/jack2/releases/tag/v1.9.21
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/jack2/jack2.mk
+++ package/jack2/jack2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-JACK2_VERSION = 1.9.20
+JACK2_VERSION = 1.9.21
 JACK2_SITE = $(call github,jackaudio,jack2,v$(JACK2_VERSION))
 JACK2_LICENSE = GPL-2.0+ (jack server), LGPL-2.1+ (jack library)
 JACK2_LICENSE_FILES = COPYING
