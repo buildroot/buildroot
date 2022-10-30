6c967d184d116a6eae9243e718b19e904582a686
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-10-30 20:33:07 +0100

    package/alsa-utils: bump version to 1.2.8
    Changelog:
    https://www.alsa-project.org/wiki/Changes_v1.2.7.2_v1.2.8#alsa-utils
    
    Removed patch 0002 due to upstream removal of UCM code:
    https://github.com/alsa-project/alsa-utils/commit/62cd05a929283d919144e092892c74ac85b00a7d
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/alsa-utils/alsa-utils.mk
+++ package/alsa-utils/alsa-utils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ALSA_UTILS_VERSION = 1.2.7
+ALSA_UTILS_VERSION = 1.2.8
 ALSA_UTILS_SOURCE = alsa-utils-$(ALSA_UTILS_VERSION).tar.bz2
 ALSA_UTILS_SITE = https://www.alsa-project.org/files/pub/utils
 ALSA_UTILS_LICENSE = GPL-2.0
