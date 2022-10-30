35a6c9cb0dc7f9f7015ecedb9761b1d63a8a587d
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-10-30 22:50:46 +0100

    package/exfatprogs: bump to version 1.2.0
    https://github.com/exfatprogs/exfatprogs/releases/tag/1.2.0
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/exfatprogs/exfatprogs.mk
+++ package/exfatprogs/exfatprogs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-EXFATPROGS_VERSION = 1.1.3
+EXFATPROGS_VERSION = 1.2.0
 EXFATPROGS_SOURCE = exfatprogs-$(EXFATPROGS_VERSION).tar.xz
 EXFATPROGS_SITE = https://github.com/exfatprogs/exfatprogs/releases/download/$(EXFATPROGS_VERSION)
 EXFATPROGS_LICENSE = GPL-2.0+
