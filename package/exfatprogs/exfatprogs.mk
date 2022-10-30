8b94b7ec32e63208837a18cb8793ff3d35f8a078
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-10-30 22:50:45 +0100

    package/exfatprogs: drop host-pkgconf dependency
    host-pkgconf is not a dependency since bump to version 1.0.3 in commit
    fbad745139125ea0995a436fd81ce7ea233638c0 and
    https://github.com/exfatprogs/exfatprogs/commit/843124cd9a2087281e00bd1b9d710db59acfbced
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 11ff62234ab139296ead705869c44c0a233682da)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/exfatprogs/exfatprogs.mk
+++ package/exfatprogs/exfatprogs.mk
@@ -9,8 +9,6 @@ EXFATPROGS_SOURCE = exfatprogs-$(EXFATPROGS_VERSION).tar.xz
 EXFATPROGS_SITE = https://github.com/exfatprogs/exfatprogs/releases/download/$(EXFATPROGS_VERSION)
 EXFATPROGS_LICENSE = GPL-2.0+
 EXFATPROGS_LICENSE_FILES = COPYING
-EXFATPROGS_DEPENDENCIES = host-pkgconf
-HOST_EXFATPROGS_DEPENDENCIES = host-pkgconf
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
