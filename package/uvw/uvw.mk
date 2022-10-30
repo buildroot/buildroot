9d136e5cc950ab3ab29ec13e430fc78291634d96
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-10-30 22:37:43 +0100

    package/{libuv, uvw}: bump to versions 1.44.2, 2.12.1_libuv_v1.44
    - libuv
      Release notes:
      https://github.com/libuv/libuv/issues/3452
      https://github.com/libuv/libuv/issues/3522
      https://github.com/libuv/libuv/issues/3586
    
      Updated license hash due to various commits regarding Android/Windows.
    
    - uvw:
      Updated license hash due to copyright year bump:
      https://github.com/skypjack/uvw/commit/cca65db1f4a6ba74cb51f77754cd1f8ce64d9d63
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/uvw/uvw.mk
+++ package/uvw/uvw.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UVW_VERSION = 2.11.0_libuv_v1.43
+UVW_VERSION = 2.12.1_libuv_v1.44
 UVW_SITE = $(call github,skypjack,uvw,v$(UVW_VERSION))
 UVW_INSTALL_STAGING = YES
 UVW_INSTALL_TARGET = NO
