b8098ff155958374f6791f81479684bac711ab03
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-10-30 23:34:40 +0100

    package/log4cxx: bump to version 0.13.0
    Drop patch which was merged:
    https://github.com/apache/logging-log4cxx/commit/5240a3db1ba10e657b29c67ae8330f3a5b3d8774
    and updated:
    https://github.com/apache/logging-log4cxx/commit/b7284609ae0b6ae3798b19d3920480c4d7e670ba
    
    https://logging.apache.org/log4cxx/latest_stable/changelog.html#0.13.0
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/log4cxx/log4cxx.mk
+++ package/log4cxx/log4cxx.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LOG4CXX_VERSION = 0.12.1
+LOG4CXX_VERSION = 0.13.0
 LOG4CXX_SITE = https://archive.apache.org/dist/logging/log4cxx/$(LOG4CXX_VERSION)
 LOG4CXX_SOURCE = apache-log4cxx-$(LOG4CXX_VERSION).tar.gz
 LOG4CXX_INSTALL_STAGING = YES
