53716828cfad5b9fe51d9edc6e4a351601f0f152
Christian Stewart <christian@paral.in>
2022-11-01 15:15:37 -0700

    package/go: security bump to go 1.19.3
    go1.19.3 (released 2022-11-01) includes security fixes to the os/exec and
    syscall packages, as well as bug fixes to the compiler and the runtime.
    
    Fixes: CVE-2022-41716
    
    NOTE: this CVE is specific to Windows and is not directly relevant to Buildroot.
    
    https://go.dev/doc/devel/release#go1.19
    https://github.com/golang/go/issues?q=milestone%3AGo1.19.3+label%3ACherryPickApproved
    
    Signed-off-by: Christian Stewart <christian@paral.in>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/go/go.mk
+++ package/go/go.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GO_VERSION = 1.19.2
+GO_VERSION = 1.19.3
 GO_SITE = https://storage.googleapis.com/golang
 GO_SOURCE = go$(GO_VERSION).src.tar.gz
 
