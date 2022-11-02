2e6c204f51f5696aa056324a5a30ac5d23648e8f
James Hilliard <james.hilliard1@gmail.com>
2022-11-02 16:17:22 -0600

    package/python-ipython: bump to version 8.6.0
    Drop patch which is no longer needed.
    
    Migrate from distutils to setuptools infrastructure.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-ipython/python-ipython.mk
+++ package/python-ipython/python-ipython.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-PYTHON_IPYTHON_VERSION = 7.31.1
+PYTHON_IPYTHON_VERSION = 8.6.0
 PYTHON_IPYTHON_SOURCE = ipython-$(PYTHON_IPYTHON_VERSION).tar.gz
-PYTHON_IPYTHON_SITE = https://files.pythonhosted.org/packages/fb/39/c1947dc1bb993a35469ca474535cb3214c91f5f9a7752a1fcbd9b6b37dde
+PYTHON_IPYTHON_SITE = https://files.pythonhosted.org/packages/7f/59/05e94e067afb61460f0d5229a1edb800e2f65b8436085fad9cd262d80d45
 PYTHON_IPYTHON_LICENSE = BSD-3-Clause
 PYTHON_IPYTHON_LICENSE_FILES = COPYING.rst LICENSE
 PYTHON_IPYTHON_CPE_ID_VENDOR = ipython
 PYTHON_IPYTHON_CPE_ID_PRODUCT = ipython
-PYTHON_IPYTHON_SETUP_TYPE = distutils
+PYTHON_IPYTHON_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
