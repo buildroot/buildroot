6293c033a906d279235a98cc2c49bea3447f5b67
James Hilliard <james.hilliard1@gmail.com>
2022-11-02 16:05:23 -0600

    package/python-ifaddr: bump to version 0.2.0
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-ifaddr/python-ifaddr.mk
+++ package/python-ifaddr/python-ifaddr.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_IFADDR_VERSION = 0.1.7
+PYTHON_IFADDR_VERSION = 0.2.0
 PYTHON_IFADDR_SOURCE = ifaddr-$(PYTHON_IFADDR_VERSION).tar.gz
-PYTHON_IFADDR_SITE = https://files.pythonhosted.org/packages/3d/fc/4ce147e3997cd0ea470ad27112087545cf83bf85015ddb3054673cb471bb
+PYTHON_IFADDR_SITE = https://files.pythonhosted.org/packages/e8/ac/fb4c578f4a3256561548cd825646680edcadb9440f3f68add95ade1eb791
 PYTHON_IFADDR_SETUP_TYPE = setuptools
 PYTHON_IFADDR_LICENSE = MIT
 PYTHON_IFADDR_LICENSE_FILES = LICENSE.txt
