3c2a62601547ef0666e1883cee0ea110ebff387e
James Hilliard <james.hilliard1@gmail.com>
2022-11-02 12:30:39 -0600

    package/python-bcrypt: bump to version 4.0.1
    Replace cffi based backend with pyo3 setuptools-rust based backend.
    
    Propagate rust reverse dependency comment.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-bcrypt/python-bcrypt.mk
+++ package/python-bcrypt/python-bcrypt.mk
@@ -4,12 +4,23 @@
 #
 ################################################################################
 
-PYTHON_BCRYPT_VERSION = 3.2.0
+PYTHON_BCRYPT_VERSION = 4.0.1
 PYTHON_BCRYPT_SOURCE = bcrypt-$(PYTHON_BCRYPT_VERSION).tar.gz
-PYTHON_BCRYPT_SITE = https://files.pythonhosted.org/packages/d8/ba/21c475ead997ee21502d30f76fd93ad8d5858d19a3fad7cd153de698c4dd
+PYTHON_BCRYPT_SITE = https://files.pythonhosted.org/packages/8c/ae/3af7d006aacf513975fd1948a6b4d6f8b4a307f8a244e1a3d3774b297aad
 PYTHON_BCRYPT_SETUP_TYPE = setuptools
 PYTHON_BCRYPT_LICENSE = Apache-2.0
 PYTHON_BCRYPT_LICENSE_FILES = LICENSE
-PYTHON_BCRYPT_DEPENDENCIES = host-python-cffi
+PYTHON_BCRYPT_DEPENDENCIES = \
+	host-python-setuptools-rust \
+	host-rustc
+PYTHON_BCRYPT_ENV = \
+	$(PKG_CARGO_ENV) \
+	PYO3_CROSS_LIB_DIR="$(STAGING_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)"
+# We need to vendor the Cargo crates at download time
+PYTHON_BCRYPT_DOWNLOAD_POST_PROCESS = cargo
+PYTHON_BCRYPT_DOWNLOAD_DEPENDENCIES = host-rustc
+PYTHON_BCRYPT_DL_ENV = \
+	$(PKG_CARGO_ENV) \
+	BR_CARGO_MANIFEST_PATH=src/_bcrypt/Cargo.toml
 
 $(eval $(python-package))
