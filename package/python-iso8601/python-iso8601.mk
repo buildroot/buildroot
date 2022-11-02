f0b9199cc4210fa5a5b51b84181c49d055a034b1
James Hilliard <james.hilliard1@gmail.com>
2022-11-02 16:26:56 -0600

    package/python-iso8601: bump to version 1.1.0
    License hash changed due to date update:
    https://github.com/micktwomey/pyiso8601/commit/406db6f23ffbf6815a320c30be070b89d28e2974
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-iso8601/python-iso8601.mk
+++ package/python-iso8601/python-iso8601.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ISO8601_VERSION = 0.1.14
+PYTHON_ISO8601_VERSION = 1.1.0
 PYTHON_ISO8601_SOURCE = iso8601-$(PYTHON_ISO8601_VERSION).tar.gz
-PYTHON_ISO8601_SITE = https://files.pythonhosted.org/packages/f9/ed/b97abc7877e5b253eef96a469f47d617b0ebcccc735405fa1a620c7ee833
+PYTHON_ISO8601_SITE = https://files.pythonhosted.org/packages/31/8c/1c342fdd2f4af0857684d16af766201393ef53318c15fa785fcb6c3b7c32
 PYTHON_ISO8601_SETUP_TYPE = setuptools
 PYTHON_ISO8601_LICENSE = MIT
 PYTHON_ISO8601_LICENSE_FILES = LICENSE
