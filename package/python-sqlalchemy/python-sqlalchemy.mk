331393e8a5903fa5b41e7854c02b56f3c0b168a5
James Hilliard <james.hilliard1@gmail.com>
2022-11-03 13:41:22 -0600

    package/python-sqlalchemy: bump to version 1.4.42
    License hash changed due to date update:
    https://github.com/sqlalchemy/sqlalchemy/commit/ad244b9b7577486bd4e22d74d64e35802af84977
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-sqlalchemy/python-sqlalchemy.mk
+++ package/python-sqlalchemy/python-sqlalchemy.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SQLALCHEMY_VERSION = 1.3.10
+PYTHON_SQLALCHEMY_VERSION = 1.4.42
 PYTHON_SQLALCHEMY_SOURCE = SQLAlchemy-$(PYTHON_SQLALCHEMY_VERSION).tar.gz
-PYTHON_SQLALCHEMY_SITE = https://files.pythonhosted.org/packages/14/0e/487f7fc1e432cec50d2678f94e4133f2b9e9356e35bacc30d73e8cb831fc
+PYTHON_SQLALCHEMY_SITE = https://files.pythonhosted.org/packages/e4/56/8ea85eaab7d93b58f9c213ad8fc5882838189a29fc8cc401d80710a12969
 PYTHON_SQLALCHEMY_SETUP_TYPE = setuptools
 PYTHON_SQLALCHEMY_LICENSE = MIT
 PYTHON_SQLALCHEMY_LICENSE_FILES = LICENSE
