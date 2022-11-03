96a9097ca814942951a4530ff294efedfed65036
James Hilliard <james.hilliard1@gmail.com>
2022-11-03 14:40:35 -0600

    package/python-webpy: bump to version 0.62
    License file was accidentially removed in latest release but project
    is still public domain licensed.
    
    Details:
    https://github.com/webpy/webpy/commit/04bf405dd86485c8f3774c526e6a869a1eb73408
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-webpy/python-webpy.mk
+++ package/python-webpy/python-webpy.mk
@@ -4,11 +4,10 @@
 #
 ################################################################################
 
-PYTHON_WEBPY_VERSION = 0.40
+PYTHON_WEBPY_VERSION = 0.62
 PYTHON_WEBPY_SOURCE = web.py-$(PYTHON_WEBPY_VERSION).tar.gz
-PYTHON_WEBPY_SITE = https://files.pythonhosted.org/packages/e3/23/ed84b174add09153329c6357984c8433e2f350de91c3859fa48c3cdbf7dc
+PYTHON_WEBPY_SITE = https://files.pythonhosted.org/packages/cd/6e/338a060bb5b52ee8229bdada422eaa5f71b13f8d33467f37f870ed2cae4b
 PYTHON_WEBPY_SETUP_TYPE = setuptools
 PYTHON_WEBPY_LICENSE = Public Domain
-PYTHON_WEBPY_LICENSE_FILES = LICENSE.txt
 
 $(eval $(python-package))
