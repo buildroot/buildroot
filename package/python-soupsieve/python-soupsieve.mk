a0aeb786c1e73acba9b0f6ff1a187c2fa328ad72
James Hilliard <james.hilliard1@gmail.com>
2022-11-03 13:36:02 -0600

    package/python-soupsieve: bump to version 2.3.2.post1
    Migrate from setuptools to hatchling pep517 build backend.
    
    License hash changed due to date update:
    https://github.com/facelessuser/soupsieve/commit/9da2de87bd67308b933d794a46b349d8da74dac1
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-soupsieve/python-soupsieve.mk
+++ package/python-soupsieve/python-soupsieve.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_SOUPSIEVE_VERSION = 2.2.1
+PYTHON_SOUPSIEVE_VERSION = 2.3.2.post1
 PYTHON_SOUPSIEVE_SOURCE = soupsieve-$(PYTHON_SOUPSIEVE_VERSION).tar.gz
-PYTHON_SOUPSIEVE_SITE = https://files.pythonhosted.org/packages/c8/3f/e71d92e90771ac2d69986aa0e81cf0dfda6271e8483698f4847b861dd449
-PYTHON_SOUPSIEVE_SETUP_TYPE = setuptools
+PYTHON_SOUPSIEVE_SITE = https://files.pythonhosted.org/packages/f3/03/bac179d539362319b4779a00764e95f7542f4920084163db6b0fd4742d38
+PYTHON_SOUPSIEVE_SETUP_TYPE = pep517
 PYTHON_SOUPSIEVE_LICENSE = MIT
 PYTHON_SOUPSIEVE_LICENSE_FILES = LICENSE.md
+PYTHON_SOUPSIEVE_DEPENDENCIES = host-python-hatchling
 
 $(eval $(python-package))
