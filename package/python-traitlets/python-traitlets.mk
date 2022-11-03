964eccb839eb169e8b3d883693dc921793287c8a
James Hilliard <james.hilliard1@gmail.com>
2022-11-03 14:19:25 -0600

    package/python-traitlets: bump to version 5.5.0
    Migrate from distutils to hatchling pep517 build backend.
    
    License hash changed due to formatting change:
    https://github.com/ipython/traitlets/commit/9fd01cc7b279a46e58fddc3c29149e73c7d67ab1
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-traitlets/python-traitlets.mk
+++ package/python-traitlets/python-traitlets.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_TRAITLETS_VERSION = 5.1.0
+PYTHON_TRAITLETS_VERSION = 5.5.0
 PYTHON_TRAITLETS_SOURCE = traitlets-$(PYTHON_TRAITLETS_VERSION).tar.gz
-PYTHON_TRAITLETS_SITE = https://files.pythonhosted.org/packages/d5/bc/37d490908e7ac949614d62767db3c86f37bc5adb6129d378c35859a75b87
+PYTHON_TRAITLETS_SITE = https://files.pythonhosted.org/packages/dd/a8/278742d17c9e95ccb0dcb86ae216df114d2166d88e72f42b60a7b58b600b
 PYTHON_TRAITLETS_LICENSE = BSD-3-Clause
 PYTHON_TRAITLETS_LICENSE_FILES = COPYING.md
-PYTHON_TRAITLETS_SETUP_TYPE = distutils
+PYTHON_TRAITLETS_SETUP_TYPE = pep517
+PYTHON_TRAITLETS_DEPENDENCIES = host-python-hatchling
 
 $(eval $(python-package))
