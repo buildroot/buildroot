0e6461c51f07e04cd42708843cf45583108f37ac
James Hilliard <james.hilliard1@gmail.com>
2022-11-03 07:19:18 -0600

    package/python-networkmanager: bump to version 2.2
    License hash changed due to date update:
    https://github.com/seveas/python-networkmanager/commit/9acb5c9d90154a68ffadd144e9d72f1b56038649
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-networkmanager/python-networkmanager.mk
+++ package/python-networkmanager/python-networkmanager.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PYTHON_NETWORKMANAGER_VERSION = 2.1
-PYTHON_NETWORKMANAGER_SITE = https://files.pythonhosted.org/packages/73/ec/2cc46290cedc99f59df3633d428dfa91d95946ca49af96fdb53514ee195f
+PYTHON_NETWORKMANAGER_VERSION = 2.2
+PYTHON_NETWORKMANAGER_SITE = https://files.pythonhosted.org/packages/f2/3d/0924d03117636b80a88af783f857fb101e4c47953a8bb0f733b964d50af4
 PYTHON_NETWORKMANAGER_SETUP_TYPE = setuptools
 PYTHON_NETWORKMANAGER_LICENSE = Zlib
 PYTHON_NETWORKMANAGER_LICENSE_FILES = COPYING
