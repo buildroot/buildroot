6f5fe64ab9208f68043acc3b6e687de21d8be614
Francois Perrad <fperrad@gmail.com>
2022-11-05 09:04:07 +0100

    package/tzdata: bump to version 2022f
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/tzdata/tzdata.mk
+++ package/tzdata/tzdata.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TZDATA_VERSION = 2022c
+TZDATA_VERSION = 2022f
 TZDATA_SOURCE = tzdata$(TZDATA_VERSION).tar.gz
 TZDATA_SITE = https://www.iana.org/time-zones/repository/releases
 TZDATA_STRIP_COMPONENTS = 0
