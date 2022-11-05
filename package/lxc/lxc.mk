db19998035701d08bf1b5fd366e0b5aa9adb8e57
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-11-05 22:23:43 +0100

    package/lxc: bump to version 5.0.1
    LXC 5.0 will be supported until June 2027 and our current LTS release,
    LXC 4.0 will now switch to a slower maintenance pace, only getting
    critical bugfixes and security updates.
    
    We strongly recommend all LXC users to plan an upgrade to the 5.0
    branch.
    
    - Switch to meson-package
    - Add an upstream patch to fix the following build failure with glibc
      2.36 (unfortunately upstream doesn't plan to fix this for 4.x:
      https://github.com/lxc/lxc/issues/4183 and patch is only working with
      meson, not autotools)
    
    https://discuss.linuxcontainers.org/t/lxc-5-0-lts-has-been-released
    https://discuss.linuxcontainers.org/t/lxc-5-0-1-has-been-released
    
    Fixes:
     - http://autobuild.buildroot.org/results/f77e2dc44c9a224f280e08089a890e85c302274f
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/lxc/lxc.mk
+++ package/lxc/lxc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LXC_VERSION = 4.0.12
+LXC_VERSION = 5.0.1
 LXC_SITE = https://linuxcontainers.org/downloads/lxc
 LXC_LICENSE = GPL-2.0 (some tools), LGPL-2.1+
 LXC_LICENSE_FILES = LICENSE.GPL2 LICENSE.LGPL2.1
@@ -13,56 +13,54 @@ LXC_DEPENDENCIES = host-pkgconf
 LXC_INSTALL_STAGING = YES
 
 LXC_CONF_OPTS = \
-	--disable-apparmor \
-	--disable-examples \
-	--with-distro=buildroot \
-	--disable-werror \
-	$(if $(BR2_PACKAGE_BASH),,--disable-bash)
+	-Dapparmor=false \
+	-Dexamples=false \
+	-Dman=false
 
 ifeq ($(BR2_PACKAGE_BASH_COMPLETION),y)
 LXC_DEPENDENCIES += bash-completion
 endif
 
 ifeq ($(BR2_PACKAGE_LIBCAP),y)
-LXC_CONF_OPTS += --enable-capabilities
+LXC_CONF_OPTS += -Dcapabilities=true
 LXC_DEPENDENCIES += libcap
 else
-LXC_CONF_OPTS += --disable-capabilities
+LXC_CONF_OPTS += -Dcapabilities=false
 endif
 
 ifeq ($(BR2_PACKAGE_LIBSECCOMP),y)
-LXC_CONF_OPTS += --enable-seccomp
+LXC_CONF_OPTS += -Dseccomp=true
 LXC_DEPENDENCIES += libseccomp
 else
-LXC_CONF_OPTS += --disable-seccomp
+LXC_CONF_OPTS += -Dseccomp=false
 endif
 
 ifeq ($(BR2_PACKAGE_LIBSELINUX),y)
-LXC_CONF_OPTS += --enable-selinux
+LXC_CONF_OPTS += -Dselinux=true
 LXC_DEPENDENCIES += libselinux
 else
-LXC_CONF_OPTS += --disable-selinux
+LXC_CONF_OPTS += -Dselinux=false
 endif
 
 ifeq ($(BR2_PACKAGE_LIBURING),y)
-LXC_CONF_OPTS += --enable-liburing
+LXC_CONF_OPTS += -Dio-uring-event-loop=true
 LXC_DEPENDENCIES += liburing
 else
-LXC_CONF_OPTS += --disable-liburing
+LXC_CONF_OPTS += -Dio-uring-event-loop=false
 endif
 
 ifeq ($(BR2_PACKAGE_LINUX_PAM),y)
-LXC_CONF_OPTS += --enable-pam
+LXC_CONF_OPTS += -Dpam-cgroup=true
 LXC_DEPENDENCIES += linux-pam
 else
-LXC_CONF_OPTS += --disable-pam
+LXC_CONF_OPTS += -Dpam-cgroup=false
 endif
 
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
-LXC_CONF_OPTS += --enable-openssl
+LXC_CONF_OPTS += -Dopenssl=true
 LXC_DEPENDENCIES += openssl
 else
-LXC_CONF_OPTS += --disable-openssl
+LXC_CONF_OPTS += -Dopenssl=false
 endif
 
-$(eval $(autotools-package))
+$(eval $(meson-package))
