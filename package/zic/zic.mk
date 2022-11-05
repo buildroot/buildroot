fcdc4b29c7a84422214ee3af116c75fa62289484
Francois Perrad <fperrad@gmail.com>
2022-11-05 09:04:06 +0100

    package/zic: bump to version 2022f
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/zic/zic.mk
+++ package/zic/zic.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ZIC_VERSION = 2022c
+ZIC_VERSION = 2022f
 ZIC_SOURCE = tzcode$(ZIC_VERSION).tar.gz
 ZIC_SITE = https://www.iana.org/time-zones/repository/releases
 ZIC_STRIP_COMPONENTS = 0
