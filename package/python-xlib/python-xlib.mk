37648d90ca5420ab5199848ac20da0d95f7d09be
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2022-11-05 23:35:04 +0100

    package/python-xlib: drop useless SOURCE variable
    Fixes:
    
    package/python-xlib/python-xlib.mk:8: remove default value of _SOURCE variable (http://nightly.buildroot.org/#generic-package-reference)
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-xlib/python-xlib.mk
+++ package/python-xlib/python-xlib.mk
@@ -5,7 +5,6 @@
 ################################################################################
 
 PYTHON_XLIB_VERSION = 0.32
-PYTHON_XLIB_SOURCE = python-xlib-$(PYTHON_XLIB_VERSION).tar.gz
 PYTHON_XLIB_SITE = https://files.pythonhosted.org/packages/4d/cf/a29ecb43a5c84a65ffd726e3b28806f56b3bc5e796ddb533ff52af107dcf
 PYTHON_XLIB_SETUP_TYPE = setuptools
 PYTHON_XLIB_LICENSE = LGPL-2.1+
