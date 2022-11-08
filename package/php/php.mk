8ba2f61f8f9a7dc772098e7145b7a6f7666c7bfd
Peter Korsgaard <peter@korsgaard.com>
2022-11-08 21:05:23 +0100

    package/php: security bump version to 8.0.25
    Changelog: https://www.php.net/ChangeLog-8.php#8.0.25
    
    Fixes:
    - CVE-2022-31630: http://bugs.php.net/81739
    - CVE-2022-37454: http://bugs.php.net/81738
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/php/php.mk
+++ package/php/php.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PHP_VERSION = 8.0.24
+PHP_VERSION = 8.0.25
 PHP_SITE = https://www.php.net/distributions
 PHP_SOURCE = php-$(PHP_VERSION).tar.xz
 PHP_INSTALL_STAGING = YES
