b44f6c2f303ef03f563edd159b1c88d1cf118ed5
Yann E. MORIN <yann.morin.1998@free.fr>
2022-11-12 23:05:37 +0100

    package/matchbox-starup-monitor: fix build without C++
    matchbox-startup-monitor is an ageing package, and uses an old
    configure.ac with archaic constructs. This had generated a configure
    script that incorrectly tries to look for and validate a C++ compiler:
    
        checking for powerpc64le-buildroot-linux-gnu-g++... no
        checking whether we are using the GNU C++ compiler... no
        checking whether no accepts -g... no
        checking dependency style of no... none
        checking how to run the C++ preprocessor... /lib/cpp
        configure: error: C++ preprocessor "/lib/cpp" fails sanity check
    
    Calling autoreconf fixes the issue, as the generated configure no longer
    tries to look for a C++ compiler at all anymore. Running autoreconf does
    not add any new dependency, as they are already in the dependency chain
    via other packages.
    
    Fixes:
        http://autobuild.buildroot.org/results/223/223f43dd76ee907c5f25c4fee94a0f5d75614dd5/
    
    See also similar changes:
        9993a36f5e12 package/pamtester: fix build without C++
        c05cc5de868c package/madplay: needs autoreconf
        eae18d01abc7 libmad: needs autoreconf
        43274dd3e0da package/libid3tag: needs autoreconf
    
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/matchbox-startup-monitor/matchbox-startup-monitor.mk
+++ package/matchbox-startup-monitor/matchbox-startup-monitor.mk
@@ -11,4 +11,9 @@ MATCHBOX_STARTUP_MONITOR_LICENSE = GPL-2.0+
 MATCHBOX_STARTUP_MONITOR_LICENSE_FILES = COPYING
 MATCHBOX_STARTUP_MONITOR_DEPENDENCIES = matchbox-lib startup-notification
 
+# Obsolete constructs in the archaic configure.ac generated an outworn
+# configure script that incorrectly searches a C++ compiler. Regenerate
+# the autoconf machinery to avoid failures without a C++ compiler.
+MATCHBOX_STARTUP_MONITOR_AUTORECONF = YES
+
 $(eval $(autotools-package))
