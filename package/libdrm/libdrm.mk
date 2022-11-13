5be56ac290c8cf2af6c8e5cf0c431430bb9d35e2
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-11-13 12:42:21 +0100

    package/libdrm: bump version to 2.4.114
    Release notes:
    https://lists.x.org/archives/xorg-announce/2022-November/003250.html
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libdrm/libdrm.mk
+++ package/libdrm/libdrm.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBDRM_VERSION = 2.4.113
+LIBDRM_VERSION = 2.4.114
 LIBDRM_SOURCE = libdrm-$(LIBDRM_VERSION).tar.xz
 LIBDRM_SITE = https://dri.freedesktop.org/libdrm
 LIBDRM_LICENSE = MIT
