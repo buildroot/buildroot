ceeb485fbc47ed582000a26df974ab061b0adaa9
charles <charles@ccxtechnologies.com>
2022-11-18 12:59:56 -0500

    include all of the freeradius plugins

--- package/freeradius-server/freeradius-server.mk
+++ package/freeradius-server/freeradius-server.mk
@@ -47,15 +47,6 @@ FREERADIUS_SERVER_CONF_OPTS += \
 # Modules for which we don't have the dependencies must be disabled
 # explicitly, to avoid that they're searched on the host.
 FREERADIUS_SERVER_CONF_OPTS += \
-	--without-rlm_eap_ike \
-	--without-rlm_eap_tnc \
-	--without-rlm_mschap \
-	--without-rlm_perl \
-	--without-rlm_realm \
-	--without-rlm_sql_iodbc \
-	--without-rlm_sql_oracle \
-	--without-rlm_sql_freetds \
-	--without-rlm_yubikey \
 	--without-rlm_sql_mongo \
 	--with-experimental-modules
 
