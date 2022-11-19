6b2ca5aa254107bf25e3bd283e3b07274a6cf5f0
Peter Korsgaard <peter@korsgaard.com>
2022-11-19 14:45:10 +0100

    package/xterm: security bump to patch 376
    Fixes the following security issue:
    
    CVE-2022-45063: xterm before 375 allows code execution via font ops, e.g.,
    because an OSC 50 response may have Ctrl-g and therefore lead to command
    execution within the vi line-editing mode of Zsh:
    
    https://www.openwall.com/lists/oss-security/2022/11/10/1
    
    Additionally, patch 376 fixes a null pointer access issue:
    
    https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1022942
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 0cc7c63f91ffadb0835bb57cdb7bfffdf7803add)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/xterm/xterm.mk
+++ package/xterm/xterm.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XTERM_VERSION = 371
+XTERM_VERSION = 376
 XTERM_SOURCE = xterm-$(XTERM_VERSION).tgz
 XTERM_SITE = http://invisible-mirror.net/archives/xterm
 XTERM_DEPENDENCIES = ncurses xlib_libXaw host-pkgconf
