9a53d1dbc17378325da7fb2d48135027b5df785c
Peter Korsgaard <peter@korsgaard.com>
2022-11-22 21:18:25 +0100

    package/python3: add upstream security fix for CVE-2022-45061
    Fixes the following security issue:
    
    CVE-2022-45061: An issue was discovered in Python before 3.11.1.  An
    unnecessary quadratic algorithm exists in one path when processing some
    inputs to the IDNA (RFC 3490) decoder, such that a crafted, unreasonably
    long name being presented to the decoder could lead to a CPU denial of
    service.  Hostnames are often supplied by remote servers that could be
    controlled by a malicious actor; in such a scenario, they could trigger
    excessive CPU consumption on the client attempting to make use of an
    attacker-supplied supposed hostname.  For example, the attack payload could
    be placed in the Location header of an HTTP response with status code 302.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 39a2ff16f92a61a3e8593c394d211c18d01de8d4)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python3/python3.mk
+++ package/python3/python3.mk
@@ -13,6 +13,9 @@ PYTHON3_LICENSE_FILES = LICENSE
 PYTHON3_CPE_ID_VENDOR = python
 PYTHON3_CPE_ID_PRODUCT = python
 
+# 0033-3.11-gh-98433-Fix-quadratic-time-idna-decoding.-GH-9.patch
+PYTHON3_IGNORE_CVES += CVE-2022-45061
+
 # This host Python is installed in $(HOST_DIR), as it is needed when
 # cross-compiling third-party Python modules.
 
