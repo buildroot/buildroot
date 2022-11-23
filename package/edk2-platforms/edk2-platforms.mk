747d556910ec38c1f2c0364c42672a0d223b77e0
Vincent Stehlé <vincent.stehle@arm.com>
2022-11-23 11:20:34 +0100

    package/edk2-platforms: refine license
    The edk2-platforms project is licensed under the BSD-2-Clause license with
    a patent grant, as per commit ae604e4ffe8f ("edk2-platforms: Change
    License.txt from 2-Clause BSD to BSD+Patent").
    
    There is a BSD-2-Clause-Patent SPDX license identifier[1] for this case,
    therefore refine the edk2-platforms package to use this more specific
    identifier.
    
    [1]: https://spdx.org/licenses/BSD-2-Clause-Patent.html
    
    Signed-off-by: Vincent Stehlé <vincent.stehle@arm.com>
    Cc: Dick Olsson <hi@senzilla.io>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 9bd126698397eb3105340f48e4f033e09b34aaf7)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/edk2-platforms/edk2-platforms.mk
+++ package/edk2-platforms/edk2-platforms.mk
@@ -7,7 +7,7 @@
 # Keep in sync with latest commit as of the release date for boot/edk2
 EDK2_PLATFORMS_VERSION = db922e1253cb6f1fc456805bc42fb7d401eed5c2
 EDK2_PLATFORMS_SITE = $(call github,tianocore,edk2-platforms,$(EDK2_PLATFORMS_VERSION))
-EDK2_PLATFORMS_LICENSE = BSD-2-Clause
+EDK2_PLATFORMS_LICENSE = BSD-2-Clause-Patent
 EDK2_PLATFORMS_LICENSE_FILES = License.txt
 EDK2_PLATFORMS_INSTALL_TARGET = NO
 EDK2_PLATFORMS_INSTALL_STAGING = YES
