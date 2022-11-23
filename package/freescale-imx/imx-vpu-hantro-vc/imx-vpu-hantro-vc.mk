4ef729d1cf86a9793c34a794f89905ca502e86d3
Chris Dimich <chris.dimich@boundarydevices.com>
2022-11-23 11:52:56 -0800

    package/freescale-imx/imx-vpu-hantro-vc: add INSTALL_STAGING_CMDS
    Add an INSTALL_STAGING_CMDS define as libraries needed by
    imx-vpu-hantro-daemon.
    
    Signed-off-by: Chris Dimich <chris.dimich@boundarydevices.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/freescale-imx/imx-vpu-hantro-vc/imx-vpu-hantro-vc.mk
+++ package/freescale-imx/imx-vpu-hantro-vc/imx-vpu-hantro-vc.mk
@@ -8,6 +8,7 @@ IMX_VPU_HANTRO_VC_VERSION = 1.9.0
 IMX_VPU_HANTRO_VC_SITE = $(FREESCALE_IMX_SITE)
 IMX_VPU_HANTRO_VC_SOURCE = imx-vpu-hantro-vc-$(IMX_VPU_HANTRO_VC_VERSION).bin
 IMX_VPU_HANTRO_VC_DEPENDENCIES = linux
+IMX_VPU_HANTRO_VC_INSTALL_STAGING = YES
 
 IMX_VPU_HANTRO_VC_LICENSE = NXP Semiconductor Software License Agreement
 IMX_VPU_HANTRO_VC_LICENSE_FILES = EULA COPYING
@@ -15,6 +16,13 @@ IMX_VPU_HANTRO_VC_REDISTRIBUTE = NO
 
 define IMX_VPU_HANTRO_VC_EXTRACT_CMDS
 	$(call NXP_EXTRACT_HELPER,$(IMX_VPU_HANTRO_VC_DL_DIR)/$(IMX_VPU_HANTRO_VC_SOURCE))
+endef
+
+define IMX_VPU_HANTRO_VC_INSTALL_STAGING_CMDS
+	mkdir -p $(STAGING_DIR)/usr/include/
+	cp -dpfr $(@D)/usr/include/hantro_VC8000E_enc/*.h $(STAGING_DIR)/usr/include/
+	$(INSTALL) -D -m 0755 $(@D)/usr/lib/libhantro_vc8000e.so $(STAGING_DIR)/usr/lib/libhantro_vc8000e.so
+	$(INSTALL) -D -m 0755 $(@D)/usr/lib/libhantro_vc8000e.so.1 $(STAGING_DIR)/usr/lib/libhantro_vc8000e.so.1
 endef
 
 define IMX_VPU_HANTRO_VC_INSTALL_TARGET_CMDS
