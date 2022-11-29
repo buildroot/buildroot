1fe4f83bf41fd403fde7d0ad23187e0d50aa0bbd
Giulio Benetti <giulio.benetti@benettiengineering.com>
2022-11-29 15:05:33 +0100

    package/rtl8188eu: bump to 2022-11-29 version on v5.2.2.4 branch
    Fixes:
    http://autobuild.buildroot.net/results/8bcb4d0adabc141ff8144f9e22bd549e3cd8858a/
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Reviewed-by: Luca Ceresoli <luca.ceresoli@bootlin.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/rtl8188eu/rtl8188eu.mk
+++ package/rtl8188eu/rtl8188eu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTL8188EU_VERSION = c4908ca4caf861d858c4d9e8452a2ad5c88cf2ba
+RTL8188EU_VERSION = f4af53305cb1e9a0d8d9957a042f7c01b7121bfc
 RTL8188EU_SITE = $(call github,lwfinger,rtl8188eu,$(RTL8188EU_VERSION))
 RTL8188EU_LICENSE = GPL-2.0, proprietary (rtl8188eufw.bin firmware blob)
 RTL8188EU_LICENSE_FILES = COPYING
