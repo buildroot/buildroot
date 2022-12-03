25ab5d22e6a1fe7efbae772400a2f5e5215ee694
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-12-03 23:19:26 +0100

    package/gdb: zlib is mandatory, not optional
    zlib is a mandatory dependency of gdb and by default, gdb will use its
    internal one. Moreover, --with-zlib has been replaced by
    --with-system-zlib since version 7.10 and
    https://github.com/bminor/binutils-gdb/commit/fa1f5da0b6ff3622f9bf60e348e149b76920abba
    
    Fixes:
     - https://bugs.buildroot.org/show_bug.cgi?id=15131
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 3341ceb1e58536cf43a4c000e4bb3cdbff3f6de5)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gdb/gdb.mk
+++ package/gdb/gdb.mk
@@ -41,7 +41,8 @@ endif
 # also need ncurses.
 # As for libiberty, gdb may use a system-installed one if present, so
 # we must ensure ours is installed first.
-HOST_GDB_DEPENDENCIES = host-expat host-libiberty host-ncurses
+GDB_DEPENDENCIES = zlib
+HOST_GDB_DEPENDENCIES = host-expat host-libiberty host-ncurses host-zlib
 
 # Disable building documentation
 GDB_MAKE_OPTS += MAKEINFO=true
@@ -139,6 +140,7 @@ GDB_CONF_OPTS = \
 	--disable-sim \
 	$(GDB_DISABLE_BINUTILS_CONF_OPTS) \
 	--without-included-gettext \
+	--with-system-zlib \
 	--disable-werror \
 	--enable-static \
 	--without-mpfr
@@ -222,13 +224,6 @@ else
 GDB_CONF_OPTS += --without-lzma
 endif
 
-ifeq ($(BR2_PACKAGE_ZLIB),y)
-GDB_CONF_OPTS += --with-zlib
-GDB_DEPENDENCIES += zlib
-else
-GDB_CONF_OPTS += --without-zlib
-endif
-
 ifeq ($(BR2_PACKAGE_GDB_PYTHON),)
 # This removes some unneeded Python scripts and XML target description
 # files that are not useful for a normal usage of the debugger.
@@ -266,6 +261,7 @@ HOST_GDB_CONF_OPTS = \
 	--enable-threads \
 	--disable-werror \
 	--without-included-gettext \
+	--with-system-zlib \
 	--with-curses \
 	--without-mpfr \
 	$(GDB_DISABLE_BINUTILS_CONF_OPTS)
