eb397066602be72fc5f38fc58ac51e2a1fa12e57
kassandra <kassandra@ccxtechnologies.com>
2022-12-05 16:13:08 -0500

    Updated licenses

--- package/python-flit-core/python-flit-core.mk
+++ package/python-flit-core/python-flit-core.mk
@@ -8,6 +8,7 @@ PYTHON_FLIT_CORE_VERSION = 3.7.1
 PYTHON_FLIT_CORE_SOURCE = flit_core-$(PYTHON_FLIT_CORE_VERSION).tar.gz
 PYTHON_FLIT_CORE_SITE = https://files.pythonhosted.org/packages/15/d1/d8798b83e953fd6f86ca9b50f93eec464a9305b0661469c8234e61095481
 PYTHON_FLIT_CORE_LICENSE = BSD-3-Clause
+PYTHON_FLIT_CORE_LICENSE_FILES = flit_core/vendor/tomli-1.2.3.dist-info/LICENSE
 PYTHON_FLIT_CORE_SETUP_TYPE = flit-bootstrap
 
 # Use flit built in bootstrap_install for installing host-python-flit-core.
