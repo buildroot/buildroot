5dd4caaa011b1efffc522d0d313725a195cfee7f
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-12-07 23:26:31 +0100

    package/kodi-pvr-nextpvr: bump version to 19.0.5-Matrix
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-pvr-nextpvr/kodi-pvr-nextpvr.mk
+++ package/kodi-pvr-nextpvr/kodi-pvr-nextpvr.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PVR_NEXTPVR_VERSION = 19.0.4-Matrix
+KODI_PVR_NEXTPVR_VERSION = 19.0.5-Matrix
 KODI_PVR_NEXTPVR_SITE = $(call github,kodi-pvr,pvr.nextpvr,$(KODI_PVR_NEXTPVR_VERSION))
 KODI_PVR_NEXTPVR_LICENSE = GPL-2.0+
 KODI_PVR_NEXTPVR_LICENSE_FILES = LICENSE.md
