090d126da88c1d529bf073626e423fceeb3cf9d5
Christian Stewart <christian@paral.in>
2022-12-06 19:16:01 -0800

    package/go: security bump to version 1.19.4
    Includes 2 security fixes following the security policy.
    
     - CVE-2022-41720: https://go.dev/issue/56694
     - CVE-2022-41717: https://go.dev/issue/56350
    
    https://groups.google.com/g/golang-nuts/c/rQgaDWEvUrE
    https://github.com/golang/go/issues?q=milestone%3AGo1.19.4+label%3ACherryPickApproved+
    
    Signed-off-by: Christian Stewart <christian@paral.in>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/go/go.mk
+++ package/go/go.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GO_VERSION = 1.19.3
+GO_VERSION = 1.19.4
 GO_SITE = https://storage.googleapis.com/golang
 GO_SOURCE = go$(GO_VERSION).src.tar.gz
 
