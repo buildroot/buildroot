51818b18b9a1fe1a5e0b31298f86cb80309ae9ab
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-12-08 23:37:48 +0100

    package/x11r7/xapp_xsetroot: bump version to 1.1.3
    https://lists.x.org/archives/xorg-announce/2022-October/003240.html
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/x11r7/xapp_xsetroot/xapp_xsetroot.mk
+++ package/x11r7/xapp_xsetroot/xapp_xsetroot.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_XSETROOT_VERSION = 1.1.2
-XAPP_XSETROOT_SOURCE = xsetroot-$(XAPP_XSETROOT_VERSION).tar.bz2
-XAPP_XSETROOT_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_XSETROOT_VERSION = 1.1.3
+XAPP_XSETROOT_SOURCE = xsetroot-$(XAPP_XSETROOT_VERSION).tar.xz
+XAPP_XSETROOT_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XSETROOT_LICENSE = MIT
 XAPP_XSETROOT_LICENSE_FILES = COPYING
 XAPP_XSETROOT_DEPENDENCIES = xlib_libX11 xlib_libXmu xlib_libXcursor xdata_xbitmaps
