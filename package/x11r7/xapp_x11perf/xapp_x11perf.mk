1261e78ee2a7994f0cf764c0a61d457984daf673
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-12-08 23:37:46 +0100

    package/x11r7/xapp_x11perf: bump version to 1.6.2
    https://lists.x.org/archives/xorg-announce/2022-October/003242.html
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/x11r7/xapp_x11perf/xapp_x11perf.mk
+++ package/x11r7/xapp_x11perf/xapp_x11perf.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_X11PERF_VERSION = 1.6.1
-XAPP_X11PERF_SOURCE = x11perf-$(XAPP_X11PERF_VERSION).tar.bz2
-XAPP_X11PERF_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_X11PERF_VERSION = 1.6.2
+XAPP_X11PERF_SOURCE = x11perf-$(XAPP_X11PERF_VERSION).tar.xz
+XAPP_X11PERF_SITE = https://xorg.freedesktop.org/archive/individual/test
 XAPP_X11PERF_LICENSE = MIT
 XAPP_X11PERF_LICENSE_FILES = COPYING
 XAPP_X11PERF_DEPENDENCIES = xlib_libX11 xlib_libXmu xlib_libXft
