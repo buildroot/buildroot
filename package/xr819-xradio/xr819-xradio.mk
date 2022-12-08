a9d68487f7540021c6051f43c4f35ea7c31fd630
Giulio Benetti <giulio.benetti@benettiengineering.com>
2022-12-08 20:47:42 +0100

    package/xr819-xradio: fix build failure with Linux 6.0
    Bump to latest version to fix build failure with Linux 6.0.
    
    Fixes:
    http://autobuild.buildroot.net/results/434c1d2a5104090366519278314bf750324d3710/
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 3fbb447cded5120135ce21ce6a5d2936dc79f645)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/xr819-xradio/xr819-xradio.mk
+++ package/xr819-xradio/xr819-xradio.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XR819_XRADIO_VERSION = 4f0cfd5e869ca101e7c624d9ce05ba5d2b2a0bc2
+XR819_XRADIO_VERSION = 58d597bcf081e44904335916e6033bc8ef28b043
 XR819_XRADIO_SITE = $(call github,fifteenhex,xradio,$(XR819_XRADIO_VERSION))
 XR819_XRADIO_LICENSE = GPL-2.0
 XR819_XRADIO_LICENSE_FILES = LICENSE
