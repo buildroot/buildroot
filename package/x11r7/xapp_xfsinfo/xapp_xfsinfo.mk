c999fb9a0527e371f4d9f433436afcc1dc88d4af
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-12-08 23:37:50 +0100

    package/x11r7/xapp_xfsinfo: bump version to 1.0.7
    https://lists.x.org/archives/xorg-announce/2022-October/003238.html
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/x11r7/xapp_xfsinfo/xapp_xfsinfo.mk
+++ package/x11r7/xapp_xfsinfo/xapp_xfsinfo.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_XFSINFO_VERSION = 1.0.6
-XAPP_XFSINFO_SOURCE = xfsinfo-$(XAPP_XFSINFO_VERSION).tar.bz2
-XAPP_XFSINFO_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_XFSINFO_VERSION = 1.0.7
+XAPP_XFSINFO_SOURCE = xfsinfo-$(XAPP_XFSINFO_VERSION).tar.xz
+XAPP_XFSINFO_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XFSINFO_LICENSE = MIT
 XAPP_XFSINFO_LICENSE_FILES = COPYING
 XAPP_XFSINFO_DEPENDENCIES = xlib_libFS xlib_libX11
