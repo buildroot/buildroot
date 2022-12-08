fc47afdc57cb7bdff3c4481607b2887f8a9cae94
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-12-08 22:19:04 +0100

    package/pugixml: bump to version 1.13
    https://pugixml.org/docs/manual.html#v1.13
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/pugixml/pugixml.mk
+++ package/pugixml/pugixml.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PUGIXML_VERSION = 1.12.1
+PUGIXML_VERSION = 1.13
 PUGIXML_SITE = https://github.com/zeux/pugixml/releases/download/v$(PUGIXML_VERSION)
 PUGIXML_LICENSE = MIT
 PUGIXML_LICENSE_FILES = LICENSE.md
