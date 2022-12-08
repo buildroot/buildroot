723eff5808451188ddc097aafc17e6969b849e57
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-12-08 23:37:34 +0100

    package/x11r7/xlib_libXdmcp: bump version to 1.1.4
    https://lists.x.org/archives/xorg-announce/2022-November/003260.html
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/x11r7/xlib_libXdmcp/xlib_libXdmcp.mk
+++ package/x11r7/xlib_libXdmcp/xlib_libXdmcp.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XLIB_LIBXDMCP_VERSION = 1.1.3
-XLIB_LIBXDMCP_SOURCE = libXdmcp-$(XLIB_LIBXDMCP_VERSION).tar.bz2
-XLIB_LIBXDMCP_SITE = http://xorg.freedesktop.org/releases/individual/lib
+XLIB_LIBXDMCP_VERSION = 1.1.4
+XLIB_LIBXDMCP_SOURCE = libXdmcp-$(XLIB_LIBXDMCP_VERSION).tar.xz
+XLIB_LIBXDMCP_SITE = https://xorg.freedesktop.org/archive/individual/lib
 XLIB_LIBXDMCP_LICENSE = MIT
 XLIB_LIBXDMCP_LICENSE_FILES = COPYING
 XLIB_LIBXDMCP_CPE_ID_VENDOR = x.org
