d6943457c4afb2fb5103875243593d77c47ec552
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-12-08 21:43:49 +0100

    package/connman: fix CVE-2022-3229{2,3}
    Fix CVE-2022-32292: In ConnMan through 1.41, remote attackers able to
    send HTTP requests to the gweb component are able to exploit a
    heap-based buffer overflow in received_data to execute code.
    
    Fix CVE-2022-32293: In ConnMan through 1.41, a man-in-the-middle attack
    against a WISPR HTTP query could be used to trigger a use-after-free in
    WISPR handling, leading to crashes or code execution.
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 2f2b4c80f44ac0da88fc087017d495e627131350)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/connman/connman.mk
+++ package/connman/connman.mk
@@ -13,6 +13,13 @@ CONNMAN_LICENSE = GPL-2.0
 CONNMAN_LICENSE_FILES = COPYING
 CONNMAN_CPE_ID_VENDOR = intel
 
+# 0001-gweb-Fix-OOB-write-in-received_data.patch
+CONNMAN_IGNORE_CVES += CVE-2022-32292
+
+# 0002-wispr-Add-reference-counter-to-portal-context.patch
+# 0003-wispr-Update-portal-context-references.patch
+CONNMAN_IGNORE_CVES += CVE-2022-32293
+
 CONNMAN_CONF_OPTS = --with-dbusconfdir=/etc
 
 ifeq ($(BR2_INIT_SYSTEMD),y)
