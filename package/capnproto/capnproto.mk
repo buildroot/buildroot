b6965012f3990bfbd0cd77cfd98dc60e9ea8ab10
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-12-08 21:21:04 +0100

    package/capnproto: security bump to version 0.9.2
    Fix CVE-2022-46149: Cap'n Proto is a data interchange format and remote
    procedure call (RPC) system. Cap'n Proro prior to versions 0.7.1, 0.8.1,
    0.9.2, and 0.10.3, as well as versions of Cap'n Proto's Rust
    implementation prior to 0.13.7, 0.14.11, and 0.15.2 are vulnerable to
    out-of-bounds read due to logic error handling list-of-list. This issue
    may lead someone to remotely segfault a peer by sending it a malicious
    message, if the victim performs certain actions on a list-of-pointer
    type. Exfiltration of memory is possible if the victim performs
    additional certain actions on a list-of-pointer type. To be vulnerable,
    an application must perform a specific sequence of actions, described in
    the GitHub Security Advisory. The bug is present in inlined code,
    therefore the fix will require rebuilding dependent applications. Cap'n
    Proto has C++ fixes available in versions 0.7.1, 0.8.1, 0.9.2, and
    0.10.3.
    
    https://github.com/capnproto/capnproto/security/advisories/GHSA-qqff-4vw4-f6hx
    https://dwrensha.github.io/capnproto-rust/2022/11/30/out_of_bounds_memory_access_bug.html
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 040ab175df6337bf39a609adea07a7756c309be8)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/capnproto/capnproto.mk
+++ package/capnproto/capnproto.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CAPNPROTO_VERSION = 0.9.1
+CAPNPROTO_VERSION = 0.9.2
 CAPNPROTO_SITE = $(call github,capnproto,capnproto,v$(CAPNPROTO_VERSION))
 CAPNPROTO_LICENSE = MIT
 CAPNPROTO_LICENSE_FILES = LICENSE
