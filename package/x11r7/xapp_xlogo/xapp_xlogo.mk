df57bbf7279757a11327361e7cd9a1091b2da40f
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-12-08 23:37:37 +0100

    package/x11r7/xapp_xlogo: bump version to 1.0.6
    https://lists.x.org/archives/xorg-announce/2022-November/003257.html
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/x11r7/xapp_xlogo/xapp_xlogo.mk
+++ package/x11r7/xapp_xlogo/xapp_xlogo.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_XLOGO_VERSION = 1.0.5
-XAPP_XLOGO_SOURCE = xlogo-$(XAPP_XLOGO_VERSION).tar.bz2
-XAPP_XLOGO_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_XLOGO_VERSION = 1.0.6
+XAPP_XLOGO_SOURCE = xlogo-$(XAPP_XLOGO_VERSION).tar.xz
+XAPP_XLOGO_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XLOGO_LICENSE = MIT
 XAPP_XLOGO_LICENSE_FILES = COPYING
 XAPP_XLOGO_DEPENDENCIES = \
