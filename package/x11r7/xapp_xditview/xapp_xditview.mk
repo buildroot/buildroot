0f18b8dcb4e622f973d73332732d3dd6f548d3a3
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-12-08 23:38:00 +0100

    package/x11r7/xapp_xditview: bump version to 1.0.6
    https://lists.x.org/archives/xorg-announce/2022-October/003220.html
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/x11r7/xapp_xditview/xapp_xditview.mk
+++ package/x11r7/xapp_xditview/xapp_xditview.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_XDITVIEW_VERSION = 1.0.5
-XAPP_XDITVIEW_SOURCE = xditview-$(XAPP_XDITVIEW_VERSION).tar.bz2
-XAPP_XDITVIEW_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_XDITVIEW_VERSION = 1.0.6
+XAPP_XDITVIEW_SOURCE = xditview-$(XAPP_XDITVIEW_VERSION).tar.xz
+XAPP_XDITVIEW_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XDITVIEW_LICENSE = MIT
 XAPP_XDITVIEW_LICENSE_FILES = COPYING
 XAPP_XDITVIEW_DEPENDENCIES = xlib_libXaw
