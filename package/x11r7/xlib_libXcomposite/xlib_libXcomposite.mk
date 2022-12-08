d39f7ecd4938158183adfc13147a9d3b61c85a8e
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-12-08 23:37:20 +0100

    package/x11r7/xlib_libXcomposite: bump version to 0.4.6
    https://lists.x.org/archives/xorg-announce/2022-December/003276.html
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/x11r7/xlib_libXcomposite/xlib_libXcomposite.mk
+++ package/x11r7/xlib_libXcomposite/xlib_libXcomposite.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XLIB_LIBXCOMPOSITE_VERSION = 0.4.5
-XLIB_LIBXCOMPOSITE_SOURCE = libXcomposite-$(XLIB_LIBXCOMPOSITE_VERSION).tar.bz2
-XLIB_LIBXCOMPOSITE_SITE = http://xorg.freedesktop.org/releases/individual/lib
+XLIB_LIBXCOMPOSITE_VERSION = 0.4.6
+XLIB_LIBXCOMPOSITE_SOURCE = libXcomposite-$(XLIB_LIBXCOMPOSITE_VERSION).tar.xz
+XLIB_LIBXCOMPOSITE_SITE = https://xorg.freedesktop.org/archive/individual/lib
 XLIB_LIBXCOMPOSITE_LICENSE = MIT
 XLIB_LIBXCOMPOSITE_LICENSE_FILES = COPYING
 XLIB_LIBXCOMPOSITE_INSTALL_STAGING = YES
