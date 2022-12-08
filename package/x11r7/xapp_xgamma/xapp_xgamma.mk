1564c1e6e9e9e72daae419c1a839b986485eff5a
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-12-08 23:37:28 +0100

    package/x11r7/xapp_xgamma: bump version to 1.0.7
    https://lists.x.org/archives/xorg-announce/2022-December/003267.html
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/x11r7/xapp_xgamma/xapp_xgamma.mk
+++ package/x11r7/xapp_xgamma/xapp_xgamma.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_XGAMMA_VERSION = 1.0.6
-XAPP_XGAMMA_SOURCE = xgamma-$(XAPP_XGAMMA_VERSION).tar.bz2
-XAPP_XGAMMA_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_XGAMMA_VERSION = 1.0.7
+XAPP_XGAMMA_SOURCE = xgamma-$(XAPP_XGAMMA_VERSION).tar.xz
+XAPP_XGAMMA_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XGAMMA_LICENSE = MIT
 XAPP_XGAMMA_LICENSE_FILES = COPYING
 XAPP_XGAMMA_DEPENDENCIES = xlib_libXxf86vm
