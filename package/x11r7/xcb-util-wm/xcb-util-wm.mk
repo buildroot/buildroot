f7f74ba0b3f8d5dd0dcf5af42e0622749e57a7e4
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-12-08 23:37:51 +0100

    package/x11r7/xcb-util-wm: bump version to 0.4.2
    https://lists.x.org/archives/xorg-announce/2022-October/003235.html
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/x11r7/xcb-util-wm/xcb-util-wm.mk
+++ package/x11r7/xcb-util-wm/xcb-util-wm.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XCB_UTIL_WM_VERSION = 0.4.1
-XCB_UTIL_WM_SITE = http://xcb.freedesktop.org/dist
-XCB_UTIL_WM_SOURCE = xcb-util-wm-$(XCB_UTIL_WM_VERSION).tar.bz2
+XCB_UTIL_WM_VERSION = 0.4.2
+XCB_UTIL_WM_SITE = https://xorg.freedesktop.org/archive/individual/lib
+XCB_UTIL_WM_SOURCE = xcb-util-wm-$(XCB_UTIL_WM_VERSION).tar.xz
 XCB_UTIL_WM_INSTALL_STAGING = YES
 XCB_UTIL_WM_LICENSE = MIT
 XCB_UTIL_WM_LICENSE_FILES = COPYING
