52264859725c6af2f88b0df6cf59f70b3b6d106d
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-12-08 23:38:01 +0100

    package/x11r7/xapp_viewres: bump version to 1.0.7
    https://lists.x.org/archives/xorg-announce/2022-October/003219.html
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/x11r7/xapp_viewres/xapp_viewres.mk
+++ package/x11r7/xapp_viewres/xapp_viewres.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_VIEWRES_VERSION = 1.0.6
-XAPP_VIEWRES_SOURCE = viewres-$(XAPP_VIEWRES_VERSION).tar.bz2
-XAPP_VIEWRES_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_VIEWRES_VERSION = 1.0.7
+XAPP_VIEWRES_SOURCE = viewres-$(XAPP_VIEWRES_VERSION).tar.xz
+XAPP_VIEWRES_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_VIEWRES_LICENSE = MIT
 XAPP_VIEWRES_LICENSE_FILES = COPYING
 XAPP_VIEWRES_DEPENDENCIES = xlib_libXaw
