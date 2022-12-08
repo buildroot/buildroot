3500526b189e4a75f3b969ac54685fc8a4b2e15c
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-12-08 23:37:26 +0100

    package/x11r7/xapp_xprop: bump version to 1.2.6
    https://lists.x.org/archives/xorg-announce/2022-December/003269.html
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/x11r7/xapp_xprop/xapp_xprop.mk
+++ package/x11r7/xapp_xprop/xapp_xprop.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_XPROP_VERSION = 1.2.5
-XAPP_XPROP_SOURCE = xprop-$(XAPP_XPROP_VERSION).tar.bz2
-XAPP_XPROP_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_XPROP_VERSION = 1.2.6
+XAPP_XPROP_SOURCE = xprop-$(XAPP_XPROP_VERSION).tar.xz
+XAPP_XPROP_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XPROP_LICENSE = MIT
 XAPP_XPROP_LICENSE_FILES = COPYING
 XAPP_XPROP_DEPENDENCIES = xlib_libX11 xlib_libXmu
