bac3d21a3fb3ef872b538adda01725c2fb77dd75
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-12-08 23:37:19 +0100

    package/x11r7/xlib_libXdamage: bump version to 1.1.6
    https://lists.x.org/archives/xorg-announce/2022-December/003277.html
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/x11r7/xlib_libXdamage/xlib_libXdamage.mk
+++ package/x11r7/xlib_libXdamage/xlib_libXdamage.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XLIB_LIBXDAMAGE_VERSION = 1.1.5
-XLIB_LIBXDAMAGE_SOURCE = libXdamage-$(XLIB_LIBXDAMAGE_VERSION).tar.bz2
-XLIB_LIBXDAMAGE_SITE = http://xorg.freedesktop.org/releases/individual/lib
+XLIB_LIBXDAMAGE_VERSION = 1.1.6
+XLIB_LIBXDAMAGE_SOURCE = libXdamage-$(XLIB_LIBXDAMAGE_VERSION).tar.xz
+XLIB_LIBXDAMAGE_SITE = https://xorg.freedesktop.org/archive/individual/lib
 XLIB_LIBXDAMAGE_LICENSE = MIT
 XLIB_LIBXDAMAGE_LICENSE_FILES = COPYING
 XLIB_LIBXDAMAGE_INSTALL_STAGING = YES
