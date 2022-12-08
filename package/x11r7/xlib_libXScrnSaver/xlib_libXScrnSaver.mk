8929d19dfba43af98ae1c981131e33d366f45e8a
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-12-08 23:37:17 +0100

    package/x11r7/xlib_libXScrnSaver: bump version to 1.2.4
    https://lists.x.org/archives/xorg-announce/2022-December/003279.html
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/x11r7/xlib_libXScrnSaver/xlib_libXScrnSaver.mk
+++ package/x11r7/xlib_libXScrnSaver/xlib_libXScrnSaver.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XLIB_LIBXSCRNSAVER_VERSION = 1.2.3
-XLIB_LIBXSCRNSAVER_SOURCE = libXScrnSaver-$(XLIB_LIBXSCRNSAVER_VERSION).tar.bz2
-XLIB_LIBXSCRNSAVER_SITE = http://xorg.freedesktop.org/releases/individual/lib
+XLIB_LIBXSCRNSAVER_VERSION = 1.2.4
+XLIB_LIBXSCRNSAVER_SOURCE = libXScrnSaver-$(XLIB_LIBXSCRNSAVER_VERSION).tar.xz
+XLIB_LIBXSCRNSAVER_SITE = https://xorg.freedesktop.org/archive/individual/lib
 XLIB_LIBXSCRNSAVER_LICENSE = MIT
 XLIB_LIBXSCRNSAVER_LICENSE_FILES = COPYING
 XLIB_LIBXSCRNSAVER_INSTALL_STAGING = YES
