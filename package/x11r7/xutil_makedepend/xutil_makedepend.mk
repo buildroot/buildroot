228780fbde4c2b645ea52a870df5ec020912ddf8
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-12-08 23:37:45 +0100

    package/x11r7/xutil_makedepend: bump version to 1.0.7
    https://lists.x.org/archives/xorg-announce/2022-October/003243.html
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/x11r7/xutil_makedepend/xutil_makedepend.mk
+++ package/x11r7/xutil_makedepend/xutil_makedepend.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XUTIL_MAKEDEPEND_VERSION = 1.0.6
-XUTIL_MAKEDEPEND_SOURCE = makedepend-$(XUTIL_MAKEDEPEND_VERSION).tar.bz2
-XUTIL_MAKEDEPEND_SITE = http://xorg.freedesktop.org/releases/individual/util
+XUTIL_MAKEDEPEND_VERSION = 1.0.7
+XUTIL_MAKEDEPEND_SOURCE = makedepend-$(XUTIL_MAKEDEPEND_VERSION).tar.xz
+XUTIL_MAKEDEPEND_SITE = https://xorg.freedesktop.org/archive/individual/util
 XUTIL_MAKEDEPEND_LICENSE = MIT
 XUTIL_MAKEDEPEND_LICENSE_FILES = COPYING
 
