d7b796406e93b405ad9c276ac96fef7c4007cb15
James Hilliard <james.hilliard1@gmail.com>
2022-12-10 10:13:30 -0700

    package/python-dnspython: bump to version 2.2.1
    Backport patch removing wheel dependency.
    
    Add new host-python-setuptools-scm build dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-dnspython/python-dnspython.mk
+++ package/python-dnspython/python-dnspython.mk
@@ -4,24 +4,14 @@
 #
 ################################################################################
 
-PYTHON_DNSPYTHON_VERSION = 2.1.0
-PYTHON_DNSPYTHON_SOURCE = dnspython-$(PYTHON_DNSPYTHON_VERSION).zip
-PYTHON_DNSPYTHON_SITE = https://files.pythonhosted.org/packages/13/27/5277de856f605f3429d752a39af3588e29d10181a3aa2e2ee471d817485a
+PYTHON_DNSPYTHON_VERSION = 2.2.1
+PYTHON_DNSPYTHON_SOURCE = dnspython-$(PYTHON_DNSPYTHON_VERSION).tar.gz
+PYTHON_DNSPYTHON_SITE = https://files.pythonhosted.org/packages/99/fb/e7cd35bba24295ad41abfdff30f6b4c271fd6ac70d20132fa503c3e768e0
 PYTHON_DNSPYTHON_LICENSE = ISC
 PYTHON_DNSPYTHON_LICENSE_FILES = LICENSE
 PYTHON_DNSPYTHON_SETUP_TYPE = setuptools
-
-define PYTHON_DNSPYTHON_EXTRACT_CMDS
-	$(UNZIP) -d $(@D) $(PYTHON_DNSPYTHON_DL_DIR)/$(PYTHON_DNSPYTHON_SOURCE)
-	mv $(@D)/dnspython-$(PYTHON_DNSPYTHON_VERSION)/* $(@D)
-	$(RM) -r $(@D)/dnspython-$(PYTHON_DNSPYTHON_VERSION)
-endef
-
-define HOST_PYTHON_DNSPYTHON_EXTRACT_CMDS
-	$(UNZIP) -d $(@D) $(HOST_PYTHON_DNSPYTHON_DL_DIR)/$(HOST_PYTHON_DNSPYTHON_SOURCE)
-	mv $(@D)/dnspython-$(HOST_PYTHON_DNSPYTHON_VERSION)/* $(@D)
-	$(RM) -r $(@D)/dnspython-$(HOST_PYTHON_DNSPYTHON_VERSION)
-endef
+PYTHON_DNSPYTHON_DEPENDENCIES = host-python-setuptools-scm
+HOST_PYTHON_DNSPYTHON_DEPENDENCIES = host-python-setuptools-scm
 
 $(eval $(python-package))
 $(eval $(host-python-package))
