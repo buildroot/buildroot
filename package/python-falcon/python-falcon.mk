3ef45500e35fac59e403af4709e16e40d2c9bb4a
James Hilliard <james.hilliard1@gmail.com>
2022-12-10 10:44:14 -0700

    package/python-falcon: bump to version 3.1.1
    Always build cython modules as sdist does not contain prebuilt
    cython sources.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-falcon/python-falcon.mk
+++ package/python-falcon/python-falcon.mk
@@ -4,15 +4,12 @@
 #
 ################################################################################
 
-PYTHON_FALCON_VERSION = 3.1.0
+PYTHON_FALCON_VERSION = 3.1.1
 PYTHON_FALCON_SOURCE = falcon-$(PYTHON_FALCON_VERSION).tar.gz
-PYTHON_FALCON_SITE = https://files.pythonhosted.org/packages/36/53/4fd90c6c841bc2e4be29ab92c65e5406df9096c421f138bef9d95d43afc9
+PYTHON_FALCON_SITE = https://files.pythonhosted.org/packages/29/bc/c11c9a14bb5b4d18a024ee51da15b793d1c869d151bb4101e324e0d055a8
 PYTHON_FALCON_SETUP_TYPE = setuptools
 PYTHON_FALCON_LICENSE = Apache-2.0
 PYTHON_FALCON_LICENSE_FILES = LICENSE
-
-ifeq ($(BR2_PACKAGE_HOST_PYTHON_CYTHON),y)
 PYTHON_FALCON_DEPENDENCIES += host-python-cython
-endif
 
 $(eval $(python-package))
