85516a05b3bbe41793d431043ab505bb89786d1d
James Hilliard <james.hilliard1@gmail.com>
2022-12-10 12:07:04 -0700

    package/python-packaging: bump to version 22.0
    Migrate to flit package infrastructure.
    
    Drop python-parsing dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-packaging/python-packaging.mk
+++ package/python-packaging/python-packaging.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-PYTHON_PACKAGING_VERSION = 21.3
+PYTHON_PACKAGING_VERSION = 22.0
 PYTHON_PACKAGING_SOURCE = packaging-$(PYTHON_PACKAGING_VERSION).tar.gz
-PYTHON_PACKAGING_SITE = https://files.pythonhosted.org/packages/df/9e/d1a7217f69310c1db8fdf8ab396229f55a699ce34a203691794c5d1cad0c
-PYTHON_PACKAGING_SETUP_TYPE = setuptools
+PYTHON_PACKAGING_SITE = https://files.pythonhosted.org/packages/6b/f7/c240d7654ddd2d2f3f328d8468d4f1f876865f6b9038b146bec0a6737c65
+PYTHON_PACKAGING_SETUP_TYPE = flit
 PYTHON_PACKAGING_LICENSE = Apache-2.0 or BSD-2-Clause
 PYTHON_PACKAGING_LICENSE_FILES = LICENSE LICENSE.APACHE LICENSE.BSD
-HOST_PYTHON_PACKAGING_DEPENDENCIES = host-python-pyparsing
+HOST_PYTHON_PACKAGING_SETUP_TYPE = flit-bootstrap
 
 $(eval $(python-package))
 $(eval $(host-python-package))
