5efe767420aed5aa88b56a37316720812adc642f
Giulio Benetti <giulio.benetti@benettiengineering.com>
2022-12-11 16:21:53 +0100

    package/mali-driver: bump to latest version to build with Linux version up to 6.0
    Update to the latest upstream version, to which a few fixes were added
    to fix build failures with Aarch64 and Linux 5.18/5.19/6.0
    
    Fixes:
    
      http://autobuild.buildroot.net/results/6f073fea149533a294dd838ce8373185236fe1d4/
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 8405dcae33b47bd5b8cf5ef4faf0db01644a1d4a)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/mali-driver/mali-driver.mk
+++ package/mali-driver/mali-driver.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MALI_DRIVER_VERSION = 3d697de9bce8bc68bf9408d5407873ce32d0f4fc
+MALI_DRIVER_VERSION = 87c13e6994b20b5068e3a5e4f4c1b64db666a4c8
 MALI_DRIVER_SITE = $(call github,bootlin,mali-driver,$(MALI_DRIVER_VERSION))
 MALI_DRIVER_DEPENDENCIES = linux
 MALI_DRIVER_LICENSE = GPL-2.0
