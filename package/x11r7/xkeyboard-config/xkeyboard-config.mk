10b329956a378f9f3806339355cd6df32133690c
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-12-12 22:27:13 +0100

    package/x11r7/xkeyboard-config: bump version to 2.37
    Release notes:
    https://gitlab.freedesktop.org/xorg/data/xkeyboard-config/-/blob/master/NEWS
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xkeyboard-config/xkeyboard-config.mk
+++ package/x11r7/xkeyboard-config/xkeyboard-config.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XKEYBOARD_CONFIG_VERSION = 2.36
+XKEYBOARD_CONFIG_VERSION = 2.37
 XKEYBOARD_CONFIG_SOURCE = xkeyboard-config-$(XKEYBOARD_CONFIG_VERSION).tar.xz
 XKEYBOARD_CONFIG_SITE = https://www.x.org/releases/individual/data/xkeyboard-config
 XKEYBOARD_CONFIG_LICENSE = MIT
