7dbe844bae38aba8e6080da3b650b89f702c0f1c
Thomas Devoogdt <thomas@devoogdt.com>
2022-12-12 16:35:34 +0100

    package/unifdef: add new package
    A small package that does "Selectively remove C preprocessor conditionals".
    
    The package will be needed for the upcoming webkitgtk 2.40.0 release.
    I personally added it to get started with 2.39.2.
    
    See: https://github.com/WebKit/WebKit/commit/f76a7e30e5749af897b83cc75b2534b1afa32552
    Signed-off-by: Thomas Devoogdt <thomas.devoogdt@barco.com>
    [Peter: use prefix=]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/unifdef/unifdef.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# unifdef
+#
+################################################################################
+
+UNIFDEF_VERSION = 2.12
+UNIFDEF_SITE = https://dotat.at/prog/unifdef
+UNIFDEF_LICENSE = BSD-2-Clause
+UNIFDEF_LICENSE_FILES = COPYING
+
+define HOST_UNIFDEF_BUILD_CMDS
+	$(HOST_CONFIGURE_OPTS) $(MAKE) -C $(@D)
+endef
+
+define HOST_UNIFDEF_INSTALL_CMDS
+	$(HOST_CONFIGURE_OPTS) $(MAKE) -C $(@D) prefix=$(HOST_DIR) install
+endef
+
+$(eval $(host-generic-package))
