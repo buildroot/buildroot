2ae7ff01f8fc704a094361ad7c54bb916c4b8762
charles <charles@ccxtechnologies.com>
2022-12-12 14:55:43 -0500

    add packages required for the latest version of ipython

---
+++ package/python-pure_eval/python-pure_eval.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# python-pure_eval
+#
+################################################################################
+
+PYTHON_PURE_EVAL_VERSION = 0.2.2
+PYTHON_PURE_EVAL_SOURCE = pure_eval-$(PYTHON_PURE_EVAL_VERSION).tar.gz
+PYTHON_PURE_EVAL_SITE = https://files.pythonhosted.org/packages/97/5a/0bc937c25d3ce4e0a74335222aee05455d6afa2888032185f8ab50cdf6fd
+PYTHON_PURE_EVAL_SETUP_TYPE = setuptools
+PYTHON_PURE_EVAL_LICENSE = MIT
+PYTHON_PURE_EVAL_LICENSE_FILES = LICENSE
+
+$(eval $(python-package))
