8ef654997caba04dffecdc4105b214abf7d02ae5
James Hilliard <james.hilliard1@gmail.com>
2022-12-13 14:10:49 -0700

    package/swupdate: bump to version 2022.12
    Drop patch which is now upstream.
    
    Update service files based on latest versions in:
    https://github.com/sbabic/meta-swupdate/tree/master/recipes-support/swupdate/swupdate
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/swupdate/swupdate.mk
+++ package/swupdate/swupdate.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SWUPDATE_VERSION = 2022.05
+SWUPDATE_VERSION = 2022.12
 SWUPDATE_SITE = $(call github,sbabic,swupdate,$(SWUPDATE_VERSION))
 SWUPDATE_LICENSE = GPL-2.0, GPL-2.0+, LGPL-2.1+, MIT, ISC, BSD-1-Clause, BSD-3-Clause, CC0-1.0, CC-BY-SA-4.0, OFL-1.1
 SWUPDATE_LICENSE_FILES = LICENSES/BSD-1-Clause.txt \
