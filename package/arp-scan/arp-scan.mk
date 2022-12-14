1975c531764828fbdaff673e4cf1f25ea009aa2c
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-12-14 23:20:12 +0100

    package/arp-scan: bump to version 1.10.0
    - Use official tarball and so drop autoreconf
    - libcap is an optional dependency since
      https://github.com/royhills/arp-scan/commit/923f123a281139f1ec3daceb1db4bc7317ec6978
    
    https://github.com/royhills/arp-scan/releases/tag/1.10.0
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/arp-scan/arp-scan.mk
+++ package/arp-scan/arp-scan.mk
@@ -4,17 +4,23 @@
 #
 ################################################################################
 
-ARP_SCAN_VERSION = 1.9.8
-ARP_SCAN_SITE = $(call github,royhills,arp-scan,$(ARP_SCAN_VERSION))
+ARP_SCAN_VERSION = 1.10.0
+ARP_SCAN_SITE = https://github.com/royhills/arp-scan/releases/download/$(ARP_SCAN_VERSION)
 ARP_SCAN_LICENSE = GPL-3.0+
 ARP_SCAN_LICENSE_FILES = COPYING
 ARP_SCAN_DEPENDENCIES = libpcap
-ARP_SCAN_AUTORECONF = YES
 
 ifeq ($(BR2_STATIC_LIBS),y)
 ARP_SCAN_CONF_OPTS += LIBS="`$(STAGING_DIR)/usr/bin/pcap-config --static --additional-libs`"
 endif
 
 ARP_SCAN_CONF_ENV = pgac_cv_snprintf_long_long_int_format='%lld'
+
+ifeq ($(BR2_PACKAGE_LIBCAP),y)
+ARP_SCAN_DEPENDENCIES += libcap
+ARP_SCAN_CONF_OPTS += --with-libcap
+else
+ARP_SCAN_CONF_OPTS += --without-libcap
+endif
 
 $(eval $(autotools-package))
