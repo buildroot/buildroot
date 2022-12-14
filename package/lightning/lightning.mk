af6f7aa76db5aed1eada0b27bd0375610a2fc728
Paul Cercueil <paul@crapouillou.net>
2022-12-14 18:17:54 +0100

    package/lightning: bump version to 2.2.0
    Notable changes in this new version include support for the Apple
    Aarch64 ABI, and compare-and-swap atomic instructions.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>
    [Peter: drop _AUTORECONF]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/lightning/lightning.mk
+++ package/lightning/lightning.mk
@@ -4,14 +4,12 @@
 #
 ################################################################################
 
-LIGHTNING_VERSION = 2.1.3
+LIGHTNING_VERSION = 2.2.0
 LIGHTNING_SITE = $(BR2_GNU_MIRROR)/lightning
 LIGHTNING_LICENSE = LGPL-3.0+
 LIGHTNING_LICENSE_FILES = COPYING.LESSER
 LIGHTNING_INSTALL_STAGING = YES
 LIGHTNING_CPE_ID_VENDOR = gnu
-# We're patching include/Makefile.am
-LIGHTNING_AUTORECONF = YES
 
 ifeq ($(BR2_PACKAGE_LIGHTNING_DISASSEMBLER),y)
 LIGHTNING_DEPENDENCIES += binutils zlib
