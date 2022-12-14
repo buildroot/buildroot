f62d409cb8d1bcd015b9ca4aeca31b2d1c8dbf02
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-12-14 20:04:36 +0100

    package/x11r7/xapp_xhost: bump version to 1.0.9
    Release notes:
    https://lists.x.org/archives/xorg-announce/2022-December/003301.html
    
    Updated license hash due to copyright year bump:
    https://gitlab.freedesktop.org/xorg/app/xhost/-/commit/167ad7fa3a339da96798da49d05c3125045a87c2
    
    Switched tarball to xz, updated _SITE.
    
    Added optional support for gettext:
    https://gitlab.freedesktop.org/xorg/app/xhost/-/commit/167ad7fa3a339da96798da49d05c3125045a87c2
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xapp_xhost/xapp_xhost.mk
+++ package/x11r7/xapp_xhost/xapp_xhost.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-XAPP_XHOST_VERSION = 1.0.8
-XAPP_XHOST_SOURCE = xhost-$(XAPP_XHOST_VERSION).tar.bz2
-XAPP_XHOST_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_XHOST_VERSION = 1.0.9
+XAPP_XHOST_SOURCE = xhost-$(XAPP_XHOST_VERSION).tar.xz
+XAPP_XHOST_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XHOST_LICENSE = MIT
 XAPP_XHOST_LICENSE_FILES = COPYING
-XAPP_XHOST_DEPENDENCIES = xlib_libX11 xlib_libXmu
+XAPP_XHOST_DEPENDENCIES = xlib_libX11 xlib_libXmu $(TARGET_NLS_DEPENDENCIES)
+XAPP_XHOST_CONF_ENV = LIBS=$(TARGET_NLS_LIBS)
 
 $(eval $(autotools-package))
