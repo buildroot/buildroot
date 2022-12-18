7854488512a96cbcb2d48313b89c7d085a1c2cfb
Bernd Kuhls <bernd.kuhls@t-online.de>
2022-12-18 23:03:18 +0100

    package/linux-firmware: bump to version 20221214
    Update WHENCE file hash due to date/firmware file updates:
    https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/commit/?id=32d8681d21f2eef052cadfdfafc47e02eed6c2b2
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/linux-firmware/linux-firmware.mk
+++ package/linux-firmware/linux-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LINUX_FIRMWARE_VERSION = 20221109
+LINUX_FIRMWARE_VERSION = 20221214
 LINUX_FIRMWARE_SOURCE = linux-firmware-$(LINUX_FIRMWARE_VERSION).tar.xz
 LINUX_FIRMWARE_SITE = $(BR2_KERNEL_MIRROR)/linux/kernel/firmware
 LINUX_FIRMWARE_INSTALL_IMAGES = YES
