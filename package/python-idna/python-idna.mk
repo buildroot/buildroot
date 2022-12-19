37939faf8cad97d1ca57c44b76a850bb734808ff
tamara <tamara@ccxtechnologies.com>
2022-12-19 12:54:17 -0500

    idna uses flit according to its pyproject.toml

--- package/python-idna/python-idna.mk
+++ package/python-idna/python-idna.mk
@@ -9,7 +9,7 @@ PYTHON_IDNA_SOURCE = idna-$(PYTHON_IDNA_VERSION).tar.gz
 PYTHON_IDNA_SITE = https://files.pythonhosted.org/packages/8b/e1/43beb3d38dba6cb420cefa297822eac205a277ab43e5ba5d5c46faf96438
 PYTHON_IDNA_LICENSE = BSD-3-Clause
 PYTHON_IDNA_LICENSE_FILES = LICENSE.md
-PYTHON_IDNA_SETUP_TYPE = setuptools
+PYTHON_IDNA_SETUP_TYPE = flit
 
 $(eval $(python-package))
 $(eval $(host-python-package))
