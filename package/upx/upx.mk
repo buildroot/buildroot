1567516d8984a4bddfbee30ee69c771cf3f8e224
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-12-23 18:38:17 +0100

    package/upx: bump to version 4.0.1
    - Drop patch (already in version)
    - cmake must be used since
      https://github.com/upx/upx/commit/c7d8b6fed1b551158540e1d2e884dc224fce474c
    - ucl and zlib are directly included in upx since
      https://github.com/upx/upx/commit/61e1366122f8d1b9e2434a8103e7af00a35ea902
    
    https://github.com/upx/upx/blob/v4.0.1/NEWS
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/upx/upx.mk
+++ package/upx/upx.mk
@@ -4,28 +4,12 @@
 #
 ################################################################################
 
-UPX_VERSION = 3.96
+UPX_VERSION = 4.0.1
 UPX_SITE = https://github.com/upx/upx/releases/download/v$(UPX_VERSION)
 UPX_SOURCE = upx-$(UPX_VERSION)-src.tar.xz
 UPX_LICENSE = GPL-2.0+
 UPX_LICENSE_FILES = COPYING
 UPX_CPE_ID_VENDOR = upx_project
+UPX_SUPPORTS_IN_SOURCE_BUILD = NO
 
-# 0001-Check-DT_REL-DT_RELA-DT_RELSZ-DT_RELASZ.patch
-UPX_IGNORE_CVES += CVE-2021-20285
-
-HOST_UPX_DEPENDENCIES = host-ucl host-zlib
-
-# We need to specify all, otherwise the default target only prints a message
-# stating to "please choose a target for 'make'"... :-(
-define HOST_UPX_BUILD_CMDS
-	$(HOST_MAKE_ENV) $(HOST_CONFIGURE_OPTS) $(MAKE) \
-		UPX_UCLDIR=$(HOST_DIR) CXXFLAGS_WERROR= -C $(@D) all
-endef
-
-# UPX has no install procedure, so install it manually.
-define HOST_UPX_INSTALL_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/src/upx.out $(HOST_DIR)/bin/upx
-endef
-
-$(eval $(host-generic-package))
+$(eval $(host-cmake-package))
