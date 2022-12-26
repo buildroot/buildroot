eda08c6b88ca466c27cb02004e92ac5ae6e60998
Francois Perrad <fperrad@gmail.com>
2022-12-26 08:47:48 +0100

    package/lua-cjson: bump to version 2.1.0.10
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/lua-cjson/lua-cjson.mk
+++ package/lua-cjson/lua-cjson.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LUA_CJSON_VERSION = 2.1.0.6-1
+LUA_CJSON_VERSION = 2.1.0.10-1
 LUA_CJSON_SUBDIR = lua-cjson
 LUA_CJSON_LICENSE = MIT
 LUA_CJSON_LICENSE_FILES = $(LUA_CJSON_SUBDIR)/LICENSE
