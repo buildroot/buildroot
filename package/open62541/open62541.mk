83b2e53dcde56d478928d917604bf3af1f903d9f
Francois Perrad <fperrad@gmail.com>
2022-12-26 08:49:01 +0100

    package/open62541: bump to version 1.3.4
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/open62541/open62541.mk
+++ package/open62541/open62541.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPEN62541_VERSION = v1.3.3
+OPEN62541_VERSION = v1.3.4
 OPEN62541_SITE_METHOD = git
 OPEN62541_SITE = https://github.com/open62541/open62541.git
 OPEN62541_GIT_SUBMODULES = YES
