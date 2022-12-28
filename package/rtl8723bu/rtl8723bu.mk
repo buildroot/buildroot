030cbe37033a3c179c5c51dbd0427cc443efb330
Giulio Benetti <giulio.benetti@benettiengineering.com>
2022-12-28 19:42:48 +0100

    package/rtl8723bu: bump to 2022-12-28 version to fix build failure with Linux 6.1
    Let's also drop local patch[0] that has been upstreamed.
    
    [0]: https://github.com/lwfinger/rtl8723bu/commit/dde7bddfddddb7212f2d9671391e2096d770c449
    
    Fixes:
    http://autobuild.buildroot.net/results/cc8/cc84b5f62ca59572f0f52086f157acc0409c03f5/
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/rtl8723bu/rtl8723bu.mk
+++ package/rtl8723bu/rtl8723bu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTL8723BU_VERSION = d79a676a8d3f0bb6ac8af126689c6ac6869cb6f2
+RTL8723BU_VERSION = f1dbe1c8ccc2c5b2cbeb84da6b9f0e547a84a255
 RTL8723BU_SITE = $(call github,lwfinger,rtl8723bu,$(RTL8723BU_VERSION))
 RTL8723BU_LICENSE = GPL-2.0, proprietary (*.bin firmware blobs)
 
