3b3dbd0183265c3f7da49b0b331688fb7573383f
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-12-29 10:33:55 +0100

    package/ulogd: add libdbi support
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ulogd/ulogd.mk
+++ package/ulogd/ulogd.mk
@@ -7,7 +7,6 @@
 ULOGD_VERSION = 2.0.8
 ULOGD_SOURCE = ulogd-$(ULOGD_VERSION).tar.bz2
 ULOGD_SITE = http://www.netfilter.org/projects/ulogd/files
-ULOGD_CONF_OPTS = --disable-dbi
 ULOGD_DEPENDENCIES = host-pkgconf \
 	libmnl libnetfilter_acct libnetfilter_conntrack libnetfilter_log \
 	libnfnetlink
@@ -17,6 +16,12 @@ ULOGD_SELINUX_MODULES = ulogd
 
 # DB backends need threads
 ifeq ($(BR2_TOOLCHAIN_HAS_THREADS),y)
+ifeq ($(BR2_PACKAGE_LIBDBI),y)
+ULOGD_CONF_OPTS += --enable-dbi
+ULOGD_DEPENDENCIES += libdbi
+else
+ULOGD_CONF_OPTS += --disable-dbi
+endif
 ifeq ($(BR2_PACKAGE_MYSQL),y)
 ULOGD_CONF_OPTS += --enable-mysql
 ULOGD_DEPENDENCIES += mysql
