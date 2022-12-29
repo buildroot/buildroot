4aca99f51cc97cfc3bcd3affa65f4694004dd444
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-12-29 11:58:18 +0100

    package/wireshark: bump to version 4.0.2
    - pcre2 is mandatory since version 3.7.0 and
      https://github.com/wireshark/wireshark/commit/b9f2e4b7fa091719b93f6256ac34be8e32bd8e94
    - Set HAVE_C99_VSNPRINTF to avoid a build failure due to
      check_c_source_runs
    - DISABLE_WERROR was renamed to ENABLE_WERROR
    
    4.x series is the new stable release
    
    https://www.wireshark.org/news/20221207.html
    https://www.wireshark.org/docs/relnotes/wireshark-4.0.2.html
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/wireshark/wireshark.mk
+++ package/wireshark/wireshark.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WIRESHARK_VERSION = 3.4.16
+WIRESHARK_VERSION = 4.0.2
 WIRESHARK_SOURCE = wireshark-$(WIRESHARK_VERSION).tar.xz
 WIRESHARK_SITE = https://www.wireshark.org/download/src/all-versions
 WIRESHARK_LICENSE = wireshark license
@@ -18,13 +18,15 @@ WIRESHARK_DEPENDENCIES = \
 	libgcrypt \
 	libglib2 \
 	libpcap \
+	pcre2 \
 	speexdsp
 
 WIRESHARK_CONF_OPTS = \
-	-DDISABLE_WERROR=ON \
 	-DENABLE_ILBC=OFF \
 	-DENABLE_PCAP=ON \
 	-DENABLE_SMI=OFF \
+	-DENABLE_WERROR=OFF \
+	-DHAVE_C99_VSNPRINTF=ON \
 	-DLEMON_C_COMPILER=$(HOSTCC_NOCCACHE)
 
 ifeq ($(BR2_TOOLCHAIN_HAS_LIBATOMIC),y)
