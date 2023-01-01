1b68271287d470f4dfeab074a1222da869328f4e
Julien Olivain <ju.o@free.fr>
2023-01-01 12:25:27 +0100

    package/fmlib: bump to version LSDK-21.08
    License file was updated in commit:
    https://github.com/nxp-qoriq/fmlib/commit/22126510006f366981c078a340e012a1e0415429
    
    Notable changes in the license update are:
    - GPL-2.0+ option was removed,
    - Copyright years updated,
    - NXP was added,
    - License text was reflowed.
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/fmlib/fmlib.mk
+++ package/fmlib/fmlib.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-FMLIB_VERSION = 43fa98fdbf0c697167e415c3f060896d5b482791
+FMLIB_VERSION = LSDK-21.08
 FMLIB_SITE = $(call github,nxp-qoriq,fmlib,$(FMLIB_VERSION))
-FMLIB_LICENSE = BSD-3-Clause, GPL-2.0+
+FMLIB_LICENSE = BSD-3-Clause
 FMLIB_LICENSE_FILES = COPYING
 FMLIB_DEPENDENCIES = linux
 FMLIB_INSTALL_STAGING = YES
