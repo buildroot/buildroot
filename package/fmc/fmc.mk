c943cb38e4174cd349b4bb80e5e16269b25079d2
Julien Olivain <ju.o@free.fr>
2023-01-01 12:25:28 +0100

    package/fmc: bump to version LSDK-21.08
    This commit also updates the sed pattern, as the default platform was
    changed in:
    https://github.com/nxp-qoriq/fmc/commit/4377bfc24626a0db3bedb6e1cbb201f6f6df964c
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/fmc/fmc.mk
+++ package/fmc/fmc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FMC_VERSION = a079d2c844edd85dff85a317a63198e7988bcd09
+FMC_VERSION = LSDK-21.08
 FMC_SITE = $(call github,nxp-qoriq,fmc,$(FMC_VERSION))
 FMC_LICENSE = MIT
 FMC_LICENSE_FILES = COPYING
@@ -26,7 +26,7 @@ endif
 FMC_PLATFORM = $(call qstrip,$(BR2_PACKAGE_FMLIB_PLATFORM))
 
 define FMC_BUILD_CMDS
-	$(SED) "s:P4080:$(FMC_PLATFORM):g" $(@D)/source/Makefile
+	$(SED) "s:LS1043:$(FMC_PLATFORM):g" $(@D)/source/Makefile
 	# The linking step has dependency issues so using MAKE1
 	$(TARGET_MAKE_ENV) $(MAKE1) $(FMC_MAKE_OPTS) -C $(@D)/source
 endef
