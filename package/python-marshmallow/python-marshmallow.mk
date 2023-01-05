9c3e7014105eb69db7d9c9f662ab0a5054f35797
Martin Hundebøll <martin@geanix.com>
2023-01-05 13:59:58 +0100

    package/python-marshmallow: new package
    marshmallow is a dependency of to-be-added python-flask-smorest.
    
    Signed-off-by: Martin Hundebøll <martin@geanix.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/python-marshmallow/python-marshmallow.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# python-marshmallow
+#
+################################################################################
+
+PYTHON_MARSHMALLOW_VERSION = 3.19.0
+PYTHON_MARSHMALLOW_SOURCE = marshmallow-$(PYTHON_MARSHMALLOW_VERSION).tar.gz
+PYTHON_MARSHMALLOW_SITE = https://files.pythonhosted.org/packages/5e/59/dd465e5ab0ccb879c410f88c75189a19fd437b12cd9a03b31579aef58709
+PYTHON_MARSHMALLOW_SETUP_TYPE = setuptools
+PYTHON_MARSHMALLOW_LICENSE = MIT
+PYTHON_MARSHMALLOW_LICENSE_FILES = LICENSE
+
+$(eval $(python-package))
