74686296a4a20438f1e94e22a5a3372df2e830cc
Peter Seiderer <ps.report@gmx.net>
2023-01-07 16:31:56 +0100

    package/qt6: bump version to 6.4.2
    For details see [1] and [2].
    
    [1] https://code.qt.io/cgit/qt/qtreleasenotes.git/about/qt/6.4.1/release-note.md
    [2] https://code.qt.io/cgit/qt/qtreleasenotes.git/about/qt/6.4.2/release-note.md
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Tested-by: Angelo Compagnucci <angelo@amarulasolutions.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/qt6/qt6.mk
+++ package/qt6/qt6.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 QT6_VERSION_MAJOR = 6.4
-QT6_VERSION = $(QT6_VERSION_MAJOR).0
+QT6_VERSION = $(QT6_VERSION_MAJOR).2
 QT6_SOURCE_TARBALL_PREFIX = everywhere-src
 QT6_SITE = https://download.qt.io/archive/qt/$(QT6_VERSION_MAJOR)/$(QT6_VERSION)/submodules
 
