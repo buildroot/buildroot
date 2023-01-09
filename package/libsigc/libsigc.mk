bb066af7f9d84abf890c5692879477aaca36bff8
Lang Daniel <d.lang@abatec.at>
2023-01-09 08:51:56 +0000

    package/libsigc: bump to version 3.4.0
    https://github.com/libsigcplusplus/libsigcplusplus/blob/3.4.0/NEWS
    
    Signed-off-by: Daniel Lang <d.lang@abatec.at>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libsigc/libsigc.mk
+++ package/libsigc/libsigc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBSIGC_VERSION_MAJOR = 3.2
+LIBSIGC_VERSION_MAJOR = 3.4
 LIBSIGC_VERSION = $(LIBSIGC_VERSION_MAJOR).0
 LIBSIGC_SOURCE = libsigc++-$(LIBSIGC_VERSION).tar.xz
 LIBSIGC_SITE = http://ftp.gnome.org/pub/GNOME/sources/libsigc++/$(LIBSIGC_VERSION_MAJOR)
@@ -13,6 +13,7 @@ LIBSIGC_LICENSE = LGPL-3.0+
 LIBSIGC_LICENSE_FILES = COPYING
 LIBSIGC_CONF_OPTS = \
 	-Dbuild-examples=false \
+	-Dbuild-tests=false \
 	-Dvalidation=false
 
 $(eval $(meson-package))
