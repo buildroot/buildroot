9b88d0f7b9775db73da55504f3968bc0ff25687a
James Hilliard <james.hilliard1@gmail.com>
2023-01-08 21:06:02 -0700

    package/python-filelock: bump to version 3.9.0
    Migrate from setuptools to hatchling pep517 build backend.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-filelock/python-filelock.mk
+++ package/python-filelock/python-filelock.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_FILELOCK_VERSION = 3.8.2
+PYTHON_FILELOCK_VERSION = 3.9.0
 PYTHON_FILELOCK_SOURCE = filelock-$(PYTHON_FILELOCK_VERSION).tar.gz
-PYTHON_FILELOCK_SITE = https://files.pythonhosted.org/packages/d8/73/292d9ea2370840a163e6dd2d2816a571244e9335e2f6ad957bf0527c492f
-PYTHON_FILELOCK_SETUP_TYPE = setuptools
+PYTHON_FILELOCK_SITE = https://files.pythonhosted.org/packages/0b/dc/eac02350f06c6ed78a655ceb04047df01b02c6b7ea3fc02d4df24ca87d24
+PYTHON_FILELOCK_SETUP_TYPE = pep517
 PYTHON_FILELOCK_LICENSE = Public Domain
 PYTHON_FILELOCK_LICENSE_FILES = LICENSE
+PYTHON_FILELOCK_DEPENDENCIES = host-python-hatchling host-python-hatch-vcs
 
 $(eval $(python-package))
