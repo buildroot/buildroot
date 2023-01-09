d5a1a38c8975153838f4e3f43ea03820613e4882
James Hilliard <james.hilliard1@gmail.com>
2023-01-08 21:43:01 -0700

    package/python-networkx: bump to version 3.0
    Drop no longer required decorator dependency.
    
    License hash changed due to year update:
    https://github.com/networkx/networkx/commit/49f20fe11d2a804a83d538759e06511eb90ad5a3
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-networkx/python-networkx.mk
+++ package/python-networkx/python-networkx.mk
@@ -4,15 +4,14 @@
 #
 ################################################################################
 
-PYTHON_NETWORKX_VERSION = 2.8.8
+PYTHON_NETWORKX_VERSION = 3.0
 PYTHON_NETWORKX_SOURCE = networkx-$(PYTHON_NETWORKX_VERSION).tar.gz
-PYTHON_NETWORKX_SITE = https://files.pythonhosted.org/packages/cd/16/c44e8550012735b8f21b3df7f39e8ba5a987fb764ac017ad5f3589735889
+PYTHON_NETWORKX_SITE = https://files.pythonhosted.org/packages/99/f9/d45c9ecf50a6b67a200e0bbd324201b5cd777dfc0e6c8f6d1620ce5a7ada
 PYTHON_NETWORKX_LICENSE = BSD-3-Clause
 PYTHON_NETWORKX_LICENSE_FILES = LICENSE.txt
 PYTHON_NETWORKX_CPE_ID_VENDOR = python
 PYTHON_NETWORKX_CPE_ID_PRODUCT = networkx
 PYTHON_NETWORKX_SETUP_TYPE = setuptools
-HOST_PYTHON_NETWORKX_DEPENDENCIES = host-python-decorator
 
 $(eval $(python-package))
 $(eval $(host-python-package))
