f947ac259692879cf651c13dc595236103b0d1f9
James Hilliard <james.hilliard1@gmail.com>
2023-01-08 21:23:14 -0700

    package/python-iniconfig: bump to version 2.0.0
    Migrate from setuptools to hatchling pep517 build backend.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Reviewed-by: Marcin Niestroj <m.niestroj@grinn-global.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-iniconfig/python-iniconfig.mk
+++ package/python-iniconfig/python-iniconfig.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_INICONFIG_VERSION = 1.1.1
+PYTHON_INICONFIG_VERSION = 2.0.0
 PYTHON_INICONFIG_SOURCE = iniconfig-$(PYTHON_INICONFIG_VERSION).tar.gz
-PYTHON_INICONFIG_SITE = https://files.pythonhosted.org/packages/23/a2/97899f6bd0e873fed3a7e67ae8d3a08b21799430fb4da15cfedf10d6e2c2
-PYTHON_INICONFIG_SETUP_TYPE = setuptools
+PYTHON_INICONFIG_SITE = https://files.pythonhosted.org/packages/d7/4b/cbd8e699e64a6f16ca3a8220661b5f83792b3017d0f79807cb8708d33913
+PYTHON_INICONFIG_SETUP_TYPE = pep517
 PYTHON_INICONFIG_LICENSE = MIT
 PYTHON_INICONFIG_LICENSE_FILES = LICENSE
+PYTHON_INICONFIG_DEPENDENCIES = host-python-hatchling host-python-hatch-vcs
 
 $(eval $(python-package))
