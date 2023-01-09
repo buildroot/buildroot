7be2ff33abdd049dc3670c5a6e2e385c1fba6f41
James Hilliard <james.hilliard1@gmail.com>
2023-01-08 20:23:18 -0700

    package/python-canopen: bump to version 2.1.0
    Verified license remains MIT after hash changed.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-canopen/python-canopen.mk
+++ package/python-canopen/python-canopen.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_CANOPEN_VERSION = 2.0.0
+PYTHON_CANOPEN_VERSION = 2.1.0
 PYTHON_CANOPEN_SOURCE = canopen-$(PYTHON_CANOPEN_VERSION).tar.gz
-PYTHON_CANOPEN_SITE = https://files.pythonhosted.org/packages/9e/d6/6ced4b410b904aeee5f7d0227187eae95a5ad769f01014a74fc0e016845e
+PYTHON_CANOPEN_SITE = https://files.pythonhosted.org/packages/29/d1/54462c949c384b1a9a2bd260143a8ec6f4259a18d119bd122e8f8a791b50
 PYTHON_CANOPEN_SETUP_TYPE = setuptools
 PYTHON_CANOPEN_LICENSE = MIT
 PYTHON_CANOPEN_LICENSE_FILES = LICENSE.txt
