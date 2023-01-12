e35750b5246e1cd7bce0c53b6e32cca592c4f3ca
Bernd Kuhls <bernd.kuhls@t-online.de>
2023-01-12 12:42:59 +0100

    package/perl-html-parser: bump version to 3.80
    Updated license hash due to typo/whitespace-only fixes in license file:
    https://github.com/libwww-perl/HTML-Parser/commit/96192e7cb1f31559e8ad834fd5d7642d04e35f65#diff-c693279643b8cd5d248172d9c22cb7cf4ed163a3c98c8a3f69c2717edd3eacb7
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/perl-html-parser/perl-html-parser.mk
+++ package/perl-html-parser/perl-html-parser.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PERL_HTML_PARSER_VERSION = 3.76
+PERL_HTML_PARSER_VERSION = 3.80
 PERL_HTML_PARSER_SOURCE = HTML-Parser-$(PERL_HTML_PARSER_VERSION).tar.gz
 PERL_HTML_PARSER_SITE = $(BR2_CPAN_MIRROR)/authors/id/O/OA/OALDERS
 PERL_HTML_PARSER_LICENSE = Artistic or GPL-1.0+
