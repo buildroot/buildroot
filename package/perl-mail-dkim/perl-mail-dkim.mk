57a64a702a1c162a33097db7ee7e069fb64b39df
Bernd Kuhls <bernd.kuhls@t-online.de>
2023-01-13 22:07:53 +0100

    package/perl-mail-dkim: bump version to 1.20220520
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/perl-mail-dkim/perl-mail-dkim.mk
+++ package/perl-mail-dkim/perl-mail-dkim.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PERL_MAIL_DKIM_VERSION = 1.20200824
+PERL_MAIL_DKIM_VERSION = 1.20220520
 PERL_MAIL_DKIM_SOURCE = Mail-DKIM-$(PERL_MAIL_DKIM_VERSION).tar.gz
 PERL_MAIL_DKIM_SITE = $(BR2_CPAN_MIRROR)/authors/id/M/MB/MBRADSHAW
 PERL_MAIL_DKIM_LICENSE = Artistic or GPL-1.0+
