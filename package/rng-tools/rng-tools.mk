a97ab5d2faf4c0498e1c7b4eb68f98fc2eb78060
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-01-14 17:10:02 +0100

    package/rng-tools: bump to version 6.16
    - Drop patches (already in version)
    - libcap is a mandatory dependency since
      https://github.com/nhorman/rng-tools/commit/50afdce9d3fed6244a0cf772d38f2d8475312bc5
    - Add Qrypt support:
      https://github.com/nhorman/rng-tools/commit/7ab7f540da567467055f77016590744d828fc9eb
    
    https://github.com/nhorman/rng-tools/releases/tag/v6.16
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/rng-tools/rng-tools.mk
+++ package/rng-tools/rng-tools.mk
@@ -4,14 +4,14 @@
 #
 ################################################################################
 
-RNG_TOOLS_VERSION = 6.15
+RNG_TOOLS_VERSION = 6.16
 RNG_TOOLS_SITE = $(call github,nhorman,$(RNG_TOOLS_NAME),v$(RNG_TOOLS_VERSION))
 RNG_TOOLS_LICENSE = GPL-2.0
 RNG_TOOLS_LICENSE_FILES = COPYING
 RNG_TOOLS_CPE_ID_VENDOR = rng-tools_project
 RNG_TOOLS_SELINUX_MODULES = rngd
 
-RNG_TOOLS_DEPENDENCIES = host-pkgconf openssl
+RNG_TOOLS_DEPENDENCIES = host-pkgconf libcap openssl
 # From git
 RNG_TOOLS_AUTORECONF = YES
 
@@ -43,6 +43,13 @@ RNG_TOOLS_DEPENDENCIES += jansson libcurl libxml2
 RNG_TOOLS_CONF_OPTS += --with-nistbeacon
 else
 RNG_TOOLS_CONF_OPTS += --without-nistbeacon
+endif
+
+ifeq ($(BR2_PACKAGE_RNG_TOOLS_QRYPT),y)
+RNG_TOOLS_DEPENDENCIES += jansson libcurl
+RNG_TOOLS_CONF_OPTS += --with-qrypt
+else
+RNG_TOOLS_CONF_OPTS += --without-qrypt
 endif
 
 define RNG_TOOLS_INSTALL_INIT_SYSV
