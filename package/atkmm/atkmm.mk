11bb66247bc364ab46b9bf10d769cecdb7b6fdc0
Lang Daniel <d.lang@abatec.at>
2023-01-19 09:49:31 +0000

    package/atkmm: switch to https download
    Signed-off-by: Daniel Lang <d.lang@abatec.at>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/atkmm/atkmm.mk
+++ package/atkmm/atkmm.mk
@@ -7,7 +7,7 @@
 ATKMM_VERSION_MAJOR = 2.36
 ATKMM_VERSION = $(ATKMM_VERSION_MAJOR).1
 ATKMM_SOURCE = atkmm-$(ATKMM_VERSION).tar.xz
-ATKMM_SITE = http://ftp.gnome.org/pub/gnome/sources/atkmm/$(ATKMM_VERSION_MAJOR)
+ATKMM_SITE = https://download.gnome.org/sources/atkmm/$(ATKMM_VERSION_MAJOR)
 ATKMM_LICENSE = LGPL-2.1+ (library), GPL-2.0+ (tools)
 ATKMM_LICENSE_FILES = COPYING COPYING.tools
 ATKMM_INSTALL_STAGING = YES
