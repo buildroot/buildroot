5bce041aec61ac9925d5059edf13c9499896daac
Francois Perrad <fperrad@gmail.com>
2023-01-23 19:58:55 +0100

    package/tcpdump: bump to version 4.99.3
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/tcpdump/tcpdump.mk
+++ package/tcpdump/tcpdump.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TCPDUMP_VERSION = 4.99.2
+TCPDUMP_VERSION = 4.99.3
 TCPDUMP_SITE = https://www.tcpdump.org/release
 TCPDUMP_LICENSE = BSD-3-Clause
 TCPDUMP_LICENSE_FILES = LICENSE
