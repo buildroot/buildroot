1e645ff7475710f4af9341602471dbeac387c776
Yann E. MORIN <yann.morin.1998@free.fr>
2023-01-29 22:59:07 +0100

    package/neon-2-sse: add missing coment
    That was missed when applying ee399a8689 (package/neon-2-sse: new
    package)...
    
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/neon-2-sse/neon-2-sse.mk
+++ package/neon-2-sse/neon-2-sse.mk
@@ -9,6 +9,7 @@ NEON_2_SSE_SITE = $(call github,intel,ARM_NEON_2_x86_SSE,$(NEON_2_SSE_VERSION))
 NEON_2_SSE_LICENSE = BSD-2-Clause
 NEON_2_SSE_LICENSE_FILES = LICENSE
 NEON_2_SSE_INSTALL_STAGING = YES
+# Only installs a header
 NEON_2_SSE_INSTALL_TARGET = NO
 
 $(eval $(cmake-package))
