98f8ddbc74962ab33df5ada2ccb53a27c49e60f1
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-01-29 17:39:53 +0100

    package/vim: security bump to version 9.0.1260
    - Fix CVE-2023-0049, CVE-2023-0051, CVE-2023-0054 and CVE-2023-0288
    - Update hash of README.txt: typo fixed by
      https://github.com/vim/vim/commit/f1dcd14fc5d4370476cd82895a4479ca2d252e54
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/vim/vim.mk
+++ package/vim/vim.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-VIM_VERSION = 9.0.0951
+VIM_VERSION = 9.0.1260
 VIM_SITE = $(call github,vim,vim,v$(VIM_VERSION))
 VIM_DEPENDENCIES = ncurses $(TARGET_NLS_DEPENDENCIES)
 VIM_SUBDIR = src
