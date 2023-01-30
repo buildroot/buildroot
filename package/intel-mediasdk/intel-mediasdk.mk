5997d0d63557488f2cbd06d4425fa3410adf336b
Bernd Kuhls <bernd.kuhls@t-online.de>
2023-01-30 18:43:39 +0100

    package/intel-mediasdk: bump version to 23.1.0
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/intel-mediasdk/intel-mediasdk.mk
+++ package/intel-mediasdk/intel-mediasdk.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-INTEL_MEDIASDK_VERSION = 22.6.5
+INTEL_MEDIASDK_VERSION = 23.1.0
 INTEL_MEDIASDK_SITE = https://github.com/Intel-Media-SDK/MediaSDK/archive
 INTEL_MEDIASDK_LICENSE = MIT
 INTEL_MEDIASDK_LICENSE_FILES = LICENSE
