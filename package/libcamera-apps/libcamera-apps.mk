80d98cd2a1c0eeb5c3083b3265c4f3221d1cb3c5
Marcus Folkesson <marcus.folkesson@gmail.com>
2023-01-31 14:23:27 +0100

    package/libcamera-apps: bump to version 1.1.1
    Signed-off-by: Marcus Folkesson <marcus.folkesson@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libcamera-apps/libcamera-apps.mk
+++ package/libcamera-apps/libcamera-apps.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBCAMERA_APPS_VERSION = 1.1.0
+LIBCAMERA_APPS_VERSION = 1.1.1
 LIBCAMERA_APPS_SITE = $(call github,raspberrypi,libcamera-apps,v$(LIBCAMERA_APPS_VERSION))
 LIBCAMERA_APPS_LICENSE = BSD-2-Clause
 LIBCAMERA_APPS_LICENSE_FILES = license.txt
