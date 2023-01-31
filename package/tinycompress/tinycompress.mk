5ee78c0a8b7854b4e8b260e1791909b8591ae5ed
James Hilliard <james.hilliard1@gmail.com>
2023-01-30 23:45:44 -0700

    package/tinycompress: install to staging
    This will be needed for upcoming pipewire compress offload support.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/tinycompress/tinycompress.mk
+++ package/tinycompress/tinycompress.mk
@@ -9,5 +9,6 @@ TINYCOMPRESS_SOURCE = tinycompress-$(TINYCOMPRESS_VERSION).tar.bz2
 TINYCOMPRESS_SITE = https://www.alsa-project.org/files/pub/tinycompress
 TINYCOMPRESS_LICENSE = BSD-3-Clause and LGPL-2.1
 TINYCOMPRESS_LICENSE_FILES = COPYING
+TINYCOMPRESS_INSTALL_STAGING = YES
 
 $(eval $(autotools-package))
