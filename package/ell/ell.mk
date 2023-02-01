607eafa2cd0656929db4bc8a743b28b0c48df8e5
James Hilliard <james.hilliard1@gmail.com>
2023-01-31 19:37:55 -0700

    package/ell: bump to version 0.56
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ell/ell.mk
+++ package/ell/ell.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ELL_VERSION = 0.53
+ELL_VERSION = 0.56
 ELL_SOURCE = ell-$(ELL_VERSION).tar.xz
 ELL_SITE = $(BR2_KERNEL_MIRROR)/linux/libs/ell
 ELL_LICENSE = LGPL-2.1+
