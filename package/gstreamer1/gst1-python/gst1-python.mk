b53e11f2ec049002a4c3c2829aaa0e7a31551f1e
James Hilliard <james.hilliard1@gmail.com>
2023-01-31 22:16:58 -0700

    package/gst1-python: bump version to 1.22.0
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gstreamer1/gst1-python/gst1-python.mk
+++ package/gstreamer1/gst1-python/gst1-python.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GST1_PYTHON_VERSION = 1.20.4
+GST1_PYTHON_VERSION = 1.22.0
 GST1_PYTHON_SOURCE = gst-python-$(GST1_PYTHON_VERSION).tar.xz
 GST1_PYTHON_SITE = https://gstreamer.freedesktop.org/src/gst-python
 GST1_PYTHON_INSTALL_STAGING = YES
