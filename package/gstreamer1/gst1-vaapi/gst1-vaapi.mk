33d4c226e582f6c0086431c4d9154e462cb3cf34
James Hilliard <james.hilliard1@gmail.com>
2023-01-31 22:16:54 -0700

    package/gst1-vaapi: bump version to 1.22.0
    Update config option names and dependency requirements.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gstreamer1/gst1-vaapi/gst1-vaapi.mk
+++ package/gstreamer1/gst1-vaapi/gst1-vaapi.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GST1_VAAPI_VERSION = 1.20.4
+GST1_VAAPI_VERSION = 1.22.0
 GST1_VAAPI_SITE = https://gstreamer.freedesktop.org/src/gstreamer-vaapi
 GST1_VAAPI_SOURCE = gstreamer-vaapi-$(GST1_VAAPI_VERSION).tar.xz
 GST1_VAAPI_LICENSE = LGPL-2.1+
@@ -18,19 +18,40 @@ GST1_VAAPI_DEPENDENCIES += \
 	libdrm
 
 GST1_VAAPI_CONF_OPTS += \
-	-Dwith_drm=yes \
-	-Dwith_x11=no \
-	-Dwith_glx=no \
-	-Dwith_wayland=no \
-	-Dwith_egl=no \
+	-Ddrm=enabled \
+	-Degl=enabled \
 	-Dexamples=disabled \
 	-Dtests=disabled \
 	-Ddoc=disabled
 
 ifeq ($(BR2_PACKAGE_GST1_VAAPI_ENCODERS),y)
-GST1_VAAPI_CONF_OPTS += -Dwith_encoders=yes
+GST1_VAAPI_CONF_OPTS += -Dencoders=enabled
 else
-GST1_VAAPI_CONF_OPTS += -Dwith_encoders=no
+GST1_VAAPI_CONF_OPTS += -Dencoders=disabled
+endif
+
+ifeq ($(BR2_PACKAGE_HAS_LIBEGL),y)
+GST1_VAAPI_CONF_OPTS += -Degl=enabled
+GST1_VAAPI_DEPENDENCIES += libegl
+else
+GST1_VAAPI_CONF_OPTS += -Degl=disabled
+endif
+
+ifeq ($(BR2_PACKAGE_WAYLAND),y)
+GST1_VAAPI_CONF_OPTS += -Dwayland=enabled
+else
+GST1_VAAPI_CONF_OPTS += -Dwayland=disabled
+endif
+
+ifeq ($(BR2_PACKAGE_XORG7),y)
+GST1_VAAPI_CONF_OPTS += -Dx11=enabled
+ifeq ($(BR2_PACKAGE_HAS_LIBGL),y)
+GST1_VAAPI_CONF_OPTS += -Dglx=enabled
+else
+GST1_VAAPI_CONF_OPTS += -Dglx=disabled
+endif
+else
+GST1_VAAPI_CONF_OPTS += -Dx11=disabled -Dglx=disabled
 endif
 
 $(eval $(meson-package))
