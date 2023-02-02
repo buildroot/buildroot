67ceef8affcb0300efcabab886e9819f56351fd6
Bernd Kuhls <bernd.kuhls@t-online.de>
2023-02-02 20:25:26 +0100

    package/xmrig: bump version to 6.19.0
    Changelog: https://github.com/xmrig/xmrig/blob/master/CHANGELOG.md
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/xmrig/xmrig.mk
+++ package/xmrig/xmrig.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XMRIG_VERSION = 6.18.1
+XMRIG_VERSION = 6.19.0
 XMRIG_SITE = $(call github,xmrig,xmrig,v$(XMRIG_VERSION))
 XMRIG_LICENSE = GPL-3.0+
 XMRIG_LICENSE_FILES = LICENSE
