3820c4b934ddc90c7f34c923254f0b2c9fad2c4f
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-02-05 15:06:02 +0100

    package/upx: security bump to version 4.0.2
    Fix CVE-2023-23456: A heap-based buffer overflow issue was discovered in
    UPX in PackTmt::pack() in p_tmt.cpp file. The flow allows an attacker to
    cause a denial of service (abort) via a crafted file.
    
    Fix CVE-2023-23457: A Segmentation fault was found in UPX in
    PackLinuxElf64::invert_pt_dynamic() in p_lx_elf.cpp. An attacker with a
    crafted input file allows invalid memory address access that could lead
    to a denial of service.
    
    https://github.com/upx/upx/blob/v4.0.2/NEWS
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 02befac8f9404ae30d0d090221d21a8460c82ec7)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/upx/upx.mk
+++ package/upx/upx.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UPX_VERSION = 4.0.1
+UPX_VERSION = 4.0.2
 UPX_SITE = https://github.com/upx/upx/releases/download/v$(UPX_VERSION)
 UPX_SOURCE = upx-$(UPX_VERSION)-src.tar.xz
 UPX_LICENSE = GPL-2.0+
