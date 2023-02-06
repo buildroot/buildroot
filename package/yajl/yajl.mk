dd67740aa9946be0c272f89fc5795ff597664cf6
Arnout Vandecappelle <arnout@mind.be>
2023-02-06 10:48:04 +0100

    package/yajl: explicitly request a tag in _SITE
    The yajl repository has both a branch and a tag names 2.1.0. Because of
    this, when we use the usual github URL [1], github doesn't return a
    tarball but rather an ASCII string:
       the given path has multiple possibilities: #<Git::Ref:0x00007f193bebe4d8>, #<Git::Ref:0x00007f193bebd858>
    
    This obviously (and fortunately) leads to a hash check failure.
    
    To make sure we get the tag and not the branch, we should use a URL that
    explicitly asks for refs/tags/2.1.0 [2]. We can't change VERSION to
    include refs/tags, because that also affects the _SOURCE default, the
    contents of the tarball, the CPE ID and the release-monitoring access.
    Therefore, add the refs/tags/ prefix to the github helper call.
    
    [1] https://github.com/lloyd/yajl/archive/2.1.0/yajl-2.1.0.tar.gz
    [1] https://github.com/lloyd/yajl/archive/refs/tags/2.1.0/yajl-2.1.0.tar.gz
    
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>
    (cherry picked from commit 5be76a39d3a0c21d6f3ceeab0889edc3cec9f2f3)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/yajl/yajl.mk
+++ package/yajl/yajl.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 YAJL_VERSION = 2.1.0
-YAJL_SITE = $(call github,lloyd,yajl,$(YAJL_VERSION))
+YAJL_SITE = $(call github,lloyd,yajl,refs/tags/$(YAJL_VERSION))
 YAJL_INSTALL_STAGING = YES
 YAJL_LICENSE = ISC
 YAJL_LICENSE_FILES = COPYING
