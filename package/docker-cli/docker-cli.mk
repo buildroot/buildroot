d5599cb1b41289adcff86ecf91ee6c069fefa9c1
Christian Stewart <christian@paral.in>
2023-02-06 20:30:10 -0800

    package/docker-cli: bump version to v23.0.0
    Like docker-engine, docker-cli uses a non-standard vendor/ directory
    structure, so we use the same fixes as we introduced for docker-engine
    (see commit: package/docker-engine: bump version to v23.0.0):
    
      - remove the problematic vendor/modules.txt
      - create a go.mod
    
    Signed-off-by: Christian Stewart <christian@paral.in>
    [yann.morin.1998@free.fr:
      - use post-extract hook
      - simplify commit message to refer to docker-engine
    ]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/docker-cli/docker-cli.mk
+++ package/docker-cli/docker-cli.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DOCKER_CLI_VERSION = 20.10.22
+DOCKER_CLI_VERSION = 23.0.0
 DOCKER_CLI_SITE = $(call github,docker,cli,v$(DOCKER_CLI_VERSION))
 
 DOCKER_CLI_LICENSE = Apache-2.0
@@ -28,6 +28,15 @@ DOCKER_CLI_LDFLAGS += -extldflags '-static'
 DOCKER_CLI_TAGS += osusergo netgo
 DOCKER_CLI_GO_ENV = CGO_ENABLED=no
 endif
+
+# create the go.mod file with language version go1.19
+# remove the conflicting vendor/modules.txt
+# https://github.com/moby/moby/issues/44618#issuecomment-1343565705
+define DOCKER_CLI_FIX_VENDORING
+	printf "module $(DOCKER_CLI_GOMOD)\n\ngo 1.19\n" > $(@D)/go.mod
+	rm -f $(@D)/vendor/modules.txt
+endef
+DOCKER_CLI_POST_EXTRACT_HOOKS += DOCKER_CLI_FIX_VENDORING
 
 DOCKER_CLI_INSTALL_BINS = $(notdir $(DOCKER_CLI_BUILD_TARGETS))
 
