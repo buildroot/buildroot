1613273542554776b089b3df2b6b71c1af2cc0da
Alexis Lothoré <alexis.lothore@bootlin.com>
2023-02-10 11:26:55 +0100

    package/python-flit-core: set license path
    When generating legal info with a configuration enabling a package which uses
    flit as setup type, we get a warning about python-flit-core license:
    WARNING: python-flit-core-3.8.0: cannot save license (HOST_PYTHON_FLIT_CORE_LICENSE_FILES not defined)
    
    Add missing variable to point to python-flit-core license file
    
    Signed-off-by: Alexis Lothoré <alexis.lothore@bootlin.com>
    [Peter: add sha256sum to .hash file]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-flit-core/python-flit-core.mk
+++ package/python-flit-core/python-flit-core.mk
@@ -8,6 +8,7 @@ PYTHON_FLIT_CORE_VERSION = 3.8.0
 PYTHON_FLIT_CORE_SOURCE = flit_core-$(PYTHON_FLIT_CORE_VERSION).tar.gz
 PYTHON_FLIT_CORE_SITE = https://files.pythonhosted.org/packages/10/e5/be08751d07b30889af130cec20955c987a74380a10058e6e8856e4010afc
 PYTHON_FLIT_CORE_LICENSE = BSD-3-Clause
+PYTHON_FLIT_CORE_LICENSE_FILES = LICENSE
 PYTHON_FLIT_CORE_SETUP_TYPE = flit-bootstrap
 
 # Use flit built in bootstrap_install for installing host-python-flit-core.
