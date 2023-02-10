25b3c5ecb6c5043676f3bed8a5923279989994c4
Joachim Wiberg <troglobit@gmail.com>
2023-02-10 19:47:04 +0100

    package/ssdp-responder; drop systemd unit installation, not needed
    When systemd is enabled the ssdp-responder build system already installs
    the unit file, with the correct name, so we can safely drop this.
    
    This fixes the recurring autobuilder problems that cropped up the last
    week+ or so after fixing the spelling error in 6e0ba6b.
    
     - http://autobuild.buildroot.net/results/f6912d6f79155c5d12811beeb6cb43860e37e7e7
     - http://autobuild.buildroot.net/results/7ee4ed26de7e45b52a9f6f36b839e578d2319247
     - http://autobuild.buildroot.net/results/33d9c10ea58a7cd3b3e2ed75d586007a92ccdaf0
     - http://autobuild.buildroot.net/results/f1238081fc94e3004aebb8e849fdd567a92a2d39
     - http://autobuild.buildroot.net/results/f7f2cf790f51e6224dca258c52bd31d21d621074
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit d6e3d36a85f19acedd2ed58f5b8616dbf24f57a4)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ssdp-responder/ssdp-responder.mk
+++ package/ssdp-responder/ssdp-responder.mk
@@ -25,9 +25,4 @@ define SSDP_RESPONDER_INSTALL_INIT_SYSV
 		$(TARGET_DIR)/etc/init.d/S50ssdpd
 endef
 
-define SSDP_RESPONDER_INSTALL_INIT_SYSTEMD
-	$(INSTALL) -D -m 644 $(@D)/ssdp-responder.service \
-		$(TARGET_DIR)/usr/lib/systemd/system/ssdp-responder.service
-endef
-
 $(eval $(autotools-package))
