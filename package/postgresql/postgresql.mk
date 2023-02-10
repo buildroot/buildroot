57e297a215193c92f6a178139db53616f2ee23b5
Maxim Kochetkov <fido_max@inbox.ru>
2023-02-10 10:22:28 +0300

    package/postgresql: bump version to 15.2
    Release notes: https://www.postgresql.org/docs/release/15.2/
    
    Signed-off-by: Maxim Kochetkov <fido_max@inbox.ru>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/postgresql/postgresql.mk
+++ package/postgresql/postgresql.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-POSTGRESQL_VERSION = 15.1
+POSTGRESQL_VERSION = 15.2
 POSTGRESQL_SOURCE = postgresql-$(POSTGRESQL_VERSION).tar.bz2
 POSTGRESQL_SITE = https://ftp.postgresql.org/pub/source/v$(POSTGRESQL_VERSION)
 POSTGRESQL_LICENSE = PostgreSQL
