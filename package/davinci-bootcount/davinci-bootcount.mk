f16b97eab1859395ddd74f557d33ef8a1f2447d6
Dario Binacchi <dario.binacchi@amarulasolutions.com>
2023-02-11 15:46:23 +0100

    package/davinci-bootcount: bump to version 2.0.0
    - stm32mp1: support for STM32 TAMP_BKP21R bootcount register
    - tab formatting
    - gitignore: added autoscan files
    - updated documentation
    - i2c_eeprom: bootcount does not use two uint16s
    - am33xx: declare registers as 'volatile'
    - src/am33xx: do not close fd, it seems to prevent reliably writing register
    - configure.ac: update version, homepage
    - Added EEPROM read/write for non-TI AM335x platforms
    
    Signed-off-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/davinci-bootcount/davinci-bootcount.mk
+++ package/davinci-bootcount/davinci-bootcount.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-DAVINCI_BOOTCOUNT_VERSION = 0973689c7556a953d2b468e4d8d46758c6d467b4
-DAVINCI_BOOTCOUNT_SITE = $(call github,VoltServer,uboot-davinci-bootcount,$(DAVINCI_BOOTCOUNT_VERSION))
+DAVINCI_BOOTCOUNT_VERSION = 2.0.0
+DAVINCI_BOOTCOUNT_SITE = $(call github,VoltServer,uboot-davinci-bootcount,v$(DAVINCI_BOOTCOUNT_VERSION))
 DAVINCI_BOOTCOUNT_LICENSE = GPL-3.0
 DAVINCI_BOOTCOUNT_LICENSE_FILES = COPYING
 
