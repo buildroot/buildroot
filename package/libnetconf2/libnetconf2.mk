d22988bfbdca479b18cce6a60b7d51b59e125a6d
Heiko Thiery <heiko.thiery@gmail.com>
2023-02-13 19:19:45 +0100

    package/libnetconf2: bump version to 2.1.28
    Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libnetconf2/libnetconf2.mk
+++ package/libnetconf2/libnetconf2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBNETCONF2_VERSION = 2.1.25
+LIBNETCONF2_VERSION = 2.1.28
 LIBNETCONF2_SITE = $(call github,CESNET,libnetconf2,v$(LIBNETCONF2_VERSION))
 LIBNETCONF2_INSTALL_STAGING = YES
 LIBNETCONF2_LICENSE = BSD-3-Clause
