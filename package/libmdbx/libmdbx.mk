c22b06335127b275e7e8cfd3bac473b2931ea7f8
Леонид Юрьев (Leonid Yuriev) <leo@yuriev.ru>
2023-02-14 18:51:29 +0300

    package/libmdbx: bump version to 0.11.14 "Sergey Kapitsa"
    This is stable bugfix release of libmdbx,
    in memory of Sergey Kapitsa (Russian physicist and demographer) on his 95th birthday.
    
    It is reasonable to backport this patch to all applicable releases/branches of Buildroot,
    at least this release fixes build for sh4 arch.
    
    Release notes for v0.11.14
    --------------------------
    
    Fixes:
    
     - backport: Refined the `__cold`/`__hot` macros to avoid the
       `error: inlining failed in call to ‘always_inline FOO(...)’: target specific option mismatch`
       issue during build using GCC >10.x for SH4 arch.
       Actually this is GCC' SH4-backend bug which triggered by the `__attribute__((__optimize__("Os")))`
       used in conjunction with the `__attribute__((__cold__))`.
    
     - backport: Fixed `SIGSEGV` or an erroneous call to `free()` in case where
       errors occur when reopening by `mdbx_env_open()` of a previously used
       environment.
    
     - backport: Fixed `cursor_put_nochecklen()` internals for case when dupsort'ed named subDb
       contains a single key with multiple values (aka duplicates), which are replaced
       with a single value by put-operation with the `MDBX_UPSERT+MDBX_ALLDUPS` flags.
       In this case, the database becomes completely empty, without any pages.
       However exactly this condition was not considered and thus wasn't handled correctly.
       See [issue#8](https://gitflic.ru/project/erthink/libmdbx/issue/8) for more information.
    
     - backport: Fixed extra assertion inside `override_meta()`, which could
       lead to false-positive failing of the assertion in a debug builds during
       DB recovery and/or auto-rollback.
    
    Minors:
    
     - backport: Fixed typos.
     - backport: Refined `const` and `noexcept` for few C++ API methods.
     - backport: Resolve false-posirive `used uninitialized` warning from GCC >10.x
       while build for SH4 arch.
     - backport: Fixed insignificant typo of `||` inside `#if` byte-order condition.
    
    The complete ChangeLog: https://gitflic.ru/project/erthink/libmdbx/blob?file=ChangeLog.md
    
    Signed-off-by: Леонид Юрьев (Leonid Yuriev) <leo@yuriev.ru>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit efdcc850be902de2596412dbf0f142c3f679ce35)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libmdbx/libmdbx.mk
+++ package/libmdbx/libmdbx.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBMDBX_VERSION = 0.11.13
+LIBMDBX_VERSION = 0.11.14
 LIBMDBX_SOURCE = libmdbx-amalgamated-$(LIBMDBX_VERSION).tar.xz
 LIBMDBX_SITE = https://libmdbx.dqdkfa.ru/release
 LIBMDBX_SUPPORTS_IN_SOURCE_BUILD = NO
