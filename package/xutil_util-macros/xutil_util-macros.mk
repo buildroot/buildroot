71c51f1920deda32ea63105163fe84b20a0f9109
Bernd Kuhls <bernd.kuhls@t-online.de>
2023-02-16 19:36:55 +0100

    package/xutil_util-macros: bump version to 1.20.0
    Release notes:
    https://lists.x.org/archives/xorg-announce/2023-February/003324.html
    
    Updated license hash due to copyright year bump:
    https://gitlab.freedesktop.org/xorg/util/macros/-/commit/cb147377e9341af05232f95814022abdecf14024
    
    Switched tarball to xz, updated _SITE.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/xutil_util-macros/xutil_util-macros.mk
+++ package/xutil_util-macros/xutil_util-macros.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XUTIL_UTIL_MACROS_VERSION = 1.19.3
-XUTIL_UTIL_MACROS_SOURCE = util-macros-$(XUTIL_UTIL_MACROS_VERSION).tar.bz2
-XUTIL_UTIL_MACROS_SITE = http://xorg.freedesktop.org/releases/individual/util
+XUTIL_UTIL_MACROS_VERSION = 1.20.0
+XUTIL_UTIL_MACROS_SOURCE = util-macros-$(XUTIL_UTIL_MACROS_VERSION).tar.xz
+XUTIL_UTIL_MACROS_SITE = https://xorg.freedesktop.org/archive/individual/util
 XUTIL_UTIL_MACROS_LICENSE = MIT
 XUTIL_UTIL_MACROS_LICENSE_FILES = COPYING
 
