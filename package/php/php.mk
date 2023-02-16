66b1926764582a628b0b6b68a3aebe4b9790ab12
Bernd Kuhls <bernd.kuhls@t-online.de>
2023-02-16 19:24:10 +0100

    package/php: security bump version to 8.2.3
    Changelog: https://www.php.net/ChangeLog-8.php#8.2.3
    
    Fixes CVE-2023-0567, CVE-2023-0568 & CVE-2023-0662.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/php/php.mk
+++ package/php/php.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PHP_VERSION = 8.2.2
+PHP_VERSION = 8.2.3
 PHP_SITE = https://www.php.net/distributions
 PHP_SOURCE = php-$(PHP_VERSION).tar.xz
 PHP_INSTALL_STAGING = YES
