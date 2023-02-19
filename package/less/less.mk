d72f336b2190d480f9026f3757e5dff3dcd9f5d4
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-02-19 18:52:04 +0100

    package/less: fix CVE-2022-46663
    In GNU Less before 609, crafted data can result in "less -R" not
    filtering ANSI escape sequences sent to the terminal.
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 924ca9414fa962ce767c39b277489a0bdcbfad79)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/less/less.mk
+++ package/less/less.mk
@@ -11,6 +11,9 @@ LESS_LICENSE_FILES = COPYING
 LESS_CPE_ID_VENDOR = gnu
 LESS_DEPENDENCIES = ncurses
 
+# 0001-End-OSC8-hyperlink-on-invalid-embedded-escape-sequence.patch
+LESS_IGNORE_CVES += CVE-2022-46663
+
 define LESS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/less $(TARGET_DIR)/usr/bin/less
 endef
