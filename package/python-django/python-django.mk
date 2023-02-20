a85ed5d21c14cbe3ec2864206fe792152598e4db
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-02-20 14:15:25 +0100

    package/python-django: security bump to version 4.1.7
    Fix CVE-2023-23969 and CVE-2023-24580
    
    https://docs.djangoproject.com/en/dev/releases/4.1.6
    https://docs.djangoproject.com/en/dev/releases/4.1.7
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 4.1.5
+PYTHON_DJANGO_VERSION = 4.1.7
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/41/c8/b3c469353f9d1b7f0e99b45520582b891da02cd87408bc867affa6e039a3
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/9f/a7/07939866241b7e8f8d3bf164b7d6ad428163723e29dd472700f8ab0e5fd5
 
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
