e9e1cd239f179e15f7f4214e274282d4e2bb5b7d
Vincent Fazio <vfazio@gmail.com>
2023-02-21 11:22:04 -0600

    package/gettext: drop GETTEXTIZE variable
    This variable is no longer used anywhere in the tree so remove it.
    
    Signed-off-by: Vincent Fazio <vfazio@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/gettext/gettext.mk
+++ package/gettext/gettext.mk
@@ -4,7 +4,5 @@
 #
 ################################################################################
 
-GETTEXTIZE = $(HOST_CONFIGURE_OPTS) AUTOM4TE=$(HOST_DIR)/bin/autom4te $(HOST_DIR)/bin/gettextize -f
-
 $(eval $(virtual-package))
 $(eval $(host-virtual-package))
