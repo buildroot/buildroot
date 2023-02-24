9b078380384f8ecd74857260d911e267c7a788b3
Lang Daniel <d.lang@abatec.at>
2023-02-24 08:42:34 +0000

    package/libsigc2: new package
    This is the version of libsigc needed for gtkmm3.
    
    Signed-off-by: Daniel Lang <d.lang@abatec.at>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

---
+++ package/libsigc2/libsigc2.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# libsigc2
+#
+################################################################################
+
+LIBSIGC2_VERSION_MAJOR = 2.12
+LIBSIGC2_VERSION = $(LIBSIGC2_VERSION_MAJOR).0
+LIBSIGC2_SOURCE = libsigc++-$(LIBSIGC2_VERSION).tar.xz
+LIBSIGC2_SITE = https://download.gnome.org/sources/libsigc++/$(LIBSIGC2_VERSION_MAJOR)
+LIBSIGC2_INSTALL_STAGING = YES
+LIBSIGC2_LICENSE = LGPL-2.1+
+LIBSIGC2_LICENSE_FILES = COPYING
+LIBSIGC2_CONF_OPTS = \
+	-Dbuild-examples=false \
+	-Dbuild-tests=false \
+	-Dvalidation=false
+
+$(eval $(meson-package))
