f91661108d85d4bb663fe555be82a246a7e6b027
Lang Daniel <d.lang@abatec.at>
2023-02-24 08:43:02 +0000

    package/pangomm2_46: new package
    This is the version needed by gtkmm3.
    
    Signed-off-by: Daniel Lang <d.lang@abatec.at>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

---
+++ package/pangomm2_46/pangomm2_46.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# pangomm2_46
+#
+################################################################################
+
+PANGOMM2_46_VERSION_MAJOR = 2.46
+PANGOMM2_46_VERSION = $(PANGOMM2_46_VERSION_MAJOR).3
+PANGOMM2_46_SOURCE = pangomm-$(PANGOMM2_46_VERSION).tar.xz
+PANGOMM2_46_SITE = https://download.gnome.org/sources/pangomm/$(PANGOMM2_46_VERSION_MAJOR)
+PANGOMM2_46_LICENSE = LGPL-2.1+ (library), GPL-2.0+ (tools)
+PANGOMM2_46_LICENSE_FILES = COPYING COPYING.tools
+PANGOMM2_46_INSTALL_STAGING = YES
+PANGOMM2_46_DEPENDENCIES = glibmm2_66 cairomm1_14 libsigc2 pango host-pkgconf
+
+$(eval $(meson-package))
