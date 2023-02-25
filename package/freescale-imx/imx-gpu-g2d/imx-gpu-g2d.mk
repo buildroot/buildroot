buildroot/buildroot

commit b84557b588b3519b8cbe4856669d33fca1c56f7c

package/freescale-imx/imx-gpu-g2d: bump version to 6.4.3.p4.4
- To match NXP 5.15.52-2.1.0 release.
- EULA/COPYING: update to LA_OPT_NXP_Software_License v39.

Signed-off-by: Chris Dimich <chris.dimich@boundarydevices.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/freescale-imx/imx-gpu-g2d/imx-gpu-g2d.mk
@@ -5,9 +5,9 @@
 ################################################################################
 
 ifeq ($(BR2_aarch64),y)
-IMX_GPU_G2D_VERSION = 6.4.3.p2.0-aarch64
+IMX_GPU_G2D_VERSION = 6.4.3.p4.4-aarch64
 else
-IMX_GPU_G2D_VERSION = 6.4.3.p2.0-arm
+IMX_GPU_G2D_VERSION = 6.4.3.p4.4-arm
 endif
 IMX_GPU_G2D_SITE = $(FREESCALE_IMX_SITE)
 IMX_GPU_G2D_SOURCE = imx-gpu-g2d-$(IMX_GPU_G2D_VERSION).bin

package/freescale-imx/imx-gpu-g2d/imx-gpu-g2d.hash
@@ -1,5 +1,5 @@
 # Locally calculated
-sha256  39fe307916e19a0892a514621121086e276a43858af90230372a1344852116a2  imx-gpu-g2d-6.4.3.p2.0-arm.bin
-sha256  2642a49e088daae4cf568a150d01e7c095995e8a6ff1883c02c3fa6d26d254b3  imx-gpu-g2d-6.4.3.p2.0-aarch64.bin
-sha256  ea58636bf34f589048fcd46e38497c47229fde8498df086ff5fb52f9ef8e15ee  COPYING
-sha256  50099e283cfd307f84f159aeb1a6f462351320273dbcdb80d7f2c9db0494b71c  EULA
+sha256  7737a3372bcde34e2d275397df67c307df5033d5b133ac770f75cac4e1ec2580  imx-gpu-g2d-6.4.3.p4.4-arm.bin
+sha256  8341d6e2cde3f3ffa035dc95b0487dd04a50531cc80f4cddc5a689ebc4946e02  imx-gpu-g2d-6.4.3.p4.4-aarch64.bin
+sha256  5bd7aab9dc380a6f9577443b2e90bd5ca73d9528175fad04b6d17da43a05a48e  COPYING
+sha256  654f0c4ce6d08f9a6d74e29c58206742cb7469a6a7c25413028a9bdbe647aa13  EULA

commit 333580b68037ef756fa637bbfc49f34f1ad5e0ea

package/freescale-imx/imx-gpu-g2d: drop BR2_PACKAGE_IMX_GPU_G2D_EXAMPLES
Arm and aarch64 packages don't contain examples anymore so drop the
config option to install them to prevent build failure when selected:

>>> imx-gpu-g2d 6.4.3.p4.4-arm Installing to target
mkdir -p /home/sszy/br-test-pkg/bootlin-armv7-glibc/target/usr/share/examples/
cp -a /home/sszy/br-test-pkg/bootlin-armv7-glibc/build/imx-gpu-g2d-6.4.3.p4.4-arm/gpu-demos/opt/* /home/sszy/br-test-pkg/bootlin-armv7-glibc/target/usr/share/examples/
cp: cannot stat '/home/sszy/br-test-pkg/bootlin-armv7-glibc/build/imx-gpu-g2d-6.4.3.p4.4-arm/gpu-demos/opt/*': No such file or directory
make: *** [package/pkg-generic.mk:384: /home/sszy/br-test-pkg/bootlin-armv7-glibc/build/imx-gpu-g2d-6.4.3.p4.4-arm/.stamp_target_installed] Error 1

Fixes: b84557b588b3 ("package/freescale-imx/imx-gpu-g2d: bump version to 6.4.3.p4.4")
Signed-off-by: Sébastien Szymanski <sebastien.szymanski@armadeus.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

Config.in.legacy
@@ -146,6 +146,12 @@ endif
 
 comment "Legacy options removed in 2023.02"
 
+config BR2_PACKAGE_IMX_GPU_G2D_EXAMPLES
+	bool "imx-gpu-g2d examples removed"
+	select BR2_LEGACY
+	help
+	  The examples are not provided by NXP anymore.
+
 config BR2_KERNEL_HEADERS_6_0
 	bool "kernel headers version 6.0.x are no longer supported"
 	select BR2_LEGACY

package/freescale-imx/imx-gpu-g2d/Config.in
@@ -18,12 +18,3 @@ config BR2_PACKAGE_IMX_GPU_G2D
 
 	  This library is provided by NXP as-is and doesn't have an
 	  upstream.
-
-if BR2_PACKAGE_IMX_GPU_G2D
-
-config BR2_PACKAGE_IMX_GPU_G2D_EXAMPLES
-	bool "install examples"
-	help
-	  Copy the Vivante G2D examples to the target.
-
-endif

package/freescale-imx/imx-gpu-g2d/imx-gpu-g2d.mk
@@ -26,15 +26,7 @@ define IMX_GPU_G2D_INSTALL_STAGING_CMDS
 	cp -a $(@D)/g2d/usr/* $(STAGING_DIR)/usr
 endef
 
-ifeq ($(BR2_PACKAGE_IMX_GPU_G2D_EXAMPLES),y)
-define IMX_GPU_G2D_INSTALL_EXAMPLES
-	mkdir -p $(TARGET_DIR)/usr/share/examples/
-	cp -a $(@D)/gpu-demos/opt/* $(TARGET_DIR)/usr/share/examples/
-endef
-endif
-
 define IMX_GPU_G2D_INSTALL_TARGET_CMDS
-	$(IMX_GPU_G2D_INSTALL_EXAMPLES)
 	cp -a $(@D)/g2d/usr/lib $(TARGET_DIR)/usr
 endef
 