40bfe7d7152c89ece8a2b90110d0b4f01958ae1e
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-02-26 14:20:26 +0100

    package/bind: security bump to version 9.16.38
    - Fix CVE-2022-3094, CVE-2022-3736 and CVE-2022-3924
    - Update hash of COPYRIGHT (year updated with
      https://gitlab.isc.org/isc-projects/bind9/-/commit/1a5d707f52282117c9912a27dcc89f2ec412577a)
    
    https://downloads.isc.org/isc/bind9/9.16.38/doc/arm/html/notes.html
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 7abc9a0554cc1c1bbc1ad4273622024020fa2c7e)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/bind/bind.mk
+++ package/bind/bind.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BIND_VERSION = 9.16.33
+BIND_VERSION = 9.16.38
 BIND_SOURCE= bind-$(BIND_VERSION).tar.xz
 BIND_SITE = https://ftp.isc.org/isc/bind9/$(BIND_VERSION)
 # bind does not support parallel builds.
