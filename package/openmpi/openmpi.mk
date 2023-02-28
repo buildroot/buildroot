bcf9d2f51cd5c80191b63020412631a548e32a7d
Julien Olivain <ju.o@free.fr>
2023-02-28 21:27:20 +0100

    package/openmpi: bump to version 4.1.5
    For change log, see:
    https://github.com/open-mpi/ompi/blob/v4.1.5/NEWS#L61
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/openmpi/openmpi.mk
+++ package/openmpi/openmpi.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 OPENMPI_VERSION_MAJOR = 4.1
-OPENMPI_VERSION = $(OPENMPI_VERSION_MAJOR).4
+OPENMPI_VERSION = $(OPENMPI_VERSION_MAJOR).5
 OPENMPI_SITE = https://www.open-mpi.org/software/ompi/v$(OPENMPI_VERSION_MAJOR)/downloads
 OPENMPI_SOURCE = openmpi-$(OPENMPI_VERSION).tar.bz2
 OPENMPI_LICENSE = BSD-3-Clause
@@ -15,7 +15,7 @@ OPENMPI_INSTALL_STAGING = YES
 # The macro searching for IME (Infinite Memory Engine) filesystem
 # brings "-I/usr/local/include" in the CPPFLAGS, even if not
 # found. This makes the configuration fail. See:
-# https://github.com/open-mpi/ompi/blob/v4.1.4/config/ompi_check_ime.m4#L35
+# https://github.com/open-mpi/ompi/blob/v4.1.5/config/ompi_check_ime.m4#L35
 # Disable explicitly to avoid the issue.
 OPENMPI_CONF_OPTS = --without-ime
 
