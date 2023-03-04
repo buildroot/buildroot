f3ae7903cabde73379c401687ee19ff18e481490
Peter Korsgaard <peter@korsgaard.com>
2023-03-04 15:20:26 +0100

    package/php: security bump to version 8.0.28
    Changelog: https://www.php.net/ChangeLog-8.php#8.0.28
    
    Fixes CVE-2023-0567, CVE-2023-0568 & CVE-2023-0662.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/php/php.mk
+++ package/php/php.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PHP_VERSION = 8.0.27
+PHP_VERSION = 8.0.28
 PHP_SITE = https://www.php.net/distributions
 PHP_SOURCE = php-$(PHP_VERSION).tar.xz
 PHP_INSTALL_STAGING = YES
