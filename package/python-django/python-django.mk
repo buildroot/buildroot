fd0e9ac8e83ec0ce552f9fa3409b56cd8571635b
Peter Korsgaard <peter@korsgaard.com>
2023-03-04 20:49:01 +0100

    package/python-django: security bump to version 4.0.10
    Fix CVE-2023-23969 and CVE-2023-24580
    
    https://docs.djangoproject.com/en/dev/releases/4.0.9
    https://docs.djangoproject.com/en/dev/releases/4.0.10
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 4.0.8
+PYTHON_DJANGO_VERSION = 4.0.10
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/1a/de/08d8a349ed0e3e1999eb86ae0347cc9eaf634cd65f1eb80b9387ac1dbe3c
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/44/92/b0ceee230f9252460abf8c0a3bcc003a914af85ad63006596c3fb3669fbf
 
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
