cde69c668d6692114845a927c8c5534ed78019db
Christian Stewart <christian@paral.in>
2023-03-05 23:25:58 -0800

    package/rtl8821cu: switch to version 20210916
    The author of the 8821cu repository has requested that we switch to a newer
    major version of the driver located at 8821cu-20210916:
    
    https://github.com/morrownr/8821cu-20210118/pull/93#issuecomment-1452675897
    
    Switch to that new repository.
    
    Fixes a build issue against PowerPC64.
    
    Signed-off-by: Christian Stewart <christian@paral.in>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rtl8821cu/rtl8821cu.mk
+++ package/rtl8821cu/rtl8821cu.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-RTL8821CU_VERSION = 86cc5ceb7c28b9b997838e1c796847f6c395c382
-RTL8821CU_SITE = $(call github,morrownr,8821cu-20210118,$(RTL8821CU_VERSION))
+RTL8821CU_VERSION = 69c903b75bde36293c66b25c051916a74dbadf58
+RTL8821CU_SITE = $(call github,morrownr,8821cu-20210916,$(RTL8821CU_VERSION))
 RTL8821CU_LICENSE = GPL-2.0
 RTL8821CU_LICENSE_FILES = LICENSE
 
