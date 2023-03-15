b36e4b10f36866537aebe54f7538324c0efc5c17
Julien Olivain <ju.o@free.fr>
2023-03-15 07:14:15 +0100

    package/octave: bump to version 8.1.0
    For change log, see:
    https://octave.org/NEWS-8.html
    
    Notable changes in this commit:
    - the package patch was rebased and updated for this new version
    - a comment in .hash file about signature verification is added
    - the pcre dependency is replaced by pcre2, following the upstream
      package recommendation and preference. See:
      https://octave.org/NEWS-8.html#general-improvements
      This change also adds the host-pkgconf as a dependency.
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/octave/octave.mk
+++ package/octave/octave.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OCTAVE_VERSION = 7.3.0
+OCTAVE_VERSION = 8.1.0
 OCTAVE_SITE = https://ftp.gnu.org/gnu/octave
 OCTAVE_SOURCE = octave-$(OCTAVE_VERSION).tar.lz
 OCTAVE_LICENSE = GPL-3.0+
@@ -15,8 +15,9 @@ OCTAVE_CONF_OPTS = --disable-java
 
 OCTAVE_DEPENDENCIES = \
 	host-gperf \
+	host-pkgconf \
 	openblas \
-	pcre
+	pcre2
 
 ifeq ($(BR2_PACKAGE_READLINE),y)
 OCTAVE_CONF_OPTS += --enable-readline
