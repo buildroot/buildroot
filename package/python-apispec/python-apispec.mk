c5b453f47892fbb3f7c505e98cb24939161d37eb
Martin Hundebøll <martin@geanix.com>
2023-03-16 13:36:52 +0100

    package/python-apispec: bump to 6.3.0
    Signed-off-by: Martin Hundebøll <martin@geanix.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-apispec/python-apispec.mk
+++ package/python-apispec/python-apispec.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_APISPEC_VERSION = 6.0.2
+PYTHON_APISPEC_VERSION = 6.3.0
 PYTHON_APISPEC_SOURCE = apispec-$(PYTHON_APISPEC_VERSION).tar.gz
-PYTHON_APISPEC_SITE = https://files.pythonhosted.org/packages/a7/2a/b42c17e0d653341b8f5916999892cd2d8489de127dac6118ae44531674f5
+PYTHON_APISPEC_SITE = https://files.pythonhosted.org/packages/3f/1e/207c3e61c805eef214d7e5c58106312f9af4733b07f470224e4e309dc65e
 PYTHON_APISPEC_SETUP_TYPE = setuptools
 PYTHON_APISPEC_LICENSE = MIT
 PYTHON_APISPEC_LICENSE_FILES = LICENSE
