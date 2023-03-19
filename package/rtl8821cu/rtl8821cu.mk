e5f209effc06c656e5eab05043af2c8bb851a5ea
Giulio Benetti <giulio.benetti@benettiengineering.com>
2023-03-19 20:44:03 +0100

    package/rtl8821cu: bump to version 2023-03-19 on main branch
    With this bump Linux 6.3 is supported.
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Reviewed-by: Christian Stewart <christian@paral.in>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/rtl8821cu/rtl8821cu.mk
+++ package/rtl8821cu/rtl8821cu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTL8821CU_VERSION = 69c903b75bde36293c66b25c051916a74dbadf58
+RTL8821CU_VERSION = 80781de376007ecd778dfb2bef05533e3a6cc7c6
 RTL8821CU_SITE = $(call github,morrownr,8821cu-20210916,$(RTL8821CU_VERSION))
 RTL8821CU_LICENSE = GPL-2.0
 RTL8821CU_LICENSE_FILES = LICENSE
