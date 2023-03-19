5b1e431500d6e7d17cd23d70d55df2ba99813852
Giulio Benetti <giulio.benetti@benettiengineering.com>
2023-03-19 13:16:59 +0100

    package/rtl8189fs: bump to version 2023-03-13 on branch rtl8189fs
    rtl8189fs branch is specific for rtl8189fs controller and diverged from
    master so let's switch branch from master to rtl8189s to fix runtime
    failure.
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Tested-by: Indrek Kruusa <indrek.kruusa@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/rtl8189fs/rtl8189fs.mk
+++ package/rtl8189fs/rtl8189fs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTL8189FS_VERSION = e58bd86c9d9408c648b1246a0dd76b16856ec172
+RTL8189FS_VERSION = ed6741b52bc998875298117b55d18b0d557c37a6
 RTL8189FS_SITE = $(call github,jwrdegoede,rtl8189ES_linux,$(RTL8189FS_VERSION))
 RTL8189FS_LICENSE = GPL-2.0
 
