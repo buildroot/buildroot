6ca823f6eb478fe5b9db06ff9bec16cb20e4d6e6
James Hilliard <james.hilliard1@gmail.com>
2023-03-21 18:07:14 -0600

    package/python-aexpect: bump to version 1.7.0
    Drop no longer required six runtime dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-aexpect/python-aexpect.mk
+++ package/python-aexpect/python-aexpect.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_AEXPECT_VERSION = 1.6.4
+PYTHON_AEXPECT_VERSION = 1.7.0
 PYTHON_AEXPECT_SOURCE = aexpect-$(PYTHON_AEXPECT_VERSION).tar.gz
-PYTHON_AEXPECT_SITE = https://files.pythonhosted.org/packages/3b/22/f87ffa70348dde4597d9314995be89c8d4c7728260033b972a8d691e3f7d
+PYTHON_AEXPECT_SITE = https://files.pythonhosted.org/packages/dd/32/738b5190adb5ed387d3e755885f646b714fbf9c22adbda7ff988db7ede49
 PYTHON_AEXPECT_SETUP_TYPE = setuptools
 PYTHON_AEXPECT_LICENSE = GPL-2.0+
 PYTHON_AEXPECT_LICENSE_FILES = LICENSE
