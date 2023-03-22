42d45a4267d5ff92d85b9b5faeba21e4c46eb357
James Hilliard <james.hilliard1@gmail.com>
2023-03-21 18:25:06 -0600

    package/python-alembic: bump to version 1.10.2
    License hash changed due to date update:
    https://github.com/sqlalchemy/alembic/commit/e3055cd5baad328641366026af66639f333c520c
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-alembic/python-alembic.mk
+++ package/python-alembic/python-alembic.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ALEMBIC_VERSION = 1.9.1
+PYTHON_ALEMBIC_VERSION = 1.10.2
 PYTHON_ALEMBIC_SOURCE = alembic-$(PYTHON_ALEMBIC_VERSION).tar.gz
-PYTHON_ALEMBIC_SITE = https://files.pythonhosted.org/packages/81/87/5c531d2923c61e7ae68b23c8660df289ae4241a0d38f3fbcbd8dba64ca6b
+PYTHON_ALEMBIC_SITE = https://files.pythonhosted.org/packages/d6/02/66ed327f944f3f85423096c438d7d290da5af21a7aecf9e5b9f09d9e34b2
 PYTHON_ALEMBIC_SETUP_TYPE = setuptools
 PYTHON_ALEMBIC_LICENSE = MIT
 PYTHON_ALEMBIC_LICENSE_FILES = LICENSE
