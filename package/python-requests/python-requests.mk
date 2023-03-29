f9c196598d58cecef769194df3c54466cf62459d
Aaron Sierra <aaron@bubbl-tek.com>
2023-03-29 10:51:54 -0500

    package/python-requests: bump to version 2.28.2
    Get explicit compatibility with charset-normalizer 3.x
    
    Signed-off-by: Aaron Sierra <aaron@bubbl-tek.com>
    Acked-by: Yann E. MORIN <yann.morin@orange.com>
    Tested-by: Yann E. MORIN <yann.morin@orange.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-requests/python-requests.mk
+++ package/python-requests/python-requests.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_REQUESTS_VERSION = 2.28.1
+PYTHON_REQUESTS_VERSION = 2.28.2
 PYTHON_REQUESTS_SOURCE = requests-$(PYTHON_REQUESTS_VERSION).tar.gz
-PYTHON_REQUESTS_SITE = https://files.pythonhosted.org/packages/a5/61/a867851fd5ab77277495a8709ddda0861b28163c4613b011bc00228cc724
+PYTHON_REQUESTS_SITE = https://files.pythonhosted.org/packages/9d/ee/391076f5937f0a8cdf5e53b701ffc91753e87b07d66bae4a09aa671897bf
 PYTHON_REQUESTS_SETUP_TYPE = setuptools
 PYTHON_REQUESTS_LICENSE = Apache-2.0
 PYTHON_REQUESTS_LICENSE_FILES = LICENSE
