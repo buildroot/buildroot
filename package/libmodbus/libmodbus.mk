853c1d053c96525ab401a51e140a479537bba3af
Daniel Lang <dalang@gmx.at>
2023-04-05 22:04:44 +0200

    package/libmodbus: bump to version 3.1.10
    https://github.com/stephane/libmodbus/releases/tag/v3.1.10
    
    Also update URL in Config.in to use https instead of http.
    
    Also remove --without-documentation configure option, which already
    didn't exist in 3.1.8.
    
    Signed-off-by: Daniel Lang <dalang@gmx.at>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/libmodbus/libmodbus.mk
+++ package/libmodbus/libmodbus.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-LIBMODBUS_VERSION = 3.1.8
+LIBMODBUS_VERSION = 3.1.10
 LIBMODBUS_SITE = https://github.com/stephane/libmodbus/releases/download/v$(LIBMODBUS_VERSION)
 LIBMODBUS_LICENSE = LGPL-2.1+
 LIBMODBUS_LICENSE_FILES = COPYING.LESSER
 LIBMODBUS_CPE_ID_VENDOR = libmodbus
 LIBMODBUS_INSTALL_STAGING = YES
-LIBMODBUS_CONF_OPTS = --without-documentation --disable-tests
+LIBMODBUS_CONF_OPTS = --disable-tests
 
 $(eval $(autotools-package))
