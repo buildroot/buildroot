1fc279aa6bb06be1e73583068a58486cc008d269
Eric Higgins <erichiggins@gmail.com>
2023-04-13 15:15:53 -0700

    Pull open trickler package from github release. Bump to version 2.1.3

--- package/python-opentrickler/python-opentrickler.mk
+++ package/python-opentrickler/python-opentrickler.mk
@@ -4,10 +4,9 @@
 #
 ################################################################################
 
-PYTHON_OPENTRICKLER_VERSION = 2.1.2
+PYTHON_OPENTRICKLER_VERSION = 2.1.3
 PYTHON_OPENTRICKLER_SOURCE = opentrickler-$(PYTHON_OPENTRICKLER_VERSION).tar.xz
-PYTHON_OPENTRICKLER_SITE = $(TOPDIR)/package/python-opentrickler
-PYTHON_OPENTRICKLER_SITE_METHOD = file
+PYTHON_OPENTRICKLER_SITE = https://github.com/ammolytics/open-trickler-peripheral/releases/download/$(PYTHON_OPENTRICKLER_VERSION)
 PYTHON_OPENTRICKLER_SETUP_TYPE = setuptools
 PYTHON_OPENTRICKLER_LICENSE = MIT
 PYTHON_OPENTRICKLER_LICENSE_FILES = LICENSE
