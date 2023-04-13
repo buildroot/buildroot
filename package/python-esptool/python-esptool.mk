9e90591b22caedfe72c87a8e3315ce8da6d1a4a9
Witold Lipieta <witold.lipieta@thaumatec.com>
2023-04-13 13:34:16 +0200

    package/python-esptool: Bump version to 4.5.1
    Signed-off-by: Witold Lipieta <witold.lipieta@thaumatec.com>
    Acked-by: Bartosz Bilas <b.bilas@grinn-global.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-esptool/python-esptool.mk
+++ package/python-esptool/python-esptool.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ESPTOOL_VERSION = 4.4
+PYTHON_ESPTOOL_VERSION = 4.5.1
 PYTHON_ESPTOOL_SOURCE = esptool-$(PYTHON_ESPTOOL_VERSION).tar.gz
-PYTHON_ESPTOOL_SITE = https://files.pythonhosted.org/packages/8b/4c/82d7fe5fa0643415bbb90bebff1f861816903c481ee5156fabd6d76dc684
+PYTHON_ESPTOOL_SITE = https://files.pythonhosted.org/packages/04/80/8eb97d1793cfaf830a4c0a7ea1d8f0674ccba8c23b7d02dcce074b5f44ea
 PYTHON_ESPTOOL_SETUP_TYPE = setuptools
 PYTHON_ESPTOOL_LICENSE = GPL-2.0+
 PYTHON_ESPTOOL_LICENSE_FILES = LICENSE
