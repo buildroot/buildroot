buildroot/buildroot

commit d9b244d8a3c83b39f67444f36b21a88cbaf4547b

board/qemu: define start qemu script outside of post-image script
The following moves the definition of the QEMU board's `start-qemu.sh`
helper script from being inlined in the post-image script into its own
file. This should, in theory, make it easier to maintain the script in
the future.

Signed-off-by: James Knight <james.d.knight@live.com>
[yann.morin.1998@free.fr:
  - don't sub-shell in the script
  - merge all 3 sed calls into one
  - create dest file with the sed, don't cp first
  - also substitute HOST_DIR
  - fix shellcheck
]
Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

board/qemu/post-image.sh
@@ -41,21 +41,10 @@ case ${DEFCONFIG_NAME} in
     ;;
 esac
 
-cat <<-_EOF_ > "${START_QEMU_SCRIPT}"
-	#!/bin/sh
-	(
-	BINARIES_DIR="\${0%/*}/"
-	cd \${BINARIES_DIR}
-
-	if [ "\${1}" = "serial-only" ]; then
-	    EXTRA_ARGS='${SERIAL_ARGS}'
-	else
-	    EXTRA_ARGS='${DEFAULT_ARGS}'
-	fi
-
-	export PATH="${HOST_DIR}/bin:\${PATH}"
-	exec ${QEMU_CMD_LINE} \${EXTRA_ARGS}
-	)
-_EOF_
-
+sed "s|@SERIAL_ARGS@|${SERIAL_ARGS}|g" \
+    "s|@DEFAULT_ARGS@|${DEFAULT_ARGS}|g" \
+    "s|@QEMU_CMD_LINE@|${QEMU_CMD_LINE}|g" \
+    "s|@HOST_DIR@|${HOST_DIR}|g" \
+    <"${QEMU_BOARD_DIR}/start-qemu.sh.in" \
+    >"${START_QEMU_SCRIPT}"
 chmod +x "${START_QEMU_SCRIPT}"

board/qemu/start-qemu.sh.in
@@ -1,0 +1,14 @@
+#!/bin/sh
+
+BINARIES_DIR="${0%/*}/"
+# shellcheck disable=SC2164
+cd "${BINARIES_DIR}"
+
+if [ "${1}" = "serial-only" ]; then
+    EXTRA_ARGS='@SERIAL_ARGS@'
+else
+    EXTRA_ARGS='@DEFAULT_ARGS@'
+fi
+
+export PATH="@HOST_DIR@/bin:${PATH}"
+exec @QEMU_CMD_LINE@ ${EXTRA_ARGS}

commit 131cb09c79ffe155c51444b5b86145d9e184c304

board/qemu: fix post-image
Last-minute changes to d9b244d8a3c8 (board/qemu: define start qemu
script outside of post-image script) were not tested before being
pushed, so they introduced a bug.

As Arnout put it:
    [...] multiple expressions need a -e to interpret them as
    expressions (now they're interpreted as file names).

Fixes: d9b244d8a3c83b39f67444f36b21a88cbaf4547b

Reported-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
Cc: James Knight <james.d.knight@live.com>

board/qemu/post-image.sh
@@ -41,10 +41,10 @@ case ${DEFCONFIG_NAME} in
     ;;
 esac
 
-sed "s|@SERIAL_ARGS@|${SERIAL_ARGS}|g" \
-    "s|@DEFAULT_ARGS@|${DEFAULT_ARGS}|g" \
-    "s|@QEMU_CMD_LINE@|${QEMU_CMD_LINE}|g" \
-    "s|@HOST_DIR@|${HOST_DIR}|g" \
+sed -e "s|@SERIAL_ARGS@|${SERIAL_ARGS}|g" \
+    -e "s|@DEFAULT_ARGS@|${DEFAULT_ARGS}|g" \
+    -e "s|@QEMU_CMD_LINE@|${QEMU_CMD_LINE}|g" \
+    -e "s|@HOST_DIR@|${HOST_DIR}|g" \
     <"${QEMU_BOARD_DIR}/start-qemu.sh.in" \
     >"${START_QEMU_SCRIPT}"
 chmod +x "${START_QEMU_SCRIPT}"