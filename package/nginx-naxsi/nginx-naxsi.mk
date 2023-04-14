cb78e8c362686660cdb3876607b67c92eacc6140
Peter Seiderer <ps.report@gmx.net>
2023-04-14 23:35:04 +0200

    package/nginx-naxsi: bump version to d714f16
    Update to latest upstream git version, fixes compile failure since
    nginx-1.23.0:
    
      src/event/ngx_event_udp.h:38:27: error: field ‘pkt6’ has incomplete type
         38 |     struct in6_pktinfo    pkt6;
            |                           ^~~~
    
      .../nginx-naxsi-1.3/naxsi_src/naxsi_runtime.c:2925:36: error: ‘r->headers_in.x_forwarded_for’ is a pointer; did you mean to use ‘->’?
       2925 |   if (r->headers_in.x_forwarded_for.nelts >= 1) {
            |                                    ^
            |                                    ->
    
    - remove 0001-naxsi_src-naxsi_runtime.c-fix-build-without-x_forwar.patch
      (upstream commit, see [1])
    - remove 0002-PCRE2-compatibility.patch
      (upstream commit, see [2])
    
    Changelog (since 1.3):
    
    - a2add9f docs: fix simple typo, registred -> registered (#538)
    - aa9da98 Fix #541 - Removing useless assert.
    - fbe6ffd Some includes are required for OpenBSD (#545)
    - 296583f naxsi_src/naxsi_runtime.c: fix build without x_forwarded_for (#568)
    - fe5df20 redirect naxsi log to a separate log file (#563)
    - c81a4e3 parse HTTP PATCH requests and associated tests (#595)
    - 2937c44 PCRE2 compatibility (#587)
    - d714f16 Fixes NGINX >= v.1.23.0 (all credits @lubomudr) (#598)
    
    Given the short history since 1.3, and rather than backporting one more
    patch, just bump to current HEAD of the upstream repository.
    
    [1] https://github.com/nbs-system/naxsi/commit/296583f06ba5c43cc859e01fd3c0ae1271eef0ce
    [2] https://github.com/nbs-system/naxsi/commit/2937c44276cba21601ade4e265d32515f570d68c
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/nginx-naxsi/nginx-naxsi.mk
+++ package/nginx-naxsi/nginx-naxsi.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NGINX_NAXSI_VERSION = 1.3
+NGINX_NAXSI_VERSION = d714f1636ea49a9a9f4f06dba14aee003e970834
 NGINX_NAXSI_SITE = $(call github,nbs-system,naxsi,$(NGINX_NAXSI_VERSION))
 NGINX_NAXSI_LICENSE = GPL-3.0, BSD-3-Clause (libinjection)
 NGINX_NAXSI_LICENSE_FILES = LICENSE naxsi_src/ext/libinjection/COPYING
