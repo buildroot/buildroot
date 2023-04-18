436f9966ce7930b4d4cbf5182b5ebaf6e52ee089
Raphaël Mélotte <raphael.melotte@mind.be>
2023-04-18 11:43:15 +0200

    package: python-botocore: bump to 1.29.115
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-botocore/python-botocore.mk
+++ package/python-botocore/python-botocore.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_BOTOCORE_VERSION = 1.29.96
+PYTHON_BOTOCORE_VERSION = 1.29.115
 PYTHON_BOTOCORE_SOURCE = botocore-$(PYTHON_BOTOCORE_VERSION).tar.gz
-PYTHON_BOTOCORE_SITE = https://files.pythonhosted.org/packages/2d/b5/4a986aeb4e3d182f7ef116705962d3c98c0263beaf87c49836b7dfb91837
+PYTHON_BOTOCORE_SITE = https://files.pythonhosted.org/packages/9e/00/4560081a431aaa14c2257040d43f56c2ccba816d8bce3b8e1577a19f4076
 PYTHON_BOTOCORE_SETUP_TYPE = setuptools
 PYTHON_BOTOCORE_LICENSE = Apache-2.0
-PYTHON_BOTOCORE_LICENSE_FILES = LICENSE.txt
+PYTHON_BOTOCORE_LICENSE_FILES = LICENSE.txt tests/unit/auth/aws4_testsuite/LICENSE
 
 $(eval $(python-package))
