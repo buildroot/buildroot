244fe62cafb428e2bce303010b4f7699640bcafe
Peter Seiderer <ps.report@gmx.net>
2023-04-20 19:00:05 +0200

    package/coreutils: bump version to 9.3
    Bugfix release, fixing regressions introduced by 9.2. For details see [1].
    
    [1] https://lists.gnu.org/archive/html/info-gnu/2023-04/msg00006.html
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/coreutils/coreutils.mk
+++ package/coreutils/coreutils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-COREUTILS_VERSION = 9.2
+COREUTILS_VERSION = 9.3
 COREUTILS_SITE = $(BR2_GNU_MIRROR)/coreutils
 COREUTILS_SOURCE = coreutils-$(COREUTILS_VERSION).tar.xz
 COREUTILS_LICENSE = GPL-3.0+
