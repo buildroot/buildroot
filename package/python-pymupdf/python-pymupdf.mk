a650d714341c0e2900dac492c02d3b5701c87c8d
Raphaël Mélotte <raphael.melotte@mind.be>
2023-04-20 16:45:35 +0200

    package/python-pymupdf: add missing host swig dependency
    It appears that python-pymupdf needs swig when building since mupdf
    was bumped to 1.18.0 in commit
    f00da714a20a7826dda67d66f2a6b6ef858ef3e3. This was not noticed before
    because when building on a machine where swig is already available
    outside buildroot, it is used and the build succeeds.
    
    This fixes the following build failure:
    
    swigging fitz/fitz.i to fitz/fitz_wrap.c
    swig -python -w509 -o fitz/fitz_wrap.c fitz/fitz.i
    error: command 'swig' failed: No such file or directory
    make: *** [package/pkg-generic.mk:293:
    /home/buildroot/autobuild/instance-2/output-1/build/python-pymupdf-1.21.1/.stamp_built]
    Error 1
    
    Fixes:
    - http://autobuild.buildroot.net/results/c689a385d984b87b63f3f2ff30338049774b1bf2
    - http://autobuild.buildroot.net/results/cf268d3631330a8288fb8ad2af009abb7f1ca6b0
    
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-pymupdf/python-pymupdf.mk
+++ package/python-pymupdf/python-pymupdf.mk
@@ -12,7 +12,7 @@ PYTHON_PYMUPDF_SETUP_TYPE = setuptools
 PYTHON_PYMUPDF_LICENSE = AGPL-3.0+
 PYTHON_PYMUPDF_LICENSE_FILES = COPYING
 # No license file included in pip, but it's present on github
-PYTHON_PYMUPDF_DEPENDENCIES = freetype mupdf zlib
+PYTHON_PYMUPDF_DEPENDENCIES = freetype host-swig mupdf zlib
 
 PYTHON_PYMUPDF_ENV = CFLAGS="-I$(STAGING_DIR)/usr/include/mupdf -I$(STAGING_DIR)/usr/include/freetype2"
 
