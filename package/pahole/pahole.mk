e98b07dedee7fc80c58b10b4a0b2a6ad57f3bb62
Francis Laniel <flaniel@linux.microsoft.com>
2023-04-21 12:36:27 +0100

    package/pahole: bump to version 1.25
    https://git.kernel.org/pub/scm/devel/pahole/pahole.git/tree/NEWS?h=v1.25
    
    Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/pahole/pahole.mk
+++ package/pahole/pahole.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PAHOLE_VERSION = 1.24
+PAHOLE_VERSION = 1.25
 PAHOLE_SITE = https://git.kernel.org/pub/scm/devel/pahole/pahole.git/snapshot
 HOST_PAHOLE_DEPENDENCIES = \
 	host-elfutils \
