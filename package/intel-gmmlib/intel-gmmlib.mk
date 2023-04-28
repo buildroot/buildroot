64d7856924d88ea06886cbb79872b353b6a1899b
Bernd Kuhls <bernd.kuhls@t-online.de>
2023-04-28 14:23:41 +0200

    package/intel-gmmlib: bump version to 22.3.5
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/intel-gmmlib/intel-gmmlib.mk
+++ package/intel-gmmlib/intel-gmmlib.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-INTEL_GMMLIB_VERSION = 22.3.4
+INTEL_GMMLIB_VERSION = 22.3.5
 INTEL_GMMLIB_SITE = https://github.com/intel/gmmlib/archive
 INTEL_GMMLIB_LICENSE = MIT
 INTEL_GMMLIB_LICENSE_FILES = LICENSE.md
