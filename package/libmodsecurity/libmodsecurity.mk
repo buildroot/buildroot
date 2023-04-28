f6f9b0938b41371d49207f99f86e50d9984dc05a
Frank Vanbever <frank.vanbever@mind.be>
2023-04-28 21:23:51 +0200

    package/libmodsecurity: security bump to version 3.0.9
    Fixes the following security issue:
    - CVE-2023-28882: Trustwave ModSecurity 3.0.5 through 3.0.8 before 3.0.9 allows
      a denial of service (worker crash and unresponsiveness) because some inputs
      cause a segfault in the Transaction class for some configurations.
    
      https://security-tracker.debian.org/tracker/CVE-2023-28882
    
    - Drop 0003-Revert-Fix-maxminddb-link-on-FreeBSD.patch, handling of libmaxminddb
      was fixed upstream in d2b700d
    - Drop 0004-build-pcre.m4-fix-build-without-pcre.patch, handling of PCRE was
      fixed upstream in 791964a
    
    Signed-off-by: Frank Vanbever <frank.vanbever@mind.be>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit a1e0e7276ca246385d7f31d2db8331f52ce34228)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libmodsecurity/libmodsecurity.mk
+++ package/libmodsecurity/libmodsecurity.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBMODSECURITY_VERSION = 3.0.8
+LIBMODSECURITY_VERSION = 3.0.9
 LIBMODSECURITY_SOURCE = modsecurity-v$(LIBMODSECURITY_VERSION).tar.gz
 LIBMODSECURITY_SITE = https://github.com/SpiderLabs/ModSecurity/releases/download/v$(LIBMODSECURITY_VERSION)
 LIBMODSECURITY_INSTALL_STAGING = YES
@@ -12,7 +12,7 @@ LIBMODSECURITY_LICENSE = Apache-2.0
 LIBMODSECURITY_LICENSE_FILES = LICENSE
 LIBMODSECURITY_CPE_ID_VENDOR = trustwave
 LIBMODSECURITY_CPE_ID_PRODUCT = modsecurity
-# We're patching build/libmaxmind.m4 and build/pcre.m4
+# We're patching configure.ac
 LIBMODSECURITY_AUTORECONF = YES
 
 LIBMODSECURITY_DEPENDENCIES = pcre2
