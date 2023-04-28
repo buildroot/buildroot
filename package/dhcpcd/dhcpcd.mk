3e60ea997233e9c5579a1d2327364396efc762d3
Xiangyu Chen <xiangyu.chen@windriver.com>
2023-04-28 11:31:37 +0800

    package/dhcpcd: bump to version 10.0.1
    Switch to github as package site as announced here:
    https://roy.marples.name/downloads/dhcpcd/
    
    Update license hash due to update in Copyright years.
    
    Signed-off-by: Xiangyu Chen <xiangyu.chen@windriver.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/dhcpcd/dhcpcd.mk
+++ package/dhcpcd/dhcpcd.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-DHCPCD_VERSION = 9.4.1
+DHCPCD_VERSION = 10.0.1
 DHCPCD_SOURCE = dhcpcd-$(DHCPCD_VERSION).tar.xz
-DHCPCD_SITE = http://roy.marples.name/downloads/dhcpcd
+DHCPCD_SITE = https://github.com/NetworkConfiguration/dhcpcd/releases/download/v$(DHCPCD_VERSION)
 DHCPCD_DEPENDENCIES = host-pkgconf
 DHCPCD_LICENSE = BSD-2-Clause
 DHCPCD_LICENSE_FILES = LICENSE
