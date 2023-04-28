73e3a8f8a8d906627a15f4f5be81ce0ceca4aeb0
James Knight <james.d.knight@live.com>
2023-04-27 21:33:35 -0400

    package/libxkbcommon: enable wayland support
    Tweak the applied configuration options to flag support for Wayland if
    the wayland package is detected. This will allow the generation of the
    `interactive-wayland` utility (when `BR2_PACKAGE_LIBXKBCOMMON_TOOLS` is
    also configured).
    
    Signed-off-by: James Knight <james.d.knight@live.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/libxkbcommon/libxkbcommon.mk
+++ package/libxkbcommon/libxkbcommon.mk
@@ -14,7 +14,6 @@ LIBXKBCOMMON_INSTALL_STAGING = YES
 LIBXKBCOMMON_DEPENDENCIES = host-bison host-flex
 LIBXKBCOMMON_CONF_OPTS = \
 	-Denable-docs=false \
-	-Denable-wayland=false \
 	-Denable-xkbregistry=false
 
 ifeq ($(BR2_PACKAGE_XORG7),y)
@@ -28,6 +27,13 @@ ifeq ($(BR2_PACKAGE_LIBXKBCOMMON_TOOLS),y)
 LIBXKBCOMMON_CONF_OPTS += -Denable-tools=true
 else
 LIBXKBCOMMON_CONF_OPTS += -Denable-tools=false
+endif
+
+ifeq ($(BR2_PACKAGE_LIBXKBCOMMON_TOOLS)$(BR2_PACKAGE_WAYLAND),yy)
+LIBXKBCOMMON_CONF_OPTS += -Denable-wayland=true
+LIBXKBCOMMON_DEPENDENCIES += wayland wayland-protocols
+else
+LIBXKBCOMMON_CONF_OPTS += -Denable-wayland=false
 endif
 
 $(eval $(meson-package))
