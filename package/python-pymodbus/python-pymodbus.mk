3d980fab03934fe88fce4bc6e55fbda99605631d
Mauricio Vidal <maovidal@users.noreply.github.com>
2023-04-28 21:18:26 -0500

    pymodbus: 3.2.2

--- package/python-pymodbus/python-pymodbus.mk
+++ package/python-pymodbus/python-pymodbus.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYMODBUS_VERSION = 3.0.2
+PYTHON_PYMODBUS_VERSION = 3.2.2
 PYTHON_PYMODBUS_SOURCE = pymodbus-$(PYTHON_PYMODBUS_VERSION).tar.gz
-PYTHON_PYMODBUS_SITE = https://files.pythonhosted.org/packages/94/f7/6149a1b107fa54378a7adec83db7cd3679bf02216a6f5c83b37f55335897
+PYTHON_PYMODBUS_SITE = https://files.pythonhosted.org/packages/be/9a/71c0dafc155990976135877391ca58e98dd3c4a6beb12bee7b3d75a71212
 PYTHON_PYMODBUS_SETUP_TYPE = setuptools
 PYTHON_PYMODBUS_LICENSE = BSD-3-Clause
 PYTHON_PYMODBUS_LICENSE_FILES = LICENSE
