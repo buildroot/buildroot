5d65570fac4d577bd3661ad892df80bc9601f4ae
Christian Stewart <christian@aperture.us>
2023-05-04 00:37:11 -0700

    package/nerdctl: bump version to 1.3.1
    Bugfixes and features.
    
    https://github.com/containerd/nerdctl/releases/tag/v1.3.1
    
    Signed-off-by: Christian Stewart <christian@aperture.us>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/nerdctl/nerdctl.mk
+++ package/nerdctl/nerdctl.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NERDCTL_VERSION = 1.2.0
+NERDCTL_VERSION = 1.3.1
 NERDCTL_SITE = $(call github,containerd,nerdctl,v$(NERDCTL_VERSION))
 
 NERDCTL_LICENSE = Apache-2.0
