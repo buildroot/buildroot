58035c687aa9128408e6519857a8b6a8c64ae6e8
Bernd Kuhls <bernd.kuhls@t-online.de>
2023-05-06 19:39:14 +0200

    package/x11r7/xdata_xcursor-themes: bump version to 1.0.7
    Release notes:
    https://lists.x.org/archives/xorg-announce/2023-February/003332.html
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/x11r7/xdata_xcursor-themes/xdata_xcursor-themes.mk
+++ package/x11r7/xdata_xcursor-themes/xdata_xcursor-themes.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XDATA_XCURSOR_THEMES_VERSION = 1.0.6
-XDATA_XCURSOR_THEMES_SOURCE = xcursor-themes-$(XDATA_XCURSOR_THEMES_VERSION).tar.bz2
-XDATA_XCURSOR_THEMES_SITE = http://xorg.freedesktop.org/releases/individual/data
+XDATA_XCURSOR_THEMES_VERSION = 1.0.7
+XDATA_XCURSOR_THEMES_SOURCE = xcursor-themes-$(XDATA_XCURSOR_THEMES_VERSION).tar.xz
+XDATA_XCURSOR_THEMES_SITE = https://xorg.freedesktop.org/archive/individual/data
 XDATA_XCURSOR_THEMES_LICENSE = MIT
 XDATA_XCURSOR_THEMES_LICENSE_FILES = COPYING
 
