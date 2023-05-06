827bc83cb1aa891fbb41ea332a013d850a121760
Bernd Kuhls <bernd.kuhls@t-online.de>
2023-05-06 18:05:23 +0200

    package/libutp: bump version
    Needed for bumping transmission to version 4.0.1.
    
    Added configure option to fix build error with gcc-12.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/libutp/libutp.mk
+++ package/libutp/libutp.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-LIBUTP_VERSION = 059c9449a104e440e4f913756a5f560dd4ae76a9
+LIBUTP_VERSION = c95738b1a6644b919e5b64d3ea9736cfc5894e0b
 LIBUTP_SITE = $(call github,transmission,libutp,$(LIBUTP_VERSION))
 LIBUTP_LICENSE = MIT
 LIBUTP_LICENSE_FILES = LICENSE
 LIBUTP_INSTALL_STAGING = YES
+LIBUTP_CONF_OPTS = -DLIBUTP_ENABLE_WERROR=OFF
 
 $(eval $(cmake-package))
