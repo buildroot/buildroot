8f059f406b823237e32004ed1fd01e3906d030f5
Bernd Kuhls <bernd.kuhls@t-online.de>
2023-05-06 19:38:57 +0200

    package/x11r7/xdriver_xf86-input-mouse: bump version to 1.9.5
    Release notes:
    https://lists.x.org/archives/xorg-announce/2023-May/003392.html
    
    Updated license hash due to update in copyrighht years with upstream
    commit:
    https://gitlab.freedesktop.org/xorg/driver/xf86-input-mouse/-/commit/5f63ab04338fd811c610ae7617757ede52471316
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit b30206b708e02adb1b287a2d450f48c4bd12f754)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xdriver_xf86-input-mouse/xdriver_xf86-input-mouse.mk
+++ package/x11r7/xdriver_xf86-input-mouse/xdriver_xf86-input-mouse.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_INPUT_MOUSE_VERSION = 1.9.4
+XDRIVER_XF86_INPUT_MOUSE_VERSION = 1.9.5
 XDRIVER_XF86_INPUT_MOUSE_SOURCE = xf86-input-mouse-$(XDRIVER_XF86_INPUT_MOUSE_VERSION).tar.xz
 XDRIVER_XF86_INPUT_MOUSE_SITE = https://xorg.freedesktop.org/archive/individual/driver
 XDRIVER_XF86_INPUT_MOUSE_LICENSE = MIT
