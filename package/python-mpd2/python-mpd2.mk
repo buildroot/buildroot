e1a5382182ea1a4d27dfed153a82050d3b53acce
Uladzimir Bely <wiselord1983@gmail.com>
2023-05-06 12:11:27 +0300

    package/python-mpd2: bump to version 3.1.0
    Signed-off-by: Uladzimir Bely <wiselord1983@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-mpd2/python-mpd2.mk
+++ package/python-mpd2/python-mpd2.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PYTHON_MPD2_VERSION = 3.0.5
-PYTHON_MPD2_SITE = https://files.pythonhosted.org/packages/ed/c2/263621bdbbc37fa1a55fa1a7f66fd601bf08b3f87481e318a3509fb6c4bf
+PYTHON_MPD2_VERSION = 3.1.0
+PYTHON_MPD2_SITE = https://files.pythonhosted.org/packages/59/32/e57725251ce3117d2ed2a7b76d9722ea9bea26f54b2cc8981d03350e4103
 PYTHON_MPD2_SETUP_TYPE = setuptools
 PYTHON_MPD2_LICENSE = LGPL-3.0+
 PYTHON_MPD2_LICENSE_FILES = LICENSE.txt
