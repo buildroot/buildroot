551a702b93ea356a7e050ce4710eed30ededf793
Bernd Kuhls <bernd.kuhls@t-online.de>
2023-05-06 19:39:15 +0200

    package/x11r7/xfont_font-util: bump version to 1.4.0
    Release notes:
    https://lists.x.org/archives/xorg-announce/2023-February/003331.html
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/x11r7/xfont_font-util/xfont_font-util.mk
+++ package/x11r7/xfont_font-util/xfont_font-util.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XFONT_FONT_UTIL_VERSION = 1.3.3
+XFONT_FONT_UTIL_VERSION = 1.4.0
 XFONT_FONT_UTIL_SOURCE = font-util-$(XFONT_FONT_UTIL_VERSION).tar.xz
-XFONT_FONT_UTIL_SITE = https://xorg.freedesktop.org/releases/individual/font
+XFONT_FONT_UTIL_SITE = https://xorg.freedesktop.org/archive/individual/font
 XFONT_FONT_UTIL_LICENSE = MIT/BSD-2-Clause
 XFONT_FONT_UTIL_LICENSE_FILES = COPYING
 
