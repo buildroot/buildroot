e24cd30a147992a6647da23cc4ff5b4aac1aa120
Bernd Kuhls <bernd.kuhls@t-online.de>
2023-05-06 19:39:10 +0200

    package/x11r7/xdriver_xf86-input-libinput: bump version to 1.3.0
    Release notes:
    https://lists.x.org/archives/xorg-announce/2023-April/003378.html
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/x11r7/xdriver_xf86-input-libinput/xdriver_xf86-input-libinput.mk
+++ package/x11r7/xdriver_xf86-input-libinput/xdriver_xf86-input-libinput.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_INPUT_LIBINPUT_VERSION = 1.2.1
+XDRIVER_XF86_INPUT_LIBINPUT_VERSION = 1.3.0
 XDRIVER_XF86_INPUT_LIBINPUT_SOURCE = xf86-input-libinput-$(XDRIVER_XF86_INPUT_LIBINPUT_VERSION).tar.xz
 XDRIVER_XF86_INPUT_LIBINPUT_SITE = https://xorg.freedesktop.org/archive/individual/driver
 XDRIVER_XF86_INPUT_LIBINPUT_LICENSE = MIT
