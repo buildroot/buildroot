2a23ae346c7563c8e9486903a13ff28d6bf0334f
Bernd Kuhls <bernd.kuhls@t-online.de>
2023-05-06 19:39:09 +0200

    package/x11r7/xapp_xwininfo: bump version to 1.1.6
    Release notes:
    https://lists.x.org/archives/xorg-announce/2023-April/003379.html
    
    Updated license hash due to upstream commit:
    https://gitlab.freedesktop.org/xorg/app/xwininfo/-/commit/9a5623e2ffc6995b2c6edf379f191d76d7b4444a
      -> drop "all rights reserved", update years
      -> no change in licensing
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit de8a3a53e52dc1ac6e04e623739aa67d412e965e)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xapp_xwininfo/xapp_xwininfo.mk
+++ package/x11r7/xapp_xwininfo/xapp_xwininfo.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-XAPP_XWININFO_VERSION = 1.1.5
-XAPP_XWININFO_SOURCE = xwininfo-$(XAPP_XWININFO_VERSION).tar.bz2
+XAPP_XWININFO_VERSION = 1.1.6
+XAPP_XWININFO_SOURCE = xwininfo-$(XAPP_XWININFO_VERSION).tar.xz
 XAPP_XWININFO_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XWININFO_LICENSE = MIT
 XAPP_XWININFO_LICENSE_FILES = COPYING
