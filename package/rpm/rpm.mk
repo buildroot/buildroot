aeff26d21dd72eb01a970dd392c06aa52268cfdc
James Knight <james.d.knight@live.com>
2023-05-07 17:34:33 -0400

    package/rpm: add readline support
    When readline support is detected, enable readline support.
    
    Signed-off-by: James Knight <james.d.knight@live.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/rpm/rpm.mk
+++ package/rpm/rpm.mk
@@ -89,6 +89,13 @@ else
 RPM_CONF_OPTS += --disable-python
 endif
 
+ifeq ($(BR2_PACKAGE_READLINE),y)
+RPM_DEPENDENCIES += readline
+RPM_CONF_OPTS += --with-readline
+else
+RPM_CONF_OPTS += --without-readline
+endif
+
 ifeq ($(BR2_PACKAGE_SQLITE),y)
 RPM_DEPENDENCIES += sqlite
 RPM_CONF_OPTS += --enable-sqlite
