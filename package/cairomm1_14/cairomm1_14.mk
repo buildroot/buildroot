9d392f43437e0ee0332cac66c9d0477230ed019f
Daniel Lang <dalang@gmx.at>
2023-05-07 22:16:36 +0200

    package/cairomm1_14: use official website
    The website [1] states that the released version can be downloaded from
    the cairo release area [2], which also features a sha1 hash, but only
    provides xz archives (no gz).
    
    [1]: https://www.cairographics.org/cairomm/
    [2]: https://www.cairographics.org/releases/
    
    Signed-off-by: Daniel Lang <dalang@gmx.at>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/cairomm1_14/cairomm1_14.mk
+++ package/cairomm1_14/cairomm1_14.mk
@@ -7,8 +7,8 @@
 CAIROMM1_14_VERSION = 1.14.4
 CAIROMM1_14_LICENSE = LGPL-2.0+
 CAIROMM1_14_LICENSE_FILES = COPYING
-CAIROMM1_14_SOURCE = cairomm-$(CAIROMM1_14_VERSION).tar.gz
-CAIROMM1_14_SITE = https://gitlab.freedesktop.org/cairo/cairomm/-/archive/$(CAIROMM1_14_VERSION)
+CAIROMM1_14_SOURCE = cairomm-$(CAIROMM1_14_VERSION).tar.xz
+CAIROMM1_14_SITE = https://cairographics.org/releases
 CAIROMM1_14_INSTALL_STAGING = YES
 CAIROMM1_14_DEPENDENCIES = cairo libglib2 libsigc2 host-pkgconf
 CAIROMM1_14_CONF_OPTS = -Dbuild-examples=false -Dbuild-tests=false
