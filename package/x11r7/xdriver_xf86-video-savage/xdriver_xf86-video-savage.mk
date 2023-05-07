9aa67d7409e0c33cdbc9a6b5103f287bc87b4be5
Bernd Kuhls <bernd.kuhls@t-online.de>
2023-05-07 12:10:29 +0200

    package/x11r7/xdriver_xf86-video-savage: bump version to 2.4.0
    Release notes:
    https://lists.x.org/archives/xorg-announce/2023-April/003381.html
    
    Removed all patches due to upstream commits:
    https://gitlab.freedesktop.org/xorg/driver/xf86-video-savage/-/commit/2ec88355123b1def99d8c79769f396ac97bc5008
    https://gitlab.freedesktop.org/xorg/driver/xf86-video-savage/-/commit/0ece556daa8a88771b669d8104396abd9166d2d0
    
    and update .checkpackageignore
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/x11r7/xdriver_xf86-video-savage/xdriver_xf86-video-savage.mk
+++ package/x11r7/xdriver_xf86-video-savage/xdriver_xf86-video-savage.mk
@@ -4,12 +4,11 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_SAVAGE_VERSION = 2.3.9
-XDRIVER_XF86_VIDEO_SAVAGE_SOURCE = xf86-video-savage-$(XDRIVER_XF86_VIDEO_SAVAGE_VERSION).tar.bz2
-XDRIVER_XF86_VIDEO_SAVAGE_SITE = http://xorg.freedesktop.org/releases/individual/driver
+XDRIVER_XF86_VIDEO_SAVAGE_VERSION = 2.4.0
+XDRIVER_XF86_VIDEO_SAVAGE_SOURCE = xf86-video-savage-$(XDRIVER_XF86_VIDEO_SAVAGE_VERSION).tar.xz
+XDRIVER_XF86_VIDEO_SAVAGE_SITE = https://xorg.freedesktop.org/archive/individual/driver
 XDRIVER_XF86_VIDEO_SAVAGE_LICENSE = MIT
 XDRIVER_XF86_VIDEO_SAVAGE_LICENSE_FILES = COPYING
-XDRIVER_XF86_VIDEO_SAVAGE_AUTORECONF = YES
 XDRIVER_XF86_VIDEO_SAVAGE_DEPENDENCIES = xserver_xorg-server libdrm xorgproto
 
 ifeq ($(BR2_PACKAGE_HAS_LIBGL),y)
