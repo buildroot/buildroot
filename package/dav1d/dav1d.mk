2b087c7c28f1d2bb441986e447b2186d3ed66c9e
Bernd Kuhls <bernd.kuhls@t-online.de>
2023-05-08 18:47:44 +0200

    package/dav1d: bump version to 1.2.0
    Additionally fxes a security issue:
    
    - CVE-2023-32570: VideoLAN dav1d before 1.2.0 has a thread_task.c race
      condition that can lead to an application crash, related to
      dav1d_decode_frame_exit.
    
    Release notes:
    https://code.videolan.org/videolan/dav1d/-/blob/master/NEWS
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 37fd4ee80a408d8fa810eb1926cac111acbba811)
    [Peter: mention security fix]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/dav1d/dav1d.mk
+++ package/dav1d/dav1d.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DAV1D_VERSION = 1.1.0
+DAV1D_VERSION = 1.2.0
 DAV1D_SOURCE = dav1d-$(DAV1D_VERSION).tar.xz
 DAV1D_SITE = https://download.videolan.org/pub/videolan/dav1d/$(DAV1D_VERSION)
 DAV1D_LICENSE = BSD-2-Clause
