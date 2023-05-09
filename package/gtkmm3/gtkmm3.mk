6a3e81bf5492c61dfe14b5e1485b9adb034ce4e8
Daniel Lang <dalang@gmx.at>
2023-05-09 14:47:20 +0200

    package/gtkmm3: bump to version 3.24.7
    https://gitlab.gnome.org/GNOME/gtkmm/-/blob/3.24.7/NEWS
    
    README [0] states:
    
        It's easiest to build with Meson, if the tarball was made with Meson,
        and to build with Autotools, if the tarball was made with Autotools.
        Then you don't have to use maintainer-mode.
        How do you know how the tarball was made? If it was made with Meson,
        it contains files in untracked/gdk/gdkmm/, untracked/gtk/gtkmm/ and
        other subdirectories of untracked/.
    
    This is the case, therefore switch to meson build system.
    
    gtkmm3 tries to detect if X11 is enabled for gtk.
    Switch to explicit definition to be safe.
    
    Also disable tests and demos.
    
    [0]: https://gitlab.gnome.org/GNOME/gtkmm/-/blob/3.24.7/README
    
    Signed-off-by: Daniel Lang <dalang@gmx.at>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/gtkmm3/gtkmm3.mk
+++ package/gtkmm3/gtkmm3.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-GTKMM3_VERSION_MAJOR = 3.22
-GTKMM3_VERSION = $(GTKMM3_VERSION_MAJOR).0
+GTKMM3_VERSION_MAJOR = 3.24
+GTKMM3_VERSION = $(GTKMM3_VERSION_MAJOR).7
 GTKMM3_SOURCE = gtkmm-$(GTKMM3_VERSION).tar.xz
 GTKMM3_SITE = https://download.gnome.org/sources/gtkmm/$(GTKMM3_VERSION_MAJOR)
 GTKMM3_LICENSE = LGPL-2.1+ (library), GPL-2.0+ (tools)
@@ -13,4 +13,14 @@ GTKMM3_LICENSE_FILES = COPYING COPYING.tools
 GTKMM3_INSTALL_STAGING = YES
 GTKMM3_DEPENDENCIES = atkmm2_28 cairomm1_14 glibmm2_66 libgtk3 libsigc2 pangomm2_46 host-pkgconf
 
-$(eval $(autotools-package))
+GTKMM3_CONF_OPTS = \
+	-Dbuild-demos=false \
+	-Dbuild-tests=false
+
+ifeq ($(BR2_PACKAGE_LIBGTK3_X11),y)
+GTKMM3_CONF_OPTS += -Dbuild-x11-api=true
+else
+GTKMM3_CONF_OPTS += -Dbuild-x11-api=false
+endif
+
+$(eval $(meson-package))
