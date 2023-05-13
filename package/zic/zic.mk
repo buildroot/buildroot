179ae2fa6b4acb49fe164dc0a2de28b21fee695f
Yann E. MORIN <yann.morin.1998@free.fr>
2023-05-13 20:50:57 +0200

    package/tzdata: bump version to 2023c
    tzdata and tzcode (for zic) are released in lock-step, as they go
    together, so we update both simultaneously.
    
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/zic/zic.mk
+++ package/zic/zic.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ZIC_VERSION = 2022f
+ZIC_VERSION = 2023c
 ZIC_SOURCE = tzcode$(ZIC_VERSION).tar.gz
 ZIC_SITE = https://www.iana.org/time-zones/repository/releases
 ZIC_STRIP_COMPONENTS = 0
