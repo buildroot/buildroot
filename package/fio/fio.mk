4bcbf467c307a8bb07e7937ebf4b35e27eb66305
Maksim Kiselev <bigunclemax@gmail.com>
2023-05-16 11:23:40 +0300

    package/fio: add optional libiscsi dependency
    Since commit 247ef2a fio has optional libiscsi engine support
    
    This patch enables fio iscsi support if BR2_PACKAGE_LIBISCSI
    was selected, which also requires pkg-config as it is used by the
    configure script to detect libiscsi.
    
    There is no --disable-libscsi option in the configure script: if
    --enable-libiscsi is not passed, it doesn't even try to detect/use
    libiscsi.
    
    Signed-off-by: Maksim Kiselev <bigunclemax@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/fio/fio.mk
+++ package/fio/fio.mk
@@ -9,12 +9,19 @@ FIO_SITE = http://brick.kernel.dk/snaps
 FIO_LICENSE = GPL-2.0
 FIO_LICENSE_FILES = COPYING MORAL-LICENSE
 
+FIO_OPTS = --cc="$(TARGET_CC)" --extra-cflags="$(TARGET_CFLAGS)"
+
 ifeq ($(BR2_PACKAGE_LIBAIO),y)
 FIO_DEPENDENCIES += libaio
 endif
 
 ifeq ($(BR2_PACKAGE_LIBNFS),y)
 FIO_DEPENDENCIES += libnfs
+endif
+
+ifeq ($(BR2_PACKAGE_LIBISCSI),y)
+FIO_OPTS += --enable-libiscsi
+FIO_DEPENDENCIES += host-pkgconf libiscsi
 endif
 
 ifeq ($(BR2_PACKAGE_NUMACTL),y)
@@ -26,7 +33,7 @@ FIO_DEPENDENCIES += zlib
 endif
 
 define FIO_CONFIGURE_CMDS
-	(cd $(@D); $(TARGET_MAKE_ENV) ./configure --cc="$(TARGET_CC)" --extra-cflags="$(TARGET_CFLAGS)")
+	(cd $(@D); $(TARGET_MAKE_ENV) ./configure $(FIO_OPTS))
 endef
 
 define FIO_BUILD_CMDS
