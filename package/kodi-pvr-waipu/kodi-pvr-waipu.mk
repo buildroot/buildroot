61bfe280d1dd2a822310796456041329bfa0596e
Bernd Kuhls <bernd.kuhls@t-online.de>
2023-05-23 18:15:22 +0200

    package/kodi-pvr-waipu: bump version to 20.9.0-Nexus
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-pvr-waipu/kodi-pvr-waipu.mk
+++ package/kodi-pvr-waipu/kodi-pvr-waipu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PVR_WAIPU_VERSION = 20.8.1-Nexus
+KODI_PVR_WAIPU_VERSION = 20.9.0-Nexus
 KODI_PVR_WAIPU_SITE = $(call github,flubshi,pvr.waipu,$(KODI_PVR_WAIPU_VERSION))
 KODI_PVR_WAIPU_LICENSE = GPL-2.0+
 KODI_PVR_WAIPU_LICENSE_FILES = pvr.waipu/LICENSE.txt
