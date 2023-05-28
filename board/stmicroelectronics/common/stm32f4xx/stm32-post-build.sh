f187fad22d692f046441976efcd4385692a48261
Dario Binacchi <dario.binacchi@amarulasolutions.com>
2023-05-28 17:31:56 +0200

    configs/stm32f4xx_defconfig: off network utils from busybox
    Kernel is built without network support, so network utils are useless.
    This has a positive impact on the size of busybox and xipImage, making
    them smaller.
    
    Signed-off-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>
    Reviewed-by: Giulio Benetti <giulio.benetti@benettiegineering.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- board/stmicroelectronics/common/stm32f4xx/stm32-post-build.sh
+++ board/stmicroelectronics/common/stm32f4xx/stm32-post-build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# Busybox is built without network support
+sed -i '/hostname/d' ${TARGET_DIR}/etc/inittab
+
 # Kernel is built without devpts support
 sed -i '/^devpts/d' ${TARGET_DIR}/etc/fstab
