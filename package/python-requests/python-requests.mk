18fb407dc4fca2fc89e824fe0a5fcbb17407e5db
James Hilliard <james.hilliard1@gmail.com>
2023-06-05 01:33:24 -0600

    package/python-requests: security bump to version 2.31.0
    Fixes the following security issue:
    
    - CVE-2023-32681: Unintended leak of Proxy-Authorization header
      https://github.com/psf/requests/security/advisories/GHSA-j8r2-6x86-q33q
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>
    (cherry picked from commit c94922beac62e66a993e0a6bc14567af4827d8d1)
    [Peter: mark as security bump]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-requests/python-requests.mk
+++ package/python-requests/python-requests.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_REQUESTS_VERSION = 2.28.2
+PYTHON_REQUESTS_VERSION = 2.31.0
 PYTHON_REQUESTS_SOURCE = requests-$(PYTHON_REQUESTS_VERSION).tar.gz
-PYTHON_REQUESTS_SITE = https://files.pythonhosted.org/packages/9d/ee/391076f5937f0a8cdf5e53b701ffc91753e87b07d66bae4a09aa671897bf
+PYTHON_REQUESTS_SITE = https://files.pythonhosted.org/packages/9d/be/10918a2eac4ae9f02f6cfe6414b7a155ccd8f7f9d4380d62fd5b955065c3
 PYTHON_REQUESTS_SETUP_TYPE = setuptools
 PYTHON_REQUESTS_LICENSE = Apache-2.0
 PYTHON_REQUESTS_LICENSE_FILES = LICENSE
