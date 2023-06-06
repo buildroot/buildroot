457cc3325859afad28f761dc30281e916157a536
Neal Frager <neal.frager@amd.com>
2023-06-06 18:54:59 +0100

    board/zynqmp: fix shellcheck issues
    This patch cleans up board/zynqmp shellcheck issues.
    
    Signed-off-by: Neal Frager <neal.frager@amd.com>
    [Peter: wrap long lines, use quotes around entire word]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 48c3d9f396dd7a9fa426e3f7ddbff65a6270aeaf)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- board/zynqmp/post-image.sh
+++ board/zynqmp/post-image.sh
@@ -6,10 +6,10 @@
 
 FIRST_DT=$(sed -nr \
                -e 's|^BR2_LINUX_KERNEL_INTREE_DTS_NAME="(xilinx/)?([-_/[:alnum:]\\.]*).*"$|\2|p' \
-               ${BR2_CONFIG})
+               "${BR2_CONFIG}")
 
-[ -z "${FIRST_DT}" ] || ln -fs ${FIRST_DT}.dtb ${BINARIES_DIR}/system.dtb
+[ -z "${FIRST_DT}" ] || ln -fs "${FIRST_DT}.dtb" "${BINARIES_DIR}/system.dtb"
 
-BOARD_DIR="$(dirname $0)"
+BOARD_DIR="$(dirname "$0")"
 
-support/scripts/genimage.sh -c $BOARD_DIR/genimage.cfg
+support/scripts/genimage.sh -c "${BOARD_DIR}/genimage.cfg"
