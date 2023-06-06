48c3d9f396dd7a9fa426e3f7ddbff65a6270aeaf
Neal Frager <neal.frager@amd.com>
2023-06-06 18:54:59 +0100

    board/zynqmp: fix shellcheck issues
    This patch cleans up board/zynqmp shellcheck issues.
    
    Signed-off-by: Neal Frager <neal.frager@amd.com>
    [Peter: wrap long lines, use quotes around entire word]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- board/zynqmp/kria/kv260/kv260.sh
+++ board/zynqmp/kria/kv260/kv260.sh
@@ -6,7 +6,11 @@
 # drivers are missing.
 # This will be removed when u-boot can build the kv260 u-boot.itb natively.
 
-UBOOT_DIR=$4
+UBOOT_DIR="$4"
 
-fdtoverlay -o ${UBOOT_DIR}/fit-dtb.blob -i ${UBOOT_DIR}/arch/arm/dts/zynqmp-smk-k26-revA.dtb ${UBOOT_DIR}/arch/arm/dts/zynqmp-sck-kv-g-revB.dtbo
-${UBOOT_DIR}/tools/mkimage -E -f ${UBOOT_DIR}/u-boot.its -B 0x8 ${BINARIES_DIR}/u-boot.itb
+fdtoverlay -o "${UBOOT_DIR}/fit-dtb.blob" \
+	   -i "${UBOOT_DIR}/arch/arm/dts/zynqmp-smk-k26-revA.dtb" \
+	   "${UBOOT_DIR}/arch/arm/dts/zynqmp-sck-kv-g-revB.dtbo"
+
+"${UBOOT_DIR}/tools/mkimage" -E -f "${UBOOT_DIR}/u-boot.its" \
+			     -B 0x8 "${BINARIES_DIR}/u-boot.itb"
