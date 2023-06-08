109f22283c3048d425fb4f0443c2191aabec2079
Romain Naour <romain.naour@gmail.com>
2023-06-09 01:28:59 +0200

    support/testing: TestPythonPy3Txtorcon: use python base configuration
    The base configuration now provide a glibc toolchain,
    we can safely switch to it.
    
    Signed-off-by: Romain Naour <romain.naour@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- support/testing/tests/package/test_python_txtorcon.py
+++ support/testing/tests/package/test_python_txtorcon.py
@@ -3,14 +3,8 @@ from tests.package.test_python import TestPythonPackageBase
 
 class TestPythonPy3Txtorcon(TestPythonPackageBase):
     __test__ = True
-    # Need to use a different toolchain than the default due to
-    # python-cryptography using Rust (not available with uclibc)
-    config = \
+    config = TestPythonPackageBase.config + \
         """
-        BR2_arm=y
-        BR2_TOOLCHAIN_EXTERNAL=y
-        BR2_TOOLCHAIN_EXTERNAL_BOOTLIN=y
-        BR2_TOOLCHAIN_EXTERNAL_BOOTLIN_ARMV5_EABI_GLIBC_STABLE=y
         BR2_PACKAGE_PYTHON3=y
         BR2_PACKAGE_PYTHON_TXTORCON=y
         BR2_TARGET_ROOTFS_CPIO=y
