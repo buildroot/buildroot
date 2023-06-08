af9a5bd7bc98200f3f4ca64692f93b10e6cfb05b
Bernd Kuhls <bernd.kuhls@t-online.de>
2023-06-08 22:48:17 +0200

    package/php: security bump version to 8.2.7
    Changelog: https://www.php.net/ChangeLog-8.php#8.2.7
    Release notes: https://www.php.net/releases/8_2_7.php
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/php/php.mk
+++ package/php/php.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PHP_VERSION = 8.2.6
+PHP_VERSION = 8.2.7
 PHP_SITE = https://www.php.net/distributions
 PHP_SOURCE = php-$(PHP_VERSION).tar.xz
 PHP_INSTALL_STAGING = YES
