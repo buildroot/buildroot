c136b777dfd8cff836a6483d9b7b6131c746acd3
Bernd Kuhls <bernd.kuhls@t-online.de>
2023-06-10 10:46:37 +0200

    package/less: bump version to 633
    Removed patch which is included in this version.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/less/less.mk
+++ package/less/less.mk
@@ -4,15 +4,12 @@
 #
 ################################################################################
 
-LESS_VERSION = 608
+LESS_VERSION = 633
 LESS_SITE = http://www.greenwoodsoftware.com/less
 LESS_LICENSE = GPL-3.0+
 LESS_LICENSE_FILES = COPYING
 LESS_CPE_ID_VENDOR = gnu
 LESS_DEPENDENCIES = ncurses
-
-# 0001-End-OSC8-hyperlink-on-invalid-embedded-escape-sequence.patch
-LESS_IGNORE_CVES += CVE-2022-46663
 
 define LESS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/less $(TARGET_DIR)/usr/bin/less
