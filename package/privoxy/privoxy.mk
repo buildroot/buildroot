b882f5679e66214e1fb54350bc75150815867359
Bernd Kuhls <bernd.kuhls@t-online.de>
2023-06-10 19:25:53 +0200

    package/privoxy: bump to version 3.0.34
    Removed md5 hash, switched _SITE to https.
    
    Release notes: https://www.privoxy.org/announce.txt
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/privoxy/privoxy.mk
+++ package/privoxy/privoxy.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PRIVOXY_VERSION = 3.0.33
-PRIVOXY_SITE = http://downloads.sourceforge.net/project/ijbswa/Sources/$(PRIVOXY_VERSION)%20%28stable%29
+PRIVOXY_VERSION = 3.0.34
+PRIVOXY_SITE = https://downloads.sourceforge.net/project/ijbswa/Sources/$(PRIVOXY_VERSION)%20%28stable%29
 PRIVOXY_SOURCE = privoxy-$(PRIVOXY_VERSION)-stable-src.tar.gz
 # configure not shipped
 PRIVOXY_AUTORECONF = YES
