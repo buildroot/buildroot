cf0cf882ee76c13356f818e1c2ae19b9de3d1ffb
Bernd Kuhls <bernd.kuhls@t-online.de>
2023-06-10 10:35:28 +0200

    package/lcms2: bump version to 2.15
    Release notes: https://littlecms.com/blog/2023/03/01/lcms2-2.15/
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/lcms2/lcms2.mk
+++ package/lcms2/lcms2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LCMS2_VERSION = 2.14
+LCMS2_VERSION = 2.15
 LCMS2_SITE = http://downloads.sourceforge.net/project/lcms/lcms/$(LCMS2_VERSION)
 LCMS2_LICENSE = MIT
 LCMS2_LICENSE_FILES = COPYING
