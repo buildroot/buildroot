f8af24707b196eb64121d29a829fc31b5506409c
Bernd Kuhls <bernd.kuhls@t-online.de>
2023-06-10 18:44:12 +0200

    package/mpg123: bump version to 1.31.3
    Removed md5 hash, switched _SITE to https.
    
    Updated license hash due to copyright year bump.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/mpg123/mpg123.mk
+++ package/mpg123/mpg123.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-MPG123_VERSION = 1.25.15
+MPG123_VERSION = 1.31.3
 MPG123_SOURCE = mpg123-$(MPG123_VERSION).tar.bz2
-MPG123_SITE = http://downloads.sourceforge.net/project/mpg123/mpg123/$(MPG123_VERSION)
+MPG123_SITE = https://downloads.sourceforge.net/project/mpg123/mpg123/$(MPG123_VERSION)
 MPG123_INSTALL_STAGING = YES
 MPG123_LICENSE = LGPL-2.1
 MPG123_LICENSE_FILES = COPYING
