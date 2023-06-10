170c218e4403944eae58bb80cc5cf1d198883a7c
Julien Olivain <ju.o@free.fr>
2023-06-10 18:58:12 +0200

    package/perftest: bump to version 23.04.0-0.23
    For release note, see:
    https://github.com/linux-rdma/perftest/releases/tag/23.04.0-0.23
    
    Note: the version scheme changed since the previous version, from
    "v4.5-0.20" to "23.04.0-0.23" (the "v" was removed).
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/perftest/perftest.mk
+++ package/perftest/perftest.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PERFTEST_VERSION = 4.5-0.20
-PERFTEST_SITE = $(call github,linux-rdma,perftest,v$(PERFTEST_VERSION))
+PERFTEST_VERSION = 23.04.0-0.23
+PERFTEST_SITE = $(call github,linux-rdma,perftest,$(PERFTEST_VERSION))
 PERFTEST_LICENSE = GPL-2.0 or BSD-2-Clause
 PERFTEST_LICENSE_FILES = COPYING
 PERFTEST_DEPENDENCIES = pciutils rdma-core
