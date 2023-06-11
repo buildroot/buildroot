84da7d929113881aad6a4f57ba1f21c54554b3e7
Bernd Kuhls <bernd.kuhls@t-online.de>
2023-06-11 14:31:07 +0200

    package/python-pyicu: bump version to 2.11
    Updated help text to follow upstream move to gitlab.
    
    This version bump introduces compatibility with icu 73-1:
    https://gitlab.pyicu.org/main/pyicu/-/commit/1ba50901161acfce691d41308be6c004a3956f4e
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-pyicu/python-pyicu.mk
+++ package/python-pyicu/python-pyicu.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYICU_VERSION = 2.10.2
+PYTHON_PYICU_VERSION = 2.11
 PYTHON_PYICU_SOURCE = PyICU-$(PYTHON_PYICU_VERSION).tar.gz
-PYTHON_PYICU_SITE = https://files.pythonhosted.org/packages/64/00/a531e119a97e54601f616f5061879ec2d4bb058d225014f9acf94b2970c3
+PYTHON_PYICU_SITE = https://files.pythonhosted.org/packages/03/1b/800fce0236be0b8a99b3ccbb797786dd178028960b3fd65544e2d8bad5ac
 PYTHON_PYICU_LICENSE = MIT
 PYTHON_PYICU_LICENSE_FILES = LICENSE
 PYTHON_PYICU_DEPENDENCIES = icu
