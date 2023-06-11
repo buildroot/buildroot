db45cc593e66598b4d345dd348b0bfe2b388b557
Bernd Kuhls <bernd.kuhls@t-online.de>
2023-06-11 16:56:05 +0200

    package/x11r7/xdriver_xf86-video-dummy: bump version to 0.4.1
    Release notes:
    https://lists.x.org/archives/xorg-announce/2023-May/003394.html
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/x11r7/xdriver_xf86-video-dummy/xdriver_xf86-video-dummy.mk
+++ package/x11r7/xdriver_xf86-video-dummy/xdriver_xf86-video-dummy.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_DUMMY_VERSION = 0.4.0
+XDRIVER_XF86_VIDEO_DUMMY_VERSION = 0.4.1
 XDRIVER_XF86_VIDEO_DUMMY_SOURCE = xf86-video-dummy-$(XDRIVER_XF86_VIDEO_DUMMY_VERSION).tar.xz
 XDRIVER_XF86_VIDEO_DUMMY_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_VIDEO_DUMMY_LICENSE = MIT
