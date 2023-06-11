6f722cf6dbbf23850da7b10e1d94e156ff5419d7
Bernd Kuhls <bernd.kuhls@t-online.de>
2023-06-11 16:56:07 +0200

    package/x11r7/xdriver_xf86-video-i128: bump version to 1.4.1
    Release notes:
    https://lists.x.org/archives/xorg-announce/2023-May/003395.html
    
    Switched tarball to xz and _SITE to https.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/x11r7/xdriver_xf86-video-i128/xdriver_xf86-video-i128.mk
+++ package/x11r7/xdriver_xf86-video-i128/xdriver_xf86-video-i128.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_I128_VERSION = 1.4.0
-XDRIVER_XF86_VIDEO_I128_SOURCE = xf86-video-i128-$(XDRIVER_XF86_VIDEO_I128_VERSION).tar.bz2
-XDRIVER_XF86_VIDEO_I128_SITE = http://xorg.freedesktop.org/releases/individual/driver
+XDRIVER_XF86_VIDEO_I128_VERSION = 1.4.1
+XDRIVER_XF86_VIDEO_I128_SOURCE = xf86-video-i128-$(XDRIVER_XF86_VIDEO_I128_VERSION).tar.xz
+XDRIVER_XF86_VIDEO_I128_SITE = https://xorg.freedesktop.org/archive/individual/driver
 XDRIVER_XF86_VIDEO_I128_LICENSE = MIT
 XDRIVER_XF86_VIDEO_I128_LICENSE_FILES = COPYING
 XDRIVER_XF86_VIDEO_I128_DEPENDENCIES = xserver_xorg-server xorgproto
