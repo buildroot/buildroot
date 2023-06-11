800c7de810f943ad6fd6ee404c92e6fac01c693a
Grzegorz Blach <grzegorz@blach.pl>
2023-06-12 01:36:16 +0200

    package/python-hiredis: bump to version 2.2.3
    The existing patch is removed as it has been applied upstream in
    commit
    https://github.com/redis/hiredis-py/commit/d4d701a06560ce76a4eaba3ac309ff70e37b3190.
    
    The license file has changed, it is now named LICENSE, and the
    contents are not the same even though it still contains the
    BSD-3-Clause license.
    
    Signed-off-by: Grzegorz Blach <grzegorz@blach.pl>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-hiredis/python-hiredis.mk
+++ package/python-hiredis/python-hiredis.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_HIREDIS_VERSION = 2.0.0
+PYTHON_HIREDIS_VERSION = 2.2.3
 PYTHON_HIREDIS_SOURCE = hiredis-$(PYTHON_HIREDIS_VERSION).tar.gz
-PYTHON_HIREDIS_SITE = https://files.pythonhosted.org/packages/0c/39/eae11344d69ba435ec13d6bcc1a9eea3d2278324506fcd0e52d1ed8958c8
+PYTHON_HIREDIS_SITE = https://files.pythonhosted.org/packages/b0/04/dab6792584fc548803ffa50b5bb2b99f01d3ab04d7c7f64e85f1a22fb847
 PYTHON_HIREDIS_SETUP_TYPE = setuptools
 PYTHON_HIREDIS_LICENSE = BSD-3-Clause
-PYTHON_HIREDIS_LICENSE_FILES = COPYING vendor/hiredis/COPYING
+PYTHON_HIREDIS_LICENSE_FILES = LICENSE vendor/hiredis/COPYING
 
 $(eval $(python-package))
