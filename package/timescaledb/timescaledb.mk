067a8864c594958004dfdb9dea55c304913e2be6
Maxim Kochetkov <fido_max@inbox.ru>
2023-06-13 09:31:39 +0300

    package/timescaledb: bump version to 2.11.0
    Release notes: https://github.com/timescale/timescaledb/blob/2.11.0/CHANGELOG.md
    
    Signed-off-by: Maxim Kochetkov <fido_max@inbox.ru>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/timescaledb/timescaledb.mk
+++ package/timescaledb/timescaledb.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TIMESCALEDB_VERSION = 2.10.3
+TIMESCALEDB_VERSION = 2.11.0
 TIMESCALEDB_SITE = $(call github,timescale,timescaledb,$(TIMESCALEDB_VERSION))
 TIMESCALEDB_LICENSE = Apache-2.0
 TIMESCALEDB_LICENSE_FILES = LICENSE
