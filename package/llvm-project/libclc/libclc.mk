5fad34d0d1a2285233ca6cb087e0509d98379a50
Lang Daniel <d.lang@abatec.at>
2023-06-15 15:43:10 +0200

    package/libclc: move to llvm-project subfolder
    To make version bumping easier, move libclc to a new llvm-project subfolder
    and specify site for the project. Since the version of libclc is more
    recent than the version specified in LLVM_PROJECT_VERSION, don't change
    it for now until a later update.
    
    Signed-off-by: Daniel Lang <d.lang@abatec.at>
    Signed-off-by: Romain Naour <romain.naour@smile.fr>
    Signed-off-by: Sebastian Weyer <sebastian.weyer@smile.fr>
    Tested-by: Adam Duskett <adam.duskett@amarulasolutions.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/llvm-project/libclc/libclc.mk
+++ package/llvm-project/libclc/libclc.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LIBCLC_VERSION = 13.0.1
-LIBCLC_SITE = https://github.com/llvm/llvm-project/releases/download/llvmorg-$(LIBCLC_VERSION)
+LIBCLC_SITE = $(LLVM_PROJECT_SITE)
 LIBCLC_SOURCE = libclc-$(LIBCLC_VERSION).src.tar.xz
 LIBCLC_LICENSE = Apache-2.0 with exceptions or MIT
 LIBCLC_LICENSE_FILES = LICENSE.TXT
