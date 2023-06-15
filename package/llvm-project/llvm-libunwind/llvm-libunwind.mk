b4c8785ca22dbff0fbcfc373e1cbe2cd6a9b629e
Lang Daniel <d.lang@abatec.at>
2023-06-15 15:43:12 +0200

    package/llvm-project/llvm-libunwind: new package
    Starting with version 15, lld needs the llvm implementation
    of libunwind, therefore it is added as a host package.
    
    Signed-off-by: Daniel Lang <d.lang@abatec.at>
    Signed-off-by: Romain Naour <romain.naour@smile.fr>
    Signed-off-by: Sebastian Weyer <sebastian.weyer@smile.fr>
    Tested-by: Adam Duskett <adam.duskett@amarulasolutions.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/llvm-project/llvm-libunwind/llvm-libunwind.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# llvm-libunwind
+#
+################################################################################
+
+LLVM_LIBUNWIND_VERSION = 15.0.3
+LLVM_LIBUNWIND_SITE = $(LLVM_PROJECT_SITE)
+LLVM_LIBUNWIND_SOURCE = libunwind-$(LLVM_LIBUNWIND_VERSION).src.tar.xz
+LLVM_LIBUNWIND_LICENSE = Apache-2.0 with exceptions
+LLVM_LIBUNWIND_LICENSE_FILES = LICENSE.TXT
+LLVM_LIBUNWIND_SUPPORTS_IN_SOURCE_BUILD = NO
+
+HOST_LLVM_LIBUNWIND_CONF_OPTS += \
+	-DCMAKE_MODULE_PATH="$(HOST_DIR)/lib/cmake/llvm" \
+	-DLIBUNWIND_INSTALL_HEADERS=ON
+
+$(eval $(host-cmake-package))
