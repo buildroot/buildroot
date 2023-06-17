cf134e6b13be0127ed80e1ac61cba914f0a6a7f7
Romain Naour <romain.naour@gmail.com>
2023-06-17 17:47:43 +0200

    toolchain/toolchain-external/toolchain-external-arm-arm: bump to version 12.2-rel1
    Updated to gcc 12.2, gdb 12, binutils 2.39, glibc 2.36.
    
    Upstream changed version scheme previously based on the release
    date (2021.07) to a version scheme based on gcc version (12.2.rel1).
    
    The runtime issue (Illegal instruction on some x86 host) [1] seems to
    be fixed since it can't be reproduced anymore on the same host.
    
    The x86_64 host variant prebuilt toolchain is built on RHEL7
    (glibc 2.17) and is likely also be useable on OS versions like
    RHEL8, Ubuntu 18.04 or later.
    
    The AArch64 host variant prebuilt toolchain is built on Ubuntu 18.04
    (glibc 2.27) is likely also be useable on OS versions like RHEL8,
    Ubuntu 18.04 or later.
    
    Use the sha256 hash that is now provided by upsteam.
    
    Tested with qemu_arm_vexpress_defconfig.
    
    [1] https://bugs.linaro.org/show_bug.cgi?id=5825#c19
    [2] https://developer.arm.com/downloads/-/arm-gnu-toolchain-downloads
    
    Signed-off-by: Romain Naour <romain.naour@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- toolchain/toolchain-external/toolchain-external-arm-arm/toolchain-external-arm-arm.mk
+++ toolchain/toolchain-external/toolchain-external-arm-arm/toolchain-external-arm-arm.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION = 2021.07
-TOOLCHAIN_EXTERNAL_ARM_ARM_SITE = https://developer.arm.com/-/media/Files/downloads/gnu-a/10.3-$(TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION)/binrel
+TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION = 12.2.rel1
+TOOLCHAIN_EXTERNAL_ARM_ARM_SITE = https://developer.arm.com/-/media/Files/downloads/gnu/$(TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION)/binrel
 
-TOOLCHAIN_EXTERNAL_ARM_ARM_SOURCE = gcc-arm-10.3-$(TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION)-$(HOSTARCH)-arm-none-linux-gnueabihf.tar.xz
+TOOLCHAIN_EXTERNAL_ARM_ARM_SOURCE = arm-gnu-toolchain-$(TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION)-$(HOSTARCH)-arm-none-linux-gnueabihf.tar.xz
 
 $(eval $(toolchain-external-package))
