dd194540a1b8e9f70a01c6bf9c574863168e6778
Sergey Bobrenok <sibobrenok@sberdevices.ru>
2023-06-18 14:14:34 +0300

    package/crudini: add host variant dependency from python-iniparse
    Signed-off-by: Sergey Bobrenok <SIBobrenok@sberdevices.ru>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/crudini/crudini.mk
+++ package/crudini/crudini.mk
@@ -9,6 +9,9 @@ CRUDINI_SITE = $(call github,pixelb,crudini,$(CRUDINI_VERSION))
 CRUDINI_SETUP_TYPE = setuptools
 CRUDINI_LICENSE = GPL-2.0
 CRUDINI_LICENSE_FILES = COPYING
+# This is a runtime dependency, but we don't have the concept of
+# runtime dependencies for host packages.
+HOST_CRUDINI_DEPENDENCIES = host-python-iniparse
 
 $(eval $(python-package))
 $(eval $(host-python-package))
