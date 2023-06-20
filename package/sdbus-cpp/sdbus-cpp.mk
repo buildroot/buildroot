6fa4131de5a5a5b156eb110d5f40ac602f6c1d87
Charles Hardin <ckhardin@gmail.com>
2023-06-20 10:43:21 -0700

    package/sdbus-cpp: add support to build the host stub generator
    Add a host option to build the xml2cpp-codegen part of the
    sdbus-cpp package for use in creating adaptor and proxy
    implementations from the D-Bus IDL.
    
    Signed-off-by: Charles Hardin <ckhardin@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/sdbus-cpp/sdbus-cpp.mk
+++ package/sdbus-cpp/sdbus-cpp.mk
@@ -12,4 +12,13 @@ SDBUS_CPP_DEPENDENCIES = host-pkgconf systemd
 SDBUS_CPP_LICENSE = LGPL-2.1+ with exception (headers)
 SDBUS_CPP_LICENSE_FILES = COPYING COPYING-LGPL-Exception
 
+# Host build for sdbus-c++-xml2cpp
+HOST_SDBUS_CPP_DEPENDENCIES = host-pkgconf host-systemd
+HOST_SDBUS_CPP_CONF_OPTS = \
+	-DBUILD_CODE_GEN=ON \
+	-DBUILD_DOC=OFF \
+	-DBUILD_TESTS=OFF \
+	-DBUILD_LIBSYSTEMD=OFF
+
 $(eval $(cmake-package))
+$(eval $(host-cmake-package))
