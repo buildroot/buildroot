c4eb3544a0fb8023678f35556a9a359d806cac4a
Peter Seiderer <ps.report@gmx.net>
2023-06-22 18:55:22 +0200

    package/ubus: bump version to f787c97
    Changelog:
    
      - 66baa44b8dee libubus: introduce new status messages
      - b3cd5ab4c010 cli: use UBUS_STATUS_PARSE_ERROR
      - 584f56a23314 cli: improve error logging for call command
      - 2f793a4eb063 lua: add optional path filter to objects() method
      - 2bebf93cd334 ubusd: handle invoke on event object without data
      - 9913aa61de73 ubusd: add lookup command queuing support
      - f787c97b3489 libubus: add missing uloop_fd_delete call in ubus_shutdown
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/ubus/ubus.mk
+++ package/ubus/ubus.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UBUS_VERSION = a72457b61df045d3c499a6211362b751710590d7
+UBUS_VERSION = f787c97b34894a38b15599886cacbca01271684f
 UBUS_SITE = https://git.openwrt.org/project/ubus.git
 UBUS_SITE_METHOD = git
 
