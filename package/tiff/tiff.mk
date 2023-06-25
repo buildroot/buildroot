a65eb8c871ad7bd17ead79035bccb97838f6d0be
Peter Korsgaard <peter@korsgaard.com>
2023-06-25 22:37:35 +0200

    package/tiff: security bump to version 4.5.1
    Fixes the following security issues:
    
    - CVE-2023-1916: A flaw was found in tiffcrop, a program distributed by the
      libtiff package.  A specially crafted tiff file can lead to an
      out-of-bounds read in the extractImageSection function in
      tools/tiffcrop.c, resulting in a denial of service and limited information
      disclosure.  This issue affects libtiff versions 4.x.
    
    - CVE-2023-25434: libtiff 4.5.0 is vulnerable to Buffer Overflow via
      extractContigSamplesBytes() at /libtiff/tools/tiffcrop.c:3215.
    
    - CVE-2023-26965: loadImage() in tools/tiffcrop.c in LibTIFF through 4.5.0
      has a heap-based use after free via a crafted TIFF image
    
    Drop the now upstream
    0001-tiffcrop-Correct-simple-copy-paste-error-Fix-488.patch.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit cb496970c0c7117f9f00d8495e234871f2bae4c1)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/tiff/tiff.mk
+++ package/tiff/tiff.mk
@@ -4,16 +4,13 @@
 #
 ################################################################################
 
-TIFF_VERSION = 4.5.0
+TIFF_VERSION = 4.5.1
 TIFF_SITE = http://download.osgeo.org/libtiff
 TIFF_LICENSE = tiff license
 TIFF_LICENSE_FILES = LICENSE.md
 TIFF_CPE_ID_VENDOR = libtiff
 TIFF_CPE_ID_PRODUCT = libtiff
 TIFF_INSTALL_STAGING = YES
-
-# 0001-tiffcrop-Correct-simple-copy-paste-error-Fix-488.patch
-TIFF_IGNORE_CVES += CVE-2022-48281
 
 # webp has a (optional) dependency on tiff, so we can't have webp
 # support in tiff, or that would create a circular dependency.
