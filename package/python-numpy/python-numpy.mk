ca63464e3777ca2596b4d396accb6a384ccf8514
James Hilliard <james.hilliard1@gmail.com>
2023-06-26 12:15:27 -0600

    package/python-numpy: bump to version 1.25.0
    Migrate to meson package infrastructure.
    
    License has changed due to date update:
    https://github.com/numpy/numpy/commit/c1ffdbc0c29d48ece717acb5bfbf811c935b41f6
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-numpy/python-numpy.mk
+++ package/python-numpy/python-numpy.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_NUMPY_VERSION = 1.23.5
+PYTHON_NUMPY_VERSION = 1.25.0
 PYTHON_NUMPY_SOURCE = numpy-$(PYTHON_NUMPY_VERSION).tar.gz
 PYTHON_NUMPY_SITE = https://github.com/numpy/numpy/releases/download/v$(PYTHON_NUMPY_VERSION)
 PYTHON_NUMPY_LICENSE = BSD-3-Clause, MIT, Zlib
@@ -15,29 +15,26 @@ PYTHON_NUMPY_LICENSE_FILES = \
 	numpy/linalg/lapack_lite/LICENSE.txt \
 	tools/npy_tempita/license.txt
 
-PYTHON_NUMPY_SETUP_TYPE = setuptools
-PYTHON_NUMPY_DEPENDENCIES = host-python-cython
+PYTHON_NUMPY_DEPENDENCIES = host-python-cython python3
 HOST_PYTHON_NUMPY_DEPENDENCIES = host-python-cython
+
+PYTHON_NUMPY_CONF_ENV += \
+	_PYTHON_SYSCONFIGDATA_NAME=$(PKG_PYTHON_SYSCONFIGDATA_NAME) \
+	PYTHONPATH=$(PYTHON3_PATH)
 
 ifeq ($(BR2_PACKAGE_LAPACK),y)
 PYTHON_NUMPY_DEPENDENCIES += lapack
+PYTHON_NUMPY_CONF_OPTS += -Dlapack=lapack
 else
-PYTHON_NUMPY_ENV += BLAS=None LAPACK=None
+PYTHON_NUMPY_CONF_OPTS += -Dlapack=""
 endif
 
-ifeq ($(BR2_TOOLCHAIN_HAS_FORTRAN),y)
-PYTHON_NUMPY_BUILD_OPTS = --fcompiler=gnu95
-PYTHON_NUMPY_ENV += F90=$(TARGET_FC)
+ifeq ($(BR2_PACKAGE_OPENBLAS),y)
+PYTHON_NUMPY_DEPENDENCIES += openblas
+PYTHON_NUMPY_CONF_OPTS += -Dblas=openblas
 else
-PYTHON_NUMPY_BUILD_OPTS = --fcompiler=None
+PYTHON_NUMPY_CONF_OPTS += -Dblas=""
 endif
-
-define PYTHON_NUMPY_CONFIGURE_CMDS
-	-rm -f $(@D)/site.cfg
-	echo "[DEFAULT]" >> $(@D)/site.cfg
-	echo "library_dirs = $(STAGING_DIR)/usr/lib" >> $(@D)/site.cfg
-	echo "include_dirs = $(STAGING_DIR)/usr/include" >> $(@D)/site.cfg
-endef
 
 # Fixup the npymath.ini prefix path with actual target staging area where
 # numpy core was built. Without this, target builds using numpy distutils
@@ -55,5 +52,5 @@ PYTHON_NUMPY_POST_INSTALL_STAGING_HOOKS += PYTHON_NUMPY_FIXUP_NPY_PKG_CONFIG_FIL
 # in the staging area.
 PYTHON_NUMPY_INSTALL_STAGING = YES
 
-$(eval $(python-package))
-$(eval $(host-python-package))
+$(eval $(meson-package))
+$(eval $(host-meson-package))
