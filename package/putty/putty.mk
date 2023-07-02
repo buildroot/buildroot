896de01e0e84aa2006fa453b9e1f583a44667e81
Bernd Kuhls <bernd@kuhls.net>
2023-07-02 13:04:47 +0200

    package/putty: bump version to 0.78
    Changelog:
    https://www.chiark.greenend.org.uk/~sgtatham/putty/changes.html
    
    Removed patch, the patched file does not exist anymore in this release.
    
    Updated license hash due to copyright year bump:
    https://git.tartarus.org/?p=simon/putty.git;a=commitdiff;h=ce59d8bb4f7a1c6aff677d1431ec84a803a24091
    
    Switched build system to cmake.
    
    Added optional dependency to libgtk3.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 5673ea3ce4d4dd721bb17fc8d5b1283f5c1080b4)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/putty/putty.mk
+++ package/putty/putty.mk
@@ -4,18 +4,17 @@
 #
 ################################################################################
 
-PUTTY_VERSION = 0.76
+PUTTY_VERSION = 0.78
 PUTTY_SITE = http://the.earth.li/~sgtatham/putty/$(PUTTY_VERSION)
 PUTTY_LICENSE = MIT
 PUTTY_LICENSE_FILES = LICENCE
 PUTTY_CPE_ID_VENDOR = putty
-PUTTY_CONF_OPTS = --disable-gtktest
+PUTTY_DEPENDENCIES = host-pkgconf
 
-ifeq ($(BR2_PACKAGE_LIBGTK2),y)
-PUTTY_CONF_OPTS += --with-gtk=2
+ifeq ($(BR2_PACKAGE_LIBGTK3),y)
+PUTTY_DEPENDENCIES += libgtk3
+else ifeq ($(BR2_PACKAGE_LIBGTK2),y)
 PUTTY_DEPENDENCIES += libgtk2
-else
-PUTTY_CONF_OPTS += --without-gtk
 endif
 
-$(eval $(autotools-package))
+$(eval $(cmake-package))
