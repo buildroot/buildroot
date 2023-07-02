2bc29d4eecb2aab67564cddf3afe3b297d51045c
Bernd Kuhls <bernd@kuhls.net>
2023-07-02 18:40:25 +0200

    package/bluez-alsa: bump version to 4.1.1
    Changelog: https://github.com/arkq/bluez-alsa/blob/master/NEWS
    
    Updated license hash due to copyright year bump:
    https://github.com/arkq/bluez-alsa/commit/165e15bce4f9c8dd6238ee8d105ab9fa857c63a1
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/bluez-alsa/bluez-alsa.mk
+++ package/bluez-alsa/bluez-alsa.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BLUEZ_ALSA_VERSION = 4.0.0
+BLUEZ_ALSA_VERSION = 4.1.1
 BLUEZ_ALSA_SITE = $(call github,Arkq,bluez-alsa,v$(BLUEZ_ALSA_VERSION))
 BLUEZ_ALSA_LICENSE = MIT
 BLUEZ_ALSA_LICENSE_FILES = LICENSE
