dc4d70ff1f828b92219f0915e6a296f3ab60edb6
Bernd Kuhls <bernd@kuhls.net>
2023-07-04 19:03:04 +0200

    package/libva-utils: bump version to 2.19.0
    Release notes:
    https://github.com/intel/libva-utils/blob/v2.19-branch/NEWS
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libva-utils/libva-utils.mk
+++ package/libva-utils/libva-utils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBVA_UTILS_VERSION = 2.18.2
+LIBVA_UTILS_VERSION = 2.19.0
 LIBVA_UTILS_SITE = $(call github,intel,libva-utils,$(LIBVA_UTILS_VERSION))
 LIBVA_UTILS_LICENSE = MIT
 LIBVA_UTILS_LICENSE_FILES = COPYING
