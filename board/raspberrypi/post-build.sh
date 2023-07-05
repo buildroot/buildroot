a5c2bafb4ffcdb8df389ef1282a7cb69726f7279
Peter Korsgaard <peter@korsgaard.com>
2023-07-05 18:46:14 +0200

    board/raspberrypi: handle dtb overlays for all variants
    Most rpi defconfigs use dtb overlays, but not rpi0 / rpi2 - Making it harder
    to use overlays on those boards as the genimage files have to be tweaked.
    
    To fix this, create the rpi-firmware/overlays directory in the post-build
    script if needed and unconditionally include it in the genimage files so
    rpi0/rpi2 works consistently with the other variants.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 6021f3678b44d43da7057e3135da842cde1ffa05)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -9,3 +9,6 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
+
+# exnsure overlays exists for genimage
+mkdir -p "${BINARIES_DIR}/rpi-firmware/overlays"
