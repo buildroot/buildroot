e37ee5acdcebb59c0bcc89eca69540cea2ed441d
Peter Korsgaard <peter@korsgaard.com>
2023-07-05 18:46:15 +0200

    board/raspberrypi/post-image.sh: generate genimage config from template if not present
    The rpi genimage configurations are all identical, except for the boot
    partition files, which include:
    
    - Device tree files (*.dtb)
    - rpi-firmware files (rpi-firmware/*)
    - Kernel image (Image/zImage)
    
    All of these are quite simple to figure out programmatically based on the
    content of BINARIES_DIR, so extend post-image.sh to fall back to generating
    a genimage configuration based on genimage.cfg.in if a board specific one
    does not exist.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- board/raspberrypi/post-image.sh
+++ board/raspberrypi/post-image.sh
@@ -7,6 +7,23 @@ BOARD_NAME="$(basename ${BOARD_DIR})"
 GENIMAGE_CFG="${BOARD_DIR}/genimage-${BOARD_NAME}.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
+# generate genimage from template if a board specific variant doesn't exists
+if [ ! -e "${GENIMAGE_CFG}" ]; then
+	GENIMAGE_CFG="${BINARIES_DIR}/genimage.cfg"
+	FILES=()
+
+	for i in "${BINARIES_DIR}"/*.dtb "${BINARIES_DIR}"/rpi-firmware/*; do
+		FILES+=( "${i#${BINARIES_DIR}/}" )
+	done
+
+	KERNEL=$(sed -n 's/^kernel=//p' "${BINARIES_DIR}/rpi-firmware/config.txt")
+	FILES+=( "${KERNEL}" )
+
+	BOOT_FILES=$(printf '\\t\\t\\t"%s",\\n' "${FILES[@]}")
+	sed "s|#BOOT_FILES#|${BOOT_FILES}|" "${BOARD_DIR}/genimage.cfg.in" \
+		> "${GENIMAGE_CFG}"
+fi
+
 # Pass an empty rootpath. genimage makes a full copy of the given rootpath to
 # ${GENIMAGE_TMP}/root so passing TARGET_DIR would be a waste of time and disk
 # space. We don't rely on genimage to build the rootfs image, just to insert a
