5bf4f3e5169c0eaed0593edd4f5bf2495c3ed5f6
Bernd Kuhls <bernd@kuhls.net>
2023-07-08 17:03:22 +0200

    package/igh-ethercat: bump to latest Git commit
    Removed_POST_PATCH_HOOKS, not needed anymore due to upstream commit
    https://gitlab.com/etherlab.org/ethercat/-/commit/3bc8703f5cf021d21730cd3ac3da839973b914e9
    
    This bump will only fix kernel 6.3-related build errors by upstream
    commit
    https://gitlab.com/etherlab.org/ethercat/-/commit/e35f35b25c43b9f6fa967b2ae92462034a14bc8d
    
    Fixes:
    http://autobuild.buildroot.net/results/ffc/ffc00cc9f949bfc692edc287867ca1e3c86edea1/
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 18a6d6db685e939cb42cb66755668e8f91bc6340)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/igh-ethercat/igh-ethercat.mk
+++ package/igh-ethercat/igh-ethercat.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-IGH_ETHERCAT_VERSION = 2e2cef6131895336f87c57c18fe78ae01a90d3de
+IGH_ETHERCAT_VERSION = f5dc109176400f540a8682a2c9ee20d411d14d61
 IGH_ETHERCAT_SITE = $(call gitlab,etherlab.org,ethercat,$(IGH_ETHERCAT_VERSION))
 IGH_ETHERCAT_LICENSE = GPL-2.0 (IgH EtherCAT master), LGPL-2.1 (libraries)
 IGH_ETHERCAT_LICENSE_FILES = COPYING COPYING.LESSER
@@ -21,12 +21,6 @@ IGH_ETHERCAT_CONF_OPTS = \
 	$(if $(BR2_PACKAGE_IGH_ETHERCAT_E1000),--enable-e1000,--disable-e1000) \
 	$(if $(BR2_PACKAGE_IGH_ETHERCAT_E1000E),--enable-e1000e,--disable-e1000e) \
 	$(if $(BR2_PACKAGE_IGH_ETHERCAT_R8169),--enable-r8169,--disable-r8169)
-
-# Needed to make autoreconf happy
-define IGH_ETHERCAT_CREATE_CHANGELOG
-	touch $(@D)/ChangeLog
-endef
-IGH_ETHERCAT_POST_PATCH_HOOKS += IGH_ETHERCAT_CREATE_CHANGELOG
 
 $(eval $(kernel-module))
 $(eval $(autotools-package))
