3304de4e818d407429bc01422adc928cc9123040
Sébastien Szymanski <sebastien.szymanski@armadeus.com>
2023-07-10 19:45:42 +0200

    package/yavta: update _SITE URL
    It looks like the project moved to https://git.ideasonboard.org/yavta.git.
    
    Update YAVA_SITE URL accordingly.
    
    Signed-off-by: Sébastien Szymanski <sebastien.szymanski@armadeus.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit b17ed57763c4809f4541b4abe6a7a0e2349e6f90)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/yavta/yavta.mk
+++ package/yavta/yavta.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 YAVTA_VERSION = 583bedefc2a247d2cfd32d1b4a0abbe3e2015c70
-YAVTA_SITE = https://git.ideasonboard.org/git/yavta.git
+YAVTA_SITE = https://git.ideasonboard.org/yavta.git
 YAVTA_SITE_METHOD = git
 YAVTA_LICENSE = GPL-2.0+
 YAVTA_LICENSE_FILES = COPYING.GPL
