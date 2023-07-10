3782ce82913ab50ce250f56590e22c7b5cd5c3f5
Giulio Benetti <giulio.benetti@benettiengineering.com>
2023-07-10 23:15:19 +0200

    package/mali-driver: bump version to 2023-07-10
    This version fixes build failure with Linux 6.2 and 6.3
    
    Fixes:
    http://autobuild.buildroot.net/results/0c043a6978fc5ea51b2c7a862de288223589926e/
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit f738d70bda9d9f57aa511c539bfc9ab1d9b43125)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/mali-driver/mali-driver.mk
+++ package/mali-driver/mali-driver.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MALI_DRIVER_VERSION = c90627f78d58567a2acb7cbf77d565e03a131294
+MALI_DRIVER_VERSION = 9baa7331c8e5ba6075e3fc42c530ae7f4c320cad
 MALI_DRIVER_SITE = $(call github,bootlin,mali-driver,$(MALI_DRIVER_VERSION))
 MALI_DRIVER_DEPENDENCIES = linux
 MALI_DRIVER_LICENSE = GPL-2.0
