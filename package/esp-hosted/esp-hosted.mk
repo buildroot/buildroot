7b2e5e6550aed4f79461919c6a59e00741eb4b11
Giulio Benetti <giulio.benetti@benettiengineering.com>
2023-07-11 23:55:02 +0200

    package/esp-hosted: add package
    This patch adds esp-hosted package that allows to build the Linux
    Kernel Driver for Espressif Esp32-* SoCs, that once programmed with
    the corresponding firmware behave like a normal Wi-Fi module. Both
    SDIO and SPI busses are supported and selectable.
    
    Cc: Jesse Taube <jesse.taube@benettiengineering.com>
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/esp-hosted/esp-hosted.mk
@@ -0,0 +1,29 @@
+################################################################################
+#
+# esp-hosted
+#
+################################################################################
+
+ESP_HOSTED_VERSION = 11e90f185a5a9922343e73e44032816a0e3ddedb
+ESP_HOSTED_SITE = $(call github,espressif,esp-hosted,$(ESP_HOSTED_VERSION))
+ESP_HOSTED_DEPENDENCIES = linux
+ESP_HOSTED_LICENSE = GPL-2.0
+ESP_HOSTED_LICENSE_FILE = LICENSE
+ESP_HOSTED_MODULE_SUBDIRS = esp_hosted_ng/host
+
+define ESP_HOSTED_LINUX_CONFIG_FIXUPS
+	$(call KCONFIG_ENABLE_OPT,CONFIG_NET)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_WIRELESS)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_CFG80211)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_MAC80211)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_BT)
+endef
+
+ifeq ($(BR2_PACKAGE_ESP_HOSTED_SPI),y)
+ESP_HOSTED_MODULE_MAKE_OPTS = target=spi
+else
+ESP_HOSTED_MODULE_MAKE_OPTS = target=sdio
+endif
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
