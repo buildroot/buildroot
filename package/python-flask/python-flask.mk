634e9c1a47bad741479761ed4d81e1f12c0559a8
Peter Seiderer <ps.report@gmx.net>
2023-07-13 09:38:51 +0200

    package/python-flask: bump version to 2.3.2
    - update docs/license.rst hash (removed duplicated BSD-3-Clause and
      reference to LICENSE.rst, removed Artwork and reference to
      artwork/LICENSE.rst)
    
    - add new runtime dependency on python-blinker
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-flask/python-flask.mk
+++ package/python-flask/python-flask.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_FLASK_VERSION = 2.2.2
+PYTHON_FLASK_VERSION = 2.3.2
 PYTHON_FLASK_SOURCE = Flask-$(PYTHON_FLASK_VERSION).tar.gz
-PYTHON_FLASK_SITE = https://files.pythonhosted.org/packages/69/b6/53cfa30eed5aa7343daff36622843688ba8c6fe9829bb2b92e193ab1163f
+PYTHON_FLASK_SITE = https://files.pythonhosted.org/packages/4d/00/ef81c18da32fdfcde6381c315f4b11597fb6691180a330418848efee0ae7
 PYTHON_FLASK_SETUP_TYPE = setuptools
 PYTHON_FLASK_LICENSE = BSD-3-Clause
 PYTHON_FLASK_LICENSE_FILES = LICENSE.rst docs/license.rst
