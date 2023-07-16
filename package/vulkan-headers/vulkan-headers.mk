d4fa5c83dbce3454c5a1a6408071bb3c0069858e
Bernd Kuhls <bernd@kuhls.net>
2023-07-16 10:54:13 +0200

    package/vulkan-headers: bump version to 1.3.257
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/vulkan-headers/vulkan-headers.mk
+++ package/vulkan-headers/vulkan-headers.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-VULKAN_HEADERS_VERSION = 1.2.203
+VULKAN_HEADERS_VERSION = 1.3.257
 VULKAN_HEADERS_SITE = $(call github,KhronosGroup,Vulkan-Headers,v$(VULKAN_HEADERS_VERSION))
 VULKAN_HEADERS_LICENSE = Apache-2.0
 VULKAN_HEADERS_LICENSE_FILES = LICENSE.txt
