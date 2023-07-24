e83a15172591fee73f1dff10d145ea56cbc4dc60
Bernd Kuhls <bernd@kuhls.net>
2023-07-24 22:44:39 +0200

    package/opensc: disable tests
    Fixes:
    http://autobuild.buildroot.net/results/50b/50b6962ccd884a91391d13ee75852dd0e9ea1a84/
    
    This error only occurs when building with BR2_SHARED_STATIC_LIBS=y, it
    fails for src/tests/fuzzing/fuzz_pkcs15init which is only available when
    static linking is enabled:
    https://github.com/OpenSC/OpenSC/blob/master/src/tests/fuzzing/Makefile.am#L14
    
    The upstream build system has no support to extract the correct ldflags
    for libatomic.so provided by libcrypto.pc. Because we do not need the
    tests anyway we just disable them to "fix" the build error for us.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/opensc/opensc.mk
+++ package/opensc/opensc.mk
@@ -9,8 +9,10 @@ OPENSC_SITE = https://github.com/OpenSC/OpenSC/releases/download/$(OPENSC_VERSIO
 OPENSC_LICENSE = LGPL-2.1+
 OPENSC_LICENSE_FILES = COPYING
 OPENSC_CPE_ID_VENDOR = opensc_project
+# 0003-configure-add-option-to-disable-tests.patch
+OPENSC_AUTORECONF = YES
 OPENSC_DEPENDENCIES = openssl pcsc-lite
 OPENSC_INSTALL_STAGING = YES
-OPENSC_CONF_OPTS = --disable-cmocka --disable-strict
+OPENSC_CONF_OPTS = --disable-cmocka --disable-strict --disable-tests
 
 $(eval $(autotools-package))
