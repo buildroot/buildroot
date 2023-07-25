14c3e8c19dc345af65c5f7f17b6954ba5112f5dc
James Hilliard <james.hilliard1@gmail.com>
2023-07-25 07:08:07 -0600

    package/python-aioconsole: bump to version 0.6.2
    License hash changed due to formatting change:
    https://github.com/vxgmichel/aioconsole/commit/2b7fbb2a73f94154d6b10cf695c681fa1fdcf263
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-aioconsole/python-aioconsole.mk
+++ package/python-aioconsole/python-aioconsole.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_AIOCONSOLE_VERSION = 0.6.1
+PYTHON_AIOCONSOLE_VERSION = 0.6.2
 PYTHON_AIOCONSOLE_SOURCE = aioconsole-$(PYTHON_AIOCONSOLE_VERSION).tar.gz
-PYTHON_AIOCONSOLE_SITE = https://files.pythonhosted.org/packages/27/a2/4bbeecceb6786a058e92469686ae1b7d3a616b3f5b99e00b96c3064349e2
+PYTHON_AIOCONSOLE_SITE = https://files.pythonhosted.org/packages/5f/14/e5c634fad6a95ffd602fbbd1aa107f05a8ffb79d33ec0d0477f3b137f8a9
 PYTHON_AIOCONSOLE_SETUP_TYPE = setuptools
 PYTHON_AIOCONSOLE_LICENSE = GPL-3.0
 PYTHON_AIOCONSOLE_LICENSE_FILES = LICENSE
