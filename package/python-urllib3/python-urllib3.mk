074bc42971181ee71d8ff5bd57641b7b7eb7580f
James Hilliard <james.hilliard1@gmail.com>
2023-07-25 08:21:20 -0600

    package/python-urllib3: bump to version 2.0.4
    Migrate from setuptools to hatchling pep517 build backend.
    
    License hash changed due contributors change:
    https://github.com/urllib3/urllib3/commit/8e94754f85c59d826ac0604d0dd2a0b41874bb36
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-urllib3/python-urllib3.mk
+++ package/python-urllib3/python-urllib3.mk
@@ -4,14 +4,16 @@
 #
 ################################################################################
 
-PYTHON_URLLIB3_VERSION = 1.26.13
+PYTHON_URLLIB3_VERSION = 2.0.4
 PYTHON_URLLIB3_SOURCE = urllib3-$(PYTHON_URLLIB3_VERSION).tar.gz
-PYTHON_URLLIB3_SITE = https://files.pythonhosted.org/packages/c2/51/32da03cf19d17d46cce5c731967bf58de9bd71db3a379932f53b094deda4
+PYTHON_URLLIB3_SITE = https://files.pythonhosted.org/packages/31/ab/46bec149bbd71a4467a3063ac22f4486ecd2ceb70ae8c70d5d8e4c2a7946
 PYTHON_URLLIB3_LICENSE = MIT
 PYTHON_URLLIB3_LICENSE_FILES = LICENSE.txt
 PYTHON_URLLIB3_CPE_ID_VENDOR = python
 PYTHON_URLLIB3_CPE_ID_PRODUCT = urllib3
-PYTHON_URLLIB3_SETUP_TYPE = setuptools
+PYTHON_URLLIB3_SETUP_TYPE = pep517
+PYTHON_URLLIB3_DEPENDENCIES = host-python-hatchling
+HOST_PYTHON_URLLIB3_DEPENDENCIES = host-python-hatchling
 
 $(eval $(python-package))
 $(eval $(host-python-package))
