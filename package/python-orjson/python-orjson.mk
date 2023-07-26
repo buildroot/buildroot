f60a122281de425fb6884304c8f8b006416b31b5
James Hilliard <james.hilliard1@gmail.com>
2023-07-26 15:37:26 -0600

    package/python-orjson: bump to version 3.9.2
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Acked-by: Graeme Smecher <gsmecher@threespeedlogic.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-orjson/python-orjson.mk
+++ package/python-orjson/python-orjson.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ORJSON_VERSION = 3.8.4
+PYTHON_ORJSON_VERSION = 3.9.2
 PYTHON_ORJSON_SOURCE = orjson-$(PYTHON_ORJSON_VERSION).tar.gz
-PYTHON_ORJSON_SITE = https://files.pythonhosted.org/packages/8f/2d/ae31eddf7e097eec5e25c7da954ef6e944f4dbf79153676f94cea3dc5116
+PYTHON_ORJSON_SITE = https://files.pythonhosted.org/packages/30/a4/96bcb52da0de9ccfcd99a60719b995c9b9c3aaa3a70701f0790ce856c10d
 PYTHON_ORJSON_SETUP_TYPE = pep517
 PYTHON_ORJSON_LICENSE = Apache-2.0 or MIT
 PYTHON_ORJSON_LICENSE_FILES = LICENSE-APACHE LICENSE-MIT
