836cb23310cbbb003225a3ea4f1d2b4686fa81f0
Christian Stewart <christian@aperture.us>
2023-07-27 16:22:35 -0700

    package/containerd: bump version to v1.6.22
    Bugfixes and updates.
    
    https://github.com/containerd/containerd/releases/tag/v1.6.22
    
    Signed-off-by: Christian Stewart <christian@aperture.us>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 718c4972c6f58fd8ac401fa4a982a9d8514f9dcd)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/containerd/containerd.mk
+++ package/containerd/containerd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CONTAINERD_VERSION = 1.6.21
+CONTAINERD_VERSION = 1.6.22
 CONTAINERD_SITE = $(call github,containerd,containerd,v$(CONTAINERD_VERSION))
 CONTAINERD_LICENSE = Apache-2.0
 CONTAINERD_LICENSE_FILES = LICENSE
