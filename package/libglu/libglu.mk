172e07029857a8087a70596b3a75dcc45e6a1dd8
Bernd Kuhls <bernd@kuhls.net>
2023-07-29 17:46:15 +0200

    package/libglu: bump version to 9.0.3
    Release notes:
    https://lists.freedesktop.org/archives/mesa-announce/2023-July/000725.html
    
    Upstream dropped autoconf, switch to meson.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/libglu/libglu.mk
+++ package/libglu/libglu.mk
@@ -4,12 +4,13 @@
 #
 ################################################################################
 
-LIBGLU_VERSION = 9.0.2
+LIBGLU_VERSION = 9.0.3
 LIBGLU_SITE = https://mesa.freedesktop.org/archive/glu
 LIBGLU_SOURCE = glu-$(LIBGLU_VERSION).tar.xz
 LIBGLU_LICENSE = SGI-B-2.0
 LIBGLU_LICENSE_FILES = include/GL/glu.h
 LIBGLU_INSTALL_STAGING = YES
 LIBGLU_DEPENDENCIES = libgl host-pkgconf
+LIBGLU_CONF_OPTS = -Dgl_provider=gl
 
-$(eval $(autotools-package))
+$(eval $(meson-package))
