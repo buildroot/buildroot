5b8aa2a21e0d84e10b261844d9304f93ce49ce8c
Bernd Kuhls <bernd@kuhls.net>
2023-07-30 08:56:37 +0200

    package/mbpoll: fix build without c++
    Add patch to enable build without c++.
    
    Add configure option to fix subsequent configure error due to patched
    source file during git version check:
    
    CMake Error at cmake/GitVersion.cmake:118 (message):
      Unable to find current project version !
    Call Stack (most recent call first):
      CMakeLists.txt:100 (GetGitVersion)
    
    Fixes:
    http://autobuild.buildroot.net/results/2f8/2f819871a89cb2a2df239b1f879c1babcdd022fc/
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/mbpoll/mbpoll.mk
+++ package/mbpoll/mbpoll.mk
@@ -9,5 +9,6 @@ MBPOLL_SITE = $(call github,epsilonrt,mbpoll,v$(MBPOLL_VERSION))
 MBPOLL_DEPENDENCIES = host-pkgconf libmodbus
 MBPOLL_LICENSE = GPL-3.0+
 MBPOLL_LICENSE_FILES = COPYING
+MBPOLL_CONF_OPTS = -DGIT_EXECUTABLE=/bin/false
 
 $(eval $(cmake-package))
