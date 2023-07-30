38b8f6aa050bcb5a8e2facdd0d053f1553bfe702
Bernd Kuhls <bernd@kuhls.net>
2023-07-30 12:14:22 +0200

    package/libuv: remove POST_PATCH_HOOK
    Buildroot commit 1738b5582ba0cf715303f4ebfa1d2323a33d0ae4, which
    switched  _SOURCE to an upstream-provided tarball, forgot to remove the
    POST_PATCH_HOOK which adds a file already present in the tarball.
    
    Fixes:
    http://autobuild.buildroot.net/results/e4d0c06e3babed6b290b6223a1cf7e4cce38064c/
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libuv/libuv.mk
+++ package/libuv/libuv.mk
@@ -15,16 +15,4 @@ LIBUV_LICENSE = BSD-2-Clause, BSD-3-Clause, ISC, MIT
 LIBUV_LICENSE_FILES = LICENSE LICENSE-extra
 LIBUV_CPE_ID_VENDOR = libuv
 
-# Upstream needs tests to be run sequentially. This is the default in
-# automake 1.11 and before, but not starting in 1.12. To maintain
-# sequentiality in 1.12 and later, the automake option 'serial-tests'
-# must be used, Unfortunately, it is not recognised by 1.11 and
-# before. So upstream only adds it conditionally. We use automake
-# 1.14, so we need it.
-define LIBUV_FIXUP_AUTOGEN
-	echo "m4_define([UV_EXTRA_AUTOMAKE_FLAGS], [serial-tests])" \
-		>$(@D)/m4/libuv-extra-automake-flags.m4
-endef
-LIBUV_POST_PATCH_HOOKS += LIBUV_FIXUP_AUTOGEN
-
 $(eval $(autotools-package))
