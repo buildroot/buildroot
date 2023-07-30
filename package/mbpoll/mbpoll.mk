eb99c88a151aa95d1ecdbf93b9cf0ce3f753bca2
Bernd Kuhls <bernd@kuhls.net>
2023-07-30 08:58:29 +0200

    package/mbpoll: bump version to 1.5.2
    Changelog: https://github.com/epsilonrt/mbpoll/releases/tag/v1.5.2
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/mbpoll/mbpoll.mk
+++ package/mbpoll/mbpoll.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MBPOLL_VERSION = 1.5
+MBPOLL_VERSION = 1.5.2
 MBPOLL_SITE = $(call github,epsilonrt,mbpoll,v$(MBPOLL_VERSION))
 MBPOLL_DEPENDENCIES = host-pkgconf libmodbus
 MBPOLL_LICENSE = GPL-3.0+
