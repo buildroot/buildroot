67cbbbfe6e9632deacd0784bdbf51496ad76701d
Bernd Kuhls <bernd@kuhls.net>
2023-07-30 21:15:26 +0200

    package/tvheadend: bump version
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/tvheadend/tvheadend.mk
+++ package/tvheadend/tvheadend.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TVHEADEND_VERSION = 18effa8ad93e901f3cdaa534123d910f14453d1f
+TVHEADEND_VERSION = fe47ecb5504a521fed9c1ca9705fb0dd2bb8443a
 TVHEADEND_SITE = $(call github,tvheadend,tvheadend,$(TVHEADEND_VERSION))
 TVHEADEND_LICENSE = GPL-3.0+
 TVHEADEND_LICENSE_FILES = LICENSE.md
