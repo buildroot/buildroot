f16afb8d9f2b410fcb190874b59971fef16dfeca
Nicolas Carrier <nicolas.carrier@nav-timing.safrangroup.com>
2023-08-02 08:22:39 +0200

    less: use default "make install" behavior
    The less package currently installs only the "less" binary, rather
    than running the default "make install" behavior from the autotools
    build system.
    
    However, at least another binary, "lessecho" is needed for the "s"
    command in less to work properly: so in that sense this commit is
    fixing a broken behavior.
    
    Since the less installation only installs 3 programs, "less",
    "lessopen" and "lesskey", there is no good reason to not use the
    default installation command, which we do in this commit.
    
    Signed-off-by: Nicolas Carrier <carrier.nicolas0@gmail.com>
    [Thomas: rework commit message]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 8b1bc64d924e386466d715637a4a09228322beef)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/less/less.mk
+++ package/less/less.mk
@@ -14,8 +14,4 @@ LESS_DEPENDENCIES = ncurses
 # 0001-End-OSC8-hyperlink-on-invalid-embedded-escape-sequence.patch
 LESS_IGNORE_CVES += CVE-2022-46663
 
-define LESS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/less $(TARGET_DIR)/usr/bin/less
-endef
-
 $(eval $(autotools-package))
