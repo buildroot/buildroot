6f372eb8ecb00a06be53ab3c131e57e2d8d6e24b
Bernd Kuhls <bernd@kuhls.net>
2023-08-05 19:37:37 +0200

    package/python-ipy: switch from distutils to setuptools
    distutils will be dropped with python 3.12.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-ipy/python-ipy.mk
+++ package/python-ipy/python-ipy.mk
@@ -9,6 +9,6 @@ PYTHON_IPY_SOURCE = IPy-$(PYTHON_IPY_VERSION).tar.gz
 PYTHON_IPY_SITE = https://files.pythonhosted.org/packages/64/a4/9c0d88d95666ff1571d7baec6c5e26abc08051801feb6e6ddf40f6027e22
 PYTHON_IPY_LICENSE = BSD-3-Clause
 PYTHON_IPY_LICENSE_FILES = COPYING
-PYTHON_IPY_SETUP_TYPE = distutils
+PYTHON_IPY_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
