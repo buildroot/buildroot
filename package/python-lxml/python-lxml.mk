05adefbafd532d901f8476e9dc59eb416108c4d4
Bernd Kuhls <bernd@kuhls.net>
2023-08-05 10:34:40 +0200

    package/python-lxml: bump version to 4.9.3
    Changelog: https://github.com/lxml/lxml/blob/master/CHANGES.txt
    
    Added sha256 hash provided by upstream.
    This release includes fixes for upcoming Python 3.12.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-lxml/python-lxml.mk
+++ package/python-lxml/python-lxml.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PYTHON_LXML_VERSION = 4.9.2
-PYTHON_LXML_SITE = https://files.pythonhosted.org/packages/06/5a/e11cad7b79f2cf3dd2ff8f81fa8ca667e7591d3d8451768589996b65dec1
+PYTHON_LXML_VERSION = 4.9.3
+PYTHON_LXML_SITE = https://files.pythonhosted.org/packages/30/39/7305428d1c4f28282a4f5bdbef24e0f905d351f34cf351ceb131f5cddf78
 PYTHON_LXML_SOURCE = lxml-$(PYTHON_LXML_VERSION).tar.gz
 
 # Not including the GPL, because it is used only for the test scripts.
