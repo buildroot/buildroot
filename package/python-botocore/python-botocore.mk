d1508c9d5cb6bcb297bf7945db20eade350ed613
James Hilliard <james.hilliard1@gmail.com>
2023-08-06 16:02:55 -0600

    package/python-botocore: bump to version 1.31.20
    When we bumped urllib3 in 074bc42971181ee71d8ff5bd57641b7b7eb7580f
    we broke compatibility with python-botocore.
    
    This compatibility error was fixed upstream in botocore 1.29.122:
    https://github.com/boto/botocore/commit/0a6d8e3c6325da395a04227599cfa292190d6841
    
    Fixes:
     - https://gitlab.com/buildroot.org/buildroot/-/jobs/4768561482
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Tested-by: Romain Naour <romain.naour@smile.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-botocore/python-botocore.mk
+++ package/python-botocore/python-botocore.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_BOTOCORE_VERSION = 1.29.115
+PYTHON_BOTOCORE_VERSION = 1.31.20
 PYTHON_BOTOCORE_SOURCE = botocore-$(PYTHON_BOTOCORE_VERSION).tar.gz
-PYTHON_BOTOCORE_SITE = https://files.pythonhosted.org/packages/9e/00/4560081a431aaa14c2257040d43f56c2ccba816d8bce3b8e1577a19f4076
+PYTHON_BOTOCORE_SITE = https://files.pythonhosted.org/packages/f9/0d/ec24cb66b7651268462d5eb5d9d3c140a99f487db3ed58a45617991b11e8
 PYTHON_BOTOCORE_SETUP_TYPE = setuptools
 PYTHON_BOTOCORE_LICENSE = Apache-2.0
 PYTHON_BOTOCORE_LICENSE_FILES = LICENSE.txt tests/unit/auth/aws4_testsuite/LICENSE
