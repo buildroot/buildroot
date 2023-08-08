69d4e3bc3ed0fec343ab0b85cac65a0a095d308c
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2023-08-09 00:08:18 +0200

    board/microchip/mpfs_icicle/post-image.sh: add quoting to make shellcheck happy
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- board/microchip/mpfs_icicle/post-image.sh
+++ board/microchip/mpfs_icicle/post-image.sh
@@ -2,7 +2,7 @@
 HSS_PAYLOAD_GENERATOR=${HOST_DIR}/bin/hss-payload-generator
 MKIMAGE=${HOST_DIR}/bin/mkimage
 
-${HSS_PAYLOAD_GENERATOR} -c board/microchip/mpfs_icicle/config.yaml ${BINARIES_DIR}/payload.bin
-cp board/microchip/mpfs_icicle/mpfs_icicle.its ${BINARIES_DIR}/mpfs_icicle.its
-(cd ${BINARIES_DIR} && ${MKIMAGE} -f mpfs_icicle.its mpfs_icicle.itb)
+"${HSS_PAYLOAD_GENERATOR}" -c board/microchip/mpfs_icicle/config.yaml "${BINARIES_DIR}"/payload.bin
+cp board/microchip/mpfs_icicle/mpfs_icicle.its "${BINARIES_DIR}"/mpfs_icicle.its
+(cd "${BINARIES_DIR}" && "${MKIMAGE}" -f mpfs_icicle.its mpfs_icicle.itb)
 support/scripts/genimage.sh -c board/microchip/mpfs_icicle/genimage.cfg
