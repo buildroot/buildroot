a4dc365b9ab68491a3bca54a0066dbbfb904fffc
Daniel Lang <dalang@gmx.at>
2023-08-08 20:49:16 +0200

    package/yajl: add CPE ID
    yajl_project:yajl is a valid CPE ID:
    https://nvd.nist.gov/products/cpe/detail/CD8C46A4-151E-4DC3-BCE8-4C322E9E10DB?namingFormat=2.3&orderBy=CPEURI&keyword=yajl&status=FINAL
    
    Signed-off-by: Daniel Lang <dalang@gmx.at>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 35d97842a234b429531be8164028497d59446ca8)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/yajl/yajl.mk
+++ package/yajl/yajl.mk
@@ -9,5 +9,6 @@ YAJL_SITE = $(call github,lloyd,yajl,refs/tags/$(YAJL_VERSION))
 YAJL_INSTALL_STAGING = YES
 YAJL_LICENSE = ISC
 YAJL_LICENSE_FILES = COPYING
+YAJL_CPE_ID_VENDOR = yajl_project
 
 $(eval $(cmake-package))
