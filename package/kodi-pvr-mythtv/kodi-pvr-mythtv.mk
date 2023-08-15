36e26a50673a6e394a317b09c662fa36a0af400d
Bernd Kuhls <bernd@kuhls.net>
2023-08-15 20:40:11 +0200

    package/kodi-pvr-mythtv: bump version to 20.4.0-Nexus
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-pvr-mythtv/kodi-pvr-mythtv.mk
+++ package/kodi-pvr-mythtv/kodi-pvr-mythtv.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PVR_MYTHTV_VERSION = 20.3.2-Nexus
+KODI_PVR_MYTHTV_VERSION = 20.4.0-Nexus
 KODI_PVR_MYTHTV_SITE = $(call github,janbar,pvr.mythtv,$(KODI_PVR_MYTHTV_VERSION))
 KODI_PVR_MYTHTV_LICENSE = GPL-2.0+
 KODI_PVR_MYTHTV_LICENSE_FILES = LICENSE.md
