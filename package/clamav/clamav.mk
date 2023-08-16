c78799639b25ab1a90e699267ae821a279d87916
Bernd Kuhls <bernd@kuhls.net>
2023-08-16 22:03:10 +0200

    package/clamav: security bump version to 1.0.2
    Release notes: https://blog.clamav.net/2023/07/2023-08-16-releases.html
    
    Fixes CVE-2023-20197 & CVE-2023-20212.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/clamav/clamav.mk
+++ package/clamav/clamav.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CLAMAV_VERSION = 1.0.1
+CLAMAV_VERSION = 1.0.2
 CLAMAV_SITE = https://www.clamav.net/downloads/production
 CLAMAV_LICENSE = GPL-2.0
 CLAMAV_LICENSE_FILES = \
