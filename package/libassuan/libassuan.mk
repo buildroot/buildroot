a9e36f73a76a079b6dde2eb4da776d7a3ddba677
Julien Olivain <ju.o@free.fr>
2023-08-21 22:22:15 +0200

    package/libassuan: update _SITE url to use https
    This new _SITE url matches the one published upstream at:
    https://gnupg.org/download/
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libassuan/libassuan.mk
+++ package/libassuan/libassuan.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LIBASSUAN_VERSION = 2.5.5
-LIBASSUAN_SITE = ftp://ftp.gnupg.org/gcrypt/libassuan
+LIBASSUAN_SITE = https://gnupg.org/ftp/gcrypt/libassuan
 LIBASSUAN_SOURCE = libassuan-$(LIBASSUAN_VERSION).tar.bz2
 LIBASSUAN_LICENSE = LGPL-2.1+ (library), GPL-3.0 (tests, doc)
 LIBASSUAN_LICENSE_FILES = COPYING.LIB COPYING
