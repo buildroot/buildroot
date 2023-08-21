875ab7d9e3d32990cd07fbab3c68f31dfa9927a4
Jamie Gibbons <jamie.gibbons@microchip.com>
2023-08-21 12:30:33 +0100

    board/microchip/mpfs_icicle: update post-image script
    The hss-payload-generator cannot find where u-boot.bin is when looking
    for it using the config.yaml. Update syntax issues and working
    directories. Fix the post image script to allow an image to get built.
    
    Signed-off-by: Jamie Gibbons <jamie.gibbons@microchip.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- board/microchip/mpfs_icicle/post-image.sh
+++ board/microchip/mpfs_icicle/post-image.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
-HSS_PAYLOAD_GENERATOR=${HOST_DIR}/bin/hss-payload-generator
-MKIMAGE=${HOST_DIR}/bin/mkimage
+#!/bin/bash
+set -e
 
-"${HSS_PAYLOAD_GENERATOR}" -c board/microchip/mpfs_icicle/config.yaml "${BINARIES_DIR}"/payload.bin
-cp board/microchip/mpfs_icicle/mpfs_icicle.its "${BINARIES_DIR}"/mpfs_icicle.its
-(cd "${BINARIES_DIR}" && "${MKIMAGE}" -f mpfs_icicle.its mpfs_icicle.itb)
-support/scripts/genimage.sh -c board/microchip/mpfs_icicle/genimage.cfg
+HSS_PAYLOAD_GENERATOR="${HOST_DIR}"/bin/hss-payload-generator
+MKIMAGE="${HOST_DIR}"/bin/mkimage
+BOARD_DIR="$(pwd)"/"${0%/*}"
+
+pushd "${BINARIES_DIR}"
+"${HSS_PAYLOAD_GENERATOR}" -c "${BOARD_DIR}"/config.yaml payload.bin
+cp "${BOARD_DIR}"/mpfs_icicle.its "${BINARIES_DIR}"/mpfs_icicle.its
+"${MKIMAGE}" -f mpfs_icicle.its mpfs_icicle.itb
+popd
+support/scripts/genimage.sh -c "${BOARD_DIR}"/genimage.cfg
