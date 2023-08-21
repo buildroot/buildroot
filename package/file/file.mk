2298de68530283f329147f0da9c5acd039ff561c
Waldemar Brodkorb <wbx@openadk.org>
2023-08-21 11:17:49 +0200

    package/file: bump version to 5.45
    Patch is included upstream.
    See here for Changes in 5.45:
    https://mailman.astron.com/pipermail/file/2023-July/001205.html
    See here for Changes in 5.44:
    https://mailman.astron.com/pipermail/file/2022-December/001042.html
    
    The hash of src/vasprintf.c, which is used as one of the license
    files, has been updated due to source code changes that do not affect
    the licensing terms.
    
    Signed-off-by: Waldemar Brodkorb <wbx@openadk.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/file/file.mk
+++ package/file/file.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FILE_VERSION = 5.43
+FILE_VERSION = 5.45
 FILE_SITE = ftp://ftp.astron.com/pub/file
 FILE_LICENSE = BSD-2-Clause, BSD-4-Clause (one file), BSD-3-Clause (one file)
 FILE_LICENSE_FILES = COPYING src/mygetopt.h src/vasprintf.c
