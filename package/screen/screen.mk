f73bf9084df2228bca051f64baeed604d1f8cc0d
Julien Olivain <ju.o@free.fr>
2023-08-22 21:49:21 +0200

    package/screen: security bump to version 4.9.1
    See release announce:
    https://lists.gnu.org/archive/html/screen-users/2023-08/msg00000.html
    
    Fixes:
    CVE-2023-24626: https://www.cve.org/CVERecord?id=CVE-2023-24626
    
    Note: Buildroot installs screen as setuid, so the described scenario
    in CVE applies.
    
    This commit also rebases all patches on this release. Patch were
    regenerated with 'git format-patch -N', so patch file name changed in
    this process. The file .checkpackageignore is also updated accordingly.
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 96a54b09074dfc7fc3c24997e1cfc50d6bd67015)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/screen/screen.mk
+++ package/screen/screen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SCREEN_VERSION = 4.9.0
+SCREEN_VERSION = 4.9.1
 SCREEN_SITE = $(BR2_GNU_MIRROR)/screen
 SCREEN_LICENSE = GPL-3.0+
 SCREEN_LICENSE_FILES = COPYING
