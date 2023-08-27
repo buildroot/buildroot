2001d3758879bc25496e726edf3da99e9dbee38f
Julien Olivain <ju.o@free.fr>
2023-08-27 16:59:55 +0200

    package/edk2-platforms: bump in sync with edk2-stable202308
    Bump the edk2-platforms commit to the one corresponding to version
    edk2-stable202308 of edk2, based on the timestamps.
    
    Cc: Dick Olsson <hi@senzilla.io>
    Cc: Vincent Stehlé <vincent.stehle@arm.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/edk2-platforms/edk2-platforms.mk
+++ package/edk2-platforms/edk2-platforms.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 # Keep in sync with latest commit as of the release date for boot/edk2
-EDK2_PLATFORMS_VERSION = b71f2bda9e4fc183068eef5d1d90a631181a2506
+EDK2_PLATFORMS_VERSION = e509ac5a729ebe2a3bc905aed1df23226aca4dc9
 EDK2_PLATFORMS_SITE = $(call github,tianocore,edk2-platforms,$(EDK2_PLATFORMS_VERSION))
 EDK2_PLATFORMS_LICENSE = BSD-2-Clause-Patent
 EDK2_PLATFORMS_LICENSE_FILES = License.txt
