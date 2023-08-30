a95d65e495ccfbf8bd91c8f9a41bd12574a937cb
Adam Duskett <aduskett@gmail.com>
2023-08-30 13:54:40 -0600

    package/vulkan-headers: bump version to 1.3.262
    Update the license hash as the license file is now located at LICENSE.md
    isntead of LICENSE.txt, and add MIT to the list of licenses.
    
    Signed-off-by: Adam Duskett <aduskett@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/vulkan-headers/vulkan-headers.mk
+++ package/vulkan-headers/vulkan-headers.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-VULKAN_HEADERS_VERSION = 1.3.257
+VULKAN_HEADERS_VERSION = 1.3.262
 VULKAN_HEADERS_SITE = $(call github,KhronosGroup,Vulkan-Headers,v$(VULKAN_HEADERS_VERSION))
-VULKAN_HEADERS_LICENSE = Apache-2.0
-VULKAN_HEADERS_LICENSE_FILES = LICENSE.txt
+VULKAN_HEADERS_LICENSE = Apache-2.0, MIT
+VULKAN_HEADERS_LICENSE_FILES = LICENSE.md
 VULKAN_HEADERS_INSTALL_STAGING = YES
 
 $(eval $(cmake-package))
