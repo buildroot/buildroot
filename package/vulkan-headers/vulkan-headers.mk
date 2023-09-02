9e057e7897008a6aabcd91109cf004e5ffd957ff
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2023-09-02 11:58:26 +0200

    package/vulkan-headers: add comment on version being shared with other packages
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/vulkan-headers/vulkan-headers.mk
+++ package/vulkan-headers/vulkan-headers.mk
@@ -4,6 +4,8 @@
 #
 ################################################################################
 
+# Note: changing this version will also affect vulkan-loader and
+# vulkan-tools, so please update those packages as well.
 VULKAN_HEADERS_VERSION = 1.3.262
 VULKAN_HEADERS_SITE = $(call github,KhronosGroup,Vulkan-Headers,v$(VULKAN_HEADERS_VERSION))
 VULKAN_HEADERS_LICENSE = Apache-2.0, MIT
