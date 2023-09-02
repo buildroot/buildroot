464ff28b7a7afc73b51aa9324b24a5aca5bf2ade
Francois Perrad <fperrad@gmail.com>
2023-09-02 12:45:00 +0200

    package/luasec: bump to version 1.3.2
    diff LICENSE:
    -LuaSec 1.2.0 license
    -Copyright (C) 2006-2022 Bruno Silvestre, UFG
    +LuaSec 1.3.2 license
    +Copyright (C) 2006-2023 Bruno Silvestre, UFG
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/luasec/luasec.mk
+++ package/luasec/luasec.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LUASEC_VERSION = 1.2.0-1
+LUASEC_VERSION = 1.3.2-1
 LUASEC_SUBDIR = luasec
 LUASEC_LICENSE = MIT
 LUASEC_LICENSE_FILES = $(LUASEC_SUBDIR)/LICENSE
