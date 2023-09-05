7fc1eeea27dc54b739e76a106541c3ad75fdf7f3
Adam Duskett <aduskett@gmail.comm>
2023-09-05 12:06:01 -0600

    package/php: bump version to 8.2.10
    Signed-off-by: Adam Duskett <aduskett@gmail.comm>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 9bf196ce287ecb4e772a935614132d492f5884a5)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/php/php.mk
+++ package/php/php.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PHP_VERSION = 8.2.9
+PHP_VERSION = 8.2.10
 PHP_SITE = https://www.php.net/distributions
 PHP_SOURCE = php-$(PHP_VERSION).tar.xz
 PHP_INSTALL_STAGING = YES
