6fb61f11b71d923baad10b0ecdaee13ca6bbc1a2
Daniel Lang <dalang@gmx.at>
2023-09-06 21:49:51 +0200

    package/libiec61850: ignore CVE-2023-27772
    Segmentation fault in example code can be exploited.
    BUILD_EXAMPLES is disabled for all cmake projects.
    
    See https://github.com/mz-automation/libiec61850/issues/442
    
    Signed-off-by: Daniel Lang <dalang@gmx.at>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit b25f1cb47c55dab9de706d61809936839cfe8f5f)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libiec61850/libiec61850.mk
+++ package/libiec61850/libiec61850.mk
@@ -11,5 +11,8 @@ LIBIEC61850_LICENSE = GPL-3.0+
 LIBIEC61850_LICENSE_FILES = COPYING
 LIBIEC61850_CPE_ID_VENDOR = mz-automation
 LIBIEC61850_CONF_OPTS = -DBUILD_PYTHON_BINDINGS=OFF
+# Examples aren't build
+# https://github.com/mz-automation/libiec61850/issues/442
+LIBIEC61850_IGNORE_CVES += CVE-2023-27772
 
 $(eval $(cmake-package))
