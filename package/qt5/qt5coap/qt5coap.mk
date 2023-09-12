6bd6b4c2cf2679354559e3fd6149b26be4ea1ebc
Yann E. MORIN <yann.morin.1998@free.fr>
2023-09-13 00:15:48 +0200

    package/qt5: fix upstream git trees
    Since commits 4e8b5f9bee9c [0], 6cfbd51d98d1 [1], and d838a416c4ed [2],
    the repository we clone from is the cgit browser, and it does not serve
    the git tree, only the browser:
    
        $ git clone https://code.qt.io/cgit/qt/qtcoap.git
        Cloning into 'qtcoap'...
        fatal: repository 'https://code.qt.io/cgit/qt/qtcoap.git/' not found
    
    Browsing there displays the cgit UI, which gives a proper URI to clone
    from; switch to using that. Things happened to "work" so far thanks to
    sources.buildroot.net.
    
    [0] 4e8b5f9bee9c package/qt5/qt5mqtt: bump version to 5.15.2 (and fix download)
    [1] 6cfbd51d98d1 package/qt5/qt5coap: bump version to 5.15.2 (and fix download)
    [2] d838a416c4ed package/qt5/qt5knx: bump version to 5.15.2 (and fix download)
    
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    Cc: Peter Seiderer <ps.report@gmx.net>
    Cc: Angelo Compagnucci <angelo.compagnucci@gmail.com>
    Cc: Giulio Benetti <giulio.benetti@benettiengineering.com>
    CC: Julien Corjon <corjon.j@ecagroup.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 0055c9c63457627159f2aff1b9b596748c20fb8f)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/qt5/qt5coap/qt5coap.mk
+++ package/qt5/qt5coap/qt5coap.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 QT5COAP_VERSION = 5.15.2
-QT5COAP_SITE = https://code.qt.io/cgit/qt/qtcoap.git
+QT5COAP_SITE = https://code.qt.io/qt/qtcoap.git
 QT5COAP_SITE_METHOD = git
 QT5COAP_INSTALL_STAGING = YES
 QT5COAP_LICENSE = GPL-3.0, GFDL-1.3
--- package/qt5/qt5mqtt/qt5mqtt.mk
+++ package/qt5/qt5mqtt/qt5mqtt.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 QT5MQTT_VERSION = 5.15.2
-QT5MQTT_SITE = https://code.qt.io/cgit/qt/qtmqtt.git
+QT5MQTT_SITE = https://code.qt.io/qt/qtmqtt.git
 QT5MQTT_SITE_METHOD = git
 QT5MQTT_INSTALL_STAGING = YES
 QT5MQTT_LICENSE = GPL-3.0 with exception
