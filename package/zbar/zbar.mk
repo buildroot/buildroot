8a0ac6c44db88eb685f833840bc535393028dac3
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-09-14 23:11:33 +0200

    package/zbar: add ZBAR_CPE_ID_VENDOR
    cpe:2.3:a:zbar_project:zbar is a valid CPE identifier for this package:
    
      https://nvd.nist.gov/products/cpe/detail/438B9E70-F8E2-4318-83B5-46A0DF320CE3
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 23e774d2de35c42ee13f618dd2a895f9a97a4d1c)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/zbar/zbar.mk
+++ package/zbar/zbar.mk
@@ -9,6 +9,7 @@ ZBAR_SOURCE = zbar-$(ZBAR_VERSION).tar.bz2
 ZBAR_SITE = https://www.linuxtv.org/downloads/zbar
 ZBAR_LICENSE = LGPL-2.1+
 ZBAR_LICENSE_FILES = LICENSE.md
+ZBAR_CPE_ID_VENDOR = zbar_project
 ZBAR_INSTALL_STAGING = YES
 ZBAR_DEPENDENCIES = libv4l jpeg $(TARGET_NLS_DEPENDENCIES)
 # uses C99 features
