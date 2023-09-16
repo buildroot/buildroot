9ca60c004d69e1f38000e3a74151b0a90aca7f21
Alexander Egorenkov <egorenar-dev@posteo.net>
2023-09-16 09:18:30 +0000

    package/multipath-tools: bump version to 0.9.6
    Signed-off-by: Alexander Egorenkov <egorenar-dev@posteo.net>
    [yann.morin.1998@free.fr: split long _DEPENDENCIES line]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/multipath-tools/multipath-tools.mk
+++ package/multipath-tools/multipath-tools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MULTIPATH_TOOLS_VERSION = 0.9.4
+MULTIPATH_TOOLS_VERSION = 0.9.6
 MULTIPATH_TOOLS_SITE = $(call github,opensvc,multipath-tools,$(MULTIPATH_TOOLS_VERSION))
 
 MULTIPATH_TOOLS_LICENSE = \
@@ -20,7 +20,16 @@ MULTIPATH_TOOLS_LICENSE_FILES = \
 	README.md
 MULTIPATH_TOOLS_CPE_ID_VENDOR = opensvc
 
-MULTIPATH_TOOLS_DEPENDENCIES = lvm2 json-c readline udev liburcu libaio host-pkgconf
+MULTIPATH_TOOLS_DEPENDENCIES = \
+	host-pkgconf \
+	json-c \
+	liburcu \
+	libaio \
+	lvm2 \
+	readline \
+	udev \
+	util-linux-libs
+
 MULTIPATH_TOOLS_MAKE_OPTS = \
 	kernel_incdir=$(STAGING_DIR)/usr/include \
 	LIB="lib" \
