528197f5e319d62c2abe2319178fc8cf527bde3c
Waldemar Brodkorb <wbx@openadk.org>
2023-09-16 14:21:06 +0200

    package/libarchive: bump version to 3.7.2
    Signed-off-by: Waldemar Brodkorb <wbx@openadk.org>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/libarchive/libarchive.mk
+++ package/libarchive/libarchive.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBARCHIVE_VERSION = 3.7.1
+LIBARCHIVE_VERSION = 3.7.2
 LIBARCHIVE_SOURCE = libarchive-$(LIBARCHIVE_VERSION).tar.xz
 LIBARCHIVE_SITE = https://www.libarchive.de/downloads
 LIBARCHIVE_INSTALL_STAGING = YES
