8f9d6e07f2f333ee3feb5bf1e6966a83ef36f40f
Angelo Compagnucci <angelo@amarulasolutions.com>
2023-09-19 14:39:57 +0200

    package/python-minimalmodbus: bump to version 2.1.1
    Signed-off-by: Angelo Compagnucci <angelo@amarulasolutions.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-minimalmodbus/python-minimalmodbus.mk
+++ package/python-minimalmodbus/python-minimalmodbus.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_MINIMALMODBUS_VERSION = 2.0.1
+PYTHON_MINIMALMODBUS_VERSION = 2.1.1
 PYTHON_MINIMALMODBUS_SOURCE = minimalmodbus-$(PYTHON_MINIMALMODBUS_VERSION).tar.gz
-PYTHON_MINIMALMODBUS_SITE = https://files.pythonhosted.org/packages/78/99/8cd22b4465e697bae2b02fd06aaccd4c5cdfbb18945d728db99f23d71df9
+PYTHON_MINIMALMODBUS_SITE = https://files.pythonhosted.org/packages/37/fc/8a58f7bcdece751f16a4a9aac780acd1288aa8ac6adbffdd764c88fb71c6
 PYTHON_MINIMALMODBUS_SETUP_TYPE = setuptools
 PYTHON_MINIMALMODBUS_LICENSE = Apache-2.0
 PYTHON_MINIMALMODBUS_LICENSE_FILES = LICENSE
