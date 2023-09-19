2d9540e248ff07eb372a220a4d3b6720b9ac7e13
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-09-19 22:49:10 +0200

    package/opensc: fix CVE-2023-2977
    A vulnerability was found in OpenSC. This security flaw cause a buffer
    overrun vulnerability in pkcs15 cardos_have_verifyrc_package. The
    attacker can supply a smart card package with malformed ASN1 context.
    The cardos_have_verifyrc_package function scans the ASN1 buffer for 2
    tags, where remaining length is wrongly caculated due to moved starting
    pointer. This leads to possible heap-based buffer oob read. In cases
    where ASAN is enabled while compiling this causes a crash. Further info
    leak or more damage is possible.
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 9c4c3c4c9c0f4e1bf8b98e6465832c05d85317c2)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/opensc/opensc.mk
+++ package/opensc/opensc.mk
@@ -13,4 +13,7 @@ OPENSC_DEPENDENCIES = openssl pcsc-lite
 OPENSC_INSTALL_STAGING = YES
 OPENSC_CONF_OPTS = --disable-cmocka --disable-strict
 
+# 0004-pkcs15init-correct-left-length-calculation-to-fix-buffer-overrun-bug.patch
+OPENSC_IGNORE_CVES += CVE-2023-2977
+
 $(eval $(autotools-package))
