motioneye-project/motioneyeos

commit 318f3db0dcc052aa770ca57c143b5f7bc40cdaf3

ne10: new package
The Ne10 project has been set up to provide a set of common, useful
functions which have been heavily optimized for the ARM Architecture
and provide consistent well tested behavior that can be easily
incorporated into applications. C interfaces to the functions are
provided for both assembler and NEON implementations.

Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

package/ne10/ne10.mk
@@ -1,0 +1,39 @@
+#############################################################
+#
+# ne10
+#
+#############################################################
+
+# We use a Git commit ID because the last tagged version is more than
+# one year old.
+NE10_VERSION = 88c18f02199947b2c8b57796f5a3ca53160aff96
+NE10_SITE = git://github.com/projectNe10/Ne10.git
+NE10_LICENSE = BSD-3c or Apache 2.0
+NE10_LICENSE_FILES = doc/LICENSE
+
+ifeq ($(BR2_PREFER_STATIC_LIB),)
+NE10_CONF_OPT = \
+	-DNE10_BUILD_SHARED=ON
+endif
+
+# The package does not have any install target, so have to provide
+# INSTALL_STAGING_CMDS and INSTALL_TARGET_CMDS.
+
+ifeq ($(BR2_PREFER_STATIC_LIB),)
+define NE10_INSTALL_STAGING_SHARED_LIB
+	cp -dpf $(@D)/modules/libNE10*.so* $(STAGING_DIR)/usr/lib/
+endef
+endif
+
+define NE10_INSTALL_STAGING_CMDS
+	cp -dpf $(@D)/inc/NE10*h $(STAGING_DIR)/usr/include/
+	cp -dpf $(@D)/modules/libNE10.a $(STAGING_DIR)/usr/lib/
+	$(NE10_INSTALL_STAGING_SHARED_LIB)
+endef
+
+
+define NE10_INSTALL_TARGET_CMDS
+	cp -dpf $(@D)/modules/libNE10*.so* $(TARGET_DIR)/usr/lib/
+endef
+
+$(eval $(cmake-package))

package/Config.in
@@ -466,6 +466,7 @@ source "package/libusb/Config.in"
 source "package/libusb-compat/Config.in"
 source "package/libv4l/Config.in"
 source "package/mtdev/Config.in"
+source "package/ne10/Config.in"
 source "package/neardal/Config.in"
 source "package/pcsc-lite/Config.in"
 source "package/urg/Config.in"

package/ne10/Config.in
@@ -1,0 +1,12 @@
+config BR2_PACKAGE_NE10
+	bool "ne10"
+	depends on BR2_arm && BR2_ARM_CPU_HAS_NEON
+	help
+	  The Ne10 project has been set up to provide a set of common,
+	  useful functions which have been heavily optimized for the
+	  ARM Architecture and provide consistent well tested behavior
+	  that can be easily incorporated into applications. C
+	  interfaces to the functions are provided for both assembler
+	  and NEON implementations.
+
+	  http://projectne10.github.io/Ne10/

buildroot/buildroot

commit 0b764a7d1e6961cb7baa0b3a74de1167bf47a6dd

package/ne10: install shared libraries only when built
The install to staging commands of the ne10 package are careful to
install the shared libraries only if they are built, but we forgot to
use the same care for the install to target commands, causing a build
failure on BR2_STATIC_LIBS=y configurations as no shared library was
built:

cp: cannot stat '/home/autobuild/autobuild/instance-15/output-1/build/ne10-1.2.1/modules/libNE10*.so*': No such file or directory

This commit fixes this by guarding the target installation commands to
BR2_STATIC_LIBS being empty.

The problem exists since the package was introduced in commit
318f3db0dcc052aa770ca57c143b5f7bc40cdaf3 ("ne10: new package"), a good
10 years ago. Most likely it was not seen for many years as this
package is only available for ARM with NEON and AArch64, and we were
not testing fully static builds, except for ARMv5 that don't have
NEON. Now that we are doing more random testing, the problem started
being visible.

Fixes:

  http://autobuild.buildroot.net/results/45b2c1af052271bc2f1bb96544f138d29e4f7dfd/

Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

package/ne10/ne10.mk
@@ -42,8 +42,10 @@ define NE10_INSTALL_STAGING_CMDS
 	$(NE10_INSTALL_STAGING_SHARED_LIB)
 endef
 
+ifeq ($(BR2_STATIC_LIBS),)
 define NE10_INSTALL_TARGET_CMDS
 	cp -dpf $(@D)/modules/libNE10*.so* $(TARGET_DIR)/usr/lib/
 endef
+endif
 
 $(eval $(cmake-package))