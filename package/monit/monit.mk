8cedb39764f70f9d467bf0cc1acc99a8bbb963d6
Jens Maus <mail@jens-maus.de>
2023-09-22 21:47:54 +0200

    package/monit: update to 5.33.0
    This commit updates monit to the latest 5.33.0 version which also
    requires an additional patch to configure.ac as well as enabled ipv6
    support.
    
    Signed-off-by: Jens Maus <mail@jens-maus.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/monit/monit.mk
+++ package/monit/monit.mk
@@ -4,26 +4,28 @@
 #
 ################################################################################
 
-MONIT_VERSION = 5.26.0
+MONIT_VERSION = 5.33.0
 MONIT_SITE = http://mmonit.com/monit/dist
 MONIT_LICENSE = AGPL-3.0 with OpenSSL exception
 MONIT_LICENSE_FILES = COPYING
 MONIT_CPE_ID_VENDOR = mmonit
 MONIT_SELINUX_MODULES = monit
-#
+
 # Touching Makefile.am:
+# 0001-configure.ac-fixes-missing-config-macro-dir.patch touches configure.ac
 MONIT_AUTORECONF = YES
 
 MONIT_CONF_ENV = \
 	libmonit_cv_setjmp_available=yes \
-	libmonit_cv_vsnprintf_c99_conformant=yes
+	libmonit_cv_vsnprintf_c99_conformant=yes \
+	ac_cv_ipv6=yes
 
 MONIT_CONF_OPTS += \
 	--without-pam \
 	--with-largefiles
 
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
-MONIT_CONF_OPTS += --with-ssl=$(STAGING_DIR)/usr
+MONIT_CONF_OPTS += --with-ssl --with-ssl-dir=$(STAGING_DIR)/usr
 MONIT_DEPENDENCIES += openssl
 else
 MONIT_CONF_OPTS += --without-ssl
