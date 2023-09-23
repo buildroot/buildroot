2314928cf828ff02939b9c0041964c3ee2dd907d
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-09-23 22:54:37 +0200

    package/open-iscsi: bump to version 2.1.9
    - Drop patch (already in version)
    - Drop license comment and add REAMDE and libopeniscsiusr/COPYING as
      license files due to
      https://github.com/open-iscsi/open-iscsi/commit/10d50ed4bcf9ef6820f7fe544df0c3605ea4144f
    
    https://github.com/open-iscsi/open-iscsi/blob/2.1.9/Changelog
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/open-iscsi/open-iscsi.mk
+++ package/open-iscsi/open-iscsi.mk
@@ -4,12 +4,10 @@
 #
 ################################################################################
 
-OPEN_ISCSI_VERSION = 2.1.8
+OPEN_ISCSI_VERSION = 2.1.9
 OPEN_ISCSI_SITE = $(call github,open-iscsi,open-iscsi,$(OPEN_ISCSI_VERSION))
-# The COPYING file says GPL-2.0, but there is some GPL-3.0+ code as
-# well. See https://github.com/open-iscsi/open-iscsi/issues/379
-OPEN_ISCSI_LICENSE = GPL-2.0+, GPL-3.0+
-OPEN_ISCSI_LICENSE_FILES = COPYING
+OPEN_ISCSI_LICENSE = GPL-2.0+, GPL-3.0+, LGPL-3.0+
+OPEN_ISCSI_LICENSE_FILES = COPYING README libopeniscsiusr/COPYING
 OPEN_ISCSI_CPE_ID_VENDOR = open-iscsi_project
 OPEN_ISCSI_DEPENDENCIES = kmod open-isns openssl util-linux
 
