c7cae57af0da81492ac11c062c1cab1693b3bbed
Giulio Benetti <giulio.benetti@benettiengineering.com>
2023-09-25 13:46:15 +0200

    package/rtl8821cu: bump to version 2023-09-25
    With this version we can build with Linux 6.5.
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rtl8821cu/rtl8821cu.mk
+++ package/rtl8821cu/rtl8821cu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTL8821CU_VERSION = dc9ee6c6a8b47d0e365fcf1977439c7243da71d5
+RTL8821CU_VERSION = de36dfeff890086a9f30cd0052715fbaa9554087
 RTL8821CU_SITE = $(call github,morrownr,8821cu-20210916,$(RTL8821CU_VERSION))
 RTL8821CU_LICENSE = GPL-2.0
 RTL8821CU_LICENSE_FILES = LICENSE
