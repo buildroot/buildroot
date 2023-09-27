725580a26ecbd554567880e95b8533b2c9baa5f9
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-09-27 22:05:43 +0200

    package/json-c: bump to version 0.17
    Disable building apps thanks to variable added by
    https://github.com/json-c/json-c/commit/bef40a342e98d090afff35ef565b41b22ddaa9a7
    
    https://github.com/json-c/json-c/blob/json-c-0.17-20230812/ChangeLog
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/json-c/json-c.mk
+++ package/json-c/json-c.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-JSON_C_VERSION = 0.16
+JSON_C_VERSION = 0.17
 JSON_C_SITE = https://s3.amazonaws.com/json-c_releases/releases
 JSON_C_INSTALL_STAGING = YES
 JSON_C_LICENSE = MIT
 JSON_C_LICENSE_FILES = COPYING
 JSON_C_CPE_ID_VENDOR = json-c_project
-JSON_C_CONF_OPTS = -DDISABLE_EXTRA_LIBS=ON
+JSON_C_CONF_OPTS = -DBUILD_APPS=OFF -DDISABLE_EXTRA_LIBS=ON
 
 $(eval $(cmake-package))
 $(eval $(host-cmake-package))
