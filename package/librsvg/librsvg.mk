962ec546d0ae6a27bff0baf3bce22194cd1ff78e
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-09-28 19:09:55 +0200

    package/librsvg: security bump to version 2.50.9
    Fix CVE-2023-38633: A directory traversal problem in the URL decoder of
    librsvg before 2.56.3 could be used by local or remote attackers to
    disclose files (on the local filesystem outside of the expected area),
    as demonstrated by href=".?../../../../../../../../../../etc/passwd" in
    an xi:include element.
    
    https://gitlab.gnome.org/GNOME/librsvg/-/blob/2.50.9/NEWS
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit e7988c7060d7d8b137d18721ef773ef266114690)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/librsvg/librsvg.mk
+++ package/librsvg/librsvg.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LIBRSVG_VERSION_MAJOR = 2.50
-LIBRSVG_VERSION = $(LIBRSVG_VERSION_MAJOR).7
+LIBRSVG_VERSION = $(LIBRSVG_VERSION_MAJOR).9
 LIBRSVG_SITE = https://download.gnome.org/sources/librsvg/$(LIBRSVG_VERSION_MAJOR)
 LIBRSVG_SOURCE = librsvg-$(LIBRSVG_VERSION).tar.xz
 LIBRSVG_INSTALL_STAGING = YES
