ce17f93e828a07292e03653be04a49480250f23f
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-09-28 18:51:26 +0200

    package/suricata: security bump to version 6.0.14
    - Fix CVE-2023-35852: In Suricata before 6.0.13 (when there is an
      adversary who controls an external source of rules), a dataset
      filename, that comes from a rule, may trigger absolute or relative
      directory traversal, and lead to write access to a local filesystem.
      This is addressed in 6.0.13 by requiring allow-absolute-filenames and
      allow-write (in the datasets rules configuration section) if an
      installation requires traversal/writing in this situation.
    - Fix CVE-2023-35853: In Suricata before 6.0.13, an adversary who
      controls an external source of Lua rules may be able to execute Lua
      code. This is addressed in 6.0.13 by disabling Lua unless allow-rules
      is true in the security lua configuration section.
    - Drop first patch (not needed since
      https://github.com/OISF/suricata/commit/c8a3aa608eaae1acbaf33dba8a7c1a3cbfeb4285)
    
    https://github.com/OISF/suricata/blob/suricata-6.0.14/ChangeLog
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/suricata/suricata.mk
+++ package/suricata/suricata.mk
@@ -4,13 +4,12 @@
 #
 ################################################################################
 
-SURICATA_VERSION = 6.0.6
+SURICATA_VERSION = 6.0.14
 SURICATA_SITE = https://www.openinfosecfoundation.org/download
 SURICATA_LICENSE = GPL-2.0
 SURICATA_LICENSE_FILES = COPYING LICENSE
 SURICATA_CPE_ID_VENDOR = oisf
-# 0001-python-ensure-proper-shabang-on-python-scripts.patch
-# 0002-configure.ac-allow-the-user-to-override-RUST_TARGET.patch
+# 0001-configure.ac-allow-the-user-to-override-RUST_TARGET.patch
 SURICATA_AUTORECONF = YES
 
 SURICATA_DEPENDENCIES = \
