e83c9d51820d8cbcf3f33376286a77209166a9aa
Giulio Benetti <giulio.benetti@benettiengineering.com>
2023-09-29 11:13:37 +0200

    package/mali-driver: bump to version 2023-09-29
    mali-driver now builds on Linux 6.5.
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/mali-driver/mali-driver.mk
+++ package/mali-driver/mali-driver.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MALI_DRIVER_VERSION = 9baa7331c8e5ba6075e3fc42c530ae7f4c320cad
+MALI_DRIVER_VERSION = d40c57d81c9cf4bc6fefeaf47153fde8eed19ef2
 MALI_DRIVER_SITE = $(call github,bootlin,mali-driver,$(MALI_DRIVER_VERSION))
 MALI_DRIVER_DEPENDENCIES = linux
 MALI_DRIVER_LICENSE = GPL-2.0
