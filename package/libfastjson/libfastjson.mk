4de11043edc321c1ad9ded7e927f830626d4d914
Peter Korsgaard <peter@korsgaard.com>
2023-09-29 08:56:46 +0200

    package/libfastjson: security bump to version 0.99.9.1
    Fixes the equivalent of CVE-2020-12762, which was a json-c vulnerability:
    
    https://github.com/advisories/GHSA-3797-gmjf-45gm
    
    https://github.com/rsyslog/libfastjson/pull/166
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit cfe830c9ee5d66d3bdca15933ed8b59e3ec16e88)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libfastjson/libfastjson.mk
+++ package/libfastjson/libfastjson.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBFASTJSON_VERSION = 0.99.9
+LIBFASTJSON_VERSION = 0.99.9.1
 LIBFASTJSON_SITE = $(call github,rsyslog,libfastjson,v$(LIBFASTJSON_VERSION))
 LIBFASTJSON_INSTALL_STAGING = YES
 LIBFASTJSON_CONF_ENV = ac_cv_prog_cc_c99='-std=gnu99'
