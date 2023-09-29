d65b960859046be486ced6c490d3671bd1392661
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-09-29 18:12:08 +0200

    package/powertop: bump to version 2.15
    - Switch site to get latest version
    - Replace patch by an upstreamable one
    
    https://github.com/fenrus75/powertop/compare/v2.13...v2.15
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/powertop/powertop.mk
+++ package/powertop/powertop.mk
@@ -4,12 +4,13 @@
 #
 ################################################################################
 
-POWERTOP_VERSION = 2.13
-POWERTOP_SITE = https://01.org/sites/default/files/downloads
+POWERTOP_VERSION = 2.15
+POWERTOP_SITE = $(call github,fenrus75,powertop,v$(POWERTOP_VERSION))
 POWERTOP_LICENSE = GPL-2.0
 POWERTOP_LICENSE_FILES = COPYING
 
 POWERTOP_DEPENDENCIES = \
+	host-autoconf-archive \
 	host-pkgconf \
 	libnl \
 	ncurses \
@@ -17,8 +18,16 @@ POWERTOP_DEPENDENCIES = \
 	$(TARGET_NLS_DEPENDENCIES)
 
 POWERTOP_CONF_ENV = LIBS=$(TARGET_NLS_LIBS)
-# 0001-dont-force-stack-smashing-protection.patch
+# 0001-add-disable-stack-protector-option.patch
 POWERTOP_AUTORECONF = YES
+POWERTOP_AUTORECONF_OPTS = --include=$(HOST_DIR)/share/autoconf-archive
+POWERTOP_CONF_OPTS = --disable-stack-protector
+
+# fix missing config.rpath (needed for autoreconf) in the codebase
+define POWERTOP_TOUCH_CONFIG_RPATH
+	touch $(@D)/config.rpath
+endef
+POWERTOP_PRE_CONFIGURE_HOOKS += POWERTOP_TOUCH_CONFIG_RPATH
 
 # Help powertop at finding the right ncurses library depending on
 # which one is available.
