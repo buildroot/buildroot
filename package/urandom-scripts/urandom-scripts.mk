11a46002ae11a0766a4ec9d3d8ccafe7be42c2fc
Peter Korsgaard <peter@korsgaard.com>
2023-09-30 15:42:34 +0200

    package/urandom-scripts: move seedrng init script to S01
    We want to initialize the kernel random number generator as soon as
    possible, as early init scripts may also need random numbers (E.G.
    syslog-ng in S01syslog-ng does).
    
    Seedrng was presumably only using S20 because the previos urandom script
    used S20, which (after som moves) dates all the way back to:
    
    commit 8262508fc4456b5230216473ea14c897268ba791
    Author: Eric Andersen <andersen@codepoet.org>
    Date:   Fri Apr 26 22:01:43 2002 +0000
    
        With this update, everything now works as expected.
         -Erik
    
    Seedrng needs persistent storage, but mount -a is run before executing the
    init scripts, so S01 should be as good as S20 - Atleast with the scripts in
    upstream Buildroot.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/urandom-scripts/urandom-scripts.mk
+++ package/urandom-scripts/urandom-scripts.mk
@@ -5,8 +5,8 @@
 ################################################################################
 
 define URANDOM_SCRIPTS_INSTALL_INIT_SYSV
-	$(INSTALL) -D -m 0755 $(URANDOM_SCRIPTS_PKGDIR)/S20seedrng \
-		$(TARGET_DIR)/etc/init.d/S20seedrng
+	$(INSTALL) -D -m 0755 $(URANDOM_SCRIPTS_PKGDIR)/S01seedrng \
+		$(TARGET_DIR)/etc/init.d/S01seedrng
 endef
 
 $(eval $(generic-package))
