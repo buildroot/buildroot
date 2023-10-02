5a0f41e56dd48ddd605d92e0571f86d1f4b40149
Saeed Kazemi <kazemi.ms@gmail.com>
2023-10-02 19:36:28 +0200

    package/eza: new package
    eza is a modern, maintained replacement for ls, built on exa.
    
    https://github.com/eza-community/eza.git
    
    Signed-off-by: Saeed Kazemi <kazemi.ms@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/eza/eza.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# eza
+#
+################################################################################
+
+EZA_VERSION = 0.14.0
+EZA_SITE = $(call github,eza-community,eza,v$(EZA_VERSION))
+EZA_LICENSE = MIT
+EZA_LICENSE_FILES = LICENCE
+
+$(eval $(cargo-package))
