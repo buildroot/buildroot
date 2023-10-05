8519de517e222c36108f0b573585de3057865ba7
Peter Korsgaard <peter@korsgaard.com>
2023-10-05 21:43:12 +0200

    package/{glibc, localedef}: security bump to version glibc-2.38-27-g750a45a783906a19591fb8ff6b7841470f1f5701
    Fixes the following security issues:
    
      CVE-2023-4527: If the system is configured in no-aaaa mode via
      /etc/resolv.conf, getaddrinfo is called for the AF_UNSPEC address
      family, and a DNS response is received over TCP that is larger than
      2048 bytes, getaddrinfo may potentially disclose stack contents via
      the returned address data, or crash.
    
      CVE-2023-4806: When an NSS plugin only implements the
      _gethostbyname2_r and _getcanonname_r callbacks, getaddrinfo could use
      memory that was freed during buffer resizing, potentially causing a
      crash or read or write to arbitrary memory.
    
      CVE-2023-5156: The fix for CVE-2023-4806 introduced a memory leak when
      an application calls getaddrinfo for AF_INET6 with AI_CANONNAME,
      AI_ALL and AI_V4MAPPED flags set.
    
      CVE-2023-4911: If a tunable of the form NAME=NAME=VAL is passed in the
      environment of a setuid program and NAME is valid, it may result in a
      buffer overflow, which could be exploited to achieve escalated
      privileges.  This flaw was introduced in glibc 2.34.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -7,7 +7,7 @@
 # Generate version string using:
 #   git describe --match 'glibc-*' --abbrev=40 origin/release/MAJOR.MINOR/master | cut -d '-' -f 2-
 # When updating the version, please also update localedef
-GLIBC_VERSION = 2.38-13-g92201f16cbcfd9eafe314ef6654be2ea7ba25675
+GLIBC_VERSION = 2.38-27-g750a45a783906a19591fb8ff6b7841470f1f5701
 # Upstream doesn't officially provide an https download link.
 # There is one (https://sourceware.org/git/glibc.git) but it's not reliable,
 # sometimes the connection times out. So use an unofficial github mirror.
