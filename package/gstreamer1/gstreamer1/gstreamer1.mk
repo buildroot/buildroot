133ced8e02f00820f6e83effa5ffac1d8a6c7200
Peter Korsgaard <peter@korsgaard.com>
2023-10-06 21:10:38 +0200

    package/gstreamer1: bump to version 1.22.6
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gstreamer1/gstreamer1/gstreamer1.mk
+++ package/gstreamer1/gstreamer1/gstreamer1.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GSTREAMER1_VERSION = 1.22.2
+GSTREAMER1_VERSION = 1.22.6
 GSTREAMER1_SOURCE = gstreamer-$(GSTREAMER1_VERSION).tar.xz
 GSTREAMER1_SITE = https://gstreamer.freedesktop.org/src/gstreamer
 GSTREAMER1_INSTALL_STAGING = YES
