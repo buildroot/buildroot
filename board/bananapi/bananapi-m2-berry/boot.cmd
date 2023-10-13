7aa75f9d16576a3206bf4ee54248dfa035522ece
Giulio Benetti <giulio.benetti@benettiengineering.com>
2023-10-13 23:08:40 +0200

    board/bananapi/bananapi-m2-berry: switch to use extlinux.conf
    While switching bump Linux version to 6.1.57 and U-Boot version to
    2023.10.
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- board/bananapi/bananapi-m2-berry/boot.cmd
+++
@@ -1,7 +0,0 @@
-setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p2 rootwait
-
-mmc dev 0
-fatload mmc 0 $kernel_addr_r zImage
-fatload mmc 0 $fdt_addr_r sun8i-v40-bananapi-m2-berry.dtb
-
-bootz $kernel_addr_r - $fdt_addr_r
