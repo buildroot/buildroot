e60fde3451236545c7ffc39d4885106507121730
Bernd Kuhls <bernd@kuhls.net>
2023-10-15 23:05:48 +0200

    package/ethtool: switch to git snapshot
    Upstream tarball does not include several header files leading to build
    errors with older kernels. For details see:
    https://patchwork.kernel.org/project/netdevbpf/patch/20231005163053.1800218-1-florian.fainelli@broadcom.com/
    
    Switch _SITE to git snapshot and add _AUTORECONF = YES which needs
    host-pkgconf to fix subsequent configure error:
    
    ./configure: line 4723: PKG_PROG_PKG_CONFIG: command not found
    
    ./configure: line 5055: syntax error near unexpected token `MNL,'
    ./configure: line 5055: `    PKG_CHECK_MODULES(MNL, libmnl)'
    
    Fixes:
    http://autobuild.buildroot.net/results/69e/69e01b7f370920630d132fc43a40104fa5b3959f/
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Reviewed-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/ethtool/ethtool.mk
+++ package/ethtool/ethtool.mk
@@ -5,8 +5,11 @@
 ################################################################################
 
 ETHTOOL_VERSION = 6.5
-ETHTOOL_SOURCE = ethtool-$(ETHTOOL_VERSION).tar.xz
-ETHTOOL_SITE = $(BR2_KERNEL_MIRROR)/software/network/ethtool
+ETHTOOL_SITE = https://git.kernel.org/pub/scm/network/ethtool/ethtool.git/snapshot
+# needed only for autoreconf
+ETHTOOL_DEPENDENCIES = host-pkgconf
+# GIT version, shipped without configure
+ETHTOOL_AUTORECONF = YES
 ETHTOOL_LICENSE = GPL-2.0
 ETHTOOL_LICENSE_FILES = LICENSE COPYING
 ETHTOOL_CPE_ID_VENDOR = kernel
