087e2cef0e96609032e80c44d54fa7c4021277d8
Giulio Benetti <giulio.benetti@benettiengineering.com>
2023-10-15 21:48:00 +0200

    package/libnvme: bump to version 1.6
    Add 2 upstream patches, one to avoid build failure due to different libc
    ioctl() prototype and one to avoid link conflict due to test unit enabled
    by default.
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/libnvme/libnvme.mk
+++ package/libnvme/libnvme.mk
@@ -4,11 +4,14 @@
 #
 ################################################################################
 
-LIBNVME_VERSION = 1.5
+LIBNVME_VERSION = 1.6
 LIBNVME_SITE = $(call github,linux-nvme,libnvme,v$(LIBNVME_VERSION))
 LIBNVME_LICENSE = LGPL-2.1
 LIBNVME_LICENSE_FILES = COPYING
 LIBNVME_INSTALL_STAGING = YES
+
+LIBNVME_CONF_OPTS += \
+	-Dtests=false
 
 ifeq ($(BR2_PACKAGE_PYTHON3),y)
 LIBNVME_DEPENDENCIES += python3
