2c2d01ac0ffc3d5c61a962dfe85b7f865b362903
Gaël PORTAY <gael.portay@rtone.fr>
2023-10-20 11:09:49 +0200

    package/libxmlrpc: bump version to 1.60.01 (code/advanced@r3176)
    Signed-off-by: Gaël PORTAY <gael.portay@rtone.fr>
    [yann.morin.1998@free.fr: bump to 01.60.01 bug fix with openssl]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/libxmlrpc/libxmlrpc.mk
+++ package/libxmlrpc/libxmlrpc.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-# 1.58.02 (code/advanced@r3119)
-LIBXMLRPC_VERSION = r3119
+# 1.60.01 (code/advanced@r3176)
+LIBXMLRPC_VERSION = r3176
 LIBXMLRPC_SITE = https://svn.code.sf.net/p/xmlrpc-c/code/advanced
 LIBXMLRPC_SITE_METHOD = svn
 LIBXMLRPC_LICENSE = BSD-3-Clause (xml-rpc main code and abyss web server), BSD like (lib/expat), Python 1.5.2 license (parts of xmlrpc_base64.c)
