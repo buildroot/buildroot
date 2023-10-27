f2a590750f5bedcee48ce7beb8f35356b42eda11
Peter Korsgaard <peter@korsgaard.com>
2023-10-27 14:25:50 +0200

    package/aufs: use HTTPS for git.code.sf.net
    git.code.sf.net is available over HTTPS, so use that for security and
    consistency with the other packages.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/aufs/aufs.mk
+++ package/aufs/aufs.mk
@@ -9,7 +9,7 @@ AUFS_LICENSE = GPL-2.0
 AUFS_LICENSE_FILES = COPYING
 
 ifeq ($(BR2_PACKAGE_AUFS_SERIES),3)
-AUFS_SITE = http://git.code.sf.net/p/aufs/aufs3-standalone
+AUFS_SITE = https://git.code.sf.net/p/aufs/aufs3-standalone
 AUFS_SITE_METHOD = git
 else ifeq ($(BR2_PACKAGE_AUFS_SERIES),4)
 AUFS_SITE = $(call github,sfjro,aufs4-standalone,$(AUFS_VERSION))
