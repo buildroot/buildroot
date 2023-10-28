cf746901dd59e4de56114ea9e9250e155d11231b
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-10-28 18:42:29 +0200

    package/powerpc-utils: bump to version 1.3.11
    https://github.com/ibm-power-utilities/powerpc-utils/blob/v1.3.11/Changelog
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/powerpc-utils/powerpc-utils.mk
+++ package/powerpc-utils/powerpc-utils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-POWERPC_UTILS_VERSION = 1.3.10
+POWERPC_UTILS_VERSION = 1.3.11
 POWERPC_UTILS_SITE = $(call github,ibm-power-utilities,powerpc-utils,v$(POWERPC_UTILS_VERSION))
 POWERPC_UTILS_DEPENDENCIES = zlib numactl
 POWERPC_UTILS_AUTORECONF = YES
