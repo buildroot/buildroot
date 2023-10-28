c3fcaeaaa7fc0a6023d44ac16c9197b847504706
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-10-28 21:41:19 +0200

    package/rabbitmq-c: fix CVE-2023-35789
    An issue was discovered in the C AMQP client library (aka rabbitmq-c)
    through 0.13.0 for RabbitMQ. Credentials can only be entered on the
    command line (e.g., for amqp-publish or amqp-consume) and are thus
    visible to local attackers by listing a process and its arguments.
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/rabbitmq-c/rabbitmq-c.mk
+++ package/rabbitmq-c/rabbitmq-c.mk
@@ -14,6 +14,9 @@ RABBITMQ_C_CONF_OPTS = \
 	-DBUILD_API_DOCS=OFF \
 	-DBUILD_TOOLS_DOCS=OFF
 
+# 0001-Add-option-to-read-username-password-from-file.patch
+RABBITMQ_C_IGNORE_CVES += CVE-2023-35789
+
 # BUILD_SHARED_LIBS is handled in pkg-cmake.mk as it is a generic cmake variable
 ifeq ($(BR2_SHARED_STATIC_LIBS),y)
 RABBITMQ_C_CONF_OPTS += -DBUILD_STATIC_LIBS=ON
