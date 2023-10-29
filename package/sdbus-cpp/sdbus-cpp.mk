651b65381494fed9ce6acc23b1805e70b7f93146
Sergey Bobrenok <bobrofon@gmail.com>
2023-10-29 13:11:25 +0300

    package/sdbus-cpp: remove redundant SDBUS_CPP_SOURCE variable
    There is no need to define SDBUS_CPP_SOURCE explicitly because the
    github helper works perfectly well with the default _SOURCE value.
    
    Signed-off-by: Sergey Bobrenok <bobrofon@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/sdbus-cpp/sdbus-cpp.mk
+++ package/sdbus-cpp/sdbus-cpp.mk
@@ -5,7 +5,6 @@
 ################################################################################
 
 SDBUS_CPP_VERSION = 1.4.0
-SDBUS_CPP_SOURCE = v$(SDBUS_CPP_VERSION).tar.gz
 SDBUS_CPP_SITE = $(call github,Kistler-Group,sdbus-cpp,v$(SDBUS_CPP_VERSION))
 SDBUS_CPP_INSTALL_STAGING = YES
 SDBUS_CPP_DEPENDENCIES = host-pkgconf systemd
