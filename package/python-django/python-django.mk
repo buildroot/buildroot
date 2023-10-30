ec7785adc8e9c03e651d879341d1a78391cc8232
Peter Korsgaard <peter@korsgaard.com>
2023-10-30 08:35:24 +0100

    package/python-django: security bump to version 4.1.12
    Fixes the following security issue:
    
    CVE-2023-43665: Denial-of-service possibility in django.utils.text.Truncator
    
    Following the fix for CVE-2019-14232, the regular expressions used in the
    implementation of django.utils.text.Truncator’s chars() and words() methods
    (with html=True) were revised and improved.  However, these regular
    expressions still exhibited linear backtracking complexity, so when given a
    very long, potentially malformed HTML input, the evaluation would still be
    slow, leading to a potential denial of service vulnerability.
    
    The chars() and words() methods are used to implement the truncatechars_html
    and truncatewords_html template filters, which were thus also vulnerable.
    
    The input processed by Truncator, when operating in HTML mode, has been
    limited to the first five million characters in order to avoid potential
    performance and memory issues.
    
    https://www.djangoproject.com/weblog/2023/oct/04/security-releases/
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 4.1.10
+PYTHON_DJANGO_VERSION = 4.1.12
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/70/d4/eded564fa5928f68771d082ec0eef4d023f9d19dfa1d2923305bc3e62afe
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/68/76/b1cf87eda3bf5b2d7dd9a23bb4df70ea5511e426a54a04ed2c4ca2ff67cd
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
 PYTHON_DJANGO_CPE_ID_VENDOR = djangoproject
