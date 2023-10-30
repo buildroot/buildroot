6f1e83065c7cbc99efcbc67d9d2b6822750aeadb
Peter Korsgaard <peter@korsgaard.com>
2023-10-30 22:52:24 +0100

    package/python-urllib3: security bump to version 1.26.18
    Fixes CVE-2023-43804 and CVE-2023-45803
    
    https://github.com/urllib3/urllib3/security/advisories/GHSA-v845-jxx5-vc9f
    https://github.com/urllib3/urllib3/security/advisories/GHSA-g4mx-q9vg-27p4
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-urllib3/python-urllib3.mk
+++ package/python-urllib3/python-urllib3.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_URLLIB3_VERSION = 1.26.13
+PYTHON_URLLIB3_VERSION = 1.26.18
 PYTHON_URLLIB3_SOURCE = urllib3-$(PYTHON_URLLIB3_VERSION).tar.gz
-PYTHON_URLLIB3_SITE = https://files.pythonhosted.org/packages/c2/51/32da03cf19d17d46cce5c731967bf58de9bd71db3a379932f53b094deda4
+PYTHON_URLLIB3_SITE = https://files.pythonhosted.org/packages/0c/39/64487bf07df2ed854cc06078c27c0d0abc59bd27b32232876e403c333a08
 PYTHON_URLLIB3_LICENSE = MIT
 PYTHON_URLLIB3_LICENSE_FILES = LICENSE.txt
 PYTHON_URLLIB3_CPE_ID_VENDOR = python
