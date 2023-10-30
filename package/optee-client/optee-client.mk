8a8fa2006846cd3480741f03372e17fedb811898
Etienne Carriere <etienne.carriere@foss.st.com>
2023-10-30 10:29:59 +0100

    package/optee-client: bump to version 4.0.0
    Bumps OP-TEE client package version to OP-TEE release 4.0.0.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/optee-client/optee-client.mk
+++ package/optee-client/optee-client.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPTEE_CLIENT_VERSION = 3.21.0
+OPTEE_CLIENT_VERSION = 4.0.0
 OPTEE_CLIENT_SITE = $(call github,OP-TEE,optee_client,$(OPTEE_CLIENT_VERSION))
 OPTEE_CLIENT_LICENSE = BSD-2-Clause
 OPTEE_CLIENT_LICENSE_FILES = LICENSE
