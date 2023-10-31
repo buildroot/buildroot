9705d300266d9b0332f898b2517fad67b586e9d6
TIAN Yuanhao <tianyuanhao3@163.com>
2023-10-31 15:50:29 +0000

    package/tini: move docker-init to /usr/libexec/docker/
    The docker-init is not intended to be a user-facing command, and as such
    it is more appropriate for it to be found in /usr/libexec/ than in $PATH.
    
    See:
    https://github.com/moby/moby/commit/6caaa8cadc9e4f1e122b7b2bb4451500bbec6086
    https://github.com/docker/docker-ce-packaging/commit/5a998af6f53d978810aa6f5b566a484911ff4ca2
    
    Signed-off-by: TIAN Yuanhao <tianyuanhao3@163.com>
    [yann.morin.1998@free.fr: use mkdir -p, not install -d]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/tini/tini.mk
+++ package/tini/tini.mk
@@ -31,7 +31,8 @@ endef
 
 define TINI_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/bin/tini $(TARGET_DIR)/usr/bin/tini
-	ln -sf tini $(TARGET_DIR)/usr/bin/docker-init
+	mkdir -p $(TARGET_DIR)/usr/libexec/docker
+	ln -sf ../../bin/tini $(TARGET_DIR)/usr/libexec/docker/docker-init
 endef
 
 # Tini's CMakeLists.txt is not suitable for Buildroot.
