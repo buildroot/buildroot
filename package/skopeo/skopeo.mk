619b83a4438667cdb4cc8a7dddc79dbf6d84ec2a
Yann E. MORIN <yann.morin@orange.com>
2023-11-02 17:09:27 +0100

    package/skopeo: new host package
    skopeo is a command line utility that performs various operations on
    container images and image repositories.
    
    We introduce it as a host-only package, as the expected usage is to
    transform an OCI image into a docker image, or to help in uploading an
    OCI image to a (local) docker daemon or to a docker registry, for
    example, in a post-build script.
    
    As such, it needs a prompt in the host packages section.
    
    Signed-off-by: Yann E. MORIN <yann.morin@orange.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/skopeo/skopeo.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# skopeo
+#
+################################################################################
+
+SKOPEO_VERSION = 1.13.3
+SKOPEO_SITE = $(call github,containers,skopeo,v$(SKOPEO_VERSION))
+
+SKOPEO_LICENSE = Apache-2.0
+SKOPEO_LICENSE_FILES = LICENSE
+
+HOST_SKOPEO_DEPENDENCIES = \
+	host-btrfs-progs \
+	host-libgpgme \
+	host-lvm2 \
+	host-pkgconf
+
+HOST_SKOPEO_GO_ENV = PKG_CONFIG_PATH=$(HOST_DIR)/lib/pkgconfig
+HOST_SKOPEO_BUILD_TARGETS = cmd/skopeo
+
+$(eval $(host-golang-package))
