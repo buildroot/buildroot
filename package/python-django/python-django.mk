be4347c0889232a70b7b944d6e9e7e48259bfd9d
Adam Duskett <adam.duskett@amarulasolutions.com>
2023-11-03 12:44:28 -0600

    package/python-django: security bump to 4.2.7
    - Fixed a regression in Django 4.2 that caused a crash of QuerySet.aggregate()
       with aggregates referencing expressions containing subqueries (#34798).
    
     - Restored, following a regression in Django 4.2, creating
       varchar/text_pattern_ops indexes on CharField and TextField with
       deterministic collations on PostgreSQL (#34932).
    
    Tested on Fedora 39 with:
    ./support/testing/run-tests tests.package.test_python_django
    
    Signed-off-by: Adam Duskett <adam.duskett@amarulasolutions.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 4.2.6
+PYTHON_DJANGO_VERSION = 4.2.7
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/23/7b/f47d10d870fabfcaa1fba403460a4e482ab7dbba4d715d43981d1f8c8d85
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/5c/62/0c6ab2f3ac9a242b4562b6be1c418685fa7d1ccb8ca302cdb97e0b23cf4b
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
 PYTHON_DJANGO_CPE_ID_VENDOR = djangoproject
