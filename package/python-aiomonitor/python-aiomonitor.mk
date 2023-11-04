5874a6e15fcbaf90f63ef6f9b8c2d993eee27f9d
James Hilliard <james.hilliard1@gmail.com>
2023-11-04 17:43:37 -0600

    package/python-aiomonitor: bump to version 0.6.0
    Add new host-python-setuptools-scm build dependency.
    
    License hash changed due to line ending removal:
    https://github.com/aio-libs/aiomonitor/commit/9ec9910145c70037da8ab52e007b9fc286c3a4c7
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/python-aiomonitor/python-aiomonitor.mk
+++ package/python-aiomonitor/python-aiomonitor.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_AIOMONITOR_VERSION = 0.4.5
+PYTHON_AIOMONITOR_VERSION = 0.6.0
 PYTHON_AIOMONITOR_SOURCE = aiomonitor-$(PYTHON_AIOMONITOR_VERSION).tar.gz
-PYTHON_AIOMONITOR_SITE = https://files.pythonhosted.org/packages/98/76/b62e9fbe267287527fb6f4b6774394d4f00650195774173bb0055a99ab3d
+PYTHON_AIOMONITOR_SITE = https://files.pythonhosted.org/packages/13/ab/36fbaf36c1c7cb4d5a81b945faf69e95a48dd38cbc9b57915d41d9445f55
 PYTHON_AIOMONITOR_SETUP_TYPE = setuptools
 PYTHON_AIOMONITOR_LICENSE = Apache-2.0
 PYTHON_AIOMONITOR_LICENSE_FILES = LICENSE
+PYTHON_AIOMONITOR_DEPENDENCIES = host-python-setuptools-scm
 
 $(eval $(python-package))
