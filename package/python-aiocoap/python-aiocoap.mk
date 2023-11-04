ae5274dd545eb011f18cae24331300d944b8be0f
James Hilliard <james.hilliard1@gmail.com>
2023-11-04 17:15:06 -0600

    package/python-aiocoap: bump to version 0.4.7
    License file moved:
    https://github.com/chrysn/aiocoap/commit/8f2e96d76bcf1f6f8d7b8065b5c5865afa50d20f
    https://github.com/chrysn/aiocoap/commit/065945fb7f34db33f641199aceb527527c1a3431
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-aiocoap/python-aiocoap.mk
+++ package/python-aiocoap/python-aiocoap.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_AIOCOAP_VERSION = 0.4.5
+PYTHON_AIOCOAP_VERSION = 0.4.7
 PYTHON_AIOCOAP_SOURCE = aiocoap-$(PYTHON_AIOCOAP_VERSION).tar.gz
-PYTHON_AIOCOAP_SITE = https://files.pythonhosted.org/packages/65/63/9051af6a2fc498f50b71f23bdbde0cc1d1f0a69eb767776dbf6d1411e240
+PYTHON_AIOCOAP_SITE = https://files.pythonhosted.org/packages/de/14/843232b56c0f09b2ceddae3da37598d9109c4b1d9383b1ab72232018e9e8
 PYTHON_AIOCOAP_SETUP_TYPE = setuptools
 PYTHON_AIOCOAP_LICENSE = MIT
-PYTHON_AIOCOAP_LICENSE_FILES = LICENSE
+PYTHON_AIOCOAP_LICENSE_FILES = doc/LICENSE.rst
 
 $(eval $(python-package))
