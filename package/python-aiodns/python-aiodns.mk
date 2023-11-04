4be2da2cef371faff39ceb7fa28b948945016fc9
James Hilliard <james.hilliard1@gmail.com>
2023-11-04 17:18:51 -0600

    package/python-aiodns: bump to version 3.1.1
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/python-aiodns/python-aiodns.mk
+++ package/python-aiodns/python-aiodns.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_AIODNS_VERSION = 3.0.0
+PYTHON_AIODNS_VERSION = 3.1.1
 PYTHON_AIODNS_SOURCE = aiodns-$(PYTHON_AIODNS_VERSION).tar.gz
-PYTHON_AIODNS_SITE = https://files.pythonhosted.org/packages/27/79/df72e25df0fdd9bf5a5ab068539731d27c5f2ae5654621ae0c92ceca94cf
+PYTHON_AIODNS_SITE = https://files.pythonhosted.org/packages/fa/10/4de99e6e67703d8f6b10ea92a4d2a6c5b96a9c0708b75389a00203387925
 PYTHON_AIODNS_SETUP_TYPE = setuptools
 PYTHON_AIODNS_LICENSE = MIT
 PYTHON_AIODNS_LICENSE_FILES = LICENSE
