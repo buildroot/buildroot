64fc2dd727ba07db1a3494a2aeafc79d171f1780
James Hilliard <james.hilliard1@gmail.com>
2023-11-05 01:55:46 -0700

    package/python-jsonschema: bump to version 4.19.2
    Drop no longer required python-pyrsistent runtime dependency.
    
    Add new python-jsonschema-specifications runtime dependency.
    
    Add new python-referencing runtime dependency.
    
    Add new python-rpds-py runtime dependency.
    
    Propagate python-rpds-py target rust support requirement to
    python-jsonschema reverse dependencies.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/python-jsonschema/python-jsonschema.mk
+++ package/python-jsonschema/python-jsonschema.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_JSONSCHEMA_VERSION = 4.17.3
+PYTHON_JSONSCHEMA_VERSION = 4.19.2
 PYTHON_JSONSCHEMA_SOURCE = jsonschema-$(PYTHON_JSONSCHEMA_VERSION).tar.gz
-PYTHON_JSONSCHEMA_SITE = https://files.pythonhosted.org/packages/36/3d/ca032d5ac064dff543aa13c984737795ac81abc9fb130cd2fcff17cfabc7
+PYTHON_JSONSCHEMA_SITE = https://files.pythonhosted.org/packages/95/18/618159fb2efbe3fb2cd32b16c40278954cde94744957734ef0482286a052
 PYTHON_JSONSCHEMA_SETUP_TYPE = pep517
 PYTHON_JSONSCHEMA_LICENSE = MIT
 PYTHON_JSONSCHEMA_LICENSE_FILES = COPYING json/LICENSE
