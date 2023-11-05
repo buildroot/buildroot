18abda1791e3ca09f96b5ca106431c5d3bce8673
James Hilliard <james.hilliard1@gmail.com>
2023-11-05 01:26:52 -0600

    package/python-gunicorn: bump to version 21.2.0
    Drop python-setuptools runtime dependency.
    
    Add new python-packaging runtime dependency.
    
    License hash changed due to date update:
    https://github.com/benoitc/gunicorn/commit/378f0d04ec390a754cb819f095bf65bfcc04ed99
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-gunicorn/python-gunicorn.mk
+++ package/python-gunicorn/python-gunicorn.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_GUNICORN_VERSION = 20.1.0
+PYTHON_GUNICORN_VERSION = 21.2.0
 PYTHON_GUNICORN_SOURCE = gunicorn-$(PYTHON_GUNICORN_VERSION).tar.gz
-PYTHON_GUNICORN_SITE = https://files.pythonhosted.org/packages/28/5b/0d1f0296485a6af03366604142ea8f19f0833894db3512a40ed07b2a56dd
+PYTHON_GUNICORN_SITE = https://files.pythonhosted.org/packages/06/89/acd9879fa6a5309b4bf16a5a8855f1e58f26d38e0c18ede9b3a70996b021
 PYTHON_GUNICORN_SETUP_TYPE = setuptools
 PYTHON_GUNICORN_LICENSE = MIT
 PYTHON_GUNICORN_LICENSE_FILES = LICENSE
