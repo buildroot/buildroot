ee389788af8e454251cbdb24d03710b3e1d83abc
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-11-05 23:06:01 +0100

    package/librtas: bump to version 2.0.5
    - Drop patch (already in version)
    - host-pkgconf is a mandatory dependency to run autoreconf (even when
      cmocka-based tests are disabled) since
      https://github.com/ibm-power-utilities/librtas/commit/b62ecd42180d7ac7b32cac6a50802455850a857a
    
    https://github.com/ibm-power-utilities/librtas/blob/v2.0.5/Changelog
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/librtas/librtas.mk
+++ package/librtas/librtas.mk
@@ -4,12 +4,13 @@
 #
 ################################################################################
 
-LIBRTAS_VERSION = 2.0.3
+LIBRTAS_VERSION = 2.0.5
 LIBRTAS_SITE = $(call github,ibm-power-utilities,librtas,v$(LIBRTAS_VERSION))
 LIBRTAS_LICENSE = LGPL-2.1+
 LIBRTAS_LICENSE_FILES = COPYING.LESSER
 LIBRTAS_INSTALL_STAGING = YES
 # From git
 LIBRTAS_AUTORECONF = YES
+LIBRTAS_DEPENDENCIES = host-pkgconf
 
 $(eval $(autotools-package))
