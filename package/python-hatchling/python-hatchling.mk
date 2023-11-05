013061755d928e96399c0e4e964a03dc623a8425
James Hilliard <james.hilliard1@gmail.com>
2023-11-04 23:45:01 -0600

    package/python-hatchling: bump to version 1.18.0
    Add new host-python-trove-classifiers build dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/python-hatchling/python-hatchling.mk
+++ package/python-hatchling/python-hatchling.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_HATCHLING_VERSION = 1.12.2
+PYTHON_HATCHLING_VERSION = 1.18.0
 PYTHON_HATCHLING_SOURCE = hatchling-$(PYTHON_HATCHLING_VERSION).tar.gz
-PYTHON_HATCHLING_SITE = https://files.pythonhosted.org/packages/f5/ea/3ed5a7ecdd8a8f7e84cffd3b5ec24279a09ce2694c218ff922c35c6f1a77
+PYTHON_HATCHLING_SITE = https://files.pythonhosted.org/packages/e3/57/87da2c5adc173950ebe9f1acce4d5f2cd0a960783992fd4879a899a0b637
 PYTHON_HATCHLING_LICENSE = MIT
 PYTHON_HATCHLING_LICENSE_FILES = LICENSE.txt
 PYTHON_HATCHLING_SETUP_TYPE = pep517
@@ -14,6 +14,7 @@ HOST_PYTHON_HATCHLING_DEPENDENCIES = \
 	host-python-editables \
 	host-python-packaging \
 	host-python-pathspec \
-	host-python-pluggy
+	host-python-pluggy \
+	host-python-trove-classifiers
 
 $(eval $(host-python-package))
