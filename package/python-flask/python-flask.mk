219ba0034ce150456ccbf8772c3c7b7831105c06
James Hilliard <james.hilliard1@gmail.com>
2023-11-05 00:46:42 -0600

    package/python-flask: bump to version 3.0.0
    Migrate from setuptools to flit build backend.
    
    Remove python-setuptools runtime dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-flask/python-flask.mk
+++ package/python-flask/python-flask.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_FLASK_VERSION = 2.3.2
-PYTHON_FLASK_SOURCE = Flask-$(PYTHON_FLASK_VERSION).tar.gz
-PYTHON_FLASK_SITE = https://files.pythonhosted.org/packages/4d/00/ef81c18da32fdfcde6381c315f4b11597fb6691180a330418848efee0ae7
-PYTHON_FLASK_SETUP_TYPE = setuptools
+PYTHON_FLASK_VERSION = 3.0.0
+PYTHON_FLASK_SOURCE = flask-$(PYTHON_FLASK_VERSION).tar.gz
+PYTHON_FLASK_SITE = https://files.pythonhosted.org/packages/d8/09/c1a7354d3925a3c6c8cfdebf4245bae67d633ffda1ba415add06ffc839c5
+PYTHON_FLASK_SETUP_TYPE = flit
 PYTHON_FLASK_LICENSE = BSD-3-Clause
 PYTHON_FLASK_LICENSE_FILES = LICENSE.rst docs/license.rst
 PYTHON_FLASK_CPE_ID_VENDOR = palletsprojects
