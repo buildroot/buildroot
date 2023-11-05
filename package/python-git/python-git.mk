8bd4d7ed63b31d5d90c5ab2d5ddec3da96623604
James Hilliard <james.hilliard1@gmail.com>
2023-11-05 01:02:38 -0600

    package/python-git: bump to version 3.1.40
    Drop no longer required python-typing-extensions dependency.
    
    Switch to pypi based source download.
    
    License hash changed due to formatting tweaks:
    https://github.com/gitpython-developers/GitPython/commit/e1af18377fd69f9c1007f8abf6ccb95b3c5a6558
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-git/python-git.mk
+++ package/python-git/python-git.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-PYTHON_GIT_VERSION = 3.1.24
-PYTHON_GIT_SITE = $(call github,gitpython-developers,GitPython,$(PYTHON_GIT_VERSION))
+PYTHON_GIT_VERSION = 3.1.40
+PYTHON_GIT_SOURCE = GitPython-$(PYTHON_GIT_VERSION).tar.gz
+PYTHON_GIT_SITE = https://files.pythonhosted.org/packages/0d/b2/37265877ae607a2cbf9a471f4581dbf5ed13a501b90cb4c773f9ccfff3ea
 PYTHON_GIT_LICENSE = BSD-3-Clause
 PYTHON_GIT_LICENSE_FILES = LICENSE
 PYTHON_GIT_SETUP_TYPE = setuptools
