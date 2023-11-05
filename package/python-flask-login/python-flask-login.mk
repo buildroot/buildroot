f650ad66d013506397d7a539c1686473e728ed73
James Hilliard <james.hilliard1@gmail.com>
2023-11-05 00:13:47 -0600

    package/python-flask-login: bump to version 0.6.3
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Reviewed-by: Sébastien Szymanski <sebastien.szymanski@armadeus.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-flask-login/python-flask-login.mk
+++ package/python-flask-login/python-flask-login.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_FLASK_LOGIN_VERSION = 0.6.2
+PYTHON_FLASK_LOGIN_VERSION = 0.6.3
 PYTHON_FLASK_LOGIN_SOURCE = Flask-Login-$(PYTHON_FLASK_LOGIN_VERSION).tar.gz
-PYTHON_FLASK_LOGIN_SITE = https://files.pythonhosted.org/packages/cc/da/eae45ba9ec58af45b46ef94c6ca04fb211ee57c06421b696e894eb11b064
+PYTHON_FLASK_LOGIN_SITE = https://files.pythonhosted.org/packages/c3/6e/2f4e13e373bb49e68c02c51ceadd22d172715a06716f9299d9df01b6ddb2
 PYTHON_FLASK_LOGIN_LICENSE = MIT
 PYTHON_FLASK_LOGIN_LICENSE_FILES = LICENSE
 PYTHON_FLASK_LOGIN_SETUP_TYPE = setuptools
