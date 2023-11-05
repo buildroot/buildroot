b9cbe43f38185ef074472cee603b68942fe983d2
James Hilliard <james.hilliard1@gmail.com>
2023-11-04 21:20:04 -0600

    package/python-construct: bump to version 2.10.69
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-construct/python-construct.mk
+++ package/python-construct/python-construct.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_CONSTRUCT_VERSION = 2.10.68
+PYTHON_CONSTRUCT_VERSION = 2.10.69
 PYTHON_CONSTRUCT_SOURCE = construct-$(PYTHON_CONSTRUCT_VERSION).tar.gz
-PYTHON_CONSTRUCT_SITE = https://files.pythonhosted.org/packages/e0/b7/a4a032e94bcfdff481f2e6fecd472794d9da09f474a2185ed33b2c7cad64
+PYTHON_CONSTRUCT_SITE = https://files.pythonhosted.org/packages/02/88/e34d7265863f3c96077aea24041b067d4646d77d596d979110eb94758b03
 PYTHON_CONSTRUCT_SETUP_TYPE = setuptools
 PYTHON_CONSTRUCT_LICENSE = MIT
 PYTHON_CONSTRUCT_LICENSE_FILES = LICENSE
