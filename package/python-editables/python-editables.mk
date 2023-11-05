7972b0394d9a32cac9c4fff1dc5f817b8897cdfe
James Hilliard <james.hilliard1@gmail.com>
2023-11-04 23:06:06 -0600

    package/python-editables: bump to version 0.5
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-editables/python-editables.mk
+++ package/python-editables/python-editables.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_EDITABLES_VERSION = 0.3
+PYTHON_EDITABLES_VERSION = 0.5
 PYTHON_EDITABLES_SOURCE = editables-$(PYTHON_EDITABLES_VERSION).tar.gz
-PYTHON_EDITABLES_SITE = https://files.pythonhosted.org/packages/01/b0/a2a87db4b6cb8e7d57004b6836faa634e0747e3e39ded126cdbe5a33ba36
+PYTHON_EDITABLES_SITE = https://files.pythonhosted.org/packages/37/4a/986d35164e2033ddfb44515168a281a7986e260d344cf369c3f52d4c3275
 PYTHON_EDITABLES_LICENSE = MIT
 PYTHON_EDITABLES_LICENSE_FILES = LICENSE.txt
 PYTHON_EDITABLES_SETUP_TYPE = setuptools
