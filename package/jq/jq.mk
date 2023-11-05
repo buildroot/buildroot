3026934aa0a1a6b375d11e322b72476a7f6aedaf
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-11-05 23:34:54 +0100

    package/jq: bump to version 1.7
    - Update site to get latest release
    - Drop autoreconf as official tarball is now used
    - _GNU_SOURCE is correctly set since
      https://github.com/jqlang/jq/commit/52d5988afb5999c71fd24484a93efefa2a9e9a8f
    - maintainer mode is disabled by default since
      https://github.com/jqlang/jq/commit/54fef09ac39d6fb003367d65709349c43078405e
    - Update hash of COPYING (BSD-2-Clause for Windows build using jv thread
      added by
      https://github.com/jqlang/jq/commit/34182cca7babec086bbc93dec209275be896ca9e,
      mispelling fixed by
      https://github.com/jqlang/jq/commit/5cebe86a7b90e5718077c5e1d5c2165939d3f3cb)
    
    https://github.com/jqlang/jq/releases/tag/jq-1.7
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/jq/jq.mk
+++ package/jq/jq.mk
@@ -4,25 +4,18 @@
 #
 ################################################################################
 
-JQ_VERSION = a17dd3248a666d01be75f6b16be37e80e20b0954
-JQ_SITE = $(call github,stedolan,jq,$(JQ_VERSION))
+JQ_VERSION = 1.7
+JQ_SITE = https://github.com/jqlang/jq/releases/download/jq-$(JQ_VERSION)
 JQ_LICENSE = MIT (code), ICU (decNumber), CC-BY-3.0 (documentation)
 JQ_LICENSE_FILES = COPYING
 JQ_CPE_ID_VENDOR = jq_project
 JQ_INSTALL_STAGING = YES
 
-# currently using git version directly
-JQ_AUTORECONF = YES
-
 # uses c99 specific features
-# _GNU_SOURCE added to fix gcc6+ host compilation
-# (https://github.com/stedolan/jq/issues/1598)
-JQ_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -std=c99 -D_GNU_SOURCE"
-HOST_JQ_CONF_ENV += CFLAGS="$(HOST_CFLAGS) -std=c99 -D_GNU_SOURCE"
+JQ_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -std=c99"
+HOST_JQ_CONF_ENV += CFLAGS="$(HOST_CFLAGS) -std=c99"
 
-# jq explicitly enables maintainer mode, which we don't need/want
-JQ_CONF_OPTS += --disable-maintainer-mode
-HOST_JQ_CONF_OPTS += --disable-maintainer-mode --without-oniguruma
+HOST_JQ_CONF_OPTS += --without-oniguruma
 
 ifeq ($(BR2_PACKAGE_ONIGURUMA),y)
 JQ_DEPENDENCIES += oniguruma
