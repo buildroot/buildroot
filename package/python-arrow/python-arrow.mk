7c4a6503723d37d5475f38b2d3004be243f01534
James Hilliard <james.hilliard1@gmail.com>
2023-11-04 18:28:52 -0600

    package/python-arrow: bump to version 1.3.0
    License hash changed due to date update:
    https://github.com/arrow-py/arrow/commit/de0aea98051f9d99f237e7edd27ae3e5c6bf1489
    
    Add new types-python-dateutil runtime dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-arrow/python-arrow.mk
+++ package/python-arrow/python-arrow.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ARROW_VERSION = 1.2.3
+PYTHON_ARROW_VERSION = 1.3.0
 PYTHON_ARROW_SOURCE = arrow-$(PYTHON_ARROW_VERSION).tar.gz
-PYTHON_ARROW_SITE = https://files.pythonhosted.org/packages/7f/c0/c601ea7811f422700ef809f167683899cdfddec5aa3f83597edf97349962
+PYTHON_ARROW_SITE = https://files.pythonhosted.org/packages/2e/00/0f6e8fcdb23ea632c866620cc872729ff43ed91d284c866b515c6342b173
 PYTHON_ARROW_SETUP_TYPE = setuptools
 PYTHON_ARROW_LICENSE = Apache-2.0
 PYTHON_ARROW_LICENSE_FILES = LICENSE
