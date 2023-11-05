3b2da40749392319d45869b50f2b8362987eff38
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-11-05 20:37:19 +0100

    package/system-config-printer: bump to version 1.5.18
    - Drop first patch (already in version)
    - Drop second patch (rejected): cups-config is deprecated in favor of pkg-config
    - Drop autoreconf (no more patches)
    - intltool has been replaced by gettext since
      https://github.com/OpenPrinting/system-config-printer/commit/e653c1a860aedc306828e60f4b99a80e0d8b3523
    
    https://github.com/OpenPrinting/system-config-printer/blob/v1.5.18/NEWS
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/system-config-printer/system-config-printer.mk
+++ package/system-config-printer/system-config-printer.mk
@@ -4,18 +4,13 @@
 #
 ################################################################################
 
-SYSTEM_CONFIG_PRINTER_VERSION = 1.5.15
+SYSTEM_CONFIG_PRINTER_VERSION = 1.5.18
 SYSTEM_CONFIG_PRINTER_SOURCE = system-config-printer-$(SYSTEM_CONFIG_PRINTER_VERSION).tar.xz
 SYSTEM_CONFIG_PRINTER_SITE = https://github.com/OpenPrinting/system-config-printer/releases/download/v$(SYSTEM_CONFIG_PRINTER_VERSION)
 SYSTEM_CONFIG_PRINTER_LICENSE = GPL-2.0+
 SYSTEM_CONFIG_PRINTER_LICENSE_FILES = COPYING
-SYSTEM_CONFIG_PRINTER_DEPENDENCIES = cups host-desktop-file-utils host-intltool host-pkgconf
-
-# 0001-Add-option-to-disable-xmlto-manual-generation.patch
-# 0002-configure-accept-non-system-cups-config.patch
-SYSTEM_CONFIG_PRINTER_AUTORECONF = YES
-
-SYSTEM_CONFIG_PRINTER_CONF_OPTS = --with-cups-config=$(STAGING_DIR)/usr/bin/cups-config
+SYSTEM_CONFIG_PRINTER_DEPENDENCIES = \
+	cups host-desktop-file-utils $(TARGET_NLS_DEPENDENCIES) host-pkgconf
 
 ifeq ($(BR2_PACKAGE_LIBGLIB2),y)
 SYSTEM_CONFIG_PRINTER_DEPENDENCIES += libglib2
