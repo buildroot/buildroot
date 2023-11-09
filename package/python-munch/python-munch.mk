ece72fffdce6eafc777235ea7b9d4b12dbdf61d9
James Hilliard <james.hilliard1@gmail.com>
2023-11-08 17:00:02 -0700

    package/python-munch: bump to version 4.0.0
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-munch/python-munch.mk
+++ package/python-munch/python-munch.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_MUNCH_VERSION = 2.5.0
+PYTHON_MUNCH_VERSION = 4.0.0
 PYTHON_MUNCH_SOURCE = munch-$(PYTHON_MUNCH_VERSION).tar.gz
-PYTHON_MUNCH_SITE = https://files.pythonhosted.org/packages/43/a1/ec48010724eedfe2add68eb7592a0d238590e14e08b95a4ffb3c7b2f0808
+PYTHON_MUNCH_SITE = https://files.pythonhosted.org/packages/e7/2b/45098135b5f9f13221820d90f9e0516e11a2a0f55012c13b081d202b782a
 PYTHON_MUNCH_SETUP_TYPE = setuptools
 PYTHON_MUNCH_LICENSE = MIT
 PYTHON_MUNCH_LICENSE_FILES = LICENSE.txt
