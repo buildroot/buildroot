a44d1a1252572bcb7638e5b832c24841303f4800
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2023-11-13 01:58:34 +0100

    package/zfs: bump version to 2.2.0
    Removed backported patch:
    - https://github.com/openzfs/zfs/commit/bc3f12bfac152a0c28951cec92340ba14f9ccee9.patch
    
    Updated ZFS test to pass this new version; drop the explicit /pool
    mountpoint option to rely on the default location (which happens to be
    /pool already).
    
    Signed-off-by: José Luis Salvador Rufo <salvador.joseluis@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    [yann.morin.1998@free.fr:
      - needed on master to further bump to a data-corruption fix
    ]
    (cherry picked from commit d153e58d13f262f96c6c7c9a2bc0d31b76c8973d)
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/zfs/zfs.mk
+++ package/zfs/zfs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ZFS_VERSION = 2.1.12
+ZFS_VERSION = 2.2.0
 ZFS_SITE = https://github.com/openzfs/zfs/releases/download/zfs-$(ZFS_VERSION)
 ZFS_SELINUX_MODULES = zfs
 ZFS_LICENSE = CDDL
