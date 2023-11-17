986a160cbe5c492acfda9bbf2048b1501d570072
Saeed Kazemi <kazemi.ms@gmail.com>
2023-11-17 19:32:16 +0100

    package/eza: bump to version 0.16.0
    Release notes:
    https://github.com/eza-community/eza/releases/tag/v0.16.0
    
    Signed-off-by: Saeed Kazemi <kazemi.ms@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/eza/eza.mk
+++ package/eza/eza.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-EZA_VERSION = 0.14.0
+EZA_VERSION = 0.16.0
 EZA_SITE = $(call github,eza-community,eza,v$(EZA_VERSION))
 EZA_LICENSE = MIT
 EZA_LICENSE_FILES = LICENCE
