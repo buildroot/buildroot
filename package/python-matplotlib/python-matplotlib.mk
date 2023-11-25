2b6c5314867ebf2f5e1370cacbfe139763bf30ec
James Hilliard <james.hilliard1@gmail.com>
2023-11-24 18:38:07 -0700

    package/python-matplotlib: bump to version 3.8.2
    Add new python-contourpy runtime dependency.
    
    Add new python-pybind dependency.
    
    Add new host-python-setuptools-scm build dependency.
    
    Update setup.cfg to new mplsetup.cfg install location.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-matplotlib/python-matplotlib.mk
+++ package/python-matplotlib/python-matplotlib.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_MATPLOTLIB_VERSION = 3.4.3
+PYTHON_MATPLOTLIB_VERSION = 3.8.2
 PYTHON_MATPLOTLIB_SOURCE = matplotlib-$(PYTHON_MATPLOTLIB_VERSION).tar.gz
-PYTHON_MATPLOTLIB_SITE = https://files.pythonhosted.org/packages/21/37/197e68df384ff694f78d687a49ad39f96c67b8d75718bc61503e1676b617
+PYTHON_MATPLOTLIB_SITE = https://files.pythonhosted.org/packages/fb/ab/38a0e94cb01dacb50f06957c2bed1c83b8f9dac6618988a37b2487862944
 PYTHON_MATPLOTLIB_LICENSE = Python-2.0
 PYTHON_MATPLOTLIB_LICENSE_FILES = LICENSE/LICENSE
 PYTHON_MATPLOTLIB_DEPENDENCIES = \
@@ -14,8 +14,10 @@ PYTHON_MATPLOTLIB_DEPENDENCIES = \
 	host-pkgconf \
 	host-python-certifi \
 	host-python-numpy \
+	host-python-setuptools-scm \
 	libpng \
 	python-cycler \
+	python-pybind \
 	qhull
 PYTHON_MATPLOTLIB_SETUP_TYPE = setuptools
 
@@ -24,7 +26,7 @@ PYTHON_MATPLOTLIB_DEPENDENCIES += python-pyqt5
 endif
 
 define PYTHON_MATPLOTLIB_COPY_SETUP_CFG
-	cp $(PYTHON_MATPLOTLIB_PKGDIR)/setup.cfg $(@D)/setup.cfg
+	cp $(PYTHON_MATPLOTLIB_PKGDIR)/setup.cfg $(@D)/mplsetup.cfg
 endef
 PYTHON_MATPLOTLIB_PRE_CONFIGURE_HOOKS += PYTHON_MATPLOTLIB_COPY_SETUP_CFG
 
