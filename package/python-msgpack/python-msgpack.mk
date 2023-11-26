1185c0256bb43bd451e73249d53915db42409476
James Hilliard <james.hilliard1@gmail.com>
2023-11-26 13:33:41 -0700

    package/python-msgpack: fix build without cpp toolchain
    Prior to being updated to version 1.0.7 in
    014a66fcde68bd0b612b62ec722ccc93bdc0d7f9 python-msgpack would
    automatically fall back to the pure python version if the cpp based
    optimized extension would fail to build for any reason.
    
    This however is no longer the case after updating to 1.0.7 where it
    is now required that we explicitely set the MSGPACK_PUREPYTHON=1 if
    we do not have cpp support enabled in the toolchain.
    
    Fixes:
     - http://autobuild.buildroot.net/results/361/36185a19bed4bd57421a4d909bce1976c89d130f
     - http://autobuild.buildroot.net/results/477/477f822cb196ebc2246bcbdc1b6eaf940fc018cd
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    [yann.morin.1998@free.fr: add the comment]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-msgpack/python-msgpack.mk
+++ package/python-msgpack/python-msgpack.mk
@@ -11,4 +11,11 @@ PYTHON_MSGPACK_LICENSE = Apache-2.0
 PYTHON_MSGPACK_LICENSE_FILES = COPYING
 PYTHON_MSGPACK_SETUP_TYPE = setuptools
 
+# When set in the environment, whatever the value, MSGPACK_PUREPYTHON drives
+# using the pure python implementation rather than rely on the C++ native code.
+# So we can't force it to use C++; we can only force it to use pure python.
+ifeq ($(BR2_INSTALL_LIBSTDCPP),)
+PYTHON_MSGPACK_ENV = MSGPACK_PUREPYTHON=1
+endif
+
 $(eval $(python-package))
