3da62675d730eec9b402f8edd1de5e046e94d71d
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-11-26 23:57:17 +0100

    package/exfatprogs: add EXFATPROGS_CPE_ID_VENDOR
    cpe:2.3:a:namjaejeon:exfatprogs is a valid CPE identifier for this
    package:
    
      https://nvd.nist.gov/products/cpe/detail/F174A846-F275-4AD8-A0E3-6D0CEFDFF308
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/exfatprogs/exfatprogs.mk
+++ package/exfatprogs/exfatprogs.mk
@@ -9,6 +9,7 @@ EXFATPROGS_SOURCE = exfatprogs-$(EXFATPROGS_VERSION).tar.xz
 EXFATPROGS_SITE = https://github.com/exfatprogs/exfatprogs/releases/download/$(EXFATPROGS_VERSION)
 EXFATPROGS_LICENSE = GPL-2.0+
 EXFATPROGS_LICENSE_FILES = COPYING
+EXFATPROGS_CPE_ID_VENDOR = namjaejeon
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
