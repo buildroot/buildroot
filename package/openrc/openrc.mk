4873799fe761ed3ac97c4f54f8079edbfd4047ef
Adam Duskett <adam.duskett@amarulasolutions.com>
2023-11-29 11:04:29 -0700

    package/openrc: bump version to 0.52.1
    The runscript and rc binaries have been removed in this release. However,
    Buildroot does not use those binaries, so only a simple version bump is
    necessary.
    
    Fixes:
      - cgroups being inconsistent
      - Start-stop-daemon did not work correctly on Linux 6.6
    
    Tested on Debian 11 and Fedora 39 with the following command:
    ./support/testing/run-tests tests.init.test_openrc
    
    Signed-off-by: Adam Duskett <adam.duskett@amarulasolutions.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/openrc/openrc.mk
+++ package/openrc/openrc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENRC_VERSION = 0.51
+OPENRC_VERSION = 0.52.1
 OPENRC_SITE = $(call github,OpenRC,openrc,$(OPENRC_VERSION))
 OPENRC_LICENSE = BSD-2-Clause
 OPENRC_LICENSE_FILES = LICENSE
