cf4ebfafaa2d8bc2c646029013f02c2cf048c8b9
Giulio Benetti <giulio.benetti@benettiengineering.com>
2023-12-06 21:07:30 +0100

    package/rtl8188eu: bump to 2023-12-04 version on v5.2.2.4 branch
    This version allows to build with Linux 6.7
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Reviewed-by: Luca Ceresoli <luca.ceresoli@bootlin.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/rtl8188eu/rtl8188eu.mk
+++ package/rtl8188eu/rtl8188eu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTL8188EU_VERSION = 3ff6f7d84cb5346ed8d5dc21056769f2e03746d9
+RTL8188EU_VERSION = 306f3e62cf7cddf8cee74f70f88a7a7b3af46f56
 RTL8188EU_SITE = $(call github,lwfinger,rtl8188eu,$(RTL8188EU_VERSION))
 RTL8188EU_LICENSE = GPL-2.0, proprietary (rtl8188eufw.bin firmware blob)
 RTL8188EU_LICENSE_FILES = COPYING
