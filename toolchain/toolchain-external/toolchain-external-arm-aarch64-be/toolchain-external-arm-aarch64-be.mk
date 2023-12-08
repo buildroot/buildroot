50ae5ea963b7ed34b1511d4d2487bef083f5b1a7
Antoine Coutant <antoine.coutant@smile.fr>
2023-12-08 14:53:29 +0100

    toolchain/toolchain-external/toolchain-external-arm-aarch64-be: bump to version 13.2-rel1
    Updated to gcc 13.2, gdb 13, binutils 2.41, glibc 2.38.
    
    The x86_64 host variant prebuilt toolchain is built on RHEL7
    (glibc 2.17) and is likely also be useable on OS versions like
    RHEL8, Ubuntu 18.04 or later.
    
    The AArch64 host variant prebuilt toolchain is built on Ubuntu 18.04
    (glibc 2.27) is likely also be useable on OS versions like RHEL8,
    Ubuntu 18.04 or later.
    
    Release note:
    https://developer.arm.com/downloads/-/arm-gnu-toolchain-downloads
    
    Signed-off-by: Antoine Coutant <antoine.coutant@smile.fr>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- toolchain/toolchain-external/toolchain-external-arm-aarch64-be/toolchain-external-arm-aarch64-be.mk
+++ toolchain/toolchain-external/toolchain-external-arm-aarch64-be/toolchain-external-arm-aarch64-be.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TOOLCHAIN_EXTERNAL_ARM_AARCH64_BE_VERSION = 12.2.rel1
+TOOLCHAIN_EXTERNAL_ARM_AARCH64_BE_VERSION = 13.2.rel1
 TOOLCHAIN_EXTERNAL_ARM_AARCH64_BE_SITE = https://developer.arm.com/-/media/Files/downloads/gnu/$(TOOLCHAIN_EXTERNAL_ARM_AARCH64_BE_VERSION)/binrel
 
 TOOLCHAIN_EXTERNAL_ARM_AARCH64_BE_SOURCE = arm-gnu-toolchain-$(TOOLCHAIN_EXTERNAL_ARM_AARCH64_BE_VERSION)-x86_64-aarch64_be-none-linux-gnu.tar.xz
