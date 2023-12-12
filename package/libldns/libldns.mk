d51cd29632650dc9dbc8e6828878127cd3f92982
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-12-12 21:59:51 +0100

    package/libldns: fix libressl build
    Fix the following build failure raised since bump of libressl to version
    3.8.2 in commit 21eca49ed5110872407b76ab9337d2877c4cda24:
    
    ./keys.c:167:35: error: 'ENGINE_METHOD_ALL' undeclared (first use in this function)
      167 |         if(!ENGINE_set_default(e, ENGINE_METHOD_ALL)) {
          |                                   ^~~~~~~~~~~~~~~~~
    
    Fixes:
     - http://autobuild.buildroot.org/results/37cc05b78a7004caa1b45d896121f059a4f8ca00
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 96d0d677790e659b822761b50561b0316b4abf43)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libldns/libldns.mk
+++ package/libldns/libldns.mk
@@ -17,7 +17,6 @@ LIBLDNS_CONF_OPTS = \
 	--with-ssl=$(STAGING_DIR)/usr \
 	--enable-dane \
 	--enable-ecdsa \
-	--enable-gost \
 	--enable-sha2 \
 	--without-examples \
 	--without-p5-dns-ldns \
@@ -28,6 +27,12 @@ ifeq ($(BR2_PACKAGE_LIBOPENSSL),y)
 LIBLDNS_CONF_OPTS += --enable-dane-verify
 else
 LIBLDNS_CONF_OPTS += --disable-dane-verify
+endif
+
+ifeq ($(BR2_PACKAGE_LIBOPENSSL_ENGINES),y)
+LIBLDNS_CONF_OPTS += --enable-gost
+else
+LIBLDNS_CONF_OPTS += --disable-gost
 endif
 
 ifeq ($(BR2_STATIC_LIBS),y)
