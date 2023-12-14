8dc5506c76a555bd7eb2455407022b1287b9a821
James Hilliard <james.hilliard1@gmail.com>
2023-12-14 03:08:33 -0700

    package/python-decouple: new package
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/python-decouple/python-decouple.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# python-decouple
+#
+################################################################################
+
+PYTHON_DECOUPLE_VERSION = 3.8
+PYTHON_DECOUPLE_SITE = https://files.pythonhosted.org/packages/e1/97/373dcd5844ec0ea5893e13c39a2c67e7537987ad8de3842fe078db4582fa
+PYTHON_DECOUPLE_SETUP_TYPE = setuptools
+PYTHON_DECOUPLE_LICENSE = MIT
+PYTHON_DECOUPLE_LICENSE_FILES = LICENSE
+
+$(eval $(python-package))
