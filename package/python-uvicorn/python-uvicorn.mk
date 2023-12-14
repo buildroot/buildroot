8e0ac9660c7247d7283d27560dbe661dadf25c03
James Hilliard <james.hilliard1@gmail.com>
2023-12-14 09:54:17 -0700

    package/python-uvicorn: new package
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Reviewed-by: Marcus Hoffmann <marcus.hoffmann@othermo.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/python-uvicorn/python-uvicorn.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# python-uvicorn
+#
+################################################################################
+
+PYTHON_UVICORN_VERSION = 0.24.0.post1
+PYTHON_UVICORN_SOURCE = uvicorn-$(PYTHON_UVICORN_VERSION).tar.gz
+PYTHON_UVICORN_SITE = https://files.pythonhosted.org/packages/e5/84/d43ce8fe6b31a316ef0ed04ea0d58cab981bdf7f17f8423491fa8b4f50b6
+PYTHON_UVICORN_SETUP_TYPE = pep517
+PYTHON_UVICORN_LICENSE = BSD-3-Clause
+PYTHON_UVICORN_LICENSE_FILES = LICENSE.md
+PYTHON_UVICORN_DEPENDENCIES = host-python-hatchling
+
+$(eval $(python-package))
