da1f15dbca77131751aa96c0701c4339aca95192
James Hilliard <james.hilliard1@gmail.com>
2023-12-14 09:54:16 -0700

    package/python-fastapi: new package
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Reviewed-by: Marcus Hoffmann <marcus.hoffmann@othermo.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/python-fastapi/python-fastapi.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# python-fastapi
+#
+################################################################################
+
+PYTHON_FASTAPI_VERSION = 0.105.0
+PYTHON_FASTAPI_SOURCE = fastapi-$(PYTHON_FASTAPI_VERSION).tar.gz
+PYTHON_FASTAPI_SITE = https://files.pythonhosted.org/packages/77/bb/5941e6e2ce3020f64b539a49d39f49be05de17d0c47fea95012589f812a5
+PYTHON_FASTAPI_SETUP_TYPE = pep517
+PYTHON_FASTAPI_LICENSE = MIT
+PYTHON_FASTAPI_LICENSE_FILES = LICENSE
+PYTHON_FASTAPI_DEPENDENCIES = host-python-hatchling
+
+$(eval $(python-package))
