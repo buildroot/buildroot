111e725b08c076dca4966cc3eac3fb073b462fc7
James Hilliard <james.hilliard1@gmail.com>
2023-12-14 09:54:14 -0700

    package/python-anyio: new package
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Reviewed-by: Marcus Hoffmann <marcus.hoffmann@othermo.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/python-anyio/python-anyio.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# python-anyio
+#
+################################################################################
+
+PYTHON_ANYIO_VERSION = 3.7.1
+PYTHON_ANYIO_SOURCE = anyio-$(PYTHON_ANYIO_VERSION).tar.gz
+PYTHON_ANYIO_SITE = https://files.pythonhosted.org/packages/28/99/2dfd53fd55ce9838e6ff2d4dac20ce58263798bd1a0dbe18b3a9af3fcfce
+PYTHON_ANYIO_SETUP_TYPE = setuptools
+PYTHON_ANYIO_LICENSE = MIT
+PYTHON_ANYIO_LICENSE_FILES = LICENSE
+PYTHON_ANYIO_DEPENDENCIES = host-python-setuptools-scm
+
+$(eval $(python-package))
