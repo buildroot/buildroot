be8bd8743d7dc2747b19141ce64c47651cdd8bae
Francois Perrad <fperrad@gmail.com>
2023-12-25 20:38:33 +0100

    package/dbus: bump to version 1.14.10
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit be24c10472666d9dbfa12668c9326bc0bc9598b9)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/dbus/dbus.mk
+++ package/dbus/dbus.mk
@@ -6,7 +6,7 @@
 
 # When updating dbus, check if there are changes in session.conf and
 # system.conf, and update the versions in the dbus-broker package accordingly.
-DBUS_VERSION = 1.14.8
+DBUS_VERSION = 1.14.10
 DBUS_SOURCE = dbus-$(DBUS_VERSION).tar.xz
 DBUS_SITE = https://dbus.freedesktop.org/releases/dbus
 DBUS_LICENSE = AFL-2.1 or GPL-2.0+ (library, tools), GPL-2.0+ (tools)
