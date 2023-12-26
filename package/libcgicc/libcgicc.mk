90c3ed83164deaece8b136d98ce0d82954f892c5
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-12-26 17:58:51 +0100

    package/libcgicc: bump to version 3.2.20
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/libcgicc/libcgicc.mk
+++ package/libcgicc/libcgicc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBCGICC_VERSION = 3.2.19
+LIBCGICC_VERSION = 3.2.20
 LIBCGICC_SITE = $(BR2_GNU_MIRROR)/cgicc
 LIBCGICC_SOURCE = cgicc-$(LIBCGICC_VERSION).tar.gz
 LIBCGICC_LICENSE = LGPL-3.0+ (library), GFDL-1.2+ (docs)
