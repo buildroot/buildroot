37eb68c9fb46279e9ce8d56af6f90456a6274070
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-12-26 17:11:29 +0100

    package/opensc: security bump to version 0.24.0
    - Drop patches (already in version) and so drop autoreconf
    - Fix the following security issues:
      - CVE-2023-40660: Fix Potential PIN bypass
      - CVE-2023-40661: Important dynamic analyzers reports
      - CVE-2023-4535: Out-of-bounds read in MyEID driver handling
        encryption using symmetric keys
    
    https://github.com/OpenSC/OpenSC/releases/tag/0.24.0
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/opensc/opensc.mk
+++ package/opensc/opensc.mk
@@ -4,18 +4,13 @@
 #
 ################################################################################
 
-OPENSC_VERSION = 0.23.0
+OPENSC_VERSION = 0.24.0
 OPENSC_SITE = https://github.com/OpenSC/OpenSC/releases/download/$(OPENSC_VERSION)
 OPENSC_LICENSE = LGPL-2.1+
 OPENSC_LICENSE_FILES = COPYING
 OPENSC_CPE_ID_VENDOR = opensc_project
-# 0003-configure-add-option-to-disable-tests.patch
-OPENSC_AUTORECONF = YES
 OPENSC_DEPENDENCIES = openssl pcsc-lite
 OPENSC_INSTALL_STAGING = YES
 OPENSC_CONF_OPTS = --disable-cmocka --disable-strict --disable-tests
-
-# 0004-pkcs15init-correct-left-length-calculation-to-fix-buffer-overrun-bug.patch
-OPENSC_IGNORE_CVES += CVE-2023-2977
 
 $(eval $(autotools-package))
