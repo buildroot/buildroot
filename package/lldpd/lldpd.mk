885cda7e0d67eff56bf803e85185f9f86f02120f
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-12-26 22:24:31 +0100

    package/lldpd: bump to version 1.0.17
    Drop patch (already in version)
    
    https://github.com/lldpd/lldpd/blob/1.0.17/NEWS
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/lldpd/lldpd.mk
+++ package/lldpd/lldpd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LLDPD_VERSION = 1.0.15
+LLDPD_VERSION = 1.0.17
 LLDPD_SITE = https://media.luffy.cx/files/lldpd
 LLDPD_DEPENDENCIES = \
 	$(if $(BR2_PACKAGE_CHECK),check) \
@@ -15,9 +15,6 @@ LLDPD_DEPENDENCIES = \
 LLDPD_LICENSE = ISC
 LLDPD_LICENSE_FILES = LICENSE
 LLDPD_CPE_ID_VENDOR = lldpd_project
-
-# 0001-daemon-fix-read-overflow-when-parsing-CDP-addresses.patch
-LLDPD_IGNORE_CVES += CVE-2023-41910
 
 # Detection of c99 support in configure fails without WCHAR. To enable
 # automatic detection of c99 support by configure, we need to enable
