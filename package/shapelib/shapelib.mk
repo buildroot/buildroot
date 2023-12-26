0a8d669f8ea35a4c486834b49cca112e57125c93
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-12-26 21:21:16 +0100

    package/shapelib: bump to version 1.6.0
    - Drop patch (already in version)
    - Update hash of web/license.html because SPDX identifier is used since
      https://github.com/OSGeo/shapelib/commit/6bf68d3086f49245470fc24b121afbbeb8e9c4f2
    - COPYING has been replaced by LICENSE-LGPL and LICENSE-MIT since
      https://github.com/OSGeo/shapelib/commit/1638416feefcee4a1eeef9aab573a5ce44be1f3e
    
    https://github.com/OSGeo/shapelib/releases/tag/v1.6.0
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/shapelib/shapelib.mk
+++ package/shapelib/shapelib.mk
@@ -4,14 +4,11 @@
 #
 ################################################################################
 
-SHAPELIB_VERSION = 1.5.0
+SHAPELIB_VERSION = 1.6.0
 SHAPELIB_SITE = http://download.osgeo.org/shapelib
 SHAPELIB_LICENSE = MIT or LGPL-2.0
-SHAPELIB_LICENSE_FILES = web/license.html COPYING
+SHAPELIB_LICENSE_FILES = LICENSE-LGPL LICENSE-MIT web/license.html
 SHAPELIB_CPE_ID_VENDOR = osgeo
 SHAPELIB_INSTALL_STAGING = YES
-
-# 0001-Remove-double-free-in-contrib-shpsrt.patch
-SHAPELIB_IGNORE_CVES += CVE-2022-0699
 
 $(eval $(autotools-package))
