9a94e85fff629b73cc3b02605793806fb9ce97dd
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-12-26 17:55:20 +0100

    package/libcap-ng: bump to version 0.8.4
    python2 support has been dropped with
    https://github.com/stevegrubb/libcap-ng/commit/868a82672a398aec96eba60e73b9a8e8892c4d4a
    
    https://github.com/stevegrubb/libcap-ng/blob/v0.8.4/ChangeLog
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/libcap-ng/libcap-ng.mk
+++ package/libcap-ng/libcap-ng.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBCAP_NG_VERSION = 0.8.3
+LIBCAP_NG_VERSION = 0.8.4
 LIBCAP_NG_SITE = http://people.redhat.com/sgrubb/libcap-ng
 LIBCAP_NG_LICENSE = GPL-2.0+ (programs), LGPL-2.1+ (library)
 LIBCAP_NG_LICENSE_FILES = COPYING COPYING.LIB
@@ -12,10 +12,10 @@ LIBCAP_NG_CPE_ID_VENDOR = libcap-ng_project
 LIBCAP_NG_INSTALL_STAGING = YES
 
 LIBCAP_NG_CONF_ENV = ac_cv_prog_swig_found=no
-LIBCAP_NG_CONF_OPTS = --without-python --without-python3
+LIBCAP_NG_CONF_OPTS = --without-python3
 
 HOST_LIBCAP_NG_CONF_ENV = ac_cv_prog_swig_found=no
-HOST_LIBCAP_NG_CONF_OPTS = --without-python --without-python3
+HOST_LIBCAP_NG_CONF_OPTS = --without-python3
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
