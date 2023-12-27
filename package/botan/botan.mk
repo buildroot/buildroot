5547abb237ffb1feb52f4fa26d834ffb3f6aa839
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-12-27 12:30:45 +0100

    package/botan: add trousers optional dependency
    trousers is an optional dependency since version 1.11.26 and
    https://github.com/randombit/botan/commit/1752f0d522eef9a4a703fccf702b4b026c1c1d01
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 55b0434940d46bd16ec1d6063f124c9560e4ceb4)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/botan/botan.mk
+++ package/botan/botan.mk
@@ -67,6 +67,11 @@ BOTAN_DEPENDENCIES += sqlite
 BOTAN_CONF_OPTS += --with-sqlite
 endif
 
+ifeq ($(BR2_PACKAGE_TROUSERS),y)
+BOTAN_DEPENDENCIES += trousers
+BOTAN_CONF_OPTS += --with-tpm
+endif
+
 ifeq ($(BR2_PACKAGE_XZ),y)
 BOTAN_DEPENDENCIES += xz
 BOTAN_CONF_OPTS += --with-lzma
