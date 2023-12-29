5cd74d6a4e347ff3807d49b6b6b8817009b26a17
Julien Olivain <ju.o@free.fr>
2023-12-29 15:50:25 +0100

    support/testing: test_python_ml_dtypes: remove BR2_PACKAGE_PYTHON3_ZLIB
    The test_python_ml_dtypes.py enabled BR2_PACKAGE_PYTHON3_ZLIB=y in its
    configuration to workaround the fact that the toolchain used to
    testing was tainted with zlib.
    
    The commit 7e0e6e3
    "toolchain/toolchain-external/toolchain-external-bootlin: update to
    2023.11-1" updated the toolchains which are no longer tainted with
    zlib.
    
    The workaround is no longer needed and this commit removes this
    config, for consistency.
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- support/testing/tests/package/test_python_ml_dtypes.py
+++ support/testing/tests/package/test_python_ml_dtypes.py
@@ -4,16 +4,9 @@ from tests.package.test_python import TestPythonPackageBase
 class TestPythonPy3MlDtypes(TestPythonPackageBase):
     __test__ = True
 
-    # Note: BR2_PACKAGE_PYTHON3_ZLIB=y is needed as a runtime
-    # dependency because the Bootlin toolchain used for this test is
-    # tainted with zlib (and gets detected by python3/numpy).
-    # See commit 8ce33fed "package/gdb: gdbserver does not need zlib".
-    # This config entry can be removed as soon as the toolchain is
-    # updated without zlib in its sysroot.
     config = TestPythonPackageBase.config + \
         """
         BR2_PACKAGE_PYTHON3=y
-        BR2_PACKAGE_PYTHON3_ZLIB=y
         BR2_PACKAGE_PYTHON_ML_DTYPES=y
         """
     sample_scripts = ["tests/package/sample_python_ml_dtypes.py"]
