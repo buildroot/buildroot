091036dc84c580691e77100f456d24f3e58337ec
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-12-30 10:50:51 +0100

    package/upx: bump to version 4.2.1
    https://github.com/upx/upx/blob/v4.2.1/NEWS
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/upx/upx.mk
+++ package/upx/upx.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UPX_VERSION = 4.1.0
+UPX_VERSION = 4.2.1
 UPX_SITE = https://github.com/upx/upx/releases/download/v$(UPX_VERSION)
 UPX_SOURCE = upx-$(UPX_VERSION)-src.tar.xz
 UPX_LICENSE = GPL-2.0+
