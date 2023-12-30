15515ed0cd7efe586dd488a91d66ca6133de9f86
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-12-30 17:21:11 +0100

    package/catatonit: bump to version 0.2.0
    - Drop first patch (already in version)
    - Update second patch
    - License changed to GPL-2.0+:
      https://github.com/openSUSE/catatonit/commit/fe191ef859c6365d44a4d7f4ac9765dee825d78c
    
    https://github.com/openSUSE/catatonit/releases/tag/v0.2.0
    https://github.com/openSUSE/catatonit/compare/v0.1.7...v0.2.0
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/catatonit/catatonit.mk
+++ package/catatonit/catatonit.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-CATATONIT_VERSION = 0.1.7
+CATATONIT_VERSION = 0.2.0
 CATATONIT_SITE = $(call github,openSUSE,catatonit,v$(CATATONIT_VERSION))
-CATATONIT_LICENSE = GPL-3.0+
+CATATONIT_LICENSE = GPL-2.0+
 CATATONIT_LICENSE_FILES = COPYING
 
 CATATONIT_AUTORECONF = YES
