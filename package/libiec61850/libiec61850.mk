8adc37e979aeae192bc4002187385d3c9e66f0b6
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-12-30 19:13:20 +0100

    package/libiec61850: bump to version 1.5.3
    - Switch site to github to get latest release
    - Drop LIBIEC61850_IGNORE_CVES as CVE-2023-27772 only affects version
      1.5.1
    
    https://github.com/mz-automation/libiec61850/blob/v1.5.3/CHANGELOG
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libiec61850/libiec61850.mk
+++ package/libiec61850/libiec61850.mk
@@ -4,16 +4,13 @@
 #
 ################################################################################
 
-LIBIEC61850_VERSION = 1.5.1
-LIBIEC61850_SITE = https://libiec61850.com/wp-content/uploads/2022/03
+LIBIEC61850_VERSION = 1.5.3
+LIBIEC61850_SITE = $(call github,mz-automation,libiec61850,v$(LIBIEC61850_VERSION))
 LIBIEC61850_INSTALL_STAGING = YES
 LIBIEC61850_LICENSE = GPL-3.0+
 LIBIEC61850_LICENSE_FILES = COPYING
 LIBIEC61850_CPE_ID_VENDOR = mz-automation
 LIBIEC61850_CONF_OPTS = -DBUILD_PYTHON_BINDINGS=OFF
-# Examples aren't build
-# https://github.com/mz-automation/libiec61850/issues/442
-LIBIEC61850_IGNORE_CVES += CVE-2023-27772
 
 ifeq ($(BR2_PACKAGE_MBEDTLS),y)
 LIBIEC61850_DEPENDENCIES += mbedtls
