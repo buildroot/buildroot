280d06236e25a01b7985e2e7951948533306a60e
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-01-01 23:45:52 +0100

    package/python-validators: bump to version 0.22.0
    - python-decorator is not a runtime dependency since
      https://github.com/python-validators/validators/commit/b6255c49088cbe6563eb93a7224c86eee04be06a
    - Replace https://github.com/kvesteri/validators by
      https://python-validators.github.io/validators
    - Update hash of LICENSE file (update in year with
      https://github.com/python-validators/validators/commit/2e0d0ec703eab5c9b0d303d8d318d5389dcef9fa)
    
    https://github.com/python-validators/validators/blob/0.22.0/CHANGES.md
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-validators/python-validators.mk
+++ package/python-validators/python-validators.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_VALIDATORS_VERSION = 0.20.0
+PYTHON_VALIDATORS_VERSION = 0.22.0
 PYTHON_VALIDATORS_SOURCE = validators-$(PYTHON_VALIDATORS_VERSION).tar.gz
-PYTHON_VALIDATORS_SITE = https://files.pythonhosted.org/packages/95/14/ed0af6865d378cfc3c504aed0d278a890cbefb2f1934bf2dbe92ecf9d6b1
+PYTHON_VALIDATORS_SITE = https://files.pythonhosted.org/packages/9b/21/40a249498eee5a244a017582c06c0af01851179e2617928063a3d628bc8f
 PYTHON_VALIDATORS_SETUP_TYPE = setuptools
 PYTHON_VALIDATORS_LICENSE = MIT
 PYTHON_VALIDATORS_LICENSE_FILES = LICENSE
