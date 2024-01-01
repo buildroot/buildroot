6febe37bacf87b56f9f0330ed01dc800232199cf
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-01-01 23:13:17 +0100

    package/python-paramiko: security bump to version 3.4.0
    Address CVE 2023-48795 (aka the “Terrapin Attack”), a vulnerability
    found in the SSH protocol.
    
    https://github.com/paramiko/paramiko/blob/3.4.0/sites/www/changelog.rst
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-paramiko/python-paramiko.mk
+++ package/python-paramiko/python-paramiko.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PARAMIKO_VERSION = 3.3.1
+PYTHON_PARAMIKO_VERSION = 3.4.0
 PYTHON_PARAMIKO_SOURCE = paramiko-$(PYTHON_PARAMIKO_VERSION).tar.gz
-PYTHON_PARAMIKO_SITE = https://files.pythonhosted.org/packages/44/03/158ae1dcb950bd96f04038502238159e116fafb27addf5df1ba35068f2d6
+PYTHON_PARAMIKO_SITE = https://files.pythonhosted.org/packages/cc/af/11996c4df4f9caff87997ad2d3fd8825078c277d6a928446d2b6cf249889
 PYTHON_PARAMIKO_SETUP_TYPE = setuptools
 PYTHON_PARAMIKO_LICENSE = LGPL-2.1+
 PYTHON_PARAMIKO_LICENSE_FILES = LICENSE
