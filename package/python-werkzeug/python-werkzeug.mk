466c72828cd5df58532cde21c3cfdac346e5dd70
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-01-01 23:56:21 +0100

    package/python-werkzeug: security bump to version 2.3.8
    Fix slow multipart parsing for large parts potentially enabling DoS
    attacks (CVE-2023-46136):
    https://github.com/pallets/werkzeug/security/advisories/GHSA-hrfv-mqp8-q5rw
    
    https://werkzeug.palletsprojects.com/en/2.3.x/changes/#version-2-3-8
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit b8bacbfb9762e4c2dc58fd36d2bddc91f033e21e)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-werkzeug/python-werkzeug.mk
+++ package/python-werkzeug/python-werkzeug.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_WERKZEUG_VERSION = 2.3.6
-PYTHON_WERKZEUG_SOURCE = Werkzeug-$(PYTHON_WERKZEUG_VERSION).tar.gz
-PYTHON_WERKZEUG_SITE = https://files.pythonhosted.org/packages/d1/7e/c35cea5749237d40effc50ed1a1c7518d9f2e768fcf30b4e9ea119e74975
+PYTHON_WERKZEUG_VERSION = 2.3.8
+PYTHON_WERKZEUG_SOURCE = werkzeug-$(PYTHON_WERKZEUG_VERSION).tar.gz
+PYTHON_WERKZEUG_SITE = https://files.pythonhosted.org/packages/3d/4b/d746f1000782c89d6c97df9df43ba8f4d126038608843d3560ae88d201b5
 PYTHON_WERKZEUG_SETUP_TYPE = setuptools
 PYTHON_WERKZEUG_LICENSE = BSD-3-Clause
 PYTHON_WERKZEUG_LICENSE_FILES = LICENSE.rst
