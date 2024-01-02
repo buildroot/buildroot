0204ecf5b15a318bde9e8ab3dd5a92563492a39f
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-01-02 23:41:08 +0100

    package/postgis: drop autoreconf
    Commit 1718c2ba6a4db1461b2490aa42ca4f0a2112adae removed all patches but
    forgot to drop autoreconf
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Reviewed-by: Maxim Kochetkov <fido_max@inbox.ru>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit e39b53dd03c1adf53e5bc204b22382a063b501ce)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/postgis/postgis.mk
+++ package/postgis/postgis.mk
@@ -11,8 +11,6 @@ POSTGIS_MAKE = $(MAKE1)
 POSTGIS_LICENSE = GPL-2.0+ (PostGIS), BSD-2-Clause, MIT, Apache-2.0, ISC, BSL-1.0, CC-BY-SA-3.0
 POSTGIS_LICENSE_FILES = LICENSE.TXT
 POSTGIS_CPE_ID_VENDOR = postgis
-# configure.ac is patched so need to run autoreconf
-POSTGIS_AUTORECONF = YES
 
 POSTGIS_DEPENDENCIES = postgresql libgeos proj libxml2
 
