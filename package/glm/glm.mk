01ad11343172ea698e4dcc1c64cb02ce26671e49
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-01-02 14:57:41 +0100

    package/glm: bump to version 0.9.9.8
    License is in a separate file since
    https://github.com/g-truc/glm/commit/347f422b9fda328a4eecfdc0e5bf5b19bc5c37e4
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/glm/glm.mk
+++ package/glm/glm.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-GLM_VERSION = 0.9.9.5
+GLM_VERSION = 0.9.9.8
 GLM_SITE = $(call github,g-truc,glm,$(GLM_VERSION))
 GLM_LICENSE = MIT
-GLM_LICENSE_FILES = manual.md
+GLM_LICENSE_FILES = copying.txt
 
 # GLM is a header-only library, it only makes sense
 # to have it installed into the staging directory.
