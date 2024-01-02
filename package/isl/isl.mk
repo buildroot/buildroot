8c2cda500315e50ea28874b2d8970ae9c568baff
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-01-02 23:17:34 +0100

    package/isl: bump to version 0.26
    https://groups.google.com/g/isl-announce/c/_yQ27dRgf7w
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/isl/isl.mk
+++ package/isl/isl.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ISL_VERSION = 0.25
+ISL_VERSION = 0.26
 ISL_SOURCE = isl-$(ISL_VERSION).tar.xz
 ISL_SITE = https://libisl.sourceforge.io
 ISL_LICENSE = MIT
