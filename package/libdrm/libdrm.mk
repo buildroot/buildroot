639d8d4979d075cda3de9e663f5aecd9ead56c42
Bernd Kuhls <bernd@kuhls.net>
2024-01-03 08:11:41 +0100

    package/libdrm: bump version to 2.4.119
    Release notes:
    https://lists.x.org/archives/xorg-announce/2023-December/003438.html
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libdrm/libdrm.mk
+++ package/libdrm/libdrm.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBDRM_VERSION = 2.4.118
+LIBDRM_VERSION = 2.4.119
 LIBDRM_SOURCE = libdrm-$(LIBDRM_VERSION).tar.xz
 LIBDRM_SITE = https://dri.freedesktop.org/libdrm
 LIBDRM_LICENSE = MIT
