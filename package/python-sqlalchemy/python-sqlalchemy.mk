0f2f090892914e3b04ff217d1ea10cceafc70515
Flávio Tapajós <flavio.tapajos@newtesc.com.br>
2024-01-03 11:28:30 -0300

    package/python-sqlalchemy: bump version to 2.0.25
    Hash of license file is changed due to:
    
    -Copyright 2005-2022 SQLAlchemy authors and contributors <see AUTHORS file>.
    +Copyright 2005-2024 SQLAlchemy authors and contributors <see AUTHORS file>.
    
    Signed-off-by: Flávio Tapajós <flavio.tapajos@newtesc.com.br>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-sqlalchemy/python-sqlalchemy.mk
+++ package/python-sqlalchemy/python-sqlalchemy.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SQLALCHEMY_VERSION = 1.4.42
+PYTHON_SQLALCHEMY_VERSION = 2.0.25
 PYTHON_SQLALCHEMY_SOURCE = SQLAlchemy-$(PYTHON_SQLALCHEMY_VERSION).tar.gz
-PYTHON_SQLALCHEMY_SITE = https://files.pythonhosted.org/packages/e4/56/8ea85eaab7d93b58f9c213ad8fc5882838189a29fc8cc401d80710a12969
+PYTHON_SQLALCHEMY_SITE = https://files.pythonhosted.org/packages/7b/bb/85bd8e211f54983e927c7cd9b2ad66773fbef507957156fc72e481a62681
 PYTHON_SQLALCHEMY_SETUP_TYPE = setuptools
 PYTHON_SQLALCHEMY_LICENSE = MIT
 PYTHON_SQLALCHEMY_LICENSE_FILES = LICENSE
