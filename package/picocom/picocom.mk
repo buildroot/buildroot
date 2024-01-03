d6a5bcf9063fec7e626867636bbe3c3adde9086a
Baruch Siach <baruch@tkos.co.il>
2024-01-03 18:49:06 +0200

    package/picocom: bump to version 2023-04
    Switch to new upstream.
    
    Drop upstream patch.
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/picocom/picocom.mk
+++ package/picocom/picocom.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PICOCOM_VERSION = 3.1
-PICOCOM_SITE = $(call github,npat-efault,picocom,$(PICOCOM_VERSION))
+PICOCOM_VERSION = 2023-04
+PICOCOM_SITE = $(call gitlab,wsakernel,picocom,$(PICOCOM_VERSION))
 PICOCOM_LICENSE = GPL-2.0+
 PICOCOM_LICENSE_FILES = LICENSE.txt
 PICOCOM_CPE_ID_VENDOR = picocom_project
