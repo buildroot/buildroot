7ff27f44ac6d3b01826819ec5c3ebabee6314e91
James Hilliard <james.hilliard1@gmail.com>
2024-01-03 20:32:49 -0700

    package/python-aiomonitor: bump to version 0.7.0
    Add new runtime dependencies:
     - python-attrs
     - python-aiohttp
     - python-click
     - python-janus
     - python-trafaret
     - python-typing-extensions
     - python-prompt-toolkit
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-aiomonitor/python-aiomonitor.mk
+++ package/python-aiomonitor/python-aiomonitor.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_AIOMONITOR_VERSION = 0.6.0
+PYTHON_AIOMONITOR_VERSION = 0.7.0
 PYTHON_AIOMONITOR_SOURCE = aiomonitor-$(PYTHON_AIOMONITOR_VERSION).tar.gz
-PYTHON_AIOMONITOR_SITE = https://files.pythonhosted.org/packages/13/ab/36fbaf36c1c7cb4d5a81b945faf69e95a48dd38cbc9b57915d41d9445f55
+PYTHON_AIOMONITOR_SITE = https://files.pythonhosted.org/packages/50/30/1d903b716489c2b5d0a92baccf172f972e97c2de94d4ea41c154287e9b60
 PYTHON_AIOMONITOR_SETUP_TYPE = setuptools
 PYTHON_AIOMONITOR_LICENSE = Apache-2.0
 PYTHON_AIOMONITOR_LICENSE_FILES = LICENSE
