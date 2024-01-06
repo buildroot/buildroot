8a170b8446585f98193bb3d780d0d8a427ff6c06
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-01-06 19:17:54 +0100

    package/json-glib: bump to version 1.8.0
    Handle new nls option added by
    https://gitlab.gnome.org/GNOME/json-glib/-/commit/31998d84cb241a4c6343c97afc06ee24e60db032
    
    https://download.gnome.org/sources/json-glib/1.8/json-glib-1.8.0.news
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/json-glib/json-glib.mk
+++ package/json-glib/json-glib.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-JSON_GLIB_VERSION_MAJOR = 1.6
-JSON_GLIB_VERSION = $(JSON_GLIB_VERSION_MAJOR).6
+JSON_GLIB_VERSION_MAJOR = 1.8
+JSON_GLIB_VERSION = $(JSON_GLIB_VERSION_MAJOR).0
 JSON_GLIB_SITE = https://download.gnome.org/sources/json-glib/$(JSON_GLIB_VERSION_MAJOR)
 JSON_GLIB_SOURCE = json-glib-$(JSON_GLIB_VERSION).tar.xz
 JSON_GLIB_LICENSE = LGPL-2.1+
@@ -23,6 +23,12 @@ JSON_GLIB_CONF_OPTS += -Dintrospection=enabled
 JSON_GLIB_DEPENDENCIES += gobject-introspection
 else
 JSON_GLIB_CONF_OPTS += -Dintrospection=disabled
+endif
+
+ifeq ($(BR2_SYSTEM_ENABLE_NLS),y)
+JSON_GLIB_CONF_OPTS += -Dnls=enabled
+else
+JSON_GLIB_CONF_OPTS += -Dnls=disabled
 endif
 
 JSON_GLIB_LDFLAGS = $(TARGET_LDFLAGS) $(TARGET_NLS_LIBS)
