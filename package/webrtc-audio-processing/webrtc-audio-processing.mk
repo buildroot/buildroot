ef0fa986eb7ff25c0a5db70ec0b62032e2d71538
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-01-06 19:36:08 +0100

    package/webrtc-audio-processing: bump to version 1.3
    - Drop patch (not needed anymore)
    - Switch to meson-package
    - libabseil-cpp is a mandatory dependency
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/webrtc-audio-processing/webrtc-audio-processing.mk
+++ package/webrtc-audio-processing/webrtc-audio-processing.mk
@@ -4,18 +4,12 @@
 #
 ################################################################################
 
-WEBRTC_AUDIO_PROCESSING_VERSION = 0.3.1
+WEBRTC_AUDIO_PROCESSING_VERSION = 1.3
 WEBRTC_AUDIO_PROCESSING_SOURCE = webrtc-audio-processing-$(WEBRTC_AUDIO_PROCESSING_VERSION).tar.xz
 WEBRTC_AUDIO_PROCESSING_SITE = http://freedesktop.org/software/pulseaudio/webrtc-audio-processing
 WEBRTC_AUDIO_PROCESSING_INSTALL_STAGING = YES
 WEBRTC_AUDIO_PROCESSING_LICENSE = BSD-3-Clause
 WEBRTC_AUDIO_PROCESSING_LICENSE_FILES = COPYING
-WEBRTC_AUDIO_PROCESSING_DEPENDENCIES = host-pkgconf
-# 0001-Proper-detection-of-cxxabi.h-and-execinfo.h.patch
-WEBRTC_AUDIO_PROCESSING_AUTORECONF = YES
+WEBRTC_AUDIO_PROCESSING_DEPENDENCIES = host-pkgconf libabseil-cpp
 
-ifeq ($(BR2_SOFT_FLOAT),y)
-WEBRTC_AUDIO_PROCESSING_CONF_OPTS += --with-ns-mode=fixed
-endif
-
-$(eval $(autotools-package))
+$(eval $(meson-package))
