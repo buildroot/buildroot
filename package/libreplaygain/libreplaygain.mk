558a25994b3ccab9b308bb667e464bdff0ab0d61
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-01-07 12:41:01 +0100

    package/libreplaygain: use version friendly to release-monitoring
    Update LIBREPLAYGAIN_VERSION to match what is returned by
    https://release-monitoring.org/project/370816
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/libreplaygain/libreplaygain.mk
+++ package/libreplaygain/libreplaygain.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-LIBREPLAYGAIN_VERSION = r475
+LIBREPLAYGAIN_VERSION = 475
 LIBREPLAYGAIN_SITE = http://files.musepack.net/source
-LIBREPLAYGAIN_SOURCE = libreplaygain_$(LIBREPLAYGAIN_VERSION).tar.gz
+LIBREPLAYGAIN_SOURCE = libreplaygain_r$(LIBREPLAYGAIN_VERSION).tar.gz
 # upstream doesn't ship configure
 LIBREPLAYGAIN_AUTORECONF = YES
 LIBREPLAYGAIN_INSTALL_STAGING = YES
