ef05ba673b964e4a1887bde7a5fe28a23ac26fc2
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-01-07 12:43:45 +0100

    package/python-bitstring: bump to version 4.1.4
    This bump will fix the following build failure raised since bump to
    version 4.1.2 in commit ce2bf7b96b9dba57bb31aca583283736af27afcc:
    
    *** Error compiling '/home/buildroot/autobuild/instance-0/output-1/target/usr/lib/python3.11/site-packages/bitstring/plugin_test.py'...
    Sorry: IndentationError: expected an indented block after function definition on line 6 (plugin_test.py, line 8)
    
    https://github.com/scott-griffiths/bitstring/blob/bitstring-4.1.4/release_notes.txt
    
    Fixes:
     - http://autobuild.buildroot.org/results/e6fac4cac16de94862e3b4697dd88e856bc9a398
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>
    (cherry picked from commit ae20955be340cbf9133c6a13a7695759aa4f464b)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-bitstring/python-bitstring.mk
+++ package/python-bitstring/python-bitstring.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_BITSTRING_VERSION = 4.1.2
+PYTHON_BITSTRING_VERSION = 4.1.4
 PYTHON_BITSTRING_SOURCE = bitstring-$(PYTHON_BITSTRING_VERSION).tar.gz
-PYTHON_BITSTRING_SITE = https://files.pythonhosted.org/packages/23/fc/b5ace4f51fea5bcc7f8cca8859748ea5eb941680b82a5b3687c980d9589b
+PYTHON_BITSTRING_SITE = https://files.pythonhosted.org/packages/7f/07/0fd502a29127b968bada3d1824a8af997546d2b9ff73f00e800b3d9888cb
 PYTHON_BITSTRING_SETUP_TYPE = setuptools
 PYTHON_BITSTRING_LICENSE = MIT
 PYTHON_BITSTRING_LICENSE_FILES = LICENSE
