fb6163cf96de64ac6df271f3080fce4dacaa7a71
James Hilliard <james.hilliard1@gmail.com>
2024-01-09 00:52:28 -0700

    package/python-gnupg: bump to version 0.5.2
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-gnupg/python-gnupg.mk
+++ package/python-gnupg/python-gnupg.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PYTHON_GNUPG_VERSION = 0.5.1
-PYTHON_GNUPG_SITE = https://files.pythonhosted.org/packages/98/62/7737485f44bd4d7d904f4094372f4119195865b29f119fa51a98e121a13a
+PYTHON_GNUPG_VERSION = 0.5.2
+PYTHON_GNUPG_SITE = https://files.pythonhosted.org/packages/b1/5d/4425390ad81d22b330a1b0df204c4d39fb3cb7c39e081d51e9f7426ce716
 PYTHON_GNUPG_LICENSE = BSD-3-Clause
 PYTHON_GNUPG_LICENSE_FILES = LICENSE.txt
 PYTHON_GNUPG_CPE_ID_VENDOR = python
