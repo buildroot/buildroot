2a211f917d2f93504f9b78547d7594f46b77a4b5
James Hilliard <james.hilliard1@gmail.com>
2024-01-09 01:11:15 -0700

    package/python-hiredis: bump to version 2.3.2
    Hash for LICENSE file changed due to license change to MIT:
    https://github.com/redis/hiredis-py/commit/1e29dd4719a44092e8317d6cddb8075a4a143e0e
    
    The vendor/hiredis/COPYING file remains BSD-3-Clause.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-hiredis/python-hiredis.mk
+++ package/python-hiredis/python-hiredis.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_HIREDIS_VERSION = 2.2.3
+PYTHON_HIREDIS_VERSION = 2.3.2
 PYTHON_HIREDIS_SOURCE = hiredis-$(PYTHON_HIREDIS_VERSION).tar.gz
-PYTHON_HIREDIS_SITE = https://files.pythonhosted.org/packages/b0/04/dab6792584fc548803ffa50b5bb2b99f01d3ab04d7c7f64e85f1a22fb847
+PYTHON_HIREDIS_SITE = https://files.pythonhosted.org/packages/fe/2d/a5ae61da1157644f7e52e088fa158ac6f5d09775112d14b1c9b9a5156bf1
 PYTHON_HIREDIS_SETUP_TYPE = setuptools
-PYTHON_HIREDIS_LICENSE = BSD-3-Clause
+PYTHON_HIREDIS_LICENSE = MIT, BSD-3-Clause
 PYTHON_HIREDIS_LICENSE_FILES = LICENSE vendor/hiredis/COPYING
 
 $(eval $(python-package))
