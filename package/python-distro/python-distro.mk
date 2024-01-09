7c83d1e930c23d0e62de4eedb99716bc5c0f6a29
James Hilliard <james.hilliard1@gmail.com>
2024-01-09 00:01:35 -0700

    package/python-distro: bump to version 1.9.0
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-distro/python-distro.mk
+++ package/python-distro/python-distro.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_DISTRO_VERSION = 1.8.0
-PYTHON_DISTRO_SITE = https://files.pythonhosted.org/packages/4b/89/eaa3a3587ebf8bed93e45aa79be8c2af77d50790d15b53f6dfc85b57f398
+PYTHON_DISTRO_VERSION = 1.9.0
 PYTHON_DISTRO_SOURCE = distro-$(PYTHON_DISTRO_VERSION).tar.gz
+PYTHON_DISTRO_SITE = https://files.pythonhosted.org/packages/fc/f8/98eea607f65de6527f8a2e8885fc8015d3e6f5775df186e443e0964a11c3
 PYTHON_DISTRO_LICENSE = Apache-2.0
 PYTHON_DISTRO_LICENSE_FILES = LICENSE
 PYTHON_DISTRO_SETUP_TYPE = setuptools
