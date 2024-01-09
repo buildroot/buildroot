3ebd06c0b13031191919caca07df86268cd758e4
James Hilliard <james.hilliard1@gmail.com>
2024-01-08 23:01:28 -0700

    package/python-cherrypy: bump to version 18.9.0
    Add new host-python-setuptools-scm-git-archive build dependency.
    
    Add new python-jaraco-collections runtime dependency.
    
    Add new python-more-itertools runtime dependency.
    
    Add new python-zc-lockfile runtime dependency.
    
    Drop no longer required python-six and python-jaraco-classes runtime
    dependencies.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-cherrypy/python-cherrypy.mk
+++ package/python-cherrypy/python-cherrypy.mk
@@ -4,12 +4,14 @@
 #
 ################################################################################
 
-PYTHON_CHERRYPY_VERSION = 18.8.0
+PYTHON_CHERRYPY_VERSION = 18.9.0
 PYTHON_CHERRYPY_SOURCE = CherryPy-$(PYTHON_CHERRYPY_VERSION).tar.gz
-PYTHON_CHERRYPY_SITE = https://files.pythonhosted.org/packages/60/ea/6c4d16b0cd1f4f64a478bac8a37d75a585e854afb5693ce80a9711efdc4a
+PYTHON_CHERRYPY_SITE = https://files.pythonhosted.org/packages/bd/5f/e265a49883bfcfb7f2c3d3d9e96197cfe8136783e96c5ce20e201550aaa0
 PYTHON_CHERRYPY_LICENSE = BSD-3-Clause
 PYTHON_CHERRYPY_LICENSE_FILES = LICENSE.md
 PYTHON_CHERRYPY_SETUP_TYPE = setuptools
-PYTHON_CHERRYPY_DEPENDENCIES = host-python-setuptools-scm
+PYTHON_CHERRYPY_DEPENDENCIES = \
+	host-python-setuptools-scm \
+	host-python-setuptools-scm-git-archive
 
 $(eval $(python-package))
