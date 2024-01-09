beef64d5ea34577f7539d4323d05a0c59bd612c9
James Hilliard <james.hilliard1@gmail.com>
2024-01-09 01:19:17 -0700

    package/python-hwdata: bump to version 2.4.1
    Drop no longer required license file download.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-hwdata/python-hwdata.mk
+++ package/python-hwdata/python-hwdata.mk
@@ -4,20 +4,11 @@
 #
 ################################################################################
 
-PYTHON_HWDATA_VERSION = 2.3.7
+PYTHON_HWDATA_VERSION = 2.4.1
 PYTHON_HWDATA_SOURCE = hwdata-$(PYTHON_HWDATA_VERSION).tar.gz
-PYTHON_HWDATA_SITE = https://files.pythonhosted.org/packages/15/26/f5bc1b42129fbcbd1c99c29714af1685fc89e2cf37680a9930d4fcac1808
+PYTHON_HWDATA_SITE = https://files.pythonhosted.org/packages/ff/ec/4a6e57d765a63f7e9a8c5348f0bf3f7d39243dc5bc695fa1e887ca5856c8
 PYTHON_HWDATA_SETUP_TYPE = setuptools
 PYTHON_HWDATA_LICENSE = GPL-2.0+
 PYTHON_HWDATA_LICENSE_FILES = LICENSE
-
-# There is no LICENSE file in the PyPi tarball, but it is available in
-# upstream git repository:
-PYTHON_HWDATA_EXTRA_DOWNLOADS = https://raw.githubusercontent.com/xsuchy/python-hwdata/python-hwdata-$(PYTHON_HWDATA_VERSION)-1/LICENSE
-
-define PYTHON_HWDATA_ADD_LICENSE_FILE
-	cp $(PYTHON_HWDATA_DL_DIR)/LICENSE $(@D)
-endef
-PYTHON_HWDATA_POST_EXTRACT_HOOKS += PYTHON_HWDATA_ADD_LICENSE_FILE
 
 $(eval $(python-package))
