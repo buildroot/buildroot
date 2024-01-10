2f4126a9cc5b7e044a90edad20e2e7de6f090fc7
James Hilliard <james.hilliard1@gmail.com>
2024-01-09 22:08:13 -0700

    package/python-pyhamcrest: bump to version 2.1.0
    Migrate from setuptools to hatchling pep517 build backend.
    
    Drop no longer required python-setuptools runtime dependency.
    
    Drop no longer required python-six runtime dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-pyhamcrest/python-pyhamcrest.mk
+++ package/python-pyhamcrest/python-pyhamcrest.mk
@@ -4,11 +4,14 @@
 #
 ################################################################################
 
-PYTHON_PYHAMCREST_VERSION = 2.0.4
+PYTHON_PYHAMCREST_VERSION = 2.1.0
 PYTHON_PYHAMCREST_SOURCE = pyhamcrest-$(PYTHON_PYHAMCREST_VERSION).tar.gz
-PYTHON_PYHAMCREST_SITE = https://files.pythonhosted.org/packages/b1/9a/588f086b64ace8d2e9843d8551e9068b2570c3c51b06cb49a107303f8700
-PYTHON_PYHAMCREST_SETUP_TYPE = setuptools
+PYTHON_PYHAMCREST_SITE = https://files.pythonhosted.org/packages/16/3f/f286caba4e64391a8dc9200e6de6ce0d07471e3f718248c3276843b7793b
+PYTHON_PYHAMCREST_SETUP_TYPE = pep517
 PYTHON_PYHAMCREST_LICENSE = BSD-3-Clause
 PYTHON_PYHAMCREST_LICENSE_FILES = LICENSE.txt
+PYTHON_PYHAMCREST_DEPENDENCIES = \
+	host-python-hatchling \
+	host-python-hatch-vcs
 
 $(eval $(python-package))
