1ddea7db1039c68f3b3ad42fe6b97c0ed0c32e5c
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-01-10 23:02:32 +0100

    package/libuev: security bump to version 2.4.1
    - Fix #27: possible buffer overrun in uev_run()
    - Update hash of LICENSE file (update in year with
      https://github.com/troglobit/libuev/commit/0f5909615439d2a3d6a9b44f5a7570e071b90448)
    
    https://github.com/troglobit/libuev/blob/v2.4.1/ChangeLog.md
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit e3518dd952d20d64bc9cd24ebf025b4e7b575baf)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libuev/libuev.mk
+++ package/libuev/libuev.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBUEV_VERSION = 2.4.0
+LIBUEV_VERSION = 2.4.1
 LIBUEV_SOURCE = libuev-$(LIBUEV_VERSION).tar.xz
 LIBUEV_SITE = https://github.com/troglobit/libuev/releases/download/v$(LIBUEV_VERSION)
 LIBUEV_LICENSE = MIT
