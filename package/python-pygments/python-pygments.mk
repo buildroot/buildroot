9d7b0259e21335c8da8566a581ef84d785d5a861
James Hilliard <james.hilliard1@gmail.com>
2024-01-09 22:03:37 -0700

    package/python-pygments: bump to version 2.17.2
    Migrate from setuptools to hatchling pep517 build backend.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-pygments/python-pygments.mk
+++ package/python-pygments/python-pygments.mk
@@ -4,13 +4,14 @@
 #
 ################################################################################
 
-PYTHON_PYGMENTS_VERSION = 2.15.1
-PYTHON_PYGMENTS_SOURCE = Pygments-$(PYTHON_PYGMENTS_VERSION).tar.gz
-PYTHON_PYGMENTS_SITE = https://files.pythonhosted.org/packages/89/6b/2114e54b290824197006e41be3f9bbe1a26e9c39d1f5fa20a6d62945a0b3
+PYTHON_PYGMENTS_VERSION = 2.17.2
+PYTHON_PYGMENTS_SOURCE = pygments-$(PYTHON_PYGMENTS_VERSION).tar.gz
+PYTHON_PYGMENTS_SITE = https://files.pythonhosted.org/packages/55/59/8bccf4157baf25e4aa5a0bb7fa3ba8600907de105ebc22b0c78cfbf6f565
 PYTHON_PYGMENTS_LICENSE = BSD-2-Clause
 PYTHON_PYGMENTS_LICENSE_FILES = LICENSE
 PYTHON_PYGMENTS_CPE_ID_VENDOR = pygments
 PYTHON_PYGMENTS_CPE_ID_PRODUCT = pygments
-PYTHON_PYGMENTS_SETUP_TYPE = setuptools
+PYTHON_PYGMENTS_SETUP_TYPE = pep517
+PYTHON_PYGMENTS_DEPENDENCIES = host-python-hatchling
 
 $(eval $(python-package))
