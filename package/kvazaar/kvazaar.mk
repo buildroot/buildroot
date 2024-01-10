8ceab1f2cd105f683b80e485422c8e10d2048b01
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-01-10 22:56:54 +0100

    package/kvazaar: bump to version 2.2.0
    Update hash of LICENSE (new line added with
    https://github.com/ultravideo/kvazaar/commit/7856583ee05f627bbab153bedb68d586ccb3750d)
    
    https://github.com/ultravideo/kvazaar/releases/tag/v2.2.0
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/kvazaar/kvazaar.mk
+++ package/kvazaar/kvazaar.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-KVAZAAR_VERSION = 2.1.0
+KVAZAAR_VERSION = 2.2.0
 KVAZAAR_SOURCE = kvazaar-$(KVAZAAR_VERSION).tar.xz
 KVAZAAR_SITE = https://github.com/ultravideo/kvazaar/releases/download/v$(KVAZAAR_VERSION)
 KVAZAAR_LICENSE = BSD-3-Clause, ISC (greatest, x264asm)
-KVAZAAR_LICENSE_FILES = LICENSE LICENSE.greatest LICENSE.x264asm
+KVAZAAR_LICENSE_FILES = LICENSE LICENSE.EXT.greatest LICENSE.EXT.x264asm
 KVAZAAR_INSTALL_STAGING = YES
 KVAZAAR_DEPENDENCIES = host-pkgconf
 
