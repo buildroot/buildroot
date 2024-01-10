3045d747369fd8b29be6733b5e85c286ff100552
James Hilliard <james.hilliard1@gmail.com>
2024-01-09 21:19:30 -0700

    package/python-pathspec: bump to version 0.12.1
    Migrate from setuptools to flit build backend.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-pathspec/python-pathspec.mk
+++ package/python-pathspec/python-pathspec.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_PATHSPEC_VERSION = 0.11.2
+PYTHON_PATHSPEC_VERSION = 0.12.1
 PYTHON_PATHSPEC_SOURCE = pathspec-$(PYTHON_PATHSPEC_VERSION).tar.gz
-PYTHON_PATHSPEC_SITE = https://files.pythonhosted.org/packages/a0/2a/bd167cdf116d4f3539caaa4c332752aac0b3a0cc0174cdb302ee68933e81
+PYTHON_PATHSPEC_SITE = https://files.pythonhosted.org/packages/ca/bc/f35b8446f4531a7cb215605d100cd88b7ac6f44ab3fc94870c120ab3adbf
 PYTHON_PATHSPEC_LICENSE = MPL-2.0
 PYTHON_PATHSPEC_LICENSE_FILES = LICENSE
-PYTHON_PATHSPEC_SETUP_TYPE = setuptools
+PYTHON_PATHSPEC_SETUP_TYPE = flit
 
 $(eval $(host-python-package))
