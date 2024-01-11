a114e72a4716d3a19480317894d5ac46e318a5f6
James Hilliard <james.hilliard1@gmail.com>
2024-01-10 22:00:53 -0700

    package/python-reedsolo: bump to version 1.7.0
    License changed to Unlicense/MIT-0:
    https://github.com/tomerfiliba-org/reedsolomon/commit/7b97461684701cf19451db1fd348d831dc526dc2
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/python-reedsolo/python-reedsolo.mk
+++ package/python-reedsolo/python-reedsolo.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_REEDSOLO_VERSION = 1.6.0
+PYTHON_REEDSOLO_VERSION = 1.7.0
 PYTHON_REEDSOLO_SOURCE = reedsolo-$(PYTHON_REEDSOLO_VERSION).tar.gz
-PYTHON_REEDSOLO_SITE = https://files.pythonhosted.org/packages/9b/10/28d1492cc82a103bc06f18cb9a9dbb3a9168ab2e4068801fa0aa0c76b231
+PYTHON_REEDSOLO_SITE = https://files.pythonhosted.org/packages/f7/61/a67338cbecf370d464e71b10e9a31355f909d6937c3a8d6b17dd5d5beb5e
 PYTHON_REEDSOLO_SETUP_TYPE = setuptools
-PYTHON_REEDSOLO_LICENSE = Public Domain
+PYTHON_REEDSOLO_LICENSE = MIT-0, Unlicense
 PYTHON_REEDSOLO_LICENSE_FILES = LICENSE
 
 $(eval $(python-package))
