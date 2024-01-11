7d91366dac08f26ce023c757631ccb6cd9e2f2ea
James Hilliard <james.hilliard1@gmail.com>
2024-01-11 00:37:24 -0700

    package/python-tempora: bump to version 5.5.0
    License hash changed due to copyright notice removal:
    https://github.com/jaraco/tempora/commit/5957d58266e479f124b31f30e4322e798fdf386b
    
    Drop no longer required python-six runtime dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-tempora/python-tempora.mk
+++ package/python-tempora/python-tempora.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_TEMPORA_VERSION = 5.2.0
+PYTHON_TEMPORA_VERSION = 5.5.0
 PYTHON_TEMPORA_SOURCE = tempora-$(PYTHON_TEMPORA_VERSION).tar.gz
-PYTHON_TEMPORA_SITE = https://files.pythonhosted.org/packages/97/70/5cf5031a83ca6d0431a53459b2a98aa387ba23d06710a5e48d681ff3956f
+PYTHON_TEMPORA_SITE = https://files.pythonhosted.org/packages/c9/dc/97d90b9f64dbe4f599023e19602b33a2cced68462db67a3d4805a77cf784
 PYTHON_TEMPORA_LICENSE = MIT
 PYTHON_TEMPORA_LICENSE_FILES = LICENSE
 PYTHON_TEMPORA_SETUP_TYPE = setuptools
