00702e3ffde1ebce9e2a2cfb63af7992a627c149
James Hilliard <james.hilliard1@gmail.com>
2024-01-10 22:22:25 -0700

    package/python-rtoml: bump to version 0.10.0
    Migrate from setuptools-rust to maturin infrastructure.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>

--- package/python-rtoml/python-rtoml.mk
+++ package/python-rtoml/python-rtoml.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_RTOML_VERSION = 0.8.0
+PYTHON_RTOML_VERSION = 0.10.0
 PYTHON_RTOML_SOURCE = rtoml-$(PYTHON_RTOML_VERSION).tar.gz
-PYTHON_RTOML_SITE = https://files.pythonhosted.org/packages/33/a6/b42d8e0e28bec9fd7fdbafb2d76db3f8578f151a669eba564d422756d909
-PYTHON_RTOML_SETUP_TYPE = setuptools-rust
+PYTHON_RTOML_SITE = https://files.pythonhosted.org/packages/ca/b2/0e77a00e75ed582ec1c4c3a9e1eeed886a15c195bcce87b7daf1171c4115
+PYTHON_RTOML_SETUP_TYPE = maturin
 PYTHON_RTOML_LICENSE = MIT
 PYTHON_RTOML_LICENSE_FILES = LICENSE
 
