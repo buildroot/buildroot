bcaf6b43279ba711888222bdad5b6988831b2d70
James Hilliard <james.hilliard1@gmail.com>
2024-01-11 00:13:54 -0700

    package/python-soupsieve: bump to version 2.5
    License hash changed due to year update:
    https://github.com/facelessuser/soupsieve/commit/1615584814d597ce87d4b4d675250cbe20e3fa30
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/python-soupsieve/python-soupsieve.mk
+++ package/python-soupsieve/python-soupsieve.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SOUPSIEVE_VERSION = 2.3.2.post1
+PYTHON_SOUPSIEVE_VERSION = 2.5
 PYTHON_SOUPSIEVE_SOURCE = soupsieve-$(PYTHON_SOUPSIEVE_VERSION).tar.gz
-PYTHON_SOUPSIEVE_SITE = https://files.pythonhosted.org/packages/f3/03/bac179d539362319b4779a00764e95f7542f4920084163db6b0fd4742d38
+PYTHON_SOUPSIEVE_SITE = https://files.pythonhosted.org/packages/ce/21/952a240de1c196c7e3fbcd4e559681f0419b1280c617db21157a0390717b
 PYTHON_SOUPSIEVE_SETUP_TYPE = pep517
 PYTHON_SOUPSIEVE_LICENSE = MIT
 PYTHON_SOUPSIEVE_LICENSE_FILES = LICENSE.md
