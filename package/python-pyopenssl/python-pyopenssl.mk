0c80a1b9f19b8cb7c40aa0f9b34a2b9214d2451d
James Hilliard <james.hilliard1@gmail.com>
2024-01-10 18:58:55 -0700

    package/python-pyopenssl: bump to version 23.3.0
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/python-pyopenssl/python-pyopenssl.mk
+++ package/python-pyopenssl/python-pyopenssl.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYOPENSSL_VERSION = 23.0.0
+PYTHON_PYOPENSSL_VERSION = 23.3.0
 PYTHON_PYOPENSSL_SOURCE = pyOpenSSL-$(PYTHON_PYOPENSSL_VERSION).tar.gz
-PYTHON_PYOPENSSL_SITE = https://files.pythonhosted.org/packages/af/6e/0706d5e0eac08fcff586366f5198c9bf0a8b46f0f45b1858324e0d94c295
+PYTHON_PYOPENSSL_SITE = https://files.pythonhosted.org/packages/bf/a0/e667c3c43b65a188cc3041fa00c50655315b93be45182b2c94d185a2610e
 PYTHON_PYOPENSSL_LICENSE = Apache-2.0
 PYTHON_PYOPENSSL_LICENSE_FILES = LICENSE
 PYTHON_PYOPENSSL_CPE_ID_VENDOR = pyopenssl
