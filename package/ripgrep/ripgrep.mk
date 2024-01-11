529b62f261ce01b347759d9516bf0852d968f751
Antoine Coutant <antoine.coutant@smile.fr>
2024-01-11 11:38:19 +0100

    package/ripgrep: bump to version 14.1.0
    Changelog:
    https://github.com/BurntSushi/ripgrep/blob/master/CHANGELOG.md
    
    The patch has been rebased to ripgrep 14.1.0.
    
    Signed-off-by: Antoine Coutant <antoine.coutant@smile.fr>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/ripgrep/ripgrep.mk
+++ package/ripgrep/ripgrep.mk
@@ -4,11 +4,7 @@
 #
 ################################################################################
 
-# Same as 13.0.0, we use a Git commit hash because the hash of this
-# tarball changed when moving to the cargo-package infrastructure, and
-# we can't change the hash of existing tarball. Please switch back to
-# a Git tag at the next version bump.
-RIPGREP_VERSION = af6b6c543b224d348a8876f0c06245d9ea7929c5
+RIPGREP_VERSION = 14.1.0
 RIPGREP_SITE = $(call github,burntsushi,ripgrep,$(RIPGREP_VERSION))
 RIPGREP_LICENSE = MIT
 RIPGREP_LICENSE_FILES = LICENSE-MIT
