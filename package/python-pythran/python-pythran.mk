4265ac62b84091c231f318fb950800926036426f
James Hilliard <james.hilliard1@gmail.com>
2024-01-10 21:08:35 -0700

    package/python-pythran: bump to version 0.15.0
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/python-pythran/python-pythran.mk
+++ package/python-pythran/python-pythran.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYTHRAN_VERSION = 0.13.1
+PYTHON_PYTHRAN_VERSION = 0.15.0
 PYTHON_PYTHRAN_SOURCE = pythran-$(PYTHON_PYTHRAN_VERSION).tar.gz
-PYTHON_PYTHRAN_SITE = https://files.pythonhosted.org/packages/8d/d8/b27e8dc3f3a03dcd317d40d9df0ae07ebbd85444585973ceba07716934d0
+PYTHON_PYTHRAN_SITE = https://files.pythonhosted.org/packages/82/31/cc6fd7a2b91efc6cdb03e7c42df895b4a65a8f049b074579d45d1def746f
 PYTHON_PYTHRAN_SETUP_TYPE = setuptools
 PYTHON_PYTHRAN_LICENSE = BSD-3-Clause
 PYTHON_PYTHRAN_LICENSE_FILES = LICENSE docs/LICENSE.rst
