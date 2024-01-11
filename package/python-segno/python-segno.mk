081162580f99d1a68133f7f513bf2cd6d1b8b6b6
James Hilliard <james.hilliard1@gmail.com>
2024-01-10 22:52:29 -0700

    package/python-segno: bump to version 1.6.0
    License hash changed due to formatting change:
    https://github.com/heuer/segno/commit/5f61bc42535b836c30015a0cef5db1e52ef8ac67
    
    Drop no longer required python-setuptools runtime dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-segno/python-segno.mk
+++ package/python-segno/python-segno.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SEGNO_VERSION = 1.5.2
+PYTHON_SEGNO_VERSION = 1.6.0
 PYTHON_SEGNO_SOURCE = segno-$(PYTHON_SEGNO_VERSION).tar.gz
-PYTHON_SEGNO_SITE = https://files.pythonhosted.org/packages/90/2a/2fedf1023f9273d8326362df7936748ebadef92ba53ab7970d9b8df1a6c2
+PYTHON_SEGNO_SITE = https://files.pythonhosted.org/packages/51/a8/960844fec8d853a4e8e91f64bdde323ea5a2a44357eca799e7f7f7bf2f1e
 PYTHON_SEGNO_SETUP_TYPE = setuptools
 PYTHON_SEGNO_LICENSE = BSD-3-Clause
 PYTHON_SEGNO_LICENSE_FILES = LICENSE
