cc1181524da559b684320ee4d3a5a0fdd69dc3bc
James Hilliard <james.hilliard1@gmail.com>
2024-01-10 23:05:52 -0700

    package/python-sentry-sdk: bump to version 1.39.2
    License changed to MIT:
    https://github.com/getsentry/sentry-python/commit/9ed5e27636d05bc30cd363c19a032ace8447f5ad
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/python-sentry-sdk/python-sentry-sdk.mk
+++ package/python-sentry-sdk/python-sentry-sdk.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_SENTRY_SDK_VERSION = 1.12.1
+PYTHON_SENTRY_SDK_VERSION = 1.39.2
 PYTHON_SENTRY_SDK_SOURCE = sentry-sdk-$(PYTHON_SENTRY_SDK_VERSION).tar.gz
-PYTHON_SENTRY_SDK_SITE = https://files.pythonhosted.org/packages/32/03/496a7c8313658dec419303e8add969f314b7538c29f4ec5ebe6d86fb8ce2
+PYTHON_SENTRY_SDK_SITE = https://files.pythonhosted.org/packages/66/90/9deac5c137e9f0ede908757d886af77938ef28f94b24decedbdf98f1ca4d
 PYTHON_SENTRY_SDK_SETUP_TYPE = setuptools
-PYTHON_SENTRY_SDK_LICENSE = BSD-2-Clause
+PYTHON_SENTRY_SDK_LICENSE = MIT
 PYTHON_SENTRY_SDK_LICENSE_FILES = LICENSE
 
 $(eval $(python-package))
