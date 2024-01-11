6925d696c1fdd89e2e86fca5ae70dc36c3b11d0a
James Hilliard <james.hilliard1@gmail.com>
2024-01-10 19:14:34 -0700

    package/python-pyrsistent: bump to version 0.20.0
    License hash changed due to year update:
    https://github.com/tobgu/pyrsistent/commit/acdffe6967a821165155ac16bee48054fd61385a
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/python-pyrsistent/python-pyrsistent.mk
+++ package/python-pyrsistent/python-pyrsistent.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYRSISTENT_VERSION = 0.19.3
+PYTHON_PYRSISTENT_VERSION = 0.20.0
 PYTHON_PYRSISTENT_SOURCE = pyrsistent-$(PYTHON_PYRSISTENT_VERSION).tar.gz
-PYTHON_PYRSISTENT_SITE = https://files.pythonhosted.org/packages/bf/90/445a7dbd275c654c268f47fa9452152709134f61f09605cf776407055a89
+PYTHON_PYRSISTENT_SITE = https://files.pythonhosted.org/packages/ce/3a/5031723c09068e9c8c2f0bc25c3a9245f2b1d1aea8396c787a408f2b95ca
 PYTHON_PYRSISTENT_SETUP_TYPE = setuptools
 PYTHON_PYRSISTENT_LICENSE = MIT
 PYTHON_PYRSISTENT_LICENSE_FILES = LICENSE.mit
