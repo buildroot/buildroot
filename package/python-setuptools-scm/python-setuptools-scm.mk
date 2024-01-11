03e360c380f5398fe168296284c7cf46318ca425
James Hilliard <james.hilliard1@gmail.com>
2024-01-10 23:36:22 -0700

    package/python-setuptools-scm: bump to version 8.0.4
    Migrate from setuptools to intree pep517 build backend.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/python-setuptools-scm/python-setuptools-scm.mk
+++ package/python-setuptools-scm/python-setuptools-scm.mk
@@ -4,15 +4,16 @@
 #
 ################################################################################
 
-PYTHON_SETUPTOOLS_SCM_VERSION = 7.1.0
-PYTHON_SETUPTOOLS_SCM_SOURCE = setuptools_scm-$(PYTHON_SETUPTOOLS_SCM_VERSION).tar.gz
-PYTHON_SETUPTOOLS_SCM_SITE = https://files.pythonhosted.org/packages/98/12/2c1e579bb968759fc512391473340d0661b1a8c96a59fb7c65b02eec1321
+PYTHON_SETUPTOOLS_SCM_VERSION = 8.0.4
+PYTHON_SETUPTOOLS_SCM_SOURCE = setuptools-scm-$(PYTHON_SETUPTOOLS_SCM_VERSION).tar.gz
+PYTHON_SETUPTOOLS_SCM_SITE = https://files.pythonhosted.org/packages/eb/b1/0248705f10f6de5eefe7ff93e399f7192257b23df4d431d2f5680bb2778f
 PYTHON_SETUPTOOLS_SCM_LICENSE = MIT
 PYTHON_SETUPTOOLS_SCM_LICENSE_FILES = LICENSE
-PYTHON_SETUPTOOLS_SCM_SETUP_TYPE = setuptools
+PYTHON_SETUPTOOLS_SCM_SETUP_TYPE = pep517
 
 HOST_PYTHON_SETUPTOOLS_SCM_DEPENDENCIES = \
 	host-python-packaging \
+	host-python-setuptools \
 	host-python-typing-extensions
 
 $(eval $(host-python-package))
