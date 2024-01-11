67b1eb8a6de5378b041cff9a0289a42a6760cbca
James Hilliard <james.hilliard1@gmail.com>
2024-01-10 21:30:52 -0700

    package/python-pyzmq: bump to version 25.1.2
    License hashes/names changed due to rename/reformat:
    https://github.com/zeromq/pyzmq/pull/1837
    
    Add new host-python-setuptools-scm build dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/python-pyzmq/python-pyzmq.mk
+++ package/python-pyzmq/python-pyzmq.mk
@@ -4,15 +4,18 @@
 #
 ################################################################################
 
-PYTHON_PYZMQ_VERSION = 24.0.1
+PYTHON_PYZMQ_VERSION = 25.1.2
 PYTHON_PYZMQ_SOURCE = pyzmq-$(PYTHON_PYZMQ_VERSION).tar.gz
-PYTHON_PYZMQ_SITE = https://files.pythonhosted.org/packages/46/0d/b06cf99a64d4187632f4ac9ddf6be99cd35de06fe72d75140496a8e0eef5
+PYTHON_PYZMQ_SITE = https://files.pythonhosted.org/packages/3a/33/1a3683fc9a4bd64d8ccc0290da75c8f042184a1a49c146d28398414d3341
 PYTHON_PYZMQ_LICENSE = LGPL-3.0+, BSD-3-Clause, Apache-2.0
 # Apache license only online: http://www.apache.org/licenses/LICENSE-2.0
-PYTHON_PYZMQ_LICENSE_FILES = COPYING.LESSER COPYING.BSD
-PYTHON_PYZMQ_DEPENDENCIES = host-python-packaging zeromq
+PYTHON_PYZMQ_LICENSE_FILES = LICENSE.LESSER LICENSE.BSD
 PYTHON_PYZMQ_SETUP_TYPE = setuptools
 PYTHON_PYZMQ_BUILD_OPTS = --zmq=$(STAGING_DIR)/usr
+PYTHON_PYZMQ_DEPENDENCIES = \
+	host-python-packaging \
+	host-python-setuptools-scm \
+	zeromq
 
 # Due to issues with cross-compiling, hardcode to the zeromq in BR
 define PYTHON_PYZMQ_PATCH_ZEROMQ_VERSION
