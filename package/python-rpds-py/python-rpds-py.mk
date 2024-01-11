dd5f808be1057434575322711b0e4775403e4d38
James Hilliard <james.hilliard1@gmail.com>
2024-01-10 22:17:26 -0700

    package/python-rpds-py: bump to version 0.16.2
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>

--- package/python-rpds-py/python-rpds-py.mk
+++ package/python-rpds-py/python-rpds-py.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_RPDS_PY_VERSION = 0.12.0
+PYTHON_RPDS_PY_VERSION = 0.16.2
 PYTHON_RPDS_PY_SOURCE = rpds_py-$(PYTHON_RPDS_PY_VERSION).tar.gz
-PYTHON_RPDS_PY_SITE = https://files.pythonhosted.org/packages/75/be/e3f366aa4cd1e3a814f136773e506fc5423eff903ef0372a251df34e6e45
+PYTHON_RPDS_PY_SITE = https://files.pythonhosted.org/packages/c2/63/94a1e9406b34888bdf8506e91d654f1cd84365a5edafa5f8ff0c97d4d9e1
 PYTHON_RPDS_PY_SETUP_TYPE = maturin
 PYTHON_RPDS_PY_LICENSE = MIT
 PYTHON_RPDS_PY_LICENSE_FILES = LICENSE
