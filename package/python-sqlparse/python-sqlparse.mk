e29d91d6ee7513b2a0e4bb49009420c255b9ca5c
James Hilliard <james.hilliard1@gmail.com>
2024-01-11 00:21:46 -0700

    package/python-sqlparse: bump to version 0.4.4
    Migrate from setuptools to flit build backend.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-sqlparse/python-sqlparse.mk
+++ package/python-sqlparse/python-sqlparse.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_SQLPARSE_VERSION = 0.4.3
+PYTHON_SQLPARSE_VERSION = 0.4.4
 PYTHON_SQLPARSE_SOURCE = sqlparse-$(PYTHON_SQLPARSE_VERSION).tar.gz
-PYTHON_SQLPARSE_SITE = https://files.pythonhosted.org/packages/ba/fa/5b7662b04b69f3a34b8867877e4dbf2a37b7f2a5c0bbb5a9eed64efd1ad1
-PYTHON_SQLPARSE_SETUP_TYPE = setuptools
+PYTHON_SQLPARSE_SITE = https://files.pythonhosted.org/packages/65/16/10f170ec641ed852611b6c9441b23d10b5702ab5288371feab3d36de2574
+PYTHON_SQLPARSE_SETUP_TYPE = flit
 PYTHON_SQLPARSE_LICENSE = BSD-3-Clause
 PYTHON_SQLPARSE_LICENSE_FILES = LICENSE
 PYTHON_SQLPARSE_CPE_ID_VENDOR = sqlparse_project
