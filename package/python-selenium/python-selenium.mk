84fd8580aaaa346176e43df35823b78fd3fa686a
James Hilliard <james.hilliard1@gmail.com>
2024-01-10 22:57:42 -0700

    package/python-selenium: bump to version 4.16.0
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-selenium/python-selenium.mk
+++ package/python-selenium/python-selenium.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SELENIUM_VERSION = 4.9.1
+PYTHON_SELENIUM_VERSION = 4.16.0
 PYTHON_SELENIUM_SOURCE = selenium-$(PYTHON_SELENIUM_VERSION).tar.gz
-PYTHON_SELENIUM_SITE = https://files.pythonhosted.org/packages/fd/e2/0e5bee6762a7bf7852b47a79c5b12f9e526e6962958dbb9719fa490ba24c
+PYTHON_SELENIUM_SITE = https://files.pythonhosted.org/packages/16/fd/a0ef793383077dd6296e4637acc82d1e9893e9a49a47f56e96996909e427
 PYTHON_SELENIUM_SETUP_TYPE = setuptools
 PYTHON_SELENIUM_LICENSE = Apache-2.0
 
