4a7afebe03803484261a82b5ca3fa59813e9b324
James Hilliard <james.hilliard1@gmail.com>
2024-01-10 20:03:56 -0700

    package/python-pytest-asyncio: bump to version 0.23.3
    Add new host-python-setuptools-scm build dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Reviewed-by: Marcin Niestroj <m.niestroj@grinn-global.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/python-pytest-asyncio/python-pytest-asyncio.mk
+++ package/python-pytest-asyncio/python-pytest-asyncio.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_PYTEST_ASYNCIO_VERSION = 0.21.1
+PYTHON_PYTEST_ASYNCIO_VERSION = 0.23.3
 PYTHON_PYTEST_ASYNCIO_SOURCE = pytest-asyncio-$(PYTHON_PYTEST_ASYNCIO_VERSION).tar.gz
-PYTHON_PYTEST_ASYNCIO_SITE = https://files.pythonhosted.org/packages/5a/85/d39ef5f69d5597a206f213ce387bcdfa47922423875829f7a98a87d33281
+PYTHON_PYTEST_ASYNCIO_SITE = https://files.pythonhosted.org/packages/1d/27/f036ec4bcbfd056c54572d7169ba3dbb54e7181f02f21caadd3aecb9cf5b
 PYTHON_PYTEST_ASYNCIO_SETUP_TYPE = setuptools
 PYTHON_PYTEST_ASYNCIO_LICENSE = Apache-2.0
 PYTHON_PYTEST_ASYNCIO_LICENSE_FILES = LICENSE
+PYTHON_PYTEST_ASYNCIO_DEPENDENCIES = host-python-setuptools-scm
 
 $(eval $(python-package))
