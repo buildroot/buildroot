8b34f72fbe882e591d6bcca0c540b3449b457190
Adam Duskett <adam.duskett@amarulasolutions.com>
2024-01-17 15:50:44 -0700

    package/flutter-engine: bump version to 3.16.8
    Signed-off-by: Adam Duskett <adam.duskett@amarulasolutions.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/flutter-engine/flutter-engine.mk
+++ package/flutter-engine/flutter-engine.mk
@@ -21,7 +21,7 @@
 #
 # There is no hash provided, as the gn binary (used for configuration) relies
 # on the .git directories. As such, a reproducible tarball is not possible.
-FLUTTER_ENGINE_VERSION = 3.16.5
+FLUTTER_ENGINE_VERSION = 3.16.8
 
 # There is nothing for Buildroot to download. This is handled by gclient.
 FLUTTER_ENGINE_SITE =
