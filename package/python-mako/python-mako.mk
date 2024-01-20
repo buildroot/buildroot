a4694fc7b09e9cf6cb2416e6a8b080930656fe0a
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-01-20 21:47:01 +0100

    package/python-mako: add CPE variables
    cpe:2.3:a:sqlalchemy:mako is a valid CPE identifier for this package:
    
      https://nvd.nist.gov/products/cpe/detail/3A26A531-FD34-4F27-ADD5-C0C88FA337B3
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit d5a20c7f245313ead143b0c30060e49e1f90d40a)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-mako/python-mako.mk
+++ package/python-mako/python-mako.mk
@@ -10,6 +10,8 @@ PYTHON_MAKO_SITE = https://files.pythonhosted.org/packages/05/5f/2ba6e026d33a0e6
 PYTHON_MAKO_SETUP_TYPE = setuptools
 PYTHON_MAKO_LICENSE = MIT
 PYTHON_MAKO_LICENSE_FILES = LICENSE
+PYTHON_MAKO_CPE_ID_VENDOR = sqlalchemy
+PYTHON_MAKO_CPE_ID_PRODUCT = mako
 
 # In host build, setup.py tries to download markupsafe if it is not installed
 HOST_PYTHON_MAKO_DEPENDENCIES = host-python-markupsafe
