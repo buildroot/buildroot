67fa11afccc08f6ca9143a0feba63d0e7680e40e
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-01-20 21:33:51 +0100

    package/python-oauthlib: add CPE variables
    cpe:2.3:a:oauthlib_project:oauthlib is a valid CPE identifier for this
    package:
    
      https://nvd.nist.gov/products/cpe/detail/204A2244-7F74-4AAE-BA8E-E9FE707EC833
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 5f905f06bdf212520ab1f37179b5b6d5a2dee132)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-oauthlib/python-oauthlib.mk
+++ package/python-oauthlib/python-oauthlib.mk
@@ -10,5 +10,7 @@ PYTHON_OAUTHLIB_SITE = https://files.pythonhosted.org/packages/6d/fa/fbf40010379
 PYTHON_OAUTHLIB_SETUP_TYPE = setuptools
 PYTHON_OAUTHLIB_LICENSE = BSD-3-Clause
 PYTHON_OAUTHLIB_LICENSE_FILES = LICENSE
+PYTHON_OAUTHLIB_CPE_ID_VENDOR = oauthlib_project
+PYTHON_OAUTHLIB_CPE_ID_PRODUCT = oauthlib
 
 $(eval $(python-package))
