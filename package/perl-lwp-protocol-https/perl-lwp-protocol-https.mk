588897736e30afd9e0f8713e3004c4173dccc434
Francois Perrad <fperrad@gmail.com>
2024-01-24 11:34:47 +0100

    package/perl-lwp-protocol-https: bump to version 6.12
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/perl-lwp-protocol-https/perl-lwp-protocol-https.mk
+++ package/perl-lwp-protocol-https/perl-lwp-protocol-https.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PERL_LWP_PROTOCOL_HTTPS_VERSION = 6.11
+PERL_LWP_PROTOCOL_HTTPS_VERSION = 6.12
 PERL_LWP_PROTOCOL_HTTPS_SOURCE = LWP-Protocol-https-$(PERL_LWP_PROTOCOL_HTTPS_VERSION).tar.gz
 PERL_LWP_PROTOCOL_HTTPS_SITE = $(BR2_CPAN_MIRROR)/authors/id/O/OA/OALDERS
 PERL_LWP_PROTOCOL_HTTPS_LICENSE = Artistic-1.0-Perl or GPL-1.0+
