a33827760823b1455326e0fc8cf6089a3081e782
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-01-26 18:32:15 +0100

    package/faad2: force arm mode instead of Thumb mode
    Fix the following build failure in Thumb mode:
    
    /tmp/ccfzn6FH.s:36: Error: selected processor does not support `smull r2,r3,r1,r0' in Thumb mode
    
    Fixes:
     - http://autobuild.buildroot.org/results/838808b4751244ee01cde6b8261212b49e511a32
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    [Peter: reword comment slightly]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/faad2/faad2.mk
+++ package/faad2/faad2.mk
@@ -12,4 +12,11 @@ FAAD2_CPE_ID_VENDOR = audiocoding
 FAAD2_CPE_ID_PRODUCT = freeware_advanced_audio_decoder_2
 FAAD2_INSTALL_STAGING = YES
 
+# faad2 contains assembly routines using ARM instructions not present in thumb1 mode:
+# Error: selected processor does not support `smull r2,r3,r1,r0' in Thumb mode
+# so force ARM mode
+ifeq ($(BR2_ARM_INSTRUCTIONS_THUMB),y)
+FAAD2_CONF_OPTS += -DCMAKE_C_FLAGS="$(TARGET_CFLAGS) -marm"
+endif
+
 $(eval $(cmake-package))
