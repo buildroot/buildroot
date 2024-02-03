0a860f330a4ecd2fa442ea2a35e16c0f87611306
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-02-03 18:58:40 +0100

    package/libjwt: add gnutls support
    gnutls can be used if openssl is not available since the addition of the
    package in commit 7251775773011e7da38e3a33c6817eceab6a9dfa
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libjwt/libjwt.mk
+++ package/libjwt/libjwt.mk
@@ -6,10 +6,18 @@
 
 LIBJWT_VERSION = 1.15.3
 LIBJWT_SITE = $(call github,benmcollins,libjwt,v$(LIBJWT_VERSION))
-LIBJWT_DEPENDENCIES = host-pkgconf jansson openssl
+LIBJWT_DEPENDENCIES = host-pkgconf jansson
 LIBJWT_AUTORECONF = YES
 LIBJWT_INSTALL_STAGING = YES
 LIBJWT_LICENSE = MPL-2.0
 LIBJWT_LICENSE_FILES = LICENSE
+
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+LIBJWT_CONF_OPTS += --with-openssl
+LIBJWT_DEPENDENCIES += openssl
+else
+LIBJWT_CONF_OPTS += --without-openssl
+LIBJWT_DEPENDENCIES += gnutls
+endif
 
 $(eval $(autotools-package))
