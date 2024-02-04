a2d461d134d27a3c27abc0efd9c429e08b2beb26
Uladzimir Bely <wiselord1983@gmail.com>
2024-02-04 11:51:28 +0300

    package/python-mpd2: bump to version 3.1.1
    Signed-off-by: Uladzimir Bely <wiselord1983@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-mpd2/python-mpd2.mk
+++ package/python-mpd2/python-mpd2.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PYTHON_MPD2_VERSION = 3.1.0
-PYTHON_MPD2_SITE = https://files.pythonhosted.org/packages/59/32/e57725251ce3117d2ed2a7b76d9722ea9bea26f54b2cc8981d03350e4103
+PYTHON_MPD2_VERSION = 3.1.1
+PYTHON_MPD2_SITE = https://files.pythonhosted.org/packages/53/be/e77206eb35eb37ccd3506fba237e1431431d04c482707730ce2a6802e95c
 PYTHON_MPD2_SETUP_TYPE = setuptools
 PYTHON_MPD2_LICENSE = LGPL-3.0+
 PYTHON_MPD2_LICENSE_FILES = LICENSE.txt
