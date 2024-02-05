24d25ac683c8691126635f77c0b2c066f1799a11
James Hilliard <james.hilliard1@gmail.com>
2024-02-05 09:46:14 -0700

    package/python-starlette: bump to version 0.36.3
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Reviewed-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/python-starlette/python-starlette.mk
+++ package/python-starlette/python-starlette.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_STARLETTE_VERSION = 0.34.0
+PYTHON_STARLETTE_VERSION = 0.36.3
 PYTHON_STARLETTE_SOURCE = starlette-$(PYTHON_STARLETTE_VERSION).tar.gz
-PYTHON_STARLETTE_SITE = https://files.pythonhosted.org/packages/4f/36/168ba6d56a28382f3b081b23b0cc431de35786e120b94c1f372708ed3059
+PYTHON_STARLETTE_SITE = https://files.pythonhosted.org/packages/be/47/1bba49d42d63f4453f0a64a20acbf2d0bd2f5a8cde6a166ee66c074a08f8
 PYTHON_STARLETTE_SETUP_TYPE = pep517
 PYTHON_STARLETTE_LICENSE = BSD-3-Clause
 PYTHON_STARLETTE_LICENSE_FILES = LICENSE.md
