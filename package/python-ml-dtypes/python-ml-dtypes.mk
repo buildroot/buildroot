06e1a3620e5268951be089d2b150c1802de7de4c
James Hilliard <james.hilliard1@gmail.com>
2024-02-06 09:17:15 -0700

    package/python-ml-dtypes: bump to version 0.3.2
    Migrate from distutils which is being deprecated to setuptools.
    
    Drop python-pybind which is no longer required:
    https://github.com/jax-ml/ml_dtypes/blob/v0.3.2/CHANGELOG.md#032---2024-01-03
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-ml-dtypes/python-ml-dtypes.mk
+++ package/python-ml-dtypes/python-ml-dtypes.mk
@@ -4,16 +4,15 @@
 #
 ################################################################################
 
-PYTHON_ML_DTYPES_VERSION = 0.3.1
+PYTHON_ML_DTYPES_VERSION = 0.3.2
 PYTHON_ML_DTYPES_SOURCE = ml_dtypes-$(PYTHON_ML_DTYPES_VERSION).tar.gz
-PYTHON_ML_DTYPES_SITE = https://files.pythonhosted.org/packages/16/6e/9a7a51ee1ca24b8e92109128260c5aec8340c8fe5572e9ceecddae559abe
+PYTHON_ML_DTYPES_SITE = https://files.pythonhosted.org/packages/39/7d/8d85fcba868758b3a546e6914e727abd8f29ea6918079f816975c9eecd63
 PYTHON_ML_DTYPES_LICENSE = Apache-2.0
 PYTHON_ML_DTYPES_LICENSE_FILES = LICENSE
-PYTHON_ML_DTYPES_SETUP_TYPE = distutils
+PYTHON_ML_DTYPES_SETUP_TYPE = setuptools
 
 PYTHON_ML_DTYPES_DEPENDENCIES = \
 	host-python-numpy \
-	python-numpy \
-	python-pybind
+	python-numpy
 
 $(eval $(python-package))
