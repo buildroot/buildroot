7b13cc2c264d20698d7f48a4bbbbdffc0698b06f
Dario Binacchi <dario.binacchi@amarulasolutions.com>
2024-02-06 18:53:29 +0100

    package/uuu: bump to version 1.5.165
    It requires the host-tinyxml2 package.
    
    The hash of README.md has changed because a reference to the tinyxml2
    license has been added:
    
        + - tinyxml (zlib license) is from https://github.com/leethomason/tinyxml2
    
    Release notes:
    https://github.com/nxp-imx/mfgtools/releases/tag/uuu_1.5.163
    https://github.com/nxp-imx/mfgtools/releases/tag/uuu_1.5.165
    
    Signed-off-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>
    [yann.morin.1998@free.fr:
      - split long dependency line
      - reorder dependencies alphabetically
    ]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/uuu/uuu.mk
+++ package/uuu/uuu.mk
@@ -4,11 +4,17 @@
 #
 ################################################################################
 
-UUU_VERSION = 1.5.141
+UUU_VERSION = 1.5.165
 UUU_SOURCE = uuu_source-uuu_$(UUU_VERSION).tar.gz
 UUU_SITE = https://github.com/NXPmicro/mfgtools/releases/download/uuu_$(UUU_VERSION)
 UUU_LICENSE = BSD 3-Clause "New" or "Revised" License
 UUU_LICENSE_FILES = LICENSE README.md
-HOST_UUU_DEPENDENCIES = host-bzip2 host-openssl host-zlib host-libusb host-zstd
+HOST_UUU_DEPENDENCIES = \
+	host-bzip2 \
+	host-libusb \
+	host-openssl \
+	host-tinyxml2 \
+	host-zlib \
+	host-zstd
 
 $(eval $(host-cmake-package))
