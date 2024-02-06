b3803513e87cb75795edfd3205337996ad971bb3
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-02-06 19:07:43 +0100

    package/python-pydantic: add CPE variables
    cpe:2.3:a:pydantic_project:pydantic is a valid CPE identifier for this
    package:
    
      https://nvd.nist.gov/products/cpe/detail/9B7C28E5-ACB1-4252-AE8F-4225C1AF739E
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-pydantic/python-pydantic.mk
+++ package/python-pydantic/python-pydantic.mk
@@ -10,6 +10,8 @@ PYTHON_PYDANTIC_SITE = https://files.pythonhosted.org/packages/aa/3f/56142232152
 PYTHON_PYDANTIC_SETUP_TYPE = pep517
 PYTHON_PYDANTIC_LICENSE = MIT
 PYTHON_PYDANTIC_LICENSE_FILES = LICENSE
+PYTHON_PYDANTIC_CPE_ID_VENDOR = pydantic_project
+PYTHON_PYDANTIC_CPE_ID_PRODUCT = pydantic
 PYTHON_PYDANTIC_DEPENDENCIES = \
 	host-python-hatchling \
 	host-python-hatch-fancy-pypi-readme
