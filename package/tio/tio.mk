4b33d7a47dd41fed5364a398320c2b7d0687ba5e
Sergio Prado <sergio.prado@e-labworks.com>
2024-02-07 12:35:32 -0300

    package/tio: bump to version 2.7
    Signed-off-by: Sergio Prado <sergio.prado@e-labworks.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/tio/tio.mk
+++ package/tio/tio.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TIO_VERSION = 1.47
+TIO_VERSION = 2.7
 TIO_SOURCE = tio-$(TIO_VERSION).tar.xz
 TIO_SITE = https://github.com/tio/tio/releases/download/v$(TIO_VERSION)
 TIO_LICENSE = GPL-2.0+
