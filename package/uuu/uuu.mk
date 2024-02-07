b5d83b4b649ee1e77811d9bf8ca6398725f6cce1
Dario Binacchi <dario.binacchi@amarulasolutions.com>
2024-02-07 18:39:11 +0100

    package/uuu: drop README.md from license files
    The patch drops the file README.md from the license files. Indeed, the
    licensing terms apply to third-party libraries that uuu links to, and
    even though they are bundled in the source tree of uuu|mfgtools, they
    are not used by Buildroot as we use the ones we build. Therefore, the
    actual license file of the package is the file LICENSE.
    
    Moreover, this also leads to simplifying the bump to new versions.
    
    Signed-off-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/uuu/uuu.mk
+++ package/uuu/uuu.mk
@@ -8,7 +8,7 @@ UUU_VERSION = 1.5.165
 UUU_SOURCE = uuu_source-uuu_$(UUU_VERSION).tar.gz
 UUU_SITE = https://github.com/NXPmicro/mfgtools/releases/download/uuu_$(UUU_VERSION)
 UUU_LICENSE = BSD 3-Clause "New" or "Revised" License
-UUU_LICENSE_FILES = LICENSE README.md
+UUU_LICENSE_FILES = LICENSE
 HOST_UUU_DEPENDENCIES = \
 	host-bzip2 \
 	host-libusb \
