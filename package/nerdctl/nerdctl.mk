e37b73f7a2a6f4d2628c7d35f402557357a11101
Christian Stewart <christian@aperture.us>
2024-02-06 18:32:57 -0800

    package/nerdctl: bump version to v1.7.3
    https://github.com/containerd/nerdctl/releases/tag/v1.7.3
    
    Signed-off-by: Christian Stewart <christian@aperture.us>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/nerdctl/nerdctl.mk
+++ package/nerdctl/nerdctl.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NERDCTL_VERSION = 1.3.1
+NERDCTL_VERSION = 1.7.3
 NERDCTL_SITE = $(call github,containerd,nerdctl,v$(NERDCTL_VERSION))
 
 NERDCTL_LICENSE = Apache-2.0
