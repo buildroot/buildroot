52b43a12472a9853ff5d69b136144427c41f2803
Julien Olivain <ju.o@free.fr>
2024-02-10 23:29:40 +0100

    package/jailhouse: bump to commit e57d1ef
    The last Jailhouse tag is v0.12. It dates from 2020-02-04, which is
    4 years old. This version no longer compiles with recent Kernels.
    
    This commit updates Jailhouse to the latest commit e57d1ef, which
    dates from 2023-01-10.
    
    For commit logs, see:
    https://github.com/siemens/jailhouse/commits/master/
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/jailhouse/jailhouse.mk
+++ package/jailhouse/jailhouse.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-JAILHOUSE_VERSION = 0.12
-JAILHOUSE_SITE = $(call github,siemens,jailhouse,v$(JAILHOUSE_VERSION))
+JAILHOUSE_VERSION = e57d1eff6d55aeed5f977fe4e2acfb6ccbdd7560
+JAILHOUSE_SITE = $(call github,siemens,jailhouse,$(JAILHOUSE_VERSION))
 JAILHOUSE_LICENSE = GPL-2.0
 JAILHOUSE_LICENSE_FILES = COPYING
 JAILHOUSE_DEPENDENCIES = \
