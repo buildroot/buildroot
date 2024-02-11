632d8f25b7a36708cddf9628dfaa92d84acc5151
Bernd Kuhls <bernd@kuhls.net>
2024-02-11 17:31:16 +0100

    package/libpng: bump version to 1.6.42
    Release notes:
    https://raw.githubusercontent.com/pnggroup/libpng/v1.6.42/ANNOUNCE
    
    Removed patch which disabled pngfix and png-fix-itxt tools and use the
    new upstream-provided configure option --disable-tools instead:
    https://github.com/pnggroup/libpng/commit/22ad62c25e7355b20d1486da301cb69159db0915
    https://github.com/pnggroup/libpng/commit/71475b064f3e628e55a2f89f55e873945650afe9
    
    Updated license hash due to copyright year bump:
    https://github.com/pnggroup/libpng/commit/0dfe422e9f872d3a851d52fc45047bdfd6da0cf4
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libpng/libpng.mk
+++ package/libpng/libpng.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBPNG_VERSION = 1.6.40
+LIBPNG_VERSION = 1.6.42
 LIBPNG_SERIES = 16
 LIBPNG_SOURCE = libpng-$(LIBPNG_VERSION).tar.xz
 LIBPNG_SITE = http://downloads.sourceforge.net/project/libpng/libpng$(LIBPNG_SERIES)/$(LIBPNG_VERSION)
@@ -15,6 +15,7 @@ LIBPNG_INSTALL_STAGING = YES
 LIBPNG_DEPENDENCIES = host-pkgconf zlib
 HOST_LIBPNG_DEPENDENCIES = host-pkgconf host-zlib
 LIBPNG_CONFIG_SCRIPTS = libpng$(LIBPNG_SERIES)-config libpng-config
+LIBPNG_CONF_OPTS = --disable-tools
 
 ifeq ($(BR2_ARM_CPU_HAS_NEON)$(BR2_aarch64),y)
 LIBPNG_CONF_OPTS += --enable-arm-neon
