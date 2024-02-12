a8c61e14ab5e93dc3b38b9cda65701cc1119611c
Francois Perrad <fperrad@gmail.com>
2024-02-12 18:26:19 +0100

    package/lua-http: fix CVE-2023-4540
    see https://nvd.nist.gov/vuln/detail/CVE-2023-4540
      & https://cert.pl/en/posts/2023/09/CVE-2023-4540
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 431c02174078e359b6379b8021e6aeb06b2dbb9d)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/lua-http/lua-http.mk
+++ package/lua-http/lua-http.mk
@@ -13,4 +13,7 @@ LUA_HTTP_LICENSE_FILES = $(LUA_HTTP_SUBDIR)/LICENSE.md
 LUA_HTTP_CPE_ID_VERSION = $(LUA_HTTP_VERSION_UPSTREAM)
 LUA_HTTP_CPE_ID_VENDOR = daurnimator
 
+# 0001-http-h1_stream-handle-EOF-when-body_read_type-length.patch
+LUA_HTTP_IGNORE_CVES += CVE-2023-4540
+
 $(eval $(luarocks-package))
