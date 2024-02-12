835c647375182da571bfb0170b7cd7750cad38e4
Giulio Benetti <giulio.benetti@benettiengineering.com>
2024-02-12 21:27:08 +0100

    package/rtl8192eu: bump to version 2024-02-09 on branch 5.11.2.1
    It supports up to Linux 6.8.
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/rtl8192eu/rtl8192eu.mk
+++ package/rtl8192eu/rtl8192eu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTL8192EU_VERSION = a928f08c1dd4f9a1e84d85811a543e974551bc4f
+RTL8192EU_VERSION = e90a6043be141bde8847e84223e106eff38ecdfd
 RTL8192EU_SITE = $(call github,clnhub,rtl8192eu-linux,$(RTL8192EU_VERSION))
 RTL8192EU_LICENSE = GPL-2.0
 
