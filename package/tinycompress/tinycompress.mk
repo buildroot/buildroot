9689b075773edc837af9c33355f3ac34b35d5419
Julien Olivain <ju.o@free.fr>
2024-02-13 19:21:57 +0100

    package/tinycompress: bump to version 1.2.11
    For commit log, see:
    https://github.com/alsa-project/tinycompress/commits/v1.2.11/
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/tinycompress/tinycompress.mk
+++ package/tinycompress/tinycompress.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TINYCOMPRESS_VERSION = 1.2.8
+TINYCOMPRESS_VERSION = 1.2.11
 TINYCOMPRESS_SOURCE = tinycompress-$(TINYCOMPRESS_VERSION).tar.bz2
 TINYCOMPRESS_SITE = https://www.alsa-project.org/files/pub/tinycompress
 TINYCOMPRESS_LICENSE = BSD-3-Clause and LGPL-2.1
