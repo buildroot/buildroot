f1759be118de16950063e3c27396fd69b8f2df23
Kadambini Nema <kadambini.nema@gmail.com>
2024-02-15 11:32:06 -0800

    package/iptables: bump version to 1.8.10
    Change Log : https://netfilter.org/projects/iptables/files/changes-iptables-1.8.10.txt
    
    Build test results:
    iptables.config
     BR2_PACKAGE_IPTABLES=y
     BR2_PACKAGE_IPTABLES_BPF_NFSYNPROXY=y
     BR2_PACKAGE_IPTABLES_NFTABLES=y
    
    $ ./utils/test-pkg -c iptables.config iptables
                        bootlin-armv5-uclibc [1/6]: OK
                         bootlin-armv7-glibc [2/6]: OK
                       bootlin-armv7m-uclibc [3/6]: SKIPPED
                         bootlin-x86-64-musl [4/6]: OK
                          br-arm-full-static [5/6]: SKIPPED
                                sourcery-arm [6/6]: OK
    6 builds, 2 skipped, 0 build failed, 0 legal-info failed, 0 show-info failed
    
    Signed-off-by: Kadambini Nema <kadambini.nema@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/iptables/iptables.mk
+++ package/iptables/iptables.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-IPTABLES_VERSION = 1.8.9
+IPTABLES_VERSION = 1.8.10
 IPTABLES_SOURCE = iptables-$(IPTABLES_VERSION).tar.xz
 IPTABLES_SITE = https://netfilter.org/projects/iptables/files
 IPTABLES_INSTALL_STAGING = YES
