56abf4742519e989207b563e7344853c72eb68c0
Gilles Talis <gilles.talis@gmail.com>
2024-02-15 10:24:12 -0400

    package/tesseract-ocr: bump to version 5.3.4
    Changes since version 5.3.3:
    https://github.com/tesseract-ocr/tesseract/compare/5.3.3...5.3.4
    
    Signed-off-by: Gilles Talis <gilles.talis@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/tesseract-ocr/tesseract-ocr.mk
+++ package/tesseract-ocr/tesseract-ocr.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TESSERACT_OCR_VERSION = 5.3.3
+TESSERACT_OCR_VERSION = 5.3.4
 TESSERACT_OCR_DATA_VERSION = 4.1.0
 TESSERACT_OCR_SITE = $(call github,tesseract-ocr,tesseract,$(TESSERACT_OCR_VERSION))
 TESSERACT_OCR_LICENSE = Apache-2.0
