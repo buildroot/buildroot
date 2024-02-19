052deababf60838a19f6334bae1746c841eba877
Raphael Pavlidis <raphael.pavlidis@gmail.com>
2024-02-19 18:48:27 +0000

    package/slirp4netns: bump to version 1.2.3
    Signed-off-by: Raphael Pavlidis <raphael.pavlidis@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/slirp4netns/slirp4netns.mk
+++ package/slirp4netns/slirp4netns.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SLIRP4NETNS_VERSION = 1.2.0
+SLIRP4NETNS_VERSION = 1.2.3
 SLIRP4NETNS_SITE = $(call github,rootless-containers,slirp4netns,v$(SLIRP4NETNS_VERSION))
 SLIRP4NETNS_LICENSE = GPL-2.0
 SLIRP4NETNS_LICENSE_FILES = COPYING
