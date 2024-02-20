buildroot/buildroot

commit b7ee5f3b0e3ef703bafd007cc99da10312d99b9e

package/uacme: bump version to 1.7.5
Signed-off-by: Nicola Di Lieto <nicola.dilieto@gmail.com>
Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

package/uacme/uacme.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UACME_VERSION = 1.7.4
+UACME_VERSION = 1.7.5
 # Released versions are on branch upstream/latest, tagged as
 # upstream/X.Y.Z Do not use vX.Y.Z tags from master, as they do not
 # include .tarball-version

package/uacme/uacme.hash
@@ -1,3 +1,3 @@
 # Locally computed:
-sha256  a638fe0878dba8fdda62f89e7a284243edbcd31b92c11b5cfdc7d3efd9547bca  uacme-1.7.4.tar.gz
+sha256  596b2fba75fedc7a410dd71583c828642ecd486dfdcfde109dfebb82a374abbe  uacme-1.7.5.tar.gz
 sha256  8ceb4b9ee5adedde47b31e975c1d90c73ad27b6b165a1dcd80c7c545eb65b903  COPYING

commit 3e48306a43ed8f21dbd2c1899e0ca02e7e460801

package/uacme: fix build
Fix the following build failure raised since bump to version 1.7.5 in
commit b7ee5f3b0e3ef703bafd007cc99da10312d99b9e and
https://github.com/ndilieto/uacme/commit/fe826f4b1931ae508047d8b2693b5b6ac2cb21fd:

checking if mmap(MAP_ANON|MAP_SHARED) works... configure: error: in `/home/autobuild/autobuild/instance-9/output-1/build/uacme-1.7.5':
configure: error: cannot run test program while cross compiling

Fixes: b7ee5f3b0e3ef703bafd007cc99da10312d99b9e
 - http://autobuild.buildroot.org/results/9715ade98f4894c07b640d151daa41813d2bec3a

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
[yann.morin.1998@free.fr: reference patch in comment]
Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

package/uacme/0001-Fix-cross-compilation.patch
@@ -1,0 +1,29 @@
+From cdf63e24dc475a558400c68714e32d32904e4c57 Mon Sep 17 00:00:00 2001
+From: Nicola Di Lieto <nicola.dilieto@gmail.com>
+Date: Tue, 20 Feb 2024 01:05:00 +0100
+Subject: [PATCH] Fix cross compilation
+
+Commit fe826f4b1931ae508047d8b2693b5b6ac2cb21fd broke cross compilation
+
+Closes https://github.com/ndilieto/uacme/issues/79
+
+Upstream: https://github.com/ndilieto/uacme/commit/cdf63e24dc475a558400c68714e32d32904e4c57
+Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
+---
+ configure.ac | 3 ++-
+ 1 file changed, 2 insertions(+), 1 deletion(-)
+
+diff --git a/configure.ac b/configure.ac
+index 77d1230..3bec75b 100644
+--- a/configure.ac
++++ b/configure.ac
+@@ -355,7 +355,8 @@ if test "x$OPT_UALPN" != "xno"; then
+                 AC_DEFINE(HAVE_MAP_DEVZERO, 1, [if mmap("/dev/zero", MAP_SHARED) works])
+                 AC_MSG_RESULT([yes]),
+                 AC_MSG_RESULT([no])
+-                AC_MSG_ERROR([ualpn requires MAP_ANON or mmap("/dev/zero", MAP_SHARED)])),
++                AC_MSG_ERROR([ualpn requires MAP_ANON or mmap("/dev/zero", MAP_SHARED)]))
++        ], [
+             AC_COMPILE_IFELSE([AC_LANG_SOURCE([#include <sys/mman.h>
+                              int main() {return mmap(0, 4096, PROT_READ|PROT_WRITE,
+                                 MAP_ANON|MAP_SHARED, -1, 0) == MAP_FAILED;}])],

package/uacme/uacme.mk
@@ -13,6 +13,9 @@ UACME_LICENSE = GPL-3.0+
 UACME_LICENSE_FILES = COPYING
 UACME_DEPENDENCIES = libcurl
 
+# 0001-Fix-cross-compilation.patch touches configure.ac
+UACME_AUTORECONF = YES
+
 UACME_CONF_ENV = ac_cv_prog_cc_c99='-std=gnu99' LIBS="$(UACME_LIBS)"
 
 ifeq ($(BR2_PACKAGE_GNUTLS),y)