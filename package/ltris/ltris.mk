buildroot/buildroot

commit 61848605cd26d29f54c16de862aedba7cf334be4

package/ltris: bump to version 1.2.7
Update to the latest release, remove fix that was merged upstream.

Signed-off-by: Julien BOIBESSOT <julien.boibessot@armadeus.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/ltris/ltris.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LTRIS_SITE = http://downloads.sourceforge.net/lgames/ltris
-LTRIS_VERSION = 1.2
+LTRIS_VERSION = 1.2.7
 LTRIS_LICENSE = GPL-2.0+
 LTRIS_LICENSE_FILES = COPYING
 

.checkpackageignore
@@ -813,7 +813,6 @@ package/lshw/0001-solve-Compile-error-when-g-version-is-less-than-5.patch Upstre
 package/ltrace/0001-arm-plt.patch Upstream
 package/ltrace/0002-sparc-add-missing-library.h-include.patch Upstream
 package/ltrace/0003-configure.ac-fix-autoreconf-with-autoconf-2.70.patch Upstream
-package/ltris/0001-fix-build-with-gcc-4.8.patch Upstream
 package/lttng-babeltrace/0001-tests-lib-Makefile.am-remove-unneeded-static-flag.patch Upstream
 package/lttng-babeltrace/0002-configure.ac-fix-popt-static-build.patch Upstream
 package/lttng-libust/0001-configure.ac-add-disable-tests.patch Upstream

package/ltris/0001-fix-build-with-gcc-4.8.patch
@@ -1,20 +1,0 @@
-Fix build with gcc 4.8
-
-Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
-[Upstream status: https://sourceforge.net/p/lgames/patches/28]
-Index: ltris/src/bowl.c
-===================================================================
---- ltris/src/bowl.c	(révision 421)
-+++ ltris/src/bowl.c	(copie de travail)
-@@ -828,9 +828,10 @@
- */
- void bowl_init_block_masks() 
- {
-+	int i;
- 	int masksize = sizeof(block_masks[0].mask); // same for all
- 
--	for (int i = 0; i < 7; i++) {
-+	for (i = 0; i < 7; i++) {
- 		block_masks[i].rx = 2;
- 		block_masks[i].ry = 2;
- 		memset(block_masks[i].mask, 0, masksize );

package/ltris/ltris.hash
@@ -1,3 +1,3 @@
 # Locally computed:
-sha256  a7bc6c5fc6539b88505077a1b4580edc9b179ce0a6b829c58fbd9416cf456195  ltris-1.2.tar.gz
+sha256  1291c6a642d06b9ee153ac0a2e7855a2c983e839c618f1a294df04d829525cb0  ltris-1.2.7.tar.gz
 sha256  67f82e045cf7acfef853ea0f426575a8359161a0a325e19f02b529a87c4b6c34  COPYING

commit 9af20d6e1e1139e59ed3dd9aaf015e72f1e2db8e

package/ltris: drop unrecognized option
--enable-sound has been dropped since version 1.2.4 and
https://sourceforge.net/p/lgames/code/432:

configure: WARNING: unrecognized options: --disable-gtk-doc, --disable-gtk-doc-html, --disable-doc, --disable-docs, --disable-documentation, --with-xmlto, --with-fop, --enable-ipv6, --enable-static, --disable-shared, --enable-sound

Fixes: 61848605cd26d29f54c16de862aedba7cf334be4

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

package/ltris/ltris.mk
@@ -18,12 +18,9 @@ LTRIS_CONF_ENV = \
 
 ifeq ($(BR2_PACKAGE_LTRIS_AUDIO),y)
 LTRIS_DEPENDENCIES += sdl_mixer host-pkgconf
-LTRIS_CONF_OPTS += --enable-sound
 # configure script does NOT use pkg-config to figure out how to link
 # with sdl_mixer, breaking static linking as sdl_mixer can use libmad
 LTRIS_LIBS += `$(PKG_CONFIG_HOST_BINARY) --libs SDL_mixer`
-else
-LTRIS_CONF_OPTS += --disable-sound
 endif
 
 $(eval $(autotools-package))