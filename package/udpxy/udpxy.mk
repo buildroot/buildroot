6c3a4107b3e636036a681749e2798b48714d5b3f
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-02-22 00:01:17 +0100

    package/udpxy: bump to version 1.0-25.1
    - Switch site to get latest release
    - Set ALL_CFLAGS to drop -Werror
    - Update hash of README (changes not related to license)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/udpxy/udpxy.mk
+++ package/udpxy/udpxy.mk
@@ -4,19 +4,19 @@
 #
 ################################################################################
 
-UDPXY_VERSION = 1.0.23-9-prod
-UDPXY_SOURCE = udpxy.$(UDPXY_VERSION).tar.gz
-UDPXY_SITE = http://www.udpxy.com/download/1_23
+UDPXY_VERSION = 1.0-25.1
+UDPXY_SITE = $(call github,pcherenkov,udpxy,$(UDPXY_VERSION))
 UDPXY_LICENSE = GPL-3.0+
 UDPXY_LICENSE_FILES = README
 
 define UDPXY_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) \
+		ALL_CFLAGS="$(TARGET_CFLAGS)" -C $(@D)/chipmunk
 endef
 
 define UDPXY_INSTALL_TARGET_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) DESTDIR=$(TARGET_DIR) PREFIX=/usr \
-		-C $(@D) install
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/chipmunk DESTDIR=$(TARGET_DIR) \
+		PREFIX=/usr -C $(@D)/chipmunk install
 endef
 
 $(eval $(generic-package))
