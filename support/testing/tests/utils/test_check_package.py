a9990552eb8b88647104bbec82555b2c407aa6be
Romain Naour <romain.naour@smile.fr>
2024-02-21 17:19:09 +0100

    support/testing: TestCheckPackage: update expected strings
    Commit dfed5acb56 ("utils/check-package: use https for the manual URL")
    replaced the default url to the Buildroot manual while it was used
    by TestCheckPackage test.
    
    Update TestCheckPackage with https url.
    
    Fixes:
    https://gitlab.com/buildroot.org/buildroot/-/jobs/6224243484
    
    Cc: Yann E. MORIN <yann.morin.1998@free.fr>
    Signed-off-by: Romain Naour <romain.naour@smile.fr>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- support/testing/tests/utils/test_check_package.py
+++ support/testing/tests/utils/test_check_package.py
@@ -200,13 +200,13 @@ class TestCheckPackage(unittest.TestCase):
                            self.WITH_UTILS_IN_PATH, abs_path)
         self.assert_file_was_processed(m)
         self.assert_warnings_generated_for_file(m)
-        self.assertIn("{}:1: should be 80 hashes (http://nightly.buildroot.org/#writing-rules-mk)".format(rel_file), w)
+        self.assertIn("{}:1: should be 80 hashes (https://nightly.buildroot.org/#writing-rules-mk)".format(rel_file), w)
 
         w, m = call_script(["check-package", "-b", abs_file],
                            self.WITH_UTILS_IN_PATH, infra.basepath())
         self.assert_file_was_processed(m)
         self.assert_warnings_generated_for_file(m)
-        self.assertIn("{}:1: should be 80 hashes (http://nightly.buildroot.org/#writing-rules-mk)".format(abs_file), w)
+        self.assertIn("{}:1: should be 80 hashes (https://nightly.buildroot.org/#writing-rules-mk)".format(abs_file), w)
 
         # br2-external with ignore list
         topdir_path = infra.filepath("tests/utils/br2-external")
