f382de2b0f8a528a09bb975cc088a4342b663134
Bernd Kuhls <bernd@kuhls.net>
2024-02-21 20:55:20 +0100

    package/libffi: bump version to 3.4.6
    Removed patch 0001 and instead added new configure option
    --disable-multi-os-directory which was added upstream:
    https://github.com/libffi/libffi/commit/877ea9bf9ac2c98cb858c12f5a6aeeec13cf978f
    
    Removed patch 0003 due to various upstream fixes for mips soft-float
    support since its addition in 2016:
    https://github.com/libffi/libffi/commits/master/src/mips
    
    Renumbered remaining patch.
    
    Updated license hash due to copyright year bump:
    https://github.com/libffi/libffi/commit/91739a1a912476adbf1e0e4dcb091b9a2c5007d9
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Reviewed-by: Adam Duskett <adam.duskett@amarulasolutions.com>
    Tested-by: Adam Duskett <adam.duskett@amarulasolutions.com>
    [yann.morin.1998@free.fr: fix check-package]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/libffi/libffi.mk
+++ package/libffi/libffi.mk
@@ -4,19 +4,20 @@
 #
 ################################################################################
 
-LIBFFI_VERSION = 3.4.4
+LIBFFI_VERSION = 3.4.6
 LIBFFI_SITE = \
 	https://github.com/libffi/libffi/releases/download/v$(LIBFFI_VERSION)
 LIBFFI_LICENSE = MIT
 LIBFFI_LICENSE_FILES = LICENSE
 LIBFFI_CPE_ID_VALID = YES
 LIBFFI_INSTALL_STAGING = YES
-# We're patching Makefile.am
+# We're patching configure.ac
 LIBFFI_AUTORECONF = YES
+LIBFFI_CONF_OPTS = --disable-multi-os-directory
 
 # The static exec trampolines is enabled by default since
 # libffi 3.4.2. However it doesn't work with gobject-introspection.
-LIBFFI_CONF_OPTS = --disable-exec-static-tramp
+LIBFFI_CONF_OPTS += --disable-exec-static-tramp
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
