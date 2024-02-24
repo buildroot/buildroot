buildroot/buildroot

commit 68df20336664d4d0b1a9d8d0285d23eba631845a

package/s390-tools: bump version to 2.29.0
Signed-off-by: Alexander Egorenkov <egorenar@linux.ibm.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/s390-tools/s390-tools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-S390_TOOLS_VERSION = 2.27.0
+S390_TOOLS_VERSION = 2.29.0
 S390_TOOLS_SITE = $(call github,ibm-s390-linux,s390-tools,v$(S390_TOOLS_VERSION))
 S390_TOOLS_LICENSE = MIT
 S390_TOOLS_LICENSE_FILES = LICENSE

package/s390-tools/s390-tools.hash
@@ -1,3 +1,3 @@
 # Locally computed:
-sha256  4315e64cf2fe0240ff9bd9929501db731d5bf3e7acf964ecf323d48d90e08f6e  s390-tools-2.27.0.tar.gz
+sha256  849ff400dc6c1eb7eebe4aa3e7a4871721c25bcee6cfdd0535a056a038fd3ab0  s390-tools-2.29.0.tar.gz
 sha256  cca17a9a944ebec769adee4aebd805c912c357785ff2705a99ffe68563021f75  LICENSE

commit 120cb19056dc22b11d921714dc4e2ac94b84242f

package/s390-tools: disable cargo
Set HAVE_CARGO=0 to avoid the following build failures raised since bump
to version 2.29.0 in commit 68df20336664d4d0b1a9d8d0285d23eba631845a and
https://github.com/ibm-s390-linux/s390-tools/commit/e6add997ebbcd967734391da8a8987c7926e168f:

error: could not compile `pvsecret` (bin "pvsecret") due to previous error

[...]

ERROR: architecture for "/usr/bin/pvsecret" is "AArch64", should be "IBM S/390"

Fixes: 68df20336664d4d0b1a9d8d0285d23eba631845a
 - http://autobuild.buildroot.org/results/22035cb4d8e251f9930a7d529b91b128919252bb
 - http://autobuild.buildroot.org/results/99be6d3b2bab7c952ce02853f54924c5219aecc1

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

package/s390-tools/s390-tools.mk
@@ -12,7 +12,8 @@ S390_TOOLS_DEPENDENCIES = zlib
 
 S390_TOOLS_MAKE_OPTS = \
 	ARCH=$(BR2_ARCH) \
-	CFLAGS="$(TARGET_CFLAGS) -D_GNU_SOURCE"
+	CFLAGS="$(TARGET_CFLAGS) -D_GNU_SOURCE" \
+	HAVE_CARGO=0
 
 ifeq ($(BR2_PACKAGE_LIBCURL_OPENSSL),y)
 S390_TOOLS_DEPENDENCIES += libcurl