buildroot/buildroot

commit 5cee6b6be6524e443012d130e09b0e6983b0ff90

package/libcurl: bump version to 8.6.0
Changelog: https://curl.se/changes.html#8_6_0

Removed patch which was included in upstream release.

Updated license hash due to copyright year bump:
https://github.com/curl/curl/commit/57cc70e559aa065426021e22e2af8166af6084f6

Added configure options for optional libpsl support due to upstream
commit which throws an error during configure when libpsl is missing and
--without-libpsl was not used:
https://github.com/curl/curl/commit/2998874bb61ac6ef3b72d6a61467cd2aaf6e53ea

Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/libcurl/libcurl.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBCURL_VERSION = 8.5.0
+LIBCURL_VERSION = 8.6.0
 LIBCURL_SOURCE = curl-$(LIBCURL_VERSION).tar.xz
 LIBCURL_SITE = https://curl.se/download
 LIBCURL_DEPENDENCIES = host-pkgconf \
@@ -110,6 +110,13 @@ LIBCURL_DEPENDENCIES += libidn2
 LIBCURL_CONF_OPTS += --with-libidn2
 else
 LIBCURL_CONF_OPTS += --without-libidn2
+endif
+
+ifeq ($(BR2_PACKAGE_LIBPSL),y)
+LIBCURL_DEPENDENCIES += libpsl
+LIBCURL_CONF_OPTS += --with-libpsl
+else
+LIBCURL_CONF_OPTS += --without-libpsl
 endif
 
 # Configure curl to support libssh2

package/libcurl/0001-gnutls-fix-build-with-disable-verbose.patch
@@ -1,60 +1,0 @@
-From 0d9f9c5ea8c15c70eb420242b60179d9083c70b7 Mon Sep 17 00:00:00 2001
-From: Baruch Siach <baruch@tkos.co.il>
-Date: Mon, 11 Dec 2023 20:45:01 +0200
-Subject: [PATCH] gnutls: fix build with --disable-verbose
-
-infof() parameters must be defined event with --disable-verbose since
-commit dac293cfb702 ("lib: apache style infof and trace
-macros/functions").
-
-Move also 'ptr' definition under !CURL_DISABLE_VERBOSE_STRINGS.
-
-Fixes the following build failure:
-
-In file included from ../lib/sendf.h:29,
-                 from vtls/gtls.c:44:
-vtls/gtls.c: In function 'Curl_gtls_verifyserver':
-vtls/gtls.c:841:34: error: 'version' undeclared (first use in this function); did you mean 'session'?
-  841 |         gnutls_protocol_get_name(version), ptr);
-      |                                  ^~~~~~~
-
-Signed-off-by: Baruch Siach <baruch@tkos.co.il>
-Upstream: https://github.com/curl/curl/commit/af520ac9fec7d88e942f05fdcd90704adb9fa566
----
- lib/vtls/gtls.c | 4 +++-
- 1 file changed, 3 insertions(+), 1 deletion(-)
-
-diff --git a/lib/vtls/gtls.c b/lib/vtls/gtls.c
-index f42b6d6691de..b95c5be3c5e5 100644
---- a/lib/vtls/gtls.c
-+++ b/lib/vtls/gtls.c
-@@ -818,16 +818,17 @@ Curl_gtls_verifyserver(struct Curl_easy *data,
-   char certname[65] = ""; /* limited to 64 chars by ASN.1 */
-   size_t size;
-   time_t certclock;
--  const char *ptr;
-   int rc;
-   CURLcode result = CURLE_OK;
- #ifndef CURL_DISABLE_VERBOSE_STRINGS
-+  const char *ptr;
-   unsigned int algo;
-   unsigned int bits;
-   gnutls_protocol_t version = gnutls_protocol_get_version(session);
- #endif
-   long * const certverifyresult = &ssl_config->certverifyresult;
- 
-+#ifndef CURL_DISABLE_VERBOSE_STRINGS
-   /* the name of the cipher suite used, e.g. ECDHE_RSA_AES_256_GCM_SHA384. */
-   ptr = gnutls_cipher_suite_get_name(gnutls_kx_get(session),
-                                      gnutls_cipher_get(session),
-@@ -835,6 +836,7 @@ Curl_gtls_verifyserver(struct Curl_easy *data,
- 
-   infof(data, "SSL connection using %s / %s",
-         gnutls_protocol_get_name(version), ptr);
-+#endif
- 
-   /* This function will return the peer's raw certificate (chain) as sent by
-      the peer. These certificates are in raw format (DER encoded for
--- 
-2.43.0
-

package/libcurl/libcurl.hash
@@ -1,5 +1,5 @@
 # Locally calculated after checking pgp signature
-# https://curl.se/download/curl-8.5.0.tar.xz.asc
+# https://curl.se/download/curl-8.6.0.tar.xz.asc
 # signed with key 27EDEAF22F3ABCEB50DB9A125CC908FDB71E12C2
-sha256  42ab8db9e20d8290a3b633e7fbb3cec15db34df65fd1015ef8ac1e4723750eeb  curl-8.5.0.tar.xz
-sha256  b1d7feb949ea5023552029fbe0bf5db4f23c2f85e9b8e51e18536f0ecbf9c524  COPYING
+sha256  3ccd55d91af9516539df80625f818c734dc6f2ecf9bada33c76765e99121db15  curl-8.6.0.tar.xz
+sha256  adb1fc06547fd136244179809f7b7c2d2ae6c4534f160aa513af9b6a12866a32  COPYING

commit bedcd1cd39c7654d1243e2cb0f4a68870f67782c

package/libcurl: fix libpsl static build
Fix the following static build failure with libpsl raised since bump to
version 8.6.0 in commit 5cee6b6be6524e443012d130e09b0e6983b0ff90:

configure:28830: /home/autobuild/autobuild/instance-6/output-1/host/bin/aarch64_be-buildroot-linux-musl-gcc -o conftest -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -O3 -g0 -static -Werror-implicit-function-declaration -Wno-system-headers -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64  -I/home/autobuild/autobuild/instance-6/output-1/host/aarch64_be-buildroot-linux-musl/sysroot/usr/include  -static -L/home/autobuild/autobuild/instance-6/output-1/host/bin/../aarch64_be-buildroot-linux-musl/sysroot/usr/lib  -L/home/autobuild/autobuild/instance-6/output-1/host/aarch64_be-buildroot-linux-musl/sysroot/usr/lib conftest.c -lpsl  -lmbedtls -lmbedx509 -lmbedcrypto -lz  -latomic >&5
/home/autobuild/autobuild/instance-6/output-1/host/lib/gcc/aarch64_be-buildroot-linux-musl/11.4.0/../../../../aarch64_be-buildroot-linux-musl/bin/ld: /home/autobuild/autobuild/instance-6/output-1/host/bin/../aarch64_be-buildroot-linux-musl/sysroot/usr/lib/libpsl.a(psl.c.o): in function `is_public_suffix':
psl.c:(.text+0x2a8): undefined reference to `idn2_lookup_u8'

[...]

checking for library containing psl_builtin... no
configure: error: libpsl was not found

Fixes: 5cee6b6be6524e443012d130e09b0e6983b0ff90
 - http://autobuild.buildroot.org/results/1fb15e1a99472c403d0d3b1a688902f32e78d002

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

package/libcurl/0001-configure.ac-find-libpsl-with-pkg-config.patch
@@ -1,0 +1,109 @@
+From 9b3f67e267d1fa8d7867655d133bdbf8830a0ab3 Mon Sep 17 00:00:00 2001
+From: Fabrice Fontaine <fontaine.fabrice@gmail.com>
+Date: Thu, 15 Feb 2024 20:59:25 +0100
+Subject: [PATCH] configure.ac: find libpsl with pkg-config
+
+Find libpsl with pkg-config to avoid static build failures.
+
+Ref: http://autobuild.buildroot.org/results/1fb15e1a99472c403d0d3b1a688902f32e78d002
+
+Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
+Closes #12947
+
+Upstream: https://github.com/curl/curl/commit/9b3f67e267d1fa8d7867655d133bdbf8830a0ab3
+---
+ configure.ac | 79 ++++++++++++++++++++++++++++++++++++++++++++--------
+ docs/TODO    |  7 -----
+ 2 files changed, 67 insertions(+), 19 deletions(-)
+
+diff --git a/configure.ac b/configure.ac
+index cd0e2d07d8d164..09d5364f4de575 100644
+--- a/configure.ac
++++ b/configure.ac
+@@ -2075,19 +2075,74 @@ dnl **********************************************************************
+ dnl Check for libpsl
+ dnl **********************************************************************
+ 
+-AC_ARG_WITH(libpsl,
+-           AS_HELP_STRING([--without-libpsl],
+-           [disable support for libpsl]),
+-           with_libpsl=$withval,
+-           with_libpsl=yes)
+-curl_psl_msg="no      (libpsl disabled)"
+-if test $with_libpsl != "no"; then
+-  AC_SEARCH_LIBS(psl_builtin, psl,
+-    [curl_psl_msg="enabled";
+-     AC_DEFINE([USE_LIBPSL], [1], [PSL support enabled])
+-     ],
+-    [AC_MSG_ERROR([libpsl was not found]) ]
++dnl Default to compiler & linker defaults for LIBPSL files & libraries.
++OPT_LIBPSL=off
++AC_ARG_WITH(libpsl,dnl
++AS_HELP_STRING([--with-libpsl=PATH],[Where to look for libpsl, PATH points to the LIBPSL installation; when possible, set the PKG_CONFIG_PATH environment variable instead of using this option])
++AS_HELP_STRING([--without-libpsl], [disable LIBPSL]),
++  OPT_LIBPSL=$withval)
++
++if test X"$OPT_LIBPSL" != Xno; then
++  dnl backup the pre-libpsl variables
++  CLEANLDFLAGS="$LDFLAGS"
++  CLEANCPPFLAGS="$CPPFLAGS"
++  CLEANLIBS="$LIBS"
++
++  case "$OPT_LIBPSL" in
++  yes)
++    dnl --with-libpsl (without path) used
++    CURL_CHECK_PKGCONFIG(libpsl)
++
++    if test "$PKGCONFIG" != "no" ; then
++      LIB_PSL=`$PKGCONFIG --libs-only-l libpsl`
++      LD_PSL=`$PKGCONFIG --libs-only-L libpsl`
++      CPP_PSL=`$PKGCONFIG --cflags-only-I libpsl`
++    else
++      dnl no libpsl pkg-config found
++      LIB_PSL="-lpsl"
++    fi
++
++    ;;
++  off)
++    dnl no --with-libpsl option given, just check default places
++    LIB_PSL="-lpsl"
++    ;;
++  *)
++    dnl use the given --with-libpsl spot
++    LIB_PSL="-lpsl"
++    PREFIX_PSL=$OPT_LIBPSL
++    ;;
++  esac
++
++  dnl if given with a prefix, we set -L and -I based on that
++  if test -n "$PREFIX_PSL"; then
++    LD_PSL=-L${PREFIX_PSL}/lib$libsuff
++    CPP_PSL=-I${PREFIX_PSL}/include
++  fi
++
++  LDFLAGS="$LDFLAGS $LD_PSL"
++  CPPFLAGS="$CPPFLAGS $CPP_PSL"
++  LIBS="$LIB_PSL $LIBS"
++
++  AC_CHECK_LIB(psl, psl_builtin,
++    [
++     AC_CHECK_HEADERS(libpsl.h,
++        curl_psl_msg="enabled"
++        LIBPSL_ENABLED=1
++        AC_DEFINE(USE_LIBPSL, 1, [if libpsl is in use])
++        AC_SUBST(USE_LIBPSL, [1])
++     )
++    ],
++      dnl not found, revert back to clean variables
++      LDFLAGS=$CLEANLDFLAGS
++      CPPFLAGS=$CLEANCPPFLAGS
++      LIBS=$CLEANLIBS
+   )
++
++  if test X"$OPT_LIBPSL" != Xoff &&
++     test "$LIBPSL_ENABLED" != "1"; then
++    AC_MSG_ERROR([libpsl libs and/or directories were not found where specified!])
++  fi
+ fi
+ AM_CONDITIONAL([USE_LIBPSL], [test "$curl_psl_msg" = "enabled"])
+ 

package/libcurl/libcurl.mk
@@ -14,6 +14,8 @@ LIBCURL_LICENSE = curl
 LIBCURL_LICENSE_FILES = COPYING
 LIBCURL_CPE_ID_VENDOR = haxx
 LIBCURL_INSTALL_STAGING = YES
+# 0001-configure.ac-find-libpsl-with-pkg-config.patch
+LIBCURL_AUTORECONF = YES
 
 # We disable NTLM delegation to winbinds ntlm_auth ('--disable-ntlm-wb')
 # support because it uses fork(), which doesn't work on non-MMU platforms.