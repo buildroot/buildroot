b1b4923d18bc6e8a40e990e172cd9f12087fcdb6
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-03-01 21:15:43 +0100

    package/python-aiohttp: security bump to version 3.9.3
    Fix CVE-2024-23334 and CVE-2024-23829
    
    https://github.com/aio-libs/aiohttp/security/advisories/GHSA-5h86-8mv2-jq9f
    https://github.com/aio-libs/aiohttp/security/advisories/GHSA-8qpw-xqxj-h4r2
    https://github.com/aio-libs/aiohttp/blob/v3.9.3/CHANGES.rst
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-aiohttp/python-aiohttp.mk
+++ package/python-aiohttp/python-aiohttp.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_AIOHTTP_VERSION = 3.9.1
+PYTHON_AIOHTTP_VERSION = 3.9.3
 PYTHON_AIOHTTP_SOURCE = aiohttp-$(PYTHON_AIOHTTP_VERSION).tar.gz
-PYTHON_AIOHTTP_SITE = https://files.pythonhosted.org/packages/54/07/9467d3f8dae29b14f423b414d9e67512a76743c5bb7686fb05fe10c9cc3e
+PYTHON_AIOHTTP_SITE = https://files.pythonhosted.org/packages/18/93/1f005bbe044471a0444a82cdd7356f5120b9cf94fe2c50c0cdbf28f1258b
 PYTHON_AIOHTTP_SETUP_TYPE = setuptools
 PYTHON_AIOHTTP_LICENSE = Apache-2.0
 PYTHON_AIOHTTP_LICENSE_FILES = LICENSE.txt
