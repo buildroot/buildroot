buildroot/buildroot

commit d49f53f8aaf0bff9c8d04c16485c844fef2832d0

package/redis: bump to v7.2.1
Signed-off-by: Titouan Christophe <titouanchristophe@gmail.com>
Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

package/redis/redis.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-REDIS_VERSION = 7.0.11
+REDIS_VERSION = 7.2.1
 REDIS_SITE = http://download.redis.io/releases
 REDIS_LICENSE = BSD-3-Clause (core); MIT and BSD family licenses (Bundled components)
 REDIS_LICENSE_FILES = COPYING

package/redis/0001-uclibc.patch
@@ -12,17 +12,19 @@ Signed-off-by: Martin Bark <martin@barkynet.com>
 Signed-off-by: Titouan Christophe <titouan.christophe@railnova.eu>
 [Fabrice: update for 6.0.9]
 Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
+[Titouan: adapt to 7.2.1]
+Signed-off-by: Titouan Christophe <titouanchristophe@gmail.com>
 
 =========================================================================
 diff -ur old/src/config.h new/src/config.h
 --- old/src/config.h	2012-10-26 07:20:24.000000000 -0700
 +++ new/src/config.h	2012-10-31 13:41:51.206309564 -0700
-@@ -62,7 +62,7 @@
+@@ -71,7 +71,7 @@
  #endif
  
  /* Test for backtrace() */
 -#if defined(__APPLE__) || (defined(__linux__) && defined(__GLIBC__)) || \
 +#if defined(__APPLE__) || (defined(__linux__) && defined(__GLIBC__) && !defined(__UCLIBC__)) || \
-     defined(__FreeBSD__) || ((defined(__OpenBSD__) || defined(__NetBSD__)) && defined(USE_BACKTRACE))\
-  || defined(__DragonFly__)
+     defined(__FreeBSD__) || ((defined(__OpenBSD__) || defined(__NetBSD__) || defined(__sun)) && defined(USE_BACKTRACE))\
+  || defined(__DragonFly__) || (defined(__UCLIBC__) && defined(__UCLIBC_HAS_BACKTRACE__))
  #define HAVE_BACKTRACE 1

package/redis/redis.hash
@@ -1,5 +1,5 @@
 # From https://github.com/redis/redis-hashes/blob/master/README
-sha256  ce250d1fba042c613de38a15d40889b78f7cb6d5461a27e35017ba39b07221e3  redis-7.0.11.tar.gz
+sha256  5c76d990a1b1c5f949bcd1eed90d0c8a4f70369bdbdcb40288c561ddf88967a4  redis-7.2.1.tar.gz
 
 # Locally calculated
 sha256  97f0a15b7bbae580d2609dad2e11f1956ae167be296ab60f4691ab9c30ee9828  COPYING

commit dee6974d729da81f80d310df262436a6136164a0

package/redis: disable -Werror
Disable -Werror to avoid the following build failure raised since bump
to version 7.2.1 in commit d49f53f8aaf0bff9c8d04c16485c844fef2832d0 and
https://github.com/redis/redis/commit/8e138ba44fc3eb676c0f31faac19d1acc6a4c7ed:

In function 'hi_sdsll2str',
    inlined from 'hi_sdsfromlonglong' at sds.c:500:15:
sds.c:457:12: error: writing 1 byte into a region of size 0 [-Werror=stringop-overflow=]
  457 |         *s = *p;
      |         ~~~^~~~

Fixes: d49f53f8aaf0bff9c8d04c16485c844fef2832d0
 - http://autobuild.buildroot.org/results/fdc9f940b348430cb4da8efecc5cc047b21df281
 - http://autobuild.buildroot.org/results/e5747674d52b065203a0442f7cab13e8be5f426b

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

package/redis/redis.mk
@@ -30,7 +30,7 @@ endif
 # https://github.com/antirez/redis/pull/609).  We set PREFIX
 # instead.
 REDIS_BUILDOPTS = $(TARGET_CONFIGURE_OPTS) \
-	PREFIX=$(TARGET_DIR)/usr MALLOC=libc
+	PREFIX=$(TARGET_DIR)/usr MALLOC=libc WARNINGS=
 
 ifeq ($(BR2_PACKAGE_SYSTEMD),y)
 REDIS_DEPENDENCIES += systemd