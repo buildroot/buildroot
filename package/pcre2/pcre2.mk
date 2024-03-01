fa9e575776ee3d3d47a31fee3c09a94753c77cc7
Francois Perrad <fperrad@gmail.com>
2024-03-01 23:03:36 +0100

    package/pcre2: bump to version 10.43
    diff LICENCE: update copyright dates
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/pcre2/pcre2.mk
+++ package/pcre2/pcre2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PCRE2_VERSION = 10.42
+PCRE2_VERSION = 10.43
 PCRE2_SITE = https://github.com/PCRE2Project/pcre2/releases/download/pcre2-$(PCRE2_VERSION)
 PCRE2_SOURCE = pcre2-$(PCRE2_VERSION).tar.bz2
 PCRE2_LICENSE = BSD-3-Clause
