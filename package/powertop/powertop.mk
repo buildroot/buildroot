buildroot/buildroot

commit d65b960859046be486ced6c490d3671bd1392661

package/powertop: bump to version 2.15
- Switch site to get latest version
- Replace patch by an upstreamable one

https://github.com/fenrus75/powertop/compare/v2.13...v2.15

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

package/powertop/powertop.mk
@@ -4,12 +4,13 @@
 #
 ################################################################################
 
-POWERTOP_VERSION = 2.13
-POWERTOP_SITE = https://01.org/sites/default/files/downloads
+POWERTOP_VERSION = 2.15
+POWERTOP_SITE = $(call github,fenrus75,powertop,v$(POWERTOP_VERSION))
 POWERTOP_LICENSE = GPL-2.0
 POWERTOP_LICENSE_FILES = COPYING
 
 POWERTOP_DEPENDENCIES = \
+	host-autoconf-archive \
 	host-pkgconf \
 	libnl \
 	ncurses \
@@ -17,8 +18,16 @@ POWERTOP_DEPENDENCIES = \
 	$(TARGET_NLS_DEPENDENCIES)
 
 POWERTOP_CONF_ENV = LIBS=$(TARGET_NLS_LIBS)
-# 0001-dont-force-stack-smashing-protection.patch
+# 0001-add-disable-stack-protector-option.patch
 POWERTOP_AUTORECONF = YES
+POWERTOP_AUTORECONF_OPTS = --include=$(HOST_DIR)/share/autoconf-archive
+POWERTOP_CONF_OPTS = --disable-stack-protector
+
+# fix missing config.rpath (needed for autoreconf) in the codebase
+define POWERTOP_TOUCH_CONFIG_RPATH
+	touch $(@D)/config.rpath
+endef
+POWERTOP_PRE_CONFIGURE_HOOKS += POWERTOP_TOUCH_CONFIG_RPATH
 
 # Help powertop at finding the right ncurses library depending on
 # which one is available.

.checkpackageignore
@@ -1108,7 +1108,6 @@ package/poke/0002-lib-getrandom.c-fix-build-with-uclibc-1.0.35.patch Upstream
 package/policycoreutils/0001-Add-DESTDIR-to-all-paths-that-use-an-absolute-path.patch Upstream
 package/policycoreutils/0002-Add-PREFIX-to-host-paths.patch Upstream
 package/postgresql/S50postgresql Variables
-package/powertop/0001-dont-force-stack-smashing-protection.patch Upstream
 package/pptp-linux/0001-susv3-legacy.patch Upstream
 package/pptp-linux/0002-fix-parallel-build.patch Upstream
 package/prboom/0001-libpng-1.4.patch Upstream

package/powertop/0001-add-disable-stack-protector-option.patch
@@ -1,0 +1,55 @@
+From 95382246ddd889839633aa0da800a03936b93986 Mon Sep 17 00:00:00 2001
+From: Fabrice Fontaine <fontaine.fabrice@gmail.com>
+Date: Sun, 31 Oct 2021 18:26:01 +0100
+Subject: [PATCH] add --disable-stack-protector option
+
+Allow the user to disable stack-protector as not all toolchains support
+this feature
+
+Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
+Upstream: https://github.com/fenrus75/powertop/pull/138
+---
+ configure.ac    | 4 ++++
+ src/Makefile.am | 5 ++++-
+ 2 files changed, 8 insertions(+), 1 deletion(-)
+
+diff --git a/configure.ac b/configure.ac
+index 37c1304..69160d5 100644
+--- a/configure.ac
++++ b/configure.ac
+@@ -43,6 +43,10 @@ AX_ADD_FORTIFY_SOURCE
+ AX_CXX_COMPILE_STDCXX([11], [noext], [mandatory])
+ PKG_PROG_PKG_CONFIG
+ 
++AC_ARG_ENABLE([stack-protector],
++	AS_HELP_STRING([--disable-stack-protector], [Disable stack-protector]))
++AM_CONDITIONAL([ENABLE_STACK_PROTECTOR], [test x$enable_stack_protector != xno])
++
+ # Checks for libraries.
+ AX_PTHREAD([
+     LIBS="$PTHREAD_LIBS $LIBS"
+diff --git a/src/Makefile.am b/src/Makefile.am
+index 6b523f6..ca30d20 100644
+--- a/src/Makefile.am
++++ b/src/Makefile.am
+@@ -138,13 +138,16 @@ powertop_CXXFLAGS = \
+ 	-Wformat \
+ 	-Wshadow \
+ 	-fno-omit-frame-pointer \
+-	-fstack-protector \
+ 	$(GLIB2_CFLAGS) \
+ 	$(LIBNL_CFLAGS) \
+ 	$(NCURSES_CFLAGS) \
+ 	$(PCIUTILS_CFLAGS) \
+ 	$(PTHREAD_CFLAGS)
+ 
++if ENABLE_STACK_PROTECTOR
++powertop_CXXFLAGS += \
++	-fstack-protector
++endif
+ 
+ powertop_CPPFLAGS = \
+ 	-DLOCALEDIR=\"$(localedir)\" \
+-- 
+2.33.0
+

package/powertop/0001-dont-force-stack-smashing-protection.patch
@@ -1,18 +1,0 @@
-Do not pass -fstack-protector unconditionally
-
-Using -fstack-protector only works when the toolchain has SSP support.
-
-Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
-
-Index: b/src/Makefile.am
-===================================================================
---- a/src/Makefile.am
-+++ b/src/Makefile.am
-@@ -127,7 +127,6 @@
- 	-Wformat \
- 	-Wshadow \
- 	-fno-omit-frame-pointer \
--	-fstack-protector \
- 	$(GLIB2_CFLAGS) \
- 	$(LIBNL_CFLAGS) \
- 	$(NCURSES_CFLAGS) \

package/powertop/Config.in
@@ -9,7 +9,7 @@ config BR2_PACKAGE_POWERTOP
 	  A tool to diagnose issues with power consumption and power
 	  management
 
-	  https://01.org/powertop/
+	  https://github.com/fenrus75/powertop/
 
 comment "powertop needs a toolchain w/ C++, threads, wchar"
 	depends on !BR2_INSTALL_LIBSTDCPP || !BR2_TOOLCHAIN_HAS_THREADS || \

package/powertop/powertop.hash
@@ -1,5 +1,3 @@
-# Locally calculated after checking pgp signature
-# https://01.org/sites/default/files/downloads/powertop-2.13.tar.gz.asc.txt
-# using key 22E8F306C8FA4BAA2A5F36F3A0303B060918941C
-sha256  a65f992ca4a419bc73b623651060eb9fc00c5a86fa03556358cd9db011ef3178  powertop-2.13.tar.gz
+# Locally calculated
+sha256  e58ab3fd7b8ff5f4dd0d17f11848817e7d83c0a6918145ac81de03b5dccf8f49  powertop-2.15.tar.gz
 sha256  b499eddebda05a8859e32b820a64577d91f1de2b52efa2a1575a2cb4000bc259  COPYING

commit 6849d17cf4d65f3c8dcc986a7a68ce51619f8508

package/powertop: fix gettext build
Fix gettext build raised since bump to version 2.15 in commit
d65b960859046be486ced6c490d3671bd1392661:

*** error: gettext infrastructure mismatch: using a Makefile.in.in from gettext version 0.18 but the autoconf macros are from gettext version 0.20

Fixes: d65b960859046be486ced6c490d3671bd1392661
 - http://autobuild.buildroot.org/results/bb80a7842d18651e5069f3a91f06a9c23218d260

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

package/powertop/powertop.mk
@@ -20,6 +20,7 @@ POWERTOP_DEPENDENCIES = \
 POWERTOP_CONF_ENV = LIBS=$(TARGET_NLS_LIBS)
 # 0001-add-disable-stack-protector-option.patch
 POWERTOP_AUTORECONF = YES
+POWERTOP_AUTOPOINT = YES
 POWERTOP_AUTORECONF_OPTS = --include=$(HOST_DIR)/share/autoconf-archive
 POWERTOP_CONF_OPTS = --disable-stack-protector
 