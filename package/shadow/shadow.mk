buildroot/buildroot

commit 8a01774d9897310c00a0945865f5a6d5167dfca3

package/shadow: bump to version 4.14.3
Also, the new version has an optional dependency on libbsd.

Signed-off-by: Raphael Pavlidis <raphael.pavlidis@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/shadow/shadow.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SHADOW_VERSION = 4.13
+SHADOW_VERSION = 4.14.3
 SHADOW_SITE = https://github.com/shadow-maint/shadow/releases/download/$(SHADOW_VERSION)
 SHADOW_SOURCE = shadow-$(SHADOW_VERSION).tar.xz
 SHADOW_LICENSE = BSD-3-Clause
@@ -118,6 +118,13 @@ ifeq ($(BR2_PACKAGE_SHADOW_YESCRYPT),y)
 SHADOW_CONF_OPTS += --with-yescrypt
 else
 SHADOW_CONF_OPTS += --without-yescrypt
+endif
+
+ifeq ($(BR2_PACKAGE_LIBBSD),y)
+SHADOW_CONF_OPTS += --with-libbsd
+SHADOW_DEPENDENCIES += libbsd
+else
+SHADOW_CONF_OPTS += --without-libbsd
 endif
 
 define SHADOW_PERMISSIONS

package/shadow/0001-Fix-yescrypt-support.patch
@@ -1,38 +1,0 @@
-From 7a63017b3c873a59e3482322b7824160a8c8ca67 Mon Sep 17 00:00:00 2001
-11;rgb:f6f6/f5f5/f4f4From: Bernd Kuhls <bernd@kuhls.net>
-Date: Sun, 9 Jul 2023 10:55:03 +0200
-Subject: [PATCH] Fix yescrypt support
-
-Fixes build error:
-newusers.c: In function 'update_passwd':
-newusers.c:433:21: error: 'sflg' undeclared (first use in this function); did you mean 'rflg'?
-
-introduced by
-https://github.com/shadow-maint/shadow/commit/5cd04d03f94622c12220d4a6352824af081b8531
-which forgot to define sflg for these configure options:
-
---without-sha-crypt --without-bcrypt --with-yescrypt
-
-Upstream: https://github.com/shadow-maint/shadow/commit/29da702491eea314b915ea9c7a83c9af80cf5797
-
-Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
----
- src/newusers.c | 2 +-
- 1 file changed, 1 insertion(+), 1 deletion(-)
-
-diff --git a/src/newusers.c b/src/newusers.c
-index 7cb8434b..08f79798 100644
---- a/src/newusers.c
-+++ b/src/newusers.c
-@@ -60,7 +60,7 @@ static bool rflg = false;	/* create a system account */
- #ifndef USE_PAM
- static /*@null@*//*@observer@*/char *crypt_method = NULL;
- #define cflg (NULL != crypt_method)
--#if defined(USE_SHA_CRYPT) || defined(USE_BCRYPT)
-+#if defined(USE_SHA_CRYPT) || defined(USE_BCRYPT) || defined(USE_YESCRYPT)
- static bool sflg = false;
- #endif
- #ifdef USE_SHA_CRYPT
--- 
-2.39.2
-

package/shadow/shadow.hash
@@ -1,3 +1,4 @@
-# Locally computed
-sha256  9afe245d79a2e7caac5f1ed62519b17416b057ec89df316df1c3935502f9dd2c  shadow-4.13.tar.xz
+# Verified from https://github.com/shadow-maint/shadow/releases/download/4.14.3/shadow-4.14.3.tar.xz.asc
+# with key A9348594CE31283A826FBDD8D57633D441E25BB5
+sha256  6969279236fe3152768573a38c9f83cb9ca109851a5a990aec1fc672ac2cfcd2  shadow-4.14.3.tar.xz
 sha256  3d25ab8f43fdc14624296a56ff8dc3e72e499ad35f32ae0c803f4959cfe17c0a  COPYING

commit 7781cdca3163035f3f89df90605cde7c5b7cd3c5

package/shadow: drop BR2_PACKAGE_SHADOW_UTMPX
utmpx is not available since bump to version 4.14.3 in commit
8a01774d9897310c00a0945865f5a6d5167dfca3 and
https://github.com/shadow-maint/shadow/commit/3be7b9d75a6b73fe24394d7ac1e315d60d45b5ee:

configure: WARNING: unrecognized options: --disable-gtk-doc, --disable-gtk-doc-html, --disable-doc, --disable-docs, --disable-documentation, --with-xmlto, --with-fop, --enable-ipv6, --enable-utmpx

Fixes: 8a01774d9897310c00a0945865f5a6d5167dfca3

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

Config.in.legacy
@@ -146,6 +146,12 @@ endif
 
 comment "Legacy options removed in 2024.02"
 
+config BR2_PACKAGE_SHADOW_UTMPX
+	bool "shadow utmpx removed"
+	select BR2_LEGACY
+	help
+	  UTMPX has been dropped by upstream.
+
 config BR2_PACKAGE_TINYMEMBENCH
 	bool "tinymembench removed"
 	select BR2_LEGACY

package/shadow/Config.in
@@ -31,11 +31,6 @@ comment "account-tools-setuid needs a toolchain w/ dynamic library, wchar, local
 	depends on BR2_STATIC_LIBS || !BR2_USE_WCHAR || \
 		!BR2_ENABLE_LOCALE || !BR2_TOOLCHAIN_GCC_AT_LEAST_4_9
 
-config BR2_PACKAGE_SHADOW_UTMPX
-	bool "utmpx"
-	help
-	  Enable loggin in utmpx / wtmpx.
-
 config BR2_PACKAGE_SHADOW_SUBORDINATE_IDS
 	bool "subordinate-ids"
 	help

package/shadow/shadow.mk
@@ -43,12 +43,6 @@ else
 SHADOW_CONF_OPTS += --disable-account-tools-setuid
 endif
 
-ifeq ($(BR2_PACKAGE_SHADOW_UTMPX),y)
-SHADOW_CONF_OPTS += --enable-utmpx
-else
-SHADOW_CONF_OPTS += --disable-utmpx
-endif
-
 ifeq ($(BR2_PACKAGE_SHADOW_SUBORDINATE_IDS),y)
 SHADOW_CONF_OPTS += --enable-subordinate-ids
 define SHADOW_SUBORDINATE_IDS_PERMISSIONS