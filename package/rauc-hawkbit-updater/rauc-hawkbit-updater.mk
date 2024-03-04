buildroot/buildroot

commit 928b7219cd7079864eadadc66dbff21c5cd72804

package/rauc-hawkbit-updater: new package
Add useful tool for bridging RAUC with the Hawkbit API.

Signed-off-by: Colin Foster <colin.foster@in-advantage.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/rauc-hawkbit-updater/rauc-hawkbit-updater.mk
@@ -1,0 +1,15 @@
+################################################################################
+#
+# rauc-hawkbit-updater
+#
+################################################################################
+
+RAUC_HAWKBIT_UPDATER_VERSION = 1.3
+RAUC_HAWKBIT_UPDATER_SITE = https://github.com/rauc/rauc-hawkbit-updater/releases/download/v$(RAUC_HAWKBIT_UPDATER_VERSION)
+RAUC_HAWKBIT_UPDATER_SOURCE = rauc-hawkbit-updater-$(RAUC_HAWKBIT_UPDATER_VERSION).tar.xz
+RAUC_HAWKBIT_UPDATER_LICENSE = LGPL-2.1
+RAUC_HAWKBIT_UPDATER_LICENSE_FILES = LICENSE
+RAUC_HAWKBIT_UPDATER_CPE_ID_VENDOR = pengutronix
+RAUC_HAWKBIT_UPDATER_DEPENDENCIES = json-glib libcurl
+
+$(eval $(meson-package))

DEVELOPERS
@@ -638,6 +638,9 @@ N:	Clément Péron <peron.clem@gmail.com>
 F:	board/beelink/gs1/
 F:	configs/beelink_gs1_defconfig
 
+N:	Colin Foster <colin.foster@in-advantage.com>
+F:	package/rauc-hawkbit-updater/
+
 N:	Corentin Guillevic <corentin.guillevic@smile.fr>
 F:	package/libloki/
 

package/Config.in
@@ -2715,6 +2715,7 @@ menu "System tools"
 	source "package/quota/Config.in"
 	source "package/quotatool/Config.in"
 	source "package/rauc/Config.in"
+	source "package/rauc-hawkbit-updater/Config.in"
 	source "package/rsyslog/Config.in"
 	source "package/runc/Config.in"
 	source "package/s390-tools/Config.in"

package/rauc-hawkbit-updater/Config.in
@@ -1,0 +1,20 @@
+config BR2_PACKAGE_RAUC_HAWKBIT_UPDATER
+	bool "rauc-hawkbit-updater"
+	depends on BR2_TOOLCHAIN_HAS_THREADS # rauc, json-glib
+	depends on BR2_USE_MMU # rauc, json-glib
+	depends on BR2_USE_WCHAR # rauc, json-glib
+	select BR2_PACKAGE_JSON_GLIB
+	select BR2_PACKAGE_LIBCURL
+	# runtime
+	select BR2_PACKAGE_RAUC
+	help
+	  rauc-hawkbit-updater is a link between Pengutronix RAUC and
+	  the Eclipse Hawkbit API. Hawkbit can communicate to remote
+	  update servers, and use the d-bus interface to trigger
+	  updates from RAUC.
+
+	  http://rauc.io/
+
+comment "rauc-hawkbit-updater needs a toolchain w/ wchar, threads"
+	depends on BR2_USE_MMU
+	depends on !BR2_USE_WCHAR || !BR2_TOOLCHAIN_HAS_THREADS

package/rauc-hawkbit-updater/rauc-hawkbit-updater.hash
@@ -1,0 +1,3 @@
+# Locally calculated
+sha256  42318e96a464e6c716edb45c48bd3ec3b874462973db902fbe099a395e4acb4b  rauc-hawkbit-updater-1.3.tar.xz
+sha256  041234c81fd6fe6c531ea1a886b9e740b9ee06759d1a910a14edbd06f4aa62c7  LICENSE

commit b146a0b944dedf45153348761ac313ca9062a7c0

package/rauc-hawkbit-updater: fix build with gcc 4.8
Fix the following build failure with gcc 4.8 raised since the addition
of the package in commit 928b7219cd7079864eadadc66dbff21c5cd72804:

../src/hawkbit-client.c:873:17: error: 'for' loop initial declarations are only allowed in C99 mode
                 for (const gint *code = &resumable_codes[0]; *code; code++)
                 ^
../src/hawkbit-client.c:873:17: note: use option -std=c99 or -std=gnu99 to compile your code

Fixes: 928b7219cd7079864eadadc66dbff21c5cd72804
 - http://autobuild.buildroot.org/results/e275d0ec4fe1da418a6163b46666316034b83b19

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

package/rauc-hawkbit-updater/rauc-hawkbit-updater.mk
@@ -10,5 +10,6 @@ RAUC_HAWKBIT_UPDATER_SOURCE = rauc-hawkbit-updater-$(RAUC_HAWKBIT_UPDATER_VERSIO
 RAUC_HAWKBIT_UPDATER_LICENSE = LGPL-2.1
 RAUC_HAWKBIT_UPDATER_LICENSE_FILES = LICENSE
 RAUC_HAWKBIT_UPDATER_DEPENDENCIES = json-glib libcurl
+RAUC_HAWKBIT_UPDATER_CFLAGS = $(TARGET_CFLAGS) -std=c99
 
 $(eval $(meson-package))