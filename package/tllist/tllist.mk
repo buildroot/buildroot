buildroot/buildroot

commit 996b786cfc03c3cf2ac631045650545c901e3075

package/tllist: new package
Signed-off-by: Adam Duskett <adam.duskett@amarulasolutions.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/tllist/tllist.mk
@@ -1,0 +1,16 @@
+################################################################################
+#
+# tllist
+#
+################################################################################
+
+TLLIST_VERSION = 1.1.0
+TLLIST_SOURCE = $(TLLIST_VERSION).tar.gz
+TLLIST_SITE = https://codeberg.org/dnkl/tllist/archive
+TLLIST_LICENSE = MIT
+TLLIST_LICENSE_FILES = LICENSE
+# header only
+TLLIST_INSTALL_TARGET = NO
+TLLIST_INSTALL_STAGING = YES
+
+$(eval $(meson-package))

DEVELOPERS
@@ -39,6 +39,7 @@ F:	package/flutter-pi/
 F:	package/flutter-sdk-bin/
 F:	package/python-kmod/
 F:	package/python-versioneer/
+F:	package/tllist/
 F:	support/testing/tests/package/test_firewalld.py
 F:	support/testing/tests/package/test_flutter.py
 

package/Config.in
@@ -2203,6 +2203,7 @@ endif
 	source "package/tbb/Config.in"
 	source "package/tinycbor/Config.in"
 	source "package/tl-expected/Config.in"
+	source "package/tllist/Config.in"
 	source "package/tz/Config.in"
 	source "package/tzdata/Config.in"
 	source "package/uvw/Config.in"

package/tllist/Config.in
@@ -1,0 +1,6 @@
+config BR2_PACKAGE_TLLIST
+	bool "tllist"
+	help
+	  A C header file only implementation of a typed linked list.
+
+	  https://codeberg.org/dnkl/tllist

package/tllist/tllist.hash
@@ -1,0 +1,3 @@
+# Locally computed
+sha256  3f3fe2f7433719cec816c63937a7aa36e566bd317763ef46d11562073ab6361d  1.1.0.tar.gz
+sha256  d534a23a31500a0ac958d9634b84f532bd73ff1aca1bb8f7debbcbebc16ff39a  LICENSE

commit a9ccdc11704de3ac7aac892080e2cd8561dc3482

package/tllist: fix build with gcc 4.8
Fix the following build failure with gcc 4.8 raised since the addition
of the package in commit 996b786cfc03c3cf2ac631045650545c901e3075:

In file included from ../test.c:7:0:
../test.c: In function 'main':
../tllist.h:213:17: error: 'for' loop initial declarations are only allowed in C99 mode
                 for (int _i = 0; _i < __insize; _i++) {                 \
                 ^

Fixes: 996b786cfc03c3cf2ac631045650545c901e3075
 - http://autobuild.buildroot.org/results/e82fdf4f9ef199e1baa169d38a75872bddd4e6dd

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

package/tllist/tllist.mk
@@ -12,5 +12,6 @@ TLLIST_LICENSE_FILES = LICENSE
 # header only
 TLLIST_INSTALL_TARGET = NO
 TLLIST_INSTALL_STAGING = YES
+TLLIST_CFLAGS = $(TARGET_CFLAGS) -std=c99
 
 $(eval $(meson-package))