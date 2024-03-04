buildroot/buildroot

commit e926c83928e08c0ecd6cd4383e1cca151a8a4d2c

package/libva: bump version to 2.20.0
Release notes: https://github.com/intel/libva/blob/v2.20-branch/NEWS

Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

package/libva/libva.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBVA_VERSION = 2.19.0
+LIBVA_VERSION = 2.20.0
 LIBVA_SITE = $(call github,intel,libva,$(LIBVA_VERSION))
 LIBVA_LICENSE = MIT
 LIBVA_LICENSE_FILES = COPYING

package/libva/libva.hash
@@ -1,3 +1,3 @@
 # Locally computed
-sha256  8cb5e2a9287a76b12c0b6cb96f4f27a0321bbe693df43cd950e5d4542db7f227  libva-2.19.0.tar.gz
+sha256  117f8d658a5fc9ea406ca80a3eb4ae1d70b15a54807c9ed77199c812bed73b60  libva-2.20.0.tar.gz
 sha256  c86a782ee845b52472dae9b9d79fb915d333628ac0efe49cdce63644814931de  COPYING

commit d4fdd78a7207935c24427bbe736c443f0ad2a745

package/libva: fix build with gcc 4.8
Fix the following build failure with gcc 4.8 raised since bump to
version 2.20.0 in commit e926c83928e08c0ecd6cd4383e1cca151a8a4d2c and
https://github.com/intel/libva/commit/4f5a4345fc9f92565763ca22026891475e3cf835:

../va/va.c: In function 'va_new_opendriver':
../va/va.c:695:9: error: 'for' loop initial declarations are only allowed in C99 mode
         for (unsigned int i = 0; i < num_drivers; i++)
         ^
../va/va.c:695:9: note: use option -std=c99 or -std=gnu99 to compile your code

Fixes: e926c83928e08c0ecd6cd4383e1cca151a8a4d2c
 - http://autobuild.buildroot.org/results/b9dbd104fa05c59883d87f74e6522c55620a4252

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

package/libva/libva.mk
@@ -10,6 +10,7 @@ LIBVA_LICENSE = MIT
 LIBVA_LICENSE_FILES = COPYING
 LIBVA_INSTALL_STAGING = YES
 LIBVA_DEPENDENCIES = host-pkgconf libdrm
+LIBVA_CFLAGS = $(TARGET_CFLAGS) -std=gnu99
 
 # libdrm is a hard-dependency
 LIBVA_CONF_OPTS = \