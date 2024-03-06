buildroot/buildroot

commit 2f8feb66204ff661e9fd5f508e2dfd1ebc0f2813

package/botan: bump to version 3.2.0
- Drop patches (already in version)
- Update hash of license.txt (update in year:
  https://github.com/randombit/botan/commit/4d0371816763f16ea0b9694155016db591293c8e)

https://github.com/randombit/botan/blob/3.2.0/news.rst

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/botan/botan.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BOTAN_VERSION = 2.19.3
+BOTAN_VERSION = 3.2.0
 BOTAN_SOURCE = Botan-$(BOTAN_VERSION).tar.xz
 BOTAN_SITE = http://botan.randombit.net/releases
 BOTAN_LICENSE = BSD-2-Clause

.checkpackageignore
@@ -208,8 +208,6 @@ package/bmx7/0001-Fix-schedule.c-378-36-error-SIOCGSTAMP-undeclared.patch Upstre
 package/bmx7/0002-Fix-linking-error.patch Upstream
 package/bmx7/0003-Reorder-includes-to-avoid-ethhdr-collision.patch Upstream
 package/boinc/S99boinc-client Indent Shellcheck Variables
-package/botan/0001-Add-superh-alias-needed-by-Debian.patch Upstream
-package/botan/0002-src-build-data-arch-superh.txt-add-sh4-eb-aeb.patch Upstream
 package/brickd/S70brickd Indent Shellcheck Variables
 package/bridge-utils/0001-fix-build-on-musl.patch Upstream
 package/brltty/0001-Fix-linking-error-on-mips64el.patch Upstream

package/botan/0001-Add-superh-alias-needed-by-Debian.patch
@@ -1,22 +1,0 @@
-From 454c7c04385a47d511cf8999ccff2746afbab06b Mon Sep 17 00:00:00 2001
-From: Jack Lloyd <jack@randombit.net>
-Date: Sat, 21 Nov 2020 12:37:06 -0500
-Subject: [PATCH] Add superh alias needed by Debian
-
-[Retrieved from:
-https://github.com/randombit/botan/commit/454c7c04385a47d511cf8999ccff2746afbab06b]
-Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
----
- src/build-data/arch/superh.txt | 1 +
- 1 file changed, 1 insertion(+)
-
-diff --git a/src/build-data/arch/superh.txt b/src/build-data/arch/superh.txt
-index 6af6dbe682..8e2833a914 100644
---- a/src/build-data/arch/superh.txt
-+++ b/src/build-data/arch/superh.txt
-@@ -1,4 +1,5 @@
- 
- <aliases>
- sh4
-+sh4a
- </aliases>

package/botan/0002-src-build-data-arch-superh.txt-add-sh4-eb-aeb.patch
@@ -1,30 +1,0 @@
-From c88897ebaf173b97068811b520a4741039f09dcd Mon Sep 17 00:00:00 2001
-From: Fabrice Fontaine <fontaine.fabrice@gmail.com>
-Date: Sat, 20 Aug 2022 15:16:22 +0200
-Subject: [PATCH] src/build-data/arch/superh.txt: add sh4{eb,aeb}
-
-Fix the following build failure with sh4{eb,aeb}:
-
-  ERROR: Unknown or unidentifiable processor "sh4aeb"
-
-Fixes:
- - http://autobuild.buildroot.org/results/d7750b734736a66e10bc5a8ee06708041b36443a
-
-Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
-[Retrieved from:
-https://github.com/randombit/botan/commit/c88897ebaf173b97068811b520a4741039f09dcd]
----
- src/build-data/arch/superh.txt | 2 ++
- 1 file changed, 2 insertions(+)
-
-diff --git a/src/build-data/arch/superh.txt b/src/build-data/arch/superh.txt
-index 8e2833a914..e17edb097a 100644
---- a/src/build-data/arch/superh.txt
-+++ b/src/build-data/arch/superh.txt
-@@ -2,4 +2,6 @@
- <aliases>
- sh4
- sh4a
-+sh4eb
-+sh4aeb
- </aliases>

package/botan/botan.hash
@@ -1,4 +1,4 @@
 # From https://botan.randombit.net/releases/sha256sums.txt
-sha256  dae047f399c5a47f087db5d3d9d9e8f11ae4985d14c928d71da1aff801802d55  Botan-2.19.3.tar.xz
+sha256  049c847835fcf6ef3a9e206b33de05dd38999c325e247482772a5598d9e5ece3  Botan-3.2.0.tar.xz
 # Locally computed
-sha256  472faf6d2231130382779f96de506be19296473750356449fc426ddc9cb03b50  license.txt
+sha256  1833cde7c7cc03296b1ef2ddc178b1cd7fd1c476840f32cf6aedb09ab0bc9004  license.txt

commit c944f455adf9730b1c72e70cf5b5e14a64a470be

package/botan: fix build without NPTL
botan unconditionally uses pthread_setname_np since bump to version
3.2.0 in commit 2f8feb66204ff661e9fd5f508e2dfd1ebc0f2813 and
https://github.com/randombit/botan/commit/313e439c786d68bcf374b2cb0edfe3ffd891db94
resulting in the following build failure:

src/lib/utils/os_utils.cpp:625:22: error: 'pthread_setname_np' was not declared in this scope
  625 |    static_cast<void>(pthread_setname_np(thread.native_handle(), name.c_str()));
      |                      ^~~~~~~~~~~~~~~~~~

So only enable threads if NPTL is available

Fixes: 2f8feb66204ff661e9fd5f508e2dfd1ebc0f2813
 - http://autobuild.buildroot.org/results/03677de6270ff5de61fff2a76e5cd723aa34e64e

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

package/botan/Config.in
@@ -15,7 +15,6 @@ config BR2_PACKAGE_BOTAN
 	bool "botan"
 	depends on BR2_INSTALL_LIBSTDCPP
 	depends on BR2_TOOLCHAIN_GCC_AT_LEAST_10 # C++20
-	depends on BR2_TOOLCHAIN_HAS_THREADS
 	depends on BR2_PACKAGE_BOTAN_ARCH_SUPPORTS
 	select BR2_PACKAGE_BOOST_FILESYSTEM if BR2_PACKAGE_BOOST && BR2_TOOLCHAIN_SUPPORTS_ALWAYS_LOCKFREE_ATOMIC_INTS
 	select BR2_PACKAGE_BOOST_SYSTEM if BR2_PACKAGE_BOOST && BR2_TOOLCHAIN_SUPPORTS_ALWAYS_LOCKFREE_ATOMIC_INTS
@@ -24,7 +23,7 @@ config BR2_PACKAGE_BOTAN
 
 	  http://botan.randombit.net
 
-comment "botan needs a toolchain w/ C++, threads, gcc >= 10"
-	depends on !BR2_TOOLCHAIN_HAS_THREADS || !BR2_INSTALL_LIBSTDCPP \
+comment "botan needs a toolchain w/ C++, gcc >= 10"
+	depends on !BR2_INSTALL_LIBSTDCPP \
 		|| !BR2_TOOLCHAIN_GCC_AT_LEAST_10
 	depends on BR2_PACKAGE_BOTAN_ARCH_SUPPORTS

package/botan/botan.mk
@@ -48,6 +48,12 @@ else
 BOTAN_CONF_OPTS += --without-stack-protector
 endif
 
+ifeq ($(BR2_TOOLCHAIN_HAS_THREADS_NPTL),y)
+BOTAN_CONF_OPTS += --with-os-feature=threads
+else
+BOTAN_CONF_OPTS += --without-os-feature=threads
+endif
+
 ifeq ($(BR2_TOOLCHAIN_USES_UCLIBC),y)
 BOTAN_CONF_OPTS += --without-os-feature=explicit_bzero,getauxval,getentropy
 endif