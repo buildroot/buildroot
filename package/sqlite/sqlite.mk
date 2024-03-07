50c7bf3b22fd609000150938df6e9b35fa8cc549
Bernd Kuhls <bernd@kuhls.net>
2024-03-07 18:49:24 +0100

    package/sqlite: bump version to 3.45.1
    Release notes: https://sqlite.org/releaselog/3_45_1.html
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/sqlite/sqlite.mk
+++ package/sqlite/sqlite.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-SQLITE_VERSION = 3.44.2
-SQLITE_TAR_VERSION = 3440200
+SQLITE_VERSION = 3.45.1
+SQLITE_TAR_VERSION = 3450100
 SQLITE_SOURCE = sqlite-autoconf-$(SQLITE_TAR_VERSION).tar.gz
-SQLITE_SITE = https://www.sqlite.org/2023
+SQLITE_SITE = https://www.sqlite.org/2024
 SQLITE_LICENSE = blessing
 SQLITE_LICENSE_FILES = tea/license.terms
 SQLITE_CPE_ID_VENDOR = sqlite
