2e6e121496bfcbe09c018558be13733077b6df9b
Julien Olivain <ju.o@free.fr>
2024-03-10 16:10:36 +0100

    package/mokutil: new package
    mokutil is a tool to import or delete the machines owner
    keys (MOK) stored in the database of shim.
    
    "shim" is the first stage bootloader to support UEFI Secure
    Boot. See also the "shim" package in the bootloaders
    section.
    
    https://github.com/lcp/mokutil
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/mokutil/mokutil.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# mokutil
+#
+################################################################################
+
+MOKUTIL_VERSION = 0.7.1
+MOKUTIL_SITE = $(call github,lcp,mokutil,$(MOKUTIL_VERSION))
+MOKUTIL_LICENSE = GPL-3.0+
+MOKUTIL_LICENSE_FILES = COPYING
+MOKUTIL_AUTORECONF = YES
+
+MOKUTIL_DEPENDENCIES = \
+	efivar \
+	host-pkgconf \
+	keyutils \
+	openssl
+
+$(eval $(autotools-package))
