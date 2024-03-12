92f4820374870bf66bc4c51fed975731fbeefbe5
Jeremy J. Peper <jeremy@jeremypeper.com>
2024-03-12 14:35:26 -0500

    package/python-configshell-fb: bump version to 1.1.30
    bump to latest version because previous version did not work with python 3.11
    
    Signed-off-by: Jeremy J. Peper <jeremy@jeremypeper.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>
    (cherry picked from commit 0c27711002a82bbbb47363efceb526be7862bab3)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-configshell-fb/python-configshell-fb.mk
+++ package/python-configshell-fb/python-configshell-fb.mk
@@ -6,7 +6,7 @@
 
 # When upgrading the version, be sure to also upgrade python-rtslib-fb
 # and targetcli-fb at the same time.
-PYTHON_CONFIGSHELL_FB_VERSION = 1.1.29
+PYTHON_CONFIGSHELL_FB_VERSION = 1.1.30
 PYTHON_CONFIGSHELL_FB_SITE = $(call github,open-iscsi,configshell-fb,v$(PYTHON_CONFIGSHELL_FB_VERSION))
 PYTHON_CONFIGSHELL_FB_LICENSE = Apache-2.0
 PYTHON_CONFIGSHELL_FB_LICENSE_FILES = COPYING
