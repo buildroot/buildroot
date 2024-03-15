buildroot/buildroot

commit f9f5b3a6cbc4e62beefdd0448842ef0c8c3eb6f6

package/libnfs: bump to version 5.0.2
- Update patch
- Handle pthread support added with
  https://github.com/sahlberg/libnfs/commit/f55637619ecbeb7c7bbd4143768b6b45100be86c

https://github.com/sahlberg/libnfs/blob/libnfs-5.0.2/CHANGELOG

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/libnfs/libnfs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBNFS_VERSION = 4.0.0
+LIBNFS_VERSION = 5.0.2
 LIBNFS_SITE = $(call github,sahlberg,libnfs,libnfs-$(LIBNFS_VERSION))
 LIBNFS_INSTALL_STAGING = YES
 LIBNFS_AUTORECONF = YES
@@ -14,6 +14,12 @@ LIBNFS_DEPENDENCIES = host-pkgconf
 
 ifeq ($(BR2_PACKAGE_LIBTIRPC),y)
 LIBNFS_DEPENDENCIES += libtirpc
+endif
+
+ifeq ($(BR2_TOOLCHAIN_HAS_THREADS),y)
+LIBNFS_CONF_OPTS += --enable-threads
+else
+LIBNFS_CONF_OPTS += --disable-threads
 endif
 
 $(eval $(autotools-package))

.checkpackageignore
@@ -705,7 +705,6 @@ package/libmpeg2/0003-fix-arm-detection.patch Upstream
 package/libmpeg2/0004-fix-sparc.patch Upstream
 package/libnetfilter_conntrack/0001-conntrack-fix-build-with-kernel-5-15-and-musl.patch Upstream
 package/libnfc/0001-autotools-make-example-build-optional.patch Upstream
-package/libnfs/0001-Fix-include-sys-time.h.patch Upstream
 package/libnids/0001-libpcap-use-pkg-config.patch Upstream
 package/libnl/0001-Add-musl-workaround-to-the-libc-compat.h-copy.patch Upstream
 package/libnss/0001-Bug-1801182-Allow-overriding-OS_ARCH-OS_TEST-and-OS_.patch Upstream

package/libnfs/0001-Fix-include-sys-time.h.patch
@@ -11,10 +11,11 @@ POSIX says `struct timeval` is defined if <sys/time.h> is included.
 Instead of the mess that is currently done based on the system on which
 the stuff is being compiled, include it unconditionally.
 
-Reported upstream:
-https://github.com/sahlberg/libnfs/issues/272
+Upstream: https://github.com/sahlberg/libnfs/issues/272
 
 Signed-off-by: Jörg Krause <joerg.krause@embedded.rocks>
+[Fabrice: update for 5.0.2]
+Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
 ---
  include/nfsc/libnfs.h | 5 -----
  1 file changed, 5 deletions(-)
@@ -27,8 +28,8 @@ index 09dcf1c..b6db58c 100755
  #define _LIBNFS_H_
  
  #include <stdint.h>
--#if defined(__ANDROID__) || defined(AROS) \
-- || ( defined(__APPLE__) && defined(__MACH__) )
+-#if defined(__ANDROID__) || defined(AROS) || defined(__PPU__) \
+- || ( defined(__APPLE__) && defined(__MACH__) ) || defined(__FreeBSD__) || defined(__OpenBSD__)
  #include <sys/time.h>
 -#else
 -#include <time.h>

package/libnfs/libnfs.hash
@@ -1,5 +1,5 @@
 # Locally calculated
-sha256  6ee77e9fe220e2d3e3b1f53cfea04fb319828cc7dbb97dd9df09e46e901d797d  libnfs-4.0.0.tar.gz
+sha256  637e56643b19da9fba98f06847788c4dad308b723156a64748041035dcdf9bd3  libnfs-5.0.2.tar.gz
 sha256  edd960c0142b8ada98b43b6396b78f4e557b0bc70ac601a51e397ad04070e2c5  COPYING
 sha256  d9406ced95457941032aa11d04623b8ab71f2827a3395ebef137aec475be35b1  LICENCE-BSD.txt
 sha256  dc626520dcd53a22f727af3ee42c770e56c97a64fe3adb063799d8ab032fe551  LICENCE-LGPL-2.1.txt

commit 9a5160ed5abded9d2b843a2422a48d4964cff42e

package/libnfs: fix pthread option
Fix typo added by commit f9f5b3a6cbc4e62beefdd0448842ef0c8c3eb6f6

Fixes: f9f5b3a6cbc4e62beefdd0448842ef0c8c3eb6f6

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

package/libnfs/libnfs.mk
@@ -17,9 +17,9 @@ LIBNFS_DEPENDENCIES += libtirpc
 endif
 
 ifeq ($(BR2_TOOLCHAIN_HAS_THREADS),y)
-LIBNFS_CONF_OPTS += --enable-threads
+LIBNFS_CONF_OPTS += --enable-pthread
 else
-LIBNFS_CONF_OPTS += --disable-threads
+LIBNFS_CONF_OPTS += --disable-pthread
 endif
 
 $(eval $(autotools-package))