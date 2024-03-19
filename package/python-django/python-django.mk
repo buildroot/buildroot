56de003fa31dc3e30e915c0d97d5af5becbdbc01
Peter Korsgaard <peter@korsgaard.com>
2024-03-19 17:11:17 +0100

    package/python-django: security bump to version 4.2.11
    Fixes: CVE-2024-27351: Potential regular expression
    denial-of-service in django.utils.text.Truncator.words()
    
    https://www.djangoproject.com/weblog/2024/mar/04/security-releases/
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 4.2.10
+PYTHON_DJANGO_VERSION = 4.2.11
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/52/ae/84530c15c4df0830837a6417956f16ff1b410412915282db78a27a7fb03c
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/d2/e2/c0e7586dd4db1140be179b1386346d1b2e2570dfc7c0d1c0343bdf0d43b0
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
 PYTHON_DJANGO_CPE_ID_VENDOR = djangoproject
