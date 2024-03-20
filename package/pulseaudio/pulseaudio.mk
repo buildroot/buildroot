buildroot/buildroot

commit ef0fa986eb7ff25c0a5db70ec0b62032e2d71538

package/webrtc-audio-processing: bump to version 1.3
- Drop patch (not needed anymore)
- Switch to meson-package
- libabseil-cpp is a mandatory dependency

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

.checkpackageignore
@@ -1405,7 +1405,6 @@ package/wampcc/0001-Add-RISC-V-endian-detection.patch Upstream
 package/wampcc/0002-include-wampcc-platform.h-fix-build-with-musl-1.2.0.patch Upstream
 package/wampcc/0003-Broken-build-on-Windows.patch Upstream
 package/watchdogd/S01watchdogd Indent NotExecutable
-package/webrtc-audio-processing/0001-Proper-detection-of-cxxabi.h-and-execinfo.h.patch Upstream
 package/wget/0001-lib-getrandom.c-fix-build-with-uclibc-1.0.35.patch Upstream
 package/wilc-driver/0001-cfg80211.c-fix-missing-prandom_u32-with-Linux-6.1.0.patch Upstream
 package/wilc-driver/0002-spi.c-fix-build-failure-on-remove-callback.patch Upstream

package/gstreamer1/gst1-plugins-bad/Config.in
@@ -675,17 +675,20 @@ config BR2_PACKAGE_GST1_PLUGINS_BAD_PLUGIN_WEBRTCDSP
 	bool "webrtcdsp"
 	# All depends from webrtc-audio-processing
 	depends on BR2_PACKAGE_WEBRTC_AUDIO_PROCESSING_ARCH_SUPPORTS
+	depends on BR2_PACKAGE_LIBABSEIL_CPP_ARCH_SUPPORTS
 	depends on BR2_INSTALL_LIBSTDCPP
-	depends on BR2_TOOLCHAIN_GCC_AT_LEAST_4_8
+	depends on BR2_TOOLCHAIN_GCC_AT_LEAST_8
 	depends on BR2_TOOLCHAIN_HAS_THREADS_NPTL
+	depends on !BR2_STATIC_LIBS
 	select BR2_PACKAGE_WEBRTC_AUDIO_PROCESSING
 	help
 	  WebRTC echo-cancellation, gain control and noise suppression
 
-comment "webrtcdsp needs a toolchain w/ C++, NPTL, gcc >= 4.8"
+comment "webrtcdsp needs a toolchain w/ C++, NPTL, dynamic library, gcc >= 8"
 	depends on BR2_PACKAGE_WEBRTC_AUDIO_PROCESSING_ARCH_SUPPORTS
+	depends on BR2_PACKAGE_LIBABSEIL_CPP_ARCH_SUPPORTS
 	depends on !BR2_INSTALL_LIBSTDCPP || !BR2_TOOLCHAIN_HAS_THREADS_NPTL \
-		|| !BR2_TOOLCHAIN_GCC_AT_LEAST_4_8
+		|| BR2_STATIC_LIBS || !BR2_TOOLCHAIN_GCC_AT_LEAST_8
 
 config BR2_PACKAGE_GST1_PLUGINS_BAD_PLUGIN_WPE
 	bool "wpe"

package/webrtc-audio-processing/0001-Proper-detection-of-cxxabi.h-and-execinfo.h.patch
@@ -1,63 +1,0 @@
-From b7a166acaddc4c78afa2b653e25114d9114699f3 Mon Sep 17 00:00:00 2001
-From: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
-Date: Sat, 6 Aug 2016 11:24:50 +0200
-Subject: [PATCH] Proper detection of cxxabi.h and execinfo.h
-
-The current code in webrtc/base/checks.cc assumes that if __GLIBCXX__ is
-defined and __UCLIBC__ is not defined, then both cxxabi.h and execinfo.h
-will be available.
-
-Unfortunately, this is not correct with the musl C library:
-
- - It defines __GLIBCXX__
- - It does not define __UCLIBC__ (it's not uClibc after all!)
- - But it also doesn't provide execinfo.h
-
-Therefore, in order to make things work properly, we switch to proper
-autoconf checks for cxxabi.h and execinfo.h, and only use the backtrace
-functionality if both are provided.
-
-Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
----
- configure.ac          | 2 ++
- webrtc/base/checks.cc | 4 ++--
- 2 files changed, 4 insertions(+), 2 deletions(-)
-
-diff --git a/configure.ac b/configure.ac
-index acbb3e2..ff4c752 100644
---- a/configure.ac
-+++ b/configure.ac
-@@ -45,6 +45,8 @@ AC_SUBST(GNUSTL_CFLAGS)
- # Borrowed from gst-plugins-bad
- AC_CHECK_HEADER(MobileCoreServices/MobileCoreServices.h, HAVE_IOS="yes", HAVE_IOS="no", [-])
- 
-+AC_CHECK_HEADERS([cxxabi.h execinfo.h])
-+
- # Based on gst-plugins-bad configure.ac and defines in
- # <chromium source>/build/config/BUILDCONFIG.gn and
- # webrtc/BUILD.gn
-diff --git a/webrtc/base/checks.cc b/webrtc/base/checks.cc
-index 49a31f2..05d23a6 100644
---- a/webrtc/base/checks.cc
-+++ b/webrtc/base/checks.cc
-@@ -16,7 +16,7 @@
- #include <cstdio>
- #include <cstdlib>
- 
--#if defined(__GLIBCXX__) && !defined(__UCLIBC__)
-+#if defined(HAVE_CXX_ABI_H) && defined(HAVE_EXECINFO_H)
- #include <cxxabi.h>
- #include <execinfo.h>
- #endif
-@@ -55,7 +55,7 @@ void PrintError(const char* format, ...) {
- // to get usable symbols on Linux. This is copied from V8. Chromium has a more
- // advanced stace trace system; also more difficult to copy.
- void DumpBacktrace() {
--#if defined(__GLIBCXX__) && !defined(__UCLIBC__)
-+#if defined(HAVE_CXX_ABI_H) && defined(HAVE_EXECINFO_H)
-   void* trace[100];
-   int size = backtrace(trace, sizeof(trace) / sizeof(*trace));
-   char** symbols = backtrace_symbols(trace, size);
--- 
-2.7.4
-

package/webrtc-audio-processing/Config.in
@@ -5,17 +5,21 @@ config BR2_PACKAGE_WEBRTC_AUDIO_PROCESSING_ARCH_SUPPORTS
 config BR2_PACKAGE_WEBRTC_AUDIO_PROCESSING
 	bool "webrtc-audio-processing"
 	depends on BR2_PACKAGE_WEBRTC_AUDIO_PROCESSING_ARCH_SUPPORTS
+	depends on BR2_PACKAGE_LIBABSEIL_CPP_ARCH_SUPPORTS
 	depends on BR2_INSTALL_LIBSTDCPP
-	depends on BR2_TOOLCHAIN_GCC_AT_LEAST_4_8
+	depends on BR2_TOOLCHAIN_GCC_AT_LEAST_8 # libabseil-cpp
 	# pthread_condattr_setclock
 	depends on BR2_TOOLCHAIN_HAS_THREADS_NPTL
+	depends on !BR2_STATIC_LIBS # libabseil-cpp
+	select BR2_PACKAGE_LIBABSEIL_CPP
 	help
 	  AudioProcessing library based on Google's implementation of
 	  WebRTC.
 
 	  http://freedesktop.org/software/pulseaudio/webrtc-audio-processing/
 
-comment "webrtc-audio-processing needs a toolchain w/ C++, NPTL, gcc >= 4.8"
+comment "webrtc-audio-processing needs a toolchain w/ C++, NPTL, dynamic library, gcc >= 8"
 	depends on BR2_PACKAGE_WEBRTC_AUDIO_PROCESSING_ARCH_SUPPORTS
+	depends on BR2_PACKAGE_LIBABSEIL_CPP_ARCH_SUPPORTS
 	depends on !BR2_INSTALL_LIBSTDCPP || !BR2_TOOLCHAIN_HAS_THREADS_NPTL \
-		|| !BR2_TOOLCHAIN_GCC_AT_LEAST_4_8
+		|| BR2_STATIC_LIBS || !BR2_TOOLCHAIN_GCC_AT_LEAST_8

package/webrtc-audio-processing/webrtc-audio-processing.hash
@@ -1,3 +1,3 @@
 # Locally calculated
-sha256  a0fdd938fd85272d67e81572c5a4d9e200a0c104753cb3c209ded175ce3c5dbf  webrtc-audio-processing-0.3.1.tar.xz
+sha256  2365e93e778d7b61b5d6e02d21c47d97222e9c7deff9e1d0838ad6ec2e86f1b9  webrtc-audio-processing-1.3.tar.xz
 sha256  9b79539028e216e813e152d45f5c1ed5fdd0554426ad50270fb03134e7082dac  COPYING

package/webrtc-audio-processing/webrtc-audio-processing.mk
@@ -4,18 +4,12 @@
 #
 ################################################################################
 
-WEBRTC_AUDIO_PROCESSING_VERSION = 0.3.1
+WEBRTC_AUDIO_PROCESSING_VERSION = 1.3
 WEBRTC_AUDIO_PROCESSING_SOURCE = webrtc-audio-processing-$(WEBRTC_AUDIO_PROCESSING_VERSION).tar.xz
 WEBRTC_AUDIO_PROCESSING_SITE = http://freedesktop.org/software/pulseaudio/webrtc-audio-processing
 WEBRTC_AUDIO_PROCESSING_INSTALL_STAGING = YES
 WEBRTC_AUDIO_PROCESSING_LICENSE = BSD-3-Clause
 WEBRTC_AUDIO_PROCESSING_LICENSE_FILES = COPYING
-WEBRTC_AUDIO_PROCESSING_DEPENDENCIES = host-pkgconf
-# 0001-Proper-detection-of-cxxabi.h-and-execinfo.h.patch
-WEBRTC_AUDIO_PROCESSING_AUTORECONF = YES
+WEBRTC_AUDIO_PROCESSING_DEPENDENCIES = host-pkgconf libabseil-cpp
 
-ifeq ($(BR2_SOFT_FLOAT),y)
-WEBRTC_AUDIO_PROCESSING_CONF_OPTS += --with-ns-mode=fixed
-endif
-
-$(eval $(autotools-package))
+$(eval $(meson-package))

commit 87504a1b7948dbfa3648ad02c3c034a3c36dcd92

package/pulseaudio: bump to version 17.0
- Drop three upstreamed patches
- This bump will fix the following build failure raised since bump of
  webrtc-audio-processsing to version 1.3 in commit
  ef0fa986eb7ff25c0a5db70ec0b62032e2d71538:

  ../output-1/build/pulseaudio-16.1/meson.build:723:15: ERROR: Dependency "webrtc-audio-processing" not found, tried pkgconfig and cmake

https://gitlab.freedesktop.org/pulseaudio/pulseaudio/-/blob/v17.0/NEWS

Fixes: ef0fa986eb7ff25c0a5db70ec0b62032e2d71538
 - http://autobuild.buildroot.org/results/528717c7481adbb5333d77758ef94830f46bc3f1

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

.checkpackageignore
@@ -1030,9 +1030,6 @@ package/ptpd2/0002-ntp_isc_md5-rename-EVP_MD_CTX-into-PTPD_EVP_MD_CTX.patch Upst
 package/ptpd2/0003-Solve-issue-25-Removing-type-U64-from-net-snmp-relat.patch Upstream
 package/ptpd2/S65ptpd2 Indent Shellcheck Variables
 package/pulseaudio/0001-shm.c-use-_Static_assert-instead-of-static_assert-fo.patch Upstream
-package/pulseaudio/0002-build-sys-Fix-atomic-support-detection.patch Upstream
-package/pulseaudio/0003-build-sys-Add-missing-libatomic_ops-dependencies.patch Upstream
-package/pulseaudio/0004-meson.build-fix-build-without-C.patch Upstream
 package/pulseaudio/S50pulseaudio ConsecutiveEmptyLines EmptyLastLine Indent Variables
 package/pulseview/0001-Replace-obsolete-deprecated-Qt-methods.patch Upstream
 package/pulseview/0002-Fix-broken-build-due-to-C-template-behind-C-linkage.patch Upstream

package/pulseaudio/0002-build-sys-Fix-atomic-support-detection.patch
@@ -1,59 +1,0 @@
-From a5392576ceba92d04706cefc1929ddd5ace5537a Mon Sep 17 00:00:00 2001
-From: Nicolas Cavallari <nicolas.cavallari@green-communications.fr>
-Date: Fri, 1 Jul 2022 14:03:44 +0200
-Subject: [PATCH] build-sys: Fix atomic support detection
-
-Attempting to use atomics operations on an architecture that does not
-support them generally results in a link error:
-
-ld: /tmp/ccjYcMPP.o: in function `func':
-testfile.c:(.text+0x1c): undefined reference to `__sync_bool_compare_and_swap_4'
-
-The current build system uses cc.compiles() to check if atomic ops are
-supported, but cc.compiles() does not attempt to link, so the test fails
-to enable libatomics_opts.
-
-Fix this by using cc.links() instead of cc.compiles().
-
-Signed-off-by: Nicolas Cavallari <nicolas.cavallari@green-communications.fr>
-Upstream-status: Submitted [https://gitlab.freedesktop.org/pulseaudio/pulseaudio/-/merge_requests/732]
----
- meson.build | 10 ++++++----
- 1 file changed, 6 insertions(+), 4 deletions(-)
-
-diff --git a/meson.build b/meson.build
-index c6db7e670..c5135330f 100644
---- a/meson.build
-+++ b/meson.build
-@@ -498,22 +498,24 @@ endif
- 
- need_libatomic_ops = false
- 
--atomictest = '''void func() {
-+atomictest = '''int main() {
-   volatile int atomic = 2;
-   __sync_bool_compare_and_swap (&atomic, 2, 3);
-+  return 0;
- }
- '''
- 
--if cc.compiles(atomictest)
-+if cc.links(atomictest)
-   cdata.set('HAVE_ATOMIC_BUILTINS', 1)
- 
--  newatomictest = '''void func() {
-+  newatomictest = '''int main() {
-     int c = 0;
-     __atomic_store_n(&c, 4, __ATOMIC_SEQ_CST);
-+    return 0;
-   }
-   '''
- 
--  if(cc.compiles(newatomictest))
-+  if(cc.links(newatomictest))
-     cdata.set('HAVE_ATOMIC_BUILTINS_MEMORY_MODEL', 1)
-   endif
- 
--- 
-2.36.1
-

package/pulseaudio/0003-build-sys-Add-missing-libatomic_ops-dependencies.patch
@@ -1,129 +1,0 @@
-From 96361ff2a8f37dd3ce7ea188ce4e7b038bb6a5aa Mon Sep 17 00:00:00 2001
-From: Nicolas Cavallari <nicolas.cavallari@green-communications.fr>
-Date: Mon, 4 Jul 2022 13:49:34 +0200
-Subject: [PATCH] build-sys: Add missing libatomic_ops dependencies
-
-Add libatomic_ops dependencies to libraries/modules that showed a
-failure on an arch that does not have native atomic operations support.
-
-Not all optional dependencies were tested, so it is possible that
-some optional modules are still missing libatomic_ops dependencies.
-
-Signed-off-by: Nicolas Cavallari <nicolas.cavallari@green-communications.fr>
-Upstream-status: Submitted [https://gitlab.freedesktop.org/pulseaudio/pulseaudio/-/merge_requests/732]
----
- src/meson.build           |  2 +-
- src/modules/meson.build   |  6 +++---
- src/pulse/meson.build     |  2 +-
- src/pulsecore/meson.build | 10 +++++-----
- 4 files changed, 10 insertions(+), 10 deletions(-)
-
-diff --git a/src/meson.build b/src/meson.build
-index 96dcec354..9efb561d8 100644
---- a/src/meson.build
-+++ b/src/meson.build
-@@ -205,7 +205,7 @@ else
-       libm_dep, thread_dep, dl_dep, shm_dep, iconv_dep, sndfile_dep, dbus_dep,
-       x11_dep, libsystemd_dep, glib_dep.partial_dependency(compile_args: true),
-       gtk_dep.partial_dependency(compile_args: true), asyncns_dep, libintl_dep,
--      platform_dep, platform_socket_dep, execinfo_dep,
-+      platform_dep, platform_socket_dep, execinfo_dep, libatomic_ops_dep,
-     ],
-     implicit_include_directories : false)
- 
-diff --git a/src/modules/meson.build b/src/modules/meson.build
-index 1d8004300..1e12569dc 100644
---- a/src/modules/meson.build
-+++ b/src/modules/meson.build
-@@ -14,7 +14,7 @@ all_modules = [
-   [ 'module-cli-protocol-tcp', 'module-protocol-stub.c', [], ['-DUSE_PROTOCOL_CLI', '-DUSE_TCP_SOCKETS'], [], libprotocol_cli ],
-   [ 'module-cli-protocol-unix', 'module-protocol-stub.c', [], ['-DUSE_PROTOCOL_CLI', '-DUSE_UNIX_SOCKETS'], [], libprotocol_cli ],
-   [ 'module-combine', 'module-combine.c' ],
--  [ 'module-combine-sink', 'module-combine-sink.c' ],
-+  [ 'module-combine-sink', 'module-combine-sink.c', [], [], [libatomic_ops_dep] ],
- #  [ 'module-coreaudio-detect', 'macosx/module-coreaudio-detect.c' ],
- #  [ 'module-coreaudio-device', 'macosx/module-coreaudio-device.c' ],
-   [ 'module-default-device-restore', 'module-default-device-restore.c', [], [], [], libprotocol_native ],
-@@ -73,7 +73,7 @@ endif
- 
- if host_machine.system() != 'windows'
-   all_modules += [
--    [ 'module-rtp-recv', 'rtp/module-rtp-recv.c', [], [], [], librtp ],
-+    [ 'module-rtp-recv', 'rtp/module-rtp-recv.c', [], [], [libatomic_ops_dep], librtp ],
-     [ 'module-rtp-send', 'rtp/module-rtp-send.c' , [], [], [], librtp ],
-   ]
- endif
-@@ -243,7 +243,7 @@ module_echo_cancel_sources = [
- ]
- module_echo_cancel_orc_sources = []
- module_echo_cancel_flags = []
--module_echo_cancel_deps = []
-+module_echo_cancel_deps = [libatomic_ops_dep]
- module_echo_cancel_libs = []
- 
- if get_option('adrian-aec')
-diff --git a/src/pulse/meson.build b/src/pulse/meson.build
-index c2128e087..1b82c807c 100644
---- a/src/pulse/meson.build
-+++ b/src/pulse/meson.build
-@@ -85,7 +85,7 @@ libpulse = shared_library('pulse',
-   link_args : [nodelete_link_args, versioning_link_args],
-   install : true,
-   install_rpath : privlibdir,
--  dependencies : [libm_dep, thread_dep, libpulsecommon_dep, dbus_dep, dl_dep, iconv_dep, libintl_dep, platform_dep, platform_socket_dep],
-+  dependencies : [libm_dep, thread_dep, libpulsecommon_dep, dbus_dep, dl_dep, iconv_dep, libintl_dep, platform_dep, platform_socket_dep, libatomic_ops_dep],
-   implicit_include_directories : false)
- 
- libpulse_dep = declare_dependency(link_with: libpulse)
-diff --git a/src/pulsecore/meson.build b/src/pulsecore/meson.build
-index b30264b3a..b37fec499 100644
---- a/src/pulsecore/meson.build
-+++ b/src/pulsecore/meson.build
-@@ -251,7 +251,7 @@ libcli = shared_library('cli',
-   c_args : [pa_c_args, server_c_args, database_c_args],
-   link_args : [nodelete_link_args],
-   include_directories : [configinc, topinc],
--  dependencies : [libpulse_dep, libpulsecommon_dep, libpulsecore_dep],
-+  dependencies : [libpulse_dep, libpulsecommon_dep, libpulsecore_dep, libatomic_ops_dep],
-   install : true,
-   install_rpath : privlibdir,
-   install_dir : modlibexecdir,
-@@ -268,7 +268,7 @@ libprotocol_cli = shared_library('protocol-cli',
-   c_args : [pa_c_args, server_c_args, database_c_args],
-   link_args : [nodelete_link_args],
-   include_directories : [configinc, topinc],
--  dependencies : [libpulse_dep, libpulsecommon_dep, libpulsecore_dep, libcli_dep],
-+  dependencies : [libpulse_dep, libpulsecommon_dep, libpulsecore_dep, libcli_dep, libatomic_ops_dep],
-   install : true,
-   install_rpath : rpath_dirs,
-   install_dir : modlibexecdir,
-@@ -280,7 +280,7 @@ libprotocol_http = shared_library('protocol-http',
-   c_args : [pa_c_args, server_c_args, database_c_args],
-   link_args : [nodelete_link_args],
-   include_directories : [configinc, topinc],
--  dependencies : [libpulse_dep, libpulsecommon_dep, libpulsecore_dep],
-+  dependencies : [libpulse_dep, libpulsecommon_dep, libpulsecore_dep, libatomic_ops_dep],
-   install : true,
-   install_rpath : privlibdir,
-   install_dir : modlibexecdir,
-@@ -292,7 +292,7 @@ libprotocol_native = shared_library('protocol-native',
-   c_args : [pa_c_args, server_c_args, database_c_args],
-   link_args : [nodelete_link_args],
-   include_directories : [configinc, topinc],
--  dependencies : [libpulse_dep, libpulsecommon_dep, libpulsecore_dep, dbus_dep],
-+  dependencies : [libpulse_dep, libpulsecommon_dep, libpulsecore_dep, dbus_dep, libatomic_ops_dep],
-   install : true,
-   install_rpath : privlibdir,
-   install_dir : modlibexecdir,
-@@ -304,7 +304,7 @@ libprotocol_simple = shared_library('protocol-simple',
-   c_args : [pa_c_args, server_c_args, database_c_args],
-   link_args : [nodelete_link_args],
-   include_directories : [configinc, topinc],
--  dependencies : [libpulse_dep, libpulsecommon_dep, libpulsecore_dep],
-+  dependencies : [libpulse_dep, libpulsecommon_dep, libpulsecore_dep, libatomic_ops_dep],
-   install : true,
-   install_rpath : privlibdir,
-   install_dir : modlibexecdir,
--- 
-2.36.1
-

package/pulseaudio/0004-meson.build-fix-build-without-C.patch
@@ -1,56 +1,0 @@
-From ac88536d7c3fde3c22933368296c9029bb67b9fc Mon Sep 17 00:00:00 2001
-From: Fabrice Fontaine <fontaine.fabrice@gmail.com>
-Date: Mon, 25 Jul 2022 21:49:25 +0200
-Subject: [PATCH] meson.build: fix build without C++
-
-Fix the following build failure without C++:
-
-../output-1/build/pulseaudio-16.1/meson.build:1:0: ERROR: Unknown compiler(s): [['/home/autobuild/autobuild/instance-1/output-1/per-package/pulseaudio/host/bin/powerpc64-buildroot-linux-gnu-g++']]
-The following exception(s) were encountered:
-Running "/home/autobuild/autobuild/instance-1/output-1/per-package/pulseaudio/host/bin/powerpc64-buildroot-linux-gnu-g++ --version" gave "[Errno 2] No such file or directory: '/home/autobuild/autobuild/instance-1/output-1/per-package/pulseaudio/host/bin/powerpc64-buildroot-linux-gnu-g++'"
-
-Fixes:
- - http://autobuild.buildroot.org/results/6526a21bd4da3b8458188f27c1ec04c381e4b673
-
-Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
-[Upstream status:
-https://gitlab.freedesktop.org/pulseaudio/pulseaudio/-/merge_requests/737]
----
- meson.build                         | 3 ++-
- src/modules/echo-cancel/meson.build | 2 ++
- 2 files changed, 4 insertions(+), 1 deletion(-)
-
-diff --git a/meson.build b/meson.build
-index c5135330f..8248f263b 100644
---- a/meson.build
-+++ b/meson.build
-@@ -1,4 +1,4 @@
--project('pulseaudio', 'c', 'cpp',
-+project('pulseaudio', 'c',
-         version : run_command(find_program('git-version-gen'), join_paths(meson.current_source_dir(), '.tarball-version')).stdout().strip(),
-         meson_version : '>= 0.50.0',
-         default_options : [ 'c_std=gnu11', 'cpp_std=c++11' ]
-@@ -433,6 +433,7 @@ endif
- # Code coverage
- 
- if get_option('gcov')
-+  add_languages('cpp')
-   add_project_arguments('--coverage', language: ['c', 'cpp'])
-   add_project_link_arguments('--coverage', language: ['c', 'cpp'])
- endif
-diff --git a/src/modules/echo-cancel/meson.build b/src/modules/echo-cancel/meson.build
-index 641cd35e7..3b998364e 100644
---- a/src/modules/echo-cancel/meson.build
-+++ b/src/modules/echo-cancel/meson.build
-@@ -6,6 +6,8 @@
- # '-Wl,--unresolved-symbols=ignore-in-object-files' otherwise it fails
- # at link time.
- 
-+add_languages('cpp')
-+
- libwebrtc_util_sources = [
-   'webrtc.cc'
- ]
--- 
-GitLab
-

package/pulseaudio/pulseaudio.hash
@@ -1,5 +1,5 @@
-# From https://lists.freedesktop.org/archives/pulseaudio-discuss/2022-June/032287.html
-sha256  8eef32ce91d47979f95fd9a935e738cd7eb7463430dabc72863251751e504ae4  pulseaudio-16.1.tar.xz
+# From https://lists.freedesktop.org/archives/pulseaudio-discuss/2024-January/032426.html
+sha256  053794d6671a3e397d849e478a80b82a63cb9d8ca296bd35b73317bb5ceb87b5  pulseaudio-17.0.tar.xz
 # Locally computed
 sha256  c38aee9e3c8c4d5d594ff548a1be05453023016d6286931f6512db215ec1fd42  GPL
 sha256  a9bdde5616ecdd1e980b44f360600ee8783b1f99b8cc83a2beb163a0a390e861  LGPL

package/pulseaudio/pulseaudio.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PULSEAUDIO_VERSION = 16.1
+PULSEAUDIO_VERSION = 17.0
 PULSEAUDIO_SOURCE = pulseaudio-$(PULSEAUDIO_VERSION).tar.xz
 PULSEAUDIO_SITE = https://freedesktop.org/software/pulseaudio/releases
 PULSEAUDIO_INSTALL_STAGING = YES