1cc8a1b21143714440d1c5c8eeafd8fbf72830ce
Adam Duskett <adam.duskett@amarulasolutions.com>
2024-03-24 15:24:32 -0600

    package/libsepol: bump to version 3.6
    Signed-off-by: Adam Duskett <adam.duskett@amarulasolutions.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libsepol/libsepol.mk
+++ package/libsepol/libsepol.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBSEPOL_VERSION = 3.5
+LIBSEPOL_VERSION = 3.6
 LIBSEPOL_SITE = https://github.com/SELinuxProject/selinux/releases/download/$(LIBSEPOL_VERSION)
 LIBSEPOL_LICENSE = LGPL-2.1+
 LIBSEPOL_LICENSE_FILES = LICENSE
