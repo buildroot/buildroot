c4878d530e8bbcf29173b62a725c66ac51849c65
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-03-24 19:06:45 +0100

    package/log4cxx: bump to version 1.1.0
    - Drop patch (already in version)
    - Drop LOG4CXX_IGNORE_CVES as CVE-2023-31038 has been fixed in 1.1.0
    - Add unixodbc optional dependency
    
    https://logging.apache.org/log4cxx/latest_stable/changelog.html#1.1.0
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/log4cxx/log4cxx.mk
+++ package/log4cxx/log4cxx.mk
@@ -4,21 +4,17 @@
 #
 ################################################################################
 
-LOG4CXX_VERSION = 0.13.0
+LOG4CXX_VERSION = 1.1.0
 LOG4CXX_SITE = https://archive.apache.org/dist/logging/log4cxx/$(LOG4CXX_VERSION)
 LOG4CXX_SOURCE = apache-log4cxx-$(LOG4CXX_VERSION).tar.gz
 LOG4CXX_INSTALL_STAGING = YES
 LOG4CXX_LICENSE = Apache-2.0
 LOG4CXX_LICENSE_FILES = LICENSE
 LOG4CXX_CPE_ID_VENDOR = apache
-# We do not support ODBC functionality
-LOG4CXX_IGNORE_CVES = CVE-2023-31038
 
-# Note: if you want to support odbc, make sure CVE-2023-31038 is fixed
 LOG4CXX_CONF_OPTS = \
 	-DAPR_CONFIG_EXECUTABLE=$(STAGING_DIR)/usr/bin/apr-1-config \
-	-DAPR_UTIL_CONFIG_EXECUTABLE=$(STAGING_DIR)/usr/bin/apu-1-config \
-	-DLOG4CXX_ENABLE_ODBC=OFF
+	-DAPR_UTIL_CONFIG_EXECUTABLE=$(STAGING_DIR)/usr/bin/apu-1-config
 
 LOG4CXX_DEPENDENCIES = apr apr-util
 
@@ -31,6 +27,13 @@ LOG4CXX_CONF_OPTS += -DLOG4CXX_ENABLE_LIBESMTP=ON
 LOG4CXX_DEPENDENCIES += libesmtp
 else
 LOG4CXX_CONF_OPTS += -DLOG4CXX_ENABLE_LIBESMTP=OFF
+endif
+
+ifeq ($(BR2_PACKAGE_UNIXODBC),y)
+LOG4CXX_CONF_OPTS += -DLOG4CXX_ENABLE_ODBC=ON
+LOG4CXX_DEPENDENCIES += unixodbc
+else
+LOG4CXX_CONF_OPTS += -DLOG4CXX_ENABLE_ODBC=OFF
 endif
 
 ifeq ($(BR2_USE_WCHAR),y)
