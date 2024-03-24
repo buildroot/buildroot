bb5cb8955bea6cd127c06ea946cf6825066385fb
Flávio Tapajós <flavio.tapajos@newtesc.com.br>
2024-03-24 13:28:48 -0300

    package/python-sqlalchemy: bump version to 2.0.29
    Signed-off-by: Flávio Tapajós <flavio.tapajos@newtesc.com.br>
    [Arnout: fix check-package error in hash file]
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/python-sqlalchemy/python-sqlalchemy.mk
+++ package/python-sqlalchemy/python-sqlalchemy.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SQLALCHEMY_VERSION = 2.0.25
+PYTHON_SQLALCHEMY_VERSION = 2.0.29
 PYTHON_SQLALCHEMY_SOURCE = SQLAlchemy-$(PYTHON_SQLALCHEMY_VERSION).tar.gz
-PYTHON_SQLALCHEMY_SITE = https://files.pythonhosted.org/packages/7b/bb/85bd8e211f54983e927c7cd9b2ad66773fbef507957156fc72e481a62681
+PYTHON_SQLALCHEMY_SITE = https://files.pythonhosted.org/packages/99/04/59971bfc2f192e3b52376ca8d1e134c78d04bc044ef7e04cf10c42d2ce17
 PYTHON_SQLALCHEMY_SETUP_TYPE = setuptools
 PYTHON_SQLALCHEMY_LICENSE = MIT
 PYTHON_SQLALCHEMY_LICENSE_FILES = LICENSE
