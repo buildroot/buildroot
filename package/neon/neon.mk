motioneye-project/motioneyeos

commit 59a9c02e1355bcd630a207e8be319a044597ad1d

* Added neon

package/neon/neon.mk
@@ -1,0 +1,25 @@
+#############################################################
+#
+# neon
+#
+#############################################################
+NEON_VERSION = 0.28.2
+NEON_SITE = http://www.webdav.org/neon/
+NEON_INSTALL_STAGING = YES
+
+NEON_CONF_OPT+=--enable-shared --without-gssapi --disable-rpath
+
+NEON_INSTALL_TARGET_OPT=DESTDIR=$(TARGET_DIR) install
+
+ifeq ($(strip $(BR2_PACKAGE_NEON_LIBXML2)),y)
+NEON_CONF_OPT+=--with-libxml2=yes
+NEON_CONF_OPT+=--with-expat=no
+NEON_DEPENDENCIES+=libxml2
+endif
+ifeq ($(strip $(BR2_PACKAGE_NEON_EXPAT)),y)
+NEON_CONF_OPT+=--with-expat=$(STAGING_DIR)/usr/lib/libexpat.la
+NEON_CONF_OPT+=--with-libxml2=no
+NEON_DEPENDENCIES+=expat
+endif
+
+$(eval $(call AUTOTARGETS,package,neon))

package/Config.in
@@ -172,6 +172,7 @@ source "package/mrouted/Config.in"
 source "package/mutt/Config.in"
 source "package/nbd/Config.in"
 source "package/ncftp/Config.in"
+source "package/neon/Config.in"
 if !BR2_PACKAGE_BUSYBOX_HIDE_OTHERS
 source "package/netcat/Config.in"
 endif

package/neon/Config.in
@@ -1,0 +1,38 @@
+config BR2_PACKAGE_NEON
+	bool "libneon"
+	default n
+	help
+	  HTTP and WebDAV client library, with a C interface.
+
+	  http://www.webdav.org/neon/
+
+choice
+	prompt "XML Support"
+	default BR2_PACKAGE_NEON_NOXML
+	depends BR2_PACKAGE_NEON
+	help
+	  Select which XML library to use...
+	  expat      use exapt
+	  libxml2    use libxml2
+	  none       do not build with XML support
+
+config BR2_PACKAGE_NEON_EXPAT
+	bool "expat"
+	depends BR2_PACKAGE_EXPAT
+	help
+	  expat      use exapt, a library for parsing XML.
+
+config BR2_PACKAGE_NEON_LIBXML2
+	bool "libxml2"
+	depends BR2_PACKAGE_LIBXML2
+	help
+	  libxml2    use libxml2, a library to read, modify and 
+	             write XML and HTML files.
+
+config BR2_PACKAGE_NEON_NOXML
+	bool "none"
+	help
+	  none       don't use XML, if this is the only option
+	             and you want XML support, you must choose
+		     XML handling below.
+endchoice

buildroot/buildroot

commit f85a1eb173954cfcdda1c0e947913bba598290d8

package/neon: drop unrecognized rpath option
rpath is an unrecognized option since the addition of the package in
commit 59a9c02e1355bcd630a207e8be319a044597ad1d

Fixes: 59a9c02e1355bcd630a207e8be319a044597ad1d

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

package/neon/neon.mk
@@ -10,7 +10,7 @@ NEON_LICENSE = LGPL-2.0+ (library), GPL-2.0+ (manual and tests)
 NEON_LICENSE_FILES = src/COPYING.LIB test/COPYING README.md
 NEON_CPE_ID_VENDOR = webdav
 NEON_INSTALL_STAGING = YES
-NEON_CONF_OPTS = --without-gssapi --disable-rpath
+NEON_CONF_OPTS = --without-gssapi
 NEON_CONFIG_SCRIPTS = neon-config
 NEON_DEPENDENCIES = host-pkgconf $(TARGET_NLS_DEPENDENCIES)
 NEON_CONF_ENV = ne_cv_libsfor_bindtextdomain=$(TARGET_NLS_LIBS)