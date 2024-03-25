e4c3de5b58cdcbd81fa1e61eb6a8e8f82e267819
Marcus Hoffmann <buildroot@bubu1.eu>
2024-03-25 14:06:35 +0100

    package/python-pytest: bump to 8.1.1
    Remove no longer used attrs dependency (already since 7.3.0[1]).
    
    Changelog:
    https://docs.pytest.org/en/stable/changelog.html#pytest-8-1-1-2024-03-08
    
    [1] https://docs.pytest.org/en/stable/changelog.html#pytest-7-3-0-2023-04-08
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-pytest/python-pytest.mk
+++ package/python-pytest/python-pytest.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYTEST_VERSION = 7.4.4
+PYTHON_PYTEST_VERSION = 8.1.1
 PYTHON_PYTEST_SOURCE = pytest-$(PYTHON_PYTEST_VERSION).tar.gz
-PYTHON_PYTEST_SITE = https://files.pythonhosted.org/packages/80/1f/9d8e98e4133ffb16c90f3b405c43e38d3abb715bb5d7a63a5a684f7e46a3
+PYTHON_PYTEST_SITE = https://files.pythonhosted.org/packages/30/b7/7d44bbc04c531dcc753056920e0988032e5871ac674b5a84cb979de6e7af
 PYTHON_PYTEST_SETUP_TYPE = setuptools
 PYTHON_PYTEST_LICENSE = MIT
 PYTHON_PYTEST_LICENSE_FILES = LICENSE
