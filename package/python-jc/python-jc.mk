5067e14191616a8e523d4b96064898774014feb0
Marcus Hoffmann <buildroot@bubu1.eu>
2024-03-25 16:22:47 +0100

    package/python-jc: bump to version 1.25.2
    Changelog:
    https://github.com/kellyjonbrazil/jc/releases/tag/v1.25.2
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-jc/python-jc.mk
+++ package/python-jc/python-jc.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_JC_VERSION = 1.25.1
+PYTHON_JC_VERSION = 1.25.2
 PYTHON_JC_SOURCE = jc-$(PYTHON_JC_VERSION).tar.gz
-PYTHON_JC_SITE = https://files.pythonhosted.org/packages/53/a6/065f0796a0a21bc040bc88c8a33410c12729a2a6f4c269d0349f685796da
+PYTHON_JC_SITE = https://files.pythonhosted.org/packages/39/2e/c0d557b2ee673e2e0aef24a01e732aa232f6b1e180f339058f674f391ab8
 PYTHON_JC_SETUP_TYPE = setuptools
 PYTHON_JC_LICENSE = MIT, BSD-3-Clause (bundled pbPlist)
 PYTHON_JC_LICENSE_FILES = LICENSE.md
