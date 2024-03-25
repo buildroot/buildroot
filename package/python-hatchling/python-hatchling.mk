66e3c7b96d5757bef7223e04bbb9f66b48182066
Marcus Hoffmann <buildroot@bubu1.eu>
2024-03-25 12:51:14 +0100

    package/python-hatchling: bump to version 1.22.4
    Changelog:
    https://hatch.pypa.io/dev/history/hatchling/#hatchling-v1.22.4
    
    Remove now optional editables dependency [1].
    
    [1] https://github.com/pypa/hatch/pull/1255
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-hatchling/python-hatchling.mk
+++ package/python-hatchling/python-hatchling.mk
@@ -4,14 +4,13 @@
 #
 ################################################################################
 
-PYTHON_HATCHLING_VERSION = 1.21.1
+PYTHON_HATCHLING_VERSION = 1.22.4
 PYTHON_HATCHLING_SOURCE = hatchling-$(PYTHON_HATCHLING_VERSION).tar.gz
-PYTHON_HATCHLING_SITE = https://files.pythonhosted.org/packages/d8/a1/7dd1caa87c0b15c04c6291e25112e5d082cce02ee87f221a8be1d594f857
+PYTHON_HATCHLING_SITE = https://files.pythonhosted.org/packages/4f/2a/c34d71531d1e1c9a5029bb73eb3816285befd0fffd7c63ffa0544253dca8
 PYTHON_HATCHLING_LICENSE = MIT
 PYTHON_HATCHLING_LICENSE_FILES = LICENSE.txt
 PYTHON_HATCHLING_SETUP_TYPE = pep517
 HOST_PYTHON_HATCHLING_DEPENDENCIES = \
-	host-python-editables \
 	host-python-packaging \
 	host-python-pathspec \
 	host-python-pluggy \
