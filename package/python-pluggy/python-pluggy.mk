c8f356ad1872b54654610ec5a89124e241b5cb6b
Marcus Hoffmann <buildroot@bubu1.eu>
2024-03-25 13:16:59 +0100

    package/python-pluggy: bump to version 1.4.0
    Changelog:
    https://github.com/pytest-dev/pluggy/blob/main/CHANGELOG.rst#pluggy-140-2024-01-24
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-pluggy/python-pluggy.mk
+++ package/python-pluggy/python-pluggy.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PLUGGY_VERSION = 1.3.0
+PYTHON_PLUGGY_VERSION = 1.4.0
 PYTHON_PLUGGY_SOURCE = pluggy-$(PYTHON_PLUGGY_VERSION).tar.gz
-PYTHON_PLUGGY_SITE = https://files.pythonhosted.org/packages/36/51/04defc761583568cae5fd533abda3d40164cbdcf22dee5b7126ffef68a40
+PYTHON_PLUGGY_SITE = https://files.pythonhosted.org/packages/54/c6/43f9d44d92aed815e781ca25ba8c174257e27253a94630d21be8725a2b59
 PYTHON_PLUGGY_SETUP_TYPE = setuptools
 PYTHON_PLUGGY_LICENSE = MIT
 PYTHON_PLUGGY_LICENSE_FILES = LICENSE
