ea8f0d72b305913d3349c6ab6899f653d68d1820
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-03-28 23:06:05 +0100

    package/mbedtls: security bump to version 2.28.8
    - Use official tar.bz2 tarball
    - Fix CVE-2024-28960
    
    https://github.com/Mbed-TLS/mbedtls-docs/blob/main/security-advisories/mbedtls-security-advisory-2024-03.md
    https://github.com/Mbed-TLS/mbedtls/releases/tag/v2.28.8
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/mbedtls/mbedtls.mk
+++ package/mbedtls/mbedtls.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-MBEDTLS_VERSION = 2.28.7
-MBEDTLS_SITE = $(call github,ARMmbed,mbedtls,v$(MBEDTLS_VERSION))
+MBEDTLS_VERSION = 2.28.8
+MBEDTLS_SITE = https://github.com/Mbed-TLS/mbedtls/releases/download/v$(MBEDTLS_VERSION)
+MBEDTLS_SOURCE = mbedtls-$(MBEDTLS_VERSION).tar.bz2
 MBEDTLS_CONF_OPTS = \
 	-DCMAKE_C_FLAGS="$(TARGET_CFLAGS) -std=c99" \
 	-DENABLE_PROGRAMS=$(if $(BR2_PACKAGE_MBEDTLS_PROGRAMS),ON,OFF) \
