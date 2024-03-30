99e9ff08d0705279532ca0cf3de22ea54660cbdc
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-03-30 10:59:33 +0100

    package/mdadm: bump to version 4.3
    https://lore.kernel.org/all/20240215183844.00003735@linux.intel.com
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/mdadm/mdadm.mk
+++ package/mdadm/mdadm.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MDADM_VERSION = 4.2
+MDADM_VERSION = 4.3
 MDADM_SOURCE = mdadm-$(MDADM_VERSION).tar.xz
 MDADM_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/raid/mdadm
 MDADM_LICENSE = GPL-2.0+
