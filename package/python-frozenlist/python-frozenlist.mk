1dd05a198701b783e2616cd86e04dc041fb99d05
James Hilliard <james.hilliard1@gmail.com>
2024-03-30 18:41:22 -0600

    package/python-frozenlist: migrate to in-tree pep517 build backend
    When building with a pep517 frontend we need to use the specified
    build backend as opposed to the fallback setuptools build which
    only works when not building with a pep517 frontend.
    
    This package currently builds using setuptools as we do not yet
    use setuptools with a pep517 build frontend. The package contains
    a setuptools fallback which only can be used when using setuptools
    without a pep517 frontend as the pep517 frontend will only use the
    build backend specified in the package pyproject.toml which is
    an internal backend and not setuptools.
    
    The custom in tree backend depends on setuptools and expandvars,
    additionally it depends on cython 3 unless disabled.
    
    As we do not currently support cython 3, let's use the pure python
    build instead.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    [Add comment explaining about Cython 3 dependency]
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/python-frozenlist/python-frozenlist.mk
+++ package/python-frozenlist/python-frozenlist.mk
@@ -7,8 +7,13 @@
 PYTHON_FROZENLIST_VERSION = 1.4.1
 PYTHON_FROZENLIST_SOURCE = frozenlist-$(PYTHON_FROZENLIST_VERSION).tar.gz
 PYTHON_FROZENLIST_SITE = https://files.pythonhosted.org/packages/cf/3d/2102257e7acad73efc4a0c306ad3953f68c504c16982bbdfee3ad75d8085
-PYTHON_FROZENLIST_SETUP_TYPE = setuptools
+PYTHON_FROZENLIST_SETUP_TYPE = pep517
 PYTHON_FROZENLIST_LICENSE = Apache-2.0
 PYTHON_FROZENLIST_LICENSE_FILES = LICENSE
+PYTHON_FROZENLIST_DEPENDENCIES = \
+	host-python-expandvars \
+	host-python-setuptools
+# C code generation required Cython 3 which we don't have in Buildroot yet.
+PYTHON_FROZENLIST_BUILD_OPTS = -C=pure-python=true
 
 $(eval $(python-package))
