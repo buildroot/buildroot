1c178b6892a7f54e38cb4edfe63a4c52fd5e3a85
Christian Stewart <christian@aperture.us>
2024-03-31 00:46:13 -0700

    package/docker-engine: security bump to version v26.0.0
    Update to the latest major release of docker-engine.
    
    Fixes CVE-2024-29018: potential data exfiltration from 'internal'
    networks via authoritative DNS servers. Do not forward requests to
    external DNS servers for a container that is only connected to an
    'internal' network.
    
    https://github.com/moby/moby/releases/tag/v26.0.0
    
    Signed-off-by: Christian Stewart <christian@aperture.us>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/docker-engine/docker-engine.mk
+++ package/docker-engine/docker-engine.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DOCKER_ENGINE_VERSION = 24.0.7
+DOCKER_ENGINE_VERSION = 26.0.0
 DOCKER_ENGINE_SITE = $(call github,moby,moby,v$(DOCKER_ENGINE_VERSION))
 
 DOCKER_ENGINE_LICENSE = Apache-2.0
