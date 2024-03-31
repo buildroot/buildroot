bc0e7c275f6041a1c97cf14391e41504b97dcaa5
James Hilliard <james.hilliard1@gmail.com>
2024-03-30 18:41:18 -0600

    package/python-dnspython: migrate to poetry core pep517 build backend
    We need to migrate python-dnspython to the pep517 poetry-core backend
    as setuptools is not supported when building with a pep517 frontend.
    
    This package currently builds using setuptools as we do not yet
    use setuptools with a pep517 build frontend. The package contains
    a setuptools fallback which only can be used when using setuptools
    without a pep517 frontend as the pep517 frontend will only use the
    build backend specified in the package pyproject.toml which is
    poetry-core and not setuptools.
    
    setuptools-scm is not mentioned in pyproject.toml (only in setup.cfg),
    so not needed as a dependency when using poetry-core.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/python-dnspython/python-dnspython.mk
+++ package/python-dnspython/python-dnspython.mk
@@ -9,9 +9,9 @@ PYTHON_DNSPYTHON_SOURCE = dnspython-$(PYTHON_DNSPYTHON_VERSION).tar.gz
 PYTHON_DNSPYTHON_SITE = https://files.pythonhosted.org/packages/65/2d/372a20e52a87b2ba0160997575809806111a72e18aa92738daccceb8d2b9
 PYTHON_DNSPYTHON_LICENSE = ISC
 PYTHON_DNSPYTHON_LICENSE_FILES = LICENSE
-PYTHON_DNSPYTHON_SETUP_TYPE = setuptools
-PYTHON_DNSPYTHON_DEPENDENCIES = host-python-setuptools-scm
-HOST_PYTHON_DNSPYTHON_DEPENDENCIES = host-python-setuptools-scm
+PYTHON_DNSPYTHON_SETUP_TYPE = pep517
+PYTHON_DNSPYTHON_DEPENDENCIES = host-python-poetry-core
+HOST_PYTHON_DNSPYTHON_DEPENDENCIES = host-python-poetry-core
 
 $(eval $(python-package))
 $(eval $(host-python-package))
