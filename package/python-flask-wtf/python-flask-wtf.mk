367104d837f84bb73e8db6b45ce96ee11b378ea3
James Hilliard <james.hilliard1@gmail.com>
2024-03-30 18:41:20 -0600

    package/python-flask-wtf: migrate to hatching pep517 build backend
    When building with a pep517 frontend we need to use the specified
    build backend as opposed to the fallback setuptools build which
    only works when not building with a pep517 frontend.
    
    This package currently builds using setuptools as we do not yet
    use setuptools with a pep517 build frontend. The package contains
    a setuptools fallback which only can be used when using setuptools
    without a pep517 frontend as the pep517 frontend will only use the
    build backend specified in the package pyproject.toml which is
    hatchling and not setuptools.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/python-flask-wtf/python-flask-wtf.mk
+++ package/python-flask-wtf/python-flask-wtf.mk
@@ -9,6 +9,7 @@ PYTHON_FLASK_WTF_SOURCE = flask_wtf-$(PYTHON_FLASK_WTF_VERSION).tar.gz
 PYTHON_FLASK_WTF_SITE = https://files.pythonhosted.org/packages/9b/ef/b6ec35e02f479f6e76e02ede14594c9cfa5e6dcbab6ea0e82fa413993a2a
 PYTHON_FLASK_WTF_LICENSE = BSD-3-Clause
 PYTHON_FLASK_WTF_LICENSE_FILES = LICENSE.rst
-PYTHON_FLASK_WTF_SETUP_TYPE = setuptools
+PYTHON_FLASK_WTF_SETUP_TYPE = pep517
+PYTHON_FLASK_WTF_DEPENDENCIES = host-python-hatchling
 
 $(eval $(python-package))
