d02877f044b5132b6173f4d7b2b0a036b7d83d9f
James Hilliard <james.hilliard1@gmail.com>
2024-03-30 18:41:29 -0600

    package/python-sh: migrate to poetry core pep517 build backend
    We need to migrate python-sh to the pep517 poetry-core
    backend as setuptools is not supported when building with a pep517
    frontend.
    
    This package currently builds using setuptools as we do not yet
    use setuptools with a pep517 build frontend. The package contains
    a setuptools fallback which only can be used when using setuptools
    without a pep517 frontend as the pep517 frontend will only use the
    build backend specified in the package pyproject.toml which is
    poetry-core and not setuptools.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/python-sh/python-sh.mk
+++ package/python-sh/python-sh.mk
@@ -7,8 +7,9 @@
 PYTHON_SH_VERSION = 2.0.6
 PYTHON_SH_SOURCE = sh-$(PYTHON_SH_VERSION).tar.gz
 PYTHON_SH_SITE = https://files.pythonhosted.org/packages/cd/51/7355831d8e1cee8348157d769ccda8a31ca9fa0548e7f93d87837d83866d
-PYTHON_SH_SETUP_TYPE = setuptools
+PYTHON_SH_SETUP_TYPE = pep517
 PYTHON_SH_LICENSE = MIT
 PYTHON_SH_LICENSE_FILES = LICENSE.txt
+PYTHON_SH_DEPENDENCIES = host-python-poetry-core
 
 $(eval $(python-package))
