c1d9e4b9f74e840f6bb805743f3872310369506e
James Hilliard <james.hilliard1@gmail.com>
2024-04-01 15:23:44 -0600

    package/python-aerich: new package
    We need to backport a patch so that the correct poetry build backend
    is used.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

---
+++ package/python-aerich/python-aerich.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# python-aerich
+#
+################################################################################
+
+PYTHON_AERICH_VERSION = 0.7.2
+PYTHON_AERICH_SOURCE = aerich-$(PYTHON_AERICH_VERSION).tar.gz
+PYTHON_AERICH_SITE = https://files.pythonhosted.org/packages/ca/cd/ae9c60ffc21e2d41e22c62cbf24a60dfad937222d880489703842d179746
+PYTHON_AERICH_SETUP_TYPE = pep517
+PYTHON_AERICH_LICENSE = Apache-2.0
+PYTHON_AERICH_LICENSE_FILES = LICENSE
+PYTHON_AERICH_DEPENDENCIES = host-python-poetry-core
+
+$(eval $(python-package))
