9fb4ed3a041eceffdc40139b33de697ca9afa30e
Giulio Benetti <giulio.benetti@benettiengineering.com>
2024-04-01 17:23:12 +0200

    package/esp-hosted: bump version to 2024-04-01
    Add support for Linux 6.9
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/esp-hosted/esp-hosted.mk
+++ package/esp-hosted/esp-hosted.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ESP_HOSTED_VERSION = 6a25417fc880fd744b3b0d93c11659c3e7d86384
+ESP_HOSTED_VERSION = 669fd9cd1892bb8b364701968d5f06225346eea9
 ESP_HOSTED_SITE = $(call github,espressif,esp-hosted,$(ESP_HOSTED_VERSION))
 ESP_HOSTED_DEPENDENCIES = linux
 ESP_HOSTED_LICENSE = GPL-2.0
