buildroot/buildroot

commit d8c440936d51b7f76472b43002780f5490b7a3bf

package/dav1d: bump version to 1.4.1
Release notes:
https://code.videolan.org/videolan/dav1d/-/blob/master/NEWS

Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

package/dav1d/dav1d.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DAV1D_VERSION = 1.3.0
+DAV1D_VERSION = 1.4.1
 DAV1D_SOURCE = dav1d-$(DAV1D_VERSION).tar.xz
 DAV1D_SITE = https://download.videolan.org/pub/videolan/dav1d/$(DAV1D_VERSION)
 DAV1D_LICENSE = BSD-2-Clause

package/dav1d/dav1d.hash
@@ -1,4 +1,4 @@
-# From https://download.videolan.org/pub/videolan/dav1d/1.3.0/dav1d-1.3.0.tar.xz.sha256
-sha256  6d8be2741c505c47f8f1ced3c9cc427759243436553d01d1acce201f87b39e71  dav1d-1.3.0.tar.xz
+# From https://download.videolan.org/pub/videolan/dav1d/1.4.1/dav1d-1.4.1.tar.xz.sha256
+sha256  8d407dd5fe7986413c937b14e67f36aebd06e1fa5cfec679d10e548476f2d5f8  dav1d-1.4.1.tar.xz
 # Locally computed
 sha256  b327887de263238deaa80c34cdd2ff3e0ba1d35db585ce14a37ce3e74ee389e9  COPYING

commit d69e13b0e965961019b4aab38e187267af3cab78

package/dav1d: fix riscv32 build
Fix the following riscv32 build failure raised since bump to version
1.4.1 in commit d8c440936d51b7f76472b43002780f5490b7a3bf and
https://github.com/videolan/dav1d/commit/43ee02a99cce1f221bdb574c9ee484180d71013f:

/home/buildroot/autobuild/run/instance-0/output-1/host/opt/ext-toolchain/bin/../lib/gcc/riscv32-buildroot-linux-gnu/13.2.0/../../../../riscv32-buildroot-linux-gnu/bin/ld: src/libdav1d_bitdepth_8.a.p/itx_tmpl.c.o: in function `.L0 ':
itx_tmpl.c:(.text.unlikely+0x76c): undefined reference to `dav1d_inv_txfm_add_dct_dct_4x4_8bpc_rvv'

Fixes: d8c440936d51b7f76472b43002780f5490b7a3bf
 - http://autobuild.buildroot.org/results/28192c494c7a2f88cc90205b6a08afd11782df40

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

package/dav1d/dav1d.mk
@@ -24,6 +24,11 @@ ifeq ($(BR2_ARM_CPU_ARMV4)$(BR2_ARM_CPU_ARMV5)$(BR2_ARM_CPU_ARMV7M),y)
 DAV1D_CONF_OPTS += -Denable_asm=false
 endif
 
+# riscv assembly requires riscv64
+ifeq ($(BR2_riscv):$(BR2_RISCV_64),y:)
+DAV1D_CONF_OPTS += -Denable_asm=false
+endif
+
 # Uses __atomic_fetch_add_4
 ifeq ($(BR2_TOOLCHAIN_HAS_LIBATOMIC),y)
 DAV1D_LDFLAGS += $(TARGET_LDFLAGS) -latomic