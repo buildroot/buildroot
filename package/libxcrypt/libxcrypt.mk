305abd7a302a7859eabc934eba0ffb85cd3657ff
James Hilliard <james.hilliard1@gmail.com>
2024-04-01 17:40:12 -0600

    package/libxcrypt: support host package build
    This is needed by host-systemd.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    [yann.morin.1998@free.fr: set the host _CONF_OPTS]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/libxcrypt/libxcrypt.mk
+++ package/libxcrypt/libxcrypt.mk
@@ -13,8 +13,11 @@ LIBXCRYPT_AUTORECONF = YES
 
 # Some warnings turn into errors with some sensitive compilers
 LIBXCRYPT_CONF_OPTS = --disable-werror
+HOST_LIBXCRYPT_CONF_OPTS = --disable-werror
 
 # Disable obsolete and unsecure API
 LIBXCRYPT_CONF_OPTS += --disable-obsolete_api
+HOST_LIBXCRYPT_CONF_OPTS += --disable-obsolete_api
 
 $(eval $(autotools-package))
+$(eval $(host-autotools-package))
