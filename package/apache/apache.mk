e0bbfaea0e983c1ce42a9c3085f3ca3dd796852f
Bernd Kuhls <bernd@kuhls.net>
2024-04-04 17:11:15 +0200

    package/apache: security bump version to 2.4.59
    Changelog: https://dlcdn.apache.org/httpd/CHANGES_2.4.59
    
    Fixes CVE-2023-38709, CVE-2024-27316 & CVE-2024-24795.
    
    Removed patch which is included in this release.
    
    Updated _SITE and hash file URLs according to
    https://httpd.apache.org/download.cgi#apache24
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/apache/apache.mk
+++ package/apache/apache.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-APACHE_VERSION = 2.4.58
+APACHE_VERSION = 2.4.59
 APACHE_SOURCE = httpd-$(APACHE_VERSION).tar.bz2
-APACHE_SITE = https://downloads.apache.org/httpd
+APACHE_SITE = https://dlcdn.apache.org/httpd
 APACHE_LICENSE = Apache-2.0
 APACHE_LICENSE_FILES = LICENSE
 APACHE_CPE_ID_VENDOR = apache
