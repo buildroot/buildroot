25ff4015b97b4bb0516a9d8d030dcf0e7cde2a50
Bryan Brattlof <bb@ti.com>
2024-04-05 11:41:01 -0500

    package/arm-gnu-toolchain: bump version to 13.2-rel1
    The arm-gnu-toolchain is used by TI's K3 generation of devices to
    compile the first stage bootloader (tiboot3.bin) which will run on the
    32bit R5 MCU.
    
    The 10.3-2021.10 version of this toolchain is no longer publicized as
    being downloadable (and it's fairly old) on ARM's website. Let's upgrade
    to the latest version of the toolchain ARM (13.2-rel1)
    
    Signed-off-by: Bryan Brattlof <bb@ti.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/arm-gnu-toolchain/arm-gnu-toolchain.mk
+++ package/arm-gnu-toolchain/arm-gnu-toolchain.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-ARM_GNU_TOOLCHAIN_VERSION = 10.3-2021.10
-ARM_GNU_TOOLCHAIN_SITE = https://developer.arm.com/-/media/Files/downloads/gnu-rm/$(ARM_GNU_TOOLCHAIN_VERSION)
-ARM_GNU_TOOLCHAIN_SOURCE = gcc-arm-none-eabi-$(ARM_GNU_TOOLCHAIN_VERSION)-x86_64-linux.tar.bz2
+ARM_GNU_TOOLCHAIN_VERSION = 13.2.rel1
+ARM_GNU_TOOLCHAIN_SITE = https://developer.arm.com/-/media/Files/downloads/gnu/$(ARM_GNU_TOOLCHAIN_VERSION)/binrel
+ARM_GNU_TOOLCHAIN_SOURCE = arm-gnu-toolchain-$(ARM_GNU_TOOLCHAIN_VERSION)-x86_64-arm-none-eabi.tar.xz
 ARM_GNU_TOOLCHAIN_LICENSE = GPL-3.0+
 
 HOST_ARM_GNU_TOOLCHAIN_INSTALL_DIR = $(HOST_DIR)/opt/gcc-arm-none-eabi
