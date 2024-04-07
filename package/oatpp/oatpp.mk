buildroot/buildroot

commit d5bba26801b352f591451cf73e0b7509de0e6850

package/oatpp: new package
Upstream suggests to use it as a static library only, so follow that
principle.

Signed-off-by: Alessandro Partesotti <a.partesotti@gmail.com>
[Arnout:
 - keep DEVELOPERS alphabetical;
 - use oatpp for the prompt;
 - add threads to the toolchain dependencies comment;
 - move comment after the main prompt;
 - rewrap the help text;
 - empty line before upstream URL;
 - hash comment Locally calulated instead of pointing to upstream
   tarball URL;
 - change hash to sha256;
 - add hash for license file;
 - reorder variables in .mk file;
 - use _CONF_OPTS instead of invalid _CMAKE_OPTS.
]
Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

package/oatpp/oatpp.mk
@@ -1,0 +1,16 @@
+################################################################################
+#
+# oatpp
+#
+################################################################################
+
+OATPP_VERSION = 1.3.0
+OATPP_SITE = $(call github,oatpp,oatpp,$(OATPP_VERSION))
+OATPP_LICENSE = Apache-2.0
+OATPP_LICENSE_FILES = LICENSE
+OATPP_CONF_OPTS = -DBUILD_SHARED_LIBS=OFF
+OATPP_INSTALL_STAGING = YES
+# Only builds a static lib and headers
+OATPP_INSTALL_TARGET = NO
+
+$(eval $(cmake-package))

DEVELOPERS
@@ -71,6 +71,9 @@ F:	package/libmbim/
 F:	package/libqmi/
 F:	package/modem-manager/
 
+N:      Alessandro Partesotti <a.partesotti@gmail.com>
+F:      package/oatpp/
+
 N:	Alex Michel <alex.michel@wiedemann-group.com>
 F:	package/libzenoh-pico/
 F:	package/network-manager-openvpn/

package/Config.in
@@ -2009,6 +2009,7 @@ menu "Networking"
 	source "package/nss-mdns/Config.in"
 	source "package/nss-myhostname/Config.in"
 	source "package/nss-pam-ldapd/Config.in"
+	source "package/oatpp/Config.in"
 	source "package/omniorb/Config.in"
 	source "package/open-isns/Config.in"
 	source "package/open62541/Config.in"

package/oatpp/Config.in
@@ -1,0 +1,13 @@
+config BR2_PACKAGE_OATPP
+	bool "oatpp"
+	depends on BR2_INSTALL_LIBSTDCPP
+	depends on BR2_TOOLCHAIN_HAS_THREADS
+	help
+	  Oat++ is an open-source C++ web framework for highly scalable
+	  and resource-efficient web applications. It provides all the
+	  necessary components for production-grade development.
+
+	  https://oatpp.io/docs/start
+
+comment "oatpp needs a toolchain w/ C++, threads"
+	depends on !BR2_INSTALL_LIBSTDCPP || !BR2_TOOLCHAIN_HAS_THREADS

package/oatpp/oatpp.hash
@@ -1,0 +1,3 @@
+# Locally calculated
+sha256  e1f80fa8fd7a74da6737e7fee1a4db68b4d7085a3f40e7d550752d6ff5714583  oatpp-1.3.0.tar.gz
+sha256  c71d239df91726fc519c6eb72d318ec65820627232b2f796219e87dcf35d0ab4  LICENSE

commit 0af98ea2d889dd63bc6934236a625732ce28b059

package/oatpp: disable tests
Fix the following build failure raised since the addition of the package
in commit d5bba26801b352f591451cf73e0b7509de0e6850:

[100%] Linking CXX executable oatppAllTests
/home/buildroot/instance-0/output-1/per-package/oatpp/host/opt/ext-toolchain/m68k-buildroot-uclinux-uclibc/bin/ld.real: cannot find -latomic: No such file or directory

Fixes: d5bba26801b352f591451cf73e0b7509de0e6850
 - http://autobuild.buildroot.org/results/1587b391baecdbb5a24a0c0b2c33d63cb71a2f7f

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

package/oatpp/oatpp.mk
@@ -8,7 +8,7 @@ OATPP_VERSION = 1.3.0
 OATPP_SITE = $(call github,oatpp,oatpp,$(OATPP_VERSION))
 OATPP_LICENSE = Apache-2.0
 OATPP_LICENSE_FILES = LICENSE
-OATPP_CONF_OPTS = -DBUILD_SHARED_LIBS=OFF
+OATPP_CONF_OPTS = -DBUILD_SHARED_LIBS=OFF -DOATPP_BUILD_TESTS=OFF
 OATPP_INSTALL_STAGING = YES
 # Only builds a static lib and headers
 OATPP_INSTALL_TARGET = NO