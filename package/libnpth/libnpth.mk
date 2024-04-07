8773e0aba0bb6778cd8c1dddbce51c3cd4b2069d
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-04-07 11:21:04 +0200

    package/libnpth: fix musl build
    Fix the following musl build failure raised since bump to version 1.7 in
    commit 3ef4d24192b20bd1eb5e34c9fa2a6aa5bde91f91 and
    https://git.gnupg.org/cgi-bin/gitweb.cgi?p=npth.git;a=commit;h=575573b5b63e147b1906240f192dbe37a26ca4b9:
    
    npth.c:392:21: error: unknown type name 'npth_rwlock_t'; did you mean 'npth_cond_t'?
      392 | npth_rwlock_rdlock (npth_rwlock_t *rwlock)
          |                     ^~~~~~~~~~~~~
          |                     npth_cond_t
    
    Fixes: 3ef4d24192b20bd1eb5e34c9fa2a6aa5bde91f91
     - http://autobuild.buildroot.org/results/d986ba27b734679fd70b4e0f23278ebd38e13c5d
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 8c456a564643c49915a8c9ab5a9f0583d3a64ad9)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libnpth/libnpth.mk
+++ package/libnpth/libnpth.mk
@@ -10,5 +10,7 @@ LIBNPTH_SITE = https://www.gnupg.org/ftp/gcrypt/npth
 LIBNPTH_LICENSE = LGPL-2.0+
 LIBNPTH_LICENSE_FILES = COPYING.LIB
 LIBNPTH_INSTALL_STAGING = YES
+# 0001-Fix-INSERT_EXPOSE_RWLOCK_API-for-musl-C-library.patch
+LIBNPTH_AUTORECONF = YES
 
 $(eval $(autotools-package))
