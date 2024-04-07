624fb727f6c91e68c39a27e4f66e2bf8de298e89
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-04-07 14:06:52 +0200

    package/libxcrypt: use official tarball
    Use official tarball and so drop autoreconf
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/libxcrypt/libxcrypt.mk
+++ package/libxcrypt/libxcrypt.mk
@@ -5,11 +5,11 @@
 ################################################################################
 
 LIBXCRYPT_VERSION = 4.4.36
-LIBXCRYPT_SITE = $(call github,besser82,libxcrypt,v$(LIBXCRYPT_VERSION))
+LIBXCRYPT_SITE = https://github.com/besser82/libxcrypt/releases/download/v$(LIBXCRYPT_VERSION)
+LIBXCRYPT_SOURCE = libxcrypt-$(LIBXCRYPT_VERSION).tar.xz
 LIBXCRYPT_LICENSE = LGPL-2.1+
 LIBXCRYPT_LICENSE_FILES = LICENSING COPYING.LIB
 LIBXCRYPT_INSTALL_STAGING = YES
-LIBXCRYPT_AUTORECONF = YES
 
 # Some warnings turn into errors with some sensitive compilers
 LIBXCRYPT_CONF_OPTS = --disable-werror
