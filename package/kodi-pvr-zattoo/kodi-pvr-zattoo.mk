dcc4793173af6f8035e87aa2ee93f048c6a3ef5d
Bernd Kuhls <bernd@kuhls.net>
2024-04-07 13:44:40 +0200

    package/kodi-pvr-*: mass version bump to Omega
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-pvr-zattoo/kodi-pvr-zattoo.mk
+++ package/kodi-pvr-zattoo/kodi-pvr-zattoo.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PVR_ZATTOO_VERSION = 20.3.14-Nexus
+KODI_PVR_ZATTOO_VERSION = 21.0.1-Omega
 KODI_PVR_ZATTOO_SITE = $(call github,rbuehlma,pvr.zattoo,$(KODI_PVR_ZATTOO_VERSION))
 KODI_PVR_ZATTOO_LICENSE = GPL-2.0+
 KODI_PVR_ZATTOO_LICENSE_FILES = LICENSE.md
