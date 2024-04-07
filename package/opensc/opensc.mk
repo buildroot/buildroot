2ebf2a85c0fe64384660def2621e37d7ee04c603
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-04-07 21:29:44 +0200

    package/opensc: openssl is optional, not mandatory
    openssl is optional, not mandatory since the addition of the package in
    commit 8aaa7ecbce1d10cbcd880c3da5e429988f96176a
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>
    (cherry picked from commit b097ed3a4b32c074130c4f4c6b97ce9983fa675a)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/opensc/opensc.mk
+++ package/opensc/opensc.mk
@@ -9,8 +9,15 @@ OPENSC_SITE = https://github.com/OpenSC/OpenSC/releases/download/$(OPENSC_VERSIO
 OPENSC_LICENSE = LGPL-2.1+
 OPENSC_LICENSE_FILES = COPYING
 OPENSC_CPE_ID_VALID = YES
-OPENSC_DEPENDENCIES = openssl pcsc-lite
+OPENSC_DEPENDENCIES = pcsc-lite
 OPENSC_INSTALL_STAGING = YES
 OPENSC_CONF_OPTS = --disable-cmocka --disable-strict --disable-tests
+
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+OPENSC_DEPENDENCIES += openssl
+OPENSC_CONF_OPTS += --enable-openssl
+else
+OPENSC_CONF_OPTS += --disable-openssl
+endif
 
 $(eval $(autotools-package))
