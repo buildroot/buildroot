c5d0d4fad8698a97b696916048b68e08b0aeba89
Bernd Kuhls <bernd@kuhls.net>
2024-04-07 13:44:39 +0200

    package/kodi-peripheral-*: mass version bump to Omega
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-peripheral-xarcade/kodi-peripheral-xarcade.mk
+++ package/kodi-peripheral-xarcade/kodi-peripheral-xarcade.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PERIPHERAL_XARCADE_VERSION = 20.1.3-Nexus
+KODI_PERIPHERAL_XARCADE_VERSION = 21.0.0-Omega
 KODI_PERIPHERAL_XARCADE_SITE = $(call github,kodi-game,peripheral.xarcade,$(KODI_PERIPHERAL_XARCADE_VERSION))
 KODI_PERIPHERAL_XARCADE_LICENSE = GPL-2.0+
 KODI_PERIPHERAL_XARCADE_LICENSE_FILES = LICENSE.md
