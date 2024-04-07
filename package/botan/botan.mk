2fcc74594c457bed7f0f2a4826ada265861be6bf
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-04-07 19:09:36 +0200

    package/botan: security bump to version 3.3.0
    - Fix a potential denial of service caused by accepting arbitrary
      length primes as potential elliptic curve parameters in ASN.1
      encodings. With very large inputs the primality verification
      can become computationally expensive. Now any prime field larger
      than 1024 bits is rejected immediately.
    
    https://botan.randombit.net/news.html#version-3-3-0-2024-02-20
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/botan/botan.mk
+++ package/botan/botan.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BOTAN_VERSION = 3.2.0
+BOTAN_VERSION = 3.3.0
 BOTAN_SOURCE = Botan-$(BOTAN_VERSION).tar.xz
 BOTAN_SITE = http://botan.randombit.net/releases
 BOTAN_LICENSE = BSD-2-Clause
