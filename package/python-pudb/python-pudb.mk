fcfad666dafe66ab4f4f8b63f7ecc0c522894372
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-04-07 12:34:36 +0200

    package/python-pudb: bump to version 2024.1
    This bump will fix the following build failure raised since commit
    8937db8dd595e0988751e5dbb568e870b07b39cc thanks to
    https://github.com/inducer/pudb/commit/8c8f9d4a25e226e77a18d4d938eecf2f969e9b32:
    
    FileExistsError: File already exists: /home/buildroot/instance-0/output-1/target/usr/lib/python3.11/site-packages/test/__init__.py
    
    https://github.com/inducer/pudb/compare/v2023.1...v2024.1
    
    Fixes: 8937db8dd595e0988751e5dbb568e870b07b39cc
     - http://autobuild.buildroot.org/results/0e3b15b9d02dda231f768595cdcff4e75ad71afd
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-pudb/python-pudb.mk
+++ package/python-pudb/python-pudb.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PUDB_VERSION = 2023.1
+PYTHON_PUDB_VERSION = 2024.1
 PYTHON_PUDB_SOURCE = pudb-$(PYTHON_PUDB_VERSION).tar.gz
-PYTHON_PUDB_SITE = https://files.pythonhosted.org/packages/96/d3/a471fe68788178f2a5024031a153673df72b9287530f32b0cd469d64a68a
+PYTHON_PUDB_SITE = https://files.pythonhosted.org/packages/b3/a1/db467e7c828e2ced173d9dbb47abc17ca0e0b3225cb1f4066293e63a2ed9
 PYTHON_PUDB_SETUP_TYPE = setuptools
 PYTHON_PUDB_LICENSE = MIT
 PYTHON_PUDB_LICENSE_FILES = LICENSE
