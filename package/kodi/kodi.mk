c61934611111cb3ea04604707052b1387bde5134
Bernd Kuhls <bernd@kuhls.net>
2024-04-07 13:44:32 +0200

    package/kodi: bump version to 21.0-Omega
    Added new dependency to libdisplay-info for gbm support:
    https://github.com/xbmc/xbmc/commit/ce9626479c1792210c7b7cc02c7dc4f819ace509
    
    Added new required dependency to tinyxml2:
    https://github.com/xbmc/xbmc/commit/9e983ed0449aaf3007d8bfa3261056b120d84188
    Please note that parts of kodi still use tinyxml so both dependencies
    are needed.
    
    Added new required dependency to libudfread: Since upstream commit
    https://github.com/xbmc/xbmc/commit/5f9b9cfa26f274e381e92d73cfa33fb55582436e
    kodi does not build anymore without libudfread.
    
    Upstream removed the bundled groovy/apache commons binaries:
    https://github.com/xbmc/xbmc/commit/d6bc920e056baad7782f47b86cba85d1336bb134
    
    - JsonSchemaBuilder fixes:
    
    Upstream moved CMakeLists.txt to src/ subfolder:
    https://github.com/xbmc/xbmc/commit/7e87d98ca55c72fcbc9b8dadf2cb979a85732606
    
    - TexturePacker fixes:
    
    texturepacker now depends on c++17:
    https://github.com/xbmc/xbmc/commit/54bd6d7ab552a4ddc04d2595826457d8bfa864d5
    
    Since the host version of texturepacker is a build dependency of kodi:
    https://github.com/xbmc/xbmc/blob/Omega/CMakeLists.txt#L187
    https://github.com/xbmc/xbmc/blob/Omega/CMakeLists.txt#L472
    we add a new dependency to BR2_HOST_GCC_AT_LEAST_9.
    
    Upstream moved CMakeLists.txt to src/ subfolder
    https://github.com/xbmc/xbmc/commit/e336a75f42d9c4efd3433f3fa6edd05309689deb
    
    Due to this update we can remove all of our patches for texturepacker.
    
    - Kodi fixes:
    
    Set KODI_SOURCE_DIR variable to root directory of the source tarball.
    When building natively, outside of a meta buildsystem like Buildroot,
    kodi builds its own tools as part of its build process. TexturePacker
    needs cmake modules available from the top of the Kodi source tree, a
    source file in a sub-directory at the top-level source tree, which
    itself needs includes from the same sub-dir. The Kodi build process sets
    KODI_SOURCE_DIR so that TexturePacker can find those files.
    
    In Buildroot, as we cross-build, we build the tools explicitly, without
    using the Kodi build process, and thus we must set KODI_SOURCE_DIR to the
    root directory of the source tarball. LibreELEC added something similar:
    https://github.com/LibreELEC/LibreELEC.tv/commit/70abdd27a22b22547060fcb00a2c70ea61721465
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi/kodi.mk
+++ package/kodi/kodi.mk
@@ -6,8 +6,8 @@
 
 # When updating the version, please also update kodi-jsonschemabuilder
 # and kodi-texturepacker
-KODI_VERSION_MAJOR = 20.5
-KODI_VERSION_NAME = Nexus
+KODI_VERSION_MAJOR = 21.0
+KODI_VERSION_NAME = Omega
 KODI_VERSION = $(KODI_VERSION_MAJOR)-$(KODI_VERSION_NAME)
 KODI_SITE = $(call github,xbmc,xbmc,$(KODI_VERSION))
 KODI_LICENSE = GPL-2.0
@@ -45,6 +45,7 @@ KODI_DEPENDENCIES = \
 	libfribidi \
 	libplist \
 	libpng \
+	libudfread \
 	lzo \
 	openssl \
 	pcre \
@@ -54,6 +55,7 @@ KODI_DEPENDENCIES = \
 	sqlite \
 	taglib \
 	tinyxml \
+	tinyxml2 \
 	zlib
 
 # taken from tools/depends/target/*/*-VERSION
@@ -61,9 +63,20 @@ KODI_LIBDVDCSS_VERSION = 1.4.3-Next-Nexus-Alpha2-2
 KODI_LIBDVDNAV_VERSION = 6.1.1-Next-Nexus-Alpha2-2
 KODI_LIBDVDREAD_VERSION = 6.1.3-Next-Nexus-Alpha2-2
 KODI_EXTRA_DOWNLOADS += \
+	https://groovy.jfrog.io/artifactory/dist-release-local/groovy-zips/apache-groovy-binary-4.0.16.zip \
+	https://dlcdn.apache.org/commons/lang/binaries/commons-lang3-3.14.0-bin.tar.gz \
+	https://dlcdn.apache.org/commons/text/binaries/commons-text-1.11.0-bin.tar.gz \
 	$(call github,xbmc,libdvdcss,$(KODI_LIBDVDCSS_VERSION))/kodi-libdvdcss-$(KODI_LIBDVDCSS_VERSION).tar.gz \
 	$(call github,xbmc,libdvdnav,$(KODI_LIBDVDNAV_VERSION))/kodi-libdvdnav-$(KODI_LIBDVDNAV_VERSION).tar.gz \
 	$(call github,xbmc,libdvdread,$(KODI_LIBDVDREAD_VERSION))/kodi-libdvdread-$(KODI_LIBDVDREAD_VERSION).tar.gz
+
+define KODI_PROVIDE_JAVA_TARBALLS
+	mkdir -p $(@D)/buildroot-build/build/download
+	cp $(KODI_DL_DIR)/apache-groovy-binary-4.0.16.zip $(@D)/buildroot-build/build/download
+	cp $(KODI_DL_DIR)/commons-lang3-3.14.0-bin.tar.gz $(@D)/buildroot-build/build/download
+	cp $(KODI_DL_DIR)/commons-text-1.11.0-bin.tar.gz $(@D)/buildroot-build/build/download
+endef
+KODI_POST_EXTRACT_HOOKS = KODI_PROVIDE_JAVA_TARBALLS
 
 KODI_CONF_OPTS += \
 	-DCMAKE_C_FLAGS="$(TARGET_CFLAGS) $(KODI_C_FLAGS)" \
@@ -104,7 +117,7 @@ endif
 
 ifeq ($(BR2_PACKAGE_KODI_PLATFORM_SUPPORTS_GBM),y)
 KODI_CORE_PLATFORM_NAME += gbm
-KODI_DEPENDENCIES += libgbm libinput libxkbcommon
+KODI_DEPENDENCIES += libdisplay-info libgbm libinput libxkbcommon
 endif
 
 ifeq ($(BR2_PACKAGE_KODI_PLATFORM_SUPPORTS_WAYLAND),y)
@@ -366,12 +379,6 @@ KODI_CONF_OPTS += -DENABLE_PULSEAUDIO=ON
 KODI_DEPENDENCIES += pulseaudio
 else
 KODI_CONF_OPTS += -DENABLE_PULSEAUDIO=OFF
-endif
-
-ifeq ($(BR2_PACKAGE_LIBUDFREAD),y)
-KODI_DEPENDENCIES += libudfread
-else
-KODI_CONF_OPTS += -DENABLE_INTERNAL_UDFREAD=OFF
 endif
 
 # Remove versioncheck addon, updating Kodi is done by building a new
