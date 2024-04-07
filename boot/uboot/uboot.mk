182756a0328d677a215551bfb2b5b22849c2d85e
Romain Naour <romain.naour@smile.fr>
2024-04-07 12:34:56 +0200

    boot/uboot: rename TI_K3_DM option to cover further TI K3 SoCs support
    BR2_TARGET_UBOOT_NEEDS_TI_K3_DM option has been introduced initially to
    support certain TI K3 devices such as AM62x and AM62Ax that require a
    Device Manager (DM) firmware.
    
    This option needs to be renamed to use a more generic name in order
    to cover further TI K3 SoCs support along with the new u-boot binman
    tool that will be added in followup patch. With binman enabled in
    u-boot on TI K3 SoC, ti-k3-boot-firmware is needed at buildtime to
    provide ti-sysfw (System Firmware) for all TI K3 SoC along with Device
    Manager (DM) firmware for AM62x and AM62Ax devices.
    
    With binman support enabled, we don't need to provide the path of the
    Device Manager firmware anymore (see [2]).
    We can remove DM="<DM firmware>.xer5f" from UBOOT_MAKE_OPTS.
    It also means that we can also remove BR2_TARGET_UBOOT_TI_K3_DM_SOCNAME
    used to define DM path.
    
      DM=$(BINARIES_DIR)/ti-dm/$(UBOOT_TI_K3_DM_SOCNAME)/ipc_echo_testb_mcu1_0_release_strip.xer5f
    
    For now, keep BR2_TARGET_UBOOT_TI_K3_DM_SOCNAME and DM only when
    ti-k3-image-gen is used. We need to update ti_am62x_sk_defconfig
    to use binman before removing them along with ti-k3-image-gen.
    
    [1] 52ce606c05ee3ac869f8087292bc3dbb0d25b79b
    [2] https://software-dl.ti.com/processor-sdk-linux/esd/AM62X/09_01_00_08/exports/docs/linux/Foundational_Components/U-Boot/UG-General-Info.html
    
    Signed-off-by: Romain Naour <romain.naour@smile.fr>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- boot/uboot/uboot.mk
+++ boot/uboot/uboot.mk
@@ -194,10 +194,12 @@ UBOOT_DEPENDENCIES += optee-os
 UBOOT_MAKE_OPTS += TEE=$(BINARIES_DIR)/tee.elf
 endif
 
-ifeq ($(BR2_TARGET_UBOOT_NEEDS_TI_K3_DM),y)
+ifeq ($(BR2_TARGET_UBOOT_NEEDS_TI_K3_BOOT_FIRMWARE),y)
 UBOOT_TI_K3_DM_SOCNAME = $(call qstrip,$(BR2_TARGET_UBOOT_TI_K3_DM_SOCNAME))
 UBOOT_DEPENDENCIES += ti-k3-boot-firmware
+ifneq ($(UBOOT_TI_K3_DM_SOCNAME),)
 UBOOT_MAKE_OPTS += DM=$(BINARIES_DIR)/ti-dm/$(UBOOT_TI_K3_DM_SOCNAME)/ipc_echo_testb_mcu1_0_release_strip.xer5f
+endif
 endif
 
 ifeq ($(BR2_TARGET_UBOOT_NEEDS_OPENSBI),y)
