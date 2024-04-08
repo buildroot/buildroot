e6fd72719537989212a7c1e9faef697d76298b0f
Giulio Benetti <giulio.benetti@benettiengineering.com>
2024-04-08 19:10:50 +0200

    package/rtl8821au: bump to version 2024-04-08 branch v5.2.6
    This version allows to build up to Linux version 6.9
    
    Fixes: http://autobuild.buildroot.net/results/3b451b427a8dbe39ed275f91e38e76c2cae9a43a
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/rtl8821au/rtl8821au.mk
+++ package/rtl8821au/rtl8821au.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTL8821AU_VERSION = 0cab870ff635ba038705ab29363b0cbbedd5c258
+RTL8821AU_VERSION = 8ea0ed9da764448c7310a27744ab5814289f35a8
 RTL8821AU_SITE = $(call github,lwfinger,rtl8812au,$(RTL8821AU_VERSION))
 RTL8821AU_LICENSE = GPL-2.0
 RTL8821AU_LICENSE_FILES = LICENSE
