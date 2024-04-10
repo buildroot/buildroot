buildroot/buildroot

commit 8280400fbadde09e51ccff312d7a4ef6a386f589

package/php-xdebug: bump to version 3.2.0
- Use official tarball
- This bump will fix the following build failure raised since bump of
  php to version 8.2.1 in commit
  90ab065162a35b8aa2c429903e3d59d4ad2c0d7f:

  checking Check for supported PHP versions... configure: error: not supported. Need a PHP version >= 7.2.0 and < 8.2.0 (found 8.2.1)

https://xdebug.org/announcements/2022-12-08
https://xdebug.org/updates#x_3_2_0

Fixes:
 - http://autobuild.buildroot.org/results/092a6d388fc6801c109eb46635e8df80d06a803e

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

package/php-xdebug/php-xdebug.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-PHP_XDEBUG_VERSION = 3.0.4
-PHP_XDEBUG_SITE = $(call github,xdebug,xdebug,$(PHP_XDEBUG_VERSION))
+PHP_XDEBUG_VERSION = 3.2.0
+PHP_XDEBUG_SOURCE = xdebug-$(PHP_XDEBUG_VERSION).tgz
+PHP_XDEBUG_SITE = https://xdebug.org/files
 PHP_XDEBUG_INSTALL_STAGING = YES
 PHP_XDEBUG_LICENSE = Xdebug License (PHP-3.0-like)
 PHP_XDEBUG_LICENSE_FILES = LICENSE

package/php-xdebug/php-xdebug.hash
@@ -1,3 +1,3 @@
 # Locally computed
-sha256  7e4f28fc65c8b535de43b6d2ec57429476a6de1d53c4d440a9108ae8d28e01f4  php-xdebug-3.0.4.tar.gz
+sha256  7769b20eecdadf5fbe9f582512c10b394fb575b6f7a8c3a3a82db6883e0032b7  xdebug-3.2.0.tgz
 sha256  ef479ee1a3da3f933e0d046ca8cd0c14601f29b2c0c41cc60c9388546a4e0272  LICENSE

commit 4b7aeee28ed2f62370084dac4a92bdef89046b41

package/php-xdebug: add zlib optional dependency
zlib is an optional dependency (enabled by default) since bump to
version 3.2.0 in commit 8280400fbadde09e51ccff312d7a4ef6a386f589 and
https://github.com/xdebug/xdebug/commit/3954e5cd9e48eec0a8a2a0c6a2fc120bbe562656

Fixes: 8280400fbadde09e51ccff312d7a4ef6a386f589

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/php-xdebug/php-xdebug.mk
@@ -25,4 +25,11 @@ endef
 
 PHP_XDEBUG_PRE_CONFIGURE_HOOKS += PHP_XDEBUG_PHPIZE
 
+ifeq ($(BR2_PACKAGE_ZLIB),y)
+PHP_XDEBUG_CONF_OPTS += --with-xdebug-compression
+PHP_XDEBUG_DEPENDENCIES += zlib
+else
+PHP_XDEBUG_CONF_OPTS += --without-xdebug-compression
+endif
+
 $(eval $(autotools-package))