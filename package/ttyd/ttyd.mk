c681f1dc7d3c6b88621fb77ad34bcbc85558cafe
TIAN Yuanhao <tianyuanhao3@163.com>
2024-04-12 20:39:57 +0200

    package/ttyd: set CPE_ID_VALID
    cpe:2.3:a:ttyd_project:ttyd is a valid CPE identifier for this package:
    
        https://nvd.nist.gov/products/cpe/detail/DBEDA75E-4E19-48C1-92D7-43E4035BC048
    
    Signed-off-by: TIAN Yuanhao <tianyuanhao3@163.com>
    [yann.morin.1998@free.fr: Move to its own patch]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 7d4ba7eaf9ef352edbb120e256a270bbe0e2115d)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ttyd/ttyd.mk
+++ package/ttyd/ttyd.mk
@@ -9,5 +9,6 @@ TTYD_SITE = $(call github,tsl0922,ttyd,$(TTYD_VERSION))
 TTYD_LICENSE = MIT
 TTYD_LICENSE_FILES = LICENSE
 TTYD_DEPENDENCIES = json-c libuv libwebsockets openssl zlib
+TTYD_CPE_ID_VALID = YES
 
 $(eval $(cmake-package))
