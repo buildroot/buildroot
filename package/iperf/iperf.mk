4d9a3f1a32e743fd5bb267442be98b4e0a2d0e14
Julien Olivain <ju.o@free.fr>
2024-04-20 12:52:30 +0200

    package/iperf: bump to version 2.2.0
    For release note, see after the file list at:
    https://sourceforge.net/projects/iperf2/files/
    
    Note: the md5 file hash is also added, because it is published on
    download page.
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/iperf/iperf.mk
+++ package/iperf/iperf.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-IPERF_VERSION = 2.1.9
+IPERF_VERSION = 2.2.0
 IPERF_SITE = https://downloads.sourceforge.net/project/iperf2
 IPERF_LICENSE = MIT-like
 IPERF_LICENSE_FILES = COPYING
