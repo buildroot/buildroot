48275e431f5f3ba3ef1c1290d3b28d5c31821172
Julien Olivain <ju.o@free.fr>
2024-04-20 16:36:17 +0200

    package/fwts: update _SITE url to use https
    The http URL redirects to https. This commit updates this URL to
    directly use https.
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 2aa3a23bbe755ef73af7dfbb65dd8ec60c8c1865)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/fwts/fwts.mk
+++ package/fwts/fwts.mk
@@ -6,7 +6,7 @@
 
 FWTS_VERSION = 23.07.00
 FWTS_SOURCE = fwts-V$(FWTS_VERSION).tar.gz
-FWTS_SITE = http://fwts.ubuntu.com/release
+FWTS_SITE = https://fwts.ubuntu.com/release
 FWTS_STRIP_COMPONENTS = 0
 FWTS_LICENSE = GPL-2.0, LGPL-2.1, Custom
 FWTS_LICENSE_FILES = debian/copyright
