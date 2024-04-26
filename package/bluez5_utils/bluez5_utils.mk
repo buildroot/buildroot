b890206876588ed0d16187abf49d35a45177cc3d
Jan Čermák <sairon@sairon.cz>
2024-04-26 09:14:43 +0200

    package/{bluez5_utils, bluez5_utils-headers}: bump to version 5.75
    Bump to version 5.75 containing various fixes. Added a patch to fix
    build with disabled CUPS (failing since 5.73, waiting to be reviewed and
    merged) and enabled autoreconf which is needed for that patch.
    
    Changelog:
    https://git.kernel.org/pub/scm/bluetooth/bluez.git/tree/ChangeLog
    
    Signed-off-by: Jan Čermák <sairon@sairon.cz>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/bluez5_utils/bluez5_utils.mk
+++ package/bluez5_utils/bluez5_utils.mk
@@ -5,9 +5,11 @@
 ################################################################################
 
 # Keep the version and patches in sync with bluez5_utils-headers
-BLUEZ5_UTILS_VERSION = 5.72
+BLUEZ5_UTILS_VERSION = 5.75
 BLUEZ5_UTILS_SOURCE = bluez-$(BLUEZ5_UTILS_VERSION).tar.xz
 BLUEZ5_UTILS_SITE = $(BR2_KERNEL_MIRROR)/linux/bluetooth
+# 0001-configure.ac-Fix-disable-cups.patch
+BLUEZ5_UTILS_AUTORECONF = YES
 BLUEZ5_UTILS_INSTALL_STAGING = YES
 BLUEZ5_UTILS_LICENSE = GPL-2.0+, LGPL-2.1+
 BLUEZ5_UTILS_LICENSE_FILES = COPYING COPYING.LIB
