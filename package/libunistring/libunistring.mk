0b3662e49218effe0a6bc9b814bb4647869cb5f6
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-04-27 18:18:50 +0200

    package/libunistring: bump to version 1.2
    https://savannah.gnu.org/news/?id=10599
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/libunistring/libunistring.mk
+++ package/libunistring/libunistring.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBUNISTRING_VERSION = 1.1
+LIBUNISTRING_VERSION = 1.2
 LIBUNISTRING_SITE = $(BR2_GNU_MIRROR)/libunistring
 LIBUNISTRING_SOURCE = libunistring-$(LIBUNISTRING_VERSION).tar.xz
 LIBUNISTRING_INSTALL_STAGING = YES
