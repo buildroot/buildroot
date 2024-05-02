ae6d1f7375534351440b2b151721cb9eb8d5bca8
Bryan Brattlof <bb@ti.com>
2024-05-02 11:26:54 -0500

    boot/ti-k3-boot-firmware: bump to version 09.02.00.009
    The 09.02.00.009 release of ti-linux-firmware has the latest updates to
    TI's TIFS, DM and DMSC firmware needed for the K3 generation of
    processors. Update to pull in these latest updates.
    
    Reviewed-by: Heiko Thiery <heiko.thiery@gmail.com>
    Tested-by: Andreas Dannenberg <dannenberg@ti.com>
    Signed-off-by: Bryan Brattlof <bb@ti.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- boot/ti-k3-boot-firmware/ti-k3-boot-firmware.mk
+++ boot/ti-k3-boot-firmware/ti-k3-boot-firmware.mk
@@ -4,9 +4,7 @@
 #
 ################################################################################
 
-# The hash 340194800a581baf976360386dfc7b5acab8d948 defined in the
-# Makefile of ti-k3-image-gen corresponds to the tag 08.06.00.006.
-TI_K3_BOOT_FIRMWARE_VERSION = 08.06.00.006
+TI_K3_BOOT_FIRMWARE_VERSION = 09.02.00.009
 TI_K3_BOOT_FIRMWARE_SITE = https://git.ti.com/cgit/processor-firmware/ti-linux-firmware/snapshot
 TI_K3_BOOT_FIRMWARE_SOURCE = ti-linux-firmware-$(TI_K3_BOOT_FIRMWARE_VERSION).tar.xz
 TI_K3_BOOT_FIRMWARE_INSTALL_IMAGES = YES
