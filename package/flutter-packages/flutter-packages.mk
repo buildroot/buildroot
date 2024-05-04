4b5e1585961f5ceb0522246532c53844ce0a987c
Yann E. MORIN <yann.morin.1998@free.fr>
2024-05-04 23:40:04 +0200

    package/flutter-packages: fix and comment the shared _SOURCE variable
    Commit 631647f4a7c5 (package/flutter-packages/flutter-markdown-example:
    new package) introduced a set of flutter packages, all sharing the same
    upstream location and sources, and thus introduced a set of shared
    variables (not unlike the qt5, qt6, and a few other similar packages).
    
    Especially, it introduced the corresponding _SOURCE variable, that is
    referenced by each sub-package of flutter-packages. Defining this
    variable is required, because flutter-packages itself is not a package
    in Buildroot parlance: it does not call any of the *-package macro. As
    such, the default _SOURCE variable is not automatically generated.
    
    The value for the variable was suffixed with the -br1 version-suffix as
    used for the archives generated by the git backend.
    
    However, this archive is not generated with our git download backend,
    but is generated remotely by github, as the _SITE is computed with our
    github helper macro.
    
    So, the -br1 suffix is both superfluous and confusing.
    
    Drop the suffix to avoid any confusion in the future, and add a little
    blurb explaining the situation close to where the variable is set, and
    add a check-package disable line.
    
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    Cc: Adam Duskett <adam.duskett@amarulasolutions.com>
    [Arnout: use check-package disable comment instead of ignoring the file]
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/flutter-packages/flutter-packages.mk
+++ package/flutter-packages/flutter-packages.mk
@@ -9,7 +9,14 @@ FLUTTER_PACKAGES_SITE = $(call github,flutter,packages,$(FLUTTER_PACKAGES_VERSIO
 FLUTTER_PACKAGES_LICENSE = BSD-3-Clause
 FLUTTER_PACKAGES_LICENSE_FILES = LICENSE
 FLUTTER_PACKAGES_DL_SUBDIR = flutter-packages
-FLUTTER_PACKAGES_SOURCE = flutter-packages-$(FLUTTER_PACKAGES_VERSION)-br1.tar.gz
+
+# This default value *is* required, because this file is not a package (it
+# does not call any of the *-package macros), so the _SOURCE variable is not
+# defined. However, we need a variable for the sub-packages to share the
+# same tarball name.
+# check-package disable lib_mk.RemoveDefaultPackageSourceVariable
+FLUTTER_PACKAGES_SOURCE = flutter-packages-$(FLUTTER_PACKAGES_VERSION).tar.gz
+
 FLUTTER_PACKAGES_DEPENDENCIES = \
 	host-flutter-sdk-bin \
 	flutter-engine
