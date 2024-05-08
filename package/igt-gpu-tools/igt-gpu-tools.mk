fa94135455db294bc33807a4217ad4f705c464af
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-05-08 10:49:18 +0200

    package/igt-gpu-tools: disable chamelium
    Disable chamelium to avoid the following build failure if gsl is found
    but not alsa-lib raised since the addition of the package in commit
    d8a729d1731339da141612be33c5e59bc9928748:
    
    In file included from ../lib/igt_audio.c:35:
    ../lib/igt_audio.h:35:10: fatal error: alsa/asoundlib.h: No such file or directory
       35 | #include <alsa/asoundlib.h>
          |          ^~~~~~~~~~~~~~~~~~
    
    Fixes: d8a729d1731339da141612be33c5e59bc9928748
     - http://autobuild.buildroot.org/results/a7818af881d9aafa9a36f87dc7c882a6880554e8
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/igt-gpu-tools/igt-gpu-tools.mk
+++ package/igt-gpu-tools/igt-gpu-tools.mk
@@ -23,7 +23,9 @@ IGT_GPU_TOOLS_DEPENDENCIES = \
 	udev \
 	zlib
 
-IGT_GPU_TOOLS_CONF_OPTS = -Dtests=disabled
+IGT_GPU_TOOLS_CONF_OPTS = \
+	-Dchamelium=disabled \
+	-Dtests=disabled
 
 # On x86 systems, libigt resolves igt_half_to_float and igt_float_to_half as
 # indirect functions at runtime by checking CPU features with igt_x86_features.
