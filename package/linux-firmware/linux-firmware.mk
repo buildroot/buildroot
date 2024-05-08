buildroot/buildroot

commit 9b25246e9b731f167454ba4f8182eed1926f50a6

package/linux-firmware: Add Intel AtomISP v2 firmwares
Many of Intel based tablets (and all phones that were designed with
Intel SoCs) have Intel AtomISP v2 engine. Add an entry for recently
added firmwares for them.

Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/linux-firmware/linux-firmware.mk
@@ -846,6 +846,12 @@ LINUX_FIRMWARE_FILES += v4l-cx23885-avcore-01.fw
 # which is installed unconditionally
 endif
 
+ifeq ($(BR2_PACKAGE_LINUX_FIRMWARE_INTEL_ATOMISP),y)
+LINUX_FIRMWARE_FILES += intel/ipu/shisp_2400b0_v21.bin
+LINUX_FIRMWARE_FILES += intel/ipu/shisp_2401a0_v21.bin
+LINUX_FIRMWARE_ALL_LICENSE_FILES += LICENCE.ivsc
+endif
+
 ifneq ($(LINUX_FIRMWARE_FILES)$(LINUX_FIRMWARE_DIRS),)
 
 define LINUX_FIRMWARE_BUILD_CMDS

package/linux-firmware/Config.in
@@ -637,6 +637,12 @@ config BR2_PACKAGE_LINUX_FIRMWARE_CX23885
 	  Firmware files for Conexant Cx23885/7/8 PCIe broadcast
 	  A/V decoder needed by the Linux CX23885 driver.
 
+config BR2_PACKAGE_LINUX_FIRMWARE_INTEL_ATOMISP
+	bool "Intel AtomISP v2"
+	help
+	  Firmware files for Intel AtomISP v2 (also know as IPU2)
+	  needed by the Linux AtomISP v2 driver.
+
 endmenu
 
 endif

commit 121c5f7b422bf393f51e0e873e7ceaef55b0d3d3

package/linux-firmware: fix legal info
Commit 9b25246e9b731f167454ba4f8182eed1926f50a6 made a typo in license
filename and forgot to add its hash:

ERROR: No hash found for LICENCE.ivsc

Fixes: 9b25246e9b731f167454ba4f8182eed1926f50a6
 - http://autobuild.buildroot.org/results/144ce581645d78a2d27a59a63130977d0f549db8

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/linux-firmware/linux-firmware.hash
@@ -16,6 +16,7 @@ sha256  60fbc9cccb455e1a3306c97db942d6f24fa93664be61d54c497637e6d0e2ae83  LICENC
 sha256  5181b0b51efc79d5acb2c9bb92042878fdbad97a92114d4ab5e32e2b5b52fce4  LICENCE.ibt_firmware
 sha256  8c45a83562a69f7ac4207e4cb5c0063af2e545791b3947bc81dc82305481c15c  LICENSE.ice_enhanced
 sha256  0e0c11073ba3c832097da38e0905da36b8a3526f219407977b13b71c6675be7d  LICENCE.it913x
+sha256  646957007887aed50d56c1f4fd9c6ef760d5b96d0a03577bda3492206dba1d05  LICENSE.ivsc
 sha256  16d5040c7cf851fc693b7542e20870935b99802533ea1bfd231c377a2305e5c5  LICENCE.iwlwifi_firmware
 sha256  2d6062d63b91eb750bf741498691604f75184b9fee97608ec537cd09bd6a42b4  LICENCE.Marvell
 sha256  a90d3f66704d85889945fec5525ea77622549da83aced1aac99828383f8f1805  LICENCE.mediatek

package/linux-firmware/linux-firmware.mk
@@ -849,7 +849,7 @@ endif
 ifeq ($(BR2_PACKAGE_LINUX_FIRMWARE_INTEL_ATOMISP),y)
 LINUX_FIRMWARE_FILES += intel/ipu/shisp_2400b0_v21.bin
 LINUX_FIRMWARE_FILES += intel/ipu/shisp_2401a0_v21.bin
-LINUX_FIRMWARE_ALL_LICENSE_FILES += LICENCE.ivsc
+LINUX_FIRMWARE_ALL_LICENSE_FILES += LICENSE.ivsc
 endif
 
 ifneq ($(LINUX_FIRMWARE_FILES)$(LINUX_FIRMWARE_DIRS),)