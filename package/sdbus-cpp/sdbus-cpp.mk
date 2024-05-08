buildroot/buildroot

commit 0f5bb364c629dd7fc91f4295f872e46d89c0feef

package/sdbus-cpp: bump to version 2.0.0
Changelog:
https://github.com/Kistler-Group/sdbus-cpp/releases/tag/v2.0.0

Signed-off-by: Sergey Bobrenok <bobrofon@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/sdbus-cpp/sdbus-cpp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SDBUS_CPP_VERSION = 1.5.0
+SDBUS_CPP_VERSION = 2.0.0
 SDBUS_CPP_SITE = $(call github,Kistler-Group,sdbus-cpp,v$(SDBUS_CPP_VERSION))
 SDBUS_CPP_INSTALL_STAGING = YES
 SDBUS_CPP_DEPENDENCIES = host-pkgconf systemd

package/sdbus-cpp/sdbus-cpp.hash
@@ -1,4 +1,4 @@
 # Locally computed:
-sha256  577986929f911320fb9ef6a3e2badd464dc38411ebc25d2966f5cb85c39f0897  sdbus-cpp-1.5.0.tar.gz
+sha256  88af4569161a0d0192f0f4a94582a1af4e75722499d06984fb7f91f638f5afb3  sdbus-cpp-2.0.0.tar.gz
 sha256  20c17d8b8c48a600800dfd14f95d5cb9ff47066a9641ddeab48dc54aec96e331  COPYING
 sha256  a1c9e75e25d8f2ce18017c88978edab2f0dbc7814ad0697d4ff2e5e59959f657  COPYING-LGPL-Exception

commit faf371a476c3125f2d2a3f50da89d9d372dfd68e

package/sdbus-cpp: fix host build
cmake options were renamed by
https://github.com/Kistler-Group/sdbus-cpp/commit/0074c79e7fd7983dda3c2a4e4ac7febe67751a28
resulting in the following host build failure since bump to version
2.0.0 in commit 0f5bb364c629dd7fc91f4295f872e46d89c0feef:

/home/buildroot/autobuild/run/instance-1/output-1/build/host-sdbus-cpp-2.0.0/src/Proxy.cpp: In member function 'virtual sdbus::PendingAsyncCall sdbus::internal::Proxy::callMethodAsync(const sdbus::MethodCall&, sdbus::async_reply_handler, uint64_t)':
/home/buildroot/autobuild/run/instance-1/output-1/build/host-sdbus-cpp-2.0.0/src/Proxy.cpp:125:91: sorry, unimplemented: non-trivial designated initializers not supported
                                                                       , .floating = false });
                                                                                           ^
/home/buildroot/autobuild/run/instance-1/output-1/build/host-sdbus-cpp-2.0.0/src/Proxy.cpp:133:60: error: class template argument deduction failed:
     auto asyncCallInfoWeakPtr = std::weak_ptr{asyncCallInfo};
                                                            ^

Fixes: 0f5bb364c629dd7fc91f4295f872e46d89c0feef
 - http://autobuild.buildroot.org/results/66668261d05203575658a243e02e78cf77018d8c

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/sdbus-cpp/sdbus-cpp.mk
@@ -14,10 +14,10 @@ SDBUS_CPP_LICENSE_FILES = COPYING COPYING-LGPL-Exception
 # Host build for sdbus-c++-xml2cpp
 HOST_SDBUS_CPP_DEPENDENCIES = host-pkgconf host-systemd
 HOST_SDBUS_CPP_CONF_OPTS = \
-	-DBUILD_CODE_GEN=ON \
-	-DBUILD_DOC=OFF \
-	-DBUILD_TESTS=OFF \
-	-DBUILD_LIBSYSTEMD=OFF
+	-DSDBUSCPP_BUILD_CODEGEN=ON \
+	-DSDBUSCPP_BUILD_DOCS=OFF \
+	-DSDBUSCPP_BUILD_TESTS=OFF \
+	-DSDBUSCPP_BUILD_LIBSYSTEMD=OFF
 
 $(eval $(cmake-package))
 $(eval $(host-cmake-package))