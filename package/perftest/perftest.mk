a70158eb1cb43e68d5ef6c7517d37d945f2c3a88
Julien Olivain <ju.o@free.fr>
2024-05-08 17:57:42 +0200

    package/perftest: bump to version 24.04.0-0.41
    For change log since 23.10.0-0.29, see:
    https://github.com/linux-rdma/perftest/releases/tag/24.01.0-0.38
    https://github.com/linux-rdma/perftest/releases/tag/24.04.0-0.41
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/perftest/perftest.mk
+++ package/perftest/perftest.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PERFTEST_VERSION = 23.10.0-0.29
+PERFTEST_VERSION = 24.04.0-0.41
 PERFTEST_SITE = $(call github,linux-rdma,perftest,$(PERFTEST_VERSION))
 PERFTEST_LICENSE = GPL-2.0 or BSD-2-Clause
 PERFTEST_LICENSE_FILES = COPYING
