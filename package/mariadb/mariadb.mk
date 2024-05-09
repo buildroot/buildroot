buildroot/buildroot

commit 821f9dee8112dc02fe58d60273d0dfdabbc46d1f

package/mariadb: needs fmt
Buildroot commit 88c7359f65381d49fa497f9e33bf54bb31a14e6c bumped mariadb
from version 10.3.36 to 10.11.4.

Upstream added fmt as dependency in version 10.7.1 with commit
https://github.com/MariaDB/server/commit/e214e60201216b2e6330f86a84fec8ae14aa2887

Fixes:
http://autobuild.buildroot.net/results/ce1/ce140a3923bc2f85b176fafcc38c6f68b177d33a/

Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

package/mariadb/mariadb.mk
@@ -18,11 +18,15 @@ MARIADB_CONFIG_SCRIPTS = mysql_config
 
 MARIADB_DEPENDENCIES = \
 	host-mariadb \
+	fmt \
 	ncurses \
 	openssl \
 	zlib \
 	libaio \
 	libxml2
+
+MARIADB_CONF_OPTS += \
+	-DWITH_FMT=system
 
 # use bundled GPL-2.0+ licensed readline as package/readline is GPL-3.0+
 MARIADB_CONF_OPTS += -DWITH_READLINE=ON

package/mysql/Config.in
@@ -16,10 +16,13 @@ choice
 
 config BR2_PACKAGE_MARIADB
 	bool "mariadb"
+	depends on BR2_INSTALL_LIBSTDCPP # fmt
 	depends on !BR2_STATIC_LIBS
 	depends on BR2_TOOLCHAIN_HAS_ATOMIC || BR2_TOOLCHAIN_HAS_SYNC_8
+	depends on BR2_USE_WCHAR # fmt
 	select BR2_PACKAGE_LIBAIO
 	select BR2_PACKAGE_LIBXML2
+	select BR2_PACKAGE_FMT
 	select BR2_PACKAGE_NCURSES
 	select BR2_PACKAGE_OPENSSL
 	select BR2_PACKAGE_HAS_MYSQL
@@ -30,8 +33,9 @@ config BR2_PACKAGE_MARIADB
 
 	  http://www.mariadb.org/
 
-comment "mariadb needs a toolchain w/ dynamic library"
-	depends on BR2_STATIC_LIBS
+comment "mariadb needs a toolchain w/ dynamic library, C++, wchar"
+	depends on BR2_STATIC_LIBS || !BR2_INSTALL_LIBSTDCPP \
+		|| !BR2_USE_WCHAR
 	depends on BR2_TOOLCHAIN_HAS_ATOMIC || BR2_TOOLCHAIN_HAS_SYNC_8
 
 config BR2_PACKAGE_ORACLE_MYSQL

commit 2cf2491a23e70dd38f037726551ed6df7a9ac340

package/mariadb: really use system fmt
Commit 821f9dee8112dc02fe58d60273d0dfdabbc46d1f wrongly used WITH_FMT
instead of WITH_LIBFMT resulting in the following build failure:

[  0%] Creating directories for 'libfmt'
[  0%] Performing download step (download, verify and extract) for 'libfmt'
make[3]: Leaving directory '/home/buildroot/autobuild/run/instance-0/output-1/build/mariadb-10.11.6'
make[3]: Leaving directory '/home/buildroot/autobuild/run/instance-0/output-1/build/mariadb-10.11.6'
-- Downloading...
   dst='/home/buildroot/autobuild/run/instance-0/output-1/build/mariadb-10.11.6/extra/libfmt/src/8.0.1.zip'
   timeout='none'
   inactivity timeout='none'
-- Using src='https://github.com/fmtlib/fmt/archive/refs/tags/8.0.1.zip'
CMake Error at libfmt-stamp/download-libfmt.cmake:170 (message):
  Each download failed!

    error: downloading 'https://github.com/fmtlib/fmt/archive/refs/tags/8.0.1.zip' failed
          status_code: 1
          status_string: "Unsupported protocol"
          log:
          --- LOG BEGIN ---
          Protocol "https" not supported or disabled in libcurl

  Closing connection

          --- LOG END ---

Fixes: 821f9dee8112dc02fe58d60273d0dfdabbc46d1f
 - http://autobuild.buildroot.org/results/fbe3bc347a83a810ffbbbff93a45994963712bee

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/mariadb/mariadb.mk
@@ -26,7 +26,7 @@ MARIADB_DEPENDENCIES = \
 	libxml2
 
 MARIADB_CONF_OPTS += \
-	-DWITH_FMT=system \
+	-DWITH_LIBFMT=system \
 	-DWITH_PCRE=system
 
 # use bundled GPL-2.0+ licensed readline as package/readline is GPL-3.0+