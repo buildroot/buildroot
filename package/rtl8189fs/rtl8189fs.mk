buildroot/buildroot

commit 25956b29e4d63f599b9baf7298f69538ba2f22fa

package/rtl8189fs: bump revision
Bump package revision to pull various fixes for v5.1 - v5.17 kernels.
Patch that disables verbose debug is no more compatible with updated
driver since build flags in Makefile has been changed. Drop it.

Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>
Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

package/rtl8189fs/rtl8189fs.hash
@@ -1,2 +1,2 @@
 # Locally calculated
-sha256  fd74248b8cc62e54a954502228be221ace6051832a972b8b3005cc05e9c87761  rtl8189fs-73f826f0fa74b2fc1ad67eaf92bf9787947ae673.tar.gz
+sha256  41eee18d8a4289c2eac07af3224c62214a9b694a717bf497cd34a95b955f3d49  rtl8189fs-94a61cbf46ce87b7c9b8aa53123aeb142133a3c9.tar.gz

package/rtl8189fs/rtl8189fs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTL8189FS_VERSION = 73f826f0fa74b2fc1ad67eaf92bf9787947ae673
+RTL8189FS_VERSION = 94a61cbf46ce87b7c9b8aa53123aeb142133a3c9
 RTL8189FS_SITE = $(call github,jwrdegoede,rtl8189ES_linux,$(RTL8189FS_VERSION))
 RTL8189FS_LICENSE = GPL-2.0
 

package/rtl8189fs/0001-include-disable-debug-messages.patch
@@ -1,28 +1,0 @@
-From 5d3a44ff1aeb8ace6e08df3cad16102aaac1ef09 Mon Sep 17 00:00:00 2001
-From: Sergey Matyukevich <geomatsi@gmail.com>
-Date: Sun, 13 Jun 2021 15:51:51 +0300
-Subject: [PATCH] include: disable debug messages
-
-Disable debug messages to make rtl8189fs driver less noisy.
-
-Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>
----
- include/autoconf.h | 2 --
- 1 file changed, 2 deletions(-)
-
-diff --git a/include/autoconf.h b/include/autoconf.h
-index 44efe42..b5df57b 100644
---- a/include/autoconf.h
-+++ b/include/autoconf.h
-@@ -224,8 +224,6 @@
- /*
-  * Debug Related Config
-  */
--#define CONFIG_DEBUG /* DBG_871X, etc... */
--
- #ifdef CONFIG_DEBUG
- #define DBG	1	// for ODM & BTCOEX debug
- //#define CONFIG_DEBUG_RTL871X /* RT_TRACE, RT_PRINT_DATA, _func_enter_, _func_exit_ */
--- 
-2.32.0
-

commit 0dd00cbce5d9d2eb4f5232080459ba32416192ef

package/rtl8189fs: bump to latest git commit
Bump to latest git commit to fix the following powerpc64 build failure
raised at least since commit 25956b29e4d63f599b9baf7298f69538ba2f22fa
thanks to
https://github.com/jwrdegoede/rtl8189ES_linux/commit/c7bf7590bc0d7252005fd0496898ad420a85e1ae:

In file included from /home/autobuild/autobuild/instance-7/output-1/build/rtl8189fs-5d523593f41c0b8d723c6aa86b217ee1d0965786/./include/drv_types.h:30,
                 from /home/autobuild/autobuild/instance-7/output-1/build/rtl8189fs-5d523593f41c0b8d723c6aa86b217ee1d0965786/./core/rtw_cmd.c:17:
/home/autobuild/autobuild/instance-7/output-1/build/rtl8189fs-5d523593f41c0b8d723c6aa86b217ee1d0965786/./include/wifi.h:459:32: error: conflicting types for 'get_ra'; have 'unsigned char *(unsigned char *)'
  459 | __inline static unsigned char *get_ra(unsigned char *pframe)
      |                                ^~~~~~

Fixes: 25956b29e4d63f599b9baf7298f69538ba2f22fa
 - http://autobuild.buildroot.org/results/83153ed38a583ce2712df9b142ac3fc55a8413e3
 - http://autobuild.buildroot.org/results/63186c7adb5ecc8b8a16a6ae641ecfb9c03478bd

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/rtl8189fs/rtl8189fs.hash
@@ -1,2 +1,2 @@
 # Locally calculated
-sha256  c92e1d800258665835922c1e927708f7a0477a1cbb281dc98ba5d33d5e3271ee  rtl8189fs-5d523593f41c0b8d723c6aa86b217ee1d0965786.tar.gz
+sha256  b28c15f4631f4151ea6fca96d59aec931766af49ccb7a0d5e1080fc41a323713  rtl8189fs-eb51e021b0e1b6f94a4b49da3f4ee5c5fb20b715.tar.gz

package/rtl8189fs/rtl8189fs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTL8189FS_VERSION = 5d523593f41c0b8d723c6aa86b217ee1d0965786
+RTL8189FS_VERSION = eb51e021b0e1b6f94a4b49da3f4ee5c5fb20b715
 RTL8189FS_SITE = $(call github,jwrdegoede,rtl8189ES_linux,$(RTL8189FS_VERSION))
 RTL8189FS_LICENSE = GPL-2.0
 