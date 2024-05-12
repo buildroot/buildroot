buildroot/buildroot

commit de6415ad9c35b2013af1f1821bcac12ba3959674

package/dmraid: fix musl build
dmraid is a very old package from 2010 with an awkward configure script
which doesn't honor CFLAGS:

 ac_cv_env_CFLAGS_set=set
 ac_cv_env_CFLAGS_value='-D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64  -Os -g0 '

[...]

 ## ----------------- ##
 ## Output variables. ##
 ## ----------------- ##

 AWK='gawk'
 CC='/home/autobuild/autobuild/instance-3/output-1/host/bin/sh4a-buildroot-linux-musl-gcc'
 CFLAGS='-O2'

resulting in the following build failure with musl >= 1.2.4 and
https://github.com/bminor/musl/commit/25e6fee27f4a293728dd15b659170e7b9c7db9bc
because _LARGEFILE64_SOURCE is not set:

/home/autobuild/autobuild/instance-3/output-1/host/lib/gcc/sh4a-buildroot-linux-musl/12.3.0/../../../../sh4a-buildroot-linux-musl/bin/ld: misc/file.o: in function `rw_file':
file.c:(.text+0x150): undefined reference to `lseek64'

To fix this issue, pass TARGET_CONFIGURE_OPTS through DMRAID_MAKE_OPTS

Fixes:
 - http://autobuild.buildroot.org/results/5c7c82959ce92db908d3ca20e2c1137509c2f981

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

package/dmraid/dmraid.mk
@@ -13,6 +13,7 @@ DMRAID_MAKE = $(MAKE1)
 DMRAID_INSTALL_STAGING = YES
 DMRAID_LICENSE = GPL-2.0
 DMRAID_LICENSE_FILES = $(DMRAID_SUBDIR)/LICENSE_GPL $(DMRAID_SUBDIR)/LICENSE
+DMRAID_MAKE_OPTS = $(TARGET_CONFIGURE_OPTS)
 
 DMRAID_DEPENDENCIES = lvm2
 

commit 880066bad68bf881cc0636cd51c177625d4fa837

package/dmraid: set -fPIC
Add -fPIC to CFLAGS to fix the following build failure raised since
commit de6415ad9c35b2013af1f1821bcac12ba3959674:

/home/autobuild/autobuild/instance-5/output-1/host/lib/gcc/x86_64-buildroot-linux-gnu/12.3.0/../../../../x86_64-buildroot-linux-gnu/bin/ld: misc/misc.o: warning: relocation against `stdout@@GLIBC_2.2.5' in read-only section `.text'
/home/autobuild/autobuild/instance-5/output-1/host/lib/gcc/x86_64-buildroot-linux-gnu/12.3.0/../../../../x86_64-buildroot-linux-gnu/bin/ld: log/log.o: relocation R_X86_64_PC32 against symbol `stdout@@GLIBC_2.2.5' can not be used when making a shared object; recompile with -fPIC

Fixes: de6415ad9c35b2013af1f1821bcac12ba3959674
 - http://autobuild.buildroot.org/results/0349c6bfd66f5e50429e4a5cc07fb7abf2b07345

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/dmraid/dmraid.mk
@@ -13,7 +13,7 @@ DMRAID_MAKE = $(MAKE1)
 DMRAID_INSTALL_STAGING = YES
 DMRAID_LICENSE = GPL-2.0
 DMRAID_LICENSE_FILES = $(DMRAID_SUBDIR)/LICENSE_GPL $(DMRAID_SUBDIR)/LICENSE
-DMRAID_MAKE_OPTS = $(TARGET_CONFIGURE_OPTS)
+DMRAID_MAKE_OPTS = $(TARGET_CONFIGURE_OPTS) CFLAGS="$(TARGET_CFLAGS) -fPIC"
 
 DMRAID_DEPENDENCIES = lvm2
 