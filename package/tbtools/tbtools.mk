ebe64a69110928c69d1573281f99b8a3204e9c35
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-05-12 09:47:53 +0200

    package/tbtools: add hashes
    Commit 8d5b3aac435880333833d4a3909b1818fe8af045 forgot to add a hash
    file. Moreover, also update TBTOOLS_VERSION to match what is returned by
    https://release-monitoring.org/project/372327
    
    Fixes: 8d5b3aac435880333833d4a3909b1818fe8af045
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/tbtools/tbtools.mk
+++ package/tbtools/tbtools.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-TBTOOLS_VERSION = v0.3.2
-TBTOOLS_SITE = $(call github,intel,tbtools,$(TBTOOLS_VERSION))
+TBTOOLS_VERSION = 0.3.2
+TBTOOLS_SITE = $(call github,intel,tbtools,v$(TBTOOLS_VERSION))
 TBTOOLS_LICENSE = MIT
 TBTOOLS_LICENSE_FILES = LICENSE
 TBTOOLS_DEPENDENCIES = udev

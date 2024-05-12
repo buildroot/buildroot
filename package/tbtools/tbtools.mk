buildroot/buildroot

commit 8d5b3aac435880333833d4a3909b1818fe8af045

package/tbtools: new package
This is a collection of tools for Linux Thunderbolt/USB4 development,
debugging and validation.

Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>
Reviewed-by: Aapo Vienamo <aapo.vienamo@linux.intel.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/tbtools/tbtools.mk
@@ -1,0 +1,13 @@
+################################################################################
+#
+# tbtools
+#
+################################################################################
+
+TBTOOLS_VERSION = v0.3.2
+TBTOOLS_SITE = $(call github,intel,tbtools,$(TBTOOLS_VERSION))
+TBTOOLS_LICENSE = MIT
+TBTOOLS_LICENSE_FILES = LICENSE
+TBTOOLS_DEPENDENCIES = udev
+
+$(eval $(cargo-package))

DEVELOPERS
@@ -2300,6 +2300,9 @@ N:	Michel Stempin <michel.stempin@wanadoo.fr>
 F:	board/sipeed/licheepi_zero/
 F:	configs/sipeed_licheepi_zero_defconfig
 
+N:	Mika Westerberg <mika.westerberg@linux.intel.com>
+F:	package/tbtools/
+
 N:	Mike Frampton <mikeframpo@gmail.com>
 F:	package/qcom-db410c-firmware/
 

package/Config.in
@@ -156,6 +156,7 @@ menu "Debugging, profiling and benchmark"
 	source "package/stress-ng/Config.in"
 	source "package/sysdig/Config.in"
 	source "package/sysprof/Config.in"
+	source "package/tbtools/Config.in"
 	source "package/tcf-agent/Config.in"
 	source "package/trace-cmd/Config.in"
 	source "package/trinity/Config.in"

package/tbtools/Config.in
@@ -1,0 +1,18 @@
+config BR2_PACKAGE_TBTOOLS
+	bool "tbtools"
+	depends on BR2_PACKAGE_HOST_RUSTC_TARGET_ARCH_SUPPORTS
+	depends on BR2_TOOLCHAIN_USES_GLIBC
+	depends on BR2_PACKAGE_HAS_UDEV
+	# Fails to build on 32-bit systems
+	depends on BR2_ARCH_IS_64
+	select BR2_PACKAGE_HOST_RUSTC
+	help
+	  tbtools is a collection of tools for Linux Thunderbolt/USB4
+	  development, debugging and validation.
+
+	  https://github.com/intel/tbtools
+
+comment "tbtools needs udev /dev management w/ glibc toolchain"
+	depends on BR2_PACKAGE_HOST_RUSTC_TARGET_ARCH_SUPPORTS
+	depends on BR2_ARCH_IS_64
+	depends on !BR2_PACKAGE_HAS_UDEV || !BR2_TOOLCHAIN_USES_GLIBC

commit ebe64a69110928c69d1573281f99b8a3204e9c35

package/tbtools: add hashes
Commit 8d5b3aac435880333833d4a3909b1818fe8af045 forgot to add a hash
file. Moreover, also update TBTOOLS_VERSION to match what is returned by
https://release-monitoring.org/project/372327

Fixes: 8d5b3aac435880333833d4a3909b1818fe8af045

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/tbtools/tbtools.hash
@@ -1,0 +1,3 @@
+# Locally calculated
+sha256  e5ddbcb3dcf6315a1fe6fe2d4846b5208e75f24e626d43a0ea68358e8b15dba7  tbtools-0.3.2-cargo2.tar.gz
+sha256  9b16a3add311d607af8d9b348a842a37840d0f47dfcc6cf2a7756ee02c24ed69  LICENSE

package/tbtools/tbtools.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-TBTOOLS_VERSION = v0.3.2
-TBTOOLS_SITE = $(call github,intel,tbtools,$(TBTOOLS_VERSION))
+TBTOOLS_VERSION = 0.3.2
+TBTOOLS_SITE = $(call github,intel,tbtools,v$(TBTOOLS_VERSION))
 TBTOOLS_LICENSE = MIT
 TBTOOLS_LICENSE_FILES = LICENSE
 TBTOOLS_DEPENDENCIES = udev