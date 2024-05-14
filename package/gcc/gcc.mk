1c1edaefb819b85d13db8b618df9dbd208a98e69
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2024-05-14 10:58:00 +0200

    docs, package/gcc: switch to Gitlab issue tracker
    Our Bugzilla is so slow and unstable that it has become
    unusable. Let's switch to using the Gitlab issue tracker instead.
    
    There are still lots of references to the Bugzilla bug tracker in our
    News page at https://buildroot.org/news.html, but these are for older
    news.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    [yann.morin.1998@free.fr: don't needlessly re-flow]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/gcc/gcc.mk
+++ package/gcc/gcc.mk
@@ -83,7 +83,7 @@ HOST_GCC_COMMON_CONF_OPTS = \
 	--with-mpc=$(HOST_DIR) \
 	--with-mpfr=$(HOST_DIR) \
 	--with-pkgversion="Buildroot $(BR2_VERSION_FULL)" \
-	--with-bugurl="http://bugs.buildroot.net/" \
+	--with-bugurl="https://gitlab.com/buildroot.org/buildroot/-/issues" \
 	--without-zstd
 
 ifeq ($(BR2_REPRODUCIBLE),y)
