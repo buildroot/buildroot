c6c173dadb7b44ba4af92237d05503b8e83e51dc
Kadambini Nema <kadambini.nema@gmail.com>
2024-05-14 09:52:51 -0700

    package/ustreamer: bump version to 6.11
    changelog - https://github.com/pikvm/ustreamer/compare/v5.51...v6.11
    
    Signed-off-by: Kadambini Nema <kadambini.nema@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ustreamer/ustreamer.mk
+++ package/ustreamer/ustreamer.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-USTREAMER_VERSION = 5.51
+USTREAMER_VERSION = 6.11
 USTREAMER_SITE = $(call github,pikvm,ustreamer,v$(USTREAMER_VERSION))
 USTREAMER_LICENSE = GPL-3.0+
 USTREAMER_LICENSE_FILES = LICENSE
