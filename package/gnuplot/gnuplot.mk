buildroot/buildroot

commit 5f11ce4aeaba99917778c384b236d267e78a7b29

package/gnuplot: bump version to 6.0.0
0001-configure-add-without-demo-option: adapt patch to 6.0.0 version

Signed-off-by: Michael Fischer <mf@go-sys.de>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/gnuplot/gnuplot.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GNUPLOT_VERSION = 5.4.10
+GNUPLOT_VERSION = 6.0.0
 GNUPLOT_SITE = http://downloads.sourceforge.net/project/gnuplot/gnuplot/$(GNUPLOT_VERSION)
 GNUPLOT_LICENSE = gnuplot license (open source)
 GNUPLOT_LICENSE_FILES = Copyright

package/gnuplot/0001-configure-add-without-demo-option.patch
@@ -8,32 +8,19 @@ Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
 Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
 [Michael: adapt patch to 5.4.0 version]
 Signed-off-by: Michael Fischer <mf@go-sys.de>
+[Michael: adapt patch to 6.0.0 version]
+Signed-off-by: Michael Fischer <mf@go-sys.de>
 
-diff -Naur a/configure.ac b/configure.ac
---- a/configure.ac	2020-07-12 02:29:47.000000000 +0200
-+++ b/configure.ac	2020-08-06 14:21:17.918756893 +0200
-@@ -713,6 +713,17 @@
-                [ Define if you want to read .gnuplot from current directory (SECURITY RISK!).])
- fi
+--- a/configure.ac	2023-12-19 00:28:58.000000000 +0100
++++ b/configure.ac	2024-05-13 15:41:18.313920528 +0200
+@@ -715,6 +715,32 @@
+ 
+ dnl Process rest of with and enable options
  
 +dnl Whether we want to build the doc files
 +dnl Does nothing here, is passed on to the doc subdir
 +AC_ARG_ENABLE(doc,dnl
 +[  --disable-doc    do not build doc files])
-+
-+dnl Whether we want to build the demo files
-+dnl Does nothing here, is passed on to the demo subdir
-+AC_ARG_ENABLE(demo,dnl
-+[  --disable-demo    do not build demo files])
-+
-+
- dnl Sort help/subtopic tables by row or column
- AC_ARG_WITH(row-help,dnl
- [  --with-row-help         format help and subtopic tables by row (default)
-@@ -1095,6 +1106,24 @@
-   AC_DEFINE_UNQUOTED([DIST_CONTACT],["$DIST_CONTACT"],[Contact address for modified and binary distributed gnuplot versions])
- fi
- 
 +dnl build doc files
 +if test "$enable_doc" != no; then
 +  AC_CONFIG_SUBDIRS(docs)
@@ -43,6 +30,10 @@ diff -Naur a/configure.ac b/configure.ac
 +fi
 +AC_SUBST(DOCSUBDIR)
 +
++dnl Whether we want to build the demo files
++dnl Does nothing here, is passed on to the demo subdir
++AC_ARG_ENABLE(demo,dnl
++[  --disable-demo    do not build demo files])
 +dnl build demo files
 +if test "$enable_demo" != no; then
 +  AC_CONFIG_SUBDIRS(demo)
@@ -52,12 +43,11 @@ diff -Naur a/configure.ac b/configure.ac
 +fi
 +AC_SUBST(DEMOSUBDIR)
 +
- dnl Substitute variables
- AC_SUBST(PACKAGE)
- AC_SUBST(VERSION_MAJOR)
-diff -Naur a/Makefile.am b/Makefile.am
---- a/Makefile.am	2020-03-31 19:28:16.000000000 +0200
-+++ b/Makefile.am	2020-08-06 14:34:12.995097799 +0200
+ dnl without-extra-coordinate may leave a hole in "struct coordinate"
+ AC_ARG_WITH(extra-coordinate,dnl
+ [  --without-extra-coordinate    leave possible hole in struct coordinate],,)
+--- a/Makefile.am	2023-12-19 00:28:58.000000000 +0100
++++ b/Makefile.am	2024-05-13 15:54:21.189906680 +0200
 @@ -1,7 +1,7 @@
  ## Process this file with automake to produce Makefile.in -*-Makefile-*-
  AUTOMAKE_OPTIONS = foreign

package/gnuplot/gnuplot.hash
@@ -1,6 +1,6 @@
-# From https://sourceforge.net/projects/gnuplot/files/gnuplot/5.4.10/
-md5  334851e63450362bdb95e67fa8a23665  gnuplot-5.4.10.tar.gz
-sha1  9981e5b0111c07d376deef571ccc75ce4b4ebbd2  gnuplot-5.4.10.tar.gz
+# From https://sourceforge.net/projects/gnuplot/files/gnuplot/6.0.0/
+md5  10246eb96bbf3a151d6eb9bbcc223e4e  gnuplot-6.0.0.tar.gz
+sha1  3abea0cccad89b0c8fb6619ad51b83a971ee24b9  gnuplot-6.0.0.tar.gz
 # Locally computed
-sha256  975d8c1cc2c41c7cedc4e323aff035d977feb9a97f0296dd2a8a66d197a5b27c  gnuplot-5.4.10.tar.gz
+sha256  635a28f0993f6ab0d1179e072ad39b8139d07f51237f841d93c6c2ff4b1758ec  gnuplot-6.0.0.tar.gz
 sha256  895928ec0735cca1c8cec42656c7e314a065d0242813bb8693c0c1bf61fd4e4d  Copyright

commit 93864cef0b1ec94c44498a4edd31de92725b28b2

package/gnuplot: fix readline dependency
--without-readline is interpreted as --with-readline=builtin since bump
to version 5.4.0 in commit 263da09323e83bb3d6552b70b739523b66459df8 and
https://github.com/gnuplot/gnuplot/commit/b492ea63bd604081ffb9dee3843e295b0b29df85
so add a mandatory dependency to readline or libedit to gnuplot

As a side-effect, this will avoid the following build failure with
builtin readline raised since bump to version 6.0.0 in commit
5f11ce4aeaba99917778c384b236d267e78a7b29:

/home/autobuild/autobuild/instance-14/output-1/host/lib/gcc/arm-buildroot-linux-gnueabi/13.2.0/../../../../arm-buildroot-linux-gnueabi/bin/ld: history.o: in function `read_history':
history.c:(.text+0xa8): undefined reference to `gp_read_history'

Fixes: 5f11ce4aeaba99917778c384b236d267e78a7b29
 - http://autobuild.buildroot.org/results/5e45dd8f9071694110c8481f222b9b07b6a97ef3

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

package/gnuplot/Config.in
@@ -1,5 +1,6 @@
 config BR2_PACKAGE_GNUPLOT
 	bool "gnuplot"
+	select BR2_PACKAGE_READLINE if !BR2_PACKAGE_LIBEDIT
 	help
 	  A portable command-line driven graphing utility.
 

package/gnuplot/gnuplot.mk
@@ -41,8 +41,9 @@ endif
 ifeq ($(BR2_PACKAGE_READLINE),y)
 GNUPLOT_CONF_OPTS += --with-readline=gnu
 GNUPLOT_DEPENDENCIES += readline
-else
-GNUPLOT_CONF_OPTS += --without-readline
+else ifeq ($(BR2_PACKAGE_LIBEDIT),y)
+GNUPLOT_CONF_OPTS += --with-readline=bsd
+GNUPLOT_DEPENDENCIES += libedit
 endif
 
 # Remove Javascript scripts, lua scripts, PostScript files