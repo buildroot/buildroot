27bec292f2ef2fc87ff4fa720cb9428a664122a5
Alex Michel <alex.michel@wiedemann-group.com>
2024-05-16 06:48:51 +0000

    package/libzenoh-c: bump to version 0.11.0.3
    https://github.com/eclipse-zenoh/zenoh-c/releases/tag/0.11.0.3
    
    Signed-off-by: Alex Michel <alex.michel@wiedemann-group.com>
    [Arnout: set hash for -cargo2 tarball instead of upstream one]
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/libzenoh-c/libzenoh-c.mk
+++ package/libzenoh-c/libzenoh-c.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBZENOH_C_VERSION = 0.11.0.2
+LIBZENOH_C_VERSION = 0.11.0.3
 LIBZENOH_C_SITE = $(call github,eclipse-zenoh,zenoh-c,$(LIBZENOH_C_VERSION))
 LIBZENOH_C_LICENSE = Apache-2.0 or EPL-2.0
 LIBZENOH_C_LICENSE_FILES = LICENSE
