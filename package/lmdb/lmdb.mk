7bea8c984e2fcff3dd058dbaecfbb6f96e1d66a2
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-05-20 10:07:06 +0200

    package/lmdb: fix static build
    Fix the following static build failure raised since the addition of the
    package in commit eb60c4054c25129d78651a66d1e7554aff04767f:
    
    __uClibc_main.c:(.text+0x15c): undefined reference to `__fini_array_start'
    
    Fixes: eb60c4054c25129d78651a66d1e7554aff04767f
     - http://autobuild.buildroot.org/results/e2bc8c35a10b6513c5605954e93b5b8a1ec7654b
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    [yann.morin.1998@free.fr:
      - s/LMBD/LMDB/
      - drop LMBD_MAKE_OPTS, directly pass ILIBS
    ]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/lmdb/lmdb.mk
+++ package/lmdb/lmdb.mk
@@ -10,13 +10,23 @@ LMDB_LICENSE = OLDAP-2.8
 LMDB_LICENSE_FILES = libraries/liblmdb/LICENSE
 LMDB_INSTALL_STAGING = YES
 
+ifeq ($(BR2_STATIC_LIBS),y)
+LMDB_ILBIBS += liblmdb.a
+else ifeq ($(BR2_SHARED_LIBS),y)
+LMDB_ILBIBS += liblmdb.so
+else ifeq ($(BR2_SHARED_STATIC_LIBS),y)
+LMDB_ILBIBS +=  liblmdb.a liblmdb.so
+endif
+
 define LMDB_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/libraries/liblmdb \
+		ILIBS="$(LMDB_ILBIBS)" \
 		XCFLAGS="$(TARGET_CFLAGS)"
 endef
 
 define LMDB_INSTALL_STAGING_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/libraries/liblmdb \
+		ILIBS="$(LMDB_ILBIBS)" \
 		DESTDIR="$(STAGING_DIR)" \
 		prefix=/usr \
 		install
@@ -24,6 +34,7 @@ endef
 
 define LMDB_INSTALL_TARGET_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/libraries/liblmdb \
+		ILIBS="$(LMDB_ILBIBS)" \
 		DESTDIR="$(TARGET_DIR)" \
 		prefix=/usr \
 		install
