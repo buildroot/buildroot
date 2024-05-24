buildroot/buildroot

commit d90ac63d9b3448a239bb9aa8030326699a3fdc3d

package/minisign: new package
Signed-off-by: Patrick Oppenlander <patrick.oppenlander@gmail.com>
[Peter: Fix check-package warnings, move to "Shell and utilities" and add
	DEVELOPERS entry]
Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

package/minisign/minisign.mk
@@ -1,0 +1,19 @@
+################################################################################
+#
+# minisign
+#
+################################################################################
+
+MINISIGN_VERSION = 0.11
+MINISIGN_SITE = $(call github,jedisct1,minisign,$(MINISIGN_VERSION))
+MINISIGN_LICENSE = ISC
+MINISIGN_LICENSE_FILES = LICENSE
+MINISIGN_DEPENDENCIES = libsodium
+HOST_MINISIGN_DEPENDENCIES = host-libsodium
+
+ifeq ($(BR2_STATIC_LIBS),y)
+MINISIGN_CONF_OPTS += -DBUILD_STATIC_EXECUTABLES=1
+endif
+
+$(eval $(cmake-package))
+$(eval $(host-cmake-package))

DEVELOPERS
@@ -2402,6 +2402,9 @@ N:	Patrick Havelange <patrick.havelange@essensium.com>
 F:	support/testing/tests/package/test_lxc.py
 F:	support/testing/tests/package/test_lxc/
 
+N:	Patrick Oppenlander <patrick.oppenlander@gmail.com>
+F:	package/minisign/
+
 N:	Paul Cercueil <paul@crapouillou.net>
 F:	package/libiio/
 F:	package/lightning/

package/Config.in
@@ -2685,6 +2685,7 @@ comment "Utilities"
 	source "package/lockfile-progs/Config.in"
 	source "package/logrotate/Config.in"
 	source "package/logsurfer/Config.in"
+	source "package/minisign/Config.in"
 	source "package/neofetch/Config.in"
 	source "package/pdmenu/Config.in"
 	source "package/pinentry/Config.in"

package/Config.in.host
@@ -64,6 +64,7 @@ menu "Host utilities"
 	source "package/meson-tools/Config.in.host"
 	source "package/mfgtools/Config.in.host"
 	source "package/microchip-hss-payload-generator/Config.in.host"
+	source "package/minisign/Config.in.host"
 	source "package/mkpasswd/Config.in.host"
 	source "package/moby-buildkit/Config.in.host"
 	source "package/mosquitto/Config.in.host"

package/minisign/Config.in
@@ -1,0 +1,9 @@
+config BR2_PACKAGE_MINISIGN
+	bool "minisign"
+	select BR2_PACKAGE_LIBSODIUM
+	select BR2_PACKAGE_LIBSODIUM_FULL
+	help
+	  Minisign is a dead simple tool to sign files and verify
+	  signatures.
+
+	  https://github.com/jedisct1/minisign

package/minisign/Config.in.host
@@ -1,0 +1,7 @@
+config BR2_PACKAGE_HOST_MINISIGN
+	bool "host minisign"
+	help
+	  Minisign is a dead simple tool to sign files and verify
+	  signatures.
+
+	  https://github.com/jedisct1/minisign

package/minisign/minisign.hash
@@ -1,0 +1,3 @@
+# Locally calculated
+sha256  74c2c78a1cd51a43a6c98f46a4eabefbc8668074ca9aa14115544276b663fc55  minisign-0.11.tar.gz
+sha256  d775d155cbf31638714c31c6f990f9fdc5f07998d91e42d1bf15483bd2d1706b  LICENSE

commit d96b3c331cb0e0cf3bec3d3b1496ed9adb426d05

package/minisign: do not strip
Set CMAKE_STRIP to an empty value to avoid the following build failure
with BR2_BINFMT_FLAT raised since the addition of the package in commit
d90ac63d9b3448a239bb9aa8030326699a3fdc3d:

/home/buildroot/instance-0/output-1/host/bin/m68k-linux-strip: /home/buildroot/instance-0/output-1/build/minisign-0.11/minisign: file format not recognized

Fixes: d90ac63d9b3448a239bb9aa8030326699a3fdc3d
 - http://autobuild.buildroot.org/results/9cc9fd17c7eb4a741c08f27f69764df4a26295fa

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

package/minisign/minisign.mk
@@ -10,6 +10,7 @@ MINISIGN_LICENSE = ISC
 MINISIGN_LICENSE_FILES = LICENSE
 MINISIGN_DEPENDENCIES = libsodium
 HOST_MINISIGN_DEPENDENCIES = host-libsodium
+MINISIGN_CONF_OPTS = -DCMAKE_STRIP=""
 
 ifeq ($(BR2_STATIC_LIBS),y)
 MINISIGN_CONF_OPTS += -DBUILD_STATIC_EXECUTABLES=1