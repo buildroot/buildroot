9fcdc50175b02adce950e90e8d2ae2b7d1396761
Alexander Mukhin <alexander.i.mukhin@gmail.com>
2024-05-24 15:54:48 +0300

    package/tinyproxy: bump version to 1.11.2
    Drop now included patch.
    
    Signed-off-by: Alexander Mukhin <alexander.i.mukhin@gmail.com>
    [Peter: drop patch from .checkpackageignore]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/tinyproxy/tinyproxy.mk
+++ package/tinyproxy/tinyproxy.mk
@@ -4,14 +4,11 @@
 #
 ################################################################################
 
-TINYPROXY_VERSION = 1.11.1
+TINYPROXY_VERSION = 1.11.2
 TINYPROXY_SITE = https://github.com/tinyproxy/tinyproxy/releases/download/$(TINYPROXY_VERSION)
 TINYPROXY_SOURCE = tinyproxy-$(TINYPROXY_VERSION).tar.xz
 TINYPROXY_LICENSE = GPL-2.0+
 TINYPROXY_LICENSE_FILES = COPYING
 TINYPROXY_CPE_ID_VALID = YES
-
-# 0001-prevent-junk-from-showing-up-in-error-page-in-invalid-requests.patch
-TINYPROXY_IGNORE_CVES += CVE-2022-40468
 
 $(eval $(autotools-package))
