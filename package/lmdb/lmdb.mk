buildroot/buildroot

commit eb60c4054c25129d78651a66d1e7554aff04767f

package/lmdb: new package
Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/lmdb/lmdb.mk
@@ -1,0 +1,32 @@
+################################################################################
+#
+# lmdb
+#
+################################################################################
+
+LMDB_VERSION = 0.9.31
+LMDB_SITE = $(call github,LMDB,lmdb,LMDB_$(LMDB_VERSION))
+LMDB_LICENSE = OLDAP-2.8
+LMDB_LICENSE_FILES = libraries/liblmdb/LICENSE
+LMDB_INSTALL_STAGING = YES
+
+define LMDB_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/libraries/liblmdb \
+		XCFLAGS="$(TARGET_CFLAGS)"
+endef
+
+define LMDB_INSTALL_STAGING_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/libraries/liblmdb \
+		DESTDIR="$(STAGING_DIR)" \
+		prefix=/usr \
+		install
+endef
+
+define LMDB_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/libraries/liblmdb \
+		DESTDIR="$(TARGET_DIR)" \
+		prefix=/usr \
+		install
+endef
+
+$(eval $(generic-package))

DEVELOPERS
@@ -1141,6 +1141,7 @@ F:	package/libwpe/
 F:	package/linenoise/
 F:	package/ljlinenoise/
 F:	package/lua-inotify/
+F:	package/lmdb/
 F:	package/lpeg/
 F:	package/lpty/
 F:	package/lrandom/

package/Config.in
@@ -1597,6 +1597,7 @@ menu "Database"
 	source "package/libodb-mysql/Config.in"
 	source "package/libodb-pgsql/Config.in"
 	source "package/libpqxx/Config.in"
+	source "package/lmdb/Config.in"
 	source "package/mariadb/Config.in"
 	source "package/mongodb/Config.in"
 	source "package/postgresql/Config.in"

package/lmdb/Config.in
@@ -1,0 +1,13 @@
+config BR2_PACKAGE_LMDB
+	bool "lmdb"
+	depends on BR2_TOOLCHAIN_HAS_THREADS
+	help
+	  OpenLDAP Lightning Memory-Mapped Database
+
+	  LMDB is compact, fast, powerful, and robust and implements
+	  a simplified variant of the BerkeleyDB (BDB) API.
+
+	  https://symas.com/lmdb/
+
+comment "lmdb needs a toolchain w/ threads"
+	depends on !BR2_TOOLCHAIN_HAS_THREADS

package/lmdb/lmdb.hash
@@ -1,0 +1,3 @@
+# Locally computed
+sha256  dd70a8c67807b3b8532b3e987b0a4e998962ecc28643e1af5ec77696b081c9b0  lmdb-0.9.31.tar.gz
+sha256  310fe25c858a9515fc8c8d7d1f24a67c9496f84a91e0a0e41ea9975b1371e569  libraries/liblmdb/LICENSE

commit 8b16603aa91080a36d27e9c7c0a63e94d98327fc

package/lmdb: fix CFLAGS handling
By passing TARGET_CONFIGURE_OPTS to make, the following usptream CFLAGS
are completely overridden:

CFLAGS	= $(THREADS) $(OPT) $(W) $(XCFLAGS)

As a result, setting XCFLAGS is a no-op and -pthread is not passed
resulting in the following build failure since the addition of the
package in commit eb60c4054c25129d78651a66d1e7554aff04767f:

//binhome//.buildroot.//autobuildlib//rungcc//instancepowerpc64--3buildroot/-outputlinux--1gnu//host9.3.0//opt./.ext/-.toolchain.//bin./../../.lib//powerpc64gcc-/buildrootpowerpc64--linuxbuildroot--gnulinux/-bingnu//ld9.3.0:/ .mdb.c.:/(...text/+.0x1a44.)/:. .undefined/ powerpc64reference- buildrootto `pthread_setspecific'

Fixes: eb60c4054c25129d78651a66d1e7554aff04767f
 - http://autobuild.buildroot.org/results/d67c5a99b7d58826e544f1210fa36bb3bc180739

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

package/lmdb/lmdb.mk
@@ -18,15 +18,21 @@ else ifeq ($(BR2_SHARED_STATIC_LIBS),y)
 LMDB_ILBIBS +=  liblmdb.a liblmdb.so
 endif
 
+LMDB_MAKE_OPTS += \
+	AR="$(TARGET_AR)" \
+	CC="$(TARGET_CC)" \
+	ILIBS="$(LMDB_ILBIBS)" \
+	LDFLAGS="$(TARGET_LDFLAGS)" \
+	OPT="" \
+	XCFLAGS="$(TARGET_CFLAGS)"
+
 define LMDB_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/libraries/liblmdb \
-		ILIBS="$(LMDB_ILBIBS)" \
-		XCFLAGS="$(TARGET_CFLAGS)"
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/libraries/liblmdb $(LMDB_MAKE_OPTS)
 endef
 
 define LMDB_INSTALL_STAGING_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/libraries/liblmdb \
-		ILIBS="$(LMDB_ILBIBS)" \
+		$(LMDB_MAKE_OPTS) \
 		DESTDIR="$(STAGING_DIR)" \
 		prefix=/usr \
 		install
@@ -34,7 +40,7 @@ endef
 
 define LMDB_INSTALL_TARGET_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/libraries/liblmdb \
-		ILIBS="$(LMDB_ILBIBS)" \
+		$(LMDB_MAKE_OPTS) \
 		DESTDIR="$(TARGET_DIR)" \
 		prefix=/usr \
 		install