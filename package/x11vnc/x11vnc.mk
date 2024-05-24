motioneye-project/motioneyeos

commit aeea662fb12e69073a233fe1058287212fbbca33

x11vnc: update to 0.9.14
This switches to x11vnc's new upstream location at github. Autoreconf is
added because it's only a code snapshot release. Subsequent releases
will most probably look the same, see
https://github.com/LibVNC/x11vnc/issues/35#issuecomment-297474900

Reviewed-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
Signed-off-by: Martin Kepplinger <martink@posteo.de>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

package/x11vnc/x11vnc.mk
@@ -4,14 +4,17 @@
 #
 ################################################################################
 
-X11VNC_VERSION = 0.9.13
-X11VNC_SITE = http://downloads.sourceforge.net/project/libvncserver/x11vnc/$(X11VNC_VERSION)
+X11VNC_VERSION = 0.9.14
+X11VNC_SITE = $(call github,LibVNC,x11vnc,$(X11VNC_VERSION))
 # sdl support is not used in x11vnc, but host include / library search paths
 # leak in if host has sdl-config
 X11VNC_CONF_OPTS = --without-sdl
 X11VNC_DEPENDENCIES = xlib_libXt xlib_libXext xlib_libXtst
 X11VNC_LICENSE = GPL-2.0+
 X11VNC_LICENSE_FILES = COPYING
+
+# Source coming from github, no configure included
+X11VNC_AUTORECONF = YES
 
 ifeq ($(BR2_PACKAGE_AVAHI_DAEMON)$(BR2_PACKAGE_DBUS),yy)
 X11VNC_DEPENDENCIES += avahi dbus

package/x11vnc/x11vnc.hash
@@ -1,2 +1,2 @@
 # Locally computed:
-sha256  f6829f2e629667a5284de62b080b13126a0736499fe47cdb447aedb07a59f13b  x11vnc-0.9.13.tar.gz
+sha256  45f87c5e4382988c73e8c7891ac2bfb45d8f9ce1196ae06651c84636684ea143  x11vnc-0.9.14.tar.gz

buildroot/buildroot

commit 3ead103f4640072b95923ea9bb15e115f14f4f0d

package/x11vnc: drop unrecognized options
--without-{jpeg,sdl,zlib} are unrecognized since bump to version
0.9.14 in commit aeea662fb12e69073a233fe1058287212fbbca33 and
https://github.com/LibVNC/x11vnc/commit/d287ed9a77c647f35e85e0b10621a6661e911fad
because those dependencies were linked to libvncserver, not x11vnc

Fixes: aeea662fb12e69073a233fe1058287212fbbca33

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

package/x11vnc/x11vnc.mk
@@ -6,9 +6,6 @@
 
 X11VNC_VERSION = 0.9.16
 X11VNC_SITE = $(call github,LibVNC,x11vnc,$(X11VNC_VERSION))
-# sdl support is not used in x11vnc, but host include / library search paths
-# leak in if host has sdl-config
-X11VNC_CONF_OPTS = --without-sdl
 X11VNC_DEPENDENCIES = xlib_libXt xlib_libXext xlib_libXtst libvncserver
 X11VNC_LICENSE = GPL-2.0+
 X11VNC_LICENSE_FILES = COPYING
@@ -23,12 +20,6 @@ ifeq ($(BR2_PACKAGE_AVAHI_DAEMON)$(BR2_PACKAGE_DBUS),yy)
 X11VNC_DEPENDENCIES += avahi dbus
 else
 X11VNC_CONF_OPTS += --without-avahi
-endif
-
-ifeq ($(BR2_PACKAGE_JPEG),y)
-X11VNC_DEPENDENCIES += jpeg
-else
-X11VNC_CONF_OPTS += --without-jpeg
 endif
 
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
@@ -59,12 +50,6 @@ ifeq ($(BR2_PACKAGE_XLIB_LIBXDAMAGE),y)
 X11VNC_DEPENDENCIES += xlib_libXdamage
 else
 X11VNC_CONF_OPTS += --without-xdamage
-endif
-
-ifeq ($(BR2_PACKAGE_ZLIB),y)
-X11VNC_DEPENDENCIES += zlib
-else
-X11VNC_CONF_OPTS += --without-zlib
 endif
 
 $(eval $(autotools-package))