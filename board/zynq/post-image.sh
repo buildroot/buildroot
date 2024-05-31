a13aca12af814b789884ae84360b2397da9c84ca
Peter Korsgaard <peter@korsgaard.com>
2024-05-31 13:57:25 +0200

    board/zynq: make post-image.sh a symlink to board/zynqmp/post-image.sh
    The zynq/zynqmp post-image scripts are now identical, so replace the zynq
    copy with a symlink to limit redundancy.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- board/zynq/post-image.sh
+++ board/zynq/post-image.sh
@@ -1,15 +1,1 @@
-#!/bin/sh
-
-# By default U-Boot loads DTB from a file named "system.dtb", so
-# let's use a symlink with that name that points to the *first*
-# devicetree listed in the config.
-
-FIRST_DT=$(sed -nr \
-               -e 's|^BR2_LINUX_KERNEL_INTREE_DTS_NAME="(xilinx/)?([-_/[:alnum:]\\.]*).*"$|\2|p' \
-               "${BR2_CONFIG}")
-
-[ -z "${FIRST_DT}" ] || ln -fs "${FIRST_DT}.dtb" "${BINARIES_DIR}/system.dtb"
-
-BOARD_DIR="$(dirname "$0")"
-
-support/scripts/genimage.sh -c "${BOARD_DIR}/genimage.cfg"
+../zynqmp/post-image.sh
