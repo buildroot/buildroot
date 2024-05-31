eefc510767c3d9ea7f7c89642e34b8cdb6174195
Neal Frager <neal.frager@amd.com>
2024-05-31 10:47:47 +0100

    board/zynq/post-image.sh: support linux 6.6 dts directory structure
    Linux 6.6 includes a xilinx directory in the dts directory structure for zynq
    boards.  This patch updates the post-image.sh to match the zynqmp post-image.sh
    in order to support this change.
    
    Signed-off-by: Neal Frager <neal.frager@amd.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- board/zynq/post-image.sh
+++ board/zynq/post-image.sh
@@ -4,9 +4,9 @@
 # let's use a symlink with that name that points to the *first*
 # devicetree listed in the config.
 
-FIRST_DT=$(sed -n \
-           's/^BR2_LINUX_KERNEL_INTREE_DTS_NAME="\([a-z0-9\-]*\).*"$/\1/p' \
-           "${BR2_CONFIG}")
+FIRST_DT=$(sed -nr \
+               -e 's|^BR2_LINUX_KERNEL_INTREE_DTS_NAME="(xilinx/)?([-_/[:alnum:]\\.]*).*"$|\2|p' \
+               "${BR2_CONFIG}")
 
 [ -z "${FIRST_DT}" ] || ln -fs "${FIRST_DT}.dtb" "${BINARIES_DIR}/system.dtb"
 
