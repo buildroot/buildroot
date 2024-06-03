0b880085c9b9e12c53b707c139362ca3f6925553
Peter Korsgaard <peter@korsgaard.com>
2024-06-03 09:17:04 +0200

    package/doom-wad: drop reference to ftp.idsoftware.com
    The server was decommisioned in 2017 and is unlikely to ever come back, so
    drop the reference:
    
    https://www.doomworld.com/forum/topic/96999-rip-ftpidsoftwarecom/
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit a1d5e366b5bb42e2655113f02256caeb17983218)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/doom-wad/doom-wad.mk
+++ package/doom-wad/doom-wad.mk
@@ -7,7 +7,6 @@
 DOOM_WAD_VERSION = 1.9
 DOOM_WAD_SOURCE = doom$(subst .,,$(DOOM_WAD_VERSION))s.zip
 # Official server currently unavailable
-# DOOM_WAD_SITE = ftp://ftp.idsoftware.com/idstuff/doom
 DOOM_WAD_SITE = http://www.jbserver.com/downloads/games/doom/misc/shareware
 
 define DOOM_WAD_EXTRACT_CMDS
