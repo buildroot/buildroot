b7a7f87bc4537547a591217032b9ab5ee5fc1d4f
Peter Korsgaard <peter@korsgaard.com>
2024-06-03 09:17:13 +0200

    package/libnetfilter_log: use https for _SITE
    http redirects to https, so use that instead.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 9f615dc406e20b0d07394ab1a863826c0928f016)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libnetfilter_log/libnetfilter_log.mk
+++ package/libnetfilter_log/libnetfilter_log.mk
@@ -6,7 +6,7 @@
 
 LIBNETFILTER_LOG_VERSION = 1.0.2
 LIBNETFILTER_LOG_SOURCE = libnetfilter_log-$(LIBNETFILTER_LOG_VERSION).tar.bz2
-LIBNETFILTER_LOG_SITE = http://www.netfilter.org/projects/libnetfilter_log/files
+LIBNETFILTER_LOG_SITE = https://www.netfilter.org/projects/libnetfilter_log/files
 LIBNETFILTER_LOG_INSTALL_STAGING = YES
 LIBNETFILTER_LOG_DEPENDENCIES = host-pkgconf libmnl libnfnetlink
 LIBNETFILTER_LOG_LICENSE = GPL-2.0+
