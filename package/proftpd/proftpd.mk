eca3ea8d3f83a19a1ce9823306e689d2b0b92c5d
Peter Korsgaard <peter@korsgaard.com>
2024-06-03 09:17:24 +0200

    package/proftpd: download from github
    The tarball is also available over https from github, so use that instead.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/proftpd/proftpd.mk
+++ package/proftpd/proftpd.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 PROFTPD_VERSION = 1.3.8b
-PROFTPD_SITE = ftp://ftp.proftpd.org/distrib/source
+PROFTPD_SITE = https://github.com/proftpd/proftpd/archive/v$(PROFTPD_VERSION)
 PROFTPD_LICENSE = GPL-2.0+
 PROFTPD_LICENSE_FILES = COPYING
 PROFTPD_CPE_ID_VENDOR = proftpd
