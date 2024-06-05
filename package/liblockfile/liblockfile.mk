ae837f1e64c5108e345c1462644083951769b8e3
Yann E. MORIN <yann.morin.1998@free.fr>
2024-06-05 19:53:13 +0200

    package/liblockfile: drop empty patch tarball
    The Debian control aarchive does not contain any patch for liblockfile
    1.17; it has had no patch since Debian packaged version 1.16-1.1.
    
    Drop the path tarball now.
    
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    [Arnout: also drop from hash file]
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>
    (cherry picked from commit f84c8d17168fe0f5dcf3d57d4c2a1e4f08b79f18)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/liblockfile/liblockfile.mk
+++ package/liblockfile/liblockfile.mk
@@ -7,7 +7,6 @@
 LIBLOCKFILE_VERSION = 1.17
 LIBLOCKFILE_SOURCE = liblockfile_$(LIBLOCKFILE_VERSION).orig.tar.gz
 LIBLOCKFILE_SITE = http://snapshot.debian.org/archive/debian/20210128T210947Z/pool/main/libl/liblockfile
-LIBLOCKFILE_PATCH = liblockfile_$(LIBLOCKFILE_VERSION)-1.debian.tar.bz2
 
 LIBLOCKFILE_LICENSE = LGPL-2.0+, GPL-2.0+ (dotlockfile)
 LIBLOCKFILE_LICENSE_FILES = COPYRIGHT licenses/GPL-2 licenses/LGPL-2
