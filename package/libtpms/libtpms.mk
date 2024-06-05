b2eec8c6265a38cdba1afadf7f60e0fd00f3b448
Jarkko Sakkinen <jarkko@kernel.org>
2024-06-06 02:35:02 +0300

    package/libtpms: add host package
    Libtpms is a library that targets the integration of TPM functionality
    into hypervisors, primarily into Qemu. Libtpms provides a very narrow
    public API for this purpose so that integration is possible. Only the
    minimum of necessary APIs are made publicly available.
    
    Signed-off-by: Jarkko Sakkinen <jarkko@kernel.org>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

---
+++ package/libtpms/libtpms.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# libtpms
+#
+################################################################################
+
+LIBTPMS_VERSION = 0.9.6
+LIBTPMS_SITE = $(call github,stefanberger,libtpms,v$(LIBTPMS_VERSION))
+LIBTPMS_LICENSE = BSD-4-Clause
+LIBTPMS_LICENSE_FILES = LICENSE
+LIBTPMS_INSTALL_STAGING = YES
+
+# Required because a plain Git clone is used:
+HOST_LIBTPMS_AUTORECONF = YES
+HOST_LIBTPMS_DEPENDENCIES = host-pkgconf host-openssl
+HOST_LIBTPMS_CONF_OPTS = --with-openssl  -with-tpm2
+
+$(eval $(host-autotools-package))
