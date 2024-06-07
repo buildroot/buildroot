115e9493b8e3b50cd56b93c47d669319d02698a7
Raphaël Mélotte <raphael.melotte@mind.be>
2024-06-07 18:32:03 +0200

    support/testing: add new test for python-pymupdf
    To give us a chance to catch runtime issues (such as missing
    dependencies) more easily, add a test that writes a sample PDF file,
    read it back and verify the text that was read.
    
    Like similar packages that lead to a big
    rootfs (e.g. python-botocore), this test requires a separate ext2
    rootfs to avoid filling the default amount of RAM available
    entirely (which would cause missing files from the root filesystem and
    in turn, test failures).
    
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ support/testing/tests/package/test_python_pymupdf.py
@@ -0,0 +1,23 @@
+import os
+from tests.package.test_python import TestPythonPackageBase
+
+
+class TestPythonPy3PyMuPDF(TestPythonPackageBase):
+    __test__ = True
+    config = TestPythonPackageBase.config + \
+        """
+        BR2_PACKAGE_PYTHON3=y
+        BR2_PACKAGE_PYTHON_PYMUPDF=y
+        BR2_TARGET_ROOTFS_EXT2=y
+        BR2_TARGET_ROOTFS_EXT2_SIZE="120M"
+        """
+    sample_scripts = ["tests/package/sample_python_pymupdf.py"]
+    timeout = 30
+
+    def login(self):
+        ext2_file = os.path.join(self.builddir, "images", "rootfs.ext2")
+        self.emulator.boot(arch="armv5",
+                           kernel="builtin",
+                           options=["-drive", "file=%s,if=scsi,format=raw" % ext2_file],
+                           kernel_cmdline=["rootwait", "root=/dev/sda"])
+        self.emulator.login()
