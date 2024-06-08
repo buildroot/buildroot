7794356576d49a614a0de789f3d7413696409c4b
Thomas Devoogdt <thomas@devoogdt.com>
2024-06-08 12:25:25 +0200

    package/libinput: bump to 1.26.0
    Announce:
      https://lists.freedesktop.org/archives/wayland-devel/2024-June/043657.html
    
    Signed-off-by: Thomas Devoogdt <thomas@devoogdt.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/libinput/libinput.mk
+++ package/libinput/libinput.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBINPUT_VERSION = 1.25.0
+LIBINPUT_VERSION = 1.26.0
 LIBINPUT_SOURCE = libinput-$(LIBINPUT_VERSION).tar.bz2
 LIBINPUT_SITE = https://gitlab.freedesktop.org/libinput/libinput/-/archive/$(LIBINPUT_VERSION)
 LIBINPUT_DEPENDENCIES = host-pkgconf libevdev mtdev udev
