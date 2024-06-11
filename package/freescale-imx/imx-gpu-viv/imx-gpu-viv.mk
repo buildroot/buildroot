d653e8edcf2df3380a9f38845f6e507d767ddf19
Chris Dimich <cdimich2188@gmail.com>
2024-06-11 10:59:59 -0700

    package/freescale-imx/imx-gpu-viv: bump to version 6.4.11.p2.2
    - To match NXP 6.1.55-2.2.0 release.
    - EULA/COPYING: update to LA_OPT_NXP_Software_License v49.
    
    Signed-off-by: Chris Dimich <cdimich2188@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/freescale-imx/imx-gpu-viv/imx-gpu-viv.mk
+++ package/freescale-imx/imx-gpu-viv/imx-gpu-viv.mk
@@ -5,9 +5,9 @@
 ################################################################################
 
 ifeq ($(BR2_aarch64),y)
-IMX_GPU_VIV_VERSION = 6.4.11.p1.2-aarch64-1e0c396
+IMX_GPU_VIV_VERSION = 6.4.11.p2.2-aarch64-fd0148d
 else
-IMX_GPU_VIV_VERSION = 6.4.11.p1.2-aarch32-1e0c396
+IMX_GPU_VIV_VERSION = 6.4.11.p2.2-aarch32-fd0148d
 endif
 IMX_GPU_VIV_SITE = $(FREESCALE_IMX_SITE)
 IMX_GPU_VIV_SOURCE = imx-gpu-viv-$(IMX_GPU_VIV_VERSION).bin
