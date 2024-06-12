d1d77eb274761d1f2d24ee4b55a374e917c67a8e
Peter Seiderer <ps.report@gmx.net>
2024-06-12 15:57:24 +0200

    package/xz: bump version to 5.6.2
    - bump version to 5.6.2
    - add BSD-0-Clause and update license file hash accordingly (see [1], [2], [3],
      [4], [5], [6], [7] and [8])
    
    For details see [9].
    
    This version skips the 5.6.0 and 5.6.1 releases which had the backdoor.
    
    [1] https://github.com/tukaani-project/xz/commit/b1ee6cf259bb49ce91abe9f622294524e37edf4c
    [2] https://github.com/tukaani-project/xz/commit/689e0228baeb95232430e90d628379db89583d71
    [3] https://github.com/tukaani-project/xz/commit/28ce45e38fbed4b5f54f2013e38dab47d22bf699
    [4] https://github.com/tukaani-project/xz/commit/17aa2e1a796d3f758802df29afc89dcf335db567
    [5] https://github.com/tukaani-project/xz/commit/bfd0c7c478e93a1911b845459549ff94587b6ea2
    [6] https://github.com/tukaani-project/xz/commit/fd7faa4c338a42a6a40e854b837d285ae2e8c609
    [7] https://github.com/tukaani-project/xz/commit/62733592a1cc6f0b41f46ef52e06d1a6fe1ff38a
    [8] https://github.com/tukaani-project/xz/commit/6bbec3bda02bf87d24fa095074456e723589921f
    [9] https://github.com/tukaani-project/xz/releases/tag/v5.6.2
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/xz/xz.mk
+++ package/xz/xz.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-XZ_VERSION = 5.4.7
+XZ_VERSION = 5.6.2
 XZ_SOURCE = xz-$(XZ_VERSION).tar.bz2
 XZ_SITE = https://github.com/tukaani-project/xz/releases/download/v$(XZ_VERSION)
 XZ_INSTALL_STAGING = YES
 XZ_CONF_ENV = ac_cv_prog_cc_c99='-std=gnu99'
-XZ_LICENSE = Public Domain, GPL-2.0+, GPL-3.0+, LGPL-2.1+
-XZ_LICENSE_FILES = COPYING COPYING.GPLv2 COPYING.GPLv3 COPYING.LGPLv2.1
+XZ_LICENSE = Public Domain, BSD-0-Clause, GPL-2.0+, GPL-3.0+, LGPL-2.1+
+XZ_LICENSE_FILES = COPYING COPYING.0BSD COPYING.GPLv2 COPYING.GPLv3 COPYING.LGPLv2.1
 XZ_CPE_ID_VENDOR = tukaani
 
 ifeq ($(BR2_TOOLCHAIN_HAS_THREADS),y)
