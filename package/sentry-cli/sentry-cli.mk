65c66c2ba170d75bf0a00ff832232c523e62de91
Yann E. MORIN <yann.morin.1998@free.fr>
2024-06-14 18:21:53 +0200

    package/sentry-cli: needs pkgconf and openssl
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 2bf45fa99c09302c4d313b79ae7b165a958e354e)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/sentry-cli/sentry-cli.mk
+++ package/sentry-cli/sentry-cli.mk
@@ -9,6 +9,6 @@ SENTRY_CLI_SITE = $(call github,getsentry,sentry-cli,$(SENTRY_CLI_VERSION))
 SENTRY_CLI_LICENSE = BSD-3-clause
 SENTRY_CLI_LICENSE_FILES = LICENSE
 
-HOST_SENTRY_CLI_DEPENDENCIES = host-zlib
+HOST_SENTRY_CLI_DEPENDENCIES = host-pkgconf host-openssl host-zlib
 
 $(eval $(host-cargo-package))
