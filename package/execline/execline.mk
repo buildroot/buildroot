buildroot/buildroot

commit 42fad03182ed53de8003b66d50e893f42b72145e

packages: bump skalibs-related packages
Update all skarnet.org packages to their current upstream version:
  - execline
  - mdevd
  - s6
  - s6-dns
  - s6-linux-init
  - s6-linux-utils
  - s6-networking
  - s6-portable-utils
  - s6-rc
  - skalibs

This patch(set) updates the packages from skarnet.org. It is one commit
to avoid build failures due to incompatible versions: for example, when
only skalibs are updated, execline fails to build; conversely, bumping
execline before skalibs also yields build failures.

Signed-off-by: J. Neuschäfer <j.neuschaefer@gmx.net>
[yann.morin.1998@free.fr:
  - cleanup commit title
  - reword commit log
  - include explanations why all bumps are done at once
]
Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

package/execline/execline.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-EXECLINE_VERSION = 2.9.0.1
-EXECLINE_SITE = http://skarnet.org/software/execline
+EXECLINE_VERSION = 2.9.5.1
+EXECLINE_SITE = https://skarnet.org/software/execline
 EXECLINE_LICENSE = ISC
 EXECLINE_LICENSE_FILES = COPYING
 EXECLINE_INSTALL_STAGING = YES

.checkpackageignore
@@ -1379,7 +1379,6 @@ package/rubix/0002-misc-fixes.patch lib_patch.Sob lib_patch.Upstream
 package/rygel/0001-meson.build-fix-g_ir_compiler-calls.patch lib_patch.Upstream
 package/rygel/S99rygel Shellcheck lib_sysv.Indent lib_sysv.Variables
 package/s6-linux-init/0001-configure-add-D_GNU_SOURCE.patch lib_patch.Upstream
-package/s6-linux-utils/0001-src-s6-linux-utils-rngseed.c-fix-build-with-glibc.patch lib_patch.Upstream
 package/safeclib/0001-fix-armv7-asm-inline-error-GH-115.patch lib_patch.Upstream
 package/samba4/0001-libreplace-disable-libbsd-support.patch lib_patch.Upstream
 package/samba4/0002-build-find-pre-built-heimdal-build-tools-in-case-of-.patch lib_patch.Upstream

package/execline/execline.hash
@@ -1,3 +1,3 @@
 # Locally generated
-sha256  01260fcaf80ffbca2a94aa55ea474dfb9e39b3033b55c8af88126791879531f6  execline-2.9.0.1.tar.gz
-sha256  35126cb8f2ff2a6e359f7bfec46c49004ead6304d8248ba67dcdebcc7d377754  COPYING
+sha256  df750035d0fb21c7265bffb7ed7e1b661de1e842944a2252bdcddc32d0d97217  execline-2.9.5.1.tar.gz
+sha256  fdc931295525dd0009cdf339189cbecd228d65cefe33402f037f9b046f07989e  COPYING

package/mdevd/mdevd.hash
@@ -1,3 +1,3 @@
 # Locally generated
-sha256  33cf2b118634538fd24f302b30f52cc8aa28ea68b414916ce482b54a4ee57283  mdevd-0.1.6.0.tar.gz
-sha256  7fe938b5e0fec18848955ca8081c532e24ae77310beb730b301e1f77bad3d390  COPYING
+sha256  f6ecf0ef7cd48d04efc75acb2dad967d850bc8815bdb0098f1c9ef043394d430  mdevd-0.1.6.3.tar.gz
+sha256  b2edd59a202a710440522f05dcbf418b0493540120efcb407876772ee42d2c7c  COPYING

package/mdevd/mdevd.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-MDEVD_VERSION = 0.1.6.0
-MDEVD_SITE = http://skarnet.org/software/mdevd
+MDEVD_VERSION = 0.1.6.3
+MDEVD_SITE = https://skarnet.org/software/mdevd
 MDEVD_LICENSE = ISC
 MDEVD_LICENSE_FILES = COPYING
 MDEVD_INSTALL_STAGING = YES

package/s6-dns/s6-dns.hash
@@ -1,3 +1,3 @@
 # Locally generated
-sha256  76af222472c4754f83cdc59ad354255ce1b1e6f6833a059328463f8e51f4db43  s6-dns-2.3.5.4.tar.gz
-sha256  35126cb8f2ff2a6e359f7bfec46c49004ead6304d8248ba67dcdebcc7d377754  COPYING
+sha256  cf0258574ec7d62b654e0c2ad78af4c7d67ac4c9cb37f781480f597e54b30f6d  s6-dns-2.3.7.1.tar.gz
+sha256  aafa4f6a12b5b24ab2ae8ead5174e1ec03aecb5eb0e82012d74fd47ccfc57607  COPYING

package/s6-dns/s6-dns.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-S6_DNS_VERSION = 2.3.5.4
-S6_DNS_SITE = http://skarnet.org/software/s6-dns
+S6_DNS_VERSION = 2.3.7.1
+S6_DNS_SITE = https://skarnet.org/software/s6-dns
 S6_DNS_LICENSE = ISC
 S6_DNS_LICENSE_FILES = COPYING
 S6_DNS_INSTALL_STAGING = YES

package/s6-linux-init/s6-linux-init.hash
@@ -1,3 +1,3 @@
 # Locally generated
-sha256  92055a7964cf66604066ad964a2c2392ee7c5e64821be03146c1341e0d8c3dc6  s6-linux-init-1.0.8.0.tar.gz
-sha256  1898df1e8703bd9c1350d2ec3397880b9b4c1451038a9ce5e1c4b2e58622da39  COPYING
+sha256  11ae08d0a66210b5e56b6baee0f6b9b1b69fbedb05fda1285b115a31ca46c77f  s6-linux-init-1.1.2.0.tar.gz
+sha256  f68451e55e6686dc187df1ca427c574d9ac0d6f6926e1bdb909ad8b657668fe5  COPYING

commit 106e43e3036f4c71419c698445ab979e3f32007a

package/execline: fix uclibc build
Fix the following uclibc build failure raised since bump to version
2.9.5.1 in commit 42fad03182ed53de8003b66d50e893f42b72145e and
https://github.com/skarnet/execline/commit/6dfe54be869c117676dcc004400ac68096945531:

src/execline/eltest.c: In function 'eltest_run':
src/execline/eltest.c:366:31: error: 'struct stat' has no member named 'st_mtim'; did you mean 'st_mtime'?
  366 |       return timespec_cmp(&st.st_mtim, &st.st_atim) > 0 ;
      |                               ^~~~~~~
      |                               st_mtime

Fixes: 42fad03182ed53de8003b66d50e893f42b72145e
 - http://autobuild.buildroot.org/results/64bf2928f36e07332052854cbaaa7a6f8f347e29

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

package/execline/execline.mk
@@ -12,6 +12,7 @@ EXECLINE_INSTALL_STAGING = YES
 EXECLINE_DEPENDENCIES = skalibs
 
 EXECLINE_CONF_OPTS = \
+	CFLAGS="$(TARGET_CFLAGS) -D_GNU_SOURCE" \
 	--prefix=/ \
 	--with-sysdeps=$(STAGING_DIR)/lib/skalibs/sysdeps \
 	--with-include=$(STAGING_DIR)/include \