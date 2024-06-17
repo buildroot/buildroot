127ddbe5118f68395c0305fb9ff44342bfc1d64b
Waldemar Brodkorb <wbx@openadk.org>
2024-06-17 09:25:24 +0200

    package/uclibc: update to 1.0.49
    git shortlog v1.0.49...v1.0.48
    Dmitry Chestnykh (6):
          Correct uClibc compilation.
          Fix uClibc build for nds32 with gcc-14.
          Fix riscv32 build with gcc-14.
          riscv32: Fix `struct ucontext` definition.
          Fix wrong `struct ucontext_t` typedef for all arches.
          Cast to proper types inside atomic macroses.
    
    Fabrice Fontaine (1):
          libc/sysdeps/linux/common/utime.c: fix riscv32 build
    
    Joe Damato (1):
          epoll.h: Add epoll ioctls
    
    Max Filippov (9):
          iconv: fix type mismatches
          ldso: FDPIC: fix type mismatches
          ldso: arm: fix build with gcc-14
          malloc/memalign: avoid integer overflow
          fix kernel_stat64 definition
          linuxthreads: enable unwinding over signal frames
          xtensa: use compiler-provided XCHAL macros
          xtensa: make _init and _fini hidden
          xtensa: add FDPIC support
    
    Waldemar Brodkorb (9):
          futimesat: add missing header
          sparc64: Fix incorrect sigreturn stub function implementation
          csky: allow time64
          m68k: fix noMMU ELF compile with gcc 14.x
          Revert "nds32: sync with binutils 2.37, gcc 11.2 and linux 5.10.93 changes"
          m68k: fix for m68000 cpu
          config: make ctor/dtor visible again
          config: enable TIME64 by default
          bump version for 1.0.49 release
    
    Yuriy Kolerov (1):
          libc: cast free() argument to void * in wchar.c
    
    Patches applied upstream.
    
    Signed-off-by: Waldemar Brodkorb <wbx@openadk.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/uclibc/uclibc.mk
+++ package/uclibc/uclibc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UCLIBC_VERSION = 1.0.48
+UCLIBC_VERSION = 1.0.49
 UCLIBC_SOURCE = uClibc-ng-$(UCLIBC_VERSION).tar.xz
 UCLIBC_SITE = https://downloads.uclibc-ng.org/releases/$(UCLIBC_VERSION)
 UCLIBC_LICENSE = LGPL-2.1+
