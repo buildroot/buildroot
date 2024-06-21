74ebd50ccc6681ef2e07896b8f16afb122db099d
Julien Olivain <ju.o@free.fr>
2024-06-21 23:09:04 +0200

    package/python-pyalsa: bump to version 1.2.12
    For change log since 1.2.7, see:
    - https://github.com/alsa-project/alsa-python/releases/tag/v1.2.12
    
    This commit also adds a comment about the pgp signature check in the
    hash file.
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-pyalsa/python-pyalsa.mk
+++ package/python-pyalsa/python-pyalsa.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_PYALSA_VERSION = 1.2.7
+PYTHON_PYALSA_VERSION = 1.2.12
 PYTHON_PYALSA_SOURCE = pyalsa-$(PYTHON_PYALSA_VERSION).tar.bz2
 PYTHON_PYALSA_SITE = https://www.alsa-project.org/files/pub/pyalsa
 PYTHON_PYALSA_SETUP_TYPE = setuptools
