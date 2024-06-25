70a319f7f9aba2d44ed7548d7ac3cb42b6b216b3
Roy Kollen Svendsen <roykollensvendsen@gmail.com>
2024-06-25 23:18:36 +0200

    package/qt6/qt6tools: new package
    The Network module is explicitly required by qt6tools:
    
      Failed to find required Qt component "Network".
    
    We need host-qt6base with Sql support for host-qt6tools to build the
    qhelpgenerator host tool. qt6tools will fail to build if qhelpgenerator
    is not available:
    
      Failed to find the host tool "Qt6::qhelpgenerator".  It is part of the
      Qt6ToolsTools package, but the package did not contain the tool.  Make sure
      that the host module Tools was built with all features enabled (no
      explicitly disabled tools).
    
    Signed-off-by: Roy Kollen Svendsen <roykollensvendsen@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/qt6/qt6tools/qt6tools.mk
@@ -0,0 +1,45 @@
+################################################################################
+#
+# qt6tools
+#
+################################################################################
+
+QT6TOOLS_VERSION = $(QT6_VERSION)
+QT6TOOLS_SITE = $(QT6_SITE)
+QT6TOOLS_SOURCE = qttools-$(QT6_SOURCE_TARBALL_PREFIX)-$(QT6TOOLS_VERSION).tar.xz
+QT6TOOLS_INSTALL_STAGING = YES
+QT6TOOLS_SUPPORTS_IN_SOURCE_BUILD = NO
+
+QT6TOOLS_CMAKE_BACKEND = ninja
+
+QT6TOOLS_LICENSE = \
+	BSD-3-Clause (examples), \
+	BSD-3-Clause (corecon), \
+	BSL-1.0 (catch), \
+	LGPL-3.0 or GPL-3.0 or GPL-2.0, \
+	GPL-3.0 WITH Qt-GPL-exception-1.0
+
+QT6TOOLS_LICENSE_FILES = \
+	LICENSES/BSD-3-Clause.txt \
+	LICENSES/BSL-1.0.txt \
+	LICENSES/GPL-2.0-only.txt \
+	LICENSES/GPL-3.0-only.txt \
+	LICENSES/LGPL-3.0-only.txt \
+	LICENSES/Qt-GPL-exception-1.0.txt
+
+QT6TOOLS_CONF_OPTS = \
+	-DQT_HOST_PATH=$(HOST_DIR) \
+	-DBUILD_WITH_PCH=OFF \
+	-DQT_BUILD_EXAMPLES=OFF \
+	-DQT_BUILD_TESTS=OFF
+
+QT6TOOLS_DEPENDENCIES = \
+	qt6base \
+	host-qt6tools
+
+ifeq ($(BR2_PACKAGE_QT6DECLARATIVE),y)
+QT6TOOLS_DEPENDENCIES += qt6declarative
+endif
+
+$(eval $(cmake-package))
+$(eval $(host-cmake-package))
