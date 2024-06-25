bab745cb97fce3b50bae50ab4743e164fd18b77b
Roy Kollen Svendsen <roykollensvendsen@gmail.com>
2024-06-25 23:18:28 +0200

    package/qt6/qt6base: add blind option to enable Test support on host
    We need host-qt6base with Testlib support when building host-qt6declarative
    with QuickTest support. QuickTest support is further required for building the
    qmltestrunner host tool. qt6declarative will fail to build if qmltestrunner is
    not available.
    
    Signed-off-by: Roy Kollen Svendsen <roykollensvendsen@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/qt6/qt6base/qt6base.mk
+++ package/qt6/qt6base/qt6base.mk
@@ -87,7 +87,6 @@ HOST_QT6BASE_CONF_OPTS = \
 	-DFEATURE_concurrent=OFF \
 	-DFEATURE_xml=ON \
 	-DFEATURE_sql=OFF \
-	-DFEATURE_testlib=OFF \
 	-DFEATURE_dbus=OFF \
 	-DFEATURE_icu=OFF \
 	-DFEATURE_glib=OFF \
@@ -127,6 +126,16 @@ ifeq ($(BR2_PACKAGE_HOST_QT6BASE_NETWORK),y)
 HOST_QT6BASE_CONF_OPTS += -DFEATURE_network=ON
 else
 HOST_QT6BASE_CONF_OPTS += -DFEATURE_network=OFF
+endif
+
+# We need host-qt6base with Testlib support when building host-qt6declarative
+# with QuickTest support. QuickTest support is further required for building the
+# qmltestrunner host tool. qt6declarative will fail to build if qmltestrunner is
+# not available.
+ifeq ($(BR2_PACKAGE_HOST_QT6BASE_TEST),y)
+HOST_QT6BASE_CONF_OPTS += -DFEATURE_testlib=ON
+else
+HOST_QT6BASE_CONF_OPTS += -DFEATURE_testlib=OFF
 endif
 
 # Conditional blocks below are ordered by alphabetic ordering of the
