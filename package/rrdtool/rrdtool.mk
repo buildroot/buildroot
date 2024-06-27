4928032d6c58ff64b0aa4af1874aed7c3aa43842
Bernd Kuhls <bernd@kuhls.net>
2024-06-28 00:06:53 +0200

    package/rrdtool: fix patch fuzz
    Commit 8f88a644ed "support/scripts/apply-patches.sh: set the maximum
    fuzz factor to 0" reduced the fuzz factor.
    
    Due to this change, rrdtool fails to build with output:
    
        patching file src/rrd_tool.c
        Hunk #2 FAILED at 42.
        Hunk #3 succeeded at 435 (offset -8 lines).
        Hunk #4 succeeded at 518 (offset -8 lines).
        Hunk #5 succeeded at 564 (offset -8 lines).
        Hunk #6 succeeded at 879 (offset -8 lines).
        1 out of 6 hunks FAILED -- saving rejects to file src/rrd_tool.c.rej
    
    Because this was caused by an upstream patch we do not change it but add
    another upstream patch to be applied before our current patch to avoid
    the problem.
    
    Fixes:
    http://autobuild.buildroot.net/results/6996dbd764b0066da49dd009f1385196342c89dc/
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Reviewed-by: Yann E. MORIN <yann.morin@orange.com>
    Tested-by: Yann E. MORIN <yann.morin@orange.com>
    [yann.morin.1998@free.fr:
      - add comment as suggested by yann.morin@orange.com
    ]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/rrdtool/rrdtool.mk
+++ package/rrdtool/rrdtool.mk
@@ -9,10 +9,13 @@ RRDTOOL_SITE = https://github.com/oetiker/rrdtool-1.x/releases/download/v$(RRDTO
 RRDTOOL_LICENSE = GPL-2.0+ with FLOSS license exceptions as explained in COPYRIGHT
 RRDTOOL_LICENSE_FILES = COPYRIGHT LICENSE
 RRDTOOL_DEPENDENCIES = host-pkgconf libglib2 $(TARGET_NLS_DEPENDENCIES)
-# autoreconf needed to avoid link failure due to missing -lintl,
+
+# 0001-Fix-BUILD_DATE-in-rrdtool-help-output.patch
+# autoreconf also needed to avoid link failure due to missing -lintl,
 # autopoint needed as a consequence of autoreconf
 RRDTOOL_AUTORECONF = YES
 RRDTOOL_AUTOPOINT = YES
+
 RRDTOOL_INSTALL_STAGING = YES
 RRDTOOL_CONF_OPTS = \
 	--disable-examples \
