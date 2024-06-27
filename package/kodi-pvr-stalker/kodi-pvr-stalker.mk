60683672be360cd0e47651e07065a5db60f7dd98
Bernd Kuhls <bernd@kuhls.net>
2024-06-27 23:03:24 +0200

    package/kodi-pvr-stalker: bump version to 21.1.0-Omega
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-pvr-stalker/kodi-pvr-stalker.mk
+++ package/kodi-pvr-stalker/kodi-pvr-stalker.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PVR_STALKER_VERSION = 21.0.1-Omega
+KODI_PVR_STALKER_VERSION = 21.1.0-Omega
 KODI_PVR_STALKER_SITE = $(call github,kodi-pvr,pvr.stalker,$(KODI_PVR_STALKER_VERSION))
 KODI_PVR_STALKER_LICENSE = GPL-2.0+
 KODI_PVR_STALKER_LICENSE_FILES = LICENSE.md
