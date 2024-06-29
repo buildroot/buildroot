7630cc8759a926e3ce098bd9a76e4ef8004cfb38
Fiona Klute <fiona.klute+wiwa@gmx.de>
2024-06-29 14:55:21 +0200

    package/python-docker: bump version to 7.1.0
    Important upstream changes:
    
    * Support for Python 3.12 has been added (in 7.0.0).
    
    * Websocket support is no longer included by default, and thus the
      dependency on "websocket-client" (since 7.0.0).
    
    * Build has been switched to hatchling and hatch-vcs (in 7.1.0), so
      Buildroot needs to use pep517 build.
    
    * Dependency on "packaging" has been removed (in 7.1.0).
    
    Upstream changelogs:
    https://github.com/docker/docker-py/releases/tag/7.0.0
    https://github.com/docker/docker-py/releases/tag/7.1.0
    
    Signed-off-by: Fiona Klute <fiona.klute+wiwa@gmx.de>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/python-docker/python-docker.mk
+++ package/python-docker/python-docker.mk
@@ -4,14 +4,14 @@
 #
 ################################################################################
 
-PYTHON_DOCKER_VERSION = 6.1.3
+PYTHON_DOCKER_VERSION = 7.1.0
 PYTHON_DOCKER_SOURCE = docker-$(PYTHON_DOCKER_VERSION).tar.gz
-PYTHON_DOCKER_SITE = https://files.pythonhosted.org/packages/f0/73/f7c9a14e88e769f38cb7fb45aa88dfd795faa8e18aea11bababf6e068d5e
-PYTHON_DOCKER_SETUP_TYPE = setuptools
+PYTHON_DOCKER_SITE = https://files.pythonhosted.org/packages/91/9b/4a2ea29aeba62471211598dac5d96825bb49348fa07e906ea930394a83ce
+PYTHON_DOCKER_SETUP_TYPE = pep517
 PYTHON_DOCKER_LICENSE = Apache-2.0
 PYTHON_DOCKER_LICENSE_FILES = LICENSE
 PYTHON_DOCKER_CPE_ID_VENDOR = docker
 PYTHON_DOCKER_CPE_ID_PRODUCT = docker-py
-PYTHON_DOCKER_DEPENDENCIES = host-python-setuptools-scm
+PYTHON_DOCKER_DEPENDENCIES = host-python-hatchling host-python-hatch-vcs
 
 $(eval $(python-package))
