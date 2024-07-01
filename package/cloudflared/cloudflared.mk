eaa801af97b77dbbe1065ccc42ac147749b4e498
James Hilliard <james.hilliard1@gmail.com>
2024-07-01 16:49:02 -0600

    package/cloudflared: new package
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/cloudflared/cloudflared.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# cloudflared
+#
+################################################################################
+
+CLOUDFLARED_VERSION = 2024.6.1
+CLOUDFLARED_SITE = $(call github,cloudflare,cloudflared,$(CLOUDFLARED_VERSION))
+CLOUDFLARED_LICENSE = Apache-2.0
+CLOUDFLARED_LICENSE_FILES = LICENSE
+CLOUDFLARED_BUILD_TARGETS = cmd/cloudflared
+CLOUDFLARED_LDFLAGS = -X main.Version=$(CLOUDFLARED_VERSION)
+
+$(eval $(golang-package))
