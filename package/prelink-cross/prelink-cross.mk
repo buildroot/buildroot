a72b3e2eee788ca4b8544ab4f6c0db41065823c6
James Hilliard <james.hilliard1@gmail.com>
2024-07-01 10:56:16 -0600

    package/prelink-cross: bump to version 440c0059a21f06b5577499a80b55482cb99d6d1c
    Drop no longer relevant patch.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/prelink-cross/prelink-cross.mk
+++ package/prelink-cross/prelink-cross.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PRELINK_CROSS_VERSION = a853a5d715d84eec93aa68e8f2df26b7d860f5b2
+PRELINK_CROSS_VERSION = 440c0059a21f06b5577499a80b55482cb99d6d1c
 PRELINK_CROSS_SITE = https://git.yoctoproject.org/git/prelink-cross
 PRELINK_CROSS_SITE_METHOD = git
 PRELINK_CROSS_LICENSE = GPL-2.0+
