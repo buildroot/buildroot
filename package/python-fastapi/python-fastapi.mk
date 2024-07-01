f33b2227416ccefe7aff4f612644cd932af1f110
James Hilliard <james.hilliard1@gmail.com>
2024-07-01 11:38:15 -0600

    package/python-fastapi: bump to version 0.111.0
    Switch from hatchling to pdm-backend pep517 build backend.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/python-fastapi/python-fastapi.mk
+++ package/python-fastapi/python-fastapi.mk
@@ -4,14 +4,14 @@
 #
 ################################################################################
 
-PYTHON_FASTAPI_VERSION = 0.110.0
+PYTHON_FASTAPI_VERSION = 0.111.0
 PYTHON_FASTAPI_SOURCE = fastapi-$(PYTHON_FASTAPI_VERSION).tar.gz
-PYTHON_FASTAPI_SITE = https://files.pythonhosted.org/packages/61/53/326977db62bf34bbdfc64acb9414e1881af7ea14e8a062fd1c11a8697616
+PYTHON_FASTAPI_SITE = https://files.pythonhosted.org/packages/0e/1f/f4a99e92c583780787e04b05aa9d8a8db9ec76d091d81545948a006f5b44
 PYTHON_FASTAPI_SETUP_TYPE = pep517
 PYTHON_FASTAPI_LICENSE = MIT
 PYTHON_FASTAPI_LICENSE_FILES = LICENSE
 PYTHON_FASTAPI_CPE_ID_VENDOR = fastapi_project
 PYTHON_FASTAPI_CPE_ID_PRODUCT = fastapi
-PYTHON_FASTAPI_DEPENDENCIES = host-python-hatchling
+PYTHON_FASTAPI_DEPENDENCIES = host-python-pdm-backend
 
 $(eval $(python-package))
