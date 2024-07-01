14388ca1b695a808c329fac8bd3470f17193785f
James Hilliard <james.hilliard1@gmail.com>
2024-07-01 13:54:34 -0600

    package/python-sniffio: bump to version 1.3.1
    Add new host-python-setuptools-scm build dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-sniffio/python-sniffio.mk
+++ package/python-sniffio/python-sniffio.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_SNIFFIO_VERSION = 1.3.0
+PYTHON_SNIFFIO_VERSION = 1.3.1
 PYTHON_SNIFFIO_SOURCE = sniffio-$(PYTHON_SNIFFIO_VERSION).tar.gz
-PYTHON_SNIFFIO_SITE = https://files.pythonhosted.org/packages/cd/50/d49c388cae4ec10e8109b1b833fd265511840706808576df3ada99ecb0ac
+PYTHON_SNIFFIO_SITE = https://files.pythonhosted.org/packages/a2/87/a6771e1546d97e7e041b6ae58d80074f81b7d5121207425c964ddf5cfdbd
 PYTHON_SNIFFIO_SETUP_TYPE = setuptools
 PYTHON_SNIFFIO_LICENSE = Apache-2.0 or MIT
 PYTHON_SNIFFIO_LICENSE_FILES = LICENSE LICENSE.APACHE2 LICENSE.MIT
+PYTHON_SNIFFIO_DEPENDENCIES = host-python-setuptools-scm
 
 $(eval $(python-package))
