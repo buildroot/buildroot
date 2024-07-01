6a8f8d83a8261971725f61b1207d7aa5ebca7234
James Hilliard <james.hilliard1@gmail.com>
2024-07-01 13:36:22 -0600

    package/python-pydantic-core: bump to version 2.20.0
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/python-pydantic-core/python-pydantic-core.mk
+++ package/python-pydantic-core/python-pydantic-core.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYDANTIC_CORE_VERSION = v2.19.0
-PYTHON_PYDANTIC_CORE_SITE = https://github.com/pydantic/pydantic-core
-PYTHON_PYDANTIC_CORE_SITE_METHOD = git
+PYTHON_PYDANTIC_CORE_VERSION = 2.20.0
+PYTHON_PYDANTIC_CORE_SOURCE = pydantic_core-$(PYTHON_PYDANTIC_CORE_VERSION).tar.gz
+PYTHON_PYDANTIC_CORE_SITE = https://files.pythonhosted.org/packages/7d/fa/9802d053f33dbcf110d46e3f28667b06cd764b164f1e3f4189848cdd6e78
 PYTHON_PYDANTIC_CORE_SETUP_TYPE = maturin
 PYTHON_PYDANTIC_CORE_LICENSE = MIT
 PYTHON_PYDANTIC_CORE_LICENSE_FILES = LICENSE
