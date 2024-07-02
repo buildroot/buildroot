11d2ad31046263d80da5ff6b36c3d25b1bbc0d82
Niklas Yann Wettengel <niyawe@niyawe.de>
2024-07-02 14:36:16 +0200

    packages/openssh: install sshd-session
    In OpenSSH 9.8 the server is split into two processes: sshd and
    sshd-session which results in the following error, when trying
    to start sshd:
    
    Starting sshd: /usr/libexec/sshd-session does not exist or is not executable
    
    This patch installs the sshd-session binary.
    
    Fixes: https://gitlab.com/buildroot.org/buildroot/-/issues/11
    Fixes: 4ac2cc5beec0eac4991fb28f286707a5659fa67e
    
    Signed-off-by: Niklas Yann Wettengel <niyawe@niyawe.de>
    Reviewed-by: Nevo Hed <nhed+buildroot@starry.com>
    Tested-by: Fiona Klute (WIWA) <fiona.klute@gmx.de>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/openssh/openssh.mk
+++ package/openssh/openssh.mk
@@ -117,6 +117,7 @@ endif
 ifeq ($(BR2_PACKAGE_OPENSSH_SERVER),y)
 define OPENSSH_INSTALL_SERVER_PROGRAMS
 	$(INSTALL) -D -m 0755 $(@D)/sshd $(TARGET_DIR)/usr/sbin/sshd
+	$(INSTALL) -D -m 0755 $(@D)/sshd-session $(TARGET_DIR)/usr/libexec/sshd-session
 	$(INSTALL) -D -m 0755 $(@D)/sftp-server $(TARGET_DIR)/usr/libexec/sftp-server
 endef
 OPENSSH_POST_INSTALL_TARGET_HOOKS += OPENSSH_INSTALL_SERVER_PROGRAMS
