f82b58d2286fb5b39ef7461b7d26d567fbccdc59
Heiko Thiery <heiko.thiery@gmail.com>
2024-07-13 14:23:30 +0200

    package/blake3: add new host package
    This package is used and required for ccache update.
    
    Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

---
+++ package/blake3/blake3.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# blake3
+#
+################################################################################
+
+BLAKE3_VERSION = 1.5.1
+BLAKE3_SITE = $(call github,BLAKE3-team,BLAKE3,$(BLAKE3_VERSION))
+BLAKE3_SUBDIR = c
+BLAKE3_LICENSE = Apache-2.0, CC0-1.0
+BLAKE3_LICENSE_FILES = LICENSE
+
+# We may be a ccache dependency, so we can't use ccache; reset the
+# options set by the cmake infra.
+HOST_BLAKE3_CONF_OPTS += \
+	-UCMAKE_C_COMPILER_LAUNCHER \
+	-UCMAKE_CXX_COMPILER_LAUNCHER
+
+$(eval $(host-cmake-package))
