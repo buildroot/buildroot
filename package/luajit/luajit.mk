b1f1358c59bb5fb9b54262111226f422d2a6ddf3
Francois Perrad <francois.perrad@gadz.org>
2024-07-13 20:30:24 +0200

    package/luajit: bump to version 04dca7911ea255f37be799c18d74c305b921c1a6
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/luajit/luajit.mk
+++ package/luajit/luajit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LUAJIT_VERSION = 41fb94defa8f830ce69a8122b03f6ac3216d392a
+LUAJIT_VERSION = 04dca7911ea255f37be799c18d74c305b921c1a6
 LUAJIT_SITE = $(call github,LuaJIT,LuaJIT,$(LUAJIT_VERSION))
 LUAJIT_LICENSE = MIT
 LUAJIT_LICENSE_FILES = COPYRIGHT
