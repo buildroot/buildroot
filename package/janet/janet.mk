31212c4c58d7547cb4bff77e17b62bc88f3ffa1b
Francois Perrad <francois.perrad@gadz.org>
2024-07-13 20:30:15 +0200

    package/janet: bump to version 1.35.2
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/janet/janet.mk
+++ package/janet/janet.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-JANET_VERSION = 1.33.0
+JANET_VERSION = 1.35.2
 JANET_SITE = $(call github,janet-lang,janet,v$(JANET_VERSION))
 JANET_LICENSE = MIT
 JANET_LICENSE_FILES = LICENSE
