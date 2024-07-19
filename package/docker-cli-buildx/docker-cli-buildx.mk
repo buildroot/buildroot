209103c756b4b5fd3637f290face7122b2fbd1e7
Christian Stewart <christian@aperture.us>
2024-07-18 20:22:05 -0700

    package/docker-cli-buildx: bump version to v0.16.1
    Drop the vendor/ workaround as it is no longer necessary.
    
    https://github.com/docker/buildx/releases/tag/v0.16.1
    
    Signed-off-by: Christian Stewart <christian@aperture.us>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/docker-cli-buildx/docker-cli-buildx.mk
+++ package/docker-cli-buildx/docker-cli-buildx.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DOCKER_CLI_BUILDX_VERSION = 0.11.2
+DOCKER_CLI_BUILDX_VERSION = 0.16.1
 DOCKER_CLI_BUILDX_SITE = $(call github,docker,buildx,v$(DOCKER_CLI_BUILDX_VERSION))
 
 DOCKER_CLI_BUILDX_LICENSE = Apache-2.0
@@ -18,15 +18,6 @@ DOCKER_CLI_BUILDX_GOMOD = github.com/docker/buildx
 DOCKER_CLI_BUILDX_LDFLAGS = \
 	-X $(DOCKER_CLI_BUILDX_GOMOD)/version.Revision=$(DOCKER_CLI_BUILDX_VERSION) \
 	-X $(DOCKER_CLI_BUILDX_GOMOD)/version.Version=$(DOCKER_CLI_BUILDX_VERSION)
-
-# create the go.mod file with required language version go1.20
-# remove the conflicting vendor/modules.txt
-# https://github.com/moby/moby/issues/44618#issuecomment-1343565705
-define DOCKER_CLI_BUILDX_FIX_VENDORING
-	printf "module $(DOCKER_CLI_BUILDX_GOMOD)\n\ngo 1.20\n" > $(@D)/go.mod
-	rm -f $(@D)/vendor/modules.txt
-endef
-DOCKER_CLI_BUILDX_POST_EXTRACT_HOOKS += DOCKER_CLI_BUILDX_FIX_VENDORING
 
 define DOCKER_CLI_BUILDX_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -D $(@D)/bin/buildx \
