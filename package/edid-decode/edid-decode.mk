buildroot/buildroot

commit 4f5790116b42768bd1bd982245e00483440a6311

package/edid-decode: bump version to 6f117a8
Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/edid-decode/edid-decode.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-EDID_DECODE_VERSION = 2d44e1b01c7ed7d65b20ecdce62d354841832201
+EDID_DECODE_VERSION = 6f117a8f8c0e76e85f599a8b05c21c5f51c5c3c1
 EDID_DECODE_SITE = https://git.linuxtv.org/edid-decode.git
 EDID_DECODE_SITE_METHOD = git
 EDID_DECODE_LICENSE = MIT

package/edid-decode/edid-decode.hash
@@ -1,3 +1,3 @@
 # Locally computed
-sha256  45b71227ceb7265da3a314a8d0969dcca584bfa12c36690e911646b58aae0738  edid-decode-2d44e1b01c7ed7d65b20ecdce62d354841832201-git4.tar.gz
+sha256  bc166bef5d0d56035b47a1513ed320c262901f97e488c83420760d7f56174dac  edid-decode-6f117a8f8c0e76e85f599a8b05c21c5f51c5c3c1-git4.tar.gz
 sha256  9bc7ab961e9a07dc84eaa0210af388def56d024357e79671fb89fe27d073eed7  LICENSE

commit 124087c51c6bf751b0caa5cd6327f7ce3d89d0e1

package/edid-decode: switch to meson-package
Switch to meson-package to fix the following build failure raised since
bump to 6f117a8 in commit 4f5790116b42768bd1bd982245e00483440a6311 and
https://git.linuxtv.org/edid-decode.git/commit/?id=cdf81907def8658a66041a76ef2542e9a888cbed:

make[1]: *** No targets specified and no makefile found.  Stop.

Fixes: 4f5790116b42768bd1bd982245e00483440a6311
 - http://autobuild.buildroot.org/results/c569b0484dcd33f6ea88a9b6f11df5b54401db96

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/edid-decode/edid-decode.mk
@@ -10,14 +10,4 @@ EDID_DECODE_SITE_METHOD = git
 EDID_DECODE_LICENSE = MIT
 EDID_DECODE_LICENSE_FILES = LICENSE
 
-define EDID_DECODE_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
-		CFLAGS="$(TARGET_CXXFLAGS) -std=c++11" WARN_FLAGS=
-endef
-
-define EDID_DECODE_INSTALL_TARGET_CMDS
-	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
-		DESTDIR="$(TARGET_DIR)" install
-endef
-
-$(eval $(generic-package))
+$(eval $(meson-package))