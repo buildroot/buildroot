3e445665ba2093fdc75dfe12bd4960f63a25915d
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-07-22 12:10:28 +0200

    package/brcm-patchram-plus: bump to latest git commit
    This bump will fix the following build failure with gcc >= 14:
    
    main.c: In function 'read_event':
    main.c:715:25: error: implicit declaration of function 'read'; did you mean 'fread'? [-Wimplicit-function-declaration]
      715 |         while ((count = read(fd, &buffer[i], len)) < len) {
          |                         ^~~~
          |                         fread
    
    Fixes:
     - http://autobuild.buildroot.org/results/67965a5245c3d98d683e2c788352bf7c1cf4f00e
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit a0ebcac06b76c4f3c6b7c2d2382ddfeee85d5c1d)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/brcm-patchram-plus/brcm-patchram-plus.mk
+++ package/brcm-patchram-plus/brcm-patchram-plus.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BRCM_PATCHRAM_PLUS_VERSION = 95b7b6916d661a4da3f9c0adf52d5e1f4f8ab042
+BRCM_PATCHRAM_PLUS_VERSION = 15bd6638dd6d3a37d22dbc18059f6d9eb885f057
 BRCM_PATCHRAM_PLUS_SITE = $(call github,AsteroidOS,brcm-patchram-plus,$(BRCM_PATCHRAM_PLUS_VERSION))
 BRCM_PATCHRAM_PLUS_LICENSE = Apache-2.0
 BRCM_PATCHRAM_PLUS_LICENSE_FILES = COPYING
