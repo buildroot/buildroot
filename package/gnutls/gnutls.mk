buildroot/buildroot

commit caff4179a4481233257f5f475ca57ce9061af900

package/gnutls: add tpm2-tss optional dependency
tpm2-tss support is available since gnutls 3.7.3 using
--{with,without}-tpm2 configure option [1].

Since the option is not handled by gnutls package, tpm2-tss support
can be enabled if tpm2-tss package is build before gnutls package.

Likewise, tpm2-tss support can be enabled for the gnutls host variant
if tpm2-tss libraries are installed on the host.
Make sure to disable tpm2-tss support for the host-gnutls.

[1] https://lists.gnupg.org/pipermail/gnutls-help/2022-January/004736.html

Signed-off-by: Romain Naour <romain.naour@smile.fr>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/gnutls/gnutls.mk
@@ -57,6 +57,7 @@ HOST_GNUTLS_CONF_OPTS = \
 	--without-librt-prefix \
 	--without-libz-prefix \
 	--without-tpm \
+	--without-tpm2 \
 	--disable-openssl-compatibility \
 	--without-libbrotli \
 	--without-idn \
@@ -96,6 +97,13 @@ GNUTLS_CONF_OPTS += --with-p11-kit
 GNUTLS_DEPENDENCIES += p11-kit
 else
 GNUTLS_CONF_OPTS += --without-p11-kit
+endif
+
+ifeq ($(BR2_PACKAGE_TPM2_TSS),y)
+GNUTLS_CONF_OPTS += --with-tpm2
+GNUTLS_DEPENDENCIES += tpm2-tss
+else
+GNUTLS_CONF_OPTS += --without-tpm2
 endif
 
 ifeq ($(BR2_PACKAGE_ZLIB),y)

commit 1b8e5c93d3789c900fbd5ab62badfe4d4eec61a6

package/gnutls: disable tpm2-tss
Disable tpm2-tss to avoid the following circular dependency if
BR2_PACKAGE_TPM2_TSS_FAPI is selected:
tpm2-tss -> libcurl -> gnutls -> tmp2-tss
resulting in the following build failure of libcurl or rtmpdump since
commit caff4179a4481233257f5f475ca57ce9061af900:

configure: error: TLS not detected, you will not be able to use HTTPS, FTPS, NTLM and more.
Use --with-openssl, --with-gnutls, --with-wolfssl, --with-mbedtls, --with-schannel, --with-secure-transport, --with-amissl, --with-bearssl or --with-rustls to address this.

[...]

In file included from rtmp.c:32:
rtmp_sys.h:110:10: fatal error: gnutls/gnutls.h: No such file or directory
  110 | #include <gnutls/gnutls.h>
      |          ^~~~~~~~~~~~~~~~~

Fixes: caff4179a4481233257f5f475ca57ce9061af900
 - http://autobuild.buildroot.org/results/72e1843de650c9ed4795f1141e9fd485b71a14c3
 - http://autobuild.buildroot.org/results/918f0f7efa810b17f6d2266a890bf0150cbed146

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/gnutls/gnutls.mk
@@ -30,6 +30,7 @@ GNUTLS_CONF_OPTS = \
 	--without-librt-prefix \
 	--without-libz-prefix \
 	--without-tpm \
+	--without-tpm2 \
 	$(if $(BR2_PACKAGE_GNUTLS_TOOLS),--enable-tools,--disable-tools) \
 	$(if $(BR2_PACKAGE_GNUTLS_ENABLE_SSL2),--enable,--disable)-ssl2-support \
 	$(if $(BR2_PACKAGE_GNUTLS_ENABLE_GOST),--enable,--disable)-gost
@@ -97,13 +98,6 @@ GNUTLS_CONF_OPTS += --with-p11-kit
 GNUTLS_DEPENDENCIES += p11-kit
 else
 GNUTLS_CONF_OPTS += --without-p11-kit
-endif
-
-ifeq ($(BR2_PACKAGE_TPM2_TSS),y)
-GNUTLS_CONF_OPTS += --with-tpm2
-GNUTLS_DEPENDENCIES += tpm2-tss
-else
-GNUTLS_CONF_OPTS += --without-tpm2
 endif
 
 ifeq ($(BR2_PACKAGE_ZLIB),y)