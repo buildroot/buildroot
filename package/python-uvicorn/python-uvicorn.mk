b2c1a8d2e258a07e69666dab4ef8f1267711a0ae
Marcus Hoffmann <buildroot@bubu1.eu>
2024-07-22 13:11:23 +0200

    package/python-uvicorn: bump to 0.30.3
    Changelog:
    
    * https://github.com/encode/uvicorn/releases/tag/0.30.3
    * https://github.com/encode/uvicorn/releases/tag/0.30.2
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-uvicorn/python-uvicorn.mk
+++ package/python-uvicorn/python-uvicorn.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_UVICORN_VERSION = 0.30.1
+PYTHON_UVICORN_VERSION = 0.30.3
 PYTHON_UVICORN_SOURCE = uvicorn-$(PYTHON_UVICORN_VERSION).tar.gz
-PYTHON_UVICORN_SITE = https://files.pythonhosted.org/packages/37/16/9f5ccaa1a76e5bfbaa0c67640e2db8a5214ca08d92a1b427fa1677b3da88
+PYTHON_UVICORN_SITE = https://files.pythonhosted.org/packages/77/40/b650be95700dc83d14c5f2b9eac9deb23cbca757a12ee20e473b5ef1ac48
 PYTHON_UVICORN_SETUP_TYPE = pep517
 PYTHON_UVICORN_LICENSE = BSD-3-Clause
 PYTHON_UVICORN_LICENSE_FILES = LICENSE.md
