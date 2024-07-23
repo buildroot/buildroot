buildroot/buildroot

commit 25254458bde6befcbf93b746d994f94665741c42

package/xz: explicitly specify all autoconf options
Explicitly specify all autoconf options with their default values, with
the following special cases:

- sandbox: use 'auto' (which will compile detect landlock in case
  kernel headers >= 5.13)

- unaligned-access: use 'auto' (which will default to yes for x86, x86-64,
  powerpc, powerpc64 and powcerpc64le and use compile detection for arm,
  aarch64 and riscv, see xz-5.6.0/configure line 21323 ff)

Since there are so many options, we reuse the target options for the
host. The options that are set dynamically need to be explicitly
overridden in that case.

Signed-off-by: Peter Seiderer <ps.report@gmx.net>
[Arnout:
 - fix type Z_CONF_OPTS, detected by check-package;
 - move setting of HOST_XZ_CONF_OPTS after all the target options.]
Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

package/xz/xz.mk
@@ -13,11 +13,50 @@ XZ_LICENSE = Public Domain, BSD-0-Clause, GPL-2.0+, GPL-3.0+, LGPL-2.1+
 XZ_LICENSE_FILES = COPYING COPYING.0BSD COPYING.GPLv2 COPYING.GPLv3 COPYING.LGPLv2.1
 XZ_CPE_ID_VENDOR = tukaani
 
+XZ_CONF_OPTS = \
+	--enable-encoders=lzma1,lzma2,delta,x86,powerpc,ia64,arm,armthumb,arm64,sparc,riscv \
+	--enable-decoders=lzma1,lzma2,delta,x86,powerpc,ia64,arm,armthumb,arm64,sparc,riscv \
+	--enable-match-finders=hc3,hc4,bt2,bt3,bt4 \
+	--enable-checks=crc32,crc64,sha256 \
+	--disable-external-sha256 \
+	--enable-microlzma \
+	--enable-lzip-decoder \
+	--enable-assembler \
+	--enable-clmul-crc \
+	--enable-arm64-crc32 \
+	--disable-small \
+	--enable-assume-ram=128 \
+	--enable-xz \
+	--enable-xzdec \
+	--enable-lzmadec \
+	--enable-lzmainfo \
+	--enable-lzma-links \
+	--enable-scripts \
+	--enable-sandbox=auto \
+	--enable-symbol-versions \
+	--enable-rpath \
+	--enable-largfile \
+	--enable-unaligned-access=auto \
+	--disable-unsafe-type-punning \
+	--disable-werror \
+	--disable-year2038
+
+ifeq ($(BR2_SYSTEM_ENABLE_NLS),y)
+XZ_CONF_OPTS += --enable-nls
+else
+XZ_CONF_OPTS += --disable-nls
+endif
+
 ifeq ($(BR2_TOOLCHAIN_HAS_THREADS),y)
 XZ_CONF_OPTS += --enable-threads
 else
 XZ_CONF_OPTS += --disable-threads
 endif
+
+HOST_XZ_CONF_OPTS = \
+	$(XZ_CONF_OPTS) \
+	--enable-nls \
+	--enable-threads
 
 # we are built before ccache
 HOST_XZ_CONF_ENV = \

commit 380cdf5c95f2e962b63269d8cf45a8939275d8e0

package/xz: fix largefile option
Fix typo in --enable-largefile added by commit
25254458bde6befcbf93b746d994f94665741c42

Fixes: 25254458bde6befcbf93b746d994f94665741c42

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/xz/xz.mk
@@ -38,7 +38,7 @@ XZ_CONF_OPTS = \
 	--enable-sandbox=auto \
 	--enable-symbol-versions \
 	--enable-rpath \
-	--enable-largfile \
+	--enable-largefile \
 	--enable-unaligned-access=auto \
 	--disable-unsafe-type-punning \
 	--disable-werror \