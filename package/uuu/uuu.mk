1ba78a69fd34f99282f47b78891e98d7c9c62b2f
Baruch Siach <baruch@tkos.co.il>
2024-07-23 19:28:15 +0300

    package/uuu: update upstream link
    NXP repositories moved to a new location. The old URL redirects to the
    new one.
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 3f31c4ef33eff191fc17cd0f8aa4d3d4ad9ed2c4)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/uuu/uuu.mk
+++ package/uuu/uuu.mk
@@ -6,7 +6,7 @@
 
 UUU_VERSION = 1.5.177
 UUU_SOURCE = uuu_source-uuu_$(UUU_VERSION).tar.gz
-UUU_SITE = https://github.com/NXPmicro/mfgtools/releases/download/uuu_$(UUU_VERSION)
+UUU_SITE = https://github.com/nxp-imx/mfgtools/releases/download/uuu_$(UUU_VERSION)
 UUU_LICENSE = BSD 3-Clause "New" or "Revised" License
 UUU_LICENSE_FILES = LICENSE
 HOST_UUU_DEPENDENCIES = \
