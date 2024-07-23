buildroot/buildroot

commit 70319b47f646f74f407ed3355baf5b581a10518a

package/libdex: new package
This package provides utilities for asynchronous programming in C
using a futures/promises model. Newer versions of sysprof require
this library.

https://gitlab.gnome.org/GNOME/libdex

Signed-off-by: Adrian Perez de Castro <aperez@igalia.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/libdex/libdex.mk
@@ -1,0 +1,31 @@
+################################################################################
+#
+# libdex
+#
+################################################################################
+
+LIBDEX_VERSION_MAJOR = 0.6
+LIBDEX_VERSION = $(LIBDEX_VERSION_MAJOR).1
+LIBDEX_SOURCE = libdex-$(LIBDEX_VERSION).tar.xz
+LIBDEX_SITE = https://download.gnome.org/sources/libdex/$(LIBDEX_VERSION_MAJOR)
+LIBDEX_LICENSE = LGPL-2.1+
+LIBDEX_LICENSE_FILES = COPYING
+LIBDEX_INSTALL_STAGING = YES
+LIBDEX_DEPENDENCIES = libglib2
+LIBDEX_CONF_OPTS = \
+	-Ddocs=false \
+	-Dexamples=false \
+	-Dsysprof=false \
+	-Dtests=false \
+	-Dvapi=false \
+	-Deventfd=enabled \
+	-Dintrospection=disabled
+
+ifeq ($(BR2_PACKAGE_LIBURING),y)
+LIBDEX_CONF_OPTS += -Dliburing=enabled
+LIBDEX_DEPENDENCIES += liburing
+else
+LIBDEX_CONF_OPTS += -Dliburing=disabled
+endif
+
+$(eval $(meson-package))

DEVELOPERS
@@ -55,6 +55,7 @@ F:	package/brotli/
 F:	package/bubblewrap/
 F:	package/cage/
 F:	package/cog/
+F:	package/libdex/
 F:	package/libepoxy/
 F:	package/libmanette/
 F:	package/libpsl/

package/Config.in
@@ -2163,6 +2163,7 @@ menu "Other"
 	source "package/libcrossguid/Config.in"
 	source "package/libcsv/Config.in"
 	source "package/libdaemon/Config.in"
+	source "package/libdex/Config.in"
 	source "package/libdill/Config.in"
 	source "package/libeastl/Config.in"
 	source "package/libee/Config.in"

package/libdex/Config.in
@@ -1,0 +1,17 @@
+config BR2_PACKAGE_LIBDEX
+	bool "libdex"
+	depends on BR2_USE_MMU # libglib2
+	depends on BR2_USE_WCHAR # libglib2, gettext
+	depends on BR2_TOOLCHAIN_HAS_THREADS # libglib2
+	select BR2_PACKAGE_LIBGLIB2
+	help
+	  Dex provides Future-based programming for GLib-based
+	  applications. It both integrates with and brings new
+	  features for application and library authors who want
+	  to structure concurrent code in an easy to manage way.
+
+	  https://gitlab.gnome.org/GNOME/libdex
+
+comment "libdex needs a toolchain w/ wchar, threads"
+	depends on BR2_USE_MMU
+	depends on !BR2_USE_WCHAR || !BR2_TOOLCHAIN_HAS_THREADS

package/libdex/libdex.hash
@@ -1,0 +1,5 @@
+# From https://download.gnome.org/sources/libdex/0.6/libdex-0.6.1.sha256sum
+sha256  d176de6578571e32a8c0b603b6a5a13fa5f87fb6b5442575b38ec5af16b17a92  libdex-0.6.1.tar.xz
+
+# License files
+sha256  dc626520dcd53a22f727af3ee42c770e56c97a64fe3adb063799d8ab032fe551  COPYING

commit 482a27a7be85e17f689b77d5d21e55cacbfd06fa

package/libdex: fix build without SSP
Fix the following build failure without stack-protector raised since the
addition of the package in commit
70319b47f646f74f407ed3355baf5b581a10518a:

/home/buildroot/instance-0/output-1/host/opt/ext-toolchain/bin/../lib/gcc/i686-buildroot-linux-uclibc/9.3.0/../../../../i686-buildroot-linux-uclibc/bin/ld: src/libdex-1.a.p/dex-async-pair.c.o: in function `dex_async_pair_ready_callback':
dex-async-pair.c:(.text+0x118): undefined reference to `__stack_chk_guard'

Fixes: 70319b47f646f74f407ed3355baf5b581a10518a
 - http://autobuild.buildroot.org/results/f53768f654a9eddd95b583a366cd7386d9ddb285

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/libdex/0001-add-stack-protector-option.patch
@@ -1,0 +1,45 @@
+From 107efb2f1a014c4475e122677faddbe0dbef1000 Mon Sep 17 00:00:00 2001
+From: Fabrice Fontaine <fontaine.fabrice@gmail.com>
+Date: Tue, 23 Jul 2024 14:54:59 +0200
+Subject: [PATCH] add stack-protector option
+
+Allow the user to disable stack-protector as it is not supported by all
+toolchains
+
+Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
+Upstream: https://gitlab.gnome.org/GNOME/libdex/-/commit/a65d33aebbe43024a046b3f3cc4a1c6c533838d0
+---
+ meson.build       | 2 +-
+ meson_options.txt | 3 +++
+ 2 files changed, 4 insertions(+), 1 deletion(-)
+
+diff --git a/meson.build b/meson.build
+index d0ca81d..2f7aac2 100644
+--- a/meson.build
++++ b/meson.build
+@@ -126,7 +126,7 @@ test_c_args = [
+   ['-Werror=format-security', '-Werror=format=2'],
+   '-FImsvc_recommended_pragmas.h',
+ ]
+-if get_option('buildtype') != 'plain'
++if get_option('buildtype') != 'plain' and get_option('stack-protector')
+   if host_machine.system() != 'windows'
+     test_c_args += '-fstack-protector-strong'
+   endif
+diff --git a/meson_options.txt b/meson_options.txt
+index 519cd97..9e3364a 100644
+--- a/meson_options.txt
++++ b/meson_options.txt
+@@ -4,6 +4,9 @@ option('docs',
+ option('examples',
+        type: 'boolean', value: true,
+        description: 'Build example programs')
++option('stack-protector',
++       type: 'boolean', value: true,
++       description: 'Enable stack-protector')
+ option('vapi',
+        type: 'boolean', value: true,
+        description: 'Generate vapi data (requires vapigen)')
+-- 
+GitLab
+

package/libdex/libdex.mk
@@ -15,6 +15,7 @@ LIBDEX_DEPENDENCIES = libglib2
 LIBDEX_CONF_OPTS = \
 	-Ddocs=false \
 	-Dexamples=false \
+	-Dstack-protector=false \
 	-Dsysprof=false \
 	-Dtests=false \
 	-Dvapi=false \