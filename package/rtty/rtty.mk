bf24fbf66ae110c679dfd27a5c4c12235eddd3ab
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-07-23 15:30:04 +0200

    package/rtty: bump to version 8.1.2
    - Drop patch (already in version)
    - This bump will fix the following build failure with musl >= 1.2.5
      thanks to
      https://github.com/zhaojh329/rtty/commit/eb6394c3d83a21a8825e47b40659cfe0ead33b6f:
    
      /home/autobuild/autobuild/instance-22/output-1/build/rtty-8.1.0/src/file.c: In function 'start_upload_file':
      /home/autobuild/autobuild/instance-22/output-1/build/rtty-8.1.0/src/file.c:156:24: error: implicit declaration of function 'basename' [-Werror=implicit-function-declaration]
        156 |     const char *name = basename(path);
            |                        ^~~~~~~~
    
    https://github.com/zhaojh329/rtty/releases/tag/v8.1.2
    https://github.com/zhaojh329/rtty/releases/tag/v8.1.1
    
    Fixes:
     - http://autobuild.buildroot.org/results/382405b421a8ea7b5b3beb553f47fa20427fa3c3
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit ca742a985cc9acc40fff3539c696e64fa040f1c4)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rtty/rtty.mk
+++ package/rtty/rtty.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTTY_VERSION = 8.1.0
+RTTY_VERSION = 8.1.2
 RTTY_SITE = https://github.com/zhaojh329/rtty/releases/download/v$(RTTY_VERSION)
 RTTY_LICENSE = MIT
 RTTY_LICENSE_FILES = LICENSE
