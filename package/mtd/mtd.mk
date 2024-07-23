buildroot/buildroot

commit bb19ffeaab5ba4be0a1033ca326b25d4d4e8203f

package/mtd: make zlib an optional dependency
Upstream changed zlib to be an optional dependency in release 2.2.0[1]

See https://github.com/sigma-star/mtd-utils/commit/2465754716634422620b4577e0b7ba79c4ce1a39

Signed-off-by: Brandon Maier <brandon.maier@collins.com>
Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

package/mtd/mtd.mk
@@ -27,17 +27,24 @@ else
 MTD_CONF_OPTS += --without-lzo
 endif
 
+ifeq ($(BR2_PACKAGE_ZLIB),y)
+MTD_DEPENDENCIES += zlib
+MTD_CONF_OPTS += --with-zlib
+else
+MTD_CONF_OPTS += --without-zlib
+endif
+
 MTD_CONF_ENV += LDFLAGS="$(MTD_LDFLAGS)"
 
 ifeq ($(BR2_PACKAGE_MTD_JFFS_UTILS),y)
-MTD_DEPENDENCIES += zlib host-pkgconf
+MTD_DEPENDENCIES += host-pkgconf
 MTD_CONF_OPTS += --with-jffs
 else
 MTD_CONF_OPTS += --without-jffs
 endif
 
 ifeq ($(BR2_PACKAGE_MTD_UBIFS_UTILS),y)
-MTD_DEPENDENCIES += util-linux zlib host-pkgconf
+MTD_DEPENDENCIES += util-linux host-pkgconf
 MTD_CONF_OPTS += --with-ubifs
 # crypto needs linux/hash_info.h
 ifeq ($(BR2_TOOLCHAIN_HEADERS_AT_LEAST_4_12)$(BR2_PACKAGE_OPENSSL),yy)

package/mtd/Config.in
@@ -12,13 +12,11 @@ if BR2_PACKAGE_MTD
 
 config BR2_PACKAGE_MTD_JFFS_UTILS
 	bool
-	select BR2_PACKAGE_ZLIB
 
 config BR2_PACKAGE_MTD_UBIFS_UTILS
 	bool
 	select BR2_PACKAGE_UTIL_LINUX
 	select BR2_PACKAGE_UTIL_LINUX_LIBUUID
-	select BR2_PACKAGE_ZLIB
 
 comment "MTD tools selection"
 

commit ef420e2016c4f1746e9aa72e160639e14ae7f406

package/mtd: fix zlib build
Commit bb19ffeaab5ba4be0a1033ca326b25d4d4e8203f forgot to move
host-pkgconf dependency from BR2_PACKAGE_MTD_JFFS_UTILS condition to the
new BR2_PACKAGE_ZLIB condition resulting in the following build failure:

hecking for ZLIB... no
configure: error: cannot find zlib

Fixes: bb19ffeaab5ba4be0a1033ca326b25d4d4e8203f
 - http://autobuild.buildroot.org/results/7cbeead738af6302d5f0048a443e3b57d6776bb2

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/mtd/mtd.mk
@@ -28,7 +28,7 @@ MTD_CONF_OPTS += --without-lzo
 endif
 
 ifeq ($(BR2_PACKAGE_ZLIB),y)
-MTD_DEPENDENCIES += zlib
+MTD_DEPENDENCIES += host-pkgconf zlib
 MTD_CONF_OPTS += --with-zlib
 else
 MTD_CONF_OPTS += --without-zlib
@@ -37,7 +37,6 @@ endif
 MTD_CONF_ENV += LDFLAGS="$(MTD_LDFLAGS)"
 
 ifeq ($(BR2_PACKAGE_MTD_JFFS_UTILS),y)
-MTD_DEPENDENCIES += host-pkgconf
 MTD_CONF_OPTS += --with-jffs
 else
 MTD_CONF_OPTS += --without-jffs