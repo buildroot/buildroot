0cd5f986c3550d425369fe0061ea9dc0cbf25b4d
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2024-07-27 18:57:15 +0200

    package/open-iscsi: open-isns is optional, not mandatory
    open-isns is optional, not mandatory, since bump to version 2.1.9 in
    commit 2314928cf828ff02939b9c0041964c3ee2dd907d and
    https://github.com/open-iscsi/open-iscsi/commit/713524df809396e11f42d7691bd8ead2ffc1ca6b
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 948b1830423421e087f6aa4923a37ce077ed5904)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/open-iscsi/open-iscsi.mk
+++ package/open-iscsi/open-iscsi.mk
@@ -9,9 +9,16 @@ OPEN_ISCSI_SITE = $(call github,open-iscsi,open-iscsi,$(OPEN_ISCSI_VERSION))
 OPEN_ISCSI_LICENSE = GPL-2.0+, GPL-3.0+, LGPL-3.0+
 OPEN_ISCSI_LICENSE_FILES = COPYING README libopeniscsiusr/COPYING
 OPEN_ISCSI_CPE_ID_VALID = YES
-OPEN_ISCSI_DEPENDENCIES = kmod open-isns openssl util-linux
+OPEN_ISCSI_DEPENDENCIES = kmod openssl util-linux
 
 OPEN_ISCSI_CONF_OPTS = -Ddbroot=/var/lib/iscsi
+
+ifeq ($(BR2_PACKAGE_OPEN_ISNS),y)
+OPEN_ISCSI_DEPENDENCIES += open-isns
+OPEN_ISCSI_CONF_OPTS += -Disns=enabled
+else
+OPEN_ISCSI_CONF_OPTS += -Disns=disabled
+endif
 
 ifeq ($(BR2_PACKAGE_SYSTEMD),y)
 OPEN_ISCSI_DEPENDENCIES += systemd
