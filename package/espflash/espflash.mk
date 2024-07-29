c4e845b5b805cc487fca6203caccc363e6e108e6
Torben Voltmer <mail@t-voltmer.net>
2024-07-29 21:32:43 +0200

    package/espflash: add udev dependency
    Add the dependency on udev, since espflash links against libudev
    (via the libudev-sys crate).
    
    Fixes:
    
      http://autobuild.buildroot.net/results/c61b6ed4b6fb4d18d503ac37add200f5e6ae3706
    
    Signed-off-by: Torben Voltmer <mail@t-voltmer.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/espflash/espflash.mk
+++ package/espflash/espflash.mk
@@ -9,5 +9,6 @@ ESPFLASH_SITE = $(call github,esp-rs,espflash,v$(ESPFLASH_VERSION))
 ESPFLASH_SUBDIR = espflash
 ESPFLASH_LICENSE = Apache-2.0 or MIT
 ESPFLASH_LICENSE_FILES = LICENSE-APACHE LICENSE-MIT
+ESPFLASH_DEPENDENCIES = udev
 
 $(eval $(cargo-package))
