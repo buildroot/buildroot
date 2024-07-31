345f48304b553b5aed92c6f5a8a56a160f7278c9
Julien Olivain <ju.o@free.fr>
2024-07-31 23:28:24 +0200

    package/fwts: bump to version 24.07.00
    Release announce:
    https://lists.ubuntu.com/archives/fwts-devel/2024-July/013869.html
    
    For release note since 24.03.00, see:
    https://wiki.ubuntu.com/FirmwareTestSuite/ReleaseNotes/24.07.00
    
    This commit removes the patch 0002, which is included in this new
    version.
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/fwts/fwts.mk
+++ package/fwts/fwts.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FWTS_VERSION = 24.03.00
+FWTS_VERSION = 24.07.00
 FWTS_SOURCE = fwts-V$(FWTS_VERSION).tar.gz
 FWTS_SITE = https://fwts.ubuntu.com/release
 FWTS_STRIP_COMPONENTS = 0
