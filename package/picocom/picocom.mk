99f65ecb6a16e75d92c3f88e4e01959a2948e4d9
Baruch Siach <baruch@tkos.co.il>
2024-08-01 15:40:57 +0300

    package/picocom: bump to version 2024-07
    This release adds support for no-MMU build.
    
    Cc: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/picocom/picocom.mk
+++ package/picocom/picocom.mk
@@ -4,14 +4,19 @@
 #
 ################################################################################
 
-PICOCOM_VERSION = 2023-04
+PICOCOM_VERSION = 2024-07
 PICOCOM_SITE = $(call gitlab,wsakernel,picocom,$(PICOCOM_VERSION))
 PICOCOM_LICENSE = GPL-2.0+
 PICOCOM_LICENSE_FILES = LICENSE.txt
 PICOCOM_CPE_ID_VALID = YES
 
+ifeq ($(BR2_USE_MMU),)
+PICOCOM_CPPFLAGS += -DNO_FORK
+endif
+
 define PICOCOM_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) \
+		EXTRA_CPPFLAGS="$(PICOCOM_CPPFLAGS)" $(MAKE) -C $(@D)
 endef
 
 define PICOCOM_INSTALL_TARGET_CMDS
