a0d1df2ac511404241f6f74d00bcde4b8fdeba8b
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2024-08-02 19:44:56 +0200

    package/bcusdk: fix build with host GCC 14.x
    With GCC 14.x as the host compiler, bcusdk fails to build as follows:
    
    configure: error: Specified CC_FOR_BUILD doesn't seem to work
    
    This is due to missing includes in the test programs used in the
    configure script to check the host compiler. We fix this with patch
    0003.
    
    However, this patch requires to autoreconf the package, and autoreconf
    would need the definition of AM_PATH_XML2, which would require libxml2
    even though we don't have libxml2 as a dependency of this package (we
    don't enable the features that requires libxml2). As it turns out that
    the AM_PATH_XML2 macro is in fact deprecated, we replaced it by its
    equivalent using PKG_CHECK_MODULES(), which is in fact exactly how
    AM_PATH_XML2 is implemented in upstream libxml2.
    
    Fixes:
    
      http://autobuild.buildroot.net/results/458880bd6c207e5bb7afce1a1186f204c30c0941/
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit e4109c1d2d28f737409dce2e346661cbf20084c6)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/bcusdk/bcusdk.mk
+++ package/bcusdk/bcusdk.mk
@@ -10,6 +10,9 @@ BCUSDK_SITE = http://www.auto.tuwien.ac.at/~mkoegler/eib
 BCUSDK_LICENSE = GPL-2.0+
 BCUSDK_LICENSE_FILES = COPYING
 BCUSDK_INSTALL_STAGING = YES
+# 0003-m4-ccforbuild.m4-include-stdlib.h-in-test-program.patch
+# 0004-configure.in-replace-obsolete-AM_PATH_XML2-by-PKG_CH.patch
+BCUSDK_AUTORECONF = YES
 BCUSDK_CONF_OPTS = \
 	--enable-onlyeibd \
 	--enable-ft12 \
@@ -21,7 +24,8 @@ BCUSDK_CONF_OPTS = \
 	--without-pth-test \
 	--with-pth=$(STAGING_DIR)/usr
 
-BCUSDK_DEPENDENCIES = libpthsem
+# host-pkgconf is only needed because of autoreconf
+BCUSDK_DEPENDENCIES = libpthsem host-pkgconf
 
 ifeq ($(BR2_PACKAGE_ARGP_STANDALONE),y)
 BCUSDK_DEPENDENCIES += argp-standalone $(TARGET_NLS_DEPENDENCIES)
