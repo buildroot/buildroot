74ac6999029a482bf0a39fab8b720b454a66a0db
Bernd Kuhls <bernd@kuhls.net>
2024-08-03 12:46:51 +0200

    package/kodi-pvr-vbox: bump version to 21.1.4-Omega
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-pvr-vbox/kodi-pvr-vbox.mk
+++ package/kodi-pvr-vbox/kodi-pvr-vbox.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PVR_VBOX_VERSION = 21.1.3-Omega
+KODI_PVR_VBOX_VERSION = 21.1.4-Omega
 KODI_PVR_VBOX_SITE = $(call github,kodi-pvr,pvr.vbox,$(KODI_PVR_VBOX_VERSION))
 KODI_PVR_VBOX_LICENSE = GPL-2.0+
 KODI_PVR_VBOX_LICENSE_FILES = LICENSE.md
