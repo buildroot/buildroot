761259c93400bc806611a242c7dc3df7ff67c231
Waldemar Brodkorb <wbx@openadk.org>
2024-08-05 16:16:01 +0200

    package/nginx: security update to 1.26.1
    See here for a Changelog and CVE's:
    http://nginx.org/en/CHANGES-1.26
    
    Patch 0006 is no longer required as the openssl library is found without
    this patch, which does not apply anymore.
    
    Patch 0009 is no longer required as it was fixed in another way upstream:
    https://hg.nginx.org/nginx/rev/fb989e24c60a
    
    Patch 0011 is upstream:
    https://hg.nginx.org/nginx/rev/f58b6f636238
    
    Reorder the remaining patches and update .checkpackageignore accordingly.
    
    The LICENSE file is changed, the year changed from 2022 to 2024.
    
    Signed-off-by: Waldemar Brodkorb <wbx@openadk.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/nginx/nginx.mk
+++ package/nginx/nginx.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NGINX_VERSION = 1.24.0
+NGINX_VERSION = 1.26.1
 NGINX_SITE = https://nginx.org/download
 NGINX_LICENSE = BSD-2-Clause
 NGINX_LICENSE_FILES = LICENSE
