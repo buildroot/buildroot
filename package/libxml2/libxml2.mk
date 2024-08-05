0eb09e7d377f30b54b329e20ccf4340d493eabde
Bernd Kuhls <bernd@kuhls.net>
2024-08-05 21:09:10 +0200

    package/libxml2: bump version to 2.13.3
    Add configure option --with-http to enable HTTP support which is now
    disabled by default to preserve previous behaviour.
    
    Release notes:
    https://download.gnome.org/sources/libxml2/2.13/libxml2-2.13.0.news
    https://download.gnome.org/sources/libxml2/2.13/libxml2-2.13.1.news
    https://download.gnome.org/sources/libxml2/2.13/libxml2-2.13.2.news
    https://download.gnome.org/sources/libxml2/2.13/libxml2-2.13.3.news
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libxml2/libxml2.mk
+++ package/libxml2/libxml2.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-LIBXML2_VERSION_MAJOR = 2.12
-LIBXML2_VERSION = $(LIBXML2_VERSION_MAJOR).9
+LIBXML2_VERSION_MAJOR = 2.13
+LIBXML2_VERSION = $(LIBXML2_VERSION_MAJOR).3
 LIBXML2_SOURCE = libxml2-$(LIBXML2_VERSION).tar.xz
 LIBXML2_SITE = \
 	https://download.gnome.org/sources/libxml2/$(LIBXML2_VERSION_MAJOR)
@@ -20,7 +20,7 @@ ifeq ($(BR2_m68k_cf),y)
 LIBXML2_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -mxgot"
 endif
 
-LIBXML2_CONF_OPTS = --with-gnu-ld --without-debug
+LIBXML2_CONF_OPTS = --with-http --with-gnu-ld --without-debug
 
 HOST_LIBXML2_DEPENDENCIES = host-pkgconf
 LIBXML2_DEPENDENCIES = host-pkgconf
