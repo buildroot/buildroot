7c4f3014ef9128a1d743e96452d388a3343c1150
Zoltan Gyarmati <zgyarmati@zgyarmati.de>
2024-08-07 22:06:56 +0200

    package/grantlee: bump to 5.3.1
    The hash of the license file has changed due to the addition of this
    line:
    
    +Grantlee is copyright Stephen Kelly <steveire@gmail.com> and others.
    
    Signed-off-by: Zoltan Gyarmati <zgyarmati@zgyarmati.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/grantlee/grantlee.mk
+++ package/grantlee/grantlee.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GRANTLEE_VERSION = 5.2.0
+GRANTLEE_VERSION = 5.3.1
 GRANTLEE_SITE = $(call github,steveire,grantlee,v$(GRANTLEE_VERSION))
 GRANTLEE_INSTALL_STAGING = YES
 GRANTLEE_LICENSE = LGPL-2.1+
