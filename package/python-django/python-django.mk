90cd6b3574b9425b0534e11a199331a0950b8856
Marcus Hoffmann <buildroot@bubu1.eu>
2024-08-07 10:22:50 +0200

    package/python-django: security bump to version 5.0.8
    Django 5.0.7 fixes the following CVEs:
    
    * CVE-2024-38875: Potential denial-of-service vulnerability in django.utils.html.urlize()
    * CVE-2024-39329: Username enumeration through timing difference for users with unusable passwords
    * CVE-2024-39330: Potential directory-traversal via Storage.save()
    * CVE-2024-39614: Potential denial-of-service vulnerability in get_supported_language_variant()
    
    Django 5.0.8 fixes the following CVEs:
    
    * CVE-2024-41989: Memory exhaustion in django.utils.numberformat.floatformat()
    * CVE-2024-41990: Potential denial-of-service vulnerability in django.utils.html.urlize()
    * CVE-2024-41991: Potential denial-of-service vulnerability in django.utils.html.urlize() and AdminURLFieldWidget
    * CVE-2024-42005: Potential SQL injection in QuerySet.values() and values_list()
    
    Further release Notes: https://docs.djangoproject.com/en/5.0/releases/
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit f777ce1fd6b9e0537593a70940dc23f4ca177054)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 5.0.3
+PYTHON_DJANGO_VERSION = 5.0.8
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/e1/b1/ac6a16aaf0049637b50afbcf06b8ec2fa5c6ce42d4ae6ba66bbaf4c3609a
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/79/1c/55733805bb735e26fee0430045efdb61df6fd704b6aebf2154875ef9e6a9
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
 PYTHON_DJANGO_CPE_ID_VENDOR = djangoproject
