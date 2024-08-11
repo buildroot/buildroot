5b54c1166b78fa0b36183d58be2fd0ff140ee41f
J. Neuschäfer <j.neuschaefer@gmx.net>
2024-08-11 12:01:13 +0200

    package/s6-linux-utils: add option to build multicall binary
    Since version 2.6.1.0, with another bugfix in 2.6.2.0, s6-linux-utils
    can be built as a multicall binary, similar to busybox.
    
    Signed-off-by: J. Neuschäfer <j.neuschaefer@gmx.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/s6-linux-utils/s6-linux-utils.mk
+++ package/s6-linux-utils/s6-linux-utils.mk
@@ -17,6 +17,7 @@ S6_LINUX_UTILS_CONF_OPTS = \
 	--with-dynlib=$(STAGING_DIR)/lib \
 	--with-lib=$(STAGING_DIR)/lib/skalibs \
 	$(if $(BR2_STATIC_LIBS),,--disable-allstatic) \
+	$(if $(BR2_PACKAGE_S6_LINUX_UTILS_MULTICALL),--enable-multicall,) \
 	$(SHARED_STATIC_LIBS_OPTS)
 
 define S6_LINUX_UTILS_CONFIGURE_CMDS
