d8335bee355309408f95714ebdb10dd0656cb441
Marcus Hoffmann <buildroot@bubu1.eu>
2024-08-16 17:38:24 +0200

    package/python-fastapi: bump to 0.112.1
    Changelog:
    * https://github.com/fastapi/fastapi/releases/tag/0.111.1
    * https://github.com/fastapi/fastapi/releases/tag/0.112.0
    * https://github.com/fastapi/fastapi/releases/tag/0.112.1
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-fastapi/python-fastapi.mk
+++ package/python-fastapi/python-fastapi.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_FASTAPI_VERSION = 0.111.0
+PYTHON_FASTAPI_VERSION = 0.112.1
 PYTHON_FASTAPI_SOURCE = fastapi-$(PYTHON_FASTAPI_VERSION).tar.gz
-PYTHON_FASTAPI_SITE = https://files.pythonhosted.org/packages/0e/1f/f4a99e92c583780787e04b05aa9d8a8db9ec76d091d81545948a006f5b44
+PYTHON_FASTAPI_SITE = https://files.pythonhosted.org/packages/2c/09/71a961740a1121d7cc90c99036cc3fbb507bf0c69860d08d4388f842196b
 PYTHON_FASTAPI_SETUP_TYPE = pep517
 PYTHON_FASTAPI_LICENSE = MIT
 PYTHON_FASTAPI_LICENSE_FILES = LICENSE
