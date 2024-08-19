70519b2d239a3336869c2eb3814f8e124c43a564
Waldemar Brodkorb <wbx@openadk.org>
2024-08-19 23:23:13 +0200

    package/memcached: update to 1.6.29
    See the release notes for 1.6.29 here:
    https://github.com/memcached/memcached/wiki/ReleaseNotes1629
    Older release notes are also available.
    
    Patch 0001 is upstream since commit
    aceefca9bc7635eab4893f626aec78e9966db93c.
    
    Patch 0002 is upstream since commit
    d9bf02f6331fc0098af8997d2a571fc4525696fc.
    
    Autoreconf of package is no longer needed.
    
    Signed-off-by: Waldemar Brodkorb <wbx@openadk.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/memcached/memcached.mk
+++ package/memcached/memcached.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MEMCACHED_VERSION = 1.6.22
+MEMCACHED_VERSION = 1.6.29
 MEMCACHED_SITE = http://www.memcached.org/files
 MEMCACHED_DEPENDENCIES = libevent
 MEMCACHED_CONF_ENV = ac_cv_prog_cc_c99='-std=gnu99'
@@ -13,8 +13,6 @@ MEMCACHED_LICENSE = BSD-3-Clause
 MEMCACHED_LICENSE_FILES = COPYING
 MEMCACHED_CPE_ID_VENDOR = memcached
 MEMCACHED_SELINUX_MODULES = memcached
-# We're patching configure.ac
-MEMCACHED_AUTORECONF = YES
 
 ifeq ($(BR2_ENDIAN),"BIG")
 MEMCACHED_CONF_ENV += ac_cv_c_endian=big
