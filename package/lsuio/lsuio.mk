8bd00c16d8af2b4b620a0b2c136e6830b9d6b3a0
Vincent Jardin <vjardin@free.fr>
2024-08-21 11:50:44 +0200

    package/lsuio: change upstream, bump version
    The new version from peter@digitalbrains.com adds some few new
    features (name, offset, etc.) and it includes a few improvements.
    
    The former version was more than 14y old.
    
    There is no tag, neither any archive, so currently we use the latest
    git commit.
    
    Signed-off-by: Vincent Jardin <vjardin@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/lsuio/lsuio.mk
+++ package/lsuio/lsuio.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-LSUIO_VERSION = 0.2.0
-LSUIO_SITE = http://www.osadl.org/projects/downloads/UIO/user
+# no tag yet
+LSUIO_VERSION = ba46808da148552462e50ba66d8d15cc5b9a3a00
+LSUIO_SITE = $(call github,DigitalBrains1,lsuio,$(LSUIO_VERSION))
 LSUIO_LICENSE = GPL-2.0
 LSUIO_LICENSE_FILES = COPYING
 
