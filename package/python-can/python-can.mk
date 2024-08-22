33b9374d222598230802025dc3025692cc4e3653
Marcus Hoffmann <buildroot@bubu1.eu>
2024-08-22 11:59:52 +0200

    package/python-can: bump to 4.4.2
    The tarball name prefix changed from python-can to python_can.
    
    Changelog:
    * https://github.com/hardbyte/python-can/blob/main/CHANGELOG.md#version-442
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-can/python-can.mk
+++ package/python-can/python-can.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-PYTHON_CAN_VERSION = 4.3.1
-PYTHON_CAN_SITE = https://files.pythonhosted.org/packages/02/fc/d5fd33ee93f17a4eb0dcd75aebf522396e3f511bf474058e99e86ae4e33f
+PYTHON_CAN_VERSION = 4.4.2
+PYTHON_CAN_SOURCE = python_can-$(PYTHON_CAN_VERSION).tar.gz
+PYTHON_CAN_SITE = https://files.pythonhosted.org/packages/a3/17/57c38abbf00993ac5ec306de0c00271685d1372ef3d907b107eb63ab13a8
 PYTHON_CAN_SETUP_TYPE = setuptools
 PYTHON_CAN_LICENSE = LGPL-3.0
 PYTHON_CAN_LICENSE_FILES = LICENSE.txt
