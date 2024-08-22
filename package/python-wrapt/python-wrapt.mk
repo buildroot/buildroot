6ff3768333f224f81a6afa4802755de9b125ca84
Marcus Hoffmann <buildroot@bubu1.eu>
2024-08-22 14:45:38 +0200

    package/python-wrapt: bump to 1.16.0
    Changelog:
    * https://wrapt.readthedocs.io/en/latest/changes.html#version-1-15-0
    * https://wrapt.readthedocs.io/en/latest/changes.html#version-1-16-0
    
    LICENSE hash changed due to updated copyright year [1].
    
    [1] https://github.com/GrahamDumpleton/wrapt/commit/dc5afd39533ce0daf933507f8a90c095448a5cbc
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-wrapt/python-wrapt.mk
+++ package/python-wrapt/python-wrapt.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_WRAPT_VERSION = 1.14.1
+PYTHON_WRAPT_VERSION = 1.16.0
 PYTHON_WRAPT_SOURCE = wrapt-$(PYTHON_WRAPT_VERSION).tar.gz
-PYTHON_WRAPT_SITE = https://files.pythonhosted.org/packages/11/eb/e06e77394d6cf09977d92bff310cb0392930c08a338f99af6066a5a98f92
+PYTHON_WRAPT_SITE = https://files.pythonhosted.org/packages/95/4c/063a912e20bcef7124e0df97282a8af3ff3e4b603ce84c481d6d7346be0a
 PYTHON_WRAPT_SETUP_TYPE = setuptools
 PYTHON_WRAPT_LICENSE = BSD-2-Clause
 PYTHON_WRAPT_LICENSE_FILES = LICENSE
