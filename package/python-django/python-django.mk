26905d3d5802c8126b50ddff93a9bc73b3d30656
Marcus Hoffmann <buildroot@bubu1.eu>
2024-08-22 13:33:29 +0200

    package/python-django: bump to 5.1
    Release notes: https://docs.djangoproject.com/en/5.1/releases/5.1/
    
    We need to add --skip-dependency-check to build options as django
    currently pins setuptools <69.3 [1] and buildroot uses a newer version.
    
    The Django pin is likely to not be affected by PEP-625 [2] handling,
    which was added to setuptools 69.3 [3][4]. We don't really care about
    the sdist name changing for django though, so we can use a newer version
    of setuptools as well.
    
    Django has been confirmed to still install and work correctly by running
    the runtime test.
    
    [1] https://github.com/django/django/commit/4686541691dbe986f58ac87630c3b7a04db4ff93
    [2] https://peps.python.org/pep-0625/
    [3] https://github.com/pypa/setuptools/issues/3593
    [4] https://github.com/pypa/setuptools/blob/main/NEWS.rst#v6930
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,14 +4,15 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 5.0.8
+PYTHON_DJANGO_VERSION = 5.1
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/79/1c/55733805bb735e26fee0430045efdb61df6fd704b6aebf2154875ef9e6a9
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/1e/0c/d854d25bb74a8a3b41e642bbd27fe6af12fadd0edfd07d487809cf0ef719
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
 PYTHON_DJANGO_CPE_ID_VENDOR = djangoproject
 PYTHON_DJANGO_CPE_ID_PRODUCT = django
 PYTHON_DJANGO_SETUP_TYPE = setuptools
+PYTHON_DJANGO_BUILD_OPTS = --skip-dependency-check
 
 $(eval $(python-package))
