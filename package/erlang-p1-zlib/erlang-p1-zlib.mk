f46a473a796bd77283a1b706e5a11701bda7f571
Johan Oudinet <johan.oudinet@gmail.com>
2024-08-29 16:49:37 +0200

    package/erlang-p1-zlib: bump version to 1.0.13
    Signed-off-by: Johan Oudinet <johan.oudinet@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/erlang-p1-zlib/erlang-p1-zlib.mk
+++ package/erlang-p1-zlib/erlang-p1-zlib.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ERLANG_P1_ZLIB_VERSION = 1.0.12
+ERLANG_P1_ZLIB_VERSION = 1.0.13
 ERLANG_P1_ZLIB_SITE = $(call github,processone,ezlib,$(ERLANG_P1_ZLIB_VERSION))
 ERLANG_P1_ZLIB_LICENSE = Apache-2.0
 ERLANG_P1_ZLIB_LICENSE_FILES = LICENSE.txt
