e1e95a78ae171c15ebaf76056cc8ce3708c610e8
Johan Oudinet <johan.oudinet@gmail.com>
2024-08-29 16:49:21 +0200

    package/erlang-xmpp: renamed from erlang-p1-xmpp
    This package has been renamed to avoid patching the packages that expect
    to find its content in the xmpp/ directory. Thus, remove the
    corresponding patch in the ejabberd package.
    
    Signed-off-by: Johan Oudinet <johan.oudinet@gmail.com>
    [yann.morin.1998@free.fr: update .checkpackageignore]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/ejabberd/ejabberd.mk
+++ package/ejabberd/ejabberd.mk
@@ -12,8 +12,8 @@ EJABBERD_CPE_ID_VENDOR = process-one
 EJABBERD_DEPENDENCIES = getent openssl erlang-eimp host-erlang-lager \
 	erlang-lager erlang-p1-cache-tab erlang-p1-sip \
 	erlang-p1-stringprep erlang-stun erlang-p1-tls \
-	erlang-p1-utils erlang-fast-xml erlang-p1-xmpp erlang-p1-yaml \
-	erlang-p1-zlib host-erlang-p1-utils host-erlang-p1-xmpp
+	erlang-p1-utils erlang-fast-xml erlang-xmpp erlang-p1-yaml \
+	erlang-p1-zlib host-erlang-p1-utils host-erlang-xmpp
 
 # 0001-Makefile.in-do-not-download-or-compile-dependencies.patch
 # updates Makefile.in
