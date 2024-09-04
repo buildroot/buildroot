7b4561650d6b0d76d04a81f7fbe6c5630ec93087
Adam Duskett <adam.duskett@amarulasolutions.com>
2024-09-04 16:25:08 +0200

    package/mender-artifact: bump version to 3.11.2
    mender-artifact now explicitly depends on host-openssl and uses host-pkgconf
    to find the correct libraries.
    
    Use the same variables as defined in HOST_MAKE_ENV (in package/Makefile.in) for
    HOST_MENDER_ARTIFACT_GO_ENV.
    
    The updated mender-artifact package introduces the write bootstrap-artifact
    argument which is needed for mender versions >= 3.5.1.
    
    License changes:
    New:
      Apache 2.0:
        vendor/google.golang.org/genproto/googleapis/api/LICENSE
        vendor/google.golang.org/genproto/googleapis/rpc/LICENSE
        vendor/cloud.google.com/go/compute/metadata/LICENSE
    
    Removed:
      Apache 2.0:
        vendor/cloud.google.com/go/LICENSE
    
      MIT:
        vendor/github.com/shurcooL/sanitized_anchor_name/LICENSE
        vendor/go.uber.org/atomic/LICENSE.txt
        vendor/github.com/mitchellh/go-testing-interface/LICENSE
        vendor/github.com/mitchellh/copystructure/LICENSE
        vendor/github.com/mitchellh/reflectwalk/LICENSE
        vendor/github.com/mattn/go-colorable/LICENSE
        vendor/github.com/fatih/color/LICENSE.md
        vendor/github.com/armon/go-radix/LICENSE
        vendor/github.com/armon/go-metrics/LICENSE
    
      MPL-2.0:
        vendor/github.com/hashicorp/go-version/LICENSE
        vendor/github.com/hashicorp/go-uuid/LICENSE
        vendor/github.com/hashicorp/go-plugin/LICENSE
        vendor/github.com/hashicorp/golang-lru/LICENSE
    
    Modified:
    
      Apache 2.0:
        vendor/gopkg.in/square/go-jose.v2/LICENSE
        vendor/github.com/oklog/run/LICENSE
    
      BSD 3 Clause:
        vendor/github.com/golang/snappy/LICENSE
        vendor/gopkg.in/square/go-jose.v2/json/LICENSE
        vendor/github.com/pierrec/lz4/LICENSE
    
      MIT:
        vendor/github.com/urfave/cli/LICENSE
        vendor/github.com/klauspost/pgzip/LICENSE
        vendor/github.com/hashicorp/go-hclog/LICENSE
    
      MPL-2.0:
        vendor/github.com/hashicorp/vault/sdk/LICENSE
        vendor/github.com/hashicorp/vault/api/LICENSE
        vendor/github.com/hashicorp/yamux/LICENSE
        vendor/github.com/hashicorp/go-immutable-radix/LICENSE
    
    Signed-off-by: Adam Duskett <adam.duskett@amarulasolutions.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/mender-artifact/mender-artifact.mk
+++ package/mender-artifact/mender-artifact.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HOST_MENDER_ARTIFACT_VERSION = 3.8.0
+HOST_MENDER_ARTIFACT_VERSION = 3.11.2
 HOST_MENDER_ARTIFACT_SITE = $(call github,mendersoftware,mender-artifact,$(HOST_MENDER_ARTIFACT_VERSION))
 HOST_MENDER_ARTIFACT_LICENSE = Apache2.0, BSD-2-Clause, BSD-3-Clause, ISC, MIT, MPL-2.0
 HOST_MENDER_ARTIFACT_LICENSE_FILES = \
@@ -13,16 +13,21 @@ HOST_MENDER_ARTIFACT_LICENSE_FILES = \
 	vendor/github.com/minio/sha256-simd/LICENSE \
 	vendor/github.com/mendersoftware/progressbar/LICENSE \
 	vendor/google.golang.org/genproto/LICENSE \
+	vendor/google.golang.org/genproto/googleapis/api/LICENSE \
+	vendor/google.golang.org/genproto/googleapis/rpc/LICENSE \
 	vendor/google.golang.org/grpc/LICENSE \
 	vendor/google.golang.org/appengine/LICENSE \
-	vendor/cloud.google.com/go/LICENSE \
 	vendor/cloud.google.com/go/kms/LICENSE \
 	vendor/cloud.google.com/go/iam/LICENSE \
 	vendor/cloud.google.com/go/compute/LICENSE \
+	vendor/cloud.google.com/go/compute/metadata/LICENSE \
 	vendor/go.opencensus.io/LICENSE \
 	vendor/github.com/golang/groupcache/LICENSE \
-	vendor/gopkg.in/square/go-jose.v2/LICENSE \
-	vendor/github.com/oklog/run/LICENSE \
+	vendor/github.com/mendersoftware/openssl/LICENSE \
+	vendor/github.com/googleapis/enterprise-certificate-proxy/LICENSE \
+	vendor/github.com/go-jose/go-jose/v3/json/LICENSE \
+	vendor/github.com/go-jose/go-jose/v3/LICENSE \
+	vendor/github.com/google/s2a-go/LICENSE.md \
 	vendor/github.com/pkg/errors/LICENSE \
 	vendor/github.com/pmezard/go-difflib/LICENSE \
 	vendor/golang.org/x/sys/LICENSE \
@@ -40,51 +45,48 @@ HOST_MENDER_ARTIFACT_LICENSE_FILES = \
 	vendor/google.golang.org/protobuf/LICENSE \
 	vendor/github.com/golang/protobuf/LICENSE \
 	vendor/github.com/googleapis/gax-go/v2/LICENSE \
-	vendor/github.com/golang/snappy/LICENSE \
-	vendor/gopkg.in/square/go-jose.v2/json/LICENSE \
-	vendor/github.com/pierrec/lz4/LICENSE \
+	vendor/golang.org/x/crypto/LICENSE \
+	vendor/golang.org/x/time/LICENSE \
+	vendor/github.com/klauspost/compress/internal/snapref/LICENSE \
 	vendor/github.com/davecgh/go-spew/LICENSE \
 	vendor/github.com/stretchr/testify/LICENSE \
 	vendor/github.com/urfave/cli/LICENSE \
 	vendor/github.com/sirupsen/logrus/LICENSE \
 	vendor/github.com/klauspost/pgzip/LICENSE \
 	vendor/github.com/cpuguy83/go-md2man/v2/LICENSE.md \
-	vendor/github.com/shurcooL/sanitized_anchor_name/LICENSE \
 	vendor/gopkg.in/yaml.v3/LICENSE \
 	vendor/github.com/mattn/go-isatty/LICENSE \
 	vendor/github.com/klauspost/cpuid/v2/LICENSE \
-	vendor/go.uber.org/atomic/LICENSE.txt \
 	vendor/github.com/mitchellh/go-homedir/LICENSE \
-	vendor/github.com/mitchellh/go-testing-interface/LICENSE \
 	vendor/github.com/mitchellh/mapstructure/LICENSE \
-	vendor/github.com/mitchellh/copystructure/LICENSE \
-	vendor/github.com/mitchellh/reflectwalk/LICENSE \
 	vendor/github.com/ryanuber/go-glob/LICENSE \
-	vendor/github.com/mattn/go-colorable/LICENSE \
-	vendor/github.com/fatih/color/LICENSE.md \
-	vendor/github.com/armon/go-radix/LICENSE \
-	vendor/github.com/armon/go-metrics/LICENSE \
 	vendor/github.com/cenkalti/backoff/v3/LICENSE \
-	vendor/github.com/hashicorp/go-hclog/LICENSE \
+	vendor/github.com/klauspost/compress/zstd/internal/xxhash/LICENSE.txt \
 	vendor/github.com/hashicorp/go-secure-stdlib/strutil/LICENSE \
 	vendor/github.com/hashicorp/go-secure-stdlib/parseutil/LICENSE \
 	vendor/github.com/hashicorp/errwrap/LICENSE \
 	vendor/github.com/hashicorp/hcl/LICENSE \
 	vendor/github.com/hashicorp/go-cleanhttp/LICENSE \
-	vendor/github.com/hashicorp/go-version/LICENSE \
 	vendor/github.com/hashicorp/go-rootcerts/LICENSE \
 	vendor/github.com/hashicorp/go-retryablehttp/LICENSE \
-	vendor/github.com/hashicorp/go-uuid/LICENSE \
-	vendor/github.com/hashicorp/go-plugin/LICENSE \
 	vendor/github.com/hashicorp/go-sockaddr/LICENSE \
-	vendor/github.com/hashicorp/vault/sdk/LICENSE \
 	vendor/github.com/hashicorp/vault/api/LICENSE \
-	vendor/github.com/hashicorp/yamux/LICENSE \
-	vendor/github.com/hashicorp/go-immutable-radix/LICENSE \
-	vendor/github.com/hashicorp/go-multierror/LICENSE \
-	vendor/github.com/hashicorp/golang-lru/LICENSE
+	vendor/github.com/hashicorp/go-multierror/LICENSE
 
-HOST_MENDER_ARTIFACT_DEPENDENCIES = host-xz
+HOST_MENDER_ARTIFACT_DEPENDENCIES = host-pkgconf host-openssl host-xz
+
+ifeq ($(BR2_PACKAGE_HOST_ZSTD),y)
+HOST_MENDER_ARTIFACT_DEPENDENCIES += host-zstd
+endif
+
+HOST_MENDER_ARTIFACT_GO_ENV += \
+	PATH=$(BR_PATH) \
+	PKG_CONFIG="$(PKG_CONFIG_HOST_BINARY)" \
+	PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1 \
+	PKG_CONFIG_ALLOW_SYSTEM_LIBS=1 \
+	PKG_CONFIG_LIBDIR="$(HOST_DIR)/lib/pkgconfig:$(HOST_DIR)/share/pkgconfig" \
+	PKG_CONFIG_PATH="$(HOST_DIR)/lib/pkgconfig" \
+	PKG_CONFIG_SYSROOT_DIR="/"
 
 HOST_MENDER_ARTIFACT_LDFLAGS = -X github.com/mendersoftware/mender-artifact/cli.Version=$(HOST_MENDER_ARTIFACT_VERSION)
 
