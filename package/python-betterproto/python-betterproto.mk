7011c7feb6875db0d13b148dbfd9aaaba92b517e
James Hilliard <james.hilliard1@gmail.com>
2024-09-06 10:54:22 -0600

    package/python-betterproto: bump to version 2.0.0b7
    Add new python-typing-extensions runtime dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-betterproto/python-betterproto.mk
+++ package/python-betterproto/python-betterproto.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_BETTERPROTO_VERSION = 2.0.0b6
+PYTHON_BETTERPROTO_VERSION = 2.0.0b7
 PYTHON_BETTERPROTO_SOURCE = betterproto-$(PYTHON_BETTERPROTO_VERSION).tar.gz
-PYTHON_BETTERPROTO_SITE = https://files.pythonhosted.org/packages/45/43/4c44efd75f2ef48a16b458c2fe2cff7aa74bab8fcadf2653bb5110a87f97
+PYTHON_BETTERPROTO_SITE = https://files.pythonhosted.org/packages/4e/94/930a1368fbed92adc897a9a1fae282e3f9d608547dbf805034ca549f381a
 PYTHON_BETTERPROTO_SETUP_TYPE = pep517
 PYTHON_BETTERPROTO_LICENSE = MIT
 PYTHON_BETTERPROTO_LICENSE_FILES = LICENSE.md
