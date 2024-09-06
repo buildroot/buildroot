ae40223b5dcc69327cba1a534262b3053669c220
James Hilliard <james.hilliard1@gmail.com>
2024-09-06 12:47:56 -0600

    package/python-pydantic: bump to version 2.9.0
    pyproject.toml declared the tzdata package as a required dependency, but
    this is in fact only needed on win32. This was fixed upstream [1]. Since
    we don't actually check the runtime dependencies from pyproject.toml, we
    don't need to do anything in Buildroot.
    
    [1] https://github.com/pydantic/pydantic/pull/10331
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    [Arnout: remove tzdata runtime dependency]
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/python-pydantic/python-pydantic.mk
+++ package/python-pydantic/python-pydantic.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYDANTIC_VERSION = 2.8.2
+PYTHON_PYDANTIC_VERSION = 2.9.0
 PYTHON_PYDANTIC_SOURCE = pydantic-$(PYTHON_PYDANTIC_VERSION).tar.gz
-PYTHON_PYDANTIC_SITE = https://files.pythonhosted.org/packages/8c/99/d0a5dca411e0a017762258013ba9905cd6e7baa9a3fd1fe8b6529472902e
+PYTHON_PYDANTIC_SITE = https://files.pythonhosted.org/packages/f6/8f/3b9f7a38caa3fa0bcb3cea7ee9958e89a9a6efc0e6f51fd6096f24cac140
 PYTHON_PYDANTIC_SETUP_TYPE = pep517
 PYTHON_PYDANTIC_LICENSE = MIT
 PYTHON_PYDANTIC_LICENSE_FILES = LICENSE
