8331fb05925b33fbff1b08367470652e31996b39
Bernd Kuhls <bernd@kuhls.net>
2024-09-06 23:57:19 +0200

    package/php: bump version to 8.3.11
    Changelog: https://www.php.net/ChangeLog-8.php#PHP_8_3
    Release notes: https://news-web.php.net/php.announce/437
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/php/php.mk
+++ package/php/php.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PHP_VERSION = 8.3.10
+PHP_VERSION = 8.3.11
 PHP_SITE = https://www.php.net/distributions
 PHP_SOURCE = php-$(PHP_VERSION).tar.xz
 PHP_INSTALL_STAGING = YES
