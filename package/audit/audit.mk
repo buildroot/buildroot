04d64ab6d563a30cee48318c4a070f6aaab5f72c
Peter Korsgaard <peter@korsgaard.com>
2024-09-10 09:07:51 +0200

    package/audit: fix deactivate typo
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 413479ffce7b3f3a401a74caed255f40ff0203cf)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/audit/audit.mk
+++ package/audit/audit.mk
@@ -17,7 +17,7 @@ AUDIT_CONF_OPTS = --without-python --without-python3 --disable-zos-remote
 
 # src/libev has some assembly function that is not present in Thumb mode:
 # Error: selected processor does not support `mcr p15,0,r3,c7,c10,5' in Thumb mode
-# so, we desactivate Thumb mode
+# so, we deactivate Thumb mode
 ifeq ($(BR2_ARM_INSTRUCTIONS_THUMB),y)
 AUDIT_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -marm"
 endif
