56282c3b12b2dc8057ee5cde390b5b53ae59b56e
James Hilliard <james.hilliard1@gmail.com>
2024-09-14 15:08:15 -0600

    package/python-alembic: bump to version 1.13.2
    License hash changed due to year update:
    https://github.com/sqlalchemy/alembic/commit/8829bd64a70708ef9144604ea0f0a5179d07ccbe
    
    Pass --skip-dependency-check build option due to python-alembic
    unnecessarily restricting the maximum setuptools version.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-alembic/python-alembic.mk
+++ package/python-alembic/python-alembic.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_ALEMBIC_VERSION = 1.13.1
+PYTHON_ALEMBIC_VERSION = 1.13.2
 PYTHON_ALEMBIC_SOURCE = alembic-$(PYTHON_ALEMBIC_VERSION).tar.gz
-PYTHON_ALEMBIC_SITE = https://files.pythonhosted.org/packages/7b/24/ddce068e2ac9b5581bd58602edb2a1be1b0752e1ff2963c696ecdbe0470d
+PYTHON_ALEMBIC_SITE = https://files.pythonhosted.org/packages/66/e2/efa88e86029cada2da5941ec664d50d9a3b2a91f5066405c6f90e5016c16
 PYTHON_ALEMBIC_SETUP_TYPE = setuptools
 PYTHON_ALEMBIC_LICENSE = MIT
 PYTHON_ALEMBIC_LICENSE_FILES = LICENSE
+PYTHON_ALEMBIC_BUILD_OPTS = --skip-dependency-check
 
 $(eval $(python-package))
