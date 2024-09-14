849af6337f6a0a1ea1ebd508625c9e6e6d7d18ac
Peter Korsgaard <peter@korsgaard.com>
2024-09-14 14:43:30 +0200

    package/mrouted: fix typos and grammar
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Cc: Federico Pellegrin <fede@evolware.org>
    [yann.morin.1998@free.fr: s/is/are/ (noticed by Federico)]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 271ea8be96abf1b30b999075500aaf91f992c25c)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/mrouted/mrouted.mk
+++ package/mrouted/mrouted.mk
@@ -23,7 +23,7 @@ define MROUTED_INSTALL_INIT_SYSTEMD
 		$(TARGET_DIR)/usr/lib/systemd/system/mrouted.service
 endef
 
-# We will asume that CONFIG_NET and CONFIG_INET are already
+# We will assume that CONFIG_NET and CONFIG_INET are already
 # set in the kernel configuration provided by the user.
 define MROUTED_LINUX_CONFIG_FIXUPS
 	$(call KCONFIG_ENABLE_OPT,CONFIG_IP_MULTICAST)
