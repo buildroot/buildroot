bf3ee43224cea89548e07f7441b2ba842908f837
James Hilliard <james.hilliard1@gmail.com>
2024-09-14 15:22:24 -0600

    package/python-apispec: bump to version 6.6.1
    Migrate from setuptools to flit build backend.
    
    License hash changed due to date removal:
    https://github.com/marshmallow-code/apispec/commit/3de29d7896ac01cb3f40d73ff226b64853064f6f
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-apispec/python-apispec.mk
+++ package/python-apispec/python-apispec.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_APISPEC_VERSION = 6.3.1
+PYTHON_APISPEC_VERSION = 6.6.1
 PYTHON_APISPEC_SOURCE = apispec-$(PYTHON_APISPEC_VERSION).tar.gz
-PYTHON_APISPEC_SITE = https://files.pythonhosted.org/packages/ff/0c/e70aa981d8ebe2e8a500a85c923579ce3363feccd704268e6ff8053875dd
-PYTHON_APISPEC_SETUP_TYPE = setuptools
+PYTHON_APISPEC_SITE = https://files.pythonhosted.org/packages/ab/5b/5d94e1ec6a8e487c026b2a7518dfb7ea90c4c82db48de33c227b7c9141fb
+PYTHON_APISPEC_SETUP_TYPE = flit
 PYTHON_APISPEC_LICENSE = MIT
 PYTHON_APISPEC_LICENSE_FILES = LICENSE
 
