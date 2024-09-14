511b426a75496820665a345cefa5fea700cd8b58
Peter Korsgaard <peter@korsgaard.com>
2024-09-14 14:43:27 +0200

    package/makedevs/makedevs.c: fix typos
    And replace Couldn't with 'Could not' for clarity.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 79d062ab99fef3d7bb5982ac99d9cfcbe6d0cd83)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/makedevs/makedevs.c
+++ package/makedevs/makedevs.c
@@ -165,7 +165,7 @@ int bb_make_directory (char *path, long mode, int flags)
 			}
 			/* Since the directory exists, don't attempt to change
 			 * permissions if it was the full target.  Note that
-			 * this is not an error conditon. */
+			 * this is not an error condition. */
 			if (!c) {
 				umask(mask);
 				return 0;
@@ -480,11 +480,11 @@ int main(int argc, char **argv)
 	}
 
 	if (optind >= argc || (rootdir=argv[optind])==NULL) {
-		bb_error_msg_and_die("root directory not speficied");
+		bb_error_msg_and_die("root directory not specified");
 	}
 
 	if (chdir(rootdir) != 0) {
-		bb_perror_msg_and_die("Couldnt chdir to %s", rootdir);
+		bb_perror_msg_and_die("Could not chdir to %s", rootdir);
 	}
 
 	umask(0);
