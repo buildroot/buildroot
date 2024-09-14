4afa0d048b2d4339d53edbe1bf54a0ec66910a93
James Hilliard <james.hilliard1@gmail.com>
2024-09-14 16:28:20 -0600

    package/python-blinker: bump to version 1.8.2
    Migrate build backend from setuptools to flit.
    
    The license file got renamed, but the contents did not change.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-blinker/python-blinker.mk
+++ package/python-blinker/python-blinker.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_BLINKER_VERSION = 1.7.0
+PYTHON_BLINKER_VERSION = 1.8.2
 PYTHON_BLINKER_SOURCE = blinker-$(PYTHON_BLINKER_VERSION).tar.gz
-PYTHON_BLINKER_SITE = https://files.pythonhosted.org/packages/a1/13/6df5fc090ff4e5d246baf1f45fe9e5623aa8565757dfa5bd243f6a545f9e
-PYTHON_BLINKER_SETUP_TYPE = setuptools
+PYTHON_BLINKER_SITE = https://files.pythonhosted.org/packages/1e/57/a6a1721eff09598fb01f3c7cda070c1b6a0f12d63c83236edf79a440abcc
+PYTHON_BLINKER_SETUP_TYPE = flit
 PYTHON_BLINKER_LICENSE = MIT
-PYTHON_BLINKER_LICENSE_FILES = LICENSE.rst
+PYTHON_BLINKER_LICENSE_FILES = LICENSE.txt
 
 $(eval $(python-package))
