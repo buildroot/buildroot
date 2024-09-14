1f21227fee6f69560f4697182d7525235d303cff
Peter Korsgaard <peter@korsgaard.com>
2024-09-14 09:19:19 +0200

    package/libftdi: fix automatically typo
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 2b6c3cc6d0f6e7d29d43e385584b2c2d64e3f923)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libftdi/libftdi.mk
+++ package/libftdi/libftdi.mk
@@ -16,7 +16,7 @@ LIBFTDI_AUTORECONF = YES
 
 LIBFTDI_CONF_OPTS = --without-examples
 
-# configure detect it automaticaly so we need to force it
+# configure detect it automatically so we need to force it
 ifeq ($(BR2_PACKAGE_LIBFTDI_CPP),y)
 LIBFTDI_LICENSE += , GPL-2.0 with exception (ftdipp)
 LIBFTDI_LICENSE_FILES += COPYING.GPL
