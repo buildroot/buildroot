45bb5fbdae591f4f5c40ff3433d375f4d7a5d9d9
Peter Korsgaard <peter@korsgaard.com>
2024-09-14 09:19:22 +0200

    package/libkrb5: fix deactivate typo
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libkrb5/libkrb5.mk
+++ package/libkrb5/libkrb5.mk
@@ -36,7 +36,7 @@ LIBKRB5_CONF_OPTS = \
 
 # libkrb5 has some assembly function that is not present in Thumb mode:
 # Error: selected processor does not support `mcr p15,0,r2,c7,c10,5' in Thumb mode
-# so, we desactivate Thumb mode
+# so, we deactivate Thumb mode
 ifeq ($(BR2_ARM_INSTRUCTIONS_THUMB),y)
 LIBKRB5_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -marm"
 endif
