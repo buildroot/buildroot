0b779eb980c5bc47e725ccfd404b43ffd59d9e7a
James Hilliard <james.hilliard1@gmail.com>
2024-09-14 23:08:10 -0600

    package/python-email-validator: bump to version 2.2.0
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-email-validator/python-email-validator.mk
+++ package/python-email-validator/python-email-validator.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_EMAIL_VALIDATOR_VERSION = 2.1.1
+PYTHON_EMAIL_VALIDATOR_VERSION = 2.2.0
 PYTHON_EMAIL_VALIDATOR_SOURCE = email_validator-$(PYTHON_EMAIL_VALIDATOR_VERSION).tar.gz
-PYTHON_EMAIL_VALIDATOR_SITE = https://files.pythonhosted.org/packages/63/82/2914bff80ebee8c027802a664ad4b4caad502cd594e358f76aff395b5e56
+PYTHON_EMAIL_VALIDATOR_SITE = https://files.pythonhosted.org/packages/48/ce/13508a1ec3f8bb981ae4ca79ea40384becc868bfae97fd1c942bb3a001b1
 PYTHON_EMAIL_VALIDATOR_SETUP_TYPE = setuptools
 PYTHON_EMAIL_VALIDATOR_LICENSE = Unlicense
 PYTHON_EMAIL_VALIDATOR_LICENSE_FILES = LICENSE
