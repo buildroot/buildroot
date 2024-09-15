f32805c4edefe4ad904f8bc29422a6db6ecacd82
James Hilliard <james.hilliard1@gmail.com>
2024-09-15 16:10:18 -0600

    package/python-magic-wormhole: bump to version 0.15.0
    Drop patch which is now upstream.
    
    Drop no longer required python-six runtime dependency.
    
    Add new python-iterable-io runtime dependency.
    
    Add new python-zipstream-ng runtime dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-magic-wormhole/python-magic-wormhole.mk
+++ package/python-magic-wormhole/python-magic-wormhole.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_MAGIC_WORMHOLE_VERSION = 0.13.0
+PYTHON_MAGIC_WORMHOLE_VERSION = 0.15.0
 PYTHON_MAGIC_WORMHOLE_SOURCE = magic-wormhole-$(PYTHON_MAGIC_WORMHOLE_VERSION).tar.gz
-PYTHON_MAGIC_WORMHOLE_SITE = https://files.pythonhosted.org/packages/cc/e1/75c31ad5db873268ba0750006b3d0e40c30b0ad39e6f58b1e28a28d6de48
+PYTHON_MAGIC_WORMHOLE_SITE = https://files.pythonhosted.org/packages/9d/7b/9320062c8b11e58dccd079dd62e3525bf1dac7f2b441c885d64a7ca7f045
 PYTHON_MAGIC_WORMHOLE_SETUP_TYPE = setuptools
 PYTHON_MAGIC_WORMHOLE_LICENSE = MIT
 PYTHON_MAGIC_WORMHOLE_LICENSE_FILES = LICENSE
