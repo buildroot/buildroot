d3b47b49d3df2ed8184f98638ab486a9dc05ea3f
James Hilliard <james.hilliard1@gmail.com>
2024-09-15 01:55:09 -0600

    package/python-gunicorn: bump to version 23.0.0
    License hash changed due to year update:
    https://github.com/benoitc/gunicorn/commit/5b68c17b170c7b021a7a982a06c08e3898a5a640
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-gunicorn/python-gunicorn.mk
+++ package/python-gunicorn/python-gunicorn.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_GUNICORN_VERSION = 21.2.0
+PYTHON_GUNICORN_VERSION = 23.0.0
 PYTHON_GUNICORN_SOURCE = gunicorn-$(PYTHON_GUNICORN_VERSION).tar.gz
-PYTHON_GUNICORN_SITE = https://files.pythonhosted.org/packages/06/89/acd9879fa6a5309b4bf16a5a8855f1e58f26d38e0c18ede9b3a70996b021
+PYTHON_GUNICORN_SITE = https://files.pythonhosted.org/packages/34/72/9614c465dc206155d93eff0ca20d42e1e35afc533971379482de953521a4
 PYTHON_GUNICORN_SETUP_TYPE = setuptools
 PYTHON_GUNICORN_LICENSE = MIT
 PYTHON_GUNICORN_LICENSE_FILES = LICENSE
