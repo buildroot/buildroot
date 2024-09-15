67562a9f1e27a4ce3fd775e53dfe096c6754fed6
James Hilliard <james.hilliard1@gmail.com>
2024-09-15 00:52:36 -0600

    package/python-future: bump to version 1.0.0
    License hash changed due to date update:
    https://github.com/PythonCharmers/python-future/commit/6eef83de7db1086633c98ba2e104de1b471944d0
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-future/python-future.mk
+++ package/python-future/python-future.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_FUTURE_VERSION = 0.18.3
+PYTHON_FUTURE_VERSION = 1.0.0
 PYTHON_FUTURE_SOURCE = future-$(PYTHON_FUTURE_VERSION).tar.gz
-PYTHON_FUTURE_SITE = https://files.pythonhosted.org/packages/8f/2e/cf6accf7415237d6faeeebdc7832023c90e0282aa16fd3263db0eb4715ec
+PYTHON_FUTURE_SITE = https://files.pythonhosted.org/packages/a7/b2/4140c69c6a66432916b26158687e821ba631a4c9273c474343badf84d3ba
 PYTHON_FUTURE_SETUP_TYPE = setuptools
 PYTHON_FUTURE_LICENSE = MIT
 PYTHON_FUTURE_LICENSE_FILES = LICENSE.txt
