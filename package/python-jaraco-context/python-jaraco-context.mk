ae29a849a1e92b5520ea15f88ab01df8147ec9a0
James Hilliard <james.hilliard1@gmail.com>
2024-09-15 03:13:55 -0600

    package/python-jaraco-context: bump to version 6.0.1
    License has changed due to copyright notice cleanup:
    https://github.com/jaraco/jaraco.context/commit/5957d58266e479f124b31f30e4322e798fdf386b
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-jaraco-context/python-jaraco-context.mk
+++ package/python-jaraco-context/python-jaraco-context.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_JARACO_CONTEXT_VERSION = 4.3.0
-PYTHON_JARACO_CONTEXT_SOURCE = jaraco.context-$(PYTHON_JARACO_CONTEXT_VERSION).tar.gz
-PYTHON_JARACO_CONTEXT_SITE = https://files.pythonhosted.org/packages/7c/b4/fa71f82b83ebeed95fe45ce587d6cba85b7c09ef3d9f61602f92f45e90db
+PYTHON_JARACO_CONTEXT_VERSION = 6.0.1
+PYTHON_JARACO_CONTEXT_SOURCE = jaraco_context-$(PYTHON_JARACO_CONTEXT_VERSION).tar.gz
+PYTHON_JARACO_CONTEXT_SITE = https://files.pythonhosted.org/packages/df/ad/f3777b81bf0b6e7bc7514a1656d3e637b2e8e15fab2ce3235730b3e7a4e6
 PYTHON_JARACO_CONTEXT_SETUP_TYPE = setuptools
 PYTHON_JARACO_CONTEXT_LICENSE = MIT
 PYTHON_JARACO_CONTEXT_LICENSE_FILES = LICENSE
