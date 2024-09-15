032b33d4fa33e03d752835a9d04532306b3710f0
James Hilliard <james.hilliard1@gmail.com>
2024-09-15 15:14:35 -0600

    package/python-lmdb: bump to version 1.5.1
    Drop no longer required python-cffi dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-lmdb/python-lmdb.mk
+++ package/python-lmdb/python-lmdb.mk
@@ -4,14 +4,13 @@
 #
 ################################################################################
 
-PYTHON_LMDB_VERSION = 1.4.1
+PYTHON_LMDB_VERSION = 1.5.1
 PYTHON_LMDB_SOURCE = lmdb-$(PYTHON_LMDB_VERSION).tar.gz
-PYTHON_LMDB_SITE = https://files.pythonhosted.org/packages/de/13/dd9b0c1924f0becc93e0bacd123a4e7a347966e3e74753ace3b1e85acc39
+PYTHON_LMDB_SITE = https://files.pythonhosted.org/packages/67/2c/0cc9375341121ca1f4b31a2137f955dc24550e095d733c9b42ea94113ba1
 PYTHON_LMDB_LICENSE = OLDAP-2.8
 PYTHON_LMDB_LICENSE_FILES = LICENSE
 PYTHON_LMDB_CPE_ID_VENDOR = py-lmdb_project
 PYTHON_LMDB_CPE_ID_PRODUCT = py-lmdb
 PYTHON_LMDB_SETUP_TYPE = setuptools
-PYTHON_LMDB_DEPENDENCIES = host-python-cffi
 
 $(eval $(python-package))
