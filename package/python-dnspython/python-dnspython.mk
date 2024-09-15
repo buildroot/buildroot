2b83df84600a68f18fe5540a3b8a4b2641a4395f
James Hilliard <james.hilliard1@gmail.com>
2024-09-14 22:46:23 -0600

    package/python-dnspython: bump to versin 2.6.1
    Replace poetry-core build backend with hatchling.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-dnspython/python-dnspython.mk
+++ package/python-dnspython/python-dnspython.mk
@@ -4,14 +4,14 @@
 #
 ################################################################################
 
-PYTHON_DNSPYTHON_VERSION = 2.4.2
+PYTHON_DNSPYTHON_VERSION = 2.6.1
 PYTHON_DNSPYTHON_SOURCE = dnspython-$(PYTHON_DNSPYTHON_VERSION).tar.gz
-PYTHON_DNSPYTHON_SITE = https://files.pythonhosted.org/packages/65/2d/372a20e52a87b2ba0160997575809806111a72e18aa92738daccceb8d2b9
+PYTHON_DNSPYTHON_SITE = https://files.pythonhosted.org/packages/37/7d/c871f55054e403fdfd6b8f65fd6d1c4e147ed100d3e9f9ba1fe695403939
 PYTHON_DNSPYTHON_LICENSE = ISC
 PYTHON_DNSPYTHON_LICENSE_FILES = LICENSE
 PYTHON_DNSPYTHON_SETUP_TYPE = pep517
-PYTHON_DNSPYTHON_DEPENDENCIES = host-python-poetry-core
-HOST_PYTHON_DNSPYTHON_DEPENDENCIES = host-python-poetry-core
+PYTHON_DNSPYTHON_DEPENDENCIES = host-python-hatchling
+HOST_PYTHON_DNSPYTHON_DEPENDENCIES = host-python-hatchling
 
 $(eval $(python-package))
 $(eval $(host-python-package))
