191e7685c8fb7d5740bcd054bbfcc36d289dfc47
James Hilliard <james.hilliard1@gmail.com>
2024-09-14 22:02:37 -0600

    package/python-cherrypy: bump to version 18.10.0
    Drop no longer required host-python-setuptools-scm-git-archive
    build dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-cherrypy/python-cherrypy.mk
+++ package/python-cherrypy/python-cherrypy.mk
@@ -4,14 +4,12 @@
 #
 ################################################################################
 
-PYTHON_CHERRYPY_VERSION = 18.9.0
-PYTHON_CHERRYPY_SOURCE = CherryPy-$(PYTHON_CHERRYPY_VERSION).tar.gz
-PYTHON_CHERRYPY_SITE = https://files.pythonhosted.org/packages/bd/5f/e265a49883bfcfb7f2c3d3d9e96197cfe8136783e96c5ce20e201550aaa0
+PYTHON_CHERRYPY_VERSION = 18.10.0
+PYTHON_CHERRYPY_SOURCE = cherrypy-$(PYTHON_CHERRYPY_VERSION).tar.gz
+PYTHON_CHERRYPY_SITE = https://files.pythonhosted.org/packages/93/e8/2f7ef142d1962d08a8885c4c9942212abecad6a80ccdd1620fd1f5c993fd
 PYTHON_CHERRYPY_LICENSE = BSD-3-Clause
 PYTHON_CHERRYPY_LICENSE_FILES = LICENSE.md
 PYTHON_CHERRYPY_SETUP_TYPE = setuptools
-PYTHON_CHERRYPY_DEPENDENCIES = \
-	host-python-setuptools-scm \
-	host-python-setuptools-scm-git-archive
+PYTHON_CHERRYPY_DEPENDENCIES = host-python-setuptools-scm
 
 $(eval $(python-package))
