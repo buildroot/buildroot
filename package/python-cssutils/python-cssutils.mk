9d713d2b7adf77954787ec66c19be773bf2a9621
James Hilliard <james.hilliard1@gmail.com>
2024-09-14 22:31:31 -0600

    package/python-cssutils: bump to version 2.11.1
    The license file got renamed, but the contents did not change.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-cssutils/python-cssutils.mk
+++ package/python-cssutils/python-cssutils.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_CSSUTILS_VERSION = 2.9.0
+PYTHON_CSSUTILS_VERSION = 2.11.1
 PYTHON_CSSUTILS_SOURCE = cssutils-$(PYTHON_CSSUTILS_VERSION).tar.gz
-PYTHON_CSSUTILS_SITE = https://files.pythonhosted.org/packages/b4/65/a054545c81eb87af898d664043578b8444ea3ded656db3da8f9d9fa21334
+PYTHON_CSSUTILS_SITE = https://files.pythonhosted.org/packages/33/9f/329d26121fe165be44b1dfff21aa0dc348f04633931f1d20ed6cf448a236
 PYTHON_CSSUTILS_LICENSE = LGPL-3.0+
-PYTHON_CSSUTILS_LICENSE_FILES = COPYING.LESSER
+PYTHON_CSSUTILS_LICENSE_FILES = LICENSE
 PYTHON_CSSUTILS_SETUP_TYPE = setuptools
 PYTHON_CSSUTILS_DEPENDENCIES = host-python-setuptools-scm
 
