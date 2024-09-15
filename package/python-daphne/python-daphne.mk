b93a35dcef85e8e3d0e3471862e3f32903da6db0
James Hilliard <james.hilliard1@gmail.com>
2024-09-14 22:41:31 -0600

    package/python-daphne: bump to version 4.1.2
    Drop remove pytest runner patch and instead pass
    --skip-dependency-check build option.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-daphne/python-daphne.mk
+++ package/python-daphne/python-daphne.mk
@@ -4,12 +4,13 @@
 #
 ################################################################################
 
-PYTHON_DAPHNE_VERSION = 4.0.0
+PYTHON_DAPHNE_VERSION = 4.1.2
 PYTHON_DAPHNE_SOURCE = daphne-$(PYTHON_DAPHNE_VERSION).tar.gz
-PYTHON_DAPHNE_SITE = https://files.pythonhosted.org/packages/d7/77/57b19d5caabf8537879aa4cf3a017b99d0b727f2521ffca7fd9140573509
+PYTHON_DAPHNE_SITE = https://files.pythonhosted.org/packages/1a/c1/aedf180beb12395835cba791ce7239b8880009d9d37564d72b7590cde605
 PYTHON_DAPHNE_SETUP_TYPE = setuptools
 # https://github.com/django/daphne/blob/master/LICENSE
 PYTHON_DAPHNE_LICENSE = BSD-3-Clause
 PYTHON_DAPHNE_LICENSE_FILES = LICENSE
+PYTHON_DAPHNE_BUILD_OPTS = --skip-dependency-check
 
 $(eval $(python-package))
