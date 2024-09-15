aff91918ea397f9745e5f1dd60665a3ab46bf142
James Hilliard <james.hilliard1@gmail.com>
2024-09-15 02:03:48 -0600

    package/python-humanize: bump to verison 4.10.0
    Migrate from setuptools to hatchling build backend.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-humanize/python-humanize.mk
+++ package/python-humanize/python-humanize.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-PYTHON_HUMANIZE_VERSION = 4.4.0
+PYTHON_HUMANIZE_VERSION = 4.10.0
 PYTHON_HUMANIZE_SOURCE = humanize-$(PYTHON_HUMANIZE_VERSION).tar.gz
-PYTHON_HUMANIZE_SITE = https://files.pythonhosted.org/packages/51/19/3e1adf0e7a8c8361496b085edcab2ddcd85410735a2b6fdd044247fc5b75
-PYTHON_HUMANIZE_SETUP_TYPE = setuptools
+PYTHON_HUMANIZE_SITE = https://files.pythonhosted.org/packages/5d/b1/c8f05d5dc8f64030d8cc71e91307c1daadf6ec0d70bcd6eabdfd9b6f153f
+PYTHON_HUMANIZE_SETUP_TYPE = pep517
 PYTHON_HUMANIZE_LICENSE = MIT
 PYTHON_HUMANIZE_LICENSE_FILES = LICENCE
-PYTHON_HUMANIZE_DEPENDENCIES = host-python-setuptools-scm
+PYTHON_HUMANIZE_DEPENDENCIES = host-python-hatchling host-python-hatch-vcs
 
 $(eval $(python-package))
