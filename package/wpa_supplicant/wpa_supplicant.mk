2417d0cf81ee54329aa941d1a588baf601711978
Guillaume GC. Chaye <guillaume.chaye@zeetim.com>
2024-09-16 10:55:10 +0200

    package/wpa_supplicant: add configuration for WPA3-Entreprise
    Extend the set of options enabled when BR2_PACKAGE_WPA_SUPPLICANT_WPA3
    by also enabling:
    
     - CONFIG_SUITEB: WPA3-Enterprise
     - CONFIG_SUITEB192: WPA3-Enterprise (SuiteB 192 bits security)
    
    While at it, document the options that are enabled.
    
    Signed-off-by: Guillaume GC. Chaye <guillaume.chaye@zeetim.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/wpa_supplicant/wpa_supplicant.mk
+++ package/wpa_supplicant/wpa_supplicant.mk
@@ -126,18 +126,29 @@ ifeq ($(BR2_PACKAGE_WPA_SUPPLICANT_WPS),)
 WPA_SUPPLICANT_CONFIG_DISABLE += CONFIG_WPS
 endif
 
+# WPA3 configurations:
+# - CONFIG_DPP: Easy Connect (Device Provisioning Protocol - DPP R1 & R2)
+# - CONFIG_SAE: Simultaneous Authentication of Equals (SAE), WPA3-Personal
+# - CONFIG_SAE_PK: SAE Public Key, WPA3-Personal
+# - CONFIG_OWE: Opportunistic Wireless Encryption (OWE)
+# - CONFIG_SUITEB: WPA3-Enterprise
+# - CONFIG_SUITEB192: WPA3-Enterprise (SuiteB 192 bits security)
 ifeq ($(BR2_PACKAGE_WPA_SUPPLICANT_WPA3),y)
 WPA_SUPPLICANT_CONFIG_ENABLE += \
 	CONFIG_DPP \
 	CONFIG_SAE \
 	CONFIG_SAE_PK \
-	CONFIG_OWE
+	CONFIG_OWE \
+	CONFIG_SUITEB \
+	CONFIG_SUITEB192
 else
 WPA_SUPPLICANT_CONFIG_DISABLE += \
 	CONFIG_DPP \
 	CONFIG_SAE \
 	CONFIG_SAE_PK \
-	CONFIG_OWE
+	CONFIG_OWE \
+	CONFIG_SUITEB \
+	CONFIG_SUITEB192
 endif
 
 # Try to use openssl if it's already available
