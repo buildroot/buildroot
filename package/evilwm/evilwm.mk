ef0903b2bd796b1b0c8e38864cc27ef765356ba1
Waldemar Brodkorb <wbx@openadk.org>
2024-09-16 16:32:28 +0200

    package/evilwm: add missing dependency
    Add missing dependency to xlib_libXrandr.
    
    Fixes:
    
     https://autobuild.buildroot.org/results/9928181557c7f99b7c74065d981f005cbd95145d/
    
    Signed-off-by: Waldemar Brodkorb <wbx@openadk.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/evilwm/evilwm.mk
+++ package/evilwm/evilwm.mk
@@ -9,7 +9,7 @@ EVILWM_SITE = https://www.6809.org.uk/evilwm/dl
 EVILWM_LICENSE = evilwm license
 EVILWM_LICENSE_FILES = README
 
-EVILWM_DEPENDENCIES = xlib_libX11
+EVILWM_DEPENDENCIES = xlib_libX11 xlib_libXrandr
 
 define EVILWM_INSTALL_XSESSION_FILE
 	$(INSTALL) -m 0755 -D package/evilwm/xsession \
