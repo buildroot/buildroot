5e84e6ea37d0e3cb9830e14301692619b22bae7e
James Hilliard <james.hilliard1@gmail.com>
2024-09-17 15:00:46 -0600

    package/python-pycairo: bump to version 1.27.0
    Migrate from setuptools build backend to meson.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-pycairo/python-pycairo.mk
+++ package/python-pycairo/python-pycairo.mk
@@ -4,12 +4,20 @@
 #
 ################################################################################
 
-PYTHON_PYCAIRO_VERSION = 1.25.1
+PYTHON_PYCAIRO_VERSION = 1.27.0
 PYTHON_PYCAIRO_SOURCE = pycairo-$(PYTHON_PYCAIRO_VERSION).tar.gz
-PYTHON_PYCAIRO_SITE = https://files.pythonhosted.org/packages/c7/7c/de4d40316ee563e4f15a0f05f28c8458833fbef80c4588b9dd51f39b78ee
-PYTHON_PYCAIRO_SETUP_TYPE = setuptools
+PYTHON_PYCAIRO_SITE = https://files.pythonhosted.org/packages/07/4a/42b26390181a7517718600fa7d98b951da20be982a50cd4afb3d46c2e603
 PYTHON_PYCAIRO_DEPENDENCIES = cairo
 PYTHON_PYCAIRO_LICENSE = LGPL-2.1 or MPL-1.1
 PYTHON_PYCAIRO_LICENSE_FILES = COPYING COPYING-LGPL-2.1 COPYING-MPL-1.1
 
-$(eval $(python-package))
+PYTHON_PYCAIRO_CONF_OPTS = \
+	-Dpython=$(HOST_DIR)/bin/python \
+	-Dtests=false \
+	-Dwheel=false
+
+PYTHON_PYCAIRO_CONF_ENV = \
+	_PYTHON_SYSCONFIGDATA_NAME=$(PKG_PYTHON_SYSCONFIGDATA_NAME) \
+	PYTHONPATH=$(PYTHON3_PATH)
+
+$(eval $(meson-package))
