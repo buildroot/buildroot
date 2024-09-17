e5dbcbf80435b4faddfad6b741c2dac06a7693dc
Waldemar Brodkorb <wbx@openadk.org>
2024-09-17 05:32:48 +0200

    package/bind: update to 9.18.29
    See here for a Changelog:
    https://downloads.isc.org/isc/bind9/9.18.29/doc/arm/html/notes.html#notes-for-bind-9-18-29
    
    Signed-off-by: Waldemar Brodkorb <wbx@openadk.org>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/bind/bind.mk
+++ package/bind/bind.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BIND_VERSION = 9.18.28
+BIND_VERSION = 9.18.29
 BIND_SOURCE= bind-$(BIND_VERSION).tar.xz
 BIND_SITE = https://ftp.isc.org/isc/bind9/$(BIND_VERSION)
 # bind does not support parallel builds.
