d7e62b11db5e2ade5116ca9ffd16ef015aaf698d
James Hilliard <james.hilliard1@gmail.com>
2024-09-17 14:40:52 -0600

    package/python-pure-eval: bump to version 0.2.3
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-pure-eval/python-pure-eval.mk
+++ package/python-pure-eval/python-pure-eval.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PURE_EVAL_VERSION = 0.2.2
+PYTHON_PURE_EVAL_VERSION = 0.2.3
 PYTHON_PURE_EVAL_SOURCE = pure_eval-$(PYTHON_PURE_EVAL_VERSION).tar.gz
-PYTHON_PURE_EVAL_SITE = https://files.pythonhosted.org/packages/97/5a/0bc937c25d3ce4e0a74335222aee05455d6afa2888032185f8ab50cdf6fd
+PYTHON_PURE_EVAL_SITE = https://files.pythonhosted.org/packages/cd/05/0a34433a064256a578f1783a10da6df098ceaa4a57bbeaa96a6c0352786b
 PYTHON_PURE_EVAL_SETUP_TYPE = setuptools
 PYTHON_PURE_EVAL_LICENSE = MIT
 PYTHON_PURE_EVAL_LICENSE_FILES = LICENSE.txt
