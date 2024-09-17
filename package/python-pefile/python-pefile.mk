687fb917378bfb1102107114d24809f84253a337
James Hilliard <james.hilliard1@gmail.com>
2024-09-17 12:33:46 -0600

    package/python-pefile: bump to version 2024.8.26
    License hash changed due to date update:
    https://github.com/erocarrera/pefile/commit/4b3b1e2e568a88d4f1897d694d684f23d9e270c4
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-pefile/python-pefile.mk
+++ package/python-pefile/python-pefile.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PEFILE_VERSION = 2023.2.7
+PYTHON_PEFILE_VERSION = 2024.8.26
 PYTHON_PEFILE_SOURCE = pefile-$(PYTHON_PEFILE_VERSION).tar.gz
-PYTHON_PEFILE_SITE = https://files.pythonhosted.org/packages/78/c5/3b3c62223f72e2360737fd2a57c30e5b2adecd85e70276879609a7403334
+PYTHON_PEFILE_SITE = https://files.pythonhosted.org/packages/03/4f/2750f7f6f025a1507cd3b7218691671eecfd0bbebebe8b39aa0fe1d360b8
 PYTHON_PEFILE_SETUP_TYPE = setuptools
 PYTHON_PEFILE_LICENSE = MIT
 PYTHON_PEFILE_LICENSE_FILES = LICENSE
