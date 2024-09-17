d9be0ed46dd7e9b2f0e10e53dc0a8389c93057e5
James Hilliard <james.hilliard1@gmail.com>
2024-09-17 15:26:25 -0600

    package/python-pydal: bump to version 20240906.1
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-pydal/python-pydal.mk
+++ package/python-pydal/python-pydal.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYDAL_VERSION = 20231114.3
+PYTHON_PYDAL_VERSION = 20240906.1
 PYTHON_PYDAL_SOURCE = pydal-$(PYTHON_PYDAL_VERSION).tar.gz
-PYTHON_PYDAL_SITE = https://files.pythonhosted.org/packages/0b/3b/86302e165af07bc3d9225aea6ed8f52386270220e4aac1aad0101a48aac1
+PYTHON_PYDAL_SITE = https://files.pythonhosted.org/packages/9f/80/cbaf7c90eec1da2404b1e6d100687ec2f01e6ccfbe695922a00de882ec84
 PYTHON_PYDAL_LICENSE = BSD-3-Clause
 PYTHON_PYDAL_LICENSE_FILES = LICENSE.txt
 PYTHON_PYDAL_SETUP_TYPE = setuptools
