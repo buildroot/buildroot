f408d01c97650a19fff1ffad3c8253dced257afa
James Hilliard <james.hilliard1@gmail.com>
2024-09-17 16:23:22 -0600

    package/python-pylibfdt: bump to version 1.7.1
    We need to pass the --skip-dependency-check build option as we do
    not provide the swig dependency as a python package but rather
    as a normal installed package.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-pylibfdt/python-pylibfdt.mk
+++ package/python-pylibfdt/python-pylibfdt.mk
@@ -4,14 +4,16 @@
 #
 ################################################################################
 
-PYTHON_PYLIBFDT_VERSION = 1.7.0.post1
+PYTHON_PYLIBFDT_VERSION = 1.7.1
 PYTHON_PYLIBFDT_SOURCE = pylibfdt-$(PYTHON_PYLIBFDT_VERSION).tar.gz
-PYTHON_PYLIBFDT_SITE = https://files.pythonhosted.org/packages/96/5c/77ef0f0459e0b13f39ecc22e21ad4ac9fbe741e8a7cd70702ac8165f80e2
+PYTHON_PYLIBFDT_SITE = https://files.pythonhosted.org/packages/1f/99/26fa7008cc0020da5c6407c48b34de21b3613232bd8875814016aec3ead5
 PYTHON_PYLIBFDT_SETUP_TYPE = pep517
 PYTHON_PYLIBFDT_LICENSE = BSD-2-Clause or GPL-2.0+
 PYTHON_PYLIBFDT_LICENSE_FILES = BSD-2-Clause GPL
 PYTHON_PYLIBFDT_DEPENDENCIES = host-python-setuptools-scm host-swig
+PYTHON_PYLIBFDT_BUILD_OPTS = --skip-dependency-check
 HOST_PYTHON_PYLIBFDT_DEPENDENCIES = host-python-setuptools-scm host-swig
+HOST_PYTHON_PYLIBFDT_BUILD_OPTS = --skip-dependency-check
 
 $(eval $(python-package))
 $(eval $(host-python-package))
