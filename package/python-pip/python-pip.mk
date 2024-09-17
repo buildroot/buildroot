ed5d5b7d54c3b9dae60464166e9e926b7c961920
James Hilliard <james.hilliard1@gmail.com>
2024-09-17 12:43:06 -0600

    package/python-pip: bump to version 24.2
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-pip/python-pip.mk
+++ package/python-pip/python-pip.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PIP_VERSION = 23.3.2
+PYTHON_PIP_VERSION = 24.2
 PYTHON_PIP_SOURCE = pip-$(PYTHON_PIP_VERSION).tar.gz
-PYTHON_PIP_SITE = https://files.pythonhosted.org/packages/b7/06/6b1ad0ae8f97d7a0d6f6ad640db10780578999e647a9593512ceb6f06469
+PYTHON_PIP_SITE = https://files.pythonhosted.org/packages/4d/87/fb90046e096a03aeab235e139436b3fe804cdd447ed2093b0d70eba3f7f8
 PYTHON_PIP_SETUP_TYPE = setuptools
 PYTHON_PIP_LICENSE = MIT
 PYTHON_PIP_LICENSE_FILES = LICENSE.txt
