d5463c142fa02a52836a345323c8c087c21429d0
James Hilliard <james.hilliard1@gmail.com>
2024-09-17 17:58:40 -0600

    package/python-pysmi: bump to version 1.5.0
    Add new python-jinja2 and python-requests runtime dependencies.
    
    Migrate from setuptools to poetry build backend.
    
    License hash changed due to formatting changes:
    https://github.com/lextudio/pysmi/commit/8341b2e13421739cb3f284c1bf725ab56f7af15a
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-pysmi/python-pysmi.mk
+++ package/python-pysmi/python-pysmi.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_PYSMI_VERSION = 0.3.4
+PYTHON_PYSMI_VERSION = 1.5.0
 PYTHON_PYSMI_SOURCE = pysmi-$(PYTHON_PYSMI_VERSION).tar.gz
-PYTHON_PYSMI_SITE = https://files.pythonhosted.org/packages/52/42/ddaeb06ff551672b17b77f81bc2e26b7c6060b28fe1552226edc6476ce37
-PYTHON_PYSMI_SETUP_TYPE = setuptools
+PYTHON_PYSMI_SITE = https://files.pythonhosted.org/packages/59/d9/136ca19b66a917377a3b40f390f525eeeb21122dba7562a726bfb2d3629e
+PYTHON_PYSMI_SETUP_TYPE = pep517
 PYTHON_PYSMI_LICENSE = BSD-2-Clause
 PYTHON_PYSMI_LICENSE_FILES = LICENSE.rst
+PYTHON_PYSMI_DEPENDENCIES = host-python-poetry-core
 
 $(eval $(python-package))
