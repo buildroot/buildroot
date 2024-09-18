d33b4a9bc9d70d3a51cdc70844d50b72f831fcfd
James Hilliard <james.hilliard1@gmail.com>
2024-09-17 18:22:20 -0600

    package/python-pysnmp: bump to version 7.1.3
    License hash changed due to date update:
    https://github.com/lextudio/pysnmp/commit/fa9909cbbf9b25eb992a945715e2511a2ba2c905
    
    Update license from BSD-3-Clause to BSD-2-Clause, the license
    changed in 8122a1d85b6cc9f2adcb1243521c04ba4cae0510 but the commit
    incorrectly indicated "no content changed" when updating the hash.
    
    Add new python-pysnmpcrypto runtime dependency.
    
    Drop no longer required python-pycryptodomex runtime dependency.
    
    Migrate from setuptools to poetry build backend.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    [yann.morin.1998@free.fr:
      - only list first-level dependency in rust arch support comment
    ]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-pysnmp/python-pysnmp.mk
+++ package/python-pysnmp/python-pysnmp.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_PYSNMP_VERSION = 4.4.12
+PYTHON_PYSNMP_VERSION = 7.1.3
 PYTHON_PYSNMP_SOURCE = pysnmp-$(PYTHON_PYSNMP_VERSION).tar.gz
-PYTHON_PYSNMP_SITE = https://files.pythonhosted.org/packages/4e/75/72f64c451bf5884715f84f8217b69b4025da0b67628d611cd14a5b7db217
-PYTHON_PYSNMP_SETUP_TYPE = setuptools
-PYTHON_PYSNMP_LICENSE = BSD-3-Clause
+PYTHON_PYSNMP_SITE = https://files.pythonhosted.org/packages/f3/0c/bb2ccb587b5e79036dd83aa5fe835c2bc460a3098c4060461e9d324c6670
+PYTHON_PYSNMP_SETUP_TYPE = pep517
+PYTHON_PYSNMP_LICENSE = BSD-2-Clause
 PYTHON_PYSNMP_LICENSE_FILES = LICENSE.rst
+PYTHON_PYSNMP_DEPENDENCIES = host-python-poetry-core
 
 $(eval $(python-package))
