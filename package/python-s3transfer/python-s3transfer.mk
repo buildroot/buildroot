fe8ff668627b2f96fdf00d97a932fb7699e68a31
James Hilliard <james.hilliard1@gmail.com>
2024-09-19 12:47:58 -0600

    package/python-s3transfer: bump to version 0.10.2
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Acked-by: Raphaël Mélotte <raphael.melotte@mind.be>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-s3transfer/python-s3transfer.mk
+++ package/python-s3transfer/python-s3transfer.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_S3TRANSFER_VERSION = 0.10.0
+PYTHON_S3TRANSFER_VERSION = 0.10.2
 PYTHON_S3TRANSFER_SOURCE = s3transfer-$(PYTHON_S3TRANSFER_VERSION).tar.gz
-PYTHON_S3TRANSFER_SITE = https://files.pythonhosted.org/packages/a0/b5/4c570b08cb85fdcc65037b5229e00412583bb38d974efecb7ec3495f40ba
+PYTHON_S3TRANSFER_SITE = https://files.pythonhosted.org/packages/cb/67/94c6730ee4c34505b14d94040e2f31edf144c230b6b49e971b4f25ff8fab
 PYTHON_S3TRANSFER_SETUP_TYPE = setuptools
 PYTHON_S3TRANSFER_LICENSE = Apache-2.0
 PYTHON_S3TRANSFER_LICENSE_FILES = LICENSE.txt
