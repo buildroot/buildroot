2cc845756e3e5012bcb1b88bdf72dd14fbd361b7
James Hilliard <james.hilliard1@gmail.com>
2024-09-22 01:07:51 -0600

    package/python-yatl: bump to version 20230507.3
    Add previously missing license file.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-yatl/python-yatl.mk
+++ package/python-yatl/python-yatl.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_YATL_VERSION = 20220907.1
+PYTHON_YATL_VERSION = 20230507.3
 PYTHON_YATL_SOURCE = yatl-$(PYTHON_YATL_VERSION).tar.gz
-PYTHON_YATL_SITE = https://files.pythonhosted.org/packages/09/68/0c9d0a1192fdad5f3d38db096106c5adabb9b26c9ae107897997f56d1788
+PYTHON_YATL_SITE = https://files.pythonhosted.org/packages/bd/3b/723a667a24512a299e1e139608e787c3b24b7819302c15c7aac09c3bec68
 PYTHON_YATL_SETUP_TYPE = setuptools
 PYTHON_YATL_LICENSE = BSD-3-Clause
+PYTHON_YATL_LICENSE_FILES = LICENSE.txt
 
 $(eval $(python-package))
 $(eval $(host-python-package))
