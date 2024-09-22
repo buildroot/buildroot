5bbaccb77ae3feb81568801418e7cdfc6e67d50d
James Hilliard <james.hilliard1@gmail.com>
2024-09-21 23:37:32 -0600

    package/python-txtorcon: bump to version 24.8.0
    Drop no longer required python-idna runtime dependency.
    
    Drop no longer required python-incremental runtime dependency.
    
    Drop no longer required python-pyopenssl runtime dependency.
    
    Drop no longer required python-service-identity runtime dependency.
    
    Add new python-cryptography runtime dependency.
    
    Add new python-twisted-tls runtime dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-txtorcon/python-txtorcon.mk
+++ package/python-txtorcon/python-txtorcon.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_TXTORCON_VERSION = 22.0.0
+PYTHON_TXTORCON_VERSION = 24.8.0
 PYTHON_TXTORCON_SOURCE = txtorcon-$(PYTHON_TXTORCON_VERSION).tar.gz
-PYTHON_TXTORCON_SITE = https://files.pythonhosted.org/packages/47/ca/7941660345c769637a97bdbaf25202460fbab35eead55c538fb1fe61b71e
+PYTHON_TXTORCON_SITE = https://files.pythonhosted.org/packages/b9/9f/7815b07d8bc775d9578d9131267bb7ce3e91e31305688736ed796ae724d1
 PYTHON_TXTORCON_SETUP_TYPE = setuptools
 PYTHON_TXTORCON_LICENSE = MIT
 PYTHON_TXTORCON_LICENSE_FILES = LICENSE
