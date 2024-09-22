c8027a021ba8951fa9c85f1c93cea46f6d97cfb4
James Hilliard <james.hilliard1@gmail.com>
2024-09-22 00:19:37 -0600

    package/python-watchdog: bump to version 5.0.2
    Drop all runtime dependendencies which are no longer required.
    
    License hash changed due to header update:
    https://github.com/gorakhargosh/watchdog/commit/80576b4124dc0892a393365754ffe4e1521e3427
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-watchdog/python-watchdog.mk
+++ package/python-watchdog/python-watchdog.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_WATCHDOG_VERSION = 2.2.1
+PYTHON_WATCHDOG_VERSION = 5.0.2
 PYTHON_WATCHDOG_SOURCE = watchdog-$(PYTHON_WATCHDOG_VERSION).tar.gz
-PYTHON_WATCHDOG_SITE = https://files.pythonhosted.org/packages/11/6f/0396d373e039b89c60e23a1a9025edc6dd203121fe0af7d1427e85d5ec98
+PYTHON_WATCHDOG_SITE = https://files.pythonhosted.org/packages/cd/5e/95dcd86d8339fcf76385f7fad5e49cbfd989b6c6199127121c9587febc65
 PYTHON_WATCHDOG_SETUP_TYPE = setuptools
 PYTHON_WATCHDOG_LICENSE = Apache-2.0
 PYTHON_WATCHDOG_LICENSE_FILES = LICENSE COPYING
