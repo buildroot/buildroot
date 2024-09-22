478f4a0b3a64e3d32d65892f3b606f0b0d67d6ea
James Hilliard <james.hilliard1@gmail.com>
2024-09-21 21:52:54 -0600

    package/python-tornado: bump to version 6.4.1
    Drop patch which is now upstream; that was a backport from an upstream
    security fix for CVE-2023-28370, so we can drop the CVE exclusion now.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    [yann.morin.1998@free.fr:
      - drop CVE exclusion
      - extend commit log accordingly
    ]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-tornado/python-tornado.mk
+++ package/python-tornado/python-tornado.mk
@@ -4,15 +4,13 @@
 #
 ################################################################################
 
-PYTHON_TORNADO_VERSION = 6.2
+PYTHON_TORNADO_VERSION = 6.4.1
 PYTHON_TORNADO_SOURCE = tornado-$(PYTHON_TORNADO_VERSION).tar.gz
-PYTHON_TORNADO_SITE = https://files.pythonhosted.org/packages/f3/9e/225a41452f2d9418d89be5e32cf824c84fe1e639d350d6e8d49db5b7f73a
+PYTHON_TORNADO_SITE = https://files.pythonhosted.org/packages/ee/66/398ac7167f1c7835406888a386f6d0d26ee5dbf197d8a571300be57662d3
 PYTHON_TORNADO_LICENSE = Apache-2.0
 PYTHON_TORNADO_LICENSE_FILES = LICENSE
 PYTHON_TORNADO_CPE_ID_VENDOR = tornadoweb
 PYTHON_TORNADO_CPE_ID_PRODUCT = tornado
 PYTHON_TORNADO_SETUP_TYPE = setuptools
-# 0001-web-Fix-an-open-redirect-in-StaticFileHandler.patch
-PYTHON_TORNADO_IGNORE_CVES += CVE-2023-28370
 
 $(eval $(python-package))
