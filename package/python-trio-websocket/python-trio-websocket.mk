d16bb45b68feb06ee72492e67b46b56fee1d693a
James Hilliard <james.hilliard1@gmail.com>
2024-09-21 23:05:52 -0600

    package/python-trio-websocket: bump to version 0.11.1
    Drop no longer required python-async-generator runtime dependency.
    
    Add license file which was previously missing.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-trio-websocket/python-trio-websocket.mk
+++ package/python-trio-websocket/python-trio-websocket.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-PYTHON_TRIO_WEBSOCKET_VERSION = 0.9.2
+PYTHON_TRIO_WEBSOCKET_VERSION = 0.11.1
 PYTHON_TRIO_WEBSOCKET_SOURCE = trio-websocket-$(PYTHON_TRIO_WEBSOCKET_VERSION).tar.gz
-PYTHON_TRIO_WEBSOCKET_SITE = https://files.pythonhosted.org/packages/75/91/44a0a016025794ba9fef530a6fbe59987153e2cbea7e11fe2f3d8c618740
+PYTHON_TRIO_WEBSOCKET_SITE = https://files.pythonhosted.org/packages/dd/36/abad2385853077424a11b818d9fd8350d249d9e31d583cb9c11cd4c85eda
 PYTHON_TRIO_WEBSOCKET_SETUP_TYPE = setuptools
 PYTHON_TRIO_WEBSOCKET_LICENSE = MIT
+PYTHON_TRIO_WEBSOCKET_LICENSE_FILES = LICENSE
 
 $(eval $(python-package))
