0144197cc996c44b6da18255ff2fb227329f0441
James Hilliard <james.hilliard1@gmail.com>
2024-09-22 02:26:42 -0600

    package/python-anyio: bump to version 4.6.0
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-anyio/python-anyio.mk
+++ package/python-anyio/python-anyio.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ANYIO_VERSION = 4.4.0
+PYTHON_ANYIO_VERSION = 4.6.0
 PYTHON_ANYIO_SOURCE = anyio-$(PYTHON_ANYIO_VERSION).tar.gz
-PYTHON_ANYIO_SITE = https://files.pythonhosted.org/packages/e6/e3/c4c8d473d6780ef1853d630d581f70d655b4f8d7553c6997958c283039a2
+PYTHON_ANYIO_SITE = https://files.pythonhosted.org/packages/78/49/f3f17ec11c4a91fe79275c426658e509b07547f874b14c1a526d86a83fc8
 PYTHON_ANYIO_SETUP_TYPE = setuptools
 PYTHON_ANYIO_LICENSE = MIT
 PYTHON_ANYIO_LICENSE_FILES = LICENSE
