3db06403bf494d3e900490408af0836b581f255d
James Hilliard <james.hilliard1@gmail.com>
2024-09-21 21:29:28 -0600

    package/python-tcolorpy: bump to version 0.1.6
    Add new host-python-setuptools-scm build dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-tcolorpy/python-tcolorpy.mk
+++ package/python-tcolorpy/python-tcolorpy.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_TCOLORPY_VERSION = 0.1.4
+PYTHON_TCOLORPY_VERSION = 0.1.6
 PYTHON_TCOLORPY_SOURCE = tcolorpy-$(PYTHON_TCOLORPY_VERSION).tar.gz
-PYTHON_TCOLORPY_SITE = https://files.pythonhosted.org/packages/ba/e0/20b99847e76bb89784b68634399261d770b01054bca16cd19a06ac9c2e67
+PYTHON_TCOLORPY_SITE = https://files.pythonhosted.org/packages/af/f6/b17885354c5addfb93c14f3ed7172c33494f989dd5f83c0340d2c8fa3b69
 PYTHON_TCOLORPY_SETUP_TYPE = setuptools
 PYTHON_TCOLORPY_LICENSE = MIT
 PYTHON_TCOLORPY_LICENSE_FILES = LICENSE
+PYTHON_TCOLORPY_DEPENDENCIES = host-python-setuptools-scm
 
 $(eval $(python-package))
