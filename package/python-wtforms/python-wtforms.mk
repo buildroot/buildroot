8b2af895a50245e4e66556c2b34769f0b80ba05a
James Hilliard <james.hilliard1@gmail.com>
2024-09-22 00:50:05 -0600

    package/python-wtforms: bump to version 3.1.2
    Migrate from setuptools to hatch build backend.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-wtforms/python-wtforms.mk
+++ package/python-wtforms/python-wtforms.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-PYTHON_WTFORMS_VERSION = 3.0.1
-PYTHON_WTFORMS_SOURCE = WTForms-$(PYTHON_WTFORMS_VERSION).tar.gz
-PYTHON_WTFORMS_SITE = https://files.pythonhosted.org/packages/9a/7d/d4aa68f5bfcb91dd61a7faf0e862512ae7b3d531c41f24c217910aec0559
-PYTHON_WTFORMS_SETUP_TYPE = setuptools
+PYTHON_WTFORMS_VERSION = 3.1.2
+PYTHON_WTFORMS_SOURCE = wtforms-$(PYTHON_WTFORMS_VERSION).tar.gz
+PYTHON_WTFORMS_SITE = https://files.pythonhosted.org/packages/6a/c7/96d10183c3470f1836846f7b9527d6cb0b6c2226ebca40f36fa29f23de60
+PYTHON_WTFORMS_SETUP_TYPE = pep517
 PYTHON_WTFORMS_LICENSE = BSD-3-Clause
 PYTHON_WTFORMS_LICENSE_FILES = LICENSE.rst docs/license.rst
-PYTHON_WTFORMS_DEPENDENCIES = host-python-babel
+PYTHON_WTFORMS_DEPENDENCIES = host-python-babel host-python-hatchling
 
 $(eval $(python-package))
