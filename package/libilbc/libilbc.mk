540a0a567a4d3f092bb5a2ff7f4fdd1042533c51
Bernd Kuhls <bernd@kuhls.net>
2024-09-26 08:15:12 +0200

    package/libilbc: bump version
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libilbc/libilbc.mk
+++ package/libilbc/libilbc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBILBC_VERSION = 829b08c7902ceb87a261279fabb36b6d523c6e07
+LIBILBC_VERSION = 51d3cf8e157c3c80d7169b738073865eca83f5a3
 LIBILBC_SITE = $(call github,freeswitch,libilbc,$(LIBILBC_VERSION))
 LIBILBC_LICENSE = Global IP Sound iLBC Public License v2.0
 LIBILBC_LICENSE_FILES = gips_iLBClicense.pdf
