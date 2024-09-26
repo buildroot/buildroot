62e24c548bd894ff095670fc91620476d8cd3ae1
James Hilliard <james.hilliard1@gmail.com>
2024-09-26 09:30:46 -0600

    package/python-alembic: bump to version 1.13.3
    Drop no longer required --skip-dependency-check build option as
    upper setuptools version is no longer pinned:
    https://github.com/sqlalchemy/alembic/commit/9d6e212b77c8ce5ea1164b6d67f2ba491beba413
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-alembic/python-alembic.mk
+++ package/python-alembic/python-alembic.mk
@@ -4,12 +4,11 @@
 #
 ################################################################################
 
-PYTHON_ALEMBIC_VERSION = 1.13.2
+PYTHON_ALEMBIC_VERSION = 1.13.3
 PYTHON_ALEMBIC_SOURCE = alembic-$(PYTHON_ALEMBIC_VERSION).tar.gz
-PYTHON_ALEMBIC_SITE = https://files.pythonhosted.org/packages/66/e2/efa88e86029cada2da5941ec664d50d9a3b2a91f5066405c6f90e5016c16
+PYTHON_ALEMBIC_SITE = https://files.pythonhosted.org/packages/94/a2/840c3b84382dce8624bc2f0ee67567fc74c32478d0c5a5aea981518c91c3
 PYTHON_ALEMBIC_SETUP_TYPE = setuptools
 PYTHON_ALEMBIC_LICENSE = MIT
 PYTHON_ALEMBIC_LICENSE_FILES = LICENSE
-PYTHON_ALEMBIC_BUILD_OPTS = --skip-dependency-check
 
 $(eval $(python-package))
