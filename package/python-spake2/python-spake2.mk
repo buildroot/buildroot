4211199772b0dd90627ad155f33e0e0c1286521a
James Hilliard <james.hilliard1@gmail.com>
2024-09-26 16:08:22 -0600

    package/python-spake2: bump to version 0.9
    Drop patch which is no longer needed/applicable.
    
    Drop no longer required python-hkdf runtime dependency.
    
    Add new python-cryptography runtime dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-spake2/python-spake2.mk
+++ package/python-spake2/python-spake2.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SPAKE2_VERSION = 0.8
+PYTHON_SPAKE2_VERSION = 0.9
 PYTHON_SPAKE2_SOURCE = spake2-$(PYTHON_SPAKE2_VERSION).tar.gz
-PYTHON_SPAKE2_SITE = https://files.pythonhosted.org/packages/60/0b/bb5eca8e18c38a10b1c207bbe6103df091e5cf7b3e5fdc0efbcad7b85b60
+PYTHON_SPAKE2_SITE = https://files.pythonhosted.org/packages/c5/4b/32ad65f8ff5c49254e218ccaae8fc16900cfc289954fb372686159ebe315
 PYTHON_SPAKE2_SETUP_TYPE = setuptools
 PYTHON_SPAKE2_LICENSE = MIT
 PYTHON_SPAKE2_LICENSE_FILES = LICENSE
