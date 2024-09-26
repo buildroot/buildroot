4e5fd24c8b7438672c475d0559200ff72c4b1cc7
Bernd Kuhls <bernd@kuhls.net>
2024-09-26 08:15:11 +0200

    package/libilbc: switch to github
    The previous repo is not available anymore.
    
    Fixes:
    https://autobuild.buildroot.org/results/8c8b073ce163131763fca978b400e596fcf39e62
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libilbc/libilbc.mk
+++ package/libilbc/libilbc.mk
@@ -5,8 +5,7 @@
 ################################################################################
 
 LIBILBC_VERSION = 829b08c7902ceb87a261279fabb36b6d523c6e07
-LIBILBC_SITE = https://freeswitch.org/stash/scm/sd/libilbc.git
-LIBILBC_SITE_METHOD = git
+LIBILBC_SITE = $(call github,freeswitch,libilbc,$(LIBILBC_VERSION))
 LIBILBC_LICENSE = Global IP Sound iLBC Public License v2.0
 LIBILBC_LICENSE_FILES = gips_iLBClicense.pdf
 LIBILBC_AUTORECONF = YES
