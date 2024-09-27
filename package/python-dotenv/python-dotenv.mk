eb11879804c1e9053e182144c077b3ff6ac6a50f
James Hilliard <james.hilliard1@gmail.com>
2024-09-27 17:08:47 -0600

    package/python-dotenv: new package
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Reviewed-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/python-dotenv/python-dotenv.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# python-dotenv
+#
+################################################################################
+
+PYTHON_DOTENV_VERSION = 1.0.1
+PYTHON_DOTENV_SITE = https://files.pythonhosted.org/packages/bc/57/e84d88dfe0aec03b7a2d4327012c1627ab5f03652216c63d49846d7a6c58
+PYTHON_DOTENV_SETUP_TYPE = setuptools
+PYTHON_DOTENV_LICENSE = BSD-3-Clause
+PYTHON_DOTENV_LICENSE_FILES = LICENSE
+
+$(eval $(python-package))
