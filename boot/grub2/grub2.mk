3d4930311871bcf7db91080991159d2d8262eb4c
Julien Olivain <ju.o@free.fr>
2024-09-29 18:53:43 +0200

    boot/grub2: replace host-python3 with BR2_PYTHON3_HOST_DEPENDENCY
    Commit 86bb1b236 "boot/grub2: needs host-python3" [1] introduced a
    dependency on host-python3.
    
    Since grub does not have any specific requirements on host Python
    modules, or recent host Python version, this commit replaces the
    host-python3 dependency with BR2_PYTHON3_HOST_DEPENDENCY. This will
    skip the host-python3 compilation if a sufficient version (3.4 or
    greater at the time of this commit) is already present on host. This
    will save build time.
    
    This optimization was suggested by Peter, in [2].
    
    Note 1: this commit was checked to ensure that grub is building with
    Python 3.4.
    
    Note 2: BR2_PYTHON3_HOST_DEPENDENCY was introduced in commit b60729784
    "support/dependencies: add a check for python3" [3].
    
    [1] https://gitlab.com/buildroot.org/buildroot/-/commit/86bb1b2360bdaed069cd087541f4edad1d5ce925
    [2] https://lists.buildroot.org/pipermail/buildroot/2024-September/763967.html
    [3] https://gitlab.com/buildroot.org/buildroot/-/commit/b60729784ab1c2f75dca30f924f4dd3176713ae8
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 8a71fda371c1785f9e4364f05ab0a632e1946c53)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- boot/grub2/grub2.mk
+++ boot/grub2/grub2.mk
@@ -9,8 +9,10 @@ GRUB2_SITE = http://ftp.gnu.org/gnu/grub
 GRUB2_SOURCE = grub-$(GRUB2_VERSION).tar.xz
 GRUB2_LICENSE = GPL-3.0+
 GRUB2_LICENSE_FILES = COPYING
-GRUB2_DEPENDENCIES = host-bison host-flex host-gawk host-grub2 host-python3
-HOST_GRUB2_DEPENDENCIES = host-bison host-flex host-gawk host-python3
+GRUB2_DEPENDENCIES = host-bison host-flex host-gawk host-grub2 \
+	$(BR2_PYTHON3_HOST_DEPENDENCY)
+HOST_GRUB2_DEPENDENCIES = host-bison host-flex host-gawk \
+	$(BR2_PYTHON3_HOST_DEPENDENCY)
 GRUB2_INSTALL_IMAGES = YES
 
 # CVE-2019-14865 is about a flaw in the grub2-set-bootflag tool, which
