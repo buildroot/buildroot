d49813e762a4ac6a4cf4e219f9d7fbeb4aa93a85
James Hilliard <james.hilliard1@gmail.com>
2024-09-30 17:11:56 -0600

    package/python-pysnmp: bump to version 7.1.4
    Drop no longer required python-pysnmpcrypto runtime dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-pysnmp/python-pysnmp.mk
+++ package/python-pysnmp/python-pysnmp.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYSNMP_VERSION = 7.1.3
+PYTHON_PYSNMP_VERSION = 7.1.4
 PYTHON_PYSNMP_SOURCE = pysnmp-$(PYTHON_PYSNMP_VERSION).tar.gz
-PYTHON_PYSNMP_SITE = https://files.pythonhosted.org/packages/f3/0c/bb2ccb587b5e79036dd83aa5fe835c2bc460a3098c4060461e9d324c6670
+PYTHON_PYSNMP_SITE = https://files.pythonhosted.org/packages/43/71/18a9d8c00efba166b66408040028f25a8ea20d2acc9cd89bd780369544b1
 PYTHON_PYSNMP_SETUP_TYPE = pep517
 PYTHON_PYSNMP_LICENSE = BSD-2-Clause
 PYTHON_PYSNMP_LICENSE_FILES = LICENSE.rst
