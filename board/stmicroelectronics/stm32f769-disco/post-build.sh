9fd8131dcb3939fba67a34626addb3260a733cb6
Dario Binacchi <dario.binacchi@amarulasolutions.com>
2024-10-05 21:15:28 +0200

    board/stm32f769-disco: move kernel and DTB to rootfs partition
    The patch, following the suggestion expressed in the review [1] for the
    stm32f746-disco board support, moves the Linux kernel and the related
    device tree to the ext4 rootfs partition. By doing so, the vfat partition,
    improperly called u-boot, no longer makes sense and can therefore be
    removed. From now on, U-Boot will load the kernel and the device tree
    from the ext4 rootfs partition.
    
    [1] https://patchwork.ozlabs.org/project/buildroot/patch/20240822183742.3550055-3-dario.binacchi@amarulasolutions.com/
    
    Suggested-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- board/stmicroelectronics/stm32f769-disco/post-build.sh
+++ board/stmicroelectronics/stm32f769-disco/post-build.sh
@@ -4,4 +4,4 @@ BOARD_DIR="$(dirname "$0")"
 # Kernel is built without devpts support
 sed -i '/^devpts/d' "${TARGET_DIR}"/etc/fstab
 
-install -m 0644 -D "${BOARD_DIR}"/extlinux.conf "${BINARIES_DIR}"/extlinux/extlinux.conf
+install -m 0644 -D "${BOARD_DIR}"/extlinux.conf "${TARGET_DIR}"/boot/extlinux/extlinux.conf
