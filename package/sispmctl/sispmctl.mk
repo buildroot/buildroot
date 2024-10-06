36f7569b6d6b6828029a5787b6b266ac8524bb98
Julien Olivain <ju.o@free.fr>
2024-10-06 15:44:55 +0200

    package/sispmctl: bump to version 4.12
    For change log, see:
    https://sourceforge.net/p/sispmctl/git/ci/release-4.12/tree/ChangeLog
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/sispmctl/sispmctl.mk
+++ package/sispmctl/sispmctl.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SISPMCTL_VERSION = 4.11
+SISPMCTL_VERSION = 4.12
 SISPMCTL_SITE = https://downloads.sourceforge.net/project/sispmctl/sispmctl/sispmctl-$(SISPMCTL_VERSION)
 SISPMCTL_LICENSE = GPL-2.0+
 SISPMCTL_LICENSE_FILES = COPYING
