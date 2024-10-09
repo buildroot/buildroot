5d2e126decfb6be0af46630d203f71845428a14e
James Hilliard <james.hilliard1@gmail.com>
2024-10-09 12:31:42 -0600

    package/python-webargs: bump to version 8.6.0
    Migrate from setuptools to flit build backend.
    
    License hash changed due to year removal:
    https://github.com/marshmallow-code/webargs/commit/0917b88a60385cb12a9fa543ca5b7f6a0d6bb336
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-webargs/python-webargs.mk
+++ package/python-webargs/python-webargs.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_WEBARGS_VERSION = 8.2.0
+PYTHON_WEBARGS_VERSION = 8.6.0
 PYTHON_WEBARGS_SOURCE = webargs-$(PYTHON_WEBARGS_VERSION).tar.gz
-PYTHON_WEBARGS_SITE = https://files.pythonhosted.org/packages/4b/7d/fece8e6547b3edde8953b1c84a63da02e1a88efcb9d7d485e4d1b25d9c47
-PYTHON_WEBARGS_SETUP_TYPE = setuptools
+PYTHON_WEBARGS_SITE = https://files.pythonhosted.org/packages/8c/51/e9ee5d8315864adf65e92f858f826514538e30db542d4782dd94c2418464
+PYTHON_WEBARGS_SETUP_TYPE = flit
 PYTHON_WEBARGS_LICENSE = MIT
 PYTHON_WEBARGS_LICENSE_FILES = LICENSE
 PYTHON_WEBARGS_CPE_ID_VENDOR = webargs_project
