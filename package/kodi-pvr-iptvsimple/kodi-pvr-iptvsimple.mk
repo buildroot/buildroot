39da566f7f8536375543823a3d59a8b5228ca636
Bernd Kuhls <bernd@kuhls.net>
2024-10-13 10:27:01 +0200

    package/kodi-pvr-iptvsimple: bump version to 21.9.3-Omega
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-pvr-iptvsimple/kodi-pvr-iptvsimple.mk
+++ package/kodi-pvr-iptvsimple/kodi-pvr-iptvsimple.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PVR_IPTVSIMPLE_VERSION = 21.8.6-Omega
+KODI_PVR_IPTVSIMPLE_VERSION = 21.9.3-Omega
 KODI_PVR_IPTVSIMPLE_SITE = $(call github,kodi-pvr,pvr.iptvsimple,$(KODI_PVR_IPTVSIMPLE_VERSION))
 KODI_PVR_IPTVSIMPLE_LICENSE = GPL-2.0+
 KODI_PVR_IPTVSIMPLE_LICENSE_FILES = LICENSE.md
