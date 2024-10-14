908ba9d637975b820eaf1299d1adbdeb0daf9c6f
Vincent Stehlé <vincent.stehle@arm.com>
2024-10-14 20:05:07 +0200

    configs/rockpro64: bump Linux kernel, U-Boot and TF-A
    - Bump Linux kernel to v6.10.14 and add the dependency on host python3.
    - Bump U-Boot to v2024.10 and add the dependency on GnuTLS and
      util-linux (for uuid).
    - Bump TF-A to v2.11.
    - Turn on hashes verification and add hashes for the Linux kernel and
      U-Boot.
    - Fix shellcheck warnings in the post-build script.
    - Remove the related exceptions in .checkpackageignore.
    
    Signed-off-by: Vincent Stehlé <vincent.stehle@arm.com>
    Cc: Jagan Teki <jagan@amarulasolutions.com>
    Cc: Suniel Mahesh <sunil@amarulasolutions.com>
    Cc: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- board/pine64/rockpro64/post-build.sh
+++ board/pine64/rockpro64/post-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-BOARD_DIR="$(dirname $0)"
+BOARD_DIR=$(dirname "$0")
 
-install -m 0644 -D $BOARD_DIR/extlinux.conf $BINARIES_DIR/extlinux/extlinux.conf
+install -m 0644 -D "$BOARD_DIR/extlinux.conf" "$BINARIES_DIR/extlinux/extlinux.conf"
