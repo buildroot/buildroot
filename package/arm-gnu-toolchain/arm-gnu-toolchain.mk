889736fa06433c6757e78d0f20cec3d9dc52a9a9
Bryce Johnson <bryce@redpinelabs.com>
2024-10-22 15:03:34 -0600

    package/arm-gnu-toolchain: use $(HOSTARCH) to allow use on aarch64
    When building on an ARM64 build server, the build of
    configs/ti_am62x_sk_defconfig fails when building the Cortex-R5
    U-Boot, as it uses host-arm-gnu-toolchain, which assumes the host
    machine is x86-64.
    
    Use $(HOSTARCH) instead so that it can download the bare-metal
    toolchain also provided by ARM for the ARM64 host architecture.
    
    Signed-off-by: Bryce Johnson <bryce@redpinelabs.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/arm-gnu-toolchain/arm-gnu-toolchain.mk
+++ package/arm-gnu-toolchain/arm-gnu-toolchain.mk
@@ -6,7 +6,7 @@
 
 ARM_GNU_TOOLCHAIN_VERSION = 13.2.rel1
 ARM_GNU_TOOLCHAIN_SITE = https://developer.arm.com/-/media/Files/downloads/gnu/$(ARM_GNU_TOOLCHAIN_VERSION)/binrel
-ARM_GNU_TOOLCHAIN_SOURCE = arm-gnu-toolchain-$(ARM_GNU_TOOLCHAIN_VERSION)-x86_64-arm-none-eabi.tar.xz
+ARM_GNU_TOOLCHAIN_SOURCE = arm-gnu-toolchain-$(ARM_GNU_TOOLCHAIN_VERSION)-$(HOSTARCH)-arm-none-eabi.tar.xz
 ARM_GNU_TOOLCHAIN_LICENSE = GPL-3.0+
 
 HOST_ARM_GNU_TOOLCHAIN_INSTALL_DIR = $(HOST_DIR)/opt/gcc-arm-none-eabi
