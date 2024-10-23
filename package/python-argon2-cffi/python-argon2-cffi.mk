3e4b1a3031c0a848bf7dd28c0f423651171062dc
James Hilliard <james.hilliard1@gmail.com>
2024-10-23 17:59:52 -0600

    package/python-argon2-cffi: use hatch setup type
    Use the new hatch setup type which automatically pulls in the
    host-python-hatchling dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-argon2-cffi/python-argon2-cffi.mk
+++ package/python-argon2-cffi/python-argon2-cffi.mk
@@ -7,11 +7,10 @@
 PYTHON_ARGON2_CFFI_VERSION = 23.1.0
 PYTHON_ARGON2_CFFI_SOURCE = argon2_cffi-$(PYTHON_ARGON2_CFFI_VERSION).tar.gz
 PYTHON_ARGON2_CFFI_SITE = https://files.pythonhosted.org/packages/31/fa/57ec2c6d16ecd2ba0cf15f3c7d1c3c2e7b5fcb83555ff56d7ab10888ec8f
-PYTHON_ARGON2_CFFI_SETUP_TYPE = pep517
+PYTHON_ARGON2_CFFI_SETUP_TYPE = hatch
 PYTHON_ARGON2_CFFI_LICENSE = MIT
 PYTHON_ARGON2_CFFI_LICENSE_FILES = LICENSE
 PYTHON_ARGON2_CFFI_DEPENDENCIES = \
-	host-python-hatchling \
 	host-python-hatch-fancy-pypi-readme \
 	host-python-hatch-vcs
 
