8f35c55bfb642b817d336111f9e0b23c98774d66
James Hilliard <james.hilliard1@gmail.com>
2024-10-23 16:42:13 -0600

    package/python-httpx: use hatch setup type
    Use the new hatch setup type which automatically pulls in the
    host-python-hatchling dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-httpx/python-httpx.mk
+++ package/python-httpx/python-httpx.mk
@@ -7,13 +7,11 @@
 PYTHON_HTTPX_VERSION = 0.27.2
 PYTHON_HTTPX_SOURCE = httpx-$(PYTHON_HTTPX_VERSION).tar.gz
 PYTHON_HTTPX_SITE = https://files.pythonhosted.org/packages/78/82/08f8c936781f67d9e6b9eeb8a0c8b4e406136ea4c3d1f89a5db71d42e0e6
-PYTHON_HTTPX_SETUP_TYPE = pep517
+PYTHON_HTTPX_SETUP_TYPE = hatch
 PYTHON_HTTPX_LICENSE = BSD-3-Clause
 PYTHON_HTTPX_LICENSE_FILES = LICENSE.md
 PYTHON_HTTPX_CPE_ID_VENDOR = encode
 PYTHON_HTTPX_CPE_ID_PRODUCT = httpx
-PYTHON_HTTPX_DEPENDENCIES = \
-	host-python-hatchling \
-	host-python-hatch-fancy-pypi-readme
+PYTHON_HTTPX_DEPENDENCIES = host-python-hatch-fancy-pypi-readme
 
 $(eval $(python-package))
