3e6603ec122a167f93b6901f7d77d00958a1047f
Giulio Benetti <giulio.benetti@benettiengineering.com>
2024-10-23 19:04:46 +0200

    package/libtraceevent: bump to version 1.8.4
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 54c88416e85136ae3da1808ce17bf9641666ae1d)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libtraceevent/libtraceevent.mk
+++ package/libtraceevent/libtraceevent.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBTRACEEVENT_VERSION = 1.8.3
+LIBTRACEEVENT_VERSION = 1.8.4
 LIBTRACEEVENT_SITE = https://git.kernel.org/pub/scm/libs/libtrace/libtraceevent.git/snapshot
 LIBTRACEEVENT_LICENSE = GPL-2.0, LGPL-2.1
 LIBTRACEEVENT_LICENSE_FILES = LICENSES/GPL-2.0 LICENSES/LGPL-2.1
