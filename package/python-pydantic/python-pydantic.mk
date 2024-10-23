27af91823e3213d6a03029b1d8cba5b28f421654
James Hilliard <james.hilliard1@gmail.com>
2024-10-23 16:22:16 -0600

    package/python-pydantic: use hatch setup type
    Use the new hatch setup type which automatically pulls in the
    host-python-hatchling dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-pydantic/python-pydantic.mk
+++ package/python-pydantic/python-pydantic.mk
@@ -7,13 +7,11 @@
 PYTHON_PYDANTIC_VERSION = 2.9.2
 PYTHON_PYDANTIC_SOURCE = pydantic-$(PYTHON_PYDANTIC_VERSION).tar.gz
 PYTHON_PYDANTIC_SITE = https://files.pythonhosted.org/packages/a9/b7/d9e3f12af310e1120c21603644a1cd86f59060e040ec5c3a80b8f05fae30
-PYTHON_PYDANTIC_SETUP_TYPE = pep517
+PYTHON_PYDANTIC_SETUP_TYPE = hatch
 PYTHON_PYDANTIC_LICENSE = MIT
 PYTHON_PYDANTIC_LICENSE_FILES = LICENSE
 PYTHON_PYDANTIC_CPE_ID_VENDOR = pydantic_project
 PYTHON_PYDANTIC_CPE_ID_PRODUCT = pydantic
-PYTHON_PYDANTIC_DEPENDENCIES = \
-	host-python-hatchling \
-	host-python-hatch-fancy-pypi-readme
+PYTHON_PYDANTIC_DEPENDENCIES = host-python-hatch-fancy-pypi-readme
 
 $(eval $(python-package))
