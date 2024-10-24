e70dd18d50d997546c31ce53c2a2b1fd796e4429
James Hilliard <james.hilliard1@gmail.com>
2024-10-23 23:01:20 -0600

    package/python-wtforms: use hatch setup type
    Use the new hatch setup type which automatically pulls in the
    host-python-hatchling dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-wtforms/python-wtforms.mk
+++ package/python-wtforms/python-wtforms.mk
@@ -7,9 +7,9 @@
 PYTHON_WTFORMS_VERSION = 3.1.2
 PYTHON_WTFORMS_SOURCE = wtforms-$(PYTHON_WTFORMS_VERSION).tar.gz
 PYTHON_WTFORMS_SITE = https://files.pythonhosted.org/packages/6a/c7/96d10183c3470f1836846f7b9527d6cb0b6c2226ebca40f36fa29f23de60
-PYTHON_WTFORMS_SETUP_TYPE = pep517
+PYTHON_WTFORMS_SETUP_TYPE = hatch
 PYTHON_WTFORMS_LICENSE = BSD-3-Clause
 PYTHON_WTFORMS_LICENSE_FILES = LICENSE.rst docs/license.rst
-PYTHON_WTFORMS_DEPENDENCIES = host-python-babel host-python-hatchling
+PYTHON_WTFORMS_DEPENDENCIES = host-python-babel
 
 $(eval $(python-package))
