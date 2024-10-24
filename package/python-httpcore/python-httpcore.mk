07525ad019b341cc8a5ff51d2607c1b6d266e0a3
James Hilliard <james.hilliard1@gmail.com>
2024-10-23 18:11:57 -0600

    package/python-httpcore: use hatch setup type
    Use the new hatch setup type which automatically pulls in the
    host-python-hatchling dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-httpcore/python-httpcore.mk
+++ package/python-httpcore/python-httpcore.mk
@@ -7,11 +7,9 @@
 PYTHON_HTTPCORE_VERSION = 1.0.6
 PYTHON_HTTPCORE_SOURCE = httpcore-$(PYTHON_HTTPCORE_VERSION).tar.gz
 PYTHON_HTTPCORE_SITE = https://files.pythonhosted.org/packages/b6/44/ed0fa6a17845fb033bd885c03e842f08c1b9406c86a2e60ac1ae1b9206a6
-PYTHON_HTTPCORE_SETUP_TYPE = pep517
+PYTHON_HTTPCORE_SETUP_TYPE = hatch
 PYTHON_HTTPCORE_LICENSE = BSD-3-Clause
 PYTHON_HTTPCORE_LICENSE_FILES = LICENSE.md
-PYTHON_HTTPCORE_DEPENDENCIES = \
-	host-python-hatchling \
-	host-python-hatch-fancy-pypi-readme
+PYTHON_HTTPCORE_DEPENDENCIES = host-python-hatch-fancy-pypi-readme
 
 $(eval $(python-package))
