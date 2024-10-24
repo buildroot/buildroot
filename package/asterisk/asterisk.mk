761eb4ad9ec61660567c7dacb6e16f437aa80a1b
Waldemar Brodkorb <wbx@openadk.org>
2024-10-24 13:51:36 +0200

    package/asterisk: update to 20.10.0
    See here for a changelog:
    https://downloads.asterisk.org/pub/telephony/asterisk/releases/ChangeLog-20.10.0.md
    
    Signed-off-by: Waldemar Brodkorb <wbx@openadk.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 5ed6aa0fe30319e250fbe700895cfcd603c495ff)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/asterisk/asterisk.mk
+++ package/asterisk/asterisk.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ASTERISK_VERSION = 20.9.3
+ASTERISK_VERSION = 20.10.0
 # Use the github mirror: it's an official mirror maintained by Digium, and
 # provides tarballs, which the main Asterisk git tree (behind Gerrit) does not.
 ASTERISK_SITE = $(call github,asterisk,asterisk,$(ASTERISK_VERSION))
