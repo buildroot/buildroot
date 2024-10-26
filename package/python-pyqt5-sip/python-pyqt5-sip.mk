bcde59be440e315c02296183fd916f720d02cbab
Julien Olivain <ju.o@free.fr>
2024-10-26 19:27:41 +0200

    package/python-pyqt5-sip: bump version to 12.15.0
    The versioning scheme of this package is not trivial: the PyQt5-sip
    version number does not corresponds to the actual version tag name in
    its Python-SIP source repository. For that reason, this commit adds a
    comment explaining this peculiar mapping. This comment will help, in
    future maintenance tasks.
    
    To better understand the upstream changes in this package explained in
    this commit log, it is worth mentioning that:
    - PyQt5-sip 12.13.0 was generated with Python-SIP 6.7.12, and
    - PyQt5-sip 12.15.0 was generated with Python-SIP 6.8.6.
    
    The package license has changed to BSD-2-Clause, in upstream commits:
    https://github.com/Python-SIP/sip/commit/0ba3b233ec8ed7f22f2f2f950b64f61b50ddfc00
    https://github.com/Python-SIP/sip/commit/4dde1491f7b8ace15833cffb86bee82d60683968
    https://github.com/Python-SIP/sip/commit/f32039b07c47280039dcdcd0f8a2b1323fda912e
    
    This commit updates the _LICENSE and _LICENSE_FILES macro
    accordingly. The license hash file is also updated. While at it, this
    commit also adds the MD5 hash published upstream.
    
    This commit also introduces minor improvements to better follow
    Buildroot package conventions:
    - the .mk file header is changed to lower case,
    - hash file comments are updated,
    - the locally calculated license file hash is changed from MD5 to
      SHA256.
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-pyqt5-sip/python-pyqt5-sip.mk
+++ package/python-pyqt5-sip/python-pyqt5-sip.mk
@@ -1,14 +1,24 @@
 ################################################################################
 #
-# python-SIP-QT5
+# python-sip-qt5
 #
 ################################################################################
 
-PYTHON_PYQT5_SIP_VERSION = 12.13.0
-PYTHON_PYQT5_SIP_SITE = https://files.pythonhosted.org/packages/ee/81/fce2a475aa56c1f49707d9306b930695b6ff078c2242c9f2fd72a3214e1f
+# Note about the package version:
+# This module version corresponds in fact to the "sip" ABI
+# version (not the version of its generator). See:
+# https://github.com/Python-SIP/sip/blob/6.8.6/sipbuild/module/source/12/sip.h.in#L43
+# The source git repository of this module is located at:
+# https://github.com/Python-SIP/sip/tree/main/sipbuild/module/source
+# The Python-SIP version/tag which generated a given "sip" module is
+# recorded in the PyPI source file "sip.h", in the SIP_VERSION_STR
+# macro. For example, PyQt5-sip 12.15.0 was generated with Python-SIP
+# 6.8.6.
+PYTHON_PYQT5_SIP_VERSION = 12.15.0
+PYTHON_PYQT5_SIP_SITE = https://files.pythonhosted.org/packages/1b/15/78318d50f10062c428e97e7ce387e772616a4673c356018b905f247a6a85
 PYTHON_PYQT5_SIP_SOURCE = PyQt5_sip-$(PYTHON_PYQT5_SIP_VERSION).tar.gz
-PYTHON_PYQT5_SIP_LICENSE = SIP license or GPL-2.0 or GPL-3.0
-PYTHON_PYQT5_SIP_LICENSE_FILES = LICENSE LICENSE-GPL2 LICENSE-GPL3
+PYTHON_PYQT5_SIP_LICENSE = BSD-2-Clause
+PYTHON_PYQT5_SIP_LICENSE_FILES = LICENSE
 PYTHON_PYQT5_SIP_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
