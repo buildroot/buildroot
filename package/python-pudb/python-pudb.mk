86f77e8cada2810e4b229a36ab6ae42f078f2aa3
James Hilliard <james.hilliard1@gmail.com>
2024-10-26 16:49:36 -0600

    package/python-pudb: bump to version 2024.1.3
    Migrate from setuptools to hatch build backend.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-pudb/python-pudb.mk
+++ package/python-pudb/python-pudb.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_PUDB_VERSION = 2024.1.2
+PYTHON_PUDB_VERSION = 2024.1.3
 PYTHON_PUDB_SOURCE = pudb-$(PYTHON_PUDB_VERSION).tar.gz
-PYTHON_PUDB_SITE = https://files.pythonhosted.org/packages/54/70/fc7d81b7ac439d5e21c8c2b51e15cdc6632b720b02219057fe098a80e766
-PYTHON_PUDB_SETUP_TYPE = setuptools
+PYTHON_PUDB_SITE = https://files.pythonhosted.org/packages/36/27/fb087bdf2cd9c8e56d0347b863ce5995967c15a3e2a0b9245c7a8f6f1598
+PYTHON_PUDB_SETUP_TYPE = hatch
 PYTHON_PUDB_LICENSE = MIT
 PYTHON_PUDB_LICENSE_FILES = LICENSE
 
