5daf49911e03ce14f17719e41d033868c1533e2a
James Hilliard <james.hilliard1@gmail.com>
2024-10-26 15:22:39 -0600

    package/python-orjson: bump to version 3.10.10
    Drop no longer required python-cffi dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-orjson/python-orjson.mk
+++ package/python-orjson/python-orjson.mk
@@ -4,13 +4,12 @@
 #
 ################################################################################
 
-PYTHON_ORJSON_VERSION = 3.10.7
+PYTHON_ORJSON_VERSION = 3.10.10
 PYTHON_ORJSON_SOURCE_PYPI = orjson-$(PYTHON_ORJSON_VERSION).tar.gz
-PYTHON_ORJSON_SITE_PYPI = https://files.pythonhosted.org/packages/9e/03/821c8197d0515e46ea19439f5c5d5fd9a9889f76800613cfac947b5d7845
+PYTHON_ORJSON_SITE_PYPI = https://files.pythonhosted.org/packages/80/44/d36e86b33fc84f224b5f2cdf525adf3b8f9f475753e721c402b1ddef731e
 PYTHON_ORJSON_SITE = $(PYTHON_ORJSON_SITE_PYPI)/$(PYTHON_ORJSON_SOURCE_PYPI)?buildroot-path=filename
 PYTHON_ORJSON_SETUP_TYPE = maturin
 PYTHON_ORJSON_LICENSE = Apache-2.0 or MIT
 PYTHON_ORJSON_LICENSE_FILES = LICENSE-APACHE LICENSE-MIT
-PYTHON_ORJSON_DEPENDENCIES = host-python-cffi
 
 $(eval $(python-package))
