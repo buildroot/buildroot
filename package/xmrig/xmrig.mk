7f562095c0a1bd7bf6b4de144b40a3dfa112cad8
Bernd Kuhls <bernd@kuhls.net>
2024-10-27 21:42:15 +0100

    package/xmrig: bump version to 6.22.1
    Changelog: https://github.com/xmrig/xmrig/blob/master/CHANGELOG.md
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/xmrig/xmrig.mk
+++ package/xmrig/xmrig.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XMRIG_VERSION = 6.22.0
+XMRIG_VERSION = 6.22.1
 XMRIG_SITE = $(call github,xmrig,xmrig,v$(XMRIG_VERSION))
 XMRIG_LICENSE = GPL-3.0+
 XMRIG_LICENSE_FILES = LICENSE
