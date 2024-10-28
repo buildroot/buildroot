6276e8044dae5e6fa48910e909f8f6b2bda15152
James Hilliard <james.hilliard1@gmail.com>
2024-10-27 19:35:37 -0600

    package/python-yarl: bump to version 1.16.0
    Add new python-propcache runtime dependency.
    
    Add new host-python-cython build dependency.
    
    Add new host-python-expandvars build dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-yarl/python-yarl.mk
+++ package/python-yarl/python-yarl.mk
@@ -4,11 +4,14 @@
 #
 ################################################################################
 
-PYTHON_YARL_VERSION = 1.9.2
+PYTHON_YARL_VERSION = 1.16.0
 PYTHON_YARL_SOURCE = yarl-$(PYTHON_YARL_VERSION).tar.gz
-PYTHON_YARL_SITE = https://files.pythonhosted.org/packages/5f/3f/04b3c5e57844fb9c034b09c5cb6d2b43de5d64a093c30529fd233e16cf09
+PYTHON_YARL_SITE = https://files.pythonhosted.org/packages/23/52/e9766cc6c2eab7dd1e9749c52c9879317500b46fb97d4105223f86679f93
 PYTHON_YARL_LICENSE = Apache-2.0
 PYTHON_YARL_LICENSE_FILES = LICENSE
 PYTHON_YARL_SETUP_TYPE = setuptools
+PYTHON_YARL_DEPENDENCIES = \
+	host-python-cython \
+	host-python-expandvars
 
 $(eval $(python-package))
