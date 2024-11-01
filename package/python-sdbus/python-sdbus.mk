cbce27d2519cc9caf5710d67282975c915a4b25a
James Hilliard <james.hilliard1@gmail.com>
2024-11-01 13:53:20 -0600

    package/python-sdbus: bump to version 0.13.0
    Add -fPIC to cflags which is now required to build python-sdbus.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- package/python-sdbus/python-sdbus.mk
+++ package/python-sdbus/python-sdbus.mk
@@ -4,13 +4,14 @@
 #
 ################################################################################
 
-PYTHON_SDBUS_VERSION = 0.12.0
+PYTHON_SDBUS_VERSION = 0.13.0
 PYTHON_SDBUS_SOURCE = sdbus-$(PYTHON_SDBUS_VERSION).tar.gz
-PYTHON_SDBUS_SITE = https://files.pythonhosted.org/packages/8e/39/3d49f0d18dcba3344af756f31e4408e7de50b3df86fa3f3ea6f604402f16
+PYTHON_SDBUS_SITE = https://files.pythonhosted.org/packages/4f/c7/8740ff78e9ffdbb9a28e7722e145795015c62ea7ce812242f5968073511c
 PYTHON_SDBUS_SETUP_TYPE = setuptools
 PYTHON_SDBUS_LICENSE = LGPL-2.1+
 PYTHON_SDBUS_LICENSE_FILES = COPYING
 PYTHON_SDBUS_DEPENDENCIES = systemd
+PYTHON_SDBUS_ENV = CFLAGS="$(TARGET_CFLAGS) -fPIC"
 
 ifeq ($(BR2_STATIC_LIBS),y)
 PYTHON_SDBUS_ENV += PYTHON_SDBUS_USE_STATIC_LINK=1
