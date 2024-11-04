a6f672846a653a8278704688514369441f9283a5
Michel Alex <Alex.Michel@wiedemann-group.com>
2024-11-04 12:41:44 +0000

    package/libzenoh-c: bump to version 1.0.1
    https://github.com/eclipse-zenoh/zenoh-c/releases/tag/1.0.1
    
    Signed-off-by: Alex Michel <alex.michel@wiedemann-group.com>
    [Peter: drop 1.0.0.12 comment]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libzenoh-c/libzenoh-c.mk
+++ package/libzenoh-c/libzenoh-c.mk
@@ -4,10 +4,7 @@
 #
 ################################################################################
 
-# This 1.0.0 version is really newer than 1.0.0.12, so please don't
-# update 1.0.0 even if release-monitoring.org claims it's a newer
-# version.
-LIBZENOH_C_VERSION = 1.0.0
+LIBZENOH_C_VERSION = 1.0.1
 LIBZENOH_C_SITE = $(call github,eclipse-zenoh,zenoh-c,$(LIBZENOH_C_VERSION))
 LIBZENOH_C_LICENSE = Apache-2.0 or EPL-2.0
 LIBZENOH_C_LICENSE_FILES = LICENSE
