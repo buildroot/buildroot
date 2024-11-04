7304a3aa4c392bc91a35b4da0d6271e0bdb146fc
Michel Alex <Alex.Michel@wiedemann-group.com>
2024-11-04 12:41:48 +0000

    package/libzenoh-pico: bump to version 1.0.1
    https://github.com/eclipse-zenoh/zenoh-pico/releases/tag/1.0.1
    
    Signed-off-by: Alex Michel <alex.michel@wiedemann-group.com>
    [Peter: drop 1.0.0.12 comment]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libzenoh-pico/libzenoh-pico.mk
+++ package/libzenoh-pico/libzenoh-pico.mk
@@ -4,10 +4,7 @@
 #
 ################################################################################
 
-# This 1.0.0 version is really newer than 1.0.0.12, so please don't
-# update 1.0.0 even if release-monitoring.org claims it's a newer
-# version.
-LIBZENOH_PICO_VERSION = 1.0.0
+LIBZENOH_PICO_VERSION = 1.0.1
 LIBZENOH_PICO_SITE = $(call github,eclipse-zenoh,zenoh-pico,$(LIBZENOH_PICO_VERSION))
 LIBZENOH_PICO_LICENSE = Apache-2.0 or EPL-2.0
 LIBZENOH_PICO_LICENSE_FILES = LICENSE
