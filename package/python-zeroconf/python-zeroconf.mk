f0baaa91ea34beddce43bfefa48f383712c28010
James Hilliard <james.hilliard1@gmail.com>
2024-11-05 10:44:10 -0700

    package/python-zeroconf: use poetry setup type
    Use the new poetry setup type which automatically pulls in the
    host-python-poetry-core dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-zeroconf/python-zeroconf.mk
+++ package/python-zeroconf/python-zeroconf.mk
@@ -7,12 +7,11 @@
 PYTHON_ZEROCONF_VERSION = 0.136.0
 PYTHON_ZEROCONF_SOURCE = zeroconf-$(PYTHON_ZEROCONF_VERSION).tar.gz
 PYTHON_ZEROCONF_SITE = https://files.pythonhosted.org/packages/82/e4/17075a9f1951b031dfd92d57916505574e0d1eab3f2fb7deecabd2be581e
-PYTHON_ZEROCONF_SETUP_TYPE = pep517
+PYTHON_ZEROCONF_SETUP_TYPE = poetry
 PYTHON_ZEROCONF_LICENSE = LGPL-2.1+
 PYTHON_ZEROCONF_LICENSE_FILES = COPYING
 PYTHON_ZEROCONF_DEPENDENCIES = \
 	host-python-cython \
-	host-python-poetry-core \
 	host-python-setuptools
 
 $(eval $(python-package))
