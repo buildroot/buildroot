b350eba1fc0a5d08b7fd6a5c75832a2299c0f54d
James Hilliard <james.hilliard1@gmail.com>
2024-11-05 10:59:31 -0700

    package/bmap-tools: use poetry setup type
    Use the new poetry setup type which automatically pulls in the
    host-python-poetry-core dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/bmap-tools/bmap-tools.mk
+++ package/bmap-tools/bmap-tools.mk
@@ -8,9 +8,7 @@ BMAP_TOOLS_VERSION = 3.8.0
 BMAP_TOOLS_SITE = $(call github,yoctoproject,bmaptool,v$(BMAP_TOOLS_VERSION))
 BMAP_TOOLS_LICENSE = GPL-2.0
 BMAP_TOOLS_LICENSE_FILES = LICENSE
-BMAP_TOOLS_SETUP_TYPE = setuptools
-BMAP_TOOLS_DEPENDENCIES = host-python-poetry-core
-HOST_BMAP_TOOLS_DEPENDENCIES = host-python-poetry-core
+BMAP_TOOLS_SETUP_TYPE = poetry
 
 $(eval $(python-package))
 $(eval $(host-python-package))
