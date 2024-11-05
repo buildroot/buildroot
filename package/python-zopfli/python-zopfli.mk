580d899382e28beb518d697315e9364c4dfd73d3
James Hilliard <james.hilliard1@gmail.com>
2024-11-05 12:26:00 -0700

    package/python-zopfli: bump to version 0.2.3.post1
    Switch from zip archive to tar.gz archive.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-zopfli/python-zopfli.mk
+++ package/python-zopfli/python-zopfli.mk
@@ -4,18 +4,12 @@
 #
 ################################################################################
 
-PYTHON_ZOPFLI_VERSION = 0.2.3
-PYTHON_ZOPFLI_SOURCE = zopfli-$(PYTHON_ZOPFLI_VERSION).zip
-PYTHON_ZOPFLI_SITE = https://files.pythonhosted.org/packages/92/d8/71230eb25ede499401a9a39ddf66fab4e4dab149bf75ed2ecea51a662d9e
+PYTHON_ZOPFLI_VERSION = 0.2.3.post1
+PYTHON_ZOPFLI_SOURCE = zopfli-$(PYTHON_ZOPFLI_VERSION).tar.gz
+PYTHON_ZOPFLI_SITE = https://files.pythonhosted.org/packages/5e/7c/a8f6696e694709e2abcbccd27d05ef761e9b6efae217e11d977471555b62
 PYTHON_ZOPFLI_SETUP_TYPE = setuptools
 PYTHON_ZOPFLI_LICENSE = Apache-2.0
 PYTHON_ZOPFLI_LICENSE_FILES = COPYING
 PYTHON_ZOPFLI_DEPENDENCIES = host-python-setuptools-scm
-
-define PYTHON_ZOPFLI_EXTRACT_CMDS
-	$(UNZIP) -d $(@D) $(PYTHON_ZOPFLI_DL_DIR)/$(PYTHON_ZOPFLI_SOURCE)
-	mv $(@D)/zopfli-$(PYTHON_ZOPFLI_VERSION)/* $(@D)
-	$(RM) -r $(@D)/zopfli-$(PYTHON_ZOPFLI_VERSION)
-endef
 
 $(eval $(python-package))
