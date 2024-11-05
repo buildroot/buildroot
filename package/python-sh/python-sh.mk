11f7631d86f8e510069b9b8d0c83aa537aeea063
James Hilliard <james.hilliard1@gmail.com>
2024-11-05 11:02:25 -0700

    package/python-sh: use poetry setup type
    Use the new poetry setup type which automatically pulls in the
    host-python-poetry-core dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-sh/python-sh.mk
+++ package/python-sh/python-sh.mk
@@ -7,9 +7,8 @@
 PYTHON_SH_VERSION = 2.1.0
 PYTHON_SH_SOURCE = sh-$(PYTHON_SH_VERSION).tar.gz
 PYTHON_SH_SITE = https://files.pythonhosted.org/packages/52/12/b7965006c5adc57ba5459385358bd27c4983cd490884a75be86eb1d3efeb
-PYTHON_SH_SETUP_TYPE = pep517
+PYTHON_SH_SETUP_TYPE = poetry
 PYTHON_SH_LICENSE = MIT
 PYTHON_SH_LICENSE_FILES = LICENSE.txt
-PYTHON_SH_DEPENDENCIES = host-python-poetry-core
 
 $(eval $(python-package))
