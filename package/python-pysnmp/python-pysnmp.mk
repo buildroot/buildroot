62b2ac7a8e277d420c37d3bc5673b7764de2aa2c
James Hilliard <james.hilliard1@gmail.com>
2024-11-09 12:02:29 -0700

    package/python-pysnmp: bump to version 7.1.13
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-pysnmp/python-pysnmp.mk
+++ package/python-pysnmp/python-pysnmp.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYSNMP_VERSION = 7.1.8
+PYTHON_PYSNMP_VERSION = 7.1.13
 PYTHON_PYSNMP_SOURCE = pysnmp-$(PYTHON_PYSNMP_VERSION).tar.gz
-PYTHON_PYSNMP_SITE = https://files.pythonhosted.org/packages/e9/af/f9e379bd9e607fd289b6cef13d633667c4d213f8c1519a86f61eed09731f
+PYTHON_PYSNMP_SITE = https://files.pythonhosted.org/packages/d2/de/b766b1dc85e02c99835e05518d5efe9b2f1c875d406079584b4799d02db9
 PYTHON_PYSNMP_SETUP_TYPE = poetry
 PYTHON_PYSNMP_LICENSE = BSD-2-Clause
 PYTHON_PYSNMP_LICENSE_FILES = LICENSE.rst
