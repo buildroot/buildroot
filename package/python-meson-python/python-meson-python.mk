a3540f26d48b80dbd062fc4963aa76d1ca70ad6c
James Hilliard <james.hilliard1@gmail.com>
2024-11-09 11:40:28 -0700

    package/python-meson-python: bump to version 0.17.1
    Add new host-python-packaging build dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-meson-python/python-meson-python.mk
+++ package/python-meson-python/python-meson-python.mk
@@ -4,15 +4,16 @@
 #
 ################################################################################
 
-PYTHON_MESON_PYTHON_VERSION = 0.15.0
+PYTHON_MESON_PYTHON_VERSION = 0.17.1
 PYTHON_MESON_PYTHON_SOURCE = meson_python-$(PYTHON_MESON_PYTHON_VERSION).tar.gz
-PYTHON_MESON_PYTHON_SITE = https://files.pythonhosted.org/packages/a2/3b/276b596824a0820987fdcc7721618453b4f9a8305fe20b611a00ac3f948e
+PYTHON_MESON_PYTHON_SITE = https://files.pythonhosted.org/packages/67/66/91d242ea8dd1729addd36069318ba2cd03874872764f316c3bb51b633ed2
 PYTHON_MESON_PYTHON_SETUP_TYPE = pep517
 PYTHON_MESON_PYTHON_LICENSE = MIT
 PYTHON_MESON_PYTHON_LICENSE_FILES = LICENSE
 HOST_PYTHON_MESON_PYTHON_DEPENDENCIES = \
 	host-meson \
 	host-patchelf \
+	host-python-packaging \
 	host-python-pyproject-metadata
 
 $(eval $(host-python-package))
