b1becf0cc4bcbae4c847c2b276b4feb163bab8bc
James Hilliard <james.hilliard1@gmail.com>
2024-11-09 09:58:51 -0700

    package/python-blinker: bump to version 1.9.0
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-blinker/python-blinker.mk
+++ package/python-blinker/python-blinker.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_BLINKER_VERSION = 1.8.2
+PYTHON_BLINKER_VERSION = 1.9.0
 PYTHON_BLINKER_SOURCE = blinker-$(PYTHON_BLINKER_VERSION).tar.gz
-PYTHON_BLINKER_SITE = https://files.pythonhosted.org/packages/1e/57/a6a1721eff09598fb01f3c7cda070c1b6a0f12d63c83236edf79a440abcc
+PYTHON_BLINKER_SITE = https://files.pythonhosted.org/packages/21/28/9b3f50ce0e048515135495f198351908d99540d69bfdc8c1d15b73dc55ce
 PYTHON_BLINKER_SETUP_TYPE = flit
 PYTHON_BLINKER_LICENSE = MIT
 PYTHON_BLINKER_LICENSE_FILES = LICENSE.txt
