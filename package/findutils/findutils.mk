77fcce23c6d91ed492fac35a215af1bf46ad34a3
Julien Olivain <ju.o@free.fr>
2024-11-11 14:13:52 +0100

    package/findutils: bump version to 4.10.0
    For release announce, see:
    https://lists.gnu.org/archive/html/info-gnu/2024-07/msg00002.html
    
    This commit also updates the hash file comment, to use the sha256 hash
    published in the release announce, to provide the signature URL and the
    pgp key id used for the signature verification.
    
    See the gnulib documentation for details [1]. Contrary to what the
    option name might suggest, it doesn't really disable Y2038 support,
    but only the check that the system is Y2038 compliant. So even with
    --disable-year2038, if the system is Y2038 compliant (uses a 64-bit
    arch, uses the musl C library, or uses the glibc C library with
    BR2_TIME_BITS_64=y), findutils will be Y2038 compliant.
    
    [1] https://www.gnu.org/software/gnulib/manual/html_node/Avoiding-the-year-2038-problem.html
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/findutils/findutils.mk
+++ package/findutils/findutils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FINDUTILS_VERSION = 4.9.0
+FINDUTILS_VERSION = 4.10.0
 FINDUTILS_SOURCE = findutils-$(FINDUTILS_VERSION).tar.xz
 FINDUTILS_SITE = $(BR2_GNU_MIRROR)/findutils
 FINDUTILS_LICENSE = GPL-3.0+
@@ -14,6 +14,7 @@ FINDUTILS_CONF_ENV = \
 	gl_cv_func_stdin=yes \
 	ac_cv_func_working_mktime=yes \
 	gl_cv_func_wcwidth_works=yes
+FINDUTILS_CONF_OPTS = --disable-year2038
 
 ifeq ($(BR2_PACKAGE_LIBSELINUX),y)
 FINDUTILS_DEPENDENCIES += libselinux
