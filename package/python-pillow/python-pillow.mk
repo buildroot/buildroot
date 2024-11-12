buildroot/buildroot

commit baa3db0c34e826cc1c6469fd8ecbfbc5c2ef2e2c

package/python-pillow: bump to version 11.0.0
Migrate build opts to new format.

Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/python-pillow/python-pillow.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PYTHON_PILLOW_VERSION = 10.4.0
-PYTHON_PILLOW_SITE = https://files.pythonhosted.org/packages/cd/74/ad3d526f3bf7b6d3f408b73fde271ec69dfac8b81341a318ce825f2b3812
+PYTHON_PILLOW_VERSION = 11.0.0
+PYTHON_PILLOW_SITE = https://files.pythonhosted.org/packages/a5/26/0d95c04c868f6bdb0c447e3ee2de5564411845e36a858cfd63766bc7b563
 PYTHON_PILLOW_SOURCE = pillow-$(PYTHON_PILLOW_VERSION).tar.gz
 PYTHON_PILLOW_LICENSE = HPND
 PYTHON_PILLOW_LICENSE_FILES = LICENSE
@@ -14,60 +14,60 @@ PYTHON_PILLOW_CPE_ID_PRODUCT = pillow
 PYTHON_PILLOW_SETUP_TYPE = setuptools
 
 PYTHON_PILLOW_DEPENDENCIES = host-pkgconf
-PYTHON_PILLOW_BUILD_OPTS = -C--build-option=build_ext -C--build-option=--disable-platform-guessing
+PYTHON_PILLOW_BUILD_OPTS = -Cplatform-guessing=disable
 
 ifeq ($(BR2_PACKAGE_FREETYPE),y)
 PYTHON_PILLOW_DEPENDENCIES += freetype
-PYTHON_PILLOW_BUILD_OPTS += -C--build-option=--enable-freetype
+PYTHON_PILLOW_BUILD_OPTS += -Cfreetype=enable
 else
-PYTHON_PILLOW_BUILD_OPTS += -C--build-option=--disable-freetype
+PYTHON_PILLOW_BUILD_OPTS += -Cfreetype=disable
 endif
 
 ifeq ($(BR2_PACKAGE_JPEG),y)
 PYTHON_PILLOW_DEPENDENCIES += jpeg
-PYTHON_PILLOW_BUILD_OPTS += -C--build-option=--enable-jpeg
+PYTHON_PILLOW_BUILD_OPTS += -Cjpeg=enable
 else
-PYTHON_PILLOW_BUILD_OPTS += -C--build-option=--disable-jpeg
+PYTHON_PILLOW_BUILD_OPTS += -Cjpeg=disable
 endif
 
 ifeq ($(BR2_PACKAGE_LCMS2),y)
 PYTHON_PILLOW_DEPENDENCIES += lcms2
-PYTHON_PILLOW_BUILD_OPTS += -C--build-option=--enable-lcms
+PYTHON_PILLOW_BUILD_OPTS += -Clcms=enable
 else
-PYTHON_PILLOW_BUILD_OPTS += -C--build-option=--disable-lcms
+PYTHON_PILLOW_BUILD_OPTS += -Clcms=disable
 endif
 
 ifeq ($(BR2_PACKAGE_LIBXCB),y)
 PYTHON_PILLOW_DEPENDENCIES += libxcb
-PYTHON_PILLOW_BUILD_OPTS += -C--build-option=--enable-xcb
+PYTHON_PILLOW_BUILD_OPTS += -Cxcb=enable
 else
-PYTHON_PILLOW_BUILD_OPTS += -C--build-option=--disable-xcb
+PYTHON_PILLOW_BUILD_OPTS += -Cxcb=disable
 endif
 
 ifeq ($(BR2_PACKAGE_OPENJPEG),y)
 PYTHON_PILLOW_DEPENDENCIES += openjpeg
-PYTHON_PILLOW_BUILD_OPTS += -C--build-option=--enable-jpeg2000
+PYTHON_PILLOW_BUILD_OPTS += -Cjpeg2000=enable
 else
-PYTHON_PILLOW_BUILD_OPTS += -C--build-option=--disable-jpeg2000
+PYTHON_PILLOW_BUILD_OPTS += -Cjpeg2000=disable
 endif
 
 ifeq ($(BR2_PACKAGE_TIFF),y)
 PYTHON_PILLOW_DEPENDENCIES += tiff
-PYTHON_PILLOW_BUILD_OPTS += -C--build-option=--enable-tiff
+PYTHON_PILLOW_BUILD_OPTS += -Ctiff=enable
 else
-PYTHON_PILLOW_BUILD_OPTS += -C--build-option=--disable-tiff
+PYTHON_PILLOW_BUILD_OPTS += -Ctiff=disable
 endif
 
 ifeq ($(BR2_PACKAGE_WEBP),y)
 PYTHON_PILLOW_DEPENDENCIES += webp
-PYTHON_PILLOW_BUILD_OPTS += -C--build-option=--enable-webp
+PYTHON_PILLOW_BUILD_OPTS += -Cwebp=enable
 ifeq ($(BR2_PACKAGE_WEBP_DEMUX)$(BR2_PACKAGE_WEBP_MUX),yy)
-PYTHON_PILLOW_BUILD_OPTS += -C--build-option=--enable-webpmux
+PYTHON_PILLOW_BUILD_OPTS += -Cwebpmux=enable
 else
-PYTHON_PILLOW_BUILD_OPTS += -C--build-option=--disable-webpmux
+PYTHON_PILLOW_BUILD_OPTS += -Cwebpmux=disable
 endif
 else
-PYTHON_PILLOW_BUILD_OPTS += -C--build-option=--disable-webp -C--build-option=--disable-webpmux
+PYTHON_PILLOW_BUILD_OPTS += -Cwebp=disable -Cwebpmux=disable
 endif
 
 $(eval $(python-package))

package/python-pillow/python-pillow.hash
@@ -1,5 +1,5 @@
 # md5, sha256 from https://pypi.org/pypi/pillow/json
-md5  869758b3c5f5d8c15ffda4c8011b78d6  pillow-10.4.0.tar.gz
-sha256  166c1cd4d24309b30d61f79f4a9114b7b2313d7450912277855ff5dfd7cd4a06  pillow-10.4.0.tar.gz
+md5  22912b3db645a51ab4b4ff036b559ca8  pillow-11.0.0.tar.gz
+sha256  72bacbaf24ac003fea9bff9837d1eedb6088758d41e100c1552930151f677739  pillow-11.0.0.tar.gz
 # Locally computed sha256 checksums
 sha256  cf234c27a3f275f5a050f4df3946f8855704226fe4e7ca8b33928cbcecbced37  LICENSE

commit a97a592a7308991f660b0004fa9757390888e23f

package/python-pillow: fix webp DependencyException
As of baa3db0c34e826cc1c6469fd8ecbfbc5c2ef2e2c webp mux/demux are no
longer optional for webp support in python-pillow. The
-Cwebpmux=enable/disable option no longer exists.

Fixes:
DependencyException:
The headers or library files could not be found for webp,
which was requested by the option flag --enable-webp

Fixes: baa3db0c34e826cc1c6469fd8ecbfbc5c2ef2e2c

Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

package/python-pillow/python-pillow.mk
@@ -58,16 +58,11 @@ else
 PYTHON_PILLOW_BUILD_OPTS += -Ctiff=disable
 endif
 
-ifeq ($(BR2_PACKAGE_WEBP),y)
+ifeq ($(BR2_PACKAGE_WEBP)$(BR2_PACKAGE_WEBP_DEMUX)$(BR2_PACKAGE_WEBP_MUX),yyy)
 PYTHON_PILLOW_DEPENDENCIES += webp
 PYTHON_PILLOW_BUILD_OPTS += -Cwebp=enable
-ifeq ($(BR2_PACKAGE_WEBP_DEMUX)$(BR2_PACKAGE_WEBP_MUX),yy)
-PYTHON_PILLOW_BUILD_OPTS += -Cwebpmux=enable
 else
-PYTHON_PILLOW_BUILD_OPTS += -Cwebpmux=disable
-endif
-else
-PYTHON_PILLOW_BUILD_OPTS += -Cwebp=disable -Cwebpmux=disable
+PYTHON_PILLOW_BUILD_OPTS += -Cwebp=disable
 endif
 
 $(eval $(python-package))