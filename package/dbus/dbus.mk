f777c7991275c62f2881d1dae0c2aa357a7f1a21
Nikita Kiryushin <kiryushin@ancud.ru>
2024-11-21 17:01:34 +0300

    package/dbus: set session-socket-dir to /tmp
    dbus has a session socket directory configuration setting,
    that, if not set, will be autodeducted based on env vars
    like TMPDIR during configuration time.
    
    Becuse of that, the builder's environment variables will
    lead to an image with a broken session bus while
    leaking builder's details to the image.
    
    Add an explicit setting of session-socket-dir to /tmp dir.
    
    Fixes: https://gitlab.com/buildroot.org/buildroot/-/issues/67
    
    Signed-off-by: Nikita Kiryushin <kiryushin@ancud.ru>
    Reviewed-by: Fiona Klute <fiona.klute@gmx.de>
    Signed-off-by: Arnout Vandecappelle <arnout@rnout.be>

--- package/dbus/dbus.mk
+++ package/dbus/dbus.mk
@@ -34,6 +34,7 @@ DBUS_CONF_OPTS = \
 	--disable-doxygen-docs \
 	--with-system-socket=/run/dbus/system_bus_socket \
 	--with-system-pid-file=/run/messagebus.pid \
+	--with-session-socket-dir=/tmp \
 	--runstatedir=/run
 
 ifeq ($(BR2_STATIC_LIBS),y)
