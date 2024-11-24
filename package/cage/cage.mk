2f47195d540f63543728fd4982f2e6ae8aac2b2b
Raphael Pavlidis <raphael.pavlidis@gmail.com>
2024-11-24 11:49:06 +0000

    package/cage: bump to version 0.2.0
    Changelog: [1].
    
    Also, upstream commit [2], included in v0.2, removed the "xwayland"
    build time option. This commit removes the corresponding
    CAGE_CONF_OPTS.
    
    The cage github project was moved from [3] to [4]. See also [5].
    This commit also updates the _SITE url accordingly.
    
    [1] https://github.com/cage-kiosk/cage/releases/tag/v0.2.0
    [2] https://github.com/cage-kiosk/cage/commit/d3fb99d6654325ec46277cfdb589f89316bed701
    [3] https://github.com/Hjdskes/cage
    [4] https://github.com/cage-kiosk/cage
    [5] https://github.com/cage-kiosk/cage/commit/9a4310f8b6d40be49ca3065742c7462278b1316e
    
    Signed-off-by: Raphael Pavlidis <raphael.pavlidis@gmail.com>
    [Julien:
      - add new line before S-o-b
      - add link justifying the removal of xwayland option
      - update _SITE
    ]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/cage/cage.mk
+++ package/cage/cage.mk
@@ -4,17 +4,11 @@
 #
 ################################################################################
 
-CAGE_VERSION = 0.1.5
-CAGE_SITE = https://github.com/Hjdskes/cage/releases/download/v$(CAGE_VERSION)
+CAGE_VERSION = 0.2.0
+CAGE_SITE = https://github.com/cage-kiosk/cage/releases/download/v$(CAGE_VERSION)
 CAGE_LICENSE = MIT
 CAGE_LICENSE_FILES = LICENSE
 CAGE_DEPENDENCIES = host-pkgconf wlroots
 CAGE_CONF_OPTS = -Dman-pages=disabled
-
-ifeq ($(BR2_PACKAGE_WLROOTS_X11),y)
-CAGE_CONF_OPTS += -Dxwayland=true
-else
-CAGE_CONF_OPTS += -Dxwayland=false
-endif
 
 $(eval $(meson-package))
