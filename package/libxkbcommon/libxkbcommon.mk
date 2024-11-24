ae0e6d14d547c0d71cf68c75c821f4b2c1d367dd
Raphael Pavlidis <raphael.pavlidis@gmail.com>
2024-11-24 11:49:04 +0000

    package/libxkbcommon: bump version to 1.7.0
    Changelog:
    https://lists.freedesktop.org/archives/wayland-devel/2024-March/043539.html
    
    Signed-off-by: Raphael Pavlidis <raphael.pavlidis@gmail.com>
    [Julien:
      - add new line before S-o-b
      - add missing space in hash file (fix "make check-package" error)
    ]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libxkbcommon/libxkbcommon.mk
+++ package/libxkbcommon/libxkbcommon.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBXKBCOMMON_VERSION = 1.4.0
+LIBXKBCOMMON_VERSION = 1.7.0
 LIBXKBCOMMON_SITE = https://xkbcommon.org/download
 LIBXKBCOMMON_SOURCE = libxkbcommon-$(LIBXKBCOMMON_VERSION).tar.xz
 LIBXKBCOMMON_LICENSE = MIT/X11
