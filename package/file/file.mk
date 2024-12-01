104449d0cb8db537fa7d3dc0502e4dd7af7500b3
Julien Olivain <ju.o@free.fr>
2024-12-01 16:07:13 +0100

    package/file: bump version to 5.46
    See release announce [1].
    
    The src/mygetopt.h file license was updated from BSD-4-Clause to
    BSD-2-Clause in upstream commit [2]. This change makes this file using
    the same license as most other files. So this commit removes the
    license entry for this file and update the _LICENSE accordingly.
    
    This commit also replaces the mention "one file" on BSD-3-Clause, to
    the actual file name using it, which is "vasprintf.c".
    
    This commit also updates the Config.in package homepage URL,
    to use https.
    
    [1] https://mailman.astron.com/pipermail/file/2024-November/001435.html
    [2] https://github.com/file/file/commit/d605bb40478c4ce94aef88316d3a928b3da746f5
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Reviewed-by: Vincent Jardin <vjardin@free.fr>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/file/file.mk
+++ package/file/file.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-FILE_VERSION = 5.45
+FILE_VERSION = 5.46
 FILE_SITE = https://astron.com/pub/file
-FILE_LICENSE = BSD-2-Clause, BSD-4-Clause (one file), BSD-3-Clause (one file)
-FILE_LICENSE_FILES = COPYING src/mygetopt.h src/vasprintf.c
+FILE_LICENSE = BSD-2-Clause, BSD-3-Clause (vasprintf.c)
+FILE_LICENSE_FILES = COPYING src/vasprintf.c
 FILE_CPE_ID_VALID = YES
 
 FILE_AUTORECONF = YES
