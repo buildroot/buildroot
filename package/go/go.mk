fbdd2acaec3303abad661583b511210d540cab7e
Christian Stewart <christian@aperture.us>
2024-12-08 08:42:30 -0800

    package/go: bump to version 1.23.4
    go1.23.4 (released 2024-12-03) includes fixes to the compiler, the runtime, the trace command, and the syscall package.
    
    https://go.dev/doc/devel/release#go1.23.4
    https://github.com/golang/go/issues?q=milestone%3AGo1.23.4+label%3ACherryPickApproved
    
    Signed-off-by: Christian Stewart <christian@aperture.us>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit ef273589633170018abe2bb20de75e11443b36a1)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/go/go.mk
+++ package/go/go.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GO_VERSION = 1.23.3
+GO_VERSION = 1.23.4
 
 HOST_GO_GOPATH = $(HOST_DIR)/share/go-path
 HOST_GO_HOST_CACHE = $(HOST_DIR)/share/host-go-cache
