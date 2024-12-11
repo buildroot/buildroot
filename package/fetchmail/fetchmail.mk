45f46779f18c3e6462a91a62ce1f43a21a1b6b03
Bernd Kuhls <bernd@kuhls.net>
2024-12-11 20:29:39 +0100

    package/fetchmail: bump version to 6.5.1
    Release notes:
    https://sourceforge.net/p/fetchmail/mailman/message/58835491/
    https://sourceforge.net/p/fetchmail/mailman/message/58840916/
    
    Updated license hash due to copyright year bump:
    https://sourceforge.net/p/fetchmail/git/ci/62a11ca4064e5b59e57ca7a8fce08c82af259731/
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/fetchmail/fetchmail.mk
+++ package/fetchmail/fetchmail.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-FETCHMAIL_VERSION_MAJOR = 6.4
-FETCHMAIL_VERSION = $(FETCHMAIL_VERSION_MAJOR).39
+FETCHMAIL_VERSION_MAJOR = 6.5
+FETCHMAIL_VERSION = $(FETCHMAIL_VERSION_MAJOR).1
 FETCHMAIL_SOURCE = fetchmail-$(FETCHMAIL_VERSION).tar.xz
 FETCHMAIL_SITE = https://downloads.sourceforge.net/project/fetchmail/branch_$(FETCHMAIL_VERSION_MAJOR)
 FETCHMAIL_LICENSE = GPL-2.0; some exceptions are mentioned in COPYING
