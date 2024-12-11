a649794913214c27960f704d62f2f3577fef7cb7
Francois Perrad <francois.perrad@gadz.org>
2024-12-11 10:46:37 +0100

    package/lua-iconv: bump to version 7.1.0
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/lua-iconv/lua-iconv.mk
+++ package/lua-iconv/lua-iconv.mk
@@ -4,7 +4,8 @@
 #
 ################################################################################
 
-LUA_ICONV_VERSION = 7-1
+LUA_ICONV_VERSION = 7.1.0-1
+LUA_ICONV_SUBDIR = lua-iconv
 LUA_ICONV_LICENSE = MIT
 LUA_ICONV_LICENSE_FILES = $(LUA_ICONV_SUBDIR)/COPYING
 LUA_ICONV_DEPENDENCIES = $(if $(BR2_PACKAGE_LIBICONV),libiconv)
