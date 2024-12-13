3a3f1f542c16f6c09445e353895052259f6c0ded
Yegor Yefremov <yegorslists@googlemail.com>
2024-12-13 08:40:31 +0100

    package/python-daemon: bump to version 3.1.2
    Change PYTHON_DAEMON_SOURCE as package's download name has changed
    from python-daemon to python_daemon.
    
    Fixes:
    https://autobuild.buildroot.org/results/be73c9a41b3ad3dea8a924ab98f6fe804e6808fa/
    https://autobuild.buildroot.org/results/4cb684c6e8758b728d54ea367d1f279b2065bd83/
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-daemon/python-daemon.mk
+++ package/python-daemon/python-daemon.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_DAEMON_VERSION = 3.0.1
-PYTHON_DAEMON_SITE = https://files.pythonhosted.org/packages/84/50/97b81327fccbb70eb99f3c95bd05a0c9d7f13fb3f4cfd975885110d1205a
+PYTHON_DAEMON_VERSION = 3.1.2
+PYTHON_DAEMON_SOURCE = python_daemon-$(PYTHON_DAEMON_VERSION).tar.gz
+PYTHON_DAEMON_SITE = https://files.pythonhosted.org/packages/3d/37/4f10e37bdabc058a32989da2daf29e57dc59dbc5395497f3d36d5f5e2694
 PYTHON_DAEMON_LICENSE = Apache-2.0 (library), GPL-3.0+ (test, build)
 PYTHON_DAEMON_LICENSE_FILES = LICENSE.ASF-2 LICENSE.GPL-3
 PYTHON_DAEMON_SETUP_TYPE = setuptools
-PYTHON_DAEMON_DEPENDENCIES = host-python-docutils
+PYTHON_DAEMON_DEPENDENCIES = host-python-changelog-chug host-python-docutils
 
 $(eval $(python-package))
