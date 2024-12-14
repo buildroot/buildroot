c2c1c27d1144713dc7e64e7b27bc39bfbad5b72b
Peter Korsgaard <peter@korsgaard.com>
2024-12-14 15:14:30 +0100

    package/python-django: security bump to version 5.1.4
    Fixes the following security issues:
    
    CVE-2024-53907: Potential denial-of-service in
    django.utils.html.strip_tags()
    
    The strip_tags() method and striptags template filter are subject to a
    potential denial-of-service attack via certain inputs containing large
    sequences of nested incomplete HTML entities.
    
    CVE-2024-53908: Potential SQL injection in HasKey(lhs, rhs) on Oracle
    
    Direct usage of the django.db.models.fields.json.HasKey lookup on Oracle is
    subject to SQL injection if untrusted data is used as a lhs value.
    Applications that use the jsonfield.has_key lookup through the __ syntax are
    unaffected.
    
    https://www.djangoproject.com/weblog/2024/dec/04/security-releases/
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 5.1.3
+PYTHON_DJANGO_VERSION = 5.1.4
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/c6/85/ba2c2b83ba8b95354f99ed8344405d9571109ce0175028876209d6b93fba
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/d3/e8/536555596dbb79f6e77418aeb40bdc1758c26725aba31919ba449e6d5e6a
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
 PYTHON_DJANGO_CPE_ID_VENDOR = djangoproject
