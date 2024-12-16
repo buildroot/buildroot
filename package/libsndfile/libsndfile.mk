0341de10c4171d1c3af3279388239397637f9610
Peter Korsgaard <peter@korsgaard.com>
2024-12-16 14:14:23 +0100

    package/libsndfile: add upstream post-1.2.2 security fixes
    Fixes the following security vulnerabilities:
    
    CVE-2022-33065: Multiple signed integers overflow in function au_read_header
    in src/au.c and in functions mat4_open and mat4_read_header in src/mat4.c in
    Libsndfile, allows an attacker to cause Denial of Service or other
    unspecified impacts.
    
    CVE-2024-50612: libsndfile through 1.2.2 has an ogg_vorbis.c
    vorbis_analysis_wrote out-of-bounds read.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit e675ffd964d65067bb115ee1e770ee44ba073958)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libsndfile/libsndfile.mk
+++ package/libsndfile/libsndfile.mk
@@ -13,6 +13,24 @@ LIBSNDFILE_LICENSE_FILES = COPYING
 LIBSNDFILE_CPE_ID_VALID = YES
 LIBSNDFILE_DEPENDENCIES = host-pkgconf
 
+# 0001-mat4-mat5-fix-int-overflow-in-dataend-calculation.patch
+# 0002-au-avoid-int-overflow-while-calculating-data_end.patch
+# 0003-avr-fix-int-overflow-in-avr_read_header.patch
+# 0004-sds-fix-int-overflow-warning-in-sample-calculations.patch
+# 0005-aiff-fix-int-overflow-when-counting-header-elements.patch
+# 0006-ircam-fix-int-overflow-in-ircam_read_header.patch
+# 0007-mat4-mat5-fix-int-overflow-when-calculating-blockwid.patch
+# 0008-common-fix-int-overflow-in-psf_binheader_readf.patch
+# 0009-nms_adpcm-fix-int-overflow-in-signal-estimate.patch
+# 0010-nms_adpcm-fix-int-overflow-in-sf.frames-calc.patch
+# 0011-pcm-fix-int-overflow-in-pcm_init.patch
+# 0012-rf64-fix-int-overflow-in-rf64_read_header.patch
+# 0013-ima_adpcm-fix-int-overflow-in-ima_reader_init.patch
+LIBSNDFILE_IGNORE_CVES += CVE-2022-33065
+
+# 0014-src-ogg-better-error-checking-for-vorbis.-Fixes-1035.patch
+LIBSNDFILE_IGNORE_CVES += CVE-2024-50612
+
 LIBSNDFILE_CONF_ENV = ac_cv_prog_cc_c99='-std=gnu99'
 LIBSNDFILE_CONF_OPTS = \
 	--disable-sqlite \
