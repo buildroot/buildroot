d21fdce7715878f2d1b2a44e450328f50c9862cb
Francois Perrad <francois.perrad@gadz.org>
2024-12-20 19:58:29 +0100

    package/libnpth: bump to version 1.8
    For the changelog, see:
    https://git.gnupg.org/cgi-bin/gitweb.cgi?p=npth.git;a=log;h=npth-1.8
    
    Remove merged patches.
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    [Julien: add link to change log]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libnpth/libnpth.mk
+++ package/libnpth/libnpth.mk
@@ -4,14 +4,12 @@
 #
 ################################################################################
 
-LIBNPTH_VERSION = 1.7
+LIBNPTH_VERSION = 1.8
 LIBNPTH_SOURCE = npth-$(LIBNPTH_VERSION).tar.bz2
 LIBNPTH_SITE = https://www.gnupg.org/ftp/gcrypt/npth
 LIBNPTH_LICENSE = LGPL-2.0+
 LIBNPTH_LICENSE_FILES = COPYING.LIB
 LIBNPTH_INSTALL_STAGING = YES
-# 0001-Fix-INSERT_EXPOSE_RWLOCK_API-for-musl-C-library.patch
-LIBNPTH_AUTORECONF = YES
 LIBNPTH_CONF_OPTS = --disable-tests
 HOST_LIBNPTH_CONF_OPTS = --disable-tests
 
