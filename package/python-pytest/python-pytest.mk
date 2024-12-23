457283d14d9a37c8581cf4118bc57cb5fb583ff9
Marcin Niestroj <m.niestroj@grinn-global.com>
2024-12-23 14:59:06 +0100

    package/python-pytest: bump to 8.3.4
    For change log, see:
    https://docs.pytest.org/en/stable/changelog.html#pytest-8-3-4-2024-12-01
    
    Signed-off-by: Marcin Niestroj <m.niestroj@grinn-global.com>
    [Julien: add link to change log]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-pytest/python-pytest.mk
+++ package/python-pytest/python-pytest.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYTEST_VERSION = 8.3.3
+PYTHON_PYTEST_VERSION = 8.3.4
 PYTHON_PYTEST_SOURCE = pytest-$(PYTHON_PYTEST_VERSION).tar.gz
-PYTHON_PYTEST_SITE = https://files.pythonhosted.org/packages/8b/6c/62bbd536103af674e227c41a8f3dcd022d591f6eed5facb5a0f31ee33bbc
+PYTHON_PYTEST_SITE = https://files.pythonhosted.org/packages/05/35/30e0d83068951d90a01852cb1cef56e5d8a09d20c7f511634cc2f7e0372a
 PYTHON_PYTEST_SETUP_TYPE = setuptools
 PYTHON_PYTEST_LICENSE = MIT
 PYTHON_PYTEST_LICENSE_FILES = LICENSE
