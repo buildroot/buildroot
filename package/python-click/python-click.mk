b780b066a1c087d20707d79bd17ec4d803a10bf1
James Hilliard <james.hilliard1@gmail.com>
2024-12-23 09:20:13 -0700

    package/python-click: bump to version 8.1.8
    Migrate from setuptools build backend to flit.
    
    License file renamed without content changes.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-click/python-click.mk
+++ package/python-click/python-click.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_CLICK_VERSION = 8.1.7
+PYTHON_CLICK_VERSION = 8.1.8
 PYTHON_CLICK_SOURCE = click-$(PYTHON_CLICK_VERSION).tar.gz
-PYTHON_CLICK_SITE = https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5
+PYTHON_CLICK_SITE = https://files.pythonhosted.org/packages/b9/2e/0090cbf739cee7d23781ad4b89a9894a41538e4fcf4c31dcdd705b78eb8b
+PYTHON_CLICK_SETUP_TYPE = flit
 PYTHON_CLICK_LICENSE = BSD-3-Clause
-PYTHON_CLICK_LICENSE_FILES = LICENSE.rst
-PYTHON_CLICK_SETUP_TYPE = setuptools
+PYTHON_CLICK_LICENSE_FILES = LICENSE.txt
 
 $(eval $(python-package))
