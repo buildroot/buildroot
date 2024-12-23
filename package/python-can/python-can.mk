2880032dce069210b9ab3008f3063efac7a597c4
James Hilliard <james.hilliard1@gmail.com>
2024-12-23 09:03:31 -0700

    package/python-can: bump to version 4.5.0
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-can/python-can.mk
+++ package/python-can/python-can.mk
@@ -4,12 +4,13 @@
 #
 ################################################################################
 
-PYTHON_CAN_VERSION = 4.4.2
+PYTHON_CAN_VERSION = 4.5.0
 PYTHON_CAN_SOURCE = python_can-$(PYTHON_CAN_VERSION).tar.gz
-PYTHON_CAN_SITE = https://files.pythonhosted.org/packages/a3/17/57c38abbf00993ac5ec306de0c00271685d1372ef3d907b107eb63ab13a8
+PYTHON_CAN_SITE = https://files.pythonhosted.org/packages/6a/4b/b6fd103c3f2eb0ae942e0704642d396ebbaf87f4d82d0102560cc738fdf1
 PYTHON_CAN_SETUP_TYPE = setuptools
 PYTHON_CAN_LICENSE = LGPL-3.0
 PYTHON_CAN_LICENSE_FILES = LICENSE.txt
+PYTHON_CAN_DEPENDENCIES = host-python-setuptools-scm
 
 ifneq ($(BR2_PACKAGE_PYTHON_CAN_VIEWER),y)
 define PYTHON_CAN_REMOVE_VIEWER
