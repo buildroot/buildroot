a44671c2cfed2e5087223d0b0634427f67c2f830
James Hilliard <james.hilliard1@gmail.com>
2024-12-23 07:53:38 -0700

    package/python-aiorwlock: bump to version 1.5.0
    Migrate from setuptools to poetry build backend.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-aiorwlock/python-aiorwlock.mk
+++ package/python-aiorwlock/python-aiorwlock.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_AIORWLOCK_VERSION = 1.4.0
+PYTHON_AIORWLOCK_VERSION = 1.5.0
 PYTHON_AIORWLOCK_SOURCE = aiorwlock-$(PYTHON_AIORWLOCK_VERSION).tar.gz
-PYTHON_AIORWLOCK_SITE = https://files.pythonhosted.org/packages/ba/c5/882b4c89d71d6f9c7d0d8dee18d267025e71d4c3241eb3b16ab39105a0d1
-PYTHON_AIORWLOCK_SETUP_TYPE = setuptools
+PYTHON_AIORWLOCK_SITE = https://files.pythonhosted.org/packages/c5/bf/d1ddcd676be027a963b3b01fdf9915daf4590b4dfd03bf1c8c2858aac7e3
+PYTHON_AIORWLOCK_SETUP_TYPE = poetry
 PYTHON_AIORWLOCK_LICENSE = Apache-2.0
 PYTHON_AIORWLOCK_LICENSE_FILES = LICENSE
 
