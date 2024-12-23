a64eb43cc5dec2bf4a383d371ff1b251f48992ad
Marcin Niestroj <m.niestroj@grinn-global.com>
2024-12-23 14:59:26 +0100

    package/dt-utils: bump to 2023.11.0
    For change log, see:
    https://git.pengutronix.de/cgit/tools/dt-utils/tree/NEWS?h=v2023.11.0#n4
    
    Signed-off-by: Marcin Niestroj <m.niestroj@grinn-global.com>
    [Julien: add link to change log]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/dt-utils/dt-utils.mk
+++ package/dt-utils/dt-utils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DT_UTILS_VERSION = 2023.08.0
+DT_UTILS_VERSION = 2023.11.0
 DT_UTILS_SOURCE = dt-utils-$(DT_UTILS_VERSION).tar.xz
 DT_UTILS_SITE = https://git.pengutronix.de/cgit/tools/dt-utils/snapshot
 DT_UTILS_LICENSE = CC0-1.0, GPL-2.0, GPL-2.0+, Zlib
