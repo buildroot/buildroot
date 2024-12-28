5027bcf2c5c6f152bd837b5f961a4eea8e92ba5a
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2024-12-28 17:23:14 +0100

    package/tinifier: bump version to 4.1.0
    3.4.0 -> 3.5.0:
    
    Changed
    
        Go version updated from 1.17.3 up to 1.18.0
    
    3.5.0 -> 4.0.0:
    
    Changed
    
        Go version updated from 1.18 up to 1.19
        Sub-command quota now supports multiple keys at once
        Default threads count for compress sub-command is now NumCPU * 6
        Go package (module) name changed to github.com/tarampampam/tinifier/v4
        New terminal UI
    
    Added
    
        .env file support
        Global --version flag
        --update-mod-date flag for compress sub-command (since this release, the original file modification date will be preserved by default)
        --keep-original-file flag for compress sub-command #103
    
    Removed
    
        Sub-command version (you can use --version flag instead)
        Global --debug flag
        Global --verbose flag
    
    4.0.0 -> 4.1.0:
    
    Changed
    
        Go dependencies updated
        Go version updated from 1.19 up to 1.20
        Module name changed from github.com/tarampampam/tinifier/v4 to gh.tarampamp.am/tinifier/v4
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/tinifier/tinifier.mk
+++ package/tinifier/tinifier.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TINIFIER_VERSION = 3.4.0
+TINIFIER_VERSION = 4.1.0
 TINIFIER_SITE = $(call github,tarampampam,tinifier,v$(TINIFIER_VERSION))
 TINIFIER_LICENSE = MIT
 TINIFIER_LICENSE_FILES = LICENSE
