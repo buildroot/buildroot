ba2fb599cd002b57fa70e978d91884c98d8aad05
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2025-01-03 21:17:44 +0100

    package/pixman: bump to version 0.44.2
    For release announces since 0.42.2, see:
    0.43.0: [1], 0.43.2: [2], 0.43.4: [3], 0.44.0: [4], 0.44.2: [5].
    
    In version 0.43.0, pixman dropped the autotools build system, and
    moved to meson, so this package update follows along. Some notes:
    
    - The arm-iwmmxt has no equivalent (in 0.44.2, there's a patch in
      master re-adding such an option)
    
    - The new mmx, sse2, ssse3 and a64-neon options are handled.
    
    - Patch 0001 is no longer needed as the new build system as a 'tests'
      option that allows to disable test.
    
    - Patches 0002 and 0003 are dropped as they are all upstream.
    
    This bump was tested with a significant subset of the pixman reverse
    dependencies:
    
    BR2_ROOTFS_DEVICE_CREATION_DYNAMIC_EUDEV=y
    BR2_PACKAGE_FOOT=y
    BR2_PACKAGE_IGT_GPU_TOOLS=y
    BR2_PACKAGE_MESA3D=y
    BR2_PACKAGE_MESA3D_GALLIUM_DRIVER_SWRAST=y
    BR2_PACKAGE_MESA3D_OPENGL_EGL=y
    BR2_PACKAGE_MESA3D_OPENGL_ES=y
    BR2_PACKAGE_XORG7=y
    BR2_PACKAGE_XSERVER_XORG_SERVER=y
    BR2_PACKAGE_XDRIVER_XF86_VIDEO_FBTURBO=y
    BR2_PACKAGE_WLROOTS=y
    BR2_PACKAGE_WLROOTS_X11=y
    BR2_PACKAGE_WLROOTS_XWAYLAND=y
    BR2_PACKAGE_QEMU=y
    BR2_PACKAGE_QEMU_SYSTEM=y
    
    See test result at:
    https://gitlab.com/tpetazzoni/buildroot/-/pipelines/1609533944. Note
    that the overall pipeline failed because the build of Qemu fails on
    RISC-V for completely unrelated reasons.
    
    [1] https://lists.cairographics.org/archives/cairo-announce/2024-January/000230.html
    [2] https://lists.cairographics.org/archives/cairo-announce/2024-January/000233.html
    [3] https://lists.cairographics.org/archives/cairo-announce/2024-February/000234.html
    [4] https://lists.cairographics.org/archives/cairo-announce/2024-November/000235.html
    [5] https://lists.cairographics.org/archives/cairo-announce/2024-December/000236.html
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    [Julien: add links to release announces]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/pixman/pixman.mk
+++ package/pixman/pixman.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PIXMAN_VERSION = 0.42.2
+PIXMAN_VERSION = 0.44.2
 PIXMAN_SOURCE = pixman-$(PIXMAN_VERSION).tar.xz
 PIXMAN_SITE = https://xorg.freedesktop.org/releases/individual/lib
 PIXMAN_LICENSE = MIT
@@ -15,14 +15,16 @@ PIXMAN_INSTALL_STAGING = YES
 PIXMAN_DEPENDENCIES = host-pkgconf
 HOST_PIXMAN_DEPENDENCIES = host-pkgconf
 
-# For 0001-Disable-tests.patch
-PIXMAN_AUTORECONF = YES
-
 # don't build gtk based demos
 PIXMAN_CONF_OPTS = \
-	--disable-gtk \
-	--disable-loongson-mmi \
-	--disable-arm-iwmmxt
+	-Dloongson-mmi=disabled \
+	-Dvmx=disabled \
+	-Dmips-dspr2=disabled \
+	-Dopenmp=disabled \
+	-Dgnuplot=false \
+	-Dgtk=disabled \
+	-Dlibpng=disabled \
+	-Dtests=disabled
 
 # Affects only tests, and we don't build tests (see
 # 0001-Disable-tests.patch). See
@@ -31,21 +33,45 @@ PIXMAN_CONF_OPTS = \
 # test executable".
 PIXMAN_IGNORE_CVES += CVE-2023-37769
 
+ifeq ($(BR2_X86_CPU_HAS_MMX),y)
+PIXMAN_CONF_OPTS += -Dmmx=enabled
+else
+PIXMAN_CONF_OPTS += -Dmmx=disabled
+endif
+
+ifeq ($(BR2_X86_CPU_HAS_SSE2),y)
+PIXMAN_CONF_OPTS += -Dsse2=enabled
+else
+PIXMAN_CONF_OPTS += -Dsse2=disabled
+endif
+
+ifeq ($(BR2_X86_CPU_HAS_SSSE3),y)
+PIXMAN_CONF_OPTS += -Dssse3=enabled
+else
+PIXMAN_CONF_OPTS += -Dssse3=disabled
+endif
+
 # The ARM SIMD code from pixman requires a recent enough ARM core, but
 # there is a runtime CPU check that makes sure it doesn't get used if
 # the HW doesn't support it. The only case where the ARM SIMD code
 # cannot be *built* at all is when the platform doesn't support ARM
 # instructions at all, so we have to disable that explicitly.
 ifeq ($(BR2_ARM_CPU_HAS_ARM),y)
-PIXMAN_CONF_OPTS += --enable-arm-simd
+PIXMAN_CONF_OPTS += -Darm-simd=enabled
 else
-PIXMAN_CONF_OPTS += --disable-arm-simd
+PIXMAN_CONF_OPTS += -Darm-simd=disabled
 endif
 
 ifeq ($(BR2_ARM_CPU_HAS_ARM)$(BR2_ARM_CPU_HAS_NEON),yy)
-PIXMAN_CONF_OPTS += --enable-arm-neon
+PIXMAN_CONF_OPTS += -Dneon=enabled
 else
-PIXMAN_CONF_OPTS += --disable-arm-neon
+PIXMAN_CONF_OPTS += -Dneon=disabled
+endif
+
+ifeq ($(BR2_aarch64)$(BR2_ARM_CPU_HAS_NEON),yy)
+PIXMAN_CONF_OPTS += -Da64-neon=enabled
+else
+PIXMAN_CONF_OPTS += -Da64-neon=disabled
 endif
 
 PIXMAN_CFLAGS = $(TARGET_CFLAGS)
@@ -54,7 +80,5 @@ ifeq ($(BR2_TOOLCHAIN_HAS_GCC_BUG_101737),y)
 PIXMAN_CFLAGS += -O0
 endif
 
-PIXMAN_CONF_OPTS += CFLAGS="$(PIXMAN_CFLAGS)"
-
-$(eval $(autotools-package))
-$(eval $(host-autotools-package))
+$(eval $(meson-package))
+$(eval $(host-meson-package))
