80f884e84375cac9f7e410e1d8d25ead3bef76bd
Akhilesh Nema <nemaakhilesh@gmail.com>
2025-01-06 09:58:32 -0800

    package/logrotate: bump version to 3.22.0
    Changelog - https://github.com/logrotate/logrotate/releases/tag/3.22.0
    
    Signed-off-by: Akhilesh Nema <nemaakhilesh@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/logrotate/logrotate.mk
+++ package/logrotate/logrotate.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LOGROTATE_VERSION = 3.21.0
+LOGROTATE_VERSION = 3.22.0
 LOGROTATE_SOURCE = logrotate-$(LOGROTATE_VERSION).tar.xz
 LOGROTATE_SITE = https://github.com/logrotate/logrotate/releases/download/$(LOGROTATE_VERSION)
 LOGROTATE_LICENSE = GPL-2.0+
