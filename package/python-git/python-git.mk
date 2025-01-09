09649af4bd539b3fb2a52b07c1f58f6f4caffaa6
James Hilliard <james.hilliard1@gmail.com>
2025-01-09 13:59:00 -0700

    package/python-git: bump to version 3.1.44
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-git/python-git.mk
+++ package/python-git/python-git.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_GIT_VERSION = 3.1.43
-PYTHON_GIT_SOURCE = GitPython-$(PYTHON_GIT_VERSION).tar.gz
-PYTHON_GIT_SITE = https://files.pythonhosted.org/packages/b6/a1/106fd9fa2dd989b6fb36e5893961f82992cf676381707253e0bf93eb1662
+PYTHON_GIT_VERSION = 3.1.44
+PYTHON_GIT_SOURCE = gitpython-$(PYTHON_GIT_VERSION).tar.gz
+PYTHON_GIT_SITE = https://files.pythonhosted.org/packages/c0/89/37df0b71473153574a5cdef8f242de422a0f5d26d7a9e231e6f169b4ad14
 PYTHON_GIT_LICENSE = BSD-3-Clause
 PYTHON_GIT_LICENSE_FILES = LICENSE
 PYTHON_GIT_SETUP_TYPE = setuptools
