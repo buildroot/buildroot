878dc204f9ff659e7fb3ba441ea84f60d020b347
James Hilliard <james.hilliard1@gmail.com>
2025-01-09 12:23:54 -0700

    package/python-asyncclick: bump to version 8.1.8
    Migrate from setuptools to flit build backend.
    
    License file renamed without content changes.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-asyncclick/python-asyncclick.mk
+++ package/python-asyncclick/python-asyncclick.mk
@@ -4,12 +4,11 @@
 #
 ################################################################################
 
-PYTHON_ASYNCCLICK_VERSION = 8.1.7.2
+PYTHON_ASYNCCLICK_VERSION = 8.1.8
 PYTHON_ASYNCCLICK_SOURCE = asyncclick-$(PYTHON_ASYNCCLICK_VERSION).tar.gz
-PYTHON_ASYNCCLICK_SITE = https://files.pythonhosted.org/packages/5e/bf/59d836c3433d7aa07f76c2b95c4eb763195ea8a5d7f9ad3311ed30c2af61
-PYTHON_ASYNCCLICK_SETUP_TYPE = setuptools
+PYTHON_ASYNCCLICK_SITE = https://files.pythonhosted.org/packages/cb/b5/e1e5fdf1c1bb7e6e614987c120a98d9324bf8edfaa5f5cd16a6235c9d91b
+PYTHON_ASYNCCLICK_SETUP_TYPE = flit
 PYTHON_ASYNCCLICK_LICENSE = BSD-3-Clause
-PYTHON_ASYNCCLICK_LICENSE_FILES = LICENSE.rst
-PYTHON_ASYNCCLICK_DEPENDENCIES = host-python-setuptools-scm
+PYTHON_ASYNCCLICK_LICENSE_FILES = LICENSE.txt
 
 $(eval $(python-package))
