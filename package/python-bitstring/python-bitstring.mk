d757916f0d0b672bf8d1976ba52fb60d30cbb070
James Hilliard <james.hilliard1@gmail.com>
2025-01-09 12:26:38 -0700

    package/python-bitstring: bump to version 4.3.0
    License hash changed due to year update:
    https://github.com/scott-griffiths/bitstring/commit/fdc3fbe76862e581997403b9f59c1a2d2ba5d85b
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-bitstring/python-bitstring.mk
+++ package/python-bitstring/python-bitstring.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_BITSTRING_VERSION = 4.2.3
+PYTHON_BITSTRING_VERSION = 4.3.0
 PYTHON_BITSTRING_SOURCE = bitstring-$(PYTHON_BITSTRING_VERSION).tar.gz
-PYTHON_BITSTRING_SITE = https://files.pythonhosted.org/packages/d8/d0/d6f57409bb50f54fe2894ec5a50b5c04cb41aa814c3bdb8a7eeb4a0f7697
+PYTHON_BITSTRING_SITE = https://files.pythonhosted.org/packages/0d/43/52859178f337661e2d496262f9061799d99644012b7b02ab5d7c491c21fd
 PYTHON_BITSTRING_SETUP_TYPE = setuptools
 PYTHON_BITSTRING_LICENSE = MIT
 PYTHON_BITSTRING_LICENSE_FILES = LICENSE
