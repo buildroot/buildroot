f5a6704308fa83063792e05654cdda75b2bd321e
James Hilliard <james.hilliard1@gmail.com>
2025-01-10 07:17:17 -0700

    package/python-jc: bump to version 1.25.4
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-jc/python-jc.mk
+++ package/python-jc/python-jc.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_JC_VERSION = 1.25.3
+PYTHON_JC_VERSION = 1.25.4
 PYTHON_JC_SOURCE = jc-$(PYTHON_JC_VERSION).tar.gz
-PYTHON_JC_SITE = https://files.pythonhosted.org/packages/a5/82/bfb1ec7d9667bc2f922254bc62e12fd460a5de3b711518f5089df0a17180
+PYTHON_JC_SITE = https://files.pythonhosted.org/packages/5b/82/146ef4c94297ef87c6a732c92dd57761bea2c2179e1b8ca8a6b9b8dd6ff7
 PYTHON_JC_SETUP_TYPE = setuptools
 PYTHON_JC_LICENSE = MIT, BSD-3-Clause (bundled pbPlist)
 PYTHON_JC_LICENSE_FILES = LICENSE.md
