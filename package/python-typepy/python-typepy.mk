e0e55920eda4c5830aa3bd5e90a2936b36745192
James Hilliard <james.hilliard1@gmail.com>
2025-01-11 11:46:13 -0700

    package/python-typepy: bump to version 1.3.4
    Add new host-python-setuptools-scm build dependency.
    
    License hash changed due to year update:
    https://github.com/thombashi/typepy/commit/974f6c5665c4ff0ee9f030e5d2e89b07bcea2073
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-typepy/python-typepy.mk
+++ package/python-typepy/python-typepy.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_TYPEPY_VERSION = 1.3.2
+PYTHON_TYPEPY_VERSION = 1.3.4
 PYTHON_TYPEPY_SOURCE = typepy-$(PYTHON_TYPEPY_VERSION).tar.gz
-PYTHON_TYPEPY_SITE = https://files.pythonhosted.org/packages/cc/86/9672794fb1c87a17b839666976ed4c8cb779ce05d471bed3166a39a53c4d
+PYTHON_TYPEPY_SITE = https://files.pythonhosted.org/packages/79/59/4c39942077d7de285f762a91024dbda731be693591732977358f77d120fb
 PYTHON_TYPEPY_SETUP_TYPE = setuptools
 PYTHON_TYPEPY_LICENSE = MIT
 PYTHON_TYPEPY_LICENSE_FILES = LICENSE
+PYTHON_TYPEPY_DEPENDENCIES = host-python-setuptools-scm
 
 $(eval $(python-package))
