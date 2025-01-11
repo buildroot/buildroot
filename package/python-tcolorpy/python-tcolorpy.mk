b74a5a1e929dd15e01108d94db51a3a1ea74d3f6
James Hilliard <james.hilliard1@gmail.com>
2025-01-11 11:05:24 -0700

    package/python-tcolorpy: bump to version 0.1.7
    License hash changed due to year update:
    https://github.com/thombashi/tcolorpy/commit/8f71e7f6672fd77170b461e0dafcce7328cc3533
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-tcolorpy/python-tcolorpy.mk
+++ package/python-tcolorpy/python-tcolorpy.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_TCOLORPY_VERSION = 0.1.6
+PYTHON_TCOLORPY_VERSION = 0.1.7
 PYTHON_TCOLORPY_SOURCE = tcolorpy-$(PYTHON_TCOLORPY_VERSION).tar.gz
-PYTHON_TCOLORPY_SITE = https://files.pythonhosted.org/packages/af/f6/b17885354c5addfb93c14f3ed7172c33494f989dd5f83c0340d2c8fa3b69
+PYTHON_TCOLORPY_SITE = https://files.pythonhosted.org/packages/80/cc/44f2d81d8f9093aad81c3467a5bf5718d2b5f786e887b6e4adcfc17ec6b9
 PYTHON_TCOLORPY_SETUP_TYPE = setuptools
 PYTHON_TCOLORPY_LICENSE = MIT
 PYTHON_TCOLORPY_LICENSE_FILES = LICENSE
