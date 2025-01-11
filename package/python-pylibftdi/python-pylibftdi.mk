9923df7b192af3a8a3cec3c5854eea79ee325c0c
James Hilliard <james.hilliard1@gmail.com>
2025-01-11 08:53:08 -0700

    package/python-pylibftdi: bump to version 0.23.0
    License hash changed due to year update:
    https://github.com/codedstructure/pylibftdi/commit/e604c863134200b370a963f6822eebf2b4315992
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-pylibftdi/python-pylibftdi.mk
+++ package/python-pylibftdi/python-pylibftdi.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYLIBFTDI_VERSION = 0.22.0
+PYTHON_PYLIBFTDI_VERSION = 0.23.0
 PYTHON_PYLIBFTDI_SOURCE = pylibftdi-$(PYTHON_PYLIBFTDI_VERSION).tar.gz
-PYTHON_PYLIBFTDI_SITE = https://files.pythonhosted.org/packages/74/b3/a3c333a250143d204a0bb60de7c1e5f841543231f04c71ff4fa65e5b90c2
+PYTHON_PYLIBFTDI_SITE = https://files.pythonhosted.org/packages/9d/f0/551ccbd8e989e898707e0e0859344fcf538c2310521847a1035ad3e9b164
 PYTHON_PYLIBFTDI_LICENSE = MIT
 PYTHON_PYLIBFTDI_LICENSE_FILES = LICENSE.txt
 PYTHON_PYLIBFTDI_SETUP_TYPE = poetry
