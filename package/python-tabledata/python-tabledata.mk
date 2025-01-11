4a2740f0482a1abb20fd8e46fea1c7ee52ffe095
James Hilliard <james.hilliard1@gmail.com>
2025-01-11 11:01:50 -0700

    package/python-tabledata: bump to version 1.3.4
    Add new host-python-setuptools-scm build dependency.
    
    License hash changed due to year update:
    https://github.com/thombashi/tabledata/commit/c86bbd425f5f2bfa8e65a4e216e4457f9bd70c4e
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-tabledata/python-tabledata.mk
+++ package/python-tabledata/python-tabledata.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_TABLEDATA_VERSION = 1.3.3
+PYTHON_TABLEDATA_VERSION = 1.3.4
 PYTHON_TABLEDATA_SOURCE = tabledata-$(PYTHON_TABLEDATA_VERSION).tar.gz
-PYTHON_TABLEDATA_SITE = https://files.pythonhosted.org/packages/48/6a/7f78fbf883f325b70ba9ce5c10d97652f5c371e224940097c2cea9d0f456
+PYTHON_TABLEDATA_SITE = https://files.pythonhosted.org/packages/b2/35/171c8977162f1163368406deddde4c59673b62bd0cb2f34948a02effb075
 PYTHON_TABLEDATA_SETUP_TYPE = setuptools
 PYTHON_TABLEDATA_LICENSE = MIT
 PYTHON_TABLEDATA_LICENSE_FILES = LICENSE
+PYTHON_TABLEDATA_DEPENDENCIES = host-python-setuptools-scm
 
 $(eval $(python-package))
