821ce21fcf3dd5b4ccdfa7bacc96b396a9ca55f8
Akhilesh Nema <nemaakhilesh@gmail.com>
2025-01-14 23:59:29 -0800

    package/sqlite: bump version to 3.48.0
    Updated SQLITE_SITE.
    
    Release notes: https://www.sqlite.org/releaselog/3_48_0.html
    
    Signed-off-by: Akhilesh Nema <nemaakhilesh@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/sqlite/sqlite.mk
+++ package/sqlite/sqlite.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-SQLITE_VERSION = 3.47.2
-SQLITE_TAR_VERSION = 3470200
+SQLITE_VERSION = 3.48.0
+SQLITE_TAR_VERSION = 3480000
 SQLITE_SOURCE = sqlite-autoconf-$(SQLITE_TAR_VERSION).tar.gz
-SQLITE_SITE = https://www.sqlite.org/2024
+SQLITE_SITE = https://www.sqlite.org/2025
 SQLITE_LICENSE = blessing
 SQLITE_LICENSE_FILES = tea/license.terms
 SQLITE_CPE_ID_VENDOR = sqlite
