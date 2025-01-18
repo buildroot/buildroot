9cd3464afa13fd0190fb3e94fd850cc8063cf297
Bernd Kuhls <bernd@kuhls.net>
2025-01-18 22:40:53 +0100

    package/taglib: bump version to 2.0.2
    Changelog: https://github.com/taglib/taglib/blob/master/CHANGELOG.md
    
    Needs utfcpp:
    https://github.com/taglib/taglib/commit/6ed0ca28db0d12a75572c0f5dcefdc5ebb34cc21
    
    Switched _SITE to https.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/taglib/taglib.mk
+++ package/taglib/taglib.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-TAGLIB_VERSION = 1.13.1
-TAGLIB_SITE = http://taglib.github.io/releases
+TAGLIB_VERSION = 2.0.2
+TAGLIB_SITE = https://taglib.org/releases
 TAGLIB_INSTALL_STAGING = YES
+TAGLIB_DEPENDENCIES = utfcpp
 TAGLIB_LICENSE = LGPL-2.1 or MPL-1.1
 TAGLIB_LICENSE_FILES = COPYING.LGPL COPYING.MPL
 TAGLIB_CPE_ID_VENDOR = taglib
