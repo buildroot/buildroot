41013bb615b9eb1e20874bbe6e2b47ed2fac062d
Bernd Kuhls <bernd@kuhls.net>
2025-01-18 09:36:35 +0100

    package/kodi-screensaver-*: mass version bump to 21.0.2-Omega
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/kodi-screensaver-pyro/kodi-screensaver-pyro.mk
+++ package/kodi-screensaver-pyro/kodi-screensaver-pyro.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_SCREENSAVER_PYRO_VERSION = b0d14d3f8cfe7d0c71ce8df8a6b8d6f121b42908
+KODI_SCREENSAVER_PYRO_VERSION = 21.0.2-Omega
 KODI_SCREENSAVER_PYRO_SITE = $(call github,xbmc,screensaver.pyro,$(KODI_SCREENSAVER_PYRO_VERSION))
 KODI_SCREENSAVER_PYRO_LICENSE = GPL-2.0+
 KODI_SCREENSAVER_PYRO_LICENSE_FILES = LICENSE.md
