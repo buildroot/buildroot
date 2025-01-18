9f887a660842cb589927d6b94f63ae92d8a47c2f
Bernd Kuhls <bernd@kuhls.net>
2025-01-18 09:36:29 +0100

    package/kodi-imagedecoder-raw: bump version to 21.0.2-Omega
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/kodi-imagedecoder-raw/kodi-imagedecoder-raw.mk
+++ package/kodi-imagedecoder-raw/kodi-imagedecoder-raw.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_IMAGEDECODER_RAW_VERSION = 7be9cf4d985c277f3a059f64873d81b24e6edd70
+KODI_IMAGEDECODER_RAW_VERSION = 21.0.2-Omega
 KODI_IMAGEDECODER_RAW_SITE = $(call github,xbmc,imagedecoder.raw,$(KODI_IMAGEDECODER_RAW_VERSION))
 KODI_IMAGEDECODER_RAW_LICENSE = GPL-2.0+
 KODI_IMAGEDECODER_RAW_LICENSE_FILES = LICENSE.md
