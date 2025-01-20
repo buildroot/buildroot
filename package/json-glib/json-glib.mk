7f6d8881050558b768bce014f6b313e17f587f7e
Fiona Klute (WIWA) <fiona.klute@gmx.de>
2025-01-20 22:17:02 +0100

    package/json-glib: bump to version 1.10.6
    Required to build with gobject-introspection >= 1.82.0, to be bumped
    in a later commit.
    
    License information has been restructured according to REUSE
    guidelines, see [1]. Update hashes and license information
    accordingly.
    
    [1] https://gitlab.gnome.org/GNOME/json-glib/-/commit/a06d43db36545aff10eb384d62548d96a7cf6020
    
    Signed-off-by: Fiona Klute (WIWA) <fiona.klute@gmx.de>
    Tested-by: Marcus Hoffmann <buildroot@bubu1.eu>
    [Thomas: do not list .reuse/dep5 in license files]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/json-glib/json-glib.mk
+++ package/json-glib/json-glib.mk
@@ -4,12 +4,15 @@
 #
 ################################################################################
 
-JSON_GLIB_VERSION_MAJOR = 1.8
-JSON_GLIB_VERSION = $(JSON_GLIB_VERSION_MAJOR).0
+JSON_GLIB_VERSION_MAJOR = 1.10
+JSON_GLIB_VERSION = $(JSON_GLIB_VERSION_MAJOR).6
 JSON_GLIB_SITE = https://download.gnome.org/sources/json-glib/$(JSON_GLIB_VERSION_MAJOR)
 JSON_GLIB_SOURCE = json-glib-$(JSON_GLIB_VERSION).tar.xz
-JSON_GLIB_LICENSE = LGPL-2.1+
-JSON_GLIB_LICENSE_FILES = COPYING
+JSON_GLIB_LICENSE = LGPL-2.1+, MIT (conformance test data), CC0-1.0 (parts of build system, metadata, translations)
+JSON_GLIB_LICENSE_FILES = LICENSES/CC0-1.0.txt \
+	LICENSES/LGPL-2.1-or-later.txt \
+	LICENSES/MIT.txt \
+	COPYING
 JSON_GLIB_INSTALL_STAGING = YES
 JSON_GLIB_CONF_OPTS = -Dgtk_doc=disabled -Dtests=false
 
