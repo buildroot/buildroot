acfdec5515b84d9bf890f0ee257ab863956ec719
James Hilliard <james.hilliard1@gmail.com>
2025-01-21 15:28:08 -0700

    package/python-django: security bump to version 5.0.11
    Fixes the following vulnerability:
    
    CVE-2024-56374: Potential denial-of-service vulnerability in IPv6 validation
    
    Lack of upper bound limit enforcement in strings passed when performing IPv6
    validation could lead to a potential denial-of-service attack.  The
    undocumented and private functions clean_ipv6_address and
    is_valid_ipv6_address were vulnerable, as was the
    django.forms.GenericIPAddressField form field, which has now been updated to
    define a max_length of 39 characters.
    
    The django.db.models.GenericIPAddressField model field was not affected.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 5.0.10
+PYTHON_DJANGO_VERSION = 5.0.11
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/a8/52/06fedfe94a2610e2ea94ba3786c5475088e1e422b31c0ecd5b4c2ae6a561
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/02/9c/614ef004c122cfdf9f2d291eab75e24ab94d24c0693a0ca0db44e9633dc3
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
 PYTHON_DJANGO_CPE_ID_VENDOR = djangoproject
