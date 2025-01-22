fb36a4635ef69e333a5a5e5642ee378d4854e8dd
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2025-01-22 23:03:59 +0100

    package/gnu-efi: change upstream, bump to 4.0.0
    https://sourceforge.net/projects/gnu-efi/ is dead and stuck at version
    3.0.18. At https://sourceforge.net/p/gnu-efi/feature-requests/11/ the
    upstream maintainer has indicated that the project is now maintained
    at https://github.com/ncroxon/gnu-efi/, so let's use this as upstream.
    
    We can drop patches 0002 and 0003 since they are upstream. Patch 0001
    is just slightly refreshed.
    
    Changes 3.0.18 -> 3.0.19:
      https://github.com/ncroxon/gnu-efi/releases/tag/3.0.19
    
    Changes 3.0.19 -> 4.0.0:
      https://github.com/ncroxon/gnu-efi/releases/tag/4.0.0
    
    The licensing files have changed:
    
    - A LICENSE file has been added, with the text of the GPLv2
    
    - The README.efilib file has been moved, unchanged to
      licenses/LICENSE.efilib
    
    - A new file containing the EDK2 license has been added at
      licenses/LICENSE.edk2. This requires an update in the list of
      licenses
    
    Tested with the following defconfigs:
    
     ARM defconfig testing gnu-efi + fwupd-efi
    
     BR2_arm=y
     BR2_cortex_a9=y
     BR2_ARM_ENABLE_VFP=y
     BR2_TOOLCHAIN_EXTERNAL=y
     BR2_TOOLCHAIN_EXTERNAL_BOOTLIN=y
     BR2_TOOLCHAIN_EXTERNAL_BOOTLIN_ARMV7_EABIHF_MUSL_STABLE=y
     BR2_INIT_NONE=y
     BR2_SYSTEM_BIN_SH_NONE=y
     # BR2_PACKAGE_BUSYBOX is not set
     BR2_PACKAGE_FWUPD_EFI=y
     # BR2_TARGET_ROOTFS_TAR is not set
    
     RISC-V defconfig testing gnu-efi + fwupd-efi
    
     BR2_riscv=y
     BR2_TOOLCHAIN_EXTERNAL=y
     BR2_TOOLCHAIN_EXTERNAL_BOOTLIN_RISCV64_LP64D_MUSL_STABLE=y
     BR2_INIT_NONE=y
     BR2_SYSTEM_BIN_SH_NONE=y
     # BR2_PACKAGE_BUSYBOX is not set
     BR2_PACKAGE_FWUPD_EFI=y
     # BR2_TARGET_ROOTFS_TAR is not set
    
     x86-64 defconfig testing gnu-efi + fwupd-efi + systemd-boot
    
     BR2_x86_64=y
     BR2_x86_corei7=y
     BR2_TOOLCHAIN_EXTERNAL=y
     BR2_TOOLCHAIN_EXTERNAL_BOOTLIN_X86_64_CORE_I7_GLIBC_BLEEDING_EDGE=y
     BR2_INIT_SYSTEMD=y
     BR2_SYSTEM_BIN_SH_NONE=y
     BR2_SYSTEM_DEFAULT_PATH="/bin:/sbin:/usr/bin:/usr/sbin"
     # BR2_PACKAGE_BUSYBOX is not set
     BR2_PACKAGE_SYSTEMD_BOOT=y
     # BR2_TARGET_ROOTFS_TAR is not set
     BR2_TARGET_SYSLINUX=y
     BR2_TARGET_SYSLINUX_EFI=y
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/gnu-efi/gnu-efi.mk
+++ package/gnu-efi/gnu-efi.mk
@@ -4,12 +4,11 @@
 #
 ################################################################################
 
-GNU_EFI_VERSION = 3.0.18
-GNU_EFI_SOURCE = gnu-efi-$(GNU_EFI_VERSION).tar.bz2
-GNU_EFI_SITE = http://downloads.sourceforge.net/project/gnu-efi
+GNU_EFI_VERSION = 4.0.0
+GNU_EFI_SITE = $(call github,ncroxon,gnu-efi,$(GNU_EFI_VERSION))
 GNU_EFI_INSTALL_STAGING = YES
-GNU_EFI_LICENSE = BSD-3-Clause and/or GPL-2.0+ (gnuefi), BSD-3-Clause (efilib)
-GNU_EFI_LICENSE_FILES = README.efilib
+GNU_EFI_LICENSE = BSD-3-Clause and/or GPL-2.0+ (gnuefi), BSD-3-Clause (efilib), BSD-2-Clause-Patent (EDK2 routines)
+GNU_EFI_LICENSE_FILES = LICENSE licenses/LICENSE.edk2 licenses/LICENSE.efilib
 
 # gnu-efi is a set of library and header files used to build
 # standalone EFI applications such as bootloaders. There is no point
