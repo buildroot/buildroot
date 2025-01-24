85a79bdbfd1707820f3f769173076ffc7f46109e
Francois Perrad <francois.perrad@gadz.org>
2025-01-24 22:13:52 +0100

    package/lua-lsdbus: bump to version 3704232a634d1c866275b9a138b860421fd1c8ac
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/lua-lsdbus/lua-lsdbus.mk
+++ package/lua-lsdbus/lua-lsdbus.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LUA_LSDBUS_VERSION = ae5e674e5e792dd72e4164c436ca7b064158e7c6
+LUA_LSDBUS_VERSION = 3704232a634d1c866275b9a138b860421fd1c8ac
 LUA_LSDBUS_SITE = $(call github,kmarkus,lsdbus,$(LUA_LSDBUS_VERSION))
 LUA_LSDBUS_DEPENDENCIES = luainterpreter mxml systemd
 LUA_LSDBUS_LICENSE = LGPL-2.1
