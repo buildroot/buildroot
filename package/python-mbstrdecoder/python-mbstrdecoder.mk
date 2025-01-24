a4dce8441b97261d56a14df0016514e2515a9e02
James Hilliard <james.hilliard1@gmail.com>
2025-01-24 12:50:47 -0700

    package/python-mbstrdecoder: bump to version 1.1.4
    Add new host-python-setuptools-scm build dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-mbstrdecoder/python-mbstrdecoder.mk
+++ package/python-mbstrdecoder/python-mbstrdecoder.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_MBSTRDECODER_VERSION = 1.1.3
+PYTHON_MBSTRDECODER_VERSION = 1.1.4
 PYTHON_MBSTRDECODER_SOURCE = mbstrdecoder-$(PYTHON_MBSTRDECODER_VERSION).tar.gz
-PYTHON_MBSTRDECODER_SITE = https://files.pythonhosted.org/packages/70/8f/dd5d4efbe3f90d2d38c948f0ca5c698e2d6cedc58ead2f5b90272cbcb4fa
+PYTHON_MBSTRDECODER_SITE = https://files.pythonhosted.org/packages/31/ab/05ae008357c8bdb6245ebf8a101d99f26c096e0ea20800b318153da23796
 PYTHON_MBSTRDECODER_SETUP_TYPE = setuptools
 PYTHON_MBSTRDECODER_LICENSE = MIT
 PYTHON_MBSTRDECODER_LICENSE_FILES = LICENSE
+PYTHON_MBSTRDECODER_DEPENDENCIES = host-python-setuptools-scm
 
 $(eval $(python-package))
