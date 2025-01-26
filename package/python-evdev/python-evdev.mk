455ef44d26f027abc1936ba9041427a4b2e86f42
James Hilliard <james.hilliard1@gmail.com>
2025-01-26 07:15:22 -0700

    package/python-evdev: bump to version 1.8.0
    License hash changed due to date update:
    https://github.com/gvalkov/python-evdev/commit/2e3b843f37f79a9404da4541bbf9cf96dc5a4d57
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-evdev/python-evdev.mk
+++ package/python-evdev/python-evdev.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_EVDEV_VERSION = 1.7.1
+PYTHON_EVDEV_VERSION = 1.8.0
 PYTHON_EVDEV_SOURCE = evdev-$(PYTHON_EVDEV_VERSION).tar.gz
-PYTHON_EVDEV_SITE = https://files.pythonhosted.org/packages/12/bb/f622a8a5e64d46ca83020a761877c0ead19140903c9aaf1431f3c531fdf6
+PYTHON_EVDEV_SITE = https://files.pythonhosted.org/packages/4c/eb/b8900082a13a47bb9f69f415174d03cbf12ce95d07345722e1f4ef0e2093
 PYTHON_EVDEV_SETUP_TYPE = setuptools
 PYTHON_EVDEV_LICENSE = Revised BSD License
 PYTHON_EVDEV_LICENSE_FILES = LICENSE
