fd02a5a2598a7ed454f84d41ec0458eb5ccc64eb
Akhilesh Nema <nemaakhilesh@gmail.com>
2025-01-28 23:34:37 -0800

    package/iw: bump version to 6.9
    Signed-off-by: Akhilesh Nema <nemaakhilesh@gmail.com>
    Tested-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/iw/iw.mk
+++ package/iw/iw.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-IW_VERSION = 5.19
+IW_VERSION = 6.9
 IW_SOURCE = iw-$(IW_VERSION).tar.xz
 IW_SITE = $(BR2_KERNEL_MIRROR)/software/network/iw
 IW_LICENSE = ISC
