d892350f644e67340091498bd2ab677f19608d85
Saeed Kazemi <kazemi.ms@gmail.com>
2025-01-31 17:40:01 +0100

    package/eza: bump to version 0.20.19
    Release notes:
    https://github.com/eza-community/eza/releases/tag/v0.20.19
    
    Now licensed under the EUPL-1.2 license, so adjust _LICENSE to match:
    https://github.com/eza-community/eza/releases/tag/v0.20.0
    
    Signed-off-by: Saeed Kazemi <kazemi.ms@gmail.com>
    [Peter: add note about license change]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/eza/eza.mk
+++ package/eza/eza.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-EZA_VERSION = 0.18.24
+EZA_VERSION = 0.20.19
 EZA_SITE = $(call github,eza-community,eza,v$(EZA_VERSION))
-EZA_LICENSE = MIT
-EZA_LICENSE_FILES = LICENCE
+EZA_LICENSE = EUPL-1.2
+EZA_LICENSE_FILES = LICENSE.txt
 
 $(eval $(cargo-package))
