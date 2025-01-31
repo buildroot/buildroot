914bd041a5cec16bef1a69c9e7509f744940c13c
Saeed Kazemi <kazemi.ms@gmail.com>
2025-01-31 18:06:48 +0100

    package/procs: bump to version 0.14.9
    Release notes:
    https://github.com/dalance/procs/releases/tag/v0.14.9
    
    Signed-off-by: Saeed Kazemi <kazemi.ms@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/procs/procs.mk
+++ package/procs/procs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PROCS_VERSION = 0.14.5
+PROCS_VERSION = 0.14.9
 PROCS_SITE = $(call github,dalance,procs,v$(PROCS_VERSION))
 PROCS_LICENSE = MIT
 PROCS_LICENSE_FILES = LICENSE
