917495a2acb870af6f4cde39cc672b136fb89304
Gabriele Besta <buildroot@bgdev.it>
2025-02-03 17:24:51 +0000

    toolchain/toolchain-external/toolchain-external-arm-aarch64: add toolchain for AArch64 host architecture
    Enabling AArch64 as a host architecture for AArch64 build.
    
    Signed-off-by: Gabriele Besta <buildroot@bgdev.it>
    Signed-off-by: Arnout Vandecappelle <arnout@mind.be>

--- toolchain/toolchain-external/toolchain-external-arm-aarch64/toolchain-external-arm-aarch64.mk
+++ toolchain/toolchain-external/toolchain-external-arm-aarch64/toolchain-external-arm-aarch64.mk
@@ -7,6 +7,6 @@
 TOOLCHAIN_EXTERNAL_ARM_AARCH64_VERSION = 14.2.rel1
 TOOLCHAIN_EXTERNAL_ARM_AARCH64_SITE = https://developer.arm.com/-/media/Files/downloads/gnu/$(TOOLCHAIN_EXTERNAL_ARM_AARCH64_VERSION)/binrel
 
-TOOLCHAIN_EXTERNAL_ARM_AARCH64_SOURCE = arm-gnu-toolchain-$(TOOLCHAIN_EXTERNAL_ARM_AARCH64_VERSION)-x86_64-aarch64-none-linux-gnu.tar.xz
+TOOLCHAIN_EXTERNAL_ARM_AARCH64_SOURCE = arm-gnu-toolchain-$(TOOLCHAIN_EXTERNAL_ARM_AARCH64_VERSION)-$(HOSTARCH)-aarch64-none-linux-gnu.tar.xz
 
 $(eval $(toolchain-external-package))
