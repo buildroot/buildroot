8af6f2eaf99c3c808742bf5154e57752b20b234a
Yann E. MORIN <yann.morin.1998@free.fr>
2025-02-03 10:34:07 +0100

    package/libserialport: update to 0.1.2
    Drop the patch, that has been upstreamed as b2359c5c994c (libserialport:
    Fix Linux files not compiled in with a toolchain for ucLinux).
    
    Accordingly, drop the now superfluous autoreconf.
    
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    Cc: Paul Cercueil <paul@crapouillou.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libserialport/libserialport.mk
+++ package/libserialport/libserialport.mk
@@ -4,12 +4,11 @@
 #
 ################################################################################
 
-LIBSERIALPORT_VERSION = 0.1.1
+LIBSERIALPORT_VERSION = 0.1.2
 LIBSERIALPORT_SITE = http://sigrok.org/download/source/libserialport
 LIBSERIALPORT_LICENSE = LGPL-3.0+
 LIBSERIALPORT_LICENSE_FILES = COPYING
 LIBSERIALPORT_INSTALL_STAGING = YES
 LIBSERIALPORT_DEPENDENCIES = host-pkgconf
-LIBSERIALPORT_AUTORECONF = YES
 
 $(eval $(autotools-package))
