16943849e090fbaca75817ba3563dd2d66bfd51d
Marcin Niestroj <m.niestroj@grinn-global.com>
2025-02-03 07:42:40 +0100

    package/python-pytest-asyncio: bump to 0.25.3
    See [1] for changelog.
    
    [1] https://pytest-asyncio.readthedocs.io/en/latest/reference/changelog.html
    
    Signed-off-by: Marcin Niestroj <m.niestroj@grinn-global.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-pytest-asyncio/python-pytest-asyncio.mk
+++ package/python-pytest-asyncio/python-pytest-asyncio.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYTEST_ASYNCIO_VERSION = 0.25.2
+PYTHON_PYTEST_ASYNCIO_VERSION = 0.25.3
 PYTHON_PYTEST_ASYNCIO_SOURCE = pytest_asyncio-$(PYTHON_PYTEST_ASYNCIO_VERSION).tar.gz
-PYTHON_PYTEST_ASYNCIO_SITE = https://files.pythonhosted.org/packages/72/df/adcc0d60f1053d74717d21d58c0048479e9cab51464ce0d2965b086bd0e2
+PYTHON_PYTEST_ASYNCIO_SITE = https://files.pythonhosted.org/packages/f2/a8/ecbc8ede70921dd2f544ab1cadd3ff3bf842af27f87bbdea774c7baa1d38
 PYTHON_PYTEST_ASYNCIO_SETUP_TYPE = setuptools
 PYTHON_PYTEST_ASYNCIO_LICENSE = Apache-2.0
 PYTHON_PYTEST_ASYNCIO_LICENSE_FILES = LICENSE
