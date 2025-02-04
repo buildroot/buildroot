4def253882ff2b16ddaff9bcaf75c15c82a7dd9f
Neal Frager <neal.frager@amd.com>
2025-02-04 08:59:01 +0000

    package/binutils-bare-metal: bump to version 2.44
    Bump binutils-bare-metal to version 2.44.
    
    Release notes:
    https://sourceware.org/pipermail/binutils/2025-February/139195.html
    
    Signed-off-by: Neal Frager <neal.frager@amd.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/binutils-bare-metal/binutils-bare-metal.mk
+++ package/binutils-bare-metal/binutils-bare-metal.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BINUTILS_BARE_METAL_VERSION = 2.43.1
+BINUTILS_BARE_METAL_VERSION = 2.44
 BINUTILS_BARE_METAL_SITE = $(BR2_GNU_MIRROR)/binutils
 BINUTILS_BARE_METAL_SOURCE = binutils-$(BINUTILS_BARE_METAL_VERSION).tar.xz
 
