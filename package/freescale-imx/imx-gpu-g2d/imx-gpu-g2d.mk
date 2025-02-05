816f239e370d57a76ff388a52acbca28f84e8911
Sébastien Szymanski <sebastien.szymanski@armadeus.com>
2025-02-05 12:41:00 +0100

    package/freescale-imx/imx-gpu-g2d: bump version to 6.4.11.p2.10
    This version is aligned with NXP Linux BSP version "lf-6.6.52-2.2.0".
    
    License hashes changed because the LA_OPT_NXP_Software_License changed
    from "v56 April 2024" to "v57 July 2024".
    Add SCR.txt to _LICENSE_FILES.
    
    Note: in this release, the package source archive name changed to
    include a revision. This commit adds a new _REVISION variable with the
    value published in the corresponding NXP BSP.
    
    Signed-off-by: Sébastien Szymanski <sebastien.szymanski@armadeus.com>
    [Julien: fixed the bsp version name in commit log]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/freescale-imx/imx-gpu-g2d/imx-gpu-g2d.mk
+++ package/freescale-imx/imx-gpu-g2d/imx-gpu-g2d.mk
@@ -4,18 +4,19 @@
 #
 ################################################################################
 
+IMX_GPU_G2D_REVISION = accdd64
 ifeq ($(BR2_aarch64),y)
-IMX_GPU_G2D_VERSION = 6.4.11.p2.6-aarch64-bc7b6a2
+IMX_GPU_G2D_VERSION = 6.4.11.p2.10-aarch64
 else
-IMX_GPU_G2D_VERSION = 6.4.11.p2.6-arm-bc7b6a2
+IMX_GPU_G2D_VERSION = 6.4.11.p2.10-arm
 endif
 IMX_GPU_G2D_SITE = $(FREESCALE_IMX_SITE)
-IMX_GPU_G2D_SOURCE = imx-gpu-g2d-$(IMX_GPU_G2D_VERSION).bin
+IMX_GPU_G2D_SOURCE = imx-gpu-g2d-$(IMX_GPU_G2D_VERSION)-$(IMX_GPU_G2D_REVISION).bin
 IMX_GPU_G2D_DEPENDENCIES = imx-gpu-viv
 IMX_GPU_G2D_INSTALL_STAGING = YES
 
 IMX_GPU_G2D_LICENSE = NXP Semiconductor Software License Agreement
-IMX_GPU_G2D_LICENSE_FILES = EULA COPYING
+IMX_GPU_G2D_LICENSE_FILES = EULA COPYING SCR-imx-gpu-g2d.txt
 IMX_GPU_G2D_REDISTRIBUTE = NO
 
 define IMX_GPU_G2D_EXTRACT_CMDS
