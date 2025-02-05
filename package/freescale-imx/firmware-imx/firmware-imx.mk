6295c5ef567d8500d6a8d1ee9e29396d39fa4a67
Sébastien Szymanski <sebastien.szymanski@armadeus.com>
2025-02-05 12:40:54 +0100

    package/freescale-imx/firmware-imx: bump version to 8.26
    This version is aligned with NXP Linux BSP version "lf-6.6.52-2.2.0".
    
    License hashes changed because the LA_OPT_NXP_Software_License changed
    from "v56 April 2024" to "v57 July 2024".
    
    Signed-off-by: Sébastien Szymanski <sebastien.szymanski@armadeus.com>
    [Julien: fixed the bsp version name in commit log]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/freescale-imx/firmware-imx/firmware-imx.mk
+++ package/freescale-imx/firmware-imx/firmware-imx.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-FIRMWARE_IMX_VERSION = 8.24
-FIRMWARE_IMX_REVISION = fbe0a4c
+FIRMWARE_IMX_VERSION = 8.26
+FIRMWARE_IMX_REVISION = d4c33ab
 FIRMWARE_IMX_SITE = $(FREESCALE_IMX_SITE)
 FIRMWARE_IMX_SOURCE = firmware-imx-$(FIRMWARE_IMX_VERSION)-$(FIRMWARE_IMX_REVISION).bin
 
