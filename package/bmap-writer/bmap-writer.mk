ebb65fcf2c96bddc17e82fa5128b73c18909ef13
Ayoub Zaki <ayoub.zaki@embetrix.com>
2025-02-05 15:47:28 +0100

    package/bmap-writer: bump to version 1.0.2
    Switch from libxml2 to tinyxml2
    
    Add http(s)/ftp stream wrapper script
    
    Signed-off-by: Ayoub Zaki <ayoub.zaki@embetrix.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/bmap-writer/bmap-writer.mk
+++ package/bmap-writer/bmap-writer.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-BMAP_WRITER_VERSION = 0.0.3
+BMAP_WRITER_VERSION = 1.0.2
 BMAP_WRITER_SITE = $(call github,embetrix,bmap-writer,$(BMAP_WRITER_VERSION))
 BMAP_WRITER_LICENSE = GPL-3.0
 BMAP_WRITER_LICENSE_FILES = LICENSE
-BMAP_WRITER_DEPENDENCIES = host-pkgconf libarchive libxml2
-HOST_BMAP_WRITER_DEPENDENCIES = host-pkgconf host-libarchive host-libxml2
+BMAP_WRITER_DEPENDENCIES = host-pkgconf libarchive tinyxml2
+HOST_BMAP_WRITER_DEPENDENCIES = host-pkgconf host-libarchive host-tinyxml2
 
 $(eval $(cmake-package))
 $(eval $(host-cmake-package))
