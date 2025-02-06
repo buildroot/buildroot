d9d76e035164b0e781a826999fa293f126134a17
Marcus Hoffmann <buildroot@bubu1.eu>
2025-02-06 13:48:24 +0100

    package/python-waitress: security bump to 3.0.2
    Both 3.0.1 and 3.0.2 fix security issues.
    
    In 3.0.1:
    
    * Fix a bug that would lead to Waitress busy looping on select() on a
      half-open socket due to a race condition that existed when creating a
      new HTTPChannel. See https://github.com/Pylons/waitress/pull/435,
      https://github.com/Pylons/waitress/issues/418 and
      https://github.com/Pylons/waitress/security/advisories/GHSA-3f84-rpwh-47g6
    
    * With thanks to Dylan Jay and Dieter Maurer for their extensive
      debugging and helping track this down.
    
    * No longer strip the header values before passing them to the WSGI
      environ. See https://github.com/Pylons/waitress/pull/434 and
      https://github.com/Pylons/waitress/issues/432
    
    * Fix a race condition in Waitress when channel_request_lookahead is
      enabled that could lead to HTTP request smuggling.
      See https://github.com/Pylons/waitress/security/advisories/GHSA-9298-4cf8-g4wj
    
    In 3.0.2:
    
    * When using Waitress to process trusted proxy headers, Waitress will
      now update the headers to drop any untrusted values, thereby making sure
      that WSGI apps only get trusted and validated values that Waitress
      itself used to update the environ. See
      https://github.com/Pylons/waitress/pull/452 and
      https://github.com/Pylons/waitress/issues/451
    
    Full Changelog:
    https://docs.pylonsproject.org/projects/waitress/en/latest/#change-history
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-waitress/python-waitress.mk
+++ package/python-waitress/python-waitress.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_WAITRESS_VERSION = 3.0.0
+PYTHON_WAITRESS_VERSION = 3.0.2
 PYTHON_WAITRESS_SOURCE = waitress-$(PYTHON_WAITRESS_VERSION).tar.gz
-PYTHON_WAITRESS_SITE = https://files.pythonhosted.org/packages/70/34/cb77e5249c433eb177a11ab7425056b32d3b57855377fa1e38b397412859
+PYTHON_WAITRESS_SITE = https://files.pythonhosted.org/packages/bf/cb/04ddb054f45faa306a230769e868c28b8065ea196891f09004ebace5b184
 PYTHON_WAITRESS_SETUP_TYPE = setuptools
 PYTHON_WAITRESS_LICENSE = ZPL-2.1
 PYTHON_WAITRESS_LICENSE_FILES = LICENSE.txt
