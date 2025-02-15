1040a4b714f530cb96589f35db017fcef5448dc8
Maxim Kochetkov <fido_max@inbox.ru>
2025-02-15 09:25:04 +0300

    package/protozero: bump version to 1.8.0
    Release-notes: https://github.com/mapbox/protozero/releases/tag/v1.8.0
    Drop upstream patch.
    Updated license hash due to copyright year bump:
    https://github.com/mapbox/protozero/commit/e91587f4bb1a0c443c663f4d1fcfd1ace674616f
    
    Bump minimal GCC version to 5. (Switched to C++14 as minimum requirement)
    
    Signed-off-by: Maxim Kochetkov <fido_max@inbox.ru>
    [Julien: remove patch entry in .checkpackageignore]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/protozero/protozero.mk
+++ package/protozero/protozero.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PROTOZERO_VERSION = 1.7.1
+PROTOZERO_VERSION = 1.8.0
 PROTOZERO_SITE = $(call github,mapbox,protozero,v$(PROTOZERO_VERSION))
 PROTOZERO_LICENSE = BSD-2-Clause, Apache-2.0
 PROTOZERO_LICENSE_FILES = LICENSE.md LICENSE.from_folly
