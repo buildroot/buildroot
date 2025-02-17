6faa08a9e3f3ad170b70439a14bf8da6256f887e
Marcin Niestroj <m.niestroj@grinn-global.com>
2025-02-17 12:01:31 +0100

    package/python-dbus-fast: bump to 2.33.0
    See [1] for changelog.
    
    [1] https://github.com/bluetooth-devices/dbus-fast/blob/main/CHANGELOG.md
    
    Signed-off-by: Marcin Niestroj <m.niestroj@grinn-global.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-dbus-fast/python-dbus-fast.mk
+++ package/python-dbus-fast/python-dbus-fast.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_DBUS_FAST_VERSION = 2.30.2
+PYTHON_DBUS_FAST_VERSION = 2.33.0
 PYTHON_DBUS_FAST_SOURCE = dbus_fast-$(PYTHON_DBUS_FAST_VERSION).tar.gz
-PYTHON_DBUS_FAST_SITE = https://files.pythonhosted.org/packages/69/4a/c5dbb4eccb97d3dd0ec49e04e17b7e93dfae517881ad439141fa6179cb53
+PYTHON_DBUS_FAST_SITE = https://files.pythonhosted.org/packages/7d/42/7a6537132dc63056509fb112517a6b5a591770df1a49892aaf90864bc7db
 PYTHON_DBUS_FAST_SETUP_TYPE = poetry
 PYTHON_DBUS_FAST_LICENSE = MIT
 PYTHON_DBUS_FAST_LICENSE_FILES = LICENSE
