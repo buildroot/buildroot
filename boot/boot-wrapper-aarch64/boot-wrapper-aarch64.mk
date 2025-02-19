bd41cccc7a9d912289315e28ca85ade8179f2c1f
Vincent Stehlé <vincent.stehle@laposte.net>
2025-02-19 09:14:44 +0100

    boot/boot-wrapper-aarch64: bump version
    Bump boot-wrapper to the latest version.
    
    Signed-off-by: Vincent Stehlé <vincent.stehle@laposte.net>
    Cc: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- boot/boot-wrapper-aarch64/boot-wrapper-aarch64.mk
+++ boot/boot-wrapper-aarch64/boot-wrapper-aarch64.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BOOT_WRAPPER_AARCH64_VERSION = 9f26a1c1f27bd6b5b66c265114848007fc22e4aa
+BOOT_WRAPPER_AARCH64_VERSION = ac6742520ded1da30d500f74e8affe86e27cabd5
 BOOT_WRAPPER_AARCH64_SITE = https://git.kernel.org/pub/scm/linux/kernel/git/mark/boot-wrapper-aarch64.git
 BOOT_WRAPPER_AARCH64_SITE_METHOD = git
 BOOT_WRAPPER_AARCH64_LICENSE = BSD-3-Clause
