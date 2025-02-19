b13373466bd7e8ac2d587cbd2489b1396fbb2f82
Waldemar Brodkorb <wbx@openadk.org>
2025-02-19 16:27:10 +0100

    package/valgrind: update to 3.24.0
    See here for a changelog:
    https://valgrind.org/docs/manual/dist.news.html
    
    Patches 0003/0004 are applied upstream and so got removed here.
    
    Signed-off-by: Waldemar Brodkorb <wbx@openadk.org>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/valgrind/valgrind.mk
+++ package/valgrind/valgrind.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-VALGRIND_VERSION = 3.23.0
+VALGRIND_VERSION = 3.24.0
 VALGRIND_SITE = https://sourceware.org/pub/valgrind
 VALGRIND_SOURCE = valgrind-$(VALGRIND_VERSION).tar.bz2
 VALGRIND_LICENSE = GPL-2.0, GFDL-1.2
