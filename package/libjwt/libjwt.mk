770f9154978c40ce5b7718174108b37c5310869d
Waldemar Brodkorb <wbx@openadk.org>
2025-02-20 20:48:50 +0100

    package/libjwt: update to 3.2.0
    For a complete changelog see here:
    https://github.com/benmcollins/libjwt/compare/v1.17.2...v3.2.0
    
    Convert to cmake infrastructure as autotools were removed
    upstream.
    
    Signed-off-by: Waldemar Brodkorb <wbx@openadk.org>
    [Arnout: remove redundant empty line]
    Signed-off-by: Arnout Vandecappelle <arnout@rnout.be>

--- package/libjwt/libjwt.mk
+++ package/libjwt/libjwt.mk
@@ -4,23 +4,24 @@
 #
 ################################################################################
 
-LIBJWT_VERSION = 1.17.2
+LIBJWT_VERSION = 3.2.0
 LIBJWT_SITE = https://github.com/benmcollins/libjwt/releases/download/v$(LIBJWT_VERSION)
-LIBJWT_SOURCE = libjwt-$(LIBJWT_VERSION).tar.bz2
+LIBJWT_SOURCE = libjwt-$(LIBJWT_VERSION).tar.xz
 LIBJWT_DEPENDENCIES = host-pkgconf jansson
 LIBJWT_INSTALL_STAGING = YES
 LIBJWT_LICENSE = MPL-2.0
 LIBJWT_LICENSE_FILES = LICENSE
 LIBJWT_CPE_ID_VENDOR = bencollins
 LIBJWT_CPE_ID_PRODUCT = jwt_c_library
-LIBJWT_CONF_OPTS = --without-examples
+
+LIBJWT_CONF_OPTS += -DWITH_TESTS=OFF
 
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
-LIBJWT_CONF_OPTS += --with-openssl
+LIBJWT_CONF_OPTS += -DWITH_OPENSSL=ON
 LIBJWT_DEPENDENCIES += openssl
 else
-LIBJWT_CONF_OPTS += --without-openssl
+LIBJWT_CONF_OPTS += -DWITH_GNUTLS=ON
 LIBJWT_DEPENDENCIES += gnutls
 endif
 
-$(eval $(autotools-package))
+$(eval $(cmake-package))
