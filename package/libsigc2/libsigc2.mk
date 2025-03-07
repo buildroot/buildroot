2af019a1bd3e4d9d8a3600cf12682a14acb6e965
Daniel Lang <dalang@gmx.at>
2025-03-07 08:56:05 +0100

    package/libsigc2: bump to version 2.12.1
    https://github.com/libsigcplusplus/libsigcplusplus/blob/2.12.1/NEWS
    
    Signed-off-by: Daniel Lang <dalang@gmx.at>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libsigc2/libsigc2.mk
+++ package/libsigc2/libsigc2.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LIBSIGC2_VERSION_MAJOR = 2.12
-LIBSIGC2_VERSION = $(LIBSIGC2_VERSION_MAJOR).0
+LIBSIGC2_VERSION = $(LIBSIGC2_VERSION_MAJOR).1
 LIBSIGC2_SOURCE = libsigc++-$(LIBSIGC2_VERSION).tar.xz
 LIBSIGC2_SITE = https://download.gnome.org/sources/libsigc++/$(LIBSIGC2_VERSION_MAJOR)
 LIBSIGC2_INSTALL_STAGING = YES
