6171150697d7a3e6756bc6167e5e5be3dcb313b1
Quentin Schulz <quentin.schulz@cherry.de>
2025-03-07 19:58:45 +0100

    package/setserial: update to 2.17-57 for compatibility with newer toolchains
    Debian has a newer setserial which compiles on newer toolchains, so
    let's update to it.
    
    Noteworthy is the change from a big diff to a tarball containing the
    whole debian directory. The series file which contains all patches to
    apply is named properly so no need to rename 00list anymore. The patches
    file extension is now .patch instead of .dpatch.
    
    This fixes the build with BR2_TOOLCHAIN_EXTERNAL_ARM_AARCH64 external
    toolchain (14.2.rel1).
    
    Signed-off-by: Quentin Schulz <quentin.schulz@cherry.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 871cdeab6a00054a1366d5e0ea56bd62a53f1c76)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/setserial/setserial.mk
+++ package/setserial/setserial.mk
@@ -6,8 +6,8 @@
 
 SETSERIAL_VERSION = 2.17
 SETSERIAL_SOURCE = setserial_$(SETSERIAL_VERSION).orig.tar.gz
-SETSERIAL_SITE = http://snapshot.debian.org/archive/debian/20141023T043132Z/pool/main/s/setserial
-SETSERIAL_EXTRA_DOWNLOADS = setserial_2.17-45.3.diff.gz
+SETSERIAL_SITE = https://snapshot.debian.org/archive/debian/20250307T084701Z/pool/main/s/setserial
+SETSERIAL_EXTRA_DOWNLOADS = setserial_$(SETSERIAL_VERSION)-57.debian.tar.xz
 SETSERIAL_LICENSE = GPL-2.0
 SETSERIAL_LICENSE_FILES = debian/copyright
 # make all also builds setserial.cat which needs nroff
@@ -18,26 +18,19 @@ SETSERIAL_AUTORECONF = YES
 # Extract the Debian tarball inside the sources
 define SETSERIAL_DEBIAN_EXTRACT
 	$(call suitable-extractor,$(notdir $(SETSERIAL_EXTRA_DOWNLOADS))) \
-		$(SETSERIAL_DL_DIR)/$(notdir $(SETSERIAL_EXTRA_DOWNLOADS)) > $(@D)/setserial_2.17-45.3.diff
+		$(SETSERIAL_DL_DIR)/$(notdir $(SETSERIAL_EXTRA_DOWNLOADS)) | \
+		$(TAR) -C $(@D) $(TAR_OPTIONS) -
 endef
 
 SETSERIAL_POST_EXTRACT_HOOKS += SETSERIAL_DEBIAN_EXTRACT
 
 define SETSERIAL_APPLY_DEBIAN_PATCHES
-	# - Applying diff gotten from SETSERIAL_EXTRA_DOWNLOADS
 	# - Touching gorhack.h is needed for the Debian patch 18 to work
-	# - Rename 00list which contains the list of patches to "series" so
-	#   Buildroot knows in which order the patches need to be applied. This
-	#   is necessary because their file extension is dpatch, which isn't
-	#   supported by the APPLY_PATCHES script outside of the series
-	#   mechanism.
-	$(APPLY_PATCHES) $(@D) $(@D) setserial_2.17-45.3.diff
-
+	# - Apply patches in the order listed in debian/patches/series
 	if [ -d $(@D)/debian/patches ]; then \
 		touch $(@D)/gorhack.h; \
-		rm $(@D)/debian/patches/01_makefile.dpatch; \
-		mv $(@D)/debian/patches/00list $(@D)/debian/patches/series; \
-		sed -i '/01_makefile.dpatch/d' $(@D)/debian/patches/series; \
+		rm $(@D)/debian/patches/01_makefile.patch; \
+		sed -i '/01_makefile.patch/d' $(@D)/debian/patches/series; \
 		$(APPLY_PATCHES) $(@D) $(@D)/debian/patches series; \
 	fi
 endef
