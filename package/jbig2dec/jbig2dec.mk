63c2dd2d40186ca6817a5be122a2ba78033f3cfc
Raphaël Mélotte <raphael.melotte@mind.be>
2025-03-10 11:08:40 +0000

    package/jbig2dec: add upstream security fix for CVE-2023-46361
    Fixes the following security issue:
    CVE-2023-46361: Artifex Software jbig2dec v0.20 was discovered to
    contain a SEGV vulnerability via jbig2_error at /jbig2dec/jbig2.c.
    
    https://www.cve.org/CVERecord?id=CVE-2023-46361
    
    Note that jbig2dec version 0.19 (used in Buildroot 2024.02.11) is not
    officially listed as affected, but it actually has the same bug.
    
    Also note that this CVE is about a crash in the CLI tool only.
    
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>
    [Julien: add JBIG2DEC_IGNORE_CVES]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/jbig2dec/jbig2dec.mk
+++ package/jbig2dec/jbig2dec.mk
@@ -12,4 +12,7 @@ JBIG2DEC_LICENSE_FILES = LICENSE
 JBIG2DEC_CPE_ID_VENDOR = artifex
 JBIG2DEC_INSTALL_STAGING = YES
 
+# 0001-Bug-705041-jbig2dec-Avoid-uninitialized-allocator-in.patch
+JBIG2DEC_IGNORE_CVES += CVE-2023-46361
+
 $(eval $(autotools-package))
