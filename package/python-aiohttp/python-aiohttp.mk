cfd6ac3d04ab94b762658167b1698706805f0a2a
James Hilliard <james.hilliard1@gmail.com>
2025-03-17 15:46:02 -0600

    package/python-aiohttp: bump to version 3.11.14
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    [Julien: rebase patch after merge of next branch]
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 03cda9370fd356910af205a9c38dc53dcb99a7b1)

--- package/python-aiohttp/python-aiohttp.mk
+++ package/python-aiohttp/python-aiohttp.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_AIOHTTP_VERSION = 3.11.12
+PYTHON_AIOHTTP_VERSION = 3.11.14
 PYTHON_AIOHTTP_SOURCE = aiohttp-$(PYTHON_AIOHTTP_VERSION).tar.gz
-PYTHON_AIOHTTP_SITE = https://files.pythonhosted.org/packages/37/4b/952d49c73084fb790cb5c6ead50848c8e96b4980ad806cf4d2ad341eaa03
+PYTHON_AIOHTTP_SITE = https://files.pythonhosted.org/packages/6c/96/91e93ae5fd04d428c101cdbabce6c820d284d61d2614d00518f4fa52ea24
 PYTHON_AIOHTTP_SETUP_TYPE = setuptools
 PYTHON_AIOHTTP_LICENSE = Apache-2.0
 PYTHON_AIOHTTP_LICENSE_FILES = LICENSE.txt
