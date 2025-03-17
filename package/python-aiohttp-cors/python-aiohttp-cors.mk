4e08ab7acd8fcfc68c6a1313eefed87972b37171
James Hilliard <james.hilliard1@gmail.com>
2025-03-17 15:40:28 -0600

    package/python-aiohttp-cors: bump to version 0.8.0
    License hash changed due to formatting changes:
    https://github.com/aio-libs/aiohttp-cors/commit/c4a4ead0df64eaa266ba36660a8a841370bf07cd
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-aiohttp-cors/python-aiohttp-cors.mk
+++ package/python-aiohttp-cors/python-aiohttp-cors.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_AIOHTTP_CORS_VERSION = 0.7.0
-PYTHON_AIOHTTP_CORS_SOURCE = aiohttp-cors-$(PYTHON_AIOHTTP_CORS_VERSION).tar.gz
-PYTHON_AIOHTTP_CORS_SITE = https://files.pythonhosted.org/packages/44/9e/6cdce7c3f346d8fd487adf68761728ad8cd5fbc296a7b07b92518350d31f
+PYTHON_AIOHTTP_CORS_VERSION = 0.8.0
+PYTHON_AIOHTTP_CORS_SOURCE = aiohttp_cors-$(PYTHON_AIOHTTP_CORS_VERSION).tar.gz
+PYTHON_AIOHTTP_CORS_SITE = https://files.pythonhosted.org/packages/1d/a8/3edbba1d565f7888c52e04ac6188e19b812a9fc39d81dea413f3abf81bfa
 PYTHON_AIOHTTP_CORS_SETUP_TYPE = setuptools
 PYTHON_AIOHTTP_CORS_LICENSE = Apache-2.0
 PYTHON_AIOHTTP_CORS_LICENSE_FILES = LICENSE
