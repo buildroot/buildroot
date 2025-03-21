d5e6d6594b45feb8604d3ac34843a6348f791c33
Bernd Kuhls <bernd@kuhls.net>
2025-03-21 18:35:30 +0100

    package/kodi-pvr-waipu: bump version to 21.9.2-Omega
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit cd8a9eba0179d0b02bfd63baea68d6de986bd4d2)

--- package/kodi-pvr-waipu/kodi-pvr-waipu.mk
+++ package/kodi-pvr-waipu/kodi-pvr-waipu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PVR_WAIPU_VERSION = 21.9.1-Omega
+KODI_PVR_WAIPU_VERSION = 21.9.2-Omega
 KODI_PVR_WAIPU_SITE = $(call github,flubshi,pvr.waipu,$(KODI_PVR_WAIPU_VERSION))
 KODI_PVR_WAIPU_LICENSE = GPL-2.0+
 KODI_PVR_WAIPU_LICENSE_FILES = pvr.waipu/LICENSE.txt
