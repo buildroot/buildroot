e75e166c4e1ed464056608354bdf1a2deff594af
Adam Duskett <adam.duskett@amarulasolutions.com>
2025-03-24 10:44:21 +0100

    package/depot-tools: bump to version 097e20723774957f21ac6d1808fcd0a91e8c7630
    Signed-off-by: Adam Duskett <adam.duskett@amarulasolutions.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/depot-tools/depot-tools.mk
+++ package/depot-tools/depot-tools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DEPOT_TOOLS_VERSION = 58625e82c685426d441be5b422c9ad88e4867d20
+DEPOT_TOOLS_VERSION = 097e20723774957f21ac6d1808fcd0a91e8c7630
 DEPOT_TOOLS_SITE = https://chromium.googlesource.com/chromium/tools/depot_tools
 DEPOT_TOOLS_SITE_METHOD = git
 DEPOT_TOOLS_LICENSE = BSD-3-Clause
