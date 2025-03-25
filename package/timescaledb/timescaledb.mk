6abdf5591f6c20dad9e7464e8b8b0eddc19c5964
Scott Fan <fancp2007@gmail.com>
2025-03-25 09:45:51 +0800

    package/timescaledb: bump version to 2.19.0
    Release notes: https://github.com/timescale/timescaledb/blob/2.19.0/CHANGELOG.md
    
    Signed-off-by: Scott Fan <fancp2007@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/timescaledb/timescaledb.mk
+++ package/timescaledb/timescaledb.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TIMESCALEDB_VERSION = 2.18.2
+TIMESCALEDB_VERSION = 2.19.0
 TIMESCALEDB_SITE = $(call github,timescale,timescaledb,$(TIMESCALEDB_VERSION))
 TIMESCALEDB_LICENSE = Apache-2.0
 TIMESCALEDB_LICENSE_FILES = LICENSE
