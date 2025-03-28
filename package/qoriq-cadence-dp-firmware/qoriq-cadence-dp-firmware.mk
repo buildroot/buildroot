88e4af822effd3877efea7534c8941fb7b8986da
Vladimir Oltean <olteanv@gmail.com>
2025-03-28 20:04:12 +0200

    package/qoriq-cadence-dp-firmware: bump version aligned to 6.12.3-1.0.0 BSP
    This commit updates the qoriq-cadence-dp-firmware to the version
    included in the 6.12.3-1.0.0 QorIQ BSP.
    
    The firmware archive was refactored and its location has changed.
    Those qoriq firmware files are now reused from the firmware-imx
    archive, which is also included in Buildroot in the directory
    "package/freescale-imx/firmware-imx". This commit keeps the Buildroot
    package name qoriq-cadence-dp-firmware, first to keep compatibility
    or Buildroot configs, and also because QorIQ BSP uses a different
    version of the firmware-imx package, compared to the i.MX BSP (at a
    same BSP version).
    
    This commit updates the archive name, the archive version and the
    download URL following the NXP Yocto BSP at [1]. More precisely,
    the information was taken from the recipe at [2].
    
    Note that regarding this qoriq-cadence-dp-firmware version:
    - the QorIQ BSP version is: 6.12.3-1.0.0
    - the Yocto dp-firmware-cadence package version is: 22.04
    - the firmware-imx archive version is: 8.16
    
    The important version we want to align with the rest of other QorIQ
    components is indeed the BSP version "6.12.3-1.0.0".
    
    This commit also updates license hashes. The license was updated from:
    LA_OPT_NXP_Software_License v17 October 2020
    to:
    LA_OPT_NXP_Software_License v35 May 2022
    Also, the license file EULA.txt was removed, so _LICENSE_FILES is
    updated accordingly.
    
    [1] https://github.com/nxp-qoriq/yocto-sdk
    [2] https://github.com/Freescale/meta-freescale/blob/6fb8816cb32db4ba9391157036fa97914cc7d103/recipes-bsp/dp-firmware-cadence/dp-firmware-cadence_22.04.bb
    
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>
    [Julien:
     - reword commit log to add info on different version schemes
     - fix license hashes
    ]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/qoriq-cadence-dp-firmware/qoriq-cadence-dp-firmware.mk
+++ package/qoriq-cadence-dp-firmware/qoriq-cadence-dp-firmware.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-QORIQ_CADENCE_DP_FIRMWARE_VERSION = lsdk2012
-QORIQ_CADENCE_DP_FIRMWARE_SITE = http://www.nxp.com/lgfiles/sdk/$(QORIQ_CADENCE_DP_FIRMWARE_VERSION)
-QORIQ_CADENCE_DP_FIRMWARE_SOURCE = firmware-cadence-$(QORIQ_CADENCE_DP_FIRMWARE_VERSION).bin
+QORIQ_CADENCE_DP_FIRMWARE_VERSION = 8.16
+QORIQ_CADENCE_DP_FIRMWARE_SITE = https://www.nxp.com/lgfiles/NMG/MAD/YOCTO
+QORIQ_CADENCE_DP_FIRMWARE_SOURCE = firmware-imx-$(QORIQ_CADENCE_DP_FIRMWARE_VERSION).bin
 QORIQ_CADENCE_DP_FIRMWARE_LICENSE = NXP Semiconductor Software License Agreement
-QORIQ_CADENCE_DP_FIRMWARE_LICENSE_FILES = COPYING EULA EULA.txt
+QORIQ_CADENCE_DP_FIRMWARE_LICENSE_FILES = COPYING EULA
 QORIQ_CADENCE_DP_FIRMWARE_REDISTRIBUTE = NO
 QORIQ_CADENCE_DP_FIRMWARE_INSTALL_IMAGES = YES
 ifeq ($(BR2_LINUX_KERNEL_INSTALL_TARGET),)
@@ -20,11 +20,11 @@ define QORIQ_CADENCE_DP_FIRMWARE_EXTRACT_CMDS
 endef
 
 define QORIQ_CADENCE_DP_FIRMWARE_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0644 $(@D)/dp/ls1028a-dp-fw.bin $(TARGET_DIR)/boot/ls1028a-dp-fw.bin
+	$(INSTALL) -D -m 0644 $(@D)/firmware/hdmi/cadence/dp_ls1028a.bin $(TARGET_DIR)/boot/ls1028a-dp-fw.bin
 endef
 
 define QORIQ_CADENCE_DP_FIRMWARE_INSTALL_IMAGES_CMDS
-	$(INSTALL) -D -m 0644 $(@D)/dp/ls1028a-dp-fw.bin $(BINARIES_DIR)/ls1028a-dp-fw.bin
+	$(INSTALL) -D -m 0644 $(@D)/firmware/hdmi/cadence/dp_ls1028a.bin $(BINARIES_DIR)/ls1028a-dp-fw.bin
 endef
 
 $(eval $(generic-package))
