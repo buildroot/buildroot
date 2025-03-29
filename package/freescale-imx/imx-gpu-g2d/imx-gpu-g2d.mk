buildroot/buildroot

commit 44355f79980562177228c5f010683fa4cffe47b9

package/freescale-imx/imx-gpu-g2d: install the specific library for i.MX8MM
For the i.MX8MM, this package contains a specific library that is not
linked to OpenCL because there is no openCL support on i.MX8MM GPU.

Signed-off-by: Sébastien Szymanski <sebastien.szymanski@armadeus.com>
Signed-off-by: Julien Olivain <ju.o@free.fr>

package/freescale-imx/imx-gpu-g2d/imx-gpu-g2d.mk
@@ -22,12 +22,14 @@ define IMX_GPU_G2D_EXTRACT_CMDS
 	$(call NXP_EXTRACT_HELPER,$(IMX_GPU_G2D_DL_DIR)/$(IMX_GPU_G2D_SOURCE))
 endef
 
+IMX_GPU_G2D_SUBDIR = $(if $(BR2_PACKAGE_FREESCALE_IMX_PLATFORM_IMX8MM),mx8mm,)
+
 define IMX_GPU_G2D_INSTALL_STAGING_CMDS
-	cp -a $(@D)/g2d/usr/* $(STAGING_DIR)/usr
+	cp -a $(@D)/g2d/usr/lib/$(IMX_GPU_G2D_SUBDIR)/libg2d*  $(STAGING_DIR)/usr/lib/
 endef
 
 define IMX_GPU_G2D_INSTALL_TARGET_CMDS
-	cp -a $(@D)/g2d/usr/lib $(TARGET_DIR)/usr
+	cp -a $(@D)/g2d/usr/lib/$(IMX_GPU_G2D_SUBDIR)/libg2d*  $(TARGET_DIR)/usr/lib/
 endef
 
 $(eval $(generic-package))

commit f3ce9a56f92a07c1fe30ea6054923a2266dcb5b2

package/freescale-imx/imx-gpu-g2d: re-install headers to staging
Commit 44355f799805 ("package/freescale-imx/imx-gpu-g2d: install the
specific library for i.MX8MM") accidentally remove the headers'
installation to the staging directory. Fix that.

Fixes: 44355f799805 ("package/freescale-imx/imx-gpu-g2d: install the specific library for i.MX8MM")
Signed-off-by: Sébastien Szymanski <sebastien.szymanski@armadeus.com>
Signed-off-by: Julien Olivain <ju.o@free.fr>

package/freescale-imx/imx-gpu-g2d/imx-gpu-g2d.mk
@@ -26,6 +26,7 @@ endef
 IMX_GPU_G2D_SUBDIR = $(if $(BR2_PACKAGE_FREESCALE_IMX_PLATFORM_IMX8MM),mx8mm,)
 
 define IMX_GPU_G2D_INSTALL_STAGING_CMDS
+	cp -a $(@D)/g2d/usr/include/* $(STAGING_DIR)/usr/include/
 	cp -a $(@D)/g2d/usr/lib/$(IMX_GPU_G2D_SUBDIR)/libg2d*  $(STAGING_DIR)/usr/lib/
 endef
 