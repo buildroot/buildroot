5cab9ee22476373d45e0505dc21cafdd3c821212
Dario Binacchi <dario.binacchi@amarulasolutions.com>
2025-04-01 18:09:57 +0200

    package/ufs-utils: new package
    It's a tool to access UFS (Universal Flash Storage) devices from user
    space.
    
    Signed-off-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

---
+++ package/ufs-utils/ufs-utils.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# ufs-utils
+#
+################################################################################
+
+UFS_UTILS_VERSION = 4.13.5
+UFS_UTILS_SITE = $(call github,SanDisk-Open-Source,ufs-utils,v$(UFS_UTILS_VERSION))
+UFS_UTILS_LICENSE = GPL-2.0
+UFS_UTILS_LICENSE_FILES = COPYING
+
+define UFS_UTILS_BUILD_CMDS
+	$(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS)
+endef
+
+define UFS_UTILS_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/ufs-utils $(TARGET_DIR)/usr/bin/ufs-utils
+endef
+
+$(eval $(generic-package))
