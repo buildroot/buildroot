593771fafc17d001438fa946b6885422e94bd769
Leo Liang <ycliang@andestech.com>
2025-04-08 18:03:36 +0800

    package/kmon: bump to version 1.7.1
    No functional change, but bump for consistency.
    
    For details, see the release Notes:
    https://github.com/orhun/kmon/releases/tag/v1.7.1
    
    Signed-off-by: Leo Yu-Chi Liang <ycliang@andestech.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/kmon/kmon.mk
+++ package/kmon/kmon.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KMON_VERSION = 1.6.5
+KMON_VERSION = 1.7.1
 KMON_SITE = $(call github,orhun,kmon,v$(KMON_VERSION))
 KMON_LICENSE = GPL-3.0
 KMON_LICENSE_FILES = LICENSE
