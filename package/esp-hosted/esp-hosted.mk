e40db2ac5526574df66407236bf771755abe9f8a
Giulio Benetti <giulio.benetti@benettiengineering.com>
2025-04-08 21:48:08 +0200

    package/esp-hosted: bump to version 2025-04-08
    This version add support for esp32c5 and local patch has been committed so
    drop it.
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/esp-hosted/esp-hosted.mk
+++ package/esp-hosted/esp-hosted.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ESP_HOSTED_VERSION = 927a34f99724bb744efaf865a7c75cc1615f59a2
+ESP_HOSTED_VERSION = 9a2312b0b2630e41bacb3091faaba489cb43b0ab
 ESP_HOSTED_SITE = $(call github,espressif,esp-hosted,$(ESP_HOSTED_VERSION))
 ESP_HOSTED_DEPENDENCIES = linux
 ESP_HOSTED_LICENSE = GPL-2.0
