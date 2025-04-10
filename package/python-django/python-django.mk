f2b14baf2576b54cca43e6f2aebbb4a19fae3bd9
Thomas Perale <thomas.perale@mind.be>
2025-04-10 22:11:54 +0200

    package/python-django: security bump to version 5.1.8
    Fixes the following security issues:
    
    - CVE-2025-26699: Potential denial-of-service vulnerability in
        django.utils.text.wrap()
    
    For more information, see:
      - https://nvd.nist.gov/vuln/detail/CVE-2025-26699
      - https://github.com/django/django/commit/8dbb44d34271637099258391dfc79df33951b841
    
    - CVE-2025-27556: Potential denial-of-service vulnerability in
        LoginView, LogoutView, and set_language() on Windows
    
    This CVE isn't related to buildroot has it is only applicable on
    Windows but this package version bump fix it.
    
    For more information, see:
      - https://nvd.nist.gov/vuln/detail/CVE-2025-27556
      - https://github.com/django/django/commit/edc2716d01a6fdd84b173c02031695231bcee1f8
    
    For more details on the version bump, see the release notes:
      - https://docs.djangoproject.com/en/5.1/releases/5.1.8/
      - https://docs.djangoproject.com/en/5.1/releases/5.1.7/
      - https://docs.djangoproject.com/en/5.1/releases/5.1.6/
    
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 5.1.5
+PYTHON_DJANGO_VERSION = 5.1.8
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/e4/17/834e3e08d590dcc27d4cc3c5cd4e2fb757b7a92bab9de8ee402455732952
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/00/40/45adc1b93435d1b418654a734b68351bb6ce0a0e5e37b2f0e9aeb1a2e233
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
 PYTHON_DJANGO_CPE_ID_VENDOR = djangoproject
