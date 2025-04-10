c497e5fcc7b5d6b0877ba0dc557c604dcd194260
Thomas Perale <thomas.perale@mind.be>
2025-04-10 22:18:01 +0200

    package/augeas: add upstream security fix for CVE-2025-2588
    Fixes the following security issue:
    
    - CVE-2025-2588: This vulnerability affects the function
        re_case_expand of the file src/fa.c. The manipulation of the
        argument re leads to null pointer dereference
    
    For more information, see:
      - https://nvd.nist.gov/vuln/detail/CVE-2025-2588
      - https://github.com/hercules-team/augeas/commit/af2aa88ab37fc48167d8c5e43b1770a4ba2ff403
    
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>
    [Julien: add patch name in comment near _IGNORE_CVES]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/augeas/augeas.mk
+++ package/augeas/augeas.mk
@@ -14,6 +14,9 @@ AUGEAS_DEPENDENCIES = host-pkgconf readline libxml2
 
 AUGEAS_CONF_OPTS = --disable-gnulib-tests
 
+# 0001-CVE-2025-2588-return-_REG_ENOSYS-if-no-specific-error-was-set-yet-parse_regexp-failed.patch
+AUGEAS_IGNORE_CVES += CVE-2025-2588
+
 # Remove the test lenses which occupy about 1.4 MB on the target
 define AUGEAS_REMOVE_TEST_LENSES
 	rm -rf $(TARGET_DIR)/usr/share/augeas/lenses/dist/tests
