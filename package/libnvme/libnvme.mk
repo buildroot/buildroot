3cfc8e7c6ffe2fbdfd729eeca3d15725278805ff
Giulio Benetti <giulio.benetti@benettiengineering.com>
2025-04-13 23:06:40 +0200

    package/libnvme: bump to version 1.13
    For release note, see:
    https://github.com/linux-nvme/libnvme/releases/tag/v1.13
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libnvme/libnvme.mk
+++ package/libnvme/libnvme.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBNVME_VERSION = 1.12
+LIBNVME_VERSION = 1.13
 LIBNVME_SITE = $(call github,linux-nvme,libnvme,v$(LIBNVME_VERSION))
 LIBNVME_LICENSE = LGPL-2.1
 LIBNVME_LICENSE_FILES = COPYING
