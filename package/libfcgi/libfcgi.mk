f7e2b2c9daa0b1321bebfed33a0bd61d9df00034
Thomas Devoogdt <thomas.devoogdt@barco.com>
2025-04-14 11:22:13 +0200

    package/libfcgi: update to 2.4.4
    Announcement:
    - https://github.com/FastCGI-Archives/fcgi2/releases/tag/2.4.3
    - https://github.com/FastCGI-Archives/fcgi2/releases/tag/2.4.4
    
    Notes:
    - drop 0001-link-against-math.patch as it is upstream [1] (by @ffontaine)
    - drop 0002-disable-examples.patch as it is upstream [2] (by @ffontaine), add --disable-examples now
    - update LICENSE as the name has been changed [3]
    
    [1] https://github.com/FastCGI-Archives/fcgi2/commit/214a7bc4da57ccbb0e8840a2f595cd99701b19f6
    [2] https://github.com/FastCGI-Archives/fcgi2/commit/aa74aa7dcec5905de3bdee7f8f5a0a6d62c86cad
    [3] https://github.com/FastCGI-Archives/fcgi2/commit/65571daaa538e9193fab5935e3ae69ff123da19d
    
    Signed-off-by: Thomas Devoogdt <thomas.devoogdt@barco.com>
    [Julien: remove .checkpackageignore entries to fix check-package errors]
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 76b4fd159bcdba97eefdbc4299a7ae9b0a4cd423)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/libfcgi/libfcgi.mk
+++ package/libfcgi/libfcgi.mk
@@ -4,13 +4,14 @@
 #
 ################################################################################
 
-LIBFCGI_VERSION = 2.4.2
+LIBFCGI_VERSION = 2.4.4
 LIBFCGI_SITE = $(call github,FastCGI-Archives,fcgi2,$(LIBFCGI_VERSION))
 LIBFCGI_LICENSE = OML
-LIBFCGI_LICENSE_FILES = LICENSE.TERMS
+LIBFCGI_LICENSE_FILES = LICENSE
 LIBFCGI_CPE_ID_VENDOR = fastcgi
 LIBFCGI_CPE_ID_PRODUCT = fcgi
 LIBFCGI_INSTALL_STAGING = YES
 LIBFCGI_AUTORECONF = YES
+LIBFCGI_CONF_OPTS = --disable-examples
 
 $(eval $(autotools-package))
