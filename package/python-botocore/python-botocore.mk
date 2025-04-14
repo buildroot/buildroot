e1500712e32413dd694ca6e35dd445a27eaf06ff
Raphaël Mélotte <raphael.melotte@mind.be>
2025-04-14 21:44:59 +0200

    package/python-botocore: bump to version 1.37.33
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-botocore/python-botocore.mk
+++ package/python-botocore/python-botocore.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_BOTOCORE_VERSION = 1.36.3
+PYTHON_BOTOCORE_VERSION = 1.37.33
 PYTHON_BOTOCORE_SOURCE = botocore-$(PYTHON_BOTOCORE_VERSION).tar.gz
-PYTHON_BOTOCORE_SITE = https://files.pythonhosted.org/packages/3a/61/69eb06a803c83e0da733b60b2bc65880c18ef2dee19ee10cf8732794a3c1
+PYTHON_BOTOCORE_SITE = https://files.pythonhosted.org/packages/f4/1d/0c539ae261d2f8fe8b47c358b369ec58645bf0ea96b78825365e48675b67
 PYTHON_BOTOCORE_SETUP_TYPE = setuptools
 PYTHON_BOTOCORE_LICENSE = Apache-2.0
 PYTHON_BOTOCORE_LICENSE_FILES = LICENSE.txt tests/unit/auth/aws4_testsuite/LICENSE
