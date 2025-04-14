d39d9b53ec3a718b2b684739e1585352577b3f1f
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2025-04-14 14:45:53 +0200

    package/bmap-tools: bump to 3.9.0
    Upstream changelog:
    
    https://github.com/yoctoproject/bmaptool/releases/tag/v3.9.0
    
    Two important changes from a packaging point of view:
    
    - Switch to using hatch to build
    - Remove six from production code
    
    So we do adjust our packaging accordingly.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/bmap-tools/bmap-tools.mk
+++ package/bmap-tools/bmap-tools.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-BMAP_TOOLS_VERSION = 3.8.0
+BMAP_TOOLS_VERSION = 3.9.0
 BMAP_TOOLS_SITE = $(call github,yoctoproject,bmaptool,v$(BMAP_TOOLS_VERSION))
 BMAP_TOOLS_LICENSE = GPL-2.0
 BMAP_TOOLS_LICENSE_FILES = LICENSE
-BMAP_TOOLS_SETUP_TYPE = poetry
+BMAP_TOOLS_SETUP_TYPE = hatch
 
 $(eval $(python-package))
 $(eval $(host-python-package))
