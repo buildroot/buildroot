8fdbbefa08308921fe096f38fa71e63dafc5d6ac
Lance Fredrickson <lancethepants@gmail.com>
2025-04-15 14:15:27 -0600

    package/ipset: bump to version 7.23
    https://ipset.netfilter.org/changelog.html
    
    Signed-off-by: Lance Fredrickson <lancethepants@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/ipset/ipset.mk
+++ package/ipset/ipset.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-IPSET_VERSION = 7.16
+IPSET_VERSION = 7.23
 IPSET_SOURCE = ipset-$(IPSET_VERSION).tar.bz2
 IPSET_SITE = http://ipset.netfilter.org
 IPSET_DEPENDENCIES = libmnl host-pkgconf
