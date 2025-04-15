06c5a024f120335d0971be6520208369f0957a52
Yann E. MORIN <yann.morin.1998@free.fr>
2025-04-15 21:29:01 +0200

    package/vim: extend comment about vi symlink
    The current comment is not very helpful; extend it to be more explicit.
    
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/vim/vim.mk
+++ package/vim/vim.mk
@@ -63,7 +63,8 @@ define VIM_REMOVE_DOCS
 	$(RM) -rf $(TARGET_DIR)/usr/share/vim/vim*/doc/
 endef
 
-# Avoid oopses with vipw/vigr, lack of $EDITOR and 'vi' command expectation
+# Avoid oopses with vipw/vigr (from package shadow), lack of $EDITOR,
+# or other packages expecting plain 'vi' command to exist.
 ifeq ($(BR2_ROOTFS_MERGED_USR),y)
 define VIM_INSTALL_VI_SYMLINK
 	ln -sf vim $(TARGET_DIR)/usr/bin/vi
