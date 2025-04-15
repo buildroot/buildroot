7ffdf75311ab6eb0ed6018556bc0212966ea95eb
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2025-04-15 21:15:47 +0200

    package/double-conversion: bump to version 3.3.1
    Upstream changelog from
    https://github.com/google/double-conversion/releases/tag/v3.3.1:
    
        Hash pin Github workflows by @joycebrum in #198
        Create dependabot.yml by @joycebrum in #199
        Add _ITERATOR_DEBUG_LEVEL=2 and _DEBUG defines by @ffa-csturdy in #202
        Add bzlmod MODULE.bazel file by @jsharpe in #205
        Add CIFuzz Github Action by @DavidKorczynski in #203
        Update MODULE.bazel and README.md. by @BYVoid in #234
        Remove the explicit dependency on rules_cc. by @BYVoid in #235
        Add missing headers by @BhavikaSharma in #239
    
    Tested with the following configuration, which includes the only two
    reverse dependencies of double-conversion.
    
     BR2_arm=y
     BR2_cortex_a9=y
     BR2_ARM_ENABLE_VFP=y
     BR2_TOOLCHAIN_EXTERNAL=y
     BR2_TOOLCHAIN_EXTERNAL_BOOTLIN=y
     BR2_INIT_NONE=y
     BR2_SYSTEM_BIN_SH_NONE=y
     # BR2_PACKAGE_BUSYBOX is not set
     BR2_PACKAGE_QT6=y
     BR2_PACKAGE_PYTHON3=y
     BR2_PACKAGE_PYTHON_UJSON=y
     # BR2_TARGET_ROOTFS_TAR is not set
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/double-conversion/double-conversion.mk
+++ package/double-conversion/double-conversion.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DOUBLE_CONVERSION_VERSION = 3.3.0
+DOUBLE_CONVERSION_VERSION = 3.3.1
 DOUBLE_CONVERSION_SITE = $(call github,google,double-conversion,v$(DOUBLE_CONVERSION_VERSION))
 DOUBLE_CONVERSION_LICENSE = BSD-3-Clause
 DOUBLE_CONVERSION_LICENSE_FILES = COPYING
