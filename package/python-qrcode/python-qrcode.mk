83f865963a47b4706209997650f7dc053696b6a9
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2025-04-15 22:56:42 +0200

    package/python-qrcode: bump to 8.1
    Changes since 8.0:
    
    - Added support for Python 3.13
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    [Julien: fix _SITE url]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-qrcode/python-qrcode.mk
+++ package/python-qrcode/python-qrcode.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_QRCODE_VERSION = 8.0
+PYTHON_QRCODE_VERSION = 8.1
 PYTHON_QRCODE_SOURCE = qrcode-$(PYTHON_QRCODE_VERSION).tar.gz
-PYTHON_QRCODE_SITE = https://files.pythonhosted.org/packages/d7/db/6fc9631cac1327f609d2c8ae3680ecd987a2e97472437f2de7ead1235156
+PYTHON_QRCODE_SITE = https://files.pythonhosted.org/packages/61/d4/d222d00f65c81945b55e8f64011c33cb11a2931957ba3e2845fb0874fffe
 PYTHON_QRCODE_SETUP_TYPE = poetry
 PYTHON_QRCODE_LICENSE = BSD-3-Clause
 PYTHON_QRCODE_LICENSE_FILES = LICENSE
