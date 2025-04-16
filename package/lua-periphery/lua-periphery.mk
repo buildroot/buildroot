28851289475c4475ab2de9abcf062095e42731b8
Francois Perrad <francois.perrad@gadz.org>
2025-04-16 08:37:32 +0200

    package/lua-periphery: bump to version 2.4.3
    diff LICENSE:
        - Copyright (c) 2014-2023 vsergeev / Ivan (Vanya) A. Sergeev
        + Copyright (c) 2014-2025 vsergeev / Ivan (Vanya) A. Sergeev
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/lua-periphery/lua-periphery.mk
+++ package/lua-periphery/lua-periphery.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LUA_PERIPHERY_VERSION = 2.4.2-1
+LUA_PERIPHERY_VERSION = 2.4.3-1
 LUA_PERIPHERY_SUBDIR = lua-periphery
 LUA_PERIPHERY_LICENSE = MIT
 LUA_PERIPHERY_LICENSE_FILES = $(LUA_PERIPHERY_SUBDIR)/LICENSE
