5ef9d07760001c9f4e6e97f2fff096026a08ef95
TIAN Yuanhao <tianyuanhao3@163.com>
2025-04-17 04:22:43 -0700

    package/tailscale: install systemd service files
    Signed-off-by: TIAN Yuanhao <tianyuanhao3@163.com>
    Cc: James Hilliard <james.hilliard1@gmail.com>
    Cc: Yann E. MORIN <yann.morin@orange.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit f027e7c5b3b84ab920f0ded6f3e2f298b7ad4f55)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/tailscale/tailscale.mk
+++ package/tailscale/tailscale.mk
@@ -15,6 +15,18 @@ TAILSCALE_LDFLAGS = \
 	-X tailscale.com/version.longStamp=$(TAILSCALE_VERSION) \
 	-X tailscale.com/version.shortStamp=$(TAILSCALE_VERSION)
 
+define TAILSCALE_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 0644 $(@D)/cmd/tailscaled/tailscaled.defaults \
+		$(TARGET_DIR)/etc/default/tailscaled
+	$(INSTALL) -D -m 0644 $(@D)/cmd/tailscaled/tailscaled.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/tailscaled.service
+endef
+
+define TAILSCALE_INSTALL_SYMLINK
+	ln -f -s ../bin/tailscaled $(TARGET_DIR)/usr/sbin/tailscaled
+endef
+TAILSCALE_POST_INSTALL_TARGET_HOOKS += TAILSCALE_INSTALL_SYMLINK
+
 define TAILSCALE_LINUX_CONFIG_FIXUPS
 	$(call KCONFIG_ENABLE_OPT,CONFIG_TUN)
 endef
