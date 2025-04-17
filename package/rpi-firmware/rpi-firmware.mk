5926b3b3ae0d10005d7eb984c7575e0a8ababb72
Gaël PORTAY <gael.portay+rtone@gmail.com>
2025-04-17 14:41:24 +0200

    package/rpi-firmware: add note to keep in sync with kernel
    The bump of packages rpi-firmware and linux have to by synced, since the
    linux package does not install yet its device-tree overlay blobs.
    
    This adds a note to remind to keep in sync the versions of rpi-firmware
    package and kernel (set in the defconfigs).
    
    Signed-off-by: Gaël PORTAY <gael.portay+rtone@gmail.com>
    [Julien: rebase patch to resolve context conflict with commit 80ccb3e667]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,6 +4,7 @@
 #
 ################################################################################
 
+# Please keep in sync with configs/raspberrypi*_deconfig
 RPI_FIRMWARE_VERSION = 1.20250326
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
