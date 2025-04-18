e984d89e1980b0bfc8d4fc0a7a3584ba5120bd93
Daniel Lang <dalang@gmx.at>
2025-04-18 16:37:28 +0200

    package/dbus-cxx: bump to version 2.5.2
    https://github.com/dbus-cxx/dbus-cxx/releases/tag/2.5.2
    
    Signed-off-by: Daniel Lang <dalang@gmx.at>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/dbus-cxx/dbus-cxx.mk
+++ package/dbus-cxx/dbus-cxx.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DBUS_CXX_VERSION = 2.4.0
+DBUS_CXX_VERSION = 2.5.2
 DBUS_CXX_SITE = $(call github,dbus-cxx,dbus-cxx,$(DBUS_CXX_VERSION))
 DBUS_CXX_LICENSE = LGPL-3.0+ or BSD-3-Clause, Boost license (cmake-modules), Apache 2.0 (libcppgenerate)
 DBUS_CXX_LICENSE_FILES = COPYING cmake-modules/LICENSE_1_0.txt tools/libcppgenerate/LICENSE
@@ -23,6 +23,13 @@ DBUS_CXX_CONF_OPTS += -DENABLE_GLIB_SUPPORT=ON
 DBUS_CXX_DEPENDENCIES += libglib2
 else
 DBUS_CXX_CONF_OPTS += -DENABLE_GLIB_SUPPORT=OFF
+endif
+
+ifeq ($(BR2_PACKAGE_LIBUV),y)
+DBUS_CXX_CONF_OPTS += -DENABLE_UV_SUPPORT=ON
+DBUS_CXX_DEPENDENCIES += libuv
+else
+DBUS_CXX_CONF_OPTS += -DENABLE_UV_SUPPORT=OFF
 endif
 
 $(eval $(cmake-package))
