83140189de30fb4d2a2941bb0b0ab30381882a2a
El Mehdi YOUNES <elmehdi.younes@smile.fr>
2025-04-22 10:53:36 +0200

    package/bat:bump to version 0.25.0
    Changelog:
    https://github.com/sharkdp/bat/blob/v0.25.0/CHANGELOG.md
    
    Signed-off-by: El Mehdi YOUNES <elmehdi.younes@smile.fr>
    [Julien: change changelog url to use tag]
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit e7a00fd93a09bbb1adb91e03e1e9ca3a8acec600)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/bat/bat.mk
+++ package/bat/bat.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BAT_VERSION = 0.24.0
+BAT_VERSION = 0.25.0
 BAT_SITE = $(call github,sharkdp,bat,v$(BAT_VERSION))
 BAT_LICENSE = Apache-2.0 or MIT
 BAT_LICENSE_FILES = LICENSE-APACHE LICENSE-MIT
