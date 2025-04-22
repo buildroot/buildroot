631ab85437d2a3a7291eae687da2f5c5af21caa4
Adam Duskett <adam.duskett@amarulasolutions.com>
2025-04-22 12:34:24 +0200

    package/mender-artifact: bump to version 4.1.0
    Changelog:
    https://docs.mender.io/release-information/release-notes-changelog/mender-artifact
    
    License changes:
    New:
        Apache 2.0:
            LICENSE: 2024 -> 2025
            vendor/cloud.google.com/go/auth/LICENSE
            vendor/cloud.google.com/go/auth/oauth2adapt/LICENSE
            vendor/github.com/kylelemons/godebug/LICENSE
            vendor/go.opentelemetry.io/otel/trace/LICENSE
            vendor/go.opentelemetry.io/otel/LICENSE
            vendor/go.opentelemetry.io/otel/metric/LICENSE
            vendor/go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp/LICENSE
            vendor/go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc/LICENSE
            vendor/go.opentelemetry.io/auto/sdk/LICENSE
            vendor/github.com/go-logr/logr/LICENSE
            vendor/github.com/go-logr/stdr/LICENSE
    
        BSD 2 clause:
            vendor/github.com/pkg/browser/LICENSE
    
        BSD 3 clause:
            vendor/golang.org/x/sync/LICENSE
            vendor/github.com/google/uuid/LICENSE
    
        ISC:
            vendor/github.com/decred/dcrd/dcrec/secp256k1/v4/LICENSE
    
        MIT:
            vendor/github.com/Azure/azure-sdk-for-go/sdk/internal/LICENSE.txt
            vendor/github.com/Azure/azure-sdk-for-go/sdk/azidentity/LICENSE.txt
            vendor/github.com/Azure/azure-sdk-for-go/sdk/security/keyvault/internal/LICENSE.txt
            vendor/github.com/Azure/azure-sdk-for-go/sdk/security/keyvault/azkeys/LICENSE.txt
            vendor/github.com/Azure/azure-sdk-for-go/sdk/azcore/LICENSE.txt
            vendor/github.com/AzureAD/microsoft-authentication-library-for-go/LICENSE
            vendor/github.com/goccy/go-json/LICENSE
            vendor/github.com/golang-jwt/jwt/v5/LICENSE
            vendor/github.com/lestrrat-go/iter/LICENSE
            vendor/github.com/lestrrat-go/blackmagic/LICENSE
            vendor/github.com/lestrrat-go/backoff/v2/LICENSE
            vendor/github.com/lestrrat-go/httpcc/LICENSE
            vendor/github.com/lestrrat-go/option/LICENSE
            vendor/github.com/lestrrat-go/jwx/LICENSE
    
        MPL-2.0:
            vendor/github.com/felixge/httpsnoop/LICENSE.txt
    
    Modified:
        Apache 2.0:
            vendor/github.com/mendersoftware/progressbar/LICENSE: 2021 -> 2025
    
        BSD 3:
            vendor/golang.org/x/net/LICENSE: Google INC -> Google LLC
            vendor/golang.org/x/oauth2/LICENSE: Google INC -> Google LLC
            vendor/golang.org/x/text/LICENSE: Google INC -> Google LLC
            vendor/golang.org/x/crypto/LICENSE: Google INC -> Google LLC
            vendor/golang.org/x/time/LICENSE: Google INC -> Google LLC
    
    Removed:
    
        Apache 2.0:
            vendor/google.golang.org/appengine/LICENSE
            vendor/cloud.google.com/go/compute/LICENSE
            vendor/go.opencensus.io/LICENSE
            vendor/github.com/golang/groupcache/LICENSE
    
        BSD 3 Clause:
            vendor/github.com/golang/protobuf/LICENSE
            vendor/golang.org/x/crypto/LICENSE
            vendor/golang.org/x/time/LICENSE
    
    Tested with ./support/testing/run-tests tests.package.test_mender
    08:48:07 TestMenderSystemd                        Starting
    08:48:35 TestMenderSystemd                        Cleaning up
    08:48:35 TestMenderRW                             Starting
    08:48:46 TestMenderRW                             Cleaning up
    08:48:46 TestMenderRO                             Starting
    08:48:46 TestMenderRO                             Building
    08:55:02 TestMenderRO                             Building done
    08:55:13 TestMenderRO                             Cleaning up
    ----------------------------------------------------------------------
    Ran 3 tests in 425.930s
    OK
    
    Signed-off-by: Adam Duskett <adam.duskett@amarulasolutions.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/mender-artifact/mender-artifact.mk
+++ package/mender-artifact/mender-artifact.mk
@@ -4,9 +4,11 @@
 #
 ################################################################################
 
-HOST_MENDER_ARTIFACT_VERSION = 4.0.0
+HOST_MENDER_ARTIFACT_VERSION = 4.1.0
 HOST_MENDER_ARTIFACT_SITE = $(call github,mendersoftware,mender-artifact,$(HOST_MENDER_ARTIFACT_VERSION))
 HOST_MENDER_ARTIFACT_LICENSE = Apache2.0, BSD-2-Clause, BSD-3-Clause, ISC, MIT, MPL-2.0
+# Vendor license paths generated with:
+#    awk '{print $2}' LIC_FILES_CHKSUM.sha256 | grep vendor
 HOST_MENDER_ARTIFACT_LICENSE_FILES = \
 	LICENSE \
 	LIC_FILES_CHKSUM.sha256 \
@@ -16,40 +18,47 @@ HOST_MENDER_ARTIFACT_LICENSE_FILES = \
 	vendor/google.golang.org/genproto/googleapis/api/LICENSE \
 	vendor/google.golang.org/genproto/googleapis/rpc/LICENSE \
 	vendor/google.golang.org/grpc/LICENSE \
-	vendor/google.golang.org/appengine/LICENSE \
 	vendor/cloud.google.com/go/kms/LICENSE \
 	vendor/cloud.google.com/go/iam/LICENSE \
-	vendor/cloud.google.com/go/compute/LICENSE \
 	vendor/cloud.google.com/go/compute/metadata/LICENSE \
-	vendor/go.opencensus.io/LICENSE \
-	vendor/github.com/golang/groupcache/LICENSE \
+	vendor/cloud.google.com/go/auth/LICENSE \
+	vendor/cloud.google.com/go/auth/oauth2adapt/LICENSE \
 	vendor/github.com/mendersoftware/openssl/LICENSE \
 	vendor/github.com/googleapis/enterprise-certificate-proxy/LICENSE \
 	vendor/github.com/go-jose/go-jose/v3/json/LICENSE \
 	vendor/github.com/go-jose/go-jose/v3/LICENSE \
 	vendor/github.com/google/s2a-go/LICENSE.md \
 	vendor/github.com/Keyfactor/signserver-go-client-sdk/LICENSE \
+	vendor/github.com/kylelemons/godebug/LICENSE \
+	vendor/go.opentelemetry.io/otel/trace/LICENSE \
+	vendor/go.opentelemetry.io/otel/LICENSE \
+	vendor/go.opentelemetry.io/otel/metric/LICENSE \
+	vendor/go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp/LICENSE \
+	vendor/go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc/LICENSE \
+	vendor/go.opentelemetry.io/auto/sdk/LICENSE \
+	vendor/github.com/go-logr/logr/LICENSE \
+	vendor/github.com/go-logr/stdr/LICENSE \
 	vendor/github.com/pkg/errors/LICENSE \
+	vendor/github.com/pkg/browser/LICENSE \
 	vendor/github.com/pmezard/go-difflib/LICENSE \
 	vendor/golang.org/x/sys/LICENSE \
 	vendor/github.com/klauspost/compress/LICENSE \
 	vendor/github.com/russross/blackfriday/v2/LICENSE.txt \
-	vendor/github.com/google/go-cmp/LICENSE \
 	vendor/golang.org/x/net/LICENSE \
 	vendor/golang.org/x/oauth2/LICENSE \
 	vendor/golang.org/x/text/LICENSE \
 	vendor/golang.org/x/crypto/LICENSE \
 	vendor/golang.org/x/time/LICENSE \
+	vendor/golang.org/x/sync/LICENSE \
 	vendor/google.golang.org/api/LICENSE \
 	vendor/google.golang.org/api/internal/third_party/uritemplates/LICENSE \
 	vendor/google.golang.org/protobuf/LICENSE \
-	vendor/github.com/golang/protobuf/LICENSE \
 	vendor/github.com/googleapis/gax-go/v2/LICENSE \
-	vendor/golang.org/x/crypto/LICENSE \
-	vendor/golang.org/x/time/LICENSE \
 	vendor/github.com/klauspost/compress/internal/snapref/LICENSE \
 	vendor/github.com/ulikunitz/xz/LICENSE \
+	vendor/github.com/google/uuid/LICENSE \
 	vendor/github.com/davecgh/go-spew/LICENSE \
+	vendor/github.com/decred/dcrd/dcrec/secp256k1/v4/LICENSE \
 	vendor/github.com/stretchr/testify/LICENSE \
 	vendor/github.com/urfave/cli/LICENSE \
 	vendor/github.com/sirupsen/logrus/LICENSE \
@@ -63,6 +72,20 @@ HOST_MENDER_ARTIFACT_LICENSE_FILES = \
 	vendor/github.com/ryanuber/go-glob/LICENSE \
 	vendor/github.com/cenkalti/backoff/v3/LICENSE \
 	vendor/github.com/klauspost/compress/zstd/internal/xxhash/LICENSE.txt \
+	vendor/github.com/Azure/azure-sdk-for-go/sdk/internal/LICENSE.txt \
+	vendor/github.com/Azure/azure-sdk-for-go/sdk/azidentity/LICENSE.txt \
+	vendor/github.com/Azure/azure-sdk-for-go/sdk/security/keyvault/internal/LICENSE.txt \
+	vendor/github.com/Azure/azure-sdk-for-go/sdk/security/keyvault/azkeys/LICENSE.txt \
+	vendor/github.com/Azure/azure-sdk-for-go/sdk/azcore/LICENSE.txt \
+	vendor/github.com/AzureAD/microsoft-authentication-library-for-go/LICENSE \
+	vendor/github.com/goccy/go-json/LICENSE \
+	vendor/github.com/golang-jwt/jwt/v5/LICENSE \
+	vendor/github.com/lestrrat-go/iter/LICENSE \
+	vendor/github.com/lestrrat-go/blackmagic/LICENSE \
+	vendor/github.com/lestrrat-go/backoff/v2/LICENSE \
+	vendor/github.com/lestrrat-go/httpcc/LICENSE \
+	vendor/github.com/lestrrat-go/option/LICENSE \
+	vendor/github.com/lestrrat-go/jwx/LICENSE \
 	vendor/github.com/hashicorp/go-secure-stdlib/strutil/LICENSE \
 	vendor/github.com/hashicorp/go-secure-stdlib/parseutil/LICENSE \
 	vendor/github.com/hashicorp/errwrap/LICENSE \
@@ -72,7 +95,8 @@ HOST_MENDER_ARTIFACT_LICENSE_FILES = \
 	vendor/github.com/hashicorp/go-retryablehttp/LICENSE \
 	vendor/github.com/hashicorp/go-sockaddr/LICENSE \
 	vendor/github.com/hashicorp/vault/api/LICENSE \
-	vendor/github.com/hashicorp/go-multierror/LICENSE
+	vendor/github.com/hashicorp/go-multierror/LICENSE \
+	vendor/github.com/felixge/httpsnoop/LICENSE.txt
 
 HOST_MENDER_ARTIFACT_DEPENDENCIES = host-pkgconf host-openssl host-xz
 
