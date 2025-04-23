a41ebd6ca1129b9945da8d5ae990b79a13f8e49d
Giulio Benetti <giulio.benetti@benettiengineering.com>
2025-04-23 16:22:03 +0200

    package/harfbuzz: bump to version 11.1.0
    Release notes:
    https://github.com/harfbuzz/harfbuzz/releases/tag/11.1.0
    
    Since the major release changed all the packages that have direct
    dependency to harfbuzz have been successfully built:
    - efl
    - libass
    - mupdf
    - pango
    - qt5base
    - qt5webengine
    - qt6base
    - sdl2_ttf
    - supertuxkart
    - vlc
    - webkitgtk
    - wpewebkit
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/harfbuzz/harfbuzz.mk
+++ package/harfbuzz/harfbuzz.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HARFBUZZ_VERSION = 10.4.0
+HARFBUZZ_VERSION = 11.1.0
 HARFBUZZ_SITE = https://github.com/harfbuzz/harfbuzz/releases/download/$(HARFBUZZ_VERSION)
 HARFBUZZ_SOURCE = harfbuzz-$(HARFBUZZ_VERSION).tar.xz
 HARFBUZZ_LICENSE = MIT, ISC (ucdn library)
