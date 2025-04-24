3672945a8da3d5f464448f9c5abdb6dba09085c5
Vincent Fazio <vfazio@xes-inc.com>
2025-04-24 13:17:06 -0500

    package/python-setuptools: bump to 79.0.1
    This version of setuptools includes an update to distutils [0] which
    fixes an issue with determining the linker command for C++ targets when
    a wrapper, such as ccache, is in the command.
    
    Fixes: https://autobuild.buildroot.org/results/5f017fca3d708388c9f66afea39b090f0976d496/
    
    [0]: https://github.com/pypa/distutils/commit/b8c06fffe4b118f3c549fc23d196c2f3d41aa17c
    
    Signed-off-by: Vincent Fazio <vfazio@xes-inc.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-setuptools/python-setuptools.mk
+++ package/python-setuptools/python-setuptools.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SETUPTOOLS_VERSION = 76.0.0
+PYTHON_SETUPTOOLS_VERSION = 79.0.1
 PYTHON_SETUPTOOLS_SOURCE = setuptools-$(PYTHON_SETUPTOOLS_VERSION).tar.gz
-PYTHON_SETUPTOOLS_SITE = https://files.pythonhosted.org/packages/32/d2/7b171caf085ba0d40d8391f54e1c75a1cda9255f542becf84575cfd8a732
+PYTHON_SETUPTOOLS_SITE = https://files.pythonhosted.org/packages/bb/71/b6365e6325b3290e14957b2c3a804a529968c77a049b2ed40c095f749707
 PYTHON_SETUPTOOLS_LICENSE = MIT
 PYTHON_SETUPTOOLS_LICENSE_FILES = LICENSE
 PYTHON_SETUPTOOLS_CPE_ID_VENDOR = python
