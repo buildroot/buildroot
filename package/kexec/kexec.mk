00b2d74dbb73790578b271cac91cb3bd9082487b
Julien Olivain <ju.o@free.fr>
2025-04-25 22:16:55 +0200

    package/kexec: bump to version 2.0.31
    For release announce, see:
    https://lists.infradead.org/pipermail/kexec/2025-April/032608.html
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/kexec/kexec.mk
+++ package/kexec/kexec.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KEXEC_VERSION = 2.0.30
+KEXEC_VERSION = 2.0.31
 KEXEC_SOURCE = kexec-tools-$(KEXEC_VERSION).tar.xz
 KEXEC_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/kernel/kexec
 KEXEC_LICENSE = GPL-2.0
