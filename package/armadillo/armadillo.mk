609ea9789f1b714c182424a3f18aa402c9766e80
Dario Binacchi <dario.binacchi@amarulasolutions.com>
2025-04-28 14:20:05 +0200

    package/armadillo: bump to version 14.4.2
    Release notes:
    https://arma.sourceforge.net/docs.html#changelog
    
    Signed-off-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/armadillo/armadillo.mk
+++ package/armadillo/armadillo.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ARMADILLO_VERSION = 14.4.1
+ARMADILLO_VERSION = 14.4.2
 ARMADILLO_SOURCE = armadillo-$(ARMADILLO_VERSION).tar.xz
 ARMADILLO_SITE = https://downloads.sourceforge.net/project/arma
 ARMADILLO_INSTALL_STAGING = YES
