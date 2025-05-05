8cdeeb536cae5fa4fb0271b5b8fb5571ff468c0a
Yegor Yefremov <yegorslists@googlemail.com>
2025-05-05 15:49:29 +0200

    package/libical: bump to version 3.0.20
    For change log, see:
    https://github.com/libical/libical/blob/v3.0.20/ReleaseNotes.txt
    
    Update license info: the source code is now licensed under MPL-2.0
    instead of MPL-1.0.
    
    Remove upstreamed patches.
    
    Add a build-time dependency to host-perl.
    
    Disable unneeded options.
    
    Use ON/OFF instead of true/false for CMake options.
    
    Fixes:
    https://autobuild.buildroot.org/results/dc2a6d9e50759e948a5f9952eaa1a866d52400fe/
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    [Julien:
     - reword commit title
     - update change log url to use tag
     - update .checkpackageignore to fix check-package errors
     - reorder LIBICAL_CONF_OPTS alphabetically
    ]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libical/libical.mk
+++ package/libical/libical.mk
@@ -4,20 +4,25 @@
 #
 ################################################################################
 
-LIBICAL_VERSION = 1.0.1
+LIBICAL_VERSION = 3.0.20
 LIBICAL_SITE = https://github.com/libical/libical/releases/download/v$(LIBICAL_VERSION)
 LIBICAL_INSTALL_STAGING = YES
-LIBICAL_LICENSE = MPL-1.0 or LGPL-2.1
-LIBICAL_LICENSE_FILES = LICENSE
+LIBICAL_LICENSE = MPL-2.0 or LGPL-2.1
+LIBICAL_LICENSE_FILES = LICENSE.MPL2.txt LICENSE.LGPL21.txt
 LIBICAL_CPE_ID_VALID = YES
+LIBICAL_DEPENDENCIES = host-perl
 
-# 0002-icaltypes-c-icalreqstattype_from_string-copy-the-reqstattype.patch
-LIBICAL_IGNORE_CVES += CVE-2016-9584
-
+LIBICAL_CONF_OPTS = \
+	-DGOBJECT_INTROSPECTION=OFF \
+	-DICAL_BUILD_DOCS=OFF \
+	-DICAL_GLIB=OFF \
+	-DLIBICAL_BUILD_EXAMPLES=OFF \
+	-DLIBICAL_BUILD_TESTING=OFF \
+	-DWITH_CXX_BINDINGS=OFF
 # building without this option is broken, it is used by
 # Gentoo/alpinelinux as well
-LIBICAL_CONF_OPTS = -DSHARED_ONLY=true
+LIBICAL_CONF_OPTS += -DSHARED_ONLY=ON
 # never build time zone info, always use system's tzinfo
-LIBICAL_CONF_OPTS += -DUSE_BUILTIN_TZDATA=false
+LIBICAL_CONF_OPTS += -DUSE_BUILTIN_TZDATA=OFF
 
 $(eval $(cmake-package))
