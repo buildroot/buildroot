7e2b4f4d4170beb41f58dc4fdad3602c0d97bb9e
Julien Olivain <ju.o@free.fr>
2025-05-05 21:08:32 +0200

    package/gmp: add patch to fix build with gcc-15
    When using host gcc 15, host-gmp fails at configure time with
    error message:
    
        configure: error: could not find a working compiler, see config.log for details
    
    The error is due to the macro GMP_PROG_CC_WORKS in the file
    acinclude.m4 containing C-code declaring functions without declaring
    its parameters. This construct is now an error in C23, which is the
    new default in gcc 15. See:
    https://gcc.gnu.org/gcc-15/porting_to.html#c23
    
    This commit fixes the issue by adding a package patch from
    upstream changesets, not yet published in a release.
    
    Even if gcc 15 is not yet included in Buildroot, this commit also
    sets _AUTORECONF = YES for the target package in order to have this
    package already fixed.
    
    Fixes:
    https://autobuild.buildroot.org/results/623634fa7bbeceeb6d90b15ce0abb1b9b4b24045/
    
    Tested-by: Brigham Campbell <me@brighamcampbell.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 31569bcc1b0762636d704baad0b35fc37aedb386)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/gmp/gmp.mk
+++ package/gmp/gmp.mk
@@ -14,6 +14,10 @@ GMP_CPE_ID_VENDOR = gmplib
 GMP_DEPENDENCIES = host-m4
 HOST_GMP_DEPENDENCIES = host-m4
 
+# 0001-Complete-function-prototype-in-acinclude.m4-for-C23-.patch
+GMP_AUTORECONF = YES
+HOST_GMP_AUTORECONF = YES
+
 GMP_CONF_ENV += CC_FOR_BUILD="$(HOSTCC) -std=c99"
 
 # GMP doesn't support assembly for coldfire or mips r6 ISA yet
