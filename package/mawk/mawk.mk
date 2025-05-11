beefa78b4998c66b41f0d7b62bf693297970e45d
Ismael Luceno <ismael@iodev.co.uk>
2025-05-11 04:59:19 +0200

    package/mawk: bump version to 1.3.4-20250131
    For change log, see:
    https://invisible-mirror.net/mawk/CHANGES.html#index-t20250131
    
    Signed-off-by: Ismael Luceno <ismael@iodev.co.uk>
    [Julien: add link to change log]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/mawk/mawk.mk
+++ package/mawk/mawk.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MAWK_VERSION = 1.3.4-20240905
+MAWK_VERSION = 1.3.4-20250131
 MAWK_SITE = https://invisible-mirror.net/archives/mawk
 MAWK_SOURCE = mawk-$(MAWK_VERSION).tgz
 MAWK_LICENSE = GPL-2.0
