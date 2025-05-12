5d8d39fc4c4891adee2587ff1dd6627bacbbe75c
Daniel Crowe <daniel.crowe@maxmine.com.au>
2025-05-12 13:53:29 +0930

    package/python-aniso8601: bump version to 10.0.1
    For change log, see:
    https://codeberg.org/nielsenb-jf/aniso8601/src/tag/v10.0.1/CHANGELOG.rst#aniso8601-10.0.1
    
    Signed-off-by: Daniel Crowe <daniel.crowe@maxmine.com.au>
    [Julien: add link to change log]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-aniso8601/python-aniso8601.mk
+++ package/python-aniso8601/python-aniso8601.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ANISO8601_VERSION = 10.0.0
+PYTHON_ANISO8601_VERSION = 10.0.1
 PYTHON_ANISO8601_SOURCE = aniso8601-$(PYTHON_ANISO8601_VERSION).tar.gz
-PYTHON_ANISO8601_SITE = https://files.pythonhosted.org/packages/f3/3f/dc8a28fa6dc72c13d8c158b01f8975f240e9e72c336cc1ae00f424e2d7ce
+PYTHON_ANISO8601_SITE = https://files.pythonhosted.org/packages/8b/8d/52179c4e3f1978d3d9a285f98c706642522750ef343e9738286130423730
 PYTHON_ANISO8601_SETUP_TYPE = setuptools
 PYTHON_ANISO8601_LICENSE = BSD-3-Clause
 PYTHON_ANISO8601_LICENSE_FILES = LICENSE
