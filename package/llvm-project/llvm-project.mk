094cede2001ab015fdfdc1354a672c2b26837bbe
James Hilliard <james.hilliard1@gmail.com>
2025-05-14 12:48:24 -0600

    package/llvm-project: bump to version 15.0.7
    For release notes since 15.0.3, see:
    https://discourse.llvm.org/t/llvm-15-0-4-released/66337
    https://discourse.llvm.org/t/llvm-15-0-5-release/66616
    https://discourse.llvm.org/t/llvm-15-0-6-released/66899
    https://discourse.llvm.org/t/llvm-15-0-7-release/67638
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    [Julien: add links to release notes]
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 9aa966863f382e7eddd8101b42599cdd585f9ce9)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/llvm-project/llvm-project.mk
+++ package/llvm-project/llvm-project.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LLVM_PROJECT_VERSION_MAJOR = 15
-LLVM_PROJECT_VERSION = $(LLVM_PROJECT_VERSION_MAJOR).0.3
+LLVM_PROJECT_VERSION = $(LLVM_PROJECT_VERSION_MAJOR).0.7
 LLVM_PROJECT_SITE = https://github.com/llvm/llvm-project/releases/download/llvmorg-$(LLVM_PROJECT_VERSION)
 
 include $(sort $(wildcard package/llvm-project/*/*.mk))
