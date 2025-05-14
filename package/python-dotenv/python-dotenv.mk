1126be8fdfe741faf48bd300c243439d46d9d71c
Marcus Hoffmann <buildroot@bubu1.eu>
2025-05-14 17:12:42 +0200

    package/python-dotenv: bump to 1.1.0
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Arnout Vandecappelle <arnout@rnout.be>

--- package/python-dotenv/python-dotenv.mk
+++ package/python-dotenv/python-dotenv.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-PYTHON_DOTENV_VERSION = 1.0.1
-PYTHON_DOTENV_SITE = https://files.pythonhosted.org/packages/bc/57/e84d88dfe0aec03b7a2d4327012c1627ab5f03652216c63d49846d7a6c58
+PYTHON_DOTENV_VERSION = 1.1.0
+PYTHON_DOTENV_SOURCE = python_dotenv-$(PYTHON_DOTENV_VERSION).tar.gz
+PYTHON_DOTENV_SITE = https://files.pythonhosted.org/packages/88/2c/7bb1416c5620485aa793f2de31d3df393d3686aa8a8506d11e10e13c5baf
 PYTHON_DOTENV_SETUP_TYPE = setuptools
 PYTHON_DOTENV_LICENSE = BSD-3-Clause
 PYTHON_DOTENV_LICENSE_FILES = LICENSE
