09299c5b0cf4841e20ee8f8196a4122890e6530f
Marcus Hoffmann <buildroot@bubu1.eu>
2025-05-14 11:46:14 +0200

    package/python-django: security bump to 5.1.9
    Fixes CVE-2025-32873 [1].
    Django also updates setuptools[2], so we can remove the --skip-dependency-check
    flag and need to update the package archive capitalization accordingly.
    
    [1] https://www.djangoproject.com/weblog/2025/may/07/security-releases/
    [2] https://github.com/django/django/commit/bbf376bbc8cdfca817b54e7df99539633d10b06e
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,15 +4,14 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 5.1.8
-PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
+PYTHON_DJANGO_VERSION = 5.1.9
+PYTHON_DJANGO_SOURCE = django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/00/40/45adc1b93435d1b418654a734b68351bb6ce0a0e5e37b2f0e9aeb1a2e233
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/10/08/2e6f05494b3fc0a3c53736846034f882b82ee6351791a7815bbb45715d79
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
 PYTHON_DJANGO_CPE_ID_VENDOR = djangoproject
 PYTHON_DJANGO_CPE_ID_PRODUCT = django
 PYTHON_DJANGO_SETUP_TYPE = setuptools
-PYTHON_DJANGO_BUILD_OPTS = --skip-dependency-check
 
 $(eval $(python-package))
