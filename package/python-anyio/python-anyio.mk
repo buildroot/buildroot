0468aae90a0124b79febf8705d9779e7a8a2ce4c
Marcus Hoffmann <buildroot@bubu1.eu>
2025-05-14 17:12:37 +0200

    package/python-anyio: bump to 4.9.0
    Release notes: https://github.com/agronholm/anyio/releases/tag/4.9.0
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Arnout Vandecappelle <arnout@rnout.be>

--- package/python-anyio/python-anyio.mk
+++ package/python-anyio/python-anyio.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ANYIO_VERSION = 4.8.0
+PYTHON_ANYIO_VERSION = 4.9.0
 PYTHON_ANYIO_SOURCE = anyio-$(PYTHON_ANYIO_VERSION).tar.gz
-PYTHON_ANYIO_SITE = https://files.pythonhosted.org/packages/a3/73/199a98fc2dae33535d6b8e8e6ec01f8c1d76c9adb096c6b7d64823038cde
+PYTHON_ANYIO_SITE = https://files.pythonhosted.org/packages/95/7d/4c1bd541d4dffa1b52bd83fb8527089e097a106fc90b467a7313b105f840
 PYTHON_ANYIO_SETUP_TYPE = setuptools
 PYTHON_ANYIO_LICENSE = MIT
 PYTHON_ANYIO_LICENSE_FILES = LICENSE
