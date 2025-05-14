7fc6421be3dc344467ec1ccbf0c8089513e3f8f8
Thomas Perale <thomas.perale@mind.be>
2025-05-14 17:04:46 +0200

    package/python-flask: security bump to version 3.1.1
    Fixes the following security issues:
    
    - CVE-2025-47278: In Flask 3.1.0, the way fallback key configuration was
        handled resulted in the last fallback key being used for signing,
        rather than the current signing key.
    
    For more information, see:
      - https://nvd.nist.gov/vuln/detail/CVE-2025-47278
      - https://github.com/pallets/flask/commit/73d6504063bfa00666a92b07a28aaf906c532f09
    
    For more details on the version bump, see the release notes:
      - https://github.com/pallets/flask/releases/tag/3.1.1
    
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 45439ae262aeb0cf720815c5a7b541467a39a60c)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/python-flask/python-flask.mk
+++ package/python-flask/python-flask.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_FLASK_VERSION = 3.1.0
+PYTHON_FLASK_VERSION = 3.1.1
 PYTHON_FLASK_SOURCE = flask-$(PYTHON_FLASK_VERSION).tar.gz
-PYTHON_FLASK_SITE = https://files.pythonhosted.org/packages/89/50/dff6380f1c7f84135484e176e0cac8690af72fa90e932ad2a0a60e28c69b
+PYTHON_FLASK_SITE = https://files.pythonhosted.org/packages/c0/de/e47735752347f4128bcf354e0da07ef311a78244eba9e3dc1d4a5ab21a98
 PYTHON_FLASK_SETUP_TYPE = flit
 PYTHON_FLASK_LICENSE = BSD-3-Clause
 PYTHON_FLASK_LICENSE_FILES = LICENSE.txt docs/license.rst
