2133c1998b8e968046aee8fbd15b004b6a94b1ba
Roy Kollen Svendsen <roykollensvendsen@gmail.com>
2025-05-15 08:02:42 +0200

    package/mariadb: bump version to 10.11.11
    Fixes:
    Build with gcc-15 was fixed upstream in 10.11.11:
    
    https://github.com/MariaDB/server/commit/1d6f857
    
    Remove patch, since the problem was fixed upstream:
    
    https://github.com/MariaDB/server/commit/4375245
    
    After bumping the version to 10.11.11 the configuration step failed for target:
    
    CMake Error: try_run() invoked in cross-compiling mode, please set the following cache variables appropriately:
       HAVE_SYSTEM_LIBFMT_EXITCODE (advanced)
    
    To fx this problem we set HAVE_SYSTEM_LIBFMT_EXITCODE=0.
    
    Fix legal-info after changes to README.md.
    
    Signed-off-by: Roy Kollen Svendsen <roykollensvendsen@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 6cd8f95346aaf7dd7c26e83d263969e5406420bd)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/mariadb/mariadb.mk
+++ package/mariadb/mariadb.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MARIADB_VERSION = 10.11.6
+MARIADB_VERSION = 10.11.11
 MARIADB_SITE = https://downloads.mariadb.org/interstitial/mariadb-$(MARIADB_VERSION)/source
 MARIADB_LICENSE = GPL-2.0 (server), GPL-2.0 with FLOSS exception (GPL client library), LGPL-2.0 (LGPL client library)
 # Tarball no longer contains LGPL license text
@@ -27,7 +27,8 @@ MARIADB_DEPENDENCIES = \
 
 MARIADB_CONF_OPTS += \
 	-DWITH_LIBFMT=system \
-	-DWITH_PCRE=system
+	-DWITH_PCRE=system \
+	-DHAVE_SYSTEM_LIBFMT_EXITCODE=0
 
 # use bundled GPL-2.0+ licensed readline as package/readline is GPL-3.0+
 MARIADB_CONF_OPTS += -DWITH_READLINE=ON
