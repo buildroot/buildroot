6541717ef963bd4e7cf5b902d2ba4781217e99dc
Thomas Perale <thomas.perale@mind.be>
2025-05-16 18:59:11 +0200

    package/python-h11: security bump to version 0.16.0
    Fixes the following security issue:
    
    - CVE-2025-43859: A leniency in h11's parsing of line terminators in
        chunked-coding message bodies can lead to request smuggling
        vulnerabilities under certain conditions.
    
    For more information, see:
      - https://nvd.nist.gov/vuln/detail/CVE-2025-43859
      - https://github.com/python-hyper/h11/commit/114803a29ce50116dc47951c690ad4892b1a36ed
    
    For more details on the version bump, see:
      - https://github.com/python-hyper/h11/compare/v0.14.0...v0.16.0
    
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-h11/python-h11.mk
+++ package/python-h11/python-h11.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_H11_VERSION = 0.14.0
+PYTHON_H11_VERSION = 0.16.0
 PYTHON_H11_SOURCE = h11-$(PYTHON_H11_VERSION).tar.gz
-PYTHON_H11_SITE = https://files.pythonhosted.org/packages/f5/38/3af3d3633a34a3316095b39c8e8fb4853a28a536e55d347bd8d8e9a14b03
+PYTHON_H11_SITE = https://files.pythonhosted.org/packages/01/ee/02a2c011bdab74c6fb3c75474d40b3052059d95df7e73351460c8588d963
 PYTHON_H11_SETUP_TYPE = setuptools
 PYTHON_H11_LICENSE = MIT
 PYTHON_H11_LICENSE_FILES = LICENSE.txt
