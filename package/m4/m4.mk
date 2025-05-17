276fc22e66f9f9aed490338b967c3aedffd70084
Vincent Fazio <vfazio@gmail.com>
2025-05-16 21:40:45 -0500

    package/m4: bump to 1.4.20
    The new version bundles an updated gnulib that includes support for
    -std=c23 which is the default for gcc 15.
    
    Signed-off-by: Vincent Fazio <vfazio@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@rnout.be>
    (cherry picked from commit d9aabc1af39fc478e5aeee446bd2d814a2dcbda5)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/m4/m4.mk
+++ package/m4/m4.mk
@@ -4,20 +4,10 @@
 #
 ################################################################################
 
-M4_VERSION = 1.4.19
+M4_VERSION = 1.4.20
 M4_SOURCE = m4-$(M4_VERSION).tar.xz
 M4_SITE = $(BR2_GNU_MIRROR)/m4
 M4_LICENSE = GPL-3.0+
 M4_LICENSE_FILES = COPYING
-
-# gcc-15 defaults to -std=gnu23 which is incorrectly detected and
-# generates build failures in the gnulib copy included in
-# m4-1.4.19. We workaround this by forcing the previous gcc default
-# standard, which is -std=gnu17 only when host gcc is >= 15. This
-# workaround can be removed when m4 will be updated to a version
-# including a fix for gcc-15.
-ifeq ($(BR2_HOST_GCC_AT_LEAST_15),y)
-HOST_M4_CONF_ENV = CFLAGS="$(HOST_CFLAGS) -std=gnu17"
-endif
 
 $(eval $(host-autotools-package))
