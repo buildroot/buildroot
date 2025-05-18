f6ff1078798946bfa96c0d973536731e814f4999
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2025-05-18 10:57:04 +0200

    package/ripgrep: drop stale ignore CVE entry
    The ignore CVE entry was added because the vulnerability only affects
    Windows. But it also only affected ripgrep versions < 13, and we're
    using ripgrep 14.x now, so the CVE is anyway no longer relevant, and
    the ignore CVE entry can be dropped.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 107e935e1c0f9776776791d7d98a536e627c3354)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/ripgrep/ripgrep.mk
+++ package/ripgrep/ripgrep.mk
@@ -10,7 +10,4 @@ RIPGREP_LICENSE = MIT
 RIPGREP_LICENSE_FILES = LICENSE-MIT
 RIPGREP_CPE_ID_VALID = YES
 
-# CVE only impacts ripgrep on Windows
-RIPGREP_IGNORE_CVES += CVE-2021-3013
-
 $(eval $(cargo-package))
