1c1ee69fa7ba55b3eefa5d5a326abb0c13220aae
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2025-05-18 10:57:11 +0200

    package/sox: add fix for CVE-2021-40426
    The patch comes from Debian. The CVEs are not reported by pkg-stats
    because the NVD database has associated it to the
    libsox_project:libsox vendor/product CPE. This has been reported to
    NVD:
    
      https://lore.kernel.org/buildroot/20250517220322.4da9bdb3@windsurf/
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/sox/sox.mk
+++ package/sox/sox.mk
@@ -69,6 +69,11 @@ SOX_IGNORE_CVES += CVE-2021-3643 CVE-2021-23210
 # due to the change of CPE ID to sox_project:sox in the NVD database
 SOX_IGNORE_CVES += CVE-2021-23159 CVE-2021-23172 CVE-2023-34318
 
+# 0008-phere-avoid-integer-underflow.patch
+# This entry is NOT stale, those CVEs are not reported by pkg-stats
+# due to the change of CPE ID to libsox_project:libsox in the NVD database
+SOX_IGNORE_CVES += CVE-2021-40426
+
 SOX_CONF_OPTS = \
 	--with-distro="Buildroot" \
 	--disable-stack-protector
