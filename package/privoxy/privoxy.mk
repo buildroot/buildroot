3f5a807810b63703e3fd959629d073cc6ccef41c
Bernd Kuhls <bernd@kuhls.net>
2025-05-18 19:39:32 +0200

    package/privoxy: bump version to 4.0.0
    Changelog:
    https://www.privoxy.org/gitweb/?p=privoxy.git;a=blob;f=ChangeLog;h=a7bffc52b8996c327a23ee562627841550ed90e9;hb=6adadda85067fb7d4cc8663fbfbcacb4b0206615
    
    Removed patches which are included in this release.
    
    Added support for pcre2 which was added upstream in the release.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/privoxy/privoxy.mk
+++ package/privoxy/privoxy.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-PRIVOXY_VERSION = 3.0.34
+PRIVOXY_VERSION = 4.0.0
 PRIVOXY_SITE = https://downloads.sourceforge.net/project/ijbswa/Sources/$(PRIVOXY_VERSION)%20%28stable%29
 PRIVOXY_SOURCE = privoxy-$(PRIVOXY_VERSION)-stable-src.tar.gz
 # configure not shipped
 PRIVOXY_AUTORECONF = YES
-PRIVOXY_DEPENDENCIES = pcre zlib
+PRIVOXY_DEPENDENCIES = $(if $(BR2_PACKAGE_PCRE2),pcre2,pcre) zlib
 PRIVOXY_LICENSE = GPL-2.0+
 PRIVOXY_LICENSE_FILES = LICENSE
 PRIVOXY_CPE_ID_VENDOR = privoxy
