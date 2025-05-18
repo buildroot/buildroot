61343ed57a4bd4035f77ec9562c198f8c8ead173
Bernd Kuhls <bernd@kuhls.net>
2025-05-18 15:03:23 +0200

    package/alsa-utils: remove autoreconf
    Buildroot commit 96a7db5a4b7526ecf744d89d9c19986b91626325 removed
    0011-configure.ac-fix-UMP-support-detection.patch but forgot to remove
    ALSA_UTILS_AUTORECONF = YES as well.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/alsa-utils/alsa-utils.mk
+++ package/alsa-utils/alsa-utils.mk
@@ -9,8 +9,6 @@ ALSA_UTILS_SOURCE = alsa-utils-$(ALSA_UTILS_VERSION).tar.bz2
 ALSA_UTILS_SITE = https://www.alsa-project.org/files/pub/utils
 ALSA_UTILS_LICENSE = GPL-2.0
 ALSA_UTILS_LICENSE_FILES = COPYING
-# 0011-configure.ac-fix-UMP-support-detection.patch
-ALSA_UTILS_AUTORECONF = YES
 ALSA_UTILS_INSTALL_STAGING = YES
 ALSA_UTILS_DEPENDENCIES = host-pkgconf alsa-lib \
 	$(if $(BR2_PACKAGE_NCURSES),ncurses) \
