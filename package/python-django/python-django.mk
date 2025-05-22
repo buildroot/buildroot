15fdc8b0caead41dcb9ce3ff81b5cb24cfce5f14
Marcus Hoffmann <buildroot@bubu1.eu>
2025-05-22 11:48:18 +0200

    package/python-django: clarify licensing
    Django includes code from a couple of other projects, add their
    respective license files and licenses to the django package.
    
    ./utils/scanpypi finds most of these. Additionally this was
    cross-checked against debian's license list [1], not including some
    things that no longer exist or no longer indicate separate licensing
    upstream:
    * django/contrib/admin/static/admin/fonts/
    * django/utils/baseconv.py
    * django/utils/ipv6.py
    * django/utils/autoreload.py
    
    Also not included are separate licensing for docs, which buildroot
    doesn't package:
    * docs/_theme/djangodocs/static/reset-fonts-grids.css
    * docs/_theme/djangodocs/static/fontawesome/LICENSE.txt
    
    [1] https://metadata.ftp-master.debian.org/changelogs//main/p/python-django/python-django_5.2-1_copyright
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    [Peter: Indent with single tab]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -8,8 +8,18 @@ PYTHON_DJANGO_VERSION = 5.2.1
 PYTHON_DJANGO_SOURCE = django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
 PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/ac/10/0d546258772b8f31398e67c85e52c66ebc2b13a647193c3eef8ee433f1a8
-PYTHON_DJANGO_LICENSE = BSD-3-Clause
-PYTHON_DJANGO_LICENSE_FILES = LICENSE
+PYTHON_DJANGO_LICENSE = BSD-3-Clause, MIT (jquery, utils/archive.py), BSD-2-Clause (inlines.js)
+PYTHON_DJANGO_LICENSE_FILES = LICENSE \
+	django/contrib/gis/measure.py \
+	django/contrib/gis/gdal/LICENSE \
+	django/contrib/gis/geos/LICENSE \
+	django/contrib/admin/static/admin/js/inlines.js \
+	django/contrib/admin/static/admin/js/vendor/jquery/LICENSE.txt \
+	django/contrib/admin/static/admin/js/vendor/select2/LICENSE.md \
+	django/contrib/admin/static/admin/js/vendor/xregexp/LICENSE.txt \
+	django/contrib/admin/static/admin/img/LICENSE \
+	django/dispatch/license.txt \
+	django/utils/archive.py
 PYTHON_DJANGO_CPE_ID_VENDOR = djangoproject
 PYTHON_DJANGO_CPE_ID_PRODUCT = django
 PYTHON_DJANGO_SETUP_TYPE = setuptools
