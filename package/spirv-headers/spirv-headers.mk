395568599c2922fc2b7bfb62a66e81db6767db38
El Mehdi YOUNES <elmehdi.younes@smile.fr>
2025-05-22 09:18:15 +0200

    package/spirv-{headers, tools}: bump to version 1.4.313.0
    Update SPIR-V headers and SPIR-V tools to version 1.4.313.0 from the
    Vulkan SDK.
    
    This versions are required for compatibility with updated
    spirv-llvm-translator (v20.1.2).
    The source archive now follows the vulkan-sdk- prefix convention.
    
    release:
    https://github.com/KhronosGroup/SPIRV-Headers/releases/tag/vulkan-sdk-1.4.313.0
    https://github.com/KhronosGroup/SPIRV-Tools/releases/tag/vulkan-sdk-1.4.313.0
    
    Signed-off-by: El Mehdi YOUNES <elmehdi.younes@smile.fr>
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    [Bernd:
     bumped to 1.4.313.0
     Updated license hash of spirv-headers due to upstream commits:
     https://github.com/KhronosGroup/SPIRV-Headers/commit/1c9115b562bab79ee2160fbd845f41b815b9f21f
     https://github.com/KhronosGroup/SPIRV-Headers/commit/23d4a398c2ccca05fc623cca8785ce7741c5345d]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/spirv-headers/spirv-headers.mk
+++ package/spirv-headers/spirv-headers.mk
@@ -5,8 +5,8 @@
 ################################################################################
 
 # Keep in sync with spirv-tools version
-SPIRV_HEADERS_VERSION = 1.3.261.1
-SPIRV_HEADERS_SITE = $(call github,KhronosGroup,SPIRV-Headers,sdk-$(SPIRV_HEADERS_VERSION))
+SPIRV_HEADERS_VERSION = 1.4.313.0
+SPIRV_HEADERS_SITE = $(call github,KhronosGroup,SPIRV-Headers,vulkan-sdk-$(SPIRV_HEADERS_VERSION))
 SPIRV_HEADERS_LICENSE = MIT
 SPIRV_HEADERS_LICENSE_FILES = LICENSE
 
--- package/spirv-tools/spirv-tools.mk
+++ package/spirv-tools/spirv-tools.mk
@@ -5,8 +5,8 @@
 ################################################################################
 
 # Keep in sync with spirv-headers version
-SPIRV_TOOLS_VERSION = 1.3.261.1
-SPIRV_TOOLS_SITE = $(call github,KhronosGroup,SPIRV-Tools,sdk-$(SPIRV_TOOLS_VERSION))
+SPIRV_TOOLS_VERSION = 1.4.313.0
+SPIRV_TOOLS_SITE = $(call github,KhronosGroup,SPIRV-Tools,vulkan-sdk-$(SPIRV_TOOLS_VERSION))
 SPIRV_TOOLS_LICENSE = Apache-2.0
 SPIRV_TOOLS_LICENSE_FILES = LICENSE
 SPIRV_TOOLS_DEPENDENCIES = spirv-headers
