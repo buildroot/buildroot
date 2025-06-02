c967602d27f5c2368d63966a5253fc7b0b17cda9
Petr Vorel <petr.vorel@gmail.com>
2025-06-02 19:29:08 +0200

    package/ltp-testsuite: bump version to 20250530
    Changelog: https://github.com/linux-test-project/ltp/releases/tag/20250530
    
    Signed-off-by: Petr Vorel <petr.vorel@gmail.com>
    [Julien: add link to change log]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/ltp-testsuite/ltp-testsuite.mk
+++ package/ltp-testsuite/ltp-testsuite.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LTP_TESTSUITE_VERSION = 20250130
+LTP_TESTSUITE_VERSION = 20250530
 LTP_TESTSUITE_SOURCE = ltp-full-$(LTP_TESTSUITE_VERSION).tar.xz
 LTP_TESTSUITE_SITE = https://github.com/linux-test-project/ltp/releases/download/$(LTP_TESTSUITE_VERSION)
 
