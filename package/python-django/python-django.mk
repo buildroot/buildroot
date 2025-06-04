bdcc1299cc48b3aec917959f3a3cd4058790c2f3
Marcus Hoffmann <buildroot@bubu1.eu>
2025-06-04 18:33:46 +0200

    package/python-django: security bump to 5.2.2
    Django 5.2.2 fixes a security issue with severity “low” and several bugs in 5.2.1.
    
    Fixes: CVE-2025-48432: Potential log injection via unescaped request path
    
    Full Release notes:
    https://docs.djangoproject.com/en/5.2/releases/5.2.2/
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 5.2.1
+PYTHON_DJANGO_VERSION = 5.2.2
 PYTHON_DJANGO_SOURCE = django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/ac/10/0d546258772b8f31398e67c85e52c66ebc2b13a647193c3eef8ee433f1a8
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/39/17/4567ee12bb84114c544d5c4a792e7226db517ac78f552111e9dc62d1de14
 PYTHON_DJANGO_LICENSE = BSD-3-Clause, MIT (jquery, utils/archive.py), BSD-2-Clause (inlines.js)
 PYTHON_DJANGO_LICENSE_FILES = LICENSE \
 	django/contrib/gis/measure.py \
