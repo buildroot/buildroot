26c4821638eff99f8676b3072e5c9bb3267562b7
Kadambini Nema <kadambini.nema@gmail.com>
2025-06-06 13:41:52 -0700

    package/diffutils: bump version to 3.12
    Release notes:
    Version 3.12 - https://lists.gnu.org/archive/html/info-gnu/2025-04/msg00005.html
    Version 3.11 - https://lists.gnu.org/archive/html/info-gnu/2025-02/msg00003.html
    
    Fix the following error seen during cross-compilation error by assuming that
    strcasecmp function is always available.
    - "configure: error: cannot run test program while cross compiling"
    
    Signed-off-by: Kadambini Nema <kadambini.nema@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/diffutils/diffutils.mk
+++ package/diffutils/diffutils.mk
@@ -4,13 +4,14 @@
 #
 ################################################################################
 
-DIFFUTILS_VERSION = 3.10
+DIFFUTILS_VERSION = 3.12
 DIFFUTILS_SOURCE = diffutils-$(DIFFUTILS_VERSION).tar.xz
 DIFFUTILS_SITE = $(BR2_GNU_MIRROR)/diffutils
 DIFFUTILS_DEPENDENCIES = $(TARGET_NLS_DEPENDENCIES)
 DIFFUTILS_LICENSE = GPL-3.0+
 DIFFUTILS_LICENSE_FILES = COPYING
 DIFFUTILS_CPE_ID_VENDOR = gnu
+DIFFUTILS_CONF_ENV += gl_cv_func_strcasecmp_works=yes
 
 # Since glibc >= 2.26, don't try to use getopt_long replacement bundled
 # with diffutils. It will conflict with the one from glibc.
