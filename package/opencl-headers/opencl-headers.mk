dba2912be3096ae815c03aa4552dbcbc9cd46f0a
El Mehdi YOUNES <elmehdi.younes@smile.fr>
2025-06-06 09:01:49 +0200

    package/opencl-headers: new package
    Add the "opencl-headers" package to install the OpenCL C API headers,
    which are required at build time by consumers such as
    opencl-icd-loader.
    
    While Mesa provides these headers internally, they are not installed
    by default since clover and opencl-gallium are deprecated and will
    be removed starting from mesa 25.2
    
    And since we should install them manually from mesa, it is better
    use external headers. This ensures these headers are always
    available and up-to-date independently from those provided by Mesa,
    folowing the same logic used for "opencl-clhpp".
    
    see:
    https://github.com/KhronosGroup/OpenCL-ICD-Loader#dependencies
    https://gitlab.freedesktop.org/mesa/mesa/-/tree/main/include/CL
    https://github.com/KhronosGroup/OpenCL-Headers
    https://gitlab.freedesktop.org/mesa/mesa/-/commit/5844dea31620f36c41ac738e17fefd57084b2dbb#feedf4d0444f717d7227468a9529c2cf9740b6fd
    
    Signed-off-by: El Mehdi YOUNES <elmehdi.younes@smile.fr>
    [Julien: add Medhi in DEVELOPERS for this package]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

---
+++ package/opencl-headers/opencl-headers.mk
@@ -0,0 +1,26 @@
+################################################################################
+#
+# opencl-headers
+#
+################################################################################
+
+# The opencl-headers, opencl-icd-loader, and opencl-clhpp packages
+# must always be updated together to compatible versions.
+# Version desynchronization may result in build or runtime errors.
+# When updating one of them, make sure to check and update the others.
+
+OPENCL_HEADERS_VERSION = 2024.10.24
+OPENCL_HEADERS_SOURCE = OpenCL-Headers-$(OPENCL_HEADERS_VERSION).tar.gz
+OPENCL_HEADERS_SITE = $(call github,KhronosGroup,OpenCL-Headers,v$(OPENCL_HEADERS_VERSION))
+OPENCL_HEADERS_LICENSE = Apache-2.0
+OPENCL_HEADERS_LICENSE_FILES = LICENSE
+OPENCL_HEADERS_INSTALL_STAGING = YES
+OPENCL_HEADERS_INSTALL_TARGET = NO
+
+define OPENCL_HEADERS_INSTALL_STAGING_CMDS
+	mkdir -p $(STAGING_DIR)/usr/include/CL
+	$(INSTALL) -D -m 0644 $(@D)/CL/* \
+		$(STAGING_DIR)/usr/include/CL
+endef
+
+$(eval $(generic-package))
