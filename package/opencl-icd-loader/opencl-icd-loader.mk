d2a82364ac923df26cfcd30edd3d755d90923323
El Mehdi YOUNES <elmehdi.younes@smile.fr>
2025-06-06 09:01:50 +0200

    package/opencl-icd-loader: new package
    This adds the OpenCL installable client driver (ICD) loader
    which provides the 'libOpenCL.so' shared library needed to
    run OpenCL programs.
    
    Mesa3D can offer opencl implementations (such as rusticl),
    but it does not provide 'libOpenCL.so'.
    
    Without it, programs like 'clinfo' fail to build not
    finding 'libOpenCL.so'.
    
    see:
    https://github.com/KhronosGroup/OpenCL-ICD-Loader
    
    Signed-off-by: El Mehdi YOUNES <elmehdi.younes@smile.fr>
    [Julien: add Medhi in DEVELOPERS for this package]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

---
+++ package/opencl-icd-loader/opencl-icd-loader.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# opencl-icd-loader
+#
+################################################################################
+
+# The opencl-headers, opencl-icd-loader, and opencl-clhpp packages
+# must always be updated together to compatible versions.
+# Version desynchronization may result in build or runtime errors.
+# When updating one of them, make sure to check and update the others.
+OPENCL_ICD_LOADER_VERSION = 2024.10.24
+OPENCL_ICD_LOADER_SOURCE = OpenCL-ICD-Loader-$(OPENCL_ICD_LOADER_VERSION).tar.gz
+OPENCL_ICD_LOADER_SITE = $(call github,KhronosGroup,OpenCL-ICD-Loader,v$(OPENCL_ICD_LOADER_VERSION))
+OPENCL_ICD_LOADER_LICENSE = Apache-2.0
+OPENCL_ICD_LOADER_LICENSE_FILES = LICENSE
+OPENCL_ICD_LOADER_INSTALL_STAGING = YES
+
+OPENCL_ICD_LOADER_DEPENDENCIES = opencl-headers
+
+OPENCL_ICD_LOADER_CONF_OPTS += -DOPENCL_ICD_LOADER_HEADERS_DIR=$(STAGING_DIR)/usr/include
+
+$(eval $(cmake-package))
