243add1034def091d875244d6a47a0ea6947436c
Titouan Christophe <titouan.christophe@mind.be>
2025-06-06 10:00:51 +0200

    package/python-django: security bump to v5.1.10
    This fixes the following CVE:
    
    - CVE-2025-48432: An issue was discovered in Django 5.2 before 5.2.2,
                      5.1 before 5.1.10, and 4.2 before 4.2.22.
                      Internal HTTP response logging does not escape request.path,
                      which allows remote attackers to potentially manipulate log
                      output via crafted URLs.
                      This may lead to log injection or forgery when logs are
                      viewed in terminals or processed by external systems.
    
    See https://www.cve.org/CVERecord?id=CVE-2025-48432
    
    Signed-off-by: Titouan Christophe <titouan.christophe@mind.be>
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 5.1.9
+PYTHON_DJANGO_VERSION = 5.1.10
 PYTHON_DJANGO_SOURCE = django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/10/08/2e6f05494b3fc0a3c53736846034f882b82ee6351791a7815bbb45715d79
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/73/ca/1c724be89e603eb8b5587ea24c63a8c30094c8ff4d990780b5033ee15c40
 PYTHON_DJANGO_LICENSE = BSD-3-Clause, MIT (jquery, utils/archive.py), BSD-2-Clause (inlines.js)
 PYTHON_DJANGO_LICENSE_FILES = LICENSE \
 	django/contrib/gis/measure.py \
