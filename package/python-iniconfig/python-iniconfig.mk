31fb707f3301965fc9c7fd45af18f700325486a8
Marcin Niestroj <m.niestroj@grinn-global.com>
2025-06-09 12:55:54 +0200

    package/python-iniconfig: bump to 2.1.0
    See [1] for changelog.
    
    This commit also updates the LICENSE hash, due to minor reformatting.
    See [2].
    
    [1] https://github.com/pytest-dev/iniconfig/blob/v2.1.0/CHANGELOG
    [2] https://github.com/pytest-dev/iniconfig/commit/f81ae0314363f35e36e01926a3a87d67eea50c39
    
    Signed-off-by: Marcin Niestroj <m.niestroj@grinn-global.com>
    [Julien:
     - update changelog link to use tag
     - fix LICENCE hash
    ]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-iniconfig/python-iniconfig.mk
+++ package/python-iniconfig/python-iniconfig.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_INICONFIG_VERSION = 2.0.0
+PYTHON_INICONFIG_VERSION = 2.1.0
 PYTHON_INICONFIG_SOURCE = iniconfig-$(PYTHON_INICONFIG_VERSION).tar.gz
-PYTHON_INICONFIG_SITE = https://files.pythonhosted.org/packages/d7/4b/cbd8e699e64a6f16ca3a8220661b5f83792b3017d0f79807cb8708d33913
+PYTHON_INICONFIG_SITE = https://files.pythonhosted.org/packages/f2/97/ebf4da567aa6827c909642694d71c9fcf53e5b504f2d96afea02718862f3
 PYTHON_INICONFIG_SETUP_TYPE = hatch
 PYTHON_INICONFIG_LICENSE = MIT
 PYTHON_INICONFIG_LICENSE_FILES = LICENSE
