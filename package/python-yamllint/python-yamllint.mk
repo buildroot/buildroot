917ed944ed1e4d35c9e1d10735d61225c0400048
Heiko Thiery <heiko.thiery@gmail.com>
2025-06-10 08:03:23 +0200

    package/python-yamllint: bump version to 1.37.1
    Upstream changelog:
    https://github.com/adrienverge/yamllint/blob/master/CHANGELOG.rst#1371-2025-05-04
    
    Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-yamllint/python-yamllint.mk
+++ package/python-yamllint/python-yamllint.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_YAMLLINT_VERSION = 1.35.1
+PYTHON_YAMLLINT_VERSION = 1.37.1
 PYTHON_YAMLLINT_SOURCE = yamllint-$(PYTHON_YAMLLINT_VERSION).tar.gz
-PYTHON_YAMLLINT_SITE = https://files.pythonhosted.org/packages/da/06/d8cee5c3dfd550cc0a466ead8b321138198485d1034130ac1393cc49d63e
+PYTHON_YAMLLINT_SITE = https://files.pythonhosted.org/packages/46/f2/cd8b7584a48ee83f0bc94f8a32fea38734cefcdc6f7324c4d3bfc699457b
 PYTHON_YAMLLINT_SETUP_TYPE = setuptools
 PYTHON_YAMLLINT_LICENSE = GPL-3.0
 PYTHON_YAMLLINT_LICENSE_FILES = LICENSE
