1b5f8d04c8798e0d4def0ea2b5dfb20329f2fc38
Bernd Kuhls <bernd@kuhls.net>
2025-06-14 12:19:59 +0200

    package/dht: fix cmake4 compatibility
    The only dependee of dht, transmission, forked dht due to upstream
    rejection of cmake support:
    https://github.com/jech/dht/pull/50
    https://github.com/jech/dht/pull/19
    
    A fix was added to this fork for building with cmake 4.x:
    https://github.com/transmission/dht/commits/post-0.27-transmission/
    
    Instead of adding yet another patch to buildroot we switch to the repo
    maintained by transmission.
    
    Fixes:
    https://autobuild.buildroot.org/results/30339a4fdb9c0b608ca3c5ae0692596b21b2fc3f/
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    [Julien: use "git describe --abbrev=40" format in _VERSION]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/dht/dht.mk
+++ package/dht/dht.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-DHT_VERSION = 0.27
-DHT_SITE = $(call github,jech,dht,dht-$(DHT_VERSION))
+DHT_VERSION = 0.27-2-g38c9f261d9b58b76b9eaf85f84ec1b35151a1eac
+DHT_SITE = $(call github,transmission,dht,dht-$(DHT_VERSION))
 DHT_LICENSE = MIT
 DHT_LICENSE_FILES = LICENCE
 DHT_INSTALL_STAGING = YES
