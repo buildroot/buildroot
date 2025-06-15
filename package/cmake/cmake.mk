a86fd0f0a0b044a1c5412f935a1f18f9950da259
Bernd Kuhls <bernd@kuhls.net>
2025-06-15 18:19:32 +0200

    package/cmake: bump version to 3.31.8
    Release notes:
    https://www.kitware.com/cmake-3-31-6-available-for-download/
    https://www.kitware.com/cmake-3-31-7-available-for-download/
    https://www.kitware.com/cmake-3-31-8-available-for-download/
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/cmake/cmake.mk
+++ package/cmake/cmake.mk
@@ -6,7 +6,7 @@
 
 # When updating the version, please also update BR2_HOST_CMAKE_AT_LEAST_X_Y
 CMAKE_VERSION_MAJOR = 3.31
-CMAKE_VERSION = $(CMAKE_VERSION_MAJOR).5
+CMAKE_VERSION = $(CMAKE_VERSION_MAJOR).8
 CMAKE_SITE = https://cmake.org/files/v$(CMAKE_VERSION_MAJOR)
 CMAKE_LICENSE = BSD-3-Clause
 CMAKE_LICENSE_FILES = Copyright.txt
