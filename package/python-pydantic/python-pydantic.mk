3ca213eadb80875e61bcd9f216842bdca8cf6f98
Marcus Hoffmann <buildroot@bubu1.eu>
2025-06-16 12:11:10 +0200

    package/python-pydantic: bump to 2.11.7
    Changelog: https://docs.pydantic.dev/latest/changelog/#v2117-2025-06-14
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-pydantic/python-pydantic.mk
+++ package/python-pydantic/python-pydantic.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYDANTIC_VERSION = 2.11.6
+PYTHON_PYDANTIC_VERSION = 2.11.7
 PYTHON_PYDANTIC_SOURCE = pydantic-$(PYTHON_PYDANTIC_VERSION).tar.gz
-PYTHON_PYDANTIC_SITE = https://files.pythonhosted.org/packages/ef/8f/9af0f46acc943b8c4592d06523f26a150acf6e6e37e8bd5f0ace925e996d
+PYTHON_PYDANTIC_SITE = https://files.pythonhosted.org/packages/00/dd/4325abf92c39ba8623b5af936ddb36ffcfe0beae70405d456ab1fb2f5b8c
 PYTHON_PYDANTIC_SETUP_TYPE = hatch
 PYTHON_PYDANTIC_LICENSE = MIT
 PYTHON_PYDANTIC_LICENSE_FILES = LICENSE
