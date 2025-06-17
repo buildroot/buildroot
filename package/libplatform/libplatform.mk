4da169b03eeb15ab3f7ed7f41c916290d5a3c928
Bernd Kuhls <bernd@kuhls.net>
2025-06-17 23:02:28 +0200

    package/libplatform: bump version
    Removed patch which is included in this release:
    https://github.com/Pulse-Eight/platform/commit/e48db06c64613ac76c820511da95228bcd072f64
    
    Use upstream-provided license file:
    https://github.com/Pulse-Eight/platform/commit/92220248b11c43a80105567dbb074c5f30b04029
    
    This bump includes compatibility with cmake 4.x:
    https://github.com/Pulse-Eight/platform/commit/d7faed1c696b1a6a67f114a63a0f4c085f0f9195
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    [Julien: remove patch entry in .checkpackageignore]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libplatform/libplatform.mk
+++ package/libplatform/libplatform.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-LIBPLATFORM_VERSION = a7cd0d5780ed80a4e70480d1650749f29e8a1fb2
+LIBPLATFORM_VERSION = 2748be52ae27e6007ef548b697d4a03ff7de4291
 LIBPLATFORM_SITE = $(call github,Pulse-Eight,platform,$(LIBPLATFORM_VERSION))
 LIBPLATFORM_LICENSE = GPL-2.0+
-LIBPLATFORM_LICENSE_FILES = src/os.h
+LIBPLATFORM_LICENSE_FILES = LICENSE.md
 LIBPLATFORM_INSTALL_STAGING = YES
 
 $(eval $(cmake-package))
