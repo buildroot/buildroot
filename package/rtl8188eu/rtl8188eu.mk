a9c5dc024a0717b68b22a352f960d9dac099f73f
Giulio Benetti <giulio.benetti@benettiengineering.com>
2025-06-18 16:58:57 +0200

    package/rtl8188eu: move upstream to Benetti Engineering Github to fix Linux 6.15 build failure
    Benetti Engineering just took over Larry Finger(lwfinger)'s repository
    rtl8188eu since Larry unfortunately passed away[0](RIP) and there are
    pending PRs that will never be checked as stated here[1]. So basically move
    github user to benetti-engineering-sas and update version with latest that
    fixes build failure with Linux version 6.15. And let's drop local patches
    that are now upstreamed as well. Let's also drop obsolete Config.in
    informations since as stated here[2]: "This driver is under development
    and has a limited feature set. In particular it does not yet support 40MHz
    channels and power management". At the same time drop other suggestions
    like enabling CONFIG_WIRELESS_EXT or "this package needs a firmware loading
    mechanism to load the binary blob for the chip to work" since they are now
    part of the package. And of course let's update package's URL due to the
    moving.
    
    [0]: https://lwn.net/Articles/979419/
    [1]: https://github.com/lwfinger/rtl8188eu/pull/464
    
    Fixes:
    https://autobuild.buildroot.org/results/d59537da8eb27d737718885dc81ec257a2791455/
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Reviewed-by: Luca Ceresoli <luca.ceresoli@bootlin.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/rtl8188eu/rtl8188eu.mk
+++ package/rtl8188eu/rtl8188eu.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-RTL8188EU_VERSION = f42fc9c45d2086c415dce70d3018031b54a7beef
-RTL8188EU_SITE = $(call github,lwfinger,rtl8188eu,$(RTL8188EU_VERSION))
+RTL8188EU_VERSION = 702a90b02edca722cc0d139260ba02c23f406333
+RTL8188EU_SITE = $(call github,benetti-engineering-sas,rtl8188eu,$(RTL8188EU_VERSION))
 RTL8188EU_LICENSE = GPL-2.0, proprietary (rtl8188eufw.bin firmware blob)
 RTL8188EU_LICENSE_FILES = COPYING
 RTL8188EU_MODULE_MAKE_OPTS = CONFIG_RTL8188EU=m
