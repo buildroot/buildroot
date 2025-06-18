cb5d3daf644395e0b31e09974fe2f47f130b4f7c
Maxime Leroy <maxime@leroys.fr>
2025-06-18 11:52:39 +0200

    package/grout: bump to 0.10.3 version
    See the release note of the new version:
    https://github.com/DPDK/grout/releases/tag/v0.10.3
    
    Locale is not needed anymore for this new version, thus comment on
    BR2_GENERATE_LOCALE has been removed in consequence.
    
    Signed-off-by: Maxime Leroy <maxime@leroys.fr>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/grout/grout.mk
+++ package/grout/grout.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-GROUT_VERSION = 0.10.0
+GROUT_VERSION = 0.10.3
 GROUT_SITE = $(call github,DPDK,grout,v$(GROUT_VERSION))
 GROUT_LICENSE = BSD-3-Clause
-GROUT_LICENSE_FILES = LICENSE
+GROUT_LICENSE_FILES = licenses/BSD-3-clause.txt
 
 # Avoid using buildroot commit hash
 GROUT_CONF_ENV = GROUT_VERSION=$(GROUT_VERSION)
