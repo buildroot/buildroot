6cf4f6cdb663d224bef0b96fedc183b27781a28b
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2025-06-19 12:34:10 +0000

    package/gqrx: bump version to 2.17.7
    For v2.17.7 release note, see:
    https://github.com/gqrx-sdr/gqrx/releases/tag/v2.17.7
    
    For all other release notes since 2.15.9, see:
    https://github.com/gqrx-sdr/gqrx/releases
    
    This commit also updates the LICENSE-CTK hash after minor
    reformatting in commit [1].
    
    [1] https://github.com/gqrx-sdr/gqrx/commit/9857b1b89f38ed77e6b9bd0525e7dd978eda59fa
    
    Signed-off-by: Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
    [Julien:
     - add links to release notes in commit log
     - sort _CONF_OPTS alphabetically in gqrx.mk
     - fix LICENSE-CTK hash
    ]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/gqrx/gqrx.mk
+++ package/gqrx/gqrx.mk
@@ -4,13 +4,15 @@
 #
 ################################################################################
 
-GQRX_VERSION = 2.15.9
+GQRX_VERSION = 2.17.7
 GQRX_SITE = $(call github,csete,gqrx,v$(GQRX_VERSION))
 GQRX_LICENSE = GPL-3.0+, Apache-2.0
 GQRX_LICENSE_FILES = COPYING LICENSE-CTK
 GQRX_DEPENDENCIES = boost gnuradio gr-osmosdr libsndfile qt5base qt5svg
 
-GQRX_CONF_OPTS = -DLINUX_AUDIO_BACKEND=Gr-audio
+GQRX_CONF_OPTS = \
+	-DFORCE_QT5=ON \
+	-DLINUX_AUDIO_BACKEND=Gr-audio
 
 # gqrx can use __atomic builtins, so we need to link with
 # libatomic when available
