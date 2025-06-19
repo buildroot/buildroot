883c977c9a65432b408f33a7d445ba6b16a36647
Joseph Kogut <joseph.kogut@gmail.com>
2025-06-18 17:59:30 -0700

    package/python-glslang: bump to version 15.3.0
    See changelog:
    https://github.com/KhronosGroup/glslang/blob/15.3.0/CHANGES.md
    
    Fixes build issue: error: ‘uint32_t’ has not been declared, encountered
    on GCC v15, caused by cstdint not being implicitly included.
    
    https://bugs.gentoo.org/937261
    
    This commit also adds host-spriv-tools in _DEPENDENCIES to use the
    version packaged in Buildroot. It also adds
    ALLOW_EXTERNAL_SPIRV_TOOLS=ON in _CONF_OPTS, which was added in upstream
    commit [1], introduced in version 13.0.0.
    
    Also, when trying to build this python-glslang version 15.3.0 on a host
    providing cmake < 3.26, the configuration fails with the error message:
    
        CMake Error at CMakeLists.txt:345 (add_test):
          Error evaluating generator expression:
    
            $<TARGET_RUNTIME_DLL_DIRS:spirv-remap>
    
          Expression did not evaluate to a known generator expression
    
    This happens in the Buildroot docker reference image, which provides a
    cmake version 3.25. To workaround this issue which happen in glslang
    testing code, this commit disable those tests by adding
    GLSLANG_TESTS=OFF in _CONF_OPTS.
    
    [1] https://github.com/KhronosGroup/glslang/commit/3805888a57c07904b6bbdec639bfe5f078e26342
    
    Signed-off-by: Joseph Kogut <joseph.kogut@gmail.com>
    [Julien:
     - resolve merge conflict
     - update changelog link to use version tag
     - disable glslang tests
    ]
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 53076f0eba85c53bcbee43cdd81a0d584ba5be22)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/python-glslang/python-glslang.mk
+++ package/python-glslang/python-glslang.mk
@@ -4,10 +4,14 @@
 #
 ################################################################################
 
-PYTHON_GLSLANG_VERSION = 11.13.0
+PYTHON_GLSLANG_VERSION = 15.3.0
 PYTHON_GLSLANG_SITE = $(call github,KhronosGroup,glslang,$(PYTHON_GLSLANG_VERSION))
 PYTHON_GLSLANG_LICENSE = BSD-3-Clause
 PYTHON_GLSLANG_LICENSE_FILES = LICENSE.txt
-HOST_PYTHON_GLSLANG_DEPENDENCIES = host-python3
+HOST_PYTHON_GLSLANG_DEPENDENCIES = host-python3 host-spirv-tools
+
+HOST_PYTHON_GLSLANG_CONF_OPTS += \
+	-DALLOW_EXTERNAL_SPIRV_TOOLS=ON \
+	-DGLSLANG_TESTS=OFF
 
 $(eval $(host-cmake-package))
