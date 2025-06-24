c0fb057a08ac8e46f6cb6d5f6f95778a9b77343e
Raphaël Mélotte <raphael.melotte@mind.be>
2025-06-24 16:32:34 +0200

    package/python-s3transfer: bump to version 0.13.0
    Changelog:
    https://github.com/boto/s3transfer/blob/0.13.0/CHANGELOG.rst
    
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-s3transfer/python-s3transfer.mk
+++ package/python-s3transfer/python-s3transfer.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_S3TRANSFER_VERSION = 0.11.4
+PYTHON_S3TRANSFER_VERSION = 0.13.0
 PYTHON_S3TRANSFER_SOURCE = s3transfer-$(PYTHON_S3TRANSFER_VERSION).tar.gz
-PYTHON_S3TRANSFER_SITE = https://files.pythonhosted.org/packages/0f/ec/aa1a215e5c126fe5decbee2e107468f51d9ce190b9763cb649f76bb45938
+PYTHON_S3TRANSFER_SITE = https://files.pythonhosted.org/packages/ed/5d/9dcc100abc6711e8247af5aa561fc07c4a046f72f659c3adea9a449e191a
 PYTHON_S3TRANSFER_SETUP_TYPE = setuptools
 PYTHON_S3TRANSFER_LICENSE = Apache-2.0
 PYTHON_S3TRANSFER_LICENSE_FILES = LICENSE.txt
