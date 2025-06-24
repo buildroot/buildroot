5d9551d303c953aa34bad4ae6bc47e56f830dfe0
Raphaël Mélotte <raphael.melotte@mind.be>
2025-06-24 14:20:43 +0200

    package/nginx-modsecurity: update upstream URLs
    'https://github.com/SpiderLabs/ModSecurity-nginx' now redirects to
    'https://github.com/owasp-modsecurity/ModSecurity-nginx', so update our
    URLs accordingly.
    
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/nginx-modsecurity/nginx-modsecurity.mk
+++ package/nginx-modsecurity/nginx-modsecurity.mk
@@ -6,7 +6,7 @@
 
 NGINX_MODSECURITY_VERSION = 1.0.3
 NGINX_MODSECURITY_SOURCE = modsecurity-nginx-v$(NGINX_MODSECURITY_VERSION).tar.gz
-NGINX_MODSECURITY_SITE = https://github.com/SpiderLabs/ModSecurity-nginx/releases/download/v$(NGINX_MODSECURITY_VERSION)
+NGINX_MODSECURITY_SITE = https://github.com/owasp-modsecurity/ModSecurity-nginx/releases/download/v$(NGINX_MODSECURITY_VERSION)
 NGINX_MODSECURITY_LICENSE = Apache-2.0
 NGINX_MODSECURITY_LICENSE_FILES = LICENSE
 NGINX_MODSECURITY_DEPENDENCIES = libmodsecurity
