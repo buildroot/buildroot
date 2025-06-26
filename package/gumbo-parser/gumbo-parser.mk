9d8bde1172930c7979375c1fdccf1dace8071afd
Raphaël Mélotte <raphael.melotte@mind.be>
2025-06-26 13:19:17 +0200

    package/gumbo-parser: update upstream repository
    Development on Google's GitHub repository has stopped a long time ago.
    A fork exists on Codeberg, and multiple distributions (Fedora, Arch
    Linux, ...) are already using it (see [1]).
    
    Update the source URL to use the new upstream location.
    
    The new upstream has a different hash for the 0.10.1 tarball, so
    update it as well.
    
    [1]: https://repology.org/project/gumbo-parser/versions
    
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 1e106d8412e35b26a7cfa5c76a264cd8ef9cb325)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/gumbo-parser/gumbo-parser.mk
+++ package/gumbo-parser/gumbo-parser.mk
@@ -5,7 +5,8 @@
 ################################################################################
 
 GUMBO_PARSER_VERSION = 0.10.1
-GUMBO_PARSER_SITE = $(call github,google,gumbo-parser,v$(GUMBO_PARSER_VERSION))
+GUMBO_PARSER_SITE = https://codeberg.org/gumbo-parser/gumbo-parser/archive
+GUMBO_PARSER_SOURCE = v$(GUMBO_PARSER_VERSION).tar.gz
 GUMBO_PARSER_LICENSE = Apache-2.0
 GUMBO_PARSER_LICENSE_FILES = COPYING
 GUMBO_PARSER_INSTALL_STAGING = YES
