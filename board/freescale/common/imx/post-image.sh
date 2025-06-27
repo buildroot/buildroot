b3b22564d6fcebcdb66a155311086cae91dc92a1
Giulio Benetti <giulio.benetti@benettiengineering.com>
2025-06-27 17:35:45 +0200

    board/freescale/common/imx/post-image.sh: make it verbose on error
    Add set -e at the top of the script to make it verbose on error.
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- board/freescale/common/imx/post-image.sh
+++ board/freescale/common/imx/post-image.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 #
 # dtb_list extracts the list of DTB files from BR2_LINUX_KERNEL_INTREE_DTS_NAME
 # in ${BR_CONFIG}, then prints the corresponding list of file names for the
