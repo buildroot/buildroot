394a8fb406fec6bcf65f8f924b107920eb9405fb
Peter Korsgaard <peter@korsgaard.com>
2025-07-01 18:15:07 +0200

    package/jose: security bump to version 14
    Jose-13 fixed the following security issue:
    
    - CVE-2023-50967: latchset jose through version 11 allows attackers to cause
      a denial of service (CPU consumption) via a large p2c (aka PBES2 Count)
      value.
      https://github.com/latchset/jose/issues/151
    
    In addition, jose-14 worked around another DoS issue related to
    decompression:
    https://github.com/latchset/jose/pull/157
    
    Drop now upstreamed patches:
    
    - 0001-lib-hsh.c-rename-hsh-local-variable.patch: Upstream as of
      https://github.com/latchset/jose/commit/3d5b287243f87ce0243b23abd690d86c41fc499c
    
    - 0002-man-add-option-to-skip-building-man-pages.patch: Upstream after
      getting reworked to use -Ddocs=disabled as of
      https://github.com/latchset/jose/commit/786b426df018edf30a53e2d82155df20d13047c1
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    [Julien: remove .checkpackageignore entries to fix check-package errors]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/jose/jose.mk
+++ package/jose/jose.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-JOSE_VERSION = 11
+JOSE_VERSION = 14
 JOSE_SOURCE = jose-$(JOSE_VERSION).tar.xz
 JOSE_SITE = https://github.com/latchset/jose/releases/download/v$(JOSE_VERSION)
 JOSE_LICENSE = Apache-2.0
 JOSE_LICENSE_FILES = COPYING
 JOSE_INSTALL_STAGING = YES
 JOSE_DEPENDENCIES = host-pkgconf zlib jansson openssl
-JOSE_CONF_OPTS = -Dskip_manpages=true
+JOSE_CONF_OPTS = -Ddocs=disabled
 
 $(eval $(meson-package))
