14272869ebdeb07f7203e4d603384581bc16a215
Julien Olivain <ju.o@free.fr>
2025-07-04 21:57:40 +0200

    package/clpeak: bump version to 1.1.5
    For release notes since 1.1.2, see:
    https://github.com/krrishnarraj/clpeak/releases/tag/1.1.3
    https://github.com/krrishnarraj/clpeak/releases/tag/1.1.4
    https://github.com/krrishnarraj/clpeak/releases/tag/1.1.5
    
    The package license changed from Unlicensed to Apache-2.0 in upstream
    commit [1]. This commit updates _LICENSE and the license file hash
    accordingly.
    
    [1] https://github.com/krrishnarraj/clpeak/commit/65e572451330b4673712dfec352842f3123a9258
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Romain Naour <romain.naour@smile.fr>

--- package/clpeak/clpeak.mk
+++ package/clpeak/clpeak.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-CLPEAK_VERSION = 1.1.2
+CLPEAK_VERSION = 1.1.5
 CLPEAK_SITE = $(call github,krrishnarraj,clpeak,$(CLPEAK_VERSION))
-CLPEAK_LICENSE = Unlicense
+CLPEAK_LICENSE = Apache-2.0
 CLPEAK_LICENSE_FILES = LICENSE
 CLPEAK_DEPENDENCIES = libopencl opencl-clhpp
 
