6baca88959bf14b7d7ef409ce1628c699bcc40c1
Bernd Kuhls <bernd@kuhls.net>
2025-07-04 18:54:17 +0200

    package/fontconfig: bump version to 2.17.1
    Removed patch which is included in this release.
    
    Switched _SITE to gitlab:
    https://lists.freedesktop.org/archives/fontconfig/2025-March/007154.html
    
    Release notes:
    2.16.1: https://lists.freedesktop.org/archives/fontconfig/2025-March/007154.html
    2.16.2: https://lists.freedesktop.org/archives/fontconfig/2025-April/007168.html
    2.17.0: https://lists.freedesktop.org/archives/fontconfig/2025-June/007224.html
    2.17.1: https://lists.freedesktop.org/archives/fontconfig/2025-July/007229.html
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/fontconfig/fontconfig.mk
+++ package/fontconfig/fontconfig.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-FONTCONFIG_VERSION = 2.16.0
-FONTCONFIG_SITE = https://www.freedesktop.org/software/fontconfig/release
+FONTCONFIG_VERSION = 2.17.1
+FONTCONFIG_SITE = https://gitlab.freedesktop.org/api/v4/projects/890/packages/generic/fontconfig/$(FONTCONFIG_VERSION)
 FONTCONFIG_SOURCE = fontconfig-$(FONTCONFIG_VERSION).tar.xz
 FONTCONFIG_INSTALL_STAGING = YES
 FONTCONFIG_DEPENDENCIES = freetype expat host-pkgconf host-gperf \
