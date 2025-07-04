ff184d4e04d35677d053cea6e516b3d96acd922f
Bernd Kuhls <bernd@kuhls.net>
2025-07-04 18:52:16 +0200

    package/libpng: bump version to 1.6.50
    Release notes:
    https://raw.githubusercontent.com/pnggroup/libpng/v1.6.50/ANNOUNCE
    
    Switched _SITE to https.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libpng/libpng.mk
+++ package/libpng/libpng.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-LIBPNG_VERSION = 1.6.49
+LIBPNG_VERSION = 1.6.50
 LIBPNG_SERIES = 16
 LIBPNG_SOURCE = libpng-$(LIBPNG_VERSION).tar.xz
-LIBPNG_SITE = http://downloads.sourceforge.net/project/libpng/libpng$(LIBPNG_SERIES)/$(LIBPNG_VERSION)
+LIBPNG_SITE = https://downloads.sourceforge.net/project/libpng/libpng$(LIBPNG_SERIES)/$(LIBPNG_VERSION)
 LIBPNG_LICENSE = Libpng-2.0
 LIBPNG_LICENSE_FILES = LICENSE
 LIBPNG_CPE_ID_VENDOR = libpng
