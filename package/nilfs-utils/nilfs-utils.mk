95985d3524d5d767dd30cdf2308278cfcbbc7cb1
Matt Silva <dev@matt-silva.com>
2025-07-06 13:13:50 -0400

    package/nilfs-utils: install to staging
    nilfs-utils provides an "nilfs.h" header file and libnilfs.so that other
    packages which depend upon it may require in order to build.
    
    Signed-off-by: Matt Silva <dev@matt-silva.com>
    [Romain: add libnilfs.so in commit log]
    Signed-off-by: Romain Naour <romain.naour@smile.fr>

--- package/nilfs-utils/nilfs-utils.mk
+++ package/nilfs-utils/nilfs-utils.mk
@@ -9,6 +9,7 @@ NILFS_UTILS_SOURCE = nilfs-utils-$(NILFS_UTILS_VERSION).tar.bz2
 NILFS_UTILS_SITE = https://nilfs.sourceforge.io/download
 NILFS_UTILS_LICENSE = GPL-2.0+ (programs), LGPL-2.1+ (libraries)
 NILFS_UTILS_LICENSE_FILES = COPYING
+NILFS_UTILS_INSTALL_STAGING = YES
 
 # need libuuid, libblkid, libmount
 NILFS_UTILS_DEPENDENCIES = util-linux
