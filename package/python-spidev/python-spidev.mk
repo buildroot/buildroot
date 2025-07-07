3c46549f890977a805ecbf460bb2f022f482feaa
Michael Trimarchi <michael@amarulasolutions.com>
2025-07-07 08:05:07 +0200

    package/python-spidev: bump to version 3.7
    This is just a minor upgrade for fixed deprecation warning
    
    Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>
    [Julien: fix hash file spacing to resolve check-package error]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-spidev/python-spidev.mk
+++ package/python-spidev/python-spidev.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SPIDEV_VERSION = 3.6
+PYTHON_SPIDEV_VERSION = 3.7
 PYTHON_SPIDEV_SOURCE = spidev-$(PYTHON_SPIDEV_VERSION).tar.gz
-PYTHON_SPIDEV_SITE = https://files.pythonhosted.org/packages/c7/d9/401c0a7be089e02826cf2c201f489876b601f15be100fe391ef9c2faed83
+PYTHON_SPIDEV_SITE = https://files.pythonhosted.org/packages/b5/99/dd50af8200e224ce9412ad01cdbeeb5b39b2d61acd72138f2b92c4a6d619
 PYTHON_SPIDEV_SETUP_TYPE = setuptools
 PYTHON_SPIDEV_LICENSE = MIT
 PYTHON_SPIDEV_LICENSE_FILES = LICENSE
