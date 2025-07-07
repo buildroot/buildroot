6d23852980d2ce92804de2c4fced38317fba6952
Yegor Yefremov <yegorslists@googlemail.com>
2025-07-07 15:02:43 +0200

    package/socketcand: bump to version 0.6.1-103-g6dd5d33
    Switch to meson build system.
    
    Remove an upstreamed patch.
    
    The license files are now gathered in the LICENSES folder.
    
    Add support for the libsocketcan library.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    [Julien:
     - use "git describe --tags" format in commit title
     - use "git describe --tags --abbrev=40" in _VERSION
    ]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/socketcand/socketcand.mk
+++ package/socketcand/socketcand.mk
@@ -4,16 +4,23 @@
 #
 ################################################################################
 
-SOCKETCAND_VERSION = 69e2201222f015a1abe7a58ecf61325012cd91b1
+SOCKETCAND_VERSION = 0.6.1-103-g6dd5d33d4645ab221e8cd265c08607366e21ddf1
 SOCKETCAND_SITE = $(call github,linux-can,socketcand,$(SOCKETCAND_VERSION))
-SOCKETCAND_AUTORECONF = YES
 SOCKETCAND_LICENSE = BSD-3-Clause or GPL-2.0
-SOCKETCAND_LICENSE_FILES = socketcand.c
+SOCKETCAND_LICENSE_FILES = LICENSES/BSD-3-Clause LICENSES/GPL-2.0-only.txt
 
 ifeq ($(BR2_PACKAGE_LIBCONFIG),y)
 SOCKETCAND_DEPENDENCIES += libconfig
+SOCKETCAND_CONF_OPTS += -Dlibconfig=true
 else
-SOCKETCAND_CONF_OPTS += --without-config
+SOCKETCAND_CONF_OPTS += -Dlibconfig=false
 endif
 
-$(eval $(autotools-package))
+ifeq ($(BR2_PACKAGE_LIBSOCKETCAN),y)
+SOCKETCAND_DEPENDENCIES += libsocketcan
+SOCKETCAND_CONF_OPTS += -Dlibsocketcan=true
+else
+SOCKETCAND_CONF_OPTS += -Dlibsocketcan=false
+endif
+
+$(eval $(meson-package))
