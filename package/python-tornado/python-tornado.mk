d31694a8ef2d1d07de07081a46481f489a2f725b
Yegor Yefremov <yegorslists@googlemail.com>
2025-07-08 16:21:38 +0200

    package/python-tornado: bump to version 6.5.1
    For change log, see:
    
    https://github.com/tornadoweb/tornado/releases/tag/v6.5.1
    
    Drop an already upstreamed patch.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-tornado/python-tornado.mk
+++ package/python-tornado/python-tornado.mk
@@ -4,16 +4,13 @@
 #
 ################################################################################
 
-PYTHON_TORNADO_VERSION = 6.5
+PYTHON_TORNADO_VERSION = 6.5.1
 PYTHON_TORNADO_SOURCE = tornado-$(PYTHON_TORNADO_VERSION).tar.gz
-PYTHON_TORNADO_SITE = https://files.pythonhosted.org/packages/63/c4/bb3bd68b1b3cd30abc6411469875e6d32004397ccc4a3230479f86f86a73
+PYTHON_TORNADO_SITE = https://files.pythonhosted.org/packages/51/89/c72771c81d25d53fe33e3dca61c233b665b2780f21820ba6fd2c6793c12b
 PYTHON_TORNADO_LICENSE = Apache-2.0
 PYTHON_TORNADO_LICENSE_FILES = LICENSE
 PYTHON_TORNADO_CPE_ID_VENDOR = tornadoweb
 PYTHON_TORNADO_CPE_ID_PRODUCT = tornado
 PYTHON_TORNADO_SETUP_TYPE = setuptools
-
-# 0001-httputil-raise-errors-instead-of-logging-in.patch
-PYTHON_TORNADO_IGNORE_CVES += CVE-2025-47287
 
 $(eval $(python-package))
