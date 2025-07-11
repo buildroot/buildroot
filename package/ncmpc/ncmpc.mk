793c5ad89de81c656a51779e042f39dd0f9b4dc7
Tim Soubry <tim.soubry@mind.be>
2025-07-11 16:32:24 +0200

    package/ncmpc: bump version to 0.52
    Applying version bump of ncmpc to version 0.52 as proposed in [1].
    As from version 0.50 of ncmpc, fmt got added as a dependency [2]. This was
    also added to package/ncmpc/Config.in and package/ncmpc/ncmpc.mk.
    
    For release notes, see:
    https://github.com/MusicPlayerDaemon/ncmpc/blob/v0.52/NEWS
    
    Comparision between version 0.49 and 0.52:
    https://github.com/MusicPlayerDaemon/ncmpc/compare/v0.49..v0.52
    
    [1] https://lore.kernel.org/buildroot/20250707112536.1644236-1-tim.soubry@mind.be/T/#rb3b968fff23bac0298e33f473d63a1ad470d38e3
    [2] https://github.com/MusicPlayerDaemon/ncmpc/commit/c3b0055d06f8e8efab7f388646beb7645b90d063
    
    Signed-off-by: Tim Soubry <tim.soubry@mind.be>
    Tested-by: Andreas Ziegler <br015@umbiko.net>
    [Julien: change release note link to use tag]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/ncmpc/ncmpc.mk
+++ package/ncmpc/ncmpc.mk
@@ -5,13 +5,14 @@
 ################################################################################
 
 NCMPC_VERSION_MAJOR = 0
-NCMPC_VERSION = $(NCMPC_VERSION_MAJOR).49
+NCMPC_VERSION = $(NCMPC_VERSION_MAJOR).52
 NCMPC_SOURCE = ncmpc-$(NCMPC_VERSION).tar.xz
 NCMPC_SITE = http://www.musicpd.org/download/ncmpc/$(NCMPC_VERSION_MAJOR)
 NCMPC_DEPENDENCIES = \
 	host-pkgconf \
 	libmpdclient \
 	ncurses \
+	fmt \
 	$(if $(BR2_PACKAGE_LIBICONV),libiconv) \
 	$(TARGET_NLS_DEPENDENCIES)
 NCMPC_LICENSE = GPL-2.0+
