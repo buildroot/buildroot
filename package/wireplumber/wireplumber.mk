c42b26bb471a260e2bb3da7988af77f78b2b4280
Florian Larysch <fl@n621.de>
2025-07-14 15:36:54 +0200

    package/wireplumber: bump to 0.5.10
    Release notes:
    https://gitlab.freedesktop.org/pipewire/wireplumber/-/blob/0.5.10/NEWS.rst
    
    Signed-off-by: Florian Larysch <fl@n621.de>
    [Julien: update release note url to use tag]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/wireplumber/wireplumber.mk
+++ package/wireplumber/wireplumber.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WIREPLUMBER_VERSION = 0.5.5
+WIREPLUMBER_VERSION = 0.5.10
 WIREPLUMBER_SOURCE = wireplumber-$(WIREPLUMBER_VERSION).tar.bz2
 WIREPLUMBER_SITE = https://gitlab.freedesktop.org/pipewire/wireplumber/-/archive/$(WIREPLUMBER_VERSION)
 WIREPLUMBER_LICENSE = MIT
