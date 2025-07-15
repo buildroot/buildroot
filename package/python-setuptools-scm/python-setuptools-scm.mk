dccb15884a48ad4c11c9883540eb9b50473dda41
James Hilliard <james.hilliard1@gmail.com>
2025-07-15 16:53:35 -0600

    package/python-setuptools-scm: bump to version 8.3.1
    Drop no longer required host-python-typing-extensions dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@rnout.be>

--- package/python-setuptools-scm/python-setuptools-scm.mk
+++ package/python-setuptools-scm/python-setuptools-scm.mk
@@ -4,16 +4,15 @@
 #
 ################################################################################
 
-PYTHON_SETUPTOOLS_SCM_VERSION = 8.1.0
+PYTHON_SETUPTOOLS_SCM_VERSION = 8.3.1
 PYTHON_SETUPTOOLS_SCM_SOURCE = setuptools_scm-$(PYTHON_SETUPTOOLS_SCM_VERSION).tar.gz
-PYTHON_SETUPTOOLS_SCM_SITE = https://files.pythonhosted.org/packages/4f/a4/00a9ac1b555294710d4a68d2ce8dfdf39d72aa4d769a7395d05218d88a42
+PYTHON_SETUPTOOLS_SCM_SITE = https://files.pythonhosted.org/packages/b9/19/7ae64b70b2429c48c3a7a4ed36f50f94687d3bfcd0ae2f152367b6410dff
 PYTHON_SETUPTOOLS_SCM_LICENSE = MIT
 PYTHON_SETUPTOOLS_SCM_LICENSE_FILES = LICENSE
 PYTHON_SETUPTOOLS_SCM_SETUP_TYPE = pep517
 
 HOST_PYTHON_SETUPTOOLS_SCM_DEPENDENCIES = \
 	host-python-packaging \
-	host-python-setuptools \
-	host-python-typing-extensions
+	host-python-setuptools
 
 $(eval $(host-python-package))
