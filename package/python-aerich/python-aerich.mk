81cf89d3501b3037062c58d1743c94fd0ed18a60
James Hilliard <james.hilliard1@gmail.com>
2025-07-15 16:16:56 -0600

    package/python-aerich: bump to version 0.9.1
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Arnout Vandecappelle <arnout@rnout.be>

--- package/python-aerich/python-aerich.mk
+++ package/python-aerich/python-aerich.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_AERICH_VERSION = 0.9.0
+PYTHON_AERICH_VERSION = 0.9.1
 PYTHON_AERICH_SOURCE = aerich-$(PYTHON_AERICH_VERSION).tar.gz
-PYTHON_AERICH_SITE = https://files.pythonhosted.org/packages/2f/4b/1facb78fea47f790040035b94fdcc31630fabe1bda49ebc7a4f374da1757
+PYTHON_AERICH_SITE = https://files.pythonhosted.org/packages/a5/98/75c6e3053a14a14ffe86e2435698b960abe0bf03c114e67f966d92ff19e7
 PYTHON_AERICH_SETUP_TYPE = poetry
 PYTHON_AERICH_LICENSE = Apache-2.0
 PYTHON_AERICH_LICENSE_FILES = LICENSE
