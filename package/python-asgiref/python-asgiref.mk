358e85b966c865e69452b50437bfc4e2f05c9f0f
Marcus Hoffmann <buildroot@bubu1.eu>
2025-07-15 13:29:48 +0200

    package/python-asgiref: bump to 0.9.1
    Release Notes:
    
    * Fixed deletion of Local values affecting other contexts. (#523)
    
    * Skip CPython specific garbage collection test on pypy. (#521)
    
    3.9.0 (2025-07-03)
    ------------------
    
    * Adds support for Python 3.13.
    
    * Drops support for (end-of-life) Python 3.8.
    
    * Fixes an error with conflicting kwargs between AsyncToSync and the wrapped
      function. (#471)
    
    * Fixes Local isolation between asyncio Tasks. (#478)
    
    * Fixes a reference cycle in Local (#508)
    
    * Fixes a deadlock in CurrentThreadExecutor with nested async_to_sync →
      sync_to_async → async_to_sync → create_task calls. (#494)
    
    * The ApplicationCommunicator testing utility will now return the task result
      if it's already completed on send_input and receive_nothing. You may need to
      catch (e.g.) the asyncio.exceptions.CancelledError if sending messages to
      already finished consumers in your tests. (#505)
    
    Source: https://github.com/django/asgiref/blob/main/CHANGELOG.txt
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-asgiref/python-asgiref.mk
+++ package/python-asgiref/python-asgiref.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ASGIREF_VERSION = 3.8.1
+PYTHON_ASGIREF_VERSION = 3.9.1
 PYTHON_ASGIREF_SOURCE = asgiref-$(PYTHON_ASGIREF_VERSION).tar.gz
-PYTHON_ASGIREF_SITE = https://files.pythonhosted.org/packages/29/38/b3395cc9ad1b56d2ddac9970bc8f4141312dbaec28bc7c218b0dfafd0f42
+PYTHON_ASGIREF_SITE = https://files.pythonhosted.org/packages/90/61/0aa957eec22ff70b830b22ff91f825e70e1ef732c06666a805730f28b36b
 PYTHON_ASGIREF_SETUP_TYPE = setuptools
 PYTHON_ASGIREF_LICENSE = BSD-3-Clause
 PYTHON_ASGIREF_LICENSE_FILES = LICENSE
