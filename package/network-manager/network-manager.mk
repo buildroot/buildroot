5e3114cd2f8380ae1fbd15eec78b182efe382303
Fiona Klute (WIWA) <fiona.klute@gmx.de>
2025-07-16 16:32:39 +0200

    package/network-manager: bump version to 1.52.1
    Upstream changelog:
    https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/blob/1.52.1/NEWS?ref_type=tags
    
    Not mentioned in the changelog is a new build option to set the path
    of ip6tables [1], add it next to the existing iptables path setting.
    
    [1] https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/commit/57321f78c9aaad23099e80f7945ef996acbaa7cc
    
    Signed-off-by: Fiona Klute (WIWA) <fiona.klute@gmx.de>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/network-manager/network-manager.mk
+++ package/network-manager/network-manager.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 NETWORK_MANAGER_VERSION_MAJOR = 1.52
-NETWORK_MANAGER_VERSION = $(NETWORK_MANAGER_VERSION_MAJOR).0
+NETWORK_MANAGER_VERSION = $(NETWORK_MANAGER_VERSION_MAJOR).1
 NETWORK_MANAGER_SOURCE = NetworkManager-$(NETWORK_MANAGER_VERSION).tar.xz
 NETWORK_MANAGER_SITE = https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/releases/$(NETWORK_MANAGER_VERSION)/downloads
 NETWORK_MANAGER_INSTALL_STAGING = YES
@@ -30,6 +30,7 @@ NETWORK_MANAGER_CONF_OPTS = \
 	-Dtests=no \
 	-Dqt=false \
 	-Diptables=/usr/sbin/iptables \
+	-Dip6tables=/usr/sbin/ip6tables \
 	-Difupdown=false \
 	-Dnm_cloud_setup=false \
 	-Dsession_tracking_consolekit=false
