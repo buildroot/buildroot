e4451602ebee8ef6f6b354e8f9b6dcc80206f079
Titouan Christophe <titouan.christophe@mind.be>
2025-07-16 12:21:59 +0200

    package/python-aiohttp: security bump to v3.12.14
    This fixes the following vulnerability:
    - CVE-2025-53643:
        In aiohttp prior to version 3.12.14, the Python parser is vulnerable
        to a request smuggling vulnerability due to not parsing trailer
        sections of an HTTP request. If a pure Python version of aiohttp is
        installed (i.e. without the usual C extensions) or
        AIOHTTP_NO_EXTENSIONS is enabled, then an attacker may be able to
        execute a request smuggling attack to bypass certain firewalls or
        proxy protections. Version 3.12.14 contains a patch for this issue.
        https://www.cve.org/CVERecord?id=CVE-2025-53643
    
    Signed-off-by: Titouan Christophe <titouan.christophe@mind.be>
    Signed-off-by: Arnout Vandecappelle <arnout@rnout.be>

--- package/python-aiohttp/python-aiohttp.mk
+++ package/python-aiohttp/python-aiohttp.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_AIOHTTP_VERSION = 3.11.14
+PYTHON_AIOHTTP_VERSION = 3.12.14
 PYTHON_AIOHTTP_SOURCE = aiohttp-$(PYTHON_AIOHTTP_VERSION).tar.gz
-PYTHON_AIOHTTP_SITE = https://files.pythonhosted.org/packages/6c/96/91e93ae5fd04d428c101cdbabce6c820d284d61d2614d00518f4fa52ea24
+PYTHON_AIOHTTP_SITE = https://files.pythonhosted.org/packages/e6/0b/e39ad954107ebf213a2325038a3e7a506be3d98e1435e1f82086eec4cde2
 PYTHON_AIOHTTP_SETUP_TYPE = setuptools
 PYTHON_AIOHTTP_LICENSE = Apache-2.0
 PYTHON_AIOHTTP_LICENSE_FILES = LICENSE.txt
