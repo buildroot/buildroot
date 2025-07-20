325fa4b316bcee70947ce10fcdfd0aaba072337b
Petr Vorel <petr.vorel@gmail.com>
2025-07-20 16:30:47 +0200

    package/ltp-testsuite: backport musl build fixes
    When building LTP with a musl toolchain with kernel headers >= 6.14,
    compilation fails with error:
    
        In file included from listmount02.c:22:
        listmount.h: In function ‘listmount’:
        listmount.h:18:16: error: variable ‘req’ has initializer but incomplete type
           18 |         struct mnt_id_req req = {
              |                ^~~~~~~~~~
    
    The issue can be reproduced with commands:
    
        cat >.config <<EOF
        BR2_aarch64=y
        BR2_TOOLCHAIN_BUILDROOT_MUSL=y
        BR2_LINUX_KERNEL=y
        BR2_LINUX_KERNEL_LATEST_VERSION=y
        BR2_LINUX_KERNEL_USE_ARCH_DEFAULT_CONFIG=y
        BR2_PACKAGE_LTP_TESTSUITE=y
        EOF
        make olddefconfig
        make ltp-testsuite
    
    Backport 2 commits from upstream which fixes LTP on musl on
    kernel >= 6.14.
    
    Signed-off-by: Petr Vorel <petr.vorel@gmail.com>
    [Julien:
     - add commands to reproduce the issue in commit log
     - move _AUTORECONF up to be nearer _VERSION
    ]
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 0ccd9300cdca0358cd7b10c3f87a3cbac1b328d6)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/ltp-testsuite/ltp-testsuite.mk
+++ package/ltp-testsuite/ltp-testsuite.mk
@@ -11,6 +11,9 @@ LTP_TESTSUITE_SITE = https://github.com/linux-test-project/ltp/releases/download
 LTP_TESTSUITE_LICENSE = GPL-2.0, GPL-2.0+
 LTP_TESTSUITE_LICENSE_FILES = COPYING
 
+# 0001-configure-Fix-build-on-kernel-6.14-headers.patch
+LTP_TESTSUITE_AUTORECONF = YES
+
 LTP_TESTSUITE_CONF_OPTS += --disable-metadata
 
 ifeq ($(BR2_PACKAGE_LTP_TESTSUITE_OPEN_POSIX),y)
