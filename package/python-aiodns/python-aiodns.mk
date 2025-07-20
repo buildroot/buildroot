611fd9b8e7ac432bc889264003beae20d1a1652c
James Hilliard <james.hilliard1@gmail.com>
2025-07-20 13:06:41 -0600

    package/python-aiodns: bump to version 3.5.0
    License hash changed due to formatting:
    https://github.com/aio-libs/aiodns/commit/924b981471b676e90a1f38b1e20223c18b899a0b
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-aiodns/python-aiodns.mk
+++ package/python-aiodns/python-aiodns.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_AIODNS_VERSION = 3.2.0
+PYTHON_AIODNS_VERSION = 3.5.0
 PYTHON_AIODNS_SOURCE = aiodns-$(PYTHON_AIODNS_VERSION).tar.gz
-PYTHON_AIODNS_SITE = https://files.pythonhosted.org/packages/e7/84/41a6a2765abc124563f5380e76b9b24118977729e25a84112f8dfb2b33dc
+PYTHON_AIODNS_SITE = https://files.pythonhosted.org/packages/17/0a/163e5260cecc12de6abc259d158d9da3b8ec062ab863107dcdb1166cdcef
 PYTHON_AIODNS_SETUP_TYPE = setuptools
 PYTHON_AIODNS_LICENSE = MIT
 PYTHON_AIODNS_LICENSE_FILES = LICENSE
