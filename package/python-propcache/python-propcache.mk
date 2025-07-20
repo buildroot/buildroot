80c44ebfd6ac05c6a3d61fae209d3859e16c8ce7
James Hilliard <james.hilliard1@gmail.com>
2025-07-20 15:59:07 -0600

    package/python-propcache: bump to version 0.3.2
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 784f52b64788ab4e3686c9e20d6c17e574f2d8e0)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/python-propcache/python-propcache.mk
+++ package/python-propcache/python-propcache.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PROPCACHE_VERSION = 0.2.1
+PYTHON_PROPCACHE_VERSION = 0.3.2
 PYTHON_PROPCACHE_SOURCE = propcache-$(PYTHON_PROPCACHE_VERSION).tar.gz
-PYTHON_PROPCACHE_SITE = https://files.pythonhosted.org/packages/20/c8/2a13f78d82211490855b2fb303b6721348d0787fdd9a12ac46d99d3acde1
+PYTHON_PROPCACHE_SITE = https://files.pythonhosted.org/packages/a6/16/43264e4a779dd8588c21a70f0709665ee8f611211bdd2c87d952cfa7c776
 PYTHON_PROPCACHE_SETUP_TYPE = setuptools
 PYTHON_PROPCACHE_LICENSE = Apache-2.0
 PYTHON_PROPCACHE_LICENSE_FILES = LICENSE
