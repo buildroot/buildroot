fdf9173555a5eccb95e2f8988b1c21eccc371916
Julien Olivain <ju.o@free.fr>
2025-07-22 01:03:53 +0200

    package/python-magic-wormhole: bump to version 0.19.2
    For change log since version 0.17.0, see:
    https://github.com/magic-wormhole/magic-wormhole/blob/0.19.2/NEWS.md
    
    Note that, since v0.18.0, magic-wormhole is using python-qrcode. This
    commit reflects that change by adding this new runtime dependency in
    Config.in.
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-magic-wormhole/python-magic-wormhole.mk
+++ package/python-magic-wormhole/python-magic-wormhole.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_MAGIC_WORMHOLE_VERSION = 0.17.0
+PYTHON_MAGIC_WORMHOLE_VERSION = 0.19.2
 PYTHON_MAGIC_WORMHOLE_SOURCE = magic_wormhole-$(PYTHON_MAGIC_WORMHOLE_VERSION).tar.gz
-PYTHON_MAGIC_WORMHOLE_SITE = https://files.pythonhosted.org/packages/1b/a8/32a54e75643206665f569dac6ab19727aefb508b148882f1d05dff003667
+PYTHON_MAGIC_WORMHOLE_SITE = https://files.pythonhosted.org/packages/e0/5b/4aff155b8e8ead4bc7c6aa3d1c19dc75aac2315e0c9b12f4e2f246b40141
 PYTHON_MAGIC_WORMHOLE_SETUP_TYPE = setuptools
 PYTHON_MAGIC_WORMHOLE_LICENSE = MIT
 PYTHON_MAGIC_WORMHOLE_LICENSE_FILES = LICENSE
