fa043ddeab68a194fe8275e9406a43d2cbf0bfc6
Julien Olivain <ju.o@free.fr>
2025-07-22 00:23:52 +0200

    package/octave: bump to version 10.2.0
    See release announce:
    https://octave.org/news/release/2025/06/03/octave-10.2.0-released.html
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/octave/octave.mk
+++ package/octave/octave.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OCTAVE_VERSION = 10.1.0
+OCTAVE_VERSION = 10.2.0
 OCTAVE_SITE = $(BR2_GNU_MIRROR)/octave
 OCTAVE_SOURCE = octave-$(OCTAVE_VERSION).tar.lz
 OCTAVE_LICENSE = GPL-3.0+
