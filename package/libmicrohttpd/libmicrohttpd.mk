2fd793312563c6fe9bdf8fb8632e0006a31092ec
Scott Fan <fancp2007@gmail.com>
2025-07-26 10:55:55 +0800

    package/libmicrohttpd: bump version to 1.0.2
    Fix double-close bugs on bind() errors reported by MC on the list.
    
    Removed MHD2 draft code, now in libmicrohttpd2.git.
    
    Changelog: https://git.gnunet.org/libmicrohttpd.git/tree/ChangeLog
    
    Signed-off-by: Scott Fan <fancp2007@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libmicrohttpd/libmicrohttpd.mk
+++ package/libmicrohttpd/libmicrohttpd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBMICROHTTPD_VERSION = 1.0.1
+LIBMICROHTTPD_VERSION = 1.0.2
 LIBMICROHTTPD_SITE = $(BR2_GNU_MIRROR)/libmicrohttpd
 LIBMICROHTTPD_LICENSE_FILES = COPYING
 LIBMICROHTTPD_CPE_ID_VENDOR = gnu
