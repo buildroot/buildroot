cfb6cf9fbe7ae6e182dd6ba21dd6cb8e9197e92f
Thomas Perale <thomas.perale@mind.be>
2025-07-27 11:55:24 +0200

    package/orc: add CPE identifier
    The cpe:2.3:a:gstreamer:orc:0.4.34:*:*:*:*:*:*:* is
    valid for this package.
    
    See https://nvd.nist.gov/products/cpe/detail/37DFA009-0707-4825-B28A-62451B1E2AFC
    
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 279cb43814abe9e3452541a0e69e2f3945aa2b1b)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/orc/orc.mk
+++ package/orc/orc.mk
@@ -9,6 +9,7 @@ ORC_SOURCE = orc-$(ORC_VERSION).tar.xz
 ORC_SITE = http://gstreamer.freedesktop.org/data/src/orc
 ORC_LICENSE = BSD-2-Clause, BSD-3-Clause
 ORC_LICENSE_FILES = COPYING
+ORC_CPE_ID_VENDOR = gstreamer
 ORC_INSTALL_STAGING = YES
 ORC_DEPENDENCIES = host-orc
 ORC_CONF_OPTS = \
