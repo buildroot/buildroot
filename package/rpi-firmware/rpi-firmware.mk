53c0b8c2f24f0c0816206ce643227fc4cc7de739
Peter Seiderer <ps.report@gmx.net>
2025-08-03 16:42:07 +0200

    package/rpi-firmware: bump version to 95be71b
    Version 95be71b matches with kernel 6.12.41.
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Tested-by: Fiona Klute (WIWA) <fiona.klute@gmx.de>
    [Fiona: tested on CM4 and Zero 2W, with the RPi bootloader
            booting into U-Boot]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 # Please keep in sync with configs/raspberrypi*_deconfig
-RPI_FIRMWARE_VERSION = 1.20250326
+RPI_FIRMWARE_VERSION = 95be71b8c0f63f03dc06dd0e4c2e5535e6fb4a93
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
