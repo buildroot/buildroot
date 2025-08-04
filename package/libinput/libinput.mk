6b607a774403f7cbdfd968a80ec1c5bcf7eff524
Bernd Kuhls <bernd@kuhls.net>
2025-08-04 20:51:59 +0200

    package/libinput: bump version to 1.29.0
    Release notes:
    https://lists.freedesktop.org/archives/wayland-devel/2025-March/044026.html
    https://lists.freedesktop.org/archives/wayland-devel/2025-April/044102.html
    https://lore.freedesktop.org/wayland-devel/20250731092534.GA4100432@quokka/T/
    
    Upstream changed mtdev into an optional dependency:
    https://gitlab.freedesktop.org/libinput/libinput/-/commit/27f4b0ae744a2b2655755188f2224dd5f8c4bdeb
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libinput/libinput.mk
+++ package/libinput/libinput.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-LIBINPUT_VERSION = 1.27.0
+LIBINPUT_VERSION = 1.29.0
 LIBINPUT_SOURCE = libinput-$(LIBINPUT_VERSION).tar.bz2
 LIBINPUT_SITE = https://gitlab.freedesktop.org/libinput/libinput/-/archive/$(LIBINPUT_VERSION)
-LIBINPUT_DEPENDENCIES = host-pkgconf libevdev mtdev udev
+LIBINPUT_DEPENDENCIES = host-pkgconf libevdev udev
 LIBINPUT_INSTALL_STAGING = YES
 LIBINPUT_LICENSE = MIT
 LIBINPUT_LICENSE_FILES = COPYING
@@ -29,6 +29,13 @@ LIBINPUT_DEPENDENCIES += xlib_libX11
 endif
 else
 LIBINPUT_CONF_OPTS += -Ddebug-gui=false
+endif
+
+ifeq ($(BR2_PACKAGE_MTDEV),y)
+LIBINPUT_CONF_OPTS += -Dmtdev=true
+LIBINPUT_DEPENDENCIES += mtdev
+else
+LIBINPUT_CONF_OPTS += -Dmtdev=false
 endif
 
 ifeq ($(BR2_PACKAGE_LIBINPUT_PYTHON_TOOLS),)
