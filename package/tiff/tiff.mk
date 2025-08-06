83e3ea0ed373d8143af43399870a0ebbe3390015
Thomas Perale <thomas.perale@mind.be>
2025-08-06 22:20:28 +0200

    package/tiff: add patches to fix CVE-2025-8176
    Fix the following vulnerability:
    
    - CVE-2025-8176
    
        A vulnerability was found in LibTIFF up to 4.7.0. It has been declared
        as critical. This vulnerability affects the function get_histogram of
        the file tools/tiffmedian.c. The manipulation leads to use after free.
        The attack needs to be approached locally. The exploit has been
        disclosed to the public and may be used. The patch is identified as
        fe10872e53efba9cc36c66ac4ab3b41a839d5172. It is recommended to apply a
        patch to fix this issue.
    
    For more information, see:
      - https://www.cve.org/CVERecord?id=CVE-2025-8176
      - https://gitlab.com/libtiff/libtiff/-/merge_requests/727
    
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit b3974df96614b08d418b291b86248ea0ff93c462)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/tiff/tiff.mk
+++ package/tiff/tiff.mk
@@ -13,6 +13,9 @@ TIFF_CPE_ID_VENDOR = libtiff
 TIFF_CPE_ID_PRODUCT = libtiff
 TIFF_INSTALL_STAGING = YES
 
+# 0001-don-t-skip-the-first-line-of-the-input-image.patch, 0002-fix-tiffmedian-bug.patch, 0003-conflict-resolution.patch
+TIFF_IGNORE_CVES += CVE-2025-8176
+
 # webp has a (optional) dependency on tiff, so we can't have webp
 # support in tiff, or that would create a circular dependency.
 TIFF_CONF_OPTS = \
