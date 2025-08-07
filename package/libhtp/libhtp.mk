35a007be1a542a6bf1905a568d9ba155d3cc91b8
Thomas Perale <thomas.perale@mind.be>
2025-08-07 22:54:39 +0200

    package/libhtp: security bump to v0.5.51
    For more information on the release, see:
     - https://github.com/OISF/libhtp/releases/tag/0.5.51
     - https://github.com/OISF/libhtp/releases/tag/0.5.50
     - https://github.com/OISF/libhtp/compare/0.5.49...0.5.51
    
    This fixes the following vulnerability:
    
    - CVE-2025-53537
    
        LibHTP is a security-aware parser for the HTTP protocol and its
        related bits and pieces. In versions 0.5.50 and below, there is a
        traffic-induced memory leak that can starve the process of memory,
        leading to loss of visibility. To workaround this issue, set
        `suricata.yaml app-layer.protocols.http.libhtp.default-config.lzma-
        enabled` to false. This issue is fixed in version 0.5.51.
    
    For more information, see:
      - https://www.cve.org/CVERecord?id=CVE-2025-53537
      - https://github.com/OISF/libhtp/security/advisories/GHSA-v3qq-h8mh-vph7
    
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 0e450a0b0837e74d1378c25c1254dfc3696c3262)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/libhtp/libhtp.mk
+++ package/libhtp/libhtp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBHTP_VERSION = 0.5.49
+LIBHTP_VERSION = 0.5.51
 LIBHTP_SITE = $(call github,OISF,libhtp,$(LIBHTP_VERSION))
 LIBHTP_LICENSE = BSD-3-Clause
 LIBHTP_LICENSE_FILES = LICENSE
