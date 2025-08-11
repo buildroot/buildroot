c3ceae347284e0bb018b074520e317160b3d16dc
Francois Perrad <francois.perrad@gadz.org>
2025-08-11 10:19:44 +0200

    package/lua-zlib: bump to version 1.3
    Changelog:
    https://github.com/brimworks/lua-zlib/releases/tag/v1.3
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    [Julien: add link to changelog]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/lua-zlib/lua-zlib.mk
+++ package/lua-zlib/lua-zlib.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LUA_ZLIB_VERSION = 1.2-0
+LUA_ZLIB_VERSION = 1.3-0
 LUA_ZLIB_SUBDIR = lua-zlib
 LUA_ZLIB_LICENSE = MIT
 LUA_ZLIB_DEPENDENCIES = zlib
