de2acc9071fb2c32a1bd95a75635d21f93685819
Thomas Perale <thomas.perale@mind.be>
2025-08-17 11:31:18 +0200

    python-yarl: bump to v1.20.1
    Since the python-cython bump to v3.1.2 (see [1]), python-yarl has been
    failing on the autobuilder with the following error message:
    
    ```
    ERROR Missing dependencies:
    	Cython~=3.0.0; python_version >= "3.12"
    make: *** [package/pkg-generic.mk:273: /home/buildroot/instance-0/output-1/build/python-yarl-1.18.3/.stamp_built] Error 1
    ```
    
    The cython dependency has been addressed in python-yarl v1.20.1, for
    more information see the github issue [2].
    
    For more information on the release, see:
    
      - https://github.com/aio-libs/yarl/releases/tag/v1.19.0
      - https://github.com/aio-libs/yarl/releases/tag/v1.20.0
      - https://github.com/aio-libs/yarl/releases/tag/v1.20.1
    
    [1] b536caaec0 package/python-cython: bump to version 3.1.2
    [2] https://github.com/aio-libs/yarl/pull/1514
    
    Fixes: https://autobuild.buildroot.org/results/d36/d367b69b85a65fa94e923ecff3ba03723b2a6e88
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>
    Signed-off-by: Arnout Vandecappelle <arnout@rnout.be>
    (cherry picked from commit 0bd88146ee355109faf55f73133dc8d8632d8ba1)
    Signed-off-by: Titouan Christophe <titouan.christophe@mind.be>

--- package/python-yarl/python-yarl.mk
+++ package/python-yarl/python-yarl.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_YARL_VERSION = 1.18.3
+PYTHON_YARL_VERSION = 1.20.1
 PYTHON_YARL_SOURCE = yarl-$(PYTHON_YARL_VERSION).tar.gz
-PYTHON_YARL_SITE = https://files.pythonhosted.org/packages/b7/9d/4b94a8e6d2b51b599516a5cb88e5bc99b4d8d4583e468057eaa29d5f0918
+PYTHON_YARL_SITE = https://files.pythonhosted.org/packages/3c/fb/efaa23fa4e45537b827620f04cf8f3cd658b76642205162e072703a5b963
 PYTHON_YARL_LICENSE = Apache-2.0
 PYTHON_YARL_LICENSE_FILES = LICENSE
 PYTHON_YARL_SETUP_TYPE = setuptools
