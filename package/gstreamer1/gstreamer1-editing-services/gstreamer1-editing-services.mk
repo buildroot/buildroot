8717624fea7d23d3c901f329b9aadf353c132c4a
Peter Korsgaard <peter@korsgaard.com>
2025-08-20 09:11:27 +0200

    package/gstreamer1-editing-services: bump to version 1.24.13
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit ed03bdb360cfc4ec9bd5c21ae9cd4b17cad37adb)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/gstreamer1/gstreamer1-editing-services/gstreamer1-editing-services.mk
+++ package/gstreamer1/gstreamer1-editing-services/gstreamer1-editing-services.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GSTREAMER1_EDITING_SERVICES_VERSION = 1.24.11
+GSTREAMER1_EDITING_SERVICES_VERSION = 1.24.13
 GSTREAMER1_EDITING_SERVICES_SOURCE = gst-editing-services-$(GSTREAMER1_EDITING_SERVICES_VERSION).tar.xz
 GSTREAMER1_EDITING_SERVICES_SITE = https://gstreamer.freedesktop.org/src/gstreamer-editing-services
 GSTREAMER1_EDITING_SERVICES_LICENSE = LGPL-2.0+
