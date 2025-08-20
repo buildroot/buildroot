f001f40c8bf74ea7290606ffb9a79daa13f9daac
Peter Korsgaard <peter@korsgaard.com>
2025-08-20 09:11:17 +0200

    package/gstreamer1: bump to version 1.24.13
    For various bugfixes. For details, see the release notes:
    https://gstreamer.freedesktop.org/releases/1.24/
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/gstreamer1/gstreamer1/gstreamer1.mk
+++ package/gstreamer1/gstreamer1/gstreamer1.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GSTREAMER1_VERSION = 1.24.11
+GSTREAMER1_VERSION = 1.24.13
 GSTREAMER1_SOURCE = gstreamer-$(GSTREAMER1_VERSION).tar.xz
 GSTREAMER1_SITE = https://gstreamer.freedesktop.org/src/gstreamer
 GSTREAMER1_INSTALL_STAGING = YES
