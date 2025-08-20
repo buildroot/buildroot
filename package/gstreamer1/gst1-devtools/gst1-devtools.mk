6ac0fb4b99a47954e961dcc58f1136d06abbc1d7
Peter Korsgaard <peter@korsgaard.com>
2025-08-20 09:11:22 +0200

    package/gst1-devtools: bump to version 1.24.13
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit d730e330ae4cac0eb69852afadbf614c098efd79)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/gstreamer1/gst1-devtools/gst1-devtools.mk
+++ package/gstreamer1/gst1-devtools/gst1-devtools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GST1_DEVTOOLS_VERSION = 1.24.11
+GST1_DEVTOOLS_VERSION = 1.24.13
 GST1_DEVTOOLS_SOURCE = gst-devtools-$(GST1_DEVTOOLS_VERSION).tar.xz
 GST1_DEVTOOLS_SITE = https://gstreamer.freedesktop.org/src/gst-devtools
 GST1_DEVTOOLS_LICENSE = LGPL-2.1+
