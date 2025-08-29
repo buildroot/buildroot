65c245324dcacff304843efa438d7012b8a4efcb
Peter Korsgaard <peter@korsgaard.com>
2025-08-29 07:49:36 +0200

    package/python-h2: security bump to version 4.3.0
    Fixes the following security vulnerability:
    
    - CVE-2025-57804: HTTP/2 request splitting via CRLF injection
    
      https://github.com/python-hyper/h2/security/advisories/GHSA-847f-9342-265h
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 2389965eaf8ba396f74e35d4558be79bf1e8c8a6)
    Signed-off-by: Titouan Christophe <titouan.christophe@mind.be>

--- package/python-h2/python-h2.mk
+++ package/python-h2/python-h2.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_H2_VERSION = 4.1.0
+PYTHON_H2_VERSION = 4.3.0
 PYTHON_H2_SOURCE = h2-$(PYTHON_H2_VERSION).tar.gz
-PYTHON_H2_SITE = https://files.pythonhosted.org/packages/2a/32/fec683ddd10629ea4ea46d206752a95a2d8a48c22521edd70b142488efe1
+PYTHON_H2_SITE = https://files.pythonhosted.org/packages/1d/17/afa56379f94ad0fe8defd37d6eb3f89a25404ffc71d4d848893d270325fc
 PYTHON_H2_SETUP_TYPE = setuptools
 PYTHON_H2_LICENSE = MIT
 PYTHON_H2_LICENSE_FILES = LICENSE
