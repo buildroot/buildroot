89d924939df8720f399d5b7991609a32644f6f5b
Thomas Perale <thomas.perale@mind.be>
2025-08-29 16:49:00 +0200

    package/go: fix CPE id
    This patch fixes the CPE ID for both go-bin and go-src packages.
    
    The go-src package did had a CPE ID because GO_SRC_CPE_ID_VENDOR is
    defined but the generated CPE ID would be the following:
    
    cpe:2.3:a:golang:go-src:*:*:*:*:*:*:*:*
    
    The CPE product go-src doesn't match to anything and should be 'go'
    instead.
    
    For go-bin no CPE_ID variable were defined so no CPE ID would be
    generated if using a pre-build toolchain.
    
    This patch duplicate the definition of the CPE_ID variables, I didn't
    put the definition in the 'package/go/go.mk' file to avoid generate a
    CPE for the go virtual package.
    
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 1869b4065c6b4df909f5623719c818c0cc6394d2)
    Signed-off-by: Titouan Christophe <titouan.christophe@mind.be>

--- package/go/go-bin/go-bin.mk
+++ package/go/go-bin/go-bin.mk
@@ -4,12 +4,15 @@
 #
 ################################################################################
 
+GO_BIN_VERSION = $(GO_VERSION)
 GO_BIN_SITE = https://go.dev/dl
 GO_BIN_SOURCE = go$(GO_VERSION).linux-$(call qstrip, $(BR2_PACKAGE_HOST_GO_BIN_HOST_ARCH)).tar.gz
 GO_BIN_DL_SUBDIR = go
 HOST_GO_BIN_ACTUAL_SOURCE_TARBALL = go$(GO_VERSION).src.tar.gz
 GO_BIN_LICENSE = BSD-3-Clause
 GO_BIN_LICENSE_FILES = LICENSE
+GO_BIN_CPE_ID_VENDOR = golang
+GO_BIN_CPE_ID_PRODUCT = go
 
 HOST_GO_BIN_PROVIDES = host-go
 
