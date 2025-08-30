2a972212b43100dadbe9c2337264849f3b51552d
Vladimir Oltean <olteanv@gmail.com>
2025-08-30 20:33:12 +0300

    package/qoriq-restool: new package
    Introduce a package for the restool program, which permits the user to
    modify the network configuration of the DPAA2 subsystem and create one
    that is adequate to their use case.
    
    We integrate the master branch, which is effectively the
    lf-6.12.20-2.0.0 release tag, plus some extra fixups specifically made
    for the Buildroot integration:
    - commit b44748ed0bb3 ("Avoid use of non-portable __WORDSIZE when
      defining BITS_PER_LONG")
    - commit b4a734f3512b ("restool: fix format string for 64 bit time_t in
      parse_obj_command()")
    
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>
    Reviewed-by: Vincent Jardin <vjardin@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/qoriq-restool/qoriq-restool.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# qoriq-restool
+#
+################################################################################
+
+QORIQ_RESTOOL_VERSION = lf-6.12.20-2.0.0-3-gb44748e
+QORIQ_RESTOOL_SITE = $(call github,nxp-qoriq,restool,$(QORIQ_RESTOOL_VERSION))
+QORIQ_RESTOOL_LICENSE = BSD-3-Clause or GPL-2.0-or-later
+QORIQ_RESTOOL_LICENSE_FILES = LICENSE
+
+define QORIQ_RESTOOL_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) \
+		CC="$(TARGET_CC)" \
+		CROSS_COMPILE="$(TARGET_CROSS)"
+endef
+
+define QORIQ_RESTOOL_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) \
+		MANPAGE="" DESTDIR=$(TARGET_DIR) \
+		prefix=/usr install
+endef
+
+$(eval $(generic-package))
