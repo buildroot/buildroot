fa4148d45a0e4bed3c8444c6ebc41bb7444580c7
Peter Korsgaard <peter@korsgaard.com>
2025-09-01 10:48:24 +0200

    package/wipe: autoreconf to fix configure with >= gcc-15
    Fixes https://autobuild.buildroot.net/results/9d4e2f156d76e2acc83e3a25adb5e5cf0705e7c6/
    
    configure:781: checking whether the C compiler (/home/buildroot/instance-0/output-1/host/bin/armeb-buildroot-linux-gnueabi-gcc -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64  -Og -g0 -D_FORTIFY_SOURCE=1 ) works
    configure:797: /home/buildroot/instance-0/output-1/host/bin/armeb-buildroot-linux-gnueabi-gcc -o conftest -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64  -Og -g0 -D_FORTIFY_SOURCE=1 -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64  conftest.c  1>&5
    configure:794:1: error: return type defaults to 'int' [-Wimplicit-int]
      794 | main(){return(0);}
          | ^~~~
    configure: failed program was:
    
     #line 792 "configure"
     #include "confdefs.h"
    
     main(){return(0);}
    
    Wipe uses a very old configure script where the compile checks are missing
    the return type on main(), breaking the configure step with >= gcc-15.
    
    As a workaround, use AUTORECONF to regenerate the configure script.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/wipe/wipe.mk
+++ package/wipe/wipe.mk
@@ -9,6 +9,8 @@ WIPE_SITE = http://downloads.sourceforge.net/project/wipe/wipe/$(WIPE_VERSION)
 WIPE_SOURCE = wipe-$(WIPE_VERSION).tar.bz2
 WIPE_LICENSE = GPL-2.0+
 WIPE_LICENSE_FILES = LICENSE
+# configure script not compatible with >= gcc-15
+WIPE_AUTORECONF = YES
 
 define WIPE_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/wipe $(TARGET_DIR)/usr/bin/wipe
