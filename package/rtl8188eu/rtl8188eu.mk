b73204ed110e388e370550f0ca6e838243a15713
Giulio Benetti <giulio.benetti@benettiengineering.com>
2025-09-03 21:55:06 +0200

    package/rtl8188eu: fix build failure with linux 6.16
    Bump to version 2025-09-03 on branch v5.2.2.4 to fix build failure with
    Linux 6.16.
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rtl8188eu/rtl8188eu.mk
+++ package/rtl8188eu/rtl8188eu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTL8188EU_VERSION = 702a90b02edca722cc0d139260ba02c23f406333
+RTL8188EU_VERSION = c3104bd094970dd5944e2b28141b552630933bde
 RTL8188EU_SITE = $(call github,benetti-engineering,rtl8188eu,$(RTL8188EU_VERSION))
 RTL8188EU_LICENSE = GPL-2.0, proprietary (rtl8188eufw.bin firmware blob)
 RTL8188EU_LICENSE_FILES = COPYING
