232718e303904683a06c7c6cff7476923c5a6fcf
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2025-09-09 09:26:52 +0200

    package/chocolate-doom: bump to 3.1.1 to fix GCC 15.x issues
    Changes in 3.1.0:
    
      https://github.com/chocolate-doom/chocolate-doom/releases/tag/chocolate-doom-3.1.0
    
    Changes in 3.1.1:
    
      https://github.com/chocolate-doom/chocolate-doom/releases/tag/chocolate-doom-3.1.1
    
    The SDL2_mixer and SDL2_net dependencies are now optional since 3.1.0,
    so adjust the packaging accordingly.
    
    Also, the license file was renamed upstream in commit:
    https://github.com/chocolate-doom/chocolate-doom/commit/91a66a7ecba5e0dea87c8d98237d6a224f13347a
    So _LICENSE_FILES and the .hash file are also updated to reflect
    that change.
    
    Fixes:
    
      https://autobuild.buildroot.net/results/90a01c5925e7f5f50b3ea06d6ea7b0da66814a15/
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    [Julien: fix license file and hash]
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit be466c265ba09ed47ec1eb40f006dd17753e69d1)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/chocolate-doom/chocolate-doom.mk
+++ package/chocolate-doom/chocolate-doom.mk
@@ -4,13 +4,15 @@
 #
 ################################################################################
 
-CHOCOLATE_DOOM_VERSION = 3.0.1
-CHOCOLATE_DOOM_SITE = http://www.chocolate-doom.org/downloads/$(CHOCOLATE_DOOM_VERSION)
+CHOCOLATE_DOOM_VERSION = 3.1.1
+CHOCOLATE_DOOM_SITE = $(call github,chocolate-doom,chocolate-doom,chocolate-doom-$(CHOCOLATE_DOOM_VERSION))
 CHOCOLATE_DOOM_LICENSE = GPL-2.0+
-CHOCOLATE_DOOM_LICENSE_FILES = COPYING
+CHOCOLATE_DOOM_LICENSE_FILES = COPYING.md
 CHOCOLATE_DOOM_CPE_ID_VENDOR = chocolate-doom
 CHOCOLATE_DOOM_CPE_ID_PRODUCT = chocolate_doom
-CHOCOLATE_DOOM_DEPENDENCIES = host-pkgconf sdl2 sdl2_mixer sdl2_net
+CHOCOLATE_DOOM_DEPENDENCIES = host-pkgconf sdl2
+# Fetch from git
+CHOCOLATE_DOOM_AUTORECONF = YES
 
 # Avoid installing desktop entries, icons, etc.
 CHOCOLATE_DOOM_INSTALL_TARGET_OPTS = DESTDIR=$(TARGET_DIR) install-exec
@@ -22,6 +24,20 @@ CHOCOLATE_DOOM_CFLAGS += -O0
 endif
 
 CHOCOLATE_DOOM_CONF_ENV += CFLAGS="$(CHOCOLATE_DOOM_CFLAGS)"
+
+ifeq ($(BR2_PACKAGE_SDL2_MIXER),y)
+CHOCOLATE_DOOM_DEPENDENCIES += sdl2_mixer
+CHOCOLATE_DOOM_CONF_OPTS += --enable-sdl2mixer
+else
+CHOCOLATE_DOOM_CONF_OPTS += --disable-sdl2mixer
+endif
+
+ifeq ($(BR2_PACKAGE_SDL2_NET),y)
+CHOCOLATE_DOOM_DEPENDENCIES += sdl2_net
+CHOCOLATE_DOOM_CONF_OPTS += --enable-sdl2net
+else
+CHOCOLATE_DOOM_CONF_OPTS += --disable-sdl2net
+endif
 
 ifeq ($(BR2_PACKAGE_LIBPNG),y)
 CHOCOLATE_DOOM_DEPENDENCIES += libpng
