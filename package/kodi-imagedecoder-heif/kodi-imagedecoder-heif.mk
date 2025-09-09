09dd8962e6658cbfdfc02b16c325228e6e1bcea8
Bernd Kuhls <bernd@kuhls.net>
2025-09-09 18:10:36 +0200

    package/kodi-imagedecoder-heif: bump version to 21.0.3-Omega
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/kodi-imagedecoder-heif/kodi-imagedecoder-heif.mk
+++ package/kodi-imagedecoder-heif/kodi-imagedecoder-heif.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_IMAGEDECODER_HEIF_VERSION = 21.0.2-Omega
+KODI_IMAGEDECODER_HEIF_VERSION = 21.0.3-Omega
 KODI_IMAGEDECODER_HEIF_SITE = $(call github,xbmc,imagedecoder.heif,$(KODI_IMAGEDECODER_HEIF_VERSION))
 KODI_IMAGEDECODER_HEIF_LICENSE = GPL-2.0+
 KODI_IMAGEDECODER_HEIF_LICENSE_FILES = LICENSE.md
