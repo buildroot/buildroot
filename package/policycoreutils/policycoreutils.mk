8de4aadda72b3ba3487badc71e75ab394a630974
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2025-09-09 14:08:13 +0200

    package/policycoreutils: bump to version 3.9
    Changes:
    https://github.com/SELinuxProject/selinux/releases/download/3.9/RELEASE-3.9.txt
    
    Notable changes related to policycoreutils: none.
    
    However, in terms of packaging, there's a change: the top-level
    Makefile of policycoreutils now uses pkg-config to find libselinux and
    libsemanage. So building directly the subdirs no longer works because
    the variables defined by the top-level Makefile are no longer set. So
    we simplify the whole packaging by building from the top-level
    Makefile. In order for this pkg-config to work for the host variant,
    we need to pass $(HOST_MAKE_ENV) in the environment of make, so we
    also did the same for consistency with $(TARGET_MAKE_ENV).
    
    In terms of dependencies: policycoreutils directly links with
    libselinux, libsemanage and libsepol, so all three are needed for both
    the target and host variants. And host-pkgconf is now needed for the
    host variant, as it's used to detect the dependent libraries.
    
    The patches also need to be refreshed, except 0003 that is dropped
    since it was a backport from upstream.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    
    merge policycoreutils
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/policycoreutils/policycoreutils.mk
+++ package/policycoreutils/policycoreutils.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-POLICYCOREUTILS_VERSION = 3.8.1
+POLICYCOREUTILS_VERSION = 3.9
 POLICYCOREUTILS_SITE = https://github.com/SELinuxProject/selinux/releases/download/$(POLICYCOREUTILS_VERSION)
 POLICYCOREUTILS_LICENSE = GPL-2.0
 POLICYCOREUTILS_LICENSE_FILES = LICENSE
 POLICYCOREUTILS_CPE_ID_VENDOR = selinuxproject
 
-POLICYCOREUTILS_DEPENDENCIES = libsemanage libsepol libcap-ng $(TARGET_NLS_DEPENDENCIES)
+POLICYCOREUTILS_DEPENDENCIES = libselinux libsemanage libsepol libcap-ng host-pkgconf $(TARGET_NLS_DEPENDENCIES)
 POLICYCOREUTILS_MAKE_OPTS = $(TARGET_CONFIGURE_OPTS) LDLIBS=$(TARGET_NLS_LIBS)
 
 ifeq ($(BR2_PACKAGE_LIBXCRYPT),y)
@@ -32,28 +32,19 @@ ifeq ($(BR2_PACKAGE_LINUX_PAM)$(BR2_PACKAGE_AUDIT),yy)
 POLICYCOREUTILS_MAKE_OPTS += LSPP_PRIV=y
 endif
 
-POLICYCOREUTILS_MAKE_DIRS = \
-	load_policy newrole run_init \
-	secon semodule sestatus setfiles \
-	setsebool scripts
-
 # We need to pass DESTDIR at build time because it's used by
 # policycoreutils build system to find headers and libraries.
 define POLICYCOREUTILS_BUILD_CMDS
-	$(foreach d,$(POLICYCOREUTILS_MAKE_DIRS),
-		$(MAKE) -C $(@D)/$(d) $(POLICYCOREUTILS_MAKE_OPTS) \
-			DESTDIR=$(STAGING_DIR) all
-	)
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) $(POLICYCOREUTILS_MAKE_OPTS) \
+		DESTDIR=$(STAGING_DIR) all
 endef
 
 define POLICYCOREUTILS_INSTALL_TARGET_CMDS
-	$(foreach d,$(POLICYCOREUTILS_MAKE_DIRS),
-		$(MAKE) -C $(@D)/$(d) $(POLICYCOREUTILS_MAKE_OPTS) \
-			DESTDIR=$(TARGET_DIR) install
-	)
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) $(POLICYCOREUTILS_MAKE_OPTS) \
+		DESTDIR=$(TARGET_DIR) install
 endef
 
-HOST_POLICYCOREUTILS_DEPENDENCIES = host-libsemanage
+HOST_POLICYCOREUTILS_DEPENDENCIES = host-libselinux host-libsemanage host-libsepol host-pkgconf
 
 # We need to pass PREFIX because it defaults to $(DESTDIR)/usr
 HOST_POLICYCOREUTILS_MAKE_OPTS = \
@@ -62,22 +53,12 @@ HOST_POLICYCOREUTILS_MAKE_OPTS = \
 	ETCDIR=$(HOST_DIR)/etc \
 	SBINDIR=$(HOST_DIR)/sbin
 
-# Note: We are only building the programs required by the refpolicy build
-HOST_POLICYCOREUTILS_MAKE_DIRS = \
-	load_policy newrole run_init \
-	secon semodule sestatus setfiles \
-	setsebool
-
 define HOST_POLICYCOREUTILS_BUILD_CMDS
-	$(foreach d,$(HOST_POLICYCOREUTILS_MAKE_DIRS),
-		$(MAKE) -C $(@D)/$(d) $(HOST_POLICYCOREUTILS_MAKE_OPTS) all
-	)
+	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) $(HOST_POLICYCOREUTILS_MAKE_OPTS) all
 endef
 
 define HOST_POLICYCOREUTILS_INSTALL_CMDS
-	$(foreach d,$(HOST_POLICYCOREUTILS_MAKE_DIRS),
-		$(MAKE) -C $(@D)/$(d) $(HOST_POLICYCOREUTILS_MAKE_OPTS) install
-	)
+	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) $(HOST_POLICYCOREUTILS_MAKE_OPTS) install
 endef
 
 $(eval $(generic-package))
