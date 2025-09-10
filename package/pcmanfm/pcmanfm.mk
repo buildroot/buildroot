b80bb9c4c52d9d8db7e30b2cf8e05306712f54fb
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2025-09-10 23:32:17 +0200

    package/{libfm, pcmanfm}: handle libgtk2 removal
    As we're about to remove libgtk2, the libfm and pcmanfm packages need
    to be adjusted so that they work with libgtk3 only. This requires a
    bit of re-arrangment of the dependencies, to properly propagate the
    libgtk3 dependencies.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Arnout Vandecappelle <arnout@rnout.be>

--- package/pcmanfm/pcmanfm.mk
+++ package/pcmanfm/pcmanfm.mk
@@ -7,18 +7,11 @@
 PCMANFM_VERSION = 1.3.2
 PCMANFM_SOURCE = pcmanfm-$(PCMANFM_VERSION).tar.xz
 PCMANFM_SITE = http://sourceforge.net/projects/pcmanfm/files
-PCMANFM_DEPENDENCIES = libglib2 menu-cache libfm $(TARGET_NLS_DEPENDENCIES)
+PCMANFM_DEPENDENCIES = libglib2 menu-cache libfm libgtk3 $(TARGET_NLS_DEPENDENCIES)
 PCMANFM_LICENSE = GPL-2.0+
 PCMANFM_LICENSE_FILES = COPYING
 PCMANFM_CPE_ID_VALID = YES
 PCMANFM_CONF_ENV = LIBS=$(TARGET_NLS_LIBS)
-
-ifeq ($(BR2_PACKAGE_LIBGTK3_X11),y)
-PCMANFM_CONF_OPTS += --with-gtk=3
-PCMANFM_DEPENDENCIES += libgtk3
-else
-PCMANFM_CONF_OPTS += --with-gtk=2
-PCMANFM_DEPENDENCIES += libgtk2
-endif
+PCMANFM_CONF_OPTS = --with-gtk=3
 
 $(eval $(autotools-package))
