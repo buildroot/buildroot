5b6b80bfc5237ab4f4e35c081fdac1376efdd396
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2025-09-10 23:32:30 +0200

    package/yad: drop support for gtk2
    As we're about to remove libgtk2, we need to first drop support for
    gtk2 in yad. It continues to support gtk3.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    [Arnout: simply .mk file: BR2_PACKAGE_LIBGTK3_X11 is always true]
    Signed-off-by: Arnout Vandecappelle <arnout@rnout.be>

--- package/yad/yad.mk
+++ package/yad/yad.mk
@@ -9,16 +9,8 @@ YAD_SOURCE = yad-$(YAD_VERSION).tar.xz
 YAD_SITE = http://sourceforge.net/projects/yad-dialog/files
 YAD_LICENSE = GPL-3.0
 YAD_LICENSE_FILES = COPYING
-YAD_DEPENDENCIES = host-intltool host-pkgconf $(TARGET_NLS_DEPENDENCIES)
+YAD_DEPENDENCIES = host-intltool host-pkgconf libgtk3 $(TARGET_NLS_DEPENDENCIES)
 YAD_CONF_ENV = LIBS=$(TARGET_NLS_LIBS)
-YAD_CONF_OPTS = --enable-html=no
-
-ifeq ($(BR2_PACKAGE_LIBGTK3_X11),y)
-YAD_DEPENDENCIES += libgtk3
-YAD_CONF_OPTS += --with-gtk=gtk3
-else
-YAD_DEPENDENCIES += libgtk2
-YAD_CONF_OPTS += --with-gtk=gtk2
-endif
+YAD_CONF_OPTS = --enable-html=no --with-gtk=gtk3
 
 $(eval $(autotools-package))
