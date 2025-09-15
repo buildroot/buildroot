5b32fdc6c567bd704abd4ba9d5711eaa26a669f8
Saeed Kazemi <kazemi.ms@gmail.com>
2025-09-15 16:06:23 +0200

    package/eza: bump to version 0.23.3
    Release notes:
    https://github.com/eza-community/eza/releases/tag/v0.23.3
    
    Signed-off-by: Saeed Kazemi <kazemi.ms@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/eza/eza.mk
+++ package/eza/eza.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-EZA_VERSION = 0.20.19
+EZA_VERSION = 0.23.3
 EZA_SITE = $(call github,eza-community,eza,v$(EZA_VERSION))
 EZA_LICENSE = EUPL-1.2
 EZA_LICENSE_FILES = LICENSE.txt
