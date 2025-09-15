0865a1ae4b929b3af21921d382d273b518a0db9c
James Hilliard <james.hilliard1@gmail.com>
2025-09-15 16:07:42 -0600

    package/python-asyncclick: bump to version 8.2.2.2
    Drop python-anyio runtime dependency which is now an optional
    dependency of asyncclick. See:
    https://github.com/python-trio/asyncclick/commit/ca93155e162cfe15127d443b1de1d76edeafddec
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    [Julien: add commit reference for anyio dependency change]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-asyncclick/python-asyncclick.mk
+++ package/python-asyncclick/python-asyncclick.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ASYNCCLICK_VERSION = 8.1.8
+PYTHON_ASYNCCLICK_VERSION = 8.2.2.2
 PYTHON_ASYNCCLICK_SOURCE = asyncclick-$(PYTHON_ASYNCCLICK_VERSION).tar.gz
-PYTHON_ASYNCCLICK_SITE = https://files.pythonhosted.org/packages/cb/b5/e1e5fdf1c1bb7e6e614987c120a98d9324bf8edfaa5f5cd16a6235c9d91b
+PYTHON_ASYNCCLICK_SITE = https://files.pythonhosted.org/packages/35/51/b01dd77c9a14fb0b312d799fd8c10b145b882535dbaa9ac055a52515b390
 PYTHON_ASYNCCLICK_SETUP_TYPE = flit
 PYTHON_ASYNCCLICK_LICENSE = BSD-3-Clause
 PYTHON_ASYNCCLICK_LICENSE_FILES = LICENSE.txt
