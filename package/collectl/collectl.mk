6aa2e0e655ac765493f454d305f41e2161134bd9
Francois Perrad <francois.perrad@gadz.org>
2025-09-15 10:23:34 +0200

    package/collectl: bump to version 4.3.20.1
    For release notes, see:
    https://github.com/sharkcz/collectl/releases
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    [Julien: add link to release notes]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/collectl/collectl.mk
+++ package/collectl/collectl.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-COLLECTL_VERSION = 4.3.2
+COLLECTL_VERSION = 4.3.20.1
 COLLECTL_SITE = $(call github,sharkcz,collectl,$(COLLECTL_VERSION))
 COLLECTL_LICENSE = Artistic or GPL-2.0
 COLLECTL_LICENSE_FILES = COPYING ARTISTIC GPL
