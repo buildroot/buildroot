2e480a88256f9947b3cb0af591dfe4ca99b7df33
James Hilliard <james.hilliard1@gmail.com>
2025-09-15 18:44:06 -0600

    package/python-pypika-tortoise: bump to version 0.6.2
    Migrate pep517 build backend from poetry to pdm-backend.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-pypika-tortoise/python-pypika-tortoise.mk
+++ package/python-pypika-tortoise/python-pypika-tortoise.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_PYPIKA_TORTOISE_VERSION = 0.6.1
+PYTHON_PYPIKA_TORTOISE_VERSION = 0.6.2
 PYTHON_PYPIKA_TORTOISE_SOURCE = pypika_tortoise-$(PYTHON_PYPIKA_TORTOISE_VERSION).tar.gz
-PYTHON_PYPIKA_TORTOISE_SITE = https://files.pythonhosted.org/packages/db/03/c293fd42c3669e5a79d508c3ed1ca3ffa501600d93abd904a0f86d493ba0
-PYTHON_PYPIKA_TORTOISE_SETUP_TYPE = poetry
+PYTHON_PYPIKA_TORTOISE_SITE = https://files.pythonhosted.org/packages/1a/7b/0a31165e22e599ba149ba35d4323d343205a70d91a4f6e8c6565f5b4fa08
+PYTHON_PYPIKA_TORTOISE_SETUP_TYPE = pep517
 PYTHON_PYPIKA_TORTOISE_LICENSE = Apache-2.0
 PYTHON_PYPIKA_TORTOISE_LICENSE_FILES = LICENSE
+PYTHON_PYPIKA_TORTOISE_DEPENDENCIES = host-python-pdm-backend
 
 $(eval $(python-package))
