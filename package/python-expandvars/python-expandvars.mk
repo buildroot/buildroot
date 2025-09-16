b08d65ac395aa95f7a675038a8316558f67cb6fc
James Hilliard <james.hilliard1@gmail.com>
2025-09-15 20:03:07 -0600

    package/python-expandvars: bump to version 1.1.2
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-expandvars/python-expandvars.mk
+++ package/python-expandvars/python-expandvars.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_EXPANDVARS_VERSION = 0.12.0
+PYTHON_EXPANDVARS_VERSION = 1.1.2
 PYTHON_EXPANDVARS_SOURCE = expandvars-$(PYTHON_EXPANDVARS_VERSION).tar.gz
-PYTHON_EXPANDVARS_SITE = https://files.pythonhosted.org/packages/2b/a5/46d1f58edcae1d632fafdfee313e378240e002ae45d26502bac938bd8751
+PYTHON_EXPANDVARS_SITE = https://files.pythonhosted.org/packages/9c/64/a9d8ea289d663a44b346203a24bf798507463db1e76679eaa72ee6de1c7a
 PYTHON_EXPANDVARS_SETUP_TYPE = hatch
 PYTHON_EXPANDVARS_LICENSE = MIT
 PYTHON_EXPANDVARS_LICENSE_FILES = LICENSE
