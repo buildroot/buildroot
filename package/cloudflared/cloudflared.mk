981efdb8ca9033656e272e1de447c5e2359f7bc0
James Hilliard <james.hilliard1@gmail.com>
2025-09-16 16:46:02 -0600

    package/cloudflared: bump to version 2025.8.1
    Also set BuiltForPackageManager=buildroot so that cloudflared's update
    command informs the user to update cloudflared via buildroot.
    
    Without BuiltForPackageManager set:
    cloudflared update
    INF cloudflared is up to date version=
    
    With BuiltForPackageManager set:
    cloudflared update
    ERR cloudflared was installed by buildroot. Please update using the same method.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/cloudflared/cloudflared.mk
+++ package/cloudflared/cloudflared.mk
@@ -4,12 +4,14 @@
 #
 ################################################################################
 
-CLOUDFLARED_VERSION = 2025.1.0
+CLOUDFLARED_VERSION = 2025.8.1
 CLOUDFLARED_SITE = $(call github,cloudflare,cloudflared,$(CLOUDFLARED_VERSION))
 CLOUDFLARED_LICENSE = Apache-2.0
 CLOUDFLARED_LICENSE_FILES = LICENSE
 CLOUDFLARED_CPE_ID_VENDOR = cloudflare
 CLOUDFLARED_BUILD_TARGETS = cmd/cloudflared
-CLOUDFLARED_LDFLAGS = -X main.Version=$(CLOUDFLARED_VERSION)
+CLOUDFLARED_LDFLAGS = \
+	-X main.Version=$(CLOUDFLARED_VERSION) \
+	-X github.com/cloudflare/cloudflared/cmd/cloudflared/updater.BuiltForPackageManager=buildroot
 
 $(eval $(golang-package))
