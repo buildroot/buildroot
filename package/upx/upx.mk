6eddb6ec8be618df26f552c7db361a6169640c39
Thomas Perale <thomas.perale@mind.be>
2025-09-16 22:27:30 +0200

    package/upx: fix deprecated CPE
    The CPE 'upx_project:upx' has been deprecated in favour of 'upx:upx'.
    
    See the following:
      - https://nvd.nist.gov/products/cpe/detail/36CA8E94-4A35-4373-8D69-22313060322B
      - https://nvd.nist.gov/vuln/detail/CVE-2020-24119
    
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit ff492a2c87933305dc6e6aa72adf944c853bd983)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/upx/upx.mk
+++ package/upx/upx.mk
@@ -9,7 +9,7 @@ UPX_SITE = https://github.com/upx/upx/releases/download/v$(UPX_VERSION)
 UPX_SOURCE = upx-$(UPX_VERSION)-src.tar.xz
 UPX_LICENSE = GPL-2.0+
 UPX_LICENSE_FILES = COPYING
-UPX_CPE_ID_VALID = YES
+UPX_CPE_ID_VENDOR = upx
 UPX_SUPPORTS_IN_SOURCE_BUILD = NO
 
 $(eval $(host-cmake-package))
