b117d439bd3de57b4e6a0d6f9f7a43f219407b41
James Hilliard <james.hilliard1@gmail.com>
2025-09-15 18:38:17 -0600

    package/python-pymodbus: bump to version 3.11.2
    License hash changed due to year update:
    https://github.com/pymodbus-dev/pymodbus/commit/c1e7f3b70648585b865524f3a7468e3e3239c35f
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-pymodbus/python-pymodbus.mk
+++ package/python-pymodbus/python-pymodbus.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYMODBUS_VERSION = 3.8.6
+PYTHON_PYMODBUS_VERSION = 3.11.2
 PYTHON_PYMODBUS_SOURCE = pymodbus-$(PYTHON_PYMODBUS_VERSION).tar.gz
-PYTHON_PYMODBUS_SITE = https://files.pythonhosted.org/packages/47/3a/a6958507d6f9c0c50f11cd299b056161c10f3cef7c227b44bc183a7e7854
+PYTHON_PYMODBUS_SITE = https://files.pythonhosted.org/packages/d1/28/34f667085cb162ac68be849e4ea1ced4c54d97b7d8099c11b27ec1cf2fa1
 PYTHON_PYMODBUS_SETUP_TYPE = setuptools
 PYTHON_PYMODBUS_LICENSE = BSD-3-Clause
 PYTHON_PYMODBUS_LICENSE_FILES = LICENSE
