5250bba8e5b1aaeb58977baf1d91e2be66656476
Waldemar Brodkorb <wbx@openadk.org>
2025-09-19 20:21:00 +0200

    package/uclibc: bump to 1.0.55
    Finally the aarch64 tls bug is fixed. (python 3.13 segfault)
    Unfortunately it is not covered by any testcase of uClibc-ng-test.
    
    Here is the git shortlog for the release:
    
    Marius Melzer (1):
          malloc-standard: Fix truncation problem in malloc
    
    Waldemar Brodkorb (5):
          Fix Sysvipc for ARM, AARCH64, RISCV64, KVX and m68k
          remove uClibc specific gettext_printf
          aarch64: do not force tls-model in CPU_CFLAGS, this isn't required
          aarch64: respect r_addend for TLSDESC relocations
          bump version for 1.0.55 release
    
    yliu (5):
          mips32: Preventing FORCE_FRAME_POINTER from being optimized away
          mips32: Define __IPC_64 according to kernel version
          time64: fixed msgctl/semctl/shmctl result errors for for MIPS32/RISCV32
          time64: fix *ctl functions in mips32eb/mips64
          time64: fix *ctl functions in mips32eb
    
    Signed-off-by: Waldemar Brodkorb <wbx@openadk.org>
    Tested-by: Romain Naour <romain.naour@smile.fr>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/uclibc/uclibc.mk
+++ package/uclibc/uclibc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UCLIBC_VERSION = 1.0.54
+UCLIBC_VERSION = 1.0.55
 UCLIBC_SOURCE = uClibc-ng-$(UCLIBC_VERSION).tar.xz
 UCLIBC_SITE = https://downloads.uclibc-ng.org/releases/$(UCLIBC_VERSION)
 UCLIBC_LICENSE = LGPL-2.1+
