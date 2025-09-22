c10902b9867e85da96e45ebe1d2ba1fb4571a1b9
Peter Korsgaard <peter@korsgaard.com>
2025-09-22 10:15:50 +0200

    package/rtl_433: do not leak Buildroot git info
    The CMake build system has logic to detect the git repo info and include it
    in the version output.  This unfortunately gets confused and adds the git
    info of the Buildroot repository when building inside the repo:
    
    -- Found Git version: refs/heads/rtl_433-25.02 commit a367362af00ed349915b0b4496ebeb9910fc86e4                                 from 2025-09-21T20:07:58+0200
    -- Using Git version tag: 2025.08-283-ga367362af0 on rtl_433-25.02 at 202509212007
    
    rtl_433 -V
    rtl_433 version 2025.08-283-ga367362af0 branch rtl_433-25.02 at 202509212007 inputs file rtl_tcp RTL-SDR
    
    Pass GIT_DIR=.  to the configure step similar to how it is already done for
    the build step to disable this.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 817611686a12535aaa15b3e930be0dab95b9b60c)
    Signed-off-by: Titouan Christophe <titouan.christophe@mind.be>

--- package/rtl_433/rtl_433.mk
+++ package/rtl_433/rtl_433.mk
@@ -18,6 +18,9 @@ RTL_433_CONF_OPTS = \
 	-DBUILD_TESTING_ANALYZER=OFF \
 	-DENABLE_SOAPYSDR=OFF
 
+# do not include Buildroot git info in version output
+RTL_433_CONF_ENV = GIT_DIR=.
+
 ifeq ($(BR2_PACKAGE_LIBRTLSDR),y)
 RTL_433_DEPENDENCIES += librtlsdr
 RTL_433_CONF_OPTS += -DENABLE_RTLSDR=ON
