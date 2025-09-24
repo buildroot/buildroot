1316225788872b33b337e2043c7690970fb107cc
Scott Fan <fancp2007@gmail.com>
2025-09-24 23:43:15 +0800

    board/qemu/{x86, x86_64}: fix shellcheck warning in post-build.sh
    This removes a warning reported by shellcheck: "Double quote to prevent
    globbing and word splitting". As a result, the file can be removed from
    .checkpackageignore.
    
    Signed-off-by: Scott Fan <fancp2007@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit e7937c52b6da69c4ff7de5ce40d836a0636c775c)
    Signed-off-by: Titouan Christophe <titouan.christophe@mind.be>

--- board/qemu/x86/post-build.sh
+++ board/qemu/x86/post-build.sh
@@ -4,8 +4,8 @@ set -u
 set -e
 
 # Add a console on tty1
-if [ -e ${TARGET_DIR}/etc/inittab ]; then
-    grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
+if [ -e "${TARGET_DIR}/etc/inittab" ]; then
+    grep -qE '^tty1::' "${TARGET_DIR}/etc/inittab" || \
 	sed -i '/GENERIC_SERIAL/a\
-tty1::respawn:/sbin/getty -L  tty1 0 vt100 # QEMU graphical window' ${TARGET_DIR}/etc/inittab
+tty1::respawn:/sbin/getty -L  tty1 0 vt100 # QEMU graphical window' "${TARGET_DIR}/etc/inittab"
 fi
