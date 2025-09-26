9824b964a7db1a69885ddbfefc40663d591dc653
Dario Binacchi <dario.binacchi@amarulasolutions.com>
2025-09-26 12:06:04 +0200

    package/embiggen-disk: fix EMBIGGEN_DISK_VERSION
    Make EMBIGGEN_DISK_VERSION compliant with release-monitoring.org.
    
    Fixes: 4ae18c511a2a7e8b0b6cd7a2961ba4c0e395fe2a
    Signed-off-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>
    Reviewed-by: Christian Stewart <christian@aperture.us>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/embiggen-disk/embiggen-disk.mk
+++ package/embiggen-disk/embiggen-disk.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-EMBIGGEN_DISK_VERSION = v20240521
-EMBIGGEN_DISK_SITE = $(call github,skiffos,embiggen-disk,$(EMBIGGEN_DISK_VERSION))
+EMBIGGEN_DISK_VERSION = 20240521
+EMBIGGEN_DISK_SITE = $(call github,skiffos,embiggen-disk,v$(EMBIGGEN_DISK_VERSION))
 EMBIGGEN_DISK_LICENSE = Apache-2.0
 EMBIGGEN_DISK_LICENSE_FILES = LICENSE
 
