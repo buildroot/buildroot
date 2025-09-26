151be15aa8c71cc5c2c4f56019a641b8aa26f6ad
Raphael Pavlidis <raphael.pavlidis@gmail.com>
2025-09-26 11:11:19 +0000

    package/cage: bump to version v0.2.0-15-g2e593fe5a
    wlroots 0.19.0 includes API breaking changes.
    
    This commit bump the cage version to include the upstream commit [1]
    which updates to this new wlroots API.
    
    [1] https://github.com/cage-kiosk/cage/commit/9ad44e4f521edf446ad66559726e6deeb8204101
    
    Signed-off-by: Raphael Pavlidis <raphael.pavlidis@gmail.com>
    [Julien: change _VERSION to use "git describe --abbrev=40" format]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/cage/cage.mk
+++ package/cage/cage.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-CAGE_VERSION = 0.2.0
-CAGE_SITE = https://github.com/cage-kiosk/cage/releases/download/v$(CAGE_VERSION)
+CAGE_VERSION = v0.2.0-15-g2e593fe5a8a2186a558ceb414674775e4ba5d2b1
+CAGE_SITE = https://github.com/cage-kiosk/cage.git
+CAGE_SITE_METHOD = git
 CAGE_LICENSE = MIT
 CAGE_LICENSE_FILES = LICENSE
 CAGE_DEPENDENCIES = host-pkgconf wlroots
