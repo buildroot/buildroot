ed8318f6eca8ce2ea54e192857d5282c093ebd3a
Fabio Estevam <festevam@gmail.com>
2025-09-30 09:22:37 -0300

    package/crucible: bump to version 2025.05.28
    Bump to version 2025.05.28. See release note:
    https://github.com/usbarmory/crucible/releases/tag/v2025.05.28
    
    Signed-off-by: Fabio Estevam <festevam@gmail.com>
    [Julien: add link to release note]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/crucible/crucible.mk
+++ package/crucible/crucible.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CRUCIBLE_VERSION = 2023.11.02
+CRUCIBLE_VERSION = 2025.05.28
 CRUCIBLE_SITE = $(call github,usbarmory,crucible,v$(CRUCIBLE_VERSION))
 CRUCIBLE_LICENSE = BSD-3-Clause
 CRUCIBLE_LICENSE_FILES = LICENSE
