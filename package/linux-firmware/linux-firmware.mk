a5ebfd4b7e46ad53926b6380199b1b5484820b47
Nicolas Cavallari <nicolas.cavallari@green-communications.fr>
2025-09-30 12:02:30 +0200

    package/linux-firmware: fix license file for ath10k firmware
    The WHENCE file [1] indicates ath10k's license file is
    LICENSE.QualcommAtheros_ath10k license and not LICENCE.atheros_firmware.
    
    [1] https://gitlab.com/kernel-firmware/linux-firmware/-/blob/20250627/WHENCE#L3700
    
    Signed-off-by: Nicolas Cavallari <nicolas.cavallari@green-communications.fr>
    [Julien: add link to WHENCE file]
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 752fc349a05910a08b08467e2f28fd80f1c239a1)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/linux-firmware/linux-firmware.mk
+++ package/linux-firmware/linux-firmware.mk
@@ -247,14 +247,14 @@ LINUX_FIRMWARE_FILES += ath10k/QCA9377/hw1.0/board-2.bin \
 			ath10k/QCA9377/hw1.0/firmware-5.bin \
 			ath10k/QCA9377/hw1.0/firmware-6.bin \
 			ath10k/QCA9377/hw1.0/firmware-sdio-5.bin
-LINUX_FIRMWARE_ALL_LICENSE_FILES += LICENCE.atheros_firmware
+LINUX_FIRMWARE_ALL_LICENSE_FILES += LICENSE.QualcommAtheros_ath10k
 endif
 
 ifeq ($(BR2_PACKAGE_LINUX_FIRMWARE_ATHEROS_10K_QCA998X),y)
 LINUX_FIRMWARE_FILES += ath10k/QCA988X/hw2.0/board.bin \
 			ath10k/QCA988X/hw2.0/firmware-4.bin \
 			ath10k/QCA988X/hw2.0/firmware-5.bin
-LINUX_FIRMWARE_ALL_LICENSE_FILES += LICENCE.atheros_firmware
+LINUX_FIRMWARE_ALL_LICENSE_FILES += LICENSE.QualcommAtheros_ath10k
 endif
 
 # sd8686 v8
