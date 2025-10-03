2ecb2d2847701795d384ee9e4eaeb0669dcc2bef
Daniel Crowe <daniel.crowe@maxmine.com.au>
2025-10-03 09:30:24 +0930

    package/python-flask-restx: bump version to 1.3.2
    For release notes, see [1].
    
    This commit removes the runtime dependency to pytz, as it was replace
    with the python standard zoneinfo. See [2]. It also adds the python
    referencing runtime dependency. See [3].
    
    [1] https://github.com/python-restx/flask-restx/releases/tag/1.3.2
    [2] https://github.com/python-restx/flask-restx/commit/dbd6f0dc3459b2b0526228a445af31590f0aafdd
    [3] https://github.com/python-restx/flask-restx/commit/0f4f53498129696c85ea150d10becb97a579de6a
    
    Signed-off-by: Daniel Crowe <daniel.crowe@maxmine.com.au>
    [Julien: add details in commit log]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-flask-restx/python-flask-restx.mk
+++ package/python-flask-restx/python-flask-restx.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_FLASK_RESTX_VERSION = 1.3.0
+PYTHON_FLASK_RESTX_VERSION = 1.3.2
 PYTHON_FLASK_RESTX_SOURCE = flask-restx-$(PYTHON_FLASK_RESTX_VERSION).tar.gz
-PYTHON_FLASK_RESTX_SITE = https://files.pythonhosted.org/packages/45/4c/2e7d84e2b406b47cf3bf730f521efe474977b404ee170d8ea68dc37e6733
+PYTHON_FLASK_RESTX_SITE = https://files.pythonhosted.org/packages/43/89/9b9ca58cbb8e9ec46f4a510ba93878e0c88d518bf03c350e3b1b7ad85cbe
 PYTHON_FLASK_RESTX_SETUP_TYPE = setuptools
 PYTHON_FLASK_RESTX_LICENSE = BSD-3-Clause
 PYTHON_FLASK_RESTX_LICENSE_FILES = LICENSE
