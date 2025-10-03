1a66cdf7b34328d2b2065bfd22f190c92318e499
Bernd Kuhls <bernd@kuhls.net>
2025-10-03 22:13:01 +0200

    package/libdisplay-info: bump version to 0.3.0
    Release notes:
    https://lore.freedesktop.org/wayland-devel/47m6wyRq0q-sgX6zWXuZfexFdml2vTqsHKkdCBXBWywBql7gG9F9or9JsCm690MBXEmnnUEML0JnCIbUNma_a7tFKUPZ6bHPYa86b0gjYB0=@emersion.fr/
    
    Added sha512 hash.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libdisplay-info/libdisplay-info.mk
+++ package/libdisplay-info/libdisplay-info.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBDISPLAY_INFO_VERSION = 0.2.0
+LIBDISPLAY_INFO_VERSION = 0.3.0
 LIBDISPLAY_INFO_SOURCE = libdisplay-info-$(LIBDISPLAY_INFO_VERSION).tar.xz
 LIBDISPLAY_INFO_SITE = https://gitlab.freedesktop.org/emersion/libdisplay-info/-/releases/$(LIBDISPLAY_INFO_VERSION)/downloads
 LIBDISPLAY_INFO_LICENSE = MIT
