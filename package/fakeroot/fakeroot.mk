989fd5ca73e87befa536995aba7cc5ea112c2805
Dario Binacchi <dario.binacchi@amarulasolutions.com>
2025-10-03 19:33:12 +0200

    package/fakeroot: bump to version 1.37.1.2
    For change log, see:
    https://salsa.debian.org/clint/fakeroot/-/blob/debian/1.37.1.2-1/debian/changelog
    
    Signed-off-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/fakeroot/fakeroot.mk
+++ package/fakeroot/fakeroot.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-FAKEROOT_VERSION = 1.37.1.1
+FAKEROOT_VERSION = 1.37.1.2
 FAKEROOT_SOURCE = fakeroot_$(FAKEROOT_VERSION).orig.tar.gz
-FAKEROOT_SITE = https://snapshot.debian.org/archive/debian/20250329T150348Z/pool/main/f/fakeroot
+FAKEROOT_SITE = https://snapshot.debian.org/archive/debian/20250513T025019Z/pool/main/f/fakeroot
 
 HOST_FAKEROOT_DEPENDENCIES = host-acl
 # Force capabilities detection off
