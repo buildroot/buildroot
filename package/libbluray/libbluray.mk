d2b2bab84af75465d94a2f577ac81f481aa02c2c
Bernd Kuhls <bernd@kuhls.net>
2025-10-03 22:12:41 +0200

    package/libbluray: bump version to 1.4.0
    Changelog:
    https://code.videolan.org/videolan/libbluray/-/blob/1.4.0/ChangeLog
    
    Switched _SITE to https.
    Follow upstream switch to meson and xz tarball.
    
    Depends on libudfread bump to 1.2.0.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libbluray/libbluray.mk
+++ package/libbluray/libbluray.mk
@@ -4,46 +4,47 @@
 #
 ################################################################################
 
-LIBBLURAY_VERSION = 1.3.4
-LIBBLURAY_SITE = http://download.videolan.org/pub/videolan/libbluray/$(LIBBLURAY_VERSION)
-LIBBLURAY_SOURCE = libbluray-$(LIBBLURAY_VERSION).tar.bz2
+LIBBLURAY_VERSION = 1.4.0
+LIBBLURAY_SOURCE = libbluray-$(LIBBLURAY_VERSION).tar.xz
+LIBBLURAY_SITE = https://download.videolan.org/pub/videolan/libbluray/$(LIBBLURAY_VERSION)
 LIBBLURAY_INSTALL_STAGING = YES
 LIBBLURAY_LICENSE = LGPL-2.1+
 LIBBLURAY_LICENSE_FILES = COPYING
 LIBBLURAY_CPE_ID_VENDOR = videolan
 LIBBLURAY_DEPENDENCIES = host-pkgconf
 
-LIBBLURAY_CONF_OPTS = --disable-bdjava-jar
+LIBBLURAY_CONF_OPTS = -Dbdj_jar=disabled
 
 ifeq ($(BR2_PACKAGE_LIBICONV),y)
 LIBBLURAY_DEPENDENCIES += libiconv
 endif
 
 ifeq ($(BR2_PACKAGE_LIBUDFREAD),y)
+LIBBLURAY_CONF_OPTS += -Dembed_udfread=false
 LIBBLURAY_DEPENDENCIES += libudfread
 else
-LIBBLURAY_CONF_OPTS += --without-external-libudfread
+LIBBLURAY_CONF_OPTS += -Dembed_udfread=true
 endif
 
 ifeq ($(BR2_PACKAGE_FREETYPE),y)
+LIBBLURAY_CONF_OPTS += -Dfreetype=enabled
 LIBBLURAY_DEPENDENCIES += freetype
-LIBBLURAY_CONF_OPTS += --with-freetype
 else
-LIBBLURAY_CONF_OPTS += --without-freetype
+LIBBLURAY_CONF_OPTS += -Dfreetype=disabled
 endif
 
 ifeq ($(BR2_PACKAGE_FONTCONFIG),y)
+LIBBLURAY_CONF_OPTS += -Dfontconfig=enabled
 LIBBLURAY_DEPENDENCIES += fontconfig
-LIBBLURAY_CONF_OPTS += --with-fontconfig
 else
-LIBBLURAY_CONF_OPTS += --without-fontconfig
+LIBBLURAY_CONF_OPTS += -Dfontconfig=disabled
 endif
 
 ifeq ($(BR2_PACKAGE_LIBXML2),y)
+LIBBLURAY_CONF_OPTS += -Dlibxml2=enabled
 LIBBLURAY_DEPENDENCIES += libxml2
-LIBBLURAY_CONF_OPTS += --with-libxml2
 else
-LIBBLURAY_CONF_OPTS += --without-libxml2
+LIBBLURAY_CONF_OPTS += -Dlibxml2=disabled
 endif
 
-$(eval $(autotools-package))
+$(eval $(meson-package))
