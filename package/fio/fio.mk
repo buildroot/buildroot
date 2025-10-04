215133c0fc2819f059fbb35071ab2e0987f5107d
Dario Binacchi <dario.binacchi@amarulasolutions.com>
2025-10-04 17:50:57 +0200

    package/fio: bump version to 3.41
    For change log since 3.38, see:
    https://git.kernel.dk/cgit/fio/log/?h=fio-3.41
    
    Signed-off-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/fio/fio.mk
+++ package/fio/fio.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FIO_VERSION = 3.38
+FIO_VERSION = 3.41
 FIO_SITE = https://brick.kernel.dk/snaps
 FIO_LICENSE = GPL-2.0
 FIO_LICENSE_FILES = COPYING MORAL-LICENSE
