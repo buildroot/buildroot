106b3d43a44fb126d4bd7ff6828110e85e3eca22
Raphael Gallais-Pou <rgallaispou@gmail.com>
2025-10-05 15:24:43 +0200

    package/waffle: bump to v1.8.0-95-g5f1f48287e
    Waffle has two build systems.  In commit f162f8126ec4 ("cmake:
    deprecate cmake in favour of meson") [1], cmake is not the preferred
    build system, and fails when used.  Bump to latest version available
    (which has been released a year ago) and switch to meson build system.
    
    This commit also removes the package patches. They make no longer
    sense since they are either merged or related to deprecated cmake
    build. The .checkpackageignore patch entries are also removed.
    
    This commit also forces the meson build.pkg_config_path value. The
    default path goes for $(HOST)/lib/pkgconfig.  This does not work for
    latest waffle version since prefix in the pkg-config file points to the
    host directory.  This results in the concatenation of $(STAGING_DIR)/xxx
    and $(HOST_DIR)/... which ultimately resolve in a file not found.
    Instead, override the default path to $(STAGING_DIR)/usr/share.
    
    [1] https://gitlab.freedesktop.org/mesa/waffle/-/commit/f162f8126ec479f794bf337cd1dbb0d898ae5640
    
    Signed-off-by: Raphael Gallais-Pou <rgallaispou@gmail.com>
    [Julien:
     - squash patches related to the bump altogether
     - change _VERSION to use "git describe --abbrev=40" format
     - remove .checkpackageignore entries to fix check-package errors
    ]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/waffle/waffle.mk
+++ package/waffle/waffle.mk
@@ -4,47 +4,49 @@
 #
 ################################################################################
 
-WAFFLE_VERSION = 1.7.0
-WAFFLE_SOURCE = waffle-$(WAFFLE_VERSION).tar.xz
-WAFFLE_SITE = https://gitlab.freedesktop.org/mesa/waffle/uploads/9eaadda4ec2ed1d8a26ddefd2cfd16be
+WAFFLE_VERSION = v1.8.0-95-g5f1f48287e806544d745e9a8f5aed47234c61292
+WAFFLE_SITE = https://gitlab.freedesktop.org/mesa/waffle.git
+WAFFLE_SITE_METHOD = git
 WAFFLE_INSTALL_STAGING = YES
 WAFFLE_LICENSE = BSD-2-Clause
 WAFFLE_LICENSE_FILES = LICENSE.txt
 
 WAFFLE_DEPENDENCIES = host-pkgconf
 
-WAFFLE_CONF_OPTS = -Dwaffle_build_tests=OFF \
-	-Dwaffle_build_examples=OFF \
-	-Dwaffle_build_manpages=OFF \
-	-Dwaffle_build_htmldocs=OFF \
-	-Dwaffle_has_nacl=OFF
+WAFFLE_CONF_OPTS = \
+	-Dbuild.pkg_config_path=$(STAGING_DIR)/usr/lib/pkgconfig \
+	-Dbuild-tests=false \
+	-Dbuild-examples=false \
+	-Dbuild-manpages=false \
+	-Dbuild-htmldocs=false \
+	-Dnacl=false
 
 ifeq ($(BR2_PACKAGE_WAFFLE_SUPPORTS_WAYLAND),y)
 WAFFLE_DEPENDENCIES += libegl wayland
-WAFFLE_CONF_OPTS += -Dwaffle_has_wayland=ON
+WAFFLE_CONF_OPTS += -Dwayland=enabled
 else
-WAFFLE_CONF_OPTS += -Dwaffle_has_wayland=OFF
+WAFFLE_CONF_OPTS += -Dwayland=disabled
 endif
 
 ifeq ($(BR2_PACKAGE_WAFFLE_SUPPORTS_X11_EGL),y)
 WAFFLE_DEPENDENCIES += libegl libxcb xlib_libX11
-WAFFLE_CONF_OPTS += -Dwaffle_has_x11_egl=ON
+WAFFLE_CONF_OPTS += -Dx11_egl=enabled
 else
-WAFFLE_CONF_OPTS += -Dwaffle_has_x11_egl=OFF
+WAFFLE_CONF_OPTS += -Dx11_egl=disabled
 endif
 
 ifeq ($(BR2_PACKAGE_WAFFLE_SUPPORTS_GLX),y)
 WAFFLE_DEPENDENCIES += libgl libxcb xlib_libX11
-WAFFLE_CONF_OPTS += -Dwaffle_has_glx=ON
+WAFFLE_CONF_OPTS += -Dglx=enabled
 else
-WAFFLE_CONF_OPTS += -Dwaffle_has_glx=OFF
+WAFFLE_CONF_OPTS += -Dglx=disabled
 endif
 
 ifeq ($(BR2_PACKAGE_WAFFLE_SUPPORTS_GBM),y)
 WAFFLE_DEPENDENCIES += libegl udev
-WAFFLE_CONF_OPTS += -Dwaffle_has_gbm=ON
+WAFFLE_CONF_OPTS += -Dgbm=enabled
 else
-WAFFLE_CONF_OPTS += -Dwaffle_has_gbm=OFF
+WAFFLE_CONF_OPTS += -Dgbm=disabled
 endif
 
 ifeq ($(BR2_PACKAGE_BASH_COMPLETION),y)
@@ -53,9 +55,9 @@ endif
 
 ifeq ($(BR2_PACKAGE_MESA3D)$(BR2_PACKAGE_MESA3D_OPENGL_EGL),yy)
 WAFFLE_DEPENDENCIES += mesa3d
-WAFFLE_CONF_OPTS += -Dwaffle_has_surfaceless_egl=ON
+WAFFLE_CONF_OPTS += -Dsurfaceless_egl=enabled
 else
-WAFFLE_CONF_OPTS += -Dwaffle_has_surfaceless_egl=OFF
+WAFFLE_CONF_OPTS += -Dsurfaceless_egl=disabled
 endif
 
-$(eval $(cmake-package))
+$(eval $(meson-package))
