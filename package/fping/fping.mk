63f1aa10151a5483fdf02d0a2f4837db35a58770
Dario Binacchi <dario.binacchi@amarulasolutions.com>
2025-10-06 18:48:55 +0200

    package/fping: bump to version 5.4
    Release notes:
    https://github.com/schweikert/fping/releases/tag/v5.3
    https://github.com/schweikert/fping/releases/tag/v5.4
    
    Signed-off-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/fping/fping.mk
+++ package/fping/fping.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FPING_VERSION = 5.2
+FPING_VERSION = 5.4
 FPING_SITE = http://fping.org/dist
 FPING_LICENSE = BSD-like
 FPING_LICENSE_FILES = COPYING
