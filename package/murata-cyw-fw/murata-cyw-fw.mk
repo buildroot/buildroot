6bbeb72ae342a25b85533096494d6b4092307b89
Kory Maincent <kory.maincent@bootlin.com>
2025-10-08 16:38:32 +0000

    package/murata-cyw-fw: Bump to last revision
    Bump to last revision and install the newly added CYW43439 firmware.
    The brcmfmac4339-sdio.1CK.txt file has been removed from the firmware
    therefore the Kconfig option has been removed.
    
    The firmware files prefix have been changed from brcmfmac to cyfmac
    therefore the rename helpers have change.
    
    Signed-off-by: Kory Maincent <kory.maincent@bootlin.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/murata-cyw-fw/murata-cyw-fw.mk
+++ package/murata-cyw-fw/murata-cyw-fw.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-MURATA_CYW_FW_VERSION = 8d87950bfad28c65926695b7357bd8995b60016a
-MURATA_CYW_FW_VERSION_NVRAM = d27f1bf105fa1e5b828e355793b88d4b66188411
-MURATA_CYW_FW_VERSION_BT_PATCH = 748462f0b02ec4aeb500bedd60780ac51c37be31
+MURATA_CYW_FW_VERSION = 35459425949f0fa7de0b5c9f0c8bc0560087e1de
+MURATA_CYW_FW_VERSION_NVRAM = fd6587f8183b612ea52404c7a73aeb2698099587
+MURATA_CYW_FW_VERSION_BT_PATCH = 83f8e16423c47e195f52a06fd68ac92a20a80a9f
 MURATA_CYW_FW_SITE = $(call github,murata-wireless,cyw-fmac-fw,$(MURATA_CYW_FW_VERSION))
 MURATA_CYW_FW_EXTRA_DOWNLOADS = \
 	$(call github,murata-wireless,cyw-fmac-nvram,$(MURATA_CYW_FW_VERSION_NVRAM))/cyw-fmac-nvram-$(MURATA_CYW_FW_VERSION_NVRAM).tar.gz \
@@ -24,69 +24,66 @@ endef
 MURATA_CYW_FW_POST_EXTRACT_HOOKS += MURATA_CYW_FW_EXTRACT_NVRAM_PATCH
 
 MURATA_CYW_FW_FILES_$(BR2_PACKAGE_MURATA_CYW_FW_CYW43012) += \
-	brcmfmac43012-sdio.bin \
-	brcmfmac43012-sdio.1LV.clm_blob \
-	brcmfmac43012-sdio.1LV.txt \
-	CYW43012C0.1LV.hcd
+	cyfmac43012-sdio.bin \
+	cyfmac43012-sdio.1LV.clm_blob \
+	cyfmac43012-sdio.1LV.txt \
+	BCM43012C0_003.001.015.0303.0267.1LV.sAnt.hcd
 MURATA_CYW_FW_FILES_$(BR2_PACKAGE_MURATA_CYW_FW_CYW43340) += \
-	brcmfmac43340-sdio.bin \
-	brcmfmac43340-sdio.1BW.txt \
+	cyfmac43340-sdio.bin \
+	cyfmac43340-sdio.1BW.txt \
 	CYW43341B0.1BW.hcd
 MURATA_CYW_FW_FILES_$(BR2_PACKAGE_MURATA_CYW_FW_CYW43362) += \
-	brcmfmac43362-sdio.bin \
-	brcmfmac43362-sdio.SN8000.txt
+	cyfmac43362-sdio.bin
 MURATA_CYW_FW_FILES_$(BR2_PACKAGE_MURATA_CYW_FW_CYW4339) += \
-	brcmfmac4339-sdio.bin
-MURATA_CYW_FW_FILES_$(BR2_PACKAGE_MURATA_CYW_FW_CYW4339_1CK) += \
-	brcmfmac4339-sdio.1CK.txt
-MURATA_CYW_FW_FILES_$(BR2_PACKAGE_MURATA_CYW_FW_CYW4339_ZP) += \
-	brcmfmac4339-sdio.ZP.txt \
+	cyfmac4339-sdio.bin \
 	CYW4335C0.ZP.hcd
 MURATA_CYW_FW_FILES_$(BR2_PACKAGE_MURATA_CYW_FW_CYW43430) += \
-	brcmfmac43430-sdio.bin
+	cyfmac43430-sdio.bin
 MURATA_CYW_FW_FILES_$(BR2_PACKAGE_MURATA_CYW_FW_CYW43430_1DX) += \
-	brcmfmac43430-sdio.1DX.clm_blob \
-	brcmfmac43430-sdio.1DX.txt \
-	CYW43430A1.1DX.hcd
+	cyfmac43430-sdio.1DX.clm_blob \
+	cyfmac43430-sdio.1DX.txt \
+	BCM43430A1_001.002.009.0159.0528.1DX.hcd
 MURATA_CYW_FW_FILES_$(BR2_PACKAGE_MURATA_CYW_FW_CYW43430_1FX) += \
-	brcmfmac43430-sdio.1FX.clm_blob \
-	brcmfmac43430-sdio.1FX.txt
+	cyfmac43430-sdio.1FX.clm_blob \
+	cyfmac43430-sdio.1FX.txt
 MURATA_CYW_FW_FILES_$(BR2_PACKAGE_MURATA_CYW_FW_CYW43430_1LN) += \
-	brcmfmac43430-sdio.1LN.clm_blob \
-	brcmfmac43430-sdio.1LN.txt
+	cyfmac43430-sdio.1LN.clm_blob
+MURATA_CYW_FW_FILES_$(BR2_PACKAGE_MURATA_CYW_FW_CYW43439) += \
+	cyfmac43439-sdio.bin \
+	cyfmac43439-sdio.1YN.clm_blob \
+	cyfmac43439-sdio.1YN.txt \
+	CYW4343A2_001.003.016.0031.0000.1YN.hcd
 MURATA_CYW_FW_FILES_$(BR2_PACKAGE_MURATA_CYW_FW_CYW43455) += \
-	brcmfmac43455-sdio.bin
+	cyfmac43455-sdio.bin
 MURATA_CYW_FW_FILES_$(BR2_PACKAGE_MURATA_CYW_FW_CYW43455_1HK) += \
-	brcmfmac43455-sdio.1HK.clm_blob \
-	brcmfmac43455-sdio.1HK.txt
+	cyfmac43455-sdio.1HK.clm_blob
 MURATA_CYW_FW_FILES_$(BR2_PACKAGE_MURATA_CYW_FW_CYW43455_1LC) += \
-	brcmfmac43455-sdio.1LC.clm_blob \
-	brcmfmac43455-sdio.1LC.txt
+	cyfmac43455-sdio.1LC.clm_blob \
+	cyfmac43455-sdio.1LC.txt
 MURATA_CYW_FW_FILES_$(BR2_PACKAGE_MURATA_CYW_FW_CYW43455_1MW) += \
-	brcmfmac43455-sdio.1MW.clm_blob \
-	brcmfmac43455-sdio.1MW.txt \
-	CYW4345C0.1MW.hcd
+	cyfmac43455-sdio.1MW.clm_blob \
+	cyfmac43455-sdio.1MW.txt \
+	BCM4345C0_003.001.025.0187.0366.1MW.hcd
 MURATA_CYW_FW_FILES_$(BR2_PACKAGE_MURATA_CYW_FW_CYW4354) += \
-	brcmfmac4354-sdio.bin \
-	brcmfmac4354-sdio.1BB.clm_blob \
-	brcmfmac4354-sdio.1BB.txt \
+	cyfmac4354-sdio.bin \
+	cyfmac4354-sdio.1BB.clm_blob \
 	CYW4350C0.1BB.hcd
 MURATA_CYW_FW_FILES_$(BR2_PACKAGE_MURATA_CYW_FW_CYW4356) += \
-	brcmfmac4356-pcie.bin \
-	brcmfmac4356-pcie.1CX.clm_blob \
-	brcmfmac4356-pcie.1CX.txt \
-	CYW4354A2.1CX.hcd
+	cyfmac4356-pcie.bin \
+	cyfmac4356-pcie.1CX.clm_blob \
+	BCM4356A2_001.003.015.0112.0410.1CX.hcd
 MURATA_CYW_FW_FILES_$(BR2_PACKAGE_MURATA_CYW_FW_CYW4359) += \
-	brcmfmac4359-pcie.bin \
-	brcmfmac4359-pcie.1FD.clm_blob
+	cyfmac4359-pcie.bin \
+	cyfmac4359-sdio.bin
 
 # Helper that assumes filename with model has two dots (CHIP.MODEL.EXT),
-# but filename without model has only single dot (CHIP.EXT).
-murata-cyw-fw-strip-model = $(shell echo -n $(1) | sed 's/\..*\./\./')
+# or a underscore with several dots (CHIP_MODEL.MODEL.MODEL.EXT), but
+# filename without model has only single dot (CHIP.EXT).
+murata-cyw-fw-strip-model = $(shell echo -n $(1) | sed -e 's/\..*\./\./' -e 's/_.*\./\./')
 
 # Helper that strips model name and renames Bluetooth patch files to the ones
 # expected by Linux kernel.
-murata-cyw-fw-file-rename = $(call murata-cyw-fw-strip-model,$(patsubst CYW%,BCM%,$(f)))
+murata-cyw-fw-file-rename = $(call murata-cyw-fw-strip-model,$(patsubst CYW%,BCM%,$(patsubst cy%,brcm%,$(f))))
 
 define MURATA_CYW_FW_INSTALL_TARGET_CMDS
 	$(foreach f,$(MURATA_CYW_FW_FILES_y), \
