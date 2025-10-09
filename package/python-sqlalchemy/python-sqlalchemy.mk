fe0df1157c4ce3548a93625faa3ca1f9c7ae8814
Flávio Tapajós <flavio.tapajos@newtesc.com.br>
2025-10-09 16:31:14 -0300

    package/python-sqlalchemy: bump to version 2.0.43
    For change log, see:
    https://docs.sqlalchemy.org/en/20/changelog/changelog_20.html#change-2.0.42
    https://docs.sqlalchemy.org/en/20/changelog/changelog_20.html#change-2.0.43
    
    Signed-off-by: Flávio Tapajós <flavio.tapajos@newtesc.com.br>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-sqlalchemy/python-sqlalchemy.mk
+++ package/python-sqlalchemy/python-sqlalchemy.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SQLALCHEMY_VERSION = 2.0.41
+PYTHON_SQLALCHEMY_VERSION = 2.0.43
 PYTHON_SQLALCHEMY_SOURCE = sqlalchemy-$(PYTHON_SQLALCHEMY_VERSION).tar.gz
-PYTHON_SQLALCHEMY_SITE = https://files.pythonhosted.org/packages/63/66/45b165c595ec89aa7dcc2c1cd222ab269bc753f1fc7a1e68f8481bd957bf
+PYTHON_SQLALCHEMY_SITE = https://files.pythonhosted.org/packages/d7/bc/d59b5d97d27229b0e009bd9098cd81af71c2fa5549c580a0a67b9bed0496
 PYTHON_SQLALCHEMY_SETUP_TYPE = setuptools
 PYTHON_SQLALCHEMY_LICENSE = MIT
 PYTHON_SQLALCHEMY_LICENSE_FILES = LICENSE
