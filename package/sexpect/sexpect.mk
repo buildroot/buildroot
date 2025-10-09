c177e65fac3ffb902586471db2fd84281a72753b
Baruch Siach <baruch@tkos.co.il>
2025-10-09 09:13:52 +0300

    package/sexpect: bump to version 2.3.15
    Drop upstream patch.
    
    Main change in this release is cmake 4 build fix.
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 7176f6b28751dfb5e1592caba7efb3f9da4d2685)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/sexpect/sexpect.mk
+++ package/sexpect/sexpect.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SEXPECT_VERSION = 2.3.14
+SEXPECT_VERSION = 2.3.15
 SEXPECT_SITE = $(call github,clarkwang,sexpect,v$(SEXPECT_VERSION))
 SEXPECT_LICENSE = GPL-3.0
 SEXPECT_LICENSE_FILES = LICENSE
