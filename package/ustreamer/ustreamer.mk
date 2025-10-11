f54bef6b42bfed26976398491ae56b9a732ab937
Akhilesh Nema <nemaakhilesh@gmail.com>
2025-10-11 02:36:06 -0700

    package/ustreamer: bump version to 6.40
    Changelog:
    https://github.com/pikvm/ustreamer/compare/v6.37...v6.40
    
    Signed-off-by: Akhilesh Nema <nemaakhilesh@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/ustreamer/ustreamer.mk
+++ package/ustreamer/ustreamer.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-USTREAMER_VERSION = 6.37
+USTREAMER_VERSION = 6.40
 USTREAMER_SITE = $(call github,pikvm,ustreamer,v$(USTREAMER_VERSION))
 USTREAMER_LICENSE = GPL-3.0+
 USTREAMER_LICENSE_FILES = LICENSE
