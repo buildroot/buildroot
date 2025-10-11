d4a505b17565641e7655e8b992da5bb6029b0ee7
Akhilesh Nema <nemaakhilesh@gmail.com>
2025-10-11 02:58:48 -0700

    package/ltrace: bump version to 0.8.1
    Release notes:
    https://gitlab.com/cespedes/ltrace/-/blob/0.8.1/NEWS
    https://gitlab.com/cespedes/ltrace/-/blob/ltrace-0.8.0/NEWS
    
    - Drop the upstreamed 0003-configure.ac-fix-autoreconf-with-autoconf-2.70.patch
      https://gitlab.com/cespedes/ltrace/-/commit/10177832689ae3411d7dad39ac66b2ceaf4e46f2
    - Switch site to gitlab
    
    Signed-off-by: Akhilesh Nema <nemaakhilesh@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/ltrace/ltrace.mk
+++ package/ltrace/ltrace.mk
@@ -4,11 +4,8 @@
 #
 ################################################################################
 
-# This github version has some extra fixes on top of https://ltrace.org/
-# in particular the following commit to avoid a crash:
-#   192e0a6 void struct members are now ignored
-LTRACE_VERSION = 82c66409c7a93ca6ad2e4563ef030dfb7e6df4d4
-LTRACE_SITE = $(call github,dkogan,ltrace,$(LTRACE_VERSION))
+LTRACE_VERSION = 0.8.1
+LTRACE_SITE = $(call gitlab,cespedes,ltrace,$(LTRACE_VERSION))
 LTRACE_DEPENDENCIES = elfutils
 LTRACE_CONF_OPTS = --disable-werror
 LTRACE_LICENSE = GPL-2.0
