7d51fadc067d9fb5eee96ef4d0b36be2f08cf79e
Bernd Kuhls <bernd@kuhls.net>
2025-10-12 20:19:58 +0200

    package/python-asyncssh: bump version to 2.21.1
    Changelog:
    https://github.com/ronf/asyncssh/blob/v2.21.1/docs/changes.rst
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-asyncssh/python-asyncssh.mk
+++ package/python-asyncssh/python-asyncssh.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ASYNCSSH_VERSION = 2.21.0
+PYTHON_ASYNCSSH_VERSION = 2.21.1
 PYTHON_ASYNCSSH_SOURCE = asyncssh-$(PYTHON_ASYNCSSH_VERSION).tar.gz
-PYTHON_ASYNCSSH_SITE = https://files.pythonhosted.org/packages/e8/d6/823ed5a227f7da70b33681e49eec4a36fae31b9296b27a8d6aead2bd3f77
+PYTHON_ASYNCSSH_SITE = https://files.pythonhosted.org/packages/6b/b8/065c20bb5c9b8991648c0f25b13e445b4f51556cc3fdd0ad13ce4787c156
 PYTHON_ASYNCSSH_SETUP_TYPE = setuptools
 PYTHON_ASYNCSSH_LICENSE = EPL-2.0 or GPL-2.0+
 PYTHON_ASYNCSSH_LICENSE_FILES = LICENSE COPYRIGHT
