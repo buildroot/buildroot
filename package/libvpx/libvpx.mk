e0f10eed1fd271ca65826cde5287e4a1c7c3f8ed
Bernd Kuhls <bernd@kuhls.net>
2025-10-12 17:23:36 +0200

    package/libvpx: security bump version to 1.15.2
    Release notes:
    https://chromium.googlesource.com/webm/libvpx/+/refs/tags/v1.15.2
    https://chromium.googlesource.com/webm/libvpx/+/refs/tags/v1.15.1
    
    Fixes CVE-2025-5283.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libvpx/libvpx.mk
+++ package/libvpx/libvpx.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBVPX_VERSION = 1.15.0
+LIBVPX_VERSION = 1.15.2
 LIBVPX_SITE = $(call github,webmproject,libvpx,v$(LIBVPX_VERSION))
 LIBVPX_LICENSE = BSD-3-Clause
 LIBVPX_LICENSE_FILES = LICENSE PATENTS
