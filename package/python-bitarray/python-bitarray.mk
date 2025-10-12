f98a4ca3a2eb801585dc3e50c812833c0aee4630
Bernd Kuhls <bernd@kuhls.net>
2025-10-12 20:20:01 +0200

    package/python-bitarray: bump version to 3.7.2
    Changelog:
    https://github.com/ilanschnell/bitarray/blob/3.7.2/CHANGE_LOG
    
    Updated license hash due to copyright year bump:
    https://github.com/ilanschnell/bitarray/commit/0d64d33960344c5e718b78f26d8124ab6626b3a2
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-bitarray/python-bitarray.mk
+++ package/python-bitarray/python-bitarray.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_BITARRAY_VERSION = 3.0.0
+PYTHON_BITARRAY_VERSION = 3.7.2
 PYTHON_BITARRAY_SOURCE = bitarray-$(PYTHON_BITARRAY_VERSION).tar.gz
-PYTHON_BITARRAY_SITE = https://files.pythonhosted.org/packages/85/62/dcfac53d22ef7e904ed10a8e710a36391d2d6753c34c869b51bfc5e4ad54
+PYTHON_BITARRAY_SITE = https://files.pythonhosted.org/packages/e8/c1/644ea86b6f1a0864f656a3b3ee5bf8c29daa895cb3233942315fe065ea3a
 PYTHON_BITARRAY_SETUP_TYPE = setuptools
 PYTHON_BITARRAY_LICENSE = Python-2.0
 PYTHON_BITARRAY_LICENSE_FILES = LICENSE
