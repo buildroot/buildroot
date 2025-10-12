6bf65a90a53a2f5860eeac0ba925f6a771141a14
Bernd Kuhls <bernd@kuhls.net>
2025-10-12 20:19:56 +0200

    package/python-aexpect: bump version to 1.8.0
    Removed patch which is included in this release.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-aexpect/python-aexpect.mk
+++ package/python-aexpect/python-aexpect.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_AEXPECT_VERSION = 1.7.0
+PYTHON_AEXPECT_VERSION = 1.8.0
 PYTHON_AEXPECT_SOURCE = aexpect-$(PYTHON_AEXPECT_VERSION).tar.gz
-PYTHON_AEXPECT_SITE = https://files.pythonhosted.org/packages/dd/32/738b5190adb5ed387d3e755885f646b714fbf9c22adbda7ff988db7ede49
+PYTHON_AEXPECT_SITE = https://files.pythonhosted.org/packages/a3/74/837acfcdb3187cb931aa2edc4979e64aa9954092e4fd36b1161f65d65fdb
 PYTHON_AEXPECT_SETUP_TYPE = setuptools
 PYTHON_AEXPECT_LICENSE = GPL-2.0+
 PYTHON_AEXPECT_LICENSE_FILES = LICENSE
