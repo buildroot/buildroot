e747a6497987a4cec8d1c041c98062620a47cee6
Bernd Kuhls <bernd@kuhls.net>
2025-10-12 22:20:02 +0200

    package/python-networkx: bump version to 3.5
    Release notes:
    https://github.com/networkx/networkx/releases/tag/networkx-3.5
    
    Updated license hash due to copyright year bump:
    https://github.com/networkx/networkx/commit/c22ad5579d1916a662b00bcf00acca9fc3da9e50
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-networkx/python-networkx.mk
+++ package/python-networkx/python-networkx.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_NETWORKX_VERSION = 3.4.2
+PYTHON_NETWORKX_VERSION = 3.5
 PYTHON_NETWORKX_SOURCE = networkx-$(PYTHON_NETWORKX_VERSION).tar.gz
-PYTHON_NETWORKX_SITE = https://files.pythonhosted.org/packages/fd/1d/06475e1cd5264c0b870ea2cc6fdb3e37177c1e565c43f56ff17a10e3937f
+PYTHON_NETWORKX_SITE = https://files.pythonhosted.org/packages/6c/4f/ccdb8ad3a38e583f214547fd2f7ff1fc160c43a75af88e6aec213404b96a
 PYTHON_NETWORKX_LICENSE = BSD-3-Clause
 PYTHON_NETWORKX_LICENSE_FILES = LICENSE.txt
 PYTHON_NETWORKX_CPE_ID_VENDOR = python
