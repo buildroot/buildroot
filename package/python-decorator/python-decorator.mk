f53d6a83a9f7f0167a5b92646be741c963afb84a
Bernd Kuhls <bernd@kuhls.net>
2025-10-12 20:20:13 +0200

    package/python-decorator: bump version to 5.2.1
    Changelog: https://github.com/micheles/decorator/blob/5.2.1/CHANGES.md
    
    Updated license hash due to copyright year bump:
    https://github.com/micheles/decorator/commit/eac33cb79fd1ba2200c4e59408d6a492511feb24
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-decorator/python-decorator.mk
+++ package/python-decorator/python-decorator.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_DECORATOR_VERSION = 5.1.1
+PYTHON_DECORATOR_VERSION = 5.2.1
 PYTHON_DECORATOR_SOURCE = decorator-$(PYTHON_DECORATOR_VERSION).tar.gz
-PYTHON_DECORATOR_SITE = https://files.pythonhosted.org/packages/66/0c/8d907af351aa16b42caae42f9d6aa37b900c67308052d10fdce809f8d952
+PYTHON_DECORATOR_SITE = https://files.pythonhosted.org/packages/43/fa/6d96a0978d19e17b68d634497769987b16c8f4cd0a7a05048bec693caa6b
 PYTHON_DECORATOR_LICENSE = BSD-2-Clause
 PYTHON_DECORATOR_LICENSE_FILES = LICENSE.txt
 PYTHON_DECORATOR_CPE_ID_VENDOR = python
