e4e257ff24be62b7d2568b420a40a74fd11f3653
Bernd Kuhls <bernd@kuhls.net>
2025-10-12 22:19:50 +0200

    package/python-grpc-requests: bump version to 0.1.21
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-grpc-requests/python-grpc-requests.mk
+++ package/python-grpc-requests/python-grpc-requests.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_GRPC_REQUESTS_VERSION = 0.1.20
+PYTHON_GRPC_REQUESTS_VERSION = 0.1.21
 PYTHON_GRPC_REQUESTS_SOURCE = grpc_requests-$(PYTHON_GRPC_REQUESTS_VERSION).tar.gz
-PYTHON_GRPC_REQUESTS_SITE = https://files.pythonhosted.org/packages/92/84/f5a3874a6e10448d8c198253d2c79f7b34a23fb4a96b78a648dc468913a6
+PYTHON_GRPC_REQUESTS_SITE = https://files.pythonhosted.org/packages/97/58/7e2be40bad0964f7d6f1c11f72be941c19575ab426c10e1b97e17aa1e42f
 PYTHON_GRPC_REQUESTS_SETUP_TYPE = setuptools
 PYTHON_GRPC_REQUESTS_LICENSE = Apache-2.0
 PYTHON_GRPC_REQUESTS_LICENSE_FILES = LICENSE
