c5ce3417f4f67ba4573008ef0efb6700ffef2f22
Bernd Kuhls <bernd@kuhls.net>
2025-10-12 17:33:19 +0200

    package/which: bump version to 2.23
    Release notes:
    https://cgit.git.savannah.gnu.org/cgit/which.git/tree/NEWS?id=3e2c8f8acc3a333b66b06de234bc9324c6fe5500
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/which/which.mk
+++ package/which/which.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WHICH_VERSION = 2.21
+WHICH_VERSION = 2.23
 WHICH_SITE = $(BR2_GNU_MIRROR)/which
 WHICH_LICENSE = GPL-3.0+
 WHICH_LICENSE_FILES = COPYING
