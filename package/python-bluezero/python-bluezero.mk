ffb71b5ecc3b13eb143bd6e92312b7d222c38fae
Bernd Kuhls <bernd@kuhls.net>
2025-10-12 20:20:02 +0200

    package/python-bluezero: bump version to 0.9.1
    Release notes:
    https://github.com/ukBaz/python-bluezero/commit/a4b2731affa3806fa74509e6c0049dfc6474359f
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-bluezero/python-bluezero.mk
+++ package/python-bluezero/python-bluezero.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_BLUEZERO_VERSION = 0.9.0
+PYTHON_BLUEZERO_VERSION = 0.9.1
 PYTHON_BLUEZERO_SOURCE = bluezero-$(PYTHON_BLUEZERO_VERSION).tar.gz
-PYTHON_BLUEZERO_SITE = https://files.pythonhosted.org/packages/7b/9c/bfb3e1f92dd39bf3de8fc483a102bfae763fbf156177b77ed96b8a38bbb1
+PYTHON_BLUEZERO_SITE = https://files.pythonhosted.org/packages/ca/09/2aec91949397d15acb794648d65646e27f0e9f999a0da962e73023198663
 PYTHON_BLUEZERO_SETUP_TYPE = setuptools
 PYTHON_BLUEZERO_LICENSE = MIT
 PYTHON_BLUEZERO_LICENSE_FILES = LICENSE
