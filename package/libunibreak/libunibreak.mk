187becc56107da519c81a2f84f8d15bcf7025baa
Bernd Kuhls <bernd@kuhls.net>
2025-10-13 20:40:10 +0200

    package/libunibreak: bump version to 6.1
    Release notes:
    https://github.com/adah1972/libunibreak/releases/tag/libunibreak_6_1
    https://github.com/adah1972/libunibreak/releases/tag/libunibreak_6_0
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libunibreak/libunibreak.mk
+++ package/libunibreak/libunibreak.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBUNIBREAK_VERSION = 5.1
+LIBUNIBREAK_VERSION = 6.1
 LIBUNIBREAK_SITE = https://github.com/adah1972/libunibreak/releases/download/libunibreak_$(subst .,_,$(LIBUNIBREAK_VERSION))
 LIBUNIBREAK_INSTALL_STAGING = YES
 LIBUNIBREAK_LICENSE = Zlib
