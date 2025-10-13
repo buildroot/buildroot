2859c51f34282cea2ac953dac2444b2f7b7a86d1
Bernd Kuhls <bernd@kuhls.net>
2025-10-13 19:54:42 +0200

    package/python-posix-ipc: bump version to 1.3.0
    Release notes:
    https://github.com/osvenskan/posix_ipc/blob/rel1.3.0/history.md
    
    Updated project URL due to note at
    http://semanchuk.com/philip/posix_ipc/
    
    Updated license hash due to upstream commits:
    https://github.com/osvenskan/posix_ipc/commit/5ddd92995681595acbc530b7fbd4b7ff36279a16
    https://github.com/osvenskan/posix_ipc/commit/5ddd92995681595acbc530b7fbd4b7ff36279a16
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-posix-ipc/python-posix-ipc.mk
+++ package/python-posix-ipc/python-posix-ipc.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_POSIX_IPC_VERSION = 1.1.1
+PYTHON_POSIX_IPC_VERSION = 1.3.0
 PYTHON_POSIX_IPC_SOURCE = posix_ipc-$(PYTHON_POSIX_IPC_VERSION).tar.gz
-PYTHON_POSIX_IPC_SITE = https://files.pythonhosted.org/packages/07/7f/b954f224a226960a4aa98b6c5fa3d4f3fafb20bb8461446e41b563aee863
+PYTHON_POSIX_IPC_SITE = https://files.pythonhosted.org/packages/2b/a1/16d4b5a7dfc3dbb5965debe0415b6dc4866d4c94a1229f5b12cde7b78de8
 PYTHON_POSIX_IPC_LICENSE = BSD-3-Clause
 PYTHON_POSIX_IPC_LICENSE_FILES = LICENSE
 PYTHON_POSIX_IPC_SETUP_TYPE = setuptools
