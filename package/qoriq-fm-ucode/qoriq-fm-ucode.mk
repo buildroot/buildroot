a0c0ca5de7e96a8f7ab4b9f1759e7d34e2e12cf6
Akhilesh Nema <nemaakhilesh@gmail.com>
2025-10-12 20:14:50 -0700

    package/qoriq-fm-ucode: bump version to lf-6.12.34-2.1.0
    No change.
    
    Signed-off-by: Akhilesh Nema <nemaakhilesh@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/qoriq-fm-ucode/qoriq-fm-ucode.mk
+++ package/qoriq-fm-ucode/qoriq-fm-ucode.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-QORIQ_FM_UCODE_VERSION = lf-6.12.20-2.0.0
+QORIQ_FM_UCODE_VERSION = lf-6.12.34-2.1.0
 QORIQ_FM_UCODE_SITE = $(call github,nxp-qoriq,qoriq-fm-ucode,$(QORIQ_FM_UCODE_VERSION))
 QORIQ_FM_UCODE_LICENSE = NXP Binary EULA
 QORIQ_FM_UCODE_LICENSE_FILES = LICENSE
