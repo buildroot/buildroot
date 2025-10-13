d0ce0acfcf7bf1cdd546fa1de0524e64823a342d
Bernd Kuhls <bernd@kuhls.net>
2025-10-13 20:50:01 +0200

    package/utfcpp: bump version to 4.0.8
    Release notes: https://github.com/nemtrif/utfcpp/releases/tag/v4.0.8
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/utfcpp/utfcpp.mk
+++ package/utfcpp/utfcpp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UTFCPP_VERSION = 4.0.6
+UTFCPP_VERSION = 4.0.8
 UTFCPP_SITE = $(call github,nemtrif,utfcpp,v$(UTFCPP_VERSION))
 UTFCPP_LICENSE = BSL-1.0
 UTFCPP_LICENSE_FILES = LICENSE
