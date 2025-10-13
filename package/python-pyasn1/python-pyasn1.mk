14f401ee7882c5da2cdd353d53ce4ee3495a2d86
Bernd Kuhls <bernd@kuhls.net>
2025-10-13 19:54:46 +0200

    package/python-pyasn1: bump version to 0.6.1
    Changelog: https://github.com/pyasn1/pyasn1/blob/v0.6.1/CHANGES.rst
    
    Updated project URL according to https://pypi.org/project/pyasn1/
    
    Updated license hash due to copyright year bump:
    https://github.com/pyasn1/pyasn1/commit/acb78299d1270a2fea0d11e5509777b7c5777f3f
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 593f46dcdcfa95f587efa05a57e796f20293d70b)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/python-pyasn1/python-pyasn1.mk
+++ package/python-pyasn1/python-pyasn1.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYASN1_VERSION = 0.4.8
+PYTHON_PYASN1_VERSION = 0.6.1
 PYTHON_PYASN1_SOURCE = pyasn1-$(PYTHON_PYASN1_VERSION).tar.gz
-PYTHON_PYASN1_SITE = https://files.pythonhosted.org/packages/a4/db/fffec68299e6d7bad3d504147f9094830b704527a7fc098b721d38cc7fa7
+PYTHON_PYASN1_SITE = https://files.pythonhosted.org/packages/ba/e9/01f1a64245b89f039897cb0130016d79f77d52669aae6ee7b159a6c4c018
 PYTHON_PYASN1_SETUP_TYPE = setuptools
 PYTHON_PYASN1_LICENSE = BSD-2-Clause
 PYTHON_PYASN1_LICENSE_FILES = LICENSE.rst
