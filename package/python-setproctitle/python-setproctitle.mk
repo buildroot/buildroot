306ddbcfd63e181a9f44e358ad0c102b2d5d1edd
Bernd Kuhls <bernd@kuhls.net>
2025-10-13 19:55:05 +0200

    package/python-setproctitle: bump version to 1.3.7
    Updated license file name and hash due to upstream commits:
    https://github.com/dvarrazzo/py-setproctitle/commit/9950f38639e8bbda8d208bd4b8ec0531821e26a8
    https://github.com/dvarrazzo/py-setproctitle/commit/11d5ba71f0dc2bee2deb32ad17ac577101d404cd
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-setproctitle/python-setproctitle.mk
+++ package/python-setproctitle/python-setproctitle.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_SETPROCTITLE_VERSION = 1.3.4
+PYTHON_SETPROCTITLE_VERSION = 1.3.7
 PYTHON_SETPROCTITLE_SOURCE = setproctitle-$(PYTHON_SETPROCTITLE_VERSION).tar.gz
-PYTHON_SETPROCTITLE_SITE = https://files.pythonhosted.org/packages/ae/4e/b09341b19b9ceb8b4c67298ab4a08ef7a4abdd3016c7bb152e9b6379031d
+PYTHON_SETPROCTITLE_SITE = https://files.pythonhosted.org/packages/8d/48/49393a96a2eef1ab418b17475fb92b8fcfad83d099e678751b05472e69de
 PYTHON_SETPROCTITLE_LICENSE = BSD-3-Clause
-PYTHON_SETPROCTITLE_LICENSE_FILES = COPYRIGHT
+PYTHON_SETPROCTITLE_LICENSE_FILES = LICENSE
 PYTHON_SETPROCTITLE_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
