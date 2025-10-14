ff42a32e9b9defcb1f9e53d5236e0f7bc7000793
Bernd Kuhls <bernd@kuhls.net>
2025-10-14 20:51:13 +0200

    package/python-pycryptodomex: bump version to 3.23.0
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-pycryptodomex/python-pycryptodomex.mk
+++ package/python-pycryptodomex/python-pycryptodomex.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYCRYPTODOMEX_VERSION = 3.21.0
+PYTHON_PYCRYPTODOMEX_VERSION = 3.23.0
 PYTHON_PYCRYPTODOMEX_SOURCE = pycryptodomex-$(PYTHON_PYCRYPTODOMEX_VERSION).tar.gz
-PYTHON_PYCRYPTODOMEX_SITE = https://files.pythonhosted.org/packages/11/dc/e66551683ade663b5f07d7b3bc46434bf703491dbd22ee12d1f979ca828f
+PYTHON_PYCRYPTODOMEX_SITE = https://files.pythonhosted.org/packages/c9/85/e24bf90972a30b0fcd16c73009add1d7d7cd9140c2498a68252028899e41
 PYTHON_PYCRYPTODOMEX_SETUP_TYPE = setuptools
 PYTHON_PYCRYPTODOMEX_LICENSE = \
 	BSD-2-Clause, \
