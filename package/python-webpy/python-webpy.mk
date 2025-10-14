62037e4e9e702c6bbc255f6225e67fe6b8722376
Bernd Kuhls <bernd@kuhls.net>
2025-10-14 21:34:23 +0200

    package/python-webpy: bump version to 0.70 and add upstream commit to fix CVE-2025-3818
    Release notes: https://github.com/webpy/webpy/releases/tag/webpy-0.70
    
    Switched _SITE to github because version 0.70 was not released to
    pypi.org: https://pypi.org/project/web.py/#history
    
    Added upstream commit to fix CVE-2025-3818.
    
    This bump includes
    https://github.com/webpy/webpy/commit/f7540bef650bce7227af796728136d41e44cdd0e
    which removes the deprecated cgi module which was removed in Python 3.13
    and causes build errors since buildroot bumped python3 to 3.13.2 with
    commit d63e207eb869063f82c867658676c2903beb08cb.
    
    Fixes:
    https://autobuild.buildroot.net/results/5a7/5a7382250d33ddfd49de2c7996fb54aaf20dd62c/
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Arnout Vandecappelle <arnout@rnout.be>
    (cherry picked from commit d05e2f785de6e0e51590160e002d1fd6e143d403)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/python-webpy/python-webpy.mk
+++ package/python-webpy/python-webpy.mk
@@ -4,9 +4,8 @@
 #
 ################################################################################
 
-PYTHON_WEBPY_VERSION = 0.62
-PYTHON_WEBPY_SOURCE = web.py-$(PYTHON_WEBPY_VERSION).tar.gz
-PYTHON_WEBPY_SITE = https://files.pythonhosted.org/packages/cd/6e/338a060bb5b52ee8229bdada422eaa5f71b13f8d33467f37f870ed2cae4b
+PYTHON_WEBPY_VERSION = 0.70
+PYTHON_WEBPY_SITE = $(call github,webpy,webpy,webpy-$(PYTHON_WEBPY_VERSION))
 PYTHON_WEBPY_SETUP_TYPE = setuptools
 PYTHON_WEBPY_LICENSE = Public Domain
 
