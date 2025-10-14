828a63c5fc69a3acca8053ddcac12b9d12a2b0d1
Bernd Kuhls <bernd@kuhls.net>
2025-10-14 21:03:26 +0200

    package/python-oauthlib: bump version to 3.3.1
    Release notes:
    https://github.com/oauthlib/oauthlib/releases/tag/v3.3.0
    https://github.com/oauthlib/oauthlib/releases/tag/v3.3.1
    
    Updated license hash due to upstream commits:
    https://github.com/oauthlib/oauthlib/commit/1137a2fbedb586f105c6512dabe3da17d0f8f88a
    https://github.com/oauthlib/oauthlib/commit/059875a888d2200e43e18b818eda8442b2c3d0cc
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-oauthlib/python-oauthlib.mk
+++ package/python-oauthlib/python-oauthlib.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_OAUTHLIB_VERSION = 3.2.2
+PYTHON_OAUTHLIB_VERSION = 3.3.1
 PYTHON_OAUTHLIB_SOURCE = oauthlib-$(PYTHON_OAUTHLIB_VERSION).tar.gz
-PYTHON_OAUTHLIB_SITE = https://files.pythonhosted.org/packages/6d/fa/fbf4001037904031639e6bfbfc02badfc7e12f137a8afa254df6c4c8a670
+PYTHON_OAUTHLIB_SITE = https://files.pythonhosted.org/packages/0b/5f/19930f824ffeb0ad4372da4812c50edbd1434f678c90c2733e1188edfc63
 PYTHON_OAUTHLIB_SETUP_TYPE = setuptools
 PYTHON_OAUTHLIB_LICENSE = BSD-3-Clause
 PYTHON_OAUTHLIB_LICENSE_FILES = LICENSE
