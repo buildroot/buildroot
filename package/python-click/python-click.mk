c31a86ebf66cc322212f1045cb2625cde1b83dc5
Marcus Hoffmann <buildroot@bubu1.eu>
2025-10-15 12:52:56 +0200

    package/python-click: bump to 8.3.0
    Release Notes: https://click.palletsprojects.com/en/stable/changes/#version-8-3-0
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-click/python-click.mk
+++ package/python-click/python-click.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_CLICK_VERSION = 8.2.1
+PYTHON_CLICK_VERSION = 8.3.0
 PYTHON_CLICK_SOURCE = click-$(PYTHON_CLICK_VERSION).tar.gz
-PYTHON_CLICK_SITE = https://files.pythonhosted.org/packages/60/6c/8ca2efa64cf75a977a0d7fac081354553ebe483345c734fb6b6515d96bbc
+PYTHON_CLICK_SITE = https://files.pythonhosted.org/packages/46/61/de6cd827efad202d7057d93e0fed9294b96952e188f7384832791c7b2254
 PYTHON_CLICK_SETUP_TYPE = flit
 PYTHON_CLICK_LICENSE = BSD-3-Clause
 PYTHON_CLICK_LICENSE_FILES = LICENSE.txt
