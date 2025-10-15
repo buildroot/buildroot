60732a6a51f42d50365b55b8255ab67757894f40
Marcus Hoffmann <buildroot@bubu1.eu>
2025-10-15 11:56:40 +0200

    package/python-starlette: bump to 0.48.0
    Release Notes: https://github.com/Kludex/starlette/releases/tag/0.48.0
    Github namespace changed, so change the link in Config.in.
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 62cfbc1409aca2a9a6d19419189f05bed947f516)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/python-starlette/python-starlette.mk
+++ package/python-starlette/python-starlette.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_STARLETTE_VERSION = 0.47.3
+PYTHON_STARLETTE_VERSION = 0.48.0
 PYTHON_STARLETTE_SOURCE = starlette-$(PYTHON_STARLETTE_VERSION).tar.gz
-PYTHON_STARLETTE_SITE = https://files.pythonhosted.org/packages/15/b9/cc3017f9a9c9b6e27c5106cc10cc7904653c3eec0729793aec10479dd669
+PYTHON_STARLETTE_SITE = https://files.pythonhosted.org/packages/a7/a5/d6f429d43394057b67a6b5bbe6eae2f77a6bf7459d961fdb224bf206eee6
 PYTHON_STARLETTE_SETUP_TYPE = hatch
 PYTHON_STARLETTE_LICENSE = BSD-3-Clause
 PYTHON_STARLETTE_LICENSE_FILES = LICENSE.md
