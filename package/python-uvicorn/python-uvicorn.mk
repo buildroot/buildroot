c41f5e2edde466ef79885f3a88322365a44e6d0d
Marcus Hoffmann <buildroot@bubu1.eu>
2025-10-15 12:27:18 +0200

    package/python-uvicorn: bump to 0.37.0
    License hash changed due to adding new author name in [1].
    Website changed to https://uvicorn.dev/.
    
    Release Notes: https://github.com/Kludex/uvicorn/blob/main/docs/release-notes.md#0370-september-23-2025
    
    [1] https://github.com/Kludex/uvicorn/commit/fe50e5d9933fc09c347b991d6376c02ada3f0f21
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-uvicorn/python-uvicorn.mk
+++ package/python-uvicorn/python-uvicorn.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_UVICORN_VERSION = 0.35.0
+PYTHON_UVICORN_VERSION = 0.37.0
 PYTHON_UVICORN_SOURCE = uvicorn-$(PYTHON_UVICORN_VERSION).tar.gz
-PYTHON_UVICORN_SITE = https://files.pythonhosted.org/packages/5e/42/e0e305207bb88c6b8d3061399c6a961ffe5fbb7e2aa63c9234df7259e9cd
+PYTHON_UVICORN_SITE = https://files.pythonhosted.org/packages/71/57/1616c8274c3442d802621abf5deb230771c7a0fec9414cb6763900eb3868
 PYTHON_UVICORN_SETUP_TYPE = hatch
 PYTHON_UVICORN_LICENSE = BSD-3-Clause
 PYTHON_UVICORN_LICENSE_FILES = LICENSE.md
