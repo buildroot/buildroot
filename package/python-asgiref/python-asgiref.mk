c53d6ad99ce80ff381c6234ef58b638c53221cef
Marcus Hoffmann <buildroot@bubu1.eu>
2025-10-15 14:02:39 +0200

    package/python-asgiref: bump to 3.10.0
    CHANGELOG:
    https://github.com/django/asgiref/blob/3.10.0/CHANGELOG.txt#L1-L13
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    [Julien: use tag in changelog url]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-asgiref/python-asgiref.mk
+++ package/python-asgiref/python-asgiref.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ASGIREF_VERSION = 3.9.1
+PYTHON_ASGIREF_VERSION = 3.10.0
 PYTHON_ASGIREF_SOURCE = asgiref-$(PYTHON_ASGIREF_VERSION).tar.gz
-PYTHON_ASGIREF_SITE = https://files.pythonhosted.org/packages/90/61/0aa957eec22ff70b830b22ff91f825e70e1ef732c06666a805730f28b36b
+PYTHON_ASGIREF_SITE = https://files.pythonhosted.org/packages/46/08/4dfec9b90758a59acc6be32ac82e98d1fbfc321cb5cfa410436dbacf821c
 PYTHON_ASGIREF_SETUP_TYPE = setuptools
 PYTHON_ASGIREF_LICENSE = BSD-3-Clause
 PYTHON_ASGIREF_LICENSE_FILES = LICENSE
