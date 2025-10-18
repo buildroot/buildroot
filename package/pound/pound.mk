7c511dacefa9142343cbd1b371b261c9c6349bfc
Bernd Kuhls <bernd@kuhls.net>
2025-10-18 21:46:20 +0200

    package/pound: bump version to 4.17
    Removed patch 0001, upstream committed a different fix:
    https://github.com/graygnuorg/pound/pull/17#issuecomment-1757235640
    
    Removed "--with-owner" and "--with-group" configure options:
    https://github.com/graygnuorg/pound/commit/6537e9c97bce1f84254ada0104f72264406a1d0d
    
    Added optional dependency to libxcrypt for glibc needed due to commit:
    https://github.com/graygnuorg/pound/commit/32c32b248fe99650f3949b7ee7502a2ff5335f24
    
    Updated pcre-related configure options due to commit:
    https://github.com/graygnuorg/pound/commit/a3d2b9e0d41c2d5c0487dba0ea5d5178cdfae9c7
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/pound/pound.mk
+++ package/pound/pound.mk
@@ -4,28 +4,26 @@
 #
 ################################################################################
 
-POUND_VERSION = 4.8
+POUND_VERSION = 4.17
 POUND_SITE = https://github.com/graygnuorg/pound/releases/download/v$(POUND_VERSION)
 POUND_LICENSE = GPL-3.0+
 POUND_LICENSE_FILES = COPYING
 POUND_DEPENDENCIES = openssl host-openssl
 
-# Force owner/group to us, otherwise it will try proxy:proxy by
-# default.
-POUND_CONF_OPTS = \
-	--with-owner=$(shell id -un) \
-	--with-group=$(shell id -gn)
+ifeq ($(BR2_PACKAGE_LIBXCRYPT),y)
+POUND_DEPENDENCIES += libxcrypt
+endif
 
 ifeq ($(BR2_PACKAGE_PCRE2),y)
-POUND_CONF_OPTS += --enable-pcreposix=pcre2
+POUND_CONF_OPTS += --enable-pcre=2
 POUND_CONF_ENV += \
 	ac_cv_path_PCRE2_CONFIG=$(STAGING_DIR)/usr/bin/pcre2-config
 POUND_DEPENDENCIES += pcre2
 else ifeq ($(BR2_PACKAGE_PCRE),y)
-POUND_CONF_OPTS += --enable-pcreposix=pcre1
+POUND_CONF_OPTS += --enable-pcre=1
 POUND_DEPENDENCIES += pcre
 else
-POUND_CONF_OPTS += --disable-pcreposix
+POUND_CONF_OPTS += --disable-pcre
 endif
 
 $(eval $(autotools-package))
