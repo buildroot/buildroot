c2b863bdb17284fc35c5188292d8491cef5c285b
Dario Binacchi <dario.binacchi@amarulasolutions.com>
2025-10-19 17:21:12 +0200

    package/libarchive: security bump to version 3.8.2
    This bump includes the security fix for CVE-2025-25724.
    
    Fixes:
    https://www.cve.org/CVERecord?id=CVE-2025-25724.
    
    Release notes:
    https://github.com/libarchive/libarchive/releases/tag/v3.8.2
    
    Signed-off-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit a36b7cf77e7d44e7abf5c939baabc67e030282e8)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/libarchive/libarchive.mk
+++ package/libarchive/libarchive.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBARCHIVE_VERSION = 3.8.1
+LIBARCHIVE_VERSION = 3.8.2
 LIBARCHIVE_SOURCE = libarchive-$(LIBARCHIVE_VERSION).tar.xz
 LIBARCHIVE_SITE = https://www.libarchive.de/downloads
 LIBARCHIVE_INSTALL_STAGING = YES
