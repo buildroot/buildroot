fabcd7c8cc44e7e03c68bfcd2b4e2d10331a93f6
Bernd Kuhls <bernd@kuhls.net>
2025-10-20 22:05:39 +0200

    package/bitcoin: bump to version 30.0
    Changelog:
    https://bitcoincore.org/en/releases/29.0/
    https://bitcoincore.org/en/releases/30.0/
    
    License file hash changed, due to year update in upstream commit:
    https://github.com/bitcoin/bitcoin/commit/b537a2c02a9921235d1ecf8c3c7dc1836ec68131
    
    Switch build system to CMake.
    
    https://bitcoincore.org/en/releases/29.0/
    - The minimum required CMake version is 3.22.
    - In-source builds are not allowed.
    - MiniUPnPc and libnatpmp have been removed as dependencies
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/bitcoin/bitcoin.mk
+++ package/bitcoin/bitcoin.mk
@@ -4,48 +4,35 @@
 #
 ################################################################################
 
-BITCOIN_VERSION = 28.0
+BITCOIN_VERSION = 30.0
 BITCOIN_SITE = https://bitcoincore.org/bin/bitcoin-core-$(BITCOIN_VERSION)
-BITCOIN_AUTORECONF = YES
 BITCOIN_LICENSE = MIT
 BITCOIN_LICENSE_FILES = COPYING
 BITCOIN_CPE_ID_VENDOR = bitcoin
 BITCOIN_CPE_ID_PRODUCT = bitcoin_core
 BITCOIN_DEPENDENCIES = host-pkgconf boost libevent
+BITCOIN_SUPPORTS_IN_SOURCE_BUILD = NO
 BITCOIN_MAKE_ENV = BITCOIN_GENBUILD_NO_GIT=1
-# Berkeley Database (bdb) support is always disabled. It is only
-# needed for legacy wallet format. New wallets are using SQLite.
 BITCOIN_CONF_OPTS = \
-	--disable-bench \
-	--disable-tests \
-	--with-boost-libdir=$(STAGING_DIR)/usr/lib/ \
-	--disable-hardening \
-	--without-bdb \
-	--without-gui
+	-DBUILD_BENCH=OFF \
+	-DBUILD_TESTS=OFF \
+	-DBUILD_GUI=OFF \
+	-DENABLE_IPC=OFF
 
 ifeq ($(BR2_PACKAGE_BITCOIN_WALLET),y)
 BITCOIN_DEPENDENCIES += sqlite
 BITCOIN_CONF_OPTS += \
-	--enable-wallet \
-	--with-sqlite
+	-DENABLE_WALLET=ON
 else
 BITCOIN_CONF_OPTS += \
-	--disable-wallet \
-	--without-sqlite
-endif
-
-ifeq ($(BR2_PACKAGE_LIBMINIUPNPC),y)
-BITCOIN_DEPENDENCIES += libminiupnpc
-BITCOIN_CONF_OPTS += --with-miniupnpc
-else
-BITCOIN_CONF_OPTS += --without-miniupnpc
+	-DENABLE_WALLET=OFF
 endif
 
 ifeq ($(BR2_PACKAGE_ZEROMQ),y)
 BITCOIN_DEPENDENCIES += zeromq
-BITCOIN_CONF_OPTS += --with-zmq
+BITCOIN_CONF_OPTS += -DWITH_ZMQ=ON
 else
-BITCOIN_CONF_OPTS += --without-zmq
+BITCOIN_CONF_OPTS += -DWITH_ZMQ=OFF
 endif
 
-$(eval $(autotools-package))
+$(eval $(cmake-package))
