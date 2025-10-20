c555b6565f2747047603dc8022f81b7ea14b4890
Dario Binacchi <dario.binacchi@amarulasolutions.com>
2025-10-20 18:50:50 +0200

    package/libbsd: bump version to 0.12.2
    Release notes:
    https://archive.hadrons.org/software/libbsd/libbsd-0.11.8.announce
    https://archive.hadrons.org/software/libbsd/libbsd-0.12.0.announce
    https://archive.hadrons.org/software/libbsd/libbsd-0.12.1.announce
    https://archive.hadrons.org/software/libbsd/libbsd-0.12.2.announce
    
    Update hash of license file, which has changed due to:
    - Copyright year updates.
    - Switch arc4random(3bsd) man page from OpenBSD to NetBSD. This gets
      rid of the last BSD-4-clause licensed file in the project.
    - Remove written-by attribution.
    
    Signed-off-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>
    [Julien:
     - fix upstream hash url in hash file comment
     - remove BSD-4-Clause from _LICENSE
    ]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libbsd/libbsd.mk
+++ package/libbsd/libbsd.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-LIBBSD_VERSION = 0.11.7
+LIBBSD_VERSION = 0.12.2
 LIBBSD_SOURCE = libbsd-$(LIBBSD_VERSION).tar.xz
 LIBBSD_SITE = https://libbsd.freedesktop.org/releases
-LIBBSD_LICENSE = BSD-2-Clause, BSD-3-Clause, BSD-4-Clause, BSD-5-Clause, \
+LIBBSD_LICENSE = BSD-2-Clause, BSD-3-Clause, BSD-5-Clause, \
 		MIT, ISC, Beerware
 LIBBSD_LICENSE_FILES = COPYING
 LIBBSD_CPE_ID_VENDOR = freedesktop
