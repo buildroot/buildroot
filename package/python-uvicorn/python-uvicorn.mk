e7a0ff61c02e755f1cbf49b78600ccb4a4069a7a
Marcus Hoffmann <buildroot@bubu1.eu>
2025-10-21 11:04:21 +0200

    package/python-uvicorn: bump to 0.38.0
    Officially supports python 3.14 now:
    https://uvicorn.dev/release-notes/#0380-october-18-2025
    
    LICENSE.md file changed due revert of copyright holder name:
    https://github.com/Kludex/uvicorn/commit/ce79f95d065c053b36449f67b3b2cefee5ecf167
    
    Both uvicorn and starlette are currently undergoing some discussions
    around the copyright holder (but not the license):
    https://github.com/Kludex/starlette/issues/3042
    
    The LICENSE.md file will very likely be updated again soon.
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-uvicorn/python-uvicorn.mk
+++ package/python-uvicorn/python-uvicorn.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_UVICORN_VERSION = 0.37.0
+PYTHON_UVICORN_VERSION = 0.38.0
 PYTHON_UVICORN_SOURCE = uvicorn-$(PYTHON_UVICORN_VERSION).tar.gz
-PYTHON_UVICORN_SITE = https://files.pythonhosted.org/packages/71/57/1616c8274c3442d802621abf5deb230771c7a0fec9414cb6763900eb3868
+PYTHON_UVICORN_SITE = https://files.pythonhosted.org/packages/cb/ce/f06b84e2697fef4688ca63bdb2fdf113ca0a3be33f94488f2cadb690b0cf
 PYTHON_UVICORN_SETUP_TYPE = hatch
 PYTHON_UVICORN_LICENSE = BSD-3-Clause
 PYTHON_UVICORN_LICENSE_FILES = LICENSE.md
