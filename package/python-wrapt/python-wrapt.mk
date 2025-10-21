f683ffcbe1f92290c8df78be06379998f3cef689
Marcus Hoffmann <buildroot@bubu1.eu>
2025-10-21 10:10:28 +0200

    package/python-wrapt: bump to 2.0.0
    LICENSE file changes due to copyright year update:
    https://github.com/GrahamDumpleton/wrapt/commit/ad97a2aa802391e2cffd31b601801214436d7ddd
    
    CHANGELOG: https://wrapt.readthedocs.io/en/latest/changes.html#version-2-0-0
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-wrapt/python-wrapt.mk
+++ package/python-wrapt/python-wrapt.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_WRAPT_VERSION = 1.17.3
+PYTHON_WRAPT_VERSION = 2.0.0
 PYTHON_WRAPT_SOURCE = wrapt-$(PYTHON_WRAPT_VERSION).tar.gz
-PYTHON_WRAPT_SITE = https://files.pythonhosted.org/packages/95/8f/aeb76c5b46e273670962298c23e7ddde79916cb74db802131d49a85e4b7d
+PYTHON_WRAPT_SITE = https://files.pythonhosted.org/packages/49/19/5e5bcd855d808892fe02d49219f97a50f64cd6d8313d75df3494ee97b1a3
 PYTHON_WRAPT_SETUP_TYPE = setuptools
 PYTHON_WRAPT_LICENSE = BSD-2-Clause
 PYTHON_WRAPT_LICENSE_FILES = LICENSE
