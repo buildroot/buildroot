75bcd1ebd60b474d155e2a835da09d927566f367
Bernd Kuhls <bernd@kuhls.net>
2025-10-25 18:48:35 +0200

    package/sqlite: bump version to 3.50.4
    This commit includes reverts of the last attempt to bump sqlite:
    git revert --no-edit c4c282f8ec 014174f00d 835b5659ea db481210e9 296744b0f4 939244b828 f0a272ef14 274c36578f
    
    and the following new changes:
    
    The autoconf-based configure script until version 3.48.0 enabled rtree
    support by default:
    https://github.com/sqlite/sqlite/blob/version-3.48.0/autoconf/configure.ac#L180
    
    while the autosetup-based needs -DSQLITE_ENABLE_RTREE
    https://github.com/sqlite/sqlite/blob/version-3.50.4/autosetup/sqlite-config.tcl#L763
    
    Added Config.in option for RTREE support and enabled it for gdal to
    provide a needed dependency:
    https://github.com/OSGeo/gdal/blob/v3.11.4/cmake/helpers/CheckDependentLibraries.cmake#L277
    
    Added upstream patch to fix forced addition of rpath and added
    configure option --disable-rpath to target build.
    
    Added configure option --soname=legacy to fix wrong library name which
    breaks linking of kodi:
    https://github.com/LibreELEC/LibreELEC.tv/commit/7f2281bd7cc59425e754ac9d720071688f75580f
    
    Added configure option --disable-shared for static builds.
    
    Added build fixes for BR2_m68k_cf.
    
    Removed SQLITE_CONF_OPTS and readline support from host build to avoid
    dependencies.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Tested-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 2cc346e763341f3abf88096701b3deb0889c43bb)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/sqlite/sqlite.mk
+++ package/sqlite/sqlite.mk
@@ -4,19 +4,14 @@
 #
 ################################################################################
 
-SQLITE_VERSION = 3.48.0
-SQLITE_TAR_VERSION = 3480000
+SQLITE_VERSION = 3.50.4
+SQLITE_TAR_VERSION = 3500400
 SQLITE_SOURCE = sqlite-autoconf-$(SQLITE_TAR_VERSION).tar.gz
 SQLITE_SITE = https://www.sqlite.org/2025
 SQLITE_LICENSE = blessing
 SQLITE_LICENSE_FILES = tea/license.terms
 SQLITE_CPE_ID_VENDOR = sqlite
 SQLITE_INSTALL_STAGING = YES
-
-# 0002-Add-a-typecast-to-avoid-32-bit-integer-overflow-in-t.patch
-SQLITE_IGNORE_CVES = CVE-2025-29087 CVE-2025-3277
-# 0003-Raise-error-if-too-many-aggregate-terms.patch
-SQLITE_IGNORE_CVES += CVE-2025-6965
 
 ifeq ($(BR2_PACKAGE_SQLITE_STAT4),y)
 SQLITE_CFLAGS += -DSQLITE_ENABLE_STAT4
@@ -26,8 +21,8 @@ ifeq ($(BR2_PACKAGE_SQLITE_ENABLE_COLUMN_METADATA),y)
 SQLITE_CFLAGS += -DSQLITE_ENABLE_COLUMN_METADATA
 endif
 
-ifeq ($(BR2_PACKAGE_SQLITE_ENABLE_FTS3),y)
-SQLITE_CFLAGS += -DSQLITE_ENABLE_FTS3
+ifeq ($(BR2_PACKAGE_SQLITE_ENABLE_RTREE),y)
+SQLITE_CFLAGS += -DSQLITE_ENABLE_RTREE
 endif
 
 ifeq ($(BR2_PACKAGE_SQLITE_ENABLE_UNLOCK_NOTIFY),y)
@@ -46,35 +41,86 @@ endif
 # Work around using -O0
 ifeq ($(BR2_microblaze):$(BR2_TOOLCHAIN_GCC_AT_LEAST_5),y:)
 SQLITE_CFLAGS += $(TARGET_CFLAGS) -O0
+else ifeq ($(BR2_TOOLCHAIN_HAS_GCC_BUG_104028),y)
+SQLITE_CFLAGS += $(TARGET_CFLAGS) -O0
 else
 # fallback to standard -O3 when -Ofast is present to avoid -ffast-math
 SQLITE_CFLAGS += $(subst -Ofast,-O3,$(TARGET_CFLAGS))
 endif
 
-SQLITE_CONF_ENV = CFLAGS="$(SQLITE_CFLAGS)"
-
-ifeq ($(BR2_STATIC_LIBS),y)
-SQLITE_CONF_OPTS += --enable-dynamic-extensions=no
-else
-SQLITE_CONF_OPTS += --disable-static-shell
-endif
-
-ifeq ($(BR2_TOOLCHAIN_HAS_THREADS),y)
-SQLITE_CONF_OPTS += --enable-threadsafe
-else
-SQLITE_CONF_OPTS += --disable-threadsafe
-SQLITE_CFLAGS += -DSQLITE_THREADSAFE=0
-endif
-
 ifeq ($(BR2_PACKAGE_NCURSES)$(BR2_PACKAGE_READLINE),yy)
 SQLITE_DEPENDENCIES += ncurses readline
-SQLITE_CONF_OPTS += --disable-editline --enable-readline
-else ifeq ($(BR2_PACKAGE_LIBEDIT),y)
-SQLITE_DEPENDENCIES += libedit
-SQLITE_CONF_OPTS += --enable-editline --disable-readline
+SQLITE_CFLAGS  += -DHAVE_READLINE=1
+SQLITE_LDFLAGS += -lreadline -lncurses
+else ifeq ($(BR2_PACKAGE_NCURSES)$(BR2_PACKAGE_LIBEDIT),yy)
+SQLITE_DEPENDENCIES += ncurses libedit
+SQLITE_CFLAGS  += -DHAVE_EDITLINE=1
+SQLITE_LDFLAGS += -ledit -lncurses
 else
-SQLITE_CONF_OPTS += --disable-editline --disable-readline
+SQLITE_CONF_OPTS += --disable-readline
 endif
 
-$(eval $(autotools-package))
-$(eval $(host-autotools-package))
+ifeq ($(BR2_PACKAGE_SQLITE_ENABLE_FTS3),y)
+SQLITE_CONF_OPTS += --fts3
+endif
+
+ifeq ($(BR2_STATIC_LIBS),y)
+SQLITE_CONF_OPTS += --disable-shared
+endif
+
+SQLITE_CONF_ENV = CFLAGS="$(SQLITE_CFLAGS)" LDFLAGS="$(SQLITE_LDFLAGS)"
+
+# https://gcc.gnu.org/bugzilla/show_bug.cgi?id=101971
+define SQLITE_FIX_MAKEFILE
+	chmod +w $(@D)/Makefile
+	sed -i "s/-fPIC//" $(@D)/Makefile
+endef
+
+define SQLITE_CONFIGURE_CMDS
+	(cd $(@D); $(TARGET_CONFIGURE_OPTS) \
+		$(if $(BR2_INSTALL_LIBSTDCPP),,CXX=false) \
+		$(SQLITE_CONF_ENV) ./configure \
+		--prefix=/usr \
+		--host="$(GNU_TARGET_NAME)" \
+		--build="$(GNU_HOST_NAME)" \
+		--sysroot="$(STAGING_DIR)" \
+		--disable-rpath \
+		--soname=legacy \
+		$(SQLITE_CONF_OPTS) \
+	)
+	$(if $(BR2_m68k_cf),$(SQLITE_FIX_MAKEFILE))
+endef
+
+define SQLITE_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define SQLITE_INSTALL_STAGING_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) DESTDIR="$(STAGING_DIR)" -C $(@D) install
+endef
+
+define SQLITE_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) DESTDIR="$(TARGET_DIR)" -C $(@D) install
+endef
+
+define HOST_SQLITE_CONFIGURE_CMDS
+	(cd $(@D); $(HOST_CONFIGURE_OPTS) \
+		./configure \
+		--prefix=/usr \
+		--host="$(GNU_HOST_NAME)" \
+		--build="$(GNU_HOST_NAME)" \
+		--sysroot="$(HOST_DIR)" \
+		--disable-readline \
+	)
+endef
+
+define HOST_SQLITE_BUILD_CMDS
+	$(HOST_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define HOST_SQLITE_INSTALL_CMDS
+	$(HOST_MAKE_ENV) $(MAKE) DESTDIR="$(HOST_DIR)" -C $(@D) install
+endef
+
+$(eval $(generic-package))
+$(eval $(host-generic-package))
