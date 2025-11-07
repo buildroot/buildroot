5d8c1af6a562cf1d0b28f03240ce098f59151464
Alexander Shirokov <shirokovalexs@gmail.com>
2025-11-07 16:10:28 +0100

    package/broot: new package
    This commit adds broot, a fast and powerful file manager written
    in Rust and licensed under MIT license. It supports filters, regular
    expressions, and real-time "show-as-you-type" search for quick and easy
    navigation, even in complex directory structures.
    
    Signed-off-by: Alexander Shirokov <shirokovalexs@gmail.com>
    [Peter: use select rather than depends on for rustc]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/broot/broot.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# broot
+#
+################################################################################
+
+BROOT_VERSION = 1.52.0
+BROOT_SITE = $(call github,Canop,broot,v$(BROOT_VERSION))
+BROOT_LICENSE = MIT
+BROOT_LICENSE_FILES = LICENSE
+
+$(eval $(cargo-package))
