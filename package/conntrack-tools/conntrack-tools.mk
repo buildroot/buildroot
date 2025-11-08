2eded963ee54173867914fee05785122e0f34ee8
Dario Binacchi <dario.binacchi@amarulasolutions.com>
2025-11-08 11:55:10 +0100

    package/conntrack-tools: bump to version 1.4.8
    Upstream now ships the source archive as .tar.xz instead of .tar.bz2.
    
    Release notes:
    https://netfilter.org/pub/conntrack-tools/changes-conntrack-tools-1.4.8.txt
    
    Signed-off-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/conntrack-tools/conntrack-tools.mk
+++ package/conntrack-tools/conntrack-tools.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-CONNTRACK_TOOLS_VERSION = 1.4.7
-CONNTRACK_TOOLS_SOURCE = conntrack-tools-$(CONNTRACK_TOOLS_VERSION).tar.bz2
+CONNTRACK_TOOLS_VERSION = 1.4.8
+CONNTRACK_TOOLS_SOURCE = conntrack-tools-$(CONNTRACK_TOOLS_VERSION).tar.xz
 CONNTRACK_TOOLS_SITE = http://www.netfilter.org/projects/conntrack-tools/files
 CONNTRACK_TOOLS_DEPENDENCIES = host-pkgconf \
 	libnetfilter_conntrack libnetfilter_cthelper libnetfilter_cttimeout \
