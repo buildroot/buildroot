582ee9c2fdcc864a86e8726c0594b27791e52b6a
Bernd Kuhls <bernd@kuhls.net>
2025-11-08 09:20:53 +0100

    package/libnvme: bump version to 1.16.1
    Release notes:
    https://github.com/linux-nvme/libnvme/releases/tag/v1.16.1
    
    Removed patch which is included in this release.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libnvme/libnvme.mk
+++ package/libnvme/libnvme.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBNVME_VERSION = 1.16
+LIBNVME_VERSION = 1.16.1
 LIBNVME_SITE = $(call github,linux-nvme,libnvme,v$(LIBNVME_VERSION))
 LIBNVME_LICENSE = LGPL-2.1
 LIBNVME_LICENSE_FILES = COPYING
