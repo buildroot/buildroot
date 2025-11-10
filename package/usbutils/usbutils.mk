addbee75174d93e8b41d4a952b84d633ccfa5187
Akhilesh Nema <nemaakhilesh@gmail.com>
2025-11-09 23:07:36 -0800

    package/usbutils: bump to version 019
    Release announcement:
    https://lwn.net/Articles/1042924/
    
    Changelog:
    https://github.com/gregkh/usbutils/compare/v018...v019
    
    Signed-off-by: Akhilesh Nema <nemaakhilesh@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/usbutils/usbutils.mk
+++ package/usbutils/usbutils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-USBUTILS_VERSION = 018
+USBUTILS_VERSION = 019
 USBUTILS_SOURCE = usbutils-$(USBUTILS_VERSION).tar.xz
 USBUTILS_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/usb/usbutils
 USBUTILS_DEPENDENCIES = host-pkgconf libusb udev
