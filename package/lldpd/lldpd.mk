03fe784a0afa0609723971577e3135d0a1f54e1a
Akhilesh Nema <nemaakhilesh@gmail.com>
2025-11-09 22:41:13 -0800

    package/lldpd: bump to version 1.0.20
    Release notes:
    https://github.com/lldpd/lldpd/releases/tag/1.0.20
    https://github.com/lldpd/lldpd/releases/tag/1.0.19
    
    Signed-off-by: Akhilesh Nema <nemaakhilesh@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 779e7cd7a926671e487d83340d130553ef430ad7)
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

--- package/lldpd/lldpd.mk
+++ package/lldpd/lldpd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LLDPD_VERSION = 1.0.18
+LLDPD_VERSION = 1.0.20
 LLDPD_SITE = https://media.luffy.cx/files/lldpd
 LLDPD_DEPENDENCIES = \
 	$(if $(BR2_PACKAGE_CHECK),check) \
