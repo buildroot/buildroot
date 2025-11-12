631259099845212c6f0d1f4699fc4782f55a102f
Martin Kepplinger-Novaković <martink@posteo.de>
2025-11-12 06:55:43 +0000

    package/tslib: bump version to 1.24
    Release notes from https://github.com/libts/tslib/releases :
    
    This release includes libts version 0.10.5 and the following changes:
    
      * improved release procedure
      * debug fixes for 32bit systems
      * CMake and autoconf updates for newer versions
      * fixes for minor cppcheck errors
      * ts_conf test program fixes
    
    Signed-off-by: Martin Kepplinger-Novaković <martink@posteo.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/tslib/tslib.mk
+++ package/tslib/tslib.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TSLIB_VERSION = 1.23
+TSLIB_VERSION = 1.24
 TSLIB_SITE = https://github.com/libts/tslib/releases/download/$(TSLIB_VERSION)
 TSLIB_SOURCE = tslib-$(TSLIB_VERSION).tar.xz
 TSLIB_LICENSE = GPL-2.0+ (programs), LGPL-2.1+ (libraries)
