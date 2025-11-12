d2afa96009857e96736285444afba071d8615986
Bernd Kuhls <bernd@kuhls.net>
2025-11-12 20:12:57 +0100

    package/sqlite: bump version to 3.51.0
    Release notes: https://sqlite.org/releaselog/3_51_0.html
    
    Removed patch which is included in this release.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/sqlite/sqlite.mk
+++ package/sqlite/sqlite.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-SQLITE_VERSION = 3.50.4
-SQLITE_TAR_VERSION = 3500400
+SQLITE_VERSION = 3.51.0
+SQLITE_TAR_VERSION = 3510000
 SQLITE_SOURCE = sqlite-autoconf-$(SQLITE_TAR_VERSION).tar.gz
 SQLITE_SITE = https://www.sqlite.org/2025
 SQLITE_LICENSE = blessing
