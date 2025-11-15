25b5ecf30bd652c8164359d1eaee4dfd3d2ea44d
Devin Steffler <devin.buildroot@gmail.com>
2025-11-15 17:48:25 +0100

    package/lttng-modules: bump to version 2.14.3
    A changelog for lttng-modules 2.14.3 can be found here:
    
      https://git.lttng.org/?p=lttng-modules.git;a=blob;f=ChangeLog;h=be6148022eca5084d05d959bccf27d88e2ed2073;hb=refs/heads/stable-2.14
    
    The license files have changed due to upstream commit:
    
      https://git.lttng.org/?p=lttng-modules.git;a=commit;h=61baff6e8de2462f45006662bc34bcbf5f645ba0
    
    Signed-off-by: Devin Steffler <devin.buildroot@gmail.com>
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    [Bernd:
     - bumped to 2.14.3, updated changelog link
     - Updated license files and hashes due to upstream commit:
       https://git.lttng.org/?p=lttng-modules.git;a=commit;h=61baff6e8de2462f45006662bc34bcbf5f645ba0]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/lttng-modules/lttng-modules.mk
+++ package/lttng-modules/lttng-modules.mk
@@ -4,12 +4,15 @@
 #
 ################################################################################
 
-LTTNG_MODULES_VERSION = 2.13.22
+LTTNG_MODULES_VERSION = 2.14.3
 LTTNG_MODULES_SITE = http://lttng.org/files/lttng-modules
 LTTNG_MODULES_SOURCE = lttng-modules-$(LTTNG_MODULES_VERSION).tar.bz2
 LTTNG_MODULES_LICENSE = LGPL-2.1/GPL-2.0 (kernel modules), MIT (lib/bitfield.h, lib/prio_heap/*)
 LTTNG_MODULES_LICENSE_FILES = \
-	LICENSES/LGPL-2.1 LICENSES/GPL-2.0 LICENSES/MIT LICENSE
+	LICENSES/GPL-2.0-only.txt \
+	LICENSES/LGPL-2.1-only.txt \
+	LICENSES/MIT.txt \
+	LICENSE
 LTTNG_MODULES_MODULE_MAKE_OPTS = CONFIG_LTTNG=m CONFIG_LTTNG_CLOCK_PLUGIN_TEST=m
 
 define LTTNG_MODULES_LINUX_CONFIG_FIXUPS
