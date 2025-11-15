c2cf998b8d610b9894467e0a6e4e57397e99ed34
Bernd Kuhls <bernd@kuhls.net>
2025-11-15 17:48:24 +0100

    package/liburcu: bump version to 0.15.5
    Changelog:
    https://git.lttng.org/?p=userspace-rcu.git;a=blob;f=ChangeLog;h=4833f6a67efb4cf0c4e56c9e4153b3ded6d89d34;hb=refs/heads/stable-0.15
    
    Added MIT license and updated other license files and hashes due to
    upstream commits:
    https://git.lttng.org/?p=userspace-rcu.git;a=commitdiff;h=29b2b11ef53eee2848dd06fba8c49003e17c539f
    https://git.lttng.org/?p=userspace-rcu.git;a=commit;h=d001c88633f6a58e94eb73f4814976d13f61bd77
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/liburcu/liburcu.mk
+++ package/liburcu/liburcu.mk
@@ -4,11 +4,15 @@
 #
 ################################################################################
 
-LIBURCU_VERSION = 0.14.0
+LIBURCU_VERSION = 0.15.5
 LIBURCU_SITE = http://lttng.org/files/urcu
 LIBURCU_SOURCE = userspace-rcu-$(LIBURCU_VERSION).tar.bz2
 LIBURCU_LICENSE = LGPL-2.1+ (library), MIT-like (few source files listed in LICENSE), GPL-2.0+ (test), GPL-3.0 (few *.m4 files)
-LIBURCU_LICENSE_FILES = lgpl-2.1.txt lgpl-relicensing.txt gpl-2.0.txt LICENSE
+LIBURCU_LICENSE_FILES = \
+	LICENSES/GPL-2.0-or-later.txt \
+	LICENSES/LGPL-2.1-or-later.txt \
+	LICENSES/MIT.txt \
+	LICENSE.md
 
 LIBURCU_INSTALL_STAGING = YES
 
