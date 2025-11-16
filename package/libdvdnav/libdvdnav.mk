a7a56546e299312fb1c47a7526171750abda9dc0
Bernd Kuhls <bernd@kuhls.net>
2025-11-16 09:56:11 +0100

    package/libdvdnav: bump version to 7.0.0
    Changelog:
    https://code.videolan.org/videolan/libdvdnav/-/blob/7.0.0/ChangeLog
    
    Updated _SITE, switched tarball to xz and build system to meson.
    Removed unneeded _CONF_ENV.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    [Julien: fix check-package error in hash file]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libdvdnav/libdvdnav.mk
+++ package/libdvdnav/libdvdnav.mk
@@ -4,13 +4,12 @@
 #
 ################################################################################
 
-LIBDVDNAV_VERSION = 6.1.1
-LIBDVDNAV_SOURCE = libdvdnav-$(LIBDVDNAV_VERSION).tar.bz2
-LIBDVDNAV_SITE = http://www.videolan.org/pub/videolan/libdvdnav/$(LIBDVDNAV_VERSION)
+LIBDVDNAV_VERSION = 7.0.0
+LIBDVDNAV_SOURCE = libdvdnav-$(LIBDVDNAV_VERSION).tar.xz
+LIBDVDNAV_SITE = https://download.videolan.org/pub/videolan/libdvdnav/$(LIBDVDNAV_VERSION)
 LIBDVDNAV_INSTALL_STAGING = YES
-LIBDVDNAV_CONF_ENV = CFLAGS="$(TARGET_CFLAGS) -std=gnu99"
 LIBDVDNAV_DEPENDENCIES = libdvdread host-pkgconf
 LIBDVDNAV_LICENSE = GPL-2.0+
 LIBDVDNAV_LICENSE_FILES = COPYING
 
-$(eval $(autotools-package))
+$(eval $(meson-package))
