cd3a2f0ff35346ad5fd6a9867113260addccf127
Bernd Kuhls <bernd@kuhls.net>
2025-11-16 09:56:09 +0100

    package/libdvdcss: bump version to 1.5.0
    Release notes:
    https://code.videolan.org/videolan/libdvdcss/-/blob/1.5.0/NEWS
    
    Added patch to fix build errors.
    
    Updated _SITE, switched tarball to xz and build system to meson.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    [Julien: fix check-package error in hash file]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libdvdcss/libdvdcss.mk
+++ package/libdvdcss/libdvdcss.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-LIBDVDCSS_VERSION = 1.4.3
-LIBDVDCSS_SOURCE = libdvdcss-$(LIBDVDCSS_VERSION).tar.bz2
-LIBDVDCSS_SITE = http://www.videolan.org/pub/videolan/libdvdcss/$(LIBDVDCSS_VERSION)
+LIBDVDCSS_VERSION = 1.5.0
+LIBDVDCSS_SOURCE = libdvdcss-$(LIBDVDCSS_VERSION).tar.xz
+LIBDVDCSS_SITE = https://download.videolan.org/pub/videolan/libdvdcss/$(LIBDVDCSS_VERSION)
 LIBDVDCSS_INSTALL_STAGING = YES
 LIBDVDCSS_LICENSE = GPL-2.0+
 LIBDVDCSS_LICENSE_FILES = COPYING
 
-$(eval $(autotools-package))
+$(eval $(meson-package))
