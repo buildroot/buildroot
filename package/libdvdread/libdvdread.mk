2496f5fde286f57ccb9f930fca2e479e8f850cdb
Bernd Kuhls <bernd@kuhls.net>
2025-11-16 09:56:10 +0100

    package/libdvdread: bump version to 7.0.1
    Release notes:
    https://code.videolan.org/videolan/libdvdread/blob/7.0.1/NEWS
    
    Updated _SITE, switched tarball to xz and build system to meson.
    Removed unneeded _CONF_ENV.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libdvdread/libdvdread.mk
+++ package/libdvdread/libdvdread.mk
@@ -4,14 +4,13 @@
 #
 ################################################################################
 
-LIBDVDREAD_VERSION = 6.1.3
-LIBDVDREAD_SOURCE = libdvdread-$(LIBDVDREAD_VERSION).tar.bz2
-LIBDVDREAD_SITE = http://www.videolan.org/pub/videolan/libdvdread/$(LIBDVDREAD_VERSION)
+LIBDVDREAD_VERSION = 7.0.1
+LIBDVDREAD_SOURCE = libdvdread-$(LIBDVDREAD_VERSION).tar.xz
+LIBDVDREAD_SITE = https://download.videolan.org/pub/videolan/libdvdread/$(LIBDVDREAD_VERSION)
 LIBDVDREAD_INSTALL_STAGING = YES
 LIBDVDREAD_LICENSE = GPL-2.0+
 LIBDVDREAD_LICENSE_FILES = COPYING
-LIBDVDREAD_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -std=gnu99"
-LIBDVDREAD_CONF_OPTS = --with-libdvdcss
+LIBDVDREAD_CONF_OPTS = -Dlibdvdcss=enabled
 LIBDVDREAD_DEPENDENCIES = libdvdcss host-pkgconf
 
-$(eval $(autotools-package))
+$(eval $(meson-package))
