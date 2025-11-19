eb2990afddfcf63197765628dfbf89d7128aa0fb
Thomas Perale <thomas.perale@mind.be>
2025-11-19 22:34:17 +0100

    support/scripts/cve.py: don't call download_nvd
    This patch move the 'download_nvd' call to the 'pkg-stats' script
    instead of automatically calling 'read_nvd_dir'.
    
    Since the cve.py file can be used as a library it's up to the caller to
    decide whether or not to update the NVD database.
    
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 867017e736ac53857685d424f043d16c97ffebfe)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- support/scripts/cve.py
+++ support/scripts/cve.py
@@ -144,7 +144,9 @@ class CVE:
         self.nvd_cve = nvd_cve
 
     @staticmethod
-    def download_nvd(nvd_git_dir):
+    def download_nvd(nvd_dir):
+        nvd_git_dir = os.path.join(nvd_dir, "git")
+
         if os.path.exists(nvd_git_dir):
             subprocess.check_call(
                 ["git", "pull"],
@@ -177,7 +179,7 @@ class CVE:
         nvd_dir, a fresh copy will be downloaded, and kept in .json.gz
         """
         nvd_git_dir = os.path.join(nvd_dir, "git")
-        CVE.download_nvd(nvd_git_dir)
+
         for year in range(NVD_START_YEAR, datetime.datetime.now().year + 1):
             for dirpath, _, filenames in os.walk(os.path.join(nvd_git_dir, f"CVE-{year}")):
                 for filename in filenames:
