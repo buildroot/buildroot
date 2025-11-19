372e3779106e3b7437766208b2a3286b0363bf94
Peter Korsgaard <peter@korsgaard.com>
2025-11-19 19:39:02 +0100

    package/python-django: security bump to version 5.1.14
    Fixed the following security issues:
    
    CVE-2025-64458: Potential denial-of-service vulnerability in
    HttpResponseRedirect and HttpResponsePermanentRedirect on Windows
    
    CVE-2025-64459: Potential SQL injection via _connector keyword argument
    
    https://docs.djangoproject.com/en/5.1/releases/5.1.14/
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 779827f765ebc721d8edbeb6579b7692129942ab)
    [thomas: bump to 5.1.14 instead]
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 5.1.13
+PYTHON_DJANGO_VERSION = 5.1.14
 PYTHON_DJANGO_SOURCE = django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/bb/57/ad9905d03a2ee39064ee7ba69f8e2790db4a7ffaef9c54f95e7a8f2cb0a1
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/6e/f9/284077f0e61f633e5279518a9ac5924ade4bef12bc841174ca48c57f2438
 PYTHON_DJANGO_LICENSE = BSD-3-Clause, MIT (jquery, utils/archive.py), BSD-2-Clause (inlines.js)
 PYTHON_DJANGO_LICENSE_FILES = LICENSE \
 	django/contrib/gis/measure.py \
