9a315994a635c1c88a71e83e70a8baaca1ff4325
Bernd Kuhls <bernd@kuhls.net>
2025-11-22 22:12:23 +0100

    package/libmbus: bump version to 0.9.0
    Release notes:
    https://github.com/rscada/libmbus/blob/0.9.0/release_notes.txt
    
    Downloading the source tarball is broken since at least Oct 30th, 2024
    as reported by the buildroot autobuilders:
    https://autobuild.buildroot.net/?reason=libmbus-0.8.0&step=50&start=50
    
    Switching to the github helper provides the correct source code but the
    tarball downloaded has a different hash. A quick search did not provide
    alternative download locations for the original tarball
    libmbus-0.8.0.tar.gz.
    
    To solve the broken build we do not only switch _SITE to github but also
    bump the package to 0.9.0 and add autoreconf which needs two directories
    being created in _POST_PATCH_HOOKS. One missing directory was already
    reported upstream: https://github.com/rscada/libmbus/issues/179
    
    Updated license hash due to upstream commit:
    https://github.com/rscada/libmbus/commit/ab8919136d3cabb943b1cbdd4d6e2f7eea4b8f3f
    
    Fixes:
    https://autobuild.buildroot.net/results/91a/91a82dc73bf87cf1969cd6a2ddea1809ab9a1613/
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    [Julien: change release notes url to use tag]
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit e42b3de3b1ccd41ef1907a791a7d3792d201c0be)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/libmbus/libmbus.mk
+++ package/libmbus/libmbus.mk
@@ -4,10 +4,18 @@
 #
 ################################################################################
 
-LIBMBUS_VERSION = 0.8.0
-LIBMBUS_SITE = http://www.rscada.se/public-dist
+LIBMBUS_VERSION = 0.9.0
+LIBMBUS_SITE = $(call github,rscada,libmbus,$(LIBMBUS_VERSION))
 LIBMBUS_INSTALL_STAGING = YES
 LIBMBUS_LICENSE = BSD-3-Clause
 LIBMBUS_LICENSE_FILES = COPYING
+# github tarball does not include configure
+LIBMBUS_AUTORECONF = YES
+# Autoreconf is missing some directories
+define LIBMBUS_CREATE_DIRS
+	mkdir -p $(@D)/libltdl
+	mkdir -p $(@D)/m4
+endef
+LIBMBUS_POST_PATCH_HOOKS += LIBMBUS_CREATE_DIRS
 
 $(eval $(autotools-package))
