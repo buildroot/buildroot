9639e6c246a86f98b6e6509bc2a0579b2d87622d
Marcus Hoffmann <buildroot@bubu1.eu>
2025-11-24 17:57:17 +0100

    package/python-starlette: security bump to 0.50.0
    Starlette 0.49.1 fixes the following security issue:
    https://github.com/Kludex/starlette/security/advisories/GHSA-7f5h-v6xp-fcq8
    
    Other changes:
    * https://github.com/Kludex/starlette/releases/tag/0.49.0
    * https://github.com/Kludex/starlette/releases/tag/0.49.1 (the security fix)
    * https://github.com/Kludex/starlette/releases/tag/0.49.2
    * https://github.com/Kludex/starlette/releases/tag/0.49.3
    * https://github.com/Kludex/starlette/releases/tag/0.50.0
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 35d8a3a0ca6bbaef1c9e2ee9f7f16f1a5b056b17)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/python-starlette/python-starlette.mk
+++ package/python-starlette/python-starlette.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_STARLETTE_VERSION = 0.48.0
+PYTHON_STARLETTE_VERSION = 0.50.0
 PYTHON_STARLETTE_SOURCE = starlette-$(PYTHON_STARLETTE_VERSION).tar.gz
-PYTHON_STARLETTE_SITE = https://files.pythonhosted.org/packages/a7/a5/d6f429d43394057b67a6b5bbe6eae2f77a6bf7459d961fdb224bf206eee6
+PYTHON_STARLETTE_SITE = https://files.pythonhosted.org/packages/ba/b8/73a0e6a6e079a9d9cfa64113d771e421640b6f679a52eeb9b32f72d871a1
 PYTHON_STARLETTE_SETUP_TYPE = hatch
 PYTHON_STARLETTE_LICENSE = BSD-3-Clause
 PYTHON_STARLETTE_LICENSE_FILES = LICENSE.md
