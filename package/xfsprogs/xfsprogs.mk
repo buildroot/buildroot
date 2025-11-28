865b690517d72c6619496448961531a206961e66
Bernd Kuhls <bernd@kuhls.net>
2025-11-28 19:14:01 +0100

    package/xfsprogs: fix build with icu >= 76
    Buildroot commit dcee99507c6767230fd1faeeefe95f5662eb03a3 bumped icu
    from version 73-2 to 77-1 causing a build error during linking.
    
    Fixes:
    https://autobuild.buildroot.net/results/a30/a30bf79be2102f3a77e4a78a488fb3b6a5b63bbb/
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/xfsprogs/xfsprogs.mk
+++ package/xfsprogs/xfsprogs.mk
@@ -10,6 +10,15 @@ XFSPROGS_SOURCE = xfsprogs-$(XFSPROGS_VERSION).tar.xz
 XFSPROGS_LICENSE = GPL-2.0, GPL-2.0+, LGPL-2.1 (libhandle, few headers)
 XFSPROGS_LICENSE_FILES = LICENSES/GPL-2.0 LICENSES/LGPL-2.1
 
+# 0002-configure-additionally-get-icu-uc-from-pkg-config.patch
+XFSPROGS_AUTORECONF = YES
+
+# restore upstream install-sh script overwritten by autoreconf
+define XFSPROGS_FIX_INSTALL_SH
+	mv $(@D)/install-sh~ $(@D)/install-sh
+endef
+XFSPROGS_POST_CONFIGURE_HOOKS = XFSPROGS_FIX_INSTALL_SH
+
 XFSPROGS_DEPENDENCIES = inih liburcu util-linux
 
 XFSPROGS_CONF_ENV = ac_cv_header_aio_h=yes ac_cv_lib_rt_lio_listio=yes PLATFORM="linux"
