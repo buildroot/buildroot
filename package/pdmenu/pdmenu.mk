baaad289ab94e5317df900cc4a369650735e9f06
Bernd Kuhls <bernd@kuhls.net>
2025-11-29 08:40:11 +0100

    package/pdmenu: fix build with gcc-15.x
    Fixes:
    https://autobuild.buildroot.net/results/554/5545162ca0206cacfca1617280f074237e72aec1/
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/pdmenu/pdmenu.mk
+++ package/pdmenu/pdmenu.mk
@@ -11,4 +11,11 @@ PDMENU_LICENSE_FILES = doc/COPYING
 PDMENU_DEPENDENCIES = slang $(TARGET_NLS_DEPENDENCIES)
 PDMENU_INSTALL_TARGET_OPTS = INSTALL_PREFIX=$(TARGET_DIR) install
 
+# gcc-15 defaults to -std=gnu23 which introduces build failures.
+# We force "-std=gnu17" for gcc version supporting it. Earlier gcc
+# versions will work, since they are using the older standard.
+ifeq ($(BR2_TOOLCHAIN_GCC_AT_LEAST_8),y)
+PDMENU_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -std=gnu17"
+endif
+
 $(eval $(autotools-package))
