1d10e677b8fe8cb757dccba8c42b746f52c1a5a4
Bernd Kuhls <bernd@kuhls.net>
2025-12-01 21:08:09 +0100

    package/riemann-c-client: bump version to 2.2.2
    Moved _SITE to https://git.madhouse-project.org/algernon/riemann-c-client
    according to https://github.com/collectd/collectd/issues/4021#issuecomment-1218597353
    (collectd is the only package in buildroot using riemann-c-client)
    
    Release notes:
    https://git.madhouse-project.org/algernon/riemann-c-client/src/tag/riemann-c-client-2.2.2/NEWS.md
    
    Updated licenses due to upstream commit
    https://git.madhouse-project.org/algernon/riemann-c-client/commit/9bada2fabff9124245426baf7beb18e1e9480b17
    
    Added optional dependencies to OpenSSL and wolfSSL.
    
    Fixes:
    https://autobuild.buildroot.net/results/29d/29d03e9ba24ae9d17ff7ad57e4906c30413d8a6e/
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/riemann-c-client/riemann-c-client.mk
+++ package/riemann-c-client/riemann-c-client.mk
@@ -4,18 +4,33 @@
 #
 ################################################################################
 
-RIEMANN_C_CLIENT_VERSION = 1.10.5
-RIEMANN_C_CLIENT_SITE = \
-	$(call github,algernon,riemann-c-client,riemann-c-client-$(RIEMANN_C_CLIENT_VERSION))
-RIEMANN_C_CLIENT_LICENSE = LGPL-3.0+
-RIEMANN_C_CLIENT_LICENSE_FILES = LICENSE
+RIEMANN_C_CLIENT_VERSION = 2.2.2
+RIEMANN_C_CLIENT_SITE = https://git.madhouse-project.org/algernon/riemann-c-client/archive
+RIEMANN_C_CLIENT_LICENSE = \
+	Apache-2.0 (lib/riemann/proto/), \
+	EUPL-1.2
+RIEMANN_C_CLIENT_LICENSE_FILES = \
+	LICENSES/Apache-2.0.txt \
+	LICENSES/EUPL-1.2.txt
 RIEMANN_C_CLIENT_INSTALL_STAGING = YES
 RIEMANN_C_CLIENT_MAKE = $(MAKE1)
 # From git
 RIEMANN_C_CLIENT_AUTORECONF = YES
 RIEMANN_C_CLIENT_DEPENDENCIES = \
 	host-pkgconf protobuf-c \
-	$(if $(BR2_PACKAGE_GNUTLS),gnutls) \
 	$(if $(BR2_PACKAGE_JSON_C),json-c)
+
+ifeq ($(BR2_PACKAGE_GNUTLS),y)
+RIEMANN_C_CLIENT_CONF_OPTS += --with-tls=gnutls
+RIEMANN_C_CLIENT_DEPENDENCIES += gnutls
+else ifeq ($(BR2_PACKAGE_OPENSSL),y)
+RIEMANN_C_CLIENT_CONF_OPTS += --with-tls=openssl
+RIEMANN_C_CLIENT_DEPENDENCIES += openssl
+else ifeq ($(BR2_PACKAGE_WOLFSSL),y)
+RIEMANN_C_CLIENT_CONF_OPTS += --with-tls=wolfssl
+RIEMANN_C_CLIENT_DEPENDENCIES += wolfssl
+else
+RIEMANN_C_CLIENT_CONF_OPTS += --without-tls
+endif
 
 $(eval $(autotools-package))
