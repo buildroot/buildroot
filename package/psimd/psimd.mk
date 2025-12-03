d5cccad9a3931f4ffe1be3f6f7d2feeb8eec519d
Bernd Kuhls <bernd@kuhls.net>
2025-12-03 22:47:06 +0100

    package/psimd: fix cmake 4 compatibility
    The current upstream repo shows this note:
    "This repository was archived by the owner on May 27, 2024. It is now
     read-only."
    
    Switched to fork whose only difference is a commit fixing build errors
    with cmake 4:
    https://github.com/snest/psimd/commit/4674816f56757dbd99b297cb5647f2520df08db1
    
    Since this commit changes _SITE, the package homepage url is also update
    to the same location.
    
    Fixes:
    https://autobuild.buildroot.net/results/130/13084ca4df5ae91d72f46ef51873676b05398ec9/
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    [Julien: change homepage url in Config.in and add commit log comment]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/psimd/psimd.mk
+++ package/psimd/psimd.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PSIMD_VERSION = 072586a71b55b7f8c584153d223e95687148a900
-PSIMD_SITE = $(call github,Maratyszcza,psimd,$(PSIMD_VERSION))
+PSIMD_VERSION = 4674816f56757dbd99b297cb5647f2520df08db1
+PSIMD_SITE = $(call github,snest,psimd,$(PSIMD_VERSION))
 PSIMD_LICENSE = MIT
 PSIMD_LICENSE_FILES = LICENSE
 PSIMD_INSTALL_STAGING = YES
