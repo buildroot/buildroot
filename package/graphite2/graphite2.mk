ca21d87300ea54217b262e06f295b816fb38eecb
Bernd Kuhls <bernd@kuhls.net>
2025-12-03 22:57:13 +0100

    package/graphite2: bump version to git 1.3.14-121-g142e1bda3
    Last release dates back to 2020 with > 120 commits since then, including
    fixes for cmake 4 compatibility.
    
    Instead of backporting several patches we bump the package to the latest
    commit which allows to remove patch 0001.
    
    Updated license hash due to upstream commit
    https://github.com/silnrsi/graphite/commit/f9dad5a35edb0c3f1031f5627623fb9e7f9bc24c
    
    For list of changes, see:
    https://github.com/silnrsi/graphite/commits/142e1bda3439d2bd376bcac9c2b247c9532bacde
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    [Julien:
     - add link to change list
     - change _VERSION to use "git describe --abbrev=40" format
    ]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/graphite2/graphite2.mk
+++ package/graphite2/graphite2.mk
@@ -4,10 +4,8 @@
 #
 ################################################################################
 
-GRAPHITE2_VERSION = 1.3.14
-GRAPHITE2_SOURCE = graphite2-$(GRAPHITE2_VERSION).tgz
-GRAPHITE2_SITE = \
-	https://github.com/silnrsi/graphite/releases/download/$(GRAPHITE2_VERSION)
+GRAPHITE2_VERSION = 1.3.14-121-g142e1bda3439d2bd376bcac9c2b247c9532bacde
+GRAPHITE2_SITE = $(call github,silnrsi,graphite,$(GRAPHITE2_VERSION))
 GRAPHITE2_INSTALL_STAGING = YES
 GRAPHITE2_LICENSE = LGPL-2.1+
 GRAPHITE2_LICENSE_FILES = LICENSE
