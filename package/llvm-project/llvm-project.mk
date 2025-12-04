251063eca0a74a9bb57217748e13f7bab7badecb
Bernd Kuhls <bernd@kuhls.net>
2025-12-04 17:34:09 +0100

    package/llvm-project: bump to version 21.1.7
    Release notes:
    https://discourse.llvm.org/t/llvm-21-1-7-released/89029
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/llvm-project/llvm-project.mk
+++ package/llvm-project/llvm-project.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LLVM_PROJECT_VERSION_MAJOR = 21
-LLVM_PROJECT_VERSION = $(LLVM_PROJECT_VERSION_MAJOR).1.6
+LLVM_PROJECT_VERSION = $(LLVM_PROJECT_VERSION_MAJOR).1.7
 LLVM_PROJECT_SITE = https://github.com/llvm/llvm-project/releases/download/llvmorg-$(LLVM_PROJECT_VERSION)
 
 include $(sort $(wildcard package/llvm-project/*/*.mk))
