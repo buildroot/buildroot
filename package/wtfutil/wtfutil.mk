dd295e5ba3fd68373da4e83837cd65266c978928
Bernd Kuhls <bernd@kuhls.net>
2025-12-04 21:54:12 +0100

    package/wtfutil: bump version to 0.47.1
    Release notes: https://github.com/wtfutil/wtf/releases
    
    Needs bump of go to >= 1.25.4.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/wtfutil/wtfutil.mk
+++ package/wtfutil/wtfutil.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WTFUTIL_VERSION = 0.41.0
+WTFUTIL_VERSION = 0.47.1
 WTFUTIL_SITE = $(call github,wtfutil,wtf,v$(WTFUTIL_VERSION))
 WTFUTIL_LICENSE = MPL-2.0
 WTFUTIL_LICENSE_FILES = LICENSE.md
