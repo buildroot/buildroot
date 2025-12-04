2774502344f35b587ea8f1a2f43ec2398c3ebdad
Bernd Kuhls <bernd@kuhls.net>
2025-12-04 23:34:43 +0100

    package/libnpupnp: bump version to 6.2.3
    Release notes:
    https://www.lesbonscomptes.com/upmpdcli/pages/releases.html
    
    Switched _SITE to https.
    
    Switched build system to meson after autotools were removed in version
    6.1.2.
    
    This bump fixes a gerbera build error introduced to buildroot by the
    gerbera bump to 2.6.1 with buildroot commit
    f8bf78911576672443a8d11eaf99bed4d383acef.
    
    Fixes:
    https://autobuild.buildroot.net/results/b6b/b6bd96cb9dbcaff9351ef156edaec7d1b5290b9e/
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libnpupnp/libnpupnp.mk
+++ package/libnpupnp/libnpupnp.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-LIBNPUPNP_VERSION = 4.2.2
-LIBNPUPNP_SITE = http://www.lesbonscomptes.com/upmpdcli/downloads
+LIBNPUPNP_VERSION = 6.2.3
+LIBNPUPNP_SITE = https://www.lesbonscomptes.com/upmpdcli/downloads
 LIBNPUPNP_LICENSE = BSD-3-Clause
 LIBNPUPNP_LICENSE_FILES = COPYING
 LIBNPUPNP_INSTALL_STAGING = YES
 LIBNPUPNP_DEPENDENCIES = expat libcurl libmicrohttpd
 
-$(eval $(autotools-package))
+$(eval $(meson-package))
