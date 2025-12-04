9c9ec285796dd5bd7db39f11b74392534b028355
Bernd Kuhls <bernd@kuhls.net>
2025-12-04 23:34:44 +0100

    package/libupnpp: bump version to 1.0.3
    Release notes:
    https://www.lesbonscomptes.com/upmpdcli/pages/releases.html
    
    Switched _SITE to https.
    
    Switched build system to meson after autotools were removed in version
    0.26.4.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libupnpp/libupnpp.mk
+++ package/libupnpp/libupnpp.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-LIBUPNPP_VERSION = 0.21.0
-LIBUPNPP_SITE = http://www.lesbonscomptes.com/upmpdcli/downloads
+LIBUPNPP_VERSION = 1.0.3
+LIBUPNPP_SITE = https://www.lesbonscomptes.com/upmpdcli/downloads
 LIBUPNPP_LICENSE = LGPL-2.1+
 LIBUPNPP_LICENSE_FILES = COPYING
 LIBUPNPP_INSTALL_STAGING = YES
 LIBUPNPP_DEPENDENCIES = host-pkgconf expat libcurl libnpupnp
 
-$(eval $(autotools-package))
+$(eval $(meson-package))
