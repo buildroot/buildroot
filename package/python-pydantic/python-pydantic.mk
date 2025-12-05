21ba521c5e9f83df9cdc46d97f9a905b1afbcc03
Marcus Hoffmann <buildroot@bubu1.eu>
2025-12-05 13:21:21 +0100

    package/python-pydantic: bump to 2.12.5
    Release Notes: https://github.com/pydantic/pydantic-core/releases/tag/v2.41.5
    
    No bump of pydantic-core as the version requirement didn't change.
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-pydantic/python-pydantic.mk
+++ package/python-pydantic/python-pydantic.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYDANTIC_VERSION = 2.12.4
+PYTHON_PYDANTIC_VERSION = 2.12.5
 PYTHON_PYDANTIC_SOURCE = pydantic-$(PYTHON_PYDANTIC_VERSION).tar.gz
-PYTHON_PYDANTIC_SITE = https://files.pythonhosted.org/packages/96/ad/a17bc283d7d81837c061c49e3eaa27a45991759a1b7eae1031921c6bd924
+PYTHON_PYDANTIC_SITE = https://files.pythonhosted.org/packages/69/44/36f1a6e523abc58ae5f928898e4aca2e0ea509b5aa6f6f392a5d882be928
 PYTHON_PYDANTIC_SETUP_TYPE = hatch
 PYTHON_PYDANTIC_LICENSE = MIT
 PYTHON_PYDANTIC_LICENSE_FILES = LICENSE
