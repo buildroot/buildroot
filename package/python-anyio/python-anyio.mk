828d2288cd4b69cd23ad3c7e1699edbd51b1dce1
Marcus Hoffmann <buildroot@bubu1.eu>
2025-12-05 13:27:12 +0100

    package/python-anyio: bump to 4.12.0
    Drop dependency on python-sniffio, that's no longer required.
    
    Release Notes https://github.com/agronholm/anyio/releases/tag/4.12.0
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-anyio/python-anyio.mk
+++ package/python-anyio/python-anyio.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ANYIO_VERSION = 4.11.0
+PYTHON_ANYIO_VERSION = 4.12.0
 PYTHON_ANYIO_SOURCE = anyio-$(PYTHON_ANYIO_VERSION).tar.gz
-PYTHON_ANYIO_SITE = https://files.pythonhosted.org/packages/c6/78/7d432127c41b50bccba979505f272c16cbcadcc33645d5fa3a738110ae75
+PYTHON_ANYIO_SITE = https://files.pythonhosted.org/packages/16/ce/8a777047513153587e5434fd752e89334ac33e379aa3497db860eeb60377
 PYTHON_ANYIO_SETUP_TYPE = setuptools
 PYTHON_ANYIO_LICENSE = MIT
 PYTHON_ANYIO_LICENSE_FILES = LICENSE
