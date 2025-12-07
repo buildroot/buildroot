442622055d02040a467d19e5573a478a063f0526
Bernd Kuhls <bernd@kuhls.net>
2025-12-07 13:14:06 +0100

    package/x11r7/xapp_xkbutils: bump version to 1.0.6
    Release notes:
    https://lists.x.org/archives/xorg-announce/2024-February/003448.html
    
    Updated license hash due to upstream commit:
    https://gitlab.freedesktop.org/xorg/app/xkbutils/-/commit/6dbcbb55951efb25af2e38034bdd86fa095aa9e5
    
    Fixes:
    https://autobuild.buildroot.net/results/eb7/eb7d32e65eece403538032a9006ea2f573ea10f8/
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xapp_xkbutils/xapp_xkbutils.mk
+++ package/x11r7/xapp_xkbutils/xapp_xkbutils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XKBUTILS_VERSION = 1.0.5
+XAPP_XKBUTILS_VERSION = 1.0.6
 XAPP_XKBUTILS_SOURCE = xkbutils-$(XAPP_XKBUTILS_VERSION).tar.xz
 XAPP_XKBUTILS_SITE = https://xorg.freedesktop.org/releases/individual/app
 XAPP_XKBUTILS_LICENSE = MIT
