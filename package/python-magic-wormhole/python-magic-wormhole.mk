2d69dfb41e3290c8220a8c9891fb480e5b13ccf8
Bernd Kuhls <bernd@kuhls.net>
2025-12-07 15:42:22 +0100

    package/python-magic-wormhole: bump version to 0.21.1
    For change log since 0.19.2, see:
    https://github.com/magic-wormhole/magic-wormhole/blob/0.21.1/NEWS.md
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    [Julien: add link to change log]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-magic-wormhole/python-magic-wormhole.mk
+++ package/python-magic-wormhole/python-magic-wormhole.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_MAGIC_WORMHOLE_VERSION = 0.19.2
+PYTHON_MAGIC_WORMHOLE_VERSION = 0.21.1
 PYTHON_MAGIC_WORMHOLE_SOURCE = magic_wormhole-$(PYTHON_MAGIC_WORMHOLE_VERSION).tar.gz
-PYTHON_MAGIC_WORMHOLE_SITE = https://files.pythonhosted.org/packages/e0/5b/4aff155b8e8ead4bc7c6aa3d1c19dc75aac2315e0c9b12f4e2f246b40141
+PYTHON_MAGIC_WORMHOLE_SITE = https://files.pythonhosted.org/packages/d1/c6/0ed9e0dca81eb5d0bcfc72690613bf26a13b95a9edb95fc35cebbfff8fba
 PYTHON_MAGIC_WORMHOLE_SETUP_TYPE = setuptools
 PYTHON_MAGIC_WORMHOLE_LICENSE = MIT
 PYTHON_MAGIC_WORMHOLE_LICENSE_FILES = LICENSE
+PYTHON_MAGIC_WORMHOLE_DEPENDENCIES = host-python-versioneer
 
 $(eval $(python-package))
