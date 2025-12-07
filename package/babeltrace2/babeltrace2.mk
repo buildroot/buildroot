cea219e5180e7117a6002c4a784995c9dad48176
Dario Binacchi <dario.binacchi@amarulasolutions.com>
2025-12-07 15:58:41 +0100

    package/babeltrace2: bump to version 2.1.2
    The removed patch was merged in version v2.1.0 [1]. Its removal made
    running autoreconf no longer necessary. Moreover, commit [2] also
    changed the names of the license files and introduced new ones.
    
    Release notes since 2.0.7:
    https://github.com/efficios/babeltrace/releases
    
    [1] https://github.com/efficios/babeltrace/commit/28ba44ad9ca14153e96c94a9100423ea224c1af3
    [2] https://github.com/efficios/babeltrace/commit/0235b0db7de5bcacdb3650c92461f2ce5eb2143d
    Signed-off-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/babeltrace2/babeltrace2.mk
+++ package/babeltrace2/babeltrace2.mk
@@ -5,12 +5,27 @@
 ################################################################################
 
 BABELTRACE2_SITE = https://www.efficios.com/files/babeltrace
-BABELTRACE2_VERSION = 2.0.6
+BABELTRACE2_VERSION = 2.1.2
 BABELTRACE2_SOURCE = babeltrace2-$(BABELTRACE2_VERSION).tar.bz2
-BABELTRACE2_LICENSE = MIT, LGPL-2.1 (src/common/list.h), GPL-2.0 (test code)
-BABELTRACE2_LICENSE_FILES = mit-license.txt lgpl-2.1.txt gpl-2.0.txt LICENSE
-# We're patching configure.ac
-BABELTRACE2_AUTORECONF = YES
+BABELTRACE2_LICENSE = \
+	MIT, \
+	BSD-2-Clause (C and Python TAP test libraries), \
+	BSD-4-Clause (CRC32 code), \
+	CC-BY-SA-4.0 (doc), \
+	GPL-2.0 (test code), \
+	GPL-3.0+ (BASH TAP library), \
+	LGPL-2.1 (src/common/list.h), \
+	PSF-2.0 (typing Python module)
+BABELTRACE2_LICENSE_FILES = \
+	LICENSE \
+	LICENSES/MIT.txt \
+	LICENSES/BSD-2-Clause.txt \
+	LICENSES/BSD-4-Clause.txt \
+	LICENSES/CC-BY-SA-4.0.txt \
+	LICENSES/GPL-2.0-only.txt \
+	LICENSES/GPL-3.0-or-later.txt \
+	LICENSES/LGPL-2.1-only.txt \
+	LICENSES/PSF-2.0.txt
 BABELTRACE2_CONF_OPTS = --disable-man-pages
 BABELTRACE2_DEPENDENCIES = libglib2 host-pkgconf
 # The host-elfutils dependency is optional, but since we don't have
