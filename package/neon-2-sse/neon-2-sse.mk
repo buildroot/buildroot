2cd7a5059bbc51a6386fd287e06b8941fa8bb2ed
Bernd Kuhls <bernd@kuhls.net>
2025-12-07 15:42:14 +0100

    package/neon-2-sse: bump version
    Changelog:
    https://github.com/intel/ARM_NEON_2_x86_SSE/commits/662a85912e8f86ec808f9b15ce77f8715ba53316/
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/neon-2-sse/neon-2-sse.mk
+++ package/neon-2-sse/neon-2-sse.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NEON_2_SSE_VERSION = 097a5ecacd527d5b5c3006e360fb9cb1c1c48a1f
+NEON_2_SSE_VERSION = 662a85912e8f86ec808f9b15ce77f8715ba53316
 NEON_2_SSE_SITE = $(call github,intel,ARM_NEON_2_x86_SSE,$(NEON_2_SSE_VERSION))
 NEON_2_SSE_LICENSE = BSD-2-Clause
 NEON_2_SSE_LICENSE_FILES = LICENSE
