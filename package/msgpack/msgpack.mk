5bfed3cd886eac1065ac91142a7c60892e6baf2b
Bernd Kuhls <bernd@kuhls.net>
2025-12-07 15:42:13 +0100

    package/msgpack: bump version to 7.0.0
    Changelog:
    https://github.com/msgpack/msgpack-c/blob/cpp-7.0.0/CHANGELOG.md
    
    Added optional dependency to boost.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/msgpack/msgpack.mk
+++ package/msgpack/msgpack.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-MSGPACK_VERSION = 3.3.0
+MSGPACK_VERSION = 7.0.0
 MSGPACK_SITE = https://github.com/msgpack/msgpack-c/releases/download/cpp-$(MSGPACK_VERSION)
+MSGPACK_SOURCE = msgpack-cxx-$(MSGPACK_VERSION).tar.gz
 MSGPACK_LICENSE = BSL-1.0
 MSGPACK_LICENSE_FILES = COPYING LICENSE_1_0.txt
 MSGPACK_INSTALL_STAGING = YES
@@ -13,6 +14,13 @@ MSGPACK_CONF_OPTS = -DMSGPACK_BUILD_EXAMPLES=OFF -DMSGPACK_BUILD_TESTS=OFF
 
 ifeq ($(BR2_STATIC_LIBS),y)
 MSGPACK_CONF_OPTS += -DMSGPACK_ENABLE_SHARED=OFF
+endif
+
+ifeq ($(BR2_PACKAGE_BOOST),y)
+MSGPACK_CONF_OPTS += -DMSGPACK_USE_BOOST=ON
+MSGPACK_DEPENDENCIES += boost
+else
+MSGPACK_CONF_OPTS += -DMSGPACK_USE_BOOST=OFF
 endif
 
 $(eval $(cmake-package))
