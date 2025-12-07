972358973fef8c8e2d314d77eaf9a6b6229eb64a
Bernd Kuhls <bernd@kuhls.net>
2025-12-07 15:42:39 +0100

    package/x11r7/xdriver_xf86-video-nv: bump version to 2.1.23
    Release notes:
    https://lists.x.org/archives/xorg-announce/2024-March/003489.html
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xdriver_xf86-video-nv/xdriver_xf86-video-nv.mk
+++ package/x11r7/xdriver_xf86-video-nv/xdriver_xf86-video-nv.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_NV_VERSION = 2.1.22
+XDRIVER_XF86_VIDEO_NV_VERSION = 2.1.23
 XDRIVER_XF86_VIDEO_NV_SOURCE = xf86-video-nv-$(XDRIVER_XF86_VIDEO_NV_VERSION).tar.xz
 XDRIVER_XF86_VIDEO_NV_SITE = https://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_VIDEO_NV_LICENSE = MIT
