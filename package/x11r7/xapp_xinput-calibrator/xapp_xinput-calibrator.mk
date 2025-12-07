66f3eb88c103ec53f8a2d776c57e62201f43cbe8
Bernd Kuhls <bernd@kuhls.net>
2025-12-07 15:42:35 +0100

    package/x11r7/xapp_xinput-calibrator: bump version to 0.8.0
    Switched _SITE to freedesktop.org:
    https://github.com/tias/xinput_calibrator/commit/92c40faea5ce899754490738e5cb24469c566d16
    
    Use xz tarball instead of gz.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_xinput-calibrator/xapp_xinput-calibrator.mk
+++ package/x11r7/xapp_xinput-calibrator/xapp_xinput-calibrator.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_XINPUT_CALIBRATOR_VERSION = 0.7.5
-XAPP_XINPUT_CALIBRATOR_SOURCE = xinput_calibrator-$(XAPP_XINPUT_CALIBRATOR_VERSION).tar.gz
-XAPP_XINPUT_CALIBRATOR_SITE = https://github.com/downloads/tias/xinput_calibrator
+XAPP_XINPUT_CALIBRATOR_VERSION = 0.8.0
+XAPP_XINPUT_CALIBRATOR_SOURCE = xinput_calibrator-$(XAPP_XINPUT_CALIBRATOR_VERSION).tar.xz
+XAPP_XINPUT_CALIBRATOR_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XINPUT_CALIBRATOR_LICENSE = MIT
 XAPP_XINPUT_CALIBRATOR_LICENSE_FILES = COPYING
 XAPP_XINPUT_CALIBRATOR_DEPENDENCIES = xlib_libX11 xlib_libXi
