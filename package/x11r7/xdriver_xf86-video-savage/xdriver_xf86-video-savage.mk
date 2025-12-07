f183a97de64cf8f3549f8544f572537a590d58e6
Bernd Kuhls <bernd@kuhls.net>
2025-12-07 15:42:40 +0100

    package/x11r7/xdriver_xf86-video-savage: bump version to 2.4.1
    Release notes:
    https://lists.x.org/archives/xorg-announce/2024-March/003490.html
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xdriver_xf86-video-savage/xdriver_xf86-video-savage.mk
+++ package/x11r7/xdriver_xf86-video-savage/xdriver_xf86-video-savage.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_SAVAGE_VERSION = 2.4.0
+XDRIVER_XF86_VIDEO_SAVAGE_VERSION = 2.4.1
 XDRIVER_XF86_VIDEO_SAVAGE_SOURCE = xf86-video-savage-$(XDRIVER_XF86_VIDEO_SAVAGE_VERSION).tar.xz
 XDRIVER_XF86_VIDEO_SAVAGE_SITE = https://xorg.freedesktop.org/archive/individual/driver
 XDRIVER_XF86_VIDEO_SAVAGE_LICENSE = MIT
