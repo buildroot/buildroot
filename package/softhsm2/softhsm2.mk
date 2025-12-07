1d13662a5fe393530abeb5896fc52168e1b5730b
Bernd Kuhls <bernd@kuhls.net>
2025-12-07 12:54:05 +0100

    package/softhsm2: update SITE
    Original site is down, unchanged tarball can be downloaded from github.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit dd2b3607940123a3aa1ab870e55021f454d19fec)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/softhsm2/softhsm2.mk
+++ package/softhsm2/softhsm2.mk
@@ -6,7 +6,7 @@
 
 SOFTHSM2_VERSION = 2.6.1
 SOFTHSM2_SOURCE = softhsm-$(SOFTHSM2_VERSION).tar.gz
-SOFTHSM2_SITE = https://dist.opendnssec.org/source
+SOFTHSM2_SITE = https://github.com/opendnssec/opendnssec/releases/download/2.1.14
 SOFTHSM2_LICENSE = BSD-2-Clause
 SOFTHSM2_LICENSE_FILES = LICENSE
 SOFTHSM2_DEPENDENCIES = openssl
