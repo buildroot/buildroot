d98d9ba28ffa5b468fa243ae1a359aadef3ed0a0
Bernd Kuhls <bernd@kuhls.net>
2025-12-07 19:33:47 +0100

    package/kbd: fix zlib build, add optional dependencies
    Buildroot commit 930660890bf8b40d9bd55cd9e202e574b53d5d49 bumped kbd to
    version 2.9.0 which includes optional support for various compression
    libraries. This commit adds the corresponding configure options and a
    fix for build errors caused by zlib.
    
    The build error was happening when a zlib was compiled before kbd.
    The issue can be reproduced with the commands:
    
        cat >.config <<EOF
        BR2_aarch64=y
        BR2_TOOLCHAIN_EXTERNAL=y
        BR2_TOOLCHAIN_EXTERNAL_BOOTLIN=y
        BR2_PACKAGE_KBD=y
        BR2_PACKAGE_ZLIB=y
        EOF
        make olddefconfig
        make zlib
        make kbd
    
    The compilation fails with the erro:
    
        kbdfile-zlib.c: In function 'dlopen_note':
        elf-note.h:27:30: error: 'sym_gzopen' undeclared (first use in this function); did you mean 'sym_gzopen64'?
           27 | #define DLSYM_ARG(symbol__) &sym_##symbol__, STRINGIFY(symbol__),
              |                              ^~~~
    
    Fixes:
    https://autobuild.buildroot.net/results/8ff/8ff6c3d940b68069f748f12646f7516ec86172c1/
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    [Julien:
     - add commands to reproduce the issue
     - update patch "Upstream:" link to upstream commit
    ]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/kbd/kbd.mk
+++ package/kbd/kbd.mk
@@ -13,6 +13,35 @@ KBD_CONF_OPTS = \
 KBD_DEPENDENCIES = \
 	$(TARGET_NLS_DEPENDENCIES) \
 	host-pkgconf
+
+ifeq ($(BR2_PACKAGE_BZIP2),y)
+KBD_CONF_OPTS += --with-bzip2
+KBD_DEPENDENCIES += bzip2
+else
+KBD_CONF_OPTS += --without-bzip2
+endif
+
+ifeq ($(BR2_PACKAGE_XZ),y)
+KBD_CONF_OPTS += --with-lzma
+KBD_DEPENDENCIES += xz
+else
+KBD_CONF_OPTS += --without-lzma
+endif
+
+ifeq ($(BR2_PACKAGE_ZLIB),y)
+KBD_CONF_OPTS += --with-zlib
+KBD_DEPENDENCIES += zlib
+else
+KBD_CONF_OPTS += --without-zlib
+endif
+
+ifeq ($(BR2_PACKAGE_ZSTD),y)
+KBD_CONF_OPTS += --with-zstd
+KBD_DEPENDENCIES += zstd
+else
+KBD_CONF_OPTS += --without-zstd
+endif
+
 KBD_LICENSE = GPL-2.0+
 KBD_LICENSE_FILES = COPYING CREDITS
 
