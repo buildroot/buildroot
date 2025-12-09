1bd758209ee909b62ac448b570d8944b0057e518
Giulio Benetti <giulio.benetti@benettiengineering.com>
2025-12-09 10:41:35 +0100

    package/rtl8723ds: bump to version 2025-12-09
    With Kernel >= 6.18 introduced in [1] rtl8723ds fails at build
    time with error:
    
        core/rtw_security.c:2183:13: error: conflicting types for 'sha256_init'; have 'void(struct rtl_sha256_state *)'
    
    Fixes:
    https://autobuild.buildroot.org/results/52081e3a500dd2566af145e8c6d793df7f72d025/
    
    [1] https://gitlab.com/buildroot.org/buildroot/-/commit/a06d79862a60d3c8d5ddd6a9ab12b5ccdd91984c
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    [Julien: add details in commit log]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/rtl8723ds/rtl8723ds.mk
+++ package/rtl8723ds/rtl8723ds.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTL8723DS_VERSION = 546ffb2cb735e0c33e765936721bd66dd58e6893
+RTL8723DS_VERSION = 122a574f780da32bb0de760bf089a3a9ec1e8f87
 RTL8723DS_SITE = $(call github,benetti-engineering,rtl8723ds,$(RTL8723DS_VERSION))
 RTL8723DS_LICENSE = GPL-2.0
 RTL8723DS_LICENSE_FILES = COPYING
