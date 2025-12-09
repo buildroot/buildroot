fe5dcf402c0075b6ab86a92733ec75135f9dd760
Bernd Kuhls <bernd@kuhls.net>
2025-12-09 20:39:44 +0100

    package/python-brotli: security bump to version 1.2.0
    https://github.com/google/brotli/blob/v1.2.0/CHANGELOG.md
    
    Adds the following security hardening:
    
    python: added Decompressor::can_accept_more_data method and optional
    output_buffer_limit argument Decompressor::process; that allows mitigation
    of unexpectedly large output
    
    Which is needed to complete the security fixes in python-urllib3 2.6.0.
    
    Added dependency to host-python-pkgconfig to fix build error which would
    be introduced by this bump.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    [Peter: mark as security bump, describe the relation with urllib3]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-brotli/python-brotli.mk
+++ package/python-brotli/python-brotli.mk
@@ -4,12 +4,13 @@
 #
 ################################################################################
 
-PYTHON_BROTLI_VERSION = 1.1.0
-PYTHON_BROTLI_SOURCE = Brotli-$(PYTHON_BROTLI_VERSION).tar.gz
-PYTHON_BROTLI_SITE = https://files.pythonhosted.org/packages/2f/c2/f9e977608bdf958650638c3f1e28f85a1b075f075ebbe77db8555463787b
+PYTHON_BROTLI_VERSION = 1.2.0
+PYTHON_BROTLI_SOURCE = brotli-$(PYTHON_BROTLI_VERSION).tar.gz
+PYTHON_BROTLI_SITE = https://files.pythonhosted.org/packages/f7/16/c92ca344d646e71a43b8bb353f0a6490d7f6e06210f8554c8f874e454285
 PYTHON_BROTLI_SETUP_TYPE = setuptools
 PYTHON_BROTLI_LICENSE = MIT
 PYTHON_BROTLI_LICENSE_FILES = LICENSE
+PYTHON_BROTLI_DEPENDENCIES = host-python-pkgconfig
 
 PYTHON_BROTLI_CFLAGS = $(TARGET_CFLAGS)
 
