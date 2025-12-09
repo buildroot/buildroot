f13bdc0120c2cd1df4f78dd78a06ca57ee5128c6
Bernd Kuhls <bernd@kuhls.net>
2025-12-09 20:39:39 +0100

    package/python-beniget: bump version to 0.5.0
    https://github.com/serge-sans-paille/beniget/commits/0.5.0/
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-beniget/python-beniget.mk
+++ package/python-beniget/python-beniget.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_BENIGET_VERSION = 0.4.2.post1
+PYTHON_BENIGET_VERSION = 0.5.0
 PYTHON_BENIGET_SOURCE = beniget-$(PYTHON_BENIGET_VERSION).tar.gz
-PYTHON_BENIGET_SITE = https://files.pythonhosted.org/packages/2e/27/5bb01af8f2860d431b98d0721b96ff2cea979106cae3f2d093ec74f6400c
+PYTHON_BENIGET_SITE = https://files.pythonhosted.org/packages/31/a9/cf7c2317da1f5034fdebe84555e14a474b3297ef2d03ad148ff02fef2e3a
 PYTHON_BENIGET_SETUP_TYPE = setuptools
 PYTHON_BENIGET_LICENSE = BSD-3-Clause
 PYTHON_BENIGET_LICENSE_FILES = LICENSE
