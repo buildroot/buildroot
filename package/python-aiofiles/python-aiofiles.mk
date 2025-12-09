ecf116f9509a87b3633fb8a11d4adc01fabfe114
Bernd Kuhls <bernd@kuhls.net>
2025-12-09 20:39:24 +0100

    package/python-aiofiles: bump version to 25.1.0
    https://github.com/Tinche/aiofiles/blob/v25.1.0/CHANGELOG.md
    
    Added dependency to host-python-hatch-vcs to fix build error which would
    be introduced by this bump.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-aiofiles/python-aiofiles.mk
+++ package/python-aiofiles/python-aiofiles.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_AIOFILES_VERSION = 24.1.0
+PYTHON_AIOFILES_VERSION = 25.1.0
 PYTHON_AIOFILES_SOURCE = aiofiles-$(PYTHON_AIOFILES_VERSION).tar.gz
-PYTHON_AIOFILES_SITE = https://files.pythonhosted.org/packages/0b/03/a88171e277e8caa88a4c77808c20ebb04ba74cc4681bf1e9416c862de237
+PYTHON_AIOFILES_SITE = https://files.pythonhosted.org/packages/41/c3/534eac40372d8ee36ef40df62ec129bee4fdb5ad9706e58a29be53b2c970
 PYTHON_AIOFILES_SETUP_TYPE = hatch
 PYTHON_AIOFILES_LICENSE = Apache-2.0
 PYTHON_AIOFILES_LICENSE_FILES = LICENSE
+PYTHON_AIOFILES_DEPENDENCIES = host-python-hatch-vcs
 
 $(eval $(python-package))
