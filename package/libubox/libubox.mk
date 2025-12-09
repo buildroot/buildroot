9e9fb093804738434f5f429d2ce59546e4eb8147
Bernd Kuhls <bernd@kuhls.net>
2025-12-09 22:14:07 +0100

    package/libubox: bump version to git ecddb31dc3
    This bump includes patches which add cmake4 compatibility.
    
    json-c is now a mandatory dependency due to upstream commit:
    https://git.openwrt.org/?p=project/libubox.git;a=commitdiff;h=b7acc8e6fd5e13611ad90a593e98f9589af4009a
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libubox/libubox.mk
+++ package/libubox/libubox.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-LIBUBOX_VERSION = 3868f47c8f6c6570e62a3cdf8a7f26ffb1a67e6a
+LIBUBOX_VERSION = ecddb31dc34d89be5c9dc4595a4c58070eb090e4
 LIBUBOX_SITE = https://git.openwrt.org/project/libubox.git
 LIBUBOX_SITE_METHOD = git
 LIBUBOX_LICENSE = ISC, BSD-3-Clause
 LIBUBOX_INSTALL_STAGING = YES
-LIBUBOX_DEPENDENCIES = $(if $(BR2_PACKAGE_JSON_C),json-c)
+LIBUBOX_DEPENDENCIES = json-c
 
 ifeq ($(BR2_USE_MMU)$(BR2_PACKAGE_LUA_5_1),yy)
 LIBUBOX_DEPENDENCIES += lua
