818218ae7e33d0e7ad6071c2778d6e61b910ae30
Bernd Kuhls <bernd@kuhls.net>
2025-12-09 20:39:46 +0100

    package/python-calver: bump version to 2025.10.20
    Changes: https://github.com/di/calver/releases
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-calver/python-calver.mk
+++ package/python-calver/python-calver.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_CALVER_VERSION = 2022.6.26
+PYTHON_CALVER_VERSION = 2025.10.20
 PYTHON_CALVER_SOURCE = calver-$(PYTHON_CALVER_VERSION).tar.gz
-PYTHON_CALVER_SITE = https://files.pythonhosted.org/packages/b5/00/96cbed7c019c49ee04b8a08357a981983db7698ae6de402e57097cefc9ad
+PYTHON_CALVER_SITE = https://files.pythonhosted.org/packages/4a/96/0c57e3e228ffc54074867406b659b197678674f1f0bf600d114965289834
 PYTHON_CALVER_SETUP_TYPE = setuptools
 PYTHON_CALVER_LICENSE = Apache-2.0
 PYTHON_CALVER_LICENSE_FILES = LICENSE
