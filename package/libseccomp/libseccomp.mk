4f9600c2f97039b4ad6eb66087a7654805536a76
Preyas <preyas17@zohomail.in>
2025-12-09 15:48:33 +0000

    package/libseccomp: bump to version 2.6.0
    For change log, see:
    https://github.com/seccomp/libseccomp/blob/v2.6.0/CHANGELOG
    
    This commit also enables LoongArch64 support,
    added in upstream commit:
    https://github.com/seccomp/libseccomp/commit/6966ec77b195ac289ae168c7c5646d59a307f33f
    
    Signed-off-by: Preyas <preyas17@zohomail.in>
    [Julien:
     - add link to change log
     - enable loongarch64 support
    ]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libseccomp/libseccomp.mk
+++ package/libseccomp/libseccomp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBSECCOMP_VERSION = 2.5.5
+LIBSECCOMP_VERSION = 2.6.0
 LIBSECCOMP_SITE = https://github.com/seccomp/libseccomp/releases/download/v$(LIBSECCOMP_VERSION)
 LIBSECCOMP_LICENSE = LGPL-2.1
 LIBSECCOMP_LICENSE_FILES = LICENSE
