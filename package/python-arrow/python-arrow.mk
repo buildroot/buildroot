976d69f6cdb306b88b64eeb883165c763c876a78
Bernd Kuhls <bernd@kuhls.net>
2025-12-09 20:39:31 +0100

    package/python-arrow: bump version to 1.4.0
    https://github.com/arrow-py/arrow/blob/1.4.0/CHANGELOG.rst
    
    Switched _SETUP_TYPE to flit.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-arrow/python-arrow.mk
+++ package/python-arrow/python-arrow.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_ARROW_VERSION = 1.3.0
+PYTHON_ARROW_VERSION = 1.4.0
 PYTHON_ARROW_SOURCE = arrow-$(PYTHON_ARROW_VERSION).tar.gz
-PYTHON_ARROW_SITE = https://files.pythonhosted.org/packages/2e/00/0f6e8fcdb23ea632c866620cc872729ff43ed91d284c866b515c6342b173
-PYTHON_ARROW_SETUP_TYPE = setuptools
+PYTHON_ARROW_SITE = https://files.pythonhosted.org/packages/b9/33/032cdc44182491aa708d06a68b62434140d8c50820a087fac7af37703357
+PYTHON_ARROW_SETUP_TYPE = flit
 PYTHON_ARROW_LICENSE = Apache-2.0
 PYTHON_ARROW_LICENSE_FILES = LICENSE
 
