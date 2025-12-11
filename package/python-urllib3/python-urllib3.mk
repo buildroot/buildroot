974b5ce907e4b14571e528afccd00e55dc21ac3e
Peter Korsgaard <peter@korsgaard.com>
2025-12-11 23:14:14 +0100

    package/python-urllib3: bump to version 2.6.2
    Fixes a regression in the 2.6.x series:
    
    - Fixed HTTPResponse.read_chunked() to properly handle leftover data in the
      decoder's buffer when reading compressed chunked responses
    
    https://github.com/urllib3/urllib3/releases/tag/2.6.2
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-urllib3/python-urllib3.mk
+++ package/python-urllib3/python-urllib3.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_URLLIB3_VERSION = 2.6.1
+PYTHON_URLLIB3_VERSION = 2.6.2
 PYTHON_URLLIB3_SOURCE = urllib3-$(PYTHON_URLLIB3_VERSION).tar.gz
-PYTHON_URLLIB3_SITE = https://files.pythonhosted.org/packages/5e/1d/0f3a93cca1ac5e8287842ed4eebbd0f7a991315089b1a0b01c7788aa7b63
+PYTHON_URLLIB3_SITE = https://files.pythonhosted.org/packages/1e/24/a2a2ed9addd907787d7aa0355ba36a6cadf1768b934c652ea78acbd59dcd
 PYTHON_URLLIB3_LICENSE = MIT
 PYTHON_URLLIB3_LICENSE_FILES = LICENSE.txt
 PYTHON_URLLIB3_CPE_ID_VENDOR = python
