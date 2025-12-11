70d868991bb29a6e8870cbf63b6fd0b712097a9e
Bernd Kuhls <bernd@kuhls.net>
2025-12-11 20:55:08 +0100

    package/python-contourpy: bump version to 1.3.3
    https://github.com/contourpy/contourpy/blob/v1.3.3/docs/changelog.rst
    
    Updated license hash due to copyright year bump:
    https://github.com/contourpy/contourpy/commit/ebb716818af775fb969099617e0d68386217d503
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-contourpy/python-contourpy.mk
+++ package/python-contourpy/python-contourpy.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_CONTOURPY_VERSION = 1.3.1
+PYTHON_CONTOURPY_VERSION = 1.3.3
 PYTHON_CONTOURPY_SOURCE = contourpy-$(PYTHON_CONTOURPY_VERSION).tar.gz
-PYTHON_CONTOURPY_SITE = https://files.pythonhosted.org/packages/25/c2/fc7193cc5383637ff390a712e88e4ded0452c9fbcf84abe3de5ea3df1866
+PYTHON_CONTOURPY_SITE = https://files.pythonhosted.org/packages/58/01/1253e6698a07380cd31a736d248a3f2a50a7c88779a1813da27503cadc2a
 PYTHON_CONTOURPY_LICENSE = BSD-3-Clause
 PYTHON_CONTOURPY_LICENSE_FILES = LICENSE
 PYTHON_CONTOURPY_DEPENDENCIES = python-pybind host-python-meson-python
