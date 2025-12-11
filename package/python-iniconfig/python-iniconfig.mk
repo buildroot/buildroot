1a17185606161d7a03198e249c02bdc2bdc972d0
Bernd Kuhls <bernd@kuhls.net>
2025-12-11 20:55:27 +0100

    package/python-iniconfig: bump version to 2.3.0
    https://github.com/pytest-dev/iniconfig/blob/v2.3.0/CHANGELOG
    "* migrate from hatchling to setuptools 77 with setuptools_scm"
    
    Updated project URL according to https://pypi.org/project/iniconfig/
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-iniconfig/python-iniconfig.mk
+++ package/python-iniconfig/python-iniconfig.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_INICONFIG_VERSION = 2.1.0
+PYTHON_INICONFIG_VERSION = 2.3.0
 PYTHON_INICONFIG_SOURCE = iniconfig-$(PYTHON_INICONFIG_VERSION).tar.gz
-PYTHON_INICONFIG_SITE = https://files.pythonhosted.org/packages/f2/97/ebf4da567aa6827c909642694d71c9fcf53e5b504f2d96afea02718862f3
-PYTHON_INICONFIG_SETUP_TYPE = hatch
+PYTHON_INICONFIG_SITE = https://files.pythonhosted.org/packages/72/34/14ca021ce8e5dfedc35312d08ba8bf51fdd999c576889fc2c24cb97f4f10
+PYTHON_INICONFIG_SETUP_TYPE = setuptools
 PYTHON_INICONFIG_LICENSE = MIT
 PYTHON_INICONFIG_LICENSE_FILES = LICENSE
 PYTHON_INICONFIG_DEPENDENCIES = host-python-hatch-vcs
