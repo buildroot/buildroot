dc2321bb50076f585b7a9a0f1858c381d13a2051
Bernd Kuhls <bernd@kuhls.net>
2025-12-11 20:55:12 +0100

    package/python-docutils: bump version to 0.22.3
    https://docutils.sourceforge.io/RELEASE-NOTES.html#release-0-22-3-2025-11-06
    
    The license file was update:
    - Renamed from COPYING.txt to COPYING.rst in:
      https://sourceforge.net/p/docutils/code/9906/
    - The ZPL was removed in:
      https://sourceforge.net/p/docutils/code/9977/
    
    This commit updates _LICENSE and _LICENSE_FILES accordingly.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    [Julien: fix license hash]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-docutils/python-docutils.mk
+++ package/python-docutils/python-docutils.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_DOCUTILS_VERSION = 0.21.2
+PYTHON_DOCUTILS_VERSION = 0.22.3
 PYTHON_DOCUTILS_SOURCE = docutils-$(PYTHON_DOCUTILS_VERSION).tar.gz
-PYTHON_DOCUTILS_SITE = https://files.pythonhosted.org/packages/ae/ed/aefcc8cd0ba62a0560c3c18c33925362d46c6075480bfa4df87b28e169a9
-PYTHON_DOCUTILS_LICENSE = Public Domain, BSD-2-Clause, BSD-3-Clause, ZPL-2.1, GPL-3.0+ (emacs mode)
-PYTHON_DOCUTILS_LICENSE_FILES = COPYING.txt
+PYTHON_DOCUTILS_SITE = https://files.pythonhosted.org/packages/d9/02/111134bfeb6e6c7ac4c74594e39a59f6c0195dc4846afbeac3cba60f1927
+PYTHON_DOCUTILS_LICENSE = Public Domain, BSD-2-Clause, BSD-3-Clause, GPL-3.0+ (emacs mode)
+PYTHON_DOCUTILS_LICENSE_FILES = COPYING.rst
 PYTHON_DOCUTILS_SETUP_TYPE = flit
 
 $(eval $(python-package))
