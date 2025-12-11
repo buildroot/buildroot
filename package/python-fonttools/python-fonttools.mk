701e9685f7effa9cbd3b4088106951b7936a84ef
Bernd Kuhls <bernd@kuhls.net>
2025-12-11 20:19:53 +0100

    package/python-fonttools: security bump version to 4.61.0
    https://github.com/fonttools/fonttools/releases/tag/4.61.0
    
    Fixes CVE-2025-66034:
    https://github.com/fonttools/fonttools/security/advisories/GHSA-768j-98cg-p3fv
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit b0aa7e5a23895627e28cbcd33f0b998c59023148)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/python-fonttools/python-fonttools.mk
+++ package/python-fonttools/python-fonttools.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_FONTTOOLS_VERSION = 4.60.1
+PYTHON_FONTTOOLS_VERSION = 4.61.0
 PYTHON_FONTTOOLS_SOURCE = fonttools-$(PYTHON_FONTTOOLS_VERSION).tar.gz
-PYTHON_FONTTOOLS_SITE = https://files.pythonhosted.org/packages/4b/42/97a13e47a1e51a5a7142475bbcf5107fe3a68fc34aef331c897d5fb98ad0
+PYTHON_FONTTOOLS_SITE = https://files.pythonhosted.org/packages/33/f9/0e84d593c0e12244150280a630999835a64f2852276161b62a0f98318de0
 PYTHON_FONTTOOLS_SETUP_TYPE = setuptools
 PYTHON_FONTTOOLS_LICENSE = MIT
 PYTHON_FONTTOOLS_LICENSE_FILES = LICENSE
