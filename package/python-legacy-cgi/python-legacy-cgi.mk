85b7dbeeeeb8a5c4b2aede270be0b5c21e620442
Bernd Kuhls <bernd@kuhls.net>
2025-12-13 15:05:11 +0100

    package/python-legacy-cgi: bump version to 2.6.4
    https://github.com/jackrosenthal/legacy-cgi/releases
    
    Switched build system to hatch:
    https://github.com/jackrosenthal/legacy-cgi/commit/0ea10a20213fe89d77110dcaa3f4c932d4a70310
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-legacy-cgi/python-legacy-cgi.mk
+++ package/python-legacy-cgi/python-legacy-cgi.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_LEGACY_CGI_VERSION = 2.6.2
+PYTHON_LEGACY_CGI_VERSION = 2.6.4
 PYTHON_LEGACY_CGI_SOURCE = legacy_cgi-$(PYTHON_LEGACY_CGI_VERSION).tar.gz
-PYTHON_LEGACY_CGI_SITE = https://files.pythonhosted.org/packages/ad/2e/e1860989bc6cfdecba66db37f2f783636b97a1248ac25fbe864b6e931c22
-PYTHON_LEGACY_CGI_SETUP_TYPE = poetry
+PYTHON_LEGACY_CGI_SITE = https://files.pythonhosted.org/packages/f4/9c/91c7d2c5ebbdf0a1a510bfa0ddeaa2fbb5b78677df5ac0a0aa51cf7125b0
+PYTHON_LEGACY_CGI_SETUP_TYPE = hatch
 PYTHON_LEGACY_CGI_LICENSE = PSF-2.0
 PYTHON_LEGACY_CGI_LICENSE_FILES = LICENSE
 
