8dde5a279ea97e24bf26af110a774b9fc698eb60
Bernd Kuhls <bernd@kuhls.net>
2025-12-13 15:05:12 +0100

    package/python-libevdev: bump version to 0.13.1
    Switched build system to hatch:
    https://gitlab.freedesktop.org/libevdev/python-libevdev/-/commit/d3542404761357e335f61530b95af8a7fe196f8b
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-libevdev/python-libevdev.mk
+++ package/python-libevdev/python-libevdev.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_LIBEVDEV_VERSION = 0.12
+PYTHON_LIBEVDEV_VERSION = 0.13.1
 PYTHON_LIBEVDEV_SOURCE = libevdev-$(PYTHON_LIBEVDEV_VERSION).tar.gz
-PYTHON_LIBEVDEV_SITE = https://files.pythonhosted.org/packages/61/6b/e0193f4ba0c3b7d20f8f1b961793d39df5538451c0959abb115020d4ec2d
-PYTHON_LIBEVDEV_SETUP_TYPE = setuptools
+PYTHON_LIBEVDEV_SITE = https://files.pythonhosted.org/packages/86/ff/4f8ab38330965168be742b772bcd151a7a052ea17b2481c43a607875d4ed
+PYTHON_LIBEVDEV_SETUP_TYPE = hatch
 PYTHON_LIBEVDEV_LICENSE = MIT
 PYTHON_LIBEVDEV_LICENSE_FILES = COPYING
 
