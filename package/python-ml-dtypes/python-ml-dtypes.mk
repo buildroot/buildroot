2da3485d3c6bfc50a656a5a01d8289d82c2cd813
Bernd Kuhls <bernd@kuhls.net>
2025-12-13 15:05:18 +0100

    package/python-ml-dtypes: bump version to 0.5.4
    https://github.com/jax-ml/ml_dtypes/blob/v0.5.4/CHANGELOG.md
    
    Added runtime comment found by scanpypi.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-ml-dtypes/python-ml-dtypes.mk
+++ package/python-ml-dtypes/python-ml-dtypes.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ML_DTYPES_VERSION = 0.5.1
+PYTHON_ML_DTYPES_VERSION = 0.5.4
 PYTHON_ML_DTYPES_SOURCE = ml_dtypes-$(PYTHON_ML_DTYPES_VERSION).tar.gz
-PYTHON_ML_DTYPES_SITE = https://files.pythonhosted.org/packages/32/49/6e67c334872d2c114df3020e579f3718c333198f8312290e09ec0216703a
+PYTHON_ML_DTYPES_SITE = https://files.pythonhosted.org/packages/0e/4a/c27b42ed9b1c7d13d9ba8b6905dece787d6259152f2309338aed29b2447b
 PYTHON_ML_DTYPES_LICENSE = Apache-2.0
 PYTHON_ML_DTYPES_LICENSE_FILES = LICENSE
 PYTHON_ML_DTYPES_SETUP_TYPE = setuptools
