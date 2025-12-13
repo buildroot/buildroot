285097051d4a7dd821f01d15c705867f7e26c635
Bernd Kuhls <bernd@kuhls.net>
2025-12-13 15:05:22 +0100

    package/python-pip: bumpp version to 25.3
    https://github.com/pypa/pip/blob/25.3/NEWS.rst
    "Building pip itself from source now uses flit-core instead of
     setuptools."
    
    Switched build system to flit.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-pip/python-pip.mk
+++ package/python-pip/python-pip.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_PIP_VERSION = 25.2
+PYTHON_PIP_VERSION = 25.3
 PYTHON_PIP_SOURCE = pip-$(PYTHON_PIP_VERSION).tar.gz
-PYTHON_PIP_SITE = https://files.pythonhosted.org/packages/20/16/650289cd3f43d5a2fadfd98c68bd1e1e7f2550a1a5326768cddfbcedb2c5
-PYTHON_PIP_SETUP_TYPE = setuptools
+PYTHON_PIP_SITE = https://files.pythonhosted.org/packages/fe/6e/74a3f0179a4a73a53d66ce57fdb4de0080a8baa1de0063de206d6167acc2
+PYTHON_PIP_SETUP_TYPE = flit
 PYTHON_PIP_LICENSE = MIT
 PYTHON_PIP_LICENSE_FILES = LICENSE.txt
 PYTHON_PIP_CPE_ID_VENDOR = pypa
