f655fc37d711af563d40fbbf6856fae5b5df7980
Bernd Kuhls <bernd@kuhls.net>
2025-12-13 15:05:20 +0100

    package/python-networkx: bump version to 3.6.1
    https://github.com/networkx/networkx/blob/networkx-3.6.1/doc/release/release_3.6.rst
    https://github.com/networkx/networkx/blob/networkx-3.6.1/doc/release/release_3.6.1.rst
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-networkx/python-networkx.mk
+++ package/python-networkx/python-networkx.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_NETWORKX_VERSION = 3.5
+PYTHON_NETWORKX_VERSION = 3.6.1
 PYTHON_NETWORKX_SOURCE = networkx-$(PYTHON_NETWORKX_VERSION).tar.gz
-PYTHON_NETWORKX_SITE = https://files.pythonhosted.org/packages/6c/4f/ccdb8ad3a38e583f214547fd2f7ff1fc160c43a75af88e6aec213404b96a
+PYTHON_NETWORKX_SITE = https://files.pythonhosted.org/packages/6a/51/63fe664f3908c97be9d2e4f1158eb633317598cfa6e1fc14af5383f17512
 PYTHON_NETWORKX_LICENSE = BSD-3-Clause
 PYTHON_NETWORKX_LICENSE_FILES = LICENSE.txt
 PYTHON_NETWORKX_CPE_ID_VENDOR = python
