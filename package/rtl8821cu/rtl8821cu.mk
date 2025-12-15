aed6e63fb8e58e47bf001184f77f36d44ee96c5e
Giulio Benetti <giulio.benetti@benettiengineering.com>
2025-12-15 19:18:28 +0100

    package/rtl8821cu: bump to version 2025-12-14
    With this commit it builds with Linux 6.18
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 7b22fcfefffcda4d839a8c10151c5dffdfef0e8d)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/rtl8821cu/rtl8821cu.mk
+++ package/rtl8821cu/rtl8821cu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTL8821CU_VERSION = 3d1fcf4bc838542ceb03b0b4e9e40600720cf6ae
+RTL8821CU_VERSION = 7f63a9da2e8ed83403f6f920e9b1628a37b38ef4
 RTL8821CU_SITE = $(call github,morrownr,8821cu-20210916,$(RTL8821CU_VERSION))
 RTL8821CU_LICENSE = GPL-2.0
 RTL8821CU_LICENSE_FILES = LICENSE
