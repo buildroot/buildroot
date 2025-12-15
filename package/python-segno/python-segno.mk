0a4d77768cb281739669ab88044bf5ca7537dc78
Bernd Kuhls <bernd@kuhls.net>
2025-12-15 20:14:15 +0100

    package/python-segno: bump version to 1.6.6
    https://github.com/heuer/segno/blob/1.6.6/CHANGES.rst
    
    Updated license hash due to copyright year bump:
    https://github.com/heuer/segno/commit/780296986b9b5c7934667935c8d654d0a7e97262
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-segno/python-segno.mk
+++ package/python-segno/python-segno.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SEGNO_VERSION = 1.6.1
+PYTHON_SEGNO_VERSION = 1.6.6
 PYTHON_SEGNO_SOURCE = segno-$(PYTHON_SEGNO_VERSION).tar.gz
-PYTHON_SEGNO_SITE = https://files.pythonhosted.org/packages/5d/74/3896e205306a1b43d6b88326e5838572d97b4b74df8c9cd11acfcd9db503
+PYTHON_SEGNO_SITE = https://files.pythonhosted.org/packages/1c/2e/b396f750c53f570055bf5a9fc1ace09bed2dff013c73b7afec5702a581ba
 PYTHON_SEGNO_SETUP_TYPE = flit
 PYTHON_SEGNO_LICENSE = BSD-3-Clause
 PYTHON_SEGNO_LICENSE_FILES = LICENSE
