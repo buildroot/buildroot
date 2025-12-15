024814af94f8d8e09a58e5a7e560dee5da830ef9
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2025-12-15 23:49:39 +0100

    package/libcec: drop rpi-userland support
    As we're about to remove the rpi-userland package, drop support for it
    from package/libcec.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libcec/libcec.mk
+++ package/libcec/libcec.mk
@@ -24,15 +24,6 @@ ifeq ($(BR2_PACKAGE_PYTHON3),y)
 LIBCEC_DEPENDENCIES += host-swig python3
 endif
 
-ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
-LIBCEC_DEPENDENCIES += rpi-userland
-LIBCEC_CONF_OPTS += \
-	-DCMAKE_C_FLAGS="$(TARGET_CFLAGS) -lvcos -lvchiq_arm" \
-	-DCMAKE_CXX_FLAGS="$(TARGET_CXXFLAGS) \
-		-I$(STAGING_DIR)/usr/include/interface/vmcs_host/linux \
-		-I$(STAGING_DIR)/usr/include/interface/vcos/pthreads"
-endif
-
 ifeq ($(BR2_PACKAGE_XLIB_LIBXRANDR),y)
 LIBCEC_DEPENDENCIES += xlib_libXrandr
 endif
