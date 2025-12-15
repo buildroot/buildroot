66b56b601f20b44cf2b4cd754f53a2c0faa64c34
Bernd Kuhls <bernd@kuhls.net>
2025-12-15 20:13:56 +0100

    package/python-pydal: bump version to 20251207.1
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-pydal/python-pydal.mk
+++ package/python-pydal/python-pydal.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYDAL_VERSION = 20250922.1
+PYTHON_PYDAL_VERSION = 20251207.1
 PYTHON_PYDAL_SOURCE = pydal-$(PYTHON_PYDAL_VERSION).tar.gz
-PYTHON_PYDAL_SITE = https://files.pythonhosted.org/packages/f9/94/bf35afe3eaf94e2c411be204b8421b3abb34a24a260513fd9bc0e45c05e4
+PYTHON_PYDAL_SITE = https://files.pythonhosted.org/packages/53/8f/f22822372765bf06a8b2aa9b0246a2f3b6abe21742b3608aa71939900cfe
 PYTHON_PYDAL_LICENSE = BSD-3-Clause
 PYTHON_PYDAL_LICENSE_FILES = LICENSE.txt
 PYTHON_PYDAL_SETUP_TYPE = setuptools
