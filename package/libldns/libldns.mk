f7fdac73ec3b67d5d3fb31170b2c9028e99c0e4f
Bernd Kuhls <bernd@kuhls.net>
2025-12-15 20:37:52 +0100

    package/libldns: bump to version 1.9.0
    Changelog: https://github.com/NLnetLabs/ldns/blob/1.9.0/Changelog
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libldns/libldns.mk
+++ package/libldns/libldns.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBLDNS_VERSION = 1.8.4
+LIBLDNS_VERSION = 1.9.0
 LIBLDNS_SOURCE = ldns-$(LIBLDNS_VERSION).tar.gz
 LIBLDNS_SITE = https://www.nlnetlabs.nl/downloads/ldns
 LIBLDNS_LICENSE = BSD-3-Clause
