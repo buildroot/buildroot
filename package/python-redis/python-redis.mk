29af58079b17ec3ce6669e0888ee30b4940fb8f7
Bernd Kuhls <bernd@kuhls.net>
2025-12-15 20:14:09 +0100

    package/python-redis: bump version to 7.1.0
    https://github.com/redis/redis-py/releases/
    
    Switched setup type to hatch added upstream in version 6.0.0:
    https://github.com/redis/redis-py/commit/483c021e88657ec045754537c3be3efc4d3a202c
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-redis/python-redis.mk
+++ package/python-redis/python-redis.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_REDIS_VERSION = 5.2.1
+PYTHON_REDIS_VERSION = 7.1.0
 PYTHON_REDIS_SOURCE = redis-$(PYTHON_REDIS_VERSION).tar.gz
-PYTHON_REDIS_SITE = https://files.pythonhosted.org/packages/47/da/d283a37303a995cd36f8b92db85135153dc4f7a8e4441aa827721b442cfb
-PYTHON_REDIS_SETUP_TYPE = setuptools
+PYTHON_REDIS_SITE = https://files.pythonhosted.org/packages/43/c8/983d5c6579a411d8a99bc5823cc5712768859b5ce2c8afe1a65b37832c81
+PYTHON_REDIS_SETUP_TYPE = hatch
 PYTHON_REDIS_LICENSE = MIT
 PYTHON_REDIS_LICENSE_FILES = LICENSE
 
