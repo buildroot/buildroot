ed899d9f18bccb93d1ce92a4789a9b196bfb901b
Bernd Kuhls <bernd@kuhls.net>
2025-12-15 20:14:05 +0100

    package/python-pysmb: bump version to 1.2.13
    https://github.com/miketeo/pysmb/blob/pysmb-1.2.13/CHANGELOG
    
    Added comments to Config.in computed by scanpypi.
    
    Updated project URL, old URL is broken.
    
    All hashes were computed by scanpypi.
    
    Updated license hash due to copyright year bump:
    https://github.com/miketeo/pysmb/commit/e9d59f356455ab29c03ccaa0a1e0219e3ad3c2e9
    
    Updated _SITE to URL computed by scanpypi.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-pysmb/python-pysmb.mk
+++ package/python-pysmb/python-pysmb.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYSMB_VERSION = 1.2.10
+PYTHON_PYSMB_VERSION = 1.2.13
 PYTHON_PYSMB_SOURCE = pysmb-$(PYTHON_PYSMB_VERSION).tar.gz
-PYTHON_PYSMB_SITE = https://miketeo.net/files/Projects/pysmb
+PYTHON_PYSMB_SITE = https://files.pythonhosted.org/packages/de/79/f9a2f2f3aea92ae5647079fcbf28e3969de9af7953bef10e31898fe435e2
 PYTHON_PYSMB_LICENSE = Libpng
 PYTHON_PYSMB_LICENSE_FILES = LICENSE
 PYTHON_PYSMB_SETUP_TYPE = setuptools
