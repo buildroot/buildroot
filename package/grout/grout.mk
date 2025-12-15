c3f7f070e6a8750ba06e63574319902d6a9cbd42
Maxime Leroy <maxime@leroys.fr>
2025-12-15 12:43:00 +0100

    package/grout: bump to 0.14.2 version
    See the release note of the new versions:
    https://github.com/DPDK/grout/releases/tag/v0.14.0
    https://github.com/DPDK/grout/releases/tag/v0.14.1
    https://github.com/DPDK/grout/releases/tag/v0.14.2
    
    Since v0.14.0, Grout uses the libmnl [1], the dependency are updated in
    consequence. Grout also requires VRF support in the kernel, as it creates
    a VRF on the Linux side to allow TCP/UDP termination by Linux
    applications in different L3 domains. [2]
    
    [1] https://github.com/DPDK/grout/commit/2eb97af1e8
    [2] https://github.com/DPDK/grout/commit/391f9fa032
    
    Signed-off-by: Maxime Leroy <maxime@leroys.fr>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/grout/grout.mk
+++ package/grout/grout.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GROUT_VERSION = 0.13.0
+GROUT_VERSION = 0.14.2
 GROUT_SITE = $(call github,DPDK,grout,v$(GROUT_VERSION))
 GROUT_LICENSE = BSD-3-Clause
 GROUT_LICENSE_FILES = licenses/BSD-3-clause.txt
@@ -16,12 +16,15 @@ GROUT_DEPENDENCIES = \
 	host-pkgconf \
 	dpdk \
 	libevent \
+	libmnl \
 	numactl \
 	libecoli \
 	util-linux
 
 define GROUT_LINUX_CONFIG_FIXUPS
 	$(call KCONFIG_ENABLE_OPT,CONFIG_TUN)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_NET_L3_MASTER)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_VRF)
 endef
 
 $(eval $(meson-package))
