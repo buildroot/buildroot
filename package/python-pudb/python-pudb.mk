a8a09c557c1c95b4acd0151ef1295aaa1a1320e8
Bernd Kuhls <bernd@kuhls.net>
2025-12-15 20:13:54 +0100

    package/python-pudb: bump version to 2025.1.5
    https://github.com/inducer/pudb/releases
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-pudb/python-pudb.mk
+++ package/python-pudb/python-pudb.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PUDB_VERSION = 2024.1.3
+PYTHON_PUDB_VERSION = 2025.1.5
 PYTHON_PUDB_SOURCE = pudb-$(PYTHON_PUDB_VERSION).tar.gz
-PYTHON_PUDB_SITE = https://files.pythonhosted.org/packages/36/27/fb087bdf2cd9c8e56d0347b863ce5995967c15a3e2a0b9245c7a8f6f1598
+PYTHON_PUDB_SITE = https://files.pythonhosted.org/packages/f5/69/0c52c6058da586e173e48ef8f9c45bc805d9df78d5ad4d69467ff68694b5
 PYTHON_PUDB_SETUP_TYPE = hatch
 PYTHON_PUDB_LICENSE = MIT
 PYTHON_PUDB_LICENSE_FILES = LICENSE
