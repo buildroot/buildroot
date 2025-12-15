64064dc39dd8bd2cf7a1b408cb59f8d62bae1f53
Bernd Kuhls <bernd@kuhls.net>
2025-12-15 20:14:12 +0100

    package/python-rsa: bump version to 4.9.1
    Please note that this project was archived by the maintainer:
    https://github.com/sybrenstuvel/python-rsa/commit/42b0e14ffbeeb9d99d1037e6440a2cc61780e4ea
    
    The only buildroot package using python-rsa is python-google-auth which
    did not switch to an alternative yet:
    https://github.com/googleapis/google-auth-library-python/pull/1771
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-rsa/python-rsa.mk
+++ package/python-rsa/python-rsa.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_RSA_VERSION = 4.9
+PYTHON_RSA_VERSION = 4.9.1
 PYTHON_RSA_SOURCE = rsa-$(PYTHON_RSA_VERSION).tar.gz
-PYTHON_RSA_SITE = https://files.pythonhosted.org/packages/aa/65/7d973b89c4d2351d7fb232c2e452547ddfa243e93131e7cfa766da627b52
+PYTHON_RSA_SITE = https://files.pythonhosted.org/packages/da/8a/22b7beea3ee0d44b1916c0c1cb0ee3af23b700b6da9f04991899d0c555d4
 PYTHON_RSA_SETUP_TYPE = poetry
 PYTHON_RSA_LICENSE = Apache-2.0
 PYTHON_RSA_LICENSE_FILES = LICENSE
