e1d159c5d5dc00233c74a2bc24994bc194e4e177
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2025-12-15 22:02:34 +0100

    package/mesa3d: add missing dependencies for imagination driver
    The build of the following basic configuration enabling the
    imagination Vulkan driver
    
    BR2_aarch64=y
    BR2_TOOLCHAIN_EXTERNAL=y
    BR2_TOOLCHAIN_EXTERNAL_BOOTLIN=y
    BR2_TOOLCHAIN_EXTERNAL_BOOTLIN_AARCH64_GLIBC_STABLE=y
    BR2_PACKAGE_MESA3D=y
    BR2_PACKAGE_MESA3D_VULKAN_DRIVER_IMAGINATION=y
    
    fails with:
    
    meson.build:847:3: ERROR: Feature llvm cannot be disabled: CLC requires LLVM
    
    Adding just LLVM as a dependency is not enough, as then libclc is
    needed, then LLVMSPIRVLib, then clangBasic, then the pco_clc tool.
    
    In fact, like the Panfrost driver, building the Imagination driver
    requires building host tools using host-mesa3d. To fix this we:
    
    - Make the BR2_PACKAGE_MESA3D_OPENCL option selectable
    
    - Make sure that BR2_PACKAGE_MESA3D_VULKAN_DRIVER_IMAGINATION depends
      on BR2_PACKAGE_MESA3D_LLVM and select
      BR2_PACKAGE_MESA3D_NEEDS_PRECOMP_COMPILER (the latter being needed to
      build host-mesa3d)
    
    - Make sure the host-mesa3d builds imagination
      tools (-Dtools=imagination) and install
      pco_clc (HOST_MESA3D_INSTALL_PCO_CLC). This requires introducing
      HOST_MESA3D_TOOLS as a list of tools to build, which then gets used
      to construct the -Dtools argument, as we can now have both
      "panfrost" and "imagination" in this list.
    
    With all this, the defconfig above builds successfully.
    
    This has been broken since Buildroot commit
    5e818c16a31a902ec29a250f920f43c76e2d9868, which introduced the vulkan
    driver support.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/mesa3d/mesa3d.mk
+++ package/mesa3d/mesa3d.mk
@@ -284,19 +284,24 @@ else
 MESA3D_CONF_OPTS += -Dglvnd=disabled
 endif
 
+ifeq ($(BR2_PACKAGE_MESA3D_GALLIUM_DRIVER_PANFROST),y)
+HOST_MESA3D_TOOLS += panfrost
+endif
+
+ifeq ($(BR2_PACKAGE_MESA3D_VULKAN_DRIVER_IMAGINATION),y)
+HOST_MESA3D_TOOLS += imagination
+endif
+
 HOST_MESA3D_CONF_OPTS = \
 	-Dglvnd=disabled \
 	-Dgallium-drivers=$(subst $(space),$(comma),$(HOST_MESA3D_GALLIUM_DRIVERS-y)) \
+	-Dtools=$(subst $(space),$(comma),$(HOST_MESA3D_TOOLS)) \
 	-Dinstall-mesa-clc=true \
 	-Dmesa-clc=enabled \
 	-Dplatforms= \
 	-Dprecomp-compiler=enabled \
 	-Dglx=disabled \
 	-Dvulkan-drivers=""
-
-ifeq ($(BR2_PACKAGE_MESA3D_GALLIUM_DRIVER_PANFROST),y)
-HOST_MESA3D_CONF_OPTS += -Dtools=panfrost
-endif
 
 HOST_MESA3D_DEPENDENCIES = \
 	host-libclc \
@@ -310,10 +315,16 @@ HOST_MESA3D_INSTALL_PANFROST_COMPILE = \
 	$(INSTALL) -D -m 0755 $(@D)/buildroot-build/src/panfrost/clc/panfrost_compile $(HOST_DIR)/bin/panfrost_compile
 endif
 
+ifeq ($(BR2_PACKAGE_MESA3D_VULKAN_DRIVER_IMAGINATION),y)
+HOST_MESA3D_INSTALL_PCO_CLC = \
+	$(INSTALL) -D -m 0755 $(@D)/buildroot-build/src/imagination/pco/uscgen/pco_clc $(HOST_DIR)/bin/pco_clc
+endif
+
 define HOST_MESA3D_INSTALL_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/buildroot-build/src/compiler/clc/mesa_clc $(HOST_DIR)/bin/mesa_clc
 	$(INSTALL) -D -m 0755 $(@D)/buildroot-build/src/compiler/spirv/vtn_bindgen2 $(HOST_DIR)/bin/vtn_bindgen2
 	$(HOST_MESA3D_INSTALL_PANFROST_COMPILE)
+	$(HOST_MESA3D_INSTALL_PCO_CLC)
 endef
 
 $(eval $(meson-package))
