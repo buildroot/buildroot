06e60c15de5195c6bfc6e166979d4d7efaa6325f
Bernd Kuhls <bernd@kuhls.net>
2025-12-16 20:41:13 +0100

    package/python-webargs: bump version to 8.7.1
    https://github.com/marshmallow-code/webargs/blob/8.7.1/CHANGELOG.rst
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-webargs/python-webargs.mk
+++ package/python-webargs/python-webargs.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_WEBARGS_VERSION = 8.6.0
+PYTHON_WEBARGS_VERSION = 8.7.1
 PYTHON_WEBARGS_SOURCE = webargs-$(PYTHON_WEBARGS_VERSION).tar.gz
-PYTHON_WEBARGS_SITE = https://files.pythonhosted.org/packages/8c/51/e9ee5d8315864adf65e92f858f826514538e30db542d4782dd94c2418464
+PYTHON_WEBARGS_SITE = https://files.pythonhosted.org/packages/37/64/17afc4e6f47eef154a553c6e56adcc9f1ac3003305c7df978d11aa62937e
 PYTHON_WEBARGS_SETUP_TYPE = flit
 PYTHON_WEBARGS_LICENSE = MIT
 PYTHON_WEBARGS_LICENSE_FILES = LICENSE
