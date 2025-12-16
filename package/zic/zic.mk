2dc61b52e8aa81104ad58e2fdb129da4c11c3327
Marcus Hoffmann <buildroot@bubu1.eu>
2025-12-16 19:28:44 +0100

    package/zic: bump to 2025c
    Release notes:
    https://lists.iana.org/hyperkitty/list/tz-announce@iana.org/thread/TAGXKYLMAQRZRFTERQ33CEKOW7KRJVAK/
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/zic/zic.mk
+++ package/zic/zic.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ZIC_VERSION = 2025b
+ZIC_VERSION = 2025c
 ZIC_SOURCE = tzcode$(ZIC_VERSION).tar.gz
 ZIC_SITE = https://www.iana.org/time-zones/repository/releases
 ZIC_STRIP_COMPONENTS = 0
