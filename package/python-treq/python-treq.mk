e4cf03b6af84be3cfac3783e1e7fd118bc437628
Bernd Kuhls <bernd@kuhls.net>
2025-12-16 20:41:02 +0100

    package/python-treq: bump version to 25.5.0
    https://github.com/twisted/treq/blob/treq-25.5.0/CHANGELOG.rst
    
    Switched build system to hatch:
    https://github.com/twisted/treq/commit/88ba8110f82cc721c37da1ce96fbaa83a0e6807b
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-treq/python-treq.mk
+++ package/python-treq/python-treq.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-PYTHON_TREQ_VERSION = 24.9.1
+PYTHON_TREQ_VERSION = 25.5.0
 PYTHON_TREQ_SOURCE = treq-$(PYTHON_TREQ_VERSION).tar.gz
-PYTHON_TREQ_SITE = https://files.pythonhosted.org/packages/08/94/424f3bb11eaf464880d076cca2e9d0ee21a1be805fe9761b87eccba5b3ae
+PYTHON_TREQ_SITE = https://files.pythonhosted.org/packages/ed/7b/b9dba8d947584481aad7e29b01f2ea6bbe794a0352e0cdb68e99ab135c31
 PYTHON_TREQ_LICENSE = MIT
 PYTHON_TREQ_LICENSE_FILES = LICENSE
-PYTHON_TREQ_SETUP_TYPE = setuptools
+PYTHON_TREQ_SETUP_TYPE = hatch
 PYTHON_TREQ_DEPENDENCIES = host-python-incremental
 
 $(eval $(python-package))
