6104d7fe226c45746bc4f4432cf00073d38fd452
Bernd Kuhls <bernd@kuhls.net>
2025-12-16 20:41:15 +0100

    package/python-wsproto: bump version to 1.3.2
    https://github.com/python-hyper/wsproto/blob/1.3.2/CHANGELOG.rst
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-wsproto/python-wsproto.mk
+++ package/python-wsproto/python-wsproto.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_WSPROTO_VERSION = 1.2.0
+PYTHON_WSPROTO_VERSION = 1.3.2
 PYTHON_WSPROTO_SOURCE = wsproto-$(PYTHON_WSPROTO_VERSION).tar.gz
-PYTHON_WSPROTO_SITE = https://files.pythonhosted.org/packages/c9/4a/44d3c295350d776427904d73c189e10aeae66d7f555bb2feee16d1e4ba5a
+PYTHON_WSPROTO_SITE = https://files.pythonhosted.org/packages/c7/79/12135bdf8b9c9367b8701c2c19a14c913c120b882d50b014ca0d38083c2c
 PYTHON_WSPROTO_SETUP_TYPE = setuptools
 PYTHON_WSPROTO_LICENSE = MIT
 PYTHON_WSPROTO_LICENSE_FILES = LICENSE
