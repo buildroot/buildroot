213e67d6f02030f7690f0c8aa241bfe0e7f5e63f
Julien Olivain <ju.o@free.fr>
2025-12-17 00:14:44 +0100

    package/kvmtool: bump to git version 7ad32e5514
    For the commit log, see:
    https://git.kernel.org/pub/scm/linux/kernel/git/will/kvmtool.git/log/?id=7ad32e5514aca2b6d19398fd3ae5a7c5e0e1ce24
    
    This commit also removes package patches which are now upstream.
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kvmtool/kvmtool.mk
+++ package/kvmtool/kvmtool.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KVMTOOL_VERSION = 1117dbc8ceb21abc6e8cd9861450695995852290
+KVMTOOL_VERSION = 7ad32e5514aca2b6d19398fd3ae5a7c5e0e1ce24
 KVMTOOL_SITE = https://git.kernel.org/pub/scm/linux/kernel/git/will/kvmtool.git
 KVMTOOL_SITE_METHOD = git
 KVMTOOL_DEPENDENCIES = \
