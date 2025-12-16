e0cea7186f194a848c723a1ee143afc97f7b9de0
Bernd Kuhls <bernd@kuhls.net>
2025-12-16 20:40:57 +0100

    package/python-soupsieve: bump version to 2.8
    https://github.com/facelessuser/soupsieve/blob/2.8/docs/src/markdown/about/changelog.md
    
    Updated license hash due to copyright year bump:
    https://github.com/facelessuser/soupsieve/commit/a616022df73ddc5ecb71092155be9baba876b52d
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-soupsieve/python-soupsieve.mk
+++ package/python-soupsieve/python-soupsieve.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SOUPSIEVE_VERSION = 2.6
+PYTHON_SOUPSIEVE_VERSION = 2.8
 PYTHON_SOUPSIEVE_SOURCE = soupsieve-$(PYTHON_SOUPSIEVE_VERSION).tar.gz
-PYTHON_SOUPSIEVE_SITE = https://files.pythonhosted.org/packages/d7/ce/fbaeed4f9fb8b2daa961f90591662df6a86c1abf25c548329a86920aedfb
+PYTHON_SOUPSIEVE_SITE = https://files.pythonhosted.org/packages/6d/e6/21ccce3262dd4889aa3332e5a119a3491a95e8f60939870a3a035aabac0d
 PYTHON_SOUPSIEVE_SETUP_TYPE = hatch
 PYTHON_SOUPSIEVE_LICENSE = MIT
 PYTHON_SOUPSIEVE_LICENSE_FILES = LICENSE.md
