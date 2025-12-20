dede411c7efd31afc0f8e0cbd37781eaade38f36
Bernd Kuhls <bernd@kuhls.net>
2025-12-20 22:41:50 +0100

    package/python-cachetools: bump version to 6.2.4
    https://github.com/tkem/cachetools/blob/v6.2.4/CHANGELOG.rst
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-cachetools/python-cachetools.mk
+++ package/python-cachetools/python-cachetools.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_CACHETOOLS_VERSION = 6.2.2
+PYTHON_CACHETOOLS_VERSION = 6.2.4
 PYTHON_CACHETOOLS_SOURCE = cachetools-$(PYTHON_CACHETOOLS_VERSION).tar.gz
-PYTHON_CACHETOOLS_SITE = https://files.pythonhosted.org/packages/fb/44/ca1675be2a83aeee1886ab745b28cda92093066590233cc501890eb8417a
+PYTHON_CACHETOOLS_SITE = https://files.pythonhosted.org/packages/bc/1d/ede8680603f6016887c062a2cf4fc8fdba905866a3ab8831aa8aa651320c
 PYTHON_CACHETOOLS_SETUP_TYPE = setuptools
 PYTHON_CACHETOOLS_LICENSE = MIT
 PYTHON_CACHETOOLS_LICENSE_FILES = LICENSE
