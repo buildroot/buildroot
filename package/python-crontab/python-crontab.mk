9fe8f09e74a09f6cffed020af58f2c0a02b46e8e
Bernd Kuhls <bernd@kuhls.net>
2025-12-20 22:41:51 +0100

    package/python-crontab: bump version to 3.3.0
    https://gitlab.com/doctormo/python-crontab/-/tags/v3.3.0
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-crontab/python-crontab.mk
+++ package/python-crontab/python-crontab.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_CRONTAB_VERSION = 3.2.0
+PYTHON_CRONTAB_VERSION = 3.3.0
 PYTHON_CRONTAB_SOURCE = python_crontab-$(PYTHON_CRONTAB_VERSION).tar.gz
-PYTHON_CRONTAB_SITE = https://files.pythonhosted.org/packages/e2/f0/25775565c133d4e29eeb607bf9ddba0075f3af36041a1844dd207881047f
+PYTHON_CRONTAB_SITE = https://files.pythonhosted.org/packages/99/7f/c54fb7e70b59844526aa4ae321e927a167678660ab51dda979955eafb89a
 PYTHON_CRONTAB_SETUP_TYPE = setuptools
 PYTHON_CRONTAB_LICENSE = LGPL-3.0+
 PYTHON_CRONTAB_LICENSE_FILES = COPYING
