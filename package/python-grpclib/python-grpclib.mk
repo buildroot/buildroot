5e9d79ac897242823aa6330a71bb3be5afce14d7
Bernd Kuhls <bernd@kuhls.net>
2025-12-20 22:41:59 +0100

    package/python-grpclib: bump version to 0.4.9
    https://github.com/vmagamedov/grpclib/blob/v0.4.9/docs/changelog/index.rst
    
    Updated license hash due to upstream commit:
    https://github.com/vmagamedov/grpclib/commit/069f2493abcd7734231e4071d5ecb9b83852a8a9
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-grpclib/python-grpclib.mk
+++ package/python-grpclib/python-grpclib.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_GRPCLIB_VERSION = 0.4.8
+PYTHON_GRPCLIB_VERSION = 0.4.9
 PYTHON_GRPCLIB_SOURCE = grpclib-$(PYTHON_GRPCLIB_VERSION).tar.gz
-PYTHON_GRPCLIB_SITE = https://files.pythonhosted.org/packages/19/75/0f0d3524b38b35e5cd07334b754aa9bd0570140ad982131b04ebfa3b0374
+PYTHON_GRPCLIB_SITE = https://files.pythonhosted.org/packages/5b/28/5a2c299ec82a876a252c5919aa895a6f1d1d35c96417c5ce4a4660dc3a80
 PYTHON_GRPCLIB_SETUP_TYPE = setuptools
 PYTHON_GRPCLIB_LICENSE = BSD-3-Clause
 PYTHON_GRPCLIB_LICENSE_FILES = LICENSE.txt
