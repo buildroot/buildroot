094ed4d09e193f4f91688a969042225609a621fd
Bernd Kuhls <bernd@kuhls.net>
2025-12-21 23:34:37 +0100

    package/python-pyinotify: update tarball URL
    Updated package as computed by scanpypi.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit d2fbbecf94d02a9ebb284b60bd243e0f9452c0e8)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/python-pyinotify/python-pyinotify.mk
+++ package/python-pyinotify/python-pyinotify.mk
@@ -5,7 +5,8 @@
 ################################################################################
 
 PYTHON_PYINOTIFY_VERSION = 0.9.6
-PYTHON_PYINOTIFY_SITE = $(call github,seb-m,pyinotify,$(PYTHON_PYINOTIFY_VERSION))
+PYTHON_PYINOTIFY_SOURCE = pyinotify-$(PYTHON_PYINOTIFY_VERSION).tar.gz
+PYTHON_PYINOTIFY_SITE = https://files.pythonhosted.org/packages/e3/c0/fd5b18dde17c1249658521f69598f3252f11d9d7a980c5be8619970646e1
 PYTHON_PYINOTIFY_SETUP_TYPE = setuptools
 PYTHON_PYINOTIFY_LICENSE = MIT
 PYTHON_PYINOTIFY_LICENSE_FILES = COPYING
