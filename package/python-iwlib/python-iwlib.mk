b5e737e5753fe0a87f329b125a96f32ffa36d8e5
Bernd Kuhls <bernd@kuhls.net>
2025-12-21 23:29:54 +0100

    package/python-iwlib: bump version to 1.7.0
    https://github.com/nhoad/python-iwlib/blob/1.7.0/README.rst
    "1.6: Added support for Python 3 (via migration to CFFI)"
    
    Switched SITE to pypi.
    
    Added dependency to python-cffi:
    https://github.com/nhoad/python-iwlib/commit/a7c14aff76a28004afb45e08bcf83be5948cb7c1
    
    Updated package as computed by scanpypi.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-iwlib/python-iwlib.mk
+++ package/python-iwlib/python-iwlib.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_IWLIB_VERSION = 1.5
-PYTHON_IWLIB_SITE = $(call github,nhoad,python-iwlib,$(PYTHON_IWLIB_VERSION))
+PYTHON_IWLIB_VERSION = 1.7.0
+PYTHON_IWLIB_SOURCE = iwlib-$(PYTHON_IWLIB_VERSION).tar.gz
+PYTHON_IWLIB_SITE = https://files.pythonhosted.org/packages/59/44/fd72c0a7094baeb448dc9e87b3d579da98e2b8593c3fe05c5f9dd20dc6bc
 PYTHON_IWLIB_LICENSE = GPL-2.0
 PYTHON_IWLIB_LICENSE_FILES = COPYING
 PYTHON_IWLIB_SETUP_TYPE = setuptools
-PYTHON_IWLIB_DEPENDENCIES = wireless_tools
+PYTHON_IWLIB_DEPENDENCIES = host-python-cffi wireless_tools
 
 $(eval $(python-package))
