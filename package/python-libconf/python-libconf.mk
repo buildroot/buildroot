432ba70009252693bac2c7b58d3449a1f9825e1e
Bernd Kuhls <bernd@kuhls.net>
2025-12-21 23:34:31 +0100

    package/python-libconf: update tarball URL
    Updated package as computed by scanpypi.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 5d76be1c041938acee9650cbb777f3880865f629)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/python-libconf/python-libconf.mk
+++ package/python-libconf/python-libconf.mk
@@ -5,7 +5,8 @@
 ################################################################################
 
 PYTHON_LIBCONF_VERSION = 2.0.1
-PYTHON_LIBCONF_SITE = $(call github,ChrisAichinger,python-libconf,$(PYTHON_LIBCONF_VERSION))
+PYTHON_LIBCONF_SOURCE = libconf-$(PYTHON_LIBCONF_VERSION).tar.gz
+PYTHON_LIBCONF_SITE = https://files.pythonhosted.org/packages/c8/41/592c24b42ec6e349629f2b6310ff31255b213bc696c0e4ad2ee1d2bbb68e
 PYTHON_LIBCONF_SETUP_TYPE = setuptools
 PYTHON_LIBCONF_LICENSE = MIT
 PYTHON_LIBCONF_LICENSE_FILES = LICENSE
