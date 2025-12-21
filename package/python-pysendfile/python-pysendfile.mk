2952d38125d2f8f0229d7daa450f7d961d3417e1
Bernd Kuhls <bernd@kuhls.net>
2025-12-21 23:34:40 +0100

    package/python-pysendfile: update tarball URL
    Updated package as computed by scanpypi.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-pysendfile/python-pysendfile.mk
+++ package/python-pysendfile/python-pysendfile.mk
@@ -5,7 +5,8 @@
 ################################################################################
 
 PYTHON_PYSENDFILE_VERSION = 2.0.1
-PYTHON_PYSENDFILE_SITE = $(call github,giampaolo,pysendfile,release-$(PYTHON_PYSENDFILE_VERSION))
+PYTHON_PYSENDFILE_SOURCE = pysendfile-$(PYTHON_PYSENDFILE_VERSION).tar.gz
+PYTHON_PYSENDFILE_SITE = https://files.pythonhosted.org/packages/cd/3f/4aa268afd0252f06b3b487c296a066a01ddd4222a46b7a3748599c8fc8c3
 PYTHON_PYSENDFILE_SETUP_TYPE = setuptools
 PYTHON_PYSENDFILE_LICENSE = MIT
 PYTHON_PYSENDFILE_LICENSE_FILES = LICENSE
