c8f90ae3935f51d73a0582ad8ffe29b519a3a617
Bernd Kuhls <bernd@kuhls.net>
2025-12-21 23:34:20 +0100

    package/python-colorzero: switch SITE to pypi.org
    Updated package as computed by scanpypi.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 6ac7c62cce1679a648502dfcab4ac52e4c0b0b41)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/python-colorzero/python-colorzero.mk
+++ package/python-colorzero/python-colorzero.mk
@@ -5,7 +5,8 @@
 ################################################################################
 
 PYTHON_COLORZERO_VERSION = 2.0
-PYTHON_COLORZERO_SITE = $(call github,waveform80,colorzero,release-$(PYTHON_COLORZERO_VERSION))
+PYTHON_COLORZERO_SOURCE = colorzero-$(PYTHON_COLORZERO_VERSION).tar.gz
+PYTHON_COLORZERO_SITE = https://files.pythonhosted.org/packages/b3/ca/688824a06e8c4d04c7d2fd2af2d8da27bed51af20ee5f094154e1d680334
 PYTHON_COLORZERO_LICENSE = BSD-3-Clause
 PYTHON_COLORZERO_LICENSE_FILES = LICENSE.txt
 PYTHON_COLORZERO_SETUP_TYPE = setuptools
