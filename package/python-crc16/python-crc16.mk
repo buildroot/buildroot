410fc5866602f73d97f95dd504472f521dd4c829
Bernd Kuhls <bernd@kuhls.net>
2025-12-21 23:34:21 +0100

    package/python-crc16: switch SITE to pypi.org
    Updated package as computed by scanpypi.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 4adcc7cac31c71f44d50c3db39db8b19c2b41f0d)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/python-crc16/python-crc16.mk
+++ package/python-crc16/python-crc16.mk
@@ -5,7 +5,8 @@
 ################################################################################
 
 PYTHON_CRC16_VERSION = 0.1.1
-PYTHON_CRC16_SITE = $(call github,gennady,pycrc16,v$(PYTHON_CRC16_VERSION))
+PYTHON_CRC16_SOURCE = crc16-$(PYTHON_CRC16_VERSION).tar.gz
+PYTHON_CRC16_SITE = https://files.pythonhosted.org/packages/a6/e0/70a44c4385f2b33df82e518005aae16b5c1feaf082c73c0acebe3426fc0a
 PYTHON_CRC16_LICENSE = LGPL-3.0+
 PYTHON_CRC16_LICENSE_FILES = COPYING.txt
 PYTHON_CRC16_SETUP_TYPE = setuptools
