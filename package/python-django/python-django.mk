be2c536db91981a745897ffa97c88dc3e561c203
Marcus Hoffmann <buildroot@bubu1.eu>
2025-12-22 15:54:27 +0100

    package/python-django: bump to 6.0
    Release Notes: https://docs.djangoproject.com/en/6.0/releases/6.0/
    
    A few changes in the license files and an actual (sub)-license update:
    
    * django/contrib/gis/measure.py -> formatting change
    * django/contrib/admin/static/admin/img -> svg files got uupdated and
      the new ones are licensed under CC-BY-4.0; separate LICENSE file got
      removed, License is now mentioned in the readme
    * django/utils/archive.py -> File got updated, license (which is only
      the header) did not change.
    
    Remove the comment about django site having an inconvenient download
    URL. We download pretty much all python package from pypi.org, so doing
    that for django shouldn't need a separate comment.
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,11 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 5.2.9
+PYTHON_DJANGO_VERSION = 6.0
 PYTHON_DJANGO_SOURCE = django-$(PYTHON_DJANGO_VERSION).tar.gz
-# The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/eb/1c/188ce85ee380f714b704283013434976df8d3a2df8e735221a02605b6794
-PYTHON_DJANGO_LICENSE = BSD-3-Clause, MIT (jquery, utils/archive.py), BSD-2-Clause (inlines.js)
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/15/75/19762bfc4ea556c303d9af8e36f0cd910ab17dff6c8774644314427a2120
+PYTHON_DJANGO_LICENSE = BSD-3-Clause, MIT (jquery, utils/archive.py), BSD-2-Clause (inlines.js), CC-BY-4.0 (admin svg files)
 PYTHON_DJANGO_LICENSE_FILES = LICENSE \
 	django/contrib/gis/measure.py \
 	django/contrib/gis/gdal/LICENSE \
@@ -17,7 +16,7 @@ PYTHON_DJANGO_LICENSE_FILES = LICENSE \
 	django/contrib/admin/static/admin/js/vendor/jquery/LICENSE.txt \
 	django/contrib/admin/static/admin/js/vendor/select2/LICENSE.md \
 	django/contrib/admin/static/admin/js/vendor/xregexp/LICENSE.txt \
-	django/contrib/admin/static/admin/img/LICENSE \
+	django/contrib/admin/static/admin/img/README.md \
 	django/dispatch/license.txt \
 	django/utils/archive.py
 PYTHON_DJANGO_CPE_ID_VENDOR = djangoproject
