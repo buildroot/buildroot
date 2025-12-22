3b7f9adda2562e4dde73402f6691d39c76b4e284
Marcus Hoffmann <buildroot@bubu1.eu>
2025-12-22 13:52:08 +0100

    package/python-apscheduler: bump to 3.11.2
    Changes:
    https://github.com/agronholm/apscheduler/releases/tag/3.11.2
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-apscheduler/python-apscheduler.mk
+++ package/python-apscheduler/python-apscheduler.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_APSCHEDULER_VERSION = 3.11.1
+PYTHON_APSCHEDULER_VERSION = 3.11.2
 PYTHON_APSCHEDULER_SOURCE = apscheduler-$(PYTHON_APSCHEDULER_VERSION).tar.gz
-PYTHON_APSCHEDULER_SITE = https://files.pythonhosted.org/packages/d0/81/192db4f8471de5bc1f0d098783decffb1e6e69c4f8b4bc6711094691950b
+PYTHON_APSCHEDULER_SITE = https://files.pythonhosted.org/packages/07/12/3e4389e5920b4c1763390c6d371162f3784f86f85cd6d6c1bfe68eef14e2
 PYTHON_APSCHEDULER_SETUP_TYPE = setuptools
 PYTHON_APSCHEDULER_DEPENDENCIES = host-python-setuptools-scm
 PYTHON_APSCHEDULER_LICENSE = MIT
