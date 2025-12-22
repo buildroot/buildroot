db6f933571be09b0da3c73e01231d114a4e78a9f
Marcus Hoffmann <buildroot@bubu1.eu>
2025-12-22 13:54:22 +0100

    package/python-sqlparse: bump to 0.5.5
    Changes:
    https://sqlparse.readthedocs.io/en/latest/changes.html#release-0-5-5-dec-19-2025
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-sqlparse/python-sqlparse.mk
+++ package/python-sqlparse/python-sqlparse.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SQLPARSE_VERSION = 0.5.4
+PYTHON_SQLPARSE_VERSION = 0.5.5
 PYTHON_SQLPARSE_SOURCE = sqlparse-$(PYTHON_SQLPARSE_VERSION).tar.gz
-PYTHON_SQLPARSE_SITE = https://files.pythonhosted.org/packages/18/67/701f86b28d63b2086de47c942eccf8ca2208b3be69715a1119a4e384415a
+PYTHON_SQLPARSE_SITE = https://files.pythonhosted.org/packages/90/76/437d71068094df0726366574cf3432a4ed754217b436eb7429415cf2d480
 PYTHON_SQLPARSE_SETUP_TYPE = hatch
 PYTHON_SQLPARSE_LICENSE = BSD-3-Clause
 PYTHON_SQLPARSE_LICENSE_FILES = LICENSE
