4b31541a840cdcd6d44b8243d535e5d4c93a0c27
Maxime Leroy <maxime@leroys.fr>
2025-12-22 22:56:34 +0100

    package/grout: bump to 0.14.3 version
    See the release note of the new versions:
    https://github.com/DPDK/grout/releases/tag/v0.14.3
    
    Signed-off-by: Maxime Leroy <maxime@leroys.fr>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/grout/grout.mk
+++ package/grout/grout.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GROUT_VERSION = 0.14.2
+GROUT_VERSION = 0.14.3
 GROUT_SITE = $(call github,DPDK,grout,v$(GROUT_VERSION))
 GROUT_LICENSE = BSD-3-Clause
 GROUT_LICENSE_FILES = licenses/BSD-3-clause.txt
