de761fa21cc737292655f7c57f1cca382367e2b4
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2025-12-26 18:14:47 +0100

    package/opencv3: drop support for ffmpeg
    opencv3's code is not compatible with newer versions of ffmpeg, and
    opencv3 is no longer maintained, so we have no choice but to disable
    its ffmpeg support.
    
    Fixes:
    
      https://autobuild.buildroot.net/results/9ae3911583cccb6362f33cd82e5eaafb059fdc76/
    
    It's not clear which ffmpeg version bump broken the build exactly, but
    this issue is definitely present in 2025.02.x as the following
    defconfig fails to build in a similar way on 2025.02.x:
    
     BR2_aarch64=y
     BR2_TOOLCHAIN_EXTERNAL=y
     BR2_TOOLCHAIN_EXTERNAL_BOOTLIN=y
     BR2_TOOLCHAIN_EXTERNAL_BOOTLIN_AARCH64_GLIBC_STABLE=y
     BR2_PACKAGE_FFMPEG_NONFREE=y
     # BR2_PACKAGE_FFMPEG_FFMPEG is not set
     # BR2_PACKAGE_FFMPEG_INDEVS is not set
     # BR2_PACKAGE_FFMPEG_OUTDEVS is not set
     BR2_PACKAGE_OPENCV3=y
     BR2_PACKAGE_OPENCV3_LIB_VIDEOIO=y
     BR2_PACKAGE_OPENCV3_BUILD_PERF_TESTS=y
     BR2_PACKAGE_OPENCV3_WITH_FFMPEG=y
     BR2_PACKAGE_OPENCV3_INSTALL_DATA=y
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit ef538cf4d9fde8e1e92364906a81310e321703d2)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/ffmpeg/ffmpeg.mk
+++ package/ffmpeg/ffmpeg.mk
@@ -297,11 +297,11 @@ else
 FFMPEG_CONF_OPTS += --disable-mmal --disable-omx --disable-omx-rpi
 endif
 
-# To avoid a circular dependency only use opencv if opencv itself does
-# not depend on ffmpeg.
-ifeq ($(BR2_PACKAGE_OPENCV3_LIB_IMGPROC)x$(BR2_PACKAGE_OPENCV3_WITH_FFMPEG),yx)
+ifeq ($(BR2_PACKAGE_OPENCV3_LIB_IMGPROC),y)
 FFMPEG_CONF_OPTS += --enable-libopencv
 FFMPEG_DEPENDENCIES += opencv3
+# To avoid a circular dependency only use opencv if opencv itself does
+# not depend on ffmpeg.
 else
 FFMPEG_CONF_OPTS += --disable-libopencv
 endif
