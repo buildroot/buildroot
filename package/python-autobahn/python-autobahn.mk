601e3ae5bdb4ba5864a2e28db77e33b1a9edfa54
James Hilliard <james.hilliard1@gmail.com>
2025-12-28 16:43:50 -0700

    package/python-autobahn: bump to version 25.12.2
    Migrate from setuptools to hatch build backend.
    
    License hash changed due to formatting changes:
    https://github.com/crossbario/autobahn-python/commit/83d985db646fb045e0b26a9075baeec45633e281
    
    Drop no longer supported AUTOBAHN_STRIP_XBR env variable.
    
    Add host-python-setuptools build dependency.
    
    Add new python-base58 encryption dependency.
    
    Add new python-ecdsa encryption dependency.
    
    Serialization dependencies moved to mandatory dependencies.
    
    Propagate new C++ reverse dependency.
    
    Add new python-brotli compression dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-autobahn/python-autobahn.mk
+++ package/python-autobahn/python-autobahn.mk
@@ -4,15 +4,14 @@
 #
 ################################################################################
 
-PYTHON_AUTOBAHN_VERSION = 25.11.1
+PYTHON_AUTOBAHN_VERSION = 25.12.2
 PYTHON_AUTOBAHN_SOURCE = autobahn-$(PYTHON_AUTOBAHN_VERSION).tar.gz
-PYTHON_AUTOBAHN_SITE = https://files.pythonhosted.org/packages/0e/3a/aab5632fbd88ed26d330ab156af80c9a90419dfa2841296fd556a65e5fac
+PYTHON_AUTOBAHN_SITE = https://files.pythonhosted.org/packages/54/d5/9adf0f5b9eb244e58e898e9f3db4b00c09835ef4b6c37d491886e0376b4f
 PYTHON_AUTOBAHN_LICENSE = MIT
 PYTHON_AUTOBAHN_LICENSE_FILES = LICENSE
 PYTHON_AUTOBAHN_CPE_ID_VENDOR = crossbar
 PYTHON_AUTOBAHN_CPE_ID_PRODUCT = autobahn
-PYTHON_AUTOBAHN_SETUP_TYPE = setuptools
-PYTHON_AUTOBAHN_DEPENDENCIES = host-python-cffi
-PYTHON_AUTOBAHN_ENV = AUTOBAHN_STRIP_XBR=1
+PYTHON_AUTOBAHN_SETUP_TYPE = hatch
+PYTHON_AUTOBAHN_DEPENDENCIES = host-python-cffi host-python-setuptools
 
 $(eval $(python-package))
