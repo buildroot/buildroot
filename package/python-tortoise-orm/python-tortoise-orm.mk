c68927229646820dbd0d2a5653811662623a5ae1
James Hilliard <james.hilliard1@gmail.com>
2025-12-28 15:20:27 -0700

    package/python-tortoise-orm: bump to version 0.25.3
    Migrate from poetry to pdm pep517 build backend.
    
    Add new python-anyio runtime dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-tortoise-orm/python-tortoise-orm.mk
+++ package/python-tortoise-orm/python-tortoise-orm.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_TORTOISE_ORM_VERSION = 0.25.1
+PYTHON_TORTOISE_ORM_VERSION = 0.25.3
 PYTHON_TORTOISE_ORM_SOURCE = tortoise_orm-$(PYTHON_TORTOISE_ORM_VERSION).tar.gz
-PYTHON_TORTOISE_ORM_SITE = https://files.pythonhosted.org/packages/d7/9b/de966810021fa773fead258efd8deea2bb73bb12479e27f288bd8ceb8763
-PYTHON_TORTOISE_ORM_SETUP_TYPE = poetry
+PYTHON_TORTOISE_ORM_SITE = https://files.pythonhosted.org/packages/dd/be/0fdfcd8295f4f02a280f33bdda99c3edba450f9a212149232f2fbb6afa96
+PYTHON_TORTOISE_ORM_SETUP_TYPE = pep517
 PYTHON_TORTOISE_ORM_LICENSE = Apache-2.0
 PYTHON_TORTOISE_ORM_LICENSE_FILES = LICENSE.txt
+PYTHON_TORTOISE_ORM_DEPENDENCIES = host-python-pdm-backend
 
 $(eval $(python-package))
