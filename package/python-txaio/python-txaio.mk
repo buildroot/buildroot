37015b15cdc8ef57fca73f84d966a0da72f26570
James Hilliard <james.hilliard1@gmail.com>
2025-12-28 15:38:05 -0700

    package/python-txaio: bump to version 25.12.2
    Migrate from setuptools to hatch build backend.
    
    License hash changed due to formatting changes:
    https://github.com/crossbario/txaio/commit/9d1cb8a83964cb52643df4a105dc368c9c4f09ea
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-txaio/python-txaio.mk
+++ package/python-txaio/python-txaio.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_TXAIO_VERSION = 23.1.1
+PYTHON_TXAIO_VERSION = 25.12.2
 PYTHON_TXAIO_SOURCE = txaio-$(PYTHON_TXAIO_VERSION).tar.gz
-PYTHON_TXAIO_SITE = https://files.pythonhosted.org/packages/51/91/bc9fd5aa84703f874dea27313b11fde505d343f3ef3ad702bddbe20bfd6e
+PYTHON_TXAIO_SITE = https://files.pythonhosted.org/packages/7f/67/ea9c9ddbaa3e0b4d53c91f8778a33e42045be352dc7200ed2b9aaa7dc229
 PYTHON_TXAIO_LICENSE = MIT
 PYTHON_TXAIO_LICENSE_FILES = LICENSE
-PYTHON_TXAIO_SETUP_TYPE = setuptools
+PYTHON_TXAIO_SETUP_TYPE = hatch
 
 $(eval $(python-package))
