39ccf809ee9dc176c9350c0a9406a8f9625e4bc6
James Hilliard <james.hilliard1@gmail.com>
2025-12-31 14:31:40 -0700

    package/python-pysnmp: bump to version 7.1.22
    Migrate from poetry to flit build backend.
    
    License hash changed due to year update:
    https://github.com/lextudio/pysnmp/commit/3a8cbfe6d22f7d159ce128058bc5d901a675bf6d
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-pysnmp/python-pysnmp.mk
+++ package/python-pysnmp/python-pysnmp.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_PYSNMP_VERSION = 7.1.16
+PYTHON_PYSNMP_VERSION = 7.1.22
 PYTHON_PYSNMP_SOURCE = pysnmp-$(PYTHON_PYSNMP_VERSION).tar.gz
-PYTHON_PYSNMP_SITE = https://files.pythonhosted.org/packages/a2/b1/c94dac49a002eed96e2cb7aeb1d148b1c98826e5cd1f6e8eebb4a20f8d49
-PYTHON_PYSNMP_SETUP_TYPE = poetry
+PYTHON_PYSNMP_SITE = https://files.pythonhosted.org/packages/c5/f7/63a4833b675f3f85296d85f2fddaed93d76799f29a813a2d5ca2bbe7fc50
+PYTHON_PYSNMP_SETUP_TYPE = flit
 PYTHON_PYSNMP_LICENSE = BSD-2-Clause
 PYTHON_PYSNMP_LICENSE_FILES = LICENSE.rst
 
