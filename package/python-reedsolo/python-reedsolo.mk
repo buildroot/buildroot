020b8a7a5567b622481b2de473f9ad8ab6092eff
James Hilliard <james.hilliard1@gmail.com>
2025-12-31 14:09:31 -0700

    package/python-reedsolo: bump to version 2.0.13
    Enable optimized cython build.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-reedsolo/python-reedsolo.mk
+++ package/python-reedsolo/python-reedsolo.mk
@@ -4,11 +4,13 @@
 #
 ################################################################################
 
-PYTHON_REEDSOLO_VERSION = 1.7.0
+PYTHON_REEDSOLO_VERSION = 2.0.13
 PYTHON_REEDSOLO_SOURCE = reedsolo-$(PYTHON_REEDSOLO_VERSION).tar.gz
-PYTHON_REEDSOLO_SITE = https://files.pythonhosted.org/packages/f7/61/a67338cbecf370d464e71b10e9a31355f909d6937c3a8d6b17dd5d5beb5e
+PYTHON_REEDSOLO_SITE = https://files.pythonhosted.org/packages/57/5b/ead15a5d182553f9d7832851a954b7f0d662d710972b0d1c479fd11a3ebd
 PYTHON_REEDSOLO_SETUP_TYPE = setuptools
 PYTHON_REEDSOLO_LICENSE = MIT-0, Unlicense
 PYTHON_REEDSOLO_LICENSE_FILES = LICENSE
+PYTHON_REEDSOLO_DEPENDENCIES = host-python-cython
+PYTHON_REEDSOLO_BUILD_OPTS = -C--build-option=--cythonize
 
 $(eval $(python-package))
