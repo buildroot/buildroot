902e711f105f335da2c92d4578b81640f835c077
James Hilliard <james.hilliard1@gmail.com>
2025-12-31 14:52:09 -0700

    package/python-pyparted: bump to version 3.13.0
    License file renamed with no content changes:
    https://github.com/dcantrell/pyparted/commit/78876337d9e0037fc8034f839c2b918a99d05e3b
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-pyparted/python-pyparted.mk
+++ package/python-pyparted/python-pyparted.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-PYTHON_PYPARTED_VERSION = 3.12.0
+PYTHON_PYPARTED_VERSION = 3.13.0
 PYTHON_PYPARTED_SOURCE = pyparted-$(PYTHON_PYPARTED_VERSION).tar.gz
-PYTHON_PYPARTED_SITE = https://files.pythonhosted.org/packages/c2/d0/d32aa5758d6567eef620075f5c84f475c93bb1bf8da9d17051ce3ef055db
+PYTHON_PYPARTED_SITE = https://files.pythonhosted.org/packages/e7/53/b02306f89dc3eaf949659e8814545cde575867bc0175f60361809af7f71a
 PYTHON_PYPARTED_SETUP_TYPE = setuptools
 PYTHON_PYPARTED_LICENSE = GPL-2.0+
-PYTHON_PYPARTED_LICENSE_FILES = COPYING
+PYTHON_PYPARTED_LICENSE_FILES = LICENSE
 PYTHON_PYPARTED_DEPENDENCIES = parted
 
 $(eval $(python-package))
