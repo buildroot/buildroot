2e16d67445fc12267f8e1634320998586a449eeb
James Hilliard <james.hilliard1@gmail.com>
2025-12-31 12:06:24 -0700

    package/python-tempora: bump to version 5.8.1
    License hash changed due to move to license generator migration:
    https://github.com/jaraco/tempora/commit/9a81db3c77bc106017dcd4b0853a5a94f43ae33c
    https://github.com/jaraco/tempora/commit/d2b8d7750f78e870def98c4e04053af4acc86e29
    
    Add new host-python-coherent-licensed build dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-tempora/python-tempora.mk
+++ package/python-tempora/python-tempora.mk
@@ -4,12 +4,14 @@
 #
 ################################################################################
 
-PYTHON_TEMPORA_VERSION = 5.8.0
+PYTHON_TEMPORA_VERSION = 5.8.1
 PYTHON_TEMPORA_SOURCE = tempora-$(PYTHON_TEMPORA_VERSION).tar.gz
-PYTHON_TEMPORA_SITE = https://files.pythonhosted.org/packages/c3/f2/bbd6d2178791eaf8bac06857cb0ee39840e69f7b64ecb0c414bf8f46daff
+PYTHON_TEMPORA_SITE = https://files.pythonhosted.org/packages/24/64/a255efe5edd367d12b770b3514194efdc1c97e5ed6ce6e8105d834750dfc
 PYTHON_TEMPORA_LICENSE = MIT
 PYTHON_TEMPORA_LICENSE_FILES = LICENSE
 PYTHON_TEMPORA_SETUP_TYPE = setuptools
-PYTHON_TEMPORA_DEPENDENCIES = host-python-setuptools-scm
+PYTHON_TEMPORA_DEPENDENCIES = \
+	host-python-coherent-licensed \
+	host-python-setuptools-scm
 
 $(eval $(python-package))
