8f7f912ead49893cc5f26f0885bc7d89387addd9
James Hilliard <james.hilliard1@gmail.com>
2025-12-31 15:05:05 -0700

    package/python-pycares: bump to version 5.0.0
    Add dependency on c-ares and set env variable to ensure it's used.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-pycares/python-pycares.mk
+++ package/python-pycares/python-pycares.mk
@@ -4,12 +4,13 @@
 #
 ################################################################################
 
-PYTHON_PYCARES_VERSION = 4.11.0
+PYTHON_PYCARES_VERSION = 5.0.0
 PYTHON_PYCARES_SOURCE = pycares-$(PYTHON_PYCARES_VERSION).tar.gz
-PYTHON_PYCARES_SITE = https://files.pythonhosted.org/packages/8d/ad/9d1e96486d2eb5a2672c4d9a2dd372d015b8d7a332c6ac2722c4c8e6bbbf
+PYTHON_PYCARES_SITE = https://files.pythonhosted.org/packages/59/5d/b50bdc30026f350f1c4b0c4fb6b7543598f2bcc370308c5148c7019d9886
 PYTHON_PYCARES_SETUP_TYPE = setuptools
 PYTHON_PYCARES_LICENSE = MIT
 PYTHON_PYCARES_LICENSE_FILES = LICENSE
-PYTHON_PYCARES_DEPENDENCIES = host-python-cffi
+PYTHON_PYCARES_DEPENDENCIES = c-ares host-python-cffi
+PYTHON_PYCARES_ENV = PYCARES_USE_SYSTEM_LIB=1
 
 $(eval $(python-package))
