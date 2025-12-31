2d19a0b332dc17c18533b8d909986c235bc4648b
James Hilliard <james.hilliard1@gmail.com>
2025-12-31 16:11:22 -0700

    package/{python-}protobuf: bump to version 33.2
    We need to set -Dprotobuf_BUILD_LIBUPB=ON for host-protobuf now
    as some libupb headers now appear to be required.
    
    Also set the new -Dprotobuf_LOCAL_DEPENDENCIES_ONLY=ON option
    to ensure cmake doesn't try to download dependencies.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/protobuf/protobuf.mk
+++ package/protobuf/protobuf.mk
@@ -7,7 +7,7 @@
 # When bumping this package, make sure to also verify if the
 # python-protobuf package still works and to update its hash,
 # as they share the same version/site variables.
-PROTOBUF_VERSION = 29.3
+PROTOBUF_VERSION = 33.2
 PROTOBUF_SITE = https://github.com/protocolbuffers/protobuf/releases/download/v$(PROTOBUF_VERSION)
 PROTOBUF_LICENSE = BSD-3-Clause
 PROTOBUF_LICENSE_FILES = LICENSE
@@ -26,6 +26,7 @@ PROTOBUF_CONF_OPTS = \
 	-Dprotobuf_BUILD_TESTS=OFF \
 	-Dprotobuf_DISABLE_RTTI=OFF \
 	-Dprotobuf_INSTALL=ON \
+	-Dprotobuf_LOCAL_DEPENDENCIES_ONLY=ON \
 	-DWITH_PROTOC=$(HOST_DIR)/bin/protoc
 
 HOST_PROTOBUF_DEPENDENCIES = host-libabseil-cpp
@@ -34,12 +35,13 @@ HOST_PROTOBUF_CONF_OPTS = \
 	-Dprotobuf_ALLOW_CCACHE=ON \
 	-Dprotobuf_BUILD_CONFORMANCE=OFF \
 	-Dprotobuf_BUILD_LIBPROTOC=ON \
-	-Dprotobuf_BUILD_LIBUPB=OFF \
+	-Dprotobuf_BUILD_LIBUPB=ON \
 	-Dprotobuf_BUILD_PROTOBUF_BINARIES=ON \
 	-Dprotobuf_BUILD_PROTOC_BINARIES=ON \
 	-Dprotobuf_BUILD_TESTS=OFF \
 	-Dprotobuf_DISABLE_RTTI=OFF \
 	-Dprotobuf_INSTALL=ON \
+	-Dprotobuf_LOCAL_DEPENDENCIES_ONLY=ON \
 	-Dprotobuf_WITH_ZLIB=OFF
 
 ifeq ($(BR2_TOOLCHAIN_HAS_LIBATOMIC),y)
--- package/python-protobuf/python-protobuf.mk
+++ package/python-protobuf/python-protobuf.mk
@@ -5,8 +5,8 @@
 ################################################################################
 
 PYTHON_PROTOBUF_VERSION = $(PROTOBUF_VERSION)
-PYTHON_PROTOBUF_SOURCE = protobuf-5.$(PYTHON_PROTOBUF_VERSION).tar.gz
-PYTHON_PROTOBUF_SITE = https://files.pythonhosted.org/packages/f7/d1/e0a911544ca9993e0f17ce6d3cc0932752356c1b0a834397f28e63479344
+PYTHON_PROTOBUF_SOURCE = protobuf-6.$(PYTHON_PROTOBUF_VERSION).tar.gz
+PYTHON_PROTOBUF_SITE = https://files.pythonhosted.org/packages/34/44/e49ecff446afeec9d1a66d6bbf9adc21e3c7cea7803a920ca3773379d4f6
 PYTHON_PROTOBUF_LICENSE = BSD-3-Clause
 PYTHON_PROTOBUF_LICENSE_FILES = LICENSE
 PYTHON_PROTOBUF_DEPENDENCIES = host-protobuf
