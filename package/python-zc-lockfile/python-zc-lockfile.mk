9e0f9b5350f0389115c7d0b8affaa0f553164651
James Hilliard <james.hilliard1@gmail.com>
2025-12-31 11:28:21 -0700

    package/python-zc-lockfile: bump to version 4.0
    Skip dependency check due to unnecessarily strict setuptools version
    restriction.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-zc-lockfile/python-zc-lockfile.mk
+++ package/python-zc-lockfile/python-zc-lockfile.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_ZC_LOCKFILE_VERSION = 3.0.post1
-PYTHON_ZC_LOCKFILE_SOURCE = zc.lockfile-$(PYTHON_ZC_LOCKFILE_VERSION).tar.gz
-PYTHON_ZC_LOCKFILE_SITE = https://files.pythonhosted.org/packages/5b/83/a5432aa08312fc834ea594473385c005525e6a80d768a2ad246e78877afd
+PYTHON_ZC_LOCKFILE_VERSION = 4.0
+PYTHON_ZC_LOCKFILE_SOURCE = zc_lockfile-$(PYTHON_ZC_LOCKFILE_VERSION).tar.gz
+PYTHON_ZC_LOCKFILE_SITE = https://files.pythonhosted.org/packages/10/9a/2fef89272d98b799e4daa50201c5582ec76bdd4e92a1a7e3deb74c52b7fa
 PYTHON_ZC_LOCKFILE_SETUP_TYPE = setuptools
 PYTHON_ZC_LOCKFILE_LICENSE = ZPL-2.1
 PYTHON_ZC_LOCKFILE_LICENSE_FILES = LICENSE.txt
+PYTHON_ZC_LOCKFILE_BUILD_OPTS = --skip-dependency-check
 
 $(eval $(python-package))
