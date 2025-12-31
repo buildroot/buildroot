e4a5d35a5f0b35b8f2dd11e5aa4de6411b414e77
James Hilliard <james.hilliard1@gmail.com>
2025-12-31 14:35:03 -0700

    package/python-pysmi: bump to version 1.6.2
    Migrate from poetry to flit build backend.
    
    License hash changed due to year update:
    https://github.com/lextudio/pysmi/commit/fdd7fe3d201b1d451eaecb858354a0e1bf9fd04f
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-pysmi/python-pysmi.mk
+++ package/python-pysmi/python-pysmi.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_PYSMI_VERSION = 1.5.9
+PYTHON_PYSMI_VERSION = 1.6.2
 PYTHON_PYSMI_SOURCE = pysmi-$(PYTHON_PYSMI_VERSION).tar.gz
-PYTHON_PYSMI_SITE = https://files.pythonhosted.org/packages/be/b0/19085322fc24f6b4a7bbc151643641c391b8b8d9aa4fd0e40d8178b2a5f5
-PYTHON_PYSMI_SETUP_TYPE = poetry
+PYTHON_PYSMI_SITE = https://files.pythonhosted.org/packages/a7/80/917111aaa4bf931bbd4d9a6c89864de2304b79cba8ce49639a6440ecd5ba
+PYTHON_PYSMI_SETUP_TYPE = flit
 PYTHON_PYSMI_LICENSE = BSD-2-Clause
 PYTHON_PYSMI_LICENSE_FILES = LICENSE.rst
 
