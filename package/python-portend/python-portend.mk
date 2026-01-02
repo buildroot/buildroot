6b12ef59c57c30f945b4606be41df5e7fe31764b
James Hilliard <james.hilliard1@gmail.com>
2026-01-02 13:47:41 -0700

    package/python-portend: bump to version 3.2.1
    License hash changed due to move to license file autogeneration:
    https://github.com/jaraco/portend/commit/9a81db3c77bc106017dcd4b0853a5a94f43ae33c
    https://github.com/jaraco/portend/commit/d2b8d7750f78e870def98c4e04053af4acc86e29
    
    Add new python-coherent-licensed build dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-portend/python-portend.mk
+++ package/python-portend/python-portend.mk
@@ -4,12 +4,14 @@
 #
 ################################################################################
 
-PYTHON_PORTEND_VERSION = 3.2.0
+PYTHON_PORTEND_VERSION = 3.2.1
 PYTHON_PORTEND_SOURCE = portend-$(PYTHON_PORTEND_VERSION).tar.gz
-PYTHON_PORTEND_SITE = https://files.pythonhosted.org/packages/8f/fc/bcfc768996b438d6e4bde7a6c8cfd62089847b0f5381a0e0ec2d8ee6b202
+PYTHON_PORTEND_SITE = https://files.pythonhosted.org/packages/b7/57/be90f42996fc4f57d5742ef2c95f7f7bb8e9183af2cc11bff8e7df338888
 PYTHON_PORTEND_LICENSE = MIT
 PYTHON_PORTEND_LICENSE_FILES = LICENSE
 PYTHON_PORTEND_SETUP_TYPE = setuptools
-PYTHON_PORTEND_DEPENDENCIES = host-python-setuptools-scm
+PYTHON_PORTEND_DEPENDENCIES = \
+	host-python-coherent-licensed \
+	host-python-setuptools-scm
 
 $(eval $(python-package))
