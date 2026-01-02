71a35f9ca7353517d7446250d9b5d4159ab4a570
James Hilliard <james.hilliard1@gmail.com>
2026-01-02 15:48:20 -0700

    package/python-jaraco-collections: bump to version 5.2.1
    License hash changed due to move to license file autogeneration:
    https://github.com/jaraco/jaraco.collections/commit/9a81db3c77bc106017dcd4b0853a5a94f43ae33c
    https://github.com/jaraco/jaraco.collections/commit/d2b8d7750f78e870def98c4e04053af4acc86e29
    
    Add new python-coherent-licensed build dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-jaraco-collections/python-jaraco-collections.mk
+++ package/python-jaraco-collections/python-jaraco-collections.mk
@@ -4,12 +4,14 @@
 #
 ################################################################################
 
-PYTHON_JARACO_COLLECTIONS_VERSION = 5.1.0
+PYTHON_JARACO_COLLECTIONS_VERSION = 5.2.1
 PYTHON_JARACO_COLLECTIONS_SOURCE = jaraco_collections-$(PYTHON_JARACO_COLLECTIONS_VERSION).tar.gz
-PYTHON_JARACO_COLLECTIONS_SITE = https://files.pythonhosted.org/packages/8c/ed/3f0ef2bcf765b5a3d58ecad8d825874a3af1e792fa89f89ad79f090a4ccc
+PYTHON_JARACO_COLLECTIONS_SITE = https://files.pythonhosted.org/packages/fa/d2/751000cf702676dbb78f97728f4d52b029e817e2b3c94088dfe5c70ff46d
 PYTHON_JARACO_COLLECTIONS_SETUP_TYPE = setuptools
 PYTHON_JARACO_COLLECTIONS_LICENSE = MIT
 PYTHON_JARACO_COLLECTIONS_LICENSE_FILES = LICENSE
-PYTHON_JARACO_COLLECTIONS_DEPENDENCIES = host-python-setuptools-scm
+PYTHON_JARACO_COLLECTIONS_DEPENDENCIES = \
+	host-python-coherent-licensed \
+	host-python-setuptools-scm
 
 $(eval $(python-package))
