b403dfe54a178a8477b347a50f9614ebfcd2be4a
James Hilliard <james.hilliard1@gmail.com>
2026-01-02 15:35:54 -0700

    package/python-jaraco-functools: bump to version 4.4.0
    License hash changed due to move to license file autogeneration:
    https://github.com/jaraco/jaraco.functools/commit/9a81db3c77bc106017dcd4b0853a5a94f43ae33c
    https://github.com/jaraco/jaraco.functools/commit/d2b8d7750f78e870def98c4e04053af4acc86e29
    
    Add new python-coherent-licensed build dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-jaraco-functools/python-jaraco-functools.mk
+++ package/python-jaraco-functools/python-jaraco-functools.mk
@@ -4,12 +4,14 @@
 #
 ################################################################################
 
-PYTHON_JARACO_FUNCTOOLS_VERSION = 4.1.0
+PYTHON_JARACO_FUNCTOOLS_VERSION = 4.4.0
 PYTHON_JARACO_FUNCTOOLS_SOURCE = jaraco_functools-$(PYTHON_JARACO_FUNCTOOLS_VERSION).tar.gz
-PYTHON_JARACO_FUNCTOOLS_SITE = https://files.pythonhosted.org/packages/ab/23/9894b3df5d0a6eb44611c36aec777823fc2e07740dabbd0b810e19594013
+PYTHON_JARACO_FUNCTOOLS_SITE = https://files.pythonhosted.org/packages/0f/27/056e0638a86749374d6f57d0b0db39f29509cce9313cf91bdc0ac4d91084
 PYTHON_JARACO_FUNCTOOLS_LICENSE = MIT
 PYTHON_JARACO_FUNCTOOLS_LICENSE_FILES = LICENSE
 PYTHON_JARACO_FUNCTOOLS_SETUP_TYPE = setuptools
-PYTHON_JARACO_FUNCTOOLS_DEPENDENCIES = host-python-setuptools-scm
+PYTHON_JARACO_FUNCTOOLS_DEPENDENCIES = \
+	host-python-coherent-licensed \
+	host-python-setuptools-scm
 
 $(eval $(python-package))
