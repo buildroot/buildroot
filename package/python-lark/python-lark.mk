294783d757745c56227ab49329dde8351f5a3991
James Hilliard <james.hilliard1@gmail.com>
2026-01-02 15:14:25 -0700

    package/python-lark: bump to version 1.3.1
    Add new host-python-setuptools-scm build dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-lark/python-lark.mk
+++ package/python-lark/python-lark.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_LARK_VERSION = 1.3.0
+PYTHON_LARK_VERSION = 1.3.1
 PYTHON_LARK_SOURCE = lark-$(PYTHON_LARK_VERSION).tar.gz
-PYTHON_LARK_SITE = https://files.pythonhosted.org/packages/1d/37/a13baf0135f348af608c667633cbe5d13aa2c5c15a56ae9ad3e6cba45ae3
+PYTHON_LARK_SITE = https://files.pythonhosted.org/packages/da/34/28fff3ab31ccff1fd4f6c7c7b0ceb2b6968d8ea4950663eadcb5720591a0
 PYTHON_LARK_SETUP_TYPE = setuptools
 PYTHON_LARK_LICENSE = MIT
 PYTHON_LARK_LICENSE_FILES = LICENSE
+PYTHON_LARK_DEPENDENCIES = host-python-setuptools-scm
 
 $(eval $(python-package))
