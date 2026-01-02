56db14b886f4a673e6a94136238b9d648c883e54
Bernd Kuhls <bernd@kuhls.net>
2026-01-02 16:26:31 +0100

    package/python-scipy: bump version to 1.16.3
    Depends on python-numpy >= 2.0.0:
    https://github.com/scipy/scipy/blob/v1.16.3/pyproject.toml#L41
    https://github.com/scipy/scipy/commit/a3e2bb639b156b01964cda1edca971521c6f39ff
    
    Updated license hash due to copyright year bumps:
    https://github.com/scipy/scipy/commits/v1.16.3/LICENSE.txt
    
    qhull license file was moved upstream:
    https://github.com/scipy/scipy/commit/6d699dded4423e4ff9ac97eff9f4912ed24964fa
    https://github.com/scipy/scipy/commit/96423e5279a4cdf698eedea6f0795ab57cf5a860
    
    Updated numpy path in PYTHON_SCIPY_MESON_EXTRA_PROPERTIES following
    upstream commit:
    https://github.com/numpy/numpy/commit/923f219077c8aa3ad7d3792d8c06cba010b70252
    
    Test was successful:
    
    $ utils/docker-run support/testing/run-tests -j33    -k -d dl -o output_folder tests.package.test_python_scipy.TestPythonPy3SciPy
    09:59:22 TestPythonPy3SciPy                       Starting
    09:59:23 TestPythonPy3SciPy                       Building
    10:10:09 TestPythonPy3SciPy                       Building done
    Downloading to /home/bernd/buildroot/dl/tmpw1frnmf9
    Renaming from /home/bernd/buildroot/dl/tmpw1frnmf9 to /home/bernd/buildroot/dl/kernel-versatile-5.10.202
    Downloading to /home/bernd/buildroot/dl/tmpiip1lach
    Renaming from /home/bernd/buildroot/dl/tmpiip1lach to /home/bernd/buildroot/dl/versatile-pb-5.10.202.dtb
    10:10:24 TestPythonPy3SciPy                       Cleaning up
    .
    ----------------------------------------------------------------------
    Ran 1 test in 661.922s
    
    OK
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-scipy/python-scipy.mk
+++ package/python-scipy/python-scipy.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SCIPY_VERSION = 1.11.4
+PYTHON_SCIPY_VERSION = 1.16.3
 PYTHON_SCIPY_SOURCE = scipy-$(PYTHON_SCIPY_VERSION).tar.gz
-PYTHON_SCIPY_SITE = https://files.pythonhosted.org/packages/6e/1f/91144ba78dccea567a6466262922786ffc97be1e9b06ed9574ef0edc11e1
+PYTHON_SCIPY_SITE = https://files.pythonhosted.org/packages/0a/ca/d8ace4f98322d01abcd52d381134344bf7b431eba7ed8b42bdea5a3c2ac9
 PYTHON_SCIPY_LICENSE = \
 	BSD-3-Clause, \
 	BSD-2-Clause, \
@@ -20,7 +20,7 @@ PYTHON_SCIPY_LICENSE_FILES = \
 	scipy/optimize/tnc/LICENSE \
 	scipy/sparse/linalg/_dsolve/SuperLU/License.txt \
 	scipy/sparse/linalg/_eigen/arpack/ARPACK/COPYING \
-	scipy/spatial/qhull_src/COPYING.txt
+	scipy/spatial/COPYING_QHULL.txt
 PYTHON_SCIPY_CPE_ID_VENDOR = scipy
 PYTHON_SCIPY_CPE_ID_PRODUCT = scipy
 PYTHON_SCIPY_DEPENDENCIES += \
@@ -42,6 +42,6 @@ PYTHON_SCIPY_CONF_ENV += \
 PYTHON_SCIPY_CONF_OPTS = -Dblas=openblas -Dlapack=lapack
 
 PYTHON_SCIPY_MESON_EXTRA_PROPERTIES = \
-	numpy-include-dir='$(STAGING_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/numpy/core/include'
+	numpy-include-dir='$(STAGING_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/numpy/_core/include'
 
 $(eval $(meson-package))
