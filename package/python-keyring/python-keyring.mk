16edb2ad254ca5386a7de470248c70edbcfe40af
James Hilliard <james.hilliard1@gmail.com>
2026-01-02 15:29:20 -0700

    package/python-keyring: bump to version 25.7.0
    License hash changed due to move to license file autogeneration:
    https://github.com/jaraco/keyring/commit/9a81db3c77bc106017dcd4b0853a5a94f43ae33c
    https://github.com/jaraco/keyring/commit/d2b8d7750f78e870def98c4e04053af4acc86e29
    
    Add new python-coherent-licensed build dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-keyring/python-keyring.mk
+++ package/python-keyring/python-keyring.mk
@@ -4,14 +4,16 @@
 #
 ################################################################################
 
-PYTHON_KEYRING_VERSION = 25.6.0
+PYTHON_KEYRING_VERSION = 25.7.0
 PYTHON_KEYRING_SOURCE = keyring-$(PYTHON_KEYRING_VERSION).tar.gz
-PYTHON_KEYRING_SITE = https://files.pythonhosted.org/packages/70/09/d904a6e96f76ff214be59e7aa6ef7190008f52a0ab6689760a98de0bf37d
+PYTHON_KEYRING_SITE = https://files.pythonhosted.org/packages/43/4b/674af6ef2f97d56f0ab5153bf0bfa28ccb6c3ed4d1babf4305449668807b
 PYTHON_KEYRING_SETUP_TYPE = setuptools
 PYTHON_KEYRING_LICENSE = MIT
 PYTHON_KEYRING_LICENSE_FILES = LICENSE
 PYTHON_KEYRING_CPE_ID_VENDOR = python
 PYTHON_KEYRING_CPE_ID_PRODUCT = keyring
-PYTHON_KEYRING_DEPENDENCIES = host-python-setuptools-scm
+PYTHON_KEYRING_DEPENDENCIES = \
+	host-python-coherent-licensed \
+	host-python-setuptools-scm
 
 $(eval $(python-package))
