353012770406783a3ee7cf8d139f463d4f0e17b6
James Hilliard <james.hilliard1@gmail.com>
2026-01-02 15:22:38 -0700

    package/python-kiwisolver: bump to version 1.4.9
    Add new host-python-setuptools-scm build dependency.
    
    License hash changed due to year update:
    https://github.com/nucleic/kiwi/commit/728f5661fdd021c68dac1b914b3c8610a46b2e7e
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-kiwisolver/python-kiwisolver.mk
+++ package/python-kiwisolver/python-kiwisolver.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-PYTHON_KIWISOLVER_VERSION = 1.4.8
+PYTHON_KIWISOLVER_VERSION = 1.4.9
 PYTHON_KIWISOLVER_SOURCE = kiwisolver-$(PYTHON_KIWISOLVER_VERSION).tar.gz
-PYTHON_KIWISOLVER_SITE = https://files.pythonhosted.org/packages/82/59/7c91426a8ac292e1cdd53a63b6d9439abd573c875c3f92c146767dd33faf
+PYTHON_KIWISOLVER_SITE = https://files.pythonhosted.org/packages/5c/3c/85844f1b0feb11ee581ac23fe5fce65cd049a200c1446708cc1b7f922875
 PYTHON_KIWISOLVER_LICENSE = BSD-3-Clause
 PYTHON_KIWISOLVER_LICENSE_FILES = LICENSE
 PYTHON_KIWISOLVER_SETUP_TYPE = setuptools
-PYTHON_KIWISOLVER_DEPENDENCIES = host-python-cppy
+PYTHON_KIWISOLVER_DEPENDENCIES = host-python-cppy host-python-setuptools-scm
 
 $(eval $(python-package))
