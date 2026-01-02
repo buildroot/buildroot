1033e83bb79f13c52164b60f8dbfb8d3bf28d521
James Hilliard <james.hilliard1@gmail.com>
2026-01-02 13:57:46 -0700

    package/python-path: bump to version 17.1.1
    License hash changed due to move to license file autogeneration:
    https://github.com/jaraco/path/commit/9a81db3c77bc106017dcd4b0853a5a94f43ae33c
    https://github.com/jaraco/path/commit/d2b8d7750f78e870def98c4e04053af4acc86e29
    
    Add new python-coherent-licensed build dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-path/python-path.mk
+++ package/python-path/python-path.mk
@@ -4,12 +4,14 @@
 #
 ################################################################################
 
-PYTHON_PATH_VERSION = 17.1.0
+PYTHON_PATH_VERSION = 17.1.1
 PYTHON_PATH_SOURCE = path-$(PYTHON_PATH_VERSION).tar.gz
-PYTHON_PATH_SITE = https://files.pythonhosted.org/packages/ed/51/127cd9fa2baae5715e24839d6fb73c6fa6eca7b84a52fc7ce8195d830802
+PYTHON_PATH_SITE = https://files.pythonhosted.org/packages/dd/52/a7bdd5ef8488977d354b7915d1e75009bebbd04f73eff14e52372d5e9435
 PYTHON_PATH_SETUP_TYPE = setuptools
 PYTHON_PATH_LICENSE = MIT
 PYTHON_PATH_LICENSE_FILES = LICENSE
-PYTHON_PATH_DEPENDENCIES = host-python-setuptools-scm
+PYTHON_PATH_DEPENDENCIES = \
+	host-python-coherent-licensed \
+	host-python-setuptools-scm
 
 $(eval $(python-package))
