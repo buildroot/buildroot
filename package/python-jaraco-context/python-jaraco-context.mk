8ed41994e4aa47b0f37dacab19fe01c8ce27b042
James Hilliard <james.hilliard1@gmail.com>
2026-01-02 15:41:15 -0700

    package/python-jaraco-context: bump to version 6.0.2
    License hash changed due to move to license file autogeneration:
    https://github.com/jaraco/jaraco.context/commit/9a81db3c77bc106017dcd4b0853a5a94f43ae33c
    https://github.com/jaraco/jaraco.context/commit/d2b8d7750f78e870def98c4e04053af4acc86e29
    
    Add new python-coherent-licensed build dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-jaraco-context/python-jaraco-context.mk
+++ package/python-jaraco-context/python-jaraco-context.mk
@@ -4,12 +4,14 @@
 #
 ################################################################################
 
-PYTHON_JARACO_CONTEXT_VERSION = 6.0.1
+PYTHON_JARACO_CONTEXT_VERSION = 6.0.2
 PYTHON_JARACO_CONTEXT_SOURCE = jaraco_context-$(PYTHON_JARACO_CONTEXT_VERSION).tar.gz
-PYTHON_JARACO_CONTEXT_SITE = https://files.pythonhosted.org/packages/df/ad/f3777b81bf0b6e7bc7514a1656d3e637b2e8e15fab2ce3235730b3e7a4e6
+PYTHON_JARACO_CONTEXT_SITE = https://files.pythonhosted.org/packages/8d/7d/41acf8e22d791bde812cb6c2c36128bb932ed8ae066bcb5e39cb198e8253
 PYTHON_JARACO_CONTEXT_SETUP_TYPE = setuptools
 PYTHON_JARACO_CONTEXT_LICENSE = MIT
 PYTHON_JARACO_CONTEXT_LICENSE_FILES = LICENSE
-PYTHON_JARACO_CONTEXT_DEPENDENCIES = host-python-setuptools-scm
+PYTHON_JARACO_CONTEXT_DEPENDENCIES = \
+	host-python-coherent-licensed \
+	host-python-setuptools-scm
 
 $(eval $(python-package))
