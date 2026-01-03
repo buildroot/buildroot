a336746832ef24c22f4f37b322f4e5b378c3c0cd
James Hilliard <james.hilliard1@gmail.com>
2026-01-02 18:35:59 -0700

    package/python-babel: bump to version 2.17.0
    License hash changed due to date update:
    https://github.com/python-babel/babel/commit/b50a1d2186c20f3359f7e10853d2b2225a46ed40
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-babel/python-babel.mk
+++ package/python-babel/python-babel.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_BABEL_VERSION = 2.16.0
+PYTHON_BABEL_VERSION = 2.17.0
 PYTHON_BABEL_SOURCE = babel-$(PYTHON_BABEL_VERSION).tar.gz
-PYTHON_BABEL_SITE = https://files.pythonhosted.org/packages/2a/74/f1bc80f23eeba13393b7222b11d95ca3af2c1e28edca18af487137eefed9
+PYTHON_BABEL_SITE = https://files.pythonhosted.org/packages/7d/6b/d52e42361e1aa00709585ecc30b3f9684b3ab62530771402248b1b1d6240
 PYTHON_BABEL_SETUP_TYPE = setuptools
 PYTHON_BABEL_LICENSE = BSD-3-Clause
 PYTHON_BABEL_LICENSE_FILES = LICENSE
