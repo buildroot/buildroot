898677d8b357f2080c61cce6ef3d0fb0b7689d6e
James Hilliard <james.hilliard1@gmail.com>
2026-01-04 10:47:07 -0700

    package/python-mypy: bump to version 1.19.1
    License hash changed due to addition of base64 license which
    is BSD-2-Clause:
    https://github.com/python/mypy/commit/8f922b3d879f31e4c52c1c643de70e27b5720654
    
    Add new python-librt build/runtime dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-mypy/python-mypy.mk
+++ package/python-mypy/python-mypy.mk
@@ -4,19 +4,21 @@
 #
 ################################################################################
 
-PYTHON_MYPY_VERSION = 1.18.2
+PYTHON_MYPY_VERSION = 1.19.1
 PYTHON_MYPY_SOURCE = mypy-$(PYTHON_MYPY_VERSION).tar.gz
-PYTHON_MYPY_SITE = https://files.pythonhosted.org/packages/c0/77/8f0d0001ffad290cef2f7f216f96c814866248a0b92a722365ed54648e7e
+PYTHON_MYPY_SITE = https://files.pythonhosted.org/packages/f5/db/4efed9504bc01309ab9c2da7e352cc223569f05478012b5d9ece38fd44d2
 PYTHON_MYPY_SETUP_TYPE = setuptools
-PYTHON_MYPY_LICENSE = Apache-2.0, Python-2.0.1
+PYTHON_MYPY_LICENSE = Apache-2.0, BSD-2-Clause, Python-2.0.1
 PYTHON_MYPY_LICENSE_FILES = LICENSE
 PYTHON_MYPY_DEPENDENCIES = \
+	host-python-librt \
 	host-python-mypy-extensions \
 	host-python-pathspec \
 	host-python-types-psutil \
 	host-python-types-setuptools \
 	host-python-typing-extensions
 HOST_PYTHON_MYPY_DEPENDENCIES = \
+	host-python-librt \
 	host-python-mypy-extensions \
 	host-python-pathspec \
 	host-python-types-psutil \
