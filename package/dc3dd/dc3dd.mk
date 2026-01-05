2d7176d71685d9c120ca67ec61b00d3620e15bef
Bernd Kuhls <bernd@kuhls.net>
2026-01-05 20:40:55 +0100

    package/dc3dd: needs host-bison
    Fixes:
    https://autobuild.buildroot.net/results/f11/f11b0b9154e9979f3f2d7ea84a019f7719565e8f/
    
        ../build-aux/ylwrap: line 176: bison: command not found
    
    The build error occurred already in 2024:
    https://autobuild.buildroot.net/results/dea/dea779bbbbc08c7192f89073b93db768c41832f4/
    so a backport to the LTS branches should be considered.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit a22fca0055e09b05c3ec402d1d69b5463253a5d7)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/dc3dd/dc3dd.mk
+++ package/dc3dd/dc3dd.mk
@@ -10,6 +10,7 @@ DC3DD_SOURCE = dc3dd-$(DC3DD_VERSION).tar.xz
 DC3DD_SITE = https://downloads.sourceforge.net/project/dc3dd/dc3dd/$(DC3DD_VERSION_MAJOR)
 DC3DD_LICENSE = GPL-3.0+
 DC3DD_LICENSE_FILES = COPYING
+DC3DD_DEPENDENCIES = host-bison
 # We are patching Makefile.am, so we need to autoreconf. We also need to
 # enable autopoint as dc3dd comes with an old gettext infra.
 DC3DD_AUTORECONF = YES
