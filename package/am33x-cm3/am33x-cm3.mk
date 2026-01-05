072f01878b46307c96d5526bebca35b75d2c43c3
Kory Maincent (TI.com) <kory.maincent@bootlin.com>
2026-01-05 10:55:53 +0000

    package/am33x-cm3: Switch to TI repo and bump to v0x192
    Switch from the RobertCNelson GitHub mirror to the official TI git
    repository and update to version 0x192 (latest release).
    
    The previous installation method used sysfs to load the firmware,
    which was specific to old downstream Beaglebone or TI kernels. The
    mainline wkup_m3_ipc driver now expects am335x-pm-firmware.elf and
    *-scale-data.bin files to be installed in /lib/firmware, making the
    sysfs init script obsolete.
    
    Changes:
     - Switch repository from github.com/RobertCNelson to git.ti.com
     - Update version from 11107db (v05.00.00.02) to fb484c5 (v0x192)
     - Refresh all patches to apply cleanly on new version
     - Install .elf and scale-data files instead of .bin
     - Remove obsolete S93-am335x-pm-firmware-load init script
     - Update .checkpackageignore to reflect changes
    
    Signed-off-by: Kory Maincent (TI.com) <kory.maincent@bootlin.com>
    [Julien: update .checkpackageignore to fix check-package errors]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/am33x-cm3/am33x-cm3.mk
+++ package/am33x-cm3/am33x-cm3.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-# This should correpsond to v05.00.00.02
-AM33X_CM3_VERSION = 11107db2f1e9e58ee75d4fe9cc38423c9a6e4365
-AM33X_CM3_SITE = https://github.com/RobertCNelson/am33x-cm3
+# This should correspond to version 0x192
+AM33X_CM3_VERSION = fb484c5e54f2e31cf0a338d2927a06a2870bcc2c
+AM33X_CM3_SITE = https://git.ti.com/git/processor-firmware/ti-amx3-cm3-pm-firmware.git
 AM33X_CM3_SITE_METHOD = git
 AM33X_CM3_LICENSE = TI Publicly Available Software License
 AM33X_CM3_LICENSE_FILES = License.txt
@@ -17,15 +17,11 @@ define AM33X_CM3_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) CC="$(TARGET_CC)" CROSS_COMPILE="$(TARGET_CROSS)" -C $(@D) all
 endef
 
-# Not all of the firmware files are used
 define AM33X_CM3_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0644 -D $(@D)/bin/am335x-pm-firmware.bin \
-		$(TARGET_DIR)/lib/firmware/am335x-pm-firmware.bin
-endef
-
-define AM33X_CM3_INSTALL_INIT_SYSV
-	$(INSTALL) -m 0755 -D package/am33x-cm3/S93-am335x-pm-firmware-load \
-		$(TARGET_DIR)/etc/init.d/S93-am335x-pm-firmware-load
+	$(INSTALL) -m 0644 -D $(@D)/bin/am335x-pm-firmware.elf \
+		$(TARGET_DIR)/lib/firmware/am335x-pm-firmware.elf
+	$(INSTALL) -m 0644 -D $(@D)/bin/*-scale-data.bin \
+		$(TARGET_DIR)/lib/firmware/
 endef
 
 $(eval $(generic-package))
