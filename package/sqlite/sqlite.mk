b26ba37f1c47aa83039f18dbcad1164de9a805cd
Bernd Kuhls <bernd@kuhls.net>
2026-01-10 19:06:04 +0100

    package/sqlite: bump version to 3.51.2
    https://sqlite.org/releaselog/3_51_2.html
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/sqlite/sqlite.mk
+++ package/sqlite/sqlite.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-SQLITE_VERSION = 3.51.1
-SQLITE_TAR_VERSION = 3510100
+SQLITE_VERSION = 3.51.2
+SQLITE_TAR_VERSION = 3510200
 SQLITE_SOURCE = sqlite-autoconf-$(SQLITE_TAR_VERSION).tar.gz
-SQLITE_SITE = https://www.sqlite.org/2025
+SQLITE_SITE = https://www.sqlite.org/2026
 SQLITE_LICENSE = blessing
 SQLITE_LICENSE_FILES = tea/license.terms
 SQLITE_CPE_ID_VENDOR = sqlite
