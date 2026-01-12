734eb062b463565a0ffb53d15eaad2387035515e
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:28:33 +0100

    package/x11r7/xapp_showfont: bump version to 1.0.7
    https://lists.x.org/archives/xorg-announce/2025-May/003602.html
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_showfont/xapp_showfont.mk
+++ package/x11r7/xapp_showfont/xapp_showfont.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_SHOWFONT_VERSION = 1.0.6
+XAPP_SHOWFONT_VERSION = 1.0.7
 XAPP_SHOWFONT_SOURCE = showfont-$(XAPP_SHOWFONT_VERSION).tar.xz
 XAPP_SHOWFONT_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_SHOWFONT_LICENSE = MIT
