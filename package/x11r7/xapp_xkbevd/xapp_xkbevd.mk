cf6c292c9c72bde74916b0bd6c0a7926cfc9d124
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:28:48 +0100

    package/x11r7/xapp_xkbevd: bump version to 1.1.6
    https://lists.x.org/archives/xorg-announce/2024-December/003566.html
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_xkbevd/xapp_xkbevd.mk
+++ package/x11r7/xapp_xkbevd/xapp_xkbevd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XKBEVD_VERSION = 1.1.5
+XAPP_XKBEVD_VERSION = 1.1.6
 XAPP_XKBEVD_SOURCE = xkbevd-$(XAPP_XKBEVD_VERSION).tar.xz
 XAPP_XKBEVD_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XKBEVD_LICENSE = MIT
