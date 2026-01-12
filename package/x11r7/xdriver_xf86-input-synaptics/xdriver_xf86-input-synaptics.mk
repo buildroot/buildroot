293e47420884109c28401ed2b144ebd924198af9
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:28:41 +0100

    package/x11r7/xdriver_xf86-input-synaptics: bump version to 1.10.0
    https://lists.x.org/archives/xorg-announce/2025-January/003578.html
    
    Rebased patch 0001 due to upstream commit:
    https://gitlab.freedesktop.org/xorg/driver/xf86-input-synaptics/-/commit/64407e809dd7a75516acd3178aa77db7dc04e19d
    
    Switched _SITE to URL mentioned in latest release notes.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xdriver_xf86-input-synaptics/xdriver_xf86-input-synaptics.mk
+++ package/x11r7/xdriver_xf86-input-synaptics/xdriver_xf86-input-synaptics.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XDRIVER_XF86_INPUT_SYNAPTICS_VERSION = 1.9.2
+XDRIVER_XF86_INPUT_SYNAPTICS_VERSION = 1.10.0
 XDRIVER_XF86_INPUT_SYNAPTICS_SOURCE = xf86-input-synaptics-$(XDRIVER_XF86_INPUT_SYNAPTICS_VERSION).tar.xz
-XDRIVER_XF86_INPUT_SYNAPTICS_SITE = https://xorg.freedesktop.org/releases/individual/driver
+XDRIVER_XF86_INPUT_SYNAPTICS_SITE = https://xorg.freedesktop.org/archive/individual/driver
 XDRIVER_XF86_INPUT_SYNAPTICS_LICENSE = MIT
 XDRIVER_XF86_INPUT_SYNAPTICS_LICENSE_FILES = COPYING
 XDRIVER_XF86_INPUT_SYNAPTICS_DEPENDENCIES = libevdev xserver_xorg-server xorgproto mtdev
