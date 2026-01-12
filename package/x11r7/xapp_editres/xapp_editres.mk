c77a2eb944159795f853e5e68b2f3c253196a33e
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:29:32 +0100

    package/x11r7/xapp_editres: bump version to 1.0.9
    https://lists.x.org/archives/xorg-announce/2024-March/003469.html
    
    Switched _SITE to URL mentioned in latest release notes.
    
    Added optional dependency to xlib_libxkbfile which was introduced by
    upstream commit:
    https://gitlab.freedesktop.org/xorg/app/editres/-/commit/80959deec664cd80cd5dbf84af86b52ded4eaee0
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_editres/xapp_editres.mk
+++ package/x11r7/xapp_editres/xapp_editres.mk
@@ -4,12 +4,19 @@
 #
 ################################################################################
 
-XAPP_EDITRES_VERSION = 1.0.8
+XAPP_EDITRES_VERSION = 1.0.9
 XAPP_EDITRES_SOURCE = editres-$(XAPP_EDITRES_VERSION).tar.xz
-XAPP_EDITRES_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_EDITRES_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_EDITRES_LICENSE = MIT
 XAPP_EDITRES_LICENSE_FILES = COPYING
 XAPP_EDITRES_DEPENDENCIES = xlib_libX11 xlib_libXaw xlib_libXmu xlib_libXt
 XAPP_EDITRES_CONF_OPTS = --with-appdefaultdir=/usr/share/X11/app-defaults
+
+ifeq ($(BR2_PACKAGE_XLIB_LIBXKBFILE),y)
+XAPP_EDITRES_CONF_OPTS += --with-xkb
+XAPP_EDITRES_DEPENDENCIES += xlib_libxkbfile
+else
+XAPP_EDITRES_CONF_OPTS += --without-xkb
+endif
 
 $(eval $(autotools-package))
