88d6e75a53c0cab855e0c4b70cfa8496ef59de75
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:29:42 +0100

    package/x11r7/xlib_libXrandr: bump version to 1.5.4
    https://lists.x.org/archives/xorg-announce/2023-October/003427.html
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xlib_libXrandr/xlib_libXrandr.mk
+++ package/x11r7/xlib_libXrandr/xlib_libXrandr.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XLIB_LIBXRANDR_VERSION = 1.5.3
+XLIB_LIBXRANDR_VERSION = 1.5.4
 XLIB_LIBXRANDR_SOURCE = libXrandr-$(XLIB_LIBXRANDR_VERSION).tar.xz
 XLIB_LIBXRANDR_SITE = https://xorg.freedesktop.org/archive/individual/lib
 XLIB_LIBXRANDR_LICENSE = MIT
