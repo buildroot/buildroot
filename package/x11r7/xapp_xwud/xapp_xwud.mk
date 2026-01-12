6576a349bf06e5eb2a8200520ba9e4907af66be1
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:29:03 +0100

    package/x11r7/xapp_xwud: bump version to 1.0.7
    https://lists.x.org/archives/xorg-announce/2024-October/003537.html
    
    Switched _SITE to URL mentioned in latest release notes.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_xwud/xapp_xwud.mk
+++ package/x11r7/xapp_xwud/xapp_xwud.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_XWUD_VERSION = 1.0.6
+XAPP_XWUD_VERSION = 1.0.7
 XAPP_XWUD_SOURCE = xwud-$(XAPP_XWUD_VERSION).tar.xz
-XAPP_XWUD_SITE = https://xorg.freedesktop.org/releases/individual/app
+XAPP_XWUD_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XWUD_LICENSE = MIT
 XAPP_XWUD_LICENSE_FILES = COPYING
 XAPP_XWUD_DEPENDENCIES = xlib_libX11
