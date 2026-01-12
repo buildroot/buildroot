735bd8bd9f74d7e10cbfc834adfef243f281094b
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:29:21 +0100

    package/x11r7/xapp_iceauth: bump version to 1.0.10
    https://lists.x.org/archives/xorg-announce/2024-March/003480.html
    
    Switched _SITE to URL mentioned in latest release notes.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_iceauth/xapp_iceauth.mk
+++ package/x11r7/xapp_iceauth/xapp_iceauth.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_ICEAUTH_VERSION = 1.0.9
+XAPP_ICEAUTH_VERSION = 1.0.10
 XAPP_ICEAUTH_SOURCE = iceauth-$(XAPP_ICEAUTH_VERSION).tar.xz
-XAPP_ICEAUTH_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_ICEAUTH_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_ICEAUTH_LICENSE = MIT
 XAPP_ICEAUTH_LICENSE_FILES = COPYING
 XAPP_ICEAUTH_DEPENDENCIES = xlib_libICE xlib_libX11
