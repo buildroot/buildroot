66d25cdfad73bfb5202ba1745356d2b26ef42377
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:28:13 +0100

    package/x11r7/xapp_fonttosfnt: bump version to 1.2.5
    https://lists.x.org/archives/xorg-announce/2025-December/003651.html
    https://lists.x.org/archives/xorg-announce/2024-October/003533.html
    https://lists.x.org/archives/xorg-announce/2023-September/003422.html
    
    Switched tarball to xz.
    Switched _SITE to URL mentioned in latest release notes.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_fonttosfnt/xapp_fonttosfnt.mk
+++ package/x11r7/xapp_fonttosfnt/xapp_fonttosfnt.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_FONTTOSFNT_VERSION = 1.2.2
-XAPP_FONTTOSFNT_SOURCE = fonttosfnt-$(XAPP_FONTTOSFNT_VERSION).tar.bz2
-XAPP_FONTTOSFNT_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_FONTTOSFNT_VERSION = 1.2.5
+XAPP_FONTTOSFNT_SOURCE = fonttosfnt-$(XAPP_FONTTOSFNT_VERSION).tar.xz
+XAPP_FONTTOSFNT_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_FONTTOSFNT_LICENSE = MIT
 XAPP_FONTTOSFNT_LICENSE_FILES = COPYING
 XAPP_FONTTOSFNT_DEPENDENCIES = freetype xlib_libX11 xlib_libfontenc
