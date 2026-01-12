59754eb13f563b4bf59cbacbb6510ec604845b5c
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:28:30 +0100

    package/x11r7/xapp_xcursorgen: bump version to 1.0.9
    https://lists.x.org/archives/xorg-announce/2025-May/003605.html
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_xcursorgen/xapp_xcursorgen.mk
+++ package/x11r7/xapp_xcursorgen/xapp_xcursorgen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XCURSORGEN_VERSION = 1.0.8
+XAPP_XCURSORGEN_VERSION = 1.0.9
 XAPP_XCURSORGEN_SOURCE = xcursorgen-$(XAPP_XCURSORGEN_VERSION).tar.xz
 XAPP_XCURSORGEN_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XCURSORGEN_LICENSE = MIT
