7ae757762c32b5a3d0cd259fc196f9d8d515f168
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:28:19 +0100

    package/x11r7/xlib_libXpresent: bump version to 1.0.2
    https://lists.x.org/archives/xorg-announce/2025-September/003627.html
    
    Switched _SITE to URL mentioned in latest release notes.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Cc: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xlib_libXpresent/xlib_libXpresent.mk
+++ package/x11r7/xlib_libXpresent/xlib_libXpresent.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XLIB_LIBXPRESENT_VERSION = 1.0.1
+XLIB_LIBXPRESENT_VERSION = 1.0.2
 XLIB_LIBXPRESENT_SOURCE = libXpresent-$(XLIB_LIBXPRESENT_VERSION).tar.xz
-XLIB_LIBXPRESENT_SITE = http://xorg.freedesktop.org/releases/individual/lib
+XLIB_LIBXPRESENT_SITE = https://xorg.freedesktop.org/archive/individual/lib
 XLIB_LIBXPRESENT_LICENSE = MIT
 XLIB_LIBXPRESENT_LICENSE_FILES = COPYING
 XLIB_LIBXPRESENT_INSTALL_STAGING = YES
