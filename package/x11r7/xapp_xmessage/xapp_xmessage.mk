bbe10c784a02d6ce10fb5a336b05ef30064c59a0
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:29:27 +0100

    package/x11r7/xapp_xmessage: bump version to 1.0.7
    https://lists.x.org/archives/xorg-announce/2024-March/003475.html
    
    Switched _SITE to URL mentioned in latest release notes.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_xmessage/xapp_xmessage.mk
+++ package/x11r7/xapp_xmessage/xapp_xmessage.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_XMESSAGE_VERSION = 1.0.6
+XAPP_XMESSAGE_VERSION = 1.0.7
 XAPP_XMESSAGE_SOURCE = xmessage-$(XAPP_XMESSAGE_VERSION).tar.xz
-XAPP_XMESSAGE_SITE = https://xorg.freedesktop.org/releases/individual/app
+XAPP_XMESSAGE_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XMESSAGE_LICENSE = MIT
 XAPP_XMESSAGE_LICENSE_FILES = COPYING
 XAPP_XMESSAGE_DEPENDENCIES = xlib_libXaw
