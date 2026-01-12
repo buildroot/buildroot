509fa5fd553eeb0cb77afed72bbf5cd1b50d3274
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:29:15 +0100

    package/x11r7/xapp_appres: bump version to 1.0.7
    https://lists.x.org/archives/xorg-announce/2024-June/003517.html
    
    Switched _SITE to URL mentioned in latest release notes.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_appres/xapp_appres.mk
+++ package/x11r7/xapp_appres/xapp_appres.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_APPRES_VERSION = 1.0.6
+XAPP_APPRES_VERSION = 1.0.7
 XAPP_APPRES_SOURCE = appres-$(XAPP_APPRES_VERSION).tar.xz
-XAPP_APPRES_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_APPRES_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_APPRES_LICENSE = MIT
 XAPP_APPRES_LICENSE_FILES = COPYING
 XAPP_APPRES_DEPENDENCIES = xlib_libX11 xlib_libXt
