4e59dc19d7ae4e33a756fc4f996a7fbaf9cafeb8
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:29:23 +0100

    package/x11r7/xapp_xsm: bump version to 1.0.6
    https://lists.x.org/archives/xorg-announce/2024-March/003479.html
    
    Switched _SITE to URL mentioned in latest release notes.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_xsm/xapp_xsm.mk
+++ package/x11r7/xapp_xsm/xapp_xsm.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_XSM_VERSION = 1.0.5
+XAPP_XSM_VERSION = 1.0.6
 XAPP_XSM_SOURCE = xsm-$(XAPP_XSM_VERSION).tar.xz
-XAPP_XSM_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_XSM_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XSM_LICENSE = MIT
 XAPP_XSM_LICENSE_FILES = COPYING
 XAPP_XSM_DEPENDENCIES = xlib_libXaw
