ef603acb90762e550c3c1396559b63b7732faf89
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:28:27 +0100

    package/x11r7/xapp_xeyes: bump version to 1.3.1
    https://lists.x.org/archives/xorg-announce/2025-July/003618.html
    https://lists.x.org/archives/xorg-announce/2023-July/003409.html
    
    Switched tarball to xz.
    
    Updated license hash due to upstream commit:
    https://gitlab.freedesktop.org/xorg/app/xeyes/-/commit/7dc4f720f57471d2eccefd87cdca54494cf75eb5
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_xeyes/xapp_xeyes.mk
+++ package/x11r7/xapp_xeyes/xapp_xeyes.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-XAPP_XEYES_VERSION = 1.2.0
-XAPP_XEYES_SOURCE = xeyes-$(XAPP_XEYES_VERSION).tar.bz2
+XAPP_XEYES_VERSION = 1.3.1
+XAPP_XEYES_SOURCE = xeyes-$(XAPP_XEYES_VERSION).tar.xz
 XAPP_XEYES_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XEYES_LICENSE = MIT
 XAPP_XEYES_LICENSE_FILES = COPYING
