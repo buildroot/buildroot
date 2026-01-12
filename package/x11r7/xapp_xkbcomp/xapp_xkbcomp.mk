47fea0b7089582755bcd492d7bba183c3651b85d
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:28:15 +0100

    package/x11r7/xapp_xkbcomp: security bump version to 1.5.0
    https://lists.x.org/archives/xorg-announce/2025-December/003645.html
    https://lists.x.org/archives/xorg-announce/2024-February/003450.html
    
    Fixes CVE-2018-15863, CVE-2018-15861, CVE-2018-15859 & CVE-2018-15853:
    https://lists.x.org/archives/xorg-announce/2025-December/003644.html
    
    Switched _SITE to URL mentioned in latest release notes.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_xkbcomp/xapp_xkbcomp.mk
+++ package/x11r7/xapp_xkbcomp/xapp_xkbcomp.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_XKBCOMP_VERSION = 1.4.6
+XAPP_XKBCOMP_VERSION = 1.5.0
 XAPP_XKBCOMP_SOURCE = xkbcomp-$(XAPP_XKBCOMP_VERSION).tar.xz
-XAPP_XKBCOMP_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_XKBCOMP_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XKBCOMP_LICENSE = MIT
 XAPP_XKBCOMP_LICENSE_FILES = COPYING
 XAPP_XKBCOMP_DEPENDENCIES = xlib_libX11 xlib_libxkbfile
