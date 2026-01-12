cb760e4da06b1a412e224a87853183383f722987
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:29:05 +0100

    package/x11r7/xapp_xkbprint: bump version to 1.0.7
    https://lists.x.org/archives/xorg-announce/2024-October/003535.html
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_xkbprint/xapp_xkbprint.mk
+++ package/x11r7/xapp_xkbprint/xapp_xkbprint.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XKBPRINT_VERSION = 1.0.6
+XAPP_XKBPRINT_VERSION = 1.0.7
 XAPP_XKBPRINT_SOURCE = xkbprint-$(XAPP_XKBPRINT_VERSION).tar.xz
 XAPP_XKBPRINT_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XKBPRINT_LICENSE = MIT
