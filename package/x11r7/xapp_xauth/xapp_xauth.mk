3a6ea017acad756e5c3afd45d3b7a0e2c55eb534
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:28:11 +0100

    package/x11r7/xapp_xauth: bump version to 1.1.5
    https://lists.x.org/archives/xorg-announce/2025-December/003653.html
    https://lists.x.org/archives/xorg-announce/2025-March/003589.html
    https://lists.x.org/archives/xorg-announce/2024-March/003472.html
    
    Switched _SITE to URL mentioned in latest release notes.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_xauth/xapp_xauth.mk
+++ package/x11r7/xapp_xauth/xapp_xauth.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_XAUTH_VERSION = 1.1.2
+XAPP_XAUTH_VERSION = 1.1.5
 XAPP_XAUTH_SOURCE = xauth-$(XAPP_XAUTH_VERSION).tar.xz
-XAPP_XAUTH_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_XAUTH_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XAUTH_LICENSE = MIT
 XAPP_XAUTH_LICENSE_FILES = COPYING
 XAPP_XAUTH_DEPENDENCIES = xlib_libX11 xlib_libXau xlib_libXext xlib_libXmu
