926381d360082e926cb6be28cb1e97639d26ea0f
Jesse Taube <mr.bossman075@gmail.com>
2026-01-12 17:19:27 -0500

    package/picotool: Add host-picotool
    Add a host-picotool package to picotool.
    This will allow users to build rpi pico applications on the host system.
    
    Signed-off-by: Jesse Taube <Mr.Bossman075@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/picotool/picotool.mk
+++ package/picotool/picotool.mk
@@ -6,9 +6,14 @@
 
 PICOTOOL_VERSION = 2.2.0-a4
 PICOTOOL_SITE = $(call github,raspberrypi,picotool,$(PICOTOOL_VERSION))
-PICOTOOL_CONF_OPTS = -DPICO_SDK_PATH=$(STAGING_DIR)/usr/share/pico-sdk
-PICOTOOL_DEPENDENCIES = libusb pico-sdk
 PICOTOOL_LICENSE = BSD-3-Clause
 PICOTOOL_LICENSE_FILES = LICENSE.TXT
 
+PICOTOOL_CONF_OPTS = -DPICO_SDK_PATH=$(STAGING_DIR)/usr/share/pico-sdk
+PICOTOOL_DEPENDENCIES = libusb pico-sdk
+
+HOST_PICOTOOL_CONF_OPTS = -DPICO_SDK_PATH=$(HOST_DIR)/usr/share/pico-sdk
+HOST_PICOTOOL_DEPENDENCIES = host-libusb host-pico-sdk
+
 $(eval $(cmake-package))
+$(eval $(host-cmake-package))
