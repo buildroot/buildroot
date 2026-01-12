22890ed8f0e663688f5a2e9faeb08954bfed1fbf
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:28:37 +0100

    package/x11r7/xapp_xinit: bump version to 1.4.4
    https://lists.x.org/archives/xorg-announce/2025-March/003593.html
    https://lists.x.org/archives/xorg-announce/2025-January/003577.html
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_xinit/xapp_xinit.mk
+++ package/x11r7/xapp_xinit/xapp_xinit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XINIT_VERSION = 1.4.2
+XAPP_XINIT_VERSION = 1.4.4
 XAPP_XINIT_SOURCE = xinit-$(XAPP_XINIT_VERSION).tar.xz
 XAPP_XINIT_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XINIT_DEPENDENCIES = xapp_xauth xlib_libX11
