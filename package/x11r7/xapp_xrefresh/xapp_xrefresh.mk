aab82b4649448371bb1bc9f288c3ad4a1ed2a9f6
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:29:24 +0100

    package/x11r7/xapp_xrefresh: bump version to 1.1.0
    https://lists.x.org/archives/xorg-announce/2024-March/003478.html
    
    Switched _SITE to URL mentioned in latest release notes.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_xrefresh/xapp_xrefresh.mk
+++ package/x11r7/xapp_xrefresh/xapp_xrefresh.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_XREFRESH_VERSION = 1.0.7
+XAPP_XREFRESH_VERSION = 1.1.0
 XAPP_XREFRESH_SOURCE = xrefresh-$(XAPP_XREFRESH_VERSION).tar.xz
-XAPP_XREFRESH_SITE = https://xorg.freedesktop.org/releases/individual/app
+XAPP_XREFRESH_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XREFRESH_LICENSE = MIT
 XAPP_XREFRESH_LICENSE_FILES = COPYING
 XAPP_XREFRESH_DEPENDENCIES = xlib_libX11
