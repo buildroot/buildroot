d7c2ce79a16293fa7eec197fa42fceebafa27237
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:29:22 +0100

    package/x11r7/xlib_libXaw: bump version to 1.0.16
    https://lists.x.org/archives/xorg-announce/2024-March/003481.html
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xlib_libXaw/xlib_libXaw.mk
+++ package/x11r7/xlib_libXaw/xlib_libXaw.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XLIB_LIBXAW_VERSION = 1.0.15
+XLIB_LIBXAW_VERSION = 1.0.16
 XLIB_LIBXAW_SOURCE = libXaw-$(XLIB_LIBXAW_VERSION).tar.xz
 XLIB_LIBXAW_SITE = https://xorg.freedesktop.org/archive/individual/lib
 XLIB_LIBXAW_LICENSE = MIT
