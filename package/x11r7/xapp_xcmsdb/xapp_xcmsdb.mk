4ac3cddf7b6af137fb819ca928043973eb1fdc4c
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:29:06 +0100

    package/x11r7/xapp_xcmsdb: bump version to 1.0.7
    https://lists.x.org/archives/xorg-announce/2024-October/003534.html
    
    Switched _SITE to URL mentioned in latest release notes.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_xcmsdb/xapp_xcmsdb.mk
+++ package/x11r7/xapp_xcmsdb/xapp_xcmsdb.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_XCMSDB_VERSION = 1.0.6
+XAPP_XCMSDB_VERSION = 1.0.7
 XAPP_XCMSDB_SOURCE = xcmsdb-$(XAPP_XCMSDB_VERSION).tar.xz
-XAPP_XCMSDB_SITE = https://xorg.freedesktop.org/releases/individual/app
+XAPP_XCMSDB_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XCMSDB_LICENSE = MIT
 XAPP_XCMSDB_LICENSE_FILES = COPYING
 XAPP_XCMSDB_DEPENDENCIES = xlib_libX11
