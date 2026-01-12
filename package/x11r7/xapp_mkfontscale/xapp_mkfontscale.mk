016e6041c012e32bc936c09b46f8a740c0aa8f3d
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:29:30 +0100

    package/x11r7/xapp_mkfontscale: bump version to 1.2.3
    https://lists.x.org/archives/xorg-announce/2024-March/003471.html
    
    Updated license hash due to upstream commit:
    https://gitlab.freedesktop.org/xorg/app/mkfontscale/-/commit/a631117eb3d816e8b3fdb969017fc5d3c7357ba3
    
    Switched _SITE to URL mentioned in latest release notes.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_mkfontscale/xapp_mkfontscale.mk
+++ package/x11r7/xapp_mkfontscale/xapp_mkfontscale.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_MKFONTSCALE_VERSION = 1.2.2
+XAPP_MKFONTSCALE_VERSION = 1.2.3
 XAPP_MKFONTSCALE_SOURCE = mkfontscale-$(XAPP_MKFONTSCALE_VERSION).tar.xz
-XAPP_MKFONTSCALE_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_MKFONTSCALE_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_MKFONTSCALE_LICENSE = MIT
 XAPP_MKFONTSCALE_LICENSE_FILES = COPYING
 XAPP_MKFONTSCALE_DEPENDENCIES = zlib freetype xlib_libfontenc xorgproto
