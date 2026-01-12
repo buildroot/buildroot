0259107f8825ee53e75f8e59d8747c03b3f261ef
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:28:44 +0100

    package/x11r7/xlib_libxcvt: bump version to 0.1.3
    https://lists.x.org/archives/xorg-announce/2024-December/003571.html
    
    Switched _SITE to URL mentioned in latest release notes.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xlib_libxcvt/xlib_libxcvt.mk
+++ package/x11r7/xlib_libxcvt/xlib_libxcvt.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XLIB_LIBXCVT_VERSION = 0.1.2
+XLIB_LIBXCVT_VERSION = 0.1.3
 XLIB_LIBXCVT_SOURCE = libxcvt-$(XLIB_LIBXCVT_VERSION).tar.xz
-XLIB_LIBXCVT_SITE = https://xorg.freedesktop.org/releases/individual/lib
+XLIB_LIBXCVT_SITE = https://xorg.freedesktop.org/archive/individual/lib
 XLIB_LIBXCVT_LICENSE = MIT
 XLIB_LIBXCVT_LICENSE_FILES = COPYING
 XLIB_LIBXCVT_INSTALL_STAGING = YES
