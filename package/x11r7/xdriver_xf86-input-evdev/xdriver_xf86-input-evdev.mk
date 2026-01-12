90b693a416e2e76d23c3b2cd8c46d236a667a646
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:28:58 +0100

    package/x11r7/xdriver_xf86-input-evdev: bump version to 2.11.0
    https://lists.x.org/archives/xorg-announce/2024-October/003544.html
    
    Switched tarball to xz.
    
    Switched _SITE to URL mentioned in latest release notes.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xdriver_xf86-input-evdev/xdriver_xf86-input-evdev.mk
+++ package/x11r7/xdriver_xf86-input-evdev/xdriver_xf86-input-evdev.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XDRIVER_XF86_INPUT_EVDEV_VERSION = 2.10.6
-XDRIVER_XF86_INPUT_EVDEV_SOURCE = xf86-input-evdev-$(XDRIVER_XF86_INPUT_EVDEV_VERSION).tar.bz2
-XDRIVER_XF86_INPUT_EVDEV_SITE = http://xorg.freedesktop.org/releases/individual/driver
+XDRIVER_XF86_INPUT_EVDEV_VERSION = 2.11.0
+XDRIVER_XF86_INPUT_EVDEV_SOURCE = xf86-input-evdev-$(XDRIVER_XF86_INPUT_EVDEV_VERSION).tar.xz
+XDRIVER_XF86_INPUT_EVDEV_SITE = https://xorg.freedesktop.org/archive/individual/driver
 XDRIVER_XF86_INPUT_EVDEV_LICENSE = MIT
 XDRIVER_XF86_INPUT_EVDEV_LICENSE_FILES = COPYING
 XDRIVER_XF86_INPUT_EVDEV_AUTORECONF = YES
