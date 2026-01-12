112cb83455f5f591de17c5ed2fec519b41dfdedd
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:29:25 +0100

    package/x11r7/xapp_xpr: bump version to 1.2.0
    https://lists.x.org/archives/xorg-announce/2024-March/003477.html
    
    Updated license hash due to upstream commit:
    https://gitlab.freedesktop.org/xorg/app/xpr/-/commit/6f34ba3a67e2c25f4fd6b2878038793422bf8770
    
    Switched _SITE to URL mentioned in latest release notes.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_xpr/xapp_xpr.mk
+++ package/x11r7/xapp_xpr/xapp_xpr.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_XPR_VERSION = 1.1.0
+XAPP_XPR_VERSION = 1.2.0
 XAPP_XPR_SOURCE = xpr-$(XAPP_XPR_VERSION).tar.xz
-XAPP_XPR_SITE = https://xorg.freedesktop.org/releases/individual/app
+XAPP_XPR_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XPR_LICENSE = MIT
 XAPP_XPR_LICENSE_FILES = COPYING
 XAPP_XPR_DEPENDENCIES = xlib_libX11 xlib_libXmu
