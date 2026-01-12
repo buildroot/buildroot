965a86b38ab6d9d74db5f114929a3eed3106ae01
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:29:41 +0100

    package/x11r7/xapp_xbiff: bump version to 1.0.5
    https://lists.x.org/archives/xorg-announce/2024-January/003445.html
    
    Switched tarball to xz.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_xbiff/xapp_xbiff.mk
+++ package/x11r7/xapp_xbiff/xapp_xbiff.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-XAPP_XBIFF_VERSION = 1.0.4
-XAPP_XBIFF_SOURCE = xbiff-$(XAPP_XBIFF_VERSION).tar.bz2
+XAPP_XBIFF_VERSION = 1.0.5
+XAPP_XBIFF_SOURCE = xbiff-$(XAPP_XBIFF_VERSION).tar.xz
 XAPP_XBIFF_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XBIFF_LICENSE = MIT
 XAPP_XBIFF_LICENSE_FILES = COPYING
