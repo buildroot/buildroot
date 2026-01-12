70d7bed695fdc12f0ddaa3248e697b9cd335e81f
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:29:04 +0100

    package/x11r7/xapp_xmag: bump version to 1.0.8
    https://lists.x.org/archives/xorg-announce/2024-October/003536.html
    
    Switched _SITE to URL mentioned in latest release notes.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_xmag/xapp_xmag.mk
+++ package/x11r7/xapp_xmag/xapp_xmag.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_XMAG_VERSION = 1.0.7
+XAPP_XMAG_VERSION = 1.0.8
 XAPP_XMAG_SOURCE = xmag-$(XAPP_XMAG_VERSION).tar.xz
-XAPP_XMAG_SITE = https://xorg.freedesktop.org/releases/individual/app
+XAPP_XMAG_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XMAG_LICENSE = MIT
 XAPP_XMAG_LICENSE_FILES = COPYING
 XAPP_XMAG_DEPENDENCIES = xlib_libXaw
