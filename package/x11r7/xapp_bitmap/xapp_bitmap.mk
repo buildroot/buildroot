fdd7607b53d6baa26d9613380feb10c8f87c599e
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:29:40 +0100

    package/x11r7/xapp_bitmap: bump version to 1.1.1
    https://lists.x.org/archives/xorg-announce/2024-February/003447.html
    
    Switched _SITE to URL mentioned in latest release notes.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_bitmap/xapp_bitmap.mk
+++ package/x11r7/xapp_bitmap/xapp_bitmap.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_BITMAP_VERSION = 1.1.0
+XAPP_BITMAP_VERSION = 1.1.1
 XAPP_BITMAP_SOURCE = bitmap-$(XAPP_BITMAP_VERSION).tar.xz
-XAPP_BITMAP_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_BITMAP_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_BITMAP_LICENSE = MIT
 XAPP_BITMAP_LICENSE_FILES = COPYING
 XAPP_BITMAP_DEPENDENCIES = xlib_libX11 xlib_libXaw xlib_libXmu xdata_xbitmaps
