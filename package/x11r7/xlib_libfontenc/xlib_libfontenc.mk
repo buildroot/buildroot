c727b801257263b412c2db673f8e942bd43d8c8e
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:29:35 +0100

    package/x11r7/xlib_libfontenc: bump version to 1.1.8
    https://lists.x.org/archives/xorg-announce/2024-March/003462.html
    
    Switched _SITE to URL mentioned in latest release notes.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xlib_libfontenc/xlib_libfontenc.mk
+++ package/x11r7/xlib_libfontenc/xlib_libfontenc.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XLIB_LIBFONTENC_VERSION = 1.1.7
+XLIB_LIBFONTENC_VERSION = 1.1.8
 XLIB_LIBFONTENC_SOURCE = libfontenc-$(XLIB_LIBFONTENC_VERSION).tar.xz
-XLIB_LIBFONTENC_SITE = http://xorg.freedesktop.org/releases/individual/lib
+XLIB_LIBFONTENC_SITE = https://xorg.freedesktop.org/archive/individual/lib
 XLIB_LIBFONTENC_LICENSE = MIT
 XLIB_LIBFONTENC_LICENSE_FILES = COPYING
 XLIB_LIBFONTENC_INSTALL_STAGING = YES
