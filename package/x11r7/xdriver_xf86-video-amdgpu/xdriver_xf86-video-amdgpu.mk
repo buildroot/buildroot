51c05053658c9874145cec6da425e881b03ae107
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:29:47 +0100

    package/x11r7/xdriver_xf86-video-amdgpu: bump version to 25.0.0
    https://lists.freedesktop.org/archives/amd-gfx/2025-July/127936.html
    
    Switched build system to meson after autotools removal.
    
    Added optional dependency to udev provided by meson build system.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xdriver_xf86-video-amdgpu/xdriver_xf86-video-amdgpu.mk
+++ package/x11r7/xdriver_xf86-video-amdgpu/xdriver_xf86-video-amdgpu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_AMDGPU_VERSION = 23.0.0
+XDRIVER_XF86_VIDEO_AMDGPU_VERSION = 25.0.0
 XDRIVER_XF86_VIDEO_AMDGPU_SOURCE = xf86-video-amdgpu-$(XDRIVER_XF86_VIDEO_AMDGPU_VERSION).tar.xz
 XDRIVER_XF86_VIDEO_AMDGPU_SITE = https://xorg.freedesktop.org/archive/individual/driver
 XDRIVER_XF86_VIDEO_AMDGPU_LICENSE = MIT
@@ -17,12 +17,16 @@ XDRIVER_XF86_VIDEO_AMDGPU_DEPENDENCIES = \
 	xserver_xorg-server
 
 ifeq ($(BR2_PACKAGE_HAS_LIBEGL)$(BR2_PACKAGE_HAS_LIBGL)$(BR2_PACKAGE_LIBEPOXY),yyy)
-XDRIVER_XF86_VIDEO_AMDGPU_CONF_OPTS += --enable-glamor
+XDRIVER_XF86_VIDEO_AMDGPU_CONF_OPTS += -Dglamor=enabled
 else
-XDRIVER_XF86_VIDEO_AMDGPU_CONF_OPTS += --disable-glamor
+XDRIVER_XF86_VIDEO_AMDGPU_CONF_OPTS += -Dglamor=disabled
 endif
 
-# xdriver_xf86-video-amdgpu requires O_CLOEXEC
-XDRIVER_XF86_VIDEO_AMDGPU_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -D_GNU_SOURCE"
+ifeq ($(BR2_PACKAGE_HAS_UDEV),y)
+XDRIVER_XF86_VIDEO_AMDGPU_CONF_OPTS += -Dudev=enabled
+XDRIVER_XF86_VIDEO_AMDGPU_DEPENDENCIES += udev
+else
+XDRIVER_XF86_VIDEO_AMDGPU_CONF_OPTS += -Dudev=disabled
+endif
 
-$(eval $(autotools-package))
+$(eval $(meson-package))
