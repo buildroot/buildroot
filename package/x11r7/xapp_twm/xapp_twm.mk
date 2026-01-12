3d0656bf489df152c65dcd317b2ac9f63aac1f6b
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:28:35 +0100

    package/x11r7/xapp_twm: bump version to 1.0.13.1
    https://lists.x.org/archives/xorg-announce/2025-May/003600.html
    https://lists.x.org/archives/xorg-announce/2025-May/003599.html
    
    Switched _SITE to URL mentioned in latest release notes.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_twm/xapp_twm.mk
+++ package/x11r7/xapp_twm/xapp_twm.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_TWM_VERSION = 1.0.12
+XAPP_TWM_VERSION = 1.0.13.1
 XAPP_TWM_SOURCE = twm-$(XAPP_TWM_VERSION).tar.xz
-XAPP_TWM_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_TWM_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_TWM_LICENSE = MIT
 XAPP_TWM_LICENSE_FILES = COPYING
 XAPP_TWM_DEPENDENCIES = \
