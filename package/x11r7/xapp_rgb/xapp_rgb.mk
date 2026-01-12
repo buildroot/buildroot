6ad7b859e08da0a2592a40c8a7083fd040ddbb4b
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:28:12 +0100

    package/x11r7/xapp_rgb: bump version to 1.1.1
    https://lists.x.org/archives/xorg-announce/2025-December/003652.html
    
    Updated license hash due to upstream commit
    https://cgit.freedesktop.org/xorg/app/rgb/commit/COPYING?h=rgb-1.1.1&id=fb06ee0b71ec15d63b6ee4797f954187ec014a37
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_rgb/xapp_rgb.mk
+++ package/x11r7/xapp_rgb/xapp_rgb.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_RGB_VERSION = 1.1.0
+XAPP_RGB_VERSION = 1.1.1
 XAPP_RGB_SOURCE = rgb-$(XAPP_RGB_VERSION).tar.xz
 XAPP_RGB_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_RGB_LICENSE = MIT
