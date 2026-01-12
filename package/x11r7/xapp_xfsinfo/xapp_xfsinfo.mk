bed191965f1685ea05d99625b9906c233048a990
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:28:26 +0100

    package/x11r7/xapp_xfsinfo: bump version to 1.0.8
    https://lists.x.org/archives/xorg-announce/2025-July/003619.html
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_xfsinfo/xapp_xfsinfo.mk
+++ package/x11r7/xapp_xfsinfo/xapp_xfsinfo.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XFSINFO_VERSION = 1.0.7
+XAPP_XFSINFO_VERSION = 1.0.8
 XAPP_XFSINFO_SOURCE = xfsinfo-$(XAPP_XFSINFO_VERSION).tar.xz
 XAPP_XFSINFO_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XFSINFO_LICENSE = MIT
