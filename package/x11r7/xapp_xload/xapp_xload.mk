b27bb473243d64806be420226f16143e6464366d
Bernd Kuhls <bernd@kuhls.net>
2026-01-12 09:29:19 +0100

    package/x11r7/xapp_xload: bump version to 1.2.0
    https://lists.x.org/archives/xorg-announce/2024-March/003485.html
    
    Updated license hash due to upstream commit:
    https://gitlab.freedesktop.org/xorg/app/xload/-/commit/ef450fb99630a406c4638d8ba251eac45e434411
    
    Switched _SITE to URL mentioned in latest release notes.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/x11r7/xapp_xload/xapp_xload.mk
+++ package/x11r7/xapp_xload/xapp_xload.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_XLOAD_VERSION = 1.1.4
+XAPP_XLOAD_VERSION = 1.2.0
 XAPP_XLOAD_SOURCE = xload-$(XAPP_XLOAD_VERSION).tar.xz
-XAPP_XLOAD_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_XLOAD_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XLOAD_LICENSE = MIT
 XAPP_XLOAD_LICENSE_FILES = COPYING
 XAPP_XLOAD_DEPENDENCIES = xlib_libXaw $(TARGET_NLS_DEPENDENCIES)
