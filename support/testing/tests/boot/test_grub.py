95cd6f8bc3d0fca48a95e96ea287d3c684e4f624
Julien Olivain <ju.o@free.fr>
2026-01-24 14:13:36 +0100

    support/testing: boot/test_grub: remove 32bit EFI test
    EDK2 removed OVMF IA32 support in commit [1], which is included in
    version edk2-stable202511. This commit removes the test relying on it.
    
    [1] https://github.com/tianocore/edk2/commit/1fb88ffe284782cc79e306306b8d19829b6248b7
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Romain Naour <romain.naour@smile.fr>
    (cherry picked from commit bd06eace372761d7ced25d73579122db8d643058)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- support/testing/tests/boot/test_grub.py
+++ support/testing/tests/boot/test_grub.py
@@ -32,56 +32,6 @@ class TestGrubi386BIOS(infra.basetest.BRTest):
         self.emulator.login()
 
 
-class TestGrubi386EFI(infra.basetest.BRTest):
-    config = \
-        """
-        BR2_x86_core2=y
-        BR2_TOOLCHAIN_EXTERNAL=y
-        BR2_TOOLCHAIN_EXTERNAL_BOOTLIN_X86_CORE2_GLIBC_STABLE=y
-        BR2_ROOTFS_POST_BUILD_SCRIPT="board/pc/post-build.sh {}"
-        BR2_ROOTFS_POST_IMAGE_SCRIPT="board/pc/post-image-efi.sh"
-        BR2_LINUX_KERNEL=y
-        BR2_LINUX_KERNEL_CUSTOM_VERSION=y
-        BR2_LINUX_KERNEL_CUSTOM_VERSION_VALUE="4.19.204"
-        BR2_LINUX_KERNEL_USE_CUSTOM_CONFIG=y
-        BR2_LINUX_KERNEL_CUSTOM_CONFIG_FILE="board/pc/linux.config"
-        BR2_LINUX_KERNEL_INSTALL_TARGET=y
-        BR2_LINUX_KERNEL_NEEDS_HOST_OPENSSL=y
-        BR2_PACKAGE_EFIVAR=y
-        BR2_TARGET_ROOTFS_EXT2=y
-        BR2_TARGET_EDK2=y
-        BR2_TARGET_GRUB2=y
-        BR2_TARGET_GRUB2_I386_EFI=y
-        BR2_TARGET_GRUB2_INSTALL_TOOLS=y
-        BR2_PACKAGE_HOST_GENIMAGE=y
-        BR2_PACKAGE_HOST_MTOOLS=y
-        BR2_PACKAGE_HOST_DOSFSTOOLS=y
-        """.format(infra.filepath("tests/boot/test_grub/post-build.sh"))
-
-    def test_run(self):
-        hda = os.path.join(self.builddir, "images", "disk.img")
-        bios = os.path.join(self.builddir, "images", "OVMF.fd")
-        # In QEMU v5.1.0 up to v7.2.0, the CPU hotplug register block misbehaves.
-        # EDK2 hang if the bug is detected in Qemu after printing errors to IO port 0x402
-        # (requires BR2_TARGET_EDK2_OVMF_DEBUG_ON_SERIAL to see them)
-        # The Docker image used by the Buildroot gitlab-ci uses Qemu 5.2.0, the workaround
-        # can be removed as soon as the Docker image is updated to provided Qemu >= 8.0.0.
-        # https://github.com/tianocore/edk2/commit/bf5678b5802685e07583e3c7ec56d883cbdd5da3
-        # http://lists.busybox.net/pipermail/buildroot/2023-July/670825.html
-        qemu_fw_cfg = "name=opt/org.tianocore/X-Cpuhp-Bugcheck-Override,string=yes"
-        self.emulator.boot(arch="i386", options=["-bios", bios, "-hda", hda, "-fw_cfg", qemu_fw_cfg])
-        self.emulator.login()
-
-        cmd = "modprobe efivarfs"
-        self.assertRunOk(cmd)
-
-        cmd = "mount -t efivarfs none /sys/firmware/efi/efivars"
-        self.assertRunOk(cmd)
-
-        cmd = "efivar -l"
-        self.assertRunOk(cmd)
-
-
 class TestGrubX8664EFI(infra.basetest.BRTest):
     config = \
         """
