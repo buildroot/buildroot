d0d88a9d472d67be6dee1a06ffec775e1cd8f9dc
Julien Olivain <ju.o@free.fr>
2026-01-24 14:13:35 +0100

    support/testing: fs/test_iso9660: remove 32bit EFI tests
    EDK2 removed OVMF IA32 support in commit [1], which is included in
    version edk2-stable202511. This commit removes tests relying on it.
    
    [1] https://github.com/tianocore/edk2/commit/1fb88ffe284782cc79e306306b8d19829b6248b7
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Romain Naour <romain.naour@smile.fr>
    (cherry picked from commit cf16e1abe01d76916a0951fc52e5b57c6998607f)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- support/testing/tests/fs/test_iso9660.py
+++ support/testing/tests/fs/test_iso9660.py
@@ -18,22 +18,9 @@ BASIC_CONFIG = \
     """.format(infra.filepath("conf/minimal-x86-qemu-kernel.config"))
 
 
-def test_mount_internal_external(emulator, builddir, internal=True, efi=False):
+def test_mount_internal_external(emulator, builddir, internal=True):
     img = os.path.join(builddir, "images", "rootfs.iso9660")
-    if efi:
-        efi_img = os.path.join(builddir, "images", "OVMF.fd")
-        # In QEMU v5.1.0 up to v7.2.0, the CPU hotplug register block misbehaves.
-        # EDK2 hang if the bug is detected in Qemu after printing errors to IO port 0x402
-        # (requires BR2_TARGET_EDK2_OVMF_DEBUG_ON_SERIAL to see them)
-        # The Docker image used by the Buildroot gitlab-ci uses Qemu 5.2.0, the workaround
-        # can be removed as soon as the Docker image is updated to provided Qemu >= 8.0.0.
-        # This workaround is needed only when efi=True since it imply EDK2 is used.
-        # https://github.com/tianocore/edk2/commit/bf5678b5802685e07583e3c7ec56d883cbdd5da3
-        # http://lists.busybox.net/pipermail/buildroot/2023-July/670825.html
-        qemu_fw_cfg = "name=opt/org.tianocore/X-Cpuhp-Bugcheck-Override,string=yes"
-        emulator.boot(arch="i386", options=["-cdrom", img, "-bios", efi_img, "-fw_cfg", qemu_fw_cfg])
-    else:
-        emulator.boot(arch="i386", options=["-cdrom", img])
+    emulator.boot(arch="i386", options=["-cdrom", img])
     emulator.login()
 
     if internal:
@@ -108,68 +95,6 @@ class TestIso9660Grub2Internal(infra.basetest.BRTest):
     def test_run(self):
         exit_code = test_mount_internal_external(self.emulator,
                                                  self.builddir, internal=True)
-        self.assertEqual(exit_code, 0)
-
-        exit_code = test_touch_file(self.emulator)
-        self.assertEqual(exit_code, 0)
-
-
-class TestIso9660Grub2EFI(infra.basetest.BRTest):
-    config = BASIC_CONFIG + \
-        """
-        BR2_TARGET_ROOTFS_ISO9660=y
-        BR2_TARGET_ROOTFS_ISO9660_INITRD=y
-        BR2_TARGET_GRUB2=y
-        BR2_TARGET_GRUB2_I386_EFI=y
-        BR2_TARGET_GRUB2_BUILTIN_MODULES_EFI="boot linux ext2 fat part_msdos part_gpt normal iso9660"
-        BR2_TARGET_GRUB2_BUILTIN_CONFIG_EFI="{}"
-        BR2_TARGET_ROOTFS_ISO9660_BOOT_MENU="{}"
-        BR2_TARGET_EDK2=y
-        """.format(infra.filepath("conf/grub2-efi.cfg"),
-                   infra.filepath("conf/grub2.cfg"))
-
-    def test_run(self):
-        exit_code = test_mount_internal_external(self.emulator,
-                                                 self.builddir, internal=True,
-                                                 efi=True)
-        self.assertEqual(exit_code, 0)
-
-        exit_code = test_touch_file(self.emulator)
-        self.assertEqual(exit_code, 0)
-
-
-class TestIso9660Grub2Hybrid(infra.basetest.BRTest):
-    config = BASIC_CONFIG + \
-        """
-        BR2_TARGET_ROOTFS_ISO9660=y
-        BR2_TARGET_ROOTFS_ISO9660_INITRD=y
-        BR2_TARGET_GRUB2=y
-        BR2_TARGET_GRUB2_I386_PC=y
-        BR2_TARGET_GRUB2_I386_EFI=y
-        BR2_TARGET_GRUB2_BOOT_PARTITION="cd"
-        BR2_TARGET_GRUB2_BUILTIN_MODULES_PC="boot linux ext2 fat squash4 part_msdos part_gpt normal iso9660 biosdisk"
-        BR2_TARGET_GRUB2_BUILTIN_CONFIG_PC=""
-        BR2_TARGET_GRUB2_BUILTIN_MODULES_EFI="boot linux ext2 fat squash4 part_msdos part_gpt normal iso9660 efi_gop"
-        BR2_TARGET_GRUB2_BUILTIN_CONFIG_EFI="{}"
-        BR2_TARGET_ROOTFS_ISO9660_BOOT_MENU="{}"
-        BR2_TARGET_EDK2=y
-        """.format(infra.filepath("conf/grub2-efi.cfg"),
-                   infra.filepath("conf/grub2.cfg"))
-
-    def test_run(self):
-        exit_code = test_mount_internal_external(self.emulator,
-                                                 self.builddir, internal=True,
-                                                 efi=False)
-        self.assertEqual(exit_code, 0)
-
-        exit_code = test_touch_file(self.emulator)
-        self.assertEqual(exit_code, 0)
-
-        self.emulator.stop()
-
-        exit_code = test_mount_internal_external(self.emulator,
-                                                 self.builddir, internal=True,
-                                                 efi=True)
         self.assertEqual(exit_code, 0)
 
         exit_code = test_touch_file(self.emulator)
