62ef552dd40842b0c7d8afe06fc57a41a5ff4895
Raphael Pavlidis <raphael.pavlidis@gmail.com>
2026-01-29 17:10:04 +0100

    package/linux-pam: security bump to version 1.7.2
    Fixes (in 1.7.1): CVE-2025-6020 - pam_namespace: potential privilege escalation
    
    https://github.com/linux-pam/linux-pam/security/advisories/GHSA-f9p8-gjr4-j9gx
    
    The build system was changed from autotools to meson in 1.7.0.
    
    Changelog:
    https://github.com/linux-pam/linux-pam/releases/tag/v1.7.0
    https://github.com/linux-pam/linux-pam/releases/tag/v1.7.1
    https://github.com/linux-pam/linux-pam/releases/tag/v1.7.2
    
    Signed-off-by: Raphael Pavlidis <raphael.pavlidis@gmail.com>
    [Marcus: add note about the CVE fixed in this bump]
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    (cherry picked from commit 30e38505e4bc6f8313509bdee2418024693088fa)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/linux-pam/linux-pam.mk
+++ package/linux-pam/linux-pam.mk
@@ -4,18 +4,15 @@
 #
 ################################################################################
 
-LINUX_PAM_VERSION = 1.6.1
+LINUX_PAM_VERSION = 1.7.2
 LINUX_PAM_SOURCE = Linux-PAM-$(LINUX_PAM_VERSION).tar.xz
 LINUX_PAM_SITE = https://github.com/linux-pam/linux-pam/releases/download/v$(LINUX_PAM_VERSION)
 LINUX_PAM_INSTALL_STAGING = YES
 LINUX_PAM_CONF_OPTS = \
-	--disable-prelude \
-	--disable-isadir \
-	--disable-nis \
-	--disable-db \
-	--disable-regenerate-docu \
-	--enable-securedir=/lib/security \
-	--libdir=/lib
+	-Disadir=disabled \
+	-Dnis=disabled \
+	-Dpam_userdb=disabled \
+	-Ddocs=disabled
 LINUX_PAM_DEPENDENCIES = host-flex host-pkgconf \
 	$(if $(BR2_PACKAGE_LIBXCRYPT),libxcrypt) \
 	$(TARGET_NLS_DEPENDENCIES)
@@ -25,46 +22,43 @@ LINUX_PAM_LIBS = $(TARGET_NLS_LIBS)
 LINUX_PAM_MAKE_OPTS += LIBS="$(LINUX_PAM_LIBS)"
 LINUX_PAM_CPE_ID_VENDOR = linux-pam
 
-# 0002-pam_access-rework-resolving-of-tokens-as-hostname.patch
-LINUX_PAM_IGNORE_CVES += CVE-2024-10963
-
 ifeq ($(BR2_TOOLCHAIN_HAS_LIBATOMIC),y)
 LINUX_PAM_LIBS += -latomic
 endif
 
 ifeq ($(BR2_PACKAGE_LIBSELINUX),y)
-LINUX_PAM_CONF_OPTS += --enable-selinux
+LINUX_PAM_CONF_OPTS += -Dselinux=enabled
 LINUX_PAM_DEPENDENCIES += libselinux
 define LINUX_PAM_SELINUX_PAMFILE_TWEAK
 	$(SED) 's/^# \(.*pam_selinux.so.*\)$$/\1/' \
 		$(TARGET_DIR)/etc/pam.d/login
 endef
 else
-LINUX_PAM_CONF_OPTS += --disable-selinux
+LINUX_PAM_CONF_OPTS += -Dselinux=disabled
 endif
 
 ifeq ($(BR2_PACKAGE_AUDIT),y)
-LINUX_PAM_CONF_OPTS += --enable-audit
+LINUX_PAM_CONF_OPTS += -Daudit=enabled
 LINUX_PAM_DEPENDENCIES += audit
 else
-LINUX_PAM_CONF_OPTS += --disable-audit
+LINUX_PAM_CONF_OPTS += -Daudit=disabled
 endif
 
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
-LINUX_PAM_CONF_OPTS += --enable-openssl
+LINUX_PAM_CONF_OPTS += -Dopenssl=enabled
 LINUX_PAM_DEPENDENCIES += openssl
 else
-LINUX_PAM_CONF_OPTS += --disable-openssl
+LINUX_PAM_CONF_OPTS += -Dopenssl=disabled
 endif
 
 ifeq ($(BR2_PACKAGE_LINUX_PAM_LASTLOG),y)
-LINUX_PAM_CONF_OPTS += --enable-lastlog
+LINUX_PAM_CONF_OPTS += -Dpam_lastlog=enabled
 define LINUX_PAM_LASTLOG_PAMFILE_TWEAK
 	$(SED) 's/^# \(.*pam_lastlog.so.*\)$$/\1/' \
 		$(TARGET_DIR)/etc/pam.d/login
 endef
 else
-LINUX_PAM_CONF_OPTS += --disable-lastlog
+LINUX_PAM_CONF_OPTS += -Dpam_lastlog=disabled
 endif
 
 # Install default pam config (deny everything except login)
@@ -79,4 +73,4 @@ endef
 
 LINUX_PAM_POST_INSTALL_TARGET_HOOKS += LINUX_PAM_INSTALL_CONFIG
 
-$(eval $(autotools-package))
+$(eval $(meson-package))
