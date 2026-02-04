d3108998390b9bbeffda3d030a2efffc830fefde
Thomas Perale <thomas.perale@mind.be>
2026-02-04 14:18:31 +0100

    package/heirloom-mailx: import patches from Debian
    In Buildroot there are multiple way to apply patches on a package [1]
    
    - Adding `.patch` file in the package directory.
    - Define `<pkg>_PATCH` variable with the location of the patch tar.gz.
      It used to download Debian patches tarball.
    - Implement custom patching logic with `PRE`/`POST` patches hooks.
    
    To make the CycloneDX SBOM generation not dependant on downloading the
    packages, the two last options have the downside of not appearing on the
    generated SBOM.
    
    The heirloom-mailx package is downloading a tarball from the Debian
    mirror with the `<pkg>_PATCH` method [2].
    
    To improve the tracking of the patched vulnerabilities for the
    heirloom-mailx package this commit import the patches previously
    downloaded with the `_PATCH` variable in the Buildroot tree. This allows
    to add the `CVE:` trailer [3] on the patches that fix vulnerabilities to
    better track which patch is fixing the vulnerability.
    
    [1] https://buildroot.org/downloads/manual/manual.html#patch-policy
    [2] http://snapshot.debian.org/archive/debian/20150815T155609Z/pool/main/h/heirloom-mailx/heirloom-mailx_12.5-5.debian.tar.xz
    [3] 1167d0ff3d docs/manual: mention CVE trailer
    
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit c9659fd9e8475a43d4dafc6df19962882046d252)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/heirloom-mailx/heirloom-mailx.mk
+++ package/heirloom-mailx/heirloom-mailx.mk
@@ -7,14 +7,13 @@
 HEIRLOOM_MAILX_VERSION = 12.5
 HEIRLOOM_MAILX_SOURCE = heirloom-mailx_$(HEIRLOOM_MAILX_VERSION).orig.tar.gz
 HEIRLOOM_MAILX_SITE = http://snapshot.debian.org/archive/debian/20150815T155609Z/pool/main/h/heirloom-mailx
-HEIRLOOM_MAILX_PATCH = heirloom-mailx_$(HEIRLOOM_MAILX_VERSION)-5.debian.tar.xz
 HEIRLOOM_MAILX_LICENSE = BSD-4-Clause, Bellcore (base64), OpenVision (imap_gssapi), RSA Data Security (md5), Network Working Group (hmac), MPL-1.1 (nss)
 HEIRLOOM_MAILX_LICENSE_FILES = COPYING
 HEIRLOOM_MAILX_CPE_ID_VENDOR = heirloom
 HEIRLOOM_MAILX_CPE_ID_PRODUCT = mailx
-# 0011-outof-Introduce-expandaddr-flag.patch in the Debian patches
+# 0011-outof-Introduce-expandaddr-flag.patch
 HEIRLOOM_MAILX_IGNORE_CVES += CVE-2014-7844
-# 0014-globname-Invoke-wordexp-with-WRDE_NOCMD.patch in the Debian patches
+# 0014-globname-Invoke-wordexp-with-WRDE_NOCMD.patch
 HEIRLOOM_MAILX_IGNORE_CVES += CVE-2004-2771
 
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
