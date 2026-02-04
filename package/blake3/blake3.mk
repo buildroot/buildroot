830726905accf698fb8621fa07d962a7ab97ce40
Yann E. MORIN <yann.morin@orange.com>
2026-02-04 13:37:14 +0100

    package/blake3: explicitly disable threading & examples
    Use of threading requires a C++20 compiler, and the oneTBB
    implementation. oneTBB is missing from Buildroot, but a system
    one may be used if found.
    
    Even if the default for threading is disabled, explicitly state so,
    in case the default changes in the future.
    
    Also disable examples, we don't and won't need them.
    
    Signed-off-by: Yann E. MORIN <yann.morin@orange.com>
    Cc: Heiko Thiery <heiko.thiery@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/blake3/blake3.mk
+++ package/blake3/blake3.mk
@@ -10,6 +10,10 @@ BLAKE3_SUBDIR = c
 BLAKE3_LICENSE = Apache-2.0 or Apache-2.0 with exceptions or CC0-1.0
 BLAKE3_LICENSE_FILES = LICENSE_A2 LICENSE_A2LLVM LICENSE_CC0
 
+HOST_BLAKE3_CONF_OPTS = \
+	-DBLAKE3_USE_TBB=OFF \
+	-DBLAKE3_EXAMPLES=OFF
+
 # The package is a dependency to ccache so ccache cannot be a dependency
 HOST_BLAKE3_ADD_CCACHE_DEPENDENCY = NO
 
