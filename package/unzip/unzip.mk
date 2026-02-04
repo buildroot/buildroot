fb8958e3dcb3800a772e743ae12f12c95dbcd4e7
Thomas Perale <thomas.perale@mind.be>
2026-02-04 14:18:30 +0100

    package/unzip: import patches from Debian
    In Buildroot there are multiple way to apply patches on a package [1]
    
    - Adding `.patch` file in the package directory.
    - Define `<pkg>_PATCH` variable with the location of the patch tar.gz.
      It used to download Debian patches tarball.
    - Implement custom patching logic with `PRE`/`POST` patches hooks.
    
    To make the CycloneDX SBOM generation not dependant on downloading the
    packages, the two last options have the downside of not appearing on the
    generated SBOM.
    
    The unzip package is downloading a tarball from the Debian mirror with
    the `<pkg>_PATCH` method [2].
    
    To improve the tracking of the patched vulnerabilities for the unzip
    package this commit import the patches previously downloaded with the
    `_PATCH` variable in the Buildroot tree.
    This allows to add the `CVE:` trailer [3] on the patches that fix
    vulnerabilities to better track which patch is fixing the vulnerability.
    
    [1] https://buildroot.org/downloads/manual/manual.html#patch-policy
    [2] https://snapshot.debian.org/archive/debian/20250311T215724Z/pool/main/u/unzip/unzip_6.0-29.debian.tar.xz
    [3] 1167d0ff3d docs/manual: mention CVE trailer
    
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/unzip/unzip.mk
+++ package/unzip/unzip.mk
@@ -6,27 +6,50 @@
 
 UNZIP_VERSION = 6.0
 UNZIP_SOURCE = unzip_$(UNZIP_VERSION).orig.tar.gz
-UNZIP_PATCH = unzip_$(UNZIP_VERSION)-29.debian.tar.xz
 UNZIP_SITE = https://snapshot.debian.org/archive/debian/20250311T215724Z/pool/main/u/unzip
 UNZIP_LICENSE = Info-ZIP
 UNZIP_LICENSE_FILES = LICENSE
 UNZIP_CPE_ID_VALID = YES
 
-# unzip_$(UNZIP_VERSION)-29.debian.tar.xz has patches to fix:
-UNZIP_IGNORE_CVES = \
-	CVE-2014-8139 \
-	CVE-2014-8140 \
-	CVE-2014-8141 \
-	CVE-2014-9636 \
-	CVE-2014-9913 \
-	CVE-2015-7696 \
-	CVE-2015-7697 \
-	CVE-2016-9844 \
-	CVE-2018-18384 \
-	CVE-2018-1000035 \
-	CVE-2019-13232 \
-	CVE-2022-0529 \
-	CVE-2022-0530
+# 0009-cve-2014-8139-crc-overflow.patch
+UNZIP_IGNORE_CVES += CVE-2014-8139
+
+# 0010-cve-2014-8140-test-compr-eb.patch
+UNZIP_IGNORE_CVES += CVE-2014-8140
+
+# 0011-cve-2014-8141-getzip64data.patch
+UNZIP_IGNORE_CVES += CVE-2014-8141
+
+# 0012-cve-2014-9636-test-compr-eb.patch
+UNZIP_IGNORE_CVES += CVE-2014-9636
+
+# 0018-cve-2014-9913-unzip-buffer-overflow.patch
+UNZIP_IGNORE_CVES += CVE-2014-9913
+
+# 0014-cve-2015-7696.patch
+UNZIP_IGNORE_CVES += CVE-2015-7696
+
+# 0015-cve-2015-7697.patch
+UNZIP_IGNORE_CVES += CVE-2015-7697
+
+# 0019-cve-2016-9844-zipinfo-buffer-overflow.patch
+UNZIP_IGNORE_CVES += CVE-2016-9844
+
+# 0007-increase-size-of-cfactorstr.patch
+UNZIP_IGNORE_CVES += CVE-2018-18384
+
+# 0020-cve-2018-1000035-unzip-buffer-overflow.patch
+UNZIP_IGNORE_CVES += CVE-2018-1000035
+
+# 0022-cve-2019-13232-fix-bug-in-undefer-input.patch
+# 0023-cve-2019-13232-zip-bomb-with-overlapped-entries.patch
+# 0024-cve-2019-13232-do-not-raise-alert-for-misplaced-central-directory.patch
+# 0025-cve-2019-13232-fix-bug-in-uzbunzip2.patch
+# 0026-cve-2019-13232-fix-bug-in-uzinflate.patch
+UNZIP_IGNORE_CVES += CVE-2019-13232
+
+# 0028-cve-2022-0529-and-cve-2022-0530.patch
+UNZIP_IGNORE_CVES += CVE-2022-0529 CVE-2022-0530
 
 # unzip already defines _LARGEFILE_SOURCE and _LARGEFILE64_SOURCE when
 # necessary, redefining it on the command line causes some warnings.
