87d2e5ff59ffaa94000a4776a3f5ab7f60a23fda
Bernd Kuhls <bernd@kuhls.net>
2026-02-05 20:52:58 +0100

    package/sane-airscan: bump version to 0.99.36
    https://github.com/alexpevzner/sane-airscan/commits/0.99.36/
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/sane-airscan/sane-airscan.mk
+++ package/sane-airscan/sane-airscan.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SANE_AIRSCAN_VERSION = 0.99.33
+SANE_AIRSCAN_VERSION = 0.99.36
 SANE_AIRSCAN_SITE = $(call github,alexpevzner,sane-airscan,$(SANE_AIRSCAN_VERSION))
 SANE_AIRSCAN_DEPENDENCIES = avahi gnutls jpeg libpng libxml2 sane-backends tiff
 SANE_AIRSCAN_LICENSE = GPL-2.0+-with-exception
