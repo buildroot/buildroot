875d6aedbc639484e9203c5bcda1a55ecb7ad4c9
Bernd Kuhls <bernd@kuhls.net>
2026-02-09 21:31:32 +0100

    package/m4: bump version to 1.4.21
    https://lists.gnu.org/archive/html/m4-announce/2026-02/msg00000.html
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>

--- package/m4/m4.mk
+++ package/m4/m4.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-M4_VERSION = 1.4.20
+M4_VERSION = 1.4.21
 M4_SOURCE = m4-$(M4_VERSION).tar.xz
 M4_SITE = $(BR2_GNU_MIRROR)/m4
 M4_LICENSE = GPL-3.0+
