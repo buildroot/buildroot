e7fce26fa04522ebcc785dca0a5a794bf4c79042
Thomas Perale <thomas.perale@mind.be>
2026-02-13 15:34:59 +0100

    package/python-jinja2: update CPE
    The `pocoo:jinja2` CPE introduced in commit [1] is not deprecated but is
    no longer used. All the security issue on the Github Advisory reference
    CVE with the `palletsprojects:jinja` CPE instead (see [2]).
    
    This commit updates the CPE accordingly.
    
    [1] 165f60a092 package/python-jinja2: add CPE variables
    [2] https://github.com/pallets/jinja/security/advisories/GHSA-cpwx-vrp4-4pq7
    
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit c01c53f5e8ea212c826e9c276d69a900dfbbbace)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/python-jinja2/python-jinja2.mk
+++ package/python-jinja2/python-jinja2.mk
@@ -10,8 +10,8 @@ PYTHON_JINJA2_SITE = https://files.pythonhosted.org/packages/df/bf/f7da0350254c0
 PYTHON_JINJA2_SETUP_TYPE = flit
 PYTHON_JINJA2_LICENSE = BSD-3-Clause
 PYTHON_JINJA2_LICENSE_FILES = LICENSE.txt
-PYTHON_JINJA2_CPE_ID_VENDOR = pocoo
-PYTHON_JINJA2_CPE_ID_PRODUCT = jinja2
+PYTHON_JINJA2_CPE_ID_VENDOR = palletsprojects
+PYTHON_JINJA2_CPE_ID_PRODUCT = jinja
 
 # In host/target build, setup.py tries to download markupsafe if it is not installed
 PYTHON_JINJA2_DEPENDENCIES = host-python-markupsafe
