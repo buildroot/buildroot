cb0b183aba99c15872aeebef272013ad9d4da52b
Alexander Shirokov <shirokovalexs@gmail.com>
2026-02-14 13:19:32 +0100

    package/broot: bump to version 1.55
    Changelog: https://github.com/Canop/broot/blob/v1.55.0/CHANGELOG.md
    
    Signed-off-by: Alexander Shirokov <shirokovalexs@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/broot/broot.mk
+++ package/broot/broot.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BROOT_VERSION = 1.54.0
+BROOT_VERSION = 1.55.0
 BROOT_SITE = $(call github,Canop,broot,v$(BROOT_VERSION))
 BROOT_LICENSE = MIT
 BROOT_LICENSE_FILES = LICENSE
