46baf529c5847ca814f94be3856dbd104ed88b73
Fiona Klute <fiona.klute@gmx.de>
2026-02-15 22:39:12 +0100

    package/python-pyasynchat: bump to version 1.0.5
    Changes: https://github.com/simonrob/pyasynchat/releases/tag/v1.0.5
    
    Signed-off-by: Fiona Klute <fiona.klute@gmx.de>
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>

--- package/python-pyasynchat/python-pyasynchat.mk
+++ package/python-pyasynchat/python-pyasynchat.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYASYNCHAT_VERSION = 1.0.4
+PYTHON_PYASYNCHAT_VERSION = 1.0.5
 PYTHON_PYASYNCHAT_SOURCE = pyasynchat-$(PYTHON_PYASYNCHAT_VERSION).tar.gz
-PYTHON_PYASYNCHAT_SITE = https://files.pythonhosted.org/packages/8a/fd/aacc6309abcc5a388c66915829cd8175daccac583828fde40a1eea5768e4
+PYTHON_PYASYNCHAT_SITE = https://files.pythonhosted.org/packages/ec/d2/b41df9021c12ca314146abcde7bdd3d9d37d44cc01559d7f13df459ee586
 PYTHON_PYASYNCHAT_SETUP_TYPE = setuptools
 PYTHON_PYASYNCHAT_LICENSE = PSF-2.0
 PYTHON_PYASYNCHAT_LICENSE_FILES = LICENSE
