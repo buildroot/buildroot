8340535bb354b580c63d81d791dbeea705ed873f
Fiona Klute <fiona.klute@gmx.de>
2026-02-15 22:39:11 +0100

    package/python-textual: bump to version 7.5.0
    Changes:
    https://github.com/Textualize/textual/releases/tag/v7.0.1
    https://github.com/Textualize/textual/releases/tag/v7.0.2
    https://github.com/Textualize/textual/releases/tag/v7.0.3
    https://github.com/Textualize/textual/releases/tag/v7.1.0
    https://github.com/Textualize/textual/releases/tag/v7.2.0
    https://github.com/Textualize/textual/releases/tag/v7.3.0
    https://github.com/Textualize/textual/releases/tag/v7.4.0
    https://github.com/Textualize/textual/releases/tag/v7.5.0
    
    Signed-off-by: Fiona Klute <fiona.klute@gmx.de>
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>

--- package/python-textual/python-textual.mk
+++ package/python-textual/python-textual.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_TEXTUAL_VERSION = 7.0.0
+PYTHON_TEXTUAL_VERSION = 7.5.0
 PYTHON_TEXTUAL_SOURCE = textual-$(PYTHON_TEXTUAL_VERSION).tar.gz
-PYTHON_TEXTUAL_SITE = https://files.pythonhosted.org/packages/d4/9c/ebc9ca1f95366bef4c0e8360f4a77400d47a79aeecc08747de1990ef8bdc
+PYTHON_TEXTUAL_SITE = https://files.pythonhosted.org/packages/9f/38/7d169a765993efde5095c70a668bf4f5831bb7ac099e932f2783e9b71abf
 PYTHON_TEXTUAL_SETUP_TYPE = poetry
 PYTHON_TEXTUAL_LICENSE = MIT
 PYTHON_TEXTUAL_LICENSE_FILES = LICENSE
