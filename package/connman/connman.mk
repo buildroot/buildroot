461eac6f23181c008110c11e911dfb453886a732
Dario Binacchi <dario.binacchi@amarulasolutions.com>
2026-02-17 17:45:24 +0100

    package/connman: bump to version 2.0
    Release notes:
    https://git.kernel.org/pub/scm/network/connman/connman.git/tree/ChangeLog?h=2.0
    https://git.kernel.org/pub/scm/network/connman/connman.git/log/?h=2.0
    
    Signed-off-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/connman/connman.mk
+++ package/connman/connman.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CONNMAN_VERSION = 1.45
+CONNMAN_VERSION = 2.0
 CONNMAN_SOURCE = connman-$(CONNMAN_VERSION).tar.xz
 CONNMAN_SITE = $(BR2_KERNEL_MIRROR)/linux/network/connman
 CONNMAN_DEPENDENCIES = libglib2 dbus
