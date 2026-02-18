7dfa4d3a25076361fbe8ec1ba7d4029830e93103
Manuel Diener <manuel.diener@oss.othermo.de>
2026-02-18 16:04:50 +0100

    package/python-starlette.mk: bump to version 0.52.1
    See the release notes here: https://starlette.dev/release-notes/
    This also includes the versions 0.51.0 and 0.52.0
    
    Signed-off-by: Manuel Diener <manuel.diener@oss.othermo.de>
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>

--- package/python-starlette/python-starlette.mk
+++ package/python-starlette/python-starlette.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_STARLETTE_VERSION = 0.50.0
+PYTHON_STARLETTE_VERSION = 0.52.1
 PYTHON_STARLETTE_SOURCE = starlette-$(PYTHON_STARLETTE_VERSION).tar.gz
-PYTHON_STARLETTE_SITE = https://files.pythonhosted.org/packages/ba/b8/73a0e6a6e079a9d9cfa64113d771e421640b6f679a52eeb9b32f72d871a1
+PYTHON_STARLETTE_SITE = https://files.pythonhosted.org/packages/c4/68/79977123bb7be889ad680d79a40f339082c1978b5cfcf62c2d8d196873ac
 PYTHON_STARLETTE_SETUP_TYPE = hatch
 PYTHON_STARLETTE_LICENSE = BSD-3-Clause
 PYTHON_STARLETTE_LICENSE_FILES = LICENSE.md
