6c83a0b8e5658b901dd35ae23217075f035af998
Manuel Diener <manuel.diener@oss.othermo.de>
2026-02-18 15:00:29 +0100

    package/python-pymodbus: bump to version 3.12.0
    See the release notes here: https://github.com/pymodbus-dev/pymodbus/releases/tag/v3.12.0
    
    Signed-off-by: Manuel Diener <manuel.diener@oss.othermo.de>
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>

--- package/python-pymodbus/python-pymodbus.mk
+++ package/python-pymodbus/python-pymodbus.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYMODBUS_VERSION = 3.11.4
+PYTHON_PYMODBUS_VERSION = 3.12.0
 PYTHON_PYMODBUS_SOURCE = pymodbus-$(PYTHON_PYMODBUS_VERSION).tar.gz
-PYTHON_PYMODBUS_SITE = https://files.pythonhosted.org/packages/2a/af/bbb716301ab9c60f0702c3cdf72cc0e373286a5648fe16bc4431400489dc
+PYTHON_PYMODBUS_SITE = https://files.pythonhosted.org/packages/41/dc/8db192f3a66032e1bd900eb87b5bbcb539d5118f520372af0cb92ba42499
 PYTHON_PYMODBUS_SETUP_TYPE = setuptools
 PYTHON_PYMODBUS_LICENSE = BSD-3-Clause
 PYTHON_PYMODBUS_LICENSE_FILES = LICENSE
