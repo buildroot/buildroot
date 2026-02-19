91a0ef51833094ed932535f6e4760b85914f5a06
Marcus Hoffmann <buildroot@bubu1.eu>
2026-02-19 10:33:47 +0100

    package/systemd: bump to 258.4
    Changes: https://github.com/systemd/systemd/compare/v258.3...v258.4
    
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/systemd/systemd.mk
+++ package/systemd/systemd.mk
@@ -19,7 +19,7 @@
 # - Diff sysusers.d with the previous version
 # - Diff factory/etc/nsswitch.conf with the previous version
 #   (details are often sprinkled around in README and manpages)
-SYSTEMD_VERSION = 258.3
+SYSTEMD_VERSION = 258.4
 SYSTEMD_SITE = $(call github,systemd,systemd,v$(SYSTEMD_VERSION))
 SYSTEMD_LICENSE = \
 	LGPL-2.1+, \
