071216c81d8612516e3304b41273437df45a1451
Bernd Kuhls <bernd@kuhls.net>
2026-02-20 20:47:02 +0100

    package/wmctrl: fix tarball URL
    Current site is down, switch to Debian.
    
    Fixes:
    https://autobuild.buildroot.net/results/679/679a9f6224d1771ec2ab52e45fd74af8d03c1604/
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit f90d38dafcadf959e6a0383376639a137a8bae4d)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/wmctrl/wmctrl.mk
+++ package/wmctrl/wmctrl.mk
@@ -5,7 +5,8 @@
 ################################################################################
 
 WMCTRL_VERSION = 1.07
-WMCTRL_SITE = https://sites.google.com/site/tstyblo/wmctrl
+WMCTRL_SOURCE = wmctrl_$(WMCTRL_VERSION).orig.tar.gz
+WMCTRL_SITE = https://snapshot.debian.org/archive/debian/20050312T000000Z/pool/main/w/wmctrl
 WMCTRL_LICENSE = GPL-2.0+
 WMCTRL_LICENSE_FILES = COPYING
 
