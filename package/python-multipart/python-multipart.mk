568fd8e6a3ed9a65df08228cd8aab777f2fb80db
Martin Bachmann <martin.bachmann@designwerk.com>
2026-02-25 12:49:15 +0000

    package/python-multipart: security bump to version 0.0.22
    For release notes, see:
    https://github.com/Kludex/python-multipart/releases/tag/0.0.22
    
    Fixes:
    https://www.cve.org/CVERecord?id=CVE-2026-24486
    
    Signed-off-by: Martin Bachmann <martin.bachmann@designwerk.com>
    [Julien: reword commit log to mark the commit as a security bump]
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 7756b39d64f97b386dc993080e1ec4c90f18eddc)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/python-multipart/python-multipart.mk
+++ package/python-multipart/python-multipart.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_MULTIPART_VERSION = 0.0.21
+PYTHON_MULTIPART_VERSION = 0.0.22
 PYTHON_MULTIPART_SOURCE = python_multipart-$(PYTHON_MULTIPART_VERSION).tar.gz
-PYTHON_MULTIPART_SITE = https://files.pythonhosted.org/packages/78/96/804520d0850c7db98e5ccb70282e29208723f0964e88ffd9d0da2f52ea09
+PYTHON_MULTIPART_SITE = https://files.pythonhosted.org/packages/94/01/979e98d542a70714b0cb2b6728ed0b7c46792b695e3eaec3e20711271ca3
 PYTHON_MULTIPART_SETUP_TYPE = hatch
 PYTHON_MULTIPART_LICENSE = Apache-2.0
 PYTHON_MULTIPART_LICENSE_FILES = LICENSE.txt
