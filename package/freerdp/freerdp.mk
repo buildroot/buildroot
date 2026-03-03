c68c55964c688ee0d4eff4dd48569059d373262e
Thomas Perale <thomas.perale@mind.be>
2026-03-03 11:12:51 +0100

    package/freerdp: bump to v2.11.8
    For more information on the release, see:
    
    - https://github.com/FreeRDP/FreeRDP/commit/9b678b6d5a40ce01607d8c3b1b1416437c8416c4
    - https://github.com/FreeRDP/FreeRDP/releases/tag/2.11.8
    - https://github.com/FreeRDP/FreeRDP/compare/2.11.7...2.11.8
    
    Note: the previous freerdp version 2.11.7-18-g0ee17e2f in Buildroot
    is only 5 commits prior the 2.11.8 tag. This is a very minimal bump.
    
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>
    [Julien: add note in commit log]
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 73861bb720a82316cb56e644fc75d3a7ce95980e)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/freerdp/freerdp.mk
+++ package/freerdp/freerdp.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-# Latest, and probably last, commit on the stable-2.0 branch
-FREERDP_VERSION = 2.11.7-18-g0ee17e2f8e49d56ab5b90d5160fa8f87ffc445e0
+# Latest, and probably last, release on the stable-2.0 branch
+FREERDP_VERSION = 2.11.8
 FREERDP_SITE = https://github.com/FreeRDP/FreeRDP
 FREERDP_SITE_METHOD = git
 FREERDP_DEPENDENCIES = libglib2 openssl zlib
