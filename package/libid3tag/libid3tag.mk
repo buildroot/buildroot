0ed1707ba559103f0aa298d40b5017b6bad0394c
Bernd Kuhls <bernd@kuhls.net>
2026-03-04 20:12:17 +0100

    package/libid3tag: bump version to 0.16.4
    https://codeberg.org/tenacityteam/libid3tag/releases/tag/0.16.4
    "We now provide our own source packages instead of using Codeberg's
     pre-generated sources, guaranteeing that the hash won't change."
    
    Removed patches which are included in this bump.
    
    Used tarball and its hashes provided by upstream.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libid3tag/libid3tag.mk
+++ package/libid3tag/libid3tag.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-LIBID3TAG_VERSION = 0.16.3
-LIBID3TAG_SOURCE = $(LIBID3TAG_VERSION).tar.gz
-LIBID3TAG_SITE = https://codeberg.org/tenacityteam/libid3tag/archive
+LIBID3TAG_VERSION = 0.16.4
+LIBID3TAG_SOURCE = id3tag-$(LIBID3TAG_VERSION)-source.tar.gz
+LIBID3TAG_SITE = https://codeberg.org/tenacityteam/libid3tag/releases/download/$(LIBID3TAG_VERSION)
 LIBID3TAG_LICENSE = GPL-2.0+
 LIBID3TAG_LICENSE_FILES = COPYING COPYRIGHT
 LIBID3TAG_INSTALL_STAGING = YES
