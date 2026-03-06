d7e8b0d2375ae70605e02426ab3d9ed9b10b82cb
Bernd Kuhls <bernd@kuhls.net>
2026-03-06 23:33:28 +0100

    package/libid3tag: needs host-gperf
    Buildroot commit 0ed1707ba559103f0aa298d40b5017b6bad0394c bumped the
    package to 0.16.4 which re-added the dependency to host-gperf:
    https://codeberg.org/tenacityteam/libid3tag/commit/2d7e73eb9f580b2258e434e0a8d63c69687ff5ef
    
    Fixes:
    https://autobuild.buildroot.net/results/133/133adafe89bc2b7df28dcb10eb30c19272100670/
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libid3tag/libid3tag.mk
+++ package/libid3tag/libid3tag.mk
@@ -10,6 +10,6 @@ LIBID3TAG_SITE = https://codeberg.org/tenacityteam/libid3tag/releases/download/$
 LIBID3TAG_LICENSE = GPL-2.0+
 LIBID3TAG_LICENSE_FILES = COPYING COPYRIGHT
 LIBID3TAG_INSTALL_STAGING = YES
-LIBID3TAG_DEPENDENCIES = zlib
+LIBID3TAG_DEPENDENCIES = host-gperf zlib
 
 $(eval $(cmake-package))
