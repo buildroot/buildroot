c3c045546a70b8a94ecab9a079aa8e5ce5d2b92c
Bernd Kuhls <bernd@kuhls.net>
2026-03-14 15:05:38 +0100

    package/python-pkgconfig: bump version to 1.6.0
    https://github.com/matze/pkgconfig?tab=readme-ov-file#changelog
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-pkgconfig/python-pkgconfig.mk
+++ package/python-pkgconfig/python-pkgconfig.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PKGCONFIG_VERSION = 1.5.5
+PYTHON_PKGCONFIG_VERSION = 1.6.0
 PYTHON_PKGCONFIG_SOURCE = pkgconfig-$(PYTHON_PKGCONFIG_VERSION).tar.gz
-PYTHON_PKGCONFIG_SITE = https://files.pythonhosted.org/packages/c4/e0/e05fee8b5425db6f83237128742e7e5ef26219b687ab8f0d41ed0422125e
+PYTHON_PKGCONFIG_SITE = https://files.pythonhosted.org/packages/52/fd/0adde075cd3bfecd557bc7d757e00e231d34d8a6edb4c8d1642759254c21
 PYTHON_PKGCONFIG_SETUP_TYPE = poetry
 PYTHON_PKGCONFIG_LICENSE = MIT
 PYTHON_PKGCONFIG_LICENSE_FILES = LICENSE
