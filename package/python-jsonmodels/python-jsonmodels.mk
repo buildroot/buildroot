348d65c7260f3b6205e1fe39de1d9a3430ac4cb9
Bernd Kuhls <bernd@kuhls.net>
2026-03-14 15:05:25 +0100

    package/python-jsonmodels: bump version to 2.8.0
    https://github.com/jazzband/jsonmodels/blob/2.8.0/HISTORY.rst
    
    Upstream switched to hatch:
    https://github.com/jazzband/jsonmodels/commit/1c4dab695a6187ff19e5a8e0aced313af6de28a5
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-jsonmodels/python-jsonmodels.mk
+++ package/python-jsonmodels/python-jsonmodels.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_JSONMODELS_VERSION = 2.4
+PYTHON_JSONMODELS_VERSION = 2.8.0
 PYTHON_JSONMODELS_SOURCE = jsonmodels-$(PYTHON_JSONMODELS_VERSION).tar.gz
-PYTHON_JSONMODELS_SITE = https://files.pythonhosted.org/packages/68/00/524668dc751f9ef91e73c795b2073bf2ddb79728a474d1bcab9c6dc426d8
-PYTHON_JSONMODELS_SETUP_TYPE = setuptools
+PYTHON_JSONMODELS_SITE = https://files.pythonhosted.org/packages/04/85/0e02cbc656ef7f0f5d3db13be5b622c231bf55bbad6a8c4612ce95fcb8cf
+PYTHON_JSONMODELS_SETUP_TYPE = hatch
 PYTHON_JSONMODELS_LICENSE = BSD-3-Clause
 PYTHON_JSONMODELS_LICENSE_FILES = LICENSE
 
