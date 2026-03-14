97c0c97452ad77ccbfdad6808e99611100ddab3b
Bernd Kuhls <bernd@kuhls.net>
2026-03-14 15:06:09 +0100

    package/python-tinyhtml5: bump version to 2.1.0
    https://github.com/CourtBouillon/tinyhtml5/blob/2.1.0/docs/changelog.rst
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-tinyhtml5/python-tinyhtml5.mk
+++ package/python-tinyhtml5/python-tinyhtml5.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_TINYHTML5_VERSION = 2.0.0
+PYTHON_TINYHTML5_VERSION = 2.1.0
 PYTHON_TINYHTML5_SOURCE = tinyhtml5-$(PYTHON_TINYHTML5_VERSION).tar.gz
-PYTHON_TINYHTML5_SITE = https://files.pythonhosted.org/packages/fd/03/6111ed99e9bf7dfa1c30baeef0e0fb7e0bd387bd07f8e5b270776fe1de3f
+PYTHON_TINYHTML5_SITE = https://files.pythonhosted.org/packages/b1/1f/cfe2f6b30557c92b3f31d41707e09cef5c1efbd87392bc6c0430c46b0e4d
 PYTHON_TINYHTML5_SETUP_TYPE = flit
 PYTHON_TINYHTML5_LICENSE = MIT
 PYTHON_TINYHTML5_LICENSE_FILES = LICENSE
