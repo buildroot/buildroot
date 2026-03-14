bdacb1a48389f4ee41facb56c941e3ca50f6c431
Bernd Kuhls <bernd@kuhls.net>
2026-03-14 15:05:00 +0100

    package/python-aiorwlock: bump version to 1.5.1
    https://github.com/aio-libs/aiorwlock/blob/v1.5.1/CHANGES.rst
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-aiorwlock/python-aiorwlock.mk
+++ package/python-aiorwlock/python-aiorwlock.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_AIORWLOCK_VERSION = 1.5.0
+PYTHON_AIORWLOCK_VERSION = 1.5.1
 PYTHON_AIORWLOCK_SOURCE = aiorwlock-$(PYTHON_AIORWLOCK_VERSION).tar.gz
-PYTHON_AIORWLOCK_SITE = https://files.pythonhosted.org/packages/c5/bf/d1ddcd676be027a963b3b01fdf9915daf4590b4dfd03bf1c8c2858aac7e3
+PYTHON_AIORWLOCK_SITE = https://files.pythonhosted.org/packages/6b/65/316cdc82c1b92953235ced1c71a3763f0cd9273c3bec5db60bdb5ad59bfe
 PYTHON_AIORWLOCK_SETUP_TYPE = poetry
 PYTHON_AIORWLOCK_LICENSE = Apache-2.0
 PYTHON_AIORWLOCK_LICENSE_FILES = LICENSE
