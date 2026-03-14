6ebadc0d9486e8855dac9e2513d2ce894ed449a9
Bernd Kuhls <bernd@kuhls.net>
2026-03-14 15:06:27 +0100

    package/python-whitenoise: bump version to 6.12.0
    https://github.com/evansd/whitenoise/blob/6.12.0/docs/changelog.rst
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-whitenoise/python-whitenoise.mk
+++ package/python-whitenoise/python-whitenoise.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_WHITENOISE_VERSION = 6.11.0
+PYTHON_WHITENOISE_VERSION = 6.12.0
 PYTHON_WHITENOISE_SOURCE = whitenoise-$(PYTHON_WHITENOISE_VERSION).tar.gz
-PYTHON_WHITENOISE_SITE = https://files.pythonhosted.org/packages/15/95/8c81ec6b6ebcbf8aca2de7603070ccf37dbb873b03f20708e0f7c1664bc6
+PYTHON_WHITENOISE_SITE = https://files.pythonhosted.org/packages/cb/2a/55b3f3a4ec326cd077c1c3defeee656b9298372a69229134d930151acd01
 PYTHON_WHITENOISE_SETUP_TYPE = setuptools
 PYTHON_WHITENOISE_LICENSE = MIT
 PYTHON_WHITENOISE_LICENSE_FILES = LICENSE
