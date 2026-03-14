9e08e5029fb375fad5a62c767acd1309f090c79b
Bernd Kuhls <bernd@kuhls.net>
2026-03-14 15:04:59 +0100

    package/python-aiomqtt: bump version to 2.5.1
    https://github.com/empicano/aiomqtt/blob/v2.5.1/CHANGELOG.md
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-aiomqtt/python-aiomqtt.mk
+++ package/python-aiomqtt/python-aiomqtt.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_AIOMQTT_VERSION = 2.4.0
+PYTHON_AIOMQTT_VERSION = 2.5.1
 PYTHON_AIOMQTT_SOURCE = aiomqtt-$(PYTHON_AIOMQTT_VERSION).tar.gz
-PYTHON_AIOMQTT_SITE = https://files.pythonhosted.org/packages/45/9a/863bc34c64bc4acb9720a9950bfc77d6f324640cdf1f420bb5d9ee624975
+PYTHON_AIOMQTT_SITE = https://files.pythonhosted.org/packages/70/44/cfc58272783a11729462dc6df5adbfeabd084f840f609054ac772ae98c19
 PYTHON_AIOMQTT_SETUP_TYPE = hatch
 PYTHON_AIOMQTT_LICENSE = BSD-3-Clause
 PYTHON_AIOMQTT_LICENSE_FILES = LICENSE
