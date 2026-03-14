29bc88d4fb42ffaa561bc9fa7891325c551496a4
Bernd Kuhls <bernd@kuhls.net>
2026-03-14 15:06:23 +0100

    package/python-weasyprint: security bump version to 68.1
    https://github.com/Kozea/WeasyPrint/blob/v68.1/docs/changelog.rst
    
    Version 68.0 fixes CVE-2025-68616.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-weasyprint/python-weasyprint.mk
+++ package/python-weasyprint/python-weasyprint.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_WEASYPRINT_VERSION = 67.0
+PYTHON_WEASYPRINT_VERSION = 68.1
 PYTHON_WEASYPRINT_SOURCE = weasyprint-$(PYTHON_WEASYPRINT_VERSION).tar.gz
-PYTHON_WEASYPRINT_SITE = https://files.pythonhosted.org/packages/fd/bc/79a65b3a406cb62a1982fec8b49134b25a3b31abb094ca493c9fddff5492
+PYTHON_WEASYPRINT_SITE = https://files.pythonhosted.org/packages/db/3e/65c0f176e6fb5c2b0a1ac13185b366f727d9723541babfa7fa4309998169
 PYTHON_WEASYPRINT_SETUP_TYPE = flit
 PYTHON_WEASYPRINT_LICENSE = BSD-3-Clause
 PYTHON_WEASYPRINT_LICENSE_FILES = LICENSE
