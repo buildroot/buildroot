b2990809a2062f5b4a640ed1ffd1f6269b2cf495
Bernd Kuhls <bernd@kuhls.net>
2026-03-14 15:05:23 +0100

    package/python-iterable-io: bump version to 1.0.1
    https://github.com/pR0Ps/iterable-io/blob/v1.0.1/CHANGELOG.md
    
    Updated license hash due to upstream typo fix:
    https://github.com/pR0Ps/iterable-io/commit/b9e785c76399612bfa13e69f0aa7167c803a915d
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-iterable-io/python-iterable-io.mk
+++ package/python-iterable-io/python-iterable-io.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ITERABLE_IO_VERSION = 1.0.0
-PYTHON_ITERABLE_IO_SOURCE = iterable-io-$(PYTHON_ITERABLE_IO_VERSION).tar.gz
-PYTHON_ITERABLE_IO_SITE = https://files.pythonhosted.org/packages/40/be/27d59b5c1d74ecbd26c1142f84b61d6cb04f0d0337697149645f34406b2d
+PYTHON_ITERABLE_IO_VERSION = 1.0.1
+PYTHON_ITERABLE_IO_SOURCE = iterable_io-$(PYTHON_ITERABLE_IO_VERSION).tar.gz
+PYTHON_ITERABLE_IO_SITE = https://files.pythonhosted.org/packages/9e/ad/cc53869e3357520033e3ab9a7d6043045bcdd666da427583678efdbb446e
 PYTHON_ITERABLE_IO_SETUP_TYPE = setuptools
 PYTHON_ITERABLE_IO_LICENSE = LGPL-3.0
 PYTHON_ITERABLE_IO_LICENSE_FILES = README.md
