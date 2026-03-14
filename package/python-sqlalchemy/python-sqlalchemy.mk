50007f836aacaac0f3878bf6499bbb8ade7ff4e0
Bernd Kuhls <bernd@kuhls.net>
2026-03-14 15:06:05 +0100

    package/python-sqlalchemy: bump version to 2.0.48
    https://github.com/sqlalchemy/sqlalchemy/blob/rel_2_0_48/doc/build/changelog/changelog_20.rst
    
    Updated license hash due to copyright year bump:
    https://github.com/sqlalchemy/sqlalchemy/commit/9ea4237f2136081ee784d12b53f4c0605a5a15a0
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-sqlalchemy/python-sqlalchemy.mk
+++ package/python-sqlalchemy/python-sqlalchemy.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SQLALCHEMY_VERSION = 2.0.45
+PYTHON_SQLALCHEMY_VERSION = 2.0.48
 PYTHON_SQLALCHEMY_SOURCE = sqlalchemy-$(PYTHON_SQLALCHEMY_VERSION).tar.gz
-PYTHON_SQLALCHEMY_SITE = https://files.pythonhosted.org/packages/be/f9/5e4491e5ccf42f5d9cfc663741d261b3e6e1683ae7812114e7636409fcc6
+PYTHON_SQLALCHEMY_SITE = https://files.pythonhosted.org/packages/1f/73/b4a9737255583b5fa858e0bb8e116eb94b88c910164ed2ed719147bde3de
 PYTHON_SQLALCHEMY_SETUP_TYPE = setuptools
 PYTHON_SQLALCHEMY_LICENSE = MIT
 PYTHON_SQLALCHEMY_LICENSE_FILES = LICENSE
