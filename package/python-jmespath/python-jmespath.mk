cd1a38e994ea9eb3ca937581d1cb403e11aab232
Bernd Kuhls <bernd@kuhls.net>
2026-03-14 15:05:24 +0100

    package/python-jmespath: bump version to 1.1.0
    Upstream removed the old license file
    https://github.com/jmespath/jmespath.py/commit/772c03afe10f5b09c5f5a31742a76777f4f0fc34
    
    and added a new one
    https://github.com/jmespath/jmespath.py/commit/5634c661b8e42294f04de610ef8906b59ed96eee
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    [Julien: remove LICENSE.txt entry in hash file]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-jmespath/python-jmespath.mk
+++ package/python-jmespath/python-jmespath.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_JMESPATH_VERSION = 1.0.1
+PYTHON_JMESPATH_VERSION = 1.1.0
 PYTHON_JMESPATH_SOURCE = jmespath-$(PYTHON_JMESPATH_VERSION).tar.gz
-PYTHON_JMESPATH_SITE = https://files.pythonhosted.org/packages/00/2a/e867e8531cf3e36b41201936b7fa7ba7b5702dbef42922193f05c8976cd6
+PYTHON_JMESPATH_SITE = https://files.pythonhosted.org/packages/d3/59/322338183ecda247fb5d1763a6cbe46eff7222eaeebafd9fa65d4bf5cb11
 PYTHON_JMESPATH_SETUP_TYPE = setuptools
 PYTHON_JMESPATH_LICENSE = MIT
-PYTHON_JMESPATH_LICENSE_FILES = LICENSE.txt
+PYTHON_JMESPATH_LICENSE_FILES = LICENSE
 
 $(eval $(python-package))
