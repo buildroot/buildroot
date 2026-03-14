9b0a90c10c03c66109b4bfcf64eb6461ecfd64b3
Bernd Kuhls <bernd@kuhls.net>
2026-03-14 15:05:08 +0100

    package/python-cssselect: bump version to 1.4.0
    https://github.com/scrapy/cssselect/blob/v1.4.0/CHANGES
    "Switched the build system to ``hatchling``."
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-cssselect/python-cssselect.mk
+++ package/python-cssselect/python-cssselect.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_CSSSELECT_VERSION = 1.3.0
+PYTHON_CSSSELECT_VERSION = 1.4.0
 PYTHON_CSSSELECT_SOURCE = cssselect-$(PYTHON_CSSSELECT_VERSION).tar.gz
-PYTHON_CSSSELECT_SITE = https://files.pythonhosted.org/packages/72/0a/c3ea9573b1dc2e151abfe88c7fe0c26d1892fe6ed02d0cdb30f0d57029d5
-PYTHON_CSSSELECT_SETUP_TYPE = setuptools
+PYTHON_CSSSELECT_SITE = https://files.pythonhosted.org/packages/ec/2e/cdfd8b01c37cbf4f9482eefd455853a3cf9c995029a46acd31dfaa9c1dd6
+PYTHON_CSSSELECT_SETUP_TYPE = hatch
 PYTHON_CSSSELECT_LICENSE = BSD-3-Clause
 PYTHON_CSSSELECT_LICENSE_FILES = LICENSE
 
