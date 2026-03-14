25867d252b92ead57d63c565f186e16dc08be134
Bernd Kuhls <bernd@kuhls.net>
2026-03-14 15:06:06 +0100

    package/python-telnetlib3: bump version to 4.0.1
    https://github.com/jquast/telnetlib3/blob/4.0.1/docs/history.rst
    
    Updated license hash due to upstream commit:
    https://github.com/jquast/telnetlib3/commit/1af3eee85961a57be1987bd31706d443b2df7d83
    
    Upstream switched to hatch:
    https://github.com/jquast/telnetlib3/commit/c4b848893067e8a673b715550bcf179354b2e61f
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-telnetlib3/python-telnetlib3.mk
+++ package/python-telnetlib3/python-telnetlib3.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_TELNETLIB3_VERSION = 2.0.8
+PYTHON_TELNETLIB3_VERSION = 4.0.1
 PYTHON_TELNETLIB3_SOURCE = telnetlib3-$(PYTHON_TELNETLIB3_VERSION).tar.gz
-PYTHON_TELNETLIB3_SITE = https://files.pythonhosted.org/packages/b7/12/305101cfdec4863c088e520cbb698b08ebd3396904ecfe0062612db77c66
-PYTHON_TELNETLIB3_SETUP_TYPE = setuptools
+PYTHON_TELNETLIB3_SITE = https://files.pythonhosted.org/packages/5d/57/42acb7ce2c1b3d88ab28d6b1de41a08fdd5180d262d728fd10071950566b
+PYTHON_TELNETLIB3_SETUP_TYPE = hatch
 PYTHON_TELNETLIB3_LICENSE = ISC
 PYTHON_TELNETLIB3_LICENSE_FILES = LICENSE.txt
 
