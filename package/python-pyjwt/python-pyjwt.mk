7c5d57fe820b2a1f74ce2e0153c2aaff9b24ca2f
Bernd Kuhls <bernd@kuhls.net>
2026-03-14 15:05:51 +0100

    package/python-pyjwt: security bump version to 2.12.1
    https://github.com/jpadilla/pyjwt/blob/2.12.1/CHANGELOG.rst
    
    Fixes CVE-2026-32597:
    https://github.com/jpadilla/pyjwt/security/advisories/GHSA-752w-5fwx-jx9f
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-pyjwt/python-pyjwt.mk
+++ package/python-pyjwt/python-pyjwt.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYJWT_VERSION = 2.10.1
+PYTHON_PYJWT_VERSION = 2.12.1
 PYTHON_PYJWT_SOURCE = pyjwt-$(PYTHON_PYJWT_VERSION).tar.gz
-PYTHON_PYJWT_SITE = https://files.pythonhosted.org/packages/e7/46/bd74733ff231675599650d3e47f361794b22ef3e3770998dda30d3b63726
+PYTHON_PYJWT_SITE = https://files.pythonhosted.org/packages/c2/27/a3b6e5bf6ff856d2509292e95c8f57f0df7017cf5394921fc4e4ef40308a
 PYTHON_PYJWT_SETUP_TYPE = setuptools
 PYTHON_PYJWT_LICENSE = MIT
 PYTHON_PYJWT_LICENSE_FILES = LICENSE
