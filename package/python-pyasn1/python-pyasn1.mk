548a063cbb8c1a6bee381959b24e1cc58398cdc8
Bernd Kuhls <bernd@kuhls.net>
2026-03-14 15:05:45 +0100

    package/python-pyasn1: security bump version to 0.6.2
    https://github.com/pyasn1/pyasn1/blob/v0.6.2/CHANGES.rst
    
    Fixes CVE-2026-23490: https://nvd.nist.gov/vuln/detail/CVE-2026-23490
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 517cdc8e13068cca524da33c3c5bab3b507c70c0)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/python-pyasn1/python-pyasn1.mk
+++ package/python-pyasn1/python-pyasn1.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYASN1_VERSION = 0.6.1
+PYTHON_PYASN1_VERSION = 0.6.2
 PYTHON_PYASN1_SOURCE = pyasn1-$(PYTHON_PYASN1_VERSION).tar.gz
-PYTHON_PYASN1_SITE = https://files.pythonhosted.org/packages/ba/e9/01f1a64245b89f039897cb0130016d79f77d52669aae6ee7b159a6c4c018
+PYTHON_PYASN1_SITE = https://files.pythonhosted.org/packages/fe/b6/6e630dff89739fcd427e3f72b3d905ce0acb85a45d4ec3e2678718a3487f
 PYTHON_PYASN1_SETUP_TYPE = setuptools
 PYTHON_PYASN1_LICENSE = BSD-2-Clause
 PYTHON_PYASN1_LICENSE_FILES = LICENSE.rst
