aae6fb35a0ba75ebd4930d9e9854956b2f817085
Bernd Kuhls <bernd@kuhls.net>
2026-03-14 15:05:14 +0100

    package/python-flask: security bump version to 3.1.3
    https://github.com/pallets/flask/blob/3.1.3/CHANGES.rst
    
    Fixes CVE-2026-27205: https://nvd.nist.gov/vuln/detail/CVE-2026-27205
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 52f46b24efc3b7ae86378d9d543cb74889c4ed5b)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/python-flask/python-flask.mk
+++ package/python-flask/python-flask.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_FLASK_VERSION = 3.1.2
+PYTHON_FLASK_VERSION = 3.1.3
 PYTHON_FLASK_SOURCE = flask-$(PYTHON_FLASK_VERSION).tar.gz
-PYTHON_FLASK_SITE = https://files.pythonhosted.org/packages/dc/6d/cfe3c0fcc5e477df242b98bfe186a4c34357b4847e87ecaef04507332dab
+PYTHON_FLASK_SITE = https://files.pythonhosted.org/packages/26/00/35d85dcce6c57fdc871f3867d465d780f302a175ea360f62533f12b27e2b
 PYTHON_FLASK_SETUP_TYPE = flit
 PYTHON_FLASK_LICENSE = BSD-3-Clause
 PYTHON_FLASK_LICENSE_FILES = LICENSE.txt docs/license.rst
