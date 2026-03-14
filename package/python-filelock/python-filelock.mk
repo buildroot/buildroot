eafd8fb98d74d7d5663adb2e1446a31bc5acd4d2
Bernd Kuhls <bernd@kuhls.net>
2026-03-14 15:05:13 +0100

    package/python-filelock: bump version to 3.25.2
    https://github.com/tox-dev/filelock/blob/3.25.2/docs/changelog.rst
    
    Upstream switched license to MIT:
    https://github.com/tox-dev/filelock/commit/4aac7862f5d8f9a92d947991b523bc03803e7213
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-filelock/python-filelock.mk
+++ package/python-filelock/python-filelock.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_FILELOCK_VERSION = 3.20.2
+PYTHON_FILELOCK_VERSION = 3.25.2
 PYTHON_FILELOCK_SOURCE = filelock-$(PYTHON_FILELOCK_VERSION).tar.gz
-PYTHON_FILELOCK_SITE = https://files.pythonhosted.org/packages/c1/e0/a75dbe4bca1e7d41307323dad5ea2efdd95408f74ab2de8bd7dba9b51a1a
+PYTHON_FILELOCK_SITE = https://files.pythonhosted.org/packages/94/b8/00651a0f559862f3bb7d6f7477b192afe3f583cc5e26403b44e59a55ab34
 PYTHON_FILELOCK_SETUP_TYPE = hatch
-PYTHON_FILELOCK_LICENSE = Public Domain
+PYTHON_FILELOCK_LICENSE = MIT
 PYTHON_FILELOCK_LICENSE_FILES = LICENSE
 PYTHON_FILELOCK_DEPENDENCIES = host-python-hatch-vcs
 
