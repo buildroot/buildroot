09bc5515fc6ef50c9071bc113225e9756b03ce5e
Flaviu Nistor <flaviu.nistor@gmail.com>
2026-03-14 09:20:49 +0200

    package/python-rich: bump to version 14.3.3
    Release notes:
    https://github.com/Textualize/rich/releases/tag/v14.3.3
    
    Signed-off-by: Flaviu Nistor <flaviu.nistor@gmail.com>
    Reviewed-by: Fiona Klute <fiona.klute@gmx.de>
    [Julien: add link to release notes]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-rich/python-rich.mk
+++ package/python-rich/python-rich.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_RICH_VERSION = 14.3.2
+PYTHON_RICH_VERSION = 14.3.3
 PYTHON_RICH_SOURCE = rich-$(PYTHON_RICH_VERSION).tar.gz
-PYTHON_RICH_SITE = https://files.pythonhosted.org/packages/74/99/a4cab2acbb884f80e558b0771e97e21e939c5dfb460f488d19df485e8298
+PYTHON_RICH_SITE = https://files.pythonhosted.org/packages/b3/c6/f3b320c27991c46f43ee9d856302c70dc2d0fb2dba4842ff739d5f46b393
 PYTHON_RICH_SETUP_TYPE = poetry
 PYTHON_RICH_LICENSE = MIT
 PYTHON_RICH_LICENSE_FILES = LICENSE
