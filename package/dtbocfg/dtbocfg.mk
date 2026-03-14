f9c20750e26ccb652e5cf0dc95d3e077440a1ce4
Bernd Kuhls <bernd@kuhls.net>
2026-03-14 18:04:41 +0100

    package/dtbocfg: bump version to 0.1.1
    https://github.com/ikwzm/dtbocfg/compare/v0.1.0...v0.1.1
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/dtbocfg/dtbocfg.mk
+++ package/dtbocfg/dtbocfg.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DTBOCFG_VERSION = 0.1.0
+DTBOCFG_VERSION = 0.1.1
 DTBOCFG_SITE = $(call github,ikwzm,dtbocfg,v$(DTBOCFG_VERSION))
 DTBOCFG_LICENSE = BSD-2-Clause
 DTBOCFG_LICENSE_FILES = LICENSE
