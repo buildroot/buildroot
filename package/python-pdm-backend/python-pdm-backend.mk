256359467cc43999cb47a7ffbdb3db573d581e46
Bernd Kuhls <bernd@kuhls.net>
2026-03-14 15:05:36 +0100

    package/python-pdm-backend: bump version to 2.4.7
    https://github.com/pdm-project/pdm-backend/releases/tag/2.4.7
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-pdm-backend/python-pdm-backend.mk
+++ package/python-pdm-backend/python-pdm-backend.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PDM_BACKEND_VERSION = 2.4.6
+PYTHON_PDM_BACKEND_VERSION = 2.4.7
 PYTHON_PDM_BACKEND_SOURCE = pdm_backend-$(PYTHON_PDM_BACKEND_VERSION).tar.gz
-PYTHON_PDM_BACKEND_SITE = https://files.pythonhosted.org/packages/71/fc/15b83c93a8e7929b62debaad63a3e89b8ce7f8a86075bda0d81e42c3cad5
+PYTHON_PDM_BACKEND_SITE = https://files.pythonhosted.org/packages/e2/38/d22c1050130b8cdf16fef76c99c59968086802744947f2813d45c69cca52
 PYTHON_PDM_BACKEND_SETUP_TYPE = pep517
 PYTHON_PDM_BACKEND_LICENSE = MIT
 PYTHON_PDM_BACKEND_LICENSE_FILES = LICENSE
