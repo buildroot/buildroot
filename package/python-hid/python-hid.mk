0b33ca109cca138b330498df3b12caf32819d9f5
Bernd Kuhls <bernd@kuhls.net>
2026-03-14 15:05:20 +0100

    package/python-hid: bump version to 1.0.9
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-hid/python-hid.mk
+++ package/python-hid/python-hid.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_HID_VERSION = 1.0.8
+PYTHON_HID_VERSION = 1.0.9
 PYTHON_HID_SOURCE = hid-$(PYTHON_HID_VERSION).tar.gz
-PYTHON_HID_SITE = https://files.pythonhosted.org/packages/29/42/9cd3eab530ffae5e0ff880c575fd18cfdfe292b71f1e9872c392329f35fd
+PYTHON_HID_SITE = https://files.pythonhosted.org/packages/e9/f8/0357a8aa8874a243e96d08a8568efaf7478293e1a3441ddca18039b690c1
 PYTHON_HID_SETUP_TYPE = setuptools
 PYTHON_HID_LICENSE = MIT
 PYTHON_HID_LICENSE_FILES = LICENSE
