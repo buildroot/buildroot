9e4e331d18d1d1cb0924d856ab3fa4aaf4d2caea
Bernd Kuhls <bernd@kuhls.net>
2026-03-14 15:06:25 +0100

    package/python-werkzeug: bump version to 3.1.6
    https://github.com/pallets/werkzeug/blob/3.1.6/CHANGES.rst
    
    Please note that the fixed CVEs CVE-2026-21860 & CVE-2026-27199 only
    relate to Windows.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-werkzeug/python-werkzeug.mk
+++ package/python-werkzeug/python-werkzeug.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_WERKZEUG_VERSION = 3.1.4
+PYTHON_WERKZEUG_VERSION = 3.1.6
 PYTHON_WERKZEUG_SOURCE = werkzeug-$(PYTHON_WERKZEUG_VERSION).tar.gz
-PYTHON_WERKZEUG_SITE = https://files.pythonhosted.org/packages/45/ea/b0f8eeb287f8df9066e56e831c7824ac6bab645dd6c7a8f4b2d767944f9b
+PYTHON_WERKZEUG_SITE = https://files.pythonhosted.org/packages/61/f1/ee81806690a87dab5f5653c1f146c92bc066d7f4cebc603ef88eb9e13957
 PYTHON_WERKZEUG_SETUP_TYPE = flit
 PYTHON_WERKZEUG_LICENSE = BSD-3-Clause
 PYTHON_WERKZEUG_LICENSE_FILES = LICENSE.txt
