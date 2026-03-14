a332ca3548fec01a2074549fcc6ea2e43f96971b
Bernd Kuhls <bernd@kuhls.net>
2026-03-14 15:05:41 +0100

    package/python-proto-plus: bump version to 1.27.1
    https://github.com/googleapis/proto-plus-python/blob/v1.27.1/CHANGELOG.md
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-proto-plus/python-proto-plus.mk
+++ package/python-proto-plus/python-proto-plus.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PROTO_PLUS_VERSION = 1.27.0
+PYTHON_PROTO_PLUS_VERSION = 1.27.1
 PYTHON_PROTO_PLUS_SOURCE = proto_plus-$(PYTHON_PROTO_PLUS_VERSION).tar.gz
-PYTHON_PROTO_PLUS_SITE = https://files.pythonhosted.org/packages/01/89/9cbe2f4bba860e149108b683bc2efec21f14d5f7ed6e25562ad86acbc373
+PYTHON_PROTO_PLUS_SITE = https://files.pythonhosted.org/packages/3a/02/8832cde80e7380c600fbf55090b6ab7b62bd6825dbedde6d6657c15a1f8e
 PYTHON_PROTO_PLUS_SETUP_TYPE = setuptools
 PYTHON_PROTO_PLUS_LICENSE = Apache-2.0
 PYTHON_PROTO_PLUS_LICENSE_FILES = LICENSE
