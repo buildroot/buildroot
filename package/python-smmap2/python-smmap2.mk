1332aaade40f30ef39022cc5da3b1e5b80e2844e
Flaviu Nistor <flaviu.nistor@gmail.com>
2026-03-23 21:23:25 +0200

    package/python-smmap2: bump to version 5.0.3
    For release notes, see:
    https://github.com/gitpython-developers/smmap/releases/tag/v5.0.3
    
    Signed-off-by: Flaviu Nistor <flaviu.nistor@gmail.com>
    [Julien: add link to release notes]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-smmap2/python-smmap2.mk
+++ package/python-smmap2/python-smmap2.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SMMAP2_VERSION = 5.0.2
+PYTHON_SMMAP2_VERSION = 5.0.3
 PYTHON_SMMAP2_SOURCE = smmap-$(PYTHON_SMMAP2_VERSION).tar.gz
-PYTHON_SMMAP2_SITE = https://files.pythonhosted.org/packages/44/cd/a040c4b3119bbe532e5b0732286f805445375489fceaec1f48306068ee3b
+PYTHON_SMMAP2_SITE = https://files.pythonhosted.org/packages/1f/ea/49c993d6dfdd7338c9b1000a0f36817ed7ec84577ae2e52f890d1a4ff909
 PYTHON_SMMAP2_SETUP_TYPE = setuptools
 PYTHON_SMMAP2_LICENSE = BSD-3-Clause
 PYTHON_SMMAP2_LICENSE_FILES = LICENSE
