06baf64bf610e9588049bb613f340d114f5ddb41
Quentin Schulz <quentin.schulz@cherry.de>
2026-03-24 12:01:48 +0100

    package/qt5/qt53d: always disable assimp support
    Neither Buildroot's nor the module from qt53d sources compile, so always
    disable assimp support.
    
    Signed-off-by: Quentin Schulz <quentin.schulz@cherry.de>
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/qt5/qt53d/qt53d.mk
+++ package/qt5/qt53d/qt53d.mk
@@ -14,14 +14,10 @@ QT53D_SYNC_QT_HEADERS = YES
 # command line argument separator
 QT53D_CONF_OPTS = --
 
-ifeq ($(BR2_PACKAGE_ASSIMP),y)
-QT53D_DEPENDENCIES += assimp
-QT53D_CONF_OPTS += \
-	-feature-system-assimp
-else
+# Assimp support is broken with both Buildroot assimp and the assimp module in
+# the qt53d sources, therefore disable it for now.
 QT53D_CONF_OPTS += \
 	-no-feature-assimp
-endif
 
 QT53D_LICENSE = GPL-2.0 or GPL-3.0 or LGPL-3.0
 QT53D_LICENSE_FILES = LICENSE.GPL LICENSE.GPLv3 LICENSE.LGPLv3
