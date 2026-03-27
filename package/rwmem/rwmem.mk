bcbae423d36e92503056682b40b815d51462e782
Vincent Cruz <mooz@blockos.org>
2026-03-27 12:25:16 +0100

    package/rwmem: bump to version 2.0
    For more details, see:
    https://github.com/tomba/rwmem/compare/c89bc9ad9...2.0
    
    Signed-off-by: Vincent Cruz <mooz@blockos.org>
    [Julien: add info in patch "Upstream:" tag]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/rwmem/rwmem.mk
+++ package/rwmem/rwmem.mk
@@ -4,11 +4,10 @@
 #
 ################################################################################
 
-RWMEM_VERSION = c89bc9ad9a8f2359f358c510db57b7678eb156d1
+RWMEM_VERSION = 2.0
 RWMEM_SITE = $(call github,tomba,rwmem,$(RWMEM_VERSION))
 RWMEM_LICENSE = GPL-2.0
 RWMEM_LICENSE_FILES = LICENSE
-RWMEM_CONF_OPTS = -Dpyrwmem=disabled
 RWMEM_DEPENDENCIES = host-pkgconf fmt inih
 
 $(eval $(meson-package))
