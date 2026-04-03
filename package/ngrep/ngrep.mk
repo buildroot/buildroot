37a21dc9392d9f6b755a658347449056d39c7fb7
Bernd Kuhls <bernd@kuhls.net>
2026-04-03 20:14:13 +0200

    package/ngrep: bump version to 1.49.0
    Removed patch 0001, included upstream:
    https://github.com/jpr5/ngrep/commit/b8b1e9751e1d074af0dd77c2554c1b25fd8f7abe
    
    Removed patch 0002, included upstream:
    https://github.com/jpr5/ngrep/commit/0a51c8bc62c7b49b8d67a360daa6b1957256f4f5
    
    Removed patch 0003, included upstream:
    https://github.com/jpr5/ngrep/commit/c9ddace67b6faee8d7dd0cdf376c9275a99224f9
    
    Removed patch 0004 which was never sent upstream, also libnet still
    provides libnet-config and causes no build errors. Porting this patch to
    the modified configure.ac script does not create any benefits so we drop
    this patch along with autoreconf.
    
    Removed patch 0005, included upstream:
    https://github.com/jpr5/ngrep/commit/742424ddd76e15bd151e40834e8f1fad65900b65
    
    Removed patch 0006, included upstream:
    https://github.com/jpr5/ngrep/commit/e97f8e9d4b80e6e26e0bc4f586c7abfd772d888c
    https://github.com/jpr5/ngrep/commit/1e340b1eb334a74a6ab5373345aa1afce2e30c7c
    
    Wchar is needed since this commit was added to 1.48.1:
    https://github.com/jpr5/ngrep/commit/8107128098392862b34204ece13ca907f1f74436
    
    Pcre2 is now required instead of pcre:
    https://github.com/jpr5/ngrep/commit/cfcf1e6e9c4f8a2404810d8d1d90e6d1eaa0abdd
    
    Updated license hash due to upstream commit:
    https://github.com/jpr5/ngrep/commit/fc5db167dec085805e76ad1f97bf38e1e850ec2c
    
    Removed /pcap from configure option pcap-includes due to upstream commit
    https://github.com/jpr5/ngrep/commit/110fa39f8fae8efe2e9b35ad9bc922a305589f0b
    which now searches for pcap/pcap.h instead of pcap.h:
    https://github.com/jpr5/ngrep/commit/110fa39f8fae8efe2e9b35ad9bc922a305589f0b#diff-49473dca262eeab3b4a43002adb08b4db31020d190caaad1594b47f1d5daa810R200-R303
    
    Added ac_cv_path_NGREP_PCRE2_CONFIG_SCRIPT to _CONF_ENV to avoid
    /usr/bin/pcre2-config being found.
    
    Added ac_cv_path_LIBNET_CONFIG_BIN to _CONF_ENV as follow-up to the
    removal of patch 0004.
    
    Build-tested using this defconfig:
    
    BR2_PACKAGE_LIBNET=y
    BR2_PACKAGE_NGREP=y
    
                                 arm-aarch64 [ 1/32]: OK
                       bootlin-aarch64-glibc [ 2/32]: OK
                   bootlin-aarch64-glibc-old [ 3/32]: OK
                   bootlin-arcle-hs38-uclibc [ 4/32]: OK
                        bootlin-armv5-uclibc [ 5/32]: OK
                         bootlin-armv7-glibc [ 6/32]: OK
                       bootlin-armv7m-uclibc [ 7/32]: OK
                          bootlin-armv7-musl [ 8/32]: OK
                    bootlin-m68k-5208-uclibc [ 9/32]: OK
                   bootlin-m68k-68040-uclibc [10/32]: OK
                 bootlin-microblazeel-uclibc [11/32]: OK
                      bootlin-mips64el-glibc [12/32]: OK
                    bootlin-mipsel32r6-glibc [13/32]: OK
                       bootlin-mipsel-uclibc [14/32]: OK
                     bootlin-openrisc-uclibc [15/32]: OK
            bootlin-powerpc64le-power8-glibc [16/32]: OK
               bootlin-powerpc-e500mc-uclibc [17/32]: OK
                       bootlin-riscv32-glibc [18/32]: OK
                       bootlin-riscv64-glibc [19/32]: OK
                        bootlin-riscv64-musl [20/32]: OK
                     bootlin-s390x-z13-glibc [21/32]: OK
                          bootlin-sh4-uclibc [22/32]: OK
                       bootlin-sparc64-glibc [23/32]: OK
                        bootlin-sparc-uclibc [24/32]: OK
                        bootlin-x86-64-glibc [25/32]: OK
                         bootlin-x86-64-musl [26/32]: OK
                       bootlin-x86-64-uclibc [27/32]: OK
                       bootlin-x86-i686-musl [28/32]: OK
                       bootlin-xtensa-uclibc [29/32]: OK
                                br-arm-basic [30/32]: SKIPPED
                        br-arm-full-nothread [31/32]: OK
                          br-arm-full-static [32/32]: OK
    
    32 builds, 1 skipped, 0 build failed, 0 legal-info failed, 0 show-info failed
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/ngrep/ngrep.mk
+++ package/ngrep/ngrep.mk
@@ -4,29 +4,29 @@
 #
 ################################################################################
 
-NGREP_VERSION = 1.47
-NGREP_SITE = $(call github,jpr5,ngrep,$(subst .,_,V$(NGREP_VERSION)))
+NGREP_VERSION = 1.49.0
+NGREP_SITE = $(call github,jpr5,ngrep,v$(NGREP_VERSION))
 NGREP_LICENSE = BSD-4-Clause-like, BSD-3-Clause (tcpkill)
 NGREP_LICENSE_FILES = LICENSE
 NGREP_INSTALL_STAGING = YES
-# We're patching configure.in
-NGREP_AUTORECONF = YES
 
 ifeq ($(BR2_STATIC_LIBS),y)
 NGREP_CONF_ENV += LIBS=`$(STAGING_DIR)/usr/bin/pcap-config --static --additional-libs`
 endif
 
 NGREP_CONF_OPTS = \
-	--with-pcap-includes=$(STAGING_DIR)/usr/include/pcap \
-	--enable-pcre \
+	--with-pcap-includes=$(STAGING_DIR)/usr/include \
+	--enable-pcre2 \
 	--disable-dropprivs \
 	--disable-pcap-restart
+NGREP_CONF_ENV += ac_cv_path_NGREP_PCRE2_CONFIG_SCRIPT=$(STAGING_DIR)/usr/bin/pcre2-config
 
-NGREP_DEPENDENCIES = host-pkgconf libpcap pcre
+NGREP_DEPENDENCIES = host-pkgconf libpcap pcre2
 
 ifeq ($(BR2_PACKAGE_LIBNET),y)
 NGREP_DEPENDENCIES += libnet
 NGREP_CONF_OPTS += --enable-tcpkill
+NGREP_CONF_ENV += ac_cv_path_LIBNET_CONFIG_BIN=$(STAGING_DIR)/usr/bin/libnet-config
 else
 NGREP_CONF_OPTS += --disable-tcpkill
 endif
