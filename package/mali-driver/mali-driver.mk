03f4edcdf84149437f847fd314e42bdd2d1c39b3
Giulio Benetti <giulio.benetti@benettiengineering.com>
2026-04-03 22:20:58 +0200

    package/mali-driver: bump to version 2026-04-03
    Fixed build with Linux 7.0
    
    Fixes:
    still not occured
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/mali-driver/mali-driver.mk
+++ package/mali-driver/mali-driver.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MALI_DRIVER_VERSION = 5731d558a34b82290eaad22a579a1bd8588d26e8
+MALI_DRIVER_VERSION = fd0865346db356f08295df49f96ce0df47ccdd2f
 MALI_DRIVER_SITE = $(call github,bootlin,mali-driver,$(MALI_DRIVER_VERSION))
 MALI_DRIVER_DEPENDENCIES = linux
 MALI_DRIVER_LICENSE = GPL-2.0
