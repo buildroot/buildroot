c8e9c53d8c477ec44b5d442065c08020e2ca00c3
Bernd Kuhls <bernd@kuhls.net>
2026-04-05 18:27:11 +0200

    package/leafnode2: bump version to 2.0.0.alpha202601
    https://gitlab.com/leafnode-2/leafnode-2/-/blob/leafnode-2.0.0.alpha202601/NEWS
    
    Rebased patch 0001.
    
    Upstream switched from pcre to pcre2.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/leafnode2/leafnode2.mk
+++ package/leafnode2/leafnode2.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-LEAFNODE2_VERSION = 9543ffaf82406f943d449d522d88baa554515e0f
-LEAFNODE2_SITE = $(call gitlab,leafnode-2,leafnode-2,$(LEAFNODE2_VERSION))
+LEAFNODE2_VERSION = 2.0.0.alpha202601
+LEAFNODE2_SITE = $(call gitlab,leafnode-2,leafnode-2,leafnode-$(LEAFNODE2_VERSION))
 LEAFNODE2_LICENSE = LGPL-2.1
 LEAFNODE2_LICENSE_FILES = COPYING COPYING.LGPL
-LEAFNODE2_DEPENDENCIES = host-pcre pcre
+LEAFNODE2_DEPENDENCIES = host-pcre2 pcre2
 LEAFNODE2_AUTORECONF = YES
 
 # pod2man doesn't work when cross compiling
@@ -18,7 +18,7 @@ endef
 LEAFNODE2_POST_PATCH_HOOKS += LEAFNODE2_DISABLE_POD2MAN
 
 LEAFNODE2_CONF_ENV = \
-	PCRECONFIG="$(STAGING_DIR)/usr/bin/pcre-config"
+	PCRE2CONFIG="$(STAGING_DIR)/usr/bin/pcre2-config"
 
 ifeq ($(BR2_PACKAGE_LIBXCRYPT),y)
 LEAFNODE2_DEPENDENCIES += libxcrypt
@@ -35,13 +35,13 @@ LEAFNODE2_CONF_OPTS = \
 # Leafnode2 needs the host version of b_sortnl during
 # compilation. Instead of creating a separate host package and
 # installing b_sortnl to $(HOST_DIR) this binary is compiled
-# on-the-fly, host-pcre is needed for this
+# on-the-fly, host-pcre2 is needed for this
 define LEAFNODE2_BUILD_SORTNL_TOOL
 	cd $(@D); \
 	$(HOSTCC) $(HOST_CFLAGS) -o b_sortnl_host \
 		arc4random.c mergesort.c b_sortnl.c critmem_malloc.c \
 		critmem_realloc.c -DHAVE_CONFIG_H -I$(HOST_DIR)/include \
-		-L $(HOST_DIR)/lib -Wl,-rpath,$(HOST_DIR)/lib -lpcre
+		-L $(HOST_DIR)/lib -Wl,-rpath,$(HOST_DIR)/lib -lpcre2-8
 endef
 
 LEAFNODE2_PRE_BUILD_HOOKS += LEAFNODE2_BUILD_SORTNL_TOOL
