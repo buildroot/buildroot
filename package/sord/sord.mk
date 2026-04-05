5bd1973ef0376ac60ca5f04ef994eaf912b5162d
Bernd Kuhls <bernd@kuhls.net>
2026-04-05 13:53:53 +0200

    package/sord: bump version to 0.16.22
    https://gitlab.com/drobilla/sord/-/blob/v0.16.22/NEWS
    
    Added dependency to zix due to upstream commit:
    https://gitlab.com/drobilla/sord/-/commit/ec186c111b2079aa2bb52f44b01b874e46ba4577
    added in version 0.16.16.
    
    Updated license hash due to upstream commits
    https://gitlab.com/drobilla/sord/-/commit/53f071a571899faf27dd730ea24838c16887836c
    https://gitlab.com/drobilla/sord/-/commit/5edcf18adfbc4240040c667c00884cbd049e3a84
    https://gitlab.com/drobilla/sord/-/commit/fb728f8a6fb0e0410f15fa7a57f9ab7833db17ac
    
    Package switched from pcre to pcre2 in version 0.16.16:
    https://gitlab.com/drobilla/sord/-/commit/67bcd63bda9d7b095489a09b9880aa730ddb5488
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/sord/sord.mk
+++ package/sord/sord.mk
@@ -4,16 +4,16 @@
 #
 ################################################################################
 
-SORD_VERSION = 0.16.12
+SORD_VERSION = 0.16.22
 SORD_SITE = https://download.drobilla.net
 SORD_SOURCE = sord-$(SORD_VERSION).tar.xz
 SORD_LICENSE = ISC
 SORD_LICENSE_FILES = COPYING
-SORD_DEPENDENCIES = host-pkgconf serd
+SORD_DEPENDENCIES = host-pkgconf serd zix
 SORD_INSTALL_STAGING = YES
 
-ifeq ($(BR2_PACKAGE_PCRE),y)
-SORD_DEPENDENCIES += pcre
+ifeq ($(BR2_PACKAGE_PCRE2),y)
+SORD_DEPENDENCIES += pcre2
 endif
 
 SORD_CONF_OPTS += -Ddocs=disabled -Dtests=disabled
