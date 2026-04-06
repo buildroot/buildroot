04689a8bc05d3a0e8d194ba795512438c10b53a0
Franciszek Stachura <fbstachura@gmail.com>
2026-04-06 20:48:59 +0200

    package/libphidget: bump to version 1.24.20260127
    This is also a backward-incompatible upgrade from version 21 to 22 [1].
    
    Version 21 was deprecated in 2020, it doesn't support latest phidget
    devices [2].
    
    Version 22 is covered by BSD-3 instead of LGPL3 [3].
    
    Release notes: https://www.phidgets.com/?view=changelog
    
    [1] https://www.phidgets.com/docs/Upgrading_Code_from_Phidget21_to_Phidget22
    [2] https://www.phidgets.com/docs21/Main_Page
    [3] https://www.phidgets.com/docs/Software_License
    
    Signed-off-by: Franciszek Stachura <fbstachura@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libphidget/libphidget.mk
+++ package/libphidget/libphidget.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-LIBPHIDGET_VERSION = 2.1.9.20190409
-LIBPHIDGET_SOURCE = libphidget_$(LIBPHIDGET_VERSION).tar.gz
-LIBPHIDGET_SITE = https://www.phidgets.com/downloads/phidget21/libraries/linux/libphidget
+LIBPHIDGET_VERSION = 1.24.20260127
+LIBPHIDGET_SOURCE = libphidget22-$(LIBPHIDGET_VERSION).tar.gz
+LIBPHIDGET_SITE = https://www.phidgets.com/downloads/phidget22/libraries/linux/libphidget22
 LIBPHIDGET_DEPENDENCIES = libusb
 LIBPHIDGET_CONF_OPTS = --disable-ldconfig
 LIBPHIDGET_INSTALL_STAGING = YES
-LIBPHIDGET_LICENSE = LGPL-3.0
+LIBPHIDGET_LICENSE = BSD-3-Clause
 LIBPHIDGET_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
