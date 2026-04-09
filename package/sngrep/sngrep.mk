3831283ca43dc75827647bec220d91e1704bb67b
Bernd Kuhls <bernd@kuhls.net>
2026-04-09 10:42:54 +0200

    package/sngrep: remove optional support for pcre, keep pcre2
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/sngrep/sngrep.mk
+++ package/sngrep/sngrep.mk
@@ -16,7 +16,7 @@ SNGREP_DEPENDENCIES = libpcap ncurses host-pkgconf
 SNGREP_CONF_ENV += \
 	$(if $(BR2_STATIC_LIBS),LIBS="`$(STAGING_DIR)/usr/bin/pcap-config --static --libs`")
 
-SNGREP_CONF_OPTS += --disable-unicode
+SNGREP_CONF_OPTS += --without-pcre --disable-unicode
 
 # openssl and gnutls can't be enabled at the same time.
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
@@ -33,12 +33,9 @@ endif
 
 ifeq ($(BR2_PACKAGE_PCRE2),y)
 SNGREP_DEPENDENCIES += pcre2
-SNGREP_CONF_OPTS += --without-pcre --with-pcre2
-else ifeq ($(BR2_PACKAGE_PCRE),y)
-SNGREP_DEPENDENCIES += pcre
-SNGREP_CONF_OPTS += --with-pcre --without-pcre2
+SNGREP_CONF_OPTS += --with-pcre2
 else
-SNGREP_CONF_OPTS += --without-pcre --without-pcre2
+SNGREP_CONF_OPTS += --without-pcre2
 endif
 
 ifeq ($(BR2_PACKAGE_ZLIB),y)
