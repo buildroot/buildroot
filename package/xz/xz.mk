e0914c0ef7ad85b3f969788af8f0c49cf021c2b8
Thomas Perale <thomas.perale@mind.be>
2026-04-17 14:06:41 +0200

    package/xz: patch CVE-2026-34743
    - CVE-2026-34743:
        XZ Utils provide a general-purpose data-compression library plus
        command-line tools. Prior to version 5.8.3, if lzma_index_decoder()
        was used to decode an Index that contained no Records, the resulting
        lzma_index was left in a state where where a subsequent
        lzma_index_append() would allocate too little memory, and a buffer
        overflow would occur. This issue has been patched in version 5.8.3.
    
    For more information, see:
      - https://www.cve.org/CVERecord?id=CVE-2026-34743
      - https://security-tracker.debian.org/tracker/CVE-2026-34743
      - https://github.com/tukaani-project/xz/commit/c8c22869e780ff57c96b46939c3d79ff99395f87
    
    (cherry picked from commit 724635227314010518372c5bad4d37d7c58950c1)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/xz/xz.mk
+++ package/xz/xz.mk
@@ -22,7 +22,10 @@ HOST_XZ_ADD_CCACHE_DEPENDENCY = NO
 # 0002-liblzma-mt-dec-Simplify-by-removing-the-THR_STOP-sta.patch
 # 0003-liblzma-mt-dec-Don-t-free-the-input-buffer-too-early.patch
 # 0004-liblzma-mt-dec-Don-t-modify-thr-in_size-in-the-worke.patch
-XZ_IGNORE_CVES = CVE-2025-31115
+XZ_IGNORE_CVES += CVE-2025-31115
+
+# 0005-liblzma-Fix-a-buffer-overflow-in-lzma-index-append.patch
+XZ_IGNORE_CVES += CVE-2026-34743
 
 XZ_CONF_OPTS = \
 	--enable-encoders=lzma1,lzma2,delta,x86,powerpc,ia64,arm,armthumb,arm64,sparc,riscv \
