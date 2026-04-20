d966da35812c943fff6dd7be55284966626c4fc9
Bernd Kuhls <bernd@kuhls.net>
2026-04-20 18:55:48 +0200

    package/libcap-ng: bump version to 0.9.3
    https://github.com/stevegrubb/libcap-ng/blob/v0.9.3/ChangeLog
    
    The source tarball is not distributed on the project site anymore so we
    switch to the github helper and need to enable autoreconf.
    
    Added dependency to BR2_TOOLCHAIN_HAS_SYNC_4 to fix a build error:
    
    /home/bernd/buildroot/test-libcap/bootlin-sparc-uclibc/host/opt/ext-toolchain/bin/../lib/gcc/sparc-buildroot-linux-uclibc/14.3.0/../../../../sparc-buildroot-linux-uclibc/bin/ld:
     ../src/.libs/libcap-ng.so: undefined reference to `__sync_bool_compare_and_swap_4'
    
    due to this upstream commit added in version 0.9.2:
    https://github.com/stevegrubb/libcap-ng/commit/ba2e9e03199541b1c07f3e925ffc5b23774bd18f
    
    Added _POST_EXTRACT_HOOKS and dependency to host-pkgconf to fix
    autoreconf errors.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libcap-ng/libcap-ng.mk
+++ package/libcap-ng/libcap-ng.mk
@@ -4,12 +4,24 @@
 #
 ################################################################################
 
-LIBCAP_NG_VERSION = 0.8.5
-LIBCAP_NG_SITE = https://people.redhat.com/sgrubb/libcap-ng
+LIBCAP_NG_VERSION = 0.9.3
+LIBCAP_NG_SITE = $(call github,stevegrubb,libcap-ng,v$(LIBCAP_NG_VERSION))
 LIBCAP_NG_LICENSE = GPL-2.0+ (programs), LGPL-2.1+ (library)
 LIBCAP_NG_LICENSE_FILES = COPYING COPYING.LIB
 LIBCAP_NG_CPE_ID_VALID = YES
 LIBCAP_NG_INSTALL_STAGING = YES
+
+# github tarball does not include configure
+LIBCAP_NG_AUTORECONF = YES
+# needed for autoreconf
+LIBCAP_NG_DEPENDENCIES = host-pkgconf
+HOST_LIBCAP_NG_DEPENDENCIES = host-pkgconf
+
+define LIBCAP_NG_CREATE_MISSING_FILES
+	touch $(@D)/NEWS
+endef
+LIBCAP_NG_POST_EXTRACT_HOOKS += LIBCAP_NG_CREATE_MISSING_FILES
+HOST_LIBCAP_NG_POST_EXTRACT_HOOKS += LIBCAP_NG_CREATE_MISSING_FILES
 
 LIBCAP_NG_CONF_ENV = ac_cv_prog_swig_found=no
 LIBCAP_NG_CONF_OPTS = --without-python3
