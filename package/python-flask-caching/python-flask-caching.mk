373f2b7d628251d689df082a5f96752e9b560608
Daniel Crowe <daniel.crowe@maxmine.com.au>
2026-04-21 09:39:39 +0930

    package/python-flask-caching: bump version to 2.4.0
    Signed-off-by: Daniel Crowe <daniel.crowe@maxmine.com.au>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-flask-caching/python-flask-caching.mk
+++ package/python-flask-caching/python-flask-caching.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_FLASK_CACHING_VERSION = 2.3.1
+PYTHON_FLASK_CACHING_VERSION = 2.4.0
 PYTHON_FLASK_CACHING_SOURCE = flask_caching-$(PYTHON_FLASK_CACHING_VERSION).tar.gz
-PYTHON_FLASK_CACHING_SITE = https://files.pythonhosted.org/packages/e2/80/74846c8af58ed60972d64f23a6cd0c3ac0175677d7555dff9f51bf82c294
+PYTHON_FLASK_CACHING_SITE = https://files.pythonhosted.org/packages/42/53/5c46b6a80adc13ed9179879a93cfc2c1f190c64c48ba732b4f5819df520e
 PYTHON_FLASK_CACHING_SETUP_TYPE = setuptools
 PYTHON_FLASK_CACHING_LICENSE = BSD-3-Clause
 PYTHON_FLASK_CACHING_LICENSE_FILES = LICENSE docs/license.rst
