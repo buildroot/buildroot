2887dd91cceed9d446508dacde74cf7c5597a9d2
Julien Olivain <ju.o@free.fr>
2026-04-21 19:26:23 +0200

    package/python-magic-wormhole: security bump to version 0.23.0
    See NEWS:
    https://github.com/magic-wormhole/magic-wormhole/blob/0.23.0/NEWS.md
    
    This commit also updates _SITE to use a stable URL which will redirect
    to the package archive, to reduce future maintenance.
    
    Fixes:
    https://nvd.nist.gov/vuln/detail/CVE-2026-32116
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    (cherry picked from commit 8de4776c609ec8afe1e603aa1b115d63bd377dbd)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/python-magic-wormhole/python-magic-wormhole.mk
+++ package/python-magic-wormhole/python-magic-wormhole.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_MAGIC_WORMHOLE_VERSION = 0.22.0
+PYTHON_MAGIC_WORMHOLE_VERSION = 0.23.0
 PYTHON_MAGIC_WORMHOLE_SOURCE = magic_wormhole-$(PYTHON_MAGIC_WORMHOLE_VERSION).tar.gz
-PYTHON_MAGIC_WORMHOLE_SITE = https://files.pythonhosted.org/packages/65/34/08813891da57999cf953e9c553193c900ff363c4cdfdd48a74f65536a883
+PYTHON_MAGIC_WORMHOLE_SITE = https://files.pythonhosted.org/packages/source/m/magic_wormhole
 PYTHON_MAGIC_WORMHOLE_SETUP_TYPE = setuptools
 PYTHON_MAGIC_WORMHOLE_LICENSE = MIT
 PYTHON_MAGIC_WORMHOLE_LICENSE_FILES = LICENSE
