7f7e1848773042d3f28bddccc9dd7e0b26e84044
Fiona Klute <fiona.klute@gmx.de>
2026-04-25 10:56:55 +0200

    Revert "kmod: workaround for microblaze ld bug"
    This reverts commit 0d81107f021bf2a663f102bace1e3f9590641170. The bug
    the workaround is for was fixed in binutils 2.29.51 (first release
    listed in bfd/ChangeLog after the fix), which is by now far older than
    any version supported by Buildroot.
    
    Compile tested with qemu_microblazeel_mmu_defconfig plus
    BR2_PACKAGE_KMOD=y.
    
    Signed-off-by: Fiona Klute <fiona.klute@gmx.de>
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>
    (cherry picked from commit 7653e616f96241b04586ecb4b612a219885d52dd)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/kmod/kmod.mk
+++ package/kmod/kmod.mk
@@ -17,12 +17,6 @@ KMOD_LICENSE_FILES = libkmod/COPYING
 
 KMOD_CPE_ID_VENDOR = kernel
 
-# --gc-sections triggers binutils ld segfault
-# https://sourceware.org/bugzilla/show_bug.cgi?id=21180
-ifeq ($(BR2_microblaze),y)
-KMOD_CONF_ENV += cc_cv_LDFLAGS__Wl___gc_sections=false
-endif
-
 # static linking not supported, see
 # https://git.kernel.org/cgit/utils/kernel/kmod/kmod.git/commit/?id=b7016153ec8
 KMOD_CONF_OPTS = --disable-static --enable-shared
