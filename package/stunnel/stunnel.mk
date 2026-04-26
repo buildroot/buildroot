6284105e0ce4fc37ab1c99d78b07da7861d19d69
Bernd Kuhls <bernd@kuhls.net>
2026-04-26 20:54:48 +0200

    package/stunnel: Switch URLs to main project site
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/stunnel/stunnel.mk
+++ package/stunnel/stunnel.mk
@@ -4,9 +4,8 @@
 #
 ################################################################################
 
-STUNNEL_VERSION_MAJOR = 5
-STUNNEL_VERSION = $(STUNNEL_VERSION_MAJOR).78
-STUNNEL_SITE = http://www.usenix.org.uk/mirrors/stunnel/archive/$(STUNNEL_VERSION_MAJOR).x
+STUNNEL_VERSION = 5.78
+STUNNEL_SITE = https://www.stunnel.org/downloads
 STUNNEL_DEPENDENCIES = host-pkgconf openssl
 STUNNEL_CONF_OPTS = --with-ssl=$(STAGING_DIR)/usr --with-threads=fork \
 	--disable-libwrap
