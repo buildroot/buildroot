4d173cc6e393b3055084c6a70f37a45c784e87cb
Bernd Kuhls <bernd@kuhls.net>
2026-05-01 14:30:30 +0200

    package/python-pyopenssl: security bump version to 26.1.0
    https://github.com/pyca/pyopenssl/blob/26.1.0/CHANGELOG.rst
    
    Version 26.1.0 fixes CVE-2026-40475
    Version 26.0.0 fixes CVE-2026-27459 & CVE-2026-27448.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 7bcba8498b257f949cecfb466e56f2891dac51d8)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/python-pyopenssl/python-pyopenssl.mk
+++ package/python-pyopenssl/python-pyopenssl.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYOPENSSL_VERSION = 25.3.0
+PYTHON_PYOPENSSL_VERSION = 26.1.0
 PYTHON_PYOPENSSL_SOURCE = pyopenssl-$(PYTHON_PYOPENSSL_VERSION).tar.gz
-PYTHON_PYOPENSSL_SITE = https://files.pythonhosted.org/packages/80/be/97b83a464498a79103036bc74d1038df4a7ef0e402cfaf4d5e113fb14759
+PYTHON_PYOPENSSL_SITE = https://files.pythonhosted.org/packages/8c/a8/26d36401e3ab8eed9030ad33f381da7856fcfad5691780fccd1b019718fc
 PYTHON_PYOPENSSL_LICENSE = Apache-2.0
 PYTHON_PYOPENSSL_LICENSE_FILES = LICENSE
 PYTHON_PYOPENSSL_CPE_ID_VENDOR = pyopenssl
