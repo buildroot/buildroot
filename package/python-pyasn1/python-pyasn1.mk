f7794333cb489f85ddf1d1ade143e31bba6057d0
Bernd Kuhls <bernd@kuhls.net>
2026-05-01 14:30:26 +0200

    package/python-pyasn1: security bump version to 0.6.3
    https://github.com/pyasn1/pyasn1/blob/v0.6.3/CHANGES.rst
    
    Fixes CVE-2026-30922.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 123136b246f93c78cf9d51df37727d1b2cfcb638)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/python-pyasn1/python-pyasn1.mk
+++ package/python-pyasn1/python-pyasn1.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYASN1_VERSION = 0.6.2
+PYTHON_PYASN1_VERSION = 0.6.3
 PYTHON_PYASN1_SOURCE = pyasn1-$(PYTHON_PYASN1_VERSION).tar.gz
-PYTHON_PYASN1_SITE = https://files.pythonhosted.org/packages/fe/b6/6e630dff89739fcd427e3f72b3d905ce0acb85a45d4ec3e2678718a3487f
+PYTHON_PYASN1_SITE = https://files.pythonhosted.org/packages/5c/5f/6583902b6f79b399c9c40674ac384fd9cd77805f9e6205075f828ef11fb2
 PYTHON_PYASN1_SETUP_TYPE = setuptools
 PYTHON_PYASN1_LICENSE = BSD-2-Clause
 PYTHON_PYASN1_LICENSE_FILES = LICENSE.rst
