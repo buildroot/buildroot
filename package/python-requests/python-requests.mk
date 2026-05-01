c8321c0500a37abaa9460810f47e8a5a39744d87
Bernd Kuhls <bernd@kuhls.net>
2026-05-01 14:30:35 +0200

    package/python-requests: security bump version to 2.33.1
    https://github.com/psf/requests/releases/tag/v2.33.1
    
    https://github.com/psf/requests/releases/tag/v2.33.0
    Fixes CVE-2026-25645.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit b595f48fd9c4077cc0ae2847c2a9d3c00a13603c)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/python-requests/python-requests.mk
+++ package/python-requests/python-requests.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_REQUESTS_VERSION = 2.32.5
+PYTHON_REQUESTS_VERSION = 2.33.1
 PYTHON_REQUESTS_SOURCE = requests-$(PYTHON_REQUESTS_VERSION).tar.gz
-PYTHON_REQUESTS_SITE = https://files.pythonhosted.org/packages/c9/74/b3ff8e6c8446842c3f5c837e9c3dfcfe2018ea6ecef224c710c85ef728f4
+PYTHON_REQUESTS_SITE = https://files.pythonhosted.org/packages/5f/a4/98b9c7c6428a668bf7e42ebb7c79d576a1c3c1e3ae2d47e674b468388871
 PYTHON_REQUESTS_SETUP_TYPE = setuptools
 PYTHON_REQUESTS_LICENSE = Apache-2.0
 PYTHON_REQUESTS_LICENSE_FILES = LICENSE
