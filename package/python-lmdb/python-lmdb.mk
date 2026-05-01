5b4194680c6bcf664d9127c070455ce36cc9125b
Bernd Kuhls <bernd@kuhls.net>
2026-05-01 14:30:11 +0200

    package/python-lmdb: security bump version to 2.2.0
    https://github.com/jnwatson/py-lmdb/blob/py-lmdb_2.2.0/ChangeLog
    
    Version 2.1.0 fixes the following CVEs:
    
    - **CVE-2019-16224**: heap buffer overflow via `MDB_DUPFIXED` without
        `MDB_DUPSORT` in on-disk `md_flags`. (#429)
    
    - **CVE-2019-16225**: `SIGSEGV` from `P_DIRTY` flag set on mmap'd disk pages,
        causing `mdb_page_touch()` to skip copy-on-write. (#429)
    
    - **CVE-2019-16226**: out-of-bounds `memmove` in `mdb_node_del` via corrupt
        `mn_hi` making `NODEDSZ()` huge. (#429)
    
    - **CVE-2019-16227**: NULL pointer dereference of `mc_xcursor` when
        `F_DUPDATA` is set on a node in a non-DUPSORT database. (#429)
    
    - **CVE-2019-16228**: divide-by-zero from zero `mm_psize` in meta page
        header. (#429)
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 2286c4a02cc0a22d1c902ed8e4cb997faefd72d1)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/python-lmdb/python-lmdb.mk
+++ package/python-lmdb/python-lmdb.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_LMDB_VERSION = 1.8.1
+PYTHON_LMDB_VERSION = 2.2.0
 PYTHON_LMDB_SOURCE = lmdb-$(PYTHON_LMDB_VERSION).tar.gz
-PYTHON_LMDB_SITE = https://files.pythonhosted.org/packages/23/19/392f028e7ebcc1cc8212fe8a315a909b7a556278456f0bab9234d3a3b665
+PYTHON_LMDB_SITE = https://files.pythonhosted.org/packages/21/44/d94934efaf8f887b6959f131fde740fcaa831edfd13eb5425574637cddd5
 PYTHON_LMDB_LICENSE = OLDAP-2.8
 PYTHON_LMDB_LICENSE_FILES = LICENSE
 PYTHON_LMDB_DEPENDENCIES = host-python-cffi host-python-patch-ng
