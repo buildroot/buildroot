045c5270cfdff50d975a2007b1555519afc9b46f
Bernd Kuhls <bernd@kuhls.net>
2026-05-06 18:31:22 +0200

    package/cups-filters: fix build against qpdf 12
    This patch adds fixes for cups-filters so it can be build with newer
    versions of qpdf that made changes to PointerHolder:
    https://github.com/qpdf/qpdf/blob/v12.3.2/manual/design.rst#smart-pointers
    
    Build-tested using this defconfig:
    
    BR2_PACKAGE_CUPS=y
    BR2_PACKAGE_CUPS_FILTERS=y
    
                                 arm-aarch64 [ 1/32]: OK
                       bootlin-aarch64-glibc [ 2/32]: OK
                   bootlin-aarch64-glibc-old [ 3/32]: SKIPPED
                   bootlin-arcle-hs38-uclibc [ 4/32]: OK
                        bootlin-armv5-uclibc [ 5/32]: OK
                         bootlin-armv7-glibc [ 6/32]: OK
                       bootlin-armv7m-uclibc [ 7/32]: SKIPPED
                          bootlin-armv7-musl [ 8/32]: OK
                    bootlin-m68k-5208-uclibc [ 9/32]: SKIPPED
                   bootlin-m68k-68040-uclibc [10/32]: OK
                 bootlin-microblazeel-uclibc [11/32]: OK
                      bootlin-mips64el-glibc [12/32]: OK
                    bootlin-mipsel32r6-glibc [13/32]: OK
                       bootlin-mipsel-uclibc [14/32]: OK
                     bootlin-openrisc-uclibc [15/32]: OK
            bootlin-powerpc64le-power8-glibc [16/32]: OK
               bootlin-powerpc-e500mc-uclibc [17/32]: OK
                       bootlin-riscv32-glibc [18/32]: OK
                       bootlin-riscv64-glibc [19/32]: OK
                        bootlin-riscv64-musl [20/32]: OK
                     bootlin-s390x-z13-glibc [21/32]: OK
                          bootlin-sh4-uclibc [22/32]: OK
                       bootlin-sparc64-glibc [23/32]: OK
                        bootlin-sparc-uclibc [24/32]: OK
                        bootlin-x86-64-glibc [25/32]: OK
                         bootlin-x86-64-musl [26/32]: OK
                       bootlin-x86-64-uclibc [27/32]: OK
                       bootlin-x86-i686-musl [28/32]: OK
                       bootlin-xtensa-uclibc [29/32]: OK
                                br-arm-basic [30/32]: SKIPPED
                        br-arm-full-nothread [31/32]: SKIPPED
                          br-arm-full-static [32/32]: SKIPPED
    
    32 builds, 6 skipped, 0 build failed, 0 legal-info failed, 0 show-info failed
    
    This solution avoids[1] a version bump of this package while keeping
    compatibility with the newest version of qpdf. The bump of qpdf is
    needed to fix build errors with gcc 16.x.
    
    [1] https://lists.buildroot.org/pipermail/buildroot/2025-August/784931.html
    Thomas: "which means duplicating all the crazy dependencies of
    libcupsfilters yes :/"
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/cups-filters/cups-filters.mk
+++ package/cups-filters/cups-filters.mk
@@ -10,6 +10,10 @@ CUPS_FILTERS_LICENSE = GPL-2.0, GPL-2.0+, GPL-3.0, GPL-3.0+, LGPL-2, LGPL-2.1+, 
 CUPS_FILTERS_LICENSE_FILES = COPYING
 CUPS_FILTERS_CPE_ID_VENDOR = linuxfoundation
 
+# 0003-libcupsfilters-Fixed-building-with-QPDF-11.x.patch
+# 0004-Updated-code-to-be-built-with-QPDF-12.x.patch
+CUPS_FILTERS_AUTORECONF = YES
+
 # 0001-beh-backend-Use-execv-instead-of-system-CVE-2023-24805.patch
 CUPS_FILTERS_IGNORE_CVES += CVE-2023-24805
 
