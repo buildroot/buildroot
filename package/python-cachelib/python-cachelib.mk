11e5c1ffdff1bfb8609caeb769ef23c4a269dd30
Daniel Crowe <daniel.crowe@maxmine.com.au>
2026-05-12 16:58:32 +0930

    package/python-cachelib: bump version to 0.14.0
    Signed-off-by: Daniel Crowe <daniel.crowe@maxmine.com.au>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-cachelib/python-cachelib.mk
+++ package/python-cachelib/python-cachelib.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_CACHELIB_VERSION = 0.13.0
+PYTHON_CACHELIB_VERSION = 0.14.0
 PYTHON_CACHELIB_SOURCE = cachelib-$(PYTHON_CACHELIB_VERSION).tar.gz
-PYTHON_CACHELIB_SITE = https://files.pythonhosted.org/packages/1d/69/0b5c1259e12fbcf5c2abe5934b5c0c1294ec0f845e2b4b2a51a91d79a4fb
-PYTHON_CACHELIB_SETUP_TYPE = setuptools
+PYTHON_CACHELIB_SITE = https://files.pythonhosted.org/packages/66/a5/5eb041dbee71766704d44cf5dfb6950ab018be0fd02cd763ade09869e33c
+PYTHON_CACHELIB_SETUP_TYPE = flit
 PYTHON_CACHELIB_LICENSE = BSD-3-Clause
-PYTHON_CACHELIB_LICENSE_FILES = LICENSE.rst docs/license.rst
+PYTHON_CACHELIB_LICENSE_FILES = LICENSE.txt docs/license.rst
 
 $(eval $(python-package))
