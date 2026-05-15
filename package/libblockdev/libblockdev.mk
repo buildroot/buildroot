buildroot/buildroot

commit c03745d7de627e70b1142a6f212fe7e5f060cdd7

package/libblockdev: bump to version 3.5.0
Release notes:
https://github.com/storaged-project/libblockdev/blob/3.5.0/NEWS.rst

Reworked local patch.

Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
Signed-off-by: Julien Olivain <ju.o@free.fr>

package/libblockdev/libblockdev.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBBLOCKDEV_VERSION = 3.4.0
+LIBBLOCKDEV_VERSION = 3.5.0
 LIBBLOCKDEV_SITE = https://github.com/storaged-project/libblockdev/releases/download/$(LIBBLOCKDEV_VERSION)
 LIBBLOCKDEV_LICENSE = LGPL-2.1
 LIBBLOCKDEV_LICENSE_FILES = LICENSE

package/libblockdev/0001-Provide-replacement-function-for-strerror_l.patch
@@ -1,5 +1,5 @@
-From 01883cc14904e55d90955ad7cd9fc55bc18364ac Mon Sep 17 00:00:00 2001
-From: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
+From 3d198be196bdc59ba332b87d8c2959de0b9160d2 Mon Sep 17 00:00:00 2001
+From: Giulio Benetti <giulio.benetti@benettiengineering.com>
 Date: Sat, 20 Jan 2024 13:29:28 +0100
 Subject: [PATCH] Provide replacement function for strerror_l()
 
@@ -11,18 +11,19 @@ Upstream: N/A since it's due uclibc only
 
 Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
 Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
-[Giulio: ported to version 3.0 and 3.1]
+[Giulio: ported to version 3.0, 3.1 and 3.5]
 ---
- configure.ac       | 2 ++
- src/utils/module.c | 1 +
- src/utils/utils.h  | 4 ++++
- 3 files changed, 7 insertions(+)
+ configure.ac          | 2 ++
+ src/utils/dev_utils.c | 1 +
+ src/utils/module.c    | 1 +
+ src/utils/utils.h     | 4 ++++
+ 4 files changed, 8 insertions(+)
 
 diff --git a/configure.ac b/configure.ac
-index 02b26e3e..67341134 100644
+index e5d65d8b..507fa854 100644
 --- a/configure.ac
 +++ b/configure.ac
-@@ -120,6 +120,8 @@ AC_CHECK_HEADERS([dlfcn.h string.h unistd.h sys/fcntl.h sys/ioctl.h linux/random
+@@ -123,6 +123,8 @@ AC_CHECK_HEADERS([dlfcn.h string.h unistd.h sys/fcntl.h sys/ioctl.h linux/random
                   [LIBBLOCKDEV_SOFT_FAILURE([Header file $ac_header not found.])],
                   [])
  
@@ -31,8 +32,20 @@ index 02b26e3e..67341134 100644
  AC_ARG_WITH([escrow],
      AS_HELP_STRING([--with-escrow], [support escrow @<:@default=yes@:>@]),
      [],
+diff --git a/src/utils/dev_utils.c b/src/utils/dev_utils.c
+index 89a54b4c..b4be5b44 100644
+--- a/src/utils/dev_utils.c
++++ b/src/utils/dev_utils.c
+@@ -23,6 +23,7 @@
+ #include <errno.h>
+ 
+ #include "dev_utils.h"
++#include "utils.h"
+ 
+ /**
+  * bd_utils_dev_utils_error_quark: (skip)
 diff --git a/src/utils/module.c b/src/utils/module.c
-index 6557c3ab..5893f0ac 100644
+index 11e139f8..c8b124e0 100644
 --- a/src/utils/module.c
 +++ b/src/utils/module.c
 @@ -28,6 +28,7 @@
@@ -59,5 +72,5 @@ index 801a8b6e..b2bdf0d0 100644
   * SECTION: utils
   * @short_description: library providing utility functions used by the blockdev library and its plugins
 -- 
-2.34.1
+2.47.3
 

package/libblockdev/libblockdev.hash
@@ -1,3 +1,3 @@
 # Locally calculated
-sha256  65ef9a37babd44b85b8ff9b273f90f9f7d5f8ff7b0c76a8edb69240325fd83f4  libblockdev-3.4.0.tar.gz
+sha256  bccd30e6b5d11504de60d9889ff6a2a25b07a4ec8f04070f2387e168301b3e3a  libblockdev-3.5.0.tar.gz
 sha256  97bdc721d875501b6243a456333fdfdb1ab64d31c4da2554de845caf4674b946  LICENSE

commit 89609ad586bc8ff387899c2733e668e7f96973f6

package/libblockdev: fix build failure due to missing dependency json-glib
When BR2_PACKAGE_LIBBLOCKDEV_LVM2 is enabled it requires package
json-glib to be enabled starting from libblockdev version 3.5.0
so let's enable it accordingly.

Fixes:
https://autobuild.buildroot.org/results/fceb64bb7fcfced61ef900b580b458135baa3713/

Fixes: c03745d7de627e70b1142a6f212fe7e5f060cdd7

Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
[Romain: add the commit hash introducing the build issue]
Signed-off-by: Romain Naour <romain.naour@smile.fr>

package/libblockdev/Config.in
@@ -47,6 +47,7 @@ config BR2_PACKAGE_LIBBLOCKDEV_LVM2
 	bool "lvm2"
 	depends on BR2_ENABLE_LOCALE # parted
 	depends on BR2_TOOLCHAIN_GCC_AT_LEAST_5 # parted
+	select BR2_PACKAGE_JSON_GLIB
 	select BR2_PACKAGE_PARTED
 	select BR2_PACKAGE_LIBYAML
 	select BR2_PACKAGE_LVM2

package/libblockdev/libblockdev.mk
@@ -47,7 +47,7 @@ LIBBLOCKDEV_CONF_OPTS += --without-loop
 endif
 
 ifeq ($(BR2_PACKAGE_LIBBLOCKDEV_LVM2),y)
-LIBBLOCKDEV_DEPENDENCIES += libyaml lvm2 parted
+LIBBLOCKDEV_DEPENDENCIES += json-glib libyaml lvm2 parted
 LIBBLOCKDEV_CONF_OPTS += --with-lvm
 else
 LIBBLOCKDEV_CONF_OPTS += --without-lvm