af42bd7679f06ddc85416e2cb60ca78e13cc41a4
Bernd Kuhls <bernd@kuhls.net>
2026-05-24 09:07:04 +0200

    package/putty: security bump version to 0.84
    https://lists.tartarus.org/pipermail/putty-announce/2026/000042.html
    
    Updated license hash due to copyright year bump in upstream commit
    995b63a82bac4f6985958cb92943e807a9f35736. A link to this commit can not
    be provided due to the shutdown of the upstream gitweb interface:
    https://git.tartarus.org/?p=simon/putty.git
    
    Switched URLs to https.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 7418ff13895be20ccdd6ad6d460e6dbf73727637)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/putty/putty.mk
+++ package/putty/putty.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PUTTY_VERSION = 0.83
-PUTTY_SITE = http://the.earth.li/~sgtatham/putty/$(PUTTY_VERSION)
+PUTTY_VERSION = 0.84
+PUTTY_SITE = https://the.earth.li/~sgtatham/putty/$(PUTTY_VERSION)
 PUTTY_LICENSE = MIT
 PUTTY_LICENSE_FILES = LICENCE
 PUTTY_CPE_ID_VENDOR = putty
