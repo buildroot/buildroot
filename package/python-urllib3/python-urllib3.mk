8b606d9074fff980fd1e73786531503b30fb14da
Peter Korsgaard <peter@korsgaard.com>
2026-05-26 15:28:26 +0200

    package/python-urllib3: security bump to version 2.7.0
    Fixes the following security issues:
    
    CVE-2026-44431: Sensitive headers forwarded across origins in proxied
    low-level redirects
    
    https://github.com/urllib3/urllib3/security/advisories/GHSA-qccp-gfcp-xxvc
    
    CVE-2026-44432: Decompression-bomb safeguards bypassed in parts of the
    streaming API
    
    https://github.com/urllib3/urllib3/security/advisories/GHSA-mf9v-mfxr-j63j
    
    For more details, see the release notes:
    https://urllib3.readthedocs.io/en/stable/changelog.html#id1
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-urllib3/python-urllib3.mk
+++ package/python-urllib3/python-urllib3.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_URLLIB3_VERSION = 2.6.3
+PYTHON_URLLIB3_VERSION = 2.7.0
 PYTHON_URLLIB3_SOURCE = urllib3-$(PYTHON_URLLIB3_VERSION).tar.gz
-PYTHON_URLLIB3_SITE = https://files.pythonhosted.org/packages/c7/24/5f1b3bdffd70275f6661c76461e25f024d5a38a46f04aaca912426a2b1d3
+PYTHON_URLLIB3_SITE = https://files.pythonhosted.org/packages/53/0c/06f8b233b8fd13b9e5ee11424ef85419ba0d8ba0b3138bf360be2ff56953
 PYTHON_URLLIB3_LICENSE = MIT
 PYTHON_URLLIB3_LICENSE_FILES = LICENSE.txt
 PYTHON_URLLIB3_CPE_ID_VENDOR = python
