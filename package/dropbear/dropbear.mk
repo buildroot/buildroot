d8a8dd14f6f275173e610c79974c624847672f36
Thomas Perale <thomas.perale@mind.be>
2026-05-27 12:50:04 +0200

    package/dropbear: security bump to version 2026.90
    https://github.com/mkj/dropbear/releases/tag/DROPBEAR_2026.90
    https://matt.ucc.asn.au/dropbear/CHANGES
    
    Fixes CVE-2019-6111 & CVE-2026-35385.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 5b136c89ffed35c30dff1802924a4dcf9bdab798)
    [thomas: reverts commit 30d1791eb26a8ef46a952d1ef66e163de1eade07]
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/dropbear/dropbear.mk
+++ package/dropbear/dropbear.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DROPBEAR_VERSION = 2025.89
+DROPBEAR_VERSION = 2026.90
 DROPBEAR_SITE = https://matt.ucc.asn.au/dropbear/releases
 DROPBEAR_SOURCE = dropbear-$(DROPBEAR_VERSION).tar.bz2
 DROPBEAR_LICENSE = MIT, BSD-2-Clause, Public domain
@@ -13,12 +13,6 @@ DROPBEAR_TARGET_BINS = dropbearkey dropbearconvert scp
 DROPBEAR_PROGRAMS = dropbear $(DROPBEAR_TARGET_BINS)
 DROPBEAR_CPE_ID_VENDOR = dropbear_ssh_project
 DROPBEAR_CPE_ID_PRODUCT = dropbear_ssh
-
-# 0001-CVE-2019-6111.patch
-DROPBEAR_IGNORE_CVES += CVE-2019-6111
-
-# 0002-CVE-2026-35385.patch
-DROPBEAR_IGNORE_CVES += CVE-2026-35385
 
 # Disable hardening flags added by dropbear configure.ac, and let
 # Buildroot add them when the relevant options are enabled. This
