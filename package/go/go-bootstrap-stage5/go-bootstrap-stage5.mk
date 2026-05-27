131ffd8b36941361f41ee287eaad42b33d944b5b
Christian Stewart <christian@aperture.us>
2026-05-27 10:24:15 +0200

    package/go: security bump to version 1.26.1
    Building Go 1.26 and later requires Go 1.24.6 or later for bootstrap.
    
    To support this we use Go version 1.25.8 as the version for
    go-bootstrap-stage5 and have the build for Go 1.26.1 depend on
    go-bootstrap-stage5.
    
    Go version 1.25.8 is the latest Go version we can build using
    go-bootstrap-stage4.
    
    The package build for go-bootstrap-stage5 is effectively identical to
    go-bootstrap-stage4 with only the Go version and stage number changed.
    
    Go 1.28 is expected to require a minor release of Go 1.26 for bootstrap.
    
    Fixes the following security vulnerabilities:
    
    - CVE-2026-25679: net/url: reject IPv6 literal not at start of host
    - CVE-2026-27142: html/template: URLs in meta attribute actions not escaped
    - CVE-2026-27137: crypto/x509: incorrect enforcement of email constraints
    - CVE-2026-27138: crypto/x509: panic in name constraint checking: certificates
    - CVE-2026-27139: os: FileInfo can escape from a Root
    
    For full release notes, see:
    https://go.dev/doc/devel/release#go1.26.0
    
    Signed-off-by: Christian Stewart <christian@aperture.us>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 43d132319652258666a9896b23c32fb5c419a6e0)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

---
+++ package/go/go-bootstrap-stage5/go-bootstrap-stage5.mk
@@ -0,0 +1,54 @@
+################################################################################
+#
+# go-bootstrap-stage5
+#
+################################################################################
+
+# Use last Go version that go-bootstrap-stage4 can build: v1.25.x
+# See https://go.dev/doc/go1.26#bootstrap
+GO_BOOTSTRAP_STAGE5_VERSION = 1.25.8
+GO_BOOTSTRAP_STAGE5_SITE = https://go.dev/dl
+GO_BOOTSTRAP_STAGE5_SOURCE = go$(GO_BOOTSTRAP_STAGE5_VERSION).src.tar.gz
+
+GO_BOOTSTRAP_STAGE5_LICENSE = BSD-3-Clause
+GO_BOOTSTRAP_STAGE5_LICENSE_FILES = LICENSE
+
+# Use go-bootstrap-stage4 to bootstrap.
+HOST_GO_BOOTSTRAP_STAGE5_DEPENDENCIES = host-go-bootstrap-stage4
+
+HOST_GO_BOOTSTRAP_STAGE5_ROOT = $(HOST_DIR)/lib/go-$(GO_BOOTSTRAP_STAGE5_VERSION)
+
+# The go build system is not compatible with ccache, so use
+# HOSTCC_NOCCACHE.  See https://github.com/golang/go/issues/11685.
+HOST_GO_BOOTSTRAP_STAGE5_MAKE_ENV = \
+	GO111MODULE=off \
+	GOCACHE=$(HOST_GO_HOST_CACHE) \
+	GOROOT_BOOTSTRAP=$(HOST_GO_BOOTSTRAP_STAGE4_ROOT) \
+	GOROOT_FINAL=$(HOST_GO_BOOTSTRAP_STAGE5_ROOT) \
+	GOROOT="$(@D)" \
+	GOBIN="$(@D)/bin" \
+	GOOS=linux \
+	CC=$(HOSTCC_NOCCACHE) \
+	CXX=$(HOSTCXX_NOCCACHE) \
+	CGO_ENABLED=0
+
+define HOST_GO_BOOTSTRAP_STAGE5_BUILD_CMDS
+	cd $(@D)/src && \
+		$(HOST_GO_BOOTSTRAP_STAGE5_MAKE_ENV) ./make.bash $(if $(VERBOSE),-v)
+endef
+
+define HOST_GO_BOOTSTRAP_STAGE5_INSTALL_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/bin/go $(HOST_GO_BOOTSTRAP_STAGE5_ROOT)/bin/go
+	$(INSTALL) -D -m 0755 $(@D)/bin/gofmt $(HOST_GO_BOOTSTRAP_STAGE5_ROOT)/bin/gofmt
+
+	cp -a $(@D)/lib $(HOST_GO_BOOTSTRAP_STAGE5_ROOT)/
+
+	mkdir -p $(HOST_GO_BOOTSTRAP_STAGE5_ROOT)/pkg
+	cp -a $(@D)/pkg/include $(HOST_GO_BOOTSTRAP_STAGE5_ROOT)/pkg/
+	cp -a $(@D)/pkg/tool $(HOST_GO_BOOTSTRAP_STAGE5_ROOT)/pkg/
+
+	# The Go sources must be installed to the host/ tree for the Go stdlib.
+	cp -a $(@D)/src $(HOST_GO_BOOTSTRAP_STAGE5_ROOT)/
+endef
+
+$(eval $(host-generic-package))
