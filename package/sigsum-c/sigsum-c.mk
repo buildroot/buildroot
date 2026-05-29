c58fb5f97b7cc6177c1ac675467f0ec43de44b8e
Florian Larysch <fl@n621.de>
2026-05-29 21:12:37 +0200

    package/sigsum-c: new package
    Add the sigsum-c library, which is a more lightweight alternative to
    sigsum-go when only proof verification is needed or needs to be embedded
    in some other program.
    
    Signed-off-by: Florian Larysch <fl@n621.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/sigsum-c/sigsum-c.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# sigsum-c
+#
+################################################################################
+
+SIGSUM_C_VERSION = v1.0.0
+SIGSUM_C_SITE = https://git.glasklar.is/sigsum/core/sigsum-c
+SIGSUM_C_SITE_METHOD = git
+SIGSUM_C_LICENSE = BSD-2-Clause
+SIGSUM_C_LICENSE_FILES = LICENSE
+SIGSUM_C_INSTALL_STAGING = YES
+SIGSUM_C_DEPENDENCIES = nettle
+
+SIGSUM_C_AUTORECONF = YES
+
+ifneq ($(BR2_PACKAGE_SIGSUM_C_TOOLS),y)
+SIGSUM_C_CONF_OPTS += --disable-tools
+endif
+
+$(eval $(autotools-package))
