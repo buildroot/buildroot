2c3bc6609b1d08f0953f85f7101da68568d96e1c
Thomas Perale <thomas.perale@mind.be>
2026-05-29 11:45:11 +0200

    package/cups-filter: update CPE vendor
    The CPE "cpe:2.3:a:openprinting:cups-filters:*:*:*:*:*:*:*:*" is valid
    for this package.
    
    See the GHSA [1] and the associated CVE [2].
    
    [1] https://github.com/OpenPrinting/cups-filters/security/advisories/GHSA-893j-2wr2-wrh9
    [2] https://nvd.nist.gov/vuln/detail/CVE-2025-64503
    
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/cups-filters/cups-filters.mk
+++ package/cups-filters/cups-filters.mk
@@ -8,7 +8,7 @@ CUPS_FILTERS_VERSION = 1.28.17
 CUPS_FILTERS_SITE = https://github.com/OpenPrinting/cups-filters/releases/download/$(CUPS_FILTERS_VERSION)
 CUPS_FILTERS_LICENSE = GPL-2.0, GPL-2.0+, GPL-3.0, GPL-3.0+, LGPL-2, LGPL-2.1+, MIT, BSD-4-Clause
 CUPS_FILTERS_LICENSE_FILES = COPYING
-CUPS_FILTERS_CPE_ID_VENDOR = linuxfoundation
+CUPS_FILTERS_CPE_ID_VENDOR = openprinting
 
 # 0001-beh-backend-Use-execv-instead-of-system-CVE-2023-24805.patch
 CUPS_FILTERS_IGNORE_CVES += CVE-2023-24805
