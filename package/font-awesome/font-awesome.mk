d1abbae02ed573ebd29b3c0984bb9a8f04232ff3
Shubham Chakraborty <chakrabortyshubham66@gmail.com>
2026-05-30 23:11:34 +0530

    package/font-awesome: bump to version 7.2.0
    This release updates the upstream icon set and related assets.
    Refresh the source hash for the new release. Also drop the "v" tag
    prefix as upstream dropped it starting from v5.0.
    
    License changes:
    - Upstream introduced SVG icons as a first-class feature alongside the
      traditional icon fonts starting with v5.0.
    - The new icon designs (delivered as SVG and JS assets) are licensed
      under CC-BY-4.0 (attribution is satisfied by the copyright comments
      in the files).
    - The webfont files themselves remain under OFL-1.1, and helper styles
      and build code remain under the MIT license.
    - Hence, the license is updated to: OFL-1.1 (fonts), MIT (code), CC-BY-4.0 (icons)
    - Add LICENSE.txt for hash verification.
    
    Directory changes:
    - Drop "less" and "fonts". LESS support has been completely dropped by
      upstream in v7.0, and the old font files directory was renamed.
    - Add "webfonts", which contains the newer .woff2 webfonts.
    - Add "js", which contains scripts for the SVG + JS rendering method.
    - Add "sprites" and "svgs", which contain SVG sprite sheets and raw SVG
      icons (the CC-BY-4.0 assets) respectively.
    - Installing all these directories allows users to choose between the
      various rendering methods (CSS+webfonts, SVG+JS, CSS sprite sheets,
      or direct SVG usage) for their target systems.
    
    For detailed Chngelog
    - https://fontawesome.com/changelog
    
    Signed-off-by: Shubham Chakraborty <chakrabortyshubham66@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/font-awesome/font-awesome.mk
+++ package/font-awesome/font-awesome.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-FONT_AWESOME_VERSION = 4.7.0
-FONT_AWESOME_SITE = $(call github,FortAwesome,Font-Awesome,v$(FONT_AWESOME_VERSION))
-FONT_AWESOME_LICENSE = OFL-1.1 (font), MIT (CSS, LESS and Sass files)
-FONT_AWESOME_DIRECTORIES_LIST = css fonts less scss
+FONT_AWESOME_VERSION = 7.2.0
+FONT_AWESOME_SITE = $(call github,FortAwesome,Font-Awesome,$(FONT_AWESOME_VERSION))
+FONT_AWESOME_LICENSE = OFL-1.1 (fonts), MIT (code), CC-BY-4.0 (icons)
+FONT_AWESOME_LICENSE_FILES = LICENSE.txt
+FONT_AWESOME_DIRECTORIES_LIST = css js scss webfonts sprites svgs
 
 define FONT_AWESOME_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/font-awesome/
