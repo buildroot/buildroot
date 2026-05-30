646ea396c14b0da0016f42eba2b147c8767e6dfb
Bernd Kuhls <bernd@kuhls.net>
2026-05-30 17:57:15 +0200

    package/stellarium: add extra downloads
    Building Stellarium includes the download of external tarballs during
    configure.
    
    This fails with host-cmake during configure stage:
    
    CMake Error at md4c-subbuild/md4c-populate-prefix/src/md4c-populate-stamp/download-md4c-populate.cmake:163 (message):
      Each download failed!
    
        error: downloading 'https://github.com/mity/md4c/archive/refs/tags/release-0.5.2.tar.gz' failed
              status_code: 1
              status_string: "Unsupported protocol"
              log:
              --- LOG BEGIN ---
              Protocol "https" not supported
    
      closing connection #-1
    
    due to buildroot commit f87138339b17bc2b1d84c59ea176abb941413550 which
    disabled SSL support for host-cmake.
    
    This patch uses _EXTRA_DOWNLOADS to provide these tarball to allow
    offline builds.
    
    No autobuilder error was recorded, the build error can be reproduced
    with this defconfig:
    
    BR2_x86_64=y
    BR2_x86_corei7=y
    BR2_TOOLCHAIN_EXTERNAL=y
    BR2_TOOLCHAIN_EXTERNAL_BOOTLIN_X86_64_CORE_I7_GLIBC_BLEEDING_EDGE=y
    BR2_FORCE_HOST_BUILD=y
    BR2_PACKAGE_STELLARIUM=y
    BR2_PACKAGE_MESA3D=y
    BR2_PACKAGE_MESA3D_GALLIUM_DRIVER_SOFTPIPE=y
    BR2_PACKAGE_MESA3D_OPENGL_GLX=y
    BR2_PACKAGE_QT5=y
    BR2_PACKAGE_XORG7=y
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/stellarium/stellarium.mk
+++ package/stellarium/stellarium.mk
@@ -9,6 +9,20 @@ STELLARIUM_SOURCE = stellarium-$(STELLARIUM_VERSION).tar.xz
 STELLARIUM_SITE = https://github.com/Stellarium/stellarium/releases/download/v$(STELLARIUM_VERSION)
 STELLARIUM_LICENSE = GPL-2.0+
 STELLARIUM_LICENSE_FILES = COPYING
+
+STELLARIUM_EXTRA_DOWNLOADS = \
+	https://github.com/mity/md4c/archive/refs/tags/release-0.5.2.tar.gz \
+	https://github.com/stevengj/nlopt/archive/refs/tags/v2.9.0.tar.gz
+
+# Prevent stellarium from downloading stuff during its configure step
+define STELLARIUM_COPY_EXTRA_DOWNLOADS
+	mkdir -p $(@D)/_deps/md4c-subbuild/md4c-populate-prefix/src/
+	cp $(STELLARIUM_DL_DIR)/release-0.5.2.tar.gz $(@D)/_deps/md4c-subbuild/md4c-populate-prefix/src/
+	mkdir -p $(@D)/_deps/nlopt-subbuild/nlopt-populate-prefix/src/
+	cp $(STELLARIUM_DL_DIR)/v2.9.0.tar.gz $(@D)/_deps/nlopt-subbuild/nlopt-populate-prefix/src/
+endef
+STELLARIUM_POST_EXTRACT_HOOKS += STELLARIUM_COPY_EXTRA_DOWNLOADS
+
 STELLARIUM_DEPENDENCIES = \
 	qt5base \
 	qt5charts \
