buildroot/buildroot

commit d2ed4565321c74772014681ca5ddcb433162e468

package/liburing: bump to version 2.12
Switch to github since git.kernel.dk is not always reachable and actual
tarball sha256 has changed. Also note that local patch is required because
it's been reverted wit this commit [1].

Release Notes:
https://github.com/axboe/liburing/releases/tag/liburing-2.12

[1]:
https://github.com/axboe/liburing/commit/6f8b3481cf511ade803fe7a2ea8fe3b99fce3658

Fixes:
https://autobuild.buildroot.net/results/ddf/ddfd0bb8f5623a1ce7aa4168c5759b2c54a933da/

Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
Signed-off-by: Julien Olivain <ju.o@free.fr>

package/liburing/liburing.hash
@@ -1,8 +1,8 @@
 # Locally calculated
-sha256  78bcc0dc0d004a238d8b5f597adbb4ec74926352a3983b872db7f0efdb72565d  liburing-2.6.tar.bz2
+sha256  f1d10cb058c97c953b4c0c446b11e9177e8c8b32a5a88b309f23fdd389e26370  liburing-2.12.tar.gz
 
 # Hash for license files
 sha256  592987e8510228d546540b84a22444bde98e48d03078d3b2eefcd889bec5ce8c  COPYING
 sha256  8177f97513213526df2cf6184d8ff986c675afb514d4e68a404010521b880643  COPYING.GPL
 sha256  2ec12f7934cdfce25ff72b0557738989d79fc7a496d1cf9b2d3f9464fd61edbe  LICENSE
-sha256  b0731ca09e6f4718d456a989d32b6c61bf27e5d4bab6ca31e409d75c5120d19e  README
+sha256  d98e81bb7a8fcfb5506f02c3190b5bc2e286970591072edc4575c26624d827dd  README

package/liburing/liburing.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-LIBURING_VERSION = 2.6
-LIBURING_SOURCE = liburing-$(LIBURING_VERSION).tar.bz2
-LIBURING_SITE = https://git.kernel.dk/cgit/liburing/snapshot
+LIBURING_VERSION = liburing-2.12
+LIBURING_SOURCE = $(LIBURING_VERSION).tar.gz
+LIBURING_SITE = $(call github,axboe,liburing,$(LIBURING_VERSION))
 LIBURING_LICENSE = (GPL-2.0 with exceptions and LGPL-2.1+) or MIT
 LIBURING_LICENSE_FILES = COPYING COPYING.GPL LICENSE README
 LIBURING_INSTALL_STAGING = YES

package/liburing/Config.in
@@ -9,7 +9,7 @@ config BR2_PACKAGE_LIBURING
 	  simplified interface for applications that don't need (or
 	  want) to deal with the full kernel side implementation.
 
-	  https://git.kernel.dk/cgit/liburing
+	  https://github.com/axboe/liburing
 
 comment "liburing needs a toolchain w/ gcc >= 4.9, headers >= 5.1"
 	depends on BR2_USE_MMU

commit 8e2d15cb760c10474862697a4917e32878e5d279

package/liburing: properly handle VERSION vs. SITE
VERSION should ideally contain only the version, i.e 2.12 and not some
additional prefix like liburing- as it prevents our logic matching
against release-monitoring.org data to work properly.

Fixes: d2ed4565321c74772014681ca5ddcb433162e468 ("package/liburing: bump to version 2.12")
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>

package/liburing/liburing.hash
@@ -1,5 +1,5 @@
 # Locally calculated
-sha256  f1d10cb058c97c953b4c0c446b11e9177e8c8b32a5a88b309f23fdd389e26370  liburing-2.12.tar.gz
+sha256  f1d10cb058c97c953b4c0c446b11e9177e8c8b32a5a88b309f23fdd389e26370  2.12.tar.gz
 
 # Hash for license files
 sha256  592987e8510228d546540b84a22444bde98e48d03078d3b2eefcd889bec5ce8c  COPYING

package/liburing/liburing.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-LIBURING_VERSION = liburing-2.12
+LIBURING_VERSION = 2.12
 LIBURING_SOURCE = $(LIBURING_VERSION).tar.gz
-LIBURING_SITE = $(call github,axboe,liburing,$(LIBURING_VERSION))
+LIBURING_SITE = $(call github,axboe,liburing,liburing-$(LIBURING_VERSION))
 LIBURING_LICENSE = (GPL-2.0 with exceptions and LGPL-2.1+) or MIT
 LIBURING_LICENSE_FILES = COPYING COPYING.GPL LICENSE README
 LIBURING_INSTALL_STAGING = YES