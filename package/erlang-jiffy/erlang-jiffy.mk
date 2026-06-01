c2f607a19bc0b466e4a511396ab326364ae883c5
Giulio Benetti <giulio.benetti@benettiengineering.com>
2026-06-01 14:33:18 +0200

    package/erlang-jiffy: bump to version 2.0.0
    Release Notes:
    https://github.com/davisp/jiffy/releases/tag/2.0.0
    
    On version 2.0.0 it's been used https://github.com/kolemannix/ffc.h
    that is covered by Apache-2.0 so let's add it to license list.
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Marcus Hoffmann <buildroot@bubu1.eu>

--- package/erlang-jiffy/erlang-jiffy.mk
+++ package/erlang-jiffy/erlang-jiffy.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-ERLANG_JIFFY_VERSION = 1.1.3
+ERLANG_JIFFY_VERSION = 2.0.0
 ERLANG_JIFFY_SITE = $(call github,davisp,jiffy,$(ERLANG_JIFFY_VERSION))
-ERLANG_JIFFY_LICENSE = MIT (core), \
+ERLANG_JIFFY_LICENSE = Apache-2.0, \
+	MIT (core), \
 	BSD-3-Clause (Google double conversion library), \
 	BSD-3-Clause (tests)
 ERLANG_JIFFY_LICENSE_FILES = LICENSE
