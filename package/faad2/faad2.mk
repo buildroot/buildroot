bd37cbe9032c9b92797437901ac435a2e6af5272
Thomas Perale <thomas.perale@mind.be>
2026-06-02 14:40:23 +0200

    package/faad2: update CPE
    The CPE audiocoding:freeware_advanced_audio_decoder_2 hasn't received
    any new CVEs since 2018 while faad2_project:faad2 received 9 new cves
    [1].
    
    See the full history in [2].
    
    [1] https://nvd.nist.gov/vuln/detail/CVE-2023-38857
    [2] https://security-tracker.debian.org/tracker/source-package/faad2
    
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>
    [Peter: set FAAD2_CPE_ID_VALID = YES for check-package]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit ab03ba7396017b8911944c3089c06c9b68822fbe)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/faad2/faad2.mk
+++ package/faad2/faad2.mk
@@ -8,8 +8,7 @@ FAAD2_VERSION = 2.11.2
 FAAD2_SITE = $(call github,knik0,faad2,$(FAAD2_VERSION))
 FAAD2_LICENSE = GPL-2.0
 FAAD2_LICENSE_FILES = COPYING
-FAAD2_CPE_ID_VENDOR = audiocoding
-FAAD2_CPE_ID_PRODUCT = freeware_advanced_audio_decoder_2
+FAAD2_CPE_ID_VALID = YES
 FAAD2_INSTALL_STAGING = YES
 
 # faad2 contains assembly routines using ARM instructions not present in thumb1 mode:
