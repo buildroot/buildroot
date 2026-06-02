d7daada10840c2c23bb29288ab2ddc7dcdf7f9d8
Thomas Perale <thomas.perale@mind.be>
2026-06-02 14:40:09 +0200

    package/lrzip: update CPE
    The CPE long_range_zip_project:long_range_zip hasn't been used since
    2023. While ckolivas:lrzip got 3 CVEs that affect the current Buildroot
    version [1][2][3].
    
    [1] https://nvd.nist.gov/vuln/detail/CVE-2025-9396
    [2] https://nvd.nist.gov/vuln/detail/CVE-2025-15570
    [2] https://nvd.nist.gov/vuln/detail/CVE-2025-15571
    
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 2dce5a7844d061860810da4b77965d96d09eb2b4)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/lrzip/lrzip.mk
+++ package/lrzip/lrzip.mk
@@ -9,8 +9,7 @@ LRZIP_SOURCE = lrzip-$(LRZIP_VERSION).tar.xz
 LRZIP_SITE = http://ck.kolivas.org/apps/lrzip
 LRZIP_LICENSE = GPL-2.0+
 LRZIP_LICENSE_FILES = COPYING
-LRZIP_CPE_ID_VENDOR = long_range_zip_project
-LRZIP_CPE_ID_PRODUCT = long_range_zip
+LRZIP_CPE_ID_VENDOR = ckolivas
 LRZIP_DEPENDENCIES = zlib lz4 lzo bzip2
 
 ifeq ($(BR2_i386)$(BR2_x86_64),y)
