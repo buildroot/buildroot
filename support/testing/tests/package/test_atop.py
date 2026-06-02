21057a30f2244a1a68092de294027a34fb6ee103
Romain Naour <romain.naour@smile.fr>
2026-06-02 21:55:38 +0200

    support/testing: TestAtop: use the default toolchain (BASIC_TOOLCHAIN_CONFIG)
    The TestAtop use the really old toolchain br-arm-full-2019.05.1 because
    BASIC_TOOLCHAIN_CONFIG from Buildroot 2019.08 provide an uClibc-ng
    toolchain with 3.10 kernel headers (br-arm-full-2017.05-1078-g95b1dae)
    [1].
    
    Since then, we switched to glibc toolchains by default in Buildroot [2]
    (2022.11) and BASIC_TOOLCHAIN_CONFIG was updated several time and now
    use a Bootlin toolchain stable (2025.08-1). This toolchain use gcc 14,
    5.3 kernel headers, glibc 2.41.
    
    We need to update the toolchain in TestAtop since Busybox 1.38.0 added
    yescrypt algorithms that requires explicit_bzero() introduced in Glibc
    2.25 [4], uClibc-ng 1.0.47 [5] and musl 1.1.20 [6].
    
    There is no real reason to keep this really old toolchain, so bump
    the toolchain to Bootlin toolchain stable (2025.08-1).
    
    Fixes:
    https://gitlab.com/buildroot.org/buildroot/-/jobs/14622325905
    
    [1] 9609a7c2c22160523bda5cb5b4417c9278ac27d9
    [2] 4057e36ca9665edd5248512e4edba2c243b8f4be
    [3] https://github.com/vda-linux/busybox_mirror/commit/80e676664e1d7a0b07f14bff44f93d1fef709cf4
    [4] https://sourceware.org/git/?p=glibc.git;a=commit;h=ea1bd74defcf9d5291d14972e63105168ca9eb4f
    [5] https://github.com/wbx-github/uclibc-ng/commit/9e7b342fde5505c0a1d4968e9d70537ac406f640
    [6] https://git.musl-libc.org/cgit/musl/commit/?id=05ac345f895098657cf44d419b5d572161ebaf43
    
    Signed-off-by: Romain Naour <romain.naour@smile.fr>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- support/testing/tests/package/test_atop.py
+++ support/testing/tests/package/test_atop.py
@@ -2,23 +2,9 @@ import os
 
 import infra.basetest
 
-BASIC_TOOLCHAIN_CONFIG_HEADERS_AT_LEAST_3_14 = \
-    """
-    BR2_arm=y
-    BR2_TOOLCHAIN_EXTERNAL=y
-    BR2_TOOLCHAIN_EXTERNAL_CUSTOM=y
-    BR2_TOOLCHAIN_EXTERNAL_DOWNLOAD=y
-    BR2_TOOLCHAIN_EXTERNAL_URL="http://autobuild.buildroot.org/toolchains/tarballs/br-arm-full-2019.05.1.tar.bz2"
-    BR2_TOOLCHAIN_EXTERNAL_GCC_4_9=y
-    BR2_TOOLCHAIN_EXTERNAL_HEADERS_4_14=y
-    BR2_TOOLCHAIN_EXTERNAL_LOCALE=y
-    # BR2_TOOLCHAIN_EXTERNAL_HAS_THREADS_DEBUG is not set
-    BR2_TOOLCHAIN_EXTERNAL_CXX=y
-    """
-
 
 class TestAtop(infra.basetest.BRTest):
-    config = BASIC_TOOLCHAIN_CONFIG_HEADERS_AT_LEAST_3_14 + \
+    config = infra.basetest.BASIC_TOOLCHAIN_CONFIG + \
         """
         BR2_PACKAGE_ATOP=y
         BR2_TARGET_ROOTFS_CPIO=y
