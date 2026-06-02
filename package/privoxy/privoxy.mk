283a7a15b287bac53533eea140f9784648942df0
Bernd Kuhls <bernd@kuhls.net>
2026-06-02 23:49:08 +0200

    package/privoxy: security bump version to 4.2.0
    Fixes the following security issues:
    
    - Parse the chunk-size with a dedicated function and reject "unreasonably"
      large values to prevent silent truncation by sscanf(), integer overflows
      and misinterpretation of the content later on. Heap buffer overflows on
      platforms with 32-bit pointers were alleged as well.
      Commit 5b3bb22b77. OVE-20260515-0002. Reported by @TristanInSec.
    
    - ssl_send_certificate_error(): Store the generated message on the heap
      instead of the stack to prevent an alleged segmentation fault if there
      are enough certificates in the chain to exceed the stack size.
      While at it, replace another variable-length array that was probably
      unproblematic with a heap-based buffer as well.
      Commit 4963aa4f08. OVE-20260515-0001. Reported by @TristanInSec.
    
    https://www.privoxy.org/gitweb/?p=privoxy.git;a=blob;f=ChangeLog;h=d62aa6bf0b2cb26100e4ba8e528638259502711d;hb=0c5225b6844dd1a838b80d09d715a6c4ccbda2c6
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    [Peter: add vulnerability info]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/privoxy/privoxy.mk
+++ package/privoxy/privoxy.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PRIVOXY_VERSION = 4.1.0
+PRIVOXY_VERSION = 4.2.0
 PRIVOXY_SITE = https://downloads.sourceforge.net/project/ijbswa/Sources/$(PRIVOXY_VERSION)%20%28stable%29
 PRIVOXY_SOURCE = privoxy-$(PRIVOXY_VERSION)-stable-src.tar.gz
 # configure not shipped
