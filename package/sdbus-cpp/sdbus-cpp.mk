1deac1ac39e79eb8f37dc276710c70d135cf4d31
Michael Nosthoff <buildroot@heine.tech>
2026-06-08 09:42:56 +0200

    package/sdbus-cpp: bump to version 2.3.1
    fixes an ABI break in 2.3.0
    
    changelog:
    https://github.com/Kistler-Group/sdbus-cpp/releases/tag/v2.3.1
    
    Signed-off-by: Michael Nosthoff <buildroot@heine.tech>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/sdbus-cpp/sdbus-cpp.mk
+++ package/sdbus-cpp/sdbus-cpp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SDBUS_CPP_VERSION = 2.3.0
+SDBUS_CPP_VERSION = 2.3.1
 SDBUS_CPP_SITE = $(call github,Kistler-Group,sdbus-cpp,v$(SDBUS_CPP_VERSION))
 SDBUS_CPP_INSTALL_STAGING = YES
 SDBUS_CPP_DEPENDENCIES = host-pkgconf systemd
