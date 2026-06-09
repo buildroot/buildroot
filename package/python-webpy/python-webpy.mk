dab0de6277c484998842f4617a9730ce2ae3b050
Yegor Yefremov <yegorslists@googlemail.com>
2026-06-09 12:05:15 +0200

    package/python-webpy: bump to version 0.76
    Change log since v0.60:
    https://github.com/webpy/webpy/releases/tag/0.61
    https://github.com/webpy/webpy/releases/tag/0.62
    https://github.com/webpy/webpy/releases/tag/v0.76
    
    Remove the upstreamed patch and add a license file.
    
    This commit adds the new python-multipart runtime dependency, needed
    since upstream commit:
    https://github.com/webpy/webpy/commit/f7540bef650bce7227af796728136d41e44cdd0e
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    [Julien:
     - move the "v" from _VERSION to _SITE
     - select new BR2_PACKAGE_PYTHON_MULTIPART runtime dependency
     - add links to all release notes in commit log
    ]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-webpy/python-webpy.mk
+++ package/python-webpy/python-webpy.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-PYTHON_WEBPY_VERSION = 0.70
-PYTHON_WEBPY_SITE = $(call github,webpy,webpy,webpy-$(PYTHON_WEBPY_VERSION))
+PYTHON_WEBPY_VERSION = 0.76
+PYTHON_WEBPY_SITE = $(call github,webpy,webpy,v$(PYTHON_WEBPY_VERSION))
 PYTHON_WEBPY_SETUP_TYPE = setuptools
 PYTHON_WEBPY_LICENSE = Public Domain
+PYTHON_WEBPY_LICENSE_FILES = LICENSE.txt
 
 $(eval $(python-package))
