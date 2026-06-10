dd7ec89b7eaf1b9e1893ea8fb5d2c9600b88ef02
Francois Perrad <francois.perrad@gadz.org>
2026-06-10 08:46:42 +0200

    package/lua-argparse: bump to version 0.7.2
    for changelog, see:
    https://github.com/luarocks/argparse/blob/0.7.2/CHANGELOG.md
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/lua-argparse/lua-argparse.mk
+++ package/lua-argparse/lua-argparse.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LUA_ARGPARSE_VERSION = 0.7.1-1
+LUA_ARGPARSE_VERSION = 0.7.2-1
 LUA_ARGPARSE_NAME_UPSTREAM = argparse
 LUA_ARGPARSE_LICENSE = MIT
 LUA_ARGPARSE_LICENSE_FILES = $(LUA_ARGPARSE_SUBDIR)/LICENSE
