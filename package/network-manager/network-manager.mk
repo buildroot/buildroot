buildroot/buildroot

commit 24827ae398db3b721eaff685bc3ee1f4bd5b7214

package/network-manager: bump to 1.56.0
Disable building of man pages, which is now a separate option from
building html docs[1].

Add conditional support for parsing the NVMe Boot Firmware Table NBFT)
depending on whether libnvme is available.

Changes [2]:

=============================================
NetworkManager-1.56
Overview of changes since NetworkManager-1.54
=============================================
* Unify the versioning to use everywhere the scheme with the -rcX or -dev
  suffixes when appropriate. This affects, for example, the URL and filename
  of the release tarball and the version reported by nmcli and the daemon.
  As an exception, the C API will continue to use the 90+ scheme for RC versions.
* nmcli now supports viewing and managing WireGuard peers.
* Support reapplying the "sriov.vfs" property as long as
  "sriov.total-vfs" is not changed.
* Support reapplying "bond-port.vlans".
* Accept hostnames longer than 64 characters from DNS lookup.
* Make that global-dns configuration overwrites DNS searches and
  options from connections, instead of merging all together.
* Add support for a new rd.net.dhcp.client-id option in
  nm-initrd-generator.
* Add gsm device-uid setting to restrict the devices the connection applies to.
* Support configuring the HSR protocol version via the
  "hsr.protocol-version" property.
* Fix a bug that makes broadband connections auto-connect getting
  blocked if the connection tries to reconnect when modem status is
  "disconnecting" / "disconnected".
* Treat modem connection not having an operator code available
  as a recoverable error.
* Add support for configuring systemd-resolved's DNSSEC option
  per-connection via the "connection.dnssec" connection property.
* Support configuring the HSR interlink port via the
  "hsr.interlink" property.
* Fix some connection properties not being applied to vpn connections
  (connection.mdns, connection.llmnr, connection.dns-over-tls,

package/network-manager/network-manager.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-NETWORK_MANAGER_VERSION_MAJOR = 1.52
-NETWORK_MANAGER_VERSION = $(NETWORK_MANAGER_VERSION_MAJOR).2
+NETWORK_MANAGER_VERSION_MAJOR = 1.56
+NETWORK_MANAGER_VERSION = $(NETWORK_MANAGER_VERSION_MAJOR).0
 NETWORK_MANAGER_SOURCE = NetworkManager-$(NETWORK_MANAGER_VERSION).tar.xz
 NETWORK_MANAGER_SITE = https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/releases/$(NETWORK_MANAGER_VERSION)/downloads
 NETWORK_MANAGER_INSTALL_STAGING = YES
@@ -26,6 +26,7 @@ NETWORK_MANAGER_DEPENDENCIES = \
 
 NETWORK_MANAGER_CONF_OPTS = \
 	-Ddocs=false \
+	-Dman=false \
 	-Dtests=no \
 	-Dqt=false \
 	-Diptables=/usr/sbin/iptables \
@@ -164,6 +165,13 @@ NETWORK_MANAGER_DEPENDENCIES += polkit
 NETWORK_MANAGER_CONF_OPTS += -Dpolkit=true
 else
 NETWORK_MANAGER_CONF_OPTS += -Dpolkit=false
+endif
+
+ifeq ($(BR2_PACKAGE_LIBNVME),y)
+NETWORK_MANAGER_DEPENDENCIES += libnvme
+NETWORK_MANAGER_CONF_OPTS += -Dnbft=true
+else
+NETWORK_MANAGER_CONF_OPTS += -Dnbft=false
 endif
 
 ifeq ($(BR2_PACKAGE_NETWORK_MANAGER_CLI),y)

package/network-manager/network-manager.hash
@@ -1,5 +1,5 @@
-# From https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/releases/1.52.2/downloads/NetworkManager-1.52.2.tar.xz.sha256sum
-sha256  3658640e788bbcb6a000c29a95f0dccc4dfebba41c6e5e9a90ad1e6daacb512c  NetworkManager-1.52.2.tar.xz
+# From https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/releases/1.56.0/downloads/NetworkManager-1.56.0.tar.xz.sha256sum
+sha256  59a32d385cc1e7ae26e43798c6f12d07ff6198abd041ec0620b3a08cfc021ccc  NetworkManager-1.56.0.tar.xz
 # Locally computed
 sha256  8177f97513213526df2cf6184d8ff986c675afb514d4e68a404010521b880643  COPYING
 sha256  dc626520dcd53a22f727af3ee42c770e56c97a64fe3adb063799d8ab032fe551  COPYING.LGPL

commit 32bdc4710a483c3ea7b0d5a805b2a857f9a11872

package/network-manager: drop initrd-related services when unneeded
Since NetworkManager 1.54, packaged since commit 24827ae398db
(package/network-manager: bump to 1.56.0), new services related to
running in an initrd are now installed, which prevents proper startup,
with systemd reporting that two units declare the same BusName.

When systemd is configured without initrd support, drop the
NetworkManager initrd-related services.

Fixes: 24827ae398db

Signed-off-by: Yann E. MORIN <yann.morin@orange.com>
Cc: Fiona Klute <fiona.klute@gmx.de>
Cc: Marcus Hoffmann <buildroot@bubu1.eu>
Acked-by: TIAN Yuanhao <tianyuanhao3@163.com>
Signed-off-by: Fiona Klute <fiona.klute@gmx.de>

package/network-manager/network-manager.mk
@@ -151,6 +151,12 @@ NETWORK_MANAGER_CONF_OPTS += \
 	-Dconfig_logging_backend_default=journal \
 	-Dsession_tracking=systemd \
 	-Dsuspend_resume=systemd
+ifneq ($(BR2_PACKAGE_SYSTEMD_INITRD),y)
+define NETWORK_MANAGER_CLEAN_INITRD
+	rm -f $(TARGET_DIR)/usr/lib/systemd/system/NetworkManager-*initrd.service
+endef
+NETWORK_MANAGER_POST_INSTALL_TARGET_HOOKS += NETWORK_MANAGER_CLEAN_INITRD
+endif
 else
 NETWORK_MANAGER_CONF_OPTS += \
 	-Dsystemd_journal=false \