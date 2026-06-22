48f8ab703c44c2739d112bd9046749b9e2f2d732
Manuel Diener <manuel.diener@oss.othermo.de>
2026-06-22 11:45:16 +0200

    package/python-jc: bump to version 1.25.7
    See the release notes here: https://github.com/kellyjonbrazil/jc/blob/master/CHANGELOG
    
    Signed-off-by: Manuel Diener <manuel.diener@oss.othermo.de>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-jc/python-jc.mk
+++ package/python-jc/python-jc.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_JC_VERSION = 1.25.6
+PYTHON_JC_VERSION = 1.25.7
 PYTHON_JC_SOURCE = jc-$(PYTHON_JC_VERSION).tar.gz
-PYTHON_JC_SITE = https://files.pythonhosted.org/packages/7c/fa/f174bfb1ecfd3bdc6259b2de32052f09cccba11c9a1b733aedf618fadd05
+PYTHON_JC_SITE = https://files.pythonhosted.org/packages/a9/da/7827d3389d87fa645556d595a2a437576de83f7a7d4a5fceda9c32208c0b
 PYTHON_JC_SETUP_TYPE = setuptools
 PYTHON_JC_LICENSE = MIT, BSD-3-Clause (bundled pbPlist)
 PYTHON_JC_LICENSE_FILES = LICENSE.md
