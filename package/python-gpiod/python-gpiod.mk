500961bebacf0b631de223ceb412abcfe5e446dc
Manuel Diener <manuel.diener@oss.othermo.de>
2026-06-22 10:17:24 +0200

    package/python-gpiod: bump to version 2.5.0
    See the release notes here: https://github.com/brgl/libgpiod/blob/master/bindings/python/CHANGELOG.md
    
    Signed-off-by: Manuel Diener <manuel.diener@oss.othermo.de>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-gpiod/python-gpiod.mk
+++ package/python-gpiod/python-gpiod.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_GPIOD_VERSION = 2.4.2
+PYTHON_GPIOD_VERSION = 2.5.0
 PYTHON_GPIOD_SOURCE = gpiod-$(PYTHON_GPIOD_VERSION).tar.gz
-PYTHON_GPIOD_SITE = https://files.pythonhosted.org/packages/13/ca/b3bd043091b4462d6c5561f86581f553df102d8990c37938ddbff2823016
+PYTHON_GPIOD_SITE = https://files.pythonhosted.org/packages/fc/c2/c7bc26965855f39ae3e1b09b404a1fdc3b172dac371012c316f5b9b6a314
 PYTHON_GPIOD_SETUP_TYPE = setuptools
 PYTHON_GPIOD_LICENSE = LGPL-2.1+
 PYTHON_GPIOD_LICENSE_FILES = LICENSE
