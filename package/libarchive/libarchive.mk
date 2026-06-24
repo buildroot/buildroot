5df5aff9caa43c896551b7c54e08114a32a3f669
Bernd Kuhls <bernd@kuhls.net>
2026-06-24 22:43:23 +0200

    package/libarchive: security bump version to 3.8.8
    https://github.com/libarchive/libarchive/releases/tag/v3.8.8
    "Libarchive 3.8.8 is a security, bugfix and minor feature release."
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Fiona Klute <fiona.klute@gmx.de>
    (cherry picked from commit c384895981eb7d98a8ba5067566601772db0258b)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/libarchive/libarchive.mk
+++ package/libarchive/libarchive.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBARCHIVE_VERSION = 3.8.7
+LIBARCHIVE_VERSION = 3.8.8
 LIBARCHIVE_SOURCE = libarchive-$(LIBARCHIVE_VERSION).tar.xz
 LIBARCHIVE_SITE = https://www.libarchive.de/downloads
 LIBARCHIVE_INSTALL_STAGING = YES
