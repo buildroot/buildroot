03b12ace154cfd58aa1be0670ad527de718352d0
Thomas Perale <thomas.perale@mind.be>
2026-06-26 22:34:15 +0200

    package/imagemagick: security bump to v7.1.2-26
    See the changelog:
    
    - https://github.com/ImageMagick/Website/blob/main/ChangeLog.md#712-24---2026-05-26
    - https://github.com/ImageMagick/Website/blob/main/ChangeLog.md#712-25---2026-06-04
    - https://github.com/ImageMagick/ImageMagick/compare/7.1.2-25...7.1.2-26
    
    The bump to the v7.1.2-25 fixes the following vulnerabilities:
    
    - CVE-2026-53465
    - CVE-2026-53464
    - CVE-2026-53463
    - CVE-2026-53462
    - CVE-2026-53461
    - CVE-2026-53460
    
    The bump to the v7.1.2-24 fixes the following vulnerabilities:
    
    - CVE-2026-49219
    - CVE-2026-49218
    - CVE-2026-48994
    - CVE-2026-48734
    - CVE-2026-48733
    - CVE-2026-48724
    
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 6bb532a100139d3189ed180945e165e3a941efb8)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/imagemagick/imagemagick.mk
+++ package/imagemagick/imagemagick.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-IMAGEMAGICK_VERSION = 7.1.2-23
+IMAGEMAGICK_VERSION = 7.1.2-26
 IMAGEMAGICK_SITE = $(call github,ImageMagick,ImageMagick,$(IMAGEMAGICK_VERSION))
 IMAGEMAGICK_LICENSE = Apache-2.0
 IMAGEMAGICK_LICENSE_FILES = LICENSE
