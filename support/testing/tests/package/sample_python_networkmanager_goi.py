2723abb9f43e99257371a4f89f6b72a79f256921
Fiona Klute <fiona.klute@gmx.de>
2026-06-27 13:34:09 +0200

    support/testing: TestPythonPy3NetworkmanagerGoi: check we actually get data
    The sample script would've passed even if the connections list was
    empty. Check that there is at least one connection, and that the
    sample script can read loopback device information.
    
    Signed-off-by: Fiona Klute <fiona.klute@gmx.de>
    Signed-off-by: Romain Naour <romain.naour@smile.fr>
    (cherry picked from commit f38f5bb5939fc3fe896ace714182e2ed8d8c0a2f)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- support/testing/tests/package/sample_python_networkmanager_goi.py
+++ support/testing/tests/package/sample_python_networkmanager_goi.py
@@ -24,3 +24,11 @@ for c in connections:
     print("{}:".format(c.get_id()))
     c.for_each_setting_value(print_values, None)
     print("\n")
+print("found {} defined connections".format(len(connections)))
+assert len(connections) > 0
+
+# check that we can find the loopback device ...
+loopback = client.get_device_by_iface("lo")
+# ... and read its type and status as expected
+assert loopback.get_device_type().name == "LOOPBACK"
+assert loopback.get_state().name == "ACTIVATED"
