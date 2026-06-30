4dec614d3172cad6363e39d84480cfff2970d932
Thomas Perale <thomas.perale@mind.be>
2026-06-30 22:15:06 +0200

    package/jq: patch various CVEs
    Fixes the following vulnerabilities by importing upstream patches:
    
    - CVE-2026-39979: https://github.com/jqlang/jq/commit/2f09060afab23fe9390cce7cb860b10416e1bf5f
    
    - CVE-2026-33948: https://github.com/jqlang/jq/commit/6374ae0bcdfe33a18eb0ae6db28493b1f34a0a5b
    
    - CVE-2026-33947: https://github.com/jqlang/jq/commit/fb59f1491058d58bdc3e8dd28f1773d1ac690a1f
    
    - CVE-2026-32316: https://github.com/jqlang/jq/commit/e47e56d226519635768e6aab2f38f0ab037c09e5
    
    - CVE-2026-40164: https://github.com/jqlang/jq/commit/0c7d133c3c7e37c00b6d46b658a02244fdd3c784
    
    - CVE-2026-40612: https://github.com/jqlang/jq/commit/d1a12569d91641135976a8536776a4a329c02cc2
    
    - CVE-2026-41256: https://github.com/jqlang/jq/commit/5a015deae35d19e3ebbc65db6c157a80e76df738
    
    - CVE-2026-41257: https://github.com/jqlang/jq/commit/01b3cded76daacbfddb7f8763700b0803bcb5c6f
    
    - CVE-2026-43894: https://github.com/jqlang/jq/commit/9761ceb7d6cc48c16b25f0ab1baaef0e701927e4
    
    - CVE-2026-43896: https://github.com/jqlang/jq/commit/532ccea6080ed6758f39fe9f6208a44b665023d2
    
    - CVE-2026-44777: https://github.com/jqlang/jq/commit/f58787c41835d9b17795730cb04925fdba25c71c
    
    - CVE-2026-49839: https://github.com/jqlang/jq/commit/e987df0d463d85fd70825e042a082427e8275b86
    
    - CVE-2026-54679: https://github.com/jqlang/jq/commit/46d1da30944ce93dd671ac72b6513fc0eb747837
    
    Tests were stripped out of the patches.
    
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit e8191c3e17dbcb3388b365c52edd3e928f0ed465)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/jq/jq.mk
+++ package/jq/jq.mk
@@ -20,6 +20,45 @@ JQ_IGNORE_CVES += CVE-2024-53427
 # 0003-CVE-2025-48060.patch
 JQ_IGNORE_CVES += CVE-2025-48060
 
+# 0004-fix-out-of-bounds-read-in-jv-parse-sized.patch
+JQ_IGNORE_CVES += CVE-2026-39979
+
+# 0005-fix-NUL-truncation-in-the-JSON-parser.patch
+JQ_IGNORE_CVES += CVE-2026-33948
+
+# 0006-limit-path-depth-to-prevent-stack-overflow.patch
+JQ_IGNORE_CVES += CVE-2026-33947
+
+# 0007-fix-heap-buffer-overflow-in-jvp-string-append-and-jvp-string-copy-replace-bad.patch
+JQ_IGNORE_CVES += CVE-2026-32316
+
+# 0008-randomize-hash-seed-to-mitigate-hash-collision-DoS-attacks.patch
+JQ_IGNORE_CVES += CVE-2026-40164
+
+# 0009-limit-the-containment-check-depth.patch
+JQ_IGNORE_CVES += CVE-2026-40612
+
+# 0010-fix-NUL-truncation-in-program-files-loaded-with-f.patch
+JQ_IGNORE_CVES += CVE-2026-41256
+
+# 0011-fix-signed-int-overflow-in-stack-reallocate.patch
+JQ_IGNORE_CVES += CVE-2026-41257
+
+# 0012-reject-numeric-literals-longer-than-DEC-MAX-DIGITS.patch
+JQ_IGNORE_CVES += CVE-2026-43894
+
+# 0013-limit-recursive-object-merge-depth-to-prevent-stack-overflow.patch
+JQ_IGNORE_CVES += CVE-2026-43896
+
+# 0014-detect-circular-module-imports-to-prevent-stack-overflow.patch
+JQ_IGNORE_CVES += CVE-2026-44777
+
+# 0015-fix-heap-buffer-overflow-in-raw-file-loading.patch
+JQ_IGNORE_CVES += CVE-2026-49839
+
+# 0016-tighten-string-length-bounds-and-propagate-invalid-jv-in-implode.patch
+JQ_IGNORE_CVES += CVE-2026-54679
+
 # uses c99 specific features
 JQ_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -std=c99"
 HOST_JQ_CONF_ENV += CFLAGS="$(HOST_CFLAGS) -std=c99"
