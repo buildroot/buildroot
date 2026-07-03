980d01157074d4403221534a54d17f5bf9563697
Bernd Kuhls <bernd@kuhls.net>
2026-07-03 21:03:14 +0200

    package/sqlite: bump version to 3.53.3
    https://sqlite.org/releaselog/3_53_3.html
    https://sqlite.org/src/timeline?from=version-3.53.2&to=version-3.53.3&to2=branch-3.53&y=ci
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/sqlite/sqlite.mk
+++ package/sqlite/sqlite.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-SQLITE_VERSION = 3.53.2
-SQLITE_TAR_VERSION = 3530200
+SQLITE_VERSION = 3.53.3
+SQLITE_TAR_VERSION = 3530300
 SQLITE_SOURCE = sqlite-autoconf-$(SQLITE_TAR_VERSION).tar.gz
 SQLITE_SITE = https://www.sqlite.org/2026
 SQLITE_LICENSE = blessing
