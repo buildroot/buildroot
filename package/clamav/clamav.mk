31cd41c84b7d76800c85df4433dc280e713890e7
Bernd Kuhls <bernd@kuhls.net>
2026-07-03 21:50:50 +0200

    package/clamav: security bump version to 1.5.3
    https://blog.clamav.net/2026/07/clamav-153-and-145-security-patch.html
    
    Fixes CVE-2026-20213, CVE-2026-20214, CVE-2026-20215, CVE-2026-20216,
    CVE-2026-20217, CVE-2026-20243 & CVE-2026-20244.
    
    "Upgraded the Rust tar dependency to resolve the RUSTSEC-2026-0067 and
     RUSTSEC-2026-0068 advisories, and upgraded the Rust openssl dependency
     to resolve CVE-2026-41676."
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/clamav/clamav.mk
+++ package/clamav/clamav.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CLAMAV_VERSION = 1.5.2
+CLAMAV_VERSION = 1.5.3
 CLAMAV_SITE = https://www.clamav.net/downloads/production
 CLAMAV_LICENSE = GPL-2.0
 CLAMAV_LICENSE_FILES = \
