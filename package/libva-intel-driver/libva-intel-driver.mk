cf95721d0049e994068ab971f4bd62a465dfe041
Bernd Kuhls <bernd@kuhls.net>
2026-07-05 20:20:16 +0200

    package/libva-intel-driver: bump version to 2.4.5
    https://github.com/irql-notlessorequal/intel-vaapi-driver/blob/2.4.5/NEWS
    
    This bump follows
    https://github.com/LibreELEC/LibreELEC.tv/commit/39d2ff612fce9bb57c43ac767df15052a896e9ed
    
    "Intel archived intel/intel-vaapi-driver at 2.4.1 and advises forking.
     irql-notlessorequal/intel-vaapi-driver is the active continuation (119
     commits ahead, tagged releases, Chromium fixes), so track it at 2.4.5.
     [...]
     The fork renamed the wayland meson option, so switch with_wayland to
     with_wayland_drm in all DISPLAYSERVER branches. Also drop the dead
     01.org PKG_SITE."
    
    Updated license file and hash due to upstream commit:
    https://github.com/irql-notlessorequal/intel-vaapi-driver/commit/7343767d87926d519f528db88ba1edfb153d8289
    
    Also switch build system to meson.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libva-intel-driver/libva-intel-driver.mk
+++ package/libva-intel-driver/libva-intel-driver.mk
@@ -4,26 +4,24 @@
 #
 ################################################################################
 
-LIBVA_INTEL_DRIVER_VERSION = 2.4.1
-LIBVA_INTEL_DRIVER_SOURCE = intel-vaapi-driver-$(LIBVA_INTEL_DRIVER_VERSION).tar.bz2
-LIBVA_INTEL_DRIVER_SITE = \
-	https://github.com/intel/intel-vaapi-driver/releases/download/$(LIBVA_INTEL_DRIVER_VERSION)
+LIBVA_INTEL_DRIVER_VERSION = 2.4.5
+LIBVA_INTEL_DRIVER_SITE = $(call github,irql-notlessorequal,intel-vaapi-driver,$(LIBVA_INTEL_DRIVER_VERSION))
 LIBVA_INTEL_DRIVER_LICENSE = MIT
-LIBVA_INTEL_DRIVER_LICENSE_FILES = COPYING
+LIBVA_INTEL_DRIVER_LICENSE_FILES = LICENSE
 LIBVA_INTEL_DRIVER_DEPENDENCIES = host-pkgconf libdrm libva
 
 ifeq ($(BR2_PACKAGE_XORG7),y)
 LIBVA_INTEL_DRIVER_DEPENDENCIES += xlib_libX11 xlib_libXext xlib_libXfixes
-LIBVA_INTEL_DRIVER_CONF_OPTS += --enable-x11
+LIBVA_INTEL_DRIVER_CONF_OPTS += -Dwith_x11=yes
 else
-LIBVA_INTEL_DRIVER_CONF_OPTS += --disable-x11
+LIBVA_INTEL_DRIVER_CONF_OPTS += -Dwith_x11=no
 endif
 
 ifeq ($(BR2_PACKAGE_WAYLAND),y)
 LIBVA_INTEL_DRIVER_DEPENDENCIES += wayland
-LIBVA_INTEL_DRIVER_CONF_OPTS += --enable-wayland
+LIBVA_INTEL_DRIVER_CONF_OPTS += -Dwith_wayland_drm=yes
 else
-LIBVA_INTEL_DRIVER_CONF_OPTS += --disable-wayland
+LIBVA_INTEL_DRIVER_CONF_OPTS += -Dwith_wayland_drm=no
 endif
 
-$(eval $(autotools-package))
+$(eval $(meson-package))
