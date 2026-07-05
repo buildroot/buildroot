9f7c484b4ab0190765dc81fb79083ae8e2be211b
Waldemar Brodkorb <wbx@openadk.org>
2026-07-05 19:01:26 +0200

    package/stunnel: update download site
    Fix the download site, which seems to be changed after
    a new release. Use the more permanent /archive.
    
    Fixes:
     - https://autobuild.buildroot.net/results/a1a/a1a88bea6fdf5ae28d813f0a12d08e504b2fad06/
     - https://autobuild.buildroot.net/results/b3d/b3d0588fbc02ede6d5f82fb5d2fefcf610a69299/
    
    Signed-off-by: Waldemar Brodkorb <wbx@openadk.org>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/stunnel/stunnel.mk
+++ package/stunnel/stunnel.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 STUNNEL_VERSION = 5.78
-STUNNEL_SITE = https://www.stunnel.org/downloads
+STUNNEL_SITE = https://www.stunnel.org/archive/5.x
 STUNNEL_DEPENDENCIES = host-pkgconf openssl
 STUNNEL_CONF_OPTS = --with-ssl=$(STAGING_DIR)/usr --with-threads=fork \
 	--disable-libwrap
