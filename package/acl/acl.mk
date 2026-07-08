d1111db6fc1b96579d799acba3edc177de9dc7a9
Bernd Kuhls <bernd@kuhls.net>
2026-07-08 21:39:40 +0200

    package/acl: security bump version to 2.4.0
    https://lists.nongnu.org/archive/html/acl-devel/2026-06/msg00000.html
    
    Fixes CVE-2026-54369, CVE-2026-54370 and CVE-2026-54371.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/acl/acl.mk
+++ package/acl/acl.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ACL_VERSION = 2.3.2
+ACL_VERSION = 2.4.0
 ACL_SOURCE = acl-$(ACL_VERSION).tar.xz
 ACL_SITE = https://download.savannah.nongnu.org/releases/acl
 ACL_LICENSE = GPL-2.0+ (programs), LGPL-2.1+ (libraries)
