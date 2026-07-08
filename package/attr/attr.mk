c804ebff5502eac6e7d84129bfb2becc400898c6
Bernd Kuhls <bernd@kuhls.net>
2026-07-08 21:39:39 +0200

    package/attr: security bump version to 2.6.0
    https://lists.nongnu.org/archive/html/acl-devel/2026-06/msg00000.html
    
    Fixes CVE-2026-54369, CVE-2026-54370 and CVE-2026-54371.
    
    Removed patches which are included in this release.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/attr/attr.mk
+++ package/attr/attr.mk
@@ -4,16 +4,12 @@
 #
 ################################################################################
 
-ATTR_VERSION = 2.5.2
+ATTR_VERSION = 2.6.0
 ATTR_SOURCE = attr-$(ATTR_VERSION).tar.xz
 ATTR_SITE = http://download.savannah.gnu.org/releases/attr
 ATTR_LICENSE = GPL-2.0+ (programs), LGPL-2.1+ (libraries)
 ATTR_LICENSE_FILES = doc/COPYING doc/COPYING.LGPL
 ATTR_CPE_ID_VALID = YES
-
-# Flag added for patch dealing with symver in configure.ac
-ATTR_AUTORECONF = YES
-
 ATTR_INSTALL_STAGING = YES
 
 ATTR_CONF_OPTS = --disable-nls
