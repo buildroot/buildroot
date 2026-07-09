92abb11ee4f574981eb194d283b97afad5dec004
Bernd Kuhls <bernd@kuhls.net>
2026-07-09 21:17:37 +0200

    package/odhcp6c: bump version to git 10a52220ae
    https://github.com/openwrt/odhcp6c/compare/f19dd37fb467c9cf10cad57aefa0d048312d7dfd...10a52220aec9d45803518d8cc4d63e552484ed61
    
    Removed patches which are included in this release.
    
    Please note that upstream committed
    https://git.openwrt.org/project/odhcp6c/commit/?id=10a52220aec9d45803518d8cc4d63e552484ed61
    instead of patch 0001.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/odhcp6c/odhcp6c.mk
+++ package/odhcp6c/odhcp6c.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ODHCP6C_VERSION = f19dd37fb467c9cf10cad57aefa0d048312d7dfd
+ODHCP6C_VERSION = 10a52220aec9d45803518d8cc4d63e552484ed61
 ODHCP6C_SITE = https://git.openwrt.org/project/odhcp6c.git
 ODHCP6C_SITE_METHOD = git
 ODHCP6C_LICENSE = GPL-2.0
