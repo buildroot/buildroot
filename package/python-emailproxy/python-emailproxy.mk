08c39b39852de9612e0ae9b7d4c5a8cfc6aa461a
Bernd Kuhls <bernd@kuhls.net>
2026-07-10 21:02:20 +0200

    package/python-emailproxy: bump version to 2026.7.3
    https://github.com/simonrob/email-oauth2-proxy/releases/tag/2026-07-03
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-emailproxy/python-emailproxy.mk
+++ package/python-emailproxy/python-emailproxy.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_EMAILPROXY_VERSION = 2025.10.4
+PYTHON_EMAILPROXY_VERSION = 2026.7.3
 PYTHON_EMAILPROXY_SOURCE = emailproxy-$(PYTHON_EMAILPROXY_VERSION).tar.gz
-PYTHON_EMAILPROXY_SITE = https://files.pythonhosted.org/packages/4f/0d/045028f891c4a83ea7d616a81f0b9af8d290316eb0c958d572b6445232f9
+PYTHON_EMAILPROXY_SITE = https://files.pythonhosted.org/packages/6e/bc/51b7adf9e9f176f838cf4649ed27584b88ca5a0c8650ca75e9b432c563e0
 PYTHON_EMAILPROXY_SETUP_TYPE = setuptools
 PYTHON_EMAILPROXY_LICENSE = Apache-2.0
 PYTHON_EMAILPROXY_LICENSE_FILES = LICENSE
