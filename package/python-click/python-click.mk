c90e5e3383a4593996e1b688a0b540c3d1956898
Manuel Diener <manuel.diener@oss.othermo.de>
2026-07-13 14:59:19 +0200

    package/python-click: bump to version 8.4.2
    See the release notes here (also including versions 8.4.0 and 8.4.1):
    https://click.palletsprojects.com/en/stable/changes/
    
    Signed-off-by: Manuel Diener <manuel.diener@oss.othermo.de>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-click/python-click.mk
+++ package/python-click/python-click.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_CLICK_VERSION = 8.3.3
+PYTHON_CLICK_VERSION = 8.4.2
 PYTHON_CLICK_SOURCE = click-$(PYTHON_CLICK_VERSION).tar.gz
-PYTHON_CLICK_SITE = https://files.pythonhosted.org/packages/bb/63/f9e1ea081ce35720d8b92acde70daaedace594dc93b693c869e0d5910718
+PYTHON_CLICK_SITE = https://files.pythonhosted.org/packages/76/d4/81420972a676e8ffea40450d8c8c92943e7218a78fe9b64359836cc9876b
 PYTHON_CLICK_SETUP_TYPE = flit
 PYTHON_CLICK_LICENSE = BSD-3-Clause
 PYTHON_CLICK_LICENSE_FILES = LICENSE.txt
