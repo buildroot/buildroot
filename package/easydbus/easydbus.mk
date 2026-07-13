d90bbf81f86acef07037ad59ed78e2809222e1d3
Marcin Niestroj <m.niestroj@grinn-global.com>
2026-07-13 20:25:53 +0200

    package/easydbus: bump to version 0.2.1
    0.2.1 builds against Lua 5.4 (previously excluded), so drop the
    !BR2_PACKAGE_LUA_5_4 dependency and the corresponding wording in
    the comment.
    
    Changelog:
    https://github.com/mniestroj/easydbus/blob/master/CHANGELOG.md#021---2026-06-05
    
    Signed-off-by: Marcin Niestroj <m.niestroj@grinn-global.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/easydbus/easydbus.mk
+++ package/easydbus/easydbus.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-EASYDBUS_VERSION = 0.1.0
+EASYDBUS_VERSION = 0.2.1
 EASYDBUS_SITE = $(call github,mniestroj,easydbus,v$(EASYDBUS_VERSION))
 EASYDBUS_DEPENDENCIES = luainterpreter libglib2
 EASYDBUS_LICENSE = MIT
