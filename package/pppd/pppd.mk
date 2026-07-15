ac4e557628d9d9d1c6f1b520e5e7a4581476d923
Bernd Kuhls <bernd@kuhls.net>
2026-07-15 22:35:09 +0200

    package/pppd: bump version to 2.5.3
    https://github.com/ppp-project/ppp/releases/tag/v2.5.3
    
    Removed patch 0001 which is not needed anymore due to fixes committed to
    all relevant kernel versions:
    https://github.com/ppp-project/ppp/pull/504#issuecomment-2337005682
    
    Removed patches 0002 & 0003 which are included in this release.
    
    Added new patch to fix build errors seen with older gcc versions
    detected by the Gitlab pipelines. These build errors are introduced in
    the new dhcpv6relay plugin which was added to pppd 2.5.3, no backport to
    buildroot LTS branches necessary.
    
    Switched back to upstream tarball which now contains the previously
    missing license files added by upstream commit
    https://github.com/ppp-project/ppp/commit/9f612dc02c34509f062ed63b60bcc7e937e25178
    and drop autoreconf as well.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/pppd/pppd.mk
+++ package/pppd/pppd.mk
@@ -4,16 +4,14 @@
 #
 ################################################################################
 
-# The tarball provided at https://download.samba.org/pub/ppp/ does not
-# include the license files yet so we use the github tarball.
-PPPD_VERSION = 2.5.2
-PPPD_SITE = $(call github,ppp-project,ppp,v$(PPPD_VERSION))
+PPPD_VERSION = 2.5.3
+PPPD_SITE = https://download.samba.org/pub/ppp
+PPPD_SOURCE = ppp-$(PPPD_VERSION).tar.gz
 PPPD_LICENSE = LGPL-2.0+, LGPL, BSD-4-Clause, BSD-3-Clause, GPL-2.0+
 PPPD_LICENSE_FILES = LICENSE.BSD LICENSE.GPL-2
 PPPD_CPE_ID_VENDOR = point-to-point_protocol_project
 PPPD_CPE_ID_PRODUCT = point-to-point_protocol
 PPPD_SELINUX_MODULES = ppp
-PPPD_AUTORECONF = YES
 PPPD_INSTALL_STAGING = YES
 PPPD_CONF_OPTS = --enable-multilink
 
