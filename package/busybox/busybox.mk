cb55591ab5ba00b9872f8c5d60152473662e4121
Fiona Klute (Othermo GmbH) <fiona.klute@gmx.de>
2026-07-20 19:04:36 +0200

    package/busybox: use SPDX license identifier for bzip2 license
    "bzip2-1.0.4" is not a valid SPDX license identifier. The license text
    in archival/libarchive/bz/LICENSE in the busybox source matches the
    text associated with the bzip2-1.0.6 identifier [1].
    
    [1] https://spdx.org/licenses/bzip2-1.0.6.html
    
    Signed-off-by: Fiona Klute (Othermo GmbH) <fiona.klute@gmx.de>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit f9687af626abefc7d7d7c81157dbf8997fd31005)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/busybox/busybox.mk
+++ package/busybox/busybox.mk
@@ -7,7 +7,7 @@
 BUSYBOX_VERSION = 1.38.0
 BUSYBOX_SITE = https://www.busybox.net/downloads
 BUSYBOX_SOURCE = busybox-$(BUSYBOX_VERSION).tar.bz2
-BUSYBOX_LICENSE = GPL-2.0, bzip2-1.0.4
+BUSYBOX_LICENSE = GPL-2.0, bzip2-1.0.6
 BUSYBOX_LICENSE_FILES = LICENSE archival/libarchive/bz/LICENSE
 BUSYBOX_CPE_ID_VENDOR = busybox
 
