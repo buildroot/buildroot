4ea73bd6763daaff9c968fd8378e5a4e9d9b7cde
Fiona Klute (Othermo GmbH) <fiona.klute@gmx.de>
2026-07-21 18:06:07 +0200

    package/usbutils: update license information
    According to the upstream changelog LGPL-2.1+, CC0-1.0, and MIT
    licenses were added with v018. Grouping is derived from individual
    SPDX-License-Identifier headers in the source files.
    
    [1] https://git.kernel.org/pub/scm/linux/kernel/git/gregkh/usbutils.git/tree/NEWS?id=bd86478bb8d09abd0aba8ead781bc8f4609521e6#n159
    
    Signed-off-by: Fiona Klute (Othermo GmbH) <fiona.klute@gmx.de>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/usbutils/usbutils.mk
+++ package/usbutils/usbutils.mk
@@ -8,8 +8,13 @@ USBUTILS_VERSION = 019
 USBUTILS_SOURCE = usbutils-$(USBUTILS_VERSION).tar.xz
 USBUTILS_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/usb/usbutils
 USBUTILS_DEPENDENCIES = host-pkgconf libudev libusb
-USBUTILS_LICENSE = GPL-2.0+ (utils) GPL-2.0 or GPL-3.0 (lsusb.py)
-USBUTILS_LICENSE_FILES = LICENSES/GPL-2.0-only.txt LICENSES/GPL-3.0-only.txt
+USBUTILS_LICENSE = GPL-2.0+, GPL-2.0 (utils), LGPL-2.1+ (config/CI), GPL-2.0 or GPL-3.0 (lsusb.py), CC0-1.0, MIT (ccan)
+USBUTILS_LICENSE_FILES = \
+	LICENSES/CC0-1.0.txt \
+	LICENSES/GPL-2.0-only.txt \
+	LICENSES/GPL-3.0-only.txt \
+	LICENSES/LGPL-2.1-or-later.txt \
+	LICENSES/MIT.txt
 
 ifeq ($(BR2_PACKAGE_LIBICONV),y)
 USBUTILS_DEPENDENCIES += libiconv
