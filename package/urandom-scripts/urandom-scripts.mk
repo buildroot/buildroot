8ae5fbe1732bc311dcf03b03b36cf55564f65b35
Fiona Klute (Othermo GmbH) <fiona.klute@gmx.de>
2026-07-22 17:32:22 +0200

    package/urandom-scripts: add license info
    The package is part of Buildroot and thus under GPL-2.0-or-later
    unless stated otherwise, as described in COPYING.
    
    Signed-off-by: Fiona Klute (Othermo GmbH) <fiona.klute@gmx.de>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 0f80126bfe8549b2e41a50a884af55fe9872be1f)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/urandom-scripts/urandom-scripts.mk
+++ package/urandom-scripts/urandom-scripts.mk
@@ -4,6 +4,8 @@
 #
 ################################################################################
 
+URANDOM_SCRIPTS_LICENSE = GPL-2.0-or-later
+
 define URANDOM_SCRIPTS_INSTALL_INIT_SYSV
 	$(INSTALL) -D -m 0755 $(URANDOM_SCRIPTS_PKGDIR)/S01seedrng \
 		$(TARGET_DIR)/etc/init.d/S01seedrng
