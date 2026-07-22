1d132d04fc0ecac014be003b7e1662c47672328b
Michael Nosthoff <buildroot@heine.tech>
2026-07-22 11:43:30 +0200

    package/libgpiod2: bump to version 2.3.1
    - buildsystem changed from autotools to meson
    - add option to install dbus-server and client
    
    changelog:
    https://git.kernel.org/pub/scm/libs/libgpiod/libgpiod.git/tree/NEWS?h=v2.3.1
    
    Signed-off-by: Michael Nosthoff <buildroot@heine.tech>
    [Julien:
     - propagate libgudev dependencies on dbus option
     - add Config.in comments for dbus option
    ]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libgpiod2/libgpiod2.mk
+++ package/libgpiod2/libgpiod2.mk
@@ -6,7 +6,7 @@
 
 # Be careful when bumping versions.
 # Dependency on kernel header versions may change.
-LIBGPIOD2_VERSION = 2.2.4
+LIBGPIOD2_VERSION = 2.3.1
 LIBGPIOD2_SOURCE = libgpiod-$(LIBGPIOD2_VERSION).tar.xz
 LIBGPIOD2_SITE = https://www.kernel.org/pub/software/libs/libgpiod
 LIBGPIOD2_LICENSE = LGPL-2.1+
@@ -14,27 +14,34 @@ LIBGPIOD2_LICENSE_FILES = COPYING
 LIBGPIOD2_INSTALL_STAGING = YES
 LIBGPIOD2_DEPENDENCIES = host-pkgconf
 LIBGPIOD2_CONF_OPTS = \
-	--disable-bindings-python \
-	--disable-examples \
-	--disable-tests
+	-Dbindings-python=disabled \
+	-Dexamples=disabled \
+	-Dtests=disabled
+
+ifeq ($(BR2_PACKAGE_LIBGPIOD2_DBUS),y)
+LIBGPIOD2_CONF_OPTS += -Ddbus=enabled
+LIBGPIOD2_DEPENDENCIES += libgudev
+else
+LIBGPIOD2_CONF_OPTS += -Ddbus=disabled
+endif
 
 ifeq ($(BR2_PACKAGE_LIBGPIOD2_TOOLS),y)
-LIBGPIOD2_CONF_OPTS += --enable-tools
+LIBGPIOD2_CONF_OPTS += -Dtools=enabled
 else
-LIBGPIOD2_CONF_OPTS += --disable-tools
+LIBGPIOD2_CONF_OPTS += -Dtools=disabled
 endif
 
 ifeq ($(BR2_PACKAGE_LIBEDIT),y)
 LIBGPIOD2_DEPENDENCIES += libedit
-LIBGPIOD2_CONF_OPTS += --enable-gpioset-interactive
+LIBGPIOD2_CONF_OPTS += -Dgpioset-interactive=enabled
 else
-LIBGPIOD2_CONF_OPTS += --disable-gpioset-interactive
+LIBGPIOD2_CONF_OPTS += -Dgpioset-interactive=disabled
 endif
 
 ifeq ($(BR2_INSTALL_LIBSTDCPP),y)
-LIBGPIOD2_CONF_OPTS += --enable-bindings-cxx
+LIBGPIOD2_CONF_OPTS += -Dbindings-cxx=enabled
 else
-LIBGPIOD2_CONF_OPTS += --disable-bindings-cxx
+LIBGPIOD2_CONF_OPTS += -Dbindings-cxx=disabled
 endif
 
-$(eval $(autotools-package))
+$(eval $(meson-package))
