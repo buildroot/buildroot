235e78caca94d8b6b5b6447a3c6158ed693929aa
Francois Perrad <francois.perrad.86@gmail.com>
2026-07-25 11:29:52 +0200

    package/lua-dkjson: bump to version 2.10
    homepage switches to https
    
    readme.txt becomes readme.md
    
    see changes on https://dkolf.de/dkjson-lua/
    
    Signed-off-by: Francois Perrad <francois.perrad.86@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/lua-dkjson/lua-dkjson.mk
+++ package/lua-dkjson/lua-dkjson.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-LUA_DKJSON_VERSION = 2.8-1
+LUA_DKJSON_VERSION = 2.10-1
 LUA_DKJSON_NAME_UPSTREAM = dkjson
 LUA_DKJSON_LICENSE = MIT
-LUA_DKJSON_LICENSE_FILES = $(LUA_DKJSON_SUBDIR)/readme.txt
+LUA_DKJSON_LICENSE_FILES = $(LUA_DKJSON_SUBDIR)/readme.md
 
 $(eval $(luarocks-package))
