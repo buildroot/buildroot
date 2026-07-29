b85f82a11ee1a55d995e8f315b6c8a9f5ef7fd47
Francois Perrad <francois.perrad.86@gmail.com>
2026-07-29 10:06:17 +0200

    package/lua-dkjson: bump to version 2.11
    see changes on https://dkolf.de/dkjson-lua/
    
    Signed-off-by: Francois Perrad <francois.perrad.86@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/lua-dkjson/lua-dkjson.mk
+++ package/lua-dkjson/lua-dkjson.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LUA_DKJSON_VERSION = 2.10-1
+LUA_DKJSON_VERSION = 2.11-1
 LUA_DKJSON_NAME_UPSTREAM = dkjson
 LUA_DKJSON_LICENSE = MIT
 LUA_DKJSON_LICENSE_FILES = $(LUA_DKJSON_SUBDIR)/readme.md
