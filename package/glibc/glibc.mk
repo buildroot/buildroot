45b24c43a0ea7122a222f659b27ef2e0baadb4c7
Fiona Klute (Othermo GmbH) <fiona.klute@gmx.de>
2026-07-30 16:45:40 +0200

    package/glibc: fix license information
    The COPYINGv3 license file was added with upstream commit
    e067e53080386e93dcf8b07e25fb6656f2c8941e, it applies to scripts used
    during build.
    
    LGPL-3.0+ applies to sysdeps/htl/raise.c, which is used for the Hurd
    platform. Buildroot builds only Linux, so the file should not be
    compiled, but it is part of the downloaded (and possibly shared, if
    "make legal-info" is used) archive. The same applies to the manual.
    
    Checked against the copyright file of the Debian "libc6" package (as
    of 2.42-17 and 2.43-2), note that Debian excludes the manual.
    
    Signed-off-by: Fiona Klute (Othermo GmbH) <fiona.klute@gmx.de>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -11,8 +11,14 @@ GLIBC_VERSION = 2.44-23-g11ac3d78fc5e4f7f2846002e099f773ad8ff82fc
 GLIBC_SITE = https://gitlab.com/gnutools/glibc.git
 GLIBC_SITE_METHOD = git
 
-GLIBC_LICENSE = GPL-2.0+ (programs), LGPL-2.1+, BSD-3-Clause, MIT (library)
-GLIBC_LICENSE_FILES = COPYINGv2 COPYING.LESSERv2 LICENSES
+GLIBC_LICENSE = \
+	GPL-2.0+ (programs), \
+	LGPL-2.1+, BSD-2-Clause, BSD-3-Clause, BSL-1.0, FSFAP, ISC, other permissive licenses, public domain (library), \
+	LGPL-3.0+ (sysdeps/htl/raise.c, for Hurd only), \
+	GPL-3.0+ (scripts/move-if-change), \
+	GPL-3.0+ WITH Texinfo-exception (manual/texinfo.tex), \
+	GFDL-1.3-or-later (manual)
+GLIBC_LICENSE_FILES = COPYINGv2 COPYING.LESSERv2 COPYINGv3 LICENSES manual/fdl-1.3.texi
 GLIBC_CPE_ID_VENDOR = gnu
 
 # Extract the base version (e.g. 2.38) from GLIBC_VERSION in order to
