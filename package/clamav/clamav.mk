edcb5aa58a930e855da4f4a8c2657bb01e3f1bef
Bernd Kuhls <bernd@kuhls.net>
2026-08-07 19:53:06 +0200

    package/clamav: security bump version to 1.5.4
    https://blog.clamav.net/2026/08/clamav-154-and-146-security-patch.html
    
    Fixes CVE-2025-8088, CVE-2026-20337, CVE-2026-20338, CVE-2026-20339,
    CVE-2026-20345, CVE-2026-20346, CVE-2026-20347 & CVE-2026-20348.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 6fe61bd9650bd2a3474b2d5c04568200ad518a28)
    Signed-off-by: Titouan Christophe <titouan.christophe@mind.be>

--- package/clamav/clamav.mk
+++ package/clamav/clamav.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CLAMAV_VERSION = 1.5.3
+CLAMAV_VERSION = 1.5.4
 CLAMAV_SITE = https://www.clamav.net/downloads/production
 CLAMAV_LICENSE = GPL-2.0
 CLAMAV_LICENSE_FILES = \
