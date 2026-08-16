ed881dfca758240ee304e5a042ffc3f7c47f31d2
Mattia Narducci <mattianarducci1@gmail.com>
2026-08-16 21:56:09 +0200

    package/ser2net: bump version to 4.6.8
    Changelog: https://sourceforge.net/p/ser2net/news
    
    Updated licenses hashes due to upstream commit:
    https://github.com/cminyard/ser2net/commit/2bc83f09548dec25b0bec48e11a3ee1942fb3c3e
    
    Drop patch 0001 that was a backport of a upstream security fix.
    
    Add a upstream patch to fix build against uClibc.
    
    Signed-off-by: Mattia Narducci <mattianarducci1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/ser2net/ser2net.mk
+++ package/ser2net/ser2net.mk
@@ -4,11 +4,17 @@
 #
 ################################################################################
 
-SER2NET_VERSION = 4.3.8
+SER2NET_VERSION = 4.6.8
 SER2NET_SITE = https://downloads.sourceforge.net/project/ser2net/ser2net
 SER2NET_LICENSE = GPL-2.0+
 SER2NET_LICENSE_FILES = COPYING
 SER2NET_DEPENDENCIES = gensio libyaml
+# We are patching configure.ac
+SER2NET_AUTORECONF = YES
+
+ifeq ($(BR2_PACKAGE_LIBXCRYPT),y)
+SER2NET_DEPENDENCIES += libxcrypt
+endif
 
 ifeq ($(BR2_TOOLCHAIN_HAS_THREADS),y)
 SER2NET_CONF_OPTS += --with-pthreads
