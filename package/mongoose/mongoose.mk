2bf6549d185aa3c106ed3f86a3314558d77d6745
Giulio Benetti <giulio.benetti@benettiengineering.com>
2026-08-16 23:52:24 +0200

    package/mongoose: security bump to version 7.23
    Release notes:
    https://github.com/cesanta/mongoose/releases/tag/7.23
    
    Fixes CVE-2026-73261, CVE-2026-73260, CVE-2026-63626, CVE-2026-73252,
    CVE-2026-73251.
    
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/mongoose/mongoose.mk
+++ package/mongoose/mongoose.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MONGOOSE_VERSION = 7.22
+MONGOOSE_VERSION = 7.23
 MONGOOSE_SITE = $(call github,cesanta,mongoose,$(MONGOOSE_VERSION))
 MONGOOSE_LICENSE = GPL-2.0
 MONGOOSE_LICENSE_FILES = LICENSE
