50f635f8dfc67a2a14e580d250d592ea40df39bc
Alexis Lothoré <alexis.lothore@bootlin.com>
2026-08-19 11:25:33 +0200

    package/python-scp: bump version to 0.16.1
    Changelog: https://github.com/jbardin/scp.py/commit/8f2a778cc6d2988b39c0523f633d27484e72f0e6
    
    Signed-off-by: Alexis Lothoré <alexis.lothore@bootlin.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-scp/python-scp.mk
+++ package/python-scp/python-scp.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SCP_VERSION = 0.16.0
+PYTHON_SCP_VERSION = 0.16.1
 PYTHON_SCP_SOURCE = scp-$(PYTHON_SCP_VERSION).tar.gz
-PYTHON_SCP_SITE = https://files.pythonhosted.org/packages/7f/e1/34634a89f100fcf4f014aca65621e701c5b9053cec62b6386866e3f015d5
+PYTHON_SCP_SITE = https://files.pythonhosted.org/packages/f8/35/fcef45d52d76a6dc70778c11eea719ff0ab550594bc51e55e66d28b5f424
 PYTHON_SCP_SETUP_TYPE = setuptools
 PYTHON_SCP_LICENSE = LGPL-2.1+
 PYTHON_SCP_LICENSE_FILES = LICENSE.txt
