2a99d1d195d1b2c170459e8d72295fdffecf2927
Stefan Müller <stemu86@gmx.ch>
2026-08-20 09:40:40 +0000

    package/wget: fix CVE-2026-58470
    Backport the upstream fix for integer overflows while parsing
    Content-Range headers, together with the follow-up fix using
    strtoll() for wgint values.
    
    Backport to: 2025.02.x
    
    Signed-off-by: Stefan Müller <stemu86@gmx.ch>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 89485adb296732e9e40e0ca7089381b3ca5486ce)
    Signed-off-by: Titouan Christophe <titouan.christophe@mind.be>

--- package/wget/wget.mk
+++ package/wget/wget.mk
@@ -17,6 +17,10 @@ WGET_CPE_ID_VENDOR = gnu
 # 0004-src-metalink.c-Include-ctype.h.patch
 WGET_IGNORE_CVES += CVE-2026-58469
 
+# 0005-src-http.c-parse_content_range-Fix-integer-overflow.patch
+# 0006-src-http.c-parse_content_range-Use-strtoll-instead-of-strtol.patch
+WGET_IGNORE_CVES += CVE-2026-58470
+
 ifeq ($(BR2_PACKAGE_LIBPSL),y)
 WGET_CONF_OPTS += --with-libpsl
 WGET_DEPENDENCIES += libpsl
