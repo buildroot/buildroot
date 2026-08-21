df9e3f9b30e1343a3642a6421600bdda1eea848d
Julien Olivain <ju.o@free.fr>
2026-08-21 18:48:32 +0200

    Revert "package/fakeroot: bump to version 2.1.4"
    This major fakeroot bump is introducing xattr issues on hosts with
    SELinux enabled (for example a Fedora 44 default installation).
    
    Trying to build defconfigs such as:
    
        make qemu_aarch64_virt_defconfig
        make
    
    produces error when building the filesystem image:
    
        >>>   Generating filesystem image rootfs.ext2
        ...
        mke2fs 1.47.4 (6-Mar-2025)
        ...
        Copying files into the device: set_inode_xattr: No data available while reading attribute "security.selinux" of /buildroot/output/build/buildroot-fs/ext2/target"
        populate_fs3: No data available while copying xattrs on root directory
        mkfs.ext4: No data available while populating file system
    
    This reverts commit 344d64f38539b28a5b8753d18ceb43ff2ee969bd.
    
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/fakeroot/fakeroot.mk
+++ package/fakeroot/fakeroot.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-FAKEROOT_VERSION = 2.1.4
-FAKEROOT_SOURCE = fakeroot_$(FAKEROOT_VERSION).orig.tar.xz
-FAKEROOT_SITE = https://snapshot.debian.org/archive/debian/20260711T202405Z/pool/main/f/fakeroot
+FAKEROOT_VERSION = 1.37.2
+FAKEROOT_SOURCE = fakeroot_$(FAKEROOT_VERSION).orig.tar.gz
+FAKEROOT_SITE = https://snapshot.debian.org/archive/debian/20260401T000000Z/pool/main/f/fakeroot
 
 HOST_FAKEROOT_DEPENDENCIES = host-acl
 # Force capabilities detection off
@@ -17,8 +17,5 @@ HOST_FAKEROOT_CONF_ENV = \
 	ac_cv_func_capset=no
 FAKEROOT_LICENSE = GPL-3.0+
 FAKEROOT_LICENSE_FILES = COPYING
-
-# source archive does not contain pre-generated configure script
-FAKEROOT_AUTORECONF = YES
 
 $(eval $(host-autotools-package))
