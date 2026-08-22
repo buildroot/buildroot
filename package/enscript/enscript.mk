8242797d31df0acb397be81e882d4f972ab847f7
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2026-08-22 23:13:10 +0200

    package/enscript: fix build issue with musl and gcc >= 15
    enscript currently fails to build with musl with gcc >= 15. In order
    to fix this, we need to bring a number of patches from upstream, and
    add 2 others that were submitted upstream.
    
    From upstream, we bring
    0002-Add-CFLAG-std-c89-so-it-compiles-with-the-old-standa.patch, which
    switches to -std=c89 to get the compiler back to "old" behavior.
    
    However, as this commit patches configure.ac, we need to autoreconf,
    but autoreconf is broken, so we also take
    0003-Automake-1.12-and-up-no-longer-supports-pre-ANSI.patch from
    upstream, which drops a problematic autoconf macro.
    
    However, once you drop this problematic autoconf macro, the PROTOTYPES
    define is never set by anything, causing the __P macro to no longer be
    defined properly. This is fixed by
    0004-Fix-prototype-detection-when-__STDC__-is-defined-but.patch that
    we have submitted upstream.
    
    Once you're there, you realize that switching to -std=c89 has the side
    effect that musl's <limits.h> no longer defines PATH_MAX, because it
    needs one of:
    
      #if defined(_POSIX_SOURCE) || defined(_POSIX_C_SOURCE) \
       || defined(_XOPEN_SOURCE) || defined(_GNU_SOURCE) || defined(_BSD_SOURCE)
    
    and a side effect of -std=c89 is that none of these is defined
    anymore. So we introduce 0005-Use-std-gnu89-instead-of-std-c89.patch,
    which switches to -std=gnu89. This patch has also been submitted
    upstream.
    
    With all of these efforts, we get a successful build on musl with gcc
    >= 15.
    
    This commit needs to be backported to Buildroot versions that support
    gcc 15.x, so that means the currently maintained 2026.x branches, but
    not 2025.02 as only up to gcc 14.x was supported then.
    
    Fixes:
    
      https://autobuild.buildroot.org/results/d39d14bbbb3a51d67fe962b877c7f66ff1204ecf/
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit edffc0bc50b329928f556b008c96bf57b583c510)
    Signed-off-by: Titouan Christophe <titouan.christophe@mind.be>

--- package/enscript/enscript.mk
+++ package/enscript/enscript.mk
@@ -9,6 +9,10 @@ ENSCRIPT_SITE = $(BR2_GNU_MIRROR)/enscript
 ENSCRIPT_LICENSE = GPL-3.0+
 ENSCRIPT_LICENSE_FILES = COPYING
 ENSCRIPT_CPE_ID_VENDOR = gnu
+# 0002-Add-CFLAG-std-c89-so-it-compiles-with-the-old-standa.patch
+# 0003-Automake-1.12-and-up-no-longer-supports-pre-ANSI.patch
+# 0005-Use-std-gnu89-instead-of-std-c89.patch
+ENSCRIPT_AUTORECONF = YES
 
 # Enable pthread threads if toolchain supports threads
 ifeq ($(BR2_TOOLCHAIN_HAS_THREADS),y)
