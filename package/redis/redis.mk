5b3425a850a4e4daf74ab86e23dc36cb419ce4ff
Titouan Christophe <titouan.christophe@mind.be>
2026-08-24 12:17:02 +0200

    package/redis: security bump to v7.2.16
    See the release notes:
    https://github.com/redis/redis/blob/7.2.16/00-RELEASENOTES
    
    This fixes a few security issues (without CVE number)
    
    Signed-off-by: Titouan Christophe <titouan.christophe@mind.be>

--- package/redis/redis.mk
+++ package/redis/redis.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-REDIS_VERSION = 7.2.15
+REDIS_VERSION = 7.2.16
 REDIS_SITE = https://download.redis.io/releases
 REDIS_LICENSE = BSD-3-Clause (core); MIT and BSD family licenses (Bundled components)
 REDIS_LICENSE_FILES = COPYING
