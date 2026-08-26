ec1773483c2b3c865ac3d1e72be829b50214e389
Titouan Christophe <titouan.christophe@mind.be>
2026-08-26 09:37:16 +0200

    package/{avro-c, python-avro}: security bump to v1.12.2
    This release includes a broad round of hardening against malformed and
    adversarial input across the Python SDK (bounding allocations and enforcing
    decompression limits before trusting size fields read from the input).
    
    See the release notes https://avro.apache.org/blog/2026/08/12/avro-1.12.2/
    
    Also update the download url, because www-eu.apache.org/dist/...
    is a redirection to downloads.apache.org/...
    
    Signed-off-by: Titouan Christophe <titouan.christophe@mind.be>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 02d8a41f0958f452570952f333cdf884f24f5723)
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

--- package/python-avro/python-avro.mk
+++ package/python-avro/python-avro.mk
@@ -5,9 +5,9 @@
 ################################################################################
 
 # When updating the version, please also update avro-c
-PYTHON_AVRO_VERSION = 1.12.1
+PYTHON_AVRO_VERSION = 1.12.2
 PYTHON_AVRO_SOURCE = avro-$(PYTHON_AVRO_VERSION).tar.gz
-PYTHON_AVRO_SITE = https://files.pythonhosted.org/packages/60/00/af1eec633637e12d0945a97f05a429eed83ac45865af60cb453db4689d95
+PYTHON_AVRO_SITE = https://files.pythonhosted.org/packages/5f/68/973b9c682aa2c3cf2b05fc4c961af11b9be1d9b46604f65aed23cd4fd1e6
 PYTHON_AVRO_LICENSE = Apache-2.0
 PYTHON_AVRO_LICENSE_FILES = avro/LICENSE
 PYTHON_AVRO_SETUP_TYPE = setuptools
