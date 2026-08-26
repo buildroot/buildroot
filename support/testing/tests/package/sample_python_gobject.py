13bccb86cc0782a1314627bb84a692c9e0c80a44
Romain Naour <romain.naour@smile.fr>
2026-08-26 17:27:30 +0200

    support/testing: TestPythonPy3Gobject: test glib 2.88 regression
    In Glib >= 2.88, GLib.unix_signal_add has been moved to a separate
    platform-specific library. This break backward compatibility from
    GLib-2.0. A workaround has been applied to pygobject >= 3.55.3
    https://gitlab.gnome.org/GNOME/pygobject/-/commit/74e4e0f40a66436a55c72d4a6e6058c1f91a7310
    
    This issue currently break TestFirewalldSysVInit and
    TestFirewalldSystemd runtime tests since the bump to glib 2.88.3 [1]:
    
    https://gitlab.com/buildroot.org/buildroot/-/jobs/16060152329 (TestFirewalldSysVInit)
    https://gitlab.com/buildroot.org/buildroot/-/jobs/16060152332 (TestFirewalldSystemd)
    
    Break the test TestPythonPy3Gobject now in order to reproduce the same
    issue than for Firewalld test.
    
    [1] e313a2d2590af689eb1f79e55b5a488154400da1
    
    Signed-off-by: Romain Naour <romain.naour@smile.fr>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 3bd3d5004df09ccb13e54d7fcb2719c629602c40)
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

--- support/testing/tests/package/sample_python_gobject.py
+++ support/testing/tests/package/sample_python_gobject.py
@@ -4,6 +4,12 @@ from gi.repository import GLib
 
 
 def main():
+    # In Glib >= 2.88, GLib.unix_signal_add has been moved to a
+    # separate platform-specific library. This break backward
+    # compatibility from GLib-2.0.
+    # A workaround has been applied to pygobject >= 3.55.3
+    # https://gitlab.gnome.org/GNOME/pygobject/-/commit/74e4e0f40a66436a55c72d4a6e6058c1f91a7310
+    unix_signal_add = GLib.unix_signal_add_full  # noqa
     sh_path = GLib.find_program_in_path('sh')
     if sh_path == "/bin/sh":
         return True
