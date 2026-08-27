cc25888c889c82c15829c16f6ad0974d03f5ab5b
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2026-08-27 11:55:17 +0200

    package/bpftrace: bump to version 0.26.1 to fix build with LLVM 22
    Since Buildroot commit 25cb3813e7fc6ed3e455f983cf0e09c7bc02fccb which
    bumped LLVM from 21.x to 22.x, the build of bpftrace is broken as
    bpftrace 0.24.2 only supports LLVM up to 21.
    
    We tried backporting the bpftrace patch that allows using LLVM up to
    version 22 but the patch didn't apply cleanly but more importantly it
    wasn't clear if this patch was sufficient. Therefore, we opt for
    bumping bpftrace entirely to fix the issue.
    
    Packaging changes:
    
    - The new version of bpftrace no longer needs host-bison/host-flex,
      because bpftrace is now using a handwritten parser.
    
    - Pass -DUSE_SYSTEM_LIBBPF:BOOL=ON to ensure the system libbpf version
      is used, and not the bundled version
    
    - Now depends on kernel headers >= 5.10 because it needs CAP_BPF and
      CAP_PERFMON
    
    Upstream changelog:
    https://github.com/bpftrace/bpftrace/blob/v0.26.1/CHANGELOG.md
    
    Fixes:
    
      https://autobuild.buildroot.net/results/a3e3fd696685864977c688aa11c2653357cf6207/
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    [Julien: add link to upstream changelog]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/bpftrace/bpftrace.mk
+++ package/bpftrace/bpftrace.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BPFTRACE_VERSION = 0.24.2
+BPFTRACE_VERSION = 0.26.1
 BPFTRACE_SITE = $(call github,bpftrace,bpftrace,v$(BPFTRACE_VERSION))
 BPFTRACE_LICENSE = Apache-2.0
 BPFTRACE_LICENSE_FILES = LICENSE
@@ -13,8 +13,6 @@ BPFTRACE_DEPENDENCIES = \
 	bzip2 \
 	cereal \
 	elfutils \
-	host-bison \
-	host-flex \
 	host-vim \
 	libbpf \
 	llvm \
@@ -29,6 +27,7 @@ BPFTRACE_CONF_OPTS += \
 	-DBUILD_SHARED_LIBS:BOOL=OFF \
 	-DBUILD_TESTING:BOOL=OFF \
 	-DCMAKE_CXX_FLAGS="$(TARGET_CXXFLAGS) -I$(STAGING_DIR)/usr/include/bpf" \
-	-DENABLE_MAN:BOOL=OFF
+	-DENABLE_MAN:BOOL=OFF \
+	-DUSE_SYSTEM_LIBBPF:BOOL=ON
 
 $(eval $(cmake-package))
