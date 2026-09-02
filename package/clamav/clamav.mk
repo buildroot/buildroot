749384f459099f1d83ce6b928cd3e53a504d5804
Titouan Christophe <titouan.christophe@mind.be>
2026-09-02 15:16:26 +0200

    package/clamav: bump to v1.4.3
    As indicated on ClamAV End of Life policy webpage [1], the 1.0 series is the
    previous LTS (and therefore unmaintained). The ClamAV 1.4 series is the current
    LTS version, maintained for 3 years.
    
    However, there is a breaking build-system change in ClamAV 1.4.4 [2]:
        "The minimum Rust version for ClamAV 1.4.4 is now 1.87.0."
    
    Unfortunately, Buildroot LTS 2025.02.x is still on Rust v1.82, and therefore
    building ClamAV>=1.4.4 fails because of missing features for Rust. Therefore,
    update to the latest upstream LTS version that can be built with Buildroot LTS.
    
    Release notes:
    https://blog.clamav.net/2024/08/clamav-140-feature-release-and-clamav.html
    https://blog.clamav.net/2024/09/clamav-141-132-107-and-010312-security.html
    https://blog.clamav.net/2025/01/clamav-142-and-108-security-patch.html
    https://blog.clamav.net/2025/06/clamav-143-and-109-security-patch.html
    
    Note: release notes of clamav version 1.4.1, 1.4.2 and 1.4.3 are
    marked upstream as security maintenance releases. This Buildroot
    commit is not marked as security fix because those security patches
    were already applied in clamav 1.0.7, 1.0.8, and 1.0.9, and we are
    precisely update from 1.0.9.
    
    [1] https://docs.clamav.net/faq/faq-eol.html
    [2] https://github.com/Cisco-Talos/clamav/releases/tag/clamav-1.4.4
    
    Signed-off-by: Titouan Christophe <titouan.christophe@mind.be>
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

--- package/clamav/clamav.mk
+++ package/clamav/clamav.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CLAMAV_VERSION = 1.0.9
+CLAMAV_VERSION = 1.4.3
 CLAMAV_SITE = https://www.clamav.net/downloads/production
 CLAMAV_LICENSE = GPL-2.0
 CLAMAV_LICENSE_FILES = \
