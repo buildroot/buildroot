87229b381a6b14171f265811da252f6b883eae8f
Francois Perrad <francois.perrad.86@gmail.com>
2026-09-04 20:59:09 +0200

    package/lsqlite3: bump to version 0.9.7
    Signed-off-by: Francois Perrad <francois.perrad.86@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/lsqlite3/lsqlite3.mk
+++ package/lsqlite3/lsqlite3.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-LSQLITE3_VERSION = 0.9.5-1
-LSQLITE3_SUBDIR = lsqlite3_fsl09y
+LSQLITE3_VERSION = 0.9.7-1
+LSQLITE3_SUBDIR = lsqlite3_v097
 LSQLITE3_LICENSE = MIT
 LSQLITE3_DEPENDENCIES = sqlite
 
