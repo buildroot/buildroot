196f186837ba5a2a0ac622b1b10875ded18230c7
Francois Perrad <francois.perrad.86@gmail.com>
2026-09-04 20:59:15 +0200

    package/luabitop: bump to version 1.0.3
    Signed-off-by: Francois Perrad <francois.perrad.86@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/luabitop/luabitop.mk
+++ package/luabitop/luabitop.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LUABITOP_VERSION = 1.0.2-1
+LUABITOP_VERSION = 1.0.3-1
 LUABITOP_NAME_UPSTREAM = LuaBitOp
 LUABITOP_LICENSE = MIT
 LUABITOP_LICENSE_FILES = $(LUABITOP_SUBDIR)/README
