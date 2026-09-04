ee5c69065eb3152b45e6d9f41550af23678b4eb9
Bernd Kuhls <bernd@kuhls.net>
2026-09-04 19:10:31 +0200

    package/sentry-cli: bump version to 3.7.0
    https://github.com/getsentry/sentry-cli/blob/3.7.0/CHANGELOG.md
    
    Removed patch which is not needed anymore.
    
    Updated license hash due to upstream commits:
    
    copyright year bump:
    https://github.com/getsentry/sentry-cli/commit/734fa8cddc8308133d6f807cec01a9e5e668aba8
    
    switch to FSL-1.1-MIT license:
    https://github.com/getsentry/sentry-cli/commit/3ed4ada174945c83702350ceb83944651f1d8ff2
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/sentry-cli/sentry-cli.mk
+++ package/sentry-cli/sentry-cli.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-SENTRY_CLI_VERSION = 2.20.3
+SENTRY_CLI_VERSION = 3.7.0
 SENTRY_CLI_SITE = $(call github,getsentry,sentry-cli,$(SENTRY_CLI_VERSION))
-SENTRY_CLI_LICENSE = BSD-3-clause
+SENTRY_CLI_LICENSE = FSL-1.1-MIT
 SENTRY_CLI_LICENSE_FILES = LICENSE
 
 HOST_SENTRY_CLI_DEPENDENCIES = host-pkgconf host-openssl host-zlib
