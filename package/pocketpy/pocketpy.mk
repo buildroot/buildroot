e8ee9feba137fba7cabfbac56e4f8bab89762c99
Dario Binacchi <dario.binacchi@amarulasolutions.com>
2026-09-07 14:24:23 +0200

    package/pocketpy: bump to version 2.2.0
    Release notes:
    https://github.com/pocketpy/pocketpy/releases/tag/v2.2.0
    
    Signed-off-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/pocketpy/pocketpy.mk
+++ package/pocketpy/pocketpy.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-POCKETPY_VERSION = 2.1.8
+POCKETPY_VERSION = 2.2.0
 POCKETPY_SITE = $(call github,pocketpy,pocketpy,v$(POCKETPY_VERSION))
 POCKETPY_LICENSE = MIT
 POCKETPY_LICENSE_FILES = LICENSE
