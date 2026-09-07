6fec797ebb79c50a496fcba6820935823aea4daf
Bernd Kuhls <bernd@kuhls.net>
2026-09-07 14:02:57 +0200

    package/xmlstarlet: bump version to 1.7.0
    Upstream site switched to Github:
    https://sourceforge.net/p/xmlstar/feature-requests/50/
    
    Release notes:
    https://github.com/xmlstarlet/xmlstarlet/releases/tag/1.7.0
    
    Removed patches which are included in this release.
    Instead of patch 0002 a different fix was committed upstream:
    https://github.com/xmlstarlet/xmlstarlet/commit/f300cd048d22ed5291f885346c62779b92c4aca4
    
    Switched to pkgconf to detect libxml2/libxslt expect for static builds:
    https://github.com/xmlstarlet/xmlstarlet/commit/27063aa63c96541fe6db335aa36e5fa29bc5f0e9
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/xmlstarlet/xmlstarlet.mk
+++ package/xmlstarlet/xmlstarlet.mk
@@ -4,31 +4,25 @@
 #
 ################################################################################
 
-XMLSTARLET_VERSION = 1.6.1
-XMLSTARLET_SITE = http://downloads.sourceforge.net/project/xmlstar/xmlstarlet/$(XMLSTARLET_VERSION)
+XMLSTARLET_VERSION = 1.7.0
+XMLSTARLET_SITE = https://github.com/xmlstarlet/xmlstarlet/releases/download/$(XMLSTARLET_VERSION)
 XMLSTARLET_LICENSE = MIT
 XMLSTARLET_LICENSE_FILES = COPYING
 
-XMLSTARLET_DEPENDENCIES += libxml2 libxslt \
+XMLSTARLET_DEPENDENCIES += host-pkgconf libxml2 libxslt \
 	$(if $(BR2_PACKAGE_LIBICONV),libiconv)
 
+ifeq ($(BR2_STATIC_LIBS),y)
 XMLSTARLET_CONF_OPTS += \
+	--enable-static-libs \
 	--with-libxml-prefix=$(STAGING_DIR)/usr \
 	--with-libxslt-prefix=$(STAGING_DIR)/usr \
 	--with-libiconv-prefix=$(STAGING_DIR)/usr
-
-ifeq ($(BR2_STATIC_LIBS),y)
-XMLSTARLET_CONF_OPTS += --enable-static-libs
-XMLSTARLET_CONF_ENV = LIBS="`$(PKG_CONFIG_HOST_BINARY) --libs libxml-2.0 libexslt`"
 else
 XMLSTARLET_CONF_OPTS += --disable-static-libs
 endif
 
-HOST_XMLSTARLET_DEPENDENCIES += host-libxml2 host-libxslt
-
-HOST_XMLSTARLET_CONF_OPTS += \
-	--with-libxml-prefix=$(HOST_DIR) \
-	--with-libxslt-prefix=$(HOST_DIR)
+HOST_XMLSTARLET_DEPENDENCIES += host-libxml2 host-libxslt host-pkgconf
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
