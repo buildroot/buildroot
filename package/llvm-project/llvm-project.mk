5f79161e4a8756acdb25c8500de9b2dee663f2ac
Bernd Kuhls <bernd@kuhls.net>
2026-09-07 17:53:44 +0200

    package/llvm-project: bump version to 23.1.0
    https://discourse.llvm.org/t/llvm-23-1-0-released/91654
    
    Removed compiler-rt patch which is included in this release.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/llvm-project/llvm-project.mk
+++ package/llvm-project/llvm-project.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-LLVM_PROJECT_VERSION_MAJOR = 22
-LLVM_PROJECT_VERSION = $(LLVM_PROJECT_VERSION_MAJOR).1.8
+LLVM_PROJECT_VERSION_MAJOR = 23
+LLVM_PROJECT_VERSION = $(LLVM_PROJECT_VERSION_MAJOR).1.0
 LLVM_PROJECT_SITE = https://github.com/llvm/llvm-project/releases/download/llvmorg-$(LLVM_PROJECT_VERSION)
 LLVM_PROJECT_SOURCE = llvm-project-$(LLVM_VERSION).src.tar.xz
 
