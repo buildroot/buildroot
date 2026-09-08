74fe3559d6d415d30833596afdb23586c95876e4
Michael Fischer <mf@go-sys.de>
2026-09-08 11:07:57 +0200

    package/sdl3_image: security bump to version 3.4.6
    For release notes, see:
    https://github.com/libsdl-org/SDL_image/releases
    
    3.1.1 was a preview release. Among the changes since, it fixes the XCF
    loader issue reported as CVE-2026-35444 (fixed in 3.4.2). See:
    https://github.com/libsdl-org/SDL_image/releases/tag/release-3.4.2
    
    The LICENSE.txt hash changes because the copyright year was updated
    upstream. The license itself is unchanged (Zlib).
    
    Signed-off-by: Michael Fischer <mf@go-sys.de>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/sdl3_image/sdl3_image.mk
+++ package/sdl3_image/sdl3_image.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SDL3_IMAGE_VERSION = 3.1.1
+SDL3_IMAGE_VERSION = 3.4.6
 SDL3_IMAGE_SITE = https://www.libsdl.org/projects/SDL_image/release
 SDL3_IMAGE_SOURCE = SDL3_image-$(SDL3_IMAGE_VERSION).tar.gz
 SDL3_IMAGE_LICENSE = Zlib
