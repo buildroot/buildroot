6dc29e431fb4105692cce08ed48eba2fe4a6e03e
Alexander Shirokov <shirokovalexs@gmail.com>
2026-09-08 10:07:50 +0200

    package/zellij: bump to version 0.45.1
    Changelog: https://github.com/zellij-org/zellij/blob/v0.45.1/CHANGELOG.md
    
    Signed-off-by: Alexander Shirokov <shirokovalexs@gmail.com>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/zellij/zellij.mk
+++ package/zellij/zellij.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ZELLIJ_VERSION = 0.45.0
+ZELLIJ_VERSION = 0.45.1
 ZELLIJ_SITE = $(call github,zellij-org,zellij,v$(ZELLIJ_VERSION))
 ZELLIJ_LICENSE = MIT
 ZELLIJ_LICENSE_FILES = LICENSE.md
