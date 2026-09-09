edd3a015cd90441291d2b5513c71e2eabf682e24
Bernd Kuhls <bernd@kuhls.net>
2026-09-09 10:13:43 +0200

    package/lttng-tools: bump version to 2.16.0
    https://git.lttng.org/?p=lttng-tools.git;a=blob_plain;f=ChangeLog;hb=72dcc536523727343cb23e2a0fefd41044511d16
    
    Added Upstream: tag to patch 0001.
    
    Removed patch 0002 which is included in this release.
    
    Added new patch 0002 to fix a build error on musl introduced by this
    bump due to upstream commit:
    https://github.com/lttng/lttng-tools/commit/968475a48be5a87139b19fddbb70b9225c5375bd
    
    Updated license file paths according to upstream commit
    https://git.lttng.org/?p=lttng-tools.git;a=history;f=LICENSES;hb=refs/heads/stable-2.16
    
    Updated license hashes due to upstream commit
    https://git.lttng.org/?p=lttng-tools.git;a=commit;h=c09078e13a4ede575724009520c37a8b8d3e68d6
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/lttng-tools/lttng-tools.mk
+++ package/lttng-tools/lttng-tools.mk
@@ -4,12 +4,18 @@
 #
 ################################################################################
 
-LTTNG_TOOLS_VERSION = 2.14.0
+LTTNG_TOOLS_VERSION = 2.16.0
 LTTNG_TOOLS_SITE = https://lttng.org/files/lttng-tools
 LTTNG_TOOLS_SOURCE = lttng-tools-$(LTTNG_TOOLS_VERSION).tar.bz2
 LTTNG_TOOLS_INSTALL_STAGING = YES
 LTTNG_TOOLS_LICENSE = GPL-2.0, LGPL-2.1 (include/lttng/*, src/lib/lttng-ctl/*)
-LTTNG_TOOLS_LICENSE_FILES = LICENSE $(addprefix LICENSES/,BSD-2-Clause BSD-3-Clause GPL-2.0 LGPL-2.1 MIT)
+LTTNG_TOOLS_LICENSE_FILES = \
+	LICENSE \
+	LICENSES/BSD-2-Clause.txt \
+	LICENSES/BSD-3-Clause.txt \
+	LICENSES/GPL-2.0-only.txt \
+	LICENSES/LGPL-2.1-only.txt \
+	LICENSES/MIT.txt
 LTTNG_TOOLS_DEPENDENCIES = liburcu libxml2 popt util-linux
 # We're patching configure.ac
 LTTNG_TOOLS_AUTORECONF = YES
