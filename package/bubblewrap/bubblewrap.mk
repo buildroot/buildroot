110d3ecb810b4a30d8b77ab3936c6de8affa49fe
Adrian Perez de Castro <aperez@igalia.com>
2026-09-10 11:52:19 +0200

    package/bubblewrap: security bump to version 0.12.0
    Fixes a sandbox escape through symlink traversal tracked in
    CVE-2026-87766, which affects all previous versions.
    
    Using the bwrap binary with the setuid bit set is no longer supported
    and user namespaces are now always required, so a kernel config fixup
    is applied.
    
    A new build option allows indicating the minimum kernel version that
    will be used, which removes code used for backwards compatibility with
    kernels older than 5.6.0 when a newer version is specified. Passing
    $(LINUX_VERSION_PROBED) seems reasonable here.
    
    This version also changed the license from LGPL-2.0+ to LGPL-2.1+,
    hence the updated hash.
    
    Release notes:
    
      https://github.com/containers/bubblewrap/releases/tag/v0.12.0
    
    Signed-off-by: Adrian Perez de Castro <aperez@igalia.com>
    [Julien: fix _LINUX_CONFIG_FIXUPS by adding the missing "_LINUX"]
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit 4cb6193d2eca39b1bd79ff4867c842f507f692b8)
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- package/bubblewrap/bubblewrap.mk
+++ package/bubblewrap/bubblewrap.mk
@@ -4,21 +4,24 @@
 #
 ################################################################################
 
-BUBBLEWRAP_VERSION = 0.11.2
+BUBBLEWRAP_VERSION = 0.12.0
 BUBBLEWRAP_SITE = https://github.com/containers/bubblewrap/releases/download/v$(BUBBLEWRAP_VERSION)
 BUBBLEWRAP_SOURCE = bubblewrap-$(BUBBLEWRAP_VERSION).tar.xz
 BUBBLEWRAP_DEPENDENCIES = host-pkgconf libcap
 
-BUBBLEWRAP_LICENSE = LGPL-2.0+
+BUBBLEWRAP_LICENSE = LGPL-2.1+
 BUBBLEWRAP_LICENSE_FILES = COPYING
 BUBBLEWRAP_CPE_ID_VENDOR = projectatomic
 
+define BUBBLEWRAP_LINUX_CONFIG_FIXUPS
+	$(call KCONFIG_ENABLE_OPT,CONFIG_USER_NS)
+endef
+
 BUBBLEWRAP_CONF_OPTS = \
+	-Dassume_kernel=$(LINUX_VERSION_PROBED) \
 	-Dzsh_completion=disabled \
 	-Dman=disabled \
 	-Dpython=$(HOST_DIR)/bin/python \
-	-Drequire_userns=false \
-	-Dsupport_setuid=true \
 	-Dtests=false
 
 ifeq ($(BR2_PACKAGE_BASH_COMPLETION),y)
@@ -35,11 +38,5 @@ BUBBLEWRAP_DEPENDENCIES += libselinux
 else
 BUBBLEWRAP_CONF_OPTS += -Dselinux=disabled
 endif
-
-# We need to mark bwrap as setuid, in case the kernel
-# has user namespaces disabled for non-root users.
-define BUBBLEWRAP_PERMISSIONS
-	/usr/bin/bwrap f 1755 0 0 - - - - -
-endef
 
 $(eval $(meson-package))
