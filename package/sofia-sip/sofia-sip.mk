4c96ea868e3191e9bea84554b800d62678e65d66
Bernd Kuhls <bernd@kuhls.net>
2026-09-17 02:01:52 +0200

    package/sofia-sip: needs OpenSSL
    Buildroot commit bb254e2304274dc308e3d8699e92ea77e4189fb7 bumped the
    package to version 1.13.18 which includes upstream commit
    https://github.com/freeswitch/sofia-sip/commit/8081a1a6d05f174cb902664f73182c32fd2eee0f
    that added the unconditional usage of OpenSSL.
    
    Tested with both LibreSSL and OpenSSL, the latter with all suboptions
    disabled.
    
    Fixes:
    https://autobuild.buildroot.net/results/afe/afe7b327f12db86e4dc31ecc25b576bff5c4a0bb/
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit ea18394dd71f2deb21792918500b1928e96177b0)
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

--- package/sofia-sip/sofia-sip.mk
+++ package/sofia-sip/sofia-sip.mk
@@ -9,28 +9,20 @@ SOFIA_SIP_SITE = $(call github,freeswitch,sofia-sip,v$(SOFIA_SIP_VERSION))
 SOFIA_SIP_INSTALL_STAGING = YES
 # Fetched from github, no pre-generated configure script provided
 SOFIA_SIP_AUTORECONF = YES
-SOFIA_SIP_DEPENDENCIES = host-pkgconf
+SOFIA_SIP_DEPENDENCIES = host-pkgconf openssl
 SOFIA_SIP_LICENSE = LGPL-2.1+
 SOFIA_SIP_LICENSE_FILES = COPYING COPYRIGHTS
 SOFIA_SIP_CPE_ID_VENDOR = signalwire
-SOFIA_SIP_CONF_OPTS = --with-doxygen=no
+SOFIA_SIP_CONF_OPTS = \
+	--with-doxygen=no \
+	--enable-nth \
+	--with-openssl=pkg-config
 
 ifeq ($(BR2_PACKAGE_LIBGLIB2),y)
 SOFIA_SIP_CONF_OPTS += --with-glib
 SOFIA_SIP_DEPENDENCIES += libglib2
 else
 SOFIA_SIP_CONF_OPTS += --without-glib
-endif
-
-ifeq ($(BR2_PACKAGE_OPENSSL),y)
-SOFIA_SIP_CONF_OPTS += \
-	--enable-nth \
-	--with-openssl=pkg-config
-SOFIA_SIP_DEPENDENCIES += openssl
-else
-SOFIA_SIP_CONF_OPTS += \
-	--disable-nth \
-	--without-openssl
 endif
 
 $(eval $(autotools-package))
