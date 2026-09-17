5a91b41f08d9b7758b8fdc2cc04a377c7a2d288b
Manuel Diener <manuel.diener@oss.othermo.de>
2026-09-17 10:14:11 +0200

    package/python-annotated-doc: bump to 0.0.5
    See the release notes here:
    https://github.com/fastapi/annotated-doc/releases/tag/0.0.5
    
    Signed-off-by: Manuel Diener <manuel.diener@oss.othermo.de>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/python-annotated-doc/python-annotated-doc.mk
+++ package/python-annotated-doc/python-annotated-doc.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ANNOTATED_DOC_VERSION = 0.0.4
+PYTHON_ANNOTATED_DOC_VERSION = 0.0.5
 PYTHON_ANNOTATED_DOC_SOURCE = annotated_doc-$(PYTHON_ANNOTATED_DOC_VERSION).tar.gz
-PYTHON_ANNOTATED_DOC_SITE = https://files.pythonhosted.org/packages/57/ba/046ceea27344560984e26a590f90bc7f4a75b06701f653222458922b558c
+PYTHON_ANNOTATED_DOC_SITE = https://files.pythonhosted.org/packages/5a/8e/38aa427ed5402449e226975b649c5dc73ccadfefeb95e6aecb8f8ea4b6b6
 PYTHON_ANNOTATED_DOC_SETUP_TYPE = pep517
 PYTHON_ANNOTATED_DOC_LICENSE = MIT
 PYTHON_ANNOTATED_DOC_LICENSE_FILES = LICENSE
