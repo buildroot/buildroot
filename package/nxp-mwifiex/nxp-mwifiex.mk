0e27b1d8a47c1c9b36e7c5761299495f9a7e8c19
Antoine Gennart <antoine.gennart@quimesis.be>
2026-09-17 11:38:47 +0200

    package/nxp-mwifiex: bump version to lf-6.18.20_2.0.0
    The previous version fails to build with Linux 6.13 and newer because
    mlinux/moal_main.h includes the removed net/lib80211.h header.
    
    The header was removed by Linux commit 02f220b52670 ("wifi:
    ipw2x00/lib80211: move remaining lib80211 into libipw").
    
    The new NXP release skips this obsolete header for kernels newer
    than 6.12.12 and includes the corresponding Linux 6.13 cfg80211 API
    compatibility fixes.
    
    Signed-off-by: Antoine Gennart <antoine.gennart@quimesis.be>
    [Julien: fix _VERSION to use a tag rather than a branch]
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/nxp-mwifiex/nxp-mwifiex.mk
+++ package/nxp-mwifiex/nxp-mwifiex.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NXP_MWIFIEX_VERSION = lf-6.6.52_2.2.0
+NXP_MWIFIEX_VERSION = lf-6.18.20-2.0.0
 NXP_MWIFIEX_SITE = $(call github,nxp-imx,mwifiex,$(NXP_MWIFIEX_VERSION))
 NXP_MWIFIEX_LICENSE = GPL-2.0
 NXP_MWIFIEX_LICENSE_FILES = LICENSE
