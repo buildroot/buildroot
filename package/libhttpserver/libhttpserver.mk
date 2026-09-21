f18c4dbdcee9e4b42406a3ef96dc2a38e947d810
Stephan Hoffmann <sho@relinux.de>
2026-09-21 18:54:37 +0200

    package/libhttpserver: bump to version 2.0.0
    Version 2.0.0 requires C++20 and GCC 11 or newer.
    
    This major release changes the API exposed to applications. However,
    the 0.x release series is end-of-life, so staying on version 0.19.0 is
    not a viable option.
    
    The build system detects GnuTLS through its headers and links to it
    directly. Make this detection deterministic by following the
    libmicrohttpd SSL option and adding the corresponding dependency.
    
    https://github.com/etr/libhttpserver/releases/tag/2.0.0
    
    Signed-off-by: Stephan Hoffmann <sho@relinux.de>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/libhttpserver/libhttpserver.mk
+++ package/libhttpserver/libhttpserver.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBHTTPSERVER_VERSION = 0.19.0
+LIBHTTPSERVER_VERSION = 2.0.0
 LIBHTTPSERVER_SITE = $(call github,etr,libhttpserver,$(LIBHTTPSERVER_VERSION))
 LIBHTTPSERVER_LICENSE = LGPL-2.1+
 LIBHTTPSERVER_LICENSE_FILES = COPYING.LESSER
@@ -14,5 +14,11 @@ LIBHTTPSERVER_CONF_OPTS = \
 	--enable-same-directory-build
 LIBHTTPSERVER_AUTORECONF = YES
 LIBHTTPSERVER_DEPENDENCIES = libmicrohttpd
+
+ifeq ($(BR2_PACKAGE_LIBMICROHTTPD_SSL),y)
+LIBHTTPSERVER_DEPENDENCIES += gnutls
+else
+LIBHTTPSERVER_CONF_ENV += ac_cv_header_gnutls_gnutls_h=no
+endif
 
 $(eval $(autotools-package))
