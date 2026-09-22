b0511042e8921a4cc9fb01ced9ae65ba31bca8d5
Bernd Kuhls <bernd@kuhls.net>
2026-09-22 08:31:06 +0200

    package/bpftrace: bump version to 0.27.0
    https://github.com/bpftrace/bpftrace/blob/v0.27.0/CHANGELOG.md
    
    This release includes
    https://github.com/bpftrace/bpftrace/commit/d7a3bb54a3d5b5682cce92fb326aa01be2092d47
    which adds support for LLVM 23.
    
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/bpftrace/bpftrace.mk
+++ package/bpftrace/bpftrace.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BPFTRACE_VERSION = 0.26.1
+BPFTRACE_VERSION = 0.27.0
 BPFTRACE_SITE = $(call github,bpftrace,bpftrace,v$(BPFTRACE_VERSION))
 BPFTRACE_LICENSE = Apache-2.0
 BPFTRACE_LICENSE_FILES = LICENSE
