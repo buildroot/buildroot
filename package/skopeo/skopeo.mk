a489e7c212b1e8b6d5ccc23bfa0b9c8e1a4621f4
Yann E. MORIN <yann.morin@orange.com>
2026-09-22 07:07:28 +0200

    package/skopeo: use new upstream location
    The upstream location has changed (with a forward from the old one, so
    we did not notice earlier) [0] [1], with 1.23.0 the first release being
    made from the new location, which was accounted for in 96aac440dd79
    (package/skopeo: bump version to 1.23.0) as it required the change of
    the gomod, but where the new location was missed.
    
    Eventually switch to the new location now.
    
    [0] https://github.com/podman-container-tools/skopeo/pull/2854
    [1] https://github.com/podman-container-tools/go.podman.io/pull/6
    
    Reported-by: Julien Olivain <ju.o@free.fr>
    Signed-off-by: Yann E. MORIN <yann.morin@orange.com>
    Signed-off-by: Fiona Klute <fiona.klute@gmx.de>

--- package/skopeo/skopeo.mk
+++ package/skopeo/skopeo.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 SKOPEO_VERSION = 1.24.1
-SKOPEO_SITE = $(call github,containers,skopeo,v$(SKOPEO_VERSION))
+SKOPEO_SITE = $(call github,podman-container-tools,skopeo,v$(SKOPEO_VERSION))
 
 SKOPEO_LICENSE = Apache-2.0
 SKOPEO_LICENSE_FILES = LICENSE
