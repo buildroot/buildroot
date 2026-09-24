a962be2eea146e06661712159698bc7cbf3305f2
Titouan Christophe <titouan.christophe@mind.be>
2026-09-24 11:08:12 +0200

    package/xen: bump to version 4.22.0
    See the release notes:
    https://wiki.xenproject.org/wiki/Xen_Project_4.22_Release_Notes?ref=xenproject.org
    
    Signed-off-by: Titouan Christophe <titouan.christophe@mind.be>
    Signed-off-by: Julien Olivain <ju.o@free.fr>

--- package/xen/xen.mk
+++ package/xen/xen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XEN_VERSION = 4.21.1
+XEN_VERSION = 4.22.0
 XEN_SITE = https://downloads.xenproject.org/release/xen/$(XEN_VERSION)
 XEN_SELINUX_MODULES = systemd udev xen
 XEN_LICENSE = GPL-2.0
